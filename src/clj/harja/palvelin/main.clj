149d7cb7d6dfd3e3cb86c376e2a776b8f5ccc777
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-02-16 13:29:00 +0200

    Lisätty q funktio ad hoc queryille REPListä.

--- src/clj/harja/palvelin/main.clj
+++ src/clj/harja/palvelin/main.clj
@@ -83,3 +83,25 @@
   [nimi fn]
   (http-palvelin/poista-palvelu (:http-palvelin harja-jarjestelma) nimi)
   (http-palvelin/julkaise-palvelu (:http-palvelin harja-jarjestelma) nimi fn))
+
+(defn q
+  "Kysele Harjan kannasta, REPL kehitystä varten"
+  [& sql]
+  (with-open [c (.getConnection (:datasource (:db harja-jarjestelma)))
+              ps (.prepareStatement c (reduce str sql))
+              rs (.executeQuery ps)]
+    (let [cols (-> (.getMetaData rs) .getColumnCount)]
+      (loop [res []
+             more? (.next rs)]
+        (if-not more?
+          res
+          (recur (conj res (loop [row []
+                                  i 1]
+                             (if (<= i cols)
+                               (recur (conj row (.getObject rs i)) (inc i))
+                               row)))
+                 (.next rs)))))))
+        
+    ]
+    (.
+    
