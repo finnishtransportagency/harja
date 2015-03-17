b288216e5412b74c7239079b053cc97bc32ce1c5
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2015-03-17 15:48:20 +0200

    Siirrä hoitokauden ja sopimuksen tila suunnittelun yläsivulle

--- src/cljs/harja/views/urakka/suunnittelu.cljs
+++ src/cljs/harja/views/urakka/suunnittelu.cljs
@@ -1,25 +1,100 @@
 (ns harja.views.urakka.suunnittelu
   "Päätason sivu Hallinta, josta kaikkeen ylläpitötyöhön pääsee käsiksi."
-  (:require [reagent.core :refer [atom] :as reagent]
+  (:require [reagent.core :refer [atom] :as r]
             [bootstrap :as bs]
             
-            [harja.views.urakka.yksikkohintaiset-tyot :as yksikkohintaiset-tyot]))
+            [harja.views.urakka.yksikkohintaiset-tyot :as yksikkohintaiset-tyot]
+            [harja.pvm :as pvm]
+            [harja.loki :refer [log]]
+            [harja.ui.yleiset :refer [ajax-loader kuuntelija linkki sisalla? alasveto-ei-loydoksia alasvetovalinta radiovalinta]])
+  
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [reagent.ratom :refer [reaction run!]]
+                   [harja.ui.yleiset :refer [deftk]]))
+
+
+(def valittu-valilehti "Valittu välilehti" (atom 0))
+
+(def valittu-sopimusnumero "Sopimusnumero" (atom nil))
+(defn valitse-sopimusnumero! [sn]
+  (reset! valittu-sopimusnumero sn))
+
+
+(def +hoitokauden-alkukk-indeksi+ "9")
+(def +hoitokauden-alkupv-indeksi+ "1")
+(def +hoitokauden-loppukk-indeksi+ "8")
+(def +hoitokauden-loppupv-indeksi+ "30")
+
+(def valittu-hoitokausi "Hoitokausi" (atom nil))
+
+(defn valitse-hoitokausi! [hk]
+  (reset! valittu-hoitokausi hk))
+
+(defn hoitokaudet [ur]
+  (let [ensimmainen-vuosi (.getYear (:alkupvm ur))
+        viimeinen-vuosi (.getYear (:loppupvm ur))]
+    (mapv (fn [vuosi]
+            {:alkupvm (pvm/luo-pvm vuosi +hoitokauden-alkukk-indeksi+ +hoitokauden-alkupv-indeksi+)
+             :loppupvm (pvm/luo-pvm (inc vuosi) +hoitokauden-loppukk-indeksi+ +hoitokauden-loppupv-indeksi+)})
+          (range ensimmainen-vuosi (inc viimeinen-vuosi)))))
 
 
 (defn suunnittelu [ur]
-  
-   [bs/tabs {:style :pills}
-
-    "Yksikköhintaiset työt"
-    ^{:key "yksikkohintaiset-tyot"}
-    [yksikkohintaiset-tyot/yksikkohintaiset-tyot ur]
-
-    "Kokonaishintaiset työt"
-    [:div "Kokonaishintaiset työt"]
-    ;;^{:key "kokonaishintaiset-tyot"}
-    ;;[yht/yksikkohintaiset-tyot]
+  ;; suunnittelu-välilehtien yhteiset valinnat hoitokaudelle ja sopimusnumerolle
+  (let [urakan-hoitokaudet (atom (hoitokaudet ur))]
+    (valitse-sopimusnumero! (first (:sopimukset ur)))
+    (valitse-hoitokausi! (first @urakan-hoitokaudet))
     
-    "Materiaalit"
-    [:div "Materiaalit"]
-    ;;^{:key "materiaalit"}
-])
+    (r/create-class
+      {:component-will-receive-props
+       (fn [this [_ ur]]
+         (reset! urakan-hoitokaudet (hoitokaudet ur))
+         (valitse-sopimusnumero! (first (:sopimukset ur)))
+         (valitse-hoitokausi! (first @urakan-hoitokaudet)))
+       
+       
+       :reagent-render 
+       (fn [ur]
+         [:span
+          [:div.alasvetovalikot
+           [:div.label-ja-alasveto 
+            [:span.alasvedon-otsikko "Sopimusnumero"]
+            [alasvetovalinta {:valinta @valittu-sopimusnumero
+                              :format-fn second
+                              :valitse-fn valitse-sopimusnumero!
+                              :class "alasveto"
+                              }
+             (:sopimukset ur)
+             ]]
+           [:div.label-ja-alasveto
+            [:span.alasvedon-otsikko "Hoitokausi"]
+            [alasvetovalinta {:valinta @valittu-hoitokausi
+                              ;;\u2014 on väliviivan unikoodi
+                              :format-fn #(if % (str (pvm/pvm (:alkupvm %)) 
+                                                     " \u2014 " (pvm/pvm (:loppupvm %))) "Valitse")
+                              :valitse-fn valitse-hoitokausi!
+                              :class "alasveto"
+                              }
+             @urakan-hoitokaudet
+             ]]]
+          
+          ;; suunnittelun välilehdet
+          [bs/tabs {:style :pills :active valittu-valilehti}
+           
+           "Yksikköhintaiset työt"
+           ^{:key "yksikkohintaiset-tyot"}
+           [yksikkohintaiset-tyot/yksikkohintaiset-tyot ur]
+           
+           "Kokonaishintaiset työt"
+           [:div "Kokonaishintaiset työt"]
+           ;;^{:key "kokonaishintaiset-tyot"}
+           ;;[yht/yksikkohintaiset-tyot]
+           
+           "Materiaalit"
+           [:div "Materiaalit"]
+           ;;^{:key "materiaalit"}
+           ]])
+       
+       })))
+
+
--- src/cljs/harja/views/urakka/yksikkohintaiset_tyot.cljs
+++ src/cljs/harja/views/urakka/yksikkohintaiset_tyot.cljs
@@ -6,6 +6,7 @@
             [harja.ui.yleiset :refer [ajax-loader kuuntelija linkki sisalla? alasveto-ei-loydoksia alasvetovalinta radiovalinta]]
             [harja.tiedot.urakka.yksikkohintaiset-tyot :as yks-hint-tyot]
             [harja.tiedot.urakka.urakan-toimenpiteet :as urakan-toimenpiteet]
+            [harja.views.urakka.suunnittelu :as suunnittelu]
             
             [harja.loki :refer [log]]
             [harja.pvm :as pvm]
@@ -19,23 +20,6 @@
                    [reagent.ratom :refer [reaction run!]]
                    [harja.ui.yleiset :refer [deftk]]))
 
-(def +hoitokauden-alkukk-indeksi+ "9")
-(def +hoitokauden-alkupv-indeksi+ "1")
-(def +hoitokauden-loppukk-indeksi+ "8")
-(def +hoitokauden-loppupv-indeksi+ "30")
-
-(def valittu-hoitokausi "Hoitokausi" (atom nil))
-
-(defn valitse-hoitokausi! [hk]
-  (reset! valittu-hoitokausi hk))
-
-(defn hoitokaudet [ur]
-  (let [ensimmainen-vuosi (.getYear (:alkupvm ur))
-        viimeinen-vuosi (.getYear (:loppupvm ur))]
-    (mapv (fn [vuosi]
-            {:alkupvm (pvm/luo-pvm vuosi +hoitokauden-alkukk-indeksi+ +hoitokauden-alkupv-indeksi+)
-             :loppupvm (pvm/luo-pvm (inc vuosi) +hoitokauden-loppukk-indeksi+ +hoitokauden-loppupv-indeksi+)})
-          (range ensimmainen-vuosi (inc viimeinen-vuosi)))))
 
 (defn tallenna-tyot [ur sopimusnumero hoitokausi tyot vanhat-tyot uudet-tyot]
   (log "tallenna-tyot enter, uudet työt" uudet-tyot)
@@ -58,76 +42,43 @@
    toimenpiteet-ja-tehtavat (<! (urakan-toimenpiteet/hae-urakan-toimenpiteet-ja-tehtavat (:id ur)))
    kolmostason-tpt nil
    nelostason-tpt nil
-   urakan-hoitokaudet nil
-   valittu-sopimusnumero (first (:sopimukset ur))
    tyorivit nil 
    ]
   
   (do
     (run! (let [toimenpiteet-ja-tehtavat (group-by :taso @toimenpiteet-ja-tehtavat)]
             (reset! kolmostason-tpt (vec (get toimenpiteet-ja-tehtavat 3)))
-            (reset! nelostason-tpt (vec (get toimenpiteet-ja-tehtavat 4)))
-            (reset! urakan-hoitokaudet (hoitokaudet ur))))
+            (reset! nelostason-tpt (vec (get toimenpiteet-ja-tehtavat 4)))))
     
     ;; vain valitun hoitokauden tyot talteen
     (run! (let [tehtavien-rivit (group-by :tehtava (filter (fn [t]
                                                              (and 
-                                                               (= (:sopimus t) (first @valittu-sopimusnumero))
+                                                               (= (:sopimus t) (first @suunnittelu/valittu-sopimusnumero))
                                                                (or
-                                                                 (pvm/sama-pvm? (:alkupvm t) (:alkupvm @valittu-hoitokausi))
-                                                                 (pvm/sama-pvm? (:loppupvm t) (:loppupvm @valittu-hoitokausi))))
+                                                                 (pvm/sama-pvm? (:alkupvm t) (:alkupvm @suunnittelu/valittu-hoitokausi))
+                                                                 (pvm/sama-pvm? (:loppupvm t) (:loppupvm @suunnittelu/valittu-hoitokausi))))
                                                              ) @tyot))
                 kirjatut-tehtavat (into #{} (keys tehtavien-rivit))
-                tyhjat-tyot  (map #(luo-tyhja-tyo % ur @valittu-hoitokausi) (filter (fn [tp]
+                tyhjat-tyot  (map #(luo-tyhja-tyo % ur @suunnittelu/valittu-hoitokausi) (filter (fn [tp]
                                                                                       (not (kirjatut-tehtavat (:id tp)))) @nelostason-tpt))]
             
             (reset! tyorivit
                     (vec (concat (mapv (fn [[_ tehtavan-rivit]]
                                          (let [pohja (first tehtavan-rivit)]
                                            (merge pohja
-                                                  (zipmap (map #(if (= (.getYear (:alkupvm @valittu-hoitokausi))
+                                                  (zipmap (map #(if (= (.getYear (:alkupvm @suunnittelu/valittu-hoitokausi))
                                                                        (.getYear (:alkupvm %)))
                                                                   :maara-kkt-10-12 :maara-kkt-1-9) tehtavan-rivit)
                                                           (map :maara tehtavan-rivit))
                                                   
                                                   {:yhteensa (reduce + 0 (map #(* (:yksikkohinta %) (:maara %)) tehtavan-rivit))}
                                                   ))) tehtavien-rivit) tyhjat-tyot)))))
-    ;; varmista järkevät oletusvalinnat
-    ;;(if (nil? @valittu-sopimusnumero)
-    ;;(valitse-sopimusnumero! (first (:sopimukset ur))))
-    (if (nil? @valittu-hoitokausi)
-      (valitse-hoitokausi! (first @urakan-hoitokaudet)))
     
-    (log "urakan-hoitokaudet" urakan-hoitokaudet)
-    (log "valittu-sopimusnumero" valittu-sopimusnumero)
-    
-    [:div.yksikkohintaiset-tyot 
-     [:div.alasvetovalikot
-      [:div.label-ja-alasveto 
-       [:span.alasvedon-otsikko "Sopimusnumero"]
-       [alasvetovalinta {:valinta @valittu-sopimusnumero
-                         :format-fn second
-                         :valitse-fn #(reset! valittu-sopimusnumero %)
-                         :class "alasveto"
-                         }
-        (:sopimukset ur)
-        ]]
-      [:div.label-ja-alasveto
-       [:span.alasvedon-otsikko "Hoitokausi"]
-       [alasvetovalinta {:valinta @valittu-hoitokausi
-                         ;;\u2014 on väliviivan unikoodi
-                         :format-fn #(if % (str (pvm/pvm (:alkupvm %)) 
-                                                " \u2014 " (pvm/pvm (:loppupvm %))) "Valitse")
-                         :valitse-fn valitse-hoitokausi!
-                         :class "alasveto"
-                         }
-        @urakan-hoitokaudet
-        ]]]
-     
+    [:div.yksikkohintaiset-tyot     
      [grid/grid
       {:otsikko "Yksikköhintaiset työt"
        :tyhja (if (nil? nelostason-tpt) [ajax-loader "Yksikköhintaisia töitä haetaan..."] "Ei yksikköhintaisia töitä")
-       :tallenna #(tallenna-tyot ur @valittu-sopimusnumero @valittu-hoitokausi 
+       :tallenna #(tallenna-tyot ur @suunnittelu/valittu-sopimusnumero @suunnittelu/valittu-hoitokausi 
                                  tyot @tyorivit %)
        :tunniste :tehtava
        :voi-lisata? false
@@ -136,8 +87,8 @@
       
       ;; sarakkeet
       [{:otsikko "Tehtävä" :nimi :tehtavan_nimi :tyyppi :string :muokattava? (constantly false) :leveys "25%"}
-       {:otsikko (str "Määrä 10-12/" (.getYear (:alkupvm @valittu-hoitokausi))) :nimi :maara-kkt-10-12 :tyyppi :numero :leveys "15%"}
-       {:otsikko (str "Määrä 1-9/"  (.getYear (:loppupvm @valittu-hoitokausi))) :nimi :maara-kkt-1-9 :tyyppi :numero :leveys "15%"}
+       {:otsikko (str "Määrä 10-12/" (.getYear (:alkupvm @suunnittelu/valittu-hoitokausi))) :nimi :maara-kkt-10-12 :tyyppi :numero :leveys "15%"}
+       {:otsikko (str "Määrä 1-9/"  (.getYear (:loppupvm @suunnittelu/valittu-hoitokausi))) :nimi :maara-kkt-1-9 :tyyppi :numero :leveys "15%"}
        {:otsikko "Yks." :nimi :yksikko :tyyppi :string :leveys "15%"}
        {:otsikko (str "\u20AC" "/yks") :nimi :yksikkohinta :tyyppi :numero :leveys "15%"}
        {:otsikko "Yhteensä" :nimi :yhteensa :tyyppi :string :leveys "15%" :fmt #(if % (str (.toFixed % 2) " \u20AC"))}
