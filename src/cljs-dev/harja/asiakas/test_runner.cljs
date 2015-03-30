639a2617a1246d5dc6724c93825b3a98c523ec76
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-03-30 13:54:50 +0300

    Lisätty vähän console.log raportointia testien juoksuttajaan.

--- src/cljs-dev/harja/asiakas/test_runner.cljs
+++ src/cljs-dev/harja/asiakas/test_runner.cljs
@@ -8,6 +8,12 @@
    [harja.app-test]))
 
 
+(defmethod test/report [:harja :begin-test-ns] [event]
+  (.log js/console "Testataan: " (:ns event)))
+
+(defmethod test/report [:harja :begin-test-var] [event]
+  (.log js/console "TEST: " (test/testing-vars-str (:var event))))
+
 (defmethod test/report [:harja :fail] [event]
   (.log js/console "FAIL: " (pr-str event))
   (viesti/nayta! [:div.testfail
@@ -18,8 +24,12 @@
                     [:div.testmessage "Viesti: " m])]
                  :danger))
 
+(defmethod test/report [:harja :summary] [event]
+  (.log js/console "Testejä ajettu: " (:test event)))
 
-(defn ^:export aja-testit []
+(defn aja-testit []
   (test/run-tests (merge (test/empty-env)
                          {:reporter :harja})
-                  'harja.app-test))
+                  'harja.app-test)) 
+ 
+ 
