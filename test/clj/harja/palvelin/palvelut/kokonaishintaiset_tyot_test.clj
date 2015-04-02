a2eb77648cf027357e671256523804392a3586a0
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2015-04-02 15:56:53 +0300

    Lisää testi jossa oikeudeton urakoitsija kysyy töitä eikä saa

--- test/clj/harja/palvelin/palvelut/kokonaishintaiset_tyot_test.clj
+++ test/clj/harja/palvelin/palvelut/kokonaishintaiset_tyot_test.clj
@@ -42,31 +42,45 @@
                           :luotu nil, 
                           :rooli "urakoitsijan urakan vastuuhenkilo"}]}))
 
+(defn ei-ole-oulun-urakan-urakoitsijan-urakkavastaava []
+    {:sahkoposti "yit_uuvh@example.org", :kayttajanimi "yit_uuvh", :puhelin 43363123, :sukunimi "Urakkavastaava", 
+     :roolit #{"urakoitsijan urakan vastuuhenkilo"}, :id 17, :etunimi "Yitin", 
+     :organisaatio {:id 10, :nimi "YIT Rakennus Oy", :tyyppi "urakoitsija"}, 
+     :urakkaroolit [{:urakka {:id 234234324234, ;;eli ei ole oulun urakan ID
+                                   :nimi "Oulun alueurakka 2005-2010", :urakoitsija {:nimi "YIT Rakennus Oy", :id 10}, 
+                                   :hallintayksikko {:nimi "Pohjois-Pohjanmaa ja Kainuu", :id 8}}, 
+                          :luotu nil, 
+                          :rooli "urakoitsijan urakan vastuuhenkilo"}]})
+
 
 ;; käyttää testidata.sql:stä tietoa
 (deftest kaikki-kokonaishintaiset-tyot-haettu-oikein []
   (let [oulun-alueurakan-id (:id (first (urk-q/hae-urakoita (:db jarjestelma) (str "%Oulun alueurakka 2005-2010%"))))
         oulun-alueurakan-sopimus (ffirst (q jarjestelma 
                                             (str "SELECT id 
-                                                    FROM sopimus 
-                                                   WHERE urakka = " oulun-alueurakan-id
-                                                         " AND paasopimus IS null")))
+                                                 FROM sopimus 
+                                                 WHERE urakka = " oulun-alueurakan-id
+                                                 " AND paasopimus IS null")))
         kokonaishintaiset-tyot (kutsu-palvelua (:http-palvelin jarjestelma)
-                                 :kokonaishintaiset-tyot +kayttaja-jvh+ oulun-alueurakan-id)
+                                               :kokonaishintaiset-tyot +kayttaja-jvh+ oulun-alueurakan-id)
         oulun-alueurakan-toiden-lkm (ffirst (q jarjestelma (str "SELECT count(*) 
-                                                                  FROM kokonaishintainen_tyo kt 
-                                                                       LEFT JOIN toimenpideinstanssi tpi ON kt.toimenpideinstanssi = tpi.id
-                                                                  WHERE tpi.urakka = " oulun-alueurakan-id)))
+                                                                FROM kokonaishintainen_tyo kt 
+                                                                LEFT JOIN toimenpideinstanssi tpi ON kt.toimenpideinstanssi = tpi.id
+                                                                WHERE tpi.urakka = " oulun-alueurakan-id)))
         
         urakoitsijan-urakanvalvoja (oulun-urakan-urakoitsijan-urakkavastaava)
+        ei-ole-taman-urakan-urakoitsijan-urakanvalvoja (ei-ole-oulun-urakan-urakoitsijan-urakkavastaava)
         kokonaishintaiset-tyot-kutsujana-urakoitsija (kutsu-palvelua (:http-palvelin jarjestelma)
                                                                      :kokonaishintaiset-tyot urakoitsijan-urakanvalvoja oulun-alueurakan-id)]
     
     (is (= (count kokonaishintaiset-tyot) oulun-alueurakan-toiden-lkm))
     (is (= (count kokonaishintaiset-tyot-kutsujana-urakoitsija) oulun-alueurakan-toiden-lkm))
+    (is (thrown? RuntimeException (kutsu-palvelua (:http-palvelin jarjestelma)
+                                                  :kokonaishintaiset-tyot ei-ole-taman-urakan-urakoitsijan-urakanvalvoja oulun-alueurakan-id)))
     (is (= "Oulu Talvihoito TP" (:tpi_nimi (first kokonaishintaiset-tyot))))
     (is (= "Oulu Talvihoito TP" (:tpi_nimi (last kokonaishintaiset-tyot))))
     (is (= oulun-alueurakan-sopimus (:sopimus (first kokonaishintaiset-tyot))))
     (is (= oulun-alueurakan-sopimus (:sopimus (last kokonaishintaiset-tyot))))
+    (is (= 3500.0 (:summa (first kokonaishintaiset-tyot))))
     (is (= 3500.0 (:summa (first kokonaishintaiset-tyot))))
     (is (= 3500.0 (:summa (last kokonaishintaiset-tyot))))))
