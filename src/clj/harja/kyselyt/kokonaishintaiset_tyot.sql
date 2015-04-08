76e2b4f4fff325850e2a7d84abfc493bd7f1b55b
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2015-04-08 15:08:44 +0300

    Anna kok.hint. työt kannasta aikajärjestyksessä

--- src/clj/harja/kyselyt/kokonaishintaiset_tyot.sql
+++ src/clj/harja/kyselyt/kokonaishintaiset_tyot.sql
@@ -1,7 +1,8 @@
 -- name: listaa-urakan-kokonaishintaiset-tyot
 -- Hakee kaikki kokonaishintaiset-tyot
-SELECT kt.vuosi, kt.kuukausi, kt.summa, kt.maksupvm, kt.toimenpideinstanssi, kt.sopimus,
-       tpi.nimi as tpi_nimi, tpi.toimenpide as toimenpide
-  FROM kokonaishintainen_tyo kt
-  	   LEFT JOIN toimenpideinstanssi tpi ON kt.toimenpideinstanssi = tpi.id
- WHERE tpi.urakka = :urakka
+SELECT    kt.vuosi, kt.kuukausi, kt.summa, kt.maksupvm, kt.toimenpideinstanssi, kt.sopimus,
+          tpi.nimi as tpi_nimi, tpi.toimenpide as toimenpide
+  FROM    kokonaishintainen_tyo kt
+  	      LEFT JOIN toimenpideinstanssi tpi ON kt.toimenpideinstanssi = tpi.id
+ WHERE    tpi.urakka = :urakka
+ ORDER BY vuosi, kuukausi
