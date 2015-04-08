d2e13205798eaf1785eb463368ad7c3e8b6582b3
mikkoro <mikko.ronkkomaki@solita.fi>
2015-04-08 19:30:49 +0300

    Lisää HTML-formatoinnin selainvirheille

--- src/clj/harja/palvelin/palvelut/selainvirhe.clj
+++ src/clj/harja/palvelin/palvelut/selainvirhe.clj
@@ -22,14 +22,14 @@
   (str/replace (str sisalto) "<" "&lt;"))
 
 (defn formatoi-virhe [{:keys [id]} {:keys [url viesti rivi sarake selain stack]}]
-  (log/info "stack: " stack)
-  (str "Selainvirhe: " (sanitoi viesti)
-       ", URL: " (sanitoi url)
-       ", selain: " (sanitoi selain)
-       ", rivi: " (sanitoi rivi)
-       ", sarake: " (sanitoi sarake)
-       ", käyttäjä id: " (sanitoi id)
-       ", stack: <pre>" (sanitoi stack) "</pre>"))
+  [:table
+   [:tr [:td {:valign "top"} [:b "Selainvirhe"]] [:td [:pre (sanitoi viesti)]]]
+   [:tr [:td {:valign "top"} [:b "URL:"]] [:td [:pre (sanitoi url)]]]
+   [:tr [:td {:valign "top"} [:b "selain: "]] [:td [:pre (sanitoi selain)]]]
+   [:tr [:td {:valign "top"} [:b "rivi: "]] [:td [:pre (sanitoi rivi)]]]
+   [:tr [:td {:valign "top"} [:b "sarake: "]] [:td [:pre (sanitoi sarake)]]]
+   [:tr [:td {:valign "top"} [:b "käyttäjä id: "]] [:td [:pre (sanitoi id)]]]
+   (when stack [:tr [:td {:valign "top"} [:b "stack: "]] [:td [:pre (sanitoi stack)]]])])
 
 (defn raportoi-selainvirhe
   "Logittaa yksittäisen selainvirheen"
