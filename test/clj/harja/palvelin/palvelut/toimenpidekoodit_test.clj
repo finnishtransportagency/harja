03b8a43d99a205084ccb966231c37fee9f748d42
Jari Hanhela <jari.hanhela@solita.fi>
2015-04-13 08:27:13 +0300

    Lisää yksinkertaisen testin funktiolle hae-urakan-kaikki-toimenpiteet. Testaa, että saadaan vastaus,

--- test/clj/harja/palvelin/palvelut/toimenpidekoodit_test.clj
+++ test/clj/harja/palvelin/palvelut/toimenpidekoodit_test.clj
@@ -1,8 +1,13 @@
 (ns harja.palvelin.palvelut.toimenpidekoodit-test
-  (:require [clojure.test :refer [use-fixtures]]
+  (:require [clojure.test :refer :all]
+            [harja.palvelin.komponentit.tietokanta :as tietokanta]
             [harja.palvelin.palvelut.toimenpidekoodit :refer :all]
-            [harja.testi :refer :all]))
+            [harja.palvelin.palvelut.urakan-toimenpiteet :as urakan-toimenpiteet]
+            [harja.testi :refer :all]
+            [com.stuartsierra.component :as component]))
 
-
-
-
+(deftest hae-urakan-kaikki-toimenpiteet []
+    (let [db (apply tietokanta/luo-tietokanta testitietokanta)
+        user "harja"
+        urakka-id 1]
+    (not (nil? (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id)))))
