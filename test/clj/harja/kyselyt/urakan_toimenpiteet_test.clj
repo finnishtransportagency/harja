16d7a71bbcf63570e69cd82a4c422d9689107a66
Jari Hanhela <jari.hanhela@solita.fi>
2015-04-13 13:12:32 +0300

    Lisää testin, joka tarkistaa että myös urakka id:llä 2 löytyy dataa. Poistaa turhat logitukset.

--- test/clj/harja/kyselyt/urakan_toimenpiteet_test.clj
+++ test/clj/harja/kyselyt/urakan_toimenpiteet_test.clj
@@ -11,7 +11,6 @@
     (let [db (apply tietokanta/luo-tietokanta testitietokanta)
          urakka-id 1
          response (urakan-toimenpiteet/hae-urakan-toimenpiteet-ja-tehtavat-tasot db urakka-id)]
-    (log/info response)
     (is (not (nil? response)))
     (is (= (count response) 4))
 
@@ -23,3 +22,10 @@
     (mapv (fn [rivi] (is (= (:taso (nth rivi 1)) 2))) response)
     (mapv (fn [rivi] (is (= (:taso (nth rivi 2)) 3))) response)
     (mapv (fn [rivi] (is (= (:taso (nth rivi 3)) 4))) response)))
+
+(deftest hae-urakan-2-toimenpiteet-ja-tehtavat-tasot []
+    (let [db (apply tietokanta/luo-tietokanta testitietokanta)
+       urakka-id 2
+       response (urakan-toimenpiteet/hae-urakan-toimenpiteet-ja-tehtavat-tasot db urakka-id)]
+     (is (not (nil? response)))
+     (is (= (count response) 2))))
