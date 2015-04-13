73608fb84a116e07f5e9944c8e259799f2bfa6f5
Jari Hanhela <jari.hanhela@solita.fi>
2015-04-13 11:09:34 +0300

    Paivittaa urakan_toimenpiteet_testin lisäämällä kaksi assertia. Lisää testin funktiolle hae-urakan-toimenpiteet-ja-tehtavat

--- test/clj/harja/palvelin/palvelut/urakan_toimenpiteet_test.clj
+++ test/clj/harja/palvelin/palvelut/urakan_toimenpiteet_test.clj
@@ -11,6 +11,15 @@
         user "harja"
         urakka-id 1]
     (is (not (nil? (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))))
+    (is (= (count (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id)) 2))
 	(is (= (:t3_nimi (first (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))) "Laaja toimenpide"))
+    (is (= (:id (first (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))) 911))
 	(is (= (:tpi_nimi (first (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))) "Oulu Talvihoito TP"))
-	(is (= (:id (first (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))) 911))))
+    (is (= (:tpi_nimi (second (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))) "Oulu Sorateiden hoito TP"))))
+
+
+(deftest hae-urakan-1-toimenpiteet-ja-tehtavat []
+    (let [db (apply tietokanta/luo-tietokanta testitietokanta)
+        user "harja"
+        urakka-id 1]
+    (is (not (nil? (urakan-toimenpiteet/hae-urakan-toimenpiteet-ja-tehtavat db user urakka-id))))))
