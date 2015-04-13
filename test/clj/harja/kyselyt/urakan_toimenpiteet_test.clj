6a66559708bc1a3e63d916f3f0fa83049689db7f
Jari Hanhela <jari.hanhela@solita.fi>
2015-04-13 12:07:02 +0300

    Lisätty testi funktiolle hae-urakan-toimenpiteet-ja-tehtavat-tasot. Muutama assert testaa, että saapuva testidata vaikuttaa oikealta.

---
+++ test/clj/harja/kyselyt/urakan_toimenpiteet_test.clj
@@ -0,0 +1,17 @@
+(ns harja.kyselyt.urakan-toimenpiteet-test
+  (:require [clojure.test :refer :all]
+            [harja.palvelin.komponentit.tietokanta :as tietokanta]
+            [harja.palvelin.palvelut.toimenpidekoodit :refer :all]
+            [harja.kyselyt.urakan-toimenpiteet :as urakan-toimenpiteet]
+            [taoensso.timbre :as log]
+            [harja.testi :refer :all]
+            [com.stuartsierra.component :as component]))
+
+(deftest hae-urakan-1-toimenpiteet-ja-tehtavat-tasot []
+    (let [db (apply tietokanta/luo-tietokanta testitietokanta)
+         urakka-id 1]
+    (log/info (urakan-toimenpiteet/hae-urakan-toimenpiteet-ja-tehtavat-tasot db urakka-id))
+    (is (not (nil? (urakan-toimenpiteet/hae-urakan-toimenpiteet-ja-tehtavat-tasot db urakka-id))))
+    (is (= (count (urakan-toimenpiteet/hae-urakan-toimenpiteet-ja-tehtavat-tasot db urakka-id)) 4))
+    (is (= (:id (first (first (urakan-toimenpiteet/hae-urakan-toimenpiteet-ja-tehtavat-tasot db urakka-id))) 906)))
+    (is (= (:taso (first (first (urakan-toimenpiteet/hae-urakan-toimenpiteet-ja-tehtavat-tasot db urakka-id))) 1)))))
