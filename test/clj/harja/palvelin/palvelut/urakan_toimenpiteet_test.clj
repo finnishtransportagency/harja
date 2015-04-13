dc7b8d5f51b4fee7e5aecc469df075b4339ad891
Jari Hanhela <jari.hanhela@solita.fi>
2015-04-13 09:25:47 +0300

    Uudelleennimeää tiedoston toimenpidekoodit_test nimelle urakan_toimenpiteet_test.

--- test/clj/harja/palvelin/palvelut/urakan_toimenpiteet_test.clj
+++ test/clj/harja/palvelin/palvelut/urakan_toimenpiteet_test.clj
@@ -1,4 +1,4 @@
-(ns harja.palvelin.palvelut.toimenpidekoodit-test
+(ns harja.palvelin.palvelut.urakan-toimenpiteet-test
   (:require [clojure.test :refer :all]
             [harja.palvelin.komponentit.tietokanta :as tietokanta]
             [harja.palvelin.palvelut.toimenpidekoodit :refer :all]
@@ -6,10 +6,11 @@
             [harja.testi :refer :all]
             [com.stuartsierra.component :as component]))
 
-(deftest hae-urakan-kaikki-toimenpiteet []
+(deftest hae-urakan-1-toimenpiteet []
     (let [db (apply tietokanta/luo-tietokanta testitietokanta)
         user "harja"
         urakka-id 1]
     (is (not (nil? (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))))
 	(is (= (:t3_nimi (first (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))) "Laaja toimenpide"))
-	(is (= (:tpi_nimi (first (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))) "Oulu Talvihoito TP"))))
+	(is (= (:tpi_nimi (first (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))) "Oulu Talvihoito TP"))
+	(is (= (:id (first (urakan-toimenpiteet/hae-urakan-toimenpiteet db user urakka-id))) 911))))
