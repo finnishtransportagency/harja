bb60c72a06862305d79c121a38a06d14107b28d1
Jari Hanhela <jari.hanhela@solita.fi>
2015-04-13 12:50:24 +0300

    Päivittää urakan_toimenpiteet_testin: lisää tarkistukset tasoille.

--- test/clj/harja/kyselyt/urakan_toimenpiteet_test.clj
+++ test/clj/harja/kyselyt/urakan_toimenpiteet_test.clj
@@ -14,5 +14,12 @@
     (log/info response)
     (is (not (nil? response)))
     (is (= (count response) 4))
-    (is (= (:id (first (first response)) 906)))
-    (is (= (:taso (first (first response)) 1)))))
+
+    (mapv (fn [rivi]
+              (is (= (:taso (first rivi)) 1))
+              (is (= (:id (first rivi)) 906)))
+              response)
+
+    (mapv (fn [rivi] (is (= (:taso (nth rivi 1)) 2))) response)
+    (mapv (fn [rivi] (is (= (:taso (nth rivi 2)) 3))) response)
+    (mapv (fn [rivi] (is (= (:taso (nth rivi 3)) 4))) response)))
