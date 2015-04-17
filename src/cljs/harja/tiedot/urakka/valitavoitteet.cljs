97215761a6bfa7cf9400dae6163a7588b2607c5f
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-04-17 12:41:17 +0300

    Välitavoitteet: tiedot ja näkymä.
    Alustava (dummy) tietojen haku palvelimelta ja näkymän alku.

---
+++ src/cljs/harja/tiedot/urakka/valitavoitteet.cljs
@@ -0,0 +1,22 @@
+(ns harja.tiedot.urakka.valitavoitteet
+  "Ylläpidon urakoiden välitavoitteiden tiedot."
+  (:require [harja.asiakas.kommunikaatio :as k]
+            [cljs.core.async :refer [<! >! chan]]
+            [harja.pvm :as pvm])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+
+
+(defn hae-urakan-valitavoitteet [urakka-id]
+  (let [ch (chan)]
+    (go
+      (>! ch [{:id 1 :nimi "Suojatiet" :takaraja (pvm/luo-pvm 2015 2 17)
+               :valmis {:pvm (pvm/luo-pvm 2015 2 16) :kommentti "saatiin ne tehtyä vaikka tiukille meni aika"}
+               :sakko 1500}
+                        
+              {:id 2 :nimi "Keskustan keltaiset viivat" :valmis nil :takaraja (pvm/luo-pvm 2015 6 7)
+               :sakko 2000}]))
+              
+      ;;(>! ch (<! (k/post! :hae-urakan-valitavoitteet urakka-id))))
+    ch))
+
---
+++ src/cljs/harja/views/urakka/valitavoitteet.cljs
@@ -0,0 +1,48 @@
+(ns harja.views.urakka.valitavoitteet
+  "Ylläpidon urakoiden välitavoitteiden näkymä"
+  (:require [reagent.core :refer [atom] :as r]
+            [harja.loki :refer [log logt]]
+            [harja.ui.komponentti :as komp]
+            [harja.tiedot.urakka.valitavoitteet :as vt]
+            [harja.ui.grid :as grid]
+            [harja.pvm :as pvm]
+            [cljs.core.async :refer [<!]])
+  (:require-macros [reagent.ratom :refer [reaction run!]]
+                   [cljs.core.async.macros :refer [go]]))
+
+(defn valitavoitteet
+  "Urakan välitavoitteet näkymä. Ottaa parametrinä urakan ja hakee välitavoitteet sille."
+  [ur]
+  (let [tavoitteet (atom nil)
+        vaihda-urakka! (fn [ur]
+                         (go (reset! tavoitteet (<! (vt/hae-urakan-valitavoitteet ur)))))]
+    (vaihda-urakka! ur)
+    (komp/luo
+     
+     {:component-will-receive-props (fn [_ & [_ ur]]
+                                      (log "uusi urakka: " (pr-str (dissoc ur :alue)))
+                                      (vaihda-urakka! ur))}
+     
+     (fn [ur]
+       [:div.valitavoitteet
+        [:br][:br][:br] ;; FIXME: mieti mitä pienelle kartalle pitäisi tehdä, jää "muokkaa" napin alle
+        [grid/grid
+         {:otsikko "Välitavoitteet"
+          :tallenna #(logt %)
+          }
+
+         [{:otsikko "Nimi" :leveys "25%" :nimi :nimi :tyyppi :string}
+          {:otsikko "Takaraja" :leveys "15%" :nimi :takaraja :fmt pvm/pvm :tyyppi :pvm}
+          {:otsikko "Valmistunut" :leveys "15%"
+           :hae (comp :pvm :valmis)
+           :nimi :valmis :fmt #(if-not % "Ei valmis" (pvm/pvm %)) :tyyppi :pvm}
+                                   
+                                          
+                                     
+          ]
+
+         @tavoitteet]]))))
+
+         
+            
+            
