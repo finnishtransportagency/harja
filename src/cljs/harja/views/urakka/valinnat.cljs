81251550d7f9b825ee790558fecf4f21c760c134
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-05-04 09:38:53 +0300

    Tee hoitokaudesta itsenäinen valintakomponentti.
    urakan-hoitokausi komponenttia voi käyttää, jos haluaa pelkän
    hoitokauden, ilman sopimusvalintaa.

--- src/cljs/harja/views/urakka/valinnat.cljs
+++ src/cljs/harja/views/urakka/valinnat.cljs
@@ -9,29 +9,30 @@
             [harja.loki :refer [log]]
             [harja.ui.yleiset :refer [ajax-loader kuuntelija linkki sisalla? livi-pudotusvalikko]]))
 
+(defn urakan-hoitokausi [ur]
+  (let [hoitokaudet (s/hoitokaudet ur)]
+    [:div.label-ja-alasveto
+     [:span.alasvedon-otsikko (if (= :hoito (:tyyppi ur)) "Hoitokausi" "Sopimuskausi")]
+     [livi-pudotusvalikko {:valinta @s/valittu-hoitokausi
+                           ;;\u2014 on väliviivan unikoodi
+                           :format-fn #(if % (str (pvm/pvm (first %))
+                                                  " \u2014 " (pvm/pvm (second %))) "Valitse")
+                           :valitse-fn s/valitse-hoitokausi!
+                           :class "suunnittelu-alasveto"
+                           }
+      hoitokaudet]]))
 
 (defn urakan-sopimus-ja-hoitokausi [ur]
-  (let [hoitokaudet (s/hoitokaudet ur)]
-    [:span
-     [:div.label-ja-alasveto
-      [:span.alasvedon-otsikko "Sopimusnumero"]
-      [livi-pudotusvalikko {:valinta @s/valittu-sopimusnumero
-                            :format-fn second
-                            :valitse-fn s/valitse-sopimusnumero!
-                            :class "suunnittelu-alasveto"
-                            }
-       (:sopimukset ur)
-       ]]
-     [:div.label-ja-alasveto
-      [:span.alasvedon-otsikko (if (= :hoito (:tyyppi ur)) "Hoitokausi" "Sopimuskausi")]
-      [livi-pudotusvalikko {:valinta @s/valittu-hoitokausi
-                            ;;\u2014 on väliviivan unikoodi
-                            :format-fn #(if % (str (pvm/pvm (first %))
-                                                   " \u2014 " (pvm/pvm (second %))) "Valitse")
-                            :valitse-fn s/valitse-hoitokausi!
-                            :class "suunnittelu-alasveto"
-                            }
-       hoitokaudet]]]))
+  [:span
+   [:div.label-ja-alasveto
+    [:span.alasvedon-otsikko "Sopimusnumero"]
+    [livi-pudotusvalikko {:valinta @s/valittu-sopimusnumero
+                          :format-fn second
+                          :valitse-fn s/valitse-sopimusnumero!
+                          :class "suunnittelu-alasveto"
+                          }
+     (:sopimukset ur)]]
+   [urakan-hoitokausi ur]])
 
 (defn urakan-sopimus-ja-hoitokausi-ja-toimenpide [ur]
   [:span
