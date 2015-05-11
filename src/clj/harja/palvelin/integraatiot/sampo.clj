5a3ddcf1497faa3314bc51b8e8ec68cfe8c018e8
mikkoro <mikko.ronkkomaki@solita.fi>
2015-05-11 16:02:21 +0300

    Lisää maksuerien lapiointia Sampon rajapintaan

--- src/clj/harja/palvelin/integraatiot/sampo.clj
+++ src/clj/harja/palvelin/integraatiot/sampo.clj
@@ -1,6 +1,13 @@
 (ns harja.palvelin.integraatiot.sampo
-  (:require [hiccup.core :refer [html]]))
+  (:require [hiccup.core :refer [html]])
+  (:import (java.text SimpleDateFormat)
+           (java.util Date Calendar)))
 
+(defn muodosta-kulu-id []
+  (clojure.string/join "" ["kulu"
+                           (let [calendar (Calendar/getInstance)]
+                             (.setTime calendar (Date.))
+                             (.get calendar Calendar/YEAR))]))
 
 (defn custom-information [values & content]
   [:CustomInformation
@@ -8,55 +15,67 @@
      [:ColumnValue {:name key} value])
    content])
 
-(defn muodosta-maksuera []
-  [:NikuDataBus
-   [:Header
-    {:objectType "product", :action "write", :externalSource "NIKU", :version "8.0"}]
-   [:Products
-    [:Product
-     {:name "Kokonaishintaiset työt 2015/11 (LA205081)", :financialProjectClass "INVCLASS", :start "2015-01-01T00:00:00.000", :finish "2015-12-31T01:01:00.000", :financialWipClass "WIPCLASS", :financialDepartment "KP911303", :managerUserName "A009717", :objectID "LA205081", :financialLocation "Kpito"}
-     [:InvestmentAssociations
-      [:Allocations
-       [:ParentInvestment
-        {:defaultAllocationPercent "1.0", :InvestmentType "project", :InvestmentID "PR00020606"}]]]
-     [:InvestmentResources
-      [:Resource
-       {:resourceID "kulu2015"}]]
-     [:InvestmentTasks
-      [:Task
-       {:outlineLevel "1", :name "Kokonaishintaiset työt 2015/11 (LA205081)", :taskID "~rmw"}
-       [:Assignments
-        [:TaskLabor
-         {:resourceID "kulu2015"}]]]]
-     [:OBSAssocs
-      {:completed "false"}
-      [:OBSAssoc#LiiviKP
-       {:unitPath "/Päivittäinen kunnossapito/Hoito", :name "Kustannuspaikat"}]
-      [:OBSAssoc#LiiviSIJ
-       {:unitPath "/Kirjanpito", :name "Sijainti"}]
-      [:OBSAssoc#tuote2013
-       {:unitPath "/Päivittäinen kunnossapito/Hoito", :name "Tuoteryhma/Tuote"}]]
-   (custom-information {"vv_tilaus"      "00LZM-0033600"
-                        "vv_inst_no"     "205081"
-                        "vv_code"        "LA205081"
-                        "vv_me_type"     "2"
-                        "vv_type"        "me"
-                        "vv_status"      "2"
-                        "travel_cost_ok" "false"
-                        }
-                       [:instance
-                        {:parentInstanceCode "LA205081", :parentObjectCode "Product", :objectCode "vv_invoice_receipt", :instanceCode "AL205081"}
-                        (custom-information {"code"                 "AL205081"
-                                             "vv_payment_date"      "FOO" ; (date-format me-paiva)
-                                             "vv_paym_sum"          123
-                                             "vv_paym_sum_currency" "EUR"
-                                             "name"                 "Laskutus- ja maksutiedot"})])] ] ] )
+(defn muodosta-maksueranumero [numero]
+  (clojure.string/join "" ["HA" numero]))
+
+(defn formatoi-paivamaara [date]
+  (.format (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss.S") date))
+
+(defn muodosta-maksuera [maksuera]
+  (let [{:keys [alkupvm loppupvm vastuuhenkilo_id talousosasto_id tuotepolku]} (:toimenpideinstanssi maksuera)
+        {:keys [sampoid]} (:urakka maksuera)
+        maksueranumero (muodosta-maksueranumero (:numero maksuera))]
+
+    [:NikuDataBus
+     [:Header {:objectType "product" :action "write" :externalSource "NIKU" :version "8.0"}]
+     [:Products
+      [:Product {:name                  (:nimi maksuera)
+                 :financialProjectClass "INVCLASS"
+                 :start                 (formatoi-paivamaara alkupvm)
+                 :finish                (formatoi-paivamaara loppupvm)
+                 :financialWipClass     "WIPCLASS"
+                 :financialDepartment   talousosasto_id
+                 :managerUserName       vastuuhenkilo_id
+                 :objectID              maksueranumero
+                 :financialLocation     "Kpito"}
+       [:InvestmentAssociations
+        [:Allocations
+         [:ParentInvestment {:defaultAllocationPercent "1.0" :InvestmentType "project" :InvestmentID sampoid}]]]
+       [:InvestmentResources
+        [:Resource {:resourceID (muodosta-kulu-id)}]]
+       [:InvestmentTasks
+        [:Task {:outlineLevel "1"
+                :name         (:nimi maksuera)
+                :taskID       "~rmw"}
+         [:Assignments
+          [:TaskLabor {:resourceID (muodosta-kulu-id)}]]]]
+       [:OBSAssocs {:completed "false"}
+        [:OBSAssoc#LiiviKP {:unitPath tuotepolku
+                            :name     "Kustannuspaikat"}]
+        [:OBSAssoc#LiiviSIJ {:unitPath "/Kirjanpito"
+                             :name     "Sijainti"}]
+        [:OBSAssoc#tuote2013 {:unitPath tuotepolku
+                              :name     "Tuoteryhma/Tuote"}]]
+       (custom-information {"vv_tilaus"      "00LZM-0033600"
+                            "vv_inst_no"     "205081"
+                            "vv_code"        "LA205081"
+                            "vv_me_type"     "2"
+                            "vv_type"        "me"
+                            "vv_status"      "2"
+                            "travel_cost_ok" "false"
+                            }
+                           [:instance {:parentInstanceCode "LA205081" :parentObjectCode "Product" :objectCode "vv_invoice_receipt" :instanceCode "AL205081"}
+                            (custom-information {"code"                 "AL205081"
+                                                 "vv_payment_date"      "FOO" ; (date-format me-paiva)
+                                                 "vv_paym_sum"          123
+                                                 "vv_paym_sum_currency" "EUR"
+                                                 "name"                 "Laskutus- ja maksutiedot"})])]]]))
 
 (defn laheta-maksuera [id]
   ;; lukitse maksuera
   ;; hae maksueran tiedot
   ;; muodosta maksueran xml-viesti
-  (muodosta-maksuera)
+  ;; (muodosta-maksuera)
 
   ;; lähetä jms jonoon xml
   ;; merkitse lähetetyksi, kirjaa lähetys id, vapauta lukko
--- test/clj/harja/palvelin/integraatiot/sampo_test.clj
+++ test/clj/harja/palvelin/integraatiot/sampo_test.clj
@@ -2,15 +2,33 @@
   (:require [clojure.test :refer [deftest is]]
             [harja.palvelin.integraatiot.sampo :refer [muodosta-maksuera]]
             [hiccup.core :refer [html]]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [clojure.xml :refer [parse]]
+            [clojure.zip :refer [xml-zip]]
+            [clojure.data.zip.xml :as z])
   (:import (javax.xml.validation SchemaFactory Schema Validator)
            (javax.xml XMLConstants)
            (javax.xml.transform.stream StreamSource)
            (java.io ByteArrayInputStream)
            (org.w3c.dom.ls LSResourceResolver LSInput)
-           (org.xml.sax SAXParseException)))
+           (org.xml.sax SAXParseException)
+           (java.text SimpleDateFormat)))
 
 (def +xsd-polku+ "test/xsd/sampo/outbound/")
+
+(defn parsi-paivamaara [teksti]
+  (.parse (SimpleDateFormat. "dd.MM.yyyy") teksti))
+
+(def +maksuera+ {:nimi                "Testimaksuera"
+                 :numero              123456789
+                 :toimenpideinstanssi {
+                                       :alkupvm          (parsi-paivamaara "12.12.2015")
+                                       :loppupvm         (parsi-paivamaara "1.1.2017")
+                                       :vastuuhenkilo_id "A009717"
+                                       :talousosasto_id  "talousosasto"
+                                       :tuotepolku       "polku/tuote"}
+                 :urakka              {
+                                       :sampoid "PR00020606"}})
 
 (defn validoi [xsd xml]
   (let
@@ -37,8 +55,18 @@
            (println "Invalidi XML: " e)
            false))))
 
-
 (deftest tarkista-maksueran-validius
-  (let [maksuera (html (muodosta-maksuera))
+  (let [maksuera (html (muodosta-maksuera +maksuera+))
         xsd "nikuxog_product.xsd"]
     (is (validoi xsd maksuera) "Muodostettu XML-tiedosto on XSD-skeeman mukainen")))
+
+(deftest tarkista-maksueran-sisalto
+  (let [maksuera-xml (xml-zip (parse (ByteArrayInputStream. (.getBytes (html (muodosta-maksuera +maksuera+)) "UTF-8"))))]
+    (is (= "2015-12-12T00:00:00.0" (z/xml1-> maksuera-xml :Products :Product (z/attr :start))))
+    (is (= "2017-01-01T00:00:00.0" (z/xml1-> maksuera-xml :Products :Product (z/attr :finish))))
+    (is (= "A009717" (z/xml1-> maksuera-xml :Products :Product (z/attr :managerUserName))))
+    (is (= "Testimaksuera" (z/xml1-> maksuera-xml :Products :Product (z/attr :name))))
+    (is (= "HA123456789" (z/xml1-> maksuera-xml :Products :Product (z/attr :objectID))))
+    (is (= "PR00020606" (z/xml1-> maksuera-xml :Products :Product :InvestmentAssociations :Allocations :ParentInvestment (z/attr :InvestmentID))))
+    (is (= "kulu2015" (z/xml1-> maksuera-xml :Products :Product :InvestmentTasks :Task :Assignments :TaskLabor (z/attr :resourceID))))
+    (is (= "Testimaksuera" (z/xml1-> maksuera-xml :Products :Product :InvestmentTasks :Task (z/attr :name))))))
