aebbb85250e0a51a2fc77210d255bc67bfca06fe
Jari Hanhela <jari.hanhela@solita.fi>
2015-05-15 10:12:13 +0300

    Lisätty funktiot maksuerille yhden tai usemaman numeron vastaanottamiseen.

--- src/clj/harja/palvelin/palvelut/maksuerat.clj
+++ src/clj/harja/palvelin/palvelut/maksuerat.clj
@@ -1,43 +1,47 @@
 (ns harja.palvelin.palvelut.maksuerat
-  (:require [com.stuartsierra.component :as component]
-            [harja.palvelin.komponentit.http-palvelin :refer [julkaise-palvelu poista-palvelu]]
-            [harja.kyselyt.maksuerat :as q]
-            [harja.palvelin.integraatiot.sampo :as sampo]
-            [harja.kyselyt.konversio :as konversio]))
+    (:require [com.stuartsierra.component :as component]
+              [harja.palvelin.komponentit.http-palvelin :refer [julkaise-palvelu poista-palvelu]]
+              [harja.kyselyt.maksuerat :as q]
+              [harja.palvelin.integraatiot.sampo :as sampo]
+              [harja.kyselyt.konversio :as konversio]))
 
 (declare hae-urakan-maksuerat)
-(declare laheta-maksuera-sampoon)
+(declare laheta-maksuerat-sampoon)
 
 (defrecord Maksuerat []
-  component/Lifecycle
-  (start [this]
-    (julkaise-palvelu (:http-palvelin this)
-                      :hae-urakan-maksuerat (fn [user urakka-id]
-                                              (hae-urakan-maksuerat (:db this) user urakka-id)))
-    (julkaise-palvelu (:http-palvelin this)
-                      :laheta-maksuera-sampoon (fn [user maksueranumero]
-                                                  (laheta-maksuera-sampoon (:sampo this) user maksueranumero)))
-    this)
+    component/Lifecycle
+    (start [this]
+        (julkaise-palvelu (:http-palvelin this)
+                          :hae-urakan-maksuerat (fn [user urakka-id]
+                                                    (hae-urakan-maksuerat (:db this) user urakka-id)))
+        (julkaise-palvelu (:http-palvelin this)
+                          :laheta-maksuerat-sampoon (fn [user maksueranumerot]
+                                                        (laheta-maksuerat-sampoon (:sampo this) user maksueranumerot)))
+        this)
 
-  (stop [this]
-    (poista-palvelu (:http-palvelin this) :hae-urakan-maksuerat)
-    (poista-palvelu (:http-palvelin this) :laheta-maksuera-sampoon)
-    this))
+    (stop [this]
+        (poista-palvelu (:http-palvelin this) :hae-urakan-maksuerat)
+        (poista-palvelu (:http-palvelin this) :laheta-maksuerat-sampoon)
+        this))
 
 (defn hae-urakan-maksuerat
-  "Palvelu, joka palauttaa urakan maksuerät."
-  [db user urakka-id]
-  (into []
-        ;; FIXME: Oikeustarkistukset?
-        (map konversio/alaviiva->rakenne (q/hae-urakan-maksuerat db urakka-id))))
+    "Palvelu, joka palauttaa urakan maksuerät."
+    [db user urakka-id]
+    (into []
+          ;; FIXME: Oikeustarkistukset?
+          (map konversio/alaviiva->rakenne (q/hae-urakan-maksuerat db urakka-id))))
 
 (defn laheta-maksuera-sampoon
-  "Palvelu, joka lähettää annetun maksuerän Sampoon."
-  [sampo user maksueranumero]
-  (into []
-        ;; FIXME: Oikeustarkistukset?
-        (sampo/laheta-maksuera-sampoon sampo maksueranumero)))
+    "Palvelu, joka lähettää annetun maksuerän Sampoon."
+    [sampo user maksueranumero]
+    (into []
+          ;; FIXME: Oikeustarkistukset?
+          (sampo/laheta-maksuera-sampoon sampo maksueranumero)))
 
-  
-  
-  
+(defn laheta-maksuerat-sampoon
+    "Palvelu, joka lähettää annetun maksuerän Sampoon."
+    [sampo user maksueranumerot]
+    (into []
+          ;; FIXME: Oikeustarkistukset?
+          (mapv (fn [maksueranumero] (laheta-maksuera-sampoon sampo user maksueranumero)) maksueranumerot)))
+
