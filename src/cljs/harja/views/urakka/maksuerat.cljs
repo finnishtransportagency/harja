4b34ca1d7d3f65187e9fca527f1ca3fb066cf0d6
Jari Hanhela <jari.hanhela@solita.fi>
2015-05-15 15:56:15 +0300

    Lajittelee maksuerät tyypin mukaan.

--- src/cljs/harja/views/urakka/maksuerat.cljs
+++ src/cljs/harja/views/urakka/maksuerat.cljs
@@ -37,7 +37,7 @@
     (let [lahetys-kaynnissa (atom false)
           maksuerarivit (atom nil)
           hae-urakan-maksuerat (fn [ur]
-                                  (go (reset! maksuerarivit (<! (maksuerat/hae-urakan-maksuerat (:id ur))))))
+                                  (go (reset! maksuerarivit (sort-by :tyyppi (<! (maksuerat/hae-urakan-maksuerat (:id ur)))))))
           laheta-maksuera (fn [maksueranumero]
                               (go (let [res (<! (maksuerat/laheta-maksuerat [maksueranumero]))]
                                       (reset! lahetys-kaynnissa true)
