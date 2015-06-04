75d778746dcf6f66869f8c9c30ed5e24e1c01cba
mikkoro <mikko.ronkkomaki@solita.fi>
2015-06-04 08:55:33 +0300

    Lisää apufunktiot tehtävien ajastamiselle

--- project.clj
+++ project.clj
@@ -85,6 +85,8 @@
                  [org.apache.poi/poi-ooxml "3.11"] ;; .xlsx tiedoston lukua varten
                  [org.clojure/data.json "0.2.6"]
 
+                 ;; Chime -ajastuskirjasto
+                 [jarohen/chime "0.1.6"]
                  
                  ]
   
---
+++ src/clj/harja/palvelin/tyokalut/ajastettu_tehtava.clj
@@ -0,0 +1,17 @@
+(ns harja.palvelin.tyokalut.ajastettu-tehtava
+  "Apufunktioita tehtävien ajastamiseen"
+
+  (:require
+    [chime :refer [chime-at]]
+    [clj-time.core :as t]
+    [clj-time.core :as t]
+    [clj-time.periodic :refer [periodic-seq]])
+  (:import (org.joda.time DateTime DateTimeZone)))
+
+(defn ajasta-paivittain [[tunti minuutti sekuntti] tehtava]
+  (chime-at (periodic-seq
+              (.. (t/now)
+                  (withZone (DateTimeZone/forID "Europe/Helsinki"))
+                  (withTime tunti minuutti sekuntti 0))
+              (t/days 1))
+            tehtava))
