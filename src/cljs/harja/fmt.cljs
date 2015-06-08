5275b4deda4eafcb90463e57bcdd82c72a9f48f4
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-06-08 11:08:20 +0300

    Euro formatointi käyttäen goog.i18n.

--- src/cljs/harja/fmt.cljs
+++ src/cljs/harja/fmt.cljs
@@ -1,11 +1,25 @@
 (ns harja.fmt
   "Yleisiä apureita erityyppisen datan formatointiin."
-  (:require [harja.pvm :as pvm]))
+  (:require [harja.pvm :as pvm]
+            [goog.i18n.currencyCodeMap]
+            [goog.i18n.NumberFormatSymbols]
+            [goog.i18n.NumberFormatSymbols_fi_FI]
+            [goog.i18n.NumberFormat]))
 
-(defn euro
+(set! goog.i18n.NumberFormatSymbols goog.i18n.NumberFormatSymbols_fi_FI)
+
+(def euro-number-format (doto (goog.i18n.NumberFormat. (.-DECIMAL goog.i18n.NumberFormat/Format))
+                          (.setShowTrailingZeros false)
+                          (.setMinimumFractionDigits 2)
+                          (.setMaximumFractionDigits 2)))
+
+(defn euro [eur]
   "Formatoi summan euroina näyttämistä varten. Tuhaterottimien ja euromerkin kanssa."
-  [summa]
-  (str (.toLocaleString summa) " \u20AC"))
+  ;; NOTE: lisätään itse perään euro symboli, koska googlella oli jotain ihan sotkua.
+  ;; Käytetään googlen formatointia, koska toLocaleString tukee tarvittavia optioita, mutta
+  ;; vasta IE11 versiosta lähtien. 
+  (str (.format euro-number-format eur) " \u20AC"))
+
 
 (defn euro-opt
   "Formatoi euromäärän tai tyhjä, jos nil."
