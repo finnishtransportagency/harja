49e4b19ce8cbfe7edb687e1bdf65a2e7bd2c926a
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-06-12 15:08:59 +0300

    Poistettu lokitusta reaction<! makrosta.
    Luotetaan, että se toimii jo riittävän varmasti.

--- src/cljs/harja/atom.clj
+++ src/cljs/harja/atom.clj
@@ -11,24 +11,15 @@ Optionaalisesti ottaa formin, jolla tulos käsitellään ennen atomin tilaksi as
   ([haku prosessoi] 
      `(let [kaynnissa# (cljs.core/atom 0) ; käynnissä olevan reaktiohaun numero
             arvo# (reagent.core/atom nil)] ; itse atomin arvo
-        (harja.loki/log "reaction<!  luotu uusi asynk reaktio..")
         (reagent.ratom/run!
          (let [num# (swap! kaynnissa# inc)]
-           (harja.loki/log "reaction<!  run! lohkossa, ennen go lohkoa, num = " num#)
            (let [chan# ~haku]
-             (harja.loki/log "reaction<!  body palautti: " chan#)
              (if (nil? chan#)
                (when (= num# @kaynnissa#)
                  (reset! arvo# nil))
                (cljs.core.async.macros/go
                  (let [res# (cljs.core.async/<! chan#)]
-                   (harja.loki/log "reaction<!  hakutulos saatu: " (pr-str res#))
-                   (if (= num# @kaynnissa#)
-                     (do
-                       (harja.loki/log "reaction<!  pyynnön numero sama, asetetaan atomin tila")
-                       (reset! arvo# ((or ~prosessoi identity) res#)))
-                     (do
-                       (harja.loki/log "reaction<!  vanha pyynnön numero " num# " => ei aseteta atomin tilaa")))))))))
-        (harja.loki/log "reaction<!  run! ajettu...")
+                   (when (= num# @kaynnissa#)
+                     (reset! arvo# ((or ~prosessoi identity) res#)))))))))
         arvo#)))
             
