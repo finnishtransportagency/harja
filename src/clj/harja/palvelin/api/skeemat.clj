46f4ac8ddcbdd403f31f26359c8242955b45e201
mikkoro <mikko.ronkkomaki@solita.fi>
2015-06-15 12:56:06 +0300

    Korjaa JSON-validointitestien namespacen

--- src/clj/harja/palvelin/api/skeemat.clj
+++ src/clj/harja/palvelin/api/skeemat.clj
@@ -1,3 +1,5 @@
-(ns )
+(ns harja.palvelin.api.skeemat)
 
 (def +urakan-haku-vastaus+ "")
+
+(def +virhevastaus+ "api/schemas/virhe-response.schema.json")
--- test/clj/harja/tyokalut/json_validointi_test.clj
+++ test/clj/harja/tyokalut/json_validointi_test.clj
@@ -1,9 +1,12 @@
-(ns harja.palvelin.integraatiot.sampo.json-validointi-test
+(ns harja.tyokalut.json-validointi-test
   (:require [clojure.test :refer [deftest is use-fixtures]]
             [harja.testi :refer :all]
             [harja.tyokalut.json_validointi :as json]
+            [harja.palvelin.api.skeemat :as skeemat]
             [clojure.java.io :as io]))
+
+
 
 (deftest tarkista-json-datan-validius
   (let [json-data (slurp (io/resource "api/examples/virhe-response.json"))]
-    (is (json/validoi "api/schemas/virhe-response.schema.json" json-data))))
+    (is (json/validoi skeemat/+virhevastaus+ json-data))))
