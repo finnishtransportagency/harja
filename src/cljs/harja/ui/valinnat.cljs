6d184b8abd5b00717c9ce53d48ae3958acb38ad9
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2015-06-24 09:26:02 +0300

    Tee urakan valintakomponenteista yleisiä
    Valintakomponentit ovat edelleen luonnollisesti hyvin sidottuja
    urakkaan, mutta nyt niille on mahdollista antaa omia atomeita.
    
    urakka.valinnat namespace on edelleen olemassa, ja sitä voi käyttää
    kuten ennenkin, mutta tämä namespace käyttää uutta, ui.valinnat
    namespacea.

---
+++ src/cljs/harja/ui/valinnat.cljs
@@ -0,0 +1,118 @@
+(ns harja.ui.valinnat
+  "Yleisiä valintoihin liittyviä komponentteja.
+  Refaktoroitu vanhasta harja.views.urakka.valinnat namespacesta."
+  (:require [reagent.core :refer [atom] :as r]
+
+            [harja.pvm :as pvm]
+            [harja.loki :refer [log]]
+            [harja.ui.kentat :refer [tee-kentta]]
+            [harja.ui.yleiset :refer [livi-pudotusvalikko]]))
+
+(defn urakan-sopimus
+  [ur valittu-sopimusnumero-atom valitse-fn]
+  [:div.label-ja-alasveto
+   [:span.alasvedon-otsikko "Sopimusnumero"]
+   [livi-pudotusvalikko {:valinta @valittu-sopimusnumero-atom
+                         :format-fn second
+                         :valitse-fn valitse-fn
+                         :class "suunnittelu-alasveto"
+                         }
+    (:sopimukset ur)]])
+
+(defn urakan-hoitokausi
+  [ur hoitokaudet valittu-hoitokausi-atom valitse-fn]
+    [:div.label-ja-alasveto
+     [:span.alasvedon-otsikko (if (= :hoito (:tyyppi ur)) "Hoitokausi" "Sopimuskausi")]
+     [livi-pudotusvalikko {:valinta @valittu-hoitokausi-atom
+                           ;;\u2014 on väliviivan unikoodi
+                           :format-fn #(if % (str (pvm/pvm (first %))
+                                                  " \u2014 " (pvm/pvm (second %))) "Valitse")
+                           :valitse-fn valitse-fn
+                           :class "suunnittelu-alasveto"
+                           }
+      hoitokaudet]])
+
+(defn hoitokauden-aikavali
+  [valittu-aikavali-atom]
+  ; TODO Harjaa vastaava tyyli tälle
+    [:span.label-ja-aikavali
+     [:span.alasvedon-otsikko "Aikaväli"]
+     [:div.aikavali-valinnat
+      [tee-kentta {:tyyppi :pvm :irrallinen? true}
+       (r/wrap (first @valittu-aikavali-atom)
+               (fn [uusi-arvo]
+                 (reset! valittu-aikavali-atom [uusi-arvo
+                                           (second (pvm/kuukauden-aikavali uusi-arvo))])))]
+      [:span " \u2014 "]
+      [tee-kentta {:tyyppi :pvm :irrallinen? true}
+       (r/wrap (second @valittu-aikavali-atom)
+               (fn [uusi-arvo]
+                 (swap! valittu-aikavali-atom (fn [[alku _]] [alku uusi-arvo]))))]
+      ]])
+
+(defn urakan-toimenpide
+  [urakan-toimenpideinstanssit-atom valittu-toimenpideinstanssi-atom valitse-fn]
+  [:div.label-ja-alasveto
+   [:span.alasvedon-otsikko "Toimenpide"]
+   [livi-pudotusvalikko {:valinta    @valittu-toimenpideinstanssi-atom
+                         ;;\u2014 on väliviivan unikoodi
+                         :format-fn  #(if % (str (:tpi_nimi %)) "Ei toimenpidettä")
+                         :valitse-fn valitse-fn}
+    @urakan-toimenpideinstanssit-atom]])
+
+;; Parametreja näissä on melkoisen hurja määrä, mutta ei voi mitään
+(defn urakan-sopimus-ja-hoitokausi
+  [ur
+   valittu-sopimusnumero-atom valitse-sopimus-fn                                            ;; urakan-sopimus
+   hoitokaudet valittu-hoitokausi-atom valitse-hoitokausi-fn]                               ;; urakan-hoitokausi
+
+  [:span
+   [urakan-sopimus ur valittu-sopimusnumero-atom valitse-sopimus-fn]
+   [urakan-hoitokausi ur hoitokaudet valittu-hoitokausi-atom valitse-hoitokausi-fn]])
+
+
+
+(defn urakan-sopimus-ja-toimenpide
+  [ur
+   valittu-sopimusnumero-atom valitse-sopimus-fn                                            ;; urakan-sopimus
+   urakan-toimenpideinstassit-atom valittu-toimenpideinstanssi-atom valitse-toimenpide-fn]  ;; urakan-toimenpide
+
+  [:span
+   [urakan-sopimus ur valittu-sopimusnumero-atom valitse-sopimus-fn]
+   [urakan-toimenpide urakan-toimenpideinstassit-atom valittu-toimenpideinstanssi-atom valitse-toimenpide-fn]])
+
+
+
+(defn urakan-sopimus-ja-hoitokausi-ja-toimenpide
+  [ur
+   valittu-sopimusnumero-atom valitse-sopimus-fn                                            ;; urakan-sopimus
+   hoitokaudet valittu-hoitokausi-atom valitse-hoitokausi-fn                                ;; urakan-hoitokausi
+   urakan-toimenpideinstassit-atom valittu-toimenpideinstanssi-atom valitse-toimenpide-fn]  ;; urakan-toimenpide
+
+  [:span
+   [urakan-sopimus-ja-hoitokausi
+    ur
+    valittu-sopimusnumero-atom valitse-sopimus-fn
+    hoitokaudet valittu-hoitokausi-atom valitse-hoitokausi-fn]
+
+   [urakan-toimenpide urakan-toimenpideinstassit-atom valittu-toimenpideinstanssi-atom valitse-toimenpide-fn]])
+
+
+
+(defn urakan-sopimus-ja-hoitokausi-ja-aikavali-ja-toimenpide
+  [ur
+   valittu-sopimusnumero-atom valitse-sopimus-fn                                            ;; urakan-sopimus
+   hoitokaudet valittu-hoitokausi-atom valitse-hoitokausi-fn                                ;; urakan-hoitokausi
+   valittu-aikavali-atom                                                                    ;; hoitokauden-aikavali
+   urakan-toimenpideinstassit-atom valittu-toimenpideinstanssi-atom valitse-toimenpide-fn]  ;; urakan-toimenpide
+
+  [:span
+
+   [urakan-sopimus-ja-hoitokausi
+    ur
+    valittu-sopimusnumero-atom valitse-sopimus-fn
+    hoitokaudet valittu-hoitokausi-atom valitse-hoitokausi-fn]
+
+   [hoitokauden-aikavali valittu-aikavali-atom]
+
+   [urakan-toimenpide urakan-toimenpideinstassit-atom valittu-toimenpideinstanssi-atom valitse-toimenpide-fn]])
--- src/cljs/harja/views/urakka/valinnat.cljs
+++ src/cljs/harja/views/urakka/valinnat.cljs
@@ -1,82 +1,47 @@
 (ns harja.views.urakka.valinnat
   "Yleiset urakkaan liittyvät valintakomponentit."
-  (:require [reagent.core :refer [atom] :as r]
-            [bootstrap :as bs]
-            
-            [harja.tiedot.urakka :as u]
+  (:require [harja.tiedot.urakka :as u]
             [harja.pvm :as pvm]
-            [harja.ui.pvm :as ui-pvm]
             [harja.loki :refer [log]]
             [harja.ui.kentat :refer [tee-kentta]]
-            [harja.ui.yleiset :refer [livi-pudotusvalikko]]))
+            [harja.ui.yleiset :refer [livi-pudotusvalikko]]
+            [harja.ui.valinnat :as valinnat]))
 
 (defn urakan-sopimus [ur]
-  [:div.label-ja-alasveto
-   [:span.alasvedon-otsikko "Sopimusnumero"]
-   [livi-pudotusvalikko {:valinta @u/valittu-sopimusnumero
-                         :format-fn second
-                         :valitse-fn u/valitse-sopimusnumero!
-                         :class "suunnittelu-alasveto"
-                         }
-    (:sopimukset ur)]])
+  (valinnat/urakan-sopimus ur u/valittu-sopimusnumero u/valitse-sopimusnumero!))
 
 (defn urakan-hoitokausi [ur]
-  (let [hoitokaudet (u/hoitokaudet ur)]
-    [:div.label-ja-alasveto
-     [:span.alasvedon-otsikko (if (= :hoito (:tyyppi ur)) "Hoitokausi" "Sopimuskausi")]
-     [livi-pudotusvalikko {:valinta @u/valittu-hoitokausi
-                           ;;\u2014 on väliviivan unikoodi
-                           :format-fn #(if % (str (pvm/pvm (first %))
-                                                  " \u2014 " (pvm/pvm (second %))) "Valitse")
-                           :valitse-fn u/valitse-hoitokausi!
-                           :class "suunnittelu-alasveto"
-                           }
-      hoitokaudet]]))
+  (valinnat/urakan-hoitokausi ur (u/hoitokaudet ur) u/valittu-hoitokausi u/valitse-hoitokausi!))
 
 (defn hoitokauden-aikavali [ur]
-                                        ; TODO Harjaa vastaava tyyli tälle
-  (let [valittu-aikavali u/valittu-aikavali]
-    [:span.label-ja-aikavali
-     [:span.alasvedon-otsikko "Aikaväli"]
-     [:div.aikavali-valinnat
-      [tee-kentta {:tyyppi :pvm :irrallinen? true}
-       (r/wrap (first @valittu-aikavali)
-               (fn [uusi-arvo]
-                 (reset! valittu-aikavali [uusi-arvo
-                                           (second (pvm/kuukauden-aikavali uusi-arvo))])))]
-      [:span " \u2014 "]
-      [tee-kentta {:tyyppi :pvm :irrallinen? true}
-       (r/wrap (second @valittu-aikavali)
-               (fn [uusi-arvo]
-                 (swap! valittu-aikavali (fn [[alku _]] [alku uusi-arvo]))))]
-      ]]))
+  (valinnat/hoitokauden-aikavali u/valittu-aikavali))
 
 (defn urakan-toimenpide []
-  [:div.label-ja-alasveto
-   [:span.alasvedon-otsikko "Toimenpide"]
-   [livi-pudotusvalikko {:valinta    @u/valittu-toimenpideinstanssi
-                         ;;\u2014 on väliviivan unikoodi
-                         :format-fn  #(if % (str (:tpi_nimi %)) "Ei toimenpidettä")
-                         :valitse-fn u/valitse-toimenpideinstanssi!}
-    @u/urakan-toimenpideinstanssit]])
+  (valinnat/urakan-toimenpide u/urakan-toimenpideinstanssit u/valittu-toimenpideinstanssi u/valitse-toimenpideinstanssi!))
 
 (defn urakan-sopimus-ja-hoitokausi [ur]
-  [:span
-   [urakan-sopimus ur]
-   [urakan-hoitokausi ur]])
+  (valinnat/urakan-sopimus-ja-hoitokausi
+    ur
+    u/valittu-sopimusnumero u/valitse-sopimusnumero!
+    (u/hoitokaudet ur) u/valittu-hoitokausi u/valitse-hoitokausi!))
 
 (defn urakan-sopimus-ja-toimenpide [ur]
-  [:span
-   [urakan-sopimus ur]
-   [urakan-toimenpide ur]])
+  (valinnat/urakan-sopimus-ja-toimenpide
+    ur
+    u/valittu-sopimusnumero u/valitse-sopimusnumero!
+    u/urakan-toimenpideinstanssit u/valittu-toimenpideinstanssi u/valitse-toimenpideinstanssi!))
 
 (defn urakan-sopimus-ja-hoitokausi-ja-toimenpide [ur]
-  [:span
-   [urakan-sopimus-ja-hoitokausi ur]
-   [urakan-toimenpide ur]])
+  (valinnat/urakan-sopimus-ja-hoitokausi-ja-toimenpide
+    ur
+    u/valittu-sopimusnumero u/valitse-sopimusnumero!
+    (u/hoitokaudet ur) u/valittu-hoitokausi u/valitse-hoitokausi!
+    u/urakan-toimenpideinstanssit u/valittu-toimenpideinstanssi u/valitse-toimenpideinstanssi!))
 
 (defn urakan-sopimus-ja-hoitokausi-ja-aikavali-ja-toimenpide [ur]
-  [:span
-   [urakan-sopimus-ja-hoitokausi ur]
-   [hoitokauden-aikavali ur]
-   [urakan-toimenpide ur]])
+  (valinnat/urakan-sopimus-ja-hoitokausi-ja-aikavali-ja-toimenpide
+    ur
+    u/valittu-sopimusnumero u/valitse-sopimusnumero!
+    (u/hoitokaudet ur) u/valittu-hoitokausi u/valitse-hoitokausi!
+    u/valittu-aikavali
+    u/urakan-toimenpideinstanssit u/valittu-toimenpideinstanssi u/valitse-toimenpideinstanssi!))
