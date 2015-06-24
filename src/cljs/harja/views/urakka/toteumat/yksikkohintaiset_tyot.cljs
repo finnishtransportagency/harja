245582588841f908c8d53c842296fc344e931cbe
Jari Hanhela <jari.hanhela@solita.fi>
2015-06-24 15:18:30 +0300

    Lisää max pituudet tekstikentille toteumien kirjaamisessa.

--- src/cljs/harja/views/urakka/toteumat/yksikkohintaiset_tyot.cljs
+++ src/cljs/harja/views/urakka/toteumat/yksikkohintaiset_tyot.cljs
@@ -166,9 +166,9 @@
                                                                         [:pvm-kentan-jalkeen :alkanut "Lopetuksen pitää olla aloituksen jälkeen"]] :leveys-col 2}
            (if  jarjestelman-lisaama-toteuma? {:otsikko "Lähde" :nimi :luoja :tyyppi :string :hae (fn [rivi] (str "Järjestelmä (" (:luoja rivi) " / " (:organisaatio rivi) ")")) :muokattava? (constantly false)})
            {:otsikko "Tehtävät" :nimi :tehtavat :leveys "20%" :tyyppi :komponentti :komponentti [tehtavat-ja-maarat lomake-tehtavat jarjestelman-lisaama-toteuma?]}
-           {:otsikko "Suorittaja" :nimi :suorittajan-nimi :tyyppi :string :muokattava? (constantly (not jarjestelman-lisaama-toteuma?))}
-           {:otsikko "Suorittajan Y-tunnus" :nimi :suorittajan-ytunnus :tyyppi :string :muokattava? (constantly (not jarjestelman-lisaama-toteuma?))}
-           {:otsikko "Lisätieto" :nimi :lisatieto :tyyppi :text :muokattava? (constantly (not jarjestelman-lisaama-toteuma?)) :koko [80 :auto]}]
+           {:otsikko "Suorittaja" :nimi :suorittajan-nimi :pituus-max 256 :tyyppi :string :muokattava? (constantly (not jarjestelman-lisaama-toteuma?))}
+           {:otsikko "Suorittajan Y-tunnus" :nimi :suorittajan-ytunnus :pituus-max 256  :tyyppi :string :muokattava? (constantly (not jarjestelman-lisaama-toteuma?))}
+           {:otsikko "Lisätieto" :nimi :lisatieto :pituus-max 256 :tyyppi :text :muokattava? (constantly (not jarjestelman-lisaama-toteuma?)) :koko [80 :auto]}]
           @lomake-toteuma]]))))
 
 (defn yksiloidyt-tehtavat [rivi tehtavien-summat]
