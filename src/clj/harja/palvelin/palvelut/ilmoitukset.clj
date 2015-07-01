4d4bf411299073382d7835b91cf726018f8c22aa
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2015-07-01 17:27:28 +0300

    Muotoile ilmoituspalvelun vastaus

--- src/clj/harja/palvelin/palvelut/ilmoitukset.clj
+++ src/clj/harja/palvelin/palvelut/ilmoitukset.clj
@@ -6,6 +6,8 @@
             [clojure.java.jdbc :as jdbc]
             [taoensso.timbre :as log]
             [harja.domain.roolit :as roolit]
+            [clj-time.core :as t]
+            [clj-time.coerce :refer [from-sql-time]]
 
             [harja.kyselyt.ilmoitukset :as q]))
 
@@ -46,12 +48,14 @@
         _ (log/debug "Suljetut ja/tai avoimet?" (if (:suljetut tilat) true false) (if (:avoimet tilat) true false))
         mankeloitava (into []
                            (comp
+                             (harja.geo/muunna-pg-tulokset :sijainti)
                              (map konv/alaviiva->rakenne)
                              (map #(assoc % :urakkatyyppi (keyword (:urakkatyyppi %))))
-                             (map #(assoc % :ilmoituksenselite (keyword (:ilmoituksenselite %))))
-                             (map #(assoc % :kuittaustyyppi (keyword (:kuittaustyyppi %))))
+                             (map #(konv/array->vec % :selitteet))
+                             (map #(assoc % :selitteet (mapv keyword (:selitteet %))))
+                             (map #(assoc-in % [:kuittaus :kuittaustyyppi] (keyword (get-in % [:kuittaus :kuittaustyyppi]))))
                              (map #(assoc % :ilmoitustyyppi (keyword (:ilmoitustyyppi %))))
-                             (map #(assoc % :ilmoittajatyyppi (keyword (:ilmoittajatyyppi %)))))
+                             (map #(assoc-in % [:ilmoittaja :tyyppi] (keyword (get-in % [:ilmoittaja :tyyppi])))))
                            (q/hae-ilmoitukset db
                                               (annettu? hallintayksikko) (annettu? urakka)
                                               hallintayksikko urakka
@@ -61,9 +65,39 @@
                                               (annettu? hakuehto) (str "%" hakuehto "%")
                                               (if (:suljetut tilat) true false) ;; Muuttaa nil arvon tai puuttuvan avaimen
                                               (if (:avoimet tilat) true false) ;; falseksi
-                                              ))]
+                                              ))
+        mankeloitu (mapv
+                     #(assoc % :kuittaukset
+                               (into []
+                                     (keep
+                                       (fn [ilm]
+                                         (when
+                                           (and (not (nil? (:id (:kuittaus ilm))))
+                                                (= (:id ilm) (:id %)))
+                                           (:kuittaus ilm)))
+                                       mankeloitava)))
+                     (set (map #(dissoc % :kuittaus) mankeloitava)))
 
-    (log/debug mankeloitava)))
+        tulos (mapv
+                #(assoc % :uusinkuittaus
+                          (when-not (empty? (:kuittaukset %))
+                            (reduce
+                              (fn [a b]
+                                (if-not (:kuitattu a)
+                                  (:kuitattu b)
+
+                                  (if-not (:kuitattu b)
+                                    (:kuitattu a)
+
+                                    (if (t/after? (from-sql-time (:kuitattu a)) (from-sql-time (:kuitattu b)))
+                                      (:kuitattu a)
+                                      (:kuitattu b)))))
+                              (:kuittaukset %))))
+                mankeloitu)]
+
+    (log/debug (pr-str tulos))
+    (log/debug "Löydettiin ilmoitukset: " (map :id mankeloitu))
+    (log/debug "Kuittauksilla: " (map :uusinkuittaus tulos))))
 
 (defrecord Ilmoitukset []
   component/Lifecycle
