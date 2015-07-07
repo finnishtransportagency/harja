5920681058f9c563631dcaee949de5256acc6c37
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-07-07 14:58:11 +0300

    Poistaa vahingossa commitoidun rikkinäisen testin.

--- test/clj/harja/palvelin/palvelut/paallystys_test.clj
+++ test/clj/harja/palvelin/palvelut/paallystys_test.clj
@@ -17,7 +17,13 @@
           :http-palvelin (testi-http-palvelin)
           :urakan-paallystyskohteet (component/using
                                         (->Paallystys)
-                                        [:http-palvelin :db])))))
+                                        [:http-palvelin :db])
+          :urakan-paallystysilmoitus-paallystyskohteella (component/using
+                                      (->Paallystys)
+                                      [:http-palvelin :db])
+          :tallenna-paallystysilmoitus (component/using
+                                      (->Paallystys)
+                                      [:http-palvelin :db])))))
 
   (testit)
   (alter-var-root #'jarjestelma component/stop))
@@ -37,30 +43,3 @@
                                       FROM paallystyskohde
                                       WHERE sopimus IN (SELECT id FROM sopimus WHERE urakka = " @muhoksen-paallystysurakan-id ")")))]
     (is (= (count res) kohteiden-lkm) "Päällystyskohteiden määrä")))
-
-(deftest tallenna-paallystysilmoitus
-  (let [paallystysilmoitus
-        {:alkupvm hoitokauden-alkupvm
-            :loppupvm hoitokauden-loppupvm
-            :pvm toteuman-pvm :rahasumma 20000.0
-            :indeksin_nimi "MAKU 2005" :toimenpideinstanssi 1 :sopimus 1
-            :tyyppi "asiakastyytyvaisyysbonus" :lisatieto toteuman-lisatieto}
-        maara-ennen-lisaysta (ffirst (q
-                                       (str "SELECT count(*)
-                                                       FROM erilliskustannus
-                                                      WHERE sopimus IN (SELECT id FROM sopimus WHERE urakka = " @oulun-alueurakan-id
-                                            ") AND pvm >= '2005-10-01' AND pvm <= '2006-09-30'")))
-        res (kutsu-palvelua (:http-palvelin jarjestelma)
-                            :tallenna-paallystysilmoitus +kayttaja-jvh+ ek)
-        lisatty (first (filter #(and
-                                 (= (:pvm %) toteuman-pvm)
-                                 (= (:lisatieto %) toteuman-lisatieto)) res))]
-    (is (= (:pvm lisatty) toteuman-pvm) "Tallennetun erilliskustannuksen pvm")
-    (is (= (:lisatieto lisatty) toteuman-lisatieto) "Tallennetun erilliskustannuksen lisätieto")
-    (is (= (:indeksin_nimi lisatty) "MAKU 2005") "Tallennetun erilliskustannuksen indeksin nimi")
-    (is (= (:rahasumma lisatty) 20000.0) "Tallennetun erilliskustannuksen pvm")
-    (is (= (:toimenpideinstanssi lisatty) 1) "Tallennetun erilliskustannuksen tp")
-    (is (= (count res) (+ 1 maara-ennen-lisaysta)) "Tallennuksen jälkeen erilliskustannusten määrä")
-    (u
-      (str "DELETE FROM paallystysilmoitus
-                    WHERE pvm = '2005-12-12' AND lisatieto = '" toteuman-lisatieto "'"))))
