8004872d87ae89fe09e59b4c8ee22591e0c612f0
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-07-08 12:21:23 +0300

    Urakoiden ryhmittely käynnissä/päättynyt.

--- src/cljs/harja/views/urakat.cljs
+++ src/cljs/harja/views/urakat.cljs
@@ -15,23 +15,9 @@
             [harja.asiakas.kommunikaatio :as k]
             [harja.tiedot.navigaatio :as nav]
 
-            [harja.views.urakka :as urakka])
+            [harja.views.urakka :as urakka]
+            [harja.pvm :as pvm])
   (:require-macros [cljs.core.async.macros :refer [go]]))
-
-
-
-(defn wms-url
-  "Määrittelee WMS osoitteen, suhteellisena sovelluksen osoitteeseen."
-  []
-  (let [l (.-location js/document)
-        port (.-port l)]
-    (str (.-protocol l)
-         "//"
-         (.-hostname l)
-         (when-not (or (= port "80")
-                       (= port "443"))
-           (str ":" port))
-         "/wms/rasteriaineistot/image?")))
 
 
 (defn urakat
@@ -40,34 +26,42 @@
   [:span
    ;; TODO: urakkasivun koon (col-sm-?) oltava dynaaminen perustuen kartan kokoon joka on navigaatio.cljs:ssä
    
-    (let [v-hal @nav/valittu-hallintayksikko
-          v-ur @nav/valittu-urakka
-          urakkalista @nav/urakkalista]
-      (if-not v-hal
-        ;; Hallintayksikköä ei ole valittu: näytetään lista hallintayksiköistä
-        [:span
-         [:h5.haku-otsikko "Valitse hallintayksikkö"]
-         [:div
-          ^{:key "hy-lista"}
-          [suodatettu-lista {:format :nimi :haku :nimi
-                             :selection nav/valittu-hallintayksikko
-                             :on-select nav/valitse-hallintayksikko
-                             :aputeksti "Kirjoita hallintayksikön nimi tähän"}
-           @hal/hallintayksikot]]]
-        
-        ;; Hallintayksikko on valittu, mutta urakkaa ei: näytetään luettelossa urakat
-        (if-not v-ur
-            (if (nil? urakkalista)
-              [yleiset/ajax-loader "Urakoita haetaan..."]
-              [:span
-               [:h5.haku-otsikko "Valitse hallintayksikön urakka"]
-               [:div
-                ^{:key "ur-lista"}
-                [suodatettu-lista {:format :nimi :haku :nimi
-                                   :selection nav/valittu-urakka
-                                   :on-select nav/valitse-urakka
-                                   :aputeksti "Kirjoita urakan nimi tähän"}
-                 @nav/suodatettu-urakkalista]]])
-          
-            ;; Urakka valittu, tähän kaikki urakan komponentit
-            [urakka/urakka v-ur])))])
+   (let [v-hal @nav/valittu-hallintayksikko
+         v-ur @nav/valittu-urakka
+         urakkalista @nav/urakkalista]
+     (if-not v-hal
+       ;; Hallintayksikköä ei ole valittu: näytetään lista hallintayksiköistä
+       [:span
+        [:h5.haku-otsikko "Valitse hallintayksikkö"]
+        [:div
+         ^{:key "hy-lista"}
+         [suodatettu-lista {:format :nimi :haku :nimi
+                            :selection nav/valittu-hallintayksikko
+                            :on-select nav/valitse-hallintayksikko
+                            :aputeksti "Kirjoita hallintayksikön nimi tähän"}
+          @hal/hallintayksikot]]]
+       
+       ;; Hallintayksikko on valittu, mutta urakkaa ei: näytetään luettelossa urakat
+       (if-not v-ur
+         (if (nil? urakkalista)
+           [yleiset/ajax-loader "Urakoita haetaan..."]
+           [:span
+            [:h5.haku-otsikko "Valitse hallintayksikön urakka"]
+            [:div
+             ^{:key "ur-lista"}
+             [suodatettu-lista {:format :nimi :haku :nimi
+                                :selection nav/valittu-urakka
+                                :nayta-ryhmat [:kaynnissa :paattyneet]
+                                :ryhmittele (let [nyt (pvm/nyt)]
+                                              #(if (pvm/jalkeen? nyt (:loppupvm %))
+                                                 :paattyneet
+                                                 :kaynnissa))
+                                :ryhman-nimi #(case %
+                                                :kaynnissa "Käynnissä olevat urakat"
+                                                :paattyneet "Päättyneet urakat")
+                                :on-select nav/valitse-urakka
+                                :aputeksti "Kirjoita urakan nimi tähän"}
+              @nav/suodatettu-urakkalista]]])
+         
+         ;; Urakka valittu, tähän kaikki urakan komponentit
+         [urakka/urakka v-ur])))])
