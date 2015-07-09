8466eee89946755685268770ea50c72d37671cb0
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-07-09 09:57:53 +0300

    Kantaan lisättyjen päällystyskohteiden kohdenumeroa ei voi muokata UI:ssa (YHAsta tulleiden kohteiden kohdenumeroa ei saa muokata).

--- src/cljs/harja/views/urakka/kohdeluettelo/yhteenveto.cljs
+++ src/cljs/harja/views/urakka/kohdeluettelo/yhteenveto.cljs
@@ -102,7 +102,8 @@
                                 (reset! paallystyskohderivit vastaus)))
            :voi-poistaa? (fn [rivi] (nil? (:paallystysilmoitus_id rivi)))}
           [{:tyyppi :vetolaatikon-tila :leveys "5%"}
-           {:otsikko "#" :nimi :kohdenumero :tyyppi :numero :leveys "10%" :validoi [[:ei-tyhja "Anna arvo"]]}
+           {:otsikko "#" :nimi :kohdenumero :tyyppi :numero :leveys "10%" :validoi [[:ei-tyhja "Anna arvo"]]
+            :muokattava? (fn [rivi] (true? (and (:id rivi) (neg? (:id rivi)))))}
            {:otsikko "Kohde" :nimi :nimi :tyyppi :string :leveys "20%" :validoi [[:ei-tyhja "Anna arvo"]]}
            {:otsikko "Tarjoushinta" :nimi :sopimuksen_mukaiset_tyot :fmt fmt/euro-opt :tyyppi :numero :leveys "10%" :validoi [[:ei-tyhja "Anna arvo"]]}
            {:otsikko "Muutokset" :nimi :muutoshinta :muokattava? (constantly false) :fmt fmt/euro-opt :tyyppi :numero :leveys "10%"}
