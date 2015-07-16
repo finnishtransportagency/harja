426549d052c32f90349c78e39f3dd01e3c941988
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-07-16 13:08:33 +0300

    Toimenpidekoodien hakemiselle yksinkertainen testi.

---
+++ test/clj/harja/palvelin/palvelut/toimenpidekoodit_test.clj
@@ -0,0 +1,35 @@
+(ns harja.palvelin.palvelut.toimenpidekoodit-test
+  (:require [clojure.test :refer :all]
+            [taoensso.timbre :as log]
+            [harja.palvelin.komponentit.tietokanta :as tietokanta]
+            [harja.palvelin.palvelut.toimenpidekoodit :refer :all]
+            [harja.testi :refer :all]
+            [com.stuartsierra.component :as component]
+            [harja.kyselyt.konversio :as konv]))
+
+
+(defn jarjestelma-fixture [testit]
+  (alter-var-root #'jarjestelma
+    (fn [_]
+      (component/start
+        (component/system-map
+          :db (apply tietokanta/luo-tietokanta testitietokanta)
+          :http-palvelin (testi-http-palvelin)
+          :toimenpidekoodit (component/using
+                                        (->Toimenpidekoodit)
+                                        [:http-palvelin :db])))))
+
+  (testit)
+  (alter-var-root #'jarjestelma component/stop))
+
+
+(use-fixtures :once (compose-fixtures
+                      jarjestelma-fixture
+                      urakkatieto-fixture))
+
+(deftest toimenpidekoodien-haku-toimii
+  (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                :hae-toimenpidekoodit +kayttaja-jvh+)]
+
+    (is (>= (count vastaus) 1300))
+    (is (not (nil? vastaus)))))
