fd8bf0ab1780a73ac85c887a7c1f48ba75bc8939
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-08-05 15:12:26 +0300

    Lomakkeessa footer-fn mahdollisuus
    Footer-fn eroaa footer optiosta siten, että sille annetaan funktio, joka
    saa parametrina validoinnin virheet ja varoitukset. Tällä tavalla
    vältytään ulkoisen virheet atomin välittelyä lomakkeelle.

--- src/cljs/harja/ui/lomake.cljs
+++ src/cljs/harja/ui/lomake.cljs
@@ -78,13 +78,13 @@
 
 (defn lomake
   "Geneerinen lomakekomponentti, joka käyttää samaa kenttien määrittelymuotoa kuin grid.
-Ottaa kolme parametria: optiot, skeeman (vektori kenttiä) sekä datan (mäppi).
+  Ottaa kolme parametria: optiot, skeeman (vektori kenttiä) sekä datan (mäppi).
 
-Kenttiä voi poistaa käytöstä ehdollisesti, kaikki nil kentät jätetään näyttämättä.
-Kenttä voi olla myös ryhmä (ks. ryhma funktio), jolla on otsikko ja kentät.
-Ryhmistä muodostetaan fieldset lomakkeelle.
+  Kenttiä voi poistaa käytöstä ehdollisesti, kaikki nil kentät jätetään näyttämättä.
+  Kenttä voi olla myös ryhmä (ks. ryhma funktio), jolla on otsikko ja kentät.
+  Ryhmistä muodostetaan fieldset lomakkeelle.
 
-Optioissa voi olla seuraavat avaimet:
+  Optioissa voi olla seuraavat avaimet:
 
   :muokkaa!       callback, jolla kaikki muutokset tehdään, ottaa sisään uudet tiedot
                   ja tekee sille jotain (oletettavasti swap! tai reset! atomille,
@@ -95,12 +95,15 @@ Optioissa voi olla seuraavat avaimet:
   :footer         Komponentti, joka asetetaan lomakkeen footer sijaintiin, yleensä
                   submit nappi tms.
 
+  :footer-fn      vaihtoehto :footer'lle, jolle annetaan footerin muodostava funktio
+                  funktiolle annetaan virheet ja varoitukset parametrina 
+
   :virheet        atomi, joka sisältää mäpin kentän nimestä virheisiin, jos tätä ei
                   anneta, lomake luo sisäisesti uuden atomin.
   :varoitukset    atomi, joka sisältää mäpin kentän nimestä varoituksiin, jos tätä ei
                   anneta, lomake luo sisäisesti uuden atomin.
   :voi-muokata?   voiko lomaketta muokata, oletuksena true
-"
+  "
 
   [{:keys [muokkaa! luokka footer virheet varoitukset voi-muokata?] :as opts} skeema data]
   (let [luokka (or luokka :default)
@@ -128,69 +131,72 @@ Optioissa voi olla seuraavat avaimet:
                                       varoitukset (into {}
                                                         (validointi/validoi-rivi nil data skeema :varoita)))
 
-    (fn [{:keys [muokkaa! luokka footer virheet varoitukset] :as opts} skeema data]
-      [:form.lomake {:class (case luokka
+    (fn [{:keys [muokkaa! luokka footer footer-fn virheet varoitukset] :as opts} skeema data]
+      (let [kaikki-skeemat (keep identity (mapcat #(if (ryhma? %) (:skeemat %) [%]) skeema))
+            kaikki-virheet (validointi/validoi-rivi nil data kaikki-skeemat :validoi)
+            kaikki-varoitukset (validointi/validoi-rivi nil data kaikki-skeemat :varoita)
+            _ (paivita-ulkoinen-validointitila! virheet kaikki-virheet varoitukset kaikki-varoitukset)
+            kentta (fn [{:keys [muokattava? fmt hae nimi pakollinen? validoi-heti?] :as kentta}]
+                     (assert (not (nil? nimi)) (str "Virheellinen kentän määrittely, :nimi arvo nil. Otsikko: " (:otsikko kentta)))
+                     (let [kentan-virheet (get kaikki-virheet nimi)
+                           kentan-varoitukset (get kaikki-varoitukset nimi)
+                           kentta (assoc kentta :lomake? true)
+                           arvo (atomina kentta data (fn [uudet-tiedot]
+                                                       (swap! muokatut conj nimi)
+                                                       (muokkaa! uudet-tiedot)))
+                           kentan-tunniste nimi]
+                        ^{:key (:nimi kentta)}
+                        [:div.form-group {:class (when pakollinen? "required")}
+                         [:div.row
+                          (if (+ei-otsikkoa+ (:tyyppi kentta))
+                            [tyhja-otsikko luokka]
+                            [kentan-otsikko luokka (name nimi) (:otsikko kentta)])
+                          [kentan-komponentti luokka kentta
+                           (if-let [komponentti (:komponentti kentta)]
+                             komponentti
+                             (if (and voi-muokata?
+                                      (or (nil? muokattava?)
+                                          (muokattava? data)))
+                               ;; Muokattava tieto, tehdään sille kenttä
+                               [:span {:class (str (when-not (empty? kentan-virheet)
+                                                     "sisaltaa-virheen")
+                                                   (when-not (empty? kentan-varoitukset)
+                                                     "sisaltaa-varoituksen"))}
+                                [tee-kentta (assoc kentta
+                                                   :focus (= @nykyinen-fokus kentan-tunniste)
+                                                   :on-focus #(aseta-fokus! kentan-tunniste)) arvo]
+                                (if (and (not (empty? kentan-virheet))
+                                         (or validoi-heti?
+                                             (@muokatut nimi)))
+                                  (virheen-ohje kentan-virheet :virhe)
+                                  (if (and (not (empty? kentan-varoitukset))
+                                           (@muokatut nimi))
+                                    (virheen-ohje kentan-varoitukset :varoitus)))]
+
+                               ;; Ei muokattava, näytetään
+                               [:div.form-control-static
+                                (if fmt
+                                  (fmt ((or hae #(get % nimi)) data))
+                                  (nayta-arvo kentta arvo))]))]
+                          [kentan-yksikko luokka kentta]]
+                         [kentan-vihje luokka kentta]]))]
+        [:form.lomake {:class (case luokka
                                 :inline "form-inline"
                                 :horizontal "form-horizontal"
                                 :default "")}
-         (let [kaikki-skeemat (keep identity (mapcat #(if (ryhma? %) (:skeemat %) [%]) skeema))
-               kaikki-virheet (validointi/validoi-rivi nil data kaikki-skeemat :validoi)
-               kaikki-varoitukset (validointi/validoi-rivi nil data kaikki-skeemat :varoita)
-               _ (paivita-ulkoinen-validointitila! virheet kaikki-virheet varoitukset kaikki-varoitukset)
-               kentta (fn [{:keys [muokattava? fmt hae nimi pakollinen? validoi-heti?] :as kentta}]
-                        (assert (not (nil? nimi)) (str "Virheellinen kentän määrittely, :nimi arvo nil. Otsikko: " (:otsikko kentta)))
-                        (let [kentan-virheet (get kaikki-virheet nimi)
-                              kentan-varoitukset (get kaikki-varoitukset nimi)
-                              kentta (assoc kentta :lomake? true)
-                              arvo (atomina kentta data (fn [uudet-tiedot]
-                                                          (swap! muokatut conj nimi)
-                                                          (muokkaa! uudet-tiedot)))
-                              kentan-tunniste nimi]
-                          ^{:key (:nimi kentta)}
-                          [:div.form-group {:class (when pakollinen? "required")}
-                           [:div.row
-                            (if (+ei-otsikkoa+ (:tyyppi kentta))
-                              [tyhja-otsikko luokka]
-                              [kentan-otsikko luokka (name nimi) (:otsikko kentta)])
-                            [kentan-komponentti luokka kentta
-                             (if-let [komponentti (:komponentti kentta)]
-                               komponentti
-                               (if (and voi-muokata?
-                                        (or (nil? muokattava?)
-                                            (muokattava? data)))
-                                 ;; Muokattava tieto, tehdään sille kenttä
-                                 [:span {:class (str (when-not (empty? kentan-virheet)
-                                                       "sisaltaa-virheen")
-                                                     (when-not (empty? kentan-varoitukset)
-                                                       "sisaltaa-varoituksen"))}
-                                  [tee-kentta (assoc kentta
-                                                :focus (= @nykyinen-fokus kentan-tunniste)
-                                                :on-focus #(aseta-fokus! kentan-tunniste)) arvo]
-                                  (if (and (not (empty? kentan-virheet))
-                                           (or validoi-heti?
-                                               (@muokatut nimi)))
-                                    (virheen-ohje kentan-virheet :virhe)
-                                    (if (and (not (empty? kentan-varoitukset))
-                                             (@muokatut nimi))
-                                      (virheen-ohje kentan-varoitukset :varoitus)))]
+         
+         (doall
+          (for [skeema (keep identity skeema)]
+            (if-let [ryhma (and (ryhma? skeema) skeema)]
+              ^{:key (:otsikko ryhma)}
+              [:fieldset
+               [:legend (:otsikko ryhma)]
+               (doall (map kentta (keep identity (:skeemat ryhma))))]
 
-                                 ;; Ei muokattava, näytetään
-                                 [:div.form-control-static
-                                  (if fmt
-                                    (fmt ((or hae #(get % nimi)) data))
-                                    (nayta-arvo kentta arvo))]))]
-                            [kentan-yksikko luokka kentta]]
-                           [kentan-vihje luokka kentta]]))]
-           (doall
-            (for [skeema (keep identity skeema)]
-              (if-let [ryhma (and (ryhma? skeema) skeema)]
-                ^{:key (:otsikko ryhma)}
-                [:fieldset
-                 [:legend (:otsikko ryhma)]
-                 (doall (map kentta (keep identity (:skeemat ryhma))))]
+              (kentta skeema))))
 
-                (kentta skeema)))))
-
-         (when footer
-           [lomake-footer luokka footer])])))
+         (if footer-fn
+           [lomake-footer luokka (footer-fn kaikki-virheet kaikki-varoitukset)]
+           (if footer
+             [lomake-footer luokka footer]))]))))
 
