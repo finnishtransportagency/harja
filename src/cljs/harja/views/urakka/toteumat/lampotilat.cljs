687d9443f9dd63bfe02a04328c371f822106d4c5
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-08-05 15:14:01 +0300

    Siivousta lämpötilanäkymälle.
    Käytetään lomakkeen validointia sekä poistettu kuollutta koodia.

--- src/cljs/harja/views/urakka/toteumat/lampotilat.cljs
+++ src/cljs/harja/views/urakka/toteumat/lampotilat.cljs
@@ -34,41 +34,6 @@
 
 (defonce muokatut-lampotilat (reaction @nykyiset-lampotilat))
 
-(defonce tallentaa-lampotilaa? (atom false))
-
-(defn kelvollinen-lampotila?
-  [lampo]
-  "Tarkastaa että syöte vastaa nuroa xx.yy
-  Alunperin tänne toteutettiin monta testiä jotka löytyvät kommenteista, mutta sitten otettiin käyttöön
-  harja.ui.lomake. Lomake varmistaa että kenttään syötetään vain lukuja, mutta ei rajoita luvun suuruutta.
-  Tietokantaan syötettynä lämmössä voi olla vain kaksi numeroa desimaalin molemmin puolin, eli
-  täällä pitää tarkastaa, että luku on pienempi kuin 100.
-  Vanhat testit kannattanee säilyttää kommenteissa."
-  (let [l (str lampo)]
-    (< (float lampo) 100)
-    #_(and
-      ; Tyhjä syöte pitää hyväksyä, se vain tarkoittaa että lämpötilaa ei ole vielä merkattu!
-      ;(not (str/blank? l)) ;Onko syöte tyhjä, tai pelkkää whitespacea.
-      (not (= (first l) \.)) ;Ensimmäinen merkki ei saa olla "." - tämä mahdollistaisi esimerkiksi syötteen ".400"
-      (< (count l) 6) ;Pituus pitäisi olla maksimissaan viisi merkkiä (xx.yy)
-      (< (count (str/split l #"\.")) 3) ; Pisteellä splitattuna, elementtejä saa olla maksimissaan 2
-      (not (= (last l) \.)) ;Tarkista ettei viimeinen merkki ole "." - tällöin splitin tulos voisi silti olla 2
-      (nil? (re-find #"[a-zA-Z]" l)) ; Etsi regexillä aakkoset
-      (nil? (some
-              (fn [liian_pitka] liian_pitka)
-              (map #(> (count %) 2) (str/split l #"\."))))))) ;Tarkista, että pilkun molemmin puolin on max. 2 merkkiä
-
-
-(defn etsi-lampotilat
-  "Etsii aloitus- ja lopetuspäivämääriin sopivat keskilämmöt"
-  [aloitus lopetus lampotilat urakka-id]
-  (some (fn [l] (when
-                  (and
-                    (pvm/sama-pvm? aloitus (:alkupvm l))
-                    (pvm/sama-pvm? lopetus (:loppupvm l))
-                    (= urakka-id (:urakka l)))
-                  l))
-        lampotilat))
 
 (defn tallenna-muutos [hoitokausi tulos]
   (let [uusi-lampo tulos]
@@ -87,36 +52,38 @@
         [lomake {:luokka   :horizontal
                  :muokkaa! (fn [uusi]
                              (reset! lampotilat uusi))
-                 :footer   (if saa-muokata?
-                             [:div.form-group
-                              [:div.col-md-4
-                               [napit/palvelinkutsu-nappi
-                                "Tallenna"
-                                #(lampotilat/tallenna-lampotilat!
-                                  (:id @lampotilat)
-                                  (:id urakka)
-                                  hoitokausi
-                                  (:keskilampo @lampotilat)
-                                  (:pitkalampo @lampotilat))
-                                {:luokka       "nappi-ensisijainen"
-                                 :disabled     (not (and
-                                                     (::muokattu @lampotilat)
-                                                     (kelvollinen-lampotila? (:pitkalampo @lampotilat))
-                                                     (kelvollinen-lampotila? (:keskilampo @lampotilat))))
-                                 :ikoni        (ikonit/search)
-                                 :kun-onnistuu #(do
-                                                  (viesti/nayta! "Tallentaminen onnistui" :success 1500)
-                                                  (tallenna-muutos hoitokausi %))}]
-                               
-                               [:button.nappi-kielteinen {:name "peruuta"
-                                                          :disabled (= @muokatut-lampotilat @nykyiset-lampotilat)
-                                                          :on-click #(do
-                                                                       (.preventDefault %)
-                                                                       (reset! muokatut-lampotilat @nykyiset-lampotilat))}
-                                (ikonit/remove) " Peruuta"]]])
+                 :footer-fn (fn [virheet _]
+                              (log "virheet: " (pr-str virheet) ", muokattu? " (::muokattu @lampotilat))
+                              (if saa-muokata?
+                                [:div.form-group
+                                 [:div.col-md-4
+                                  [napit/palvelinkutsu-nappi
+                                   "Tallenna"
+                                   #(lampotilat/tallenna-lampotilat!
+                                     (:id @lampotilat)
+                                     (:id urakka)
+                                     hoitokausi
+                                     (:keskilampo @lampotilat)
+                                     (:pitkalampo @lampotilat))
+                                   {:luokka       "nappi-ensisijainen"
+                                    :disabled     (or (not= true (::muokattu @lampotilat))
+                                                      (not (empty? virheet)))
+                                    :ikoni        (ikonit/search)
+                                    :kun-onnistuu #(do
+                                                     (viesti/nayta! "Tallentaminen onnistui" :success 1500)
+                                                     (tallenna-muutos hoitokausi %))}]
+                                  
+                                  [:button.nappi-kielteinen {:name "peruuta"
+                                                             :disabled (= @muokatut-lampotilat @nykyiset-lampotilat)
+                                                             :on-click #(do
+                                                                          (.preventDefault %)
+                                                                          (reset! muokatut-lampotilat @nykyiset-lampotilat))}
+                                   (ikonit/remove) " Peruuta"]]]))
                  }
-         [{:otsikko "Keskilämpötila" :nimi :keskilampo :tyyppi :numero :leveys-col 2}
-          {:otsikko "Pitkän aikavälin keskilämpötila" :nimi :pitkalampo :tyyppi :numero :leveys-col 2}]
+         [{:otsikko "Keskilämpötila" :nimi :keskilampo :tyyppi :numero :leveys-col 2
+           :validoi [[:lampotila]]}
+          {:otsikko "Pitkän aikavälin keskilämpötila" :nimi :pitkalampo :tyyppi :numero :leveys-col 2
+           :validoi [[:lampotila]]}]
          @lampotilat]))))
 
 (defn lampotilat [ur]
