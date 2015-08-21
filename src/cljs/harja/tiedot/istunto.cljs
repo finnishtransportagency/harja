ec013c7edbd26bc55499577b8ad923416f3746d4
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-08-21 12:21:55 +0300

    Lisää varoitin-modal jos käyttöaika umpeutumassa.

--- src/cljs/harja/tiedot/istunto.cljs
+++ src/cljs/harja/tiedot/istunto.cljs
@@ -5,7 +5,8 @@
 
             [reagent.core :refer [atom]]
             [cljs.core.async :refer [<!]]
-            [cljs.core.async :refer [<! >! timeout chan]])
+            [cljs.core.async :refer [<! >! timeout chan]]
+            [harja.ui.modal :as modal])
   (:require-macros [cljs.core.async.macros :refer [go]]
                    [reagent.ratom :refer [reaction]]))
 
@@ -21,11 +22,54 @@
   (reset! kayttaja k)
   (tapahtumat/julkaise! (merge {:aihe :kayttajatiedot} k)))
 
-(def kayttoaika-ilman-kayttajasyotteita-sekunteina (* 60 60 2))
+(def oletuskayttoaika-ilman-kayttajasyotteita-sekunteina (* 60 60 2))
 
 (def ajastin-kaynnissa (atom false))
 
-(def kayttoaikaa-jaljella-sekunteina (atom kayttoaika-ilman-kayttajasyotteita-sekunteina))
+(def kayttoaikaa-jaljella-sekunteina (atom oletuskayttoaika-ilman-kayttajasyotteita-sekunteina))
+
+(defn pysayta-ajastin []
+  (reset! ajastin-kaynnissa false))
+
+; TODO Lisää tälle event listener
+(defn resetoi-ajastin []
+  (reset! kayttoaikaa-jaljella-sekunteina oletuskayttoaika-ilman-kayttajasyotteita-sekunteina))
+
+(def varoitus-nakyvissa (atom false))
+
+(defn kirjaudu-ulos []
+  ;; TODO Unmounttaa komponentit
+  )
+
+(defn kirjaudu-ulos-jos-kayttoaika-umpeutunut []
+  (if (<= @kayttoaikaa-jaljella-sekunteina 0)
+    (kirjaudu-ulos)))
+
+(defn nayta-varoitus-aikakatkaisusta []
+  (reset! varoitus-nakyvissa true)
+  (modal/nayta! {:otsikko "Haluatko jatkaa käyttöä?"
+                 :footer  [:span
+                           [:button.nappi-kielteinen {:type     "button"
+                                                      :on-click #(do (.preventDefault %)
+                                                                     (reset! varoitus-nakyvissa false)
+                                                                     (kirjaudu-ulos)
+                                                                     (modal/piilota!))}
+                            "Kirjaudu ulos"]
+                           [:button.nappi-myonteinen {:type     "button"
+                                                      :on-click #(do (.preventDefault %)
+                                                                     (reset! varoitus-nakyvissa false)
+                                                                     (resetoi-ajastin)
+                                                                     (modal/piilota!))}
+                            "Jatka"]
+                           ]}
+                [:div
+                 [:p (str "Et ole käyttänyt Harjaa aktiivisesti vähään aikaan. Sinut kirjataan pian ulos. Haluatko jatkaa käyttöä?")]
+                 [:p (str "Käyttöaikaa jäljellä: " "XX:XX")]])) ; TODO Näytä käyttöaika ja jos 0, tekstinä: Harjan käyttö aikakatkaistu kahden tunnin käyttämättömyyden takia. Lataa sivu uudelleen.
+
+(defn varoita-jos-kayttoaika-umpeutumassa []
+  (if (and (< @kayttoaikaa-jaljella-sekunteina (* 60 15))
+           (false? @varoitus-nakyvissa))
+    (nayta-varoitus-aikakatkaisusta)))
 
 (defn kaynnista-ajastin []
   (if (false? @ajastin-kaynnissa)
@@ -36,10 +80,6 @@
         (if @ajastin-kaynnissa
           (do
             (reset! kayttoaikaa-jaljella-sekunteina (- @kayttoaikaa-jaljella-sekunteina 1))
+            (varoita-jos-kayttoaika-umpeutumassa)
+            (kirjaudu-ulos-jos-kayttoaika-umpeutunut)
             (recur)))))))
-
-(defn pysayta-ajastin []
-  (reset! ajastin-kaynnissa false))
-
-(defn resetoi-ajastin []
-  (reset! kayttoaikaa-jaljella-sekunteina kayttoaika-ilman-kayttajasyotteita-sekunteina))
