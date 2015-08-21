a3b52321d5bda26ed5f6e58d547e7766562730c1
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-08-21 10:34:58 +0300

    Lisää uusi migraatio: muokkauslukon sarakkeilla NOT NULL.

---
+++ tietokanta/src/main/resources/db/migration/V1_168__muokkauslukolle_NOT_NULL.sql
@@ -0,0 +1,3 @@
+ALTER TABLE muokkauslukko ALTER COLUMN id SET NOT NULL;
+ALTER TABLE muokkauslukko ALTER COLUMN kayttaja SET NOT NULL;
+ALTER TABLE muokkauslukko ALTER COLUMN aikaleima SET NOT NULL;
