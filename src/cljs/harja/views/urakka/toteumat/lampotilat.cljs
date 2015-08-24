bbd708a394e50b9e8dfeeaf6b2dc607ef81c006f
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2015-08-24 10:46:40 +0300

    Korjaa lämpötilan hakua suolasakkonäkymässä
    Vie hoitokausi-binding render-funktion bodyn sisään.
    Passaa palvelinkutsunapille sulkemiseen liittyviä parametrejä.

--- src/cljs/harja/views/urakka/toteumat/lampotilat.cljs
+++ src/cljs/harja/views/urakka/toteumat/lampotilat.cljs
@@ -45,81 +45,85 @@
   (let [urakka @nav/valittu-urakka
         saa-muokata? (roolit/rooli-urakassa? roolit/urakanvalvoja
                                              (:id urakka))
-        hoitokausi @u/valittu-hoitokausi
         valitun-hoitokauden-tiedot (reaction (when @suolasakot-ja-lampotilat
-                                      (first (filter #(or (= (:hoitokauden_alkuvuosi %) (pvm/vuosi (first @u/valittu-hoitokausi)))
-                                                          (= (:lt_alkupvm %) (first @u/valittu-hoitokausi)))
-                                                     @suolasakot-ja-lampotilat))))
+                                               (first (filter #(or (= (:hoitokauden_alkuvuosi %) (pvm/vuosi (first @u/valittu-hoitokausi)))
+                                                                   (= (:lt_alkupvm %) (first @u/valittu-hoitokausi)))
+                                                              @suolasakot-ja-lampotilat))))
         lampotilaerotus (reaction (- (:keskilampotila @valitun-hoitokauden-tiedot)
                                      (:pitkakeskilampotila @valitun-hoitokauden-tiedot)))]
 
     (fn []
-      [:span.suolasakkolomake
-      [lomake {:luokka    :horizontal
-               :muokkaa!  (fn [uusi]
-                            (log "lomaketta muokattu, tiedot:" (pr-str uusi))
-                            (reset! valitun-hoitokauden-tiedot (assoc uusi :muokattu true)))
-               :footer-fn (fn [virheet _]
-                            (log "virheet: " (pr-str virheet) ", muokattu? " (:muokattu @valitun-hoitokauden-tiedot))
-                            [:span.lampotilalomake-footer
-                             (if saa-muokata?
-                               [:div.form-group
-                                [:div.col-md-4
-                                 [napit/palvelinkutsu-nappi
-                                  "Tallenna"
-                                  #(tallenna-suolasakko-ja-lampotilat
-                                    @valitun-hoitokauden-tiedot)
-                                  {:luokka       "nappi-ensisijainen"
-                                   :disabled     (or (not= true (:muokattu @valitun-hoitokauden-tiedot))
-                                                     (not (empty? virheet)))
-                                   :ikoni        (ikonit/tallenna)
-                                   :kun-onnistuu #(do
-                                                   (viesti/nayta! "Tallentaminen onnistui" :success 1500)
-                                                   (reset! suolasakot-ja-lampotilat %))}]]])])}
-       [{:otsikko "Suolasakko" :nimi :maara :tyyppi :numero :leveys-col 2 :yksikko "€ / 5% rajan ylittävä tonni"}
-        {:otsikko       "Maksukuukausi" :nimi :maksukuukausi :tyyppi :valinta :leveys-col 2
-         :valinta-arvo  first
-         :valinta-nayta #(if (nil? %) yleiset/+valitse-kuukausi+ (second %))
-         :valinnat      [[5 "Toukokuu"] [6 "Kesäkuu"] [7 "Heinäkuu"]
-                         [8 "Elokuu"] [9 "Syyskuu"]]}
-        {:otsikko       "Indeksi" :nimi :indeksi :tyyppi :valinta
-         :valinta-nayta #(if (nil? %) yleiset/+valitse-indeksi+ (str %))
-         :valinnat      (conj @i/indeksien-nimet yleiset/+ei-sidota-indeksiin+)
+      (let [hoitokausi @u/valittu-hoitokausi]
+        [:span.suolasakkolomake
+         [lomake {:luokka    :horizontal
+                  :muokkaa!  (fn [uusi]
+                               (log "lomaketta muokattu, tiedot:" (pr-str uusi))
+                               (reset! valitun-hoitokauden-tiedot (assoc uusi :muokattu true)))
+                  :footer-fn (fn [virheet _]
+                               (log "virheet: " (pr-str virheet) ", muokattu? " (:muokattu @valitun-hoitokauden-tiedot))
+                               [:span.lampotilalomake-footer
+                                (if saa-muokata?
+                                  [:div.form-group
+                                   [:div.col-md-4
+                                    [napit/palvelinkutsu-nappi
+                                     "Tallenna"
+                                     #(tallenna-suolasakko-ja-lampotilat
+                                       @valitun-hoitokauden-tiedot)
+                                     {:luokka       "nappi-ensisijainen"
+                                      :disabled     (or (not= true (:muokattu @valitun-hoitokauden-tiedot))
+                                                        (not (empty? virheet)))
+                                      :ikoni        (ikonit/tallenna)
+                                      :kun-onnistuu #(do
+                                                      (viesti/nayta! "Tallentaminen onnistui" :success 1500)
+                                                      (reset! suolasakot-ja-lampotilat %))}]]])])}
+          [{:otsikko "Suolasakko" :nimi :maara :tyyppi :numero :leveys-col 2 :yksikko "€ / 5% rajan ylittävä tonni"}
+           {:otsikko       "Maksukuukausi" :nimi :maksukuukausi :tyyppi :valinta :leveys-col 2
+            :valinta-arvo  first
+            :valinta-nayta #(if (nil? %) yleiset/+valitse-kuukausi+ (second %))
+            :valinnat      [[5 "Toukokuu"] [6 "Kesäkuu"] [7 "Heinäkuu"]
+                            [8 "Elokuu"] [9 "Syyskuu"]]}
+           {:otsikko       "Indeksi" :nimi :indeksi :tyyppi :valinta
+            :valinta-nayta #(if (nil? %) yleiset/+valitse-indeksi+ (str %))
+            :valinnat      (conj @i/indeksien-nimet yleiset/+ei-sidota-indeksiin+)
 
-         :leveys-col    2}
+            :leveys-col    2}
 
-        {:otsikko "Hoitokauden sydäntalven keskilämpötila" :nimi :keskilampotila :tyyppi :numero :leveys-col 1
-         :validoi [[:lampotila]] :yksikko "\u2103"}
-        {:otsikko "Pitkän aikavälin sydäntalven keskilämpötila" :nimi :pitkakeskilampotila :tyyppi :numero :leveys-col 1
-         :validoi [[:lampotila]] :yksikko "\u2103"}
-        {:otsikko "Lämpötilat ilmatieteenlaitokselta" :nimi :haku :tyyppi :komponentti
-         :komponentti (when (roolit/rooli-urakassa? roolit/urakanvalvoja
-                                                    (:id urakka))
-                        [napit/palvelinkutsu-nappi "Hae"
-                         #(lampotilat/hae-lampotilat-ilmatieteenlaitokselta (:id urakka) (pvm/vuosi (first hoitokausi)))
-                         {:ikoni        (ikonit/download)
-                          :luokka       "nappi-ensisijainen nappi-hae-itl"
-                          :kun-onnistuu (fn [{:keys [keskilampotila ilmastollinen-keskiarvo]}]
-                                          (if (and keskilampotila ilmastollinen-keskiarvo)
-                                            (swap! valitun-hoitokauden-tiedot
-                                                   (fn [tiedot]
-                                                     (assoc tiedot
-                                                       :keskilampotila keskilampotila
-                                                       :pitkakeskilampotila ilmastollinen-keskiarvo
-                                                       :muokattu true)))
-                                            (viesti/nayta! (str "Talvikauden "
-                                                                (pvm/vuosi (first hoitokausi)) " \u2014 " (pvm/vuosi (second hoitokausi))
-                                                                " lämpötiloja ei löytynyt.") :warning)))}])}
+           {:otsikko "Hoitokauden sydäntalven keskilämpötila" :nimi :keskilampotila :tyyppi :numero :leveys-col 1
+            :validoi [[:lampotila]] :yksikko "\u2103"}
+           {:otsikko "Pitkän aikavälin sydäntalven keskilämpötila" :nimi :pitkakeskilampotila :tyyppi :numero :leveys-col 1
+            :validoi [[:lampotila]] :yksikko "\u2103"}
+           {:otsikko     "Lämpötilat ilmatieteenlaitokselta" :nimi :haku :tyyppi :komponentti
+            :komponentti (when (roolit/rooli-urakassa? roolit/urakanvalvoja
+                                                       (:id urakka))
+                           [napit/palvelinkutsu-nappi "Hae"
+                            #(lampotilat/hae-lampotilat-ilmatieteenlaitokselta (:id urakka) (pvm/vuosi (first hoitokausi)))
+                            {:ikoni              (ikonit/download)
+                             :luokka             "nappi-ensisijainen nappi-hae-itl"
+                             :virheviesti        (str "Lämpötilan haku hoitokaudelle "
+                                                      (pvm/vuosi (first hoitokausi)) " -  " (pvm/vuosi (second hoitokausi))
+                                                      " epäonnistui. Joko tietoja ei vielä ole, tai palvelussa on tekninen ongelma. Yritä myöhemmin uudelleen.")
+                             :virheen-esitystapa :flash
+                             :kun-onnistuu       (fn [{:keys [keskilampotila ilmastollinen-keskiarvo]}]
+                                                   (if (and keskilampotila ilmastollinen-keskiarvo)
+                                                     (swap! valitun-hoitokauden-tiedot
+                                                            (fn [tiedot]
+                                                              (assoc tiedot
+                                                                :keskilampotila keskilampotila
+                                                                :pitkakeskilampotila ilmastollinen-keskiarvo
+                                                                :muokattu true)))
+                                                     (viesti/nayta! (str "Talvikauden "
+                                                                         (pvm/vuosi (first hoitokausi)) " \u2014 " (pvm/vuosi (second hoitokausi))
+                                                                         " lämpötiloja ei löytynyt.") :warning)))}])}
 
-        {:otsikko     "Lämpötilojen erotus" :nimi :lampotilaerotus
-         :hae         #(when @lampotilaerotus
-                        (.toFixed @lampotilaerotus 1))
-         :tyyppi      :numero :leveys-col 1
-         :muokattava? (constantly false) :yksikko "\u2103"
-         :vihje       (when (> @lampotilaerotus 2)
-                        "Yli 2 astetta tavanomaista lämpimämpi sydäntalvi nostaa sallittua suolankäytön määrää.")}]
-       @valitun-hoitokauden-tiedot]
-      ])))
+           {:otsikko     "Lämpötilojen erotus" :nimi :lampotilaerotus
+            :hae         #(when @lampotilaerotus
+                           (.toFixed @lampotilaerotus 1))
+            :tyyppi      :numero :leveys-col 1
+            :muokattava? (constantly false) :yksikko "\u2103"
+            :vihje       (when (> @lampotilaerotus 2)
+                           "Yli 2 astetta tavanomaista lämpimämpi sydäntalvi nostaa sallittua suolankäytön määrää.")}]
+          @valitun-hoitokauden-tiedot]
+         ]))))
 
 (defn suolasakot []
   (komp/luo
