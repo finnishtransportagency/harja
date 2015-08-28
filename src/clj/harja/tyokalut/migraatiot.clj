0f095892ea13cfba9bfc485459426e0f78f6cea7
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-08-28 14:06:22 +0300

    Tarkista ettei migraation nimessä ole skandeja

--- src/clj/harja/tyokalut/migraatiot.clj
+++ src/clj/harja/tyokalut/migraatiot.clj
@@ -2,11 +2,14 @@
   (require [clojure.string :as str])
   (:gen-class))
 
-(defn tarkista-duplikaattinumerot []
+(defn migraatiotiedostot []
   (->> "tietokanta/src/main/resources/db/migration"
        (java.io.File.)
        .listFiles
-       (map #(.getName %))
+       (map #(.getName %))))
+
+(defn tarkista-duplikaattinumerot []
+  (->> (migraatiotiedostot)
        (map #(re-find #"V1_(\d+)__(.*).sql" %))
        (map #(drop 1 %))
        (group-by first)
@@ -14,8 +17,17 @@
                (when (and num (> (count filet) 1))
                  (str num ": " (str/join ", " (map second filet))))))))
 
+(defn tarkista-skandit []
+  (->> (migraatiotiedostot)
+       (keep #(when (some {\ä \Ä \ö \Ö} %) %))))
+
+                    
 (defn -main [& args]
-  (let [dup (tarkista-duplikaattinumerot)]
+  (let [dup (tarkista-duplikaattinumerot)
+        skandit (tarkista-skandit)]
     (if dup
       (println "DUPLIKAATTI: " dup)
-      (println "Ei duplikaatteja."))))
+      (println "Ei duplikaatteja."))
+    (if-not (empty? skandit)
+      (println "Skandeja nimessä: " (str/join ", " skandit))
+      (println "Ei skandeja."))))
