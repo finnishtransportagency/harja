32994308c126faa3186d6a7fa22349e9569fd43b
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-08-28 12:44:23 +0300

    Mahdollista lämmin talvi
    Talven lämpötila voi olla myös plussalla! Nyt generoidaan -40.0 ja +5.0
    väliset lämpötilat.

--- test/clj/harja/kyselyt/suolasakko_test.clj
+++ test/clj/harja/kyselyt/suolasakko_test.clj
@@ -32,13 +32,13 @@
   ;; - sallittua käyttömäärää
   ;; - toteumaa
   ;; varmista, että sakko on aina oikein laskettu
-  (prop/for-all [lampotila  (gen/fmap #(/ % 10.0) (gen/choose -60 -1))
-                 lampotila-pitka (gen/fmap #(/ % 10.0) (gen/choose -60 -1))
+  (prop/for-all [;; luodaan lämpötilat -40.0 ja +5.0 välillä
+                 lampotila  (gen/fmap #(/ % 10.0) (gen/choose -400 50))
+                 lampotila-pitka (gen/fmap #(/ % 10.0) (gen/choose -400 50))
                  sakko-per-tonni (gen/choose 1 100) ; gen/s-pos-int
                  sallittu-maara (gen/choose 1 10000)
                  kaytetty-maara (gen/choose 1 10000)]
 
-                ;; 10% lämpötilakorjaus (testidatan hk on 2.8 astetta lämpimpämpi)
                 (let [erotus (- lampotila lampotila-pitka)
                       sal (cond
                             (>= erotus 4) (* sallittu-maara 1.30)
