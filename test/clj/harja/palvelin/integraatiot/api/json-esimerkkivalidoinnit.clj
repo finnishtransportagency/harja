93432cd53b721daa823ed017d86e88dec28a5b56
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-09-02 09:24:29 +0300

    Refactoroi JSON-esimerkkivalidaattoria.

--- test/clj/harja/palvelin/integraatiot/api/json-esimerkkivalidoinnit.clj
+++ test/clj/harja/palvelin/integraatiot/api/json-esimerkkivalidoinnit.clj
@@ -1,21 +1,16 @@
 (ns harja.palvelin.integraatiot.api.json-esimerkkivalidoinnit
   (:require [clojure.test :refer [deftest is use-fixtures]]
             [harja.testi :refer :all]
-            [taoensso.timbre :as log]
-            [harja.palvelin.integraatiot.api.urakat :as api-urakat]
-            [com.stuartsierra.component :as component]
             [harja.tyokalut.json_validointi :as json-validointi]
-            [clojure.string :as str]
             [harja.palvelin.integraatiot.api.tyokalut.skeemat :as skeemat]
             [harja.palvelin.integraatiot.api.tyokalut.esimerkit :as esimerkit]
-            [clojure.java.io :as io]
-            [cheshire.core :as cheshire]))
+            [clojure.java.io :as io]))
 
 (deftest validoi-jsonit
   (let [skeemapolku-esimerkkipolku [[skeemat/+urakan-haku-vastaus+ esimerkit/+urakan-haku-vastaus+]
                                     [skeemat/+urakoiden-haku-vastaus+ esimerkit/+urakoiden-haku-vastaus+]
                                     #_[skeemat/+havainnon-kirjaus+ esimerkit/+havainnon-kirjaus+] ; FIXME RIKKI
-                                    #_[skeemat/+pistetoteuman-kirjaus+ esimerkit/+pistetoteuman-kirjaus+]; FIXME RIKKI
+                                    #_[skeemat/+pistetoteuman-kirjaus+ esimerkit/+pistetoteuman-kirjaus+]; FIXME RIKKI "JSON ei ole validia: /pistetoteuma/toteuma: Ylimääräisiä kenttiä: selite"
                                     [skeemat/+reittitoteuman-kirjaus+ esimerkit/+reittitoteuman-kirjaus+]
                                     [skeemat/+tiestotarkastuksen-kirjaus+ esimerkit/+tiestotarkastuksen-kirjaus+]
                                     [skeemat/+soratietarkastuksen-kirjaus+ esimerkit/+soratietarkastuksen-kirjaus+]
