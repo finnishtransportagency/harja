6b4223e429f180b08f4af8ce6e46e5ec54c2c59b
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-09-02 12:00:08 +0300

    Lisää puuttuvat JSON-esimerkit symboleihin.

--- src/clj/harja/palvelin/integraatiot/api/tyokalut/esimerkit.clj
+++ src/clj/harja/palvelin/integraatiot/api/tyokalut/esimerkit.clj
@@ -9,12 +9,41 @@
 
 (def +havainnon-kirjaus+ "api/examples/havainnon-kirjaus-request.json")
 
+(def +ilmoituskuittauksen-kirjaaminen+ "api/examples/ilmoituskuittauksen-kirjaaminen-request.json")
+(def +ilmoitusten-haku+ "api/examples/ilmoitusten-haku-request.json")
+(def +tietyoilmoituksen-kirjaus+ "api/examples/tietyoilmoituksen-kirjaus-request.json")
+(def +tietyoilmoituksen-kirjaus-vastaus+ "api/examples/tietyoilmoituksen-kirjaus-response.json")
+
 (def +pistetoteuman-kirjaus+ "api/examples/pistetoteuman-kirjaus-request.json")
 (def +reittitoteuman-kirjaus+ "api/examples/reittitoteuman-kirjaus-request.json")
+(def +reittitoteuman-kirjaus+ "api/examples/reittitoteuman-kirjaus-request.json")
+
+(def +turvallisuuspoikkeamien-kirjaus+ "api/examples/turvallisuuspoikkeamien-kirjaus.json")
+
+(def +siltatarkastuksen-kirjaus+ "api/examples/siltatarkastuksen-kirjaus-request.json")
+(def +talvihoitotarkastuksen-kirjaus+ "api/examples/talvihoitotarkastuksen-kirjaus-request.json")
+
+(def +tielupien-haku+ "api/examples/tielupien-haku-request.json")
+(def +tielupien-haku-vastaus+ "api/examples/tielupien-haku-response.json")
+
+(def +poikkeamien-kirjaus+ "api/examples/poikkeamien-kirjaus-request.json")
+
+(def +tietolajien-haku+ "api/examples/tietolajien-haku-response.json")
+
+(def +varusteen-haku-vastaus+ "api/examples/varusteen-haku-response.json")
+(def +varusteiden-haku-vastaus+ "api/examples/varusteiden-haku-response.json")
+(def +varusteen-lisays+ "api/examples/varusteen-lisays-request.json")
+(def +varusteen-paivitys+ "api/examples/varusteen-paivitys-request.json")
+(def +varusteen-poisto+ "api/examples/varusteen-poisto-request.json")
+(def +vrustetoteuman-kirjaus+ "api/examples/varustetoteuman-kirjaus-request.json")
+
 
 (def +tiestotarkastuksen-kirjaus+ "api/examples/tiestotarkastuksen-kirjaus-request.json")
 (def +soratietarkastuksen-kirjaus+ "api/examples/soratietarkastuksen-kirjaus-request.json")
 (def +talvihoitotarkastuksen-kirjaus+ "api/examples/talvihoitotarkastuksen-kirjaus-request.json")
+
+(def +paivystajatietojen-haku+ "api/examples/paivystajatietojen-haku-request.json")
+(def +paivystajatietojen-haku-vastaus+ "api/examples/paivystajatietojen-haku-response.json")
 
 (def +paivystajatietojen-kirjaus+ "api/examples/paivystajatietojen-kirjaus-request.json")
 
