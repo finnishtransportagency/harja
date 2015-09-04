aa80c3ae973b1e1d67ef58dd5bd8bde36369d7a3
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-09-04 10:48:53 +0300

    Kopioi tiedot toisesta branchista joka alkoi bugittaa.

--- src/cljs/harja/views/raportit.cljs
+++ src/cljs/harja/views/raportit.cljs
@@ -14,23 +14,24 @@
 (defonce valittu-raportti (atom nil))
 (defonce valittu-raporttityyppi (atom nil))
 
-
 (def +raporttityypit+
-    [[:laskutusyhteenveto "Laskutusyhteenveto" #{:urakka} [{:otsikko "Hoitokausi ":nimi :hoitokausi
-                                                            :tyyppi :valinta
-                                                            :valinnat :valitun-urakan-hoitokaudet}
-                                                           {:otsikko  "Kuukausi" :nimi :kuukausi
-                                                            :tyyppi   :valinta
-                                                            :valinnat :valitun-aikavalin-kuukaudet}
-                                                           {:otsikko "Kuinka kivaa 1-5?"
-                                                            :nimi :kivaa
-                                                            :tyyppi :numero}
-                                                           ]]
-   [:ymparistoraportti "Ympäristöraportti" #{:urakka :hallintayksikko :yt-alue :koko-maa}]
-   [:turvallisuusraportti "Turvallisuusraportti"]
-   [:tyomaakokous "Työmaakokousraportti"]])
+  ; HUOM: Hardcoodattu testidata vectori mappeja, tämä saadaan myöhemmin kannasta
+  [{:nimi :laskutusyhteenveto
+    :otsikko "Yks.hint. töiden toteumat -raportti"
+    :konteksti #{:urakka}
+    :parametrit
+    [{:otsikko  "Hoitokausi"
+      :nimi     :hoitokausi
+      :tyyppi   :valinta
+      :valinnat :valitun-urakan-hoitokaudet}
+     {:otsikko  "Kuukausi"
+      :nimi     :kuukausi
+      :tyyppi   :valinta
+      :valinnat :valitun-aikavalin-kuukaudet}]
+    :suorita (constantly nil)}])
 
-(tarkkaile! "valittu-raporttityyppi" valittu-raporttityyppi)
+(tarkkaile! "[RAPORTTI] Valittu-raportti" valittu-raportti)
+(tarkkaile! "[RAPORTTI] Valittu-raporttityyppi" valittu-raporttityyppi)
 
 (defn lomake-kentta [kentta lomakkeen-tiedot]
   (if (= :valinta (:tyyppi kentta))
@@ -38,7 +39,6 @@
       :valitun-urakan-hoitokaudet
       (assoc kentta :valinnat @u/valitun-urakan-hoitokaudet
                     :valinta-nayta fmt/pvm-vali-opt)
-
       :valitun-aikavalin-kuukaudet
       (assoc kentta :valinnat (if-let [hk (:hoitokausi lomakkeen-tiedot)]
                                 (pvm/hoitokauden-kuukausivalit hk)
@@ -55,38 +55,35 @@
   (let [lomakkeen-tiedot (atom nil)
         lomakkeen-virheet (atom nil)]
     (komp/luo
-     (fn []
-       [:div.raportit
-        [:div.label-ja-alasveto
-         [:span.alasvedon-otsikko "Valitse raportti"]
-         [livi-pudotusvalikko {:valinta    @valittu-raporttityyppi
-                               ;;\u2014 on väliviivan unikoodi
-                               :format-fn  #(if % (second %) "Valitse")
-                               :valitse-fn #(reset! valittu-raporttityyppi %)
-                               :class      "valitse-raportti-alasveto"}
-          +raporttityypit+]]
+      (fn []
+        [:div.raportit
+         [:div.label-ja-alasveto
+          [:span.alasvedon-otsikko "Valitse raportti"]
+          [livi-pudotusvalikko {:valinta    @valittu-raporttityyppi
+                                ;;\u2014 on väliviivan unikoodi
+                                :format-fn  #(if % (second %) "Valitse")
+                                :valitse-fn #(reset! valittu-raporttityyppi %)
+                                :class      "valitse-raportti-alasveto"}
+           +raporttityypit+]]
 
-        (when-let [tyyppi (second @valittu-raporttityyppi)]
-          [:h5 (str "Luo " tyyppi)]
-          [lomake/lomake
-           {:luokka   :horizontal
-            :virheet  lomakkeen-virheet
-            :muokkaa! (fn [uusi]
-                        (reset! lomakkeen-tiedot uusi))}
-           (let [tiedot @lomakkeen-tiedot
-                 k (into []
-                 (concat
-                   [{:otsikko "Kohde" :nimi :kohteen-nimi :hae #(:nimi @nav/valittu-urakka) :muokattava? (constantly false)}]
-                   (map #(lomake-kentta % tiedot) (nth @valittu-raporttityyppi 3))))]
-             (log "kentät: " (pr-str k))
-             k)
+         (when @valittu-raporttityyppi
+           [lomake/lomake
+            {:luokka   :horizontal
+             :virheet  lomakkeen-virheet
+             :muokkaa! (fn [uusi]
+                         (reset! lomakkeen-tiedot uusi))}
+            (let [tiedot @lomakkeen-tiedot
+                  kentat (into []
+                               (concat
+                                 [{:otsikko "Kohde" :nimi :kohteen-nimi :hae #(:nimi @nav/valittu-urakka) :muokattava? (constantly false)}]
+                                 (map (fn [kentta] (lomake-kentta kentta tiedot)) (:parametrit @valittu-raporttityyppi))))]
+              kentat)
 
-           @lomakkeen-tiedot])]))))
+            @lomakkeen-tiedot])]))))
 
 (defn raportit []
   (komp/luo
-    (fn []
-      [:div "Raporttinäkymiä ei ole vielä aloitettu."]
-      #_(if @valittu-raportti
+    (fn [] ; FIXME Urakan oltava valittuna, muuten ei toimi. Valitse hallintayksikkö -komponentti voisi olla myös täällä geneerisenä komponenttina.
+      (if @valittu-raportti
         [raporttinakyma]
         [raporttivalinnat]))))
