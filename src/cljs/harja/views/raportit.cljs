c4092529937a35a0ce408625efc8e406e3055d67
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-09-07 14:32:07 +0300

    Hae uusi raportti kun valittu kuukausi muuttuu.

--- src/cljs/harja/views/raportit.cljs
+++ src/cljs/harja/views/raportit.cljs
@@ -23,8 +23,51 @@
                    [cljs.core.async.macros :refer [go]]))
 
 (defonce valittu-raporttityyppi (atom nil))
-(defonce valitun-raportin-sisalto (atom nil))
-(tarkkaile! "[RAPORTTI] Valitun raportin sisältö: " valitun-raportin-sisalto)
+(defonce raportin-parametrit-lomake (atom nil))
+(defonce yksikkohintaiset-toteumat
+         (reaction<! [urakka-id (:id @nav/valittu-urakka)
+                      alkupvm (first (:kuukausi @raportin-parametrit-lomake))
+                      loppupvm (second (:kuukausi @raportin-parametrit-lomake))]
+                     (when (and urakka-id alkupvm loppupvm)
+                       (log "[RAPORTTI] Haetaan yks. hint. kuukausiraportti parametreilla: " urakka-id alkupvm loppupvm)
+                       (raportit/hae-yksikkohintaisten-toiden-kuukausiraportti urakka-id alkupvm loppupvm))))
+
+(defonce yksikkohintaiset-toteumat-kaikkine-tietoineen
+         (reaction
+           (when @yksikkohintaiset-toteumat
+                     (let [tehtavat (map
+                                      (fn [tasot] (nth tasot 3))
+                                      @u/urakan-toimenpiteet-ja-tehtavat)
+                           toteumat-kaikkine-tietoineen (mapv
+                                                          (fn [toteuma]
+                                                            (let [tehtavan-tiedot
+                                                                  (first (filter (fn [tehtava]
+                                                                                   (= (:id tehtava) (:toimenpidekoodi_id toteuma)))
+                                                                                 tehtavat))
+                                                                  yksikkohinta-hoitokaudella (or (:yksikkohinta (first (filter
+                                                                                                                         (fn [tyo] (and (= (:tehtava tyo) (:toimenpidekoodi_id toteuma))
+                                                                                                                                        (pvm/sama-pvm? (:alkupvm tyo) (first (:hoitokausi @raportin-parametrit-lomake)))))
+                                                                                                                         @u/urakan-yks-hint-tyot))) nil)
+                                                                  suunniteltu-maara-hoitokaudella (or (:maara (first (filter
+                                                                                                                       (fn [tyo] (and (= (:tehtava tyo) (:toimenpidekoodi_id toteuma))
+                                                                                                                                      (pvm/sama-pvm? (:alkupvm tyo) (first (:hoitokausi @raportin-parametrit-lomake)))))
+                                                                                                                       @u/urakan-yks-hint-tyot))) nil)]
+                                                              (-> toteuma
+                                                                  (merge (dissoc tehtavan-tiedot :id))
+                                                                  (assoc :yksikkohinta yksikkohinta-hoitokaudella)
+                                                                  (assoc :suunniteltu-maara-hoitokaudella suunniteltu-maara-hoitokaudella))))
+                                                          @yksikkohintaiset-toteumat)]
+                       toteumat-kaikkine-tietoineen))))
+
+(tarkkaile! "[RAPORTTI] Valitun raportin sisältö: " yksikkohintaiset-toteumat-kaikkine-tietoineen)
+(tarkkaile! "[RAPORTTI] Raporttivalinnat-tiedot: " raportin-parametrit-lomake)
+(def raportin-parametrit-lomakkeen-virheet (atom nil))
+(def raportti-valmis-naytettavaksi?
+  (reaction (let [valittu-raporttityyppi @valittu-raporttityyppi
+                  lomake-virheet @raportin-parametrit-lomakkeen-virheet]
+              (and valittu-raporttityyppi
+                   (not (nil? lomake-virheet))
+                   (empty? lomake-virheet)))))
 
 (def +raporttityypit+
   ; FIXME: Hardcoodattu testidata, tämän on tarkoitus tulla myöhemmin serveriltä(?)
@@ -42,28 +85,28 @@
                  :tyyppi   :valinta
                  :validoi  [[:ei-tyhja "Anna arvo"]]
                  :valinnat :valitun-aikavalin-kuukaudet}]
-    :render   (fn []
-                [grid/grid
-                 {:otsikko      "Yksikköhintaisten töiden kuukausiraportti"
-                  :tyhja        (if (empty? @valitun-raportin-sisalto) "Ei raportoitavaa tietoa.")
-                  :voi-muokata? false}
-                 [{:otsikko "Päivämäärä" :nimi :alkanut :muokattava? (constantly false) :tyyppi :pvm :fmt pvm/pvm-aika :leveys "20%"}
-                  {:otsikko "Tehtävän nro" :nimi :toimenpidekoodi_id :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
-                  {:otsikko "Tehtävä" :nimi :nimi :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
-                  {:otsikko "Yksikkö" :nimi :yksikko :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
-                  {:otsikko "Yksikköhinta" :nimi :yksikkohinta :muokattava? (constantly false) :tyyppi :numero :leveys "20%" :fmt fmt/euro-opt}
-                  {:otsikko "Suunniteltu määrä hoitokaudella" :nimi :suunniteltu-maara-hoitokaudella :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
-                  {:otsikko "Toteutunut määrä" :nimi :toteutunut_maara :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
-                  {:otsikko "Suunnitellut kustannukset hoitokaudella" :nimi :suunnitellut-kustannukset-hoitokaudella :fmt fmt/euro-opt :hae (fn [rivi] (* (:yksikkohinta rivi) (:suunniteltu-maara-hoitokaudella rivi))) :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
-                  {:otsikko "Toteutuneet kustannukset" :nimi :toteutuneet-kustannukset :fmt fmt/euro-opt :hae (fn [rivi] (* (:yksikkohinta rivi) (:toteutunut_maara rivi))) :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
-                  {:otsikko "Lisätieto" :nimi :lisatieto :muokattava? (constantly false) :hae (fn [rivi] (let [max-pituus 80
-                                                                                                               lisatieto (if (vector? (:lisatieto rivi))
-                                                                                                                           (clojure.string/join "\n\n" (:lisatieto rivi))
-                                                                                                                           (:lisatieto rivi))]
-                                                                                                           (if (> (count lisatieto) max-pituus)
-                                                                                                             (str (subs lisatieto 0 max-pituus) "...")
-                                                                                                             lisatieto))) :tyyppi :string :leveys "20%"}]
-                 @valitun-raportin-sisalto])}])
+    :render    (fn []
+                 [grid/grid
+                  {:otsikko      "Yksikköhintaisten töiden kuukausiraportti"
+                   :tyhja        (if (empty? @yksikkohintaiset-toteumat-kaikkine-tietoineen) "Ei raportoitavaa tietoa.")
+                   :voi-muokata? false}
+                  [{:otsikko "Päivämäärä" :nimi :alkanut :muokattava? (constantly false) :tyyppi :pvm :fmt pvm/pvm-aika :leveys "20%"}
+                   {:otsikko "Tehtävän nro" :nimi :toimenpidekoodi_id :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
+                   {:otsikko "Tehtävä" :nimi :nimi :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
+                   {:otsikko "Yksikkö" :nimi :yksikko :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
+                   {:otsikko "Yksikköhinta" :nimi :yksikkohinta :muokattava? (constantly false) :tyyppi :numero :leveys "20%" :fmt fmt/euro-opt}
+                   {:otsikko "Suunniteltu määrä hoitokaudella" :nimi :suunniteltu-maara-hoitokaudella :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
+                   {:otsikko "Toteutunut määrä" :nimi :toteutunut_maara :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
+                   {:otsikko "Suunnitellut kustannukset hoitokaudella" :nimi :suunnitellut-kustannukset-hoitokaudella :fmt fmt/euro-opt :hae (fn [rivi] (* (:yksikkohinta rivi) (:suunniteltu-maara-hoitokaudella rivi))) :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
+                   {:otsikko "Toteutuneet kustannukset" :nimi :toteutuneet-kustannukset :fmt fmt/euro-opt :hae (fn [rivi] (* (:yksikkohinta rivi) (:toteutunut_maara rivi))) :muokattava? (constantly false) :tyyppi :numero :leveys "20%"}
+                   {:otsikko "Lisätieto" :nimi :lisatieto :muokattava? (constantly false) :hae (fn [rivi] (let [max-pituus 80
+                                                                                                                lisatieto (if (vector? (:lisatieto rivi))
+                                                                                                                            (clojure.string/join "\n\n" (:lisatieto rivi))
+                                                                                                                            (:lisatieto rivi))]
+                                                                                                            (if (> (count lisatieto) max-pituus)
+                                                                                                              (str (subs lisatieto 0 max-pituus) "...")
+                                                                                                              lisatieto))) :tyyppi :string :leveys "20%"}]
+                  @yksikkohintaiset-toteumat-kaikkine-tietoineen])}])
 
 (defn tee-lomakekentta [kentta lomakkeen-tiedot]
   (if (= :valinta (:tyyppi kentta))
@@ -78,52 +121,9 @@
                     :valinta-nayta #(if % (str (t/month (first %)) "/" (t/year (first %))) "- Valitse kuukausi -")))
     kentta))
 
-(def raporttivalinnat-tiedot (atom nil))
-(tarkkaile! "[RAPORTTI] Raporttivalinnat-tiedot: " raporttivalinnat-tiedot)
-(def raporttivalinnat-virheet (atom nil))
-(def raportti-valmis-naytettavaksi?
-  (reaction (let [valittu-raporttityyppi @valittu-raporttityyppi
-                  lomake-virheet @raporttivalinnat-virheet]
-              (and valittu-raporttityyppi
-                   (not (nil? lomake-virheet))
-                   (empty? lomake-virheet)))))
-
-(defn yk-hint-kuukausiraportti []
-  (let [urakka-id (:id @nav/valittu-urakka)
-        alkupvm (first (:kuukausi @raporttivalinnat-tiedot)) ; FIXME Näiden pitäisi päivittyä kun putodotusvalikon item vaihtuu.
-        loppupvm (second (:kuukausi @raporttivalinnat-tiedot))
-        tehtavat (map
-                   (fn [tasot] (nth tasot 3))
-                   @u/urakan-toimenpiteet-ja-tehtavat)]
-    (go
-      (log "[RAPORTTI] Haetaan yks. hint. kuukausiraportti parametreilla: " urakka-id alkupvm loppupvm)
-      (let [toteumat (<! (raportit/hae-yksikkohintaisten-toiden-kuukausiraportti urakka-id alkupvm loppupvm))
-            toteumat-kaikkine-tietoineen (mapv
-                                           (fn [toteuma]
-                                             (let [tehtavan-tiedot
-                                                   (first (filter (fn [tehtava]
-                                                                    (= (:id tehtava) (:toimenpidekoodi_id toteuma)))
-                                                                  tehtavat))
-                                                   yksikkohinta-hoitokaudella (or (:yksikkohinta (first (filter
-                                                                                            (fn [tyo] (and (= (:tehtava tyo) (:toimenpidekoodi_id toteuma))
-                                                                                                           (pvm/sama-pvm? (:alkupvm tyo) (first (:hoitokausi @raporttivalinnat-tiedot)))))
-                                                                                            @u/urakan-yks-hint-tyot))) nil)
-                                                   suunniteltu-maara-hoitokaudella (or (:maara (first (filter
-                                                                                                        (fn [tyo] (and (= (:tehtava tyo) (:toimenpidekoodi_id toteuma))
-                                                                                                                       (pvm/sama-pvm? (:alkupvm tyo) (first (:hoitokausi @raporttivalinnat-tiedot)))))
-                                                                                                        @u/urakan-yks-hint-tyot))) nil)]
-                                               (-> toteuma
-                                                   (merge (dissoc tehtavan-tiedot :id))
-                                                   (assoc :yksikkohinta yksikkohinta-hoitokaudella)
-                                                   (assoc :suunniteltu-maara-hoitokaudella suunniteltu-maara-hoitokaudella))))
-                                           toteumat)]
-        (reset! valitun-raportin-sisalto toteumat-kaikkine-tietoineen)))
-    (fn []
-      (:render @valittu-raporttityyppi))))
-
 (defn raporttinakyma []
-  (case (:nimi @valittu-raporttityyppi)
-    :yk-hint-kuukausiraportti (yk-hint-kuukausiraportti)))
+  (fn []
+    (:render @valittu-raporttityyppi)))
 
 (defn raporttivalinnat
   []
@@ -142,10 +142,10 @@
        (when @valittu-raporttityyppi
          [lomake/lomake
           {:luokka   :horizontal
-           :virheet  raporttivalinnat-virheet
+           :virheet  raportin-parametrit-lomakkeen-virheet
            :muokkaa! (fn [uusi]
-                       (reset! raporttivalinnat-tiedot uusi))}
-          (let [lomake-tiedot @raporttivalinnat-tiedot
+                       (reset! raportin-parametrit-lomake uusi))}
+          (let [lomake-tiedot @raportin-parametrit-lomake
                 kentat (into []
                              (concat
                                [{:otsikko "Kohde" :nimi :kohteen-nimi :hae #(:nimi @nav/valittu-urakka) :muokattava? (constantly false)}]
@@ -155,10 +155,10 @@
                                  (:parametrit @valittu-raporttityyppi))))]
             kentat)
 
-          @raporttivalinnat-tiedot])])))
+          @raportin-parametrit-lomake])])))
 
 (defn raporttivalinnat-ja-raportti []
-  (go (reset! u/urakan-yks-hint-tyot ; FIXME Tämä on pöllitty suoraan views.urakka-namespacesta. Voisiko jotenkin yhdistää yhdeksi funktioksi?
+  (go (reset! u/urakan-yks-hint-tyot                        ; FIXME Tämä on pöllitty suoraan views.urakka-namespacesta. Voisiko jotenkin yhdistää yhdeksi funktioksi?
               (s/prosessoi-tyorivit @nav/valittu-urakka
                                     (<! (yks-hint-tyot/hae-urakan-yksikkohintaiset-tyot (:id @nav/valittu-urakka))))))
   [:span
@@ -170,5 +170,5 @@
   (komp/luo
     (fn []
       (or
-        (urakat/valitse-hallintayksikko-ja-urakka) ; FIXME Voi olla tarve luoda raportti hallintayksikön alueesta tai koko Suomesta.
+        (urakat/valitse-hallintayksikko-ja-urakka)          ; FIXME Voi olla tarve luoda raportti hallintayksikön alueesta tai koko Suomesta.
         (raporttivalinnat-ja-raportti)))))
