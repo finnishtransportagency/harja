9f55480617ed6da075aadd2ee3f5287d27c110eb
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-09-08 13:56:42 +0300

    Lisää migraatio varustetoteuman kirjaamiselle API:n kautta.

---
+++ tietokanta/src/main/resources/db/migration/V1_187__.sql
@@ -0,0 +1,21 @@
+-- Nimi: Varustetoteuma-taulun lisääminen
+-- Kuvaus: ks. nimi
+
+CREATE TYPE varustetoteuma_tyyppi AS ENUM ('lisatty','paivitetty','poistettu');
+
+CREATE TABLE varustetoteuma (
+  id serial PRIMARY KEY,
+  tunniste varchar(128) UNIQUE,
+  tarkastus integer references toteuma (id),
+  toimenpide varustetoteuma_tyyppi,
+  tietolaji varchar(128),
+  ominaisuudet varchar(4096),
+  tr_numero integer,
+  tr_alkuosa integer,
+  tr_loppuosa integer,
+  tr_loppuetaisyys integer,
+  tr_alkuetaisyys integer,
+  aika date,
+  piiri integer,
+  kuntoluokka integer
+);
