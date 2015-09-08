d2b21bdf8f929939a11a95779d21aedf9b6f765a
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2015-09-08 13:11:46 +0300

    Paranna test.sh skriptiä
    Nyt skripti ajaa migraatiot, ja keskeyttää jos migraatiot epäonnistuvat.
    Ei tulosta testidata.sql:n lisäämistä. Ajaa unit testit,
    keskeyttää skriptin jos tulee virheitä. Lopuksi käynnistää fighweelin
    taustalle ja replin, ja lopettaessa oikeasti tappaa taustalla olevan
    figwheelin. Aiemmin tapettiin ilmeisesti lein-prosessi, ja figwheel
    jäi taustalle varaamaan portin. Tai jotain sinne päin.

--- tietokanta/testikanta_template.sh
+++ tietokanta/testikanta_template.sh
@@ -2,6 +2,10 @@
 
 psql=psql
 
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
+
 $psql -p5432 -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'harjatest' AND pid <> pg_backend_pid();"
 $psql -p5432 -c "DROP DATABASE IF EXISTS harjatest_template;"
 $psql -p5432 -c "CREATE USER harjatest WITH CREATEDB;"
@@ -14,7 +18,11 @@ $psql -p5432 harjatest_template -c "CREATE EXTENSION IF NOT EXISTS postgis_topol
 
 $psql -p5432 harjatest_template -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO harjatest;"
 
-mvn clean compile -Pharjatest flyway:migrate
+echo "** Aloitetaan migrate **"
+try mvn clean compile -Pharjatest flyway:migrate
 
-PGOPTIONS='--client-min-messages=warning' $psql -p5432 harjatest_template -X -q -a -l -v ON_ERROR_STOP=1 --pset pager=off -f testidata.sql
+PGOPTIONS='--client-min-messages=warning'
 
+echo "\n** Luodaan testidataa **"
+try $psql -p5432 harjatest_template -X -q -a -l -v ON_ERROR_STOP=1 --pset pager=off -f testidata.sql > /dev/null
+echo "** Testikannan template on valmis **"
--- vagrant/migrate_and_clean.sh
+++ vagrant/migrate_and_clean.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "Muunnetaan ja putsataan sovelluskanta"
+set -e
 
 vagrant ssh -c "cd /harja-tietokanta; sudo -u postgres sh ./kanta_template.sh"
-vagrant ssh -c "cd /harja-tietokanta; sudo -u postgres sh ./kanta_uusiksi.sh"
+vagrant ssh -c "cd /harja-tietokanta; sudo -u postgres sh ./kanta_uusiksi.sh" &> /dev/null
