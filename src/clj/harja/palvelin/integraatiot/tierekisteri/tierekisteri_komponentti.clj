e644392478f6724ee25b491089a72dae674b0811
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2015-09-08 15:00:33 +0300

    Tietueiden haku id:llä, alussa

--- src/clj/harja/palvelin/integraatiot/tierekisteri/tierekisteri_komponentti.clj
+++ src/clj/harja/palvelin/integraatiot/tierekisteri/tierekisteri_komponentti.clj
@@ -1,10 +1,19 @@
 (ns harja.palvelin.integraatiot.tierekisteri.tierekisteri-komponentti
   (:require
     [com.stuartsierra.component :as component]
-    [harja.palvelin.integraatiot.tierekisteri.tietolajit :as tietolajit]))
+    [harja.palvelin.integraatiot.tierekisteri.tietolajit :as tietolajit]
+    [harja.palvelin.integraatiot.tierekisteri.tietueet :as tietueet]))
+
+(defn validoi-tunniste [tunniste]
+  (when (not
+          (contains? #{"tl523" "tl501" "tl517" "tl507" "tl508" "tl506" "tl522" "tl513" "tl196" "tl519" "tl505" "tl195"
+                       "tl504" "tl198" "tl518" "tl514" "tl509" "tl515" "tl503" "tl510" "tl512" "tl165" "tl516" "tl511"}
+                     tunniste))
+    (throw+ {:type :tierekisteri-kutsu-epaonnistui :error (str "Tietolajia ei voida hakea. Tuntematon tietolaji: " tunniste)})))
 
 (defprotocol TierekisteriPalvelut
-  (hae-tietolajit [this tunniste muutospvm]))
+  (hae-tietolajit [this tietolajitunniste muutospvm])
+  (hae-tietue [this tietueen-tunniste tietolajitunniste]))
 
 (defrecord Tierekisteri [tierekisteri-api-url]
   component/Lifecycle
@@ -12,10 +21,13 @@
   (stop [this] this)
 
   TierekisteriPalvelut
-  (hae-tietolajit [this tunniste muutospvm]
-    (let [tietolajien-haku
-          (if (not (empty? tierekisteri-api-url))
-            (tietolajit/hae-tietolajit (:integraatioloki this) tierekisteri-api-url tunniste muutospvm)
-            (fn []))]
-      {:tietolajien-haku tietolajien-haku})))
+  (hae-tietolajit [this tietolajitunniste muutospvm]
+    (validoi-tunniste tietolajitunniste)
+    (when (not (empty? tierekisteri-api-url))
+      (tietolajit/hae-tietolajit (:integraatioloki this) tierekisteri-api-url tietolajitunniste muutospvm)))
+
+  (hae-tietue [this tietueen-tunniste tietolajitunniste]
+    (validoi-tunniste tietolajitunniste)
+    (when-not (empty? tierekisteri-api-url)
+      (tietueet/hae-tietueet (:integraatioloki this) tierekisteri-api-url tietueen-tunniste tietolajitunniste))))
 
--- src/clj/harja/palvelin/integraatiot/tierekisteri/tietolajit.clj
+++ src/clj/harja/palvelin/integraatiot/tierekisteri/tietolajit.clj
@@ -6,13 +6,6 @@
             [harja.palvelin.integraatiot.integraatiopisteet.http :as http])
   (:use [slingshot.slingshot :only [try+ throw+]]))
 
-(defn validoi-tunniste [tunniste]
-  (when (not
-          (contains? #{"tl523" "tl501" "tl517" "tl507" "tl508" "tl506" "tl522" "tl513" "tl196" "tl519" "tl505" "tl195"
-                       "tl504" "tl198" "tl518" "tl514" "tl509" "tl515" "tl503" "tl510" "tl512" "tl165" "tl516" "tl511"}
-                     tunniste))
-    (throw+ {:type :tierekisteri-kutsu-epaonnistui :error (str "Tietolajia ei voida hakea. Tuntematon tietolaji: " tunniste)})))
-
 (defn kasittele-virheet [url tunniste muutospvm virheet]
   (throw+ {:type  :tierekisteri-kutsu-epaonnistui
            :error (str "Tietolajin haku epäonnistui (URL: " url ") tunnisteella: " tunniste
@@ -36,7 +29,6 @@
         vastausdata))))
 
 (defn hae-tietolajit [integraatioloki url tunniste muutospvm]
-  (validoi-tunniste tunniste)
   (log/debug "Hae tietolajin: " tunniste " ominaisuudet muutospäivämäärällä: " nil " Tierekisteristä")
   (let [kutsudata (kutsusanoma/muodosta tunniste muutospvm)
         palvelu-url (str url "/haetietolajit")
