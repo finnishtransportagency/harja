57eef48d960a3745fcc03256f53a68c955143501
mikkoro <mikko.ronkkomaki@solita.fi>
2015-09-08 09:00:30 +0300

    Lisää käsittely Tierekisteristä palautuville virheille

--- src/clj/harja/palvelin/integraatiot/tierekisteri/tietolajit.clj
+++ src/clj/harja/palvelin/integraatiot/tierekisteri/tietolajit.clj
@@ -1,5 +1,6 @@
 (ns harja.palvelin.integraatiot.tierekisteri.tietolajit
   (:require [taoensso.timbre :as log]
+            [clojure.string :as string]
             [harja.palvelin.integraatiot.tierekisteri.sanomat.tietolajin-hakukutsu :as kutsusanoma]
             [harja.palvelin.integraatiot.tierekisteri.sanomat.vastaus :as vastaussanoma]
             [harja.palvelin.integraatiot.tierekisteri.tyokalut.kutsukasittely :as tierekisteri-palvelu]
@@ -16,7 +17,18 @@
           (contains? #{"tl523" "tl501" "tl517" "tl507" "tl508" "tl506" "tl522" "tl513" "tl196" "tl519" "tl505" "tl195"
                        "tl504" "tl198" "tl518" "tl514" "tl509" "tl515" "tl503" "tl510" "tl512" "tl165" "tl516" "tl511"}
                      tunniste))
-    (throw+ {:type :tierekisteri-kutsu-epaonnistui :error (str "Tuntematon tietolaji: " tunniste)})))
+    (throw+ {:type :tierekisteri-kutsu-epaonnistui :error (str "Tietolajia ei voida hakea. Tuntematon tietolaji: " tunniste)})))
+
+(defn kasittele-virheet [url tunniste muutospvm virheet]
+  (throw+ {:type  :tierekisteri-kutsu-epaonnistui
+           :error (str "Tietolajin haku epäonnistui (URL: " url ") tunnisteella: " tunniste
+                       " & muutospäivämäärällä: " muutospvm "."
+                       "Virheet: " (string/join virheet))}))
+
+(defn kirjaa-varoitukset [url tunniste muutospvm virheet]
+  (log/warn (str "Tietolajin haku palautti virheitä (URL: " url ") tunnisteella: " tunniste
+                 " & muutospäivämäärällä: " muutospvm "."
+                 "Virheet: " (string/join virheet))))
 
 (defn hae-tietolajit [integraatioloki url tunniste muutospvm]
   (validoi-tunniste tunniste)
@@ -24,8 +36,16 @@
   (let [kutsudata (kutsusanoma/muodosta tunniste muutospvm)
         palvelu-url (str url "/haetietolajit")
         vastausxml (tierekisteri-palvelu/kutsu-palvelua integraatioloki "hae-tietolaji" palvelu-url kutsudata)
-        vastausdata (vastaussanoma/lue vastausxml)]
-    vastausdata))
+        vastausdata (vastaussanoma/lue vastausxml)
+        onnistunut (:onnistunut vastausdata)
+        virheet (:virheet vastausdata)  ]
+
+    (if (not onnistunut)
+      (kasittele-virheet url tunniste muutospvm virheet)
+      (do
+        (when (not-empty virheet)
+          (kirjaa-varoitukset url tunniste muutospvm virheet))
+        vastausdata))))
 
 (defn kutsu [tunniste]
   (let [testitietokanta (apply tietokanta/luo-tietokanta testi/testitietokanta)
