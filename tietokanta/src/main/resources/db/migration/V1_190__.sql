f11168727b662fc078a53bf7eab6ac0c9dd0b7f2
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-09-09 15:44:25 +0300

    Korjaa varustetoteuman tauluun tarkempi luotu-kenttä.

--- tietokanta/src/main/resources/db/migration/V1_190__.sql
+++ tietokanta/src/main/resources/db/migration/V1_190__.sql
@@ -2,4 +2,4 @@
 -- Kuvaus: Koska jos toteumaan linkitetään useampi varustetoteuma, ei ole omia aikaleimoja.
 
 ALTER TABLE varustetoteuma ADD COLUMN luoja integer REFERENCES kayttaja (id);
-ALTER TABLE varustetoteuma ADD COLUMN luotu date;
+ALTER TABLE varustetoteuma ADD COLUMN luotu timestamp DEFAULT current_timestamp,;
