14eef1f614ab764329956037a8c58ddf2df6d705
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-09-11 11:48:05 +0300

    Käytä uusinta toteuma-apia.

--- src/clj/harja/palvelin/integraatiot/api/varustetoteuma.clj
+++ src/clj/harja/palvelin/integraatiot/api/varustetoteuma.clj
@@ -55,7 +55,8 @@
 (defn tallenna-toteuma [db urakka-id kirjaaja data]
   (jdbc/with-db-transaction [transaktio db]
                             (let [toteuma (get-in data [:varustetoteuma :toteuma])
-                                  toteuma-id (api-toteuma/paivita-tai-luo-uusi-toteuma transaktio urakka-id kirjaaja toteuma)
+                                  tunniste (api-toteuma/luo-toteuman-tunniste (get-in data [:otsikko :lahettaja :jarjestelma]) (get-in data [:varustetoteuma :toteuma :tunniste :id]))
+                                  toteuma-id (api-toteuma/paivita-tai-luo-uusi-toteuma transaktio urakka-id kirjaaja tunniste toteuma)
                                   varustetiedot (get-in data [:varustetoteuma :varuste])]
                               (log/debug "Toteuman perustiedot tallennettu. id: " toteuma-id)
                               (log/debug "Aloitetaan sijainnin tallennus")
