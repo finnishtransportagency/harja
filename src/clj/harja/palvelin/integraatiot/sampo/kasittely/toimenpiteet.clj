bd4f093e57d7564124efc0474d28ea24f191c735
mikkoro <mikko.ronkkomaki@solita.fi>
2015-09-14 09:58:48 +0300

    Estä toimepideinstanssin tallentaminen ilman koodia

--- src/clj/harja/palvelin/integraatiot/sampo/kasittely/toimenpiteet.clj
+++ src/clj/harja/palvelin/integraatiot/sampo/kasittely/toimenpiteet.clj
@@ -38,18 +38,20 @@
 (defn kasittele-toimenpide [db {:keys [viesti-id sampo-id nimi alkupvm loppupvm vastuuhenkilo-id talousosasto-id talousosasto-polku tuote-id tuote-polku urakka-sampo-id sampo-toimenpidekoodi]}]
   (log/debug "Käsitellään toimenpide Sampo id:llä: " sampo-id ", viesti id:" viesti-id)
 
-  (try
-    (let [toimenpide-id (tallenna-toimenpide db sampo-id nimi alkupvm loppupvm vastuuhenkilo-id talousosasto-id talousosasto-polku tuote-id tuote-polku urakka-sampo-id sampo-toimenpidekoodi)]
-      (log/debug "Käsiteltävän toimenpiteet id on:" toimenpide-id)
+  (if (empty? sampo-toimenpidekoodi)
+    (log/warn "Samposta tuodulla toimenpideinstanssilla (id: " sampo-id ") ei ole toimenpidekoodia, joten sitä ei voi tallentaa")
+    (try
+      (let [toimenpide-id (tallenna-toimenpide db sampo-id nimi alkupvm loppupvm vastuuhenkilo-id talousosasto-id talousosasto-polku tuote-id tuote-polku urakka-sampo-id sampo-toimenpidekoodi)]
+        (log/debug "Käsiteltävän toimenpiteet id on:" toimenpide-id)
 
-      (log/debug "Toimenpide käsitelty onnistuneesti")
-      (kuittaus-sanoma/muodosta-onnistunut-kuittaus viesti-id "Operation"))
-    (catch Exception e
-      (log/error e "Tapahtui poikkeus tuotaessa toimenpidettä Samposta (Sampo id:" sampo-id ", viesti id:" viesti-id ").")
-      (let [kuittaus (kuittaus-sanoma/muodosta-muu-virhekuittaus viesti-id "Operation" "Internal Error")]
-        (throw+ {:type     virheet/+poikkeus-samposisaanluvussa+
-                 :kuittaus kuittaus
-                 :virheet  [{:poikkeus e}]})))))
+        (log/debug "Toimenpide käsitelty onnistuneesti")
+        (kuittaus-sanoma/muodosta-onnistunut-kuittaus viesti-id "Operation"))
+      (catch Exception e
+        (log/error e "Tapahtui poikkeus tuotaessa toimenpidettä Samposta (Sampo id:" sampo-id ", viesti id:" viesti-id ").")
+        (let [kuittaus (kuittaus-sanoma/muodosta-muu-virhekuittaus viesti-id "Operation" "Internal Error")]
+          (throw+ {:type     virheet/+poikkeus-samposisaanluvussa+
+                   :kuittaus kuittaus
+                   :virheet  [{:poikkeus e}]}))))))
 
 (defn kasittele-toimenpiteet [db toimenpiteet]
   (mapv #(kasittele-toimenpide db %) toimenpiteet))
