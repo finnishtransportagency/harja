cb93868cd8fe35558e247313deba8fd369037f16
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-09-15 14:45:41 +0300

    Parempi migraatiotarkistaja
    Nyt tarkista-migraatiot lein taski käy kurkkimassa develop haaraan ja
    vertaa migraatioiden numeroita ja kuvauksia.

--- src/clj/harja/tyokalut/migraatiot.clj
+++ src/clj/harja/tyokalut/migraatiot.clj
@@ -1,33 +1,62 @@
 (ns harja.tyokalut.migraatiot
-  (require [clojure.string :as str])
+  (require [clojure.string :as str]
+           [clojure.java.shell :as sh])
+  
   (:gen-class))
 
+(def migraatiohakemisto "tietokanta/src/main/resources/db/migration")
+
 (defn migraatiotiedostot []
-  (->> "tietokanta/src/main/resources/db/migration"
+  (->> migraatiohakemisto
        (java.io.File.)
        .listFiles
-       (map #(.getName %))))
+       (map #(.getName %))
+       (filter #(and (not= % "V1__Perustiedot.sql")
+                     (.startsWith % "V1_")
+                     (.endsWith % ".sql")))))
 
-(defn tarkista-duplikaattinumerot []
-  (->> (migraatiotiedostot)
-       (map #(re-find #"V1_(\d+)__(.*).sql" %))
-       (map #(drop 1 %))
-       (group-by first)
-       (some (fn [[num filet]]
-               (when (and num (> (count filet) 1))
-                 (str num ": " (str/join ", " (map second filet))))))))
+(defn numero-ja-kuvaus [tiedostonimi]
+  (let [[_ num kuvaus] (re-find #"V1_(\d+)__(.*).sql" tiedostonimi)
+        kuvaus (if (str/blank? kuvaus)
+                 (first (str/split (slurp (str migraatiohakemisto "/" tiedostonimi)) #"\n"))
+                 kuvaus)]
+    [(Integer/parseInt num) kuvaus]))
+
+(defn migraatiot []
+  (as-> (migraatiotiedostot) m
+    (map numero-ja-kuvaus m)
+    (zipmap (map first m)
+            (map second m))))
 
 (defn tarkista-skandit []
   (->> (migraatiotiedostot)
        (keep #(when (some {\ä \Ä \ö \Ö} %) %))))
 
-                    
+(defn nykyinen-branch []
+  (as-> (:out (sh/sh "git" "branch")) res
+    (str/split res #"\n")
+    (filter #(.startsWith % "* ") res)
+    (first res)
+    (.substring res 2)))
+
+(defn vaihda-branch [b]
+  (sh/sh "git" "checkout" b)
+  (assert (= (nykyinen-branch) b) "Branching vaihto ei onnistunut"))
+
 (defn -main [& args]
-  (let [dup (tarkista-duplikaattinumerot)
-        skandit (tarkista-skandit)]
-    (if dup
-      (println "DUPLIKAATTI: " dup)
-      (println "Ei duplikaatteja."))
-    (if-not (empty? skandit)
-      (println "Skandeja nimessä: " (str/join ", " skandit))
-      (println "Ei skandeja."))))
+  (let [nykyinen-branch (nykyinen-branch)]
+    (if (= "develop" nykyinen-branch)
+      (println "Olet jo developissa!")
+      (let [branchin-migraatiot (migraatiot)]
+        (vaihda-branch "develop")
+        (let [developin-migraatiot (migraatiot)]
+          (println "Korkein migraatio tässä branchissa: " (reduce max (keys branchin-migraatiot)))
+          (println "Korkein migraatio develop branchissa: " (reduce max (keys developin-migraatiot)))
+          (doseq [[num branch-kuvaus] branchin-migraatiot
+                  :let [develop-kuvaus (get developin-migraatiot num)]]
+            (when (not= branch-kuvaus develop-kuvaus)
+              (println "Migraatio " num " kuvaus eroaa tämän ja developin välillä!\n"
+                       "  Tässä haarassa:" branch-kuvaus "\n"
+                       "  Developissa:   " develop-kuvaus))))
+        (vaihda-branch nykyinen-branch)))
+    (System/exit 0)))
