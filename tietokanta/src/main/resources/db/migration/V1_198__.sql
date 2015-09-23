98d84d8936b6bfa849c559a4db4635593758a848
mikkoro <mikko.ronkkomaki@solita.fi>
2015-09-23 14:26:38 +0300

    Aseta oletuksena toimenpidekoodin kokonaishintaisuus falseksi

--- tietokanta/src/main/resources/db/migration/V1_198__.sql
+++ tietokanta/src/main/resources/db/migration/V1_198__.sql
@@ -1,2 +1,6 @@
 -- Kuvaus: Lisää toimenpidekoodille lippu kokonaishintaisuudesta
-ALTER TABLE toimenpidekoodi ADD COLUMN kokonaishintainen BOOLEAN;
+ALTER TABLE toimenpidekoodi ADD COLUMN kokonaishintainen BOOLEAN DEFAULT FALSE;
+
+UPDATE toimenpidekoodi
+SET kokonaishintainen = FALSE
+WHERE kokonaishintainen IS NULL;
