be7700e0069c0f42da85525d8388d949e87dea79
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-09-25 13:43:50 +0300

    Luo testidata lähetystä varten

--- test/clj/harja/palvelin/integraatiot/tierekisteri/sanomat/lisaa_tietue_test.clj
+++ test/clj/harja/palvelin/integraatiot/tierekisteri/sanomat/lisaa_tietue_test.clj
@@ -5,7 +5,28 @@
   (:import (java.text SimpleDateFormat)
            (harja.palvelin.integraatiot.integraatioloki Integraatioloki)))
 
-(def lisattava-testitietue {:asd 123}) ; FIXME TODO
+(def lisattava-testitietue {:lisaaja {:henkilo      "Keijo Käsittelijä"
+                                      :jarjestelma  "FastMekka"
+                                      :organisaatio "Asfaltia Oy"
+                                      :yTunnus      "1234567-8"}
+                            :tietue  {:tunniste          "1245rgfsd"
+                                      :alkupvm           "2015-03-03+02:00"
+                                      :loppupvm          "2015-03-03+02:00"
+                                      :karttapvm         "2015-03-03+02:00"
+                                      :piiri             "1"
+                                      :kuntoluokka       "1"
+                                      :urakka            "100"
+                                      :sijainti          {:tie {:numero  "1"
+                                                                :aet     "1"
+                                                                :aosa    "1"
+                                                                :let     "1"
+                                                                :losa    "1"
+                                                                :ajr     "1"
+                                                                :puoli   "1"
+                                                                :alkupvm "2015-03-03+02:00"}}
+                                      :tietolajitunniste "tl506"
+                                      :arvot             "998 2 0 1 0 1 1 Testi liikennemerkki Omistaja O 4 123456789 40"}
+                            :lisatty "2015-05-26+03:00"})
 
 (def +xsd-polku+ "xsd/tierekisteri/schemas/")
 
