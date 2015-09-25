7375134db6db7adc87d25f5c3f6564d9f665551f
mikkoro <mikko.ronkkomaki@solita.fi>
2015-09-25 12:15:46 +0300

    Palauta XSD-validointiin alkuperäinen skeemojen importtaus
    Uusi tapa ei toiminut haettaessa JAR -paketista skeemoja.

--- src/clj/harja/tyokalut/xml.clj
+++ src/clj/harja/tyokalut/xml.clj
@@ -3,8 +3,7 @@
             [clojure.java.io :as io]
             [clojure.zip :refer [xml-zip]]
             [taoensso.timbre :as log]
-            [hiccup.core :refer [html]]
-            [clojure.string :as str])
+            [hiccup.core :refer [html]])
   (:import (javax.xml.validation SchemaFactory)
            (javax.xml XMLConstants)
            (javax.xml.transform.stream StreamSource)
@@ -14,45 +13,25 @@
            (java.text SimpleDateFormat ParseException)
            (java.util Date)))
 
-(defn listaa-xsd-tiedostot [polku]
-  (let [xsdt (mapcat (fn [f]
-                       (if (.isDirectory f)
-                         (listaa-xsd-tiedostot f)
-                         (if-not (.endsWith (.getName f) ".xsd")
-                           nil
-                           [[(.getName f) (.getAbsolutePath f)]])))
-                     (seq (.listFiles polku)))]
-    (zipmap (map first xsdt)
-            (map second xsdt))))
-
-;; Validointi hakee rekursiivisesti kaikki xsd-tiedostot annetun polun alta
-;; on tärkeää välttää duplikaattinimiä tiedostoissa. Esimerkiksi xsd/tierekisteri/schemas ja
-;; xsd/jokumuu/schemas saavat sisältää samannimisiä tiedostoja, mutta tierekisteri/schemas sisällä tiedostoilla
-;; pitää olla uniikit nimet.
-;; Ratkaisuun päädyttiin koska aiempi versio ei tukenut tilanteita, joissa xsd keskenäiset riippuvuudet menivät
-;; syvemmälle kuin 1 taso (sijainti->tie->puoli)
 (defn validoi
   "Validoi annetun XML sisällön vasten annettua XSD-skeemaa. XSD:n tulee olla tiedosto annettussa XSD-polussa. XML on
   String, joka on sisältö."
   [xsd-polku xsd xml]
   (log/debug "Validoidaan XML käyttäen XSD-skeemaa:" xsd ". XML:n sisältö on:" xml)
-  (let [xsd-tiedostot (listaa-xsd-tiedostot (io/as-file (io/resource xsd-polku)))
-        schema-factory (SchemaFactory/newInstance XMLConstants/W3C_XML_SCHEMA_NS_URI)]
-
+  (let [schema-factory (SchemaFactory/newInstance XMLConstants/W3C_XML_SCHEMA_NS_URI)]
 
     (.setResourceResolver schema-factory
-                            (reify LSResourceResolver
-                              (resolveResource [this type namespaceURI publicId systemId baseURI]
-                                (let [xsd-file (xsd-tiedostot (last (str/split systemId #"/")))]
-                                  (reify LSInput
-                                    (getByteStream [_] (io/input-stream xsd-file))
-                                    (getPublicId [_] publicId)
-                                    (getSystemId [_] systemId)
-                                    (getBaseURI [_] baseURI)
-                                    (getCharacterStream [_] (io/reader xsd-file))
-                                    (getEncoding [_] "UTF-8")
-                                    (getStringData [_] (slurp xsd-file)))))))
-
+                          (reify LSResourceResolver
+                            (resolveResource [this type namespaceURI publicId systemId baseURI]
+                              (let [xsd-file (io/resource (str xsd-polku systemId))]
+                                (reify LSInput
+                                  (getByteStream [_] (io/input-stream xsd-file))
+                                  (getPublicId [_] publicId)
+                                  (getSystemId [_] systemId)
+                                  (getBaseURI [_] baseURI)
+                                  (getCharacterStream [_] (io/reader xsd-file))
+                                  (getEncoding [_] "UTF-8")
+                                  (getStringData [_] (slurp xsd-file)))))))
     (try (-> schema-factory
              (.newSchema (StreamSource. (io/input-stream (io/resource (str xsd-polku xsd)))))
              .newValidator
@@ -90,7 +69,7 @@
   (when (and data (not (empty? data))) (Boolean/parseBoolean data)))
 
 (defn parsi-avain [data]
-  (when (and data (not (empty? data))) (keyword (str/lower-case data))))
+  (when (and data (not (empty? data))) (keyword (clojure.string/lower-case data))))
 
 (defn parsi-aika [formaatti data]
   (when (and data (not (empty? data)))
