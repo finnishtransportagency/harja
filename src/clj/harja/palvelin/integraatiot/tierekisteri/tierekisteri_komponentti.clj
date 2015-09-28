73ebc3daef39bd3057d0a72a9e4c387c07e2c0a0
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-09-28 13:59:28 +0300

    Korjaa virhe tietolajitunnisteen validoinnissa

--- src/clj/harja/palvelin/integraatiot/tierekisteri/tierekisteri_komponentti.clj
+++ src/clj/harja/palvelin/integraatiot/tierekisteri/tierekisteri_komponentti.clj
@@ -8,7 +8,7 @@
     [harja.palvelin.integraatiot.api.tyokalut.virheet :as virheet])
   (:use [slingshot.slingshot :only [try+ throw+]]))
 
-(defn validoi-tunniste [tunniste]
+(defn validoi-tietolajitunniste [tunniste]
   (when (not
           (contains? #{"tl523" "tl501" "tl517" "tl507" "tl508" "tl506" "tl522" "tl513" "tl196" "tl519" "tl505" "tl195"
                        "tl504" "tl198" "tl518" "tl514" "tl509" "tl515" "tl503" "tl510" "tl512" "tl165" "tl516" "tl511"}
@@ -30,23 +30,23 @@
 
   TierekisteriPalvelut
   (hae-tietolajit [this tietolajitunniste muutospvm]
-    (validoi-tunniste tietolajitunniste)
+    (validoi-tietolajitunniste tietolajitunniste)
     (when (not (empty? tierekisteri-api-url))
       (tietolajit/hae-tietolajit (:integraatioloki this) tierekisteri-api-url tietolajitunniste muutospvm)))
 
   (hae-tietue [this tietueen-tunniste tietolajitunniste]
-    (validoi-tunniste tietolajitunniste)
+    (validoi-tietolajitunniste tietolajitunniste)
     (when-not (empty? tierekisteri-api-url)
       (tietue/hae-tietue (:integraatioloki this) tierekisteri-api-url tietueen-tunniste tietolajitunniste)))
 
   (hae-tietueet [this tr tietolajitunniste muutospvm]
-    (validoi-tunniste tietolajitunniste)
+    (validoi-tietolajitunniste tietolajitunniste)
     (when-not (empty? tierekisteri-api-url)
       (tietueet/hae-tietueet
         (:integraatioloki this) tierekisteri-api-url tr tietolajitunniste muutospvm)))
 
-  (lisaa-tietue [this tietue]
-    (validoi-tunniste (get-in tietue [:tietue :tunniste])
+  (lisaa-tietue [this tiedot]
+    (validoi-tietolajitunniste (get-in tiedot [:tietue :tietolaji :tietolajitunniste] tiedot))
     (when-not (empty? tierekisteri-api-url)
-      (tietue/lisaa-tietue (:integraatioloki this) tierekisteri-api-url tietue)))))
+      (tietue/lisaa-tietue (:integraatioloki this) tierekisteri-api-url tiedot))))
 
