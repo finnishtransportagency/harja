975b515dbde9f1bab19d6e7c53e5c6b3e71017a7
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2015-09-29 13:34:20 +0300

    Korjaa turvallisuuspoikkeaman kartan paikka

--- src/cljs/harja/views/urakka/turvallisuus/turvallisuuspoikkeamat.cljs
+++ src/cljs/harja/views/urakka/turvallisuus/turvallisuuspoikkeamat.cljs
@@ -125,7 +125,6 @@
       {:on-click #(reset! tiedot/valittu-turvallisuuspoikkeama tiedot/+uusi-turvallisuuspoikkeama+)}
       (ikonit/plus) " Lisää turvallisuuspoikkeama"]
 
-     [kartta/kartan-paikka]
      [grid/grid
       {:otsikko       "Turvallisuuspoikkeamat"
        :tyhja         (if @tiedot/haetut-turvallisuuspoikkeamat "Ei löytyneitä tietoja" [ajax-loader "Haetaan sanktioita."])
@@ -156,6 +155,8 @@
        (reset! tiedot/karttataso-turvallisuuspoikkeamat false))}
 
     (fn []
-      (if @tiedot/valittu-turvallisuuspoikkeama
-        [turvallisuuspoikkeaman-tiedot]
-        [turvallisuuspoikkeamalistaus]))))
+      [:span
+       [kartta/kartan-paikka]
+       (if @tiedot/valittu-turvallisuuspoikkeama
+         [turvallisuuspoikkeaman-tiedot]
+         [turvallisuuspoikkeamalistaus])])))
