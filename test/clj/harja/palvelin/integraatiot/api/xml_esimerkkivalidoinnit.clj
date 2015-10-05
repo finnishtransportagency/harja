0ba8d3a1a73436ac355f1e0bc7cd8e8c51844bef
mikkoro <mikko.ronkkomaki@solita.fi>
2015-10-05 13:33:01 +0300

    Lisää vastausten validointi

--- test/clj/harja/palvelin/integraatiot/api/xml_esimerkkivalidoinnit.clj
+++ test/clj/harja/palvelin/integraatiot/api/xml_esimerkkivalidoinnit.clj
@@ -2,7 +2,6 @@
   (:require [clojure.test :refer [deftest is use-fixtures]]
             [harja.testi :refer :all]
             [harja.tyokalut.xml :as xml]
-            [harja.palvelin.integraatiot.api.tyokalut.xml-skeemat :as xml-skeemat]
             [harja.palvelin.integraatiot.api.tyokalut.xml-esimerkit :as xml-esimerkit]
             [clojure.java.io :as io]))
 
@@ -17,4 +16,12 @@
   (is (true? (validoi "haeTietueet.xsd" xml-esimerkit/+hae-tietueet-request+)))
   (is (true? (validoi "lisaaTietue.xsd" xml-esimerkit/+lisaa-tietue-request+)))
   (is (true? (validoi "paivitaTietue.xsd" xml-esimerkit/+paivita-tietue-request+)))
-  (is (true? (validoi "poistaTietue.xsd" xml-esimerkit/+poista-tietue-request+))))
+  (is (true? (validoi "poistaTietue.xsd" xml-esimerkit/+poista-tietue-request+)))
+  (is (true? (validoi "vastaus.xsd" xml-esimerkit/+hae-tietue-response+)))
+  (is (true? (validoi "vastaus.xsd" xml-esimerkit/+hae-tietueet-response+)))
+  (is (true? (validoi "vastaus.xsd" xml-esimerkit/+ok-vastaus-response+)))
+  (is (true? (validoi "vastaus.xsd" xml-esimerkit/+virhe-tietueen-lisays-epaonnistui-response+)))
+  (is (true? (validoi "vastaus.xsd" xml-esimerkit/+virhe-vastaus-tietolajia-ei-loydy-response+)))
+  (is (true? (validoi "vastaus.xsd" xml-esimerkit/+virhe-vastaus-tietuetta-ei-loydy-response+))))
+
+
