c5a10eb448051f45806fd26fb686eb192491ef51
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-10-05 10:13:12 +0300

    Käytä positiivinen numero -tyyppiä materiaalitoteumissa

--- src/cljs/harja/views/urakka/toteumat/materiaalit.cljs
+++ src/cljs/harja/views/urakka/toteumat/materiaalit.cljs
@@ -254,8 +254,8 @@
 
           [{:otsikko "Päivämäärä" :tyyppi :pvm :nimi :aloitus :leveys "20%"
             :hae (comp pvm/pvm :alkanut :toteuma) :muokattava? (constantly false)}
-           {:otsikko "Määrä" :nimi :toteuman_maara :tyyppi :numero :hae (comp :maara :toteuma) :aseta #(assoc-in %1 [:toteuma :maara] %2)
-            :validoi [[:positiivinen-luku]] :leveys "20%"}
+           {:otsikko "Määrä" :nimi :toteuman_maara :tyyppi :positiivinen-numero :hae (comp :maara :toteuma) :aseta #(assoc-in %1 [:toteuma :maara] %2)
+            :leveys "20%"}
            {:otsikko "Suorittaja" :nimi :suorittaja :tyyppi :text :hae (comp :suorittaja :toteuma) :muokattava? (constantly false) :leveys "20%"}
            {:otsikko "Lisätietoja" :nimi :lisatiedot :tyyppi :text :hae (comp :lisatieto :toteuma) :muokattava? (constantly false) :leveys "20%"}
            {:otsikko "Tarkastele koko toteumaa" :nimi :tarkastele-toteumaa :tyyppi :komponentti
