39bb4450d2df0aaf9491bd737ae91c93f092020b
mikkoro <mikko.ronkkomaki@solita.fi>
2015-10-05 10:33:01 +0300

    Testaa tietueen haku

--- test/clj/harja/palvelin/integraatiot/tierekisteri/tietolajin_haku_test.clj
+++ test/clj/harja/palvelin/integraatiot/tierekisteri/tietolajin_haku_test.clj
@@ -79,3 +79,35 @@
                                :alkupvm     #inst "2015-03-02T22:00:00.000-00:00",
                                :tunniste    "1245rgfsd"}]
           (is (= odotettu-tietue tietue)))))))
+
+(deftest tarkista-tietueen-haku
+  (let [vastaus-xml (slurp (io/resource "xsd/tierekisteri/examples/hae-tietue-response.xml"))]
+    (with-fake-http
+      [(str +testi-tierekisteri-url+ "/haetietue") vastaus-xml]
+      (let [vastausdata (tierekisteri/hae-tietue (:tierekisteri jarjestelma) "asdf" "tl506")
+            odotettu-tietue {:sijainti    {:koordinaatit {:x 0.0,
+                                                          :y 0.0,
+                                                          :z 0.0},
+                                           :linkki       {:id    1,
+                                                          :marvo 10},
+                                           :tie          {:numero  1,
+                                                          :aet     1,
+                                                          :aosa    1,
+                                                          :let     1,
+                                                          :losa    1,
+                                                          :ajr     1,
+                                                          :puoli   1,
+                                                          :alkupvm #inst "2017-03-02T22:00:00.000-00:00"}},
+                             :loppupvm    #inst "2015-03-02T22:00:00.000-00:00",
+                             :piiri       "1",
+                             :karttapvm   #inst "2015-03-02T22:00:00.000-00:00",
+                             :urakka      100,
+                             :tietolaji   {:tietolajitunniste "tl506",
+                                           :arvot             "9987 2 2 0 1 0 1 1 Testiliikennemerkki Omistaja O K 123456789 40"},
+                             :kuntoluokka "1",
+                             :alkupvm     #inst "2015-03-02T22:00:00.000-00:00",
+                             :tunniste    "1245rgfsd"}
+            tietue (:tietue vastausdata)]
+        (is (true? (:onnistunut vastausdata)))
+        (is (= odotettu-tietue tietue))))))
+
