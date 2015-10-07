561b17236bcc8b91a4cef747ad76d6003a389028
mikkoro <mikko.ronkkomaki@solita.fi>
2015-10-07 13:17:01 +0300

    Lisään ALK:n integraatiot integraatiologiin

---
+++ tietokanta/src/main/resources/db/migration/V1_206__.sql
@@ -0,0 +1,3 @@
+-- Kuvaus: Lisää ALK:n integraatiot
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('alk', 'hae-tiedoston-muutospaivamaara');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('alk', 'hae-tiedosto');
