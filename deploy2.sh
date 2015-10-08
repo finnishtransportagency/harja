36e8b55de156a5bf508474212d3d028426f5cfe6
Marko Lauronen <Marko.Lauronen@solita.fi>
2015-10-08 11:53:58 +0300

    git deploy korjauksia

--- deploy2.sh
+++ deploy2.sh
@@ -6,5 +6,5 @@ BRANCH=$2
 git push $HARJA_ENV $BRANCH
 
 pushd test_envs/upcloud
-ansible-playbook deploy2.yml -i inventory/harjadev --extra-vars "harja_migrate_only=false" --limit $HARJA_ENV
+ansible-playbook deploy2.yml -i inventory/harjadev --extra-vars "harja_migrate_only=false harja_branch=$BRANCH" --limit $HARJA_ENV
 popd
