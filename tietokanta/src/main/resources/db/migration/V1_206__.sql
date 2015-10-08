4f33af36fa83a7ec9b5fbc973b8dc13c13fa0d30
mikkoro <mikko.ronkkomaki@solita.fi>
2015-10-08 10:19:40 +0300

    Lisää integraatiot ALK:n sijasta PTJ:lle

--- tietokanta/src/main/resources/db/migration/V1_206__.sql
+++ tietokanta/src/main/resources/db/migration/V1_206__.sql
@@ -1,3 +1,16 @@
--- Kuvaus: Lisää ALK:n integraatiot
-INSERT INTO integraatio (jarjestelma, nimi) VALUES ('alk', 'hae-tiedoston-muutospaivamaara');
-INSERT INTO integraatio (jarjestelma, nimi) VALUES ('alk', 'hae-tiedosto');
+-- Kuvaus: Lisää PTJ:n integraatiot
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'tieverkon-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'tieverkon-muutospaivamaaran-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'talvihoitoluokkien-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'talvihoitoluokkien-muutospaivamaaran-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'soratienhoitoluokkien-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'soratienhoitoluokkien-muutospaivamaaran-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'pohjavesialueiden-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'pohjavesialueiden-muutospaivamaaran-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'siltojen-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'siltojen-muutospaivamaaran-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'ely-alueiden-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'ely-alueiden-muutospaivamaaran-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'hoidon-alueurakoiden-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('ptj', 'hoidon-alueurakoiden-muutospaivamaaran-haku');
+
