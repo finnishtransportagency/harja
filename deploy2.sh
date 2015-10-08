91b551eb8aaf879218cf53216d73d035c113f66d
Marko Lauronen <Marko.Lauronen@solita.fi>
2015-10-08 11:41:49 +0300

    deploy gitin kautta testipalvelimelle

---
+++ deploy2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+HARJA_ENV=harja-dev$1
+BRANCH=$2
+
+git push $HARJA_ENV $BRANCH
+
+pushd test_envs/upcloud
+ansible-playbook deploy2.yml -i inventory/harjadev --extra-vars "harja_migrate_only=false" --limit $HARJA_ENV
+popd
