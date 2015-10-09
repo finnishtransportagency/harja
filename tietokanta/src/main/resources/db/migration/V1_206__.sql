e0a9d64dced012432841bb0d26b611136d6d3884
mikkoro <mikko.ronkkomaki@solita.fi>
2015-10-09 15:10:40 +0300

    Korjaa migraatio

--- tietokanta/src/main/resources/db/migration/V1_206__.sql
+++ tietokanta/src/main/resources/db/migration/V1_206__.sql
@@ -11,40 +11,40 @@ CREATE OR REPLACE FUNCTION tallenna_tai_paivita_tyokonehavainto(
   urakkaid_ integer,
   tehtavat_ suoritettavatehtava[]
 ) RETURNS VOID AS
-  $$
-  DECLARE
-    organisaatioid integer;
-  BEGIN
-    SELECT id INTO organisaatioid FROM organisaatio WHERE nimi=organisaationimi_ AND ytunnus=ytunnus_;
+$$
+DECLARE
+  organisaatioid integer;
+BEGIN  
+  SELECT id INTO organisaatioid FROM organisaatio WHERE nimi=organisaationimi_ AND ytunnus=ytunnus_;
 
-    LOOP
-      UPDATE tyokonehavainto
-      SET jarjestelma=jarjestelma_,
-        organisaatio=organisaatioid,
-        viestitunniste=viestitunniste_,
-        lahetysaika=lahetysaika_,
-        vastaanotettu=DEFAULT,
-        tyokonetyyppi=tyokonetyyppi_,
-        sijainti=sijainti_,
-        suunta=suunta_,
-        urakkaid=urakkaid_,
-        tehtavat=tehtavat_,
-        edellinensijainti=sijainti
-      WHERE tyokoneid=tyokoneid_;
-      IF FOUND THEN
-        RETURN;
-      END IF;
+  LOOP
+    UPDATE tyokonehavainto
+  	 SET jarjestelma=jarjestelma_,
+	     organisaatio=organisaatioid,
+	     viestitunniste=viestitunniste_,
+	     lahetysaika=lahetysaika_,
+	     vastaanotettu=DEFAULT,
+	     tyokonetyyppi=tyokonetyyppi_,
+	     sijainti=sijainti_,
+	     suunta=suunta_,
+	     urakkaid=urakkaid_,
+	     tehtavat=tehtavat_,
+	     edellinensijainti=sijainti
+  	 WHERE tyokoneid=tyokoneid_;
+    IF FOUND THEN
+       RETURN;
+    END IF;
 
-      BEGIN
-        INSERT INTO tyokonehavainto (jarjestelma, organisaatio, viestitunniste, lahetysaika,
-                                     tyokoneid, tyokonetyyppi, sijainti, urakkaid, tehtavat, suunta)
-        VALUES (jarjestelma_, organisaatioid, viestitunniste_, lahetysaika_, tyokoneid_, tyokonetyyppi_, sijainti_,
-                urakkaid_, tehtavat_, suunta_);
-        RETURN;
-        EXCEPTION WHEN unique_violation THEN
-        -- retry
-      END;
-    END LOOP;
-  END;
-  $$
+    BEGIN
+     INSERT INTO tyokonehavainto (jarjestelma, organisaatio, viestitunniste, lahetysaika,
+     	    	 		  tyokoneid, tyokonetyyppi, sijainti, urakkaid, tehtavat, suunta)
+	    VALUES (jarjestelma_, organisaatioid, viestitunniste_, lahetysaika_, tyokoneid_, tyokonetyyppi_, sijainti_,
+	    	   urakkaid_, tehtavat_, suunta_);
+	RETURN;
+    EXCEPTION WHEN unique_violation THEN
+      -- retry
+    END;
+  END LOOP;
+END;
+$$
 LANGUAGE plpgsql;
