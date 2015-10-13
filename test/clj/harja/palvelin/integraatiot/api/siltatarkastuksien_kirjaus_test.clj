1cf278eaa13c0591bf87d9e03eeb15a295c348e2
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-10-13 15:49:48 +0300

    Paranna siltatarkastus-apin testiä

--- test/clj/harja/palvelin/integraatiot/api/siltatarkastuksien_kirjaus_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/siltatarkastuksien_kirjaus_test.clj
@@ -18,8 +18,13 @@
 
 (use-fixtures :once jarjestelma-fixture)
 
+(defn hae-siltanumerot []
+  (q (str "SELECT siltanro FROM silta;")))
+
 (deftest tallenna-siltatarkastus
   (let [ulkoinen-id 12345
+        siltanumero (ffirst (hae-siltanumerot))
+        tarkastusaika "2014-01-30T12:00:00Z"
         tarkastaja-etunimi "Martti"
         tarkastaja-sukunimi "Ahtisaari"
         vastaus-lisays (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/tarkastus/siltatarkastus"] kayttaja portti
@@ -27,19 +32,23 @@
                                                     slurp
                                                     (.replace "__ID__" (str ulkoinen-id))
                                                     (.replace "__ETUNIMI__" tarkastaja-etunimi)
-                                                    (.replace "__SUKUNIMI__" tarkastaja-sukunimi)))]
+                                                    (.replace "__SUKUNIMI__" tarkastaja-sukunimi)
+                                                    (.replace "__SILTANUMERO__" (str siltanumero))
+                                                    (.replace "__TARKASTUSAIKA__" tarkastusaika)))]
     (is (= 200 (:status vastaus-lisays)))
-    (let [siltatarkastus-kannassa (first (q (str "SELECT id, ulkoinen_id, tarkastaja, tarkastusaika FROM siltatarkastus WHERE ulkoinen_id = '" ulkoinen-id "'")))
+    (let [siltatarkastus-kannassa (first (q (str "SELECT id, ulkoinen_id, tarkastaja, tarkastusaika FROM siltatarkastus WHERE ulkoinen_id = '" ulkoinen-id "';")))
           siltatarkastus-kannassa-id (first siltatarkastus-kannassa)]
     (is (not (nil? siltatarkastus-kannassa)))
     (is (= (nth siltatarkastus-kannassa 1) (str ulkoinen-id)))
     (is (= (nth siltatarkastus-kannassa 2) (str tarkastaja-etunimi " " tarkastaja-sukunimi)))
 
-    (let [kohteet-kannassa (q (str "SELECT kohde, tulos, lisatieto FROM siltatarkastuskohde WHERE siltatarkastus = " siltatarkastus-kannassa-id))]
+    (let [kohteet-kannassa (q (str "SELECT kohde, tulos, lisatieto FROM siltatarkastuskohde WHERE siltatarkastus = " siltatarkastus-kannassa-id ";"))]
     (is (= (count kohteet-kannassa) 24))))))
 
 (deftest yrita-tallentaa-virheellinen-siltatarkastus-ilman-kaikkia-kohteita
-  (let [ulkoinen-id 12345
+  (let [ulkoinen-id 666
+        siltanumero (ffirst (hae-siltanumerot))
+        tarkastusaika "2004-01-30T12:00:00Z"
         tarkastaja-etunimi "Martti"
         tarkastaja-sukunimi "Ahtisaari"
         vastaus-lisays (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/tarkastus/siltatarkastus"] kayttaja portti
@@ -47,9 +56,43 @@
                                                     slurp
                                                     (.replace "__ID__" (str ulkoinen-id))
                                                     (.replace "__ETUNIMI__" tarkastaja-etunimi)
-                                                    (.replace "__SUKUNIMI__" tarkastaja-sukunimi)))]
+                                                    (.replace "__SUKUNIMI__" tarkastaja-sukunimi)
+                                                    (.replace "__SILTANUMERO__" (str siltanumero))
+                                                    (.replace "__TARKASTUSAIKA__" tarkastusaika)))]
     (is (not= 200 (:status vastaus-lisays)))))
 
-#_(deftest paivita-siltatarkastus
-  ; TODO
-  )
+(deftest paivita-siltatarkastus
+  (let [ulkoinen-id 787878
+        siltanumero (first (second (hae-siltanumerot)))
+        tarkastusaika "2016-01-30T12:00:00Z"
+        tarkastaja-etunimi "Simo"
+        tarkastaja-sukunimi "Sillantarkastaja"
+        vastaus-lisays (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/tarkastus/siltatarkastus"] kayttaja portti
+                                                (-> "test/resurssit/api/siltatarkastus.json"
+                                                    slurp
+                                                    (.replace "__ID__" (str ulkoinen-id))
+                                                    (.replace "__ETUNIMI__" tarkastaja-etunimi)
+                                                    (.replace "__SUKUNIMI__" tarkastaja-sukunimi)
+                                                    (.replace "__SILTANUMERO__" (str siltanumero))
+                                                    (.replace "__TARKASTUSAIKA__" tarkastusaika)))]
+    (is (= 200 (:status vastaus-lisays)))
+    (let [siltatarkastus-kannassa (first (q (str "SELECT id, ulkoinen_id, tarkastaja, tarkastusaika FROM siltatarkastus WHERE ulkoinen_id = '" ulkoinen-id "';")))]
+      (is (not (nil? siltatarkastus-kannassa)))
+      (is (= (nth siltatarkastus-kannassa 1) (str ulkoinen-id)))
+      (is (= (nth siltatarkastus-kannassa 2) (str tarkastaja-etunimi " " tarkastaja-sukunimi))))
+
+  ;; Nyt Simo huomaa virheen tarkastuksessa ja yrittää vierittää vastuun Antti Ahtaajalle
+  (let [tarkastaja-etunimi "Antti"
+        tarkastaja-sukunimi "Ahtaaja"
+        vastaus-lisays (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/tarkastus/siltatarkastus"] kayttaja portti
+                                                (-> "test/resurssit/api/siltatarkastus.json"
+                                                    slurp
+                                                    (.replace "__ID__" (str ulkoinen-id))
+                                                    (.replace "__ETUNIMI__" tarkastaja-etunimi)
+                                                    (.replace "__SUKUNIMI__" tarkastaja-sukunimi)
+                                                    (.replace "__SILTANUMERO__" (str siltanumero))
+                                                    (.replace "__TARKASTUSAIKA__" tarkastusaika)))]
+    (is (= 200 (:status vastaus-lisays)))
+    (let [siltatarkastus-kannassa (first (q (str "SELECT id, ulkoinen_id, tarkastaja, tarkastusaika FROM siltatarkastus WHERE ulkoinen_id = '" ulkoinen-id "';")))]
+      (is (not (nil? siltatarkastus-kannassa)))
+      (is (= (nth siltatarkastus-kannassa 2) (str tarkastaja-etunimi " " tarkastaja-sukunimi)))))))
