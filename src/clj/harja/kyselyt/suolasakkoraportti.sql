d961c721cb2bc9b8b55260830973813ce571384f
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-10-15 09:52:31 +0300

    Tee pohjaa suolasakkoraportille

---
+++ src/clj/harja/kyselyt/suolasakkoraportti.sql
@@ -0,0 +1,15 @@
+-- name: hae-tiedot-suolasakkoraportille
+SELECT
+  ss.maara AS sakko_maara,
+  ss.hoitokauden_alkuvuosi AS sakko_hoitokauden_alkuvuosi,
+  ss.maksukuukausi AS sakko_maksukuukausi,
+  ss.indeksi AS sakko_indeksi,
+  ss.urakka,
+  lt.alkupvm AS lampotila_alkupvm,
+  lt.loppupvm AS lampotila_loppupvm,
+  lt.keskilampotila as keskilampotila,
+  lt.pitka_keskilampotila as pitkakeskilampotila
+FROM lampotilat lt
+  LEFT JOIN suolasakko ss ON ss.urakka = lt.urakka
+                             AND ss.hoitokauden_alkuvuosi = (SELECT EXTRACT(YEAR FROM lt.alkupvm))
+WHERE lt.urakka = :urakkah;
--- src/clj/harja/palvelin/raportointi/raportit/suolasakko.clj
+++ src/clj/harja/palvelin/raportointi/raportit/suolasakko.clj
@@ -1,47 +1,84 @@
 (ns harja.palvelin.raportointi.raportit.suolasakko
-  (:require [harja.palvelin.palvelut.toteumat :as toteumat]
-            [harja.kyselyt.konversio :as konv]
+  "Materiaaliraportti"
+  (:require [taoensso.timbre :as log]
             [harja.domain.roolit :as roolit]
-            [harja.kyselyt.yksikkohintaiset-tyot :refer [hae-yksikkohintaiset-tyot-per-paiva]]
-            [harja.kyselyt.toimenpideinstanssit :refer [hae-urakan-toimenpideinstanssi]]
-            [harja.fmt :as fmt]
+            [harja.kyselyt.urakat :as urakat-q]
+            [harja.kyselyt.hallintayksikot :as hallintayksikot-q]
+            [harja.kyselyt.konversio :as konv]
             [harja.pvm :as pvm]
-            [taoensso.timbre :as log]))
+            [yesql.core :refer [defqueries]]))
 
-(defn suorita [db user {:keys [urakka-id aikavali-alkupvm aikavali-loppupvm] :as parametrit}]
-  (let [naytettavat-rivit (hae-yksikkohintaiset-tyot-per-paiva db
-                                                               urakka-id aikavali-alkupvm aikavali-loppupvm
-                                                               (if toimenpide-id true false) toimenpide-id)
-        otsikko (str "Yksikköhintaisten töiden raportti"
-                     (some->> toimenpide-id
-                              (hae-urakan-toimenpideinstanssi db urakka-id)
-                              first :nimi
-                              (str ", "))
-                     ", " (pvm/pvm aikavali-alkupvm) " - " (pvm/pvm aikavali-loppupvm))]
-    [:raportti {:orientaatio :landscape
-                :nimi otsikko}
+(defqueries "harja/kyselyt/suolasakkoraportti.sql")
+
+(defn muodosta-suolasakkoraportti-urakalle [db user {:keys [urakka-id alkupvm loppupvm]}]
+  (log/debug "Haetaan tiedot materiaaliraportille: " urakka-id alkupvm loppupvm)
+  (roolit/vaadi-rooli user "tilaajan kayttaja")
+  (let [toteuma-parametrit [db
+                            urakka-id
+                            (konv/sql-timestamp alkupvm)
+                            (konv/sql-timestamp loppupvm)]
+        materiaalin-tiedot (into [] (apply hae-tiedot-suolasakkoraportille toteuma-parametrit))]
+        materiaalin-tiedot))
+
+; TODO
+#_(defn muodosta-materiaaliraportti-hallintayksikolle [db user {:keys [hallintayksikko-id alkupvm loppupvm]}]
+  (log/debug "Haetaan hallintayksikon toteutuneet materiaalit raporttia varten: " hallintayksikko-id alkupvm loppupvm)
+  (roolit/vaadi-rooli user "tilaajan kayttaja")
+  (let [toteutuneet-materiaalit (into []
+                                      (materiaalit-q/hae-hallintayksikon-toteutuneet-materiaalit-raportille db
+                                                                                                            (konv/sql-timestamp alkupvm)
+                                                                                                            (konv/sql-timestamp loppupvm)
+                                                                                                            hallintayksikko-id))]
+    toteutuneet-materiaalit))
+
+; TODO
+#_(defn muodosta-materiaaliraportti-koko-maalle [db user {:keys [alkupvm loppupvm]}]
+  (log/debug "Haetaan koko maan toteutuneet materiaalit raporttia varten: " alkupvm loppupvm)
+  (roolit/vaadi-rooli user "tilaajan kayttaja")
+  (let [toteutuneet-materiaalit (into []
+                                      (materiaalit-q/hae-koko-maan-toteutuneet-materiaalit-raportille db
+                                                                                                      (konv/sql-timestamp alkupvm)
+                                                                                                      (konv/sql-timestamp loppupvm)))]
+    toteutuneet-materiaalit))
+
+
+
+(defn suorita [db user {:keys [urakka-id hk-alkupvm hk-loppupvm
+                               hallintayksikko-id alkupvm loppupvm] :as parametrit}]
+  (let [[konteksti toteumat]
+        (cond
+          (and urakka-id hk-alkupvm hk-loppupvm)
+          [:urakka (muodosta-suolasakkoraportti-urakalle db user {:urakka-id urakka-id
+                                                                  :alkupvm hk-alkupvm
+                                                                  :loppupvm hk-loppupvm})]
+
+          ; TODO
+          #_(and hallintayksikko-id alkupvm loppupvm)
+          #_[:hallintayksikko (muodosta-materiaaliraportti-hallintayksikolle db user {:hallintayksikko-id hallintayksikko-id
+                                                                                    :alkupvm alkupvm
+                                                                                    :loppupvm loppupvm})]
+          ; TODO
+          #_(and alkupvm loppupvm)
+          #_[:koko-maa (muodosta-materiaaliraportti-koko-maalle db user {:alkupvm alkupvm :loppupvm loppupvm})]
+
+          :default
+          ;; FIXME Pitäisikö tässä heittää jotain, tänne ei pitäisi päästä, jos parametrit ovat oikein?
+          nil)
+        otsikko (str (case konteksti
+                       :urakka (:nimi (first (urakat-q/hae-urakka db urakka-id)))
+                       :hallintayksikko (:nimi (first (hallintayksikot-q/hae-organisaatio db hallintayksikko-id)))
+                       :koko-maa "KOKO MAA")
+                     ", Suolabonus/sakkoraportti "
+                     (pvm/pvm (or hk-alkupvm alkupvm)) " \u2010 " (pvm/pvm (or hk-loppupvm loppupvm)))]
+    [:raportti {:nimi otsikko}
      [:taulukko {:otsikko otsikko
-                 :viimeinen-rivi-yhteenveto? true
-                 :tyhja   (if (empty? naytettavat-rivit) "Ei raportoitavia tehtäviä.")}
-      [{:leveys "10%" :otsikko "Päivämäärä"}
-       {:leveys "25%" :otsikko "Tehtävä"}
-       {:leveys "5%" :otsikko "Yks."}
-       {:leveys "10%" :otsikko "Yksikkö\u00adhinta"}
-       {:leveys "10%" :otsikko "Suunniteltu määrä hoitokaudella"}
-       {:leveys "10%" :otsikko "Toteutunut määrä"}
-       {:leveys "15%" :otsikko "Suunnitellut kustannukset hoitokaudella"}
-       {:leveys "15%" :otsikko "Toteutuneet kustannukset"}]
+                 :viimeinen-rivi-yhteenveto? true}
+      [{:leveys "10%" :otsikko "Urakka"}
+       {:leveys "10%" :otsikko "Pitkän aikavälin kesklämpätila"}
+       {:leveys "10%" :otsikko "Keskiläpötila"}
+       {:leveys "10%" :otsikko "Sopimuksen mukainen suolamäärä"}
+       {:leveys "10%" :otsikko "Käytetty suolamäärä"}
+       {:leveys "10%" :otsikko "Suolaerotus"}
+       {:leveys "10%" :otsikko "Sakko/Bonus"}]]))
 
-      (conj (mapv (juxt (comp pvm/pvm :pvm)
-                        :nimi
-                        :yksikko
-                        (comp fmt/euro-opt :yksikkohinta)
-                        :suunniteltu_maara
-                        :toteutunut_maara
-                        (comp fmt/euro-opt :suunnitellut_kustannukset)
-                        (comp fmt/euro-opt :toteutuneet_kustannukset))
-                  naytettavat-rivit)
-            [nil "Yhteensä" nil nil nil nil
-             (fmt/euro-opt (reduce + (keep :suunnitellut_kustannukset naytettavat-rivit)))
-             (fmt/euro-opt (reduce + (keep :toteutuneet_kustannukset naytettavat-rivit)))])]]))
-
+    
