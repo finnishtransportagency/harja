2bd9248b9d4f48e7d2c5ddb08dc3ba44303cfe98
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-10-15 15:34:57 +0300

    Lisää funktiot suolasakkoraportin muodostamiselle hallintayksikön ja koko maan kontekstissa

--- src/clj/harja/palvelin/raportointi/raportit/suolasakko.clj
+++ src/clj/harja/palvelin/raportointi/raportit/suolasakko.clj
@@ -12,7 +12,7 @@
 (defqueries "harja/kyselyt/suolasakkoraportti.sql")
 
 (defn muodosta-suolasakkoraportti-urakalle [db user {:keys [urakka-id alkupvm loppupvm]}]
-  (log/debug "Haetaan tiedot materiaaliraportille: " urakka-id alkupvm loppupvm)
+  (log/debug "Haetaan tiedot suolasakon raportille urakka-kontekstissa: " urakka-id alkupvm loppupvm)
   (roolit/vaadi-rooli user "tilaajan kayttaja")
   (let [toteuma-parametrit [db
                             urakka-id
@@ -24,31 +24,33 @@
     (log/debug (str "Raporttidata saatu: " (pr-str raportin-tiedot)))
     raportin-tiedot))
 
-; TODO
-#_(defn muodosta-materiaaliraportti-hallintayksikolle [db user {:keys [hallintayksikko-id alkupvm loppupvm]}]
-  (log/debug "Haetaan hallintayksikon toteutuneet materiaalit raporttia varten: " hallintayksikko-id alkupvm loppupvm)
+
+(defn muodosta-suolasakkoraportti-hallintayksikolle [db user {:keys [hallintayksikko-id alkupvm loppupvm]}]
+  (log/debug "Haetaan tiedot suolasakon raportille hallintayksikkö-kontekstissa: " hallintayksikko-id alkupvm loppupvm)
   (roolit/vaadi-rooli user "tilaajan kayttaja")
-  (let [toteutuneet-materiaalit (into []
-                                      (materiaalit-q/hae-hallintayksikon-toteutuneet-materiaalit-raportille db
-                                                                                                            (konv/sql-timestamp alkupvm)
-                                                                                                            (konv/sql-timestamp loppupvm)
-                                                                                                            hallintayksikko-id))]
-    toteutuneet-materiaalit))
+  (let [toteuma-parametrit [db
+                            hallintayksikko-id
+                            (+ (.getYear (konv/sql-timestamp alkupvm)) 1900)
+                            ; FIXME .getYear vanhentunut & vuoden "purkaminen" suoraan SQL:llä ei toiminut :(
+                            #_(konv/sql-timestamp alkupvm) ; FIXME Miksei päivämäärät toimi ;_____;
+                            #_(konv/sql-timestamp loppupvm)]
+        raportin-tiedot (into [] (apply hae-tiedot-hallintayksikon-suolasakkoraportille toteuma-parametrit))]
+    (log/debug (str "Raporttidata saatu: " (pr-str raportin-tiedot)))
+    raportin-tiedot))
 
-; TODO
-#_(defn muodosta-materiaaliraportti-koko-maalle [db user {:keys [alkupvm loppupvm]}]
-  (log/debug "Haetaan koko maan toteutuneet materiaalit raporttia varten: " alkupvm loppupvm)
+(defn muodosta-suolasakkoraportti-koko-maalle [db user {:keys [alkupvm loppupvm]}]
+  (log/debug "Haetaan tiedot suolasakon raportille koko maa -kontekstissa: " alkupvm loppupvm)
   (roolit/vaadi-rooli user "tilaajan kayttaja")
-  (let [toteutuneet-materiaalit (into []
-                                      (materiaalit-q/hae-koko-maan-toteutuneet-materiaalit-raportille db
-                                                                                                      (konv/sql-timestamp alkupvm)
-                                                                                                      (konv/sql-timestamp loppupvm)))]
-    toteutuneet-materiaalit))
+  (let [toteuma-parametrit [db
+                            (+ (.getYear (konv/sql-timestamp alkupvm)) 1900)
+                            ; FIXME .getYear vanhentunut & vuoden "purkaminen" suoraan SQL:llä ei toiminut :(
+                            #_(konv/sql-timestamp alkupvm) ; FIXME Miksei päivämäärät toimi ;_____;
+                            #_(konv/sql-timestamp loppupvm)]
+        raportin-tiedot (into [] (apply hae-tiedot-koko-maan-suolasakkoraportille toteuma-parametrit))]
+    (log/debug (str "Raporttidata saatu: " (pr-str raportin-tiedot)))
+    raportin-tiedot))
 
-
-
-(defn suorita [db user {:keys [urakka-id hk-alkupvm hk-loppupvm
-                               hallintayksikko-id alkupvm loppupvm] :as parametrit}]
+(defn suorita [db user {:keys [urakka-id hk-alkupvm hk-loppupvm hallintayksikko-id] :as parametrit}]
   (let [[konteksti raportin-data]
         (cond
           (and urakka-id hk-alkupvm hk-loppupvm)
@@ -56,14 +58,12 @@
                                                                   :alkupvm hk-alkupvm
                                                                   :loppupvm hk-loppupvm})]
 
-          ; TODO
-          #_(and hallintayksikko-id alkupvm loppupvm)
-          #_[:hallintayksikko (muodosta-materiaaliraportti-hallintayksikolle db user {:hallintayksikko-id hallintayksikko-id
-                                                                                    :alkupvm alkupvm
-                                                                                    :loppupvm loppupvm})]
-          ; TODO
-          #_(and alkupvm loppupvm)
-          #_[:koko-maa (muodosta-materiaaliraportti-koko-maalle db user {:alkupvm alkupvm :loppupvm loppupvm})]
+          (and hallintayksikko-id hk-alkupvm hk-loppupvm)
+          [:hallintayksikko (muodosta-suolasakkoraportti-hallintayksikolle db user {:hallintayksikko-id hallintayksikko-id
+                                                                                    :alkupvm hk-alkupvm
+                                                                                    :loppupvm hk-loppupvm})]
+          (and hk-alkupvm hk-loppupvm)
+          [:koko-maa (muodosta-suolasakkoraportti-koko-maalle db user {:alkupvm hk-alkupvm :loppupvm hk-loppupvm})]
 
           :default
           ;; FIXME Pitäisikö tässä heittää jotain, tänne ei pitäisi päästä, jos parametrit ovat oikein?
@@ -73,7 +73,7 @@
                        :hallintayksikko (:nimi (first (hallintayksikot-q/hae-organisaatio db hallintayksikko-id)))
                        :koko-maa "KOKO MAA")
                      ", Suolabonus/sakkoraportti "
-                     (pvm/pvm (or hk-alkupvm alkupvm)) " \u2010 " (pvm/pvm (or hk-loppupvm loppupvm)))]
+                     (pvm/pvm (or hk-alkupvm hk-alkupvm)) " \u2010 " (pvm/pvm (or hk-loppupvm hk-loppupvm)))]
     [:raportti {:nimi otsikko}
      [:taulukko {:otsikko                    otsikko
                  :viimeinen-rivi-yhteenveto? true}
