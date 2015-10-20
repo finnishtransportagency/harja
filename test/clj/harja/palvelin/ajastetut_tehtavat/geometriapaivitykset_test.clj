7852c84885841ebb0b13316e896ee66e00df8c9f
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-10-20 13:29:46 +0300

    Testaa kansion tyhjennys

--- test/clj/harja/palvelin/ajastetut_tehtavat/geometriapaivitykset_test.clj
+++ test/clj/harja/palvelin/ajastetut_tehtavat/geometriapaivitykset_test.clj
@@ -9,7 +9,9 @@
             [harja.palvelin.komponentit.tietokanta :as tietokanta]
             [harja.testi :refer :all]
             [harja.palvelin.tyokalut.arkisto :as arkisto]
-            [harja.palvelin.komponentit.tietokanta :as tietokanta]))
+            [harja.palvelin.komponentit.tietokanta :as tietokanta]
+            [harja.palvelin.tyokalut.kansio :as kansio]
+            [clojure.java.io :as io]))
 
 
 (defn aja-tieverkon-paivitys []
@@ -52,13 +54,21 @@
           "file:///Users/mikkoro/Desktop/Soratiehoitoluokat-testi/Sorateiden-hoitoluokat.shp")))))
 
 (def +arkistot-polku+ "test/resurssit/arkistot/")
+(def +arkistot-target-polku+ "test/resurssit/arkistot/target/")
 
 (defn testaa-tiedoston-purku [tiedosto-nimi]
   (arkisto/pura-paketti (str +arkistot-polku+ tiedosto-nimi))
+  ; Tarkista, että tiedostot purkautuivat
   (is (true? (.exists (clojure.java.io/file (str +arkistot-polku+ "teksti.txt")))))
   (is (true? (.exists (clojure.java.io/file (str +arkistot-polku+ "kuva.png")))))
+  ; Siirrä puretut tiedostot target-kansioon
+  (io/copy (io/file (str +arkistot-polku+ "teksti.txt")) (io/file (str +arkistot-target-polku+ "teksti.txt")))
+  (io/copy (io/file (str +arkistot-polku+ "kuva.png")) (io/file (str +arkistot-target-polku+ "kuva.png")))
   (clojure.java.io/delete-file (str +arkistot-polku+ "kuva.png"))
-  (clojure.java.io/delete-file (str +arkistot-polku+ "teksti.txt")))
+  (clojure.java.io/delete-file (str +arkistot-polku+ "teksti.txt"))
+  ; Tyhjennä target-kansio
+  (kansio/poista-tiedostot +arkistot-target-polku+)
+  (is (= 0 (count (.listFiles (clojure.java.io/file +arkistot-target-polku+))))))
 
 (deftest testaa-pura-macissa-tehty-zip
   (testaa-tiedoston-purku"test_zip_mac.zip"))
