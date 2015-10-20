ff6e018e14f2179d592ab0f08554073262476fd9
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-10-20 13:35:34 +0300

    Erota arkistotesti omaksi tiedostokseen

--- test/clj/harja/palvelin/ajastetut_tehtavat/geometriapaivitykset_test.clj
+++ test/clj/harja/palvelin/ajastetut_tehtavat/geometriapaivitykset_test.clj
@@ -6,13 +6,11 @@
             [harja.palvelin.integraatiot.paikkatietojarjestelma.tuonnit.tieverkko :as tieverkon-tuonti]
             [harja.palvelin.ajastetut-tehtavat.geometriapaivitykset :as geometriapaivitykset]
             [harja.palvelin.integraatiot.integraatioloki :as integraatioloki]
-            [harja.palvelin.komponentit.tietokanta :as tietokanta]
             [harja.testi :refer :all]
             [harja.palvelin.tyokalut.arkisto :as arkisto]
             [harja.palvelin.komponentit.tietokanta :as tietokanta]
             [harja.palvelin.tyokalut.kansio :as kansio]
             [clojure.java.io :as io]))
-
 
 (defn aja-tieverkon-paivitys []
   "REPL-testiajofunktio"
@@ -52,33 +50,3 @@
         (tieverkon-tuonti/vie-tieverkko-kantaan
           testitietokanta
           "file:///Users/mikkoro/Desktop/Soratiehoitoluokat-testi/Sorateiden-hoitoluokat.shp")))))
-
-(def +arkistot-polku+ "test/resurssit/arkistot/")
-(def +arkistot-target-polku+ "test/resurssit/arkistot/target/")
-
-(defn testaa-tiedoston-purku [tiedosto-nimi]
-  (arkisto/pura-paketti (str +arkistot-polku+ tiedosto-nimi))
-  ; Tarkista, että tiedostot purkautuivat oikein
-  (is (true? (.exists (clojure.java.io/file (str +arkistot-polku+ "teksti.txt")))))
-  (is (= "Terve!" (slurp (str +arkistot-polku+ "teksti.txt"))))
-  (is (true? (.exists (clojure.java.io/file (str +arkistot-polku+ "kuva.png")))))
-  ; Siirrä puretut tiedostot target-kansioon
-  (io/copy (io/file (str +arkistot-polku+ "teksti.txt")) (io/file (str +arkistot-target-polku+ "teksti.txt")))
-  (io/copy (io/file (str +arkistot-polku+ "kuva.png")) (io/file (str +arkistot-target-polku+ "kuva.png")))
-  (clojure.java.io/delete-file (str +arkistot-polku+ "kuva.png"))
-  (clojure.java.io/delete-file (str +arkistot-polku+ "teksti.txt"))
-  ; Tyhjennä target-kansio
-  (kansio/poista-tiedostot +arkistot-target-polku+)
-  (is (= 0 (count (.listFiles (clojure.java.io/file +arkistot-target-polku+))))))
-
-(deftest testaa-pura-macissa-tehty-zip
-  (testaa-tiedoston-purku"test_zip_mac.zip"))
-
-(deftest testaa-pura-macissa-tehty-gzip
-  (testaa-tiedoston-purku"test_gzip_mac.tgz"))
-
-(deftest testaa-pura-zip
-  (testaa-tiedoston-purku "test_zip.zip"))
-
-(deftest testaa-pura-gzip
-  (testaa-tiedoston-purku "test_gzip.tgz"))
