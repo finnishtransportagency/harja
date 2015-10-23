31010958d8254874fb6e0a3bc3d285516201d727
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-10-23 15:38:38 +0300

    Pohjavesialueille talvisuolarajoitus omaan tauluun

--- tietokanta/src/main/resources/db/migration/V1_217__.sql
+++ tietokanta/src/main/resources/db/migration/V1_217__.sql
@@ -6,3 +6,11 @@ ALTER TABLE suolasakko ADD COLUMN talvisuolaraja NUMERIC;
 -- Onko talvisuolan rajoitukset ja sakot lainkaan käytössä
 ALTER TABLE suolasakko ADD COLUMN kaytossa BOOLEAN NOT NULL DEFAULT true;
 
+-- Pohjavesialueiden talvisuolarajat
+CREATE TABLE pohjavesialue_talvisuola (
+  pohjavesialue INTEGER REFERENCES pohjavesialue (id),
+  urakka INTEGER REFERENCES urakka (id),
+  hoitokauden_alkuvuosi smallint, 
+  talvisuolaraja NUMERIC
+);
+
