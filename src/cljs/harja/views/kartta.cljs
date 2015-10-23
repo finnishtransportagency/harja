f0162ff0ca6ec45a0c1466cc505a49df18edb8e4
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-10-23 16:09:12 +0300

    Refactoroi karttaa

--- src/cljs/harja/views/kartta.cljs
+++ src/cljs/harja/views/kartta.cljs
@@ -303,7 +303,8 @@
                (if selitys
                  ^{:key (:tyyppi selitys)}
                  [:tr
-                  [:td.kartan-ikonien-selitykset-ikoni-sarake [:img.kartan-ikonien-selitykset-ikoni {:src (str openlayers/+karttaikonipolku+ (get-in geo [:alue :img]))}]]
+                  [:td.kartan-ikonien-selitykset-ikoni-sarake
+                   [:img.kartan-ikonien-selitykset-ikoni {:src (str openlayers/+karttaikonipolku+ (get-in geo [:alue :img]))}]]
                   [:td.kartan-ikonien-selitykset-selitys-sarake (:selitys selitys)]]
                  (log "Geometrialle tyypillä " (pr-str (:tyyppi-kartalla geo)) " ei löydy selitystä"))))]
           [:div.kartan-ikonien-selitykset-sulje.klikattava {:on-click (fn [event]
