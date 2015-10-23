51855cbab40db6776a1f74ef76afbc11c23d664d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-10-23 13:47:01 +0300

    Näytä lämpötilat ilman muokkausmahdollisuutta
    Vain jvh voi muokata lämpötiloja hallinnan kautta, urakassa vain
    näytetään lämpötilat.

--- src/cljs/harja/views/urakka/suunnittelu/suola.cljs
+++ src/cljs/harja/views/urakka/suunnittelu/suola.cljs
@@ -12,15 +12,17 @@
             [harja.tiedot.istunto :as oikeudet]
             [harja.ui.yleiset :refer [ajax-loader] :as yleiset]
             [harja.ui.napit :refer [palvelinkutsu-nappi]]
-            [harja.ui.lomake :refer [lomake]]
+            [harja.ui.lomake :as lomake :refer [lomake]]
             [harja.ui.ikonit :as ikonit]
+            [harja.ui.grid :as grid]
             [harja.asiakas.kommunikaatio :as k]
             [harja.domain.roolit :as roolit]
             [harja.ui.napit :as napit]
             [harja.ui.viesti :as viesti]
             [harja.tiedot.indeksit :as i]
             [harja.tiedot.navigaatio :as nav]
-            [harja.views.kartta :as kartta])
+            [harja.views.kartta :as kartta]
+            [harja.fmt :as fmt])
   (:require-macros [cljs.core.async.macros :refer [go]]
                    [reagent.ratom :refer [reaction]]
                    [harja.atom :refer [reaction<!]]))
@@ -52,8 +54,7 @@
                                                (first (filter #(or (= (:hoitokauden_alkuvuosi %) (pvm/vuosi (first @u/valittu-hoitokausi)))
                                                                    (= (:lt_alkupvm %) (first @u/valittu-hoitokausi)))
                                                               @suolasakot-ja-lampotilat))))
-        lampotilaerotus (reaction (- (:keskilampotila @valitun-hoitokauden-tiedot)
-                                     (:pitkakeskilampotila @valitun-hoitokauden-tiedot)))]
+        lampotilaerotus (reaction )]
 
     (fn []
       (let [hoitokausi @u/valittu-hoitokausi]
@@ -79,7 +80,7 @@
                                       :kun-onnistuu #(do
                                                       (viesti/nayta! "Tallentaminen onnistui" :success 1500)
                                                       (reset! suolasakot-ja-lampotilat %))}]]])])}
-          [{:otsikko "Suolasakko" :pakollinen? true :nimi :maara :tyyppi :positiivinen-numero :leveys-col 2 :yksikko "€ / 5% rajan ylittävä tonni"}
+          [{:otsikko "Suolasakko" :pakollinen? true :nimi :maara :tyyppi :positiivinen-numero :leveys-col 2 :yksikko "€ / ylittävä tonni"}
            {:otsikko       "Maksukuukausi" :nimi :maksukuukausi :tyyppi :valinta :leveys-col 2
             :valinta-arvo  first
             :valinta-nayta #(if (nil? %) yleiset/+valitse-kuukausi+ (second %))
@@ -91,40 +92,24 @@
 
             :leveys-col    2}
 
-           {:otsikko "Hoitokauden sydäntalven keskilämpötila" :nimi :keskilampotila :tyyppi :numero :leveys-col 1
-            :validoi [[:lampotila]] :yksikko "\u2103"}
-           {:otsikko "Pitkän aikavälin sydäntalven keskilämpötila" :nimi :pitkakeskilampotila :tyyppi :numero :leveys-col 1
-            :validoi [[:lampotila]] :yksikko "\u2103"}
-           {:otsikko     "Lämpötilat ilmatieteenlaitokselta" :nimi :haku :tyyppi :komponentti
-            :komponentti (when (roolit/rooli-urakassa? roolit/urakanvalvoja
-                                                       (:id urakka))
-                           [napit/palvelinkutsu-nappi "Hae"
-                            #(lampotilat/hae-lampotilat-ilmatieteenlaitokselta (:id urakka) (pvm/vuosi (first hoitokausi)))
-                            {:ikoni              (ikonit/download)
-                             :luokka             "nappi-ensisijainen nappi-hae-itl"
-                             :virheviesti        (str "Lämpötilan haku hoitokaudelle "
-                                                      (pvm/vuosi (first hoitokausi)) " -  " (pvm/vuosi (second hoitokausi))
-                                                      " epäonnistui. Joko tietoja ei vielä ole, tai palvelussa on tekninen ongelma. Yritä myöhemmin uudelleen.")
-                             :virheen-esitystapa :flash
-                             :kun-onnistuu       (fn [{:keys [keskilampotila ilmastollinen-keskiarvo]}]
-                                                   (if (and keskilampotila ilmastollinen-keskiarvo)
-                                                     (swap! valitun-hoitokauden-tiedot
-                                                            (fn [tiedot]
-                                                              (assoc tiedot
-                                                                :keskilampotila keskilampotila
-                                                                :pitkakeskilampotila ilmastollinen-keskiarvo
-                                                                :muokattu true)))
-                                                     (viesti/nayta! (str "Talvikauden "
-                                                                         (pvm/vuosi (first hoitokausi)) " \u2014 " (pvm/vuosi (second hoitokausi))
-                                                                         " lämpötiloja ei löytynyt.") :warning)))}])}
+           {:otsikko "Sydäntalven keskilämpötila" :leveys-col 4
+            :nimi :lampotilat
+            :komponentti [grid/grid {}
+                          [{:otsikko "Tämä talvikausi" :nimi :keskilampotila :fmt #(if % (fmt/asteina %) "-")
+                            :tasaa :oikea}
+                           {:otsikko "Pitkä aikaväli" :nimi :pitkakeskilampotila :fmt #(if % (fmt/asteina %) "-")
+                            :tasaa :oikea}
+                           {:otsikko "Erotus" :nimi :erotus :fmt #(if % (fmt/asteina %) "-")
+                            :tasaa :oikea}]
 
-           {:otsikko     "Lämpötilojen erotus" :nimi :lampotilaerotus
-            :hae         #(when @lampotilaerotus
-                           (.toFixed @lampotilaerotus 1))
-            :tyyppi      :numero :leveys-col 1
-            :muokattava? (constantly false) :yksikko "\u2103"
-            :vihje       (when (> @lampotilaerotus 2)
-                           "Yli 2 astetta tavanomaista lämpimämpi sydäntalvi nostaa sallittua suolankäytön määrää.")}]
+                          [(let [{:keys [keskilampotila pitkakeskilampotila] :as hk}
+                                 @valitun-hoitokauden-tiedot]
+                             (log "HK: " (pr-str hk))
+                             (assoc hk
+                                    :erotus (and keskilampotila pitkakeskilampotila
+                                                 (.toFixed (- keskilampotila pitkakeskilampotila)))))]]}
+                 
+           ]
           @valitun-hoitokauden-tiedot]
          ]))))
 
