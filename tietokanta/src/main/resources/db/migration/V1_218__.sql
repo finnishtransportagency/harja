3d5f76ce6375c1517aa9cd5c0fe057632ba2f4fe
mikkoro <mikko.ronkkomaki@solita.fi>
2015-10-26 09:42:21 +0200

    Poista id lukkotaulusta

--- tietokanta/src/main/resources/db/migration/V1_218__.sql
+++ tietokanta/src/main/resources/db/migration/V1_218__.sql
@@ -1,29 +1,27 @@
 --- Lukkotaulut
 CREATE TABLE lukko (
-  id       SERIAL PRIMARY KEY,
-  tunniste VARCHAR(20),
+  tunniste VARCHAR(30) PRIMARY KEY,
   lukko    CHAR(36),
   lukittu  TIMESTAMP
 );
-ALTER TABLE lukko ADD CONSTRAINT uniikki_tunniste UNIQUE (tunniste);
 
 -- Lukitseminen
-CREATE OR REPLACE FUNCTION aseta_lukko(tarkistettava_tunniste VARCHAR(20), uusi_lukko CHAR(36), aikaraja integer)
+CREATE OR REPLACE FUNCTION aseta_lukko(tarkistettava_tunniste VARCHAR(30), uusi_lukko CHAR(36), aikaraja INTEGER)
   RETURNS BOOLEAN LANGUAGE plpgsql AS $$
 DECLARE
-  rivi_id        INTEGER;
-  asetettu_lukko CHAR(36);
-  lukko_asetettu TIMESTAMP;
+  loytynyt_tunniste VARCHAR(30);
+  asetettu_lukko    CHAR(36);
+  lukko_asetettu    TIMESTAMP;
 BEGIN
   SELECT
-    id,
+    tunniste,
     lukko,
     lukittu
-  INTO rivi_id, asetettu_lukko, lukko_asetettu
+  INTO loytynyt_tunniste, asetettu_lukko, lukko_asetettu
   FROM lukko
   WHERE tunniste = tarkistettava_tunniste;
 
-  IF rivi_id IS NULL
+  IF loytynyt_tunniste IS NULL
   THEN
     INSERT INTO lukko (tunniste, lukko, lukittu) VALUES (tarkistettava_tunniste, uusi_lukko, now());
     RETURN TRUE;
@@ -44,17 +42,17 @@ END
 $$;
 
 -- Lukon avaus
-CREATE OR REPLACE FUNCTION avaa_lukko(tarkistettava_tunniste VARCHAR(20))
+CREATE OR REPLACE FUNCTION avaa_lukko(tarkistettava_tunniste VARCHAR(30))
   RETURNS BOOLEAN LANGUAGE plpgsql AS $$
 DECLARE
-  rivi_id INTEGER;
+  loytynyt_tunniste VARCHAR(30);
 BEGIN
-  SELECT id
-  INTO rivi_id
+  SELECT tunniste
+  INTO loytynyt_tunniste
   FROM lukko
   WHERE tunniste = tarkistettava_tunniste;
 
-  IF rivi_id IS NULL
+  IF loytynyt_tunniste IS NULL
   THEN
     RETURN FALSE;
   ELSE
@@ -66,4 +64,3 @@ BEGIN
   RETURN FALSE;
 END
 $$;
-
