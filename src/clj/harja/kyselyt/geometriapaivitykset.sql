3032a31cb10ff1d2cbaacf6005d2eb8779814c71
mikkoro <mikko.ronkkomaki@solita.fi>
2015-10-26 12:27:06 +0200

    Estä paikalliset päivitykset, jos tiedosto ei ole muuttunut

--- src/clj/harja/kyselyt/geometriapaivitykset.sql
+++ src/clj/harja/kyselyt/geometriapaivitykset.sql
@@ -8,14 +8,3 @@ WHERE nimi = :nimi;
 UPDATE geometriapaivitys
 SET viimeisin_paivitys = :viimeisin_paivitys
 WHERE nimi = :nimi;
-
--- name: lukitse-paivitys!
-UPDATE geometriapaivitys
-SET lukko = :lukko, lukittu = current_timestamp
-WHERE nimi = :nimi AND (lukko IS NULL OR
-                        (EXTRACT(EPOCH FROM (current_timestamp - lukittu)) > 300));
-
--- name: avaa-paivityksen-lukko!
-UPDATE geometriapaivitys
-SET lukko = NULL, lukittu = NULL
-WHERE nimi = :nimi;
