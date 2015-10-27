8614ee719286b5bfdb9977a3edaa19cdaf3d7195
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2015-10-27 14:59:48 +0200

    Näytä turvallisuuspoikkeama ikonina kartalla

--- src/cljs/harja/tiedot/urakka/turvallisuus/turvallisuuspoikkeamat.cljs
+++ src/cljs/harja/tiedot/urakka/turvallisuus/turvallisuuspoikkeamat.cljs
@@ -6,7 +6,8 @@
             [harja.tiedot.urakka :as urakka]
             [harja.tiedot.navigaatio :as nav]
             [harja.asiakas.tapahtumat :as tapahtumat]
-            [harja.geo :as geo])
+            [harja.geo :as geo]
+            [harja.ui.kartta.esitettavat-asiat :refer [kartalla-esitettavaan-muotoon]])
   (:require-macros [harja.atom :refer [reaction<!]]
                    [reagent.ratom :refer [reaction]]
                    [cljs.core.async.macros :refer [go]]))
@@ -22,10 +23,10 @@
                                         :loppu     loppu}))
 
 (defn hae-turvallisuuspoikkeama [urakka-id turvallisuuspoikkeama-id]
-  (k/post! :hae-turvallisuuspoikkeama {:urakka-id urakka-id
+  (k/post! :hae-turvallisuuspoikkeama {:urakka-id                urakka-id
                                        :turvallisuuspoikkeama-id turvallisuuspoikkeama-id}))
 
-  
+
 (defonce haetut-turvallisuuspoikkeamat (reaction<! [urakka-id (:id @nav/valittu-urakka)
                                                     hoitokausi @urakka/valittu-hoitokausi
                                                     nakymassa? @nakymassa?]
@@ -34,46 +35,26 @@
 
 (def karttataso-turvallisuuspoikkeamat (atom false))
 
-(def turvallisuuspoikkeama-kartalla-xf
-  #(assoc %
-    :tyyppi-kartalla :turvallisuuspoikkeama
-    :type :turvallisuuspoikkeama
-    :alue {:type        :icon
-           :coordinates (geo/ikonin-sijainti (:sijainti %))
-           :anchor [0.5 1]
-           :direction 0
-           :img (if (= (:id %) (:id @valittu-turvallisuuspoikkeama))
-                  "images/turvallisuuspoikkeama_korostettu.png"
-                  "images/turvallisuuspoikkeama.png")}))
-
 (defonce turvallisuuspoikkeamat-kartalla
-         (reaction @valittu-turvallisuuspoikkeama
-                   (when @karttataso-turvallisuuspoikkeamat
-                     (into []
-                           (comp
-                            (filter :sijainti)
-                            (map turvallisuuspoikkeama-kartalla-xf))
-
-                           @haetut-turvallisuuspoikkeamat))))
-
-(defonce turvallisuuspoikkeamaa-klikattu
-         (tapahtumat/kuuntele! :turvallisuuspoikkeama-klikattu
-                               (fn [tp]
-                                 (reset! valittu-turvallisuuspoikkeama (dissoc tp :type :alue)))))
-
+         (reaction (when @karttataso-turvallisuuspoikkeamat
+                     (kartalla-esitettavaan-muotoon
+                       (map
+                         #(assoc % :tyyppi-kartalla :turvallisuuspoikkeama)
+                         @haetut-turvallisuuspoikkeamat)
+                       @valittu-turvallisuuspoikkeama))))
 
 (defn kasaa-tallennuksen-parametrit
   [tp]
-  {:tp                 (assoc
-                         (dissoc tp :liitteet :kommentit :korjaavattoimenpiteet :uusi-kommentti)
-                         :urakka (:id @nav/valittu-urakka))
+  {:tp                    (assoc
+                            (dissoc tp :liitteet :kommentit :korjaavattoimenpiteet :uusi-kommentti)
+                            :urakka (:id @nav/valittu-urakka))
    :korjaavattoimenpiteet (:korjaavattoimenpiteet tp)
    ;; Lomakkeessa voidaan lisätä vain yksi kommentti kerrallaan, joka menee uusi-kommentti avaimeen
    ;; Täten tallennukseen ei tarvita :liitteitä eikä :kommentteja
    ;:liitteet           (:liitteet tp)
    ;:kommentit          (:kommentit tp)
-   :uusi-kommentti     (:uusi-kommentti tp)
-   :hoitokausi         @urakka/valittu-hoitokausi})
+   :uusi-kommentti        (:uusi-kommentti tp)
+   :hoitokausi            @urakka/valittu-hoitokausi})
 
 (defn tallenna-turvallisuuspoikkeama
   [tp]
