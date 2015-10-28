7024aca07e3d58f18a24a7fc04798880f7567684
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-10-28 12:06:10 +0200

    Lisää pohja kokonaishintaisen työn näyttämiselle kartalla

--- src/cljs/harja/tiedot/urakka/toteumat/kokonaishintaiset_tyot.cljs
+++ src/cljs/harja/tiedot/urakka/toteumat/kokonaishintaiset_tyot.cljs
@@ -4,7 +4,8 @@
             [harja.loki :refer [log tarkkaile!]]
             [harja.tiedot.urakka :as urakka]
             [harja.tiedot.navigaatio :as nav]
-            [harja.asiakas.kommunikaatio :as k])
+            [harja.asiakas.kommunikaatio :as k]
+            [harja.pvm :as pvm])
   (:require-macros [harja.atom :refer [reaction<!]]
                    [reagent.ratom :refer [reaction]]
                    [cljs.core.async.macros :refer [go]]))
@@ -33,3 +34,23 @@
             nakymassa? @nakymassa?]
            (when nakymassa?
              (hae-toteumat urakka-id sopimus-id (or kuukausi hoitokausi) toimenpide tehtava))))
+
+
+(def karttataso-kokonaishintainen-toteuma (atom false))
+
+(def kokonaishintainen-toteuma-kartalla-xf
+  (map #(do
+         (assoc %
+           :type :kokonaishintainen-toteuma
+           :alue {:type   :arrow-line
+                  :points (mapv (comp :coordinates :sijainti)
+                                (sort-by
+                                  :aika
+                                  pvm/ennen?
+                                  (:reittipisteet %)))}))))
+
+(defonce kokonaishintainen-toteuma-kartalla
+         (reaction
+           @haetut-toteumat
+           (when @karttataso-kokonaishintainen-toteuma
+             (into [] kokonaishintainen-toteuma-kartalla-xf haetut-toteumat))))
