bf6bc72552817ea4f2c75772b29142f5d80fb1a7
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-11-06 13:50:10 +0200

    Lisää eri raportit työmaakokousraportiin.

--- tietokanta/src/main/resources/db/migration/V1_230__.sql
+++ tietokanta/src/main/resources/db/migration/V1_230__.sql
@@ -6,7 +6,8 @@ VALUES ('tyomaakokous', 'Työmaakokousraportti',
 	ARRAY[('Hoitokausi', 'hoitokausi', true, 'urakka'::raporttikonteksti)::raporttiparametri,
 	      ('Kuukausi', 'hoitokauden-kuukausi', true, 'urakka'::raporttikonteksti)::raporttiparametri,
 	      ('Laskutusyhteenveto', 'checkbox', true, 'urakka'::raporttikonteksti)::raporttiparametri,
-	      ('Laskutusyhteenveto', 'checkbox', true, 'urakka'::raporttikonteksti)::raporttiparametri,
-	      ('Laskutusyhteenveto', 'checkbox', true, 'urakka'::raporttikonteksti)::raporttiparametri,
-	      ('Laskutusyhteenveto', 'checkbox', true, 'urakka'::raporttikonteksti)::raporttiparametri],
+	      ('Yksikköhintaisten töiden raportti', 'checkbox', true, 'urakka'::raporttikonteksti)::raporttiparametri,
+	      ('Ympäristöraportti', 'checkbox', true, 'urakka'::raporttikonteksti)::raporttiparametri
+	      -- lisää raportteja: lisätyöt ja äkilliset hoitotyöt, muistutukset ja sanktiot, turvallisuusraportti
+	      ],
         '#''harja.palvelin.raportointi.raportit.tyomaakokous/suorita');
