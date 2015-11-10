b3b7601dfb5cbc6181d89685ae246db12b6bb264
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-11-10 11:45:29 +0200

    Poista turhaa koodia hoitoluokka-kyselystä

--- src/clj/harja/kyselyt/hoitoluokat.clj
+++ src/clj/harja/kyselyt/hoitoluokat.clj
@@ -4,22 +4,3 @@
             [harja.kyselyt.konversio :as konv]))
 
 (defqueries "harja/kyselyt/hoitoluokat.sql")
-
-(defn onko-olemassa-ulkoisella-idlla? [db ulkoinen-id luoja]
-  (:exists (first (onko-olemassa-ulkoisella-idlla db ulkoinen-id luoja))))
-
-(defn luo-tai-paivita-havainto
-  "Luo uuden havainnon tai päivittää olemassaolevan havainnon perustiedot. Palauttaa havainnon id:n."
-  [db user {:keys [id kohde tekija urakka aika selvitys-pyydetty kuvaus] :as havainto}]
-  (if id
-    (do (paivita-havainnon-perustiedot<! db
-                                        (konv/sql-timestamp aika) (when tekija (name tekija)) kohde
-                                        (if selvitys-pyydetty true false)
-                                        (:id user)
-                                        kuvaus
-                                        id)
-        id)
-
-    (:id (luo-havainto<! db urakka (konv/sql-timestamp aika) (when tekija (name tekija)) kohde
-                         (if selvitys-pyydetty true false) (:id user) kuvaus
-                         nil nil nil nil nil nil nil nil))))
