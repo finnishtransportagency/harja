488f16b53aa47c0b47ea931838163f374ec3fc6a
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-11-12 09:26:25 +0200

    Lisää uudelle valaistusurakalle uusi suunniteltu yksikköhintainen työ

---
+++ tietokanta/src/main/resources/db/migration/V1_234__.sql
@@ -0,0 +1,3 @@
+-- Lisää valaistukseen liittyviä toimenpiteitä
+
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: SpNa 50-100W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
