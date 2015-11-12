3e85b027a5d54e94cdca87fa92158ebbf418e28e
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-11-12 14:08:29 +0200

    Poista turha funktio

--- src/clj/harja/kyselyt/urakat.sql
+++ src/clj/harja/kyselyt/urakat.sql
@@ -334,7 +334,3 @@ SELECT id, nimi
  WHERE hallintayksikko = :hal
    AND (alkupvm IS NULL OR alkupvm <= current_date)
    AND (loppupvm IS NULL OR loppupvm >= current_date);
-
--- name: onko-urakka-hoidon-urakka
--- Tarkistaa onko Samposta tuodun urakan tyyppi hoito
-SELECT EXISTS(SELECT * FROM urakka WHERE sampoid = :sampoid AND tyyppi = 'hoito');
