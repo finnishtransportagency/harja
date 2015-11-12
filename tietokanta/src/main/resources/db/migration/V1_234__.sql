5abb2e4f69b631100c0e4787f1ca367e0e2c6c34
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-11-12 09:35:10 +0200

    Lisää yksikköhintaisten töiden suunnitelmia uudelle valaistusurakalle

--- tietokanta/src/main/resources/db/migration/V1_234__.sql
+++ tietokanta/src/main/resources/db/migration/V1_234__.sql
@@ -1,3 +1,8 @@
 -- Lisää valaistukseen liittyviä toimenpiteitä
 
-SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: SpNa 50-100W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: SpNa 50 - 100 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: SpNa 150 - 400 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: PpNa 35 - 180 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: Monimetalli 35 - 150 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: Monimetalli 250 - 400 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: Loistelamppu',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
