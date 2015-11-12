b9a40ba3c7a96e94b5bd17544dc003f4ad8860b0
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-11-12 09:53:01 +0200

    Lisää uuteen valaistusurakkaan lisää suunniteltuja yks. hint. töitä

--- tietokanta/src/main/resources/db/migration/V1_234__.sql
+++ tietokanta/src/main/resources/db/migration/V1_234__.sql
@@ -6,3 +6,38 @@ SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: PpNa 35 - 180 
 SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: Monimetalli 35 - 150 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
 SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: Monimetalli 250 - 400 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
 SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen ryhmävaihto: Loistelamppu',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen yksittäisvaihto: Hg 50 - 400 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen yksittäisvaihto: SpNa 50 - 100 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen yksittäisvaihto: SpNa 150 - 400 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen yksittäisvaihto: Hg 50 - 400 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen yksittäisvaihto: Sytytin',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen yksittäisvaihto: PpNa 35  - 180 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen yksittäisvaihto: Monimetalli 35 - 400 W',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen yksittäisvaihto: Loistelamppu',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Lamppujen yksittäisvaihto: LED-yksikkö VP 2221/2223 valaisimeen',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Jalustan vaihto SJ 4',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Teräskartiopylväs HE3 h=10m V=2,5m',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Myötäävä puupylväs h=10m',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Puupylväsvarsi V= 1.0 - 2,5m',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Valaisin SpNa 50 - 70 W, lamppuineen',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Valaisin SpNa 100 - 250 W, lamppuineen',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Valaisin SpNa 100 - 250 W, 2-tehokuristin ja tehonvaihtorele, lamppuineen',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Valaisin LED, h=6 m, K4',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Valaisin LED, h=10 m, AL4a',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Valaisin LED VP 2221/2223 M1 - M3',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Maajakokeskus',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden vaihto: Haruksen uusiminen',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden korjaus: Puupylvään oikaisu',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden korjaus: Metallipylvään oikaisu alle 13 m',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden korjaus: Metallipylvään oikaisu yli 13 m',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden korjaus: Valaisinvarsien suuntaus alle 13 m',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Valaistuslaitteiden korjaus: Valaisinvarsien suuntaus yli 13 m',NULL,4,'kpl',NULL,false,false,'Valaistuksen korjaus','20172',3);
+
+SELECT lisaa_tai_paivita_toimenpidekoodi('Asentaja',NULL,4,'h',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Tr- kaivuri',NULL,4,'h',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Törmäysvaimennin',NULL,4,'h',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Kuorma-auto nosturilla',NULL,4,'h',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Nostolava alle 13 m',NULL,4,'h',NULL,false,false,'Valaistuksen korjaus','20172',3);
+SELECT lisaa_tai_paivita_toimenpidekoodi('Nostolava yli 13 m',NULL,4,'h',NULL,false,false,'Valaistuksen korjaus','20172',3);
