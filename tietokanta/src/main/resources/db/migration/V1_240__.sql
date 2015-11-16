ac0279736af30f808a54fcc73103d051691d10fa
mikkoro <mikko.ronkkomaki@solita.fi>
2015-11-16 08:40:26 +0200

    Korjaa migraatio

--- tietokanta/src/main/resources/db/migration/V1_240__.sql
+++ tietokanta/src/main/resources/db/migration/V1_240__.sql
@@ -1,3 +1,3 @@
 ALTER TABLE integraatio ADD CONSTRAINT uniikki_jarjestelman_integraatio UNIQUE (jarjestelma, nimi);
-INSERT INTO integraatio (jarjestelma, nimi) VALUES (''tloik'', ''toimenpiteen-lahetys'');
-INSERT INTO integraatio (jarjestelma, nimi) VALUES (''api'', ''hae-paivystajatiedot'');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('tloik', 'toimenpiteen-lahetys');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'hae-paivystajatiedot');
