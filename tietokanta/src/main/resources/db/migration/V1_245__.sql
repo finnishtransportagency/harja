501550c8f809c2622eb9c4bf372c1eedc1709e79
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-11-25 09:27:29 +0200

    Lisää migraatiossa uudet päivystäjähakupalvelut kantaan

---
+++ tietokanta/src/main/resources/db/migration/V1_245__.sql
@@ -0,0 +1,4 @@
+-- Lisää päivystäjän haun integraatiot
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'hae-paivystajatiedot-urakka-idlla');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'hae-paivystajatiedot-tyypilla-ja-sijainnilla');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'hae-paivystajatiedot-puhelinnumerolla');
