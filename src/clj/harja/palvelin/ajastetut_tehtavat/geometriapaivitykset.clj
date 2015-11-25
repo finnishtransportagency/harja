841d60e212ffd741f1a03a2986882ded5ef6cdd6
Marko Lauronen <Marko.Lauronen@solita.fi>
2015-11-25 11:03:30 +0200

    lisää debug-tulosteita

--- src/clj/harja/palvelin/ajastetut_tehtavat/geometriapaivitykset.clj
+++ src/clj/harja/palvelin/ajastetut_tehtavat/geometriapaivitykset.clj
@@ -32,6 +32,7 @@
     (let [polku (if (not tiedostourl) nil (.substring (.getSchemeSpecificPart (URI. tiedostourl)) 2))
           tiedosto (if (not polku) nil (io/file polku))
           tiedoston-muutospvm (if (not tiedosto) nil (coerce/to-sql-time (Timestamp. (.lastModified tiedosto))))]
+      (log/debug "Tarvitaanko paikallinen paivitys: " polku tiedosto tiedoston-muutospvm)
       (if (and
             (not (nil? tiedosto))
             (.exists tiedosto)
@@ -39,7 +40,9 @@
         (do
           (log/debug (format "Tarvitaan ajaa paikallinen geometriapäivitys: %s." paivitystunnus))
           true)
-        false))
+        (do
+          (log/debug "ei tarvita paikallista päivitystä")
+          false)))
     (catch Exception e
       (log/warn e (format "Tarkistettaessa paikallista ajoa geometriapäivitykselle: %s tapahtui poikkeus." paivitystunnus))
       false)))
@@ -68,6 +71,7 @@
           db (:db this)]
       (log/debug "Paikallinen päivitystehtävä: " paivitystunnus alk-osoite-avain alk-tuontikohde-avain shapefile-avain paivitys)
       (when (and (not alk-osoite) (not alk-tuontikohde))
+        (log/debug "Startataan paikallinen paivitystehtava, tiedosto=" shapefile)
         (chime-at
           (periodic-seq (tee-alkuajastus) (-> tuontivali time/minutes))
           (fn [_]
