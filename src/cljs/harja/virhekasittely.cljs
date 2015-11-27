70c68e29902510a4484059c4715df0a5e717e777
Marko Lauronen <Marko.Lauronen@solita.fi>
2015-11-27 11:50:07 +0200

    muuta komponentin kaatumisviesti ystävällisemmäksi

--- src/cljs/harja/virhekasittely.cljs
+++ src/cljs/harja/virhekasittely.cljs
@@ -5,7 +5,7 @@
   (let [auki (atom false)]
     (fn [e]
       [:div.crash-component {:on-click #(swap! auki not)}
-       [:span "Component render crashed"]
+       [:span "Ups... noloa. Pieniä teknisiä ongelmia, lataathan sivun uudelleen?"]
        [:div.crash-details {:class (if @auki "details-open" "")}
         (if (instance? js/Error e)
           (.-stack e)
