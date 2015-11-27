278a76fbe89eb5c61657910fc254cf8784b6a138
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-11-27 07:44:46 +0200

    Luo pohjaa urakoiden geometrian päivittämiselle

---
+++ src/clj/harja/palvelin/integraatiot/paikkatietojarjestelma/tuonnit/urakat.clj
@@ -0,0 +1,49 @@
+(ns harja.palvelin.integraatiot.paikkatietojarjestelma.tuonnit.urakat
+  (:require [taoensso.timbre :as log]
+            [clojure.java.jdbc :as jdbc]
+            [clj-time.periodic :refer [periodic-seq]]
+            [chime :refer [chime-at]]
+            [harja.kyselyt.urakat :as u]
+            [harja.palvelin.integraatiot.paikkatietojarjestelma.tuonnit.shapefile :as shapefile]))
+
+; FIXME Kesken...
+
+(defn paivita-silta [db silta]
+  (s/paivita-silta-siltanumerolla! db
+                       (:siltatyy silta)
+                       (int (:nro silta))
+                       (:nimi silta)
+                       (.toString (:the_geom silta))
+                       (:tie silta)
+                       (:aosa silta)
+                       (:aet silta)))
+
+(defn luo-silta [db silta]
+  (s/vie-siltatauluun! db
+                       (:siltatyy silta)
+                       (int (:nro silta))
+                       (:nimi silta)
+                       (.toString (:the_geom silta))
+                       (:tie silta)
+                       (:aosa silta)
+                       (:aet silta)))
+
+(defn luo-tai-paivita-urakka [db urakka]
+  (if-let [urakka-kannassa (first (s/hae-silta-numerolla db (:nro urakka)))]
+    (paivita-silta db urakka)
+    (luo-silta db urakka)))
+
+(defn vie-urakka-entry [db urakka]
+  (if (:the_geom urakka)
+    (luo-tai-paivita-urakka db urakka)
+    (log/warn "Urakka ei voida tuoda ilman geometriaa. Virheviesti: " (:loc_error urakka))))
+
+(defn vie-urakat-kantaan [db shapefile]
+  (if shapefile
+    (do
+      (log/debug (str "Tuodaan urakat kantaan tiedostosta " shapefile))
+      (jdbc/with-db-transaction [transaktio db]
+                                (doseq [urakka (shapefile/tuo shapefile)]
+                                  (vie-urakka-entry transaktio urakka)))
+      (log/debug "Urakoiden tuonti kantaan valmis."))
+    (log/debug "Urakoiden tiedostoa ei löydy konfiguraatiosta. Tuontia ei suoriteta.")))
