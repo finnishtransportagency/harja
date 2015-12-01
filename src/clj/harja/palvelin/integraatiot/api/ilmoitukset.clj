3cb0ef8f4e8cc41b16ca52be997781e9ed8d6913
mikkoro <mikko.ronkkomaki@solita.fi>
2015-12-01 12:43:25 +0200

    Korjaa ilmoitusten haku

--- src/clj/harja/palvelin/integraatiot/api/ilmoitukset.clj
+++ src/clj/harja/palvelin/integraatiot/api/ilmoitukset.clj
@@ -20,26 +20,38 @@
 
 (defn hae-ilmoituksen-jalkeen-saapuneet-idt [db viimeisin-id]
   (when viimeisin-id
-    (ilmoitukset/hae-ilmoituksen-jalkeen-saapuneet-ilmoitukset db viimeisin-id)))
+    (mapv #(:ilmoitusid %) (ilmoitukset/hae-ilmoituksen-jalkeen-saapuneet-ilmoitukset db viimeisin-id))))
 
 (defn hae-ilmoitukset [db ilmoitus-id viimeisin-id]
-  ;; todo: jos viimeisin haettu id annettu, pitää hakea kaikki ilmoitukset, jotka ovat saapuneet viimeksi haetun jälkeen
   (let [jalkeen-saapuneet-id (hae-ilmoituksen-jalkeen-saapuneet-idt db viimeisin-id)
         ilmoitus-idt (if (empty? jalkeen-saapuneet-id)
                        (vector ilmoitus-id)
                        (vec (conj jalkeen-saapuneet-id ilmoitus-id)))
-        data (ilmoitukset/hae-ilmoitukset-idlla db ilmoitus-idt )
+        data (ilmoitukset/hae-ilmoitukset-idlla db ilmoitus-idt)
         ilmoitukset (mapv (fn [ilmoitus] (sanomat/rakenna-ilmoitus (konversio/alaviiva->rakenne ilmoitus))) data)]
-    {:ilmoitukset [ilmoitukset]}))
+    {:ilmoitukset ilmoitukset})
+
+  (defn vastaanota-ilmoitus [integraatioloki db kanava tapahtuma-id urakka-id ilmoitus-id viimeisin-id sulje-lahetyksen-jalkeen?]
+    (log/debug (format "Vastaanotettiin ilmoitus id:llä %s urakalle id:llä %s." ilmoitus-id urakka-id))
+    (send! kanava
+           (aja-virhekasittelyn-kanssa
+             (fn []
+               (let [ilmoitus-id (Integer/parseInt ilmoitus-id)
+                     data (hae-ilmoitukset db ilmoitus-id viimeisin-id)
+                     vastaus (tee-vastaus json-skeemat/+ilmoitusten-haku+ data)]
+                 ;; todo: merkitse ilmoitus välitetyksi ja lähetä t-loik:n välitystiedot
+                 (lokita-vastaus integraatioloki :hae-ilmoitukset vastaus tapahtuma-id)
+                 vastaus)))
+           sulje-lahetyksen-jalkeen?)))
 
 (defn kaynnista-ilmoitusten-kuuntelu [db integraatioloki tapahtumat request]
   (let [parametrit (:params request)
         urakka-id (when (:id parametrit) (Integer/parseInt (:id parametrit)))
-        viimeisin-id (when (:viimeisinId parametrit) (Integer/parseInt (:viimeisinId parametrit)))
-        stream (if (get parametrit "stream") (not (Boolean/valueOf (get parametrit "stream"))) true)
+        viimeisin-id (when (get parametrit "viimeisinId") (Integer/parseInt (get parametrit "viimeisinId")))
+        sulje-lahetyksen-jalkeen? (if (get parametrit "stream") (not (Boolean/valueOf (get parametrit "stream"))) true)
         tapahtuma-id (lokita-kutsu integraatioloki :hae-ilmoitukset request nil)
         kayttaja (hae-kayttaja db (get (:headers request) "oam_remote_user"))]
-    (log/debug (format "Käynnistetään ilmoitusten kuuntelu urakalle id: %s." urakka-id))
+    (log/debug (format "Käynnistetään ilmoitusten kuuntelu urakalle id: %s. Viimeisin haettu ilmoitus id: %s." urakka-id viimeisin-id))
     (aja-virhekasittelyn-kanssa
       (fn []
         (validointi/tarkista-urakka-ja-kayttaja db urakka-id kayttaja)
@@ -48,17 +60,7 @@
                         tapahtumat
                         urakka-id
                         (fn [ilmoitus-id]
-                          (log/debug (format "Vastaanotettiin ilmoitus id:llä %s urakalle id:llä %s." ilmoitus-id urakka-id))
-                          (send! kanava
-                                 (aja-virhekasittelyn-kanssa
-                                   (fn []
-                                     (let [ilmoitus-id (Integer/parseInt ilmoitus-id)
-                                           data (hae-ilmoitukset db ilmoitus-id viimeisin-id)
-                                           vastaus (tee-vastaus json-skeemat/+ilmoitusten-haku+ data)]
-                                       ;; todo: merkitse ilmoitus välitetyksi ja lähetä t-loik:n välitystiedot
-                                       (lokita-vastaus integraatioloki :hae-ilmoitukset vastaus tapahtuma-id)
-                                       vastaus)))
-                                 stream)))
+                          (vastaanota-ilmoitus integraatioloki db kanava tapahtuma-id urakka-id ilmoitus-id viimeisin-id sulje-lahetyksen-jalkeen?)))
                       (on-close kanava
                                 (fn [_]
                                   (log/debug (format "Suljetaan urakan id: %s ilmoitusten kuuntelu." urakka-id))
