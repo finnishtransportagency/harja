01081f4f2dc0fc79db734a7a06317c4e27b0652f
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-12-03 08:19:31 +0200

    Korjaa puuttuvat reagent-atom

--- src/cljs/harja/tiedot/urakka/laadunseuranta.cljs
+++ src/cljs/harja/tiedot/urakka/laadunseuranta.cljs
@@ -1,11 +1,12 @@
 (ns harja.tiedot.urakka.laadunseuranta
   "Tämä nimiavaruus hallinnoi laadunseurantaa sekä havaintoja ja tarkastuksia"
-  (:require [harja.loki :refer [log logt tarkkaile!]]
+  (:require [reagent.core :refer [atom]]
+            [harja.loki :refer [log logt tarkkaile!]]
             [cljs.core.async :refer [<!]]
             [harja.loki :refer [log]])
   (:require-macros [harja.atom :refer [reaction<!]]
                    [reagent.ratom :refer [reaction]]
                    [cljs.core.async.macros :refer [go]]))
 
-(defonce laadunseurannassa? (atom false))                   ; jos true, laadunseurantaosio nyt käytössä
+(defonce laadunseurannassa? (atom false)) ; jos true, laadunseurantaosio nyt käytössä
 (defonce valittu-valilehti (atom :tarkastukset))
