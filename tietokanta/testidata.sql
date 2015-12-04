af0a0081741f5a4ecd9c2e8a451b07edafcb296c
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2015-12-04 12:23:19 +0200

    Korjaa kömpelö subqueryn käyttö turpon testidatassa

--- tietokanta/testidata.sql
+++ tietokanta/testidata.sql
@@ -135,7 +135,7 @@ INSERT INTO turvallisuuspoikkeama
  sairaalavuorokaudet, luotu, luoja, sijainti, tr_numero, tr_alkuosa, tr_loppuosa, tr_alkuetaisyys, tr_loppuetaisyys, tyyppi)
 VALUES
   ((SELECT id FROM urakka WHERE nimi='Oulun alueurakka 2014-2019'), '2015-10-01 20:00.00', '2015-10-01 22:20.00', '2015-10-06 23:00.00',
-                                                                    'Trukkikuski2', 'Lastaus', 'Sepolla oli kiire lastata laatikot, ja torni kaatui päälle. Ehti onneksi pois alta niin ei henki lähtenyt.',
+                                                                    'Trukkikuski', 'Lastaus', 'Sepolla oli kiire lastata laatikot, ja torni kaatui päälle. Ehti onneksi pois alta niin ei henki lähtenyt.',
                                                                     'Murtunut peukalo', 7, 1, NOW(), (SELECT id FROM kayttaja WHERE kayttajanimi='jvh'), ST_MakePoint(435847, 7216217)::GEOMETRY, 6, 6, 6, 6, 6,
    ARRAY['prosessipoikkeama']::turvallisuuspoikkeamatyyppi[]);
 
@@ -144,7 +144,7 @@ INSERT INTO turvallisuuspoikkeama
  sairaalavuorokaudet, luotu, luoja, sijainti, tr_numero, tr_alkuosa, tr_loppuosa, tr_alkuetaisyys, tr_loppuetaisyys, tyyppi)
 VALUES
   ((SELECT id FROM urakka WHERE nimi='Oulun alueurakka 2014-2019'), '2015-11-01 20:00.00', '2015-11-01 22:20.00', '2015-11-06 23:00.00',
-                                                                    'Trukkikuski3', 'Lastaus', 'Sepolla oli kiire lastata laatikot, ja torni kaatui päälle. Ehti onneksi pois alta niin ei henki lähtenyt.',
+                                                                    'Trukkikuski', 'Lastaus', 'Sepolla oli kiire lastata laatikot, ja torni kaatui päälle. Ehti onneksi pois alta niin ei henki lähtenyt.',
                                                                     'Murtunut peukalo', 7, 1, NOW(), (SELECT id FROM kayttaja WHERE kayttajanimi='jvh'), ST_MakePoint(435847, 7216217)::GEOMETRY, 6, 6, 6, 6, 6,
    ARRAY['tyoturvallisuuspoikkeama']::turvallisuuspoikkeamatyyppi[]);
 
@@ -154,7 +154,7 @@ INSERT INTO turvallisuuspoikkeama
  sairaalavuorokaudet, luotu, luoja, sijainti, tr_numero, tr_alkuosa, tr_loppuosa, tr_alkuetaisyys, tr_loppuetaisyys, tyyppi)
 VALUES
   ((SELECT id FROM urakka WHERE nimi='Pudasjärven alueurakka 2007-2012'), '2012-10-01 10:00.00', '2012-10-01 12:20.00', '2012-10-06 09:00.00',
-                                                                    'Trukkikuski4', 'Lastaus', 'Sepolla oli kiire lastata laatikot, ja torni kaatui päälle. Ehti onneksi pois alta niin ei henki lähtenyt.',
+                                                                    'Trukkikuski', 'Lastaus', 'Sepolla oli kiire lastata laatikot, ja torni kaatui päälle. Ehti onneksi pois alta niin ei henki lähtenyt.',
                                                                     'Murtunut peukalo', 7, 1, NOW(), (SELECT id FROM kayttaja WHERE kayttajanimi='jvh'), ST_MakePoint(227110, 6820660) :: GEOMETRY, 6, 6, 6, 6, 6,
    ARRAY['turvallisuuspoikkeama']::turvallisuuspoikkeamatyyppi[]);
 
@@ -163,7 +163,7 @@ VALUES
 INSERT INTO korjaavatoimenpide
 (turvallisuuspoikkeama, kuvaus, vastaavahenkilo)
 VALUES
-((SELECT id FROM turvallisuuspoikkeama WHERE tyontekijanammatti='Trukkikuski'), 'Pidetään huoli että ei kenenkään tarvi liikaa kiirehtiä',
+((SELECT id FROM turvallisuuspoikkeama WHERE tyontekijanammatti='Trukkikuski' AND tapahtunut = '2005-10-01 10:00.00'), 'Pidetään huoli että ei kenenkään tarvi liikaa kiirehtiä',
 'Tomi Työnjohtaja');
 
 -- Havainnot
