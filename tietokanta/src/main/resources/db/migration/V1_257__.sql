71357d5e3ea06bc663e2316949762d8c0f74d9f4
Marko Lauronen <Marko.Lauronen@solita.fi>
2015-12-08 09:51:50 +0200

    palautetaan kaikki kaistat tierekisteriosoittelle

--- tietokanta/src/main/resources/db/migration/V1_257__.sql
+++ tietokanta/src/main/resources/db/migration/V1_257__.sql
@@ -1,3 +1,4 @@
+
 -- kuvaus: tierekisteriosoitteelle_viiva multilinestring korjaus
 CREATE OR REPLACE FUNCTION tierekisteriosoitteelle_viiva(
   tie_ INTEGER, aosa_ INTEGER, aet_ INTEGER, losa_ INTEGER, let_ INTEGER)
@@ -6,12 +7,12 @@ AS $$
 DECLARE
 BEGIN
    IF aosa_ = losa_ THEN
-	SELECT ST_Line_Substring(geom, aet_/tr_pituus::FLOAT, let_/tr_pituus::FLOAT)
+	RETURN QUERY SELECT ST_Line_Substring(geom, aet_/tr_pituus::FLOAT, let_/tr_pituus::FLOAT)
 	FROM tieverkko_paloina
 	WHERE tie=tie_
 	  AND osa=aosa_;
    ELSE
-        WITH q as (SELECT ST_LineMerge(ST_Union((CASE WHEN osa=aosa_ THEN ST_Line_Substring(geom, LEAST(1, aet_/ST_Length(geom)), 1)
+        RETURN QUERY WITH q as (SELECT ST_LineMerge(ST_Union((CASE WHEN osa=aosa_ THEN ST_Line_Substring(geom, LEAST(1, aet_/ST_Length(geom)), 1)
 				      WHEN osa=losa_ THEN ST_Line_Substring(geom, 0, LEAST(1,let_/ST_Length(geom)))
 				      ELSE geom END) ORDER BY osa)) AS geom
 		     FROM tieverkko_paloina
