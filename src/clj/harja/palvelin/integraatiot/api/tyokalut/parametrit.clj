7c42dcd121c2ab3e9c91b3598e288fe45a5afc6d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-12-09 13:42:31 +0200

    Ota time zone mukaan ja paranna virheviestiä

--- src/clj/harja/palvelin/integraatiot/api/tyokalut/parametrit.clj
+++ src/clj/harja/palvelin/integraatiot/api/tyokalut/parametrit.clj
@@ -5,19 +5,21 @@
   (:import (java.text ParseException SimpleDateFormat)
            (java.util Date)))
 
-(def esimerkki-aika (.format (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ssX") (Date.)))
+(def pvm-aika-muoto "yyyy-MM-dd'T'HH:mm:ssX")
+(def esimerkki-aika (.format (SimpleDateFormat. pvm-aika-muoto) (Date.)))
 
 (defn pvm-aika-opt
   "Parsii parametrina saadun ajan. Heittää parametripoikkeuksen, jos pvm ei ole validi. 
 Sallii puuttuvan parametrin, jolloin palautetaan nil."
-  ([string] (pvm-aika-opt string "Anna päivämäärä ja aika muodossa: \"yyyy-MM-dd'T'HH:mm:ssX\" (esim. " esimerkki-aika ")"))
+  ([string] (pvm-aika-opt string "Anna päivämäärä ja aika muodossa: \"yyyy-MM-dd'T'HH:mm:ssX\" (esim. " esimerkki-aika "), sain: " string))
   ([string & viestit]
-   (try
-     (pvm-string->java-sql-date string)
-     (catch ParseException e
-       (heita-puutteelliset-parametrit-poikkeus
-        {:koodi "virheellinen-pvm-aika-muoto"
-         :viesti (apply str viestit)})))))
+   (when string 
+     (try
+       (.parse (SimpleDateFormat. pvm-aika-muoto) string)
+       (catch ParseException e
+         (heita-puutteelliset-parametrit-poikkeus
+          {:koodi "virheellinen-pvm-aika-muoto"
+           :viesti (apply str viestit)}))))))
 
 (defn pvm-aika 
   "Sama kuin pvm-aika-opt mutta ei salli puuttuvaa arvoa"
@@ -25,4 +27,6 @@ Sallii puuttuvan parametrin, jolloin palautetaan nil."
   ([string & virheet]
    (if-let [aika (pvm-aika-opt string)]
      aika
-     (heita-puutteelliset-parametrit-poikkeus (apply str virheet)))))
+     (heita-puutteelliset-parametrit-poikkeus
+      {:koodi "pakollinen-pv-aika-parametri-puuttuu"
+       :viesti (apply str virheet)}))))
