7c70073221478f420487bd512676b5c0143168a9
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2015-12-10 11:32:50 +0200

    Päivitä ylläpidon aikataulunäkymää palaverin perusteella

--- src/cljs/harja/views/urakka/aikataulu.cljs
+++ src/cljs/harja/views/urakka/aikataulu.cljs
@@ -27,22 +27,32 @@
          [grid/grid
           {:otsikko "Kohteiden aikataulu"}
 
-          [{:otsikko "Kohde\u00ADnumero" :leveys "10%" :nimi :kohdenumero :tyyppi :string :pituus-max 128 :muokattava? (constantly false)}
-           {:otsikko "TR-osoite" :leveys "10%" :nimi :tr-osoite :tyyppi :string :muokattava? (constantly false)}
-           {:otsikko "KVL" :leveys "10%" :nimi :kvl :tyyppi :string :muokattava? (constantly false)}
-           {:otsikko "Toimenpide" :leveys "10%" :nimi :toimenpide :tyyppi :string :muokattava? (constantly false)}
-           {:otsikko "YP LK" :leveys "10%" :nimi :yp-lk :tyyppi :string :muokattava? (constantly false)}
-           {:otsikko "Aloitus\u00ADaika" :leveys "10%" :nimi :alkuaika :tyyppi :string :muokattava? (constantly false)}
-           {:otsikko "Pääll. valmis" :leveys "10%" :nimi :valmistumisaika :tyyppi :string :muokattava? (constantly false)}
-           {:otsikko "TM aloitus\u00ADaika" :leveys "10%" :nimi :tm-alkuaika :tyyppi :string :muokattava? (constantly false)}
-           {:otsikko "TM valmis" :leveys "10%" :nimi :tm-valmistumisaika :tyyppi :string :muokattava? (constantly false)}
-           {:otsikko "Kohde valmis" :leveys "10%" :nimi :kohde-valmis :tyyppi :string :muokattava? (constantly false)}]
+          [{:otsikko "Kohde\u00AD ID" :leveys "5%" :nimi :kohdenumero :tyyppi :string :pituus-max 128 :muokattava? (constantly false)}
+           {:otsikko "Kohteen nimi" :leveys "10%" :nimi :kohdenimi :tyyppi :string :pituus-max 128 :muokattava? (constantly false)}
 
-          [{:kohdenumero 1 :tr-osoite "4/1/100/4/534" :kvl 123 :toimenpide "MPKJ" :yp-lk "1"
-            :alkuaika "10.6.2015" :valmistumisaika "5.7.2015" :tm-alkuaika "6.7.2015" :tm-valmistumisaika "14.7.2015" :kohde-valmis "Ei"}
-           {:kohdenumero 2 :tr-osoite "4/4/534/6/134" :kvl 123 :toimenpide "MPKJ" :yp-lk "1"
-            :alkuaika "11.6.2015" :valmistumisaika "6.7.2015" :tm-alkuaika "7.7.2015" :tm-valmistumisaika "14.7.2015" :kohde-valmis "Ei"}
-           {:kohdenumero 3 :tr-osoite "4/6/134/7/1534" :kvl 123 :toimenpide "MPKJ" :yp-lk "1"
-            :alkuaika "12.6.2015" :valmistumisaika "7.7.2015" :tm-alkuaika "8.7.2015" :tm-valmistumisaika "14.7.2015" :kohde-valmis "Ei"}
+           {:otsikko "TR-osoite" :leveys "10%" :nimi :tr-osoite :tyyppi :string :muokattava? (constantly false)}
+           {:otsikko "Pääll. aloitus\u00AD" :leveys "8%" :nimi :paall-aloitusaika :tyyppi :string :muokattava? (constantly false)}
+           {:otsikko "Pääll. valmis" :leveys "8%" :nimi :paall-valmistumisaika :tyyppi :string :muokattava? (constantly false)}
+           {:otsikko "TM valmis" :leveys "8%" :nimi :tm-valmistumisaika :tyyppi :string :muokattava? (constantly false)}
+           {:otsikko "Valmis tie\u00ADmerkin\u00ADtään" :leveys "7%" :nimi :valmis-tiemerkintaan :tyyppi :string :muokattava? (constantly false)}
+           {:otsikko "Kohde valmis" :leveys "5%" :nimi :kohde-valmis :tyyppi :string :muokattava? (constantly false)}
+           {:otsikko     "Toiminnot" :nimi :toiminnot :tyyppi :komponentti
+            :komponentti (fn [rivi]
+                           (let [valmis-tiemerkintaan? (= "Kyllä" (:valmis-tiemerkintaan rivi))]
+                             [:button.nappi-ensisijainen.nappi-grid
+                              {:class    (str "nappi-ensisijainen " (if valmis-tiemerkintaan? "disabled"))
+                               :type     "button"
+                               :on-click #(log "Painettu")} "Valmis tiemerkintään"]))
+            :leveys      "14%"}]
+
+          [{:kohdenumero       1 :kohdenimi "Mt 4 Ylä-Laakkola":tr-osoite "4/1/100/4/534" :kvl 123 :toimenpide "MPKJ" :yp-lk "1"
+            :paall-aloitusaika "10.6.2015" :paall-valmistumisaika "5.7.2015" :tm-alkuaika "6.7.2015" :tm-valmistumisaika "14.7.2015"
+            :valmis-tiemerkintaan "Kyllä" :kohde-valmis "Ei"}
+           {:kohdenumero       2 :kohdenimi "Mt 4 Ala-Laakkola":tr-osoite "4/4/534/6/134" :kvl 123 :toimenpide "MPKJ" :yp-lk "1"
+            :paall-aloitusaika "11.6.2015" :paall-valmistumisaika "6.7.2015" :tm-alkuaika "7.7.2015" :tm-valmistumisaika "14.7.2015"
+            :valmis-tiemerkintaan "Ei" :kohde-valmis "Ei"}
+           {:kohdenumero       3 :kohdenimi "Mt 4 Päkylä":tr-osoite "4/6/134/7/1534" :kvl 123 :toimenpide "MPKJ" :yp-lk "1"
+            :paall-aloitusaika "12.6.2015" :paall-valmistumisaika "7.7.2015" :tm-alkuaika "8.7.2015" :tm-valmistumisaika "14.7.2015"
+            :valmis-tiemerkintaan "Ei" :kohde-valmis "Ei"}
 
            ]]]))))
