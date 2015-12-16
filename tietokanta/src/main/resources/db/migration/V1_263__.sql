c8485c78f4a1ba5be3b656f7d320552585ce7073
Marko Lauronen <Marko.Lauronen@solita.fi>
2015-12-16 12:37:46 +0200

    lisää puuttuvat arvot suoritettavatehtava -enumiin.

---
+++ tietokanta/src/main/resources/db/migration/V1_263__.sql
@@ -0,0 +1,4 @@
+ALTER TYPE suoritettavatehtava ADD VALUE 'liuossuolaus';
+ALTER TYPE suoritettavatehtava ADD VALUE 'aurausviitoitus ja kinostimet';
+ALTER TYPE suoritettavatehtava ADD VALUE 'lumensiirto';
+ALTER TYPE suoritettavatehtava ADD VALUE 'paannejaan poisto';
