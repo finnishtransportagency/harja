350e637c6b74d262fd71ac460ddaf563cbdb8f41
mikkoro <mikko.ronkkomaki@solita.fi>
2015-12-17 12:45:56 +0200

    Lisää testit yksikköhintaisten töiden vuosikohtaiselle laskennalle

--- test/clj/harja/palvelin/integraatiot/sampo/kasittely/kustannussuunnitelmat_test.clj
+++ test/clj/harja/palvelin/integraatiot/sampo/kasittely/kustannussuunnitelmat_test.clj
@@ -48,3 +48,12 @@
     (is (= odotettu vuosittaiset-summat))))
 
 
+(deftest tarkista-yksikkohintaisten-vuosisummien-muodostus
+  (let [db (apply tietokanta/luo-tietokanta testitietokanta)
+        odotettu [{:alkupvm "2014-10-01T00:00:00.0", :loppupvm "2014-12-31T02:00:00.0", :summa 300M} {:alkupvm "2015-01-01T02:00:00.0", :loppupvm "2015-12-31T02:00:00.0", :summa 900M} {:alkupvm "2016-01-01T02:00:00.0", :loppupvm "2016-12-31T02:00:00.0", :summa 0} {:alkupvm "2017-01-01T02:00:00.0", :loppupvm "2017-12-31T02:00:00.0", :summa 0} {:alkupvm "2018-01-01T02:00:00.0", :loppupvm "2018-12-31T02:00:00.0", :summa 0} {:alkupvm "2019-01-01T02:00:00.0", :loppupvm "2019-09-30T00:00:00.0", :summa 0}]
+        maksuera (maksuera/hae-maksuera db 18)
+        vuosittaiset-summat (tee-vuosittaiset-summat db 18 maksuera)]
+
+    (is (= 6 (count vuosittaiset-summat)))
+    (is (= odotettu vuosittaiset-summat))))
+
