c3a79e223c677f946f2fb527f88103ae06d2dcbb
mikkoro <mikko.ronkkomaki@solita.fi>
2015-12-17 14:30:50 +0200

    Luo summien segmentit vuosittain ja kuukausittain jaoteltuina

--- src/clj/harja/palvelin/integraatiot/sampo/sanomat/kustannussuunnitelma_sanoma.clj
+++ src/clj/harja/palvelin/integraatiot/sampo/sanomat/kustannussuunnitelma_sanoma.clj
@@ -4,8 +4,7 @@
             [clojure.string :as str]
             [clj-time.core :as time]
             [clj-time.periodic :as time-period]
-            [clj-time.coerce :as coerce]
-            [harja.pvm :as pvm])
+            [clj-time.coerce :as coerce])
   (:use [slingshot.slingshot :only [try+ throw+]]))
 
 (defn muodosta-maksueranumero [numero]
@@ -14,69 +13,19 @@
 (defn muodosta-kustannussuunnitelmanumero [numero]
   (str/join "" ["AK" numero]))
 
-(defn muodosta-kustannuselementti [alkupvm loppupvm summa]
-  [:segment
-   {:value  summa
-    :finish loppupvm
-    :start  alkupvm}])
-
 (defn aikavali
   [alku loppu askel]
   (let [vali (time-period/periodic-seq alku askel)
         valilla? (fn [aika] (time/within? (time/interval alku loppu) aika))]
     (take-while valilla? vali)))
 
-(defn muodosta-ensimmaisen-vuoden-elementti [alkupvm kuukausittainen-summa ensimmaisen-vuoden-kuukaudet]
-  (let [loppupvm (pvm/aika-iso8601 (pvm/vuoden-viim-pvm (time/year alkupvm)))
-        summa (* kuukausittainen-summa ensimmaisen-vuoden-kuukaudet)]
-    (muodosta-kustannuselementti alkupvm loppupvm summa)))
-
-(defn muodosta-valivuosien-elementit [alkuvuosi taysien-vuosien-maara kuukausittainen-summa]
-  (concat
-    (into []
-          (for [i (range taysien-vuosien-maara)]
-            (do
-              (inc i)
-              (let [vuosi (+ 1 i alkuvuosi)
-                    alkupvm (pvm/aika-iso8601 (pvm/vuoden-eka-pvm vuosi))
-                    loppupvm (pvm/aika-iso8601 (pvm/vuoden-viim-pvm vuosi))
-                    summa (* 12 kuukausittainen-summa)]
-                (muodosta-kustannuselementti alkupvm loppupvm summa)))))))
-
-(defn muodosta-viimeisen-vuoden-elementti [loppupvm kuukausittainen-summa viimeisen-vuoden-kuukaudet]
-  (let [alkupvm (pvm/aika-iso8601 (pvm/vuoden-eka-pvm (time/year loppupvm)))
-        summa (* kuukausittainen-summa viimeisen-vuoden-kuukaudet)]
-    (muodosta-kustannuselementti alkupvm loppupvm summa)))
-
-(defn luo-summat [alkupvm loppupvm maksuera]
-  (let [alkupvm (coerce/from-sql-date alkupvm)
-        _ (println alkupvm)
-        loppupvm (coerce/from-sql-date loppupvm)
-        kuukausien-maara (count (aikavali alkupvm loppupvm (time/months 1)))
-        _ (println kuukausien-maara)
-        summa (:summa (:kustannussuunnitelma maksuera))
-        summa (if summa (double summa) 0)
-        kuukausittainen-summa (if (and (< 0 kuukausien-maara) summa (< 0 summa))
-                                (/ summa kuukausien-maara)
-                                0)
-        ensimmaisen-vuoden-kuukaudet (time/month alkupvm)
-        viimeisen-vuoden-kuukaudet (rem (- kuukausien-maara ensimmaisen-vuoden-kuukaudet) 12)
-        taysien-vuosien-maara (- kuukausien-maara viimeisen-vuoden-kuukaudet)
-        kustannuselementit []
-        alkuvuosi (time/year alkupvm)
-        loppuvuosi (time/year loppupvm)]
-
-    (into [] (-> kustannuselementit
-                 ;; ensimmäinen vuosi
-                 (concat [(muodosta-ensimmaisen-vuoden-elementti alkupvm kuukausittainen-summa ensimmaisen-vuoden-kuukaudet)])
-
-                 ;; välissä olevat täydet vuodet
-                 (when (> 0 taysien-vuosien-maara)
-                       (muodosta-valivuosien-elementit alkuvuosi taysien-vuosien-maara kuukausittainen-summa))
-
-                 ;; viimeinen vuosi
-                 (when (< alkuvuosi loppuvuosi)
-                       (concat [(muodosta-viimeisen-vuoden-elementti loppupvm kuukausittainen-summa viimeisen-vuoden-kuukaudet)]))))))
+(defn luo-summat [vuosisummat]
+  (mapv (fn [vuosisumma]
+          [:segment
+           {:value  (:summa vuosisumma)
+            :finish (:loppupvm vuosisumma)
+            :start  (:alkupvm vuosisumma)}])
+        vuosisummat))
 
 (defn muodosta-custom-information [nimi arvo]
   [:CustomInformation
@@ -151,7 +100,7 @@
         [:GroupingAttribute "lov1_id"]]
        [:Details
         [:Detail
-         (luo-summat alkupvm loppupvm maksuera)
+         (reduce conj [:Cost] (luo-summat (:vuosittaiset-summat maksuera)))
          [:GroupingAttributes
           (muodosta-grouping-attribute "lov1_id" "3110201")
           (muodosta-grouping-attribute "role_id" (valitse-lkp-tilinumero koodi tuotenumero))]
--- test/clj/harja/palvelin/integraatiot/sampo/sanomat/kustannussuunnitelma_sanoma_test.clj
+++ test/clj/harja/palvelin/integraatiot/sampo/sanomat/kustannussuunnitelma_sanoma_test.clj
@@ -17,8 +17,8 @@
                  :maksuera             {:nimi
                                                 "Testimaksuera"
                                         :tyyppi "kokonaishintainen"}
-                 :toimenpideinstanssi  {:alkupvm         (parsi-paivamaara "12.12.2015")
-                                        :loppupvm        (parsi-paivamaara "1.1.2017")
+                 :toimenpideinstanssi  {:alkupvm         (parsi-paivamaara "1.10.2014")
+                                        :loppupvm        (parsi-paivamaara "30.9.2019")
                                         :vastuuhenkilo   "A009717"
                                         :talousosasto    "talousosasto"
                                         :tuotepolku      "polku/tuote"
@@ -28,11 +28,30 @@
                  :urakka               {:sampoid "PR00020606"}
                  :sopimus              {:sampoid "00LZM-0033600"}
                  :kustannussuunnitelma {:summa 93999M}
-                 :tuotenumero          111})
+                 :tuotenumero          111
+                 :vuosittaiset-summat    [{:alkupvm "2014-10-01T00:00:00.0",
+                                           :loppupvm "2014-12-31T02:00:00.0",
+                                           :summa 300M}
+                                          {:alkupvm "2015-01-01T02:00:00.0",
+                                           :loppupvm "2015-12-31T02:00:00.0",
+                                           :summa 900M}
+                                          {:alkupvm "2016-01-01T02:00:00.0",
+                                           :loppupvm "2016-12-31T02:00:00.0",
+                                           :summa 0}
+                                          {:alkupvm "2017-01-01T02:00:00.0",
+                                           :loppupvm "2017-12-31T02:00:00.0",
+                                           :summa 0}
+                                          {:alkupvm "2018-01-01T02:00:00.0",
+                                           :loppupvm "2018-12-31T02:00:00.0",
+                                           :summa 0}
+                                          {:alkupvm "2019-01-01T02:00:00.0",
+                                           :loppupvm "2019-09-30T00:00:00.0",
+                                           :summa 0}]})
 
 (deftest tarkista-kustannussuunnitelman-validius
   (let [kustannussuunnitelma (html (kustannussuunnitelma-sanoma/muodosta +maksuera+))
         xsd "nikuxog_costPlan.xsd"]
+    (println "----> kustannussuunnitelma" kustannussuunnitelma)
     (is (xml/validoi +xsd-polku+ xsd kustannussuunnitelma) "Muodostettu XML-tiedosto on XSD-skeeman mukainen")))
 
 (deftest tarkista-lkp-tilinnumeron-paattely
@@ -55,15 +74,31 @@
 
 (deftest tarkista-kulun-jakaminen-vuosille
   (let [segmentit (kustannussuunnitelma-sanoma/luo-summat
-                    (parsi-paivamaara "12.12.2015")
-                    (parsi-paivamaara "1.11.2017") +maksuera+)]
-    (is (= 4 (count segmentit)) "Segmentit on jaoteltu 3 vuodelle")
+                    [{:alkupvm  "2014-10-01T00:00:00.0",
+                      :loppupvm "2014-12-31T02:00:00.0",
+                      :summa    300M}
+                     {:alkupvm  "2015-01-01T02:00:00.0",
+                      :loppupvm "2015-12-31T02:00:00.0",
+                      :summa    900M}
+                     {:alkupvm  "2016-01-01T02:00:00.0",
+                      :loppupvm "2016-12-31T02:00:00.0",
+                      :summa    0}
+                     {:alkupvm  "2017-01-01T02:00:00.0",
+                      :loppupvm "2017-12-31T02:00:00.0",
+                      :summa    0}
+                     {:alkupvm  "2018-01-01T02:00:00.0",
+                      :loppupvm "2018-12-31T02:00:00.0",
+                      :summa    0}
+                     {:alkupvm  "2019-01-01T02:00:00.0",
+                      :loppupvm "2019-09-30T00:00:00.0",
+                      :summa    0}])]
+    (println segmentit)
+    (is (= 6 (count segmentit)) "Segmentit on jaoteltu 3 vuodelle")
 
-    (let [segmentti (second (second segmentit))
-          odotettu-summa (/ (double (get-in +maksuera+ [:kustannussuunnitelma :summa])) 3)]
-      (is (= odotettu-summa (:value segmentti)))
-      (is (= "2015-01-01T00:00:00.0" (:start segmentti)))
-      (is (= "2015-12-31T00:00:00.0" (:finish segmentti))))))
+    (let [segmentti (second (second segmentit))]
+      (is (= 900M (:value segmentti)))
+      (is (= "2015-01-01T02:00:00.0" (:start segmentti)))
+      (is (= "2015-12-31T02:00:00.0" (:finish segmentti))))))
 
 (deftest tarkista-kustannussuunnitelmajakson-muodostus
   (is (= "1.1.2015-31.12.2015"
