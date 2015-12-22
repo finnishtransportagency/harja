c41e20cf34b0a46430594d7b039960db6bcf01eb
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2015-12-22 12:41:58 +0200

    Korjaa ie? var muoto

--- src/cljs/harja/ui/yleiset.cljs
+++ src/cljs/harja/ui/yleiset.cljs
@@ -10,10 +10,10 @@
                    [reagent.ratom :refer [reaction run!]]))
 
 
-(def ie? let [ua (-> js/window .-navigator .-userAgent)]
-  (or (not= -1 (.indexOf ua "MSIE "))
-      (not= -1 (.indexOf ua "Trident/"))
-      (not= -1 (.indexOf ua "Edge/"))))
+(def ie? (let [ua (-> js/window .-navigator .-userAgent)]
+           (or (not= -1 (.indexOf ua "MSIE "))
+               (not= -1 (.indexOf ua "Trident/"))
+               (not= -1 (.indexOf ua "Edge/")))))
 
 (defn karttakuva
   "Palauttaa kuvatiedoston nimen, jos käytössä IE palauttaa .png kuvan, muuten .svg"
