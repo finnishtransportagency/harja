d7b89f084cfbbdf335bdddb4f811a40f7fa4536b
Jari Hanhela <Jari.Hanhela@solita.fi>
2015-12-23 13:28:38 +0200

    Näytä tehtävän tiedot oikein kok. hint. toteuman popupissa

--- src/clj/harja/kyselyt/toteumat.sql
+++ src/clj/harja/kyselyt/toteumat.sql
@@ -569,6 +569,10 @@ SELECT
   t.lisatieto,
   tk.nimi             AS tehtava_nimi,
   tk.id               AS tehtava_id,
+  tpk.nimi            AS tehtava_toimenpide,
+  tt.maara            AS tehtava_maara,
+  tt.paivan_hinta     AS tehtava_paivanhinta,
+  tt.lisatieto        AS tehtava_lisatieto,
   mk.nimi             AS materiaali_nimi,
   tm.maara            AS materiaali_maara
 FROM toteuma_tehtava tt
@@ -576,6 +580,7 @@ FROM toteuma_tehtava tt
   JOIN toteuma t ON tt.toteuma = t.id
   JOIN toimenpidekoodi tk ON tt.toimenpidekoodi = tk.id
   LEFT JOIN toteuma_materiaali tm ON tm.toteuma = t.id
+  LEFT JOIN toimenpidekoodi tpk ON tt.toimenpidekoodi = tpk.id
   LEFT JOIN materiaalikoodi mk ON tm.materiaalikoodi = mk.id
 WHERE
   t.urakka = :urakkaid
