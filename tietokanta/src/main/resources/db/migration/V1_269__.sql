1c0b2373e773aa67aeb0162d2dc1d3a424a9aba9
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-04 13:19:12 +0200

    Lisää migraatio yks. hint. töiden kuukausiraportille

---
+++ tietokanta/src/main/resources/db/migration/V1_269__.sql
@@ -0,0 +1,12 @@
+-- Yksikköhintaisten töiden kuukausiraportti
+
+INSERT INTO raportti (nimi, kuvaus, konteksti, parametrit, koodi) VALUES (
+ 'yks-hint-kuukausiraportti', 'Yksikköhintaisten työt kuukausittain',
+ ARRAY['urakka'::raporttikonteksti],
+ ARRAY[('Hoitokausi', 'hoitokausi',true,NULL)::raporttiparametri,
+       ('Kuukausi','hoitokauden-kuukausi',true,NULL)::raporttiparametri,
+       ('Toimenpide','urakan-toimenpide',false,NULL)::raporttiparametri],
+ '#''harja.palvelin.raportointi.raportit.yksikkohintaiset-tyot-kuukausittain/suorita'
+);
+
+UPDATE raportti SET kuvaus 'Yksikköhintaiset työt päivittäin' WHERE nimi = 'yks-hint-tyot';
