5f96a74aedeb4bf7b4a830b08a5f8a90589e5952
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-04 14:30:42 +0200

    Lisää uudelle raporttityypille oikea urakkatyyppi

--- tietokanta/src/main/resources/db/migration/V1_269__.sql
+++ tietokanta/src/main/resources/db/migration/V1_269__.sql
@@ -1,12 +1,13 @@
 -- Yksikköhintaisten töiden kuukausiraportti
 
-INSERT INTO raportti (nimi, kuvaus, konteksti, parametrit, koodi) VALUES (
+INSERT INTO raportti (nimi, kuvaus, konteksti, parametrit, koodi, urakkatyyppi) VALUES (
  'yks-hint-kuukausiraportti', 'Yksikköhintaiset työt kuukausittain',
  ARRAY['urakka'::raporttikonteksti],
  ARRAY[('Hoitokausi', 'hoitokausi',true,NULL)::raporttiparametri,
        ('Kuukausi','hoitokauden-kuukausi',true,NULL)::raporttiparametri,
        ('Toimenpide','urakan-toimenpide',false,NULL)::raporttiparametri],
- '#''harja.palvelin.raportointi.raportit.yksikkohintaiset-tyot-kuukausittain/suorita'
+ '#''harja.palvelin.raportointi.raportit.yksikkohintaiset-tyot-kuukausittain/suorita',
+ 'hoito'::urakkatyyppi
 );
 
 UPDATE raportti SET kuvaus = 'Yksikköhintaiset työt päivittäin' WHERE nimi = 'yks-hint-tyot';
