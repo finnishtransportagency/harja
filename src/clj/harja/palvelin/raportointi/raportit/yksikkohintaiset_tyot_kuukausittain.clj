a48b80548c6b04c491f8610f0e3e82a8449e0411
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-05 08:32:21 +0200

    Laske yks. hint. kuukausiraportin kuukausittaiset summat oikein raportille

--- src/clj/harja/palvelin/raportointi/raportit/yksikkohintaiset_tyot_kuukausittain.clj
+++ src/clj/harja/palvelin/raportointi/raportit/yksikkohintaiset_tyot_kuukausittain.clj
@@ -11,13 +11,22 @@
   (let [tehtavat-kuukausittain-summattuna (hae-yksikkohintaiset-tyot-per-kuukausi db
                                                                urakka-id alkupvm loppupvm
                                                                (if toimenpide-id true false) toimenpide-id)
+        ;; FIXME Jostain syystä kannasta nostettu kuukausi / vuosi tulee desimaaliarvona, täytyy selvittää
+        ;; mistä johtuu mutta toistaiseksi korjataan varmistamalla että ovat integerejä
+        tehtavat-kuukausittain-summattuna (mapv (fn [tehtava]
+                                                  (-> tehtava
+                                                      (assoc :kuukausi (int (:kuukausi tehtava)))
+                                                      (assoc :vuosi (int (:vuosi tehtava)))))
+                                                tehtavat-kuukausittain-summattuna)
+        ;; Muutetaan tehtävät muotoon, jossa jokainen tehtävä ensiintyy kerran ja kuukausittaiset
+        ;; summat esitetään avaimina
         naytettavat-rivit (mapv (fn [tehtava]
                                   (let [taman-tehtavan-rivit (filter #(= (:nimi %) tehtava)
                                                                       tehtavat-kuukausittain-summattuna)
                                         kuukausittaiset-summat (reduce
                                                                  (fn [eka toka]
                                                                    (assoc eka
-                                                                     (str (int (:kuukausi toka)) "/" (int (:vuosi toka)))
+                                                                     (str (:vuosi toka) "/" (:kuukausi toka))
                                                                      (:toteutunut_maara  toka)))
                                                                  (first taman-tehtavan-rivit)
                                                                  taman-tehtavan-rivit)]
@@ -25,6 +34,9 @@
                                         (dissoc :kuukausi)
                                         (dissoc :vuosi))))
                                 (distinct (mapv :nimi tehtavat-kuukausittain-summattuna)))
+        listattavat-pvmt (distinct (mapv (fn [rivi]
+                                           {:vuosi (:vuosi rivi) :kuukausi (:kuukausi rivi)})
+                                         (sort-by #(vec (map % [:vuosi :kuukausi])) tehtavat-kuukausittain-summattuna)))
         raportin-nimi "Yksikköhintaiset työt kuukausittain"
         konteksti :urakka ;; myöhemmin tähänkin rapsaan voi tulla muitakin kontekseja, siksi alle yleistä koodia
         otsikko (raportin-otsikko
@@ -35,19 +47,25 @@
                     )
                   raportin-nimi alkupvm loppupvm)]
     [:raportti {:orientaatio :landscape
-                :nimi raportin-nimi}
+                :nimi        raportin-nimi}
      [:taulukko {:otsikko otsikko
                  :tyhja   (if (empty? naytettavat-rivit) "Ei raportoitavia tehtäviä.")}
-      [{:leveys "25%" :otsikko "Tehtävä"}
-       {:leveys "10%" :otsikko "Yksikkö"}
-       {:leveys "15%" :otsikko "Määrä yhteensä"}
-       {:leveys "15%" :otsikko "Tot-%"}
-       {:leveys "15%" :otsikko "Suunniteltu määrä hoitokaudella"}]
-
-      (mapv (juxt :nimi
-                  :yksikko
-                  :toteutunut_maara
-                  :toteumaprosentti
-                  :suunniteltu_maara)
+      (flatten [{:leveys "25%" :otsikko "Tehtävä"}
+                {:leveys "10%" :otsikko "Yksikkö"}
+                (mapv (fn [rivi]
+                        {:otsikko (str (:vuosi rivi) "/" (:kuukausi rivi))})
+                      listattavat-pvmt)
+                {:leveys "15%" :otsikko "Määrä yhteensä"}
+                {:leveys "15%" :otsikko "Tot-%"}
+                {:leveys "15%" :otsikko "Suunniteltu määrä hoitokaudella"}])
+      (mapv (fn [rivi]
+              (flatten [(:nimi rivi)
+                        (:yksikko rivi)
+                        (mapv (fn [pvm]
+                                (get rivi (str (:vuosi pvm) "/" (:kuukausi pvm))))
+                              listattavat-pvmt)
+                        ; (:toteutunut_maara rivi) TODO SUMMAA NÄMÄ OIKEIN
+                        ; (:toteumaprosentti rivi)
+                        (:suunniteltu_maara rivi)]))
             naytettavat-rivit)]]))
 
