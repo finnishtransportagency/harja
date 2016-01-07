ddf84f6867088f03e64c763ab6c110336bde14fb
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-07 10:17:05 +0200

    Mahdollista yks. hint. työt -kuukausittain raportin suorittaminen urakoittain eriteltynä

--- src/clj/harja/kyselyt/yksikkohintaiset_tyot.sql
+++ src/clj/harja/kyselyt/yksikkohintaiset_tyot.sql
@@ -144,8 +144,53 @@ WHERE (tot.alkanut >= :alkupvm AND tot.alkanut <= :loppupvm)
 GROUP BY t4.nimi, yksikko, vuosi, kuukausi
 ORDER BY vuosi, kuukausi;
 
+-- name: hae-yksikkohintaiset-tyot-kuukausittain-hallintayksikolle-urakoittain
+-- Hakee yksikköhintaiset työt annetulle hallintayksikolle ja aikavälille summattuna kuukausittain, eriteltynä urakoittain
+-- Optionaalisesti voidaan antaa vain tietty toimenpide, jonka työt haetaan.
+SELECT
+  extract(YEAR FROM tot.alkanut)::INT as vuosi,
+  extract(MONTH FROM tot.alkanut)::INT as kuukausi,
+  u.id AS urakka_id,
+  u.nimi AS urakka_nimi,
+  t4.nimi,
+  t4.yksikko,
+  SUM(tt.maara) as toteutunut_maara
+FROM toteuma tot
+  JOIN toteuma_tehtava tt ON tt.toteuma=tot.id AND tt.poistettu IS NOT TRUE
+  JOIN toimenpidekoodi t4 ON tt.toimenpidekoodi=t4.id
+  JOIN urakka u ON tot.urakka = u.id
+WHERE tot.urakka IN (SELECT id
+                     FROM urakka
+                     WHERE hallintayksikko = :hallintayksikko)
+      AND (tot.alkanut >= :alkupvm AND tot.alkanut <= :loppupvm)
+      AND (tot.alkanut >= :alkupvm AND tot.alkanut <= :loppupvm)
+      AND (:rajaa_tpi = false OR tt.toimenpidekoodi IN (SELECT tpk.id FROM toimenpidekoodi tpk WHERE tpk.emo=:tpi))
+GROUP BY t4.nimi, yksikko, vuosi, kuukausi, u.id
+ORDER BY vuosi, kuukausi;
+
+-- name: hae-yksikkohintaiset-tyot-kuukausittain-koko-maalle-urakoittain
+-- Hakee yksikköhintaiset työt koko maalle ja aikavälille summattuna kuukausittain, eriteltynä urakoittain
+-- Optionaalisesti voidaan antaa vain tietty toimenpide, jonka työt haetaan.
+SELECT
+  extract(YEAR FROM tot.alkanut)::INT as vuosi,
+  extract(MONTH FROM tot.alkanut)::INT as kuukausi,
+  u.id AS urakka_id,
+  u.nimi AS urakka_nimi,
+  t4.nimi,
+  t4.yksikko,
+  SUM(tt.maara) as toteutunut_maara
+FROM toteuma tot
+  JOIN toteuma_tehtava tt ON tt.toteuma=tot.id AND tt.poistettu IS NOT TRUE
+  JOIN toimenpidekoodi t4 ON tt.toimenpidekoodi=t4.id
+  JOIN urakka u ON tot.urakka = u.id
+WHERE (tot.alkanut >= :alkupvm AND tot.alkanut <= :loppupvm)
+      AND (tot.alkanut >= :alkupvm AND tot.alkanut <= :loppupvm)
+      AND (:rajaa_tpi = false OR tt.toimenpidekoodi IN (SELECT tpk.id FROM toimenpidekoodi tpk WHERE tpk.emo=:tpi))
+GROUP BY t4.nimi, yksikko, vuosi, kuukausi, u.id
+ORDER BY vuosi, kuukausi;
+
 -- name: hae-yksikkohintaiset-tyot-tehtavittain-summattuna-urakalle
--- Hakee yksikköhintaiset työt annetulle urakalle ja aikavälille summattuna tehtävittäin
+-- Hakee yksikköhintaiset työt annetulle urakalle ja aikavälille, summattuna tehtävittäin
 SELECT
   t4.nimi,
   yht.yksikko,
@@ -168,7 +213,7 @@ GROUP BY t4.nimi, yht.yksikko, yht.yksikkohinta, yht.maara
 ORDER BY toteutuneet_kustannukset;
 
 -- name: hae-yksikkohintaiset-tyot-tehtavittain-summattuna-hallintayksikolle
--- Hakee yksikköhintaiset työt annetulle hallintayksikölle ja aikavälille summattuna tehtävittäin
+-- Hakee yksikköhintaiset työt annetulle hallintayksikölle aikavälille, summattuna tehtävittäin
 SELECT
   t4.nimi,
   t4.yksikko,
@@ -186,7 +231,7 @@ WHERE tot.urakka IN (SELECT id
 GROUP BY t4.nimi, t4.yksikko;
 
 -- name: hae-yksikkohintaiset-tyot-tehtavittain-summattuna-koko-maalle
--- Hakee yksikköhintaiset työt koko maasta aikavälille summattuna tehtävittäin
+-- Hakee yksikköhintaiset työt koko maasta aikavälille, summattuna tehtävittäin
 SELECT
   t4.nimi,
   t4.yksikko,
@@ -201,7 +246,7 @@ WHERE (tot.alkanut >= :alkupvm AND tot.alkanut <= :loppupvm)
 GROUP BY t4.nimi, t4.yksikko;
 
 -- name: hae-yksikkohintaiset-tyot-tehtavittain-summattuna-hallintayksikolle-urakoittain
--- Hakee yksikköhintaiset työt annetulle hallintayksikölle ja aikavälille summattuna tehtävittäin
+-- Hakee yksikköhintaiset työt hallintayksikstä aikavälille, summattuna tehtävittäin ja eriteltynä urakoittain
 SELECT
   u.id                           AS urakka_id,
   u.nimi                         AS urakka_nimi,
@@ -222,7 +267,7 @@ WHERE tot.urakka IN (SELECT id
 GROUP BY t4.nimi, t4.yksikko, u.id;
 
 -- name: hae-yksikkohintaiset-tyot-tehtavittain-summattuna-koko-maalle-urakoittain
--- Hakee yksikköhintaiset työt koko maasta aikavälille summattuna tehtävittäin
+-- Hakee yksikköhintaiset työt koko maasta aikavälille, summattuna tehtävittäin ja eriteltynä urakoittain
 SELECT
   u.id                           AS urakka_id,
   u.nimi                         AS urakka_nimi,
--- src/clj/harja/palvelin/raportointi/raportit/yksikkohintaiset_tyot_kuukausittain.clj
+++ src/clj/harja/palvelin/raportointi/raportit/yksikkohintaiset_tyot_kuukausittain.clj
@@ -16,15 +16,23 @@
                                                       urakka-id alkupvm loppupvm
                                                       (if toimenpide-id true false) toimenpide-id))
 
-(defn hae-tehtavat-hallintayksikolle [db {:keys [hallintayksikko-id alkupvm loppupvm toimenpide-id]}]
-  (q/hae-yksikkohintaiset-tyot-kuukausittain-hallintayksikolle db
-                                                               hallintayksikko-id alkupvm loppupvm
-                                                               (if toimenpide-id true false) toimenpide-id))
+(defn hae-tehtavat-hallintayksikolle [db {:keys [hallintayksikko-id alkupvm loppupvm toimenpide-id urakoittain?]}]
+  (if urakoittain?
+    (q/hae-yksikkohintaiset-tyot-kuukausittain-hallintayksikolle-urakoittain db
+                                                                             hallintayksikko-id alkupvm loppupvm
+                                                                             (if toimenpide-id true false) toimenpide-id)
+    (q/hae-yksikkohintaiset-tyot-kuukausittain-hallintayksikolle db
+                                                                 hallintayksikko-id alkupvm loppupvm
+                                                                 (if toimenpide-id true false) toimenpide-id)))
 
-(defn hae-tehtavat-koko-maalle [db {:keys [alkupvm loppupvm toimenpide-id]}]
-  (q/hae-yksikkohintaiset-tyot-kuukausittain-koko-maalle db
-                                                         alkupvm loppupvm
-                                                         (if toimenpide-id true false) toimenpide-id))
+(defn hae-tehtavat-koko-maalle [db {:keys [alkupvm loppupvm toimenpide-id urakoittain?]}]
+  (if urakoittain?
+    (q/hae-yksikkohintaiset-tyot-kuukausittain-koko-maalle-urakoittain db
+                                                                       alkupvm loppupvm
+                                                                       (if toimenpide-id true false) toimenpide-id)
+    (q/hae-yksikkohintaiset-tyot-kuukausittain-koko-maalle db
+                                                           alkupvm loppupvm
+                                                           (if toimenpide-id true false) toimenpide-id)))
 
 (defn suorita [db user {:keys [urakka-id hallintayksikko-id alkupvm loppupvm toimenpide-id urakoittain?] :as parametrit}]
   (roolit/vaadi-rooli user "tilaajan kayttaja")
@@ -43,37 +51,56 @@
                                                                  {:hallintayksikko-id hallintayksikko-id
                                                                   :alkupvm            alkupvm
                                                                   :loppupvm           loppupvm
-                                                                  :toimenpide-id      toimenpide-id})
+                                                                  :toimenpide-id      toimenpide-id
+                                                                  :urakoittain?       urakoittain?})
                                  :koko-maa
                                  (hae-tehtavat-koko-maalle db
                                                            {:alkupvm       alkupvm
                                                             :loppupvm      loppupvm
-                                                            :toimenpide-id toimenpide-id}))
-        ;; Muutetaan tehtävät vectoriksi mappeja, jossa jokainen tehtävä ensiintyy kerran mapissa ja kuukausittaiset
-        ;; summat esitetään avaimina ("kuukausi/vuosi" on avain ja sen arvo on summa)
-        naytettavat-rivit (mapv (fn [tehtava-nimi]
-                                  (let [taman-tehtavan-rivit (filter #(= (:nimi %) tehtava-nimi)
-                                                                     kuukausittaiset-summat)
-                                        suunniteltu-maara (:suunniteltu_maara (first taman-tehtavan-rivit))
-                                        maara-yhteensa (reduce + (mapv :toteutunut_maara taman-tehtavan-rivit))
-                                        toteumaprosentti (if suunniteltu-maara
-                                                           (fmt/desimaaliluku (float (with-precision 10 (* (/ maara-yhteensa suunniteltu-maara) 100))) 1)
-                                                           "-")
-                                        kuukausittaiset-summat (reduce
-                                                                 (fn [map tehtava]
-                                                                   (assoc map
-                                                                     (pvm/kuukausi-ja-vuosi (c/to-date (t/local-date (:vuosi tehtava) (:kuukausi tehtava) 1)))
-                                                                     (or (:toteutunut_maara tehtava) 0)))
-                                                                 {}
-                                                                 taman-tehtavan-rivit)]
-                                    ;; Kasataan tehtävästä näytettävä rivi
-                                    (-> kuukausittaiset-summat
-                                        (assoc :nimi tehtava-nimi)
-                                        (assoc :yksikko (:yksikko (first taman-tehtavan-rivit)))
-                                        (assoc :suunniteltu_maara suunniteltu-maara)
-                                        (assoc :toteutunut_maara maara-yhteensa)
-                                        (assoc :toteumaprosentti toteumaprosentti))))
-                                (distinct (mapv :nimi kuukausittaiset-summat)))
+                                                            :toimenpide-id toimenpide-id
+                                                            :urakoittain?  urakoittain?}))
+        urakat-set (into #{} (map :urakka_nimi kuukausittaiset-summat))
+        tehtavat-set (into #{} (map :nimi kuukausittaiset-summat))
+        naytettavat-rivit (let [muodosta-tehtava-rivi (fn [tehtava-nimi urakka-nimi]
+                                                        (let [taman-tehtavan-rivit (if urakka-nimi
+                                                                                     (filter #(and (= (:nimi %) tehtava-nimi)
+                                                                                                   (= (:urakka_nimi %) urakka-nimi))
+                                                                                             kuukausittaiset-summat)
+                                                                                     (filter #(= (:nimi %) tehtava-nimi)
+                                                                                             kuukausittaiset-summat))
+                                                              suunniteltu-maara (:suunniteltu_maara (first taman-tehtavan-rivit))
+                                                              maara-yhteensa (reduce + (mapv :toteutunut_maara taman-tehtavan-rivit))
+                                                              toteumaprosentti (if suunniteltu-maara
+                                                                                 (fmt/desimaaliluku (float (with-precision 10 (* (/ maara-yhteensa suunniteltu-maara) 100))) 1)
+                                                                                 "-")
+                                                              kuukausittaiset-summat (reduce
+                                                                                       (fn [map tehtava]
+                                                                                         (assoc map
+                                                                                           (pvm/kuukausi-ja-vuosi (c/to-date (t/local-date (:vuosi tehtava) (:kuukausi tehtava) 1)))
+                                                                                           (or (:toteutunut_maara tehtava) 0)))
+                                                                                       {}
+                                                                                       taman-tehtavan-rivit)]
+                                                          ;; Kasataan tehtävästä näytettävä rivi
+                                                          (-> kuukausittaiset-summat
+                                                              (assoc :urakka_nimi urakka-nimi)
+                                                              (assoc :nimi tehtava-nimi)
+                                                              (assoc :yksikko (:yksikko (first taman-tehtavan-rivit)))
+                                                              (assoc :suunniteltu_maara suunniteltu-maara)
+                                                              (assoc :toteutunut_maara maara-yhteensa)
+                                                              (assoc :toteumaprosentti toteumaprosentti))))]
+                            (if urakoittain?
+                              ;; Käydään jokainen urakka läpi ja muutetaan sen tehtävät vectoriksi mappeja,
+                              ;; jossa jokainen tehtävä ensiintyy kerran mapissa ja kuukausittaiset
+                              ;; summat esitetään avaimina ("kuukausi/vuosi" on avain ja sen arvo on summa).
+                              (flatten (mapv (fn [urakka-nimi]
+                                               (mapv
+                                                 (fn [tehtava] (muodosta-tehtava-rivi tehtava urakka-nimi))
+                                                 tehtavat-set))
+                                             urakat-set))
+                              ;; Sama homma, mutta tarkastellaankin suoraan tehtäviä
+                              (mapv
+                                (fn [tehtava] (muodosta-tehtava-rivi tehtava nil))
+                                tehtavat-set)))
         listattavat-pvmt (take-while (fn [pvm]
                                        ;; Nykyisen iteraation kk ei ole myöhempi kuin loppupvm:n kk
                                        (not (t/after?
@@ -97,7 +124,9 @@
                 :nimi        raportin-nimi}
      [:taulukko {:otsikko otsikko
                  :tyhja   (if (empty? naytettavat-rivit) "Ei raportoitavia tehtäviä.")}
-      (flatten (keep identity [{:leveys "20%" :otsikko "Tehtävä"}
+      (flatten (keep identity [(when urakoittain?
+                                 {:leveys "20%" :otsikko "Urakka"})
+                               {:leveys "20%" :otsikko "Tehtävä"}
                                {:leveys "5%" :otsikko "Yk\u00ADsik\u00ADkö"}
                                (mapv (fn [rivi]
                                        {:otsikko (pvm/kuukausi-ja-vuosi (c/to-date rivi))})
@@ -107,7 +136,9 @@
                                  [{:leveys "5%" :otsikko "Tot-%"}
                                  {:leveys "10%" :otsikko "Suun\u00ADni\u00ADtel\u00ADtu määrä hoi\u00ADto\u00ADkau\u00ADdella"}])]))
       (mapv (fn [rivi]
-              (flatten (keep identity [(:nimi rivi)
+              (flatten (keep identity [(when urakoittain?
+                                         (:urakka_nimi rivi))
+                                       (:nimi rivi)
                                        (:yksikko rivi)
                                        (mapv (fn [pvm]
                                                (or
