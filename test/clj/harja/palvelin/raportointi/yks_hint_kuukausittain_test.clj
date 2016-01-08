64609b4f63ec0be02bd2b500248d905cdad0bc0c
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-08 11:20:23 +0200

    Lisää yks. hint. toteumien kuukausiraportille testejä

---
+++ test/clj/harja/palvelin/raportointi/yks_hint_kuukausittain_test.clj
@@ -0,0 +1,59 @@
+(ns harja.palvelin.raportointi.yksikkohintaiset-tyot-kuukausittain-test
+  (:require [clojure.test :refer :all]
+            [harja.palvelin.komponentit.tietokanta :as tietokanta]
+            [harja.palvelin.palvelut.toimenpidekoodit :refer :all]
+            [harja.palvelin.palvelut.urakat :refer :all]
+            [harja.kyselyt.urakat :as urk-q]
+            [harja.testi :refer :all]
+            [taoensso.timbre :as log]
+            [com.stuartsierra.component :as component]
+            [harja.palvelin.raportointi.raportit.yksikkohintaiset-tyot-kuukausittain :as raportti]
+            [harja.pvm :as pvm]
+            [clj-time.core :as t]
+            [clj-time.coerce :as c]))
+
+(deftest raportin-suoritus-urakalle-toimii
+  (let [vastaus (harja.palvelin.main/with-db  db
+                                              (raportti/suorita
+                                                db
+                                                +kayttaja-jvh+
+                                                {:urakka-id (hae-oulun-alueurakan-2005-2010-id)
+                                                 :alkupvm   (c/to-date (t/local-date 2005 10 10))
+                                                 :loppupvm  (c/to-date (t/local-date 2010 10 10))}))]
+    (is (vector? vastaus))
+    (is (= :raportti (first vastaus)))))
+
+(deftest raportin-suoritus-koko-maalle-toimii
+  (let [vastaus (harja.palvelin.main/with-db  db
+                                              (raportti/suorita
+                                                db
+                                                +kayttaja-jvh+
+                                                {:alkupvm   (c/to-date (t/local-date 2005 10 10))
+                                                 :loppupvm  (c/to-date (t/local-date 2010 10 10))}))]
+    (is (vector? vastaus))
+    (is (= :raportti (first vastaus)))))
+
+(deftest kuukausittaisten-summien-haku-urakalle-palauttaa-arvot-oikealta-aikavalilta
+  (let [vastaus (harja.palvelin.main/with-db  db
+                                              (raportti/hae-kuukausittaiset-summat
+                                                db
+                                                {:konteksti :urakka
+                                                 :urakka-id (hae-oulun-alueurakan-2005-2010-id)
+                                                 :alkupvm   (c/to-date (t/local-date 2000 10 10))
+                                                 :loppupvm  (c/to-date (t/local-date 2030 10 10))}))]
+    (is (not (empty? vastaus)))
+    (is (every? #(and (>= % 2005)
+                      (<= % 2010)) (map :vuosi vastaus)))
+    (is (every? #(and (>= % 1)
+                      (<= % 12)) (map :kuukausi vastaus)))))
+
+(deftest kuukausittaisten-summien-haku-urakalle-ei-palauta-tyhjia-toteumia
+  (let [vastaus (harja.palvelin.main/with-db  db
+                                              (raportti/hae-kuukausittaiset-summat
+                                                db
+                                                {:konteksti :urakka
+                                                 :urakka-id (hae-oulun-alueurakan-2005-2010-id)
+                                                 :alkupvm   (c/to-date (t/local-date 2000 10 10))
+                                                 :loppupvm  (c/to-date (t/local-date 2030 10 10))}))]
+    (is (not (empty? vastaus)))
+    (is (every? #(> % 0) (map :toteutunut_maara vastaus)))))
