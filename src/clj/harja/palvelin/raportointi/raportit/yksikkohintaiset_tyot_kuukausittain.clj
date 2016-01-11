8b4854006e66106503a51969b8090f1f8a070e62
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-11 11:15:47 +0200

    Kavenna yks. hint. kuukausirapsan sarakkeita niin, että suurille numeroille jää enemmän tilaa

--- src/clj/harja/palvelin/raportointi/raportit/yksikkohintaiset_tyot_kuukausittain.clj
+++ src/clj/harja/palvelin/raportointi/raportit/yksikkohintaiset_tyot_kuukausittain.clj
@@ -140,12 +140,12 @@
                  :tyhja   (if (empty? naytettavat-rivit) "Ei raportoitavia tehtäviä.")}
       (flatten (keep identity [(when urakoittain?
                                  {:leveys "20%" :otsikko "Urakka"})
-                               {:leveys "20%" :otsikko "Tehtävä"}
+                               {:leveys "15%" :otsikko "Tehtävä"}
                                {:leveys "5%" :otsikko "Yk\u00ADsik\u00ADkö"}
                                (mapv (fn [rivi]
                                        {:otsikko (pvm/kuukausi-ja-vuosi (c/to-date rivi)) :luokka "grid-kk-sarake"})
                                      listattavat-pvmt)
-                               {:leveys "10%" :otsikko "Määrä yhteensä"}
+                               {:leveys "7%" :otsikko "Mää\u00ADrä yh\u00ADteen\u00ADsä"}
                                (when (= konteksti :urakka)
                                  [{:leveys "5%" :otsikko "Tot-%"}
                                  {:leveys "10%" :otsikko "Suun\u00ADni\u00ADtel\u00ADtu määrä hoi\u00ADto\u00ADkau\u00ADdella"}])]))
