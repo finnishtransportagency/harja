2d936b91e34889d830ab62d09b962c1b823e9911
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-12 13:58:12 +0200

    Tee alustava tuki hakea tiestötarkastuksia kannasta raportille

--- src/clj/harja/palvelin/raportointi/raportit/tiestotarkastus.clj
+++ src/clj/harja/palvelin/raportointi/raportit/tiestotarkastus.clj
@@ -1,7 +1,7 @@
 (ns harja.palvelin.raportointi.raportit.tiestotarkastus
   (:require [harja.kyselyt.urakat :as urakat-q]
             [harja.kyselyt.hallintayksikot :as hallintayksikot-q]
-            [harja.kyselyt.yksikkohintaiset-tyot :as q]
+            [harja.kyselyt.tarkastukset :as tarkastukset-q]
             [harja.kyselyt.toimenpideinstanssit :refer [hae-urakan-toimenpideinstanssi]]
             [harja.fmt :as fmt]
             [harja.pvm :as pvm]
@@ -9,12 +9,52 @@
             [taoensso.timbre :as log]
             [harja.domain.roolit :as roolit]))
 
-(defn suorita [db user {:keys [urakka-id hallintayksikko-id alkupvm loppupvm] :as parametrit}]
+(defn hae-tarkastukset-urakalle [db {:keys [urakka-id alkupvm loppupvm tienumero]}]
+  (tarkastukset-q/hae-urakan-tarkastukset db urakka-id alkupvm loppupvm (not (nil? tienumero)) tienumero true "tiestotarkastus"))
+
+(defn hae-tarkastukset-hallintayksikolle [db {:keys [hallintayksikko-id alkupvm loppupvm tienumero]}]
+  ; TODO Puuttuu
+  []
+  #_(q/hae-yksikkohintaiset-tyot-kuukausittain-hallintayksikolle db
+                                                                 hallintayksikko-id alkupvm loppupvm
+                                                                 (if toimenpide-id true false) toimenpide-id))
+
+(defn hae-tarkastukset-koko-maalle [db {:keys [alkupvm loppupvm tienumero]}]
+  ; TODO Puuttuu
+  []
+  #_(q/hae-yksikkohintaiset-tyot-kuukausittain-koko-maalle db
+                                                           alkupvm loppupvm
+                                                           (if toimenpide-id true false) toimenpide-id))
+
+(defn hae-tiestotarkastukset [db {:keys [konteksti urakka-id hallintayksikko-id alkupvm loppupvm tienumero]}]
+  (case konteksti
+    :urakka
+    (hae-tarkastukset-urakalle db
+                               {:urakka-id urakka-id
+                                :alkupvm   alkupvm
+                                :tienumero tienumero})
+    :hallintayksikko
+    (hae-tarkastukset-hallintayksikolle db
+                                        {:hallintayksikko-id hallintayksikko-id
+                                         :alkupvm            alkupvm
+                                         :loppupvm           loppupvm
+                                         :tienumero          tienumero})
+    :koko-maa
+    (hae-tarkastukset-koko-maalle db
+                                  {:alkupvm   alkupvm
+                                   :loppupvm  loppupvm
+                                   :tienumero tienumero})))
+
+(defn suorita [db user {:keys [urakka-id hallintayksikko-id alkupvm loppupvm tienumero] :as parametrit}]
   (roolit/vaadi-rooli user "tilaajan kayttaja") ; FIXME Selvitä oikeudet
   (let [konteksti (cond urakka-id :urakka
                         hallintayksikko-id :hallintayksikko
                         :default :koko-maa)
-        naytettavat-rivit [1 2 3]
+        naytettavat-rivit (hae-tiestotarkastukset db {:konteksti konteksti
+                                                      :urakka-id urakka-id
+                                                      :alkupvm   alkupvm
+                                                      :loppupvm  loppupvm
+                                                      :tienumero nil})
         raportin-nimi "Tiestötarkastusraportti"
         otsikko (raportin-otsikko
                   (case konteksti
@@ -36,5 +76,14 @@
                                {:leveys "20%" :otsikko "Havainnot"}
                                {:leveys "10%" :otsikko "Kuvanumerot"}]))
       (mapv (fn [rivi]
-              [1 2 3 4 5 6 7 8 9])
+              [(:aika rivi)
+               (:aika rivi)
+               (:tr_numero rivi)
+               (:tr_alkuosa rivi)
+               (:tr_alkuetaisyys rivi)
+               (:tr_loppuetaisyys rivi)
+               (:tr_loppuosa rivi)
+               (:tarkastaja rivi)
+               (:havainnot rivi)
+               (:kuvanumerot rivi)])
             naytettavat-rivit)]]))
