9a4592f25d8ecd0363696423d5b433e035c61de0
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-01-12 15:20:10 +0200

    Näytä erilliskustannukset raportissa

--- src/clj/harja/palvelin/raportointi/raportit/erilliskustannukset.clj
+++ src/clj/harja/palvelin/raportointi/raportit/erilliskustannukset.clj
@@ -3,7 +3,7 @@
   (:require [taoensso.timbre :as log]
             [yesql.core :refer [defqueries]]
             [harja.palvelin.raportointi.raportit.yleinen :refer [raportin-otsikko vuosi-ja-kk vuosi-ja-kk-fmt kuukaudet
-                                                                 pylvaat ei-osumia-aikavalilla-teksti]]
+                                                                 pylvaat ei-osumia-aikavalilla-teksti rivi]]
             [harja.domain.roolit :as roolit]
             [clj-time.coerce :as tc]
             [harja.kyselyt.urakat :as urakat-q]
@@ -26,30 +26,31 @@
 (defn hae-erilliskustannukset-aikavalille
   [db user urakka-annettu? urakka-id
    hallintayksikko-annettu? hallintayksikko-id
-   alkupvm loppupvm]
+   toimenpide-id alkupvm loppupvm]
   (roolit/vaadi-lukuoikeus-urakkaan user urakka-id)
-  (into []
-        (map konv/alaviiva->rakenne)
-        (hae-erilliskustannukset
-          db
-          urakka-annettu? urakka-id
-          hallintayksikko-annettu? hallintayksikko-id
-          alkupvm loppupvm)))
+  (let [kustannukset (hae-erilliskustannukset
+                       db
+                       urakka-annettu? urakka-id
+                       hallintayksikko-annettu? hallintayksikko-id
+                       toimenpide-id
+                       alkupvm loppupvm)]
+    (into []
+          (map konv/alaviiva->rakenne)
+          kustannukset)))
 
-(defn suorita [db user {:keys [urakka-id hallintayksikko-id alkupvm loppupvm] :as parametrit}]
+(defn suorita [db user {:keys [urakka-id hallintayksikko-id toimenpide-id alkupvm loppupvm] :as parametrit}]
   (let [konteksti (cond urakka-id :urakka
                         hallintayksikko-id :hallintayksikko
                         :default :koko-maa)
-        kyseessa-kk-vali? (pvm/kyseessa-kk-vali? alkupvm loppupvm)
-        hoitokauden-alkupvm (first (pvm/paivamaaran-hoitokausi alkupvm))
         urakka-annettu? (if urakka-id true false)
         hallintayksikko-annettu? (if hallintayksikko-id true false)
-        erilliskustannukset (hae-erilliskustannukset-aikavalille db user
-                                                                 urakka-annettu? urakka-id
-                                                                 hallintayksikko-annettu? hallintayksikko-id
-                                                                 alkupvm loppupvm)
+        erilliskustannukset (reverse (sort-by (juxt (comp :id :urakka) :pvm)
+                                              (hae-erilliskustannukset-aikavalille db user
+                                                                                   urakka-annettu? urakka-id
+                                                                                   hallintayksikko-annettu? hallintayksikko-id
+                                                                                   toimenpide-id
+                                                                                   alkupvm loppupvm)))
         _ (log/debug "erilliskustannukset: " erilliskustannukset)
-        _ (log/debug "urakka " urakka-id)
         raportin-nimi "Erilliskustannusten raportti"
         otsikko (raportin-otsikko
                   (case konteksti
@@ -58,67 +59,29 @@
                     :koko-maa "KOKO MAA")
                   raportin-nimi alkupvm loppupvm)
         erilliskustannukset-hyn-mukaan (sort-by #(or (:id (first %)) 100000)
-                                        (seq (group-by :hallintayksikko
-                                                       erilliskustannukset)))
-        nayta-pylvaat? (and (> (count erilliskustannukset) 0)
-                            (not= (vuosi-ja-kk alkupvm) (vuosi-ja-kk loppupvm)))]
+                                                (seq (group-by :hallintayksikko
+                                                               erilliskustannukset)))]
     [:raportti {:nimi raportin-nimi}
      [:taulukko {:otsikko                    otsikko
                  :viimeinen-rivi-yhteenveto? true}
-      [{:leveys "10" :otsikko "Urakka"}
-       {:leveys "7" :otsikko "Pvm"}
-       {:leveys "5" :otsikko "Sop. nro"}
-       {:leveys "10" :otsikko "Toimenpide"}
-       {:leveys "10" :otsikko "Tyyppi"}
-       {:leveys "10" :otsikko "Summa"}
-       {:leveys "15" :otsikko "Ind.korotus"}]
+      (keep identity [(when-not (= konteksti :urakka) {:leveys "10" :otsikko "Urakka"})
+                      {:leveys 7 :otsikko "Pvm"}
+                      {:leveys 8 :otsikko "Sop. nro"}
+                      {:leveys 10 :otsikko "Toimenpide"}
+                      {:leveys 10 :otsikko "Tyyppi"}
+                      {:leveys 7 :otsikko "Summa"}
+                      {:leveys 7 :otsikko "Ind.korotus"}])
 
-      (conj (mapv (juxt (comp :nimi :urakka)
-                        (comp pvm/pvm :pvm)
-                        :sopimus
-                        :toimenpideinstanssi
-                        (comp erilliskustannuksen-nimi :tyyppi)
-                        (comp fmt/euro-opt :rahasumma)
-                        :indeksikorjattuna)
+      (conj (mapv #(rivi (when-not (= konteksti :urakka) (get-in % [:urakka :nimi]))
+                         (pvm/pvm (:pvm %))
+                         (get-in % [:sopimus :sampoid])
+                         (:tpinimi %)
+                         (erilliskustannuksen-nimi (:tyyppi %))
+                         (fmt/euro-opt (:rahasumma %))
+                         (or (fmt/euro-opt (:indeksikorjattuna %)) ""))
                   erilliskustannukset)
-            [nil "Yhteensä" nil nil nil
-             (fmt/euro-opt (reduce + (keep :rahasumma erilliskustannukset)))
-             (fmt/euro-opt (reduce + (keep :indeksikorjattuna erilliskustannukset)))])
-
-      #_(into
-        []
-        (concat
-          (apply concat
-                 ;; Tehdään rivi jokaiselle urakalle, ja näytetään niiden erityyppistem kustannusten määrä
-                 (for [[hy erilliskustannukset] erilliskustannukset-hyn-mukaan]
-                   (concat
-                     [{:otsikko (or (:nimi hy) "Kustannukset ilman urakkaa")}]
-                     (for [[urakka hyn-erilliskustannukset] (group-by :urakka erilliskustannukset)
-                           :let [urakan-nimi (or (:nimi urakka) "Ei urakkaa")
-                                 bonus (:rahasumma (filter #(= "asiakastyytyvaisyysbonus" (:tyyppi %)) hyn-erilliskustannukset))
-                                 muu (:rahasumma (filter #(= "muu" (:tyyppi %)) hyn-erilliskustannukset))]]
-                       [urakan-nimi bonus muu])
-                     ;; lasketaan myös hallintayksiköiden summarivi
-                     (when (= :koko-maa konteksti)
-                       (let [bonus-yht (:rahasumma (filter #(= "asiakastyytyvaisyysbonus" (:tyyppi %)) erilliskustannukset))
-                             muu-yht (:rahasumma (filter #(= "muu" (:tyyppi %)) erilliskustannukset))]
-                         (when (:nimi hy)
-                           [(seq [(str (:nimi hy) " yhteensä") bonus-yht muu-yht])]))))))
-
-          ;; Tehdään yhteensä rivi, jossa kaikki erilliskustannukset lasketaan yhteen materiaalin perusteella
-          (when-not (= :urakka konteksti)
-            (let [bonus-yht (count (filter #(= "asiakastyytyvaisyysbonus" (:tyyppi %)) erilliskustannukset))
-                  muu-yht (count (filter #(= "muu" (:tyyppi %)) erilliskustannukset))]
-              [(concat ["Yhteensä"]
-                       [bonus-yht muu-yht])]))))]
-
-     #_(when nayta-pylvaat?
-       (if-not (empty? erilliskustannukset-kuukausittain-tyyppiryhmiteltyna)
-         (pylvaat {:otsikko              (str "Kustannukset kuukausittain" hoitokaudella-tahan-asti-opt)
-                   :alkupvm              graafin-alkupvm :loppupvm loppupvm
-                   :kuukausittainen-data erilliskustannukset-kuukausittain-tyyppiryhmiteltyna :piilota-arvo? #{0}
-                   :legend               ["Bonus" "Muu"]})
-         (ei-osumia-aikavalilla-teksti "kustannuksia" graafin-alkupvm loppupvm)))
-     ]))
+            (keep identity [(when-not (= konteksti :urakka) "") "Yhteensä" "" "" ""
+                            (fmt/euro-opt (reduce + (keep :rahasumma erilliskustannukset)))
+                            (fmt/euro-opt (reduce + (keep :indeksikorjattuna erilliskustannukset)))]))]]))
 
 
--- src/clj/harja/palvelin/raportointi/raportit/erilliskustannukset.sql
+++ src/clj/harja/palvelin/raportointi/raportit/erilliskustannukset.sql
@@ -3,7 +3,8 @@
 SELECT
   e.id,
   e.tyyppi,
-  e.sopimus,
+  e.urakka as urakka_id,
+  e.sopimus as sopimus_id,
   e.toimenpideinstanssi,
   e.pvm,
   e.rahasumma,
@@ -11,19 +12,20 @@ SELECT
   e.lisatieto,
   e.luotu,
   e.luoja,
-  kuukauden_indeksikorotus(e.pvm, e.indeksin_nimi, e.rahasumma) AS indeksikorjattuna,
-SELECT korotus
-FROM laske_kuukauden_indeksikorotus(kht.vuosi, kht.kuukausi, ind,
-                                    kht.summa, perusluku)) AS ind
+  kuukauden_indeksikorotus(e.pvm, e.indeksin_nimi, e.rahasumma, e.urakka) AS indeksikorjattuna,
   hy.nimi as hallintayksikko_nimi,
   hy.id as hallintayksikko_id,
-  u.nimi as urakka_nimi,
-  u.id as urakka_id
+  s.sampoid as sopimus_sampoid,
+  tpi.nimi as tpinimi,
+  u.nimi as urakka_nimi
 FROM erilliskustannus e
-  LEFT JOIN urakka u ON e.sopimus IN (SELECT id FROM sopimus WHERE urakka = u.id)
+  LEFT JOIN toimenpideinstanssi tpi ON tpi.id = e.toimenpideinstanssi
+  LEFT JOIN sopimus s ON e.sopimus = s.id
+  LEFT JOIN urakka u ON e.urakka = u.id
   LEFT JOIN organisaatio hy ON (u.hallintayksikko = hy.id AND hy.tyyppi = 'hallintayksikko')
 WHERE (:urakka_annettu IS FALSE OR e.sopimus in
                                    (SELECT id FROM sopimus WHERE urakka = :urakka))
       AND (:hallintayksikko_annettu IS FALSE OR
            u.id IN (SELECT id FROM urakka WHERE hallintayksikko = :hallintayksikko))
+      AND (:toimenpide::INTEGER IS NULL OR (tpi.toimenpide = :toimenpide AND e.urakka = tpi.urakka))
       AND e.pvm :: DATE BETWEEN :alku AND :loppu;
