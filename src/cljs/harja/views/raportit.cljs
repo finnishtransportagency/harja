ea07ef81e8d01009f6261dcabf7b809d3e08c757
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-13 11:39:45 +0200

    Lisää alustava tuki raporttien tienumerohaulle

--- src/cljs/harja/views/raportit.cljs
+++ src/cljs/harja/views/raportit.cljs
@@ -160,21 +160,25 @@ Raporttia ei voi suorittaa, jos parametreissä on virheitä"
            nil false (when @vapaa-aikavali?
                        [ui-valinnat/aikavali vapaa-aikavali])]]]))))
 
-     
+(defmethod raportin-parametri "tienumero" [p arvo]
+  (fn [_ _]
+    [valinnat/tienumero (r/wrap nil (fn [uusi]
+                                             (reset! arvo {:tienumero uusi})))]))
+
 (defmethod raportin-parametri "urakan-toimenpide" [p arvo]
   (let [aseta-tpi (fn [tpi]
                     (reset! arvo (if tpi
                                    {:toimenpide-id (:id tpi)}
                                    {:virhe "Ei tpi valintaa"})))]
     (komp/luo
-     (komp/watcher u/valittu-toimenpideinstanssi
-                   (fn [_ _ tpi]
-                     (aseta-tpi tpi)))
-     (komp/piirretty #(reset! u/valittu-toimenpideinstanssi {:tpi_nimi "Kaikki"}))
-     
-     (fn [_ _]
-       @u/valittu-toimenpideinstanssi
-       [valinnat/urakan-toimenpide+kaikki]))))
+      (komp/watcher u/valittu-toimenpideinstanssi
+                    (fn [_ _ tpi]
+                      (aseta-tpi tpi)))
+      (komp/piirretty #(reset! u/valittu-toimenpideinstanssi {:tpi_nimi "Kaikki"}))
+
+      (fn [_ _]
+        @u/valittu-toimenpideinstanssi
+        [valinnat/urakan-toimenpide+kaikki]))))
 
 
 (defonce urakoittain? (atom false))
@@ -228,7 +232,7 @@ Raporttia ei voi suorittaa, jos parametreissä on virheitä"
   {"aikavali" 1
    "urakan-toimenpide" 3})
 
-(def omalle-riville? #{"checkbox" "aikavali" "urakoittain"})
+(def parametri-omalle-riville? #{"checkbox" "aikavali" "urakoittain"})
 
 (defn raportin-parametrit [raporttityyppi konteksti v-ur v-hal]
   (let [parametri-arvot (atom {})]
@@ -268,13 +272,13 @@ Raporttia ei voi suorittaa, jos parametreissä on virheitä"
                   (if-not p
                     (conj rows row)
                     (let [par ^{:key (:nimi p)} [:div
-                                                 {:class (if (omalle-riville? (:tyyppi p))
+                                                 {:class (if (parametri-omalle-riville? (:tyyppi p))
                                                            "col-md-12"
                                                            "col-md-4")}
                                                  [raportin-parametri p arvo]]]
                       (cond
                         ;; checkboxit ja aikaväli aina omalle riville
-                        (omalle-riville? (:tyyppi p))
+                        (parametri-omalle-riville? (:tyyppi p))
                         (recur (conj (if row
                                        (conj rows row)
                                        rows)
