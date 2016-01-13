cc61d3d47b2944a77c96521684442e85d8e8ea19
mikkoro <mikko.ronkkomaki@solita.fi>
2016-01-13 11:57:09 +0200

    Paranna geometriapäivitysten logitusta

--- src/clj/harja/kyselyt/geometriapaivitykset.clj
+++ src/clj/harja/kyselyt/geometriapaivitykset.clj
@@ -1,6 +1,7 @@
 (ns harja.kyselyt.geometriapaivitykset
   "Geometriapäivityksiin liittyvät tietokantakyselyt"
-  (:require [yesql.core :refer [defqueries]]
+  (:require [taoensso.timbre :as log]
+            [yesql.core :refer [defqueries]]
             [harja.pvm :as pvm]
             [clj-time.coerce :as time-coerce]))
 
@@ -9,6 +10,7 @@
 (defn pitaako-paivittaa? [db paivitystunnus tiedoston-muutospvm]
   (let [paivityksen-tiedot (first (harja.kyselyt.geometriapaivitykset/hae-paivitys db paivitystunnus))
         viimeisin-paivitys (:viimeisin_paivitys paivityksen-tiedot)]
+    (log/debug (format "Geometriapäivitys: %s on päivitetty viimeksi: %s. Tiedosto on muuttunut viimeksi: %s " paivitystunnus viimeisin-paivitys tiedoston-muutospvm))
     (or (nil? viimeisin-paivitys)
         (pvm/jalkeen?
           (time-coerce/from-sql-time tiedoston-muutospvm)
