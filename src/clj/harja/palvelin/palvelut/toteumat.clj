bd11de7d907a21f4a7f56e3af9dc7f3621bf5660
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-01-13 12:41:20 +0200

    Korjaa erilliskustannusten testi

--- src/clj/harja/palvelin/palvelut/toteumat.clj
+++ src/clj/harja/palvelin/palvelut/toteumat.clj
@@ -248,18 +248,22 @@
         (q/listaa-urakan-hoitokauden-erilliskustannukset db urakka-id (konv/sql-date alkupvm) (konv/sql-date loppupvm))))
 
 (defn tallenna-erilliskustannus [db user ek]
+  (log/debug "tallenna erilliskustannus:" ek)
   (roolit/vaadi-toteumien-kirjaus-urakkaan user (:urakka-id ek))
-  (jdbc/with-db-transaction [c db]
-    (let [parametrit [c (:tyyppi ek) (:urakka ek) (:sopimus ek) (:toimenpideinstanssi ek)
+  (jdbc/with-db-transaction
+    [c db]
+    (let [parametrit [c (:tyyppi ek) (:urakka-id ek) (:sopimus ek) (:toimenpideinstanssi ek)
                       (konv/sql-date (:pvm ek)) (:rahasumma ek) (:indeksin_nimi ek) (:lisatieto ek) (:id user)]]
       (if (not (:id ek))
         (apply q/luo-erilliskustannus<! parametrit)
-
-        (apply q/paivita-erilliskustannus! (concat parametrit [(or (:poistettu ek) false) (:id ek)]))))
-    (q/merkitse-toimenpideinstanssin-kustannussuunnitelma-likaiseksi! c (:toimenpideinstanssi ek))
-    (hae-urakan-erilliskustannukset c user {:urakka-id (:urakka-id ek)
-                                            :alkupvm   (:alkupvm ek)
-                                            :loppupvm  (:loppupvm ek)})))
+        (apply q/paivita-erilliskustannus! (concat parametrit [(or (:poistettu ek) false) (:id ek)])))
+      (q/merkitse-toimenpideinstanssin-kustannussuunnitelma-likaiseksi! c (:toimenpideinstanssi ek))
+      (log/debug "seuraavkasi haetaan ek:t" {:urakka-id (:urakka-id ek)
+                                             :alkupvm   (:alkupvm ek)
+                                             :loppupvm  (:loppupvm ek)})
+      (hae-urakan-erilliskustannukset c user {:urakka-id (:urakka-id ek)
+                                              :alkupvm   (:alkupvm ek)
+                                              :loppupvm  (:loppupvm ek)}))))
 
 
 (def muut-tyot-rahasumma-xf
