7831f0f991db8707e9d54822bc70a3c3a3f03a4b
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-15 09:44:23 +0200

    Lisää uudet raportit kantaan migraatiossa

---
+++ tietokanta/src/main/resources/db/migration/V1_278__.sql
@@ -0,0 +1,24 @@
+-- Laatupoikkeama- & soratietarkastusraportti
+
+INSERT INTO raportti (nimi, kuvaus, konteksti, parametrit, koodi, urakkatyyppi) VALUES (
+ 'laatupoikkeamaraportti', 'Laatupoikkeamaraportti',
+ ARRAY['urakka'::raporttikonteksti,
+       'hallintayksikko'::raporttikonteksti,
+       'hankinta-alue'::raporttikonteksti,
+       'koko maa'::raporttikonteksti],
+ ARRAY[('Aikaväli', 'aikavali', true, NULL)::raporttiparametri],
+ '#''harja.palvelin.raportointi.raportit.laatupoikkeamaraportti/suorita',
+  'hoito'::urakkatyyppi
+);
+
+INSERT INTO raportti (nimi, kuvaus, konteksti, parametrit, koodi, urakkatyyppi) VALUES (
+ 'soratietarkastusraportti', 'Soratietarkastusraportti',
+ ARRAY['urakka'::raporttikonteksti,
+       'hallintayksikko'::raporttikonteksti,
+       'hankinta-alue'::raporttikonteksti,
+       'koko maa'::raporttikonteksti],
+ ARRAY[('Aikaväli', 'aikavali', true, NULL)::raporttiparametri,
+       ('Tienumero', 'tienumero', false, NULL)::raporttiparametri],
+ '#''harja.palvelin.raportointi.raportit.soratietarkastusraportti/suorita',
+  'hoito'::urakkatyyppi
+);
