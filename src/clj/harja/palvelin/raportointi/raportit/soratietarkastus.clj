de6543ab47ceecde97c15925524a44150c332ddb
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-19 15:13:49 +0200

    Laske soratieraportin mittausten nurkkasumma oikein

--- src/clj/harja/palvelin/raportointi/raportit/soratietarkastus.clj
+++ src/clj/harja/palvelin/raportointi/raportit/soratietarkastus.clj
@@ -223,7 +223,7 @@
                    (str (nth laatuarvo-summat 2) " (" (Math/round (laske-luvun-osuus laatuarvo-summat 2)) "%)")
                    (str (nth laatuarvo-summat 3) " (" (Math/round (laske-luvun-osuus laatuarvo-summat 3)) "%)")
                    (str (nth laatuarvo-summat 4) " (" (Math/round (laske-luvun-osuus laatuarvo-summat 4)) "%)")
-                   (str (reduce + [(first laatuarvo-summat) (second laatuarvo-summat)]) " (100%)")
+                   (str (reduce + laatuarvo-summat) " (100%)")
                    (str laatuarvot-1+2-summa " (" laatuarvot-1+2-osuus "%)")
                    nil]))))]
      ;; Poikkeamien selitykset
