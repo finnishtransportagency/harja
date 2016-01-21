17b91e56a51f293a31286bfa2a5771d0c1a29cef
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-01-21 08:48:09 +0200

    Poista turha oikeustarkastus

--- src/clj/harja/palvelin/raportointi/raportit/soratietarkastus.clj
+++ src/clj/harja/palvelin/raportointi/raportit/soratietarkastus.clj
@@ -186,7 +186,6 @@
                   nil]))))
 
 (defn suorita [db user {:keys [urakka-id hallintayksikko-id alkupvm loppupvm tienumero] :as parametrit}]
-  (roolit/vaadi-rooli user "tilaajan kayttaja")
   (let [konteksti (cond urakka-id :urakka
                         hallintayksikko-id :hallintayksikko
                         :default :koko-maa)
