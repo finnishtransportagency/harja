282a186fee2591ff07783cc39b72e66fc3ac4542
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-01-27 15:19:28 +0200

    Sorttaa laatupoikkeamat pvm:n mukaan

--- src/clj/harja/palvelin/raportointi/raportit/laatupoikkeama.clj
+++ src/clj/harja/palvelin/raportointi/raportit/laatupoikkeama.clj
@@ -96,7 +96,7 @@
        {:leveys 35 :otsikko "Ku\u00ADvaus"}
        {:leveys 25 :otsikko "Liit\u00ADtei\u00ADtä"}]
       (yleinen/ryhmittele-tulokset-raportin-taulukolle
-        laatupoikkeamarivit
+        (sort-by :aika laatupoikkeamarivit)
         :urakka
         (fn [rivi]
           [(pvm/pvm (:aika rivi))
