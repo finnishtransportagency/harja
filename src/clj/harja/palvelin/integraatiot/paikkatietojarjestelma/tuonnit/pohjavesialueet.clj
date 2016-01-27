5d8f4a94af78b8a6559e371e26cc94a30bb61141
mikkoro <mikko.ronkkomaki@solita.fi>
2016-01-27 10:01:03 +0200

    Päivitä pohjavesialueet poistamisen sijasta

--- src/clj/harja/palvelin/integraatiot/paikkatietojarjestelma/tuonnit/pohjavesialueet.clj
+++ src/clj/harja/palvelin/integraatiot/paikkatietojarjestelma/tuonnit/pohjavesialueet.clj
@@ -6,12 +6,15 @@
             [harja.kyselyt.pohjavesialueet :as p]
             [harja.palvelin.integraatiot.paikkatietojarjestelma.tuonnit.shapefile :as shapefile]))
 
-(defn vie-pohjavesialue-entry [db pohjavesialue]
+(defn vie-pohjavesialue [db pohjavesialue]
   (if (:the_geom pohjavesialue)
-    (p/vie-pohjavesialuetauluun! db
-                                 (:urakka_lyh pohjavesialue)
-                                 (:urakka_id pohjavesialue)
-                                 (.toString (:the_geom pohjavesialue)))
+    (let [nimi (:urakka_lyh pohjavesialue)
+          tunnus (:urakka_id pohjavesialue)
+          ulkoinen-id (int (:id pohjavesialue))
+          geometria (.toString (:the_geom pohjavesialue))]
+      (if (p/onko-olemassa-ulkoisella-idlla? db ulkoinen-id)
+        (p/paivita-pohjavesialue! db nimi tunnus geometria ulkoinen-id)
+        (p/luo-pohjavesialue! db nimi tunnus ulkoinen-id geometria)))
     (log/warn "Pohjavesialuetta ei voida tuoda ilman geometriaa. Virheviesti: " (:loc_error pohjavesialue))))
 
 (defn vie-pohjavesialue-kantaan [db shapefile]
@@ -19,9 +22,8 @@
     (do
       (log/debug (str "Tuodaan pohjavesialuetta kantaan tiedostosta " shapefile))
       (jdbc/with-db-transaction [transaktio db]
-        (p/tuhoa-pohjavesialuedata! transaktio)
         (doseq [pohjavesialue (shapefile/tuo shapefile)]
-          (vie-pohjavesialue-entry transaktio pohjavesialue)))
+          (vie-pohjavesialue transaktio pohjavesialue)))
       (p/paivita-pohjavesialueet db)
       (log/debug "Pohjavesialueen tuonti kantaan valmis."))
     (log/debug "Pohjavesialueen tiedostoa ei löydy konfiguraatiosta. Tuontia ei suoriteta.")))
