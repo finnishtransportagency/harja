8c3ff4386213cafb56a8434da1c6fbb5380d9b38
mikkoro <mikko.ronkkomaki@solita.fi>
2016-01-27 14:54:00 +0200

    Lisää kantarajoitteet

--- tietokanta/src/main/resources/db/migration/V1_282__.sql
+++ tietokanta/src/main/resources/db/migration/V1_282__.sql
@@ -1,3 +1,5 @@
 ALTER TABLE silta DROP COLUMN siltaid;
-ALTER TABLE silta ADD COLUMN siltatunnus TEXT;
-ALTER TABLE silta ADD COLUMN siltaid INTEGER;
+ALTER TABLE silta ADD COLUMN siltatunnus TEXT NOT NULL;
+ALTER TABLE silta ADD COLUMN siltaid INTEGER NOT NULL;
+ALTER TABLE silta ADD CONSTRAINT uniikki_siltatunnus UNIQUE (siltatunnus);
+ALTER TABLE silta ADD CONSTRAINT uniikki_siltaid UNIQUE (siltaid);
