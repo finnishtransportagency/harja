4201a28e578be8f739af9849ba25e2eff69d87c7
mikkoro <mikko.ronkkomaki@solita.fi>
2016-01-29 14:54:46 +0200

    Korjaa migraatiot

--- tietokanta/src/main/resources/db/migration/V1_284__.sql
+++ tietokanta/src/main/resources/db/migration/V1_284__.sql
@@ -1,6 +1,16 @@
 ALTER TABLE silta DROP COLUMN siltaid;
 ALTER TABLE silta DROP CONSTRAINT uniikki_siltanro;
-ALTER TABLE silta ADD COLUMN siltatunnus TEXT NOT NULL DEFAULT 'silta';
-ALTER TABLE silta ADD COLUMN siltaid INTEGER NOT NULL DEFAULT 0;
+
+ALTER TABLE silta ADD COLUMN siltatunnus TEXT;
+ALTER TABLE silta ADD COLUMN siltaid INTEGER;
+
+UPDATE silta
+SET siltatunnus = 'silta' || CAST(id AS TEXT), siltaid = id;
+
+ALTER TABLE silta ALTER COLUMN siltatunnus SET NOT NULL;
+ALTER TABLE silta ALTER COLUMN siltaid SET NOT NULL;
+
 ALTER TABLE silta ADD CONSTRAINT uniikki_siltatunnus UNIQUE (siltatunnus);
 ALTER TABLE silta ADD CONSTRAINT uniikki_siltaid UNIQUE (siltaid);
+
+
