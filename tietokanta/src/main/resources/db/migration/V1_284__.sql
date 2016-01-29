6a7764f9c0c2002bbfe2b6933c738d82f70c5e70
mikkoro <mikko.ronkkomaki@solita.fi>
2016-01-29 14:11:41 +0200

    Aseta dummy silta tunnukset

--- tietokanta/src/main/resources/db/migration/V1_284__.sql
+++ tietokanta/src/main/resources/db/migration/V1_284__.sql
@@ -2,5 +2,10 @@ ALTER TABLE silta DROP COLUMN siltaid;
 ALTER TABLE silta DROP CONSTRAINT uniikki_siltanro;
 ALTER TABLE silta ADD COLUMN siltatunnus TEXT NOT NULL;
 ALTER TABLE silta ADD COLUMN siltaid INTEGER NOT NULL;
+
+-- Päivitä dummy id:t aluksi paikalleen
+UPDATE silta
+SET siltatunnus = 'silta' || CAST(id AS TEXT);
+
 ALTER TABLE silta ADD CONSTRAINT uniikki_siltatunnus UNIQUE (siltatunnus);
 ALTER TABLE silta ADD CONSTRAINT uniikki_siltaid UNIQUE (siltaid);
