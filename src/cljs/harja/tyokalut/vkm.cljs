3fde8c8b74eb9865d00c1bfb90dd95665a135543
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2016-02-03 14:07:47 +0200

    Palauta tr-haulle palvelinvirheet

--- src/cljs/harja/tyokalut/vkm.cljs
+++ src/cljs/harja/tyokalut/vkm.cljs
@@ -9,7 +9,7 @@
   "https://testportal.tiehallinto.fi/vkm/")
 
 (defn koordinaatti->trosoite-kahdella [[x1 y1] [x2 y2]]
-  (k/post! :hae-tr-pisteilla {:x1 x1 :y1 y1 :x2 x2 :y2 y2}))
+  (k/post! :hae-tr-pisteilla {:x1 x1 :y1 y1 :x2 x2 :y2 y2} nil true))
 
 (let [juokseva-id (atom 0)]	
   (defn- vkm-kutsu-id []	
