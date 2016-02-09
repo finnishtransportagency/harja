d295cdb3f79d0f50d823f5a6825ac5d0434e7862
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-02-09 15:42:55 +0200

    Päivitä turpon migraatio

--- tietokanta/src/main/resources/db/migration/V1_288__.sql
+++ tietokanta/src/main/resources/db/migration/V1_288__.sql
@@ -1,7 +1,11 @@
 -- Turvallisuuspoikkeaman muutokset (HAR-1743)
 
+CREATE TYPE turvallisuuspoikkeama_luokittelu AS ENUM ('tyotapaturma', 'vaaratilanne', 'turvallisuushavainto');
 CREATE TYPE turvallisuuspoikkeama_vahinkoluokittelu AS ENUM ('henkilovahinko','omaisuusvahinko', 'ymparistovahinko');
 CREATE TYPE turvallisuuspoikkeama_vakavuusaste AS ENUM ('vakava','lievä');
 
+ALTER TABLE turvallisuuspoikkeama ADD COLUMN luokittelu turvallisuuspoikkeama_luokittelu;
 ALTER TABLE turvallisuuspoikkeama ADD COLUMN vahinkoluokittelu turvallisuuspoikkeama_vahinkoluokittelu;
 ALTER TABLE turvallisuuspoikkeama ADD COLUMN vakavuusaste turvallisuuspoikkeama_vakavuusaste;
+
+-- FIXME Pudota poikkeamatyyppi.
