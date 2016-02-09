b9d96a4ce8a4842fcf53bbafc5d892d6fc39cb90
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-02-09 10:22:45 +0200

    Tee testi usean pistetoteuman tallentamiselle

--- test/clj/harja/palvelin/integraatiot/api/pistetoteuman_kirjaus_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/pistetoteuman_kirjaus_test.clj
@@ -17,10 +17,10 @@
 
 (use-fixtures :once jarjestelma-fixture)
 
-(deftest tallenna-pistetoteuma
-  (let [ulkoinen-id (tyokalut/hae-vapaa-toteuma-ulkoinen-id)
+(deftest tallenna-yksittainen-pistetoteuma
+         (let [ulkoinen-id (tyokalut/hae-vapaa-toteuma-ulkoinen-id)
         vastaus-lisays (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/toteumat/piste"] kayttaja portti
-                                                (-> "test/resurssit/api/pistetoteuma.json"
+                                                (-> "test/resurssit/api/pistetoteuma_yksittainen.json"
                                                     slurp
                                                     (.replace "__ID__" (str ulkoinen-id))
                                                     (.replace "__SUORITTAJA_NIMI__" "Tienpesijät Oy")
@@ -34,7 +34,7 @@
 
       ; Päivitetään toteumaa ja tarkistetaan, että se päivittyy
       (let [vastaus-paivitys (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/toteumat/piste"] kayttaja portti
-                                                      (-> "test/resurssit/api/pistetoteuma.json"
+                                                      (-> "test/resurssit/api/pistetoteuma_yksittainen.json"
                                                           slurp
                                                           (.replace "__ID__" (str ulkoinen-id))
                                                           (.replace "__SUORITTAJA_NIMI__" "Peltikoneen Pojat Oy")
@@ -48,3 +48,28 @@
         (u (str "DELETE FROM reittipiste WHERE toteuma = " toteuma-id))
         (u (str "DELETE FROM toteuma_tehtava WHERE toteuma = " toteuma-id))
         (u (str "DELETE FROM toteuma WHERE ulkoinen_id = " ulkoinen-id))))))
+
+(deftest tallenna-usea-pistetoteuma
+         (let [ulkoiset-idt (tyokalut/hae-usea-vapaa-toteuma-ulkoinen-id 2)
+               ulkoinen-id-1 (first ulkoiset-idt)
+               ulkoinen-id-2 (second ulkoiset-idt)
+               vastaus-lisays (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/toteumat/piste"] kayttaja portti
+                                                       (-> "test/resurssit/api/pistetoteuma_monta.json"
+                                                           slurp
+                                                           (.replace "__ID1__" (str ulkoinen-id-1))
+                                                           (.replace "__SUORITTAJA1_NIMI__" "Tienpesijät Oy")
+                                                           (.replace "__TOTEUMA1_TYYPPI__" "yksikkohintainen")
+                                                           (.replace "__ID2__" (str ulkoinen-id-2))
+                                                           (.replace "__SUORITTAJA2_NIMI__" "Tienraivaajat Ry")
+                                                           (.replace "__TOTEUMA2_TYYPPI__" "yksikkohintainen")))]
+           (is (= 200 (:status vastaus-lisays)))
+           (let [toteuma1-id (ffirst (q (str "SELECT id FROM toteuma WHERE ulkoinen_id = " ulkoinen-id-1)))
+                 toteuma1-kannassa (first (q (str "SELECT ulkoinen_id, suorittajan_ytunnus, suorittajan_nimi, tyyppi FROM toteuma WHERE ulkoinen_id = " ulkoinen-id-1)))
+                 toteuma1-tehtava-idt (into [] (flatten (q (str "SELECT id FROM toteuma_tehtava WHERE toteuma = " toteuma1-id))))]
+             (is (= toteuma1-kannassa [ulkoinen-id-1 "8765432-1" "Tienpesijät Oy" "yksikkohintainen"]))
+             (is (= (count toteuma1-tehtava-idt) 1)))
+           (let [toteuma2-id (ffirst (q (str "SELECT id FROM toteuma WHERE ulkoinen_id = " ulkoinen-id-2)))
+                 toteuma2-kannassa (first (q (str "SELECT ulkoinen_id, suorittajan_ytunnus, suorittajan_nimi, tyyppi FROM toteuma WHERE ulkoinen_id = " ulkoinen-id-2)))
+                 toteuma2-tehtava-idt (into [] (flatten (q (str "SELECT id FROM toteuma_tehtava WHERE toteuma = " toteuma2-id))))]
+             (is (= toteuma2-kannassa [ulkoinen-id-2 "8765432-1" "Tienraivaajat Ry" "yksikkohintainen"]))
+             (is (= (count toteuma2-tehtava-idt) 1)))))
