e6279cff47d29806c12489400f4c2a07649033ad
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-02-09 11:27:34 +0200

    Korjaa reittitoteuman testi

--- test/clj/harja/palvelin/integraatiot/api/reittitoteuman_kirjaus_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/reittitoteuman_kirjaus_test.clj
@@ -31,7 +31,7 @@
 
       ; Päivitetään toteumaa ja tarkistetaan, että se päivittyy
       (let [vastaus-paivitys (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/toteumat/reitti"] kayttaja portti
-                                                      (-> "test/resurssit/api/reittitoteuma.json"
+                                                      (-> "test/resurssit/api/rreittitoteuma_yksittainen.json"
                                                           slurp
                                                           (.replace "__ID__" (str ulkoinen-id))
                                                           (.replace "__SUORITTAJA_NIMI__" "Peltikoneen Pojat Oy")))]
