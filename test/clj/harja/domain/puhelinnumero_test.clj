81e9e82062ad4908c696f4e7bc537b9a37fa7683
mikkoro <mikko.ronkkomaki@solita.fi>
2016-02-16 18:37:04 +0200

    Korjaa puhelinnumeron kanonisointi

--- test/clj/harja/domain/puhelinnumero_test.clj
+++ test/clj/harja/domain/puhelinnumero_test.clj
@@ -7,6 +7,8 @@
   (is (= "+358415442083" (puhelinnumero/kanonisoi "0415442083")) "Lokaali muoto muutettiin kansainväliseksi")
   (is (= "+358415442083" (puhelinnumero/kanonisoi "+358-415442083")) "Väliviivat on pudotettu")
   (is (= "+358415442083" (puhelinnumero/kanonisoi "+358 41 544 20 83")) "Välilyönnit on pudotettu")
-  (is (= "+358415442083" (puhelinnumero/kanonisoi "+358 E 41 544 a 20 83")) "Aakkoset on pudotettu"))
+  (is (= "+358415442083" (puhelinnumero/kanonisoi "+358 E 41 544 a 20 83")) "Aakkoset on pudotettu")
+  (is (nil? (puhelinnumero/kanonisoi nil)) "Tyhjä puhelinnumero käsitellään oikein")
+  (is (nil? (puhelinnumero/kanonisoi "")) "Tyhjä puhelinnumero käsitellään oikein"))
 
 
