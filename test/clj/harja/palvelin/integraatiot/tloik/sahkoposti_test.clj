cbc0caa946f18789305e20e9d5ad7b7ab1272262
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-02-17 15:55:52 +0200

    Testaa sähköpostin lähteminen päivystäjälle

--- test/clj/harja/palvelin/integraatiot/tloik/sahkoposti_test.clj
+++ test/clj/harja/palvelin/integraatiot/tloik/sahkoposti_test.clj
@@ -1,6 +1,35 @@
 (ns harja.palvelin.integraatiot.tloik.sahkoposti-test
-  (:require [harja.palvelin.integraatiot.tloik.sahkoposti :as sut]
-            [clojure.test :as t :refer [deftest is]]))
+  (:require [taoensso.timbre :as log]
+            [harja.palvelin.integraatiot.tloik.sahkoposti :as sut]
+            [clojure.test :as t :refer [deftest is use-fixtures]]
+            [harja.testi :refer :all]
+            [harja.sms :refer [feikki-labyrintti]]
+            [harja.jms :refer [feikki-sonja]]
+            [com.stuartsierra.component :as component]
+            [harja.palvelin.integraatiot.sonja.sahkoposti :as sahkoposti]
+            [harja.palvelin.integraatiot.tloik.tyokalut :refer [luo-tloik-komponentti tuo-ilmoitus] :as tloik-apurit]
+            [harja.palvelin.komponentit.sonja :as sonja]
+            [harja.palvelin.integraatiot.sonja.sahkoposti.sanomat :as sahkoposti-sanomat]
+            [harja.kyselyt.yhteyshenkilot :as yhteyshenkilot]))
+
+(def kayttaja "jvh")
+(def jarjestelma-fixture
+  (laajenna-integraatiojarjestelmafixturea
+   kayttaja
+   :sonja (feikki-sonja)
+   :sonja-sahkoposti (component/using
+                      (sahkoposti/luo-sahkoposti "foo@example.com"
+                                                 {:sahkoposti-sisaan-jono "email-to-harja"
+                                                  :sahkoposti-sisaan-kuittausjono "email-to-harja-ack"
+                                                  :sahkoposti-ulos-jono "harja-to-email"
+                                                  :sahkoposti-ulos-kuittausjono "harja-to-email-ack"})
+                      [:sonja :db :integraatioloki])
+   :labyrintti (feikki-labyrintti)
+   :tloik (component/using
+           (luo-tloik-komponentti)
+           [:db :sonja :integraatioloki :klusterin-tapahtumat :sonja-sahkoposti])))
+
+(use-fixtures :each jarjestelma-fixture)
 
 (deftest kuittausviestin-lukeminen
   (let [{:keys [urakka-id ilmoitus-id kuittaustyyppi kommentti]}
@@ -12,3 +41,19 @@
 
 (deftest virheellisen-viestin-lukeminen
   (is (= '(:virhe) (keys (sut/lue-kuittausviesti "#[12/3333 asdasd" "eipä mitään")))))
+
+(deftest tarkista-kuisttauksen-vastaanotto-sahkopostilla
+  (let [{:keys [db sonja]} jarjestelma
+        ilmoitusviesti (atom nil)]
+    (tloik-apurit/tee-testipaivystys)
+    (sonja/kuuntele (:sonja jarjestelma) "harja-to-email" (partial reset! ilmoitusviesti))
+    (sonja/laheta (:sonja jarjestelma) tloik-apurit/+tloik-ilmoitusviestijono+ tloik-apurit/+testi-ilmoitus-sanoma+)
+    (let [saapunut (-> (odota-arvo ilmoitusviesti)
+                       .getText
+                       sahkoposti-sanomat/lue-sahkoposti)
+          vastaanottaja (:vastaanottaja saapunut)]
+      ;; Tarkista että viesti lähtee päivystäjälle
+      (is (= (:otsikko saapunut) "#[4/123456789] Toimenpide­pyyntö"))
+
+      
+      )))
