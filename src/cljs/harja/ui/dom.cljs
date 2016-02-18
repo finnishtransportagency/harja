280279064724271e40512eafff92435f8ca85eb5
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2016-02-18 10:25:45 +0200

    Assertoi että ikonien värit ovat joukossa mahdollisia värejä

--- src/cljs/harja/ui/dom.cljs
+++ src/cljs/harja/ui/dom.cljs
@@ -30,15 +30,25 @@
   [perusnimi]
   (str perusnimi (if ie? ".png" ".svg")))
 
+(defn assertoi-ikonin-vari [vari]
+  (assert #{"keltainen" "lime" "magenta" "musta" "oranssi" "pinkki"
+            "punainen" "sininen" "syaani" "tummansininen" "turkoosi"
+            "vihrea" "violetti"} vari))
+
 (defn sijainti-ikoni
   "Oletukena palautetaan <vari-str> värinen sijainti-ikoni, jolla on musta reuna."
   ([vari-str] (sijainti-ikoni "musta" vari-str))
-  ([tila-str vari-str] (karttakuva (str "images/sijainnit/sijainti-"tila-str"-"vari-str))))
+  ([tila-str vari-str]
+   (assert (#{"vihrea" "punainen" "oranssi" "musta" "harmaa"} tila-str))
+   (assertoi-ikonin-vari vari-str)
+   (karttakuva (str "images/sijainnit/sijainti-"tila-str"-"vari-str))))
 
 (defn nuoli-ikoni [vari-str]
+  (assertoi-ikonin-vari vari-str)
   (karttakuva (str "images/nuolet/nuoli-"vari-str)))
 
 (defn pinni-ikoni [vari-str]
+  (assertoi-ikonin-vari vari-str)
   (karttakuva (str "images/pinnit/pinni-"vari-str)))
 
 (defonce korkeus (atom (-> js/window .-innerHeight)))
