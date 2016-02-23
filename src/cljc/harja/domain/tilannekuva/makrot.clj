7a8cde5e2d9f5540cf3b116b521808452913b90f
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-02-23 13:16:36 +0200

    Partitioi määrittelyt koska arg raja

--- src/cljc/harja/domain/tilannekuva/makrot.clj
+++ src/cljc/harja/domain/tilannekuva/makrot.clj
@@ -1,12 +1,17 @@
 (ns harja.domain.tilannekuva.makrot)
 
 (defmacro maarittele-suodattimet [& sym-nimi-otsikko]
+  (println "SUODATTIMIA: " (count sym-nimi-otsikko))
   (let [id (atom 0)]
     `(do
-       ~@(mapv (fn [[sym nimi otsikko]]
-                 `(def ~sym (harja.domain.tilannekuva/->Suodatin
-                             ~(swap! id inc) ~nimi ~otsikko)))
-               sym-nimi-otsikko)
+       ~@(mapv
+          (fn [sym-nimi-otsikko]
+            `(do ~@(mapv
+                    (fn [[sym nimi otsikko]]
+                      `(def ~sym (harja.domain.tilannekuva/->Suodatin
+                                  ~(swap! id inc) ~nimi ~otsikko)))
+                    sym-nimi-otsikko)))
+          (partition 16 16 [] sym-nimi-otsikko))
        (def ~'suodattimet-idlla
          (into {}
                (map (juxt :id identity))
