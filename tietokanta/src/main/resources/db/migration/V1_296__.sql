b4efc68378164aeae101ebb79c7463e0527901c7
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-02-25 13:44:05 +0200

    Korjaa SQL-funktio hae_seuraava_vapaa_viestinumero poistamalla viittaus poistettu-sarakkeeseen

--- tietokanta/src/main/resources/db/migration/V1_296__.sql
+++ tietokanta/src/main/resources/db/migration/V1_296__.sql
@@ -1,2 +1,13 @@
 -- Pudota suljettu-boolean ilmoitukselta, vanhentunut
 ALTER TABLE ilmoitus DROP COLUMN suljettu;
+
+CREATE OR REPLACE FUNCTION hae_seuraava_vapaa_viestinumero(yhteyshenkilo_id INTEGER)
+  RETURNS INTEGER AS $$
+BEGIN
+  LOCK TABLE paivystajatekstiviesti IN ACCESS EXCLUSIVE MODE;
+  RETURN (SELECT coalesce((SELECT (SELECT max(p.viestinumero)
+                                   FROM paivystajatekstiviesti p
+                                     INNER JOIN ilmoitus i ON p.ilmoitus = i.id
+                                   WHERE yhteyshenkilo = yhteyshenkilo_id)), 0) + 1 AS viestinumero);
+END;
+$$ LANGUAGE plpgsql;
