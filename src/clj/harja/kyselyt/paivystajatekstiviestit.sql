ca506ed64da5bfdea98039e5fb3746603900675d
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-02-25 13:11:07 +0200

    Poista viittaus suljettu-sarakkeeseen paivystajatekstiviesti-kyselyssä

--- src/clj/harja/kyselyt/paivystajatekstiviestit.sql
+++ src/clj/harja/kyselyt/paivystajatekstiviestit.sql
@@ -14,5 +14,4 @@ FROM paivystajatekstiviesti p
   INNER JOIN ilmoitus i ON i.id = p.ilmoitus
 WHERE p.yhteyshenkilo = :yhteyshenkilo AND
       p.viestinumero = :viestinumero AND
-      i.suljettu IS NOT TRUE
 LIMIT 1;
