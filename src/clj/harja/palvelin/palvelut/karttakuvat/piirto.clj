738a5e915b0c0058e6dec019714660f1cbfdb0a2
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-03-01 13:42:30 +0200

    Siirrä piirto omaan ns:ään

---
+++ src/clj/harja/palvelin/palvelut/karttakuvat/piirto.clj
@@ -0,0 +1,32 @@
+(ns harja.palvelin.palvelut.karttakuvat.piirto
+  "Hoitaa karttalla esitettävien asioiden piirtämisen Java Graphics2D
+  piirtoalustaan."
+  (:import (java.awt Color BasicStroke RenderingHints)
+           (java.awt.geom AffineTransform Line2D$Double)))
+
+(def ^:dynamic *px-scale* 1)
+
+(defn px [pikselit]
+  (* *px-scale* pikselit))
+
+(defmulti piirra (fn [_ toteuma alue] (:type alue)))
+
+(defn- aseta-viiva-tyyli [g {:keys [color width dash cap join miter]}]
+  (.setColor g color)
+  (.setStroke g (BasicStroke. (px width)
+                              BasicStroke/CAP_ROUND
+                              BasicStroke/JOIN_MITER)))
+
+(defmethod piirra :viiva [g toteuma {:keys [viivat points ikonit :as viiva]}]
+  (doseq [viiva viivat]
+    (aseta-viiva-tyyli g viiva)
+    (doseq [[[x1 y1] [x2 y2]] (partition 2 1 points)
+          :let [line (Line2D$Double.  x1 y1 x2 y2)]]
+      (.draw g line))))
+
+(defn piirra-karttakuvaan [px-scale g asiat]
+  (binding [*px-scale* px-scale]
+    (doseq [{alue :alue :as asia} asiat
+            :when alue]
+      ;;(println "PIIRRÄ " (:type alue) " ALUE: " (pr-str alue))
+      (piirra g asia alue))))
