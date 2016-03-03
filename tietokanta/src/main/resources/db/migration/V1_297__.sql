f907bef2c769a85d1d75a0a4681719d7baf46423
mikkoro <mikko.ronkkomaki@solita.fi>
2016-03-03 09:03:34 +0200

    Lisää puuttuvia selitteitä

--- tietokanta/src/main/resources/db/migration/V1_297__.sql
+++ tietokanta/src/main/resources/db/migration/V1_297__.sql
@@ -1,3 +1,102 @@
+-- Lisää puuttuvia selitteitä ilmoituksiin
+
+ALTER TYPE ilmoituksenselite
+RENAME TO _ilmoituksenselite_temp;
+
+CREATE TYPE ilmoituksenselite AS ENUM (
+  'tyomaajarjestelyihinLiittyvaIlmoitus',
+  'kuoppiaTiessa',
+  'kelikysely',
+  'soratienKuntoHuono',
+  'saveaTiella',
+  'liikennettaVaarantavaEsteTiella',
+  'irtokiviaTiella',
+  'kevyenLiikenteenVaylaanLiittyvaIlmoitus',
+  'raivausJaKorjaustoita',
+  'auraustarve',
+  'kaivonKansiRikki',
+  'kevyenLiikenteenVaylatOvatLiukkaita',
+  'routaheitto',
+  'avattavatPuomit',
+  'tievalaistusVioittunutOnnettomuudessa',
+  'muuKyselyTaiNeuvonta',
+  'soratienTasaustarve',
+  'tieTaiTienReunaOnPainunut',
+  'siltaanLiittyvaIlmoitus',
+  'polynsidontatarve',
+  'liikennevalotEivatToimi',
+  'kunnossapitoJaHoitotyo',
+  'vettaTiella',
+  'aurausvallitNakemaesteena',
+  'ennakoivaVaroitus',
+  'levahdysalueeseenLiittyvaIlmoitus',
+  'sohjonPoisto',
+  'liikennekeskusKuitannutLoppuneeksi',
+  'muuToimenpidetarve',
+  'hiekoitustarve',
+  'tietOvatJaatymassa',
+  'jaatavaaSadetta',
+  'tienvarsilaitteisiinLiittyvaIlmoitus',
+  'oljyaTiella',
+  'sahkojohtoOnPudonnutTielle',
+  'tieOnSortunut',
+  'tievalaistusVioittunut',
+  'testilahetys',
+  'tievalaistuksenLamppujaPimeana',
+  'virkaApupyynto',
+  'tiemerkintoihinLiittyvaIlmoitus',
+  'tulvavesiOnNoussutTielle',
+  'niittotarve',
+  'kuormaOnLevinnytTielle',
+  'tieOnLiukas',
+  'tiellaOnEste',
+  'harjaustarve',
+  'hoylaystarve',
+  'tietyokysely',
+  'paallystevaurio',
+  'rikkoutunutAjoneuvoTiella',
+  'mustaaJaataTiella',
+  'kevyenLiikenteenVaylillaOnLunta',
+  'hirviaitaVaurioitunut',
+  'korvauskysely',
+  'puitaOnKaatunutTielle',
+  'rumpuunLiittyvaIlmoitus',
+  'lasiaTiella',
+  'liukkaudentorjuntatarve',
+  'alikulkukaytavassaVetta',
+  'tievalaistuksenLamppuPimeana',
+  'kevyenLiikenteenVaylatOvatJaisiaJaLiukkaita',
+  'kuoppa',
+  'toimenpidekysely',
+  'pysakkiinLiittyvaIlmoitus',
+  'nakemaalueenRaivaustarve',
+  'vesakonraivaustarve',
+  'muuttuvatOpasteetEivatToimi',
+  'tievalaistus',
+  'vesiSyovyttanytTienReunaa',
+  'raskasAjoneuvoJumissa',
+  'myrskyvaurioita',
+  'kaidevaurio',
+  'liikennemerkkeihinLiittyvaIlmoitus',
+  'siirrettavaAjoneuvo',
+  'tielleOnVuotanutNestettäLiikkuvastaAjoneuvosta',
+  'tapahtumaOhi',
+  'kevyenLiikenteenVaylatOvatjaatymassa',
+  'tietOvatjaisiäJamarkia'
+);
+
+ALTER TABLE ilmoitus RENAME COLUMN selitteet TO _selitteet;
+
+ALTER TABLE ilmoitus ADD selitteet ilmoituksenselite [];
+
+UPDATE ilmoitus
+SET selitteet = _selitteet :: TEXT :: ilmoituksenselite [];
+
+ALTER TABLE ilmoitus DROP COLUMN _selitteet;
+DROP TYPE _ilmoituksenselite_temp;
+
+-- Luo asiakaspalauteluokille taulu ja aseta arvot sinne
+
 CREATE TABLE asiakaspalauteluokka (
   id        SERIAL PRIMARY KEY,
   nimi      VARCHAR(128),
