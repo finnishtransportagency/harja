082d85c374dfcf5d00cf8d911365a926af232f9f
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-03-10 13:18:57 +0200

    Lisää yksilöintivaatimus (ulkoinen_id, luoja) yhteyshenkilo-tauluun

--- tietokanta/src/main/resources/db/migration/V1_304__.sql
+++ tietokanta/src/main/resources/db/migration/V1_304__.sql
@@ -1,3 +1,7 @@
 -- Lisää päivystys tauluun yksilöintivaatimus
 ALTER TABLE paivystys ADD COLUMN luoja INTEGER;
 ALTER TABLE paivystys ADD CONSTRAINT uniikki_paivystys UNIQUE (ulkoinen_id, luoja);
+
+-- Lisää yhteyshenkilo tauluun yksilöintivaatimus
+ALTER TABLE yhteyshenkilo ADD COLUMN luoja INTEGER;
+ALTER TABLE yhteyshenkilo ADD CONSTRAINT uniikki_yhteyshenkilo UNIQUE (ulkoinen_id, luoja);
