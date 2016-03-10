896ad757abdb9bc8aa46c630c973a1e8e1cbbb1c
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-03-10 13:13:57 +0200

    Lisää ohjeteksti myös yks. hint. työt päivittäin rapsalle

--- src/clj/harja/palvelin/raportointi/raportit/yksikkohintaiset_tyot_paivittain.clj
+++ src/clj/harja/palvelin/raportointi/raportit/yksikkohintaiset_tyot_paivittain.clj
@@ -63,5 +63,6 @@
                   (when (not (empty? naytettavat-rivit))
                     ["Yhteensä" nil nil nil nil nil
                      (fmt/euro-opt false (reduce + (keep :suunnitellut_kustannukset naytettavat-rivit)))
-                     (fmt/euro-opt false (reduce + (keep :toteutuneet_kustannukset naytettavat-rivit)))])))]]))
+                     (fmt/euro-opt false (reduce + (keep :toteutuneet_kustannukset naytettavat-rivit)))])))]
+     (yks-hint-tyot/suunnitelutietojen-nayttamisilmoitus konteksti alkupvm loppupvm suunnittelutiedot)]))
 
