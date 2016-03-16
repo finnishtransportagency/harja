813e7f31102c5f5ace4dce5b106fb963bc9ec3c8
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-03-16 15:47:34 +0200

    Päivitä migraatio, luo tyontekijän ammatin enumit

--- tietokanta/src/main/resources/db/migration/V1_310__.sql
+++ tietokanta/src/main/resources/db/migration/V1_310__.sql
@@ -1,3 +1,41 @@
 ALTER TABLE turvallisuuspoikkeama ADD COLUMN tyontekijanammatti_muu VARCHAR(512);
 UPDATE turvallisuuspoikkeama SET tyontekijanammatti_muu = turvallisuuspoikkeama.tyontekijanammatti;
+
+CREATE TYPE tyontekijanammatti AS ENUM (
+  'aluksen_paallikko',
+  'asentaja',
+  'asfalttityontekija',
+  'harjoittelija',
+  'hitsaaja',
+  'kunnossapitotyontekija',
+  'kansimies',
+  'kiskoilla_liikkuvan_tyokoneen_kuljettaja',
+  'konemies',
+  'kuorma-autonkuljettaja',
+  'liikenteenohjaaja',
+  'mittamies',
+  'panostaja',
+  'perämies',
+  'porari',
+  'rakennustyontekija',
+  'ratatyontekija',
+  'ratatyosta_vastaava',
+  'sukeltaja',
+  'sahkotoiden_ammattihenkilo',
+  'tilaajan_edustaja',
+  'turvalaiteasentaja',
+  'turvamies',
+  'tyokoneen_kuljettaja',
+  'tyonjohtaja',
+  'valvoja',
+  'veneenkuljettaja',
+  'vaylanhoitaja',
+  'muu_tyontekija',
+  'tyomaan_ulkopuolinen'
+);
+
 ALTER TABLE turvallisuuspoikkeama DROP COLUMN tyontekijanammatti;
+ALTER TABLE turvallisuuspoikkeama ADD COLUMN tyontekijanammatti tyontekijanammatti;
+
+
+
