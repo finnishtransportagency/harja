f3c287c569d8799b5963fba87bda87076324a1ef
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-03-17 12:58:17 +0200

    Siirrä Kajaanin urakan testidataa
    Urakan perustiedot on ajettava sisään ennen mm. ilmoituksia, jotta
    linkitys voidaan tehdä. Siirretään testidataa Kajaanin urakasta
    niin että menee ajoissa ajoon. Ei sisältömuutoksia.

--- tietokanta/testidata/urakat.sql
+++ tietokanta/testidata/urakat.sql
@@ -23,3 +23,8 @@ INSERT INTO urakka (sampoid, sopimustyyppi, hallintayksikko, nimi, alkupvm, lopp
 INSERT INTO urakka (sampoid, sopimustyyppi, hallintayksikko, nimi, alkupvm, loppupvm, tyyppi, urakoitsija) VALUES ('4242523-TES7', 'kokonaisurakka'::sopimustyyppi, (SELECT id FROM organisaatio WHERE lyhenne='KAS'), 'Tienpäällystysurakka KAS ELY 1 2015', '2015-10-01','2018-09-30', 'paallystys', (SELECT id FROM organisaatio WHERE ytunnus='0651792-4'));
 
 INSERT INTO urakka (sampoid, sopimustyyppi, hallintayksikko, nimi, alkupvm, loppupvm, tyyppi, urakoitsija, alue) VALUES ('4242599-TES6', 'palvelusopimus'::sopimustyyppi, (SELECT id FROM organisaatio WHERE lyhenne='POP'), 'Tievalaistuksen palvelusopimus 2015-2020', '2015-01-01','2020-09-30', 'valaistus', (SELECT id FROM organisaatio WHERE ytunnus='0651792-4'), ST_GeomFromText('POLYGON((64.609748673276 25.037841796875, 64.84893726357947 25.125732421875, 65.07213008560697 25.400390625, 65.28428227574943 25.960693359374996, 65.31641761810764 26.8505859375, 65.0025821781929 27.61962890625, 64.543718384468 27.509765625, 64.06259063688063 27.344970703125, 64.10580478684241 26.54296875, 64.239820464092 25.598144531249996, 64.41117678357296 25.125732421875, 64.609748673276 25.037841796875))'));
+
+-- Luodaan Kajaanin urakka ja sopimus
+INSERT INTO urakka (sampoid, hallintayksikko, nimi, alkupvm, loppupvm, tyyppi, urakoitsija)
+VALUES ('1245142-KAJ2', (SELECT id FROM organisaatio WHERE lyhenne='POP'), 'Kajaanin alueurakka 2014-2019', '2014-10-01', '2019-09-30', 'hoito',
+        (SELECT id FROM organisaatio WHERE ytunnus='1565583-5'));
