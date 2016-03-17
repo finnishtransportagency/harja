20ea9688453ba75c630b7eda1c53c9d072e9bd81
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-03-17 10:28:38 +0200

    Korjaa pvm-testi

--- test/clj/harja/pvm_test.clj
+++ test/clj/harja/pvm_test.clj
@@ -5,110 +5,112 @@
     [harja.pvm :as pvm]
     [clj-time.core :as t]))
 
+(def nyt (t/now))
+
 (deftest ennen?
   (is (false? (pvm/ennen? nil nil)))
-  (is (false? (pvm/ennen? (t/now) nil)))
-  (is (false? (pvm/ennen? nil (t/now))))
-  (is (false? (pvm/ennen? (t/now) (t/now))))
-  (is (false? (pvm/ennen? (t/plus (t/now) (t/hours 4))
-                          (t/now))))
-  (is (true? (pvm/ennen? (t/now)
-                         (t/plus (t/now) (t/hours 4))))))
+  (is (false? (pvm/ennen? nyt nil)))
+  (is (false? (pvm/ennen? nil nyt)))
+  (is (false? (pvm/ennen? nyt nyt)))
+  (is (false? (pvm/ennen? (t/plus nyt (t/hours 4))
+                          nyt)))
+  (is (true? (pvm/ennen? nyt
+                         (t/plus nyt (t/hours 4))))))
 
 (deftest sama-tai-ennen?
   (is (false? (pvm/sama-tai-ennen? nil nil)))
-  (is (false? (pvm/sama-tai-ennen? (t/now) nil)))
-  (is (false? (pvm/sama-tai-ennen? nil (t/now))))
-  (is (false? (pvm/sama-tai-ennen? (t/plus (t/now) (t/hours 4))
-                                   (t/now)
+  (is (false? (pvm/sama-tai-ennen? nyt nil)))
+  (is (false? (pvm/sama-tai-ennen? nil nyt)))
+  (is (false? (pvm/sama-tai-ennen? (t/plus nyt (t/hours 4))
+                                   nyt
                                    false)))
   (is (true? (pvm/sama-tai-ennen? (t/local-date 2005 10 10)
                                   (t/local-date 2005 10 10))))
   (is (true? (pvm/sama-tai-ennen? (t/local-date-time 2005 10 10 11 11 11)
                                   (t/local-date-time 2005 10 10 11 11 11)
                                   false)))
-  (is (true? (pvm/sama-tai-ennen? (t/plus (t/now) (t/hours 4))
-                                  (t/now))))
-  (is (true? (pvm/sama-tai-ennen? (t/now)
-                                  (t/now))))
-  (is (true? (pvm/sama-tai-ennen? (t/now)
-                                  (t/plus (t/now) (t/hours 4))))))
+  (is (true? (pvm/sama-tai-ennen? (t/plus nyt (t/hours 4))
+                                  nyt)))
+  (is (true? (pvm/sama-tai-ennen? nyt
+                                  nyt)))
+  (is (true? (pvm/sama-tai-ennen? nyt
+                                  (t/plus nyt (t/hours 4))))))
 
 (deftest jalkeen?
   (is (false? (pvm/jalkeen? nil nil)))
-  (is (false? (pvm/jalkeen? (t/now) nil)))
-  (is (false? (pvm/jalkeen? nil (t/now))))
-  (is (false? (pvm/jalkeen? (t/now) (t/now))))
-  (is (false? (pvm/jalkeen? (t/now)
-                            (t/plus (t/now) (t/hours 4)))))
-  (is (true? (pvm/jalkeen? (t/plus (t/now) (t/hours 4))
-                           (t/now)))))
+  (is (false? (pvm/jalkeen? nyt nil)))
+  (is (false? (pvm/jalkeen? nil nyt)))
+  (is (false? (pvm/jalkeen? nyt nyt)))
+  (is (false? (pvm/jalkeen? nyt
+                            (t/plus nyt (t/hours 4)))))
+  (is (true? (pvm/jalkeen? (t/plus nyt (t/hours 4))
+                           nyt))))
 
 (deftest sama-tai-jalkeen?
   (is (false? (pvm/sama-tai-jalkeen? nil nil)))
-  (is (false? (pvm/sama-tai-jalkeen? (t/now) nil)))
-  (is (false? (pvm/sama-tai-jalkeen? nil (t/now))))
-  (is (false? (pvm/sama-tai-jalkeen? (t/now)
-                                     (t/plus (t/now) (t/hours 4))
+  (is (false? (pvm/sama-tai-jalkeen? nyt nil)))
+  (is (false? (pvm/sama-tai-jalkeen? nil nyt)))
+  (is (false? (pvm/sama-tai-jalkeen? nyt
+                                     (t/plus nyt (t/hours 4))
                                      false)))
   (is (true? (pvm/sama-tai-jalkeen? (t/local-date 2005 10 10)
                                      (t/local-date 2005 10 10))))
   (is (true? (pvm/sama-tai-jalkeen? (t/local-date-time 2005 10 10 11 11 11)
                                      (t/local-date-time 2005 10 10 11 11 11)
                                      false)))
-  (is (true? (pvm/sama-tai-jalkeen? (t/now)
-                                     (t/plus (t/now) (t/hours 4))
+  (is (true? (pvm/sama-tai-jalkeen? nyt
+                                     (t/plus nyt (t/hours 4))
                                      true)))
-  (is (true? (pvm/sama-tai-jalkeen? (t/now)
-                                    (t/now))))
-  (is (true? (pvm/sama-tai-jalkeen? (t/plus (t/now) (t/hours 4))
-                                    (t/now)))))
+  (is (true? (pvm/sama-tai-jalkeen? nyt
+                                    nyt)))
+  (is (true? (pvm/sama-tai-jalkeen? (t/plus nyt (t/hours 4))
+                                    nyt))))
 
 (deftest valissa?
-  (is (true? (pvm/valissa? (t/now) (t/now) (t/now))))
-  (is (true? (pvm/valissa? (t/now)
-                           (t/minus (t/now) (t/minutes 5))
-                           (t/plus (t/now) (t/minutes 5)))))
-  (is (true? (pvm/valissa? (t/plus (t/now) (t/minutes 8))
-                           (t/minus (t/now) (t/minutes 5))
-                           (t/plus (t/now) (t/minutes 5)))))
-  (is (true? (pvm/valissa? (t/plus (t/now) (t/minutes 15))
-                            (t/minus (t/now) (t/minutes 5))
-                            (t/plus (t/now) (t/minutes 5))
+  (is (true? (pvm/valissa? nyt nyt nyt)))
+  (is (true? (pvm/valissa? nyt
+                           (t/minus nyt (t/minutes 5))
+                           (t/plus nyt (t/minutes 5)))))
+  (is (true? (pvm/valissa? (t/plus nyt (t/minutes 8))
+                           (t/minus nyt (t/minutes 5))
+                           (t/plus nyt (t/minutes 5)))))
+  (is (true? (pvm/valissa? (t/plus nyt (t/minutes 15))
+                            (t/minus nyt (t/minutes 5))
+                            (t/plus nyt (t/minutes 5))
                             true)))
-  (is (false? (pvm/valissa? (t/plus (t/now) (t/minutes 15))
-                           (t/minus (t/now) (t/minutes 5))
-                           (t/plus (t/now) (t/minutes 5))
+  (is (false? (pvm/valissa? (t/plus nyt (t/minutes 15))
+                           (t/minus nyt (t/minutes 5))
+                           (t/plus nyt (t/minutes 5))
                             false))))
 
 (deftest paivia-valissa-toimii
-  (is (= (pvm/paivia-valissa [(t/now)
-                          (t/plus (t/now) (t/days 5))]
-                         [(t/plus (t/now) (t/days 1))
-                          (t/plus (t/now) (t/days 3))])
+  (is (= (pvm/paivia-valissa [nyt
+                          (t/plus nyt (t/days 5))]
+                         [(t/plus nyt (t/days 1))
+                          (t/plus nyt (t/days 3))])
          2))
-  (is (= (pvm/paivia-valissa [(t/now)
-                          (t/plus (t/now) (t/days 2))]
-                         [(t/plus (t/now) (t/days 1))
-                          (t/plus (t/now) (t/days 5))])
+  (is (= (pvm/paivia-valissa [nyt
+                          (t/plus nyt (t/days 2))]
+                         [(t/plus nyt (t/days 1))
+                          (t/plus nyt (t/days 5))])
          1))
-  (is (= (pvm/paivia-valissa [(t/plus (t/now) (t/days 1))
-                          (t/plus (t/now) (t/days 3))]
-                         [(t/now)
-                          (t/plus (t/now) (t/days 2))])
+  (is (= (pvm/paivia-valissa [(t/plus nyt (t/days 1))
+                          (t/plus nyt (t/days 3))]
+                         [nyt
+                          (t/plus nyt (t/days 2))])
          1))
-  (is (= (pvm/paivia-valissa [(t/plus (t/now) (t/days 1))
-                          (t/plus (t/now) (t/days 3))]
-                         [(t/now)
-                          (t/plus (t/now) (t/days 5))])
+  (is (= (pvm/paivia-valissa [(t/plus nyt (t/days 1))
+                          (t/plus nyt (t/days 3))]
+                         [nyt
+                          (t/plus nyt (t/days 5))])
          2))
-  (is (= (pvm/paivia-valissa [(t/now)
-                          (t/plus (t/now) (t/days 3))]
-                         [(t/plus (t/now) (t/days 5))
-                          (t/plus (t/now) (t/days 10))])
+  (is (= (pvm/paivia-valissa [nyt
+                          (t/plus nyt (t/days 3))]
+                         [(t/plus nyt (t/days 5))
+                          (t/plus nyt (t/days 10))])
          0))
-  (is (= (pvm/paivia-valissa [(t/now)
-                          (t/plus (t/now) (t/days 3))]
-                         [(t/minus (t/now) (t/days 3))
-                          (t/minus (t/now) (t/days 2))])
+  (is (= (pvm/paivia-valissa [nyt
+                          (t/plus nyt (t/days 3))]
+                         [(t/minus nyt (t/days 3))
+                          (t/minus nyt (t/days 2))])
          0)))
