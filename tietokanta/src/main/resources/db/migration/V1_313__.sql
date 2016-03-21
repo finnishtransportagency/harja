c91357aa24863a949b6a2864e51a3bea1b5fb1e2
mikkoro <mikko.ronkkomaki@solita.fi>
2016-03-21 14:53:47 +0200

    Lisää TURI:lle integraatio kantaan

--- tietokanta/src/main/resources/db/migration/V1_313__.sql
+++ tietokanta/src/main/resources/db/migration/V1_313__.sql
@@ -1,3 +1,5 @@
 ALTER TABLE turvallisuuspoikkeama
 ADD COLUMN lahetetty TIMESTAMP,
 ADD COLUMN lahetys_onnistunut BOOLEAN;
+
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('turi', 'laheta-turvallisuuspoikkeama');
