5dd7d3ad0f81fa8057658e7cfd026334586011d7
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-03-22 15:12:15 +0200

    Tee kuvataso tarkastusreiteille

--- src/cljs/harja/tiedot/urakka/laadunseuranta/tarkastukset_kartalla.cljs
+++ src/cljs/harja/tiedot/urakka/laadunseuranta/tarkastukset_kartalla.cljs
@@ -1,6 +1,12 @@
 (ns harja.tiedot.urakka.laadunseuranta.tarkastukset-kartalla
   (:require [harja.ui.kartta.esitettavat-asiat :refer [kartalla-esitettavaan-muotoon]]
-            [harja.tiedot.urakka.laadunseuranta.tarkastukset :as tarkastukset])
+            [harja.tiedot.urakka.laadunseuranta.tarkastukset :as tarkastukset]
+            [harja.tiedot.urakka :as tiedot-urakka]
+            [harja.tiedot.navigaatio :as nav]
+            [harja.ui.openlayers :as openlayers]
+            [harja.ui.kartta.varit.puhtaat :as varit]
+
+            [harja.asiakas.kommunikaatio :as k])
   (:require-macros [harja.atom :refer [reaction<!]]
                    [reagent.ratom :refer [reaction]]
                    [cljs.core.async.macros :refer [go]]))
@@ -19,3 +25,18 @@
                  (filter #(not (nil? (:sijainti %))))
                  (map #(assoc % :tyyppi-kartalla :tarkastus)))))))
 
+(defn- luo-tarkastusreitit-kuvataso [taso-paalla? urakka [alku loppu] tienumero tyyppi]
+  (when taso-paalla?
+    (openlayers/luo-kuvataso
+     :tarkastusreitit #{{:teksti "Tarkastusreitti" :vari varit/harmaa}}
+     "tr" (k/url-parametri {:urakka-id (:id urakka)
+                            :alkupvm alku
+                            :loppupvm loppu
+                            :tienumero tienumero
+                            :tyyppi tyyppi}))))
+(defonce tarkastusreitit
+  (reaction
+   (luo-tarkastusreitit-kuvataso
+    @karttataso-tarkastukset
+    @nav/valittu-urakka @tiedot-urakka/valittu-aikavali
+    @tarkastukset/tienumero @tarkastukset/tarkastustyyppi)))
