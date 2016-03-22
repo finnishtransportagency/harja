a73d58221fe63dd78ca5203e6e0ebb6a0e30c70f
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-03-22 08:11:48 +0200

    Korjaa vanhat migraatiot

--- tietokanta/src/main/resources/db/migration/V1_310__.sql
+++ tietokanta/src/main/resources/db/migration/V1_310__.sql
@@ -1,2 +1,10 @@
--- Turpon seuraukset
-ALTER TABLE turvallisuuspoikkeama ADD COLUMN aiheutuneet_seuraukset VARCHAR(2048);
+-- Uusi turpotyyppi: muu
+ALTER TYPE turvallisuuspoikkeama_luokittelu RENAME TO turvallisuuspoikkeama_luokittelu_;
+CREATE TYPE turvallisuuspoikkeama_luokittelu AS ENUM ('tyotapaturma', 'vaaratilanne', 'turvallisuushavainto', 'muu');
+
+ALTER TABLE turvallisuuspoikkeama RENAME COLUMN tyyppi TO tyyppi_;
+ALTER TABLE turvallisuuspoikkeama ADD COLUMN tyyppi turvallisuuspoikkeama_luokittelu[];
+UPDATE turvallisuuspoikkeama SET tyyppi = tyyppi_ :: text ::turvallisuuspoikkeama_luokittelu[];
+
+ALTER TABLE turvallisuuspoikkeama DROP COLUMN tyyppi_;
+DROP TYPE turvallisuuspoikkeama_luokittelu_;
