1c48e710c9fc8d63d08f47ea42142399dca3b6ec
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-03-24 13:41:58 +0200

    Korjaa typo migraatiossa

--- tietokanta/src/main/resources/db/migration/V1_320__.sql
+++ tietokanta/src/main/resources/db/migration/V1_320__.sql
@@ -7,5 +7,5 @@ ALTER TABLE turvallisuuspoikkeama ADD COLUMN toteuttaja VARCHAR(1024);
 ALTER TABLE turvallisuuspoikkeama ADD COLUMN tilaaja VARCHAR(1024);
 ALTER TABLE turvallisuuspoikkeama ADD COLUMN turvallisuuskoordinaattori_etunimi VARCHAR(1024);
 ALTER TABLE turvallisuuspoikkeama ADD COLUMN turvallisuuskoordinaattori_sukunimi VARCHAR(1024);
-ALTER TABLE turvallisuuspoikkeama ADD COLUMN laatika_etunimi VARCHAR(1024);
+ALTER TABLE turvallisuuspoikkeama ADD COLUMN laatija_etunimi VARCHAR(1024);
 ALTER TABLE turvallisuuspoikkeama ADD COLUMN laatija_sukunimi VARCHAR(1024);
