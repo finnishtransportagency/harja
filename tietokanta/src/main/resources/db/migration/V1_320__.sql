970699d9b4712988d2d9a4409fc32147e64cc7e0
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-03-24 14:06:19 +0200

    Lisää sanktiolajeja AURA-konversiota varten

---
+++ tietokanta/src/main/resources/db/migration/V1_320__.sql
@@ -0,0 +1,20 @@
+-- Lisää unohtuneet luojan (käyttäjä) foreign keyt
+ALTER TABLE paivystys ADD CONSTRAINT paivystys_luoja_fkey FOREIGN KEY (luoja) REFERENCES kayttaja (id);
+ALTER TABLE yhteyshenkilo ADD CONSTRAINT yhteyshenkilo_luoja_fkey FOREIGN KEY (luoja) REFERENCES kayttaja (id);
+ALTER TABLE yhteyshenkilo_urakka ADD CONSTRAINT yhteyshenkilo_urakka_luoja_fkey FOREIGN KEY (luoja) REFERENCES kayttaja (id);
+ALTER TABLE organisaatio ADD CONSTRAINT organisaatio_luoja_fkey FOREIGN KEY (luoja) REFERENCES kayttaja (id);
+
+-- Lisää sanktio tauluun ulkoinen_id ja vaadi uniikki (luoja, ulkoinen_id) (konversioita varten)
+ALTER TABLE sanktio ADD COLUMN ulkoinen_id INTEGER;
+ALTER TABLE sanktio ADD CONSTRAINT uniikki_ulkoinen_sanktio UNIQUE (ulkoinen_id, luoja);
+
+-- Jotta AURA-konversio onnistuu, on varmistettava että nämä mäppäytyvät Harjan sanktiotyypeiksi:
+-- SELECT  DISTINCT  tehtavakokonaisuus from sanktio;
+INSERT
+INTO sanktiotyyppi
+(nimi, sanktiolaji, toimenpidekoodi)
+VALUES
+  ('Muu tuote', ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'muistutus'::sanktiolaji], NULL),
+  ('Talvihoito', ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'muistutus'::sanktiolaji], (SELECT id FROM toimenpidekoodi WHERE koodi='23104')),
+  ('Laatuasiakirjojen, seurantaraprottien yms. vastavien tietojen paikkansa pitämättömyyt', ARRAY['C'::sanktiolaji], NULL),
+  ('Määräpäivän ylitys', ARRAY['C'::sanktiolaji], NULL);
