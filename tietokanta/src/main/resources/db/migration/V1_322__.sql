68c3d9bf971d785dfccce2fde842d964eef9b7a4
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2016-03-29 14:29:26 +0300

    Migraatio: Toteumille TR-kentät

---
+++ tietokanta/src/main/resources/db/migration/V1_322__.sql
@@ -0,0 +1,8 @@
+-- Lisää toteumille TR-tiedot (käytetään kun tallennetaan frontilta)
+
+ALTER TABLE toteuma
+    ADD tr_numero INTEGER,
+    ADD tr_alkuosa INTEGER,
+    ADD tr_alkuetaisyys INTEGER,
+    ADD tr_loppuosa INTEGER,
+    ADD tr_loppuetaisyys INTEGER;
