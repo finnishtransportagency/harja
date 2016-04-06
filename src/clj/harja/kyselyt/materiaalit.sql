b2fe1943a53560e1fd62b2ab9fe84cfc2277df68
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-04-06 10:42:45 +0300

    Lisää urakkatyypin rajaus materiaaliraporttiin.

--- src/clj/harja/kyselyt/materiaalit.sql
+++ src/clj/harja/kyselyt/materiaalit.sql
@@ -132,7 +132,7 @@ FROM toteuma_materiaali
                         AND toteuma.poistettu IS NOT TRUE
                         AND toteuma_materiaali.poistettu IS NOT TRUE
   LEFT JOIN urakka ON urakka.id = toteuma.urakka
-WHERE urakka.hallintayksikko = :hallintayksikko
+WHERE urakka.hallintayksikko = :hallintayksikko AND (:rajaa_urakkatyyppi = FALSE OR urakka.tyyppi = :urakkatyyppi :: urakkatyyppi)
 GROUP BY materiaali_nimi, urakka_nimi, materiaalikoodi.yksikko, toteuma_materiaali.id;
 
 -- name: hae-koko-maan-toteutuneet-materiaalit-raportille
@@ -151,6 +151,7 @@ FROM toteuma_materiaali
                         AND toteuma.poistettu IS NOT TRUE
                         AND toteuma_materiaali.poistettu IS NOT TRUE
   LEFT JOIN urakka ON urakka.id = toteuma.urakka
+  WHERE (:rajaa_urakkatyyppi = FALSE OR urakka.tyyppi = :urakkatyyppi :: urakkatyyppi)
 GROUP BY materiaali_nimi, urakka_nimi, materiaalikoodi.yksikko;
 
 -- name: hae-urakan-toteumat-materiaalille
