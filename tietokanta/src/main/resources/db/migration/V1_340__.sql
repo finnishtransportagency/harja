09171a873a014603892d4e5417270417b628f51c
Marko Lauronen <Marko.Lauronen@solita.fi>
2016-04-14 16:09:29 +0300

    Lisää kesätarkastuksen vakiohavaintoja

---
+++ tietokanta/src/main/resources/db/migration/V1_340__.sql
@@ -0,0 +1,30 @@
+
+INSERT INTO vakiohavainto (nimi, jatkuva, avain) VALUES ('Soratiehavainto', 'true', 'soratie');
+
+INSERT INTO vakiohavainto (nimi, jatkuva, avain)
+   VALUES ('Siltasaumoissa puutteita', 'false', 'siltasaumoissa-puutteita'),
+          ('Siltavaurioita', 'false', 'siltavaurioita'),
+          ('Silta puhdistamatta', 'false', 'silta-puhdistamatta'),
+          ('Oja, kiviä poistamatta', 'false', 'ojat-kivia-poistamatta'),
+          ('Ylijäämämassa tasattu huonosti', 'false', 'ylijaamamassa-tasattu-huonosti'),
+          ('Oja tukossa', 'false', 'oja-tukossa'),
+          ('Luiskavaurio', 'false', 'luiskavaurio'),
+          ('Reunatäyttö puutteellinen', 'false', 'reunataytto-puutteellinen'),
+          ('Reunapalletta', 'false', 'reunapalletta'),
+          ('Istutukset hoitamatta', 'false', 'istutukset-hoitamatta'),
+          ('Liikennetila hoitamatta', 'false', 'liikennetila-hoitamatta'),
+          ('Näkemäalue raivaamatta', 'false', 'nakemaalue-raivaamatta'),
+          ('Niittämättä', 'false', 'niittamatta'),
+          ('Vesakko raivaamatta', 'false', 'vesakko-raivaamatta'),
+          ('Liikennemerkki vinossa', 'false', 'liikennemerkki-vinossa'),
+          ('Reunapaalut vinossa', 'false', 'reunapaalut-vinossa'),
+          ('Reunapaalut likaisia', 'false', 'reunapaalut-likaisia'),
+          ('PL-alue puhdistettava', 'false', 'pl-alue-puhdistettava'),
+          ('PL-alueessa korjattavaa', 'false', 'pl-alue-korjattavaa'),
+          ('Viheralueet hoitamatta', 'false', 'viheralueet-hoitamatta'),
+          ('Rumpu tukossa', 'false', 'rumpu-tukossa'),
+          ('Rumpu liettynyt', 'false', 'rumpu-liettynyt'),
+          ('Rumpu rikki', 'false', 'rumpu-rikki'),
+          ('Kaidevaurio', 'false', 'kaidevaurio'),
+          ('Kiveysvaurio', 'false', 'kiveysvaurio');
+	  
