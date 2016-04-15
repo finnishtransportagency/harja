5efcfe5d9b3dedc39eef3d549583cec33cb324f0
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-04-15 11:58:56 +0300

    Luo kysely sanktioiden haulle

---
+++ src/clj/harja/palvelin/raportointi/raportit/sanktiot.sql
@@ -0,0 +1,29 @@
+-- name: hae-sanktiot
+-- Hakee sanktiot
+SELECT
+  sakkoryhma,
+  maara,
+  indeksi,
+  suorasanktio,
+  st.id          AS sanktiotyyppi_id,
+  st.nimi        AS sanktiotyyppi_nimi,
+  st.sanktiolaji AS sanktiotyyppi_laji,
+  tpi.id         AS toimenpideinstanssi_id,
+  tpi.nimi       AS toimenpideinstanssi_nimi,
+  lp.id          AS laatupoikkeama_id,
+  lp.aika        AS laatupoikkeama_aika,
+  lp.urakka      AS urakka_id
+FROM sanktio s
+  JOIN toimenpideinstanssi tpi ON s.toimenpideinstanssi = tpi.id
+  JOIN sanktiotyyppi st ON s.tyyppi = st.id
+  JOIN laatupoikkeama lp ON s.laatupoikkeama = lp.id
+  JOIN urakka u ON lp.urakka = u.id
+WHERE (:urakka IS NULL OR lp.urakka = :urakka)
+      AND (:hallintayksikko IS NULL OR lp.urakka IN (SELECT id
+                                                     FROM urakka
+                                                     WHERE hallintayksikko =
+                                                           :hallintayksikko))
+      AND (:urakka IS NOT NULL OR
+           (:urakka IS NULL AND (:urakkatyyppi :: urakkatyyppi IS NULL OR
+                                 u.tyyppi = :urakkatyyppi :: urakkatyyppi)))
+      AND lp.aika :: DATE BETWEEN :alku AND :loppu;
