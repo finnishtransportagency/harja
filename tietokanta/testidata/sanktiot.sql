4f0be8c4d0b026f9c1efc551caa35f27e4b53147
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-04-20 10:00:28 +0300

    Lisää sanktioiden testidataa myös Espoolle ja Vantaalle

--- tietokanta/testidata/sanktiot.sql
+++ tietokanta/testidata/sanktiot.sql
@@ -81,3 +81,31 @@ INSERT INTO sanktio (sakkoryhma, maara, perintapvm, indeksi, laatupoikkeama, toi
 
 INSERT INTO laatupoikkeama (kohde, tekija, kasittelytapa, muu_kasittelytapa, paatos, perustelu, tarkastuspiste, luoja, luotu, aika, kasittelyaika, selvitys_pyydetty, selvitys_annettu, urakka, kuvaus, tr_numero, tr_alkuosa, tr_loppuosa, tr_loppuetaisyys, sijainti, tr_alkuetaisyys) VALUES ('Testikohde', 'tilaaja'::osapuoli, 'puhelin'::laatupoikkeaman_kasittelytapa, '', 'hylatty'::laatupoikkeaman_paatostyyppi, 'Ei tässä ole mitään järkeä', 123, 1, NOW(), '2011-10-11 06:06.37', '2011-10-11 06:06.37', false, false, (SELECT id FROM urakka WHERE nimi='Pudasjärven alueurakka 2007-2012'), 'Sanktion sisältävä laatupoikkeama 700', 1, 2, 3, 4, point(418237, 7207744)::GEOMETRY, 5);
 INSERT INTO sanktio (sakkoryhma, maara, perintapvm, indeksi, laatupoikkeama, toimenpideinstanssi, tyyppi, suorasanktio, luoja) VALUES ('B'::sanktiolaji, null, '2016-10-12 06:06.37', 'MAKU 2010', (SELECT id FROM laatupoikkeama WHERE kuvaus = 'Sanktion sisältävä laatupoikkeama 700'), (SELECT id FROM toimenpideinstanssi WHERE nimi = 'Oulu Liikenneympäristön hoito TP 2014-2019'), (SELECT id FROM sanktiotyyppi WHERE nimi = 'Muu tuote'), false, 2);
+
+-----------------------------------------
+-- Vantaan alueurakka 2014-2019
+-----------------------------------------
+
+-- Sakkoryhmä A, Sanktiotyyppi Talvihoito, päätiet
+
+INSERT INTO laatupoikkeama (kohde, tekija, kasittelytapa, muu_kasittelytapa, paatos, perustelu, tarkastuspiste, luoja, luotu, aika, kasittelyaika, selvitys_pyydetty, selvitys_annettu, urakka, kuvaus, tr_numero, tr_alkuosa, tr_loppuosa, tr_loppuetaisyys, sijainti, tr_alkuetaisyys) VALUES ('Testikohde', 'tilaaja'::osapuoli, 'puhelin'::laatupoikkeaman_kasittelytapa, '', 'hylatty'::laatupoikkeaman_paatostyyppi, 'Ei tässä ole mitään järkeä', 123, 1, NOW(), '2015-10-11 06:06.37', '2015-10-11 06:06.37', false, false, (SELECT id FROM urakka WHERE nimi='Vantaan alueurakka 2014-2019'), 'Sanktion sisältävä laatupoikkeama 999', 1, 2, 3, 4, point(418237, 7207744)::GEOMETRY, 5);
+INSERT INTO sanktio (sakkoryhma, maara, perintapvm, indeksi, laatupoikkeama, toimenpideinstanssi, tyyppi, suorasanktio, luoja) VALUES ('A'::sanktiolaji, 1000, '2016-10-12 06:06.37', 'MAKU 2010', (SELECT id FROM laatupoikkeama WHERE kuvaus = 'Sanktion sisältävä laatupoikkeama 999'), (SELECT id FROM toimenpideinstanssi WHERE nimi = 'Oulu Talvihoito TP'), (SELECT id FROM sanktiotyyppi WHERE nimi = 'Talvihoito, päätiet (talvihoitoluokat Is ja I)'), false, 2);
+
+-- Sakkoryhmä B, Sanktiotyyppi Talvihoito, päätiet
+
+INSERT INTO laatupoikkeama (kohde, tekija, kasittelytapa, muu_kasittelytapa, paatos, perustelu, tarkastuspiste, luoja, luotu, aika, kasittelyaika, selvitys_pyydetty, selvitys_annettu, urakka, kuvaus, tr_numero, tr_alkuosa, tr_loppuosa, tr_loppuetaisyys, sijainti, tr_alkuetaisyys) VALUES ('Testikohde', 'tilaaja'::osapuoli, 'puhelin'::laatupoikkeaman_kasittelytapa, '', 'hylatty'::laatupoikkeaman_paatostyyppi, 'Ei tässä ole mitään järkeä', 123, 1, NOW(), '2015-10-11 06:06.37', '2015-10-11 06:06.37', false, false, (SELECT id FROM urakka WHERE nimi='Vantaan alueurakka 2014-2019'), 'Sanktion sisältävä laatupoikkeama 9990', 1, 2, 3, 4, point(418237, 7207744)::GEOMETRY, 5);
+INSERT INTO sanktio (sakkoryhma, maara, perintapvm, indeksi, laatupoikkeama, toimenpideinstanssi, tyyppi, suorasanktio, luoja) VALUES ('B'::sanktiolaji, 666.666, '2016-10-12 06:06.37', 'MAKU 2010', (SELECT id FROM laatupoikkeama WHERE kuvaus = 'Sanktion sisältävä laatupoikkeama 9990'), (SELECT id FROM toimenpideinstanssi WHERE nimi = 'Oulu Talvihoito TP'), (SELECT id FROM sanktiotyyppi WHERE nimi = 'Talvihoito, päätiet (talvihoitoluokat Is ja I)'), false, 2);
+
+-----------------------------------------
+-- Espoon alueurakka 2014-2019
+-----------------------------------------
+
+-- Sakkoryhmä A, Sanktiotyyppi Talvihoito, päätiet
+
+INSERT INTO laatupoikkeama (kohde, tekija, kasittelytapa, muu_kasittelytapa, paatos, perustelu, tarkastuspiste, luoja, luotu, aika, kasittelyaika, selvitys_pyydetty, selvitys_annettu, urakka, kuvaus, tr_numero, tr_alkuosa, tr_loppuosa, tr_loppuetaisyys, sijainti, tr_alkuetaisyys) VALUES ('Testikohde', 'tilaaja'::osapuoli, 'puhelin'::laatupoikkeaman_kasittelytapa, '', 'hylatty'::laatupoikkeaman_paatostyyppi, 'Ei tässä ole mitään järkeä', 123, 1, NOW(), '2015-10-11 06:06.37', '2015-10-11 06:06.37', false, false, (SELECT id FROM urakka WHERE nimi='Espoon alueurakka 2014-2019'), 'Sanktion sisältävä laatupoikkeama 6767', 1, 2, 3, 4, point(418237, 7207744)::GEOMETRY, 5);
+INSERT INTO sanktio (sakkoryhma, maara, perintapvm, indeksi, laatupoikkeama, toimenpideinstanssi, tyyppi, suorasanktio, luoja) VALUES ('A'::sanktiolaji, 1000, '2016-10-12 06:06.37', 'MAKU 2010', (SELECT id FROM laatupoikkeama WHERE kuvaus = 'Sanktion sisältävä laatupoikkeama 6767'), (SELECT id FROM toimenpideinstanssi WHERE nimi = 'Oulu Talvihoito TP'), (SELECT id FROM sanktiotyyppi WHERE nimi = 'Talvihoito, päätiet (talvihoitoluokat Is ja I)'), false, 2);
+
+-- Sakkoryhmä B, Sanktiotyyppi Talvihoito, päätiet
+
+INSERT INTO laatupoikkeama (kohde, tekija, kasittelytapa, muu_kasittelytapa, paatos, perustelu, tarkastuspiste, luoja, luotu, aika, kasittelyaika, selvitys_pyydetty, selvitys_annettu, urakka, kuvaus, tr_numero, tr_alkuosa, tr_loppuosa, tr_loppuetaisyys, sijainti, tr_alkuetaisyys) VALUES ('Testikohde', 'tilaaja'::osapuoli, 'puhelin'::laatupoikkeaman_kasittelytapa, '', 'hylatty'::laatupoikkeaman_paatostyyppi, 'Ei tässä ole mitään järkeä', 123, 1, NOW(), '2015-10-11 06:06.37', '2015-10-11 06:06.37', false, false, (SELECT id FROM urakka WHERE nimi='Espoon alueurakka 2014-2019'), 'Sanktion sisältävä laatupoikkeama 3424', 1, 2, 3, 4, point(418237, 7207744)::GEOMETRY, 5);
+INSERT INTO sanktio (sakkoryhma, maara, perintapvm, indeksi, laatupoikkeama, toimenpideinstanssi, tyyppi, suorasanktio, luoja) VALUES ('B'::sanktiolaji, 666.666, '2016-10-12 06:06.37', 'MAKU 2010', (SELECT id FROM laatupoikkeama WHERE kuvaus = 'Sanktion sisältävä laatupoikkeama 3424'), (SELECT id FROM toimenpideinstanssi WHERE nimi = 'Oulu Talvihoito TP'), (SELECT id FROM sanktiotyyppi WHERE nimi = 'Talvihoito, päätiet (talvihoitoluokat Is ja I)'), false, 2);
