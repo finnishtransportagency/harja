44111ddd3a9559ef5ce1e9b8b274f95ac8d5e667
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-04-22 13:32:07 +0300

    :voi-poistaa? ottaa funktion, ei booleania

--- src/cljs/harja/views/urakka/suunnittelu/muut_tyot.cljs
+++ src/cljs/harja/views/urakka/suunnittelu/muut_tyot.cljs
@@ -68,7 +68,7 @@
                :muutos #(reset! jo-valitut-tehtavat (into #{} (map (fn [rivi]
                                                                      (:tehtava rivi))
                                                                    (vals (grid/hae-muokkaustila %)))))
-               :voi-poistaa? (oikeudet/on-muu-oikeus? "poista" oikeudet/urakat-suunnittelu-muutos-ja-lisatyot (:id @nav/valittu-urakka) @istunto/kayttaja)}
+               :voi-poistaa? (constantly (oikeudet/on-muu-oikeus? "poista" oikeudet/urakat-suunnittelu-muutos-ja-lisatyot (:id @nav/valittu-urakka) @istunto/kayttaja))}
 
               [{:otsikko       "Tehtävä" :nimi :tehtavanimi
                 :jos-tyhja     "Ei valittavia tehtäviä"
