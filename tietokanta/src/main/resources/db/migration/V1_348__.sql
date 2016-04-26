0ca7a300cd77c55716dc2de8ffae5abc4c6f1536
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-04-26 13:20:37 +0300

    Korjaa migraatio

--- tietokanta/src/main/resources/db/migration/V1_348__.sql
+++ tietokanta/src/main/resources/db/migration/V1_348__.sql
@@ -15,8 +15,13 @@ CREATE TABLE yhatiedot (
   muokattu                 TIMESTAMP
 );
 
-ALTER TABLE yhatiedot ADD CONSTRAINT yhatiedot_uniikki_yhatunnus UNIQUE (yhatunnus);
 ALTER TABLE yhatiedot ADD CONSTRAINT yhatiedot_uniikki_yhaid UNIQUE (yhaid);
+
+-- Uudelleennimeä päällystyskohde ja päällystyskohdeosa
+
+ALTER TABLE paallystyskohde RENAME TO yllapitokohde;
+ALTER TABLE paallystyskohdeosa RENAME TO yllapitokohdeosa;
+ALTER TABLE yllapitokohdeosa RENAME COLUMN paallystyskohde TO yllapitokohde;
 
 -- Kohteelle tarvittavat tiedot
 
@@ -26,25 +31,11 @@ ALTER TABLE yllapitokohde ADD COLUMN tr_alkuetaisyys INTEGER;
 ALTER TABLE yllapitokohde ADD COLUMN tr_loppuosa INTEGER;
 ALTER TABLE yllapitokohde ADD COLUMN tr_loppuetaisyys INTEGER;
 
--- Uudelleennimeä päällystyskohde ja päällystyskohdeosa
-
-ALTER TABLE paallystyskohde RENAME TO yllapitokohde;
-ALTER TABLE paallystyskohdeosa RENAME TO yllapitokohdeosa;
-ALTER TABLE yllapitokohdeosa RENAME COLUMN paallystyskohde TO yllapitokohde;
-
 -- Ylläpitokohteelle tieto siitä onko se päällystystä vai paikkausta
 
 CREATE TYPE yllapitokohdetyyppi AS ENUM ('paallystys', 'paikkaus');
 ALTER TABLE yllapitokohde ADD COLUMN tyyppi yllapitokohdetyyppi;
 UPDATE yllapitokohde SET tyyppi = (SELECT tyyppi FROM urakka WHERE id = urakka)::TEXT::yllapitokohdetyyppi;
-
--- Ylläpitokohteelle TR-osoite:
-
-ALTER TABLE yllapitokohde ADD COLUMN tr_numero INTEGER;
-ALTER TABLE yllapitokohde ADD COLUMN tr_alkuosa INTEGER;
-ALTER TABLE yllapitokohde ADD COLUMN tr_alkuetaisyys INTEGER;
-ALTER TABLE yllapitokohde ADD COLUMN tr_loppuosa INTEGER;
-ALTER TABLE yllapitokohde ADD COLUMN tr_loppueosataisyys INTEGER;
 
 -- Ylläpitokohteelle muut puuttuvat YHA-tiedot
 
@@ -53,7 +44,6 @@ ALTER TABLE yllapitokohde ADD COLUMN yhaid INTEGER;
 ALTER TABLE yllapitokohde ADD COLUMN yllapitoluokka INTEGER;
 ALTER TABLE yllapitokohde ADD COLUMN lahetysaika TIMESTAMP;
 
-ALTER TABLE yllapitokohde ADD CONSTRAINT yllapitokohde_uniikki_yhatunnus UNIQUE (yhatunnus);
 ALTER TABLE yllapitokohde ADD CONSTRAINT yllapitokohde_uniikki_yhaid UNIQUE (yhaid);
 
 -- Ylläpitokohdeosalle muut puuttuvat YHA-tiedot
