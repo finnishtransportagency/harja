538a7088c41b9f5d72431e1a7f67520c2643e52d
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-04-26 13:17:26 +0300

    Lisää loput puuttuvat YHA-tiedot kantaan

--- tietokanta/src/main/resources/db/migration/V1_348__.sql
+++ tietokanta/src/main/resources/db/migration/V1_348__.sql
@@ -1,6 +1,6 @@
--- Lisää urakan YHA-tiedoille uusi taulu
+-- YHA:n vaatimat tietomallimuutokset
 
--- FIXME SELVITÄ ARVOJEN TYYPIT XSD-SKEEMASTA
+-- Uusi taulu urakan yhatiedoille, linkittyy urakkaan
 
 CREATE TABLE yhatiedot (
   id                       SERIAL PRIMARY KEY,
@@ -15,16 +15,48 @@ CREATE TABLE yhatiedot (
   muokattu                 TIMESTAMP
 );
 
-ALTER TABLE yhatiedot ADD CONSTRAINT uniikki_yhatunnus UNIQUE (yhatunnus);
-ALTER TABLE yhatiedot  ADD CONSTRAINT uniikki_yhaid UNIQUE (yhaid);
+ALTER TABLE yhatiedot ADD CONSTRAINT yhatiedot_uniikki_yhatunnus UNIQUE (yhatunnus);
+ALTER TABLE yhatiedot ADD CONSTRAINT yhatiedot_uniikki_yhaid UNIQUE (yhaid);
 
--- Uudelleennimeä päällystystaulut
+-- Kohteelle tarvittavat tiedot
+
+ALTER TABLE yllapitokohde ADD COLUMN tr_numero INTEGER;
+ALTER TABLE yllapitokohde ADD COLUMN tr_alkuosa INTEGER;
+ALTER TABLE yllapitokohde ADD COLUMN tr_alkuetaisyys INTEGER;
+ALTER TABLE yllapitokohde ADD COLUMN tr_loppuosa INTEGER;
+ALTER TABLE yllapitokohde ADD COLUMN tr_loppuetaisyys INTEGER;
+
+-- Uudelleennimeä päällystyskohde ja päällystyskohdeosa
 
 ALTER TABLE paallystyskohde RENAME TO yllapitokohde;
 ALTER TABLE paallystyskohdeosa RENAME TO yllapitokohdeosa;
+ALTER TABLE yllapitokohdeosa RENAME COLUMN paallystyskohde TO yllapitokohde;
+
+-- Ylläpitokohteelle tieto siitä onko se päällystystä vai paikkausta
 
 CREATE TYPE yllapitokohdetyyppi AS ENUM ('paallystys', 'paikkaus');
 ALTER TABLE yllapitokohde ADD COLUMN tyyppi yllapitokohdetyyppi;
-ALTER TABLE yllapitokohdeosa RENAME COLUMN paallystyskohde TO yllapitokohde;
-
 UPDATE yllapitokohde SET tyyppi = (SELECT tyyppi FROM urakka WHERE id = urakka)::TEXT::yllapitokohdetyyppi;
+
+-- Ylläpitokohteelle TR-osoite:
+
+ALTER TABLE yllapitokohde ADD COLUMN tr_numero INTEGER;
+ALTER TABLE yllapitokohde ADD COLUMN tr_alkuosa INTEGER;
+ALTER TABLE yllapitokohde ADD COLUMN tr_alkuetaisyys INTEGER;
+ALTER TABLE yllapitokohde ADD COLUMN tr_loppuosa INTEGER;
+ALTER TABLE yllapitokohde ADD COLUMN tr_loppueosataisyys INTEGER;
+
+-- Ylläpitokohteelle muut puuttuvat YHA-tiedot
+
+ALTER TABLE yllapitokohde ADD COLUMN yhatunnus VARCHAR(2048);
+ALTER TABLE yllapitokohde ADD COLUMN yhaid INTEGER;
+ALTER TABLE yllapitokohde ADD COLUMN yllapitoluokka INTEGER;
+ALTER TABLE yllapitokohde ADD COLUMN lahetysaika TIMESTAMP;
+
+ALTER TABLE yllapitokohde ADD CONSTRAINT yllapitokohde_uniikki_yhatunnus UNIQUE (yhatunnus);
+ALTER TABLE yllapitokohde ADD CONSTRAINT yllapitokohde_uniikki_yhaid UNIQUE (yhaid);
+
+-- Ylläpitokohdeosalle muut puuttuvat YHA-tiedot
+
+ALTER TABLE yllapitokohdeosa ADD COLUMN yhaid INTEGER;
+ALTER TABLE yllapitokohdeosa ADD CONSTRAINT yllapitokohdeosa_uniikki_yhaid UNIQUE (yhaid);
