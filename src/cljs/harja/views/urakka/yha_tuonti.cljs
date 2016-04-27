3d902ee42ef65dbd13cff471175fb6c849115d79
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-04-27 11:57:52 +0300

    Rakenna tukea YHA-haulle

--- src/cljs/harja/views/urakka/yha_tuonti.cljs
+++ src/cljs/harja/views/urakka/yha_tuonti.cljs
@@ -3,24 +3,39 @@
             [harja.ui.modal :as modal]
             [harja.ui.yleiset :refer [ajax-loader vihje]]
             [harja.loki :refer [log logt tarkkaile!]]
+            [cljs.core.async :refer [<! >! chan timeout]]
             [harja.ui.lomake :refer [lomake]]
-            [harja.ui.grid :refer [grid]]))
+            [harja.ui.grid :refer [grid]])
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [harja.atom :refer [reaction<!]]
+                   [reagent.ratom :refer [reaction]]))
 
 (def hakutiedot (atom nil))
-(def hakutulokset (atom []))
-(def hakutulokset-testidata (atom [{:tunnus "YHA1"
-                          :nimi "YHA-urakka"
-                          :elyt "Pohjois-Pohjanmaa"
-                          :vuodet "2005"}]))
+
+(tarkkaile! "[YHA] Hakutiedot " hakutiedot)
+
+(defn hae-yha-urakat [hakuparametrit]
+  ;; TODO
+  (log "[YHA] Suoritetaan YHA-haku")
+  (go []))
+
+(def hakutulokset
+  (reaction<! [hakutiedot @hakutiedot]
+              {:nil-kun-haku-kaynnissa? true
+               :odota 500}
+              (hae-yha-urakat hakutiedot)))
 
 (defn- sido-yha-urakka-harja-urakkaan [yha-urakka harja-urakka]
   ;; TODO
   (log "[YHA] Sidotaan YHA-urakka Harja-urakkaan..."))
 
 (defn- tuontidialogi [optiot]
+  (log "[YHA] Render dialog tiedoilla:" (pr-str @hakutiedot))
   [:div
    [vihje "Urakka tätyy sitoa YHA:n vastaavaan urakkaan tietojen siirtämiseksi Harjaan. Etsi YHA-urakka ja tee sidonta."]
-   [lomake {:otsikko "Urakan tiedot"}
+   [lomake {:otsikko "Urakan tiedot"
+            :muokkaa! (fn [uusi-data]
+                        (reset! hakutiedot uusi-data))}
     [{:otsikko "Nimi"
       :nimi :nimi
       :pituus-max 512
@@ -60,11 +75,15 @@
       :komponentti (fn [rivi]
                      [:button.nappi-ensisijainen.nappi-grid
                       {:on-click #(sido-yha-urakka-harja-urakkaan nil nil)}
-                        "Valitse"])}]
-    @hakutulokset-testidata]])
+                      "Valitse"])}]
+    @hakutulokset]])
 
 (defn nayta-tuontidialogi []
   (modal/nayta!
     {:otsikko "Urakan sitominen YHA-urakkaan"
-     :luokka "yha-tuonti"}
+     :luokka "yha-tuonti"
+     :footer  [:button.nappi-toissijainen {:on-click (fn [e]
+                                                       (.preventDefault e)
+                                                       (modal/piilota!))}
+               "Sulje"]}
     (tuontidialogi {})))
