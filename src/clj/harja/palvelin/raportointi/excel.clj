9617c5cd0475c24465bd8399c2cee787640ada77
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-04-27 10:14:56 +0300

    Paranna Excel vientiä
    Excel viennissä poistetaan soft-hyphen merkit. Otsikottomat taulukot
    jatkavat jo olemassaolevaa sheettiä. Sheetin nimen voi asettaa taulukon
    :sheet-nimi optiolla. Sarakkeiden koot automaattisesti sisällön mukaan.

--- src/clj/harja/palvelin/raportointi/excel.clj
+++ src/clj/harja/palvelin/raportointi/excel.clj
@@ -1,8 +1,9 @@
 (ns harja.palvelin.raportointi.excel
   "Harja raporttielementtien vienti Excel muotoon"
   (:require [taoensso.timbre :as log]
-            [dk.ative.docjure.spreadsheet :as excel])
-  (:import (org.apache.poi.ss.util CellReference)))
+            [dk.ative.docjure.spreadsheet :as excel]
+            [clojure.string :as str])
+  (:import (org.apache.poi.ss.util CellReference WorkbookUtil)))
 
 (defmulti muodosta-excel
   "Muodostaa Excel data annetulle raporttielementille.
@@ -31,26 +32,37 @@
                         (solu rivi-nro (dec sarake-nro))
                         ")")))
 
+
+(defn- ilman-soft-hyphenia [data]
+  (if (string? data)
+    (.replace data "\u00AD" "")
+    data))
+
 (defmethod muodosta-excel :taulukko [[_ optiot sarakkeet data] workbook]
   (try
-    (let [sheet (excel/add-sheet! workbook (:otsikko optiot))
+    (let [nimi (:otsikko optiot)
+          aiempi-sheet (last (excel/sheet-seq workbook))
+          [sheet nolla] (if (and (nil? nimi) aiempi-sheet)
+                          [aiempi-sheet (+ 2 (.getLastRowNum aiempi-sheet))]
+                          [(excel/add-sheet! workbook
+                                             (WorkbookUtil/createSafeSheetName
+                                              (or (:sheet-nimi optiot) nimi))) 0])
           sarake-tyyli (excel/create-cell-style! workbook {:background :blue
                                                            :font {:color :white}})
-          otsikko-rivi (.createRow sheet 0)]
+          otsikko-rivi (.createRow sheet nolla)]
       ;; Luodaan otsikot saraketyylillä
       (dorun
        (map-indexed
         (fn [sarake-nro {:keys [otsikko] :as sarake}]
-          (log/info "saraketta taulukkoon " sarake)
           (let [cell (.createCell otsikko-rivi sarake-nro)]
-            (excel/set-cell! cell otsikko)
+            (excel/set-cell! cell (ilman-soft-hyphenia otsikko))
             (excel/set-cell-style! cell sarake-tyyli)))
         sarakkeet))
 
       (dorun
        (map-indexed
         (fn [rivi-nro rivi]
-          (let [rivi-nro (inc rivi-nro)
+          (let [rivi-nro (+ nolla 1 rivi-nro)
                 [data optiot] (if (map? rivi)
                                 [(:rivi rivi) rivi]
                                 [rivi {}])
@@ -65,16 +77,21 @@
 
                   (if-let [kaava (:excel sarake)]
                     (aseta-kaava! kaava cell rivi-nro sarake-nro)
-                    (excel/set-cell! cell (nth data sarake-nro)))
+                    (excel/set-cell! cell (ilman-soft-hyphenia (nth data sarake-nro))))
                   (excel/set-cell-style! cell tyyli)))
               sarakkeet))))
-        data)))
+        data))
+
+      ;; Laitetaan automaattiset leveydet
+      (dotimes [i (count sarakkeet)]
+        (.autoSizeColumn sheet i)))
     (catch Throwable t
       (log/error t "Virhe Excel muodostamisessa"))))
 
 (defmethod muodosta-excel :raportti [[_ raportin-tunnistetiedot & sisalto] workbook]
-  (doseq [elementti sisalto]
-    (muodosta-excel elementti workbook))
+  (let [sisalto (mapcat #(if (seq? %) % [%]) sisalto)]
+    (doseq [elementti sisalto]
+      (muodosta-excel elementti workbook)))
   (:nimi raportin-tunnistetiedot))
 
 (defmethod muodosta-excel :default [elementti workbook]
