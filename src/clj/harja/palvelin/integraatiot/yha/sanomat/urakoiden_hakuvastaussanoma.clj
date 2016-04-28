03d42efef086bc0a7c9e0ea75fe924bd482072db
mikkoro <mikko.ronkkomaki@solita.fi>
2016-04-28 15:28:15 +0300

    Lue YHA:n urakoiden hakuvastaus

--- src/clj/harja/palvelin/integraatiot/yha/sanomat/urakoiden_hakuvastaussanoma.clj
+++ src/clj/harja/palvelin/integraatiot/yha/sanomat/urakoiden_hakuvastaussanoma.clj
@@ -5,15 +5,21 @@
 
 (def +xsd-polku+ "xsd/yha/")
 
-#_(defn lue-kuittaus [viesti]
+(defn lue-urakat [data]
+  (mapv (fn [urakka]
+          (hash-map :yha-id (z/xml1-> urakka :yha:yha-id z/text)
+                    :elyt (mapv #(z/xml1-> % :yha:ely z/text) (z/xml-> urakka :yha:elyt))
+                    :vuodet (mapv #(z/xml1-> % :yha:vuosi z/text) (z/xml-> urakka :yha:vuodet))
+                    :tunnus (z/xml1-> urakka :yha:tunnus z/text)
+                    :sampotunnus (z/xml1-> urakka :yha:sampotunnus z/text)))
+        (z/xml-> data :yha:urakat :yha:urakka)))
+
+(defn lue-virhe [data]
+  (z/xml1-> data :yha:virhe z/text))
+
+(defn lue-sanoma [viesti]
   (when (not (xml/validoi +xsd-polku+ "yha.xsd" viesti))
     (throw (new RuntimeException "XML-sanoma ei ole XSD-skeeman yha.xsd mukaan validi.")))
-  (let [data (xml/lue viesti)
-        urakat (mapv (fn [urakka]
-                       (hash-map :yha-id (z/xml1-> urakka :yha:yha-id z/text)
-                                 :elyt (mapv #(z/xml1-> % :yha:ely z/text) (z/xml-> urakka :yha:elyt))
-                                 :vuodet (mapv #(z/xml1-> % :yha:vuosi z/text) (z/xml-> urakka :yha:vuodet))
-                                 :tunnus (z/xml1-> urakka :yha:tunnus z/text)
-                                 :nimi (z/xml1-> urakka :yha:nimi z/text)))
-                     (z/xml-> homma :yha:urakat :yha:urakka))]
-    urakat)) ;; FIXME Ei käänny, unable to resolve symbol homma.
+  (let [data (xml/lue viesti)]
+    {:urakat (lue-urakat data)
+     :virhe (lue-virhe data)}))
