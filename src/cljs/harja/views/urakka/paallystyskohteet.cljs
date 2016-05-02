4b9bf12041a58d36661b2a57d850db783437c3de
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-05-02 12:05:30 +0300

    Lisää alustava UI kohdeluettelon päivitykselle & uusi oikeus sitoa paikkausurakka

--- src/cljs/harja/views/urakka/paallystyskohteet.cljs
+++ src/cljs/harja/views/urakka/paallystyskohteet.cljs
@@ -2,7 +2,7 @@
   "Päällystyskohteet"
   (:require [reagent.core :refer [atom] :as r]
             [harja.ui.yleiset :refer [ajax-loader linkki raksiboksi
-                                      livi-pudotusvalikko]]
+                                      livi-pudotusvalikko tietoja]]
             [harja.tiedot.urakka.paallystys :as paallystys]
             [harja.loki :refer [log logt tarkkaile!]]
             [cljs.core.async :refer [<!]]
@@ -10,12 +10,14 @@
             [harja.ui.komponentti :as komp]
             [harja.views.kartta :as kartta]
             [harja.ui.komponentti :as komp]
-            [harja.views.kartta :as kartta])
+            [harja.views.kartta :as kartta]
+            [harja.domain.oikeudet :as oikeudet]
+            [harja.tiedot.istunto :as istunto])
   (:require-macros [reagent.ratom :refer [reaction]]
                    [cljs.core.async.macros :refer [go]]
                    [harja.atom :refer [reaction<!]]))
 
-(defn paallystyskohteet []
+(defn paallystyskohteet [ur]
   (komp/luo
     (komp/ulos #(kartta/poista-popup!))
     (komp/lippu paallystys/paallystyskohteet-nakymassa?)
@@ -25,4 +27,11 @@
        [yllapitokohteet-view/yllapitokohteet paallystys/paallystyskohteet {:otsikko "Päällystyskohteet"
                                                                            :paallystysnakyma? true
                                                                            :tallenna :ei-mahdollinen}]
-       [yllapitokohteet-view/yllapitokohteet-yhteensa paallystys/paallystyskohteet {:paallystysnakyma? true}]])))
+       [yllapitokohteet-view/yllapitokohteet-yhteensa paallystys/paallystyskohteet {:paallystysnakyma? true}]
+
+       [:div.kohdeluettelon-paivitys
+        [:button.nappi-ensisijainen {:on-click #()
+                                    :disabled (oikeudet/on-muu-oikeus? "sido" oikeudet/urakat-kohdeluettelo-paallystyskohteet (:id ur) @istunto/kayttaja)}
+        "Päivitä kohdeluettelo"]
+        ; FIXME Milloin päivitetty
+       [:div "Kohdeluettelo päivitetty: Ei koskaan"]]])))
