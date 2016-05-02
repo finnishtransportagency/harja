d8ef37bb62ecc3f49ac3011ac328a22238af062d
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2016-05-02 13:46:31 +0300

    Lisää laatupoikkeamalle myös käsittelytapa, -aika, päätös ja muu_tapa

--- tietokanta/src/main/resources/db/migration/V1_351__.sql
+++ tietokanta/src/main/resources/db/migration/V1_351__.sql
@@ -1,17 +1,27 @@
 UPDATE laatupoikkeama
-  SET kasittelyaika = muokattu
-WHERE kasittelyaika IS NULL AND paatos IS NOT NULL;
-
-UPDATE laatupoikkeama
-  SET paatos = 'ei_sanktiota'::laatupoikkeaman_paatostyyppi,
-      perustelu = 'Tämä laatupoikkeama on tallennettu virheellisesti ilman päätöstä. Päätökseksi on asetettu koneellisesti ei sanktiota.'
-WHERE paatos IS NULL AND kasittelyaika IS NOT NULL AND perustelu IS NULL;
-
-UPDATE laatupoikkeama
   SET paatos = 'ei_sanktiota'::laatupoikkeaman_paatostyyppi
-WHERE paatos IS NULL AND kasittelyaika IS NOT NULL;
+WHERE paatos IS NULL AND
+      kasittelyaika IS NOT NULL OR
+      perustelu IS NOT NULL OR
+      kasittelytapa IS NOT NULL;
+
+UPDATE laatupoikkeama
+  SET kasittelyaika = muokattu
+WHERE kasittelyaika IS NULL
+      AND paatos IS NOT NULL;
+
+UPDATE laatupoikkeama
+SET perustelu = 'Tämä laatupoikkeama on tallennettu virheellisesti ilman perustelua. Tämä on koneellisesti asetettu perusteluteksti.'
+WHERE perustelu IS NULL
+      AND paatos IS NOT NULL;
+
+UPDATE laatupoikkeama
+SET kasittelytapa = 'muu'::laatupoikkeaman_kasittelytapa,
+    muu_kasittelytapa = 'Puuttuvat tiedot täydennetty koneellisesti'
+WHERE kasittelytapa IS NULL
+      AND paatos IS NOT NULL;
 
 ALTER TABLE laatupoikkeama
     ADD CONSTRAINT kaikki_sanktiotiedot CHECK
-((paatos IS NULL AND perustelu IS NULL AND kasittelyaika IS NULL) OR
- (paatos IS NOT NULL AND kasittelyaika IS NOT NULL));
+((paatos IS NULL AND kasittelyaika IS NULL AND perustelu IS NULL AND kasittelytapa IS NULL) OR
+ (paatos IS NOT NULL AND kasittelyaika IS NOT NULL AND perustelu IS NOT NULL AND kasittelytapa IS NOT NULL));
