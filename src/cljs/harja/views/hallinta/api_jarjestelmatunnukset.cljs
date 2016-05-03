1240074763880080abe535f7bd46d207aabdb6af
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-05-03 14:43:32 +0300

    API järjestelmätunnuslistauksen sommittelua

--- src/cljs/harja/views/hallinta/api_jarjestelmatunnukset.cljs
+++ src/cljs/harja/views/hallinta/api_jarjestelmatunnukset.cljs
@@ -1,5 +1,26 @@
 (ns harja.views.hallinta.api-jarjestelmatunnukset
-  "Harja API:n järjestelmätunnuksien listaus ja muokkaus.")
+  "Harja API:n järjestelmätunnuksien listaus ja muokkaus."
+  (:require [harja.ui.grid :as grid]
+            [harja.pvm :as pvm]))
 
 (defn api-jarjestelmatunnukset []
-  [:div "coming soon to a Harja near you"])
+
+  [grid/grid {:otsikko "API järjestelmätunnukset"}
+   [{:otsikko "Tunnus"
+     :nimi :tunnus}
+    {:otsikko "Nimi"
+     :nimi :nimi}
+    {:otsikko "Urakoitsija"
+     :nimi :urakoitsija}
+    {:otsikko "Urakat"
+     :nimi :urakat}
+    {:otsikko "Luotu"
+     :nimi :luotu
+     :tyyppi :pvm
+     :fmt pvm/pvm-aika}
+    ]
+
+   [{:tunnus "Foo" :urakoitsija "Barsky" :urakat "Oulun au, Jokumuu HJU"
+     :nimi "Softatoimittaja X" :luotu (pvm/luo-pvm 2016 4 2)}]
+   ]
+  )
