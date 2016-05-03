460f0b6ee77c97d63e04130abc9c37b829769b73
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-05-03 13:52:24 +0300

    Lisää API-järjestelmätunnukset osio Hallintaan

---
+++ src/cljs/harja/views/hallinta/api_jarjestelmatunnukset.cljs
@@ -0,0 +1,5 @@
+(ns harja.views.hallinta.api-jarjestelmatunnukset
+  "Harja API:n järjestelmätunnuksien listaus ja muokkaus.")
+
+(defn api-jarjestelmatunnukset []
+  [:div "coming soon to a Harja near you"])
