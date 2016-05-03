b9bd4d93bcb20bde1083c35963d700e86544bcb7
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-05-03 17:24:53 +0300

    Toimiva järj.tun. tallennus

--- src/clj/harja/kyselyt/api_jarjestelmatunnukset.sql
+++ src/clj/harja/kyselyt/api_jarjestelmatunnukset.sql
@@ -11,3 +11,17 @@ SELECT k.id, k.kayttajanimi, k.kuvaus, k.luotu,
        JOIN organisaatio o ON k.organisaatio = o.id
  WHERE jarjestelma = true AND
        poistettu = false
+
+-- name: poista-jarjestelmatunnus!
+UPDATE kayttaja
+   SET poistettu = true
+ WHERE id = :id AND
+       jarjestelma = true
+
+-- name: luo-jarjestelmatunnus<!
+INSERT
+   INTO kayttaja (kayttajanimi, kuvaus, organisaatio, luotu, jarjestelma, poistettu)
+   VALUES (:kayttajanimi, :kuvaus, :organisaatio, NOW(), true, false)
+ON CONFLICT ON CONSTRAINT uniikki_kayttajanimi DO
+   UPDATE SET kuvaus = :kuvaus, organisaatio = :organisaatio, poistettu = false,
+              muokattu = NOW(), jarjestelma = true;
--- src/clj/harja/palvelin/palvelut/api_jarjestelmatunnukset.clj
+++ src/clj/harja/palvelin/palvelut/api_jarjestelmatunnukset.clj
@@ -4,7 +4,8 @@
             [com.stuartsierra.component :as component]
             [harja.palvelin.komponentit.http-palvelin :refer [julkaise-palvelu poista-palvelut]]
             [harja.domain.oikeudet :as oikeudet]
-            [taoensso.timbre :as log]))
+            [taoensso.timbre :as log]
+            [clojure.java.jdbc :as jdbc]))
 
 (defn hae-jarjestelmatunnukset [db user]
   (oikeudet/lue oikeudet/hallinta-api-jarjestelmatunnukset user)
@@ -15,7 +16,13 @@
 
 (defn tallenna-jarjestelmatunnukset [db user tunnukset]
   (oikeudet/kirjoita oikeudet/hallinta-api-jarjestelmatunnukset user)
-  (log/info "TALLENNA: " tunnukset)
+  (jdbc/with-db-transaction [c db]
+    (doseq [{:keys [id kayttajanimi kuvaus organisaatio poistettu]} tunnukset]
+      (if poistettu
+        (q/poista-jarjestelmatunnus! c {:id id})
+        (q/luo-jarjestelmatunnus<! c {:kayttajanimi kayttajanimi
+                                      :kuvaus kuvaus
+                                      :organisaatio (:id organisaatio)}))))
   (hae-jarjestelmatunnukset db user))
 
 (defrecord APIJarjestelmatunnukset []
