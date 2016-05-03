d9b3cdbc0da7c3737c137cda0adb5c04b9bff091
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-05-03 13:23:38 +0300

    orjKorjaa testit vastaamaan uutta tietomallia

--- tietokanta/src/main/resources/db/migration/V1_355__.sql
+++ tietokanta/src/main/resources/db/migration/V1_355__.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- Ylläpitokohteelle keskimääräinen vuorokausiliikenne ja nykyinen päällyste (YHA-integraatio)
 
 ALTER TABLE yllapitokohde ADD COLUMN keskimaarainen_vuorokausiliikenne INTEGER;
@@ -6,17 +5,3 @@ ALTER TABLE yllapitokohde ADD COLUMN nykyinen_paallyste INTEGER;
 
 ALTER TABLE yllapitokohdeosa DROP COLUMN kvl;
 ALTER TABLE yllapitokohdeosa DROP COLUMN nykyinen_paallyste;
-=======
--- Lisää toimenpiteiden ajoittuminen -raportti
-INSERT INTO raportti (nimi, kuvaus, konteksti, parametrit, koodi, urakkatyyppi)
-  VALUES ('toimenpideajat','Toimenpiteiden ajoittuminen',
-          ARRAY['urakka'::raporttikonteksti, 'hallintayksikko'::raporttikonteksti,
-               'hankinta-alue'::raporttikonteksti, 'koko maa'::raporttikonteksti],
-          ARRAY[('Aikaväli','aikavali',true,NULL)::raporttiparametri,
-               ('Näytä urakka-alueet eriteltynä','urakoittain',true,'koko maa')::raporttiparametri,
-               ('Näytä urakka-alueet eriteltynä','urakoittain',true,'hallintayksikko')::raporttiparametri,
-               ('Näytä urakka-alueet eriteltynä','urakoittain',true,'hankinta-alue')::raporttiparametri,
-               ('Hoitoluokat', 'hoitoluokat', true, NULL)::raporttiparametri],
-          '#''harja.palvelin.raportointi.raportit.toimenpideajat/suorita',
-          'hoito');
->>>>>>> develop
