7d7f6468556829c290d2d312afa4a6eadad74ac0
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-05-09 13:36:20 +0300

    Lisää when->> ja dokumentoi

--- src/cljc/harja/tyokalut/threading.clj
+++ src/cljc/harja/tyokalut/threading.clj
@@ -1,21 +1,37 @@
 (ns harja.tyokalut.threading)
 
-(defn- expand-operation [pred-sym value-sym [operation & operations]]
+(defn- expand-operation [pos pred-sym value-sym [operation & operations]]
   (let [next-op-form (if (list? operation)
-                       (concat (list (first operation)
-                                     value-sym)
-                               (rest operation))
+                       (if (= :first pos)
+                         (concat (list (first operation)
+                                       value-sym)
+                                 (rest operation))
+                         (concat operation
+                                 (list value-sym)))
                        (list operation value-sym))]
     `(let [~value-sym ~next-op-form]
        (if-not (~pred-sym ~value-sym)
          ~value-sym
          ~(if (empty? operations)
             value-sym
-            (expand-operation pred-sym value-sym operations))))))
+            (expand-operation pos pred-sym value-sym operations))))))
 
-(defmacro when-> [pred value & operations]
+(defmacro when->
+  "Threads value through operations as the first argument (like ->). Stopping if the current value
+  matches the predicate. Returns the first value that matches the predicate or the final value."
+  [pred value & operations]
   (let [pred-sym (gensym "PRED")
         value-sym (gensym "VAL")]
     `(let [~pred-sym ~pred
            ~value-sym ~value]
-       ~(expand-operation pred-sym value-sym operations))))
+       ~(expand-operation :first pred-sym value-sym operations))))
+
+(defmacro when->>
+  "Threads value through operations as the last argument (like ->>). Stopping if the current value
+  matches the predicate. Returns the first value that matches the predicate or the final value."
+  [pred value & operations]
+  (let [pred-sym (gensym "PRED")
+        value-sym (gensym "VAL")]
+    `(let [~pred-sym ~pred
+           ~value-sym ~value]
+       ~(expand-operation :last pred-sym value-sym operations))))
