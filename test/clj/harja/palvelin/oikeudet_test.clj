6a92f33d1e287f4c5c74679251d282deba6d3c9e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-05-11 09:12:58 +0300

    Lisää tuki periytyville oikeuksille
    Oikeus voidaan merkitä Exceliin esim R+, jolloin se on voimassa kaikissa
    oman organisaation urakoissa. R* tarkoittaa kaikkia urakoita. Pelkkä R
    on vain siihen urakkaan annettu oikeus. Sama myös W oikeuksille.

--- test/clj/harja/palvelin/oikeudet_test.clj
+++ test/clj/harja/palvelin/oikeudet_test.clj
@@ -1,32 +1,57 @@
 (ns harja.palvelin.oikeudet-test
     (:require [clojure.test :refer :all]
               [taoensso.timbre :as log]
-              [harja.domain.roolit :as roolit]
+              [harja.domain.oikeudet :as oikeudet]
               [harja.testi :refer :all]))
 
-;; PENDING: nämä testit ovat vanhentuneet Sähke käyttäjähallinnan myötä
+;; Järjestelmävastaava
+(def jvh {:roolit #{"Jarjestelmavastaava"}
+          :urakkaroolit {}
+          :organisaatio {:id 1 :tyyppi "liikennevirasto" :nimi "Liikennevirasto"}})
 
+;; ELYn urakanvalvoja
+(def ely-uv {:roolit #{}
+             :urakkaroolit {1 #{"ELY_Urakanvalvoja"}}
+             :organisaation-urakat #{1 2 3}
+             :organisaatio {:id 666 :tyyppi "hallintayksikko" :nimi "Mun ely"}})
 
-#_(deftest vaadi-rooli-urakassa-on-jvh
-  (let [kayttaja +kayttaja-jvh+
-        urakka 1
-        rooli "jarjestelmavastuuhenkilo"]
-    (roolit/vaadi-rooli-urakassa kayttaja rooli urakka)))
+(def urakoitsija {:id 123 :tyyppi "urakoitsija" :nimi "Asfaltia"})
+;; Urakoitsijan pääkäyttäjä
+(def ur-pk {:roolit #{"Paakayttaja"}
+            :urakkaroolit {}
+            :organisaatio urakoitsija
+            :organisaation-urakat #{1 2 3}})
 
-#_(deftest vaadi-rooli-urakassa-ei-ole-jvh
-  (let [kayttaja +kayttaja-tero+
-        urakka 1
-        rooli "jarjestelmavastuuhenkilo"]
-    (is (thrown? RuntimeException (roolit/vaadi-rooli-urakassa kayttaja rooli urakka)))))
+;; Urakoitsijan urakanvastuuhenkilö
+(def ur-uvh {:roolit #{}
+             :urakkaroolit {1 #{"vastuuhenkilo"}}
+             :organisaatio urakoitsija
+             :organisaation-urakat #{1 2 3}})
 
-#_(deftest rooli-urakassa-ei-ole-jvh
-  (let [kayttaja +kayttaja-tero+
-        urakka 1
-        rooli "jarjestelmavastuuhenkilo"]
-    (is (= (roolit/rooli-urakassa? kayttaja rooli urakka) false))))
+(deftest vaadi-jvh-saa-tehda-mita-vaan
+  (is (oikeudet/voi-kirjoittaa? oikeudet/hallinta-lampotilat nil jvh))
+  (is (oikeudet/voi-lukea? oikeudet/raportit-laskutusyhteenveto 1 jvh))
+  (is (oikeudet/voi-kirjoittaa? oikeudet/urakat-laadunseuranta-sanktiot 2 jvh)))
 
-#_(deftest rooli-urakassa-on-jvh
-  (let [kayttaja +kayttaja-jvh+
-        urakka 1
-        rooli "jarjestelmavastuuhenkilo"]
-    (is (= (roolit/rooli-urakassa? kayttaja rooli urakka) true))))
+(deftest ely-uv-voi-lukea-kaikista-ja-kirjoittaa-omaan
+  (is (oikeudet/voi-lukea? oikeudet/urakat-suunnittelu-materiaalit 42 ely-uv))
+  (is (not (oikeudet/voi-kirjoittaa? oikeudet/urakat-suunnittelu-materiaalit 42 ely-uv)))
+  (is (oikeudet/voi-kirjoittaa? oikeudet/urakat-suunnittelu-materiaalit 1 ely-uv)))
+
+(deftest urakoitsijan-pk-voi-lukea-ja-kirjoittaa-omiaan
+  (doseq [u [1 2 3]]
+    (is (oikeudet/voi-lukea? oikeudet/urakat-laadunseuranta-tarkastukset u ur-pk))
+    (is (oikeudet/voi-kirjoittaa? oikeudet/urakat-laadunseuranta-tarkastukset u ur-pk)))
+  (is (not (oikeudet/voi-lukea? oikeudet/urakat-laadunseuranta-tarkastukset 4 ur-pk)))
+  (is (not (oikeudet/voi-kirjoittaa? oikeudet/urakat-laadunseuranta-tarkastukset 4 ur-pk))))
+
+(deftest urakoitsija-uvh-voi-lukea-ja-kirjoittaa-omaa-urakkaa
+  (is (oikeudet/voi-lukea? oikeudet/urakat-toteumat-kokonaishintaisettyot 1 ur-uvh))
+  (is (oikeudet/voi-kirjoittaa? oikeudet/urakat-toteumat-kokonaishintaisettyot 1 ur-uvh))
+  (is (not (oikeudet/voi-lukea? oikeudet/urakat-toteumat-kokonaishintaisettyot 2 ur-uvh)))
+  (is (not (oikeudet/voi-kirjoittaa? oikeudet/urakat-toteumat-kokonaishintaisettyot 2 ur-uvh))))
+
+(deftest vain-jvh-nakee-hallinnan
+  (is (oikeudet/voi-lukea? oikeudet/hallinta nil jvh))
+  (doseq [k [ely-uv ur-pk ur-uvh]]
+    (is (not (oikeudet/voi-lukea? oikeudet/hallinta nil k)))))
