e166af6f211e515092c465f6b698b764cb7fb90d
mikkoro <mikko.ronkkomaki@solita.fi>
2016-05-11 15:15:58 +0300

    Tarkista VKM-osoitteen yhdistäminen, kun osoitetta ei löydy

--- test/cljs/harja/tiedot/urakka/yhatuonti_test.cljs
+++ test/cljs/harja/tiedot/urakka/yhatuonti_test.cljs
@@ -34,13 +34,30 @@
                              :tienumero 3},
     :tunnus "1",
     :yha-id 5,
-    :yllapitoluokka 1}])
+    :yllapitoluokka 1}
+   {:keskimaarainen-vuorokausiliikenne 1000,
+    :kohdetyyppi :paikkaus,
+    :nykyinen-paallyste 1,
+    :tierekisteriosoitevali {:aet 3,
+                             :ajorata 0,
+                             :aosa 3,
+                             :kaista 11,
+                             :karttapaivamaara nil,
+                             :let 3,
+                             :losa 3,
+                             :tienumero 3},
+    :tunnus "2",
+    :yha-id 5,
+    :yllapitoluokka 1}
+   ])
 
 (def vkm-data
   {"tieosoitteet" [{"ajorata" 0, "palautusarvo" 1, "osa" 1, "etaisyys" 2, "tie" 20, "tunniste" "kohde-1-alku"}
                    {"ajorata" 0, "palautusarvo" 1, "osa" 3, "etaisyys" 4, "tie" 20, "tunniste" "kohde-1-loppu"}
                    {"ajorata" 0, "palautusarvo" 1, "osa" 5, "etaisyys" 6, "tie" 20, "tunniste" "alikohde-1-A-alku"}
-                   {"ajorata" 0, "palautusarvo" 1, "osa" 7, "etaisyys" 8, "tie" 20, "tunniste" "alikohde-1-A-loppu"}]})
+                   {"ajorata" 0, "palautusarvo" 1, "osa" 7, "etaisyys" 8, "tie" 20, "tunniste" "alikohde-1-A-loppu"}
+                   {"palautusarvo" 0, "virheteksti" "Tieosoitteelle ei saatu historiatietoa.", "tunniste" "kohde-2-alku"}
+                   {"palautusarvo" 0, "virheteksti" "Tieosoitteelle ei saatu historiatietoa.", "tunniste" "kohde-2-loppu"}]})
 
 (deftest tarkista-vkm-ja-yha-osoitteiden-yhdistaminen
   (let [yhdistetty-data (yha/yhdista-yha-ja-vkm-kohteet yha-data vkm-data)
@@ -60,4 +77,15 @@
     (is (= 5 (get-in alikohde [:tierekisteriosoitevali :aosa])))
     (is (= 6 (get-in alikohde [:tierekisteriosoitevali :aet])))
     (is (= 7 (get-in alikohde [:tierekisteriosoitevali :losa])))
-    (is (= 8 (get-in alikohde [:tierekisteriosoitevali :let]))))Lisää)
+    (is (= 8 (get-in alikohde [:tierekisteriosoitevali :let])))))
+
+(deftest tarkista-yhdistaminen-kun-osoitetta-ei-loydy
+  (let [yhdistetty-data (yha/yhdista-yha-ja-vkm-kohteet yha-data vkm-data)
+        kohde (second yhdistetty-data)]
+    ;; Kohteen osoite pitäisi pysyä samana
+    (is (= 3 (get-in kohde [:tierekisteriosoitevali :tienumero])))
+    (is (= 0 (get-in kohde [:tierekisteriosoitevali :ajorata])))
+    (is (= 3 (get-in kohde [:tierekisteriosoitevali :aosa])))
+    (is (= 3 (get-in kohde [:tierekisteriosoitevali :aet])))
+    (is (= 3 (get-in kohde [:tierekisteriosoitevali :losa])))
+    (is (= 3 (get-in kohde [:tierekisteriosoitevali :let])))))
