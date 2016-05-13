0d14f755f26952e3d31603aa56d2021cd6aeacc7
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2016-05-13 15:06:57 +0300

    Korjaa ääkkösongelma suoritettavan tehtävän liittämisessä toimenpiteeseen
    Suoritettavatehtävä liitettiin toimenpiteen nimen perusteella, ja
    näissä korjatuissa neljässä toimenpiteessä oli ääkkösiä. Vanhassa
    migraatiossa ei niitä otettu huomioon.

---
+++ tietokanta/src/main/resources/db/migration/V1_361__.sql
@@ -0,0 +1,16 @@
+UPDATE toimenpidekoodi
+SET suoritettavatehtava = 'sorateiden muokkaushoylays'::suoritettavatehtava
+WHERE nimi = 'Sorateiden muokkaushöyläys';
+
+UPDATE toimenpidekoodi
+SET suoritettavatehtava = 'sorateiden polynsidonta'::suoritettavatehtava
+WHERE nimi = 'Sorateiden pölynsidonta';
+
+UPDATE toimenpidekoodi
+SET suoritettavatehtava = 'paallysteiden juotostyot'::suoritettavatehtava
+WHERE nimi = 'Päällysteiden juotostyöt';
+
+UPDATE toimenpidekoodi
+SET suoritettavatehtava = 'paallysteiden paikkaus'::suoritettavatehtava
+WHERE nimi = 'Päällysteiden paikkaus';
+
