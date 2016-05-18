5dfd271a1313f9579f055becde093cc481bdc17c
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-05-18 11:31:52 +0300

    Takuun asetus palvelimella

--- src/clj/harja/kyselyt/urakat.sql
+++ src/clj/harja/kyselyt/urakat.sql
@@ -446,3 +446,8 @@ WHERE u.id = :id;
 -- name: hae-urakan-sampo-id
 -- single?: true
 SELECT sampoid FROM urakka where id = :urakka
+
+-- name: aseta-takuun-loppupvm!
+UPDATE urakka
+   SET takuu_loppupvm = :loppupvm
+ WHERE id = :urakka
