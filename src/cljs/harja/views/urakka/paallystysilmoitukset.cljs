4e83e0eec2ac6e15c83767bfc7662f5db490aecf
Jari Hanhela <Jari.Hanhela@solita.fi>
2016-05-19 07:42:20 +0300

    Korjaa FIXME-kommentti

--- src/cljs/harja/views/urakka/paallystysilmoitukset.cljs
+++ src/cljs/harja/views/urakka/paallystysilmoitukset.cljs
@@ -349,7 +349,7 @@
               :virheet alikohteet-virheet
               :rivinumerot? true
               :uusi-id (inc (count @paallystystoimenpiteet))}
-             ;; FIXME Käytä kohdeosat komponenttia tai muuten yhtenäistä logiikka, jotta tien tarkistus, sijainti yms. toimii samalla tavalla
+             ;; FIXME Yhtenäistä logiikka yllapitokohdeosa-komponentin kanssa, jotta tien tarkistus, sijainti yms. toimii samalla tavalla
              [{:otsikko "Nimi" :nimi :nimi :tyyppi :string :leveys "15%"}
               {:otsikko "Tienumero" :nimi :tie :tyyppi :positiivinen-numero :leveys "10%"
                :validoi [[:ei-tyhja "Tienumero puuttuu"]]}
