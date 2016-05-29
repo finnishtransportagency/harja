d66b1b842284fe075a253a5187c3f748cfaa153c
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2016-05-29 10:49:15 +0300

    Testaa tieosan katkaisu

--- test/cljs/harja/views/urakka/yllapitokohteet_test.cljs
+++ test/cljs/harja/views/urakka/yllapitokohteet_test.cljs
@@ -7,7 +7,12 @@
 
     [harja.loki :refer [log]]
     [harja.domain.tierekisteri :as tr]
-    [harja.tyokalut.functor :refer [fmap]]))
+    [harja.tyokalut.functor :refer [fmap]]
+    [clojure.test.check :as tc]
+    [clojure.test.check.generators :as gen]
+    [clojure.test.check.properties :as prop])
+  (:require-macros [clojure.test.check.clojure-test :refer [defspec]]
+                   [clojure.test.check.properties :as prop]))
 
 (def kohdeosat
   {1 {:nimi "Laivaniemi 1"
@@ -101,3 +106,75 @@
         "Loppu siirtyy yhdellä aiemmaksi")
     (is (= (pituus-yht vanhat-kohdeosat) (pituus-yht uudet-kohdeosat))
         "Pilkkominen ja yhdistäminen ei muuta pituutta")))
+
+(defn tierekisteriosoite [tie osien-pituus osa-min osa-max]
+  (gen/fmap
+   #(merge {:tr-numero tie}
+           (zipmap [:tr-alkuosa :tr-alkuetaisyys
+                    :tr-loppuosa :tr-loppuetaisyys]
+                   %))
+   (gen/bind
+    (gen/such-that #(<= (first %) (second %))
+                   (gen/vector (gen/choose osa-min osa-max) 2))
+    #(let [a-pit (get osien-pituus (first %))
+           l-pit (get osien-pituus (second %))]
+       (gen/such-that
+        (fn [[aosa alkuet losa loppuet]]
+          (or (< aosa losa)
+              (> (- loppuet alkuet) 2)))
+        (gen/tuple (gen/return (first %))
+                   (gen/choose 1 a-pit)
+                   (gen/return (second %))
+                   (gen/choose 1 l-pit)))))))
+
+
+(defn tien-kohta
+  "Paluttaa generaattorin, joka valitsee sattumanvaraisen kohdan annetusta tiestä.
+  Generaattorin arvo on tuple, jossa on annettu tie ja tuple [katkaisuosa katkaisuetäisyys]."
+  [osien-pituus
+   {aosa :tr-alkuosa alkuet :tr-alkuetaisyys
+    losa :tr-loppuosa loppuet :tr-loppuetaisyys :as tie}]
+  (gen/tuple (gen/return tie)
+             (if (= aosa losa)
+               ;; Jos alku- ja loppuosa on sama, ota kohta etäisyyksien väliltä
+               (gen/tuple (gen/return aosa)
+                          (gen/choose (inc alkuet) (dec loppuet)))
+
+               ;; Muuten arvotaan osa, josta arvotaan
+               (gen/bind (gen/choose aosa losa)
+                         (fn [osa]
+                           (gen/tuple
+                            (gen/return osa)
+                            (cond
+                              (= osa aosa)
+                              (gen/choose (inc alkuet)
+                                          (dec (get osien-pituus osa)))
+
+                              (= osa losa)
+                              (gen/choose 1 (dec loppuet))
+
+                              :default
+                              (gen/choose 1 (get osien-pituus osa)))))))))
+(defspec
+  osan-katkaisu
+  100
+  (prop/for-all
+   [[osa [katkaisuosa katkaisuet]] (gen/bind (tierekisteriosoite 1 osien-pituus 1 3)
+                                             (partial tien-kohta osien-pituus))]
+
+   (let [vanhat-kohdeosat {1 osa}
+         uudet-kohdeosat (as-> vanhat-kohdeosat ko
+                           (yllapitokohteet/lisaa-uusi-kohdeosa ko 1)
+                           (yllapitokohteet/kasittele-paivittyneet-kohdeosat
+                            ko
+                            (-> ko
+                                (assoc-in [1 :tr-loppuosa] katkaisuosa)
+                                (assoc-in [1 :tr-loppuetaisyys] katkaisuet))))]
+     (is (= #{1 2} (avaimet uudet-kohdeosat))
+         "Osia on lisäyksen jälkeen 2")
+     (is (= [katkaisuosa katkaisuet] (loppu (get uudet-kohdeosat 1))))
+     (is (= [katkaisuosa katkaisuet] (alku (get uudet-kohdeosat 2)))
+         "Loppuosa on kopioitunut seuraavan rivin alkuosaksi")
+     (is (= (pituus-yht vanhat-kohdeosat)
+            (pituus-yht uudet-kohdeosat))
+         "Osan katkaisu ei vaikuta yhteenlaskettuun pituuteen"))))
