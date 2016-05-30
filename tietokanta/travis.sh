c68428cc09d751a24a625a48d8760ed4d0bee136
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-05-30 13:18:33 +0300

    Kokeile asentaa puuttuva PostgreSQL 9.5 ja PostGIS

--- tietokanta/travis.sh
+++ tietokanta/travis.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+sudo /etc/init.d/postgresql stop
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main 9.5" >> /etc/apt/sources.list.d/postgresql.list'
+sudo apt-get update
+sudo apt-get install postgresql-9.5
+sudo apt-get install postgresql-9.5-postgis
+
 cd tietokanta
 psql -c "create database harja;" -U postgres
 psql -c "create extension postgis" -U postgres harja
