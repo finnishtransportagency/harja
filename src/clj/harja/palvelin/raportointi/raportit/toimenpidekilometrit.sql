a014d98f5808e98c25a2f3f8c3c41e64c2d21a55
Jari Hanhela <jarihanhela@gmail.com>
2016-05-31 09:27:01 +0300

    Jätä huomiotta toimenpidekilometrien kyselyssä poistetut toteumat

--- src/clj/harja/palvelin/raportointi/raportit/toimenpidekilometrit.sql
+++ src/clj/harja/palvelin/raportointi/raportit/toimenpidekilometrit.sql
@@ -10,6 +10,7 @@ SELECT
 FROM toteuma t
   JOIN urakka u ON t.urakka = u.id
   JOIN toteuma_tehtava tt ON t.id = tt.toteuma
+  AND tt.poistettu IS NOT TRUE
   JOIN toimenpidekoodi tpk ON tt.toimenpidekoodi = tpk.id
 WHERE (:urakka::INTEGER IS NULL OR t.urakka = :urakka)
       AND (:hallintayksikko::INTEGER IS NULL OR t.urakka IN (SELECT id
@@ -20,4 +21,5 @@ WHERE (:urakka::INTEGER IS NULL OR t.urakka = :urakka)
            (:urakka::INTEGER IS NULL AND (:urakkatyyppi :: urakkatyyppi IS NULL OR
                                           u.tyyppi = :urakkatyyppi :: urakkatyyppi)))
       AND t.alkanut :: DATE BETWEEN :alku AND :loppu;
-AND t.tyyppi = 'kokonaishintainen';
+AND t.tyyppi = 'kokonaishintainen'
+AND t.poistettu IS NOT TRUE;
