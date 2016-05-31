db009e8c1c774a7f1c385c2df83d81625af83762
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-05-31 13:56:23 +0300

    Poista -i parametri

--- tietokanta/travis_pg_conf.sh
+++ tietokanta/travis_pg_conf.sh
@@ -1,5 +1,5 @@
 echo "Konfiguroidaan PostgreSQL 9.5"
 echo "listen_addresses = '*'" >>
-sed -i "" "s/#listen_addresses.*/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
+sed -i "s/#listen_addresses.*/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
 
-sed -i "" "s/port = 5433/port = 5432/g" /etc/postgresql/9.5/main/postgresql.conf
+sed -i "s/port = 5433/port = 5432/g" /etc/postgresql/9.5/main/postgresql.conf
