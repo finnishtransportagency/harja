c0852c12187e6d7ae18f5e0e0607ff4cbd1728d7
Jari Hanhela <jarihanhela@gmail.com>
2016-05-31 11:02:29 +0300

    Mahdollista toimenpidekilometrien raportin testiversion suoritus

--- src/clj/harja/palvelin/raportointi/raportit/toimenpidekilometrit.clj
+++ src/clj/harja/palvelin/raportointi/raportit/toimenpidekilometrit.clj
@@ -4,6 +4,9 @@
             [harja.pvm :as pvm]
             [harja.tyokalut.functor :refer [fmap]]
             [jeesql.core :refer [defqueries]]
+            [harja.kyselyt.urakat :as urakat-q]
+            [harja.kyselyt.hallintayksikot :as hallintayksikot-q]
+            [harja.palvelin.raportointi.raportit.yleinen :refer [raportin-otsikko]]
             [taoensso.timbre :as log]
             [harja.domain.hoitoluokat :as hoitoluokat]))
 
@@ -11,7 +14,10 @@
 
 (defn suorita [db user {:keys [alkupvm loppupvm hoitoluokat urakka-id
                                hallintayksikko-id urakoittain?] :as parametrit}]
-  (let [hoitoluokat (or hoitoluokat
+  (let [konteksti (cond urakka-id :urakka
+                        hallintayksikko-id :hallintayksikko
+                        :default :koko-maa)
+        hoitoluokat (or hoitoluokat
                         ;; Jos hoitoluokkia ei annettu, näytä kaikki (työmaakokous)
                         (into #{} (map :numero) hoitoluokat/talvihoitoluokat))
         parametrit {:urakka urakka-id
@@ -19,8 +25,18 @@
                     :alkupvm alkupvm
                     :loppupvm loppupvm
                     :hoitoluokat hoitoluokat}
-        toimenpidekilometrit #_(hae-toimenpidekilometrit db parametrit urakoittain?) nil]
+        toimenpidekilometrit #_(hae-toimenpidekilometrit db parametrit urakoittain?) nil
+        raportin-nimi "Toimenpidekilometrit"
+        otsikko (raportin-otsikko
+                  (case konteksti
+                    :urakka (:nimi (first (urakat-q/hae-urakka db urakka-id)))
+                    :hallintayksikko (:nimi (first (hallintayksikot-q/hae-organisaatio db hallintayksikko-id)))
+                    :koko-maa "KOKO MAA")
+                  raportin-nimi alkupvm loppupvm)]
     [:raportti {:nimi "Toimenpidekilometrit"
                 :orientaatio :landscape}
+     [:taulukko {:otsikko                    otsikko
+                 :tyhja                      (if (empty? toimenpidekilometrit) "Ei raportoitavia tehtäviä.")
+                 :sheet-nimi raportin-nimi}
      [{:otsikko "Teh\u00ADtä\u00ADvä" :leveys 10}]
-     ["123"]]))
+     [["123"]]]]))
