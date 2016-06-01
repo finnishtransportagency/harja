fe43ac76608413e210c52fc7b5de8eb48bafb5bc
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-06-01 08:57:47 +0300

    hba luottavaiseksi

--- tietokanta/travis_pg_conf.sh
+++ tietokanta/travis_pg_conf.sh
@@ -3,3 +3,5 @@ echo "Konfiguroidaan PostgreSQL 9.5"
 sed -i "s/#listen_addresses.*/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
 
 sed -i "s/port = 5433/port = 5432/g" /etc/postgresql/9.5/main/postgresql.conf
+
+echo "\nhost    all             all             0.0.0.0/0            trust" >> /etc/postgresql/9.5/main/pg_hba.conf
