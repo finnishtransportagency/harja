fcfa824d0d5e747e3e41662a1f819a0ded2e1a49
Jari Hanhela <jarihanhela@gmail.com>
2016-06-01 13:47:37 +0300

    Poista urakoittain erittely -parametri (ei tarvita)

--- tietokanta/src/main/resources/db/migration/V1_373__.sql
+++ tietokanta/src/main/resources/db/migration/V1_373__.sql
@@ -4,9 +4,6 @@ INSERT INTO raportti (nimi, kuvaus, konteksti, parametrit, koodi, urakkatyyppi)
           ARRAY['urakka'::raporttikonteksti, 'hallintayksikko'::raporttikonteksti,
                'hankinta-alue'::raporttikonteksti, 'koko maa'::raporttikonteksti],
           ARRAY[('Aikaväli','aikavali',true,NULL)::raporttiparametri,
-               ('Näytä urakka-alueet eriteltynä','urakoittain',true,'koko maa')::raporttiparametri,
-               ('Näytä urakka-alueet eriteltynä','urakoittain',true,'hallintayksikko')::raporttiparametri,
-               ('Näytä urakka-alueet eriteltynä','urakoittain',true,'hankinta-alue')::raporttiparametri,
                ('Hoitoluokat', 'hoitoluokat', true, NULL)::raporttiparametri],
           '#''harja.palvelin.raportointi.raportit.toimenpidekilometrit/suorita',
           'hoito');
