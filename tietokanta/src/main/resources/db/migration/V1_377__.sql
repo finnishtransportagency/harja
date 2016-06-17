3126d0dabf03ba2c653308ad803faed0ec8ebf41
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-06-17 14:48:55 +0300

    Listaa pakolliset tehtävät API-seurantaan

--- tietokanta/src/main/resources/db/migration/V1_377__.sql
+++ tietokanta/src/main/resources/db/migration/V1_377__.sql
@@ -1,3 +1,13 @@
 ALTER TABLE toimenpidekoodi ADD COLUMN api_seuranta BOOLEAN;
 
--- todo: lisää oletuksena seurattavat tehtävät tähän migraatioon
+UPDATE toimenpidekoodi SET api_seuranta = TRUE WHERE nimi = 'Auraus ja sohjonpoisto';
+UPDATE toimenpidekoodi SET api_seuranta = TRUE WHERE nimi = 'Liikennemerkkien puhdistus';
+UPDATE toimenpidekoodi SET api_seuranta = TRUE WHERE nimi = 'Linjahiekoitus';
+UPDATE toimenpidekoodi SET api_seuranta = TRUE WHERE nimi = 'Lumivallien madaltaminen';
+UPDATE toimenpidekoodi SET api_seuranta = TRUE WHERE nimi = 'Pinnan tasaus';
+UPDATE toimenpidekoodi SET api_seuranta = TRUE WHERE nimi = 'Pistehiekoitus';
+UPDATE toimenpidekoodi SET api_seuranta = TRUE WHERE nimi = 'Sulamisveden haittojen torjunta';
+UPDATE toimenpidekoodi SET api_seuranta = TRUE WHERE nimi = 'Suolaus';
+UPDATE toimenpidekoodi SET api_seuranta = TRUE WHERE nimi = 'Suolaus';
+
+-- todo: tee loppuun, kun saadaan valmis lista
