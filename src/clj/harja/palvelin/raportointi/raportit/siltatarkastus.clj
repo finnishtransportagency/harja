0b838a9c87d2d7a1e84fd795121519121e156a35
Jari Hanhela <jarihanhela@gmail.com>
2016-06-22 12:06:53 +0300

    Listaa sillan perustiedot urakan kaikkien siltojen siltaraportissa

--- src/clj/harja/palvelin/raportointi/raportit/siltatarkastus.clj
+++ src/clj/harja/palvelin/raportointi/raportit/siltatarkastus.clj
@@ -1,22 +1,83 @@
-
 (ns harja.palvelin.raportointi.raportit.siltatarkastus
-  (:require [harja.kyselyt.urakat :as urakat-q]
-            [harja.kyselyt.hallintayksikot :as hallintayksikot-q]
-            [harja.kyselyt.toimenpideinstanssit :refer [hae-urakan-toimenpideinstanssi]]
-            [harja.fmt :as fmt]
-            [harja.pvm :as pvm]
-            [harja.palvelin.raportointi.raportit.yleinen :refer [raportin-otsikko-vuodella]]
-            [taoensso.timbre :as log]
-            [harja.domain.roolit :as roolit]
-            [harja.kyselyt.konversio :as konv]
-            [harja.palvelin.raportointi.raportit.yleinen :as yleinen]
-            [clj-time.coerce :as c]))
+  (:require
+    [jeesql.core :refer [defqueries]]
+    [harja.kyselyt.urakat :as urakat-q]
+    [harja.kyselyt.hallintayksikot :as hallintayksikot-q]
+    [harja.kyselyt.toimenpideinstanssit :refer [hae-urakan-toimenpideinstanssi]]
+    [harja.fmt :as fmt]
+    [harja.pvm :as pvm]
+    [harja.palvelin.raportointi.raportit.yleinen :refer [raportin-otsikko-vuodella]]
+    [taoensso.timbre :as log]
+    [harja.domain.roolit :as roolit]
+    [harja.kyselyt.konversio :as konv]
+    [harja.palvelin.raportointi.raportit.yleinen :as yleinen]
+    [clj-time.coerce :as c]))
 
-(defn suorita [db user {:keys [urakka-id hallintayksikko-id  silta vuosi] :as parametrit}]
+(defqueries "harja/palvelin/raportointi/raportit/siltatarkastus.sql")
+
+(defn muodosta-sillan-datarivit []
+  )
+
+(defn muodosta-siltojen-datarivit [db urakka-id vuosi]
+  (let [tarkastukset (hae-urakan-siltatarkastukset db {:urakka urakka-id
+                                                       :vuosi vuosi})
+        rivit (mapv
+                (fn [tarkastus]
+                  [(:siltanro tarkastus)
+                   (:siltanimi tarkastus)
+                   (fmt/pvm-opt (:tarkastusaika tarkastus))
+                   (:tarkastaja tarkastus)
+                   (:A tarkastus)
+                   (:B tarkastus)
+                   (:C tarkastus)
+                   (:D tarkastus)
+                   (:liitteet tarkastus)])
+                tarkastukset)]
+    rivit))
+
+(defn muodosta-urakan-datarivit []
+  )
+
+(defn muodosta-hallintayksikon-datarivit []
+  )
+
+(defn muodosta-koko-maan-datarivit []
+  )
+
+(defn muodosta-raportin-otsikkorivit [db konteksti silta]
+  (case konteksti
+    :urakka (if (= silta :kaikki)
+              [{:leveys 5 :otsikko "Siltanumero"}
+               {:leveys 10 :otsikko "Silta"}
+               {:leveys 5 :otsikko "Tarkastettu"}
+               {:leveys 5 :otsikko "Tarkastaja"}
+               {:leveys 5 :otsikko "A summa"}
+               {:leveys 5 :otsikko "B summa"}
+               {:leveys 5 :otsikko "C summa"}
+               {:leveys 5 :otsikko "D summa"}
+               {:leveys 5 :otsikko "Liitteet"}]
+              [{:leveys 2 :otsikko "#"}
+               {:leveys 15 :otsikko "Kohde"}
+               {:leveys 2 :otsikko "Tulos"}
+               {:leveys 10 :otsikko "Lisätieto"}
+               {:leveys 5 :otsikko "Liitteet"}])
+    :hallintayksikko []
+    :koko-maa []))
+
+(defn muodosta-raportin-datarivit [db urakka-id konteksti silta vuosi]
+  (case konteksti
+    :urakka (if (= silta :kaikki)
+              (muodosta-siltojen-datarivit db urakka-id vuosi)
+              (muodosta-sillan-datarivit))
+    :hallintayksikko (muodosta-hallintayksikon-datarivit)
+    :koko-maa (muodosta-koko-maan-datarivit)))
+
+(defn suorita [db user {:keys [urakka-id hallintayksikko-id silta vuosi] :as parametrit}]
   (let [konteksti (cond urakka-id :urakka
                         hallintayksikko-id :hallintayksikko
                         :default :koko-maa)
-        sillat nil
+        otsikkorivit (muodosta-raportin-otsikkorivit db konteksti silta)
+        datarivit (muodosta-raportin-datarivit db urakka-id konteksti silta vuosi)
         raportin-nimi "Siltatarkastusraportti"
         otsikko (raportin-otsikko-vuodella
                   (case konteksti
@@ -27,7 +88,7 @@
     [:raportti {:orientaatio :landscape
                 :nimi        raportin-nimi}
      [:taulukko {:otsikko otsikko
-                 :tyhja   (if (empty? sillat) "Ei raportoitavia siltatarkastuksia.")
+                 :tyhja   (if (empty? datarivit) "Ei raportoitavia siltatarkastuksia.")
                  :sheet-nimi raportin-nimi}
-      [{:leveys 15 :otsikko "Silta"}]
-      [["1"]]]]))
+      otsikkorivit
+      datarivit]]))
---
+++ src/clj/harja/palvelin/raportointi/raportit/siltatarkastus.sql
@@ -0,0 +1,23 @@
+-- name: hae-urakan-siltatarkastukset
+-- Hakee urakan kaikki sillat ja sekä annettuna vuonna tehdyn uusimman siltatarkastuksen
+SELECT
+  siltanro,
+  siltanimi,
+  (SELECT tarkastusaika
+   FROM siltatarkastus st
+   WHERE st.silta = s.id
+         AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
+         AND st.poistettu = FALSE
+   ORDER BY tarkastusaika DESC
+   LIMIT 1),
+  (SELECT tarkastaja
+   FROM siltatarkastus st
+   WHERE st.silta = s.id
+         AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
+         AND st.poistettu = FALSE
+   ORDER BY tarkastusaika DESC
+   LIMIT 1)
+FROM silta s
+WHERE s.id IN (SELECT silta
+               FROM sillat_alueurakoittain
+               WHERE urakka = :urakka);
