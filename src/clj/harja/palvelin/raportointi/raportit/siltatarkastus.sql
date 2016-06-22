54acb7f7caa1ac413fb0a41ed17524c6e50a66fb
Jari Hanhela <jarihanhela@gmail.com>
2016-06-22 12:55:42 +0300

    Kohdista liitteet aina sillan omalle riville

--- src/clj/harja/palvelin/raportointi/raportit/siltatarkastus.sql
+++ src/clj/harja/palvelin/raportointi/raportit/siltatarkastus.sql
@@ -57,12 +57,21 @@ SELECT
                                      AND st.poistettu = FALSE
                                ORDER BY tarkastusaika DESC
                                LIMIT 1)) AS "d",
-   l.id AS liite_id,
-   l.tyyppi AS liite_tyyppi,
-   l.koko AS liite_koko,
-   l.nimi AS liite_nimi
+  l.id AS liite_id,
+  l.tyyppi AS liite_tyyppi,
+  l.koko AS liite_koko,
+  l.nimi AS liite_nimi
 FROM silta s
-  LEFT JOIN liite l ON l.urakka = :urakka
+  LEFT JOIN liite l ON l.id IN (SELECT id
+                                FROM
+                                  liite l
+                                  JOIN siltatarkastus_kohde_liite skl ON l.id = skl.liite
+                                WHERE skl.siltatarkastus IN (SELECT id
+                                                             FROM siltatarkastus st
+                                                             WHERE st.silta = s.id
+                                                                   AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
+                                                                   AND st.poistettu = FALSE
+                                                                   AND urakka = :urakka))
 WHERE s.id IN (SELECT silta
                FROM sillat_alueurakoittain
                WHERE urakka = :urakka)
