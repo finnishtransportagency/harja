cfc4f1b9fd9d58c7b93bc594f0d2f7b47fb2ad03
Jari Hanhela <jarihanhela@gmail.com>
2016-06-22 12:29:57 +0300

    Laske siltojen eri tulokset oikein raportille

--- src/clj/harja/palvelin/raportointi/raportit/siltatarkastus.sql
+++ src/clj/harja/palvelin/raportointi/raportit/siltatarkastus.sql
@@ -16,7 +16,47 @@ SELECT
          AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
          AND st.poistettu = FALSE
    ORDER BY tarkastusaika DESC
-   LIMIT 1)
+   LIMIT 1),
+  (SELECT COUNT(*)
+   FROM siltatarkastuskohde
+   WHERE tulos = 'A'
+         AND siltatarkastus = (SELECT id
+                               FROM siltatarkastus st
+                               WHERE st.silta = s.id
+                                     AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
+                                     AND st.poistettu = FALSE
+                               ORDER BY tarkastusaika DESC
+                               LIMIT 1)) AS "a",
+  (SELECT COUNT(*)
+   FROM siltatarkastuskohde
+   WHERE tulos = 'B'
+         AND siltatarkastus = (SELECT id
+                               FROM siltatarkastus st
+                               WHERE st.silta = s.id
+                                     AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
+                                     AND st.poistettu = FALSE
+                               ORDER BY tarkastusaika DESC
+                               LIMIT 1)) AS "b",
+  (SELECT COUNT(*)
+   FROM siltatarkastuskohde
+   WHERE tulos = 'C'
+         AND siltatarkastus = (SELECT id
+                               FROM siltatarkastus st
+                               WHERE st.silta = s.id
+                                     AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
+                                     AND st.poistettu = FALSE
+                               ORDER BY tarkastusaika DESC
+                               LIMIT 1)) AS "c",
+  (SELECT COUNT(*)
+   FROM siltatarkastuskohde
+   WHERE tulos = 'D'
+         AND siltatarkastus = (SELECT id
+                               FROM siltatarkastus st
+                               WHERE st.silta = s.id
+                                     AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
+                                     AND st.poistettu = FALSE
+                               ORDER BY tarkastusaika DESC
+                               LIMIT 1)) AS "d"
 FROM silta s
 WHERE s.id IN (SELECT silta
                FROM sillat_alueurakoittain
