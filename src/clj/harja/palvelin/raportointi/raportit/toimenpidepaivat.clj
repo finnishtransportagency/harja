877cb17035ea1d6d9badfa258eb0066c36df6b3f
Jari Hanhela <jarihanhela@gmail.com>
2016-06-23 15:20:32 +0300

    Lisää testi toimenpidepäivät-raportille

--- src/clj/harja/palvelin/raportointi/raportit/toimenpidepaivat.clj
+++ src/clj/harja/palvelin/raportointi/raportit/toimenpidepaivat.clj
@@ -54,12 +54,13 @@
                         ;; Jos hoitoluokkia ei annettu, näytä kaikki (työmaakokous)
                         (into #{} (map :numero) hoitoluokat/talvihoitoluokat))
         talvihoitoluokat (filter #(hoitoluokat (:numero %)) hoitoluokat/talvihoitoluokat)
-        parametrit {:urakka          urakka-id
+        parametrit {:urakka urakka-id
                     :hallintayksikko hallintayksikko-id
-                    :alku         alkupvm
-                    :loppu        loppupvm
-                    :hoitoluokat     hoitoluokat
-                    :urakkatyyppi    (name urakkatyyppi)}
+                    :alku alkupvm
+                    :loppu loppupvm
+                    :hoitoluokat hoitoluokat
+                    :urakkatyyppi (when-not urakka-id
+                                    (name urakkatyyppi))}
         konteksti (cond urakka-id :urakka
                         hallintayksikko-id :hallintayksikko
                         :default :koko-maa)
