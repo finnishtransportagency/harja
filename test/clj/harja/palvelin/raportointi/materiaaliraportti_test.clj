7a490d8a361022a53baa15111873b2a877e58889
Jari Hanhela <jarihanhela@gmail.com>
2016-06-23 14:38:08 +0300

    Lisää testi materiaalirapsalle

---
+++ test/clj/harja/palvelin/raportointi/materiaaliraportti_test.clj
@@ -0,0 +1,114 @@
+(ns harja.palvelin.raportointi.materiaaliraportti-test
+  (:require [clojure.test :refer :all]
+            [harja.palvelin.komponentit.tietokanta :as tietokanta]
+            [harja.palvelin.palvelut.toimenpidekoodit :refer :all]
+            [harja.palvelin.palvelut.urakat :refer :all]
+            [harja.testi :refer :all]
+            [taoensso.timbre :as log]
+            [com.stuartsierra.component :as component]
+            [harja.pvm :as pvm]
+            [clj-time.core :as t]
+            [clj-time.coerce :as c]
+            [harja.palvelin.komponentit.pdf-vienti :as pdf-vienti]
+            [harja.palvelin.raportointi :as raportointi]
+            [harja.palvelin.palvelut.raportit :as raportit]))
+
+(defn jarjestelma-fixture [testit]
+  (alter-var-root #'jarjestelma
+                  (fn [_]
+                    (component/start
+                      (component/system-map
+                        :db (tietokanta/luo-tietokanta testitietokanta)
+                        :http-palvelin (testi-http-palvelin)
+                        :pdf-vienti (component/using
+                                      (pdf-vienti/luo-pdf-vienti)
+                                      [:http-palvelin])
+                        :raportointi (component/using
+                                       (raportointi/luo-raportointi)
+                                       [:db :pdf-vienti])
+                        :raportit (component/using
+                                    (raportit/->Raportit)
+                                    [:http-palvelin :db :raportointi :pdf-vienti])))))
+
+  (testit)
+  (alter-var-root #'jarjestelma component/stop))
+
+(use-fixtures :once (compose-fixtures
+                      jarjestelma-fixture
+                      urakkatieto-fixture))
+
+(deftest raportin-suoritus-urakalle-toimii
+  (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                :suorita-raportti
+                                +kayttaja-jvh+
+                                {:nimi :materiaaliraportti
+                                 :konteksti "urakka"
+                                 :urakka-id (hae-oulun-alueurakan-2014-2019-id)
+                                 :parametrit {:alkupvm (c/to-date (t/local-date 2014 10 1))
+                                              :loppupvm (c/to-date (t/local-date 2015 10 1))}})]
+    (is (vector? vastaus))
+    (is (= vastaus [:raportti
+                    {:nimi "Materiaaliraportti"}
+                    [:taulukko
+                     {:otsikko "Oulun alueurakka 2014-2019, Materiaaliraportti ajalta 01.10.2014 - 01.10.2015"
+                      :sheet-nimi "Materiaaliraportti"
+                      :viimeinen-rivi-yhteenveto? true}
+                     [{:otsikko "Urakka"}
+                      {:otsikko "Talvisuolaliuos NaCl (t)"}]
+                     [["Oulun alueurakka 2014-2019"
+                       "1000,00"]
+                       ["Yhteensä"
+                         "1000,00"]]]]))))
+
+(deftest raportin-suoritus-hallintayksikolle-toimii
+  (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                :suorita-raportti
+                                +kayttaja-jvh+
+                                {:nimi :materiaaliraportti
+                                 :konteksti "hallintayksikko"
+                                 :hallintayksikko-id (hae-pohjois-pohjanmaan-hallintayksikon-id)
+                                 :parametrit {:alkupvm (c/to-date (t/local-date 2014 10 1))
+                                              :loppupvm (c/to-date (t/local-date 2015 10 1))}})]
+    (is (vector? vastaus))
+    (is (= vastaus [:raportti
+                    {:nimi "Materiaaliraportti"}
+                    [:taulukko
+                     {:otsikko "Pohjois-Pohjanmaa, Materiaaliraportti ajalta 01.10.2014 - 01.10.2015"
+                      :sheet-nimi "Materiaaliraportti"
+                      :viimeinen-rivi-yhteenveto? true}
+                     [{:otsikko "Urakka"}
+                      {:otsikko "Talvisuolaliuos NaCl (t)"}]
+                     [["Oulun alueurakka 2014-2019"
+                       "1000,00"]
+                       ["Kajaanin alueurakka 2014-2019"
+                        "1000,00"]
+                       ["Yhteensä"
+                         "2000,00"]]]]))))
+
+(deftest raportin-suoritus-koko-maalle-toimii
+  (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                :suorita-raportti
+                                +kayttaja-jvh+
+                                {:nimi :materiaaliraportti
+                                 :konteksti "koko maa"
+                                 :parametrit {:alkupvm (c/to-date (t/local-date 2014 10 1))
+                                              :loppupvm (c/to-date (t/local-date 2015 10 1))}})]
+    (is (vector? vastaus))
+    (is (= vastaus [:raportti
+                    {:nimi "Materiaaliraportti"}
+                    [:taulukko
+                     {:otsikko "KOKO MAA, Materiaaliraportti ajalta 01.10.2014 - 01.10.2015"
+                      :sheet-nimi "Materiaaliraportti"
+                      :viimeinen-rivi-yhteenveto? true}
+                     [{:otsikko "Urakka"}
+                      {:otsikko "Talvisuolaliuos NaCl (t)"}]
+                     [["Vantaan alueurakka 2014-2019"
+                       "1000,00"]
+                       ["Kajaanin alueurakka 2014-2019"
+                        "1000,00"]
+                       ["Oulun alueurakka 2014-2019"
+                        "1000,00"]
+                       ["Espoon alueurakka 2014-2019"
+                        "1000,00"]
+                       ["Yhteensä"
+                         "4000,00"]]]]))))
