4a08d7fa8beb0041e85a08bd8dece6ecfc811753
Jari Hanhela <jarihanhela@gmail.com>
2016-06-27 15:57:36 +0300

    Näytä eri välitavoiteryhmien osuudet raportilla

--- src/clj/harja/palvelin/raportointi/raportit/valitavoiteraportti.clj
+++ src/clj/harja/palvelin/raportointi/raportit/valitavoiteraportti.clj
@@ -9,56 +9,61 @@
             [harja.palvelin.raportointi.raportit.yleinen :as yleinen]
             [clj-time.core :as t]
             [harja.pvm :as pvm]
-            [clj-time.coerce :as c]))
+            [clj-time.coerce :as c]
+            [harja.math :as math]))
 
 (defqueries "harja/palvelin/raportointi/raportit/valitavoitteet.sql")
 
+(defn suodata-ajoissa [valitavoitteet]
+          (filter
+            (fn [valitavoite]
+              (and (:takaraja valitavoite)
+                   (:valmis-pvm valitavoite)
+                   (pvm/sama-tai-ennen? (c/from-date (:valmis-pvm valitavoite))
+                                        (c/from-date (:takaraja valitavoite)))))
+            valitavoitteet))
+
+(defn suodata-myohassa [valitavoitteet]
+  (filter
+    (fn [valitavoite]
+      (and (:takaraja valitavoite)
+           (:valmis-pvm valitavoite)
+           (pvm/jalkeen? (c/from-date (:valmis-pvm valitavoite))
+                         (c/from-date (:takaraja valitavoite)))))
+    valitavoitteet))
+
+(defn suodata-kesken [valitavoitteet]
+  (filter
+    (fn [valitavoite]
+      (and (:takaraja valitavoite)
+           (pvm/ennen? (t/now) (c/from-date (:takaraja valitavoite)))
+           (not (:valmis-pvm valitavoite))))
+    valitavoitteet))
+
+(defn suodata-toteumatta [valitavoitteet]
+  (filter
+    (fn [valitavoite]
+      (and (:takaraja valitavoite)
+           (not (:valmis-pvm valitavoite))
+           (pvm/jalkeen? (t/now)
+                         (c/from-date (:takaraja valitavoite)))))
+    valitavoitteet))
+
 (defn muodosta-raportin-rivit [valitavoitteet]
-  (let [ajoissa (fn [valitavoitteet]
-                  (filter
-                    (fn [valitavoite]
-                      (and (:takaraja valitavoite)
-                           (:valmis-pvm valitavoite)
-                           (pvm/sama-tai-ennen? (c/from-date (:valmis-pvm valitavoite))
-                                                (c/from-date (:takaraja valitavoite)))))
-                    valitavoitteet))
-        myohassa (fn [valitavoitteet]
-                   (filter
-                     (fn [valitavoite]
-                       (and (:takaraja valitavoite)
-                            (:valmis-pvm valitavoite)
-                            (pvm/jalkeen? (c/from-date (:valmis-pvm valitavoite))
-                                          (c/from-date (:takaraja valitavoite)))))
-                     valitavoitteet))
-        kesken (fn [valitavoitteet]
-                 (filter
-                   (fn [valitavoite]
-                     (and (:takaraja valitavoite)
-                          (pvm/ennen? (t/now) (c/from-date (:takaraja valitavoite)))
-                          (not (:valmis-pvm valitavoite))))
-                   valitavoitteet))
-        toteumatta (fn [valitavoitteet]
-                     (filter
-                       (fn [valitavoite]
-                         (and (:takaraja valitavoite)
-                              (not (:valmis-pvm valitavoite))
-                              (pvm/jalkeen? (t/now)
-                                            (c/from-date (:takaraja valitavoite)))))
-                       valitavoitteet))
-        valitavoiterivi (fn [valitavoite]
+  (let [valitavoiterivi (fn [valitavoite]
                           [(:nimi valitavoite)
                            (pvm/pvm-opt (:takaraja valitavoite))
                            (pvm/pvm-opt (:valmis-pvm valitavoite))
                            (:valmis-kommentti valitavoite)])]
     (into [] (concat
                [{:otsikko "Ajoissa toteutuneet"}]
-               (mapv valitavoiterivi (ajoissa valitavoitteet))
+               (mapv valitavoiterivi (suodata-ajoissa valitavoitteet))
                [{:otsikko "Myöhässä toteutuneet"}]
-               (mapv valitavoiterivi (myohassa valitavoitteet))
+               (mapv valitavoiterivi (suodata-myohassa valitavoitteet))
                [{:otsikko "Kesken"}]
-               (mapv valitavoiterivi (kesken valitavoitteet))
+               (mapv valitavoiterivi (suodata-kesken valitavoitteet))
                [{:otsikko "Toteutumatta jääneet"}]
-               (mapv valitavoiterivi (toteumatta valitavoitteet))))))
+               (mapv valitavoiterivi (suodata-toteumatta valitavoitteet))))))
 
 (defn muodosta-otsikkorivit []
   [{:otsikko "Välitavoite" :leveys 10}
@@ -80,4 +85,15 @@
                  :tyhja (when (empty? datarivit) "Ei raportoitavia välitavoitteita.")
                  :sheet-nimi raportin-nimi}
       otsikkorivit
+      datarivit]
+     ;; Yhteenveto
+     [:taulukko {:otsikko "Osuudet"
+                 :tyhja (when (empty? datarivit) "Ei raportoitavia välitavoitteita.")
+                 :sheet-nimi raportin-nimi}
+      [{:otsikko "Ryhmä" :leveys 50}
+       {:otsikko "Osuus" :leveys 50}]
+      [["Ajoissa toteutuneet" (math/osuus-prosentteina (suodata-ajoissa valitavoitteet) valitavoitteet)]
+       ["Myöhässä toteutuneet" (math/osuus-prosentteina (suodata-myohassa valitavoitteet) valitavoitteet)]
+       ["Kesken" (math/osuus-prosentteina (suodata-kesken valitavoitteet) valitavoitteet)]
+       ["Toteutumatta jääneet" (math/osuus-prosentteina (suodata-toteumatta valitavoitteet) valitavoitteet)]]
       datarivit]]))
