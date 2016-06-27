cd82fb22001e697d915becb1f9c8e5bd55ec1222
Jari Hanhela <jarihanhela@gmail.com>
2016-06-27 10:02:24 +0300

    Lisää migraatiossa uusi raportti: Välitavoiteraportti

---
+++ tietokanta/src/main/resources/db/migration/V1_381__.sql
@@ -0,0 +1,8 @@
+-- Välitavoiteraportti
+INSERT INTO raportti (nimi, kuvaus, konteksti, parametrit, koodi, urakkatyyppi) VALUES (
+ 'valitavoiteraportti', 'Välitavoiteraportti',
+ ARRAY['urakka'::raporttikonteksti],
+ ARRAY[NULL::raporttiparametri],
+ '#''harja.palvelin.raportointi.raportit.valitavoiteraportti/suorita',
+ 'hoito'::urakkatyyppi
+);
