d63f30923d7b39f51d852cf04d35382ca56e574d
Jari Hanhela <jarihanhela@gmail.com>
2016-06-29 13:59:22 +0300

    Lisää välitavoitteelle myös urakkatyyppi, jota tavoite koskee

--- tietokanta/src/main/resources/db/migration/V1_382__.sql
+++ tietokanta/src/main/resources/db/migration/V1_382__.sql
@@ -1,6 +1,6 @@
 -- Välitavoitteelle valtakunnallisuus ja linkitys toiseen (valtakunnalliseen) välitavoitteeseen
 CREATE TYPE valitavoite_tyyppi AS ENUM ('kertaluontoinen','toistuva');
-
 ALTER TABLE valitavoite ADD COLUMN tyyppi valitavoite_tyyppi;
 ALTER TABLE valitavoite ADD COLUMN toistopvm DATE;
-ALTER TABLE valitavoite ADD COLUMN valtakunnallinen_valitavoite integer REFERENCES valitavoite (id);
+ALTER TABLE valitavoite ADD COLUMN urakkatyyppi urakkatyyppi; -- Urakkatyyppi jota valtakunnallinen tavoite koskee
+ALTER TABLE valitavoite ADD COLUMN valtakunnallinen_valitavoite integer REFERENCES valitavoite (id); -- Viittaus välitavoitteeseen ilman urakka-id:tä
