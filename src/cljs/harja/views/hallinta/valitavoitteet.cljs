482d613df16a1871f9e4a85dcccd5b0bee2bb024
Jari Hanhela <jarihanhela@gmail.com>
2016-06-29 14:59:44 +0300

    Erottele kaksi eri välitavoite-gridiä

--- src/cljs/harja/views/hallinta/valitavoitteet.cljs
+++ src/cljs/harja/views/hallinta/valitavoitteet.cljs
@@ -17,11 +17,12 @@
                    [reagent.ratom :refer [reaction run!]]))
 
 (defn valitavoitteet-grid [tavoitteet-atom]
-  [grid/grid
-   {:otsikko "Valtakunnalliset välitavoitteet"
+  [:div
+   [grid/grid
+   {:otsikko "Valtakunnalliset kertaluontoiset välitavoitteet"
     :tyhja (if (nil? @tavoitteet-atom)
              [y/ajax-loader "Välitavoitteita haetaan..."]
-             "Ei valtakunnallisia välitavoitteita")
+             "Ei kertaluontoisia välitavoitteita")
     :tallenna (when (oikeudet/voi-kirjoittaa? oikeudet/hallinta-valitavoitteet)
                 #(go (let [vastaus (<! (tiedot/tallenna-valitavoitteet %))]
                        (if (k/virhe? vastaus)
@@ -31,12 +32,37 @@
    [{:otsikko "Nimi" :leveys 60 :nimi :nimi :tyyppi :string :pituus-max 128}
     {:otsikko "Urakkatyyppi" :leveys 20 :nimi :urakkatyyppi
      :tyyppi :valinta
-     :validoi [[:ei-tyhja "Valitse materiaali"]]
+     :validoi [[:ei-tyhja "Valitse urakkatyyppi, jota tämä välitavoite koskee"]]
      :valinta-arvo :arvo
      :valinta-nayta #(or (:nimi %) "- valitse -")
      :valinnat nav/+urakkatyypit+}
+
     {:otsikko "Takaraja" :leveys 20 :nimi :takaraja :fmt pvm/pvm-opt :tyyppi :pvm}]
-   @tavoitteet-atom])
+    (filter #(= (:tyyppi %) :kertaluontoinen) @tavoitteet-atom)]
+
+   [grid/grid
+    {:otsikko "Valtakunnalliset vuosittain toistuvat välitavoitteet"
+     :tyhja (if (nil? @tavoitteet-atom)
+              [y/ajax-loader "Välitavoitteita haetaan..."]
+              "Ei toistuvia välitavoitteita")
+     :tallenna (when (oikeudet/voi-kirjoittaa? oikeudet/hallinta-valitavoitteet)
+                 #(go (let [vastaus (<! (tiedot/tallenna-valitavoitteet %))]
+                        (if (k/virhe? vastaus)
+                          (viesti/nayta! "Välitavoitteiden tallentaminen epännistui"
+                                         :warning viesti/viestin-nayttoaika-keskipitka)
+                          (reset! tavoitteet-atom vastaus)))))}
+    [{:otsikko "Nimi" :leveys 60 :nimi :nimi :tyyppi :string :pituus-max 128}
+     {:otsikko "Urakkatyyppi" :leveys 20 :nimi :urakkatyyppi
+      :tyyppi :valinta
+      :validoi [[:ei-tyhja "Valitse urakkatyyppi, jota tämä välitavoite koskee"]]
+      :valinta-arvo :arvo
+      :valinta-nayta #(or (:nimi %) "- valitse -")
+      :valinnat nav/+urakkatyypit+}
+     {:otsikko "Takaraja-päivä" :leveys 10 :nimi :takaraja-paiva
+      :tyyppi :numero :desimaalien-maara 0 :validoi [[:rajattu-numero nil 1 31 "Anna päivä välillä 1 - 31"]]}
+     {:otsikko "Takaraja-kuukausi" :leveys 10 :nimi :takaraja-kuukausi
+      :tyyppi :numero :desimaalien-maara 0 :validoi [[:rajattu-numero nil 1 12 "Anna kuukausi välillä 1 - 12"]]}]
+    (filter #(= (:tyyppi %) :toistuva) @tavoitteet-atom)]])
 
 (defn valitavoitteet []
   (komp/luo
--- tietokanta/src/main/resources/db/migration/V1_382__.sql
+++ tietokanta/src/main/resources/db/migration/V1_382__.sql
@@ -1,6 +1,7 @@
 -- Välitavoitteelle valtakunnallisuus ja linkitys toiseen (valtakunnalliseen) välitavoitteeseen
 CREATE TYPE valitavoite_tyyppi AS ENUM ('kertaluontoinen','toistuva');
 ALTER TABLE valitavoite ADD COLUMN tyyppi valitavoite_tyyppi;
-ALTER TABLE valitavoite ADD COLUMN toistopvm DATE;
+ALTER TABLE valitavoite ADD COLUMN toistopaiva INT CHECK (toistopaiva > 0 AND toistopaiva <= 31);
+ALTER TABLE valitavoite ADD COLUMN toistokuukausi INT CHECK (toistokuukausi > 0 AND toistokuukausi <= 12);
 ALTER TABLE valitavoite ADD COLUMN urakkatyyppi urakkatyyppi; -- Urakkatyyppi jota valtakunnallinen tavoite koskee
 ALTER TABLE valitavoite ADD COLUMN valtakunnallinen_valitavoite integer REFERENCES valitavoite (id); -- Viittaus välitavoitteeseen ilman urakka-id:tä
