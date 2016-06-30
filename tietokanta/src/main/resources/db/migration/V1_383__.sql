e14a6f9ba7bb1c9dd38e6fff402cac9ad40009f5
Jari Hanhela <jarihanhela@gmail.com>
2016-06-30 14:16:20 +0300

    Merge branch 'develop' into HAR-2579
    # Conflicts:
    #	tietokanta/src/main/resources/db/migration/V1_382__.sql

--- tietokanta/src/main/resources/db/migration/V1_383__.sql
+++ tietokanta/src/main/resources/db/migration/V1_383__.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- Välitavoitteelle valtakunnallisuus ja linkitys toiseen (valtakunnalliseen) välitavoitteeseen
 CREATE TYPE valitavoite_tyyppi AS ENUM ('kertaluontoinen','toistuva');
 ALTER TABLE valitavoite ADD COLUMN tyyppi valitavoite_tyyppi;
@@ -6,13 +5,3 @@ ALTER TABLE valitavoite ADD COLUMN takaraja_toistopaiva INT CHECK (takaraja_tois
 ALTER TABLE valitavoite ADD COLUMN takaraja_toistokuukausi INT CHECK (takaraja_toistokuukausi > 0 AND takaraja_toistokuukausi <= 12);
 ALTER TABLE valitavoite ADD COLUMN urakkatyyppi urakkatyyppi; -- Urakkatyyppi jota valtakunnallinen tavoite koskee
 ALTER TABLE valitavoite ADD COLUMN valtakunnallinen_valitavoite integer REFERENCES valitavoite (id); -- Viittaus välitavoitteeseen ilman urakka-id:tä
-=======
--- Välitavoiteraportti
-INSERT INTO raportti (nimi, kuvaus, konteksti, parametrit, koodi, urakkatyyppi) VALUES (
- 'valitavoiteraportti', 'Välitavoiteraportti',
- ARRAY['urakka'::raporttikonteksti],
- ARRAY[]::raporttiparametri[],
- '#''harja.palvelin.raportointi.raportit.valitavoiteraportti/suorita',
- 'hoito'::urakkatyyppi
-);
->>>>>>> develop
