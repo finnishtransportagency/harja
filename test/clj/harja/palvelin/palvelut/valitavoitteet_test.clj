44beb7b7ac12853400c3fb3ed490ed15fc15b6dd
Jari Hanhela <jarihanhela@gmail.com>
2016-07-01 13:30:02 +0300

    Luo yksinkertainen testi uusien valtakunnallisten välitavoitteiden lisäämiselle

--- test/clj/harja/palvelin/palvelut/valitavoitteet_test.clj
+++ test/clj/harja/palvelin/palvelut/valitavoitteet_test.clj
@@ -4,19 +4,22 @@
             [harja.palvelin.komponentit.tietokanta :as tietokanta]
             [harja.palvelin.palvelut.valitavoitteet :refer :all]
             [harja.testi :refer :all]
-            [com.stuartsierra.component :as component]))
+            [com.stuartsierra.component :as component]
+            [harja.testi :as testi]
+            [clj-time.core :as t]
+            [clj-time.coerce :as c]))
 
 
 (defn jarjestelma-fixture [testit]
   (alter-var-root #'jarjestelma
-    (fn [_]
-      (component/start
-        (component/system-map
-          :db (tietokanta/luo-tietokanta testitietokanta)
-          :http-palvelin (testi-http-palvelin)
-          :hae (component/using
-                      (->Valitavoitteet)
-                      [:http-palvelin :db])))))
+                  (fn [_]
+                    (component/start
+                      (component/system-map
+                        :db (tietokanta/luo-tietokanta testitietokanta)
+                        :http-palvelin (testi-http-palvelin)
+                        :hae (component/using
+                               (->Valitavoitteet)
+                               [:http-palvelin :db])))))
 
   (testit)
   (alter-var-root #'jarjestelma component/stop))
@@ -30,3 +33,25 @@
 
     (log/debug "Vastaus: " vastaus)
     (is (>= (count vastaus) 4))))
+
+
+(deftest urakan-valitavoitteiden-haku-toimii
+  (let [vastaus (kutsu-palvelua
+                  (:http-palvelin jarjestelma)
+                  :tallenna-valtakunnalliset-valitavoitteet
+                  +kayttaja-jvh+
+                  {:valitavoitteet [{:id -2, :nimi "Kertaluontoinen",
+                                     :takaraja (c/to-date (t/plus (t/now) (t/years 5))),
+                                     :tyyppi :kertaluontoinen, :urakkatyyppi :hoito,
+                                     :takaraja-toistopaiva nil, :takaraja-toistokuukausi nil}
+                                    {:id -5, :nimi "Sepon mökkitien vuosittainen auraus",
+                                     :takaraja nil, :tyyppi :toistuva,
+                                     :urakkatyyppi :hoito, :takaraja-toistopaiva 1,
+                                     :takaraja-toistokuukausi 7}]})]
+
+    (log/debug "Vastaus: " vastaus)
+    (is (= (count vastaus) 2))
+    (is (= (count (filter #(= (:tyyppi %) :kertaluontoinen) vastaus)) 1))
+    (is (= (count (filter #(= (:tyyppi %) :toistuva) vastaus)) 1))
+    (u (str "DELETE FROM valitavoite WHERE valtakunnallinen_valitavoite IS NOT NULL"))
+    (u (str "DELETE FROM valitavoite WHERE urakka IS NULL"))))
