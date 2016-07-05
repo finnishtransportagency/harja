841405f49e6a3f3cada71e805a90ca6aa2bd5710
Jari Hanhela <jarihanhela@gmail.com>
2016-07-05 12:15:49 +0300

    Paranna välitavoitteiden vihjetta

--- src/cljs/harja/views/hallinta/valtakunnalliset_valitavoitteet.cljs
+++ src/cljs/harja/views/hallinta/valtakunnalliset_valitavoitteet.cljs
@@ -90,11 +90,12 @@
      {:otsikko "Taka\u00ADrajan toisto\u00ADkuu\u00ADkausi" :leveys 10 :nimi :takaraja-toistokuukausi
       :tyyppi :numero :desimaalien-maara 0 :validoi [[:rajattu-numero nil 1 12 "Anna kuukausi välillä 1 - 12"]]}]
     (sort-by :takaraja @toistuvat-valitavoitteet-atom)]
-   [yleiset/vihje
-    "Uudet kertaluontoiset välitavoitteet liitetään valituntyyppisiin urakoihin jos välitavoitteen takaraja on urakan voimassaoloaikana eikä urakka ole päättynyt.
-     Toistuvat välitavoitteet liitetään valituntyyppisiin ei-päättyneisiin urakoihin kertaalleen per jäljellä oleva urakkavuosi.
-     Välitavoitteen päivittäminen päivittää tiedot urakoihin, ellei tavoitetta ole muokattu urakassa.
-     Poistettu välitavoite jää näkyviin päättyneisiin urakoihin tai jos se on ehditty tehdä valmiiksi."]])
+   [yleiset/vihje-elementti
+    [:span
+     "Uudet kertaluontoiset välitavoitteet liitetään valituntyyppisiin ei-päättyneisiin urakoihin jos välitavoitteen takaraja on urakan voimassaoloaikana."
+     [:br]"Uudet Toistuvat välitavoitteet liitetään valituntyyppisiin ei-päättyneisiin urakoihin kertaalleen per jäljellä oleva urakkavuosi."
+     [:br]"Välitavoitteen päivittäminen päivittää tiedot urakoihin, ellei tavoitetta ole muokattu urakassa."
+     [:br]"Poistettu välitavoite jää näkyviin päättyneisiin urakoihin tai jos se on ehditty tehdä valmiiksi."]]])
 
 (defn valitavoitteet []
   (komp/luo
