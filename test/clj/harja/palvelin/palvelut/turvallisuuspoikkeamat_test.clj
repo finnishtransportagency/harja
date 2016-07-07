83f2b279189f7fcd4c8cc82a11c094fb5ec4c44b
Jari Hanhela <jarihanhela@gmail.com>
2016-07-07 11:27:27 +0300

    Tarkenna turvallisuuspoikkeaman palvelun testiä

--- test/clj/harja/palvelin/palvelut/turvallisuuspoikkeamat_test.clj
+++ test/clj/harja/palvelin/palvelut/turvallisuuspoikkeamat_test.clj
@@ -39,8 +39,8 @@
     (is (match vastaus [{:id _
                          :ilmoituksetlahetetty nil
                          :kasitelty (_ :guard #(and (= (t/year (c/from-sql-date %)) 2005)
-                                                       (= (t/month (c/from-sql-date %)) 10)
-                                                       (= (t/day (c/from-sql-date %)) 5)))
+                                                    (= (t/month (c/from-sql-date %)) 10)
+                                                    (= (t/day (c/from-sql-date %)) 5)))
                          :kommentti {:tyyppi nil}
                          :korjaavattoimenpiteet []
                          :kuvaus "Sepolla oli kiire lastata laatikot, ja torni kaatui päälle. Ehti onneksi pois alta niin ei henki lähtenyt."
@@ -117,5 +117,124 @@
            :hoitokausi hoitokausi}))
 
     (is (= (hae-tp-maara) (+ 1 vanha-maara)))
+
+    ;; Tiukka testi, datan pitää olla tallentunut oikein
+    (let [uusin-tp (as-> (first (q (str "SELECT
+                                  id,
+                                  urakka,
+                                  tapahtunut,
+                                  kasitelty,
+                                  sijainti,
+                                  kuvaus,
+                                  sairauspoissaolopaivat,
+                                  sairaalavuorokaudet,
+                                  tr_numero,
+                                  tr_alkuosa,
+                                  tr_alkuetaisyys,
+                                  tr_loppuosa,
+                                  tr_loppuetaisyys,
+                                  vahinkoluokittelu,
+                                  vakavuusaste,
+                                  tyyppi,
+                                  tyontekijanammatti,
+                                  tyontekijanammatti_muu,
+                                  aiheutuneet_seuraukset,
+                                  vammat,
+                                  vahingoittuneet_ruumiinosat,
+                                  sairauspoissaolo_jatkuu,
+                                  ilmoittaja_etunimi,
+                                  ilmoittaja_sukunimi,
+                                  vaylamuoto,
+                                  toteuttaja,
+                                  tilaaja,
+                                  turvallisuuskoordinaattori_etunimi,
+                                  turvallisuuskoordinaattori_sukunimi,
+                                  laatija_etunimi,
+                                  laatija_sukunimi,
+                                  tapahtuman_otsikko,
+                                  paikan_kuvaus,
+                                  vaarallisten_aineiden_kuljetus,
+                                  vaarallisten_aineiden_vuoto
+                                  FROM turvallisuuspoikkeama
+                                  ORDER BY luotu DESC
+                                  LIMIT 1;")))
+                         turpo
+                         ;; Tapahtumapvm ja käsittely -> clj-time
+                         (assoc turpo 2 (c/from-sql-date (get turpo 2)))
+                         (assoc turpo 3 (c/from-sql-date (get turpo 3)))
+                         ;; Vahinkoluokittelu -> set
+                         (assoc turpo 13 (into #{} (.getArray (get turpo 13))))
+                         ;; Tyyppi -> set
+                         (assoc turpo 15 (into #{} (.getArray (get turpo 15))))
+                         ;; Vammat -> set
+                         (assoc turpo 19 (into #{} (.getArray (get turpo 19))))
+                         ;; Vahingoittuneet ruumiinosat -> set
+                         (assoc turpo 20 (into #{} (.getArray (get turpo 20)))))]
+      (is (vector uusin-tp))
+      (is (match uusin-tp [_
+                           1
+                           (_ :guard #(and (= (t/year %) 2005)
+                                           (= (t/month %) 9)
+                                           (= (t/day %) 30)))
+                           (_ :guard #(and (= (t/year %) 2005)
+                                           (= (t/month %) 9)
+                                           (= (t/day %) 30)))
+                           (_ :guard #(some? %))
+                           "e2e taas punaisena"
+                           0
+                           0
+                           1
+                           4
+                           2
+                           5
+                           3
+                           #{"ymparistovahinko"}
+                           "lieva"
+                           #{"tyotapaturma"}
+                           "kuorma-autonkuljettaja"
+                           nil
+                           nil
+                           #{"luunmurtumat"}
+                           #{}
+                           nil
+                           nil
+                           nil
+                           "tie"
+                           nil
+                           nil
+                           nil
+                           nil
+                           nil
+                           nil
+                           "Kävi möhösti"
+                           nil
+                           false
+                           false]
+                 true))
+
+      ;; Myös korjaava toimenpide kirjattu täysin oikein
+      (let [turpo-id (first uusin-tp)
+              korjaava-toimenpide (as-> (first (q (str "SELECT
+                                                      kuvaus,
+                                                      suoritettu,
+                                                      otsikko,
+                                                      vastuuhenkilo,
+                                                      toteuttaja,
+                                                      tila
+                                                      FROM korjaavatoimenpide
+                                                      WHERE turvallisuuspoikkeama = " turpo-id ";")))
+                                        toimenpide
+                                        (assoc toimenpide 1 (c/from-sql-date (get toimenpide 1))))]
+
+          (is (number? turpo-id))
+          (is (vector korjaava-toimenpide))
+          (is (match korjaava-toimenpide
+                     ["Ei ressata liikaa"
+                      nil
+                      "Ressi pois!"
+                      nil
+                      nil
+                      "avoin"]
+                     true))))
 
     (poista-tp-taulusta "e2e taas punaisena")))
