91e13bec7bd817d4b6568b3bf84f40fd7276060f
Jari Hanhela <jarihanhela@gmail.com>
2016-07-08 11:56:12 +0300

    Käytä sanomamuodostuksen testissä TURI-komponentin omaa rajapintaa turpon hakemiseen

--- test/clj/harja/palvelin/integraatiot/turi/turvallisuuspoikkeamasanoma_test.clj
+++ test/clj/harja/palvelin/integraatiot/turi/turvallisuuspoikkeamasanoma_test.clj
@@ -3,18 +3,11 @@
             [org.httpkit.fake :refer [with-fake-http]]
             [harja.testi :refer :all]
             [taoensso.timbre :as log]
-            [harja.kyselyt.turvallisuuspoikkeamat :as q]
             [harja.palvelin.integraatiot.turi.turvallisuuspoikkeamasanoma :as sanoma]
             [harja.tyokalut.xml :as xml]
-            [harja.kyselyt.konversio :as konv]))
+            [harja.palvelin.integraatiot.turi.turi-komponentti :as turi]))
 
 (deftest tarkista-sanoman-muodostus
-  (let [data (first (konv/sarakkeet-vektoriin
-                (into []
-                      q/turvallisuuspoikkeama-xf
-                      (q/hae-turvallisuuspoikkeama (luo-testitietokanta) 1))
-                {:korjaavatoimenpide :korjaavattoimenpiteet
-                 :kommentti :kommentit
-                 :liite :liitteet}))
+  (let [data (turi/hae-turvallisuuspoikkeama (luo-liitteidenhallinta) (luo-testitietokanta) 1)
         xml (sanoma/muodosta data)]
     (is (xml/validoi "xsd/turi/" "poikkeama-rest.xsd" xml)) "Tehty sanoma on XSD-skeeman mukainen"))
