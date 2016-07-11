54643e0bc634d80a8e62d4dd4e7c901949aeee88
Jari Hanhela <jarihanhela@gmail.com>
2016-07-11 16:08:56 +0300

    LIsää testidataan valtakunnallisia tavoitteita Sampo-testiä varten

--- tietokanta/testidata/valitavoitteet.sql
+++ tietokanta/testidata/valitavoitteet.sql
@@ -1,33 +1,69 @@
+-- Urakkakohtaiset
+
 INSERT INTO valitavoite (urakka, nimi, takaraja, valmis_pvm, valmis_kommentti, poistettu)
 VALUES ((SELECT id
-                   FROM   urakka
-                   WHERE  nimi = 'Oulun alueurakka 2014-2019'),
-                   'Koko Suomi aurattu',
-                   '2014-05-29',
-                   '2014-05-01',
-                   'Homma hoidettu hyvästi ennen tavoitepäivää!',
-                   false),
-                   ((SELECT id
-                   FROM   urakka
-                   WHERE  nimi = 'Oulun alueurakka 2014-2019'),
-                   'Pelkosentie 678 suolattu',
-                   '2015-09-23',
-                   '2015-09-25',
-                   'Aurattu, mutta vähän tuli myöhässä',
-                   false),
-                   ((SELECT id
-                   FROM   urakka
-                   WHERE  nimi = 'Oulun alueurakka 2014-2019'),
-                   'Sepon mökkitie suolattu',
-                   '2014-12-24',
-                   NULL,
-                   NULL,
-                   false),
-                   ((SELECT id
-                   FROM   urakka
-                   WHERE  nimi = 'Oulun alueurakka 2014-2019'),
-                   'Oulaisten liikenneympyrä aurattu',
-                   '2050-1-1',
-                   NULL,
-                   NULL,
-                   false)
+         FROM   urakka
+         WHERE  nimi = 'Oulun alueurakka 2014-2019'),
+         'Koko urakan alue aurattu',
+         '2014-05-29',
+         '2014-05-01',
+         'Homma hoidettu hyvästi ennen tavoitepäivää!',
+         false),
+         ((SELECT id
+         FROM   urakka
+         WHERE  nimi = 'Oulun alueurakka 2014-2019'),
+         'Pelkosentie 678 suolattu',
+         '2015-09-23',
+         '2015-09-25',
+         'Aurattu, mutta vähän tuli myöhässä',
+         false),
+         ((SELECT id
+         FROM   urakka
+         WHERE  nimi = 'Oulun alueurakka 2014-2019'),
+         'Sepon mökkitie suolattu',
+         '2014-12-24',
+         NULL,
+         NULL,
+         false),
+         ((SELECT id
+         FROM   urakka
+         WHERE  nimi = 'Oulun alueurakka 2014-2019'),
+         'Oulaisten liikenneympyrä aurattu',
+         '2050-1-1',
+         NULL,
+         NULL,
+         false);
+
+-- Valtakunnalliset
+
+INSERT INTO valitavoite (urakka, nimi, takaraja, tyyppi, poistettu)
+VALUES (null,
+       'Koko Suomi aurattu',
+       '2019-05-29',
+       'kertaluontoinen'::valitavoite_tyyppi,
+       false),
+       (null,
+       'Liikennemerkit tarkistettu',
+       '2015-05-29',
+       'kertaluontoinen'::valitavoite_tyyppi,
+       true),
+       (null,
+       'Kaikkien urakoiden kalusto huollettu',
+       null,
+       'kertaluontoinen'::valitavoite_tyyppi,
+       false),
+       (null,
+       'Koko Suomi suolattu',
+       '2005-8-23',
+       'kertaluontoinen'::valitavoite_tyyppi,
+       false),
+       (null,
+       'Koko Suomen liikenneympäristö hoidettu',
+       '2019-12-12',
+       'toistuva'::valitavoite_tyyppi,
+       false),
+       (null,
+       'Kaikki tiet putsattu',
+       '2005-12-12',
+       'toistuva'::valitavoite_tyyppi,
+       false);
