afcc12945b0f15156f1cf79b1dfa642b36f13a1d
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-07-12 14:31:50 +0300

    Lisää liikenneohjausaidoille taulu

--- tietokanta/src/main/resources/db/migration/V1_390__.sql
+++ tietokanta/src/main/resources/db/migration/V1_390__.sql
@@ -1,38 +1,12 @@
-CREATE TYPE suoritettavatehtava AS
-ENUM ('auraus ja sohjonpoisto',
-  'suolaus',
-  'pistehiekoitus',
-  'linjahiekoitus',
-  'pinnan tasaus',
-  'liikennemerkkien puhdistus',
-  'lumivallien madaltaminen',
-  'sulamisveden haittojen torjunta',
-  'tiestotarkastus',
-  'kelintarkastus',
-  'harjaus',
-  'koneellinen niitto',
-  'koneellinen vesakonraivaus',
-  'sorateiden muokkaushoylays',
-  'sorateiden polynsidonta',
-  'sorateiden tasaus',
-  'sorastus',
-  'paallysteiden paikkaus',
-  'paallysteiden juotostyot',
-  'siltojen puhdistus',
-  'l- ja p-alueiden puhdistus',
-  'muu'
-);
-
-CREATE TABLE liikenneohjausaitahavainto (
-  tyokoneid      INTEGER PRIMARY KEY,
-  jarjestelma    VARCHAR(128) NOT NULL,
-  organisaatio   INTEGER REFERENCES organisaatio (id),
-  viestitunniste INTEGER      NOT NULL,
-  lahetysaika    TIMESTAMP    NOT NULL,
-  vastaanotettu  TIMESTAMP DEFAULT current_timestamp,
-  tyokonetyyppi  VARCHAR(64)  NOT NULL,
-  sijainti       POINT        NOT NULL,
-  urakkaid       INTEGER REFERENCES urakka (id),
-  sopimusid      INTEGER REFERENCES sopimus (id),
-  tehtavat       suoritettavatehtava []
+CREATE TABLE liikenneohjausaidat (
+  id            INTEGER PRIMARY KEY,
+  jarjestelma   VARCHAR(128) NOT NULL,
+  aita_id       INTEGER      NOT NULL,
+  sijainti      POINT        NOT NULL,
+  vastaanotettu TIMESTAMP DEFAULT current_timestamp,
+  asetettu      TIMESTAMP,
+  muokattu      TIMESTAMP,
+  poistettu     TIMESTAMP,
+  yllapitokohde INTEGER REFERENCES yllapitokohde (id),
+  UNIQUE (jarjestelma, aita_id)
 );
