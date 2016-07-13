fa751e9b6b05191dae2c782949811648b6650f81
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-07-13 08:43:20 +0300

    Lisää kaista ja ajorata liikenneohjausaidalle

--- tietokanta/src/main/resources/db/migration/V1_390__.sql
+++ tietokanta/src/main/resources/db/migration/V1_390__.sql
@@ -7,6 +7,8 @@ CREATE TABLE liikenneohjausaidat (
   asetettu      TIMESTAMP,
   muokattu      TIMESTAMP,
   poistettu     TIMESTAMP,
+  kaista        INTEGER,
+  ajorata       INTEGER,
   yllapitokohde INTEGER REFERENCES yllapitokohde (id),
   UNIQUE (jarjestelma, aita_id)
 );
