535409f4f27b7cfbe8ea53cb05c55d994e8bd33b
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-07-13 15:12:40 +0300

    Testaa suljettujen tieosien käsittely API:n kautta

--- test/clj/harja/palvelin/integraatiot/api/suljetut_tieosat_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/suljetut_tieosat_test.clj
@@ -15,14 +15,33 @@
 
 (use-fixtures :once jarjestelma-fixture)
 
-(deftest tarkista-suljetun-tieosan-kirjaus
-  (let [data (slurp "test/resurssit/api/suljetun-tieosuuden-kirjaus.json")]
-    (api-tyokalut/post-kutsu ["/api/urakat/5/yllapitokohteet/5/suljettu-tieosuus"] kayttaja portti data)
-    (let [uusi-suljettu-tieosuus  (q (str "SELECT id, muokattu FROM suljettu_tieosuus WHERE osuus_id = 1 AND jarjestelma = 'Urakoitsijan järjestelmä' AND yllapitokohde = 5;"))
+(defn hae-suljettu-tieosuus []
+  (first (q (str
+              "SELECT id, muokattu, poistettu FROM suljettu_tieosuus
+               WHERE osuus_id = 1 AND jarjestelma = 'Urakoitsijan järjestelmä' AND yllapitokohde = 5;"))))
+
+(deftest tarkista-suljetun-tieosan-kasittely
+  (let [lisays-kutsu (slurp "test/resurssit/api/suljetun-tieosuuden-kirjaus.json")
+        poisto-kutsu (slurp "test/resurssit/api/suljetun-tieosuuden-poisto.json")]
+    (api-tyokalut/post-kutsu ["/api/urakat/5/yllapitokohteet/5/suljettu-tieosuus"] kayttaja portti lisays-kutsu)
+    (let [uusi-suljettu-tieosuus (hae-suljettu-tieosuus)
           id (first uusi-suljettu-tieosuus)
           muokattu (second uusi-suljettu-tieosuus)]
-
       (is (not (nil? id)) "Kannasta löytyy uusi suljettu tieosuus")
-      (is (nil? muokattu)) "Muokkauspäivämäärä on tyhjä")))
+      (is (nil? muokattu)) "Muokkauspäivämäärä on tyhjä")
+
+    (api-tyokalut/post-kutsu ["/api/urakat/5/yllapitokohteet/5/suljettu-tieosuus"] kayttaja portti lisays-kutsu)
+    (let [muokattu-suljettu-tieosuus (hae-suljettu-tieosuus)
+          id (first muokattu-suljettu-tieosuus)
+          muokattu (second muokattu-suljettu-tieosuus)]
+      (is (not (nil? id)) "Kannasta löytyy yha sama suljettu tieosuus")
+      (is (not (nil? muokattu))) "Tieosuus on merkitty muuttuneeksi")
+
+    (api-tyokalut/delete-kutsu ["/api/urakat/5/yllapitokohteet/5/suljettu-tieosuus"] kayttaja portti poisto-kutsu)
+    (let [muokattu-suljettu-tieosuus (hae-suljettu-tieosuus)
+          id (first muokattu-suljettu-tieosuus)
+          muokattu (nth muokattu-suljettu-tieosuus 2)]
+      (is (not (nil? id)) "Kannasta löytyy yha sama suljettu tieosuus")
+      (is (not (nil? muokattu))) "Tieosuus on merkitty poistetuksi")))
 
 
