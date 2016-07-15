8f4834cfb332850cca855617706d02192bc939cb
Jari Hanhela <jarihanhela@gmail.com>
2016-07-15 08:52:50 +0300

    Lisää uusi (alustava) migraatio uusista reaaliaikaseurannan työkoneen tehtävistä

---
+++ tietokanta/src/main/resources/db/migration/V1_392__.sql
@@ -0,0 +1,50 @@
+-- Reaaliaikaseurannan uudet tehtävät
+
+ALTER TYPE suoritettavatehtava RENAME TO suoritettavatehtava_tmp;
+
+CREATE TYPE suoritettavatehtava AS
+ ENUM ('auraus ja sohjonpoisto',
+       'suolaus',
+       'pistehiekoitus',
+       'linjahiekoitus',
+       'pinnan tasaus',
+       'liikennemerkkien puhdistus',
+       'lumivallien madaltaminen',
+       'sulamisveden haittojen torjunta',
+       'tiestotarkastus',
+       'kelintarkastus',
+       'harjaus',
+       'koneellinen niitto',
+       'koneellinen vesakonraivaus',
+       'sorateiden muokkaushoylays',
+       'sorateiden polynsidonta',
+       'sorateiden tasaus',
+       'sorastus',
+       'paallysteiden paikkaus',
+       'paallysteiden juotostyot',
+       'siltojen puhdistus',
+       'l- ja p-alueiden puhdistus',
+       'muu',
+       'liuossuolaus',
+       'aurausviitoitus ja kinostimet',
+       'lumensiirto',
+       'paannejaan poisto',
+
+       -- Ylläpidon uudet tyypit
+       'pääasfalttilevitin',
+       'remix-laite',
+       'sekoitus- ja stabilointijyrsin',
+       'tma-laite'
+
+       -- Nämä olivat skeemassa, mutta puuttuvat kannasta(?)
+       'liikennemerkkien, opasteiden ja liikenteenohjauslaitteiden hoito sekä reunapaalujen kunnossapito',
+       'lumen siirto',
+       'palteen poisto',
+       'päällystetyn tien sorapientareen täyttö'
+
+       );
+
+ALTER TABLE tyokonehavainto ALTER COLUMN tehtavat TYPE suoritettavatehtava[] USING tehtavat::text::suoritettavatehtava[];
+ALTER TABLE toimenpidekoodi ALTER COLUMN suoritettavatehtava TYPE suoritettavatehtava USING suoritettavatehtava::text::suoritettavatehtava;
+
+DROP TYPE suoritettavatehtava_tmp CASCADE;
