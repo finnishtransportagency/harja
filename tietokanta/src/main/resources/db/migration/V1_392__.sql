f366e045c3436e0346ec9503c81ccef66d02b957
Jari Hanhela <jarihanhela@gmail.com>
2016-07-15 09:18:26 +0300

    Päivitä työkoneseurannan migraatio ja skeema, lisättävät tehtävät raportoidaan työkonetyyppeinä

--- tietokanta/src/main/resources/db/migration/V1_392__.sql
+++ tietokanta/src/main/resources/db/migration/V1_392__.sql
@@ -30,14 +30,8 @@ CREATE TYPE suoritettavatehtava AS
        'lumensiirto',
        'paannejaan poisto',
 
-       -- Ylläpidon uudet tyypit
-       'pääasfalttilevitin',
-       'remix-laite',
-       'sekoitus- ja stabilointijyrsin',
-       'tma-laite'
-
        -- Nämä olivat skeemassa, mutta puuttuvat kannasta(?)
-       'liikennemerkkien ym. kunnossapito', -- max. 63 merkkiä, skeemassa: "liikennemerkkien, opasteiden ja liikenteenohjauslaitteiden hoito sekä reunapaalujen kunnossapito"
+       'liik., opast., ja ohjausl. hoito sekä reunapaalujen kun.pito', -- max. 63 merkkiä, skeemassa: "liikennemerkkien, opasteiden ja liikenteenohjauslaitteiden hoito sekä reunapaalujen kunnossapito"
        'lumen siirto',
        'palteen poisto',
        'päällystetyn tien sorapientareen täyttö');
@@ -46,3 +40,55 @@ ALTER TABLE tyokonehavainto ALTER COLUMN tehtavat TYPE suoritettavatehtava[] USI
 ALTER TABLE toimenpidekoodi ALTER COLUMN suoritettavatehtava TYPE suoritettavatehtava USING suoritettavatehtava::text::suoritettavatehtava;
 
 DROP TYPE suoritettavatehtava_tmp CASCADE;
+
+-- Muutos vaatii päivityksen tyokonehavainnon tallennusfunktioon
+CREATE OR REPLACE FUNCTION tallenna_tai_paivita_tyokonehavainto(
+      jarjestelma_ character varying,
+      organisaationimi_ character varying,
+      ytunnus_ character varying,
+      viestitunniste_ integer,
+      lahetysaika_ timestamp,
+      tyokoneid_ integer,
+      tyokonetyyppi_ character varying,
+      sijainti_ point,
+      suunta_ real,
+      urakkaid_ integer,
+      tehtavat_ suoritettavatehtava[]
+) RETURNS VOID AS
+$$
+DECLARE
+      organisaatioid integer;
+BEGIN
+      SELECT id INTO organisaatioid FROM organisaatio WHERE nimi=organisaationimi_ AND ytunnus=ytunnus_;
+
+      LOOP
+            UPDATE tyokonehavainto
+            SET jarjestelma=jarjestelma_,
+                  organisaatio=organisaatioid,
+                  viestitunniste=viestitunniste_,
+                  lahetysaika=lahetysaika_,
+                  vastaanotettu=DEFAULT,
+                  tyokonetyyppi=tyokonetyyppi_,
+                  sijainti=sijainti_,
+                  suunta=suunta_,
+                  urakkaid=urakkaid_,
+                  tehtavat=tehtavat_,
+                  edellinensijainti=sijainti
+            WHERE tyokoneid=tyokoneid_;
+            IF FOUND THEN
+                  RETURN;
+            END IF;
+
+            BEGIN
+                  INSERT INTO tyokonehavainto (jarjestelma, organisaatio, viestitunniste, lahetysaika,
+                                               tyokoneid, tyokonetyyppi, sijainti, urakkaid, tehtavat, suunta)
+                  VALUES (jarjestelma_, organisaatioid, viestitunniste_, lahetysaika_, tyokoneid_, tyokonetyyppi_, sijainti_,
+                          urakkaid_, tehtavat_, suunta_);
+                  RETURN;
+                  EXCEPTION WHEN unique_violation THEN
+                  -- retry
+            END;
+      END LOOP;
+END;
+$$
+LANGUAGE plpgsql;
