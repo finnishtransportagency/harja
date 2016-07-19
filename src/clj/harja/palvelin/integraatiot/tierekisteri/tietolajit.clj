80b9a51e4ccd7f199ad045b2f92f2b338acc975c
Jari Hanhela <jarihanhela@gmail.com>
2016-07-19 10:01:00 +0300

    Cacheta haetun tietolajin vastaus

--- src/clj/harja/palvelin/integraatiot/tierekisteri/tietolajit.clj
+++ src/clj/harja/palvelin/integraatiot/tierekisteri/tietolajit.clj
@@ -5,6 +5,20 @@
             [harja.palvelin.integraatiot.integraatiotapahtuma :as integraatiotapahtuma])
   (:use [slingshot.slingshot :only [try+ throw+]]))
 
+(def tietolajien-kuvaukset-cache
+  ^{:doc "Atomi, johon cachetetaan haettujen tietolajien ominaisuudet (kenttien kuvaukset)
+          aina kun haku tehdään."}
+  (atom {}))
+
+(defn cacheta-tietolajin-kuvaus
+  "Ottaa tierekisteristä saadun vastauksen haetulle tietolajille ja cachettaa sen."
+  [vastaus]
+  (let [tietolaji (:tietolaji vastaus)]
+    (swap! tietolajien-kuvaukset-cache
+           assoc
+           (:tunniste tietolaji)
+           (:ominaisuudet tietolaji))))
+
 (defn kasittele-tietolajin-hakuvastaus [url tunniste muutospvm vastaus-xml]
   (kasittele-vastaus
     vastaus-xml
@@ -23,4 +37,6 @@
                     (let [kutsudata (kutsusanoma/muodosta-kutsu tunniste muutospvm)
                           {vastaus-xml :body} (integraatiotapahtuma/laheta konteksti :http http-asetukset kutsudata)]
                       (kasittele-tietolajin-hakuvastaus url tunniste muutospvm vastaus-xml)))]
-    (integraatiotapahtuma/suorita-integraatio db integraatioloki "tierekisteri" "hae-tietolaji" tyonkulku)))
+    (let [vastaus (integraatiotapahtuma/suorita-integraatio db integraatioloki "tierekisteri" "hae-tietolaji" tyonkulku)]
+      (cacheta-tietolajin-kuvaus vastaus)
+      vastaus)))
