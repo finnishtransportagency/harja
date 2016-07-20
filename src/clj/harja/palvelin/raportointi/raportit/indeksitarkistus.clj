0e708febef6155897e3aa46fe76d2a7481344961
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-07-20 11:38:51 +0300

    Toimiva indeksitarkastusraportti

--- src/clj/harja/palvelin/raportointi/raportit/indeksitarkistus.clj
+++ src/clj/harja/palvelin/raportointi/raportit/indeksitarkistus.clj
@@ -3,16 +3,53 @@
              :refer [hae-laskutusyhteenvedon-tiedot]]
             [harja.pvm :as pvm]
             [harja.palvelin.raportointi.raportit.yleinen :as yleinen]
-            [taoensso.timbre :as log]))
+            [taoensso.timbre :as log]
+            [harja.tyokalut.functor :refer [fmap]]))
 
 (def ly (atom nil))
 
 (defn summa [laskutusyhteenvedot avain]
   (reset! ly laskutusyhteenvedot)
   (reduce + 0
-          (remove nil?
-                  (mapcat #(map avain %)
-                          laskutusyhteenvedot))))
+          (keep avain laskutusyhteenvedot)))
+
+(defn indeksitaulukko [nimi kuukaudet laskutusyhteenvedot-kk]
+  (let [summa-kk (memoize (fn [kk kentta]
+                            (summa (laskutusyhteenvedot-kk kk) kentta)))
+        suola? (#{"Kaikki yhteensä" "Talvihoito"} nimi)
+        kentat [:kht_laskutettu_ind_korotus
+                :yht_laskutetaan_ind_korotus
+                :akilliset_hoitotyot_laskutettu_ind_korotus
+                :sakot_laskutettu_ind_korotus]
+        kentat (if suola?
+                 (conj kentat :suolasakot_laskutettu_ind_korotus)
+                 kentat)]
+    [:taulukko {:otsikko nimi
+                :viimeinen-rivi-yhteenveto? true}
+     (into [{:otsikko "Kuukausi" :leveys 2}
+            {:otsikko "Kokonaishintaiset työt" :leveys 2 :fmt :raha :tasaa :oikea}
+            {:otsikko "Yksikköhintaiset työt" :leveys 2 :fmt :raha :tasaa :oikea}
+            {:otsikko "Äkillinen hoitotyö" :leveys 2 :fmt :raha :tasaa :oikea}
+            {:otsikko "Sanktiot" :leveys 2 :fmt :raha :tasaa :oikea}]
+           (concat
+            (when suola?
+              [{:otsikko "Suolabonukset ja -sanktiot" :leveys 2 :fmt :raha :tasaa :oikea}])
+            [{:otsikko "Yhteensä (€)" :leveys 2 :fmt :raha :tasaa :oikea}]))
+
+     (into
+      []
+      (concat
+       (for [[alku _ :as kk] kuukaudet
+             :let [kentan-arvot (map #(summa-kk kk %) kentat)]]
+         (into [(pvm/kuukauden-lyhyt-nimi (pvm/kuukausi alku))]
+               (concat kentan-arvot
+                       [(reduce + kentan-arvot)])))
+       (let [summat (for [kentta kentat]
+                      (reduce + (map #(summa-kk % kentta) kuukaudet)))]
+         [(into ["Yhteensä:"]
+                (concat
+                 summat
+                 [(reduce + summat)]))])))]))
 
 (defn suorita [db user {:keys [alkupvm loppupvm urakka-id hallintayksikko-id] :as parametrit}]
   (let [urakat (yleinen/hae-kontekstin-urakat db {:urakka urakka-id
@@ -20,46 +57,34 @@
                                                   :urakkatyyppi "hoito"
                                                   :alku alkupvm :loppu loppupvm})
         urakka-idt (mapv :urakka-id urakat)
+        _ (log/info "URAKAT: " urakka-idt, "USER: " (pr-str user))
         kuukaudet (yleinen/kuukausivalit alkupvm loppupvm)
         laskutusyhteenvedot-kk (zipmap kuukaudet
-                                       (map (fn [[alku loppu]]
-                                              (->> urakka-idt
-                                                   (map #(hae-laskutusyhteenvedon-tiedot
-                                                          db user {:urakka-id %
-                                                                   :alkupvm alku
-                                                                   :loppupvm loppu}))))
-                                            kuukaudet))
-        summa-kk (memoize (fn [kk kentta]
-                            (summa (laskutusyhteenvedot-kk kk) kentta)))
-        kentat [:kht_laskutettu_ind_korotus
-                :yht_laskutetaan_ind_korotus
-                :akilliset_hoitotyot_laskutettu_ind_korotus
-                :sakot_laskutettu_ind_korotus
-                :suolasakot_laskutettu_ind_korotus]]
+                                       (map
+                                        (fn [[alku loppu]]
+                                          (->> urakka-idt
+                                               (mapcat #(hae-laskutusyhteenvedon-tiedot
+                                                         db user {:urakka-id %
+                                                                  :alkupvm alku
+                                                                  :loppupvm loppu}))))
+                                        kuukaudet))
+        tuotteet (into #{}
+                       (mapcat
+                        (fn [kuukauden-laskutusyhteenvedot]
+                          (map :nimi kuukauden-laskutusyhteenvedot)))
+                       (vals laskutusyhteenvedot-kk))
+
+        ;; Tehdään jokaiselle tuottelle omat kk-rivit tarkempia
+        ;; taulukoita varten
+        tuotteen-laskutusyhteenvedot-kk
+        (zipmap tuotteet
+                (map (fn [tuote]
+                       (fmap (fn [kk-rivit]
+                               (filter #(= (:nimi %) tuote) kk-rivit))
+                             laskutusyhteenvedot-kk))
+                     tuotteet))]
 
     [:raportti {:nimi "Indeksitarkistus"}
-     [:taulukko {:viimeinen-rivi-yhteenveto? true}
-      [{:otsikko "Kuukausi" :leveys 2}
-       {:otsikko "Kokonaishintaiset työt" :leveys 2 :fmt :raha :tasaa :oikea}
-       {:otsikko "Yksikköhintaiset työt" :leveys 2 :fmt :raha :tasaa :oikea}
-       {:otsikko "Äkillinen hoitotyö" :leveys 2 :fmt :raha :tasaa :oikea}
-       {:otsikko "Sanktiot" :leveys 2 :fmt :raha :tasaa :oikea}
-       {:otsikko "Suolabonukset ja -sanktiot" :leveys 2 :fmt :raha :tasaa :oikea}
-       {:otsikko "Yhteensä (€)" :leveys 2 :fmt :raha :tasaa :oikea}]
-
-      (into
-       []
-       (concat
-        (for [[alku _ :as kk] kuukaudet
-              :let [kentan-arvot (map #(summa-kk kk %) kentat)]]
-          (into [(pvm/kuukauden-lyhyt-nimi (pvm/kuukausi alku))]
-                (concat kentan-arvot
-                        [(reduce + kentan-arvot)])))
-        (let [summat (for [kentta kentat]
-                       (reduce + (map #(summa-kk % kentta) kuukaudet)))]
-          [(into ["Yhteensä:"]
-                 (concat
-                  summat
-                  [(reduce + summat)]))])))]
-
-     ]))
+     (indeksitaulukko "Kaikki yhteensä" kuukaudet laskutusyhteenvedot-kk)
+     (for [tuote tuotteet]
+       (indeksitaulukko tuote kuukaudet (get tuotteen-laskutusyhteenvedot-kk tuote)))]))
