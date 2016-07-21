4982bbc3b558ab8d2202c4404ddb33fadfe485f1
Jari Hanhela <jarihanhela@gmail.com>
2016-07-21 12:50:17 +0300

    Älä kutsu poistettua kyselyä

--- src/clj/harja/kyselyt/toteumat.clj
+++ src/clj/harja/kyselyt/toteumat.clj
@@ -8,17 +8,3 @@
 (defn onko-olemassa-ulkoisella-idlla? [db ulkoinen-id luoja]
   (log/debug "Tarkistetaan onko olemassa toteuma ulkoisella id:llä " ulkoinen-id " ja luojalla " luoja)
   (:exists (first (onko-olemassa-ulkoisella-idlla db ulkoinen-id luoja))))
-
-(defn onko-olemassa-varustetoteuma? [db toteuma-id tietolaji toimenpide
-                                     tr-numero aosa aet losa let puoli]
-  (log/debug "Tarkistetaan onko olemassa varustetoteuma.")
-  (:exists (first (onko-olemassa-varustetoteuma db
-                                                {:toteumaid toteuma-id
-                                                :tietolaji tietolaji
-                                                :toimenpide toimenpide
-                                                :tr_numero tr-numero
-                                                :tr_aosa aosa
-                                                :tr_aet aet
-                                                :tr_losa losa
-                                                :tr_let let
-                                                :tr_puoli puoli}))))
