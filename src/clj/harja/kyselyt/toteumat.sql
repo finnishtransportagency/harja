0796e94966d41f22d15e3bb813ed38e50f3a04f4
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-07-22 13:10:55 +0300

    Sovita yks.hint. haku kuvatasolle

--- src/clj/harja/kyselyt/toteumat.sql
+++ src/clj/harja/kyselyt/toteumat.sql
@@ -538,26 +538,28 @@ ORDER BY t.alkanut
 LIMIT 501;
 
 -- name: hae-yksikkohintaisten-toiden-reitit
+-- fetch-size: 64
+-- row-fn: muunna-reitti
 SELECT
-  t.reitti,
-  tk.nimi AS tehtava_toimenpide,
-  tk.id AS tehtava_id,
-  tt.toteuma AS toteumaid
+  ST_Simplify(t.reitti, :toleranssi) as reitti,
+  tt.toimenpidekoodi AS tehtava_toimenpidekoodi,
+  tpk.nimi AS tehtava_toimenpide
 FROM toteuma_tehtava tt
   JOIN toteuma t ON tt.toteuma = t.id
-  JOIN toimenpidekoodi tk ON tt.toimenpidekoodi = tk.id
+  JOIN toimenpidekoodi tpk ON tt.toimenpidekoodi = tpk.id
 WHERE
-  t.urakka = :urakkaid
-  AND t.sopimus = :sopimusid
+  t.urakka = :urakka-id
+  AND t.sopimus = :sopimus-id
   AND t.alkanut >= :alkupvm
   AND t.alkanut <= :loppupvm
+  AND ST_Intersects(t.reitti, ST_MakeEnvelope(:xmin, :ymin, :xmax, :ymax))
   AND t.tyyppi = 'yksikkohintainen' :: toteumatyyppi
   AND t.poistettu IS NOT TRUE
   AND (:toimenpide :: INTEGER IS NULL OR
-       tk.emo = (SELECT toimenpide
-                 FROM toimenpideinstanssi
-                 WHERE id = :toimenpide))
-  AND (:tehtava :: INTEGER IS NULL OR tk.id = :tehtava)
+       tpk.emo = (SELECT toimenpide
+                    FROM toimenpideinstanssi
+                  WHERE id = :toimenpide))
+  AND (:tehtava :: INTEGER IS NULL OR tpk.id = :tehtava)
   AND t.poistettu IS NOT TRUE;
 
 -- name: hae-kokonaishintaisten-toiden-reitit
