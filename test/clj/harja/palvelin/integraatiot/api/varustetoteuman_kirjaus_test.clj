92de76e51e72647444ed239b1610d5220247e52e
Jari Hanhela <jarihanhela@gmail.com>
2016-07-22 08:26:30 +0300

    Poista vanhentunut testi

--- test/clj/harja/palvelin/integraatiot/api/varustetoteuman_kirjaus_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/varustetoteuman_kirjaus_test.clj
@@ -63,26 +63,13 @@
     (with-fake-http
       [#"http?://localhost" :allow]
       (let [varustetoteumat-ennen-uutta-pyyntoa (ffirst (q
-                                                                        (str "SELECT count(*)
+                                                          (str "SELECT count(*)
                                                        FROM varustetoteuma")))
             vastaus-lisays (api-tyokalut/post-kutsu varustetoteuma-api-url kayttaja portti
                                                     payload)]
         (is (= 200 (:status vastaus-lisays)))
         (let [varustetoteumat-uuden-pyynnon-jalkeen (ffirst (q
-                                                        (str "SELECT count(*)
+                                                              (str "SELECT count(*)
                                                        FROM varustetoteuma")))]
           (is (= varustetoteumat-ennen-uutta-pyyntoa
                  varustetoteumat-uuden-pyynnon-jalkeen)))))))
-
-(deftest tallenna-varustetoteuma
-  (let [ulkoinen-id (hae-vapaa-toteuma-ulkoinen-id)
-        vastaus-lisays (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/toteumat/varuste"] kayttaja portti
-                                                (-> "test/resurssit/api/varustetoteuma.json"
-                                                    slurp
-                                                    (.replace "__ID__" (str ulkoinen-id))))]
-    (is (= 200 (:status vastaus-lisays)))
-    (let [toteuma-kannassa (first (q (str "SELECT ulkoinen_id, suorittajan_ytunnus, suorittajan_nimi FROM toteuma WHERE ulkoinen_id = " ulkoinen-id)))
-          toteuma-id (ffirst (q (str "SELECT id FROM toteuma WHERE ulkoinen_id = " ulkoinen-id)))
-          varuste-arvot-kannassa (first (q (str "SELECT arvot FROM varustetoteuma WHERE toteuma = " toteuma-id)))]
-      (is (= toteuma-kannassa [ulkoinen-id "8765432-1" "Tehotekijät Oy"]))
-      (is (= varuste-arvot-kannassa ["----livitunniste----        2                           ----livitunniste----          01  "])))))
