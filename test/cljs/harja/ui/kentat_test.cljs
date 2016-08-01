9254e5ac53b05f659a6b3043f9eeba4361e10325
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-08-01 19:52:11 +0300

    Lisää päivämääräkentän testi

--- test/cljs/harja/ui/kentat_test.cljs
+++ test/cljs/harja/ui/kentat_test.cljs
@@ -5,7 +5,8 @@
             [harja.testutils :as u]
             [cljs.core.async :as async]
             [reagent.core :as r]
-            [cljs-react-test.simulate :as sim])
+            [cljs-react-test.simulate :as sim]
+            [harja.pvm :as pvm])
   (:require-macros [harja.testutils.macros :refer [komponenttitesti]]))
 
 (t/use-fixtures :each u/komponentti-fixture)
@@ -60,5 +61,55 @@
      "Datasta tuleva arvo päivittää tekstin"
      (reset! data 0.66)
      --
-     (is (= "0,66" (val)))
-     )))
+     (is (= "0,66" (val))))))
+
+(deftest pvm
+  (let [data (r/atom nil)
+        val! #(u/change :input %)
+        val #(some-> :input u/sel1 .-value)]
+    (komponenttitesti
+     [kentat/tee-kentta {:tyyppi :pvm :placeholder "anna pvm"} data]
+
+     "Alkutilanteessa arvo on tyhjä ja placeholder on asetettu"
+     (is (= "" (val)))
+     (is (= "anna pvm" (.getAttribute (u/sel1 :input) "placeholder")))
+
+     "Virheellistä tekstiä ei voi syöttää"
+     (val! "66...")
+     --
+     (is (= "" (val)))
+     (is (nil? @data))
+
+     "Keskeneräinen pvm on ok"
+     (val! "12.")
+     --
+     (is (= "12." (val)))
+     (is (nil? @data))
+
+     "Täytetty pvm asettaa arvon"
+     (val! "7.7.2010")
+     --
+     (is (= "7.7.2010" (val)))
+     (is (nil? @data)) ;; arvoa ei aseteta ennen blur tai selectiä
+     (sim/blur (u/sel1 :input) nil)
+     --
+     (is (= (pvm/->pvm "7.7.2010") @data))
+
+     "Picker ei ole näkyvissä"
+     (is (nil? (u/sel1 :table.pvm-valinta)))
+
+     "Klikkauksesta picker tulee näkyviin"
+     (u/click :input)
+     --
+     (is (u/sel1 :table.pvm-valinta))
+
+     "Seuraava kk napin klikkaaminen elokuun 2010"
+     (u/click :.pvm-seuraava-kuukausi)
+     --
+     (is (= "Elo 2010" (u/text ".pvm-kontrollit tr td:nth-child(2)")))
+
+     "Viidestoista päivä klikkaus (su, 3. rivi)"
+     (u/click ".pvm-paivat tr:nth-child(3) td:nth-child(7)")
+     --
+     (is (= "15.08.2010" (val)))
+     (is (pvm/sama-pvm? (pvm/->pvm "15.8.2010") @data)))))
