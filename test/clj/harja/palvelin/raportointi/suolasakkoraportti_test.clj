c9c196327539992ebb8bca555e625733fd3d083e
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-08-04 13:57:38 +0300

    Päivitä lisää sakko per tonnin yksikköä testissä

--- test/clj/harja/palvelin/raportointi/suolasakkoraportti_test.clj
+++ test/clj/harja/palvelin/raportointi/suolasakkoraportti_test.clj
@@ -188,7 +188,7 @@
                       {:fmt     :numero
                        :leveys  5
                        :otsikko "Suola­erotus (t)"}
-                      {:fmt     :numero
+                      {:fmt     :raha
                        :leveys  4
                        :otsikko "Sakko € / tonni"}
                       {:fmt     :raha
@@ -295,7 +295,7 @@
                       {:fmt     :numero
                        :leveys  5
                        :otsikko "Suola­erotus (t)"}
-                      {:fmt     :numero
+                      {:fmt     :raha
                        :leveys  4
                        :otsikko "Sakko € / tonni"}
                       {:fmt     :raha
