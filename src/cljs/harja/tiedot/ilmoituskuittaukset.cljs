927864169adbd049f440222c82c1411d6ce06f2f
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-08-19 16:25:52 +0300

    Erota kuittausdatan luonti atomin manipuloinnista

--- src/cljs/harja/tiedot/ilmoituskuittaukset.cljs
+++ src/cljs/harja/tiedot/ilmoituskuittaukset.cljs
@@ -16,19 +16,30 @@
 (defn laheta-uusi-kuittaus [kuittaus]
   (k/post! :tallenna-ilmoitustoimenpide kuittaus nil true))
 
+(defn uusi-kuittaus-ilmoitukselle [ilmoitus]
+  (let [kayttaja @istunto/kayttaja
+        organisaatio (:organisaatio kayttaja)]
+    {:ilmoituksen-id          (:id ilmoitus)
+     :ulkoinen-ilmoitusid     (:ilmoitusid ilmoitus)
+     :tyyppi                  :vastaanotto
+     :ilmoittaja-etunimi      (:etunimi kayttaja)
+     :ilmoittaja-sukunimi     (:sukunimi kayttaja)
+     :ilmoittaja-matkapuhelin (:puhelin kayttaja)
+     :ilmoittaja-tyopuhelin   (:puhelin kayttaja)
+     :ilmoittaja-sahkoposti   (:sahkoposti kayttaja)
+     :ilmoittaja-organisaatio (:nimi organisaatio)
+     :ilmoittaja-ytunnus      (:ytunnus organisaatio)}))
+
+(defn laheta-kuittaukset! [ilmoitukset kuittaus]
+  (k/post! :tallenna-ilmoitustoimenpiteet
+           (into []
+                 (map #(merge (uusi-kuittaus-ilmoitukselle %) kuittaus))
+                 ilmoitukset)))
+
 (defn alusta-uusi-kuittaus [valittu-ilmoitus]
   (if (nil? valittu-ilmoitus)
     (reset! uusi-kuittaus nil)
     (let [kayttaja @istunto/kayttaja
           organisaatio (:organisaatio kayttaja)]
       (reset! uusi-kuittaus
-              {:ilmoituksen-id          (:id @valittu-ilmoitus)
-               :ulkoinen-ilmoitusid     (:ilmoitusid @valittu-ilmoitus)
-               :tyyppi                  :vastaanotto
-               :ilmoittaja-etunimi      (:etunimi kayttaja)
-               :ilmoittaja-sukunimi     (:sukunimi kayttaja)
-               :ilmoittaja-matkapuhelin (:puhelin kayttaja)
-               :ilmoittaja-tyopuhelin   (:puhelin kayttaja)
-               :ilmoittaja-sahkoposti   (:sahkoposti kayttaja)
-               :ilmoittaja-organisaatio (:nimi organisaatio)
-               :ilmoittaja-ytunnus      (:ytunnus organisaatio)}))))
+              (uusi-kuittaus-ilmoitukselle @valittu-ilmoitus)))))
