aa4247cf0d2db02527dbed5f8147d30f17943a5e
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-08-24 15:31:32 +0300

    Lisää migraatio ELY:jen Sampo hasheille

---
+++ tietokanta/src/main/resources/db/migration/V1_410__.sql
@@ -0,0 +1,9 @@
+UPDATE organisaatio SET sampo_ely_hash = 'KP981' where nimi = 'Pohjois-Pohjanmaa ja Kainuu';
+UPDATE organisaatio SET sampo_ely_hash = 'KP941' where nimi = 'Kaakkois-Suomi';
+UPDATE organisaatio SET sampo_ely_hash = 'KP921' where nimi = 'Varsinais-Suomi';
+UPDATE organisaatio SET sampo_ely_hash = 'KP951' where nimi = 'Pohjois-Savo';
+UPDATE organisaatio SET sampo_ely_hash = 'KP931' where nimi = 'Pirkanmaa';
+UPDATE organisaatio SET sampo_ely_hash = 'KP911' where nimi = 'Uusimaa';
+UPDATE organisaatio SET sampo_ely_hash = 'KP961' where nimi = 'Keski-Suomi';
+UPDATE organisaatio SET sampo_ely_hash = 'KP991' where nimi = 'Lappi';
+UPDATE organisaatio SET sampo_ely_hash = 'KP971' where nimi = 'Etelä-Pohjanmaa';
