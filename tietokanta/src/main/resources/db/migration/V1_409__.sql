979b3e2834bb2cf0346e8c436b6d5a97b419ae4a
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-08-24 10:43:40 +0300

    Poista viittaajat kun tarkastus poistetaan

--- tietokanta/src/main/resources/db/migration/V1_409__.sql
+++ tietokanta/src/main/resources/db/migration/V1_409__.sql
@@ -1,5 +1,42 @@
 -- Tee tarkastuksen ulkoinen_id,luoja parista uniikki
 
+-- Kun tarkstus poistetaan, poistetaan viittaavat myös
+ALTER TABLE talvihoitomittaus
+      DROP CONSTRAINT talvihoitomittaus_tarkastus_fkey,
+      ADD CONSTRAINT talvihoitomittaus_tarkastus_fkey
+          FOREIGN KEY (tarkastus)
+          REFERENCES tarkastus (id) ON DELETE CASCADE;
+
+ALTER TABLE soratiemittaus
+      DROP CONSTRAINT soratiemittaus_tarkastus_fkey,
+      ADD CONSTRAINT soratiemittaus_tarkastus_fkey
+          FOREIGN KEY (tarkastus)
+	  REFERENCES tarkastus (id) ON DELETE CASCADE;
+
+ALTER TABLE tarkastus_kommentti
+      DROP CONSTRAINT tarkastus_kommentti_tarkastus_fkey,
+      ADD CONSTRAINT tarkastus_kommentti_tarkastus_fkey
+          FOREIGN KEY (tarkastus)
+	  REFERENCES tarkastus (id) ON DELETE CASCADE;
+
+ALTER TABLE tarkastus_laatupoikkeama
+      DROP CONSTRAINT tarkastus_laatupoikkeama_tarkastus_fkey,
+      ADD CONSTRAINT tarkastus_laatupoikkeama_tarkastus_fkey
+          FOREIGN KEY (tarkastus)
+	  REFERENCES tarkastus (id) ON DELETE CASCADE;
+
+ALTER TABLE tarkastus_liite
+      DROP CONSTRAINT tarkastus_liite_tarkastus_fkey,
+      ADD CONSTRAINT tarkastus_liite_tarkastus_fkey
+          FOREIGN KEY (tarkastus)
+	  REFERENCES tarkastus (id) ON DELETE CASCADE;
+
+ALTER TABLE tarkastus_vakiohavainto
+      DROP CONSTRAINT tarkastus_vakiohavainto_tarkastus_fkey,
+      ADD CONSTRAINT tarkastus_vakiohavainto_tarkastus_fkey
+          FOREIGN KEY (tarkastus)
+	  REFERENCES tarkastus (id) ON DELETE CASCADE;
+
 -- Poista duplikaatit tarkastukset ennen constraintin luontia
 DELETE FROM
   tarkastus
