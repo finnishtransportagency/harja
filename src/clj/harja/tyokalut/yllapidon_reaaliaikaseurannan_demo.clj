5309163091db656631944602259033ec71d40db0
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-08-26 13:54:20 +0300

    Lisää suljetut tieosuudet & TMA-aidat

--- src/clj/harja/tyokalut/yllapidon_reaaliaikaseurannan_demo.clj
+++ src/clj/harja/tyokalut/yllapidon_reaaliaikaseurannan_demo.clj
@@ -1,7 +1,22 @@
 (ns harja.tyokalut.yllapidon-reaaliaikaseurannan-demo
   (:require [harja.palvelin.komponentit.tietokanta :as tietokanta]
             [harja.kyselyt.konversio :as konv]
-            [harja.kyselyt.suljetut-tieosuudet :as q-suljetut-tieosuudet]))
+            [harja.kyselyt.suljetut-tieosuudet :as q-suljetut-tieosuudet]
+            [harja.kyselyt.tyokoneseuranta :as tks])
+  (:import (java.util Calendar)
+           (java.sql Timestamp)))
+
+(defn arrayksi [db v]
+  (with-open [conn (.getConnection (:datasource db))]
+    (.createArrayOf conn "text" (to-array v))))
+
+(defn paivita [db sql]
+  (with-open [c (.getConnection (:datasource db))
+              ps (.prepareStatement c (reduce str sql))]
+    (.executeUpdate ps)))
+
+(defn nyt []
+  (new Timestamp (.getTime (.getTime (Calendar/getInstance)))))
 
 (defn aja []
   (let [tietokanta {:palvelin "localhost"
@@ -10,28 +25,65 @@
                     :yhteyspoolin-koko 16
                     :kayttaja "flyway"
                     :salasana "migrate123"}
+        urakka-id 348
+        yllapitokohde-id 7
+        alkux 570095
+        alkuy 6771092
+        loppux 574110
+        loppuy 6774221
         db (tietokanta/luo-tietokanta tietokanta true)
+        _ (paivita db "DELETE FROM suljettu_tieosuus WHERE osuus_id = 123456789;")
         suljettutieosuus {:jarjestelma "Harja"
                           :osuusid 123456789
-                          :alkux 432040
-                          :alkuy 7213557
-                          :loppux 438382
-                          :loppuy 7220022
-                          :asetettu ()
-                          :kaistat (konv/seq->array (:kaistat suljettu-tieosuus))
-                          :ajoradat (konv/seq->array (:ajoradat suljettu-tieosuus))
-                          :yllapitokohde kohde-id
-                          :kirjaaja (:id kayttaja)
-                          :tr_tie (:tie tr-osoite)
-                          :tr_aosa (:aosa tr-osoite)
-                          :tr_aet (:aet tr-osoite)
-                          :tr_losa (:losa tr-osoite)
-                          :tr_let (:let tr-osoite)}
-        suljettutieosuusid (:id (first (q-suljetut-tieosuudet/luo-suljettu-tieosuus<! db suljettutieosuus)))]
+                          :alkux alkux
+                          :alkuy alkuy
+                          :loppux loppux
+                          :loppuy loppuy
+                          :asetettu (nyt)
+                          :kaistat (konv/seq->array [1])
+                          :ajoradat (konv/seq->array [0])
+                          :yllapitokohde yllapitokohde-id
+                          :kirjaaja 13
+                          :tr_tie 6
+                          :tr_aosa 302
+                          :tr_aet 4240
+                          :tr_losa 304
+                          :tr_let 688}
+        suljettutieosuusid (:id (q-suljetut-tieosuudet/luo-suljettu-tieosuus<! db suljettutieosuus))]
 
 
+    ;; Aseta TMA-aidat suljetun tieosuuden alkuun ja loppuun
+    (tks/tallenna-tyokonehavainto
+      db
+      "Harja"
+      "Solita Oy"
+      "1060155-5"
+      1000001
+      (nyt)
+      1000001
+      "TMA-aita 1"
+      alkux
+      alkuy
+      425
+      urakka-id
+      (arrayksi db ["turvalaite"]))
 
+    (tks/tallenna-tyokonehavainto
+      db
+      "Harja"
+      "Solita Oy"
+      "1060155-5"
+      1000002
+      (nyt)
+      1000002
+      "TMA-aita 2"
+      loppux
+      loppuy
+      425
+      urakka-id
+      (arrayksi db ["turvalaite"]))
 
     )
 
   )
+
