774470342b1ae4a775521d265737b3c1e3374266
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-08-29 15:10:19 +0300

    Hae perusluku vain urakan id:n mukaan
    Käytettävä indeksi haetaan urakan tiedoista perusluvun laskentasprocissa.

--- tietokanta/src/main/resources/db/migration/R__Laskutusyhteenveto.sql
+++ tietokanta/src/main/resources/db/migration/R__Laskutusyhteenveto.sql
@@ -4,7 +4,6 @@ CREATE OR REPLACE FUNCTION laskutusyhteenveto(
   RETURNS SETOF laskutusyhteenveto_rivi AS $$
 DECLARE
   t                                      RECORD;
-  urakan_alkuvuosi INTEGER;
   ind VARCHAR; -- hoitourakassa käytettävä indeksi
   perusluku NUMERIC; -- urakan indeksilaskennan perusluku (urakkasopimusta edeltävän vuoden joulukuusta 3kk ka)
 
@@ -142,16 +141,7 @@ BEGIN
 
   cache := ARRAY[]::laskutusyhteenveto_rivi[];
 
-  -- Päätellään indeksilaskennan perustiedot
-  SELECT INTO urakan_alkuvuosi EXTRACT(year from alkupvm) FROM urakka WHERE id = ur;
-
-  IF urakan_alkuvuosi < 2017 THEN
-    ind := 'MAKU 2005';
-  ELSE
-    ind := 'MAKU 2010';
-  END IF;
-
-  perusluku := hoitourakan_indeksilaskennan_perusluku(ur, ind);
+  perusluku := hoitourakan_indeksilaskennan_perusluku(ur);
 
   -- Loopataan urakan toimenpideinstanssien läpi
   FOR t IN SELECT
