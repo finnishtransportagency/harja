36008fea066aa8019f0ec74c274cd33e70a9aef6
Jari Hanhela <jarihanhela@gmail.com>
2016-08-29 11:31:55 +0300

    Refactoroi laheta-kohteet-yhaan -komponenttia

--- src/cljs/harja/tiedot/urakka/yha_tuonti.cljs
+++ src/cljs/harja/tiedot/urakka/yha_tuonti.cljs
@@ -264,10 +264,11 @@
                  "ei koskaan"))]))
 
 (defn laheta-kohteet-yhaan [oikeus urakka-id sopimus-id paallystysilmoitukset]
-  (let [kohde-idt (mapv :paallystyskohde-id (filter #(and (= :hyvaksytty (:paatos-tekninen-osa %))
-                                                          (or (= :valmis (:tila %))
-                                                              (= :lukittu (:tila %))))
-                                                    paallystysilmoitukset))]
+  (let [ilmoituksen-voi-lahettaa? (fn [paallystysilmoitus]
+                                    (and (= :hyvaksytty (:paatos-tekninen-osa paallystysilmoitus))
+                                          (or (= :valmis (:tila paallystysilmoitus))
+                                              (= :lukittu (:tila paallystysilmoitus)))))
+        kohde-idt (mapv :paallystyskohde-id (filter ilmoituksen-voi-lahettaa? paallystysilmoitukset))]
     (when-not @yha-kohteiden-paivittaminen-kaynnissa?
       [harja.ui.napit/palvelinkutsu-nappi
        (if (= 1 (count paallystysilmoitukset))
