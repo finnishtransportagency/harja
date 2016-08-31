b45b60a5408cb6338d5989d421816fba3b966b87
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-08-31 14:57:29 +0300

    Muuta ilmoituksen ulkoinen id tekstiksi

--- tietokanta/src/main/resources/db/migration/V1_415__.sql
+++ tietokanta/src/main/resources/db/migration/V1_415__.sql
@@ -1,6 +1,6 @@
 ALTER TABLE ilmoitus
   ADD COLUMN tr_lopputienumero INTEGER,
-  ADD COLUMN ulkoinen_id INTEGER,
+  ADD COLUMN ulkoinen_id VARCHAR(25),
   ADD COLUMN luoja INTEGER,
   ADD CONSTRAINT ilmoitus_luoja_fkey FOREIGN KEY (luoja) REFERENCES kayttaja (id),
   ADD CONSTRAINT ilmoitus_uniikki_ulkoinen_id_luoja UNIQUE (ulkoinen_id, luoja),
