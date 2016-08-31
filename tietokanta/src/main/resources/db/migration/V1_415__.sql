d9d1376dac558646f8fd318d72c572bbb47e7979
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-08-31 08:48:52 +0300

    Päivitä materiaalit migraatiossa

--- tietokanta/src/main/resources/db/migration/V1_415__.sql
+++ tietokanta/src/main/resources/db/migration/V1_415__.sql
@@ -2,7 +2,6 @@
 ALTER TABLE lampotilat ADD pitka_keskilampotila_vanha NUMERIC (4,2);
 
 -- Tehdään hoitokauden suolasakolle uusi tyyppi
-
 CREATE TYPE hk_suolasakko AS (
  urakka INTEGER,                 -- urakka
  keskilampotila NUMERIC,         -- hoitokauden talven keskilämpötila
@@ -16,3 +15,13 @@ CREATE TYPE hk_suolasakko AS (
  maara NUMERIC,                  -- suolasakon määrä per ylitystonni
  suolasakko NUMERIC              -- sakon loppusumma
 );
+
+-- Muodostetaan sopimuksen_kaytetty_materiaali uudestaan (suolassa oli bugi)
+-- Tästä on myös sproc tulevaisuuden varalle, mutta repeatablet ajetaan numeroitujen
+-- jälkeen, joten sitä ei voi vielä käyttää.
+DELETE FROM sopimuksen_kaytetty_materiaali;
+
+INSERT INTO sopimuksen_kaytetty_materiaali (sopimus, alkupvm, materiaalikoodi, maara)
+    SELECT t.sopimus, t.alkanut::date as alkupvm, tm.materiaalikoodi, SUM(tm.maara)
+      FROM toteuma_materiaali tm join toteuma t ON tm.toteuma=t.id
+  GROUP BY t.sopimus, t.alkanut::date, tm.materiaalikoodi;
