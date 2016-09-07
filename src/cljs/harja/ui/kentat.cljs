ed45054d876e5453b7d7d0fba59ca262157cd408
Jari Hanhela <jarihanhela@gmail.com>
2016-09-07 08:52:42 +0300

    Avaa pudotusvalikko vasemmalle jos liian lähellä oikeaa reunaa (samaan tapaan kuin datepicker)

--- src/cljs/harja/ui/kentat.cljs
+++ src/cljs/harja/ui/kentat.cljs
@@ -8,7 +8,7 @@
             [harja.ui.ikonit :as ikonit]
             [harja.ui.tierekisteri :as tr]
             [harja.ui.yleiset :refer [linkki ajax-loader livi-pudotusvalikko nuolivalinta
-                                      maarita-pudotusvalikon-max-korkeus avautumissuunta-ja-korkeus-tyylit]]
+                                      maarita-pudotusvalikon-suunta-ja-max-korkeus avautumissuunta-ja-korkeus-tyylit]]
             [harja.loki :refer [log logt tarkkaile!]]
             [harja.tiedot.navigaatio :as nav]
             [clojure.string :as str]
@@ -82,8 +82,9 @@ toisen eventin kokonaan (react eventtiä ei laukea)."}
         avautumissuunta (atom :alas)
         max-korkeus (atom 0)
         pudotusvalikon-korkeuden-kasittelija-fn (fn [this _]
-                                                  (maarita-pudotusvalikon-max-korkeus
-                                                    this max-korkeus avautumissuunta))]
+                                                  (let [maaritys (maarita-pudotusvalikon-suunta-ja-max-korkeus this)]
+                                                    (reset! avautumissuunta (:suunta maaritys))
+                                                    (reset! max-korkeus (:max-korkeus maaritys))))]
     (komp/luo
       (komp/dom-kuuntelija js/window
                            EventType/SCROLL pudotusvalikon-korkeuden-kasittelija-fn
