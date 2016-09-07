ee0ebd6e57c16b13c6b72b1b53a5962a88bf138a
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-09-07 10:00:10 +0300

    Salli duplikaatit ulkoisella id:lla

--- tietokanta/src/main/resources/db/migration/V1_416__.sql
+++ tietokanta/src/main/resources/db/migration/V1_416__.sql
@@ -1,37 +1,6 @@
-<<<<<<< HEAD
 ALTER TABLE ilmoitus
   ADD COLUMN tr_lopputienumero INTEGER,
   ADD COLUMN ulkoinen_id VARCHAR(25),
   ADD COLUMN luoja INTEGER,
   ADD CONSTRAINT ilmoitus_luoja_fkey FOREIGN KEY (luoja) REFERENCES kayttaja (id),
-  ADD CONSTRAINT ilmoitus_uniikki_ulkoinen_id_luoja UNIQUE (ulkoinen_id, luoja),
   ALTER COLUMN ilmoitusid DROP NOT NULL;
-=======
--- Lisää myös vanha vertailukausi
-ALTER TABLE lampotilat ADD pitka_keskilampotila_vanha NUMERIC (4,2);
-
--- Tehdään hoitokauden suolasakolle uusi tyyppi
-CREATE TYPE hk_suolasakko AS (
- urakka INTEGER,                 -- urakka
- keskilampotila NUMERIC,         -- hoitokauden talven keskilämpötila
- pitkakeskilampotila NUMERIC,    -- vertailukauden pitkä keskilämpötila
- lampotilapoikkeama NUMERIC,     -- keskilämmön ja vertailujakson erotus
- suolankaytto NUMERIC,           -- hk toteutunut suolan käyttö
- sallittu_suolankaytto NUMERIC,  -- hoitokauden suolankäyttöraja
- suolankayton_sakkoraja NUMERIC, -- suolankäytön sakkoraja ennen kohtuullistamista
- kohtuullisuustarkistettu_sakkoraja NUMERIC, -- yo raja kohtuullisuustarkistuksen jälkeen
- sakkoraja NUMERIC,              -- suolankäytön sakkoraja
- maara NUMERIC,                  -- suolasakon määrä per ylitystonni
- suolasakko NUMERIC              -- sakon loppusumma
-);
-
--- Muodostetaan sopimuksen_kaytetty_materiaali uudestaan (suolassa oli bugi)
--- Tästä on myös sproc tulevaisuuden varalle, mutta repeatablet ajetaan numeroitujen
--- jälkeen, joten sitä ei voi vielä käyttää.
-DELETE FROM sopimuksen_kaytetty_materiaali;
-
-INSERT INTO sopimuksen_kaytetty_materiaali (sopimus, alkupvm, materiaalikoodi, maara)
-    SELECT t.sopimus, t.alkanut::date as alkupvm, tm.materiaalikoodi, SUM(tm.maara)
-      FROM toteuma_materiaali tm join toteuma t ON tm.toteuma=t.id
-  GROUP BY t.sopimus, t.alkanut::date, tm.materiaalikoodi;
->>>>>>> develop
