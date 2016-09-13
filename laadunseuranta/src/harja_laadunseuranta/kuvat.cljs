defdeadb271b728c2261cd228bca1b4121b1c85b
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-09-13 12:29:50 +0300

    Lisää päällystyksen työvirheikonit

--- laadunseuranta/src/harja_laadunseuranta/kuvat.cljs
+++ laadunseuranta/src/harja_laadunseuranta/kuvat.cljs
@@ -1,7 +1,10 @@
-(ns harja-laadunseuranta.kuvat)
+(ns harja-laadunseuranta.kuvat
+  (:require-macros [harja-laadunseuranta.inline-kuva :refer [inline-svg]]))
 
 (defn- ikoni [nimi]
   (str "img/" nimi))
+
+
 
 (def +autonuoli+ (ikoni "nuoli.svg"))
 (def +harja-logo+ (ikoni "harja_logo_soft.svg"))
@@ -14,3 +17,19 @@
 (def +avattu+ (ikoni "avattu.png"))
 (def +keskityspainike+ (ikoni "keskitys.png"))
 (def +havaintopiste+ (ikoni "havaintopiste.png"))
+
+(def paallystys-tyovirheet
+  {:saumavirhe (inline-svg "resources/public/img/tyovirheet/saumavirhe-24.svg")
+   :lajittuma (inline-svg "resources/public/img/tyovirheet/lajittuma-24.svg")
+   :epatasaisuus (inline-svg "resources/public/img/tyovirheet/epatasa-24.svg")
+   :halkeamat (inline-svg "resources/public/img/tyovirheet/halkeama-24.svg")
+   :vesilammikot (inline-svg "resources/public/img/tyovirheet/vesilammikko-24.svg")
+   :epatasaisetreunat (inline-svg "resources/public/img/tyovirheet/epatasaiset-reunat-24.svg")
+   :jyranjalkia (inline-svg "resources/public/img/tyovirheet/jyran-jalki-24.svg")
+   :sideainelaikkia (inline-svg "resources/public/img/tyovirheet/laikka-24.svg")
+   :vaarakorkeusasema (inline-svg "resources/public/img/tyovirheet/vaara-korkeus-24.svg")
+   :pintaharva (inline-svg "resources/public/img/tyovirheet/harva-pinta-24.svg")
+   :pintakuivatuspuute (inline-svg "resources/public/img/tyovirheet/pintakuivatus-puute-24.svg")
+   :kaivojenkorkeusasema (inline-svg "resources/public/img/tyovirheet/kaivon-korkeus-24.svg")}
+  ;;(map (juxt first #(inline (str "resources/public/img/tyovirheet/" (second %) "-24.svg"))))
+  )
