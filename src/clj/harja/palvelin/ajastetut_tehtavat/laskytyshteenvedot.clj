a92ef142117636f9351e9e7185b8a6c9a8f95476
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-09-14 12:54:19 +0300

    Ajasta laskutusyhteenvedot

--- src/clj/harja/palvelin/ajastetut_tehtavat/laskytyshteenvedot.clj
+++ src/clj/harja/palvelin/ajastetut_tehtavat/laskytyshteenvedot.clj
@@ -1,15 +1,54 @@
-(ns harja.palvelin.ajastetut-tehtavat.laskytyshteenvedot
+(ns harja.palvelin.ajastetut-tehtavat.laskutusyhteenvedot
   "Ajastettu tehtävä laskutusyhteenvetojen muodostamiseksi valmiiksi välimuistiin"
   (:require [com.stuartsierra.component :as component]
             [harja.kyselyt.laskutusyhteenveto :as q]
             [harja.palvelin.tyokalut.lukot :as lukot]
-            [taoensso.timbre :as log]))
+            [taoensso.timbre :as log]
+            [harja.pvm :as pvm]
+            [clj-time.core :as t]
+            [clj-time.periodic :as time-periodic]
+            [chime :as chime]))
 
-(defn muodosta-laskutusyhteenveto [db urakka]
-  (log/info "muodostellaan...."))
+(defn- muodosta-laskutusyhteenveto [db alku loppu urakka]
+  (let [hk-alku (pvm/luo-pvm (pvm/vuosi alku) 9 1)
+        hk-loppu (pvm/luo-pvm (pvm/vuosi loppu) 8 30)]
+    (q/hae-laskutusyhteenvedon-tiedot db {:urakka urakka
+                                          :hk_alkupvm hk-alku
+                                          :hk_loppupvm hk-loppu
+                                          :aikavali_alkupvm alku
+                                          :aikavali_loppupvm loppu})))
 
-(defn muodosta-laskutusyhteenvedot [db]
-  (doseq [{:keys [id nimi]} (q/hae-urakat-joille-laskutusyhteenveto-ajetaan db)]
-    (log/info "Muodostetaan laskutusyhteenveto valmiiksi urakalle: " nimi)
-    (lukot/aja-lukon-kanssa db (str "laskutusyhteenveto:" id)
-                            #(muodosta-laskutusyhteenveto db id))))
+(defn- muodosta-laskutusyhteenvedot [db]
+  (let [nyt (pvm/nyt)
+        vuosi (pvm/vuosi nyt)
+        kk (- (pvm/kuukausi nyt) 2)
+        viimeinen-paiva (t/day (t/last-day-of-the-month vuosi (inc kk)))
+        alku (pvm/luo-pvm vuosi kk 1)
+        loppu (pvm/luo-pvm vuosi kk viimeinen-paiva)]
+    (doseq [{:keys [id nimi]} (q/hae-urakat-joille-laskutusyhteenveto-voidaan-tehda
+                               db {:alku alku :loppu loppu})]
+      (log/info "Muodostetaan laskutusyhteenveto valmiiksi urakalle: " nimi)
+      (lukot/aja-lukon-kanssa
+       db (str "laskutusyhteenveto:" id)
+       #(try
+          (muodosta-laskutusyhteenveto db alku loppu id)
+          (catch Throwable t
+            (log/error t "Virhe muodostettaessa laskutusyhteenvetoa, urakka: " id ", aikavali "
+                       alku " -- " loppu)))))))
+
+(defn- ajasta []
+  (let [now (t/now)]
+    (chime/chime-at (time-periodic/periodic-seq
+                     (pvm/suomen-aikavyohykkeessa
+                      (t/date-time (t/year now) (t/month now) (t/day now)
+                                   4 30))
+                     (t/days 1)))))
+
+(defrecord LaskutusyhteenvetojenMuodostus []
+  component/Lifecycle
+  (start [this]
+    (assoc this ::laskutusyhteenvetojen-ajastus
+           (ajasta)))
+  (stop [{poista ::laskutusyhteenvetojen-ajastus :as this}]
+    (poista)
+    (dissoc this ::laskutusyhteenvetojen-ajastus)))
