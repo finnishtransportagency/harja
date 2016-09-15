9e18a87b93ca0c8a4386fcf8471163b8d47b8b33
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-09-15 13:33:01 +0300

    Korjaa ls-linkki

--- laadunseuranta/deploy.sh
+++ laadunseuranta/deploy.sh
@@ -16,5 +16,5 @@ CURRENT_BRANCH=`git symbolic-ref --short HEAD`
 HIPCHAT_TOKEN=`cat ../../.harja/hipchat-token`
 # HipChat notifikaatio
 CONFIG="from=laadunseuranta/deploy.sh&color=purple"
-MESSAGE="$USER deployasi juuri uuden Harja-laadunseurantatyökalun version haarasta $CURRENT_BRANCH palvelimelle <a href=\"https://$SERVER/laadunseuranta/index.html\">$SERVER/harja/laadunseuranta/index.html</a>"
+MESSAGE="$USER deployasi juuri uuden Harja-laadunseurantatyökalun version haarasta $CURRENT_BRANCH palvelimelle <a href=\"https://$SERVER/harja/laadunseuranta/index.html\">$SERVER/harja/laadunseuranta/index.html</a>"
 curl -d $CONFIG --data-urlencode "message=${MESSAGE}" "https://api.hipchat.com/v2/room/914801/notification?auth_token=$HIPCHAT_TOKEN&format=json"
