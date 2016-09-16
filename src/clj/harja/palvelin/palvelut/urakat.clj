5c3bc661f142754ddfff16fc84d84738244ac515
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-09-16 12:19:25 +0300

    Korjaa kyselyt

--- src/clj/harja/palvelin/palvelut/urakat.clj
+++ src/clj/harja/palvelin/palvelut/urakat.clj
@@ -114,12 +114,12 @@
        aluekokonaisuudet))))
 
 (defn hae-urakka-idt-sijainnilla [db urakkatyyppi {:keys [x y]}]
-  ;; Oletuksena haetaan valaistusurakat & päällystyksen palvelusopimukset 5 metrin thesholdilla
-  (let [urakka-idt (map :id (q/hae-urakka-sijainnilla db urakkatyyppi x y))]
+  ;; Oletuksena haetaan valaistusurakat & päällystyksen palvelusopimukset 10 metrin thesholdilla
+  (let [urakka-idt (map :id (q/hae-urakka-sijainnilla db urakkatyyppi x y 10))]
     (if (and (empty? urakka-idt)
-             (not= "hoito" urakkatyyppi 5))
-        ;; Jos ei löytynyt urakoita eri tyypillä, kokeillaan hoido urakoita
-      (map :id (q/hae-urakka-sijainnilla db "hoito" x y 5))
+             (not= "hoito" urakkatyyppi))
+      ;; Jos ei löytynyt urakoita eri tyypillä, kokeillaan hoido urakoita
+      (map :id (q/hae-urakka-sijainnilla db "hoito" x y 10))
       urakka-idt)))
 
 (def urakka-xf
