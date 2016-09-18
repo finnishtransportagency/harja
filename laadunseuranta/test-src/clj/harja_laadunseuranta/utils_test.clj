b76b0e7529aee8ad94b0b4e2de9265602fe4a153
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-09-18 14:03:39 +0300

    Poista kuolleen koodin testi

--- laadunseuranta/test-src/clj/harja_laadunseuranta/utils_test.clj
+++ laadunseuranta/test-src/clj/harja_laadunseuranta/utils_test.clj
@@ -2,11 +2,5 @@
   (:require [harja-laadunseuranta.utils :refer [ryhmat select-non-nil-keys]])
   (:use [clojure.test]))
 
-(deftest ryhmien-parsinta
-  (is (= #{} (ryhmat {:headers nil})))
-  (is (= #{} (ryhmat {:headers {}})))
-  (is (= #{} (ryhmat {:headers {"oam_groups" ""}})))
-  (is (= #{"jarjestelmanvalvoja" "paakayttaja"} (ryhmat {:headers {"oam_groups" "Jarjestelmanvalvoja,Paakayttaja"}}))))
-
 (deftest non-nil-keys
   (is (= {:a 2} (select-non-nil-keys {:a 2 :b nil} [:a :b :c]))))
