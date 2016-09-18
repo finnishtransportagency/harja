52cde36d7d92db87623aeb269a8934594cb59f82
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-09-18 14:00:50 +0300

    Poista kuollutta koodia

--- laadunseuranta/clj-src/harja_laadunseuranta/utils.clj
+++ laadunseuranta/clj-src/harja_laadunseuranta/utils.clj
@@ -1,6 +1,5 @@
 (ns harja-laadunseuranta.utils
   (:require [harja-laadunseuranta.config :as c]
-            [ring.util.http-response :refer :all]
             [taoensso.timbre :as log]
             [clojure.string :as str]
 
@@ -10,12 +9,6 @@
 
 (defn polku [s]
   (str (:url-prefix @c/config) s))
-
-(defn poikkeuskasittelija [^Exception e data req]
-  (log/error e "Virhe " (.getMessage e))
-  (when-let [next-ex (.getNextException e)]
-    (log/error next-ex "-- Sisempi virhe " (.getMessage next-ex)))
-  (internal-server-error {:error (.getMessage e)}))
 
 (defn select-non-nil-keys [c keys]
   (into {} (filterv #(not (nil? (second %))) (into [] (select-keys c keys)))))
