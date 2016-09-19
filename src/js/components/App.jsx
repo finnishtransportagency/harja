18dca4dee15c08a57411105f7ffe28d754421ccb
Antti Ahola <Antti.Ahola@solita.fi>
2016-09-19 16:49:39 +0300

    Remove react-router

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,17 +1,55 @@
-import React, { Component }  from 'react';
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Container from './Container.jsx';
+import request from 'superagent';
 import Home from './Home.jsx';
 
-export default class App extends Component {
+export default React.createClass({
+
+  getInitialState() {
+    return {
+      careNotices: [],
+      maintenanceNotices: [],
+      faqNotices: []
+    };
+  },
+
+  componentDidMount() {
+    setTimeout(() => { this.getNotices('care.json', 'careNotices'); }, 500);
+    setTimeout(() => { this.getNotices('maintenance.json', 'maintenanceNotices'); }, 3000);
+    setTimeout(() => { this.getNotices('faq.json', 'faqNotices'); }, 5000);
+    /*
+    this.getNotices('care.json', 'careNotices');
+    this.getNotices('maintenance.json', 'maintenanceNotices');
+    this.getNotices('faq.json', 'faqNotices');
+    */
+  },
+
+  getNotices(file, type) {
+    const url = '../data/' + file;
+    request.get(url)
+      .set('Accept', 'application/json')
+      .end((err, response) => {
+        if (err) return console.error(err);
+
+        const latestId = this.state[type].length
+        const notices = response.body.map((notice, index) => {
+          notice.id = latestId + index;
+          notice.type = type
+          return notice;
+        });
+
+        this.setState({
+          [type]: response.body,
+        });
+      });
+  },
+
   render () {
     return (
-      <Router history={hashHistory}>
-        <Route path="/" component={Container}>
-          <IndexRoute component={Home} />
-        </Route>
-      </Router>
+      <Home
+        careNotices={this.state.careNotices}
+        maintenanceNotices={this.state.maintenanceNotices}
+        faqNotices={this.state.faqNotices}/>
     )
   }
-}
+});
