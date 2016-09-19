e1cf2fa77bc0eb55f66550c4e3369a4fce4cc34b
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-09-19 12:18:21 +0300

    Poista erillinen config tarpeettomana

--- laadunseuranta/clj-src/harja_laadunseuranta/config.clj
+++
@@ -1,9 +0,0 @@
-(ns harja-laadunseuranta.config
-  (:require [clojure.edn :as edn]))
-
-(def +asetustiedosto+ "laadunseuranta/laadunseuranta_asetukset.edn")
-
-(def config (atom nil))
-
-(defn aseta-config! [c]
-  (reset! config c))
