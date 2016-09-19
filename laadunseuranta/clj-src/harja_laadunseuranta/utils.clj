d8a355256c1ba40b1f0e8b13eb6e82dbec23d370
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-09-19 12:19:06 +0300

    Siisti ns määrittelyä

--- laadunseuranta/clj-src/harja_laadunseuranta/utils.clj
+++ laadunseuranta/clj-src/harja_laadunseuranta/utils.clj
@@ -1,14 +1,5 @@
-(ns harja-laadunseuranta.utils
-  (:require [harja-laadunseuranta.config :as c]
-            [taoensso.timbre :as log]
-            [clojure.string :as str]
+(ns harja-laadunseuranta.utils)
 
-            [harja.palvelin.komponentit.todennus :as todennus]
-            [harja.domain.oikeudet :as oikeudet]))
-
-
-(defn polku [s]
-  (str (:url-prefix @c/config) s))
 
 (defn select-non-nil-keys [c keys]
   (into {} (filterv #(not (nil? (second %))) (into [] (select-keys c keys)))))
