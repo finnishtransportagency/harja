c330dcf81eed2b2fa95b1cf66036577e1f4a2b05
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-09-20 12:05:00 +0300

    Korjaa typo geometriatuonnissa

--- src/clj/harja/palvelin/asetukset.clj
+++ src/clj/harja/palvelin/asetukset.clj
@@ -88,9 +88,9 @@
                                            (s/optional-key :valaistusurakoiden-shapefile)              s/Str
                                            (s/optional-key :valaistusurakoiden-alk-osoite)             s/Str
                                            (s/optional-key :valaistusurakoiden-alk-tuontikohde)        s/Str
-                                           (s/optional-key :paallystyspalvelusopimuste-shapefile)              s/Str
-                                           (s/optional-key :paallystyspalvelusopimuste-alk-osoite)             s/Str
-                                           (s/optional-key :paallystyspalvelusopimuste-alk-tuontikohde)         s/Str}
+                                           (s/optional-key :paallystyspalvelusopimusten-shapefile)              s/Str
+                                           (s/optional-key :paallystyspalvelusopimusten-alk-osoite)             s/Str
+                                           (s/optional-key :paallystyspalvelusopimusten-alk-tuontikohde)         s/Str}
 
    (s/optional-key :yha)                  {:url            s/Str
                                            :kayttajatunnus s/Str
