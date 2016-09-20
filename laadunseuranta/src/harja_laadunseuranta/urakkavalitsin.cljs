38ed79e07499cae985a9b3c03cf3acd36a33b2d3
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-09-20 15:47:17 +0300

    Korosta tarkastajan omia urakoita urakkavalitsimessa

--- laadunseuranta/src/harja_laadunseuranta/urakkavalitsin.cljs
+++ laadunseuranta/src/harja_laadunseuranta/urakkavalitsin.cljs
@@ -8,21 +8,34 @@
                        :paallystys "paallystys"
                        :tiemerkinta "tiemerkinta"
 
-                       nil)]
+                       nil)
+        urakat-joissa-urakkaroolissa (filter #(= true (:urakkaroolissa? %))
+                                             urakkatyypin-urakat)
+        urakat-joissa-ei-urakkaroolissa (filter #(= false (:urakkaroolissa? %))
+                                                urakkatyypin-urakat)
+        listaus (fn [urakkalista]
+                  [:table.urakkataulu
+                   [:tbody
+                    (map-indexed
+                      (fn [i urakat]
+                        ^{:key i}
+                        [:tr.urakkarivi
+                         (for [u urakat]
+                           ^{:key (:id u)}
+                           [:td.inline.urakkapainike {:on-click #(valitse-fn u)}
+                            (:nimi u)])])
+                      (partition-all 3 urakkalista))]])]
     [:div.urakkavalitsin
-     [:h3 "Valitse urakka"]
+     [:h3.urakkaotsikko "Valitse tarkastuksen urakka"]
      (if (empty? urakkatyypin-urakat)
        [:span "Ei urakoita saatavilla urakkatyypille " urakkatyyppi]
-       [:div
-        (map-indexed
-          (fn [i urakat]
-            ^{:key i}
-            [:div.painikerivi
-             (for [u urakat]
-               ^{:key (:id u)}
-               [:button.pikavalintapainike {:on-click #(valitse-fn u)}
-                (:nimi u)])])
-          (partition-all 3 urakkatyypin-urakat))])
+       [:div.urakkataulut
+        (when-not (empty? urakat-joissa-urakkaroolissa)
+          [:div.urakoiden-valiotsikko "Omat urakat"])
+        [listaus urakat-joissa-urakkaroolissa]
+        (when-not (empty? urakat-joissa-urakkaroolissa)
+          [:div.urakoiden-valiotsikko "Muut urakat"])
+        [listaus urakat-joissa-ei-urakkaroolissa]])
      [:div
       [:span.tyhja-nappi]
       [:span.tyhja-nappi]
