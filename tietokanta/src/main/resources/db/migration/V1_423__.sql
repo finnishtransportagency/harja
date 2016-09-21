518bf28a93cc76e049cfea0feecfbce4b42d2424
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-09-21 13:59:45 +0300

    Lisää tieverkon osat taulu

---
+++ tietokanta/src/main/resources/db/migration/V1_423__.sql
@@ -0,0 +1,12 @@
+-- Tieverkon osien ajoradat
+
+CREATE TABLE tr_osan_ajorata (
+  tie INTEGER,
+  osa INTEGER,
+  ajorata INTEGER,
+  alkuajorata INTEGER,
+  oikea GEOMETRY,
+  vasen GEOMETRY
+);
+
+CREATE INDEX tr_osan_ajorata_tie_osa_idx ON tr_osan_ajorata (tie,osa);
