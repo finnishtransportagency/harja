4d528f55d7358731dd670b46aac48f0e97f55d8c
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-09-22 20:45:39 +0300

    Uusi fallback, joka löytää 1-ajr linestring osan
    Koska 0/2 ajoradoissa voi olla aukkoja, pitää niihin kohtiin leikata
    sopiva osa 1-ajoradan geometriasta. Tämä löydetään ottamalla kaikki
    1-ajr osat ja etsimällä niistä linestring, jossa nykyinen loppupiste
    esiintyy ja ottamalla sen linestringin häntä. Näitä tapauksia on noin
    1.1% prosentissa tieosien ajoratoja.

--- src/clj/harja/palvelin/integraatiot/paikkatietojarjestelma/tuonnit/tieverkko.clj
+++ src/clj/harja/palvelin/integraatiot/paikkatietojarjestelma/tuonnit/tieverkko.clj
@@ -4,46 +4,11 @@
             [clj-time.periodic :refer [periodic-seq]]
             [chime :refer [chime-at]]
             [harja.kyselyt.tieverkko :as k]
-            [harja.palvelin.integraatiot.paikkatietojarjestelma.tuonnit.shapefile :as shapefile])
+            [harja.palvelin.integraatiot.paikkatietojarjestelma.tuonnit.shapefile :as shapefile]
+            [harja.geo :as geo])
   (:import (com.vividsolutions.jts.geom Coordinate LineString MultiLineString GeometryFactory)
            (com.vividsolutions.jts.geom.impl CoordinateArraySequence)
            (com.vividsolutions.jts.operation.linemerge LineSequencer)))
-
-#_(defn paattele-alkuajorata
-  "Päättelee miltä ajoradalta tien osa alkaa. Palauttaa 0 tai 1 sen mukaan kummalla
-  ajoradalla osa alkaa. Alkupiste on tien alku tien kasvusuuntaan katsottuna."
-  [{g0 :the_geom :as ajr0} {g1 :the_geom :as ajr1}]
-  ;; Kun otetaan ensimmäiset pisteet sekä 0 että 1 ajoradoille, jompi kumpi niistä
-  ;; on alkupiste.
-  ;; Alkupiste on se, joka ei toistu muissa geometrian pisteissä.
-  (cond
-    ;; Jos jompi kumpi on nil, valitaan toinen
-    (nil? ajr0)
-    ajr1
-
-    (nil? ajr1)
-    ajr0
-
-    ;; Muuten katsotaan kumman alkuosa ei esiinny
-    :default
-    (let [coords0 (some-> g0 .getCoordinates vec)
-          coords1 (some-> g1 .getCoordinates vec)
-          alku0 (first coords0)
-          alku1 (first coords1)
-          esiintymat (frequencies (concat coords0 coords1))]
-      (cond
-        (= 1 (esiintymat alku0))
-        ajr0
-
-        (= 1 (esiintymat alku1))
-        ajr1
-
-        :default
-        (do (println "EI VOI SANOA, "
-                     "esiintymät0: " (esiintymat alku0)
-                     "; esiintymät1: " (esiintymat alku1))
-            ;; Oleta nolla, vanha toiminta säilyy
-            (or ajr0 ajr1))))))
 
 (defn- line-string-seq
   ([multilinestring]
@@ -62,6 +27,11 @@
   (let [arr (.getCoordinates g)]
     (aget arr (dec (alength arr)))))
 
+(defn luo-line-string [pisteet]
+  (LineString. (CoordinateArraySequence.
+                (into-array Coordinate pisteet))
+               (GeometryFactory.)))
+
 (defn jatkuva-line-string
   "Koska tiedetään että viivan viimeinen ja seuraavan ensimmäinen piste
   ovat sama, voidaan linestringit yhdistää helposti yhdeksi."
@@ -69,25 +39,40 @@
   (loop [pisteet (into [] (.getCoordinates l))
          [l & lines] lines]
     (if-not l
-      (LineString. (CoordinateArraySequence.
-                    (into-array Coordinate pisteet))
-                   (GeometryFactory.))
+      (luo-line-string pisteet)
       (recur (into pisteet (drop 1 (.getCoordinates l)))
              lines))))
+
+(defn- coord [c]
+  (str "X: " (.x c) ", Y: " (.y c)))
+
+(defn- piste [^Coordinate c]
+  [(.x c) (.y c)])
+
+(defn ota-ls-alkupisteella [alkupiste ls]
+  (let [loytynyt-ls (some #(when (= alkupiste (ensimmainen-piste %))
+                             %)
+                          ls)]
+    (if-not loytynyt-ls
+      [nil ls]
+      [loytynyt-ls (filter #(not= loytynyt-ls %) ls)])))
 
 (defn- yhdista-viivat
   "Yhdistää kaksi multilinestringiä siten, että viiva alkaa ensimmäisen
   multilinestringin osalla. Jos yhdistys ei onnistu siten, että kaikki
   linestringit tulevat käytettyä, palautetaan nil."
-  [g0 g1]
+  [g0 g1 fallback]
   (let [ls0 (line-string-seq g0)
         ls1 (line-string-seq g1)]
     (loop [result [(first ls0)]
            loppupiste (viimeinen-piste (first ls0))
            ls0 (rest ls0)
            ls1 ls1]
+      #_(println "ALKU-LOPPU: " (coord (ensimmainen-piste (last result)))
+               " -> " (coord (viimeinen-piste (last result))))
       (if (and (empty? ls0) (empty? ls1))
         ;; Molemmat empty, onnistui!
+        ;; HUOM: fallbackin ei tarvitse olla tyhjä
         (jatkuva-line-string result)
 
         ;; Ei vielä loppu, ota jommasta kummasta seuraava pala, joka
@@ -97,27 +82,44 @@
           (cond
             ;; ls0 jatkaa geometriaa
             (and seuraava-ls0 (= loppupiste (ensimmainen-piste seuraava-ls0)))
-            (recur (conj result seuraava-ls0)
-                   (viimeinen-piste seuraava-ls0)
-                   (rest ls0)
-                   ls1)
+            (do #_(println "LS0 " (coord loppupiste) " -> " (coord (viimeinen-piste seuraava-ls0)))
+                (recur (conj result seuraava-ls0)
+                       (viimeinen-piste seuraava-ls0)
+                       (rest ls0)
+                       ls1))
 
             ;; ls1 jatkaa geometriaa
             (and seuraava-ls1 (= loppupiste (ensimmainen-piste seuraava-ls1)))
-            (recur (conj result seuraava-ls1)
-                   (viimeinen-piste seuraava-ls1)
-                   ls0
-                   (rest ls1))
+            (do #_(println "LS1 " (coord loppupiste) " -> " (coord (viimeinen-piste seuraava-ls1)))
+                (recur (conj result seuraava-ls1)
+                       (viimeinen-piste seuraava-ls1)
+                       ls0
+                       (rest ls1)))
 
-            ;; Jatkoa ei löydy, tämä viiva ei onnistunut
+            ;; Last ditch effort: kutsutaan fallbackia etsimään
+            ;; jatkopala joko seuraavaan ls0 tai ls1 pätkään.
+            ;; fallback on funktio joka ottaa nykyisen loppupisteen
+            ;; ja seuraavat alkukohdat
             :default
-            nil))))))
+            (let [fallback-ls (and fallback
+                                   (fallback (piste loppupiste)
+                                             (some-> seuraava-ls0 ensimmainen-piste piste)
+                                             (some-> seuraava-ls1 ensimmainen-piste piste)))]
+              (if fallback-ls
+                (do #_(println "LSF " (coord loppupiste) " -> " (coord (viimeinen-piste fallback-ls)))
+                    (recur (conj result fallback-ls)
+                           (viimeinen-piste fallback-ls)
+                           ls0
+                           ls1))
+                (do #_(println "EI FALLBACKIA")
+                    nil)))))))))
 
 (defn- keraa-geometriat
   "Yhdistää 1-ajorataisen (ajr0) ja 2-ajorataisen halutun suunnan mukaisen osan
   viivat yhdeksi viivaksi. Osasta ei tiedetä kummalla ajoradalle se alkaa, mutta
   koko viiva on kulutettava, joten pitää yrittää molempia."
-  [{g0 :the_geom :as ajr0} {g1 :the_geom :as ajr1}]
+  [{g0 :the_geom :as ajr0} {g1 :the_geom :as ajr1} fallback]
+  ;(println "KERÄTÄÄN GEOM " ajr0 " JA " ajr1)
   (cond
     ;; Jos toinen on nil, valitaan suoraan toinen
     (nil? g0) (jatkuva-line-string (line-string-seq g1))
@@ -125,52 +127,35 @@
 
     ;; Muuten yhdistetään viivat molemmin päin
     :default
-    (or (yhdista-viivat g0 g1)
-        (yhdista-viivat g1 g0))))
+    (or (yhdista-viivat g0 g1 fallback)
+        (yhdista-viivat g1 g0 fallback))))
 
-(def onnistui (atom 0))
-(def ei-onnistu (atom 0))
+(defn- alkaen-pisteesta
+  "Palauttaa LineString, joka alkaa annetusta [x y] pisteestä. Jos input
+  linestring ei sisällä pistettä, palauttaa nil."
+  [ls alkupiste]
+  (let [coords (drop-while #(not= alkupiste (piste %))
+                           (seq (.getCoordinates ls)))]
+    (when (> (count coords) 1)
+      #_(println "PALAUTETAAN LEIKATTU LS alkupisteella " alkupiste
+               ", jossa coords: " coords)
+      (luo-line-string coords))))
+
+(defn luo-fallback [{g :the_geom}]
+  (fn [alkupiste & _]
+    (some #(alkaen-pisteesta % alkupiste)
+          (line-string-seq g))))
 
 (defn vie-tieosa [db tie osa osan-geometriat]
-  ;; todo: Onko ok, jos rivejä missä tätä tietoa ei ole ei tuoda?
-
   (let [ajoradat (into {}
                        (map (juxt :ajorata identity))
                        osan-geometriat)
-        oikea (keraa-geometriat (ajoradat 0) (ajoradat 1))
-        vasen (keraa-geometriat (ajoradat 0) (ajoradat 2))]
-
-    (if (nil? oikea)
-      (swap! ei-onnistu inc)
-      (swap! onnistui inc))
-    (if (nil? oikea)
-      (swap! ei-onnistu inc)
-      (swap! onnistui inc))
+        oikea (keraa-geometriat (ajoradat 0) (ajoradat 1) (constantly nil))
+        vasen (keraa-geometriat (ajoradat 0) (ajoradat 2)
+                                (luo-fallback (ajoradat 1)))]
 
     (k/vie-tien-osan-ajorata! db {:tie tie :osa osa :ajorata 1 :geom (some-> oikea str)})
-    (k/vie-tien-osan-ajorata! db {:tie tie :osa osa :ajorata 2 :geom (some-> vasen str)})
-
-    ;; Onko nämä enää tarpeellisia?
-    #_(doseq [tv osan-geometriat]
-      (k/vie-tieverkkotauluun! db (:osoite3 tv) (:tie tv) (:ajorata tv) (:osa tv) (:tiepiiri tv)
-                               (:tr_pituus tv)
-                               (.toString (:the_geom tv))))
-
-    #_(k/paivita-tr-osan-ajoradat! db {:tie tie :osa osa})))
-
-(defmacro prosessoi [sym items & body]
-  `(loop [i# 0
-          [~sym & items#] ~items]
-     (when ~sym
-       (try
-         ~@body
-         (catch Throwable t#
-           (log/error t# "Virhe prosessoinnissa rivillä " i# ", tiedot: " ~sym)
-           (throw (ex-info (str "Virhe rivillä " i#)
-                           {:data ~sym
-                            :exception t#}))))
-       (recur (inc i#)
-              items#))))
+    (k/vie-tien-osan-ajorata! db {:tie tie :osa osa :ajorata 2 :geom (some-> vasen str)})))
 
 (defn vie-tieverkko-kantaan [db shapefile]
   (if shapefile
