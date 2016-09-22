5c17e5a499cae7cdd6bd7a88dd66d1778cb25e26
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-09-22 14:26:02 +0300

    Omat rivit ajoradoille
    Tehdään ajoradoille omat rivit, ei kahta columnia

--- tietokanta/src/main/resources/db/migration/V1_423__.sql
+++ tietokanta/src/main/resources/db/migration/V1_423__.sql
@@ -3,8 +3,10 @@
 CREATE TABLE tr_osan_ajorata (
   tie INTEGER,
   osa INTEGER,
-  oikea GEOMETRY,
-  vasen GEOMETRY
+  ajorata INTEGER,
+  geom GEOMETRY
 );
 
 CREATE INDEX tr_osan_ajorata_tie_osa_idx ON tr_osan_ajorata (tie,osa);
+
+CREATE INDEX tr_osan_ajorata_geom_idx ON tr_osan_ajorata USING GIST (geom);
