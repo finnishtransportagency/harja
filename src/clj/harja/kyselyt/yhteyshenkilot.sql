9c387851c776f94c31b6a18c5bd16493f679c1d6
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-09-22 13:52:14 +0300

    Korjaa yhteyshenkilön haku sähköpostilla
    Sonjan sähköpostipalvelu lähettä viestien vastaanottajan myös formaatissa
    Erkki Esimerkki <erkki.esimerkki@example.com>
    Tämä korjataan kyselyssä tekemällä käärimällä yhteyshenkilön sähköpostiosoite wild cardeihin ja käyttämällä likehakua

--- src/clj/harja/kyselyt/yhteyshenkilot.sql
+++ src/clj/harja/kyselyt/yhteyshenkilot.sql
@@ -298,8 +298,7 @@ FROM yhteyshenkilo y
   JOIN paivystys p ON p.yhteyshenkilo = y.id
   LEFT JOIN organisaatio o ON o.id = y.organisaatio
 WHERE p.urakka = :urakka AND
-      LOWER(sahkoposti) = LOWER(:sahkoposti)
-
+      :sahkoposti ILIKE (SELECT '%' || y.sahkoposti || '%');
 
 -- name: hae-yhteyshenkilo
 SELECT
