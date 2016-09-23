c69b498c6083d552b446b88aaedb78e70aad515b
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2016-09-23 10:45:36 +0300

    Poista yhteyshenkilöt joilla on väärä rooli eikä päivystystä

---
+++ tietokanta/src/main/resources/db/migration/V1_424__.sql
@@ -0,0 +1,13 @@
+-- Poistetaan nämä roolit
+DELETE FROM yhteyshenkilo_urakka
+WHERE rooli = 'Kunnossapitopäällikkö'
+      OR rooli = 'Sillanvalvoja'
+      OR rooli = 'Kelikeskus'
+      OR rooli = 'Tieliikennekeskus';
+
+-- Poistetaan yhteyshenkilöt joilla ei ole enää muita rooleja, eikä päivystystä
+DELETE FROM yhteyshenkilo
+WHERE yhteyshenkilo.id in (SELECT yhteyshenkilo.id FROM yhteyshenkilo
+  LEFT JOIN yhteyshenkilo_urakka ON yhteyshenkilo.id = yhteyshenkilo_urakka.yhteyshenkilo
+  LEFT JOIN paivystys ON yhteyshenkilo.id = paivystys.yhteyshenkilo
+WHERE yhteyshenkilo_urakka.id IS NULL AND paivystys.id IS NULL);
