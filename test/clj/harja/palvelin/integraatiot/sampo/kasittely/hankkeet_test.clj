dcb247ce3ea4905f7603b662446bc9d8e52742db
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-09-26 14:43:05 +0300

    Korjaa testejä

--- test/clj/harja/palvelin/integraatiot/sampo/kasittely/hankkeet_test.clj
+++ test/clj/harja/palvelin/integraatiot/sampo/kasittely/hankkeet_test.clj
@@ -1,8 +1,7 @@
 (ns harja.palvelin.integraatiot.sampo.kasittely.hankkeet-test
   (:require [clojure.test :refer [deftest is use-fixtures]]
             [harja.testi :refer :all]
-            [harja.palvelin.integraatiot.sampo.tyokalut :refer :all]
-            [harja.palvelin.integraatiot.sampo.kasittely.hankkeet :as hankkeet]))
+            [harja.palvelin.integraatiot.sampo.tyokalut :refer :all]))
 
 (use-fixtures :once tietokanta-fixture)
 
@@ -16,11 +15,3 @@
   (tuo-hanke)
   (is (= 1 (count (hae-hankkeet))) "Tuotaessa sama hanke uudestaan, päivitetään vanhaa eikä luoda uutta.")
   (poista-hanke))
-
-(deftest tarkista-alueurakkanumeron-purku
-  (let [osat (hankkeet/pura-alueurakkanro "TYS-0666")]
-    (is (= "TYS" (:tyypit osat)) "Tyypit on purettu oikein")
-    (is (= "0666" (:alueurakkanro osat)) "Alueurakkanumero on purettu oikein"))
-  (let [osat (hankkeet/pura-alueurakkanro "TYS0666")]
-    (is (nil? (:tyypit osat)) "Tyyppiä ei ole päätelty")
-    (is (= "TYS0666" (:alueurakkanro osat)) "Alueurakkanumero on purettu oikein")))
