adace7d53912f14e60ef8848b576281306124391
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-09-28 10:45:14 +0300

    Palauta virhe, jos sproci antaa NULL

--- src/clj/harja/palvelin/palvelut/tierek_haku.clj
+++ src/clj/harja/palvelin/palvelut/tierek_haku.clj
@@ -33,19 +33,22 @@
   (log/debug "Haetaan viiva osoiteelle " (pr-str params))
   (let [korjattu-osoite params
         viiva? (and (:loppuosa korjattu-osoite)
-                    (:loppuetaisyys korjattu-osoite))]
-    [(geo/pg->clj
-      (if viiva?
-        (tv/tierekisteriosoite-viivaksi db
-                                        (:numero korjattu-osoite)
-                                        (:alkuosa korjattu-osoite)
-                                        (:alkuetaisyys korjattu-osoite)
-                                        (:loppuosa korjattu-osoite)
-                                        (:loppuetaisyys korjattu-osoite))
-        (tv/tierekisteriosoite-pisteeksi db
-                                         (:numero korjattu-osoite)
-                                         (:alkuosa korjattu-osoite)
-                                         (:alkuetaisyys korjattu-osoite))))]))
+                    (:loppuetaisyys korjattu-osoite))
+        geom (geo/pg->clj
+              (if viiva?
+                (tv/tierekisteriosoite-viivaksi db
+                                                (:numero korjattu-osoite)
+                                                (:alkuosa korjattu-osoite)
+                                                (:alkuetaisyys korjattu-osoite)
+                                                (:loppuosa korjattu-osoite)
+                                                (:loppuetaisyys korjattu-osoite))
+                (tv/tierekisteriosoite-pisteeksi db
+                                                 (:numero korjattu-osoite)
+                                                 (:alkuosa korjattu-osoite)
+                                                 (:alkuetaisyys korjattu-osoite))))]
+    (if geom
+      [geom]
+      {:virhe "Tierekisteriosoitetta ei löydy"})))
 
 (defn hae-osien-pituudet
   "Hakee tierekisteriosien pituudet annetulle tielle ja osan välille.
