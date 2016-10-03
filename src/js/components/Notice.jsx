ff6226fe5d7672caabc0450171f20b343d3ce994
Antti Ahola <Antti.Ahola@solita.fi>
2016-10-03 10:18:01 +0300

    Fix images in single notice

--- src/js/components/Notice.jsx
+++ src/js/components/Notice.jsx
@@ -17,30 +17,28 @@ export default React.createClass({
     let {notice} = this.props;
     const images = notice.images;
     let imagesEl = (
-        images.map(url =>
-          <div className="row">
-            <img className="harja-notice-image" src={url} alt="kuvitusta" />
+        images.map( (url, index) =>
+          <div className="column row harja-notice-image" key={index}>
+            <img src={url} />
           </div>
         )
     );
     return (
-      <div className="notice">
-        <div className="row">
+      <div className="column row notice">
+        <div className="column row">
           <h4>{notice.title}</h4>
         </div>
-        <div className="row">
+        <div className="column row">
           <img className="harja-icon medium-2 hide-for-small left columns" src="images/clock.png" alt="harja logo" srcSet="images/clock.svg" />
           <div className="harja-date medium-10 small-12 columns">{notice.date}</div>
         </div>
-        <div className="row">
+        <div className="column row">
           <h5>{notice.short}</h5>
         </div>
-        <div className="row">
+        <div className="column row">
           <p>{notice.body}</p>
         </div>
-        <div className="row">
-          {imagesEl}
-        </div>
+        {imagesEl}
       </div>
     );
   }
