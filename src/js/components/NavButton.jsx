6f50e29a264330621b0589b7e9d530a017105f50
Antti Ahola <Antti.Ahola@solita.fi>
2016-10-04 19:40:10 +0300

    Add breadcrumbs

--- src/js/components/NavButton.jsx
+++ src/js/components/NavButton.jsx
@@ -3,11 +3,22 @@ import {Events, Category} from '../enums.js';
 import pubsub from 'pubsub-js';
 
 export default React.createClass({
+
+  getDefaultProps() {
+    return {
+      buttonStyle: null
+    };
+  },
+
   onclick: function() {
     pubsub.publish(Events.NAV, this.props.item.data);
   },
 
   render: function() {
-    return <a className="button" onClick={this.onclick}>{this.props.item.title}</a>;
+    let className = 'button';
+    if (this.props.item.buttonStyle) {
+      className = this.props.item.buttonStyle;
+    }
+    return <a className={className} onClick={this.onclick}>{this.props.item.title}</a>;
   }
 });
--- src/js/components/SingleNoticeView.jsx
+++ src/js/components/SingleNoticeView.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Notice from './Notice.jsx';
 import NoticeList from './NoticeList.jsx';
+import {Category, Events} from '../enums.js';
+import NavButton from './NavButton.jsx';
 
 export default React.createClass({
   getDefaultProps() {
@@ -11,15 +13,48 @@ export default React.createClass({
     };
   },
 
+  onclick: function() {
+    pubsub.publish(Events.NAV, this.props.navLink);
+  },
+
   render() {
     let {notice, category, notices} = this.props;
+    const title = notice.title;
+    const homeLink = {title: 'Harja-projekti',
+      data: {action: Events.HOME},
+      buttonStyle: 'harja-breadcrumb'};
+    let categoryLink = '';
+    switch (category) {
+      case Category.CARE:
+        categoryLink = {title: 'Teiden hoito',
+          data: {action: Events.CATEGORY, category: Category.CARE},
+          buttonStyle: 'harja-breadcrumb'};
+        break;
+      case Category.MAINTENANCE:
+        categoryLink = {title: 'Teiden ylläpito',
+          data: {action: Events.CATEGORY, category: Category.MAINTENANCE},
+          buttonStyle: 'harja-breadcrumb'};
+        break;
+      case Category.FAQ:
+        categoryLink = {title: 'Usein Kysytyt Kysymykset',
+          data: {action: Events.CATEGORY, category: Category.FAQ},
+          buttonStyle: 'harja-breadcrumb'}
+        break;
+    }
     return (
-      <div className="row">
-        <div className="medium-8 columns">
-          <Notice notice={notice}/>
+      <div>
+        <div className="harja-breadcrumbs">
+          <div className="row column">
+            <p><NavButton item={homeLink} /><span>&gt;</span><NavButton item={categoryLink} /><span>&gt;</span><a className="harja-breadcrumb" href="#">{title}</a></p>
+          </div>
         </div>
-        <div className="medium-4 columns">
-          <NoticeList notices={notices} category={category}/>
+        <div className="row">
+          <div className="medium-8 columns">
+            <Notice notice={notice}/>
+          </div>
+          <div className="medium-4 columns">
+            <NoticeList notices={notices} category={category}/>
+          </div>
         </div>
       </div>
     );
