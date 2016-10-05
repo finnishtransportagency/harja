ffe4b407a87ddae5ca99f6b917fdc8b9e67ca693
Antti Ahola <Antti.Ahola@solita.fi>
2016-10-05 10:50:56 +0300

    Dev links to menu

--- src/js/components/DevLinks.jsx
+++ src/js/components/DevLinks.jsx
@@ -6,10 +6,10 @@ export default React.createClass({
       <div className="harja-devlinks show-for-medium">
         <div className="row align-middle text-center">
           <div className="harja-more harja-icon-link column medium-6">
-            <a className="button" href="apidoc/api.html">Katso API</a>
+            <a className="button" href="apidoc/api.html" target="_blank">Katso API</a>
           </div>
           <div className="harja-more harja-icon-link column medium-6">
-            <a className="button" href="https://github.com/finnishtransportagency/harja">Projekti GitHubissa</a>
+            <a className="button" href="https://github.com/finnishtransportagency/harja" target="_blank">Projekti GitHubissa</a>
           </div>
         </div>
       </div>
--- src/js/components/Nav.jsx
+++ src/js/components/Nav.jsx
@@ -42,6 +42,12 @@ export default React.createClass({
                     <NavButton item={link} key={link.title}/>
                   </li>)
               }
+              <li className="show-for-small-only">
+                <a className="button" href="apidoc/api.html" target="_blank">Katso API</a>
+              </li>
+              <li className="show-for-small-only">
+                <a className="button" href="https://github.com/finnishtransportagency/harja" target="_blank">Projekti GitHubissa</a>
+              </li>
             </ul>
           </div>
         </div>
