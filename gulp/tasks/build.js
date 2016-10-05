9ac6cd03b1f8d585bd3717eed421cdec6ba658a6
Antti Ahola <Antti.Ahola@solita.fi>
2016-10-05 13:43:38 +0300

    Remove temporary files

--- gulp/tasks/build.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var connect = require('gulp-connect');
-var config = require('../config').watch;
-
-gulp.task('build', ['browserify', 'styles', 'style_images', 'html'], function() {
-  gulp.src(config.src).pipe(connect.reload());
-});
--- gulp/tasks/style_images.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var config = require('../config').style_images;
-
-gulp.task('style_images', function() {
-  return gulp.src(config.src)
-    .pipe(gulp.dest(config.dest));
-});
