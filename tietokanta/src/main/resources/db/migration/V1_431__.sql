23b161bd9b8da075b5be310dcf91f972dc16abb7
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-10-06 17:48:21 +0300

    Korjaa dummy funktio

--- tietokanta/src/main/resources/db/migration/V1_431__.sql
+++ tietokanta/src/main/resources/db/migration/V1_431__.sql
@@ -1,4 +1,13 @@
 -- Luo TR osoite usealle pisteelle aggregate
+
+-- Luodaan dummy deklaraation vuoksi, myöhemmin R__Tieverkko luo oikean
+CREATE FUNCTION yrita_sama_tierekisteriosoite_pisteille (pisteet geometry[]) RETURNS tr_osoite
+AS $$
+BEGIN
+  RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE AGGREGATE sama_tr_pisteille ( geometry ) (
   sfunc = array_append,
   finalfunc = yrita_sama_tierekisteriosoite_pisteille,
