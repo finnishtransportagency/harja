07c723a92fd6858d07d57284e7e041dee48d4e75
Antti Ahola <Antti.Ahola@solita.fi>
2016-10-07 19:35:01 +0300

    Styling footer

--- src/js/components/footer.jsx
+++ src/js/components/footer.jsx
@@ -6,7 +6,7 @@ export default React.createClass({
       <footer id="harja-footer">
         <div className="row align-middle">
           <div className="columns show-for-medium medium-4 large-4">
-            <img className="livi-header-logo" src="images/livi_logo_white.png" alt="liikennevirasto logo" />
+            <img className="livi-footer-logo" src="images/livi_logo_white.png" alt="liikennevirasto logo" />
           </div>
           <div className="columns small-12 medium-8 large-8">
             <p>Tätä projektia ylläpitää <span><a href="http://www.liikennevirasto.fi/" target="_blank">Liikennevirasto</a></span></p>
