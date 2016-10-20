7f9aa1c6f11f60b7f7828e738124e78950b15f12
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-10-20 13:46:32 +0300

    Refaktoroi urakkatyypin päättely

--- src/clj/harja/palvelin/integraatiot/sampo/kasittely/urakkatyyppi.clj
+++ src/clj/harja/palvelin/integraatiot/sampo/kasittely/urakkatyyppi.clj
@@ -2,36 +2,27 @@
   (:require [clojure.string :as str]
             [taoensso.timbre :as log]))
 
-(defn paattele-alityyppi [tyypit]
-  (when (< 2 (count tyypit))
+(defn paattele-alityyppi [tyypit default-fn]
+  (if (< 2 (count tyypit))
     (let [tunniste (str/upper-case (subs tyypit 2 3))]
       (case tunniste
         "V" "valaistus"
         "P" "paallystys"
         "T" "tiemerkinta"
         "S" "siltakorjaus"
-        nil))))
-
-(defn paattele-yllapidon-alityyppi [tyypit]
-  (if-let [alityyppi (paattele-alityyppi tyypit)]
-    alityyppi
-    (do (log/error "Samposta luettiin sisään ylläpidon urakka tuntemattomalla alityypillä")
-        "paallystys")))
-
-(defn paattele-hoidon-alityyppi [tyypit]
-  (if-let [alityyppi (paattele-alityyppi tyypit)]
-    alityyppi
-    "hoito"))
+        (default-fn)))
+    (default-fn)))
 
 (defn paattele-urakkatyyppi [tyypit]
   ;; Ensimmäinen kirjain kertoo yläkategorian (tie, rata, vesi)
   ;; Toinen kirjain määrittää kuuluuko urakka hoitoon vai ylläpitoon
   ;; Kolmas kirjain määrittää lopulta palautettavan urakkatyypin (hoito, päällystys, tiemerkintä...)
-  (if (< 1 (count tyypit))
-    (let [tunniste (str/upper-case (subs tyypit 1 2))]
+  (if (< 1 (count tyypite))
+    (let [tunniste (str/upper-case (subs tyypit 1 2))
+          virhe (format "Samposta luettiin sisään ylläpidon urakka tuntemattomalla alityypillä. Tyypit: %s." tyypit)]
       (case tunniste
-        "H" (paattele-hoidon-alityyppi tyypit)
-        "Y" (paattele-yllapidon-alityyppi tyypit)
+        "H" (paattele-alityyppi tyypit #(str "hoito"))
+        "Y" (paattele-alityyppi tyypit #(do (log/error virhe) "paallystys"))
         "hoito"))
     "hoito"))
 
