0dec8c2b2773710f60796f98c890b78b7371a8b8
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-10-21 15:37:27 +0300

    Poista kuollutta koodia sanktioraportin SQL-kyselystä

--- src/clj/harja/palvelin/raportointi/raportit/sanktiot.sql
+++ src/clj/harja/palvelin/raportointi/raportit/sanktiot.sql
@@ -34,30 +34,3 @@ WHERE ((:urakka::INTEGER IS NULL AND u.urakkanro IS NOT NULL) OR u.id = :urakka)
            (:urakka::INTEGER IS NULL AND (:urakkatyyppi :: urakkatyyppi IS NULL OR
                                  u.tyyppi = :urakkatyyppi :: urakkatyyppi)))
       AND s.perintapvm BETWEEN :alku AND :loppu;
-
--- name: hae-kontekstin-urakat
--- Listaa kaikki ne urakat, joita haku koskee
-SELECT
-  u.id           AS urakka_id,
-  u.nimi         AS urakka_nimi
-FROM urakka u
-WHERE
-  (:urakka :: INTEGER IS NULL OR u.id = :urakka)
-  AND (:hallintayksikko :: INTEGER IS NULL OR hallintayksikko = :hallintayksikko)
-  AND (:urakka :: INTEGER IS NOT NULL OR
-       (:urakka :: INTEGER IS NULL AND (:urakkatyyppi :: urakkatyyppi IS NULL OR
-                                        u.tyyppi = :urakkatyyppi :: urakkatyyppi)))
-  AND (:urakka :: INTEGER IS NOT NULL OR :urakka :: INTEGER IS NULL AND ((alkupvm :: DATE BETWEEN :alku AND :loppu)
-                                                                         OR (loppupvm :: DATE BETWEEN :alku AND :loppu)
-                                                                         OR (:alku >= alkupvm AND :loppu <= loppupvm)))
-ORDER BY urakka_nimi;
-
--- name: hae-kontekstin-hallintayksikot
--- Listaa kaikki ne hallintayksikot, joita haku koskee
-SELECT
-  o.id           AS hallintayksikko_id,
-  o.nimi         AS hallintayksikko_nimi,
-  o.elynumero    AS hallintayksikko_elynumero
-FROM organisaatio o
-WHERE elynumero IS NOT NULL
-ORDER BY hallintayksikko_elynumero;
