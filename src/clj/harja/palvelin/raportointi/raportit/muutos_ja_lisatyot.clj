62468587ff1c18a7b9cba32e45c640c74015924d
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-10-21 15:37:59 +0300

    Lisää urakoittain erittely Muutos- ja lisätyöraporttiin

--- src/clj/harja/palvelin/raportointi/raportit/muutos_ja_lisatyot.clj
+++ src/clj/harja/palvelin/raportointi/raportit/muutos_ja_lisatyot.clj
@@ -6,8 +6,9 @@
             [harja.kyselyt.urakat :as urakat-q]
             [harja.kyselyt.hallintayksikot :as hallintayksikot-q]
             [harja.kyselyt.urakan-toimenpiteet :as toimenpiteet-q]
-            [harja.palvelin.raportointi.raportit.yleinen :refer [raportin-otsikko vuosi-ja-kk vuosi-ja-kk-fmt kuukaudet
-                                                                 pylvaat-kuukausittain ei-osumia-aikavalilla-teksti rivi]]
+            [harja.palvelin.raportointi.raportit.yleinen :as yleinen
+             :refer [raportin-otsikko vuosi-ja-kk vuosi-ja-kk-fmt kuukaudet
+                     pylvaat-kuukausittain ei-osumia-aikavalilla-teksti rivi]]
 
             [taoensso.timbre :as log]
             [jeesql.core :refer [defqueries]]
@@ -31,7 +32,7 @@
 (defn hae-muutos-ja-lisatyot-aikavalille
   [db user urakka-annettu? urakka-id
    urakkatyyppi hallintayksikko-annettu? hallintayksikko-id
-   toimenpide-id alkupvm loppupvm]
+   toimenpide-id alkupvm loppupvm ryhmiteltyna?]
   (let [parametrit {:urakka_annettu urakka-annettu?
                     :urakka urakka-id
                     :urakkatyyppi urakkatyyppi
@@ -39,28 +40,62 @@
                     :hallintayksikko hallintayksikko-id
                     :rajaa_tpi (not (nil? toimenpide-id)) :tpi toimenpide-id
                     :alku alkupvm :loppu loppupvm}
-        toteumat (hae-muutos-ja-lisatyot-raportille
-                       db
-                       parametrit)]
+        toteumat (if ryhmiteltyna?
+                   (hae-tyypin-mukaan-ryhmitellyt-muutos-ja-lisatyot-raportille db parametrit)
+                   (hae-muutos-ja-lisatyot-raportille db parametrit))]
     toteumat))
 
+(defn yksittaiset-tyorivit
+  [konteksti tyot]
+  (mapv #(rivi (when-not (= konteksti :urakka) (get-in % [:urakka :nimi]))
+               (pvm/pvm (:alkanut %))
+               (tyon-tyypin-nimi (:tyyppi %))
+               (get-in % [:tpi :nimi])
+               (get-in % [:tehtava :nimi])
+               (if (get-in % [:tehtava :paivanhinta])
+                 "Päivän hinta"
+                 (get-in % [:tehtava :maara]))
+               (or (get-in % [:tehtava :summa]) [:info "Ei rahasummaa"])
+               (or (:korotus %) [:info "Indeksi puuttuu"]))
+        tyot))
+
+(defn tyyppikohtaiset-rivit
+  [tyot]
+  (mapv #(rivi (tyon-tyypin-nimi (:tyyppi %))
+               (get-in % [:tpi :nimi])
+               (or (get-in % [:tehtava :summa]) [:info "Ei rahasummaa"])
+               (or (:korotus %) [:info "Indeksi puuttuu"])) tyot))
+
 (defn suorita [db user {:keys [urakka-id hallintayksikko-id toimenpide-id
-                               alkupvm loppupvm urakkatyyppi] :as parametrit}]
+                               alkupvm loppupvm urakkatyyppi urakoittain?] :as parametrit}]
   (let [konteksti (cond urakka-id :urakka
                         hallintayksikko-id :hallintayksikko
                         :default :koko-maa)
         urakka-annettu? (boolean urakka-id)
         hallintayksikko-annettu? (boolean hallintayksikko-id)
-        muutos-ja-lisatyot-kannasta (hae-muutos-ja-lisatyot-aikavalille db user
-                                                               urakka-annettu? urakka-id
-                                                               (when urakkatyyppi (name urakkatyyppi))
-                                                               hallintayksikko-annettu? hallintayksikko-id
-                                                               toimenpide-id
-                                                               alkupvm loppupvm)
-        muutos-ja-lisatyot (reverse (sort-by (juxt (comp :id :urakka) :alkanut)
-                                             (into []
-                                                   (map konv/alaviiva->rakenne)
-                                                   muutos-ja-lisatyot-kannasta)))
+        naytettavat-alueet (yleinen/naytettavat-alueet db konteksti {:urakka          urakka-id
+                                                                     :hallintayksikko hallintayksikko-id
+                                                                     :urakkatyyppi    (when urakkatyyppi (name urakkatyyppi))
+                                                                     :alku            alkupvm
+                                                                     :loppu           loppupvm})
+        kayta-ryhmittelya? (and (not urakoittain?)
+                                (not= :urakka konteksti))
+        muutos-ja-lisatyot-kannasta (into []
+                                          (map konv/alaviiva->rakenne)
+                                          (hae-muutos-ja-lisatyot-aikavalille db user
+                                                                              urakka-annettu? urakka-id
+                                                                              (when urakkatyyppi (name urakkatyyppi))
+                                                                              hallintayksikko-annettu? hallintayksikko-id
+                                                                              toimenpide-id
+                                                                              alkupvm loppupvm kayta-ryhmittelya?))
+        muutos-ja-lisatyot (if kayta-ryhmittelya?
+                             muutos-ja-lisatyot-kannasta
+                             (reverse (sort-by (juxt (comp :id :urakka) :alkanut) muutos-ja-lisatyot-kannasta)))
+
+        muutos-ja-lisatyot-hyn-mukaan (sort-by #(or (:id (first %)) 100000)
+                                               (seq (group-by :hallintayksikko
+                                                              muutos-ja-lisatyot)))
+
         raportin-nimi "Muutos- ja lisätöiden raportti"
         tpi-nimi (if toimenpide-id
                    (:nimi (first (toimenpiteet-q/hae-tuote-kolmostason-toimenpidekoodilla db {:id toimenpide-id})))
@@ -70,39 +105,63 @@
                     :urakka (:nimi (first (urakat-q/hae-urakka db urakka-id)))
                     :hallintayksikko (:nimi (first (hallintayksikot-q/hae-organisaatio db hallintayksikko-id)))
                     :koko-maa "KOKO MAA")
-                  raportin-nimi alkupvm loppupvm)]
+                  raportin-nimi alkupvm loppupvm)
+
+        otsikot (if kayta-ryhmittelya?
+                  [{:leveys 7 :otsikko "Tyyppi"}
+                   {:leveys 12 :otsikko "Toimenpide"}
+                   {:leveys 5 :otsikko "Summa €" :fmt :raha}
+                   {:leveys 5 :otsikko "Ind.korotus €" :fmt :raha}]
+                  [(when-not (= konteksti :urakka) {:leveys 10 :otsikko "Urakka"})
+                   {:leveys 5 :otsikko "Pvm"}
+                   {:leveys 7 :otsikko "Tyyppi"}
+                   {:leveys 12 :otsikko "Toimenpide"}
+                   {:leveys 12 :otsikko "Tehtävä"}
+                   {:leveys 5 :otsikko "Määrä"}
+                   {:leveys 5 :otsikko "Summa €" :fmt :raha}
+                   {:leveys 5 :otsikko "Ind.korotus €" :fmt :raha}])]
     [:raportti {:nimi raportin-nimi}
      [:taulukko {:otsikko (str otsikko ", " tpi-nimi)
                  :viimeinen-rivi-yhteenveto? true
                  :sheet-nimi raportin-nimi}
-      (keep identity [(when-not (= konteksti :urakka) {:leveys 10 :otsikko "Urakka"})
-                      {:leveys 5 :otsikko "Pvm"}
-                      {:leveys 7 :otsikko "Tyyppi"}
-                      {:leveys 12 :otsikko "Toimenpide"}
-                      {:leveys 12 :otsikko "Tehtävä"}
-                      {:leveys 5 :otsikko "Määrä"}
-                      {:leveys 5 :otsikko "Summa €" :fmt :raha}
-                      {:leveys 5 :otsikko "Ind.korotus €" :fmt :raha}])
+      (keep identity otsikot)
 
 
       (keep identity
-            (conj (mapv #(rivi (when-not (= konteksti :urakka) (get-in % [:urakka :nimi]))
-                               (pvm/pvm (:alkanut %))
-                               (tyon-tyypin-nimi (:tyyppi %))
-                               (get-in % [:tpi :nimi])
-                               (get-in % [:tehtava :nimi])
-                               (if (get-in % [:tehtava :paivanhinta])
-                                 "Päivän hinta"
-                                 (get-in % [:tehtava :maara]))
-                               (or (get-in % [:tehtava :summa])  [:info "Ei rahasummaa"])
-                               (or (:korotus %)  [:info "Indeksi puuttuu"]))
-                        muutos-ja-lisatyot)
-                  (when (not (empty? muutos-ja-lisatyot))
-                    (keep identity (flatten [(if (not= konteksti :urakka) ["Yhteensä" ""]
-                                                                          ["Yhteensä"])
-                                             "" "" "" ""
-                                             (reduce + (keep #(get-in % [:tehtava :summa]) muutos-ja-lisatyot))
-                                             (reduce + (keep :korotus muutos-ja-lisatyot))])))))]
+            (into []
+                  (concat
+                    (apply concat
+                           (for [[hy hyn-tyot] muutos-ja-lisatyot-hyn-mukaan]
+                             (concat
+                               (when (or urakoittain? (not= :urakka konteksti))
+                                 [{:otsikko (if (:nimi hy)
+                                              (str (:elynumero hy) " " (:nimi hy))
+                                              "Ilmoitukset ilman urakkaa")}])
+
+                               (when (or urakoittain? (= :urakka konteksti))
+                                 (apply concat
+                                        (for [[urakka tyot] (group-by :urakka hyn-tyot)]
+                                          (if urakoittain?
+                                            (yksittaiset-tyorivit konteksti tyot)
+                                            (tyyppikohtaiset-rivit tyot)))))
+                               (when-not (empty? hyn-tyot)
+                                 (let [summat-yht (reduce + (keep #(get-in % [:tehtava :summa]) hyn-tyot))
+                                       korotukset-yht (reduce + (keep :korotus hyn-tyot))
+                                       alueen-teksti (if (= :urakka konteksti)
+                                                       (str (get-in (first hyn-tyot) [:urakka :nimi]) " yhteensä")
+                                                       (str (:nimi hy) " yhteensä"))
+                                       kentat (if kayta-ryhmittelya?
+                                                (keep identity [alueen-teksti "" summat-yht korotukset-yht])
+                                                (if (or (not urakoittain?) (= :urakka konteksti))
+                                                 (keep identity [alueen-teksti "" "" "" "" summat-yht korotukset-yht])
+                                                 (keep identity [alueen-teksti "" "" "" "" "" summat-yht korotukset-yht])))]
+                                   (when (:nimi hy)
+                                     [{:lihavoi? true
+                                       :rivi     kentat}]))))
+
+
+
+                             )))))]
      [:teksti (str "Summat ja indeksit yhteensä "
                    (fmt/euro-opt (+
                                    (reduce + (keep #(get-in % [:tehtava :summa]) muutos-ja-lisatyot))
--- src/clj/harja/palvelin/raportointi/raportit/muutos_ja_lisatyot.sql
+++ src/clj/harja/palvelin/raportointi/raportit/muutos_ja_lisatyot.sql
@@ -27,6 +27,9 @@ SELECT
        hoitourakan_indeksilaskennan_perusluku(u.id)))    AS korotus,
   u.id                                                   AS urakka_id,
   u.nimi                                                 AS urakka_nimi,
+  hy.id                                                  AS hallintayksikko_id,
+  hy.nimi                                                AS hallintayksikko_nimi,
+  lpad(cast(hy.elynumero as varchar), 2, '0')            AS hallintayksikko_elynumero,
   tpi.id                                                 AS tpi_id,
   tpi.nimi                                               AS tpi_nimi,
   t.sopimus                                              AS sopimus_id,
@@ -48,6 +51,7 @@ FROM toteuma_tehtava tt
         mht.sopimus = t.sopimus)
   JOIN sopimus s ON t.sopimus = s.id
   JOIN urakka u ON t.urakka = u.id
+  JOIN organisaatio hy ON hy.id = u.hallintayksikko
 WHERE
   tt.poistettu IS NOT TRUE AND
   ((:urakka_annettu IS FALSE AND u.urakkanro IS NOT NULL) OR u.id = :urakka)
@@ -63,3 +67,60 @@ WHERE
                                                     FROM toimenpidekoodi tpk
                                                     WHERE tpk.emo = :tpi))
   AND t.alkanut :: DATE BETWEEN :alku AND :loppu;
+
+
+-- name: hae-tyypin-mukaan-ryhmitellyt-muutos-ja-lisatyot-raportille
+-- Hakee tyypin mukaan ryhmitellyt muutos ja lisatyöt raportille
+SELECT
+  t.tyyppi,
+  sum(COALESCE(tt.paivan_hinta, tt.maara * mht.yksikkohinta)) AS tehtava_summa,
+  sum((SELECT korotus
+   FROM laske_kuukauden_indeksikorotus(
+       (SELECT EXTRACT(YEAR FROM t.alkanut) :: INTEGER),
+       (SELECT EXTRACT(MONTH FROM t.alkanut) :: INTEGER),
+       CASE WHEN tt.indeksi IS TRUE
+         THEN (SELECT indeksi
+               FROM urakka
+               WHERE id = u.id)
+       ELSE
+         NULL
+       END,
+       COALESCE(tt.paivan_hinta, tt.maara * mht.yksikkohinta),
+       hoitourakan_indeksilaskennan_perusluku(u.id))))   AS korotus,
+  hy.id                                                  AS hallintayksikko_id,
+  hy.nimi                                                AS hallintayksikko_nimi,
+  lpad(cast(hy.elynumero as varchar), 2, '0')            AS hallintayksikko_elynumero,
+  tpi.id                                                 AS tpi_id,
+  tpi.nimi                                               AS tpi_nimi
+FROM toteuma_tehtava tt
+  JOIN toteuma t ON (tt.toteuma = t.id AND
+                     t.tyyppi IN ('akillinen-hoitotyo' :: toteumatyyppi,
+                                  'lisatyo' :: toteumatyyppi,
+                                  'muutostyo' :: toteumatyyppi,
+                                  'vahinkojen-korjaukset' :: toteumatyyppi) AND
+                     t.poistettu IS NOT TRUE)
+  JOIN toimenpidekoodi tpk4 ON tpk4.id = tt.toimenpidekoodi
+  JOIN toimenpideinstanssi tpi
+    ON (tpi.toimenpide = tpk4.emo AND tpi.urakka = t.urakka)
+  LEFT JOIN muutoshintainen_tyo mht
+    ON (mht.tehtava = tt.toimenpidekoodi AND mht.urakka = t.urakka AND
+        mht.sopimus = t.sopimus)
+  JOIN sopimus s ON t.sopimus = s.id
+  JOIN urakka u ON t.urakka = u.id
+  JOIN organisaatio hy ON hy.id = u.hallintayksikko
+WHERE
+  tt.poistettu IS NOT TRUE AND
+  ((:urakka_annettu IS FALSE AND u.urakkanro IS NOT NULL) OR u.id = :urakka)
+  AND (:urakka_annettu IS TRUE OR (:urakka_annettu IS FALSE AND
+                                   (:urakkatyyppi :: urakkatyyppi IS NULL OR
+                                    u.tyyppi =
+                                    :urakkatyyppi :: urakkatyyppi)))
+  AND (:hallintayksikko_annettu IS FALSE OR
+       u.id IN (SELECT id
+                FROM urakka
+                WHERE hallintayksikko = :hallintayksikko))
+  AND (:rajaa_tpi = FALSE OR tt.toimenpidekoodi IN (SELECT tpk.id
+                                                    FROM toimenpidekoodi tpk
+                                                    WHERE tpk.emo = :tpi))
+  AND t.alkanut :: DATE BETWEEN :alku AND :loppu
+GROUP BY hy.id, t.tyyppi, tpi.id;
