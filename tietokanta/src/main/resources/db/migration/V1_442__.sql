e42856c032d77de234322cc62f60cf0e0ae7e934
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-10-21 15:01:03 +0300

    Lisää Liville testijärjestelmätunnus

--- tietokanta/src/main/resources/db/migration/V1_442__.sql
+++ tietokanta/src/main/resources/db/migration/V1_442__.sql
@@ -1,1 +1,5 @@
-INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'hae-yhteystiedot')
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'hae-yhteystiedot');
+INSERT INTO kayttaja (kayttajanimi, organisaatio, jarjestelma, kuvaus) VALUES
+('livi', (SELECT id
+FROM organisaatio
+WHERE nimi = 'Liikennevirasto'), TRUE, 'Liikenneviraston testi järjestelmätunnus')
