850563c925f71110df69f990e1be4cc8cf7fdca5
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2016-10-21 10:15:26 +0300

    Pompauta migraatiota

--- tietokanta/src/main/resources/db/migration/V1_439__.sql
+++ tietokanta/src/main/resources/db/migration/V1_439__.sql
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-UPDATE sanktio
-SET sakkoryhma = 'muistutus' :: sanktiolaji
-WHERE maara IS NULL;
-
-UPDATE sanktio
-SET maara = NULL
-WHERE sakkoryhma = 'muistutus' :: sanktiolaji;
-
-DELETE FROM sanktio WHERE sakkoryhma IS NULL;
-
-ALTER TABLE sanktio
-  ALTER COLUMN sakkoryhma SET NOT NULL,
-  ADD CONSTRAINT sakoille_on_maara
-CHECK ((maara IS NOT NULL AND sakkoryhma != 'muistutus' :: sanktiolaji) OR
-       (maara IS NULL AND sakkoryhma = 'muistutus' :: sanktiolaji))
-=======
 ALTER TABLE ilmoitus
   RENAME COLUMN ilmoittaja_tyyppi TO ilmoittaja_tyyppi_temp;
 ALTER TABLE ilmoitus
@@ -28,4 +11,3 @@ ALTER TABLE ilmoitus
   DROP COLUMN ilmoittaja_tyyppi_temp;
 
 DROP TYPE ilmoittajatyyppi;
->>>>>>> develop
