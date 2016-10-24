77b28db991db3e267fa4ba1d4d9f71f8d1b9e9f4
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-10-24 15:54:46 +0300

    Lisää testit rajapinnalle

--- test/clj/harja/palvelin/integraatiot/api/yhteystiedot_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/yhteystiedot_test.clj
@@ -1,169 +1,39 @@
 (ns harja.palvelin.integraatiot.api.yhteystiedot-test
   (:require [clojure.test :refer [deftest is use-fixtures]]
             [harja.testi :refer :all]
-            [taoensso.timbre :as log]
             [harja.palvelin.integraatiot.api.tyokalut :as api-tyokalut]
             [com.stuartsierra.component :as component]
             [harja.palvelin.integraatiot.api.yhteystiedot :as api-yhteystiedot]
             [org.httpkit.fake :refer [with-fake-http]]
-            [harja.palvelin.komponentit.fim :as fim]))
+            [harja.palvelin.komponentit.fim :as fim]
+            [cheshire.core :as cheshire]
+            [clojure.walk :as walk]))
 
 (def livi-jarjestelmakayttaja "livi")
 (def urakoitsija-jarjestelmakayttaja "skanska")
 
-(def fim-url "https://localhost:6666/FIM/SimpleREST4FIM/1/Group.svc/getGroupUsersFromEntitity")
+(def fim-url "https://testi.oag/FIM")
 
 (def fim-vastaus "<Members><member>
 <AccountName>A000014</AccountName>
-<FirstName>Tarja</FirstName>
-<LastName>Bäck</LastName>
-<DisplayName>Bäck Tarja</DisplayName>
-<Email>tarja.back@ely-keskus.fi</Email>
-<MobilePhone></MobilePhone>
+<FirstName>Erkki</FirstName>
+<LastName>Elyläinen</LastName>
+<DisplayName>Elyläinen erkki</DisplayName>
+<Email>erkki@example.com</Email>
+<MobilePhone>0982345</MobilePhone>
 <Company>ELY</Company>
-<Role>PR00015003_ELY_Urakanvalvoja</Role>
+<Role>1242141-OULU2_ELY_Urakanvalvoja</Role>
 </member>
 <member>
-<AccountName>A009910</AccountName>
-<FirstName>Jorma</FirstName>
-<LastName>Lusikka</LastName>
-<DisplayName>Lusikka Jorma</DisplayName>
-<Email>jorma.lusikka@ely-keskus.fi</Email>
-<MobilePhone>+358400290283</MobilePhone>
-<Company>ELY</Company>
-<Role>PR00015003_ELY_Urakanvalvoja</Role>
-</member>
-<member>
-<AccountName>A010008</AccountName>
-<FirstName>Irma</FirstName>
-<LastName>Petäjäjärvi</LastName>
-<DisplayName>Petäjäjärvi Irma</DisplayName>
-<Email>irma.petajajarvi@ely-keskus.fi</Email>
-<MobilePhone>+358407265193</MobilePhone>
-<Company>ELY</Company>
-<Role>PR00015003_ELY_Urakanvalvoja</Role>
-</member>
-<member>
-<AccountName>A010150</AccountName>
-<FirstName>Pekka</FirstName>
-<LastName>Toiviainen</LastName>
-<DisplayName>Toiviainen Pekka</DisplayName>
-<Email>pekka.toiviainen@ely-keskus.fi</Email>
-<MobilePhone>+358400293138</MobilePhone>
-<Company>ELY</Company>
-<Role>PR00015003_ELY_Urakanvalvoja</Role>
-</member>
-<member>
-<AccountName>A004133</AccountName>
-<FirstName>Jukka</FirstName>
-<LastName>Vanhanen</LastName>
-<DisplayName>Vanhanen Jukka</DisplayName>
-<Email>jukka.vanhanen@ely-keskus.fi</Email>
-<MobilePhone></MobilePhone>
-<Company>ELY</Company>
-<Role>PR00015003_ELY_Urakanvalvoja</Role>
-</member>
-<member>
-<AccountName>A010186</AccountName>
-<FirstName>Elisa</FirstName>
-<LastName>Virta</LastName>
-<DisplayName>Virta Elisa</DisplayName>
-<Email>elisa.virta@ely-keskus.fi</Email>
-<MobilePhone></MobilePhone>
-<Company>ELY</Company>
-<Role>PR00015003_ELY_Urakanvalvoja</Role>
-</member>
-<member>
-<AccountName>U007790</AccountName>
-<FirstName>Tero</FirstName>
-<LastName>Vilen</LastName>
-<DisplayName>Vilen Tero</DisplayName>
-<Email>tero.vilen@carement.fi</Email>
-<MobilePhone>+358401848982</MobilePhone>
-<Company>ELY</Company>
-<Role>PR00015003_ELY_Laadunvalvoja</Role>
-</member>
-<member>
-<AccountName>LX444316</AccountName>
-<FirstName>Pilvi</FirstName>
-<LastName>Hyvönen</LastName>
-<DisplayName>Hyvönen Pilvi YIT Rakennus Oy</DisplayName>
-<Email>pilvi.hyvonen@yit.fi</Email>
-<MobilePhone>0503831293</MobilePhone>
+<AccountName>LXTESTI</AccountName>
+<FirstName>Ulla</FirstName>
+<LastName>Urakoitsija</LastName>
+<DisplayName>Ulla Urakoitsija YIT Rakennus Oy</DisplayName>
+<Email>ulla@example.com</Email>
+<MobilePhone>234234</MobilePhone>
 <Company>YIT Rakennus Oy</Company>
-<Role>PR00015003_vastuuhenkilo</Role>
-</member>
-<member>
-<AccountName>LXKARHUJA</AccountName>
-<FirstName>Jaakko</FirstName>
-<LastName>Karhu</LastName>
-<DisplayName>Karhu Jaakko YIT Rakennus Oy</DisplayName>
-<Email>jaakko.karhu@yit.fi</Email>
-<MobilePhone>0504402557</MobilePhone>
-<Company>YIT Rakennus Oy</Company>
-<Role>PR00015003_vastuuhenkilo</Role>
-</member>
-<member>
-<AccountName>LX147762</AccountName>
-<FirstName>Pekka</FirstName>
-<LastName>Lääkkö</LastName>
-<DisplayName>Lääkkö Pekka YIT Rakennus Oy</DisplayName>
-<Email>pekka.laakko@yit.fi</Email>
-<MobilePhone>0503828213</MobilePhone>
-<Company>YIT Rakennus Oy</Company>
-<Role>PR00015003_vastuuhenkilo</Role>
-</member>
-<member>
-<AccountName>LXNISSIIL</AccountName>
-<FirstName>Ilkka</FirstName>
-<LastName>Nissilä</LastName>
-<DisplayName>Nissilä Ilkka YIT Rakennus Oy</DisplayName>
-<Email>Ilkka.nissila@yit.fi</Email>
-<MobilePhone>0403570833</MobilePhone>
-<Company>YIT Rakennus Oy</Company>
-<Role>PR00015003_vastuuhenkilo</Role>
-</member>
-<member>
-<AccountName>LX152636</AccountName>
-<FirstName>Panu</FirstName>
-<LastName>Palvelukeskus</LastName>
-<DisplayName>Palvelukeskus Panu YIT Rakennus Oy</DisplayName>
-<Email>palvelukeskus.panu@gmail.com</Email>
-<MobilePhone>0505393879</MobilePhone>
-<Company>YIT Rakennus Oy</Company>
-<Role>PR00015003_vastuuhenkilo</Role>
-</member>
-<member>
-<AccountName>LX844229</AccountName>
-<FirstName>Henna</FirstName>
-<LastName>Penttilä</LastName>
-<DisplayName>Penttilä Henna YIT Rakennus Oy</DisplayName>
-<Email>henna.penttila@yit.fi</Email>
-<MobilePhone>0505600569</MobilePhone>
-<Company>YIT Rakennus Oy</Company>
-<Role>PR00015003_vastuuhenkilo</Role>
-</member>
-<member>
-<AccountName>LXSAUREJA</AccountName>
-<FirstName>Jarno</FirstName>
-<LastName>Saurento</LastName>
-<DisplayName>Saurento Jarno YIT Rakennus Oy</DisplayName>
-<Email>jarno.saurento@yit.fi</Email>
-<MobilePhone>0503902669</MobilePhone>
-<Company>YIT Rakennus Oy</Company>
-<Role>PR00015003_vastuuhenkilo</Role>
-</member>
-<member>
-<AccountName>LXYITPA</AccountName>
-<FirstName>Panu</FirstName>
-<LastName>Yit</LastName>
-<DisplayName>Yit Panu YIT Rakennus Oy</DisplayName>
-<Email>yr.kelipalvelu@yit.fi</Email>
-<MobilePhone>0503906852</MobilePhone>
-<Company>YIT Rakennus Oy</Company>
-<Role>PR00015003_vastuuhenkilo</Role>
-</member>
-</Members>")
+<Role>1242141-OULU2_vastuuhenkilo</Role>
+</member>\n</Members>")
 
 (def jarjestelma-fixture
   (laajenna-integraatiojarjestelmafixturea
@@ -177,12 +47,19 @@
 
 (use-fixtures :each jarjestelma-fixture)
 
-(deftest tarkista-yllapitokohteiden-haku
+(deftest tarkista-yhteystietojen-haku
   (with-fake-http
     [(str "http://localhost:" portti "/api/urakat/yhteystiedot/1238") :allow
      fim-url fim-vastaus]
-    (let [vastaus (api-tyokalut/get-kutsu "/api/urakat/yhteystiedot/1238" livi-jarjestelmakayttaja portti)]
-      (log/debug "Vastaus saatiin: " (pr-str vastaus))
-      (is (= 200 (:status vastaus)) "Haku onnistui validilla kutsulla"))))
+    (let [vastaus (api-tyokalut/get-kutsu "/api/urakat/yhteystiedot/1238" livi-jarjestelmakayttaja portti)
+          odotettu-vastaus "{\"urakka\":{\"elynro\":12,\"alueurakkanro\":\"1238\",\"loppupvm\":\"2019-09-29T21:00:00Z\",\"nimi\":\"Oulun alueurakka 2014-2019\",\"sampoid\":\"1242141-OULU2\",\"alkupvm\":\"2014-09-30T21:00:00Z\",\"elynimi\":\"Pohjois-Pohjanmaa\",\"urakoitsija\":{\"nimi\":\"YIT Rakennus Oy\",\"ytunnus\":\"1565583-5\",\"katuosoite\":\"Panuntie 11, PL 36\",\"postinumero\":\"621  \"},\"yhteyshenkilot\":[{\"yhteyshenkilo\":{\"rooli\":\"ELY urakanvalvoja\",\"nimi\":\"Erkki Elyläinen\",\"puhelinnumero\":\"0982345\",\"email\":\"erkki@example.com\",\"organisaatio\":\"ELY\"}},{\"yhteyshenkilo\":{\"rooli\":\"Urakan vastuuhenkilö\",\"nimi\":\"Ulla Urakoitsija\",\"puhelinnumero\":\"234234\",\"email\":\"ulla@example.com\",\"organisaatio\":\"YIT Rakennus Oy\"}}]}}"
+          data (walk/keywordize-keys (cheshire/decode (:body vastaus)))]
+      (is (= 200 (:status vastaus)) "Haku onnistui validilla kutsulla")
+      (is (= odotettu-vastaus (:body vastaus)))
+      (is (= 2 (count(get-in data [:urakka :yhteyshenkilot])))))))
+
+(deftest tarkista-oikeudet-rajapintaan
+  (let [vastaus (api-tyokalut/get-kutsu "/api/urakat/yhteystiedot/1238" urakoitsija-jarjestelmakayttaja portti)]
+    (is (= 403 (:status vastaus)) "Urakoitsijan järjestelmätunnuksella ei ole oikeutta palveluun")))
 
 
