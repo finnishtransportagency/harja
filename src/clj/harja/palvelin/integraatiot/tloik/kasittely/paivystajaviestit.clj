ce3749422f0c5dc538af421204e3ef71713f2c56
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-10-25 14:04:19 +0300

    Lähetä tekstiviestit vain vastuullisille päivystäjille

--- src/clj/harja/palvelin/integraatiot/tloik/kasittely/paivystajaviestit.clj
+++ src/clj/harja/palvelin/integraatiot/tloik/kasittely/paivystajaviestit.clj
@@ -18,6 +18,8 @@
   (tloik-tekstiviesti/laheta-ilmoitus-tekstiviestilla sms db ilmoitus paivystaja))
 
 (defn laheta [{sms :sms :as ilmoitusasetukset} db ilmoitus paivystaja]
-  (when (and (= "toimenpidepyynto" (:ilmoitustyyppi ilmoitus)) sms)
+  (when (and (= "toimenpidepyynto" (:ilmoitustyyppi ilmoitus))
+             (:vastuuhenkilo paivystaja)
+             sms)
     (laheta-ilmoitus-tekstiviestilla sms db ilmoitus paivystaja))
   (laheta-ilmoitus-sahkopostilla ilmoitusasetukset ilmoitus paivystaja))
