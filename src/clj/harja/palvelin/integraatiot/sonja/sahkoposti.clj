16d341e10abc0c5892a82cfb436b5994a0c85746
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-10-25 11:45:26 +0300

    Älä välitä erillistä viesti-id:tä sähköpostilähetyksessä

--- src/clj/harja/palvelin/integraatiot/sonja/sahkoposti.clj
+++ src/clj/harja/palvelin/integraatiot/sonja/sahkoposti.clj
@@ -55,7 +55,7 @@
     (let [viesti-id (str (UUID/randomUUID))
           sahkoposti (sanomat/sahkoposti viesti-id lahettaja vastaanottaja otsikko sisalto)
           viesti (xml/tee-xml-sanoma sahkoposti)]
-      (jms-lahettaja viesti viesti-id)))
+      (jms-lahettaja viesti)))
 
   (vastausosoite [this]
     vastausosoite))
