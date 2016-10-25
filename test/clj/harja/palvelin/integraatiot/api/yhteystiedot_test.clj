74573aa721df018703095da4d4dfdbc8e53a6c53
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-10-25 09:02:56 +0300

    Tarkenna testejä

--- test/clj/harja/palvelin/integraatiot/api/yhteystiedot_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/yhteystiedot_test.clj
@@ -33,7 +33,8 @@
 <MobilePhone>234234</MobilePhone>
 <Company>YIT Rakennus Oy</Company>
 <Role>1242141-OULU2_vastuuhenkilo</Role>
-</member>\n</Members>")
+</member>
+</Members>")
 
 (def jarjestelma-fixture
   (laajenna-integraatiojarjestelmafixturea
@@ -47,16 +48,25 @@
 
 (use-fixtures :each jarjestelma-fixture)
 
+(defn sisaltaa-roolin? [yhteyshenkilot rooli]
+  (some #(= rooli (get-in % [:yhteyshenkilo :rooli])) yhteyshenkilot))
+
 (deftest tarkista-yhteystietojen-haku
   (with-fake-http
     [(str "http://localhost:" portti "/api/urakat/yhteystiedot/1238") :allow
      fim-url fim-vastaus]
     (let [vastaus (api-tyokalut/get-kutsu "/api/urakat/yhteystiedot/1238" livi-jarjestelmakayttaja portti)
-          odotettu-vastaus "{\"urakka\":{\"elynro\":12,\"alueurakkanro\":\"1238\",\"loppupvm\":\"2019-09-29T21:00:00Z\",\"nimi\":\"Oulun alueurakka 2014-2019\",\"sampoid\":\"1242141-OULU2\",\"alkupvm\":\"2014-09-30T21:00:00Z\",\"elynimi\":\"Pohjois-Pohjanmaa\",\"urakoitsija\":{\"nimi\":\"YIT Rakennus Oy\",\"ytunnus\":\"1565583-5\",\"katuosoite\":\"Panuntie 11, PL 36\",\"postinumero\":\"621  \"},\"yhteyshenkilot\":[{\"yhteyshenkilo\":{\"rooli\":\"ELY urakanvalvoja\",\"nimi\":\"Erkki Elyläinen\",\"puhelinnumero\":\"0982345\",\"email\":\"erkki@example.com\",\"organisaatio\":\"ELY\"}},{\"yhteyshenkilo\":{\"rooli\":\"Urakan vastuuhenkilö\",\"nimi\":\"Ulla Urakoitsija\",\"puhelinnumero\":\"234234\",\"email\":\"ulla@example.com\",\"organisaatio\":\"YIT Rakennus Oy\"}}]}}"
-          data (walk/keywordize-keys (cheshire/decode (:body vastaus)))]
+          odotettu-vastaus "{\"urakka\":{\"elynro\":12,\"alueurakkanro\":\"1238\",\"loppupvm\":\"2019-09-29T21:00:00Z\",\"nimi\":\"Oulun alueurakka 2014-2019\",\"sampoid\":\"1242141-OULU2\",\"alkupvm\":\"2014-09-30T21:00:00Z\",\"elynimi\":\"Pohjois-Pohjanmaa\",\"urakoitsija\":{\"nimi\":\"YIT Rakennus Oy\",\"ytunnus\":\"1565583-5\",\"katuosoite\":\"Panuntie 11, PL 36\",\"postinumero\":\"621  \"},\"yhteyshenkilot\":[{\"yhteyshenkilo\":{\"rooli\":\"ELY urakanvalvoja\",\"nimi\":\"Erkki Elyläinen\",\"puhelinnumero\":\"0982345\",\"email\":\"erkki@example.com\",\"organisaatio\":\"ELY\"}},{\"yhteyshenkilo\":{\"rooli\":\"Urakan vastuuhenkilö\",\"nimi\":\"Ulla Urakoitsija\",\"puhelinnumero\":\"234234\",\"email\":\"ulla@example.com\",\"organisaatio\":\"YIT Rakennus Oy\"}},{\"yhteyshenkilo\":{\"rooli\":\"Kunnossapitopäällikkö\",\"nimi\":\"Jouko Kasslin\",\"puhelinnumero\":\"046 248 7808\",\"email\":\"JoukoKasslin@gustr.com\",\"organisaatio\":\"YIT Rakennus Oy\"}},{\"yhteyshenkilo\":{\"rooli\":\"Sillanvalvoja\",\"nimi\":\"Tiina Frösén\",\"puhelinnumero\":\"042 869 5938\",\"email\":\"TiinaFrosen@teleworm.us\",\"organisaatio\":\"YIT Rakennus Oy\"}}]}}"
+          data (walk/keywordize-keys (cheshire/decode (:body vastaus)))
+          yhteyshenkilot (get-in data [:urakka :yhteyshenkilot])]
       (is (= 200 (:status vastaus)) "Haku onnistui validilla kutsulla")
+      (println vastaus)
       (is (= odotettu-vastaus (:body vastaus)))
-      (is (= 2 (count(get-in data [:urakka :yhteyshenkilot])))))))
+      (is (= 4 (count yhteyshenkilot)))
+      (is (sisaltaa-roolin? yhteyshenkilot "Sillanvalvoja"))
+      (is (sisaltaa-roolin? yhteyshenkilot "Urakan vastuuhenkilö"))
+      (is (sisaltaa-roolin? yhteyshenkilot "ELY urakanvalvoja"))
+      (is (sisaltaa-roolin? yhteyshenkilot "Kunnossapitopäällikkö")))))
 
 (deftest tarkista-oikeudet-rajapintaan
   (let [vastaus (api-tyokalut/get-kutsu "/api/urakat/yhteystiedot/1238" urakoitsija-jarjestelmakayttaja portti)]
