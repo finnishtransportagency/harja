9ba67fe010e36b68edbe2b2b48ca033692821fe8
Jari Hanhela <jarihanhela@gmail.com>
2016-10-27 12:27:43 +0300

    Hae tiemerkintäurakan sampo-id oikein sähköpostilähetykseen

--- src/clj/harja/kyselyt/yllapitokohteet.sql
+++ src/clj/harja/kyselyt/yllapitokohteet.sql
@@ -448,7 +448,8 @@ SELECT
   pu.id as "paallystysurakka-id",
   pu.nimi as "paallystysurakka-nimi",
   tu.id as "tiemerkintaurakka-id",
-  tu.nimi as "tiemerkintaurakka-nimi"
+  tu.nimi as "tiemerkintaurakka-nimi",
+  tu.sampoid as "tiemerkintaurakka-sampo-id"
 FROM yllapitokohde ypk
   JOIN urakka pu ON ypk.urakka = pu.id
   JOIN urakka tu ON ypk.urakka = tu.id
