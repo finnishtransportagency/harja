111a44417198c58776d25997dead184550549144
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-10-27 15:14:54 +0300

    Mahdollista muistutus myös Sorat. ja Liik. ymp. hoitoon

--- tietokanta/src/main/resources/db/migration/V1_444__.sql
+++ tietokanta/src/main/resources/db/migration/V1_444__.sql
@@ -12,4 +12,12 @@ ALTER TABLE sanktio
   ALTER COLUMN sakkoryhma SET NOT NULL,
   ADD CONSTRAINT sakoille_on_maara
 CHECK ((maara IS NOT NULL AND sakkoryhma != 'muistutus' :: sanktiolaji) OR
-       (maara IS NULL AND sakkoryhma = 'muistutus' :: sanktiolaji))
+       (maara IS NULL AND sakkoryhma = 'muistutus' :: sanktiolaji));
+
+UPDATE sanktiotyyppi
+   SET sanktiolaji = ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'muistutus'::sanktiolaji]
+ WHERE nimi = 'Liikenneympäristön hoito';
+
+UPDATE sanktiotyyppi
+SET sanktiolaji = ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'muistutus'::sanktiolaji]
+WHERE nimi = 'Sorateiden hoito ja ylläpito';
