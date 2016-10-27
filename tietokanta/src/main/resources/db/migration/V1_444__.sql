4d85a9f3f6ca68562ae58ca71f55a2e971c90079
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-10-27 15:43:26 +0300

    Palauta sanktio updatet migraatiosta

--- tietokanta/src/main/resources/db/migration/V1_444__.sql
+++ tietokanta/src/main/resources/db/migration/V1_444__.sql
@@ -1,3 +1,13 @@
+UPDATE sanktio
+SET sakkoryhma = 'muistutus' :: sanktiolaji
+WHERE maara IS NULL;
+
+UPDATE sanktio
+SET maara = NULL
+WHERE sakkoryhma = 'muistutus' :: sanktiolaji;
+
+DELETE FROM sanktio WHERE sakkoryhma IS NULL;
+
 ALTER TABLE sanktio
   ALTER COLUMN sakkoryhma SET NOT NULL,
   ADD CONSTRAINT sakoille_on_maara
