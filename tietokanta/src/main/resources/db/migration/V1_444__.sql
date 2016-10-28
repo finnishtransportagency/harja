4092e49e0ec799af3044f3824db38f98e91b98eb
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-10-28 07:39:58 +0300

    Käännä sanktiotyypin järjestys migraatiossa

--- tietokanta/src/main/resources/db/migration/V1_444__.sql
+++ tietokanta/src/main/resources/db/migration/V1_444__.sql
@@ -1,3 +1,11 @@
+UPDATE sanktiotyyppi
+SET sanktiolaji = ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'muistutus'::sanktiolaji]
+WHERE nimi = 'Liikenneympäristön hoito';
+
+UPDATE sanktiotyyppi
+SET sanktiolaji = ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'muistutus'::sanktiolaji]
+WHERE nimi = 'Sorateiden hoito ja ylläpito';
+
 UPDATE sanktio
 SET sakkoryhma = 'muistutus' :: sanktiolaji
 WHERE maara IS NULL;
@@ -13,11 +21,3 @@ ALTER TABLE sanktio
   ADD CONSTRAINT sakoille_on_maara
 CHECK ((maara IS NOT NULL AND sakkoryhma != 'muistutus' :: sanktiolaji) OR
        (maara IS NULL AND sakkoryhma = 'muistutus' :: sanktiolaji));
-
-UPDATE sanktiotyyppi
-   SET sanktiolaji = ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'muistutus'::sanktiolaji]
- WHERE nimi = 'Liikenneympäristön hoito';
-
-UPDATE sanktiotyyppi
-SET sanktiolaji = ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'muistutus'::sanktiolaji]
-WHERE nimi = 'Sorateiden hoito ja ylläpito';
