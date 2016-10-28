eeea4b44672eb2ce8b405dcf1f6036134f30066e
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-10-28 09:03:28 +0300

    Poista testifunktio

--- src/clj/harja/palvelin/integraatiot/tloik/tloik_komponentti.clj
+++ src/clj/harja/palvelin/integraatiot/tloik/tloik_komponentti.clj
@@ -33,16 +33,12 @@
                ilmoitusasetukset klusterin-tapahtumat db ilmoituskuittausjono
                jms-lahettaja))))
 
-(defn hilipati [t]
-  (println "---> " t)
-  (:viesti-id t))
-
 (defn tee-toimenpidekuittauskuuntelija [this toimenpidekuittausjono]
   (when (and toimenpidekuittausjono (not (empty? toimenpidekuittausjono)))
     (jms/kuittausjonokuuntelija
       (tee-lokittaja this "toimenpiteen-lahetys") (:sonja this) toimenpidekuittausjono
       (fn [kuittaus] (tloik-kuittaus-sanoma/lue-kuittaus kuittaus))
-      (fn [t] (hilipati t))
+      :viesti-id
       (comp not :virhe)
       (fn [_ viesti-id onnistunut]
         (ilmoitustoimenpiteet/vastaanota-kuittaus (:db this) viesti-id onnistunut)))))
