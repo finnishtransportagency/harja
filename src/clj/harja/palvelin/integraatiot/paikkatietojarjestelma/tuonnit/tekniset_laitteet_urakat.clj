51843698b095bd4260cb05367cd06092cf8f98f3
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-11-02 15:18:41 +0200

    Lisää pohjat geometriapäivitykselle

---
+++ src/clj/harja/palvelin/integraatiot/paikkatietojarjestelma/tuonnit/tekniset_laitteet_urakat.clj
@@ -0,0 +1,24 @@
+(ns harja.palvelin.integraatiot.paikkatietojarjestelma.tuonnit.tekniset-laitteet-urakat
+  (:require [taoensso.timbre :as log]
+            [clojure.java.jdbc :as jdbc]
+            [clj-time.periodic :refer [periodic-seq]]
+            [chime :refer [chime-at]]
+            [harja.kyselyt.hoitoluokat :as hoitoluokat]
+            [harja.palvelin.integraatiot.paikkatietojarjestelma.tuonnit.shapefile :as shapefile]))
+
+(defn vie-urakka-entry [db urakka]
+  (println "----> " urakka)
+  #_(if (:the_geom urakka)
+    ()
+    (log/warn "Talvihoitoluokkaa ei voida tuoda ilman geometriaa. Virheviesti: " (:loc_error urakka))))
+
+(defn vie-tekniset-laitteet-urakat-kantaan [db shapefile]
+  (if shapefile
+    (do
+      (log/debug (str "Tuodaan tekniset laitteet urakat kantaan tiedostosta " shapefile))
+      (jdbc/with-db-transaction [db db]
+        ;; todo: nuketa kannasta vanhat
+        (doseq [urakka (shapefile/tuo shapefile)]
+          (vie-urakka-entry db urakka))
+        (log/debug "Tekniset laitteet urakoiden tuonti kantaan valmis")))
+    (log/debug "Tekniset laitteet urakoiden tiedostoa ei löydy konfiguraatiosta. Tuontia ei suoriteta.")))
