8d9e094dabace0babb15ada07eb823f3d3b55ad8
Jari Hanhela <jarihanhela@gmail.com>
2016-11-02 09:26:48 +0200

    Lisää uusi taulu kayttajan_lisaoikeudet_urakkaan

---
+++ tietokanta/src/main/resources/db/migration/V1_447__.sql
@@ -0,0 +1,8 @@
+-- Uusi kantataulu, jossa järjestelmä-käyttäjille annetaan oikeudet toiseen urakkaan
+
+CREATE TABLE kayttajan_lisaoikeudet_urakkaan (
+  id          SERIAL PRIMARY KEY,
+  kayttaja    INTEGER REFERENCES kayttaja (id) NOT NULL,
+  urakka      INTEGER REFERENCES urakka (id) NOT NULL
+);
+COMMENT ON TABLE kayttajan_lisaoikeudet_urakkaan IS 'Rivi tässä taulussa antaa järjestelmäkäyttäjälle oikeudet urakkaan.';
