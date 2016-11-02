8ffff7f671474669699013c9f8831e9bd536b963
Jari Hanhela <jarihanhela@gmail.com>
2016-11-02 12:57:57 +0200

    Lisää testejä api-järjestelmätunnusten palvelulle

--- test/clj/harja/palvelin/palvelut/api_jarjestelmatunnukset_test.clj
+++ test/clj/harja/palvelin/palvelut/api_jarjestelmatunnukset_test.clj
@@ -12,14 +12,14 @@
 
 (defn jarjestelma-fixture [testit]
   (alter-var-root #'jarjestelma
-    (fn [_]
-      (component/start
-        (component/system-map
-          :db (tietokanta/luo-tietokanta testitietokanta)
-          :http-palvelin (testi-http-palvelin)
-          :api-jarjestelmatunnukset (component/using
-                                      (api-jarjestelmatunnukset/->APIJarjestelmatunnukset)
-                      [:http-palvelin :db])))))
+                  (fn [_]
+                    (component/start
+                      (component/system-map
+                        :db (tietokanta/luo-tietokanta testitietokanta)
+                        :http-palvelin (testi-http-palvelin)
+                        :api-jarjestelmatunnukset (component/using
+                                                    (api-jarjestelmatunnukset/->APIJarjestelmatunnukset)
+                                                    [:http-palvelin :db])))))
 
   (testit)
   (alter-var-root #'jarjestelma component/stop))
@@ -56,10 +56,11 @@
       (is e))))
 
 (deftest urakoiden-haku-toimii
-  (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+  (let [odotettu-maara (ffirst (q "SELECT COUNT(*) FROM urakka;"))
+        vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
                                 :hae-urakat-lisaoikeusvalintaan +kayttaja-jvh+ nil)]
     (is (vector? vastaus))
-    (is (= (count vastaus) 23))))
+    (is (= (count vastaus) odotettu-maara))))
 
 (deftest urakoiden-haku-ei-toimi-ilman-oikeuksia
   (try+
@@ -69,5 +70,18 @@
     (catch EiOikeutta e
       (is e))))
 
+(deftest jarjestelmatunnusten-tallennus-toimii
+  (let [testitunnukset [{:id -1, :kayttajanimi "juha88",
+                         :kuvaus "noni",
+                         :organisaatio {:nimi "Liikennevirasto", :id 1}}
+                        {:id (ffirst (q "SELECT id FROM kayttaja WHERE kayttajanimi = 'yit-rakennus'")),
+                         :kayttajanimi "yit-rakennus",
+                         :kuvaus "testissä muutettu",
+                         :organisaatio {:nimi "Liikennevirasto", :id 1}}]
+        _ (kutsu-palvelua (:http-palvelin jarjestelma)
+                          :tallenna-jarjestelmatunnukset +kayttaja-jvh+ testitunnukset)]
 
-
+    (is (integer? (ffirst (q "SELECT id FROM kayttaja WHERE kayttajanimi = 'juha88'"))))
+    (is (= (ffirst (q "SELECT kuvaus FROM kayttaja WHERE kayttajanimi = 'yit-rakennus'"))
+           "testissä muutettu"))
+    (u "DELETE FROM kayttaja WHERE kayttajanimi = 'juha88';")))
