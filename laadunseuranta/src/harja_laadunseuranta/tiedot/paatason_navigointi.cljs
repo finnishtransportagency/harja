68e827bda65835bbb9b9664c64f754565a093a00
Jari Hanhela <jarihanhela@gmail.com>
2016-11-04 11:57:55 +0200

    Lisää välilehdille tieto, minkä tyyppisiä ne on

--- laadunseuranta/src/harja_laadunseuranta/tiedot/paatason_navigointi.cljs
+++ laadunseuranta/src/harja_laadunseuranta/tiedot/paatason_navigointi.cljs
@@ -4,24 +4,44 @@
   [{:avain :talvihoito
     :nimi "Talvihoito"
     :sisalto [{:nimi "Liu\u00ADkas\u00ADta"
-               :ikoni ""}
+               :ikoni ""
+               :tyyppi :vali
+               :vaatii-nappaimiston? true}
               {:nimi "Lu\u00ADmis\u00ADta"
-               :ikoni ""}
+               :ikoni ""
+               :tyyppi :vali
+               :vaatii-nappaimiston? true}
               {:nimi "Tasaus\u00ADpuute"
-               :ikoni ""}
+               :ikoni ""
+               :tyyppi :vali
+               :vaatii-nappaimiston? true}
               {:nimi "Py\u00ADsäkki: epä\u00ADtasainen polanne"
-               :ikoni ""}
+               :ikoni ""
+               :tyyppi :piste
+               :vaatii-nappaimiston? false}
               {:nimi "Py\u00ADsäkki auraa\u00ADmatta"
-               :ikoni ""}
+               :ikoni ""
+               :tyyppi :piste
+               :vaatii-nappaimiston? false}
               {:nimi "Py\u00ADsäkki hiekoit\u00ADtamatta"
-               :ikoni ""}
+               :ikoni ""
+               :tyyppi :piste
+               :vaatii-nappaimiston? false}
               {:nimi "Liikenne\u00ADmerkki likai\u00ADnen"
-               :ikoni ""}
+               :ikoni ""
+               :tyyppi :piste
+               :vaatii-nappaimiston? false}
               {:nimi "Liikenne\u00ADmerkki lumi\u00ADnen"
-               :ikoni ""}]}
+               :ikoni ""
+               :tyyppi :piste
+               :vaatii-nappaimiston? false}]}
    {:avain :liikenneympariston-hoito
     :nimi "Liikenneympäristön hoito"
     :sisalto [{:nimi "Liikenne\u00ADmerkki likai\u00ADnen"
-               :ikoni ""}
+               :ikoni ""
+               :tyyppi :piste
+               :vaatii-nappaimiston? false}
               {:nimi "Liikenne\u00ADmerkki vinos\u00ADsa"
-               :ikoni ""}]}])
+               :ikoni ""
+               :tyyppi :piste
+               :vaatii-nappaimiston? false}]}])
