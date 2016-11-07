8d3a8a5db9b5e15f8d9f011ac14e30143914a496
Jari Hanhela <jarihanhela@gmail.com>
2016-11-07 14:06:12 +0200

    Lisää uusi migraatio

--- tietokanta/src/main/resources/db/migration/V1_449__.sql
+++ tietokanta/src/main/resources/db/migration/V1_449__.sql
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 -- Tarkastusajolta pois tyyppi, ei enää käytössä
 ALTER TABLE tarkastusajo DROP COLUMN tyyppi;
-=======
-ALTER TABLE suolasakko ADD COLUMN vainsakkomaara NUMERIC DEFAULT NULL;
-
-ALTER TYPE hk_suolasakko ADD ATTRIBUTE vainsakkomaara NUMERIC;
-ALTER TYPE hk_suolasakko ADD ATTRIBUTE suolankayton_bonusraja NUMERIC;
->>>>>>> develop
