8f70c570147a97400a1daf36444d4eda8e73fbe4
Jari Hanhela <jarihanhela@gmail.com>
2016-11-07 13:43:13 +0200

    Lisää puuttuva migraatio

--- tietokanta/src/main/resources/db/migration/V1_448__.sql
+++ tietokanta/src/main/resources/db/migration/V1_448__.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- Uusi kantataulu, jossa järjestelmä-käyttäjille annetaan oikeudet toiseen urakkaan
 
 CREATE TABLE kayttajan_lisaoikeudet_urakkaan (
@@ -9,9 +8,3 @@ CREATE TABLE kayttajan_lisaoikeudet_urakkaan (
 COMMENT ON TABLE kayttajan_lisaoikeudet_urakkaan IS 'Rivi tässä taulussa antaa järjestelmäkäyttäjälle oikeudet urakkaan.';
 
 ALTER TABLE kayttajan_lisaoikeudet_urakkaan ADD CONSTRAINT uniikki_lisaoikeus UNIQUE (urakka, kayttaja);
-=======
-ALTER TABLE suolasakko ADD COLUMN vainsakkomaara NUMERIC DEFAULT NULL;
-
-ALTER TYPE hk_suolasakko ADD ATTRIBUTE vainsakkomaara NUMERIC;
-ALTER TYPE hk_suolasakko ADD ATTRIBUTE suolankayton_bonusraja NUMERIC;
->>>>>>> develop
