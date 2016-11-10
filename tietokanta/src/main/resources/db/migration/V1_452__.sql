134c92975b48d28eb86706a8de755cf93c869886
Jari Hanhela <jarihanhela@gmail.com>
2016-11-10 18:10:09 +0200

    Päivitä migraatioon uudelle vakiohavainnolle oikea id ja vaadi uniikki vakiohavainto

--- tietokanta/src/main/resources/db/migration/V1_452__.sql
+++ tietokanta/src/main/resources/db/migration/V1_452__.sql
@@ -6,13 +6,15 @@ ALTER TABLE tarkastusreitti ADD COLUMN soratie_tasaisuus INTEGER;
 ALTER TABLE tarkastusreitti RENAME COLUMN tasaisuus TO talvihoito_tasaisuus;
 
 -- Lisää puuttuvat vakiohavainnot
-INSERT INTO public.vakiohavainto (nimi, jatkuva, avain)
+INSERT INTO vakiohavainto (nimi, jatkuva, avain)
 VALUES ('Liikennemerkki likainen', false, 'liikennemerkki-likainen'),
-('Liikennemerkki likainen', false, 'liikennemerkki-likainen'),
-('P- tai L-alueet hoitamatta', false, 'p-tai-l-alue-hoitamatta'),
+('P- tai L-alueet hoitamatta', false, 'pl-alue-hoitamatta'),
 ('Päällysteessä vaurioita', false, 'sillan-paallysteessa-vaurioita'),
 ('Kaidevauroita', false, 'sillassa-kaidevaurioita'),
 ('Reunapalkkivaurioita likainen', false, 'sillassa-reunapalkkivaurioita');
+
+-- Vakiohavainnon avain uniikki
+ALTER TABLE vakiohavainto ADD CONSTRAINT uniikki_vakiohavainto UNIQUE (avain);
 
 -- Päivitä vakiohavaintojen jatkuvuustieto
 UPDATE vakiohavainto SET jatkuva = TRUE WHERE nimi = 'Vesakko raivaamatta';
