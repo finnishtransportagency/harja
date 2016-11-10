74b24b014e3112b38dee2baca584c96dc923cda6
Jari Hanhela <jarihanhela@gmail.com>
2016-11-10 11:47:28 +0200

    Luo pohjaa uudelle näppäimistölle

--- laadunseuranta/src/harja_laadunseuranta/ui/nappaimisto.cljs
+++ laadunseuranta/src/harja_laadunseuranta/ui/nappaimisto.cljs
@@ -1,11 +1,28 @@
 (ns harja-laadunseuranta.ui.nappaimisto
   (:require [reagent.core :as reagent :refer [atom]]
-            [cljs-time.local :as l])
+            [cljs-time.local :as l]
+            [harja-laadunseuranta.tiedot.sovellus :as s])
   (:require-macros
     [harja-laadunseuranta.macros :as m]
     [cljs.core.async.macros :refer [go go-loop]]
     [devcards.core :as dc :refer [defcard deftest]]))
 
-(defn nappaimisto []
-  [:div.nappaimisto
-   "Näppis tulee tähän"])
+(defn off-painike [otsikko havainnot avain]
+  [:button.nappaimisto-lopeta
+   {:on-click (fn [_]
+                (swap! havainnot #(assoc % avain false)))}
+   otsikko])
+
+(defn- nappaimistokomponentti [{:keys [otsikko havainnot] :as tiedot}]
+  []
+  #_[:div.nappaimisto
+   [off-painike otsikko havainnot :liukasta :on-click #(reset! keskiarvo-atom nil)]
+   [avattu-nuoli]
+   [kitkamittaustiedot keskiarvo-atom]
+   [kitkamittaus/kitkamittauskomponentti (fn [mittaus]
+                                           (swap! keskiarvo-atom #(conj % mittaus))
+                                           (kitkamittaus-kirjattu mittaus))]])
+
+(defn nappaimisto [otsikko]
+  #_[nappaimistokomponentti {:otsikko otsikko
+                           :havainnot @s/havainnot}])
