2c15eec69650bd3e8ebe1cf34b7e2373042df68c
Jari Hanhela <jarihanhela@gmail.com>
2016-11-12 17:28:30 +0200

    Poista turhat devcard-viittaukset

--- laadunseuranta/src/harja_laadunseuranta/devcards_core.cljs
+++ laadunseuranta/src/harja_laadunseuranta/devcards_core.cljs
@@ -2,7 +2,4 @@
   (:require [harja-laadunseuranta.ui.kartta]
             [harja-laadunseuranta.ui.kamera]
             [harja-laadunseuranta.ui.tarkastusajon-luonti]
-            [harja-laadunseuranta.ui.havaintolomake]
-            [harja-laadunseuranta.ui.kitkamittaus]
-            [harja-laadunseuranta.ui.pikavalintapaneeli]
-            [harja-laadunseuranta.ui.arviokomponentti]))
+            [harja-laadunseuranta.ui.havaintolomake]))
