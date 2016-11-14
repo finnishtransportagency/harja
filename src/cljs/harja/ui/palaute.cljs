95ed5ef03d6825c59889285d8dae45699970b1ba
Jari Hanhela <jarihanhela@gmail.com>
2016-11-14 11:57:02 +0200

    Luo pohja uudelle palaute-dialogille

--- src/cljs/harja/ui/palaute.cljs
+++ src/cljs/harja/ui/palaute.cljs
@@ -1,24 +1,49 @@
 (ns harja.ui.palaute
-  (:require [clojure.string :as string]
-            [harja.ui.ikonit :as ikonit]
-            [clojure.string :as str]
+  (:require
             [reagent.core :refer [atom]]
-            [harja.tiedot.istunto :as istunto]
-            [harja.asiakas.tapahtumat :as t]
-            [harja.pvm :as pvm]
+            [harja.ui.ikonit :as ikonit]
+            [reagent.core :refer [atom]]
             [harja.loki :refer [log]]
-            [harja.tiedot.palaute :as tiedot]
-            [harja.ui.napit :as napit]
             [harja.ui.modal :as modal]
-            [harja.ui.ikonit :as ikonit]))
+            [harja.ui.yleiset :as yleiset]
+            [harja.ui.ikonit :as ikonit]
+            [harja.ui.lomake :as lomake]))
 
-(defn palautelomake []
-  (let [laheta-palaute (fn [] (log "Painoit nappia"))]
-    [napit/yleinen "Lähetä palaute" laheta-palaute]))
+(def +linkki-koulutusvideot+ "http://finnishtransportagency.github.io/harja/")
+
+(def palautetyypit
+  [{:nimi "Yleinen palaute" :avain :yleinen}
+   {:nimi "Kehitysidea" :avain :kehitysidea}
+   {:nimi "Bugi" :avain :ongelma}
+   {:nimi "Käyttöoikeusongelma" :avain :kayttooikeus}
+   {:nimi "Tehtävälista" :avain :tehtavalista}])
+
+(defn- palautelomake []
+  (let [lomakedata-atom (atom nil)]
+    (fn []
+      [:div
+       [:p "Valitse, mitä palautteesi koskee:"]
+       [lomake/lomake
+        {:otsikko ""
+         :muokkaa! (fn [uusi]
+                     (reset! lomakedata-atom uusi))}
+        [{:otsikko ""
+          :nimi :palaute-tyyppi
+          :tyyppi :valinta
+          :valinnat palautetyypit
+          :valinta-arvo :avain
+          :valinta-nayta #(or (:nimi %) "- valitse -")}]
+        @lomakedata-atom]
+
+       [yleiset/vihje-elementti [:span
+                                 [:span "Olethan tutustunut "]
+                                 [:a {:href +linkki-koulutusvideot+} "Harjan koulutusvideoihin"]
+                                 [:span " ennen palautteen lähettämistä?"]]]])))
 
 (defn palaute-linkki []
   [:a {:class "klikattava"
        :id "palautelinkki"
-       :on-click #(modal/nayta! {:otsikko "Palautteen lähettäminen"}
-                               palautelomake)}
+       :on-click #(modal/nayta! {:otsikko "Palautteen lähettäminen"
+                                 :luokka "palaute-dialog"}
+                                [palautelomake])}
    [ikonit/ikoni-ja-teksti (ikonit/livicon-kommentti) "Palautetta!"]])
