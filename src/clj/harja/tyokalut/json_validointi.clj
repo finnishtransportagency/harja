ef4b84e5844ff678ded5c138c6f5c858198023a1
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-11-18 10:11:41 +0200

    Korjaa JSON-virheen lokitus

--- src/clj/harja/tyokalut/json_validointi.clj
+++ src/clj/harja/tyokalut/json_validointi.clj
@@ -31,7 +31,7 @@
 
 (defn kasittele-validointivirheet
   [skeema virheet]
-  (log/error (format "JSON ei ole validia (skeema: %s). Validointivirheet: %s") skeema virheet)
+  (log/error (format "JSON ei ole validia (skeema: %s). Validointivirheet: %s" skeema virheet))
   (throw+ {:type    virheet/+invalidi-json+
            :virheet [{:koodi  virheet/+invalidi-json-koodi+
                       :viesti (str "JSON ei ole validia: " (formatoi-virhe "" virheet))}]}))
