cd0c2046943cde48341f7f55f6f8be37ec5442a2
Jari Hanhela <jarihanhela@gmail.com>
2016-11-18 15:59:19 +0200

    Merge develop

--- tietokanta/src/main/resources/db/migration/V1_455__.sql
+++ tietokanta/src/main/resources/db/migration/V1_455__.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- Tarkastusajolla ei ole enää tyyppiä (säilytetään sarake vanhojen ajojen takia)
 ALTER TABLE tarkastusajo ALTER COLUMN tyyppi DROP NOT NULL;
 
@@ -22,39 +21,3 @@ UPDATE vakiohavainto SET jatkuva = TRUE WHERE nimi = 'Vesakko raivaamatta';
 UPDATE vakiohavainto SET jatkuva = TRUE WHERE nimi = 'Niittämättä';
 UPDATE vakiohavainto SET jatkuva = TRUE WHERE nimi = 'Reunapalletta';
 UPDATE vakiohavainto SET jatkuva = TRUE WHERE nimi = 'Reunatäyttö puutteellinen';
-=======
--- Tiemerkintöjen valtakunnalliset välitavoitteet (välitavoitepohjat)
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Massavaatimusteiden keskiviistaton merkinnät kunnostettu', 'tiemerkinta', 31, 7, 'toistuva'::valitavoite_tyyppi, false);
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Massavaatimusteiden keskiviistaton reunaviivat kunnostettu', 'tiemerkinta', 31, 8, 'toistuva'::valitavoite_tyyppi, false);
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Maalivaatimusteiden pituussuuntaiset merkinnät kunnostettu', 'tiemerkinta', 30, 9, 'toistuva'::valitavoite_tyyppi, false);
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Suojatiet ja pyörätiet kunnostettu', 'tiemerkinta', 31, 7, 'toistuva'::valitavoite_tyyppi, false);
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Pienmerkinnät kunnostettu', 'tiemerkinta', 30, 9, 'toistuva'::valitavoite_tyyppi, false);
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Kevään kuntoarvo, massavaatimustiet, pituussuuntaiset merkinnät raportoitu tilaajalle', 'tiemerkinta', 31, 5, 'toistuva'::valitavoite_tyyppi, false);
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Syksyn kuntoarvo, maalivaatimustiet, pituussuuntaiset merkinnät raportoitu tilaajalle', 'tiemerkinta', 15, 10, 'toistuva'::valitavoite_tyyppi, false);
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Suojatiet raportoitu tilaajalle', 'tiemerkinta', 31, 8, 'toistuva'::valitavoite_tyyppi, false);
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Pienmerkinnät raportoitu tilaajalle', 'tiemerkinta', 15, 10, 'toistuva'::valitavoite_tyyppi, false);
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Massavaatimustiet, pituussuuntaiset merkinnät tulee olla raportoitu tilaajalle', 'tiemerkinta', 30, 9, 'toistuva'::valitavoite_tyyppi, false);
-
-INSERT INTO valitavoite (nimi, urakkatyyppi, takaraja_toistopaiva, takaraja_toistokuukausi, tyyppi, poistettu)
-VALUES ('Massavaatimustiet, pituussuuntaiset merkinnät tulee olla raportoitu tilaajalle', 'tiemerkinta', 15, 10, 'toistuva'::valitavoite_tyyppi, false);
->>>>>>> develop
