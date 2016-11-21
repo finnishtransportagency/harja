cf38739cffb9b5a564b3e0f585d3907ab084f591
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-11-21 13:14:19 +0200

    Muuta migraatio

--- tietokanta/src/main/resources/db/migration/V1_455__.sql
+++ tietokanta/src/main/resources/db/migration/V1_455__.sql
@@ -5,6 +5,8 @@ CREATE TABLE urakanvastuuhenkilo (
   urakka INTEGER REFERENCES urakka (id),
   kayttajatunnus VARCHAR(32), -- livin extranet tunnus
   nimi varchar(255), -- Käyttäjän oikea nimi
+  sahkoposti varchar(255),
+  puhelin varchar(32),
   rooli VARCHAR(32), -- ELY_Urakanvalvoja (ELY) tai vastuuhenkilo (urakoitsija)
   ensisijainen BOOLEAN
 );
