9d5c4c4ff3f0391add66796b62254667bcfd9a89
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-11-23 12:02:07 +0200

    Refaktoroi Sampo-komponenttia

--- src/clj/harja/palvelin/integraatiot/sampo/sampo_komponentti.clj
+++ src/clj/harja/palvelin/integraatiot/sampo/sampo_komponentti.clj
@@ -12,40 +12,35 @@
 (defprotocol Maksueralahetys
   (laheta-maksuera-sampoon [this numero]))
 
-(defn tee-sonja-viestikuuntelija [this lahetysjono-sisaan kuittausjono-sisaan]
+(defn tee-sonja-viestikuuntelija [{:keys [db integraatioloki sonja]} lahetysjono-sisaan kuittausjono-sisaan]
   (log/debug "Käynnistetään Sampon Sonja viestikuuntelija kuuntelemaan jonoa: " lahetysjono-sisaan)
-  (sonja/kuuntele (:sonja this) lahetysjono-sisaan
+  (sonja/kuuntele sonja lahetysjono-sisaan
                   (fn [viesti]
-                    (tuonti/kasittele-viesti (:sonja this) (:integraatioloki this)
-                                             (:db this) kuittausjono-sisaan viesti))))
+                    (tuonti/kasittele-viesti sonja integraatioloki db kuittausjono-sisaan viesti))))
 
-(defn tee-sonja-kuittauskuuntelija [this kuittausjono-ulos]
+(defn tee-sonja-kuittauskuuntelija [{:keys [db integraatioloki sonja]} kuittausjono-ulos]
   (log/debug "Käynnistetään Sampon Sonja kuittauskuuntelija kuuntelemaan jonoa: " kuittausjono-ulos)
-  (sonja/kuuntele (:sonja this) kuittausjono-ulos
+  (sonja/kuuntele sonja kuittausjono-ulos
                   (fn [viesti]
-                    (vienti/kasittele-kuittaus (:integraatioloki this) (:db this) viesti kuittausjono-ulos))))
+                    (vienti/kasittele-kuittaus integraatioloki db viesti kuittausjono-ulos))))
 
-(defn tee-paivittainen-lahetys-tehtava [this paivittainen-lahetysaika lahetysjono-ulos]
+(defn tee-paivittainen-lahetys-tehtava [{:keys [db integraatioloki sonja]} paivittainen-lahetysaika lahetysjono-ulos]
   (if paivittainen-lahetysaika
     (do
       (log/debug "Ajastetaan maksuerien ja kustannussuunnitelmien lähetys ajettavaksi joka päivä kello: "
                  paivittainen-lahetysaika)
       (ajastettu-tehtava/ajasta-paivittain
         paivittainen-lahetysaika
-        (fn [_] (vienti/aja-paivittainen-lahetys (:sonja this) (:integraatioloki this)
-                                                 (:db this) lahetysjono-ulos))))
-    (fn [] ())))
+        (fn [_] (vienti/aja-paivittainen-lahetys sonja integraatioloki db lahetysjono-ulos))))
+    (constantly nil)))
 
 (defrecord Sampo [lahetysjono-sisaan kuittausjono-sisaan lahetysjono-ulos kuittausjono-ulos paivittainen-lahetysaika]
   component/Lifecycle
   (start [this]
     (log/debug "Käynnistetään Sampo komponentti")
-    (assoc this :sonja-viestikuuntelija (tee-sonja-viestikuuntelija this
-                                                                    lahetysjono-sisaan
-                                                                    kuittausjono-sisaan)
+    (assoc this :sonja-viestikuuntelija (tee-sonja-viestikuuntelija this lahetysjono-sisaan kuittausjono-sisaan)
                 :sonja-kuittauskuuntelija (tee-sonja-kuittauskuuntelija this kuittausjono-ulos)
-                :paivittainen-lahetys-tehtava (tee-paivittainen-lahetys-tehtava this
-                                                                                paivittainen-lahetysaika
+                :paivittainen-lahetys-tehtava (tee-paivittainen-lahetys-tehtava this paivittainen-lahetysaika
                                                                                 lahetysjono-ulos)))
   (stop [this]
     (let [poista-viestikuuntelija (:sonja-viestikuuntelija this)
