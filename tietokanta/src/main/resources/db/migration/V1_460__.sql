000e5ab9936198b795908b0d56da8452708c25ce
Tatu Tarvainen <tatu.tarvainen@me.com>
2016-11-25 11:28:17 +0200

    Kerää työkoneen reitti talteen
    Kerätään reitti talteen ja lisätään uusi "alkanut" kenttä, joka kertoo
    ensimmäisen tälle työkoneelle tehdyn havainnon.

---
+++ tietokanta/src/main/resources/db/migration/V1_460__.sql
@@ -0,0 +1,21 @@
+-- Lisää työkoneelle reitti
+
+ALTER TABLE tyokonehavainto
+  ADD COLUMN reitti geometry,
+  ADD COLUMN alkanut timestamp without time zone DEFAULT NOW();
+
+CREATE OR REPLACE FUNCTION paivita_tyokoneen_reitti() RETURNS trigger AS $$
+BEGIN
+  IF NEW.reitti IS NULL THEN
+    NEW.reitti = ST_MakeLine(ARRAY[NEW.sijainti]::GEOMETRY[]);
+  ELSE
+    NEW.reitti = ST_AddPoint(NEW.reitti, NEW.sijainti);
+ END IF;
+ RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tg_paivita_tyokoneen_reitti
+BEFORE INSERT OR UPDATE ON tyokonehavainto
+FOR EACH ROW
+EXECUTE PROCEDURE paivita_tyokoneen_reitti();
