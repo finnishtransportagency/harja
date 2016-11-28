db12111b09f70e87974255c8100c142c365700be
Jari Hanhela <jarihanhela@gmail.com>
2016-11-28 14:54:10 +0200

    Päivitä migraatio, vaihda sarakkeiden järjestys mielekkäämpään

--- tietokanta/src/main/resources/db/migration/V1_461__.sql
+++ tietokanta/src/main/resources/db/migration/V1_461__.sql
@@ -3,8 +3,8 @@
 
 CREATE TABLE urakka_laskentakohde (
   id serial primary key NOT NULL,
-  nimi VARCHAR(256) NOT NULL,
-  urakka integer references urakka (id) NOT NULL
+  urakka integer references urakka (id) NOT NULL,
+  nimi VARCHAR(256) NOT NULL
 );
 
 ALTER TABLE urakka_laskentakohde ADD CONSTRAINT uniikki_laskentakohde UNIQUE (nimi, urakka);
