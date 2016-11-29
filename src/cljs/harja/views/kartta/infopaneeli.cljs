7cca5af312c2d0b9323f080a76a22c9e62eabd89
Erno Kuusela <erno.kuusela@solita.fi>
2016-11-29 12:15:55 +0000

    infopaneeli: siistimistä

--- src/cljs/harja/views/kartta/infopaneeli.cljs
+++ src/cljs/harja/views/kartta/infopaneeli.cljs
@@ -18,35 +18,36 @@
   [:div {:on-click #(reset! valittu-asia asia)} (:otsikko asia)
    ])
 
-(defn esita-yksityiskohdat [asia ainoa-asia?]
+(defn esita-yksityiskohdat [asia]
   (let []
     [:div
      [:div (:otsikko asia)]
      [:div (-> asia :tiedot count)]]))
 
-(defn infopaneeli [asiat-pisteessa]
-  (when-let [sisalto @asiat-pisteessa]
-    (let [vain-yksi-asia? (-> sisalto :asiat count (= 1))
+(defn infopaneeli [asiat-pisteessa-atomi]
+  (when-let [{:keys [asiat haetaan? koordinaatti]} sisalto @asiat-pisteessa-atomi]
+    (let [
+          vain-yksi-asia? (-> asiat count (= 1))
           useampi-asia? (not vain-yksi-asia?)
           esita-yksityiskohdat? (or @valittu-asia vain-yksi-asia?)
-          ainoa-asia (when vain-yksi-asia? (-> sisalto :asiat first))]
+          ainoa-asia (when vain-yksi-asia? (first asiat))]
       ;; tyhjennetään valinta kun valinnassa on asia joka ei esiinny @asiat-pisteessa
       ;; (esim latauksen aikana)
-      (when-not (some #(= @valittu-asia %) (:asiat sisalto))
+      (when-not (some #(= @valittu-asia %) asiat)
         (reset! valittu-asia nil))
       [:div#kartan-infopaneeli
        [:div
         (when (and @valittu-asia useampi-asia?)
            [napit/takaisin "" #(reset! valittu-asia nil)])
-        (when (:haetaan? sisalto)
+        (when haetaan?
           [ajax-loader])
         [:button.close {:on-click #(reset! asiat-pisteessa nil)
                         :type "button"}
          [:span "×"]
          ]]
-       (when-not (-> sisalto :asiat empty?)
+       (when-not (empty? asiat)
          (if esita-yksityiskohdat?
-           [esita-yksityiskohdat (or @valittu-asia ainoa-asia) vain-yksi-asia?]
-           [:div (doall (for [[idx asia] (map-indexed #(do [%1 %2]) (-> sisalto :asiat))]
+           [esita-yksityiskohdat (or @valittu-asia ainoa-asia)]
+           [:div (doall (for [[idx asia] (map-indexed #(do [%1 %2]) asiat)]
                           ^{:key idx}
                           [esita-otsikko asia]))]))])))
