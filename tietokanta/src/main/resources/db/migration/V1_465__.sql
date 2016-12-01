ada30e9254fca8d467453a1a0a07ed9eb7b6acbc
maaritlaine <maarit.laine@gmail.com>
2016-12-01 15:19:13 +0200

    Korjaa migraatio

--- tietokanta/src/main/resources/db/migration/V1_465__.sql
+++ tietokanta/src/main/resources/db/migration/V1_465__.sql
@@ -2,11 +2,18 @@ ALTER TABLE suljettu_tieosuus
   RENAME TO tietyomaa;
 
 ALTER TABLE tietyomaa
-  RENAME CONSTRAINT suljettu_tieosuus_jarjestelma_osuus_id_key TO tietyomaa_jarjestelma_osuus_id_key;
+  RENAME CONSTRAINT suljettu_tieosuus_yllapitokohde_fkey TO tietyomaa_yllapitokohde_fkey;
 ALTER TABLE tietyomaa
-  RENAME CONSTRAINT suljettu_tieosuus_geom_index TO tietyomaa_geom_index;
+  RENAME CONSTRAINT suljettu_tieosuus_kirjaaja_fkey TO tietyomaa_kirjaaja_fkey;
 ALTER TABLE tietyomaa
-  RENAME CONSTRAINT suljettu_tieosuus_envelope_idx TO tietyomaa_envelope_idx;
+  RENAME CONSTRAINT suljettu_tieosuus_poistaja_fkey TO tietyomaa_poistaja_fkey;
+
+ALTER INDEX suljettu_tieosuus_jarjestelma_osuus_id_key
+RENAME TO tietyomaa_jarjestelma_osuus_id_key;
+ALTER INDEX suljettu_tieosuus_geom_index
+RENAME TO tietyomaa_geom_index;
+ALTER INDEX suljettu_tieosuus_envelope_idx
+RENAME TO tietyomaa_envelope_idx;
 
 UPDATE integraatio
 SET nimi = 'kirjaa-tietyomaa'
