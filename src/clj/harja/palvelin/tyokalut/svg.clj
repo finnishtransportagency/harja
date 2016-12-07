6e9857e6777d033db703f5416c0c54475fa390a1
Jari Hanhela <jarihanhela@gmail.com>
2016-12-07 10:03:37 +0200

    Debuggaa SVG-tiedostojen lataamista

--- src/clj/harja/palvelin/tyokalut/svg.clj
+++ src/clj/harja/palvelin/tyokalut/svg.clj
@@ -23,7 +23,7 @@
 
 (defn create-inline-svg [path]
   (let [svgs (get-svgs path)]
-    (apply conj [:div {:style "display: none;"}]
+    (apply conj [:div {:style "display: none;" :comment (pr-str svgs)}]
            (into [] (map :contents svgs)))))
 
 (defmacro inline-svg [path]
