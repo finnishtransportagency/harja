a40e49c5743594e52bd5142e4a728917732d5bcf
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2016-12-09 11:27:21 +0200

    Lisää testi laskentakohteiden muuntamiselle

--- src/cljs/harja/tiedot/urakka/toteumat/tiemerkinta_muut_tyot.cljs
+++ src/cljs/harja/tiedot/urakka/toteumat/tiemerkinta_muut_tyot.cljs
@@ -1,6 +1,5 @@
 (ns harja.tiedot.urakka.toteumat.tiemerkinta-muut-tyot
   (:require [reagent.core :refer [atom]]
-            [harja.tiedot.urakka.suunnittelu.muut-tyot :as muut-tyot]
             [cljs.core.async :refer [<! >! chan]]
             [harja.loki :refer [log logt]]
             [harja.tiedot.urakka :as u]
