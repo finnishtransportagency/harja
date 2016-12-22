5f4d4d88c3c83ee0650b1ffefd70965af33518cb
Erno Kuusela <erno.kuusela@solita.fi>
2016-12-22 13:42:57 +0000

    Korjaa yllapidon-reaaliaikaseurattava? -kutsun parametri

--- src/clj/harja/palvelin/palvelut/tilannekuva.clj
+++ src/clj/harja/palvelin/palvelut/tilannekuva.clj
@@ -190,7 +190,7 @@
 (defn- tyokoneiden-toimenpiteet
   "Palauttaa haettavat tehtävä työkonekyselyille"
   [talvi kesa yllapito]
-  (let [yllapito (filter #(tk/yllapidon-reaaliaikaseurattava? (:id %)) yllapito)
+  (let [yllapito (filter tk/yllapidon-reaaliaikaseurattava? yllapito)
         haettavat-toimenpiteet (haettavat (union talvi kesa yllapito))]
     (konv/seq->array haettavat-toimenpiteet)))
 
@@ -472,13 +472,14 @@
                                            :kayttaja_on_urakoitsija (roolit/urakoitsija? user)
                                            :x x :y y)))))
 
-
 (defn- hae-tyokoneiden-sijainnit-kartalle [db user parametrit]
   (hae-karttakuvan-tiedot db user parametrit hae-tyokoneiden-reitit
                           (comp (geo/muunna-pg-tulokset :reitti)
                                 (map #(konv/array->set % :tehtavat))
                                 (map #(assoc %
                                              :tyyppi-kartalla :tyokone)))))
+
+
 
 (defn- hae-tyokoneiden-tiedot-kartalle
   "Hakee työkoneiden tiedot pisteessä infopaneelia varten."
@@ -489,11 +490,11 @@
               (map #(konv/string->keyword % :tyyppi)))
         (q/hae-tyokoneiden-asiat db
                                  (-> parametrit
-                                   suodattimet-parametreista
-                                   (assoc :x x
-                                          :y y
-                                          :nayta-kaikki (roolit/tilaajan-kayttaja? user)
-                                          :organisaatio (-> user :organisaatio :id))))))
+                                     suodattimet-parametreista
+                                     (assoc :x x
+                                            :y y
+                                            :nayta-kaikki (roolit/tilaajan-kayttaja? user)
+                                            :organisaatio (-> user :organisaatio :id))))))
 
 (defrecord Tilannekuva []
   component/Lifecycle
