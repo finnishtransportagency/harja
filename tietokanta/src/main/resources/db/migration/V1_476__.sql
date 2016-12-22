32ac1cc9f1e3b94c1876a2143c66b610a397e58b
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2016-12-22 08:12:25 +0200

    Merge branch 'develop' into HAR-3648-nopeusrajoitukset-tietyomaille
    # Conflicts:
    #	resources/api/documentation/harja-api-versiohistoria.md
    #	tietokanta/src/main/resources/db/migration/V1_475__.sql

--- tietokanta/src/main/resources/db/migration/V1_476__.sql
+++ tietokanta/src/main/resources/db/migration/V1_476__.sql
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 ALTER TABLE tietyomaa
   ADD COLUMN nopeusrajoitus INT;
-=======
--- Indeksoi ilmoituksen urakka
-CREATE INDEX ilmoitus_urakka_idx ON ilmoitus (urakka);
->>>>>>> develop
