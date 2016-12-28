fd339ff60ffc485258ca028a3db8a2750e68c8a8
Jari Hanhela <jarihanhela@gmail.com>
2016-12-28 08:53:35 +0200

    Aseta laatu- ja turvallisuuspoikkeamaraportit suoritettavaksi ylläpidon urakoissa

--- tietokanta/src/main/resources/db/migration/V1_477__.sql
+++ tietokanta/src/main/resources/db/migration/V1_477__.sql
@@ -3,3 +3,8 @@ ALTER TABLE raportti ADD COLUMN urakkatyyppi_ urakkatyyppi[];
 UPDATE raportti SET urakkatyyppi_ = ARRAY['hoito']::urakkatyyppi[];
 ALTER TABLE raportti DROP COLUMN urakkatyyppi;
 ALTER TABLE raportti RENAME COLUMN urakkatyyppi_ TO urakkatyyppi;
+
+-- Laatu- ja turvallisuuspoikkeamaraportit ylläpidolle
+
+UPDATE raportti SET urakkatyyppi = ARRAY['hoito', 'paallystys', 'paikkaus', 'tiemerkinta']::urakkatyyppi[]
+WHERE nimi = 'laatupoikkeamaraportti' OR nimi = 'turvallisuus';
