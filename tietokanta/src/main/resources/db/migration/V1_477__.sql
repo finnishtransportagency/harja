0933c62b527adca9ccb0753ff721127a4f7018fe
Jari Hanhela <jarihanhela@gmail.com>
2016-12-28 14:24:20 +0200

    Pakota ylläpitokohteelle tyyppi (päällystys / paikkaus) kantatasolla

--- tietokanta/src/main/resources/db/migration/V1_477__.sql
+++ tietokanta/src/main/resources/db/migration/V1_477__.sql
@@ -1,3 +1,15 @@
 -- Lisää ylläpitokohteelle tieto siitä, minä vuosina ko. kohde on työn alla
 
 ALTER TABLE yllapitokohde ADD COLUMN vuodet int[] NOT NULL DEFAULT ARRAY[]::integer[];
+
+-- Pakota ylläpitokohteille tyyppi
+
+UPDATE yllapitokohde
+SET yllapitokohdetyotyyppi = 'paikkaus'
+WHERE yllapitokohdetyotyyppi IS NULL AND yhaid IS NULL;
+
+UPDATE yllapitokohde
+SET yllapitokohdetyotyyppi = 'paallystys'
+WHERE yllapitokohdetyotyyppi IS NULL AND yhaid IS NOT NULL;
+
+ALTER TABLE yllapitokohde ALTER COLUMN yllapitokohdetyotyyppi SET NOT NULL;
