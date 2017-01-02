c222ea49b5eb4fb1bd464c2f0434c3c0cc3cd619
Jari Hanhela <jarihanhela@gmail.com>
2017-01-02 14:53:53 +0200

    Näytä myös valmistuneen kohteen pvm oikein POT-näkymän hallintapaneelissa

--- src/clj/harja/kyselyt/paallystys.sql
+++ src/clj/harja/kyselyt/paallystys.sql
@@ -18,7 +18,8 @@ SELECT
   ypk.aikataulu_paallystys_alku AS "paallystys-alkupvm",
   ypk.aikataulu_paallystys_loppu AS "paallystys-loppupvm",
   ypk.aikataulu_tiemerkinta_alku AS "tiemerkinta-alkupvm",
-  ypk.aikataulu_tiemerkinta_loppu AS "tiemerkinta-loppupvm"
+  ypk.aikataulu_tiemerkinta_loppu AS "tiemerkinta-loppupvm",
+  ypk.aikataulu_kohde_valmis AS "kohde-valmispvm"
 FROM yllapitokohde ypk
   LEFT JOIN paallystysilmoitus pi ON pi.paallystyskohde = ypk.id
                                      AND pi.poistettu IS NOT TRUE
--- src/clj/harja/kyselyt/paikkaus.sql
+++ src/clj/harja/kyselyt/paikkaus.sql
@@ -11,7 +11,8 @@ SELECT
   ypk.aikataulu_paallystys_alku AS "paallystys-alkupvm",
   ypk.aikataulu_paallystys_loppu AS "paallystys-loppupvm",
   ypk.aikataulu_tiemerkinta_alku AS "tiemerkinta-alkupvm",
-  ypk.aikataulu_tiemerkinta_loppu AS "tiemerkinta-loppupvm"
+  ypk.aikataulu_tiemerkinta_loppu AS "tiemerkinta-loppupvm",
+  ypk.aikataulu_kohde_valmis AS "kohde-valmispvm"
 FROM yllapitokohde ypk
   LEFT JOIN paikkausilmoitus pi ON pi.paikkauskohde = yllapitokohde.id
                                    AND pi.poistettu IS NOT TRUE
