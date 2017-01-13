1e7a2e6bd46d2983ab137d323b7cdc2d4d356a4a
Jari Hanhela <jarihanhela@gmail.com>
2017-01-13 15:20:06 +0200

    Lisää Muhoksen päällystysurakalle yhteyshenkilöt

--- tietokanta/testidata/yhteyshenkilot.sql
+++ tietokanta/testidata/yhteyshenkilot.sql
@@ -159,6 +159,29 @@ VALUES ((SELECT id
            sukunimi = 'Frösén'
          LIMIT 1));
 
+INSERT INTO yhteyshenkilo_urakka (urakka, rooli, yhteyshenkilo)
+VALUES ((SELECT id
+         FROM urakka
+         WHERE nimi = 'Muhoksen päällystysurakka'),
+        'Kunnossapitopäällikkö',
+        (SELECT id
+         FROM yhteyshenkilo
+         WHERE
+           etunimi = 'Vihtori' AND
+           sukunimi = 'Ollila'
+         LIMIT 1));
+INSERT INTO yhteyshenkilo_urakka (urakka, rooli, yhteyshenkilo)
+VALUES ((SELECT id
+         FROM urakka
+         WHERE nimi = 'Muhoksen päällystysurakka'),
+        'Tieliikennekeskus',
+        (SELECT id
+         FROM yhteyshenkilo
+         WHERE
+           etunimi = 'Reijo' AND
+           sukunimi = 'Vänskä'
+         LIMIT 1));
+
 -- Yhteyshenkilöiden organisaatio
 
 UPDATE yhteyshenkilo SET organisaatio=(SELECT id FROM organisaatio WHERE ytunnus='1565583-5');
