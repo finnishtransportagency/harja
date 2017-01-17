a7d328cee4eb14519a32e7de53ca2cf98e50411f
Jari Hanhela <jarihanhela@gmail.com>
2017-01-17 12:08:43 +0200

    Peru tilannekuvan muutos turhana

--- src/cljs/harja/views/tilannekuva/tilannekuva.cljs
+++ src/cljs/harja/views/tilannekuva/tilannekuva.cljs
@@ -61,7 +61,7 @@
   [:div#tk-historiakuvan-aikavalit
    [ui-valinnat/aikavali tiedot/historiakuvan-aikavali {:nayta-otsikko? false
                                                         :aikavalin-rajoitus [12 :kuukausi]
-                                                        :aloitusaika-pakota-suunta :ylos-vasen
+                                                        :aloitusaika-pakota-suunta :alas-oikea
                                                         :paattymisaika-pakota-suunta :alas-vasen}]])
 
 (defn yksittainen-suodatincheckbox
