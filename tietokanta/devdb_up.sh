7d5e1e820a8c9abb5ec12ee8961cef1e71406ba2
Tatu Tarvainen <tatu.tarvainen@me.com>
2017-01-18 19:54:23 +0200

    Lisää odottelua

--- tietokanta/devdb_up.sh
+++ tietokanta/devdb_up.sh
@@ -3,9 +3,14 @@
 echo "Käynnistetään harjadb docker image (PostgreSQL 9.5 + PostGIS 2.3)"
 docker run -p 5432:5432 --name harjadb -e POSTGRES_DB=harja -e POSTGRES_USER=harja -d mdillon/postgis:9.5
 
-echo "Odotetaan, että postgres käynnissä"
-sleep 10
+echo "Odotetaan, että PostgreSQL käynnissä ja vastaa yhteyksiin portissa 5432"
+while ! nc -z localhost 5432; do
+    sleep 0.5;
+done;
 
+echo "Odotellaan vielä hetki..."
+# Ylempi tarkistus ei vielä takaa, että flyway saa yhteyden, vaan docker on käynnissä
+sleep 20
 
 
 echo "Ajetaan migraatiot"
