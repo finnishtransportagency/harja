c4bf45747f4043a118a750ebb0a7a817ac7b5d1b
Jari Hanhela <jarihanhela@gmail.com>
2017-01-20 11:07:27 +0200

    Refactoroi havaintojen jakaminen pistemäisiin ja jatkuviin

--- laadunseuranta/clj-src/harja_laadunseuranta/kyselyt.clj
+++ laadunseuranta/clj-src/harja_laadunseuranta/kyselyt.clj
@@ -9,27 +9,28 @@
 
 (def jatkuvat-vakiohavainto-idt (delay (into #{} (map :id (hae-jatkuvat-vakiohavainto-idt @db)))))
 
-(defn jatkuvat-havainnot [havainnot]
+(defn- suodata-jatkuvat-havainnot [havainnot]
   (filterv @jatkuvat-vakiohavainto-idt havainnot))
 
-(defn pistemainen-havainto [havainnot]
+(defn- suodata-pistemainen-havainto [havainnot]
   (first (filterv (comp not @jatkuvat-vakiohavainto-idt) havainnot)))
 
-(defn muunna-merkinta [merkinta]
+(defn kasittele-kantamerkinta [merkinta]
   (let [p (when (:sijainti merkinta)
             (.getGeometry (:sijainti merkinta)))
         havainnot (when (:havainnot merkinta)
                     (seq (.getArray (:havainnot merkinta))))]
     (-> merkinta
-     (assoc :sijainti [(.x p) (.y p)]
-            :jatkuvat-havainnot (jatkuvat-havainnot havainnot)
-            :pistemainen-havainto (pistemainen-havainto havainnot))
-     (dissoc :havainnot))))
+        ;; Jaetaan havainnot pistemäisiin ja jatkuviin
+        (assoc :sijainti [(.x p) (.y p)]
+               :jatkuvat-havainnot (suodata-jatkuvat-havainnot havainnot)
+               :pistemainen-havainto (suodata-pistemainen-havainto havainnot))
+        (dissoc :havainnot))))
 
 (defn hae-reitin-merkinnat-tieosoitteilla
   "Hakee reittimerkinnät ja niiden projisoidun tieverkon osoitteet."
   [db args]
-  (mapv muunna-merkinta (hae-reitin-merkinnat-tieosoitteilla-raw db args)))
+  (mapv kasittele-kantamerkinta (hae-reitin-merkinnat-tieosoitteilla-raw db args)))
 
 (defn hae-vakiohavaintojen-kuvaukset [db]
   (into {} (mapv (fn [r] [(keyword (:avain r)) (:nimi r)])
@@ -46,62 +47,62 @@
 
 (def vakiohavainto-idt
   (delay
-   (try
-     (hae-vakiohavaintoidt @db)
-     (catch Exception e
-       {7 :sulamisvesihaittoja
-        20 :siltasaumoissa-puutteita
-        27 :reunataytto-puutteellinen
-        1 :liukasta
-        24 :ylijaamamassa-tasattu-huonosti
-        39 :viheralueet-hoitamatta
-        4 :liikennemerkki-luminen
-        15 :sohjoa
-        21 :siltavaurioita
-        31 :nakemaalue-raivaamatta
-        32 :niittamatta
-        40 :rumpu-tukossa
-        33 :vesakko-raivaamatta
-        13 :pl-alue-auraamatta
-        22 :silta-puhdistamatta
-        36 :reunapaalut-likaisia
-        41 :rumpu-liettynyt
-        43 :kaidevaurio
-        29 :istutukset-hoitamatta
-        44 :kiveysvaurio
-        6 :aurausvalli
-        28 :reunapalletta
-        25 :oja-tukossa
-        34 :liikennemerkki-vinossa
-        17 :lumikielekkeita
-        3 :lumista
-        12 :pl-epatasainen-polanne
-        2 :tasauspuute
-        23 :ojat-kivia-poistamatta
-        35 :reunapaalut-vinossa
-        19 :soratie
-        11 :pysakki-hiekoittamatta
-        9 :hiekoittamatta
-        5 :pysakilla-epatasainen-polanne
-        14 :pl-alue-hiekoittamatta
-        26 :luiskavaurio
-        16 :irtolunta
-        38 :pl-alue-korjattavaa
-        30 :liikennetila-hoitamatta
-        10 :pysakki-auraamatta
-        18 :yleishavainto
-        42 :rumpu-rikki
-        37 :pl-alue-puhdistettava
-        8 :polanteessa-jyrkat-urat
-        45 :saumavirhe
-        46 :lajittuma
-        47 :epatasaisuus
-        48 :halkeamat
-        49 :vesilammikot
-        50 :epatasaisetreunat
-        51 :jyranjalkia
-        52 :sideainelaikkia
-        53 :vaarakorkeusasema
-        54 :pintaharva
-        55 :pintakuivatuspuute
-        56 :kaivojenkorjeusasema}))))
+    (try
+      (hae-vakiohavaintoidt @db)
+      (catch Exception e
+        {7 :sulamisvesihaittoja
+         20 :siltasaumoissa-puutteita
+         27 :reunataytto-puutteellinen
+         1 :liukasta
+         24 :ylijaamamassa-tasattu-huonosti
+         39 :viheralueet-hoitamatta
+         4 :liikennemerkki-luminen
+         15 :sohjoa
+         21 :siltavaurioita
+         31 :nakemaalue-raivaamatta
+         32 :niittamatta
+         40 :rumpu-tukossa
+         33 :vesakko-raivaamatta
+         13 :pl-alue-auraamatta
+         22 :silta-puhdistamatta
+         36 :reunapaalut-likaisia
+         41 :rumpu-liettynyt
+         43 :kaidevaurio
+         29 :istutukset-hoitamatta
+         44 :kiveysvaurio
+         6 :aurausvalli
+         28 :reunapalletta
+         25 :oja-tukossa
+         34 :liikennemerkki-vinossa
+         17 :lumikielekkeita
+         3 :lumista
+         12 :pl-epatasainen-polanne
+         2 :tasauspuute
+         23 :ojat-kivia-poistamatta
+         35 :reunapaalut-vinossa
+         19 :soratie
+         11 :pysakki-hiekoittamatta
+         9 :hiekoittamatta
+         5 :pysakilla-epatasainen-polanne
+         14 :pl-alue-hiekoittamatta
+         26 :luiskavaurio
+         16 :irtolunta
+         38 :pl-alue-korjattavaa
+         30 :liikennetila-hoitamatta
+         10 :pysakki-auraamatta
+         18 :yleishavainto
+         42 :rumpu-rikki
+         37 :pl-alue-puhdistettava
+         8 :polanteessa-jyrkat-urat
+         45 :saumavirhe
+         46 :lajittuma
+         47 :epatasaisuus
+         48 :halkeamat
+         49 :vesilammikot
+         50 :epatasaisetreunat
+         51 :jyranjalkia
+         52 :sideainelaikkia
+         53 :vaarakorkeusasema
+         54 :pintaharva
+         55 :pintakuivatuspuute
+         56 :kaivojenkorjeusasema}))))
