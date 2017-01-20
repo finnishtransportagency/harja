e849be895877aaf545860d4887f539813ca679f2
Tatu Tarvainen <tatu.tarvainen@me.com>
2017-01-20 15:19:50 +0200

    Placeholder testins tienäkymän palvelulle

---
+++ test/clj/harja/palvelin/palvelut/tienakyma_test.clj
@@ -0,0 +1,8 @@
+(ns harja.palvelin.palvelut.tienakyma-test
+  (:require [harja.palvelin.palvelut.tienakyma :as sut]
+            [clojure.test :as t]))
+
+;; FIXME: TESTAA tienäkymää!
+
+(deftest foo
+  (is false "ÄLÄ UNOHDA KIRJOITTAA TESTEJÄ"))
