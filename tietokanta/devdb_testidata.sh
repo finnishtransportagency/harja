cdb81c2ac40a6ef66e2181b7e723a97de74bf4ec
Tatu Tarvainen <tatu.tarvainen@me.com>
2017-01-23 09:52:27 +0200

    Luo myös testikanta ja template dockeriin

--- tietokanta/devdb_testidata.sh
+++ tietokanta/devdb_testidata.sh
@@ -5,3 +5,8 @@
 cd /tietokanta
 
 psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U harja harja -X -q -a -v ON_ERROR_STOP=1 --pset pager=off -f testidata.sql
+
+psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U harja harja -c "CREATE USER harjatest WITH CREATEDB;"
+psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U harja harja -c "ALTER USER harjatest WITH SUPERUSER;"
+psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U harja harja -c "CREATE DATABASE harjatest WITH TEMPLATE harja OWNER harjatest;"
+psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U harja harja -c "CREATE DATABASE harjatest_template WITH TEMPLATE harjatest OWNER harjatest;"
