1ef7cc6cb65fb5d4254297d146515a458cd69479
Jari Hanhela <jarihanhela@gmail.com>
2017-01-24 20:45:27 +0200

    Nimeä korjaa-virheelliset-merkinnat -> korjaa-virheelliset-rampit

--- laadunseuranta/clj-src/harja_laadunseuranta/tarkastusreittimuunnin/ramppianalyysi.clj
+++ laadunseuranta/clj-src/harja_laadunseuranta/tarkastusreittimuunnin/ramppianalyysi.clj
@@ -3,20 +3,13 @@
    reittimerkintöjen ja niihin liittyvän geometrisoinnin korjaamiseen, mikäli
    geometrisointi on osunut virheellisesti rampille."
   (:require [taoensso.timbre :as log]
-            [harja-laadunseuranta.tietokanta :as tietokanta]
-            [harja-laadunseuranta.kyselyt :as q]
-            [harja-laadunseuranta.utils :as utils]
-            [harja.kyselyt.tarkastukset :as tark-q]
-            [harja.domain.tierekisteri :as tr-domain]
-            [clojure.string :as str]
-            [clj-time.core :as t]
-            [clj-time.coerce :as c]))
+            [harja.domain.tierekisteri :as tr-domain]))
 
 (defn- lisaa-merkintoihin-ramppitiedot [merkinnat]
   (mapv #(assoc % :piste-rampilla? (tr-domain/tie-rampilla? %))
         merkinnat))
 
-(defn korjaa-virheelliset-merkinnat [merkinnat]
+(defn korjaa-virheelliset-rampit [merkinnat]
   (let [merkinnat-ramppitiedoilla (lisaa-merkintoihin-ramppitiedot merkinnat)
         lopputulos merkinnat]
     lopputulos))
