3e1b988bfc973e7a35de750898e0d767a929987a
Jari Hanhela <jarihanhela@gmail.com>
2017-01-25 18:23:21 +0200

    Merge branch 'develop' into HAR-4094
    # Conflicts:
    #	tietokanta/src/main/resources/db/migration/V1_484__.sql

--- tietokanta/src/main/resources/db/migration/V1_486__.sql
+++ tietokanta/src/main/resources/db/migration/V1_486__.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE TYPE laheinen_osoiterivi AS (
  tie INTEGER,      -- TR tie
  osa INTEGER,      -- TR osa
@@ -6,22 +5,3 @@ CREATE TYPE laheinen_osoiterivi AS (
  ajorata INTEGER,  -- TR ajorata
  d NUMERIC 	   -- ST_Distance annetusta pisteesta
 );
-=======
--- Jos erilliskustannuksia muokataan, poistetaan hoitokauden laskutusyhteenvedot
-CREATE OR REPLACE FUNCTION poista_muistetut_laskutusyht_erilliskustannus() RETURNS trigger AS $$
-BEGIN
-        PERFORM poista_hoitokauden_muistetut_laskutusyht(NEW.urakka, NEW.pvm::DATE);
-        RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE TRIGGER tg_poista_muistetut_laskutusyht_erilliskustannus
-AFTER INSERT OR UPDATE
-        ON erilliskustannus
-FOR EACH ROW
-EXECUTE PROCEDURE poista_muistetut_laskutusyht_erilliskustannus();
-
-
-ALTER TABLE sanktio ADD COLUMN poistettu BOOLEAN DEFAULT FALSE;
->>>>>>> develop
