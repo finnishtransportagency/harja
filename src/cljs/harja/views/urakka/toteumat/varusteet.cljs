809da269a0a41732cde9d74024450a301691338e
Mikko Rönkkömäki <mikko.ronkkomaki@gmail.com>
2017-01-25 10:55:25 +0200

    Kytke TR-komponentit pois käytöstä

--- src/cljs/harja/views/urakka/toteumat/varusteet.cljs
+++ src/cljs/harja/views/urakka/toteumat/varusteet.cljs
@@ -41,8 +41,7 @@
                    [reagent.ratom :refer [reaction run!]]
                    [tuck.intercept :refer [intercept send-to]]))
 
-;; todo: aseta falseksi ennen mergeämistä
-(def tr-kaytossa? true)
+(def tr-kaytossa? false)
 
 (def nayta-max-toteumaa 500)
 
