5ba76ad0972ed4d4e0b5efe49649a2ccce3727c9
Jari Hanhela <jarihanhela@gmail.com>
2017-01-25 13:50:10 +0200

    Refactoroi ramppianalyysia

--- laadunseuranta/clj-src/harja_laadunseuranta/tarkastusreittimuunnin/ramppianalyysi.clj
+++ laadunseuranta/clj-src/harja_laadunseuranta/tarkastusreittimuunnin/ramppianalyysi.clj
@@ -5,6 +5,30 @@
   (:require [taoensso.timbre :as log]
             [harja.domain.tierekisteri :as tr-domain]))
 
+(defn maarittele-alkavien-ramppien-indeksit
+  "Palauttaa indeksit merkintöihin, joissa siirrytään ei-ramppitieltä rampille.
+   Indeksiä seuraavat merkinnät ovat rampilla ajettuja pisteitä
+   (paitsi jos rampilla ajoa on vain yksi piste)."
+  [merkinnat]
+  (let [tulos (reduce (fn [tulos seuraava]
+                        (if
+                          ;; Edellinen piste ei ollut rampilla, mutta seuraava on
+                          ;; --> Otetaan indeksi talteen
+                          (and (not (-> tulos :edellinen-piste :piste-rampilla?))
+                               (:piste-rampilla? seuraava))
+                          ;; Jatketaan...
+                          (assoc tulos :edellinen-piste seuraava
+                                       :kasiteltava-indeksi (inc (:kasiteltava-indeksi tulos))
+                                       :ramppien-alut
+                                       (conj (:ramppien-alut tulos) (:kasiteltava-indeksi tulos)))
+                          (assoc tulos :edellinen-piste seuraava
+                                       :kasiteltava-indeksi (inc (:kasiteltava-indeksi tulos)))))
+                      {:ramppien-alut []
+                       :edellinen-piste nil
+                       :kasiteltava-indeksi 0}
+                      merkinnat)]
+    (:ramppien-alut tulos)))
+
 (defn- projisoi-merkinta-oikealle-tielle
   "Projisoi yksittäisen merkinnän annetulle tielle"
   [merkinta projisoitava-tie]
@@ -13,7 +37,7 @@
         lahin-vastaava-osoite (first (sort-by :etaisyys-gps-pisteesta projisoitavaa-tieta-vastaavat-osoitteet))]
     (if lahin-vastaava-osoite
       (do
-        (log/debug "Projisoidaan yksittäinen ramppimerkintä tielle: " projisoitava-tie)
+        (log/debug "Projisoidaan ramppimerkintä tielle: " projisoitava-tie)
         (-> merkinta
             (assoc-in [:tr-osoite :tie] (:tie lahin-vastaava-osoite))
             (assoc-in [:tr-osoite :aosa] (:aosa lahin-vastaava-osoite))
@@ -21,7 +45,7 @@
             (assoc-in [:tr-osoite :losa] (:losa lahin-vastaava-osoite))
             (assoc-in [:tr-osoite :let] (:let lahin-vastaava-osoite))))
       (do
-        (log/debug (str "Merkintää ei voida projisoida annetulle tielle: " projisoitava-tie))
+        (log/debug (str "Ei voitu projisoida ramppimerkintää tielle: " projisoitava-tie))
         ;; Poistetaan merkinnältä projisoitu osoite. Tarkastusreittimuunnin olettaa merkinnän olevan
         ;; osa samaa tietä niin kauan kunnes oikea osoite löytyy.
         ;; Merkintöjä ei kuitenkaan sovi poistaa, sillä muuten saatetaan menettää havaintoja / mittauksia.
@@ -49,12 +73,7 @@
                                  indeksin-jalkeiset-merkinnat)]
     rampin-merkinnat))
 
-(defn- korjaa-vahapatoiset-rampit
-  "Ottaa reittimerkinnät ramppitiedolla sekä indeksin, joissa siirrytään rampille.
-   Mikäli rampilla ajo sisältää vähemmän kuin N pistettä, projisoi rampin takaisin edeltävälle tielle.
-   Muussa tapauksessa ei tee rampille mitään.
-
-   Palauttaa kaikki merkinnät, joissa indeksin kuvaava ramppi-osa on korjattu."
+(defn- korjaa-vahapatoinen-ramppi
   [merkinnat-ramppitiedoilla ramppi-indeksi n]
   (log/debug "Analysoidaan mahdollisesti vähäpätöinen ramppi indeksissä: " ramppi-indeksi)
   (if (= ramppi-indeksi 0)
@@ -65,7 +84,7 @@
                                                                   (dec ramppi-indeksi))
                                                              rampin-merkinnat)
                             rampin-merkinnat)
-          _ (log/debug "Vähäpätöinen ramppi havaittu? " (not= rampin-merkinnat korjattu-ramppi))
+          _ (log/debug "Vähäpätöinen ramppi havaittu ja korjattu? " (not= rampin-merkinnat korjattu-ramppi))
           osa-ennen-ramppia (take ramppi-indeksi merkinnat-ramppitiedoilla)
           osa-rampin-jalkeen (drop (+ ramppi-indeksi (count korjattu-ramppi)) merkinnat-ramppitiedoilla)]
       (concat
@@ -73,44 +92,29 @@
         korjattu-ramppi
         osa-rampin-jalkeen))))
 
-(defn maarittele-alkavien-ramppien-indeksit
-  "Palauttaa indeksit merkintöihin, joissa siirrytään ei-ramppitieltä rampille.
-   Indeksiä seuraavat merkinnät ovat rampilla ajettuja pisteitä
-   (paitsi jos rampilla ajoa on vain yksi piste)."
-  [merkinnat]
-  (let [tulos (reduce (fn [tulos seuraava]
-                        (if
-                          ;; Edellinen piste ei ollut rampilla, mutta seuraava on
-                          ;; --> Otetaan indeksi talteen
-                          (and (not (-> tulos :edellinen-piste :piste-rampilla?))
-                               (:piste-rampilla? seuraava))
-                          ;; Jatketaan...
-                          (assoc tulos :edellinen-piste seuraava
-                                       :kasiteltava-indeksi (inc (:kasiteltava-indeksi tulos))
-                                       :ramppien-alut
-                                       (conj (:ramppien-alut tulos) (:kasiteltava-indeksi tulos)))
-                          (assoc tulos :edellinen-piste seuraava
-                                       :kasiteltava-indeksi (inc (:kasiteltava-indeksi tulos)))))
-                      {:ramppien-alut []
-                       :edellinen-piste nil
-                       :kasiteltava-indeksi 0}
-                      merkinnat)]
-    (:ramppien-alut tulos)))
+(defn- korjaa-vahapatoiset-rampit
+  "Ottaa reittimerkinnät ramppitiedolla ja analysoi kaikki rampille siirtymiset.
+   Mikäli rampilla ajo sisältää vähemmän kuin N pistettä, projisoi rampin takaisin edeltävälle tielle.
+   Muussa tapauksessa ei tee rampille mitään.
+
+   Palauttaa kaikki merkinnät, joissa indeksin kuvaava ramppi-osa on korjattu."
+  [merkinnat-ramppitiedoilla n]
+  (let [alkavien-ramppien-indeksit (maarittele-alkavien-ramppien-indeksit merkinnat-ramppitiedoilla)
+        ;; Käydään jokainen rampille siirtymä erikseen läpi ja korjataan tarvittaessa.
+        korjatut-vahapatoiset-rampit (reduce (fn [edellinen-tulos seuraava-indeksi]
+                                               (korjaa-vahapatoinen-ramppi edellinen-tulos seuraava-indeksi n))
+                                             merkinnat-ramppitiedoilla
+                                             alkavien-ramppien-indeksit)]
+    korjatut-vahapatoiset-rampit))
 
 (defn- projisoi-virheelliset-rampit-uudelleen
   "Projisoi virheelliset rampit seuraavasti:
   - Jos rampilla ajoa on erittäin pieni osuus, projisoi rampin takaisin tielle, josta rampille ajettiin"
   [merkinnat-ramppitiedoilla]
-  (let [;; Projisoi ramppi suoraan takaisin ramppia edeltäneelle tielle jos pisteitä on erittäin vähän
-        alkavien-ramppien-indeksit (maarittele-alkavien-ramppien-indeksit merkinnat-ramppitiedoilla)
-        _ (log/debug "Rampeille siirtymisiä havaittu: " (count alkavien-ramppien-indeksit) "kpl. "
-                     "Indeksit: " (pr-str alkavien-ramppien-indeksit))
-        ;; Käydään jokainen rampille siirtymä erikseen läpi ja korjataan tarvittaessa.
-        korjatut-merkinnat (reduce (fn [edellinen-tulos seuraava-indeksi]
-                                     (korjaa-vahapatoiset-rampit edellinen-tulos seuraava-indeksi 5))
-                                   merkinnat-ramppitiedoilla
-                                   alkavien-ramppien-indeksit)]
-    korjatut-merkinnat))
+  (log/debug "Projisoidaan virheelliset rampit uudelleen")
+  (log/debug "Rampeille siirtymisiä havaittu: "
+             (count (maarittele-alkavien-ramppien-indeksit merkinnat-ramppitiedoilla)) "kpl.")
+  (korjaa-vahapatoiset-rampit merkinnat-ramppitiedoilla 5))
 
 (defn lisaa-merkintoihin-ramppitiedot
   "Lisää merkintöihin tiedon siitä, onko piste projisoitu rampille."
