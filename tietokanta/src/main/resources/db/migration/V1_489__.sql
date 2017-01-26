3a7155ededfe37337d5be439dc114828b6dc0c5f
Jari Hanhela <jarihanhela@gmail.com>
2017-01-26 14:44:07 +0200

    Merge remote-tracking branch 'origin/HAR-4346-geometriapaivitysten-korjaus' into HAR-4346-geometriapaivitysten-korjaus
    # Conflicts:
    #	tietokanta/src/main/resources/db/migration/V1_486__.sql

--- tietokanta/src/main/resources/db/migration/V1_489__.sql
+++ tietokanta/src/main/resources/db/migration/V1_489__.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- Luo funktio, joka lisää uuden geometriapäivityksen, jos sellaista ei löydy päivittämisen yhteydessä.
 
 CREATE OR REPLACE FUNCTION paivita_geometriapaivityksen_viimeisin_paivitys(
@@ -30,37 +29,3 @@ LANGUAGE plpgsql;
 
 ALTER TABLE geometriapaivitys
   ALTER COLUMN nimi TYPE VARCHAR(30);
-=======
--- Jos muutoshintaista työtä muokataan, poistetaan laskutusyhteenvedot urakan ajalta, koska muutoshinnan voimassa urakan ajan
-CREATE OR REPLACE FUNCTION poista_muistetut_laskutusyht_muutoshintainen_tyo() RETURNS trigger AS $$
-BEGIN
-  DELETE
-    FROM laskutusyhteenveto_cache
-   WHERE urakka = NEW.urakka;
-  RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE TRIGGER tg_poista_muistetut_laskutusyht_muutoshintainen_tyo
-AFTER INSERT OR UPDATE
-  ON muutoshintainen_tyo
-FOR EACH ROW
-EXECUTE PROCEDURE poista_muistetut_laskutusyht_muutoshintainen_tyo();
-
-
--- Jos urakan lämpötila muuttuu, poistetaan laskutusyhteenvedot ko. hoitokauden ajalta, voi vaikuttaa suolasakon lasketaan
-CREATE OR REPLACE FUNCTION poista_muistetut_laskutusyht_lampotila() RETURNS trigger AS $$
-BEGIN
-  PERFORM poista_hoitokauden_muistetut_laskutusyht(NEW.urakka, NEW.alkupvm::DATE);
-  RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE TRIGGER tg_poista_muistetut_laskutusyht_lampotila
-AFTER INSERT OR UPDATE
-  ON lampotilat
-FOR EACH ROW
-EXECUTE PROCEDURE poista_muistetut_laskutusyht_lampotila();
->>>>>>> develop
