19af1b5a0f9de0d96ad909ec4e30d238c9cc2a2a
Jari Hanhela <jarihanhela@gmail.com>
2017-01-26 11:28:44 +0200

    Merge branch 'develop' into HAR-4094
    # Conflicts:
    #	tietokanta/src/main/resources/db/migration/V1_486__.sql

--- tietokanta/src/main/resources/db/migration/V1_487__.sql
+++ tietokanta/src/main/resources/db/migration/V1_487__.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE TYPE laheinen_osoiterivi AS (
  tie INTEGER,      -- TR tie
  osa INTEGER,      -- TR osa
@@ -6,37 +5,3 @@ CREATE TYPE laheinen_osoiterivi AS (
  ajorata INTEGER,  -- TR ajorata
  d NUMERIC 	   -- ST_Distance annetusta pisteesta
 );
-=======
--- Jos muutoshintaista työtä muokataan, poistetaan laskutusyhteenvedot urakan ajalta, koska muutoshinnan voimassa urakan ajan
-CREATE OR REPLACE FUNCTION poista_muistetut_laskutusyht_muutoshintainen_tyo() RETURNS trigger AS $$
-BEGIN
-  DELETE
-    FROM laskutusyhteenveto_cache
-   WHERE urakka = NEW.urakka;
-  RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE TRIGGER tg_poista_muistetut_laskutusyht_muutoshintainen_tyo
-AFTER INSERT OR UPDATE
-  ON muutoshintainen_tyo
-FOR EACH ROW
-EXECUTE PROCEDURE poista_muistetut_laskutusyht_muutoshintainen_tyo();
-
-
--- Jos urakan lämpötila muuttuu, poistetaan laskutusyhteenvedot ko. hoitokauden ajalta, voi vaikuttaa suolasakon lasketaan
-CREATE OR REPLACE FUNCTION poista_muistetut_laskutusyht_lampotila() RETURNS trigger AS $$
-BEGIN
-  PERFORM poista_hoitokauden_muistetut_laskutusyht(NEW.urakka, NEW.alkupvm::DATE);
-  RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE TRIGGER tg_poista_muistetut_laskutusyht_lampotila
-AFTER INSERT OR UPDATE
-  ON lampotilat
-FOR EACH ROW
-EXECUTE PROCEDURE poista_muistetut_laskutusyht_lampotila();
->>>>>>> develop
