79369076b43b933b01410f3bac68a29d9a128e60
Erno Kuusela <erno.kuusela@solita.fi>
2017-01-27 10:54:40 +0000

    paranna interpoloinnin testiä

--- test/clj/harja/palvelin/palvelut/tilannekuva_test.clj
+++ test/clj/harja/palvelin/palvelut/tilannekuva_test.clj
@@ -265,10 +265,5 @@
         pisteen-tr-osoite (:tierekisteriosoite asia-ulos)]
 
     (is (some? interpoloitu-aika))
-    (is (->> interpoloitu-aika
-             (compare (:paattynyt asia-ulos))
-             (= 1)))
-    (is (->> interpoloitu-aika
-             (compare (:alkanut asia-ulos))
-             (= -1)))
-    (is (some? pisteen-tr-osoite))))
+    (is (= interpoloitu-aika #inst "2015-02-01T17:17:23.112-00:00"))
+    (is (= {:numero 4, :alkuosa 403, :alkuetaisyys 173} pisteen-tr-osoite))))
