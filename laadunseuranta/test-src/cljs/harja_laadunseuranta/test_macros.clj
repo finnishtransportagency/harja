5cf4f9f2d88528614e6fe03394cfd04218c21ecb
Jari Hanhela <jarihanhela@gmail.com>
2017-02-10 09:23:12 +0200

    Käytä yhteisiä testityökaluja LS:n testeissä

--- laadunseuranta/test-src/cljs/harja_laadunseuranta/test_macros.clj
+++ laadunseuranta/test-src/cljs/harja_laadunseuranta/test_macros.clj
@@ -2,10 +2,10 @@
 
 (defmacro prepare-component-tests []
   `(cljs.test/use-fixtures :each
-     {:before (fn [] (reset! harja-laadunseuranta.testutils/*test-container* (cljs-react-test.utils/new-container!)))
-      :after (fn [] (cljs-react-test.utils/unmount! @harja-laadunseuranta.testutils/*test-container*))}))
+     {:before (fn [] (reset! harja.testutils.shared-testutils/*test-container* (cljs-react-test.utils/new-container!)))
+      :after (fn [] (cljs-react-test.utils/unmount! @harja.testutils.shared-testutils/*test-container*))}))
 
 (defmacro with-component [component & body]
   `(do
-     (reagent.core/render ~component @harja-laadunseuranta.testutils/*test-container*)
+     (reagent.core/render ~component @harja.testutils.shared-testutils/*test-container*)
      ~@body))
