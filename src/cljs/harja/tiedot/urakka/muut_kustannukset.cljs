298b1cf9d226ef66b11214648dfd6045f24eb22b
Erno Kuusela <erno.kuusela@solita.fi>
2017-02-15 12:09:42 +0000

    korjaa kohdistamattomien sanktioiden data

--- src/cljs/harja/tiedot/urakka/muut_kustannukset.cljs
+++ src/cljs/harja/tiedot/urakka/muut_kustannukset.cljs
@@ -24,14 +24,13 @@
                            :id     (-> ks :id)})]
     (concat
      (map #(assoc % :muokattava true :id (mk-id %)) mk-tiedot)
-     (map #(-> % ks->grid (assoc % :muokattava false :id (ks-id %))) ks-tiedot))))
+     (map #(-> % ks->grid (assoc :muokattava false :id (ks-id %))) ks-tiedot))))
 
 (def grid-tiedot
   (reaction (grid-tiedot* @muiden-kustannusten-tiedot @kohdistamattomien-sanktioiden-tiedot)))
 
 (defn hae-muiden-kustannusten-tiedot! [urakka-id sopimus-id [alkupvm loppupvm]]
-  (do (log "hae-muiden-kustannusten-tiedot! post")
-      (k/post! :hae-yllapito-toteumat {:urakka urakka-id :sopimus sopimus-id :alkupvm alkupvm :loppupvm loppupvm})))
+  (k/post! :hae-yllapito-toteumat {:urakka urakka-id :sopimus sopimus-id :alkupvm alkupvm :loppupvm loppupvm}))
 
 (defn tallenna-toteuma! [toteuman-tiedot]
   (k/post! :tallenna-yllapito-toteuma toteuman-tiedot))
