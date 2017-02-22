bbcf0c892a615bf47ba2ab02218698c384e8ca22
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-02-22 13:46:57 +0200

    Frontilla piirretyt reitit reagoivat zoom-tasoon
    Aiemmin featuren luonnin yhteydeyssä palautettiin vektori
    Openlayersin Style-objekteja. Nyt käytetään Openlayersin
    StyleFunctionia, joka saa parametrikseen kartan nykyisen resoluution,
    ja palauttaa vektorin Style-objekteja. Näin myös frontilla piirretyt
    asiat voivat reagoida zoom-tasoon, ja voimme mm. skaalata ikoneita
    alas- tai ylöspäin, ja vähentää taitoksiin piirrettyjen
    nuolten lukumäärää.
    
    Palvelimella skaalaukseen käytetään kartalla näkyvän alueen hypotenuusaa,
    ja frontilla kartan resoluutiota, eli montako metriä on yksi pikseli.
    Tämän takia frontti ja palvelin eivät voi suoraan käyttää samoja
    skaalausfunktioita, mutta yritetään silti pitää näiden lopputulokset
    lähellä toisiaan.

--- src/cljs/harja/ui/openlayers/featuret.cljs
+++ src/cljs/harja/ui/openlayers/featuret.cljs
@@ -23,15 +23,15 @@
             [harja.tiedot.navigaatio :as nav]))
 
 (def ^{:doc "Viivaan piirrettävien nuolten välimatka, jotta nuolia ei piirretä
-turhaan liikaa"
+turhaan liikaa. Kasvata lukua = Vähemmän nuolia."
        :const true
        :private true}
-maksimi-valimatka 30000)
+maksimi-valimatka 200)
 (def ^{:doc "Viivaan piirrettävien nuolten minimietäisyys, jotta taitoksissa nuolia ei piirretä l
-iian lähekkäin."
+iian lähekkäin. Kasvata lukua = vähemmän nuolia, käännöksiin reagoidaan vähemmän."
        :const true
        :private true}
-minimi-valimatka 300)
+minimi-valimatka 50)
 
 (def ^{:doc "Kartalle piirrettävien asioiden oletus-zindex. Urakat ja muut piirretään
 pienemmällä zindexillä." :const true}
@@ -64,60 +64,70 @@ pienemmällä zindexillä." :const true}
                      }))))
 
 (defn- tee-nuoli
-  [kasvava-zindex {:keys [img scale zindex anchor rotation]} [piste rotaatio]]
-  (ol.style.Style.
-    #js {:geometry piste
-         :zIndex   (or zindex (swap! kasvava-zindex inc))
-         :image    (ol.style.Icon.
-                     #js {:src            (str img)
-                          :scale          (or scale 1)
-                          :rotation       (or rotation rotaatio) ;; Rotaatio on laskettu, rotation annettu.
-                          :anchor         (or (clj->js anchor) #js [0.5 0.5])
-                          :rotateWithView false})}))
+  [kasvava-zindex {:keys [img scale zindex anchor rotation min-resolution max-resolution]} [piste rotaatio] reso]
+  (log "piirretään nuoli resoluutiolla " (pr-str reso) ", asetukset sanoo " (pr-str min-resolution) " - " (pr-str max-resolution))
+  (let [sopiva-reso? (and (or (nil? min-resolution)
+                               (>= reso min-resolution))
+                           (or (nil? max-resolution)
+                               (<= reso max-resolution)))
+        image (ol.style.Icon.
+                #js {:src (str img)
+                     :scale (apurit/ikonin-skaala-resoluutiolle reso (or scale 1))
+                     :rotation (or rotation rotaatio) ;; Rotaatio on laskettu, rotation annettu.
+                     :anchor (or (clj->js anchor) #js [0.5 0.5])
+                     :rotateWithView false})]
+    (ol.style.Style.
+     #js {:geometry piste
+          :zIndex (or zindex (swap! kasvava-zindex inc))
+          :image (when sopiva-reso? image)})))
 
 ;; Käytetään sisäisesti :viiva featurea rakentaessa
 (defn- tee-merkki
-  [kasvava-zindex tiedot [piste _]]
-  (tee-nuoli kasvava-zindex (merge {:anchor [0.5 1]} tiedot) [piste 0]))
+  [kasvava-zindex tiedot [piste _] reso]
+  (tee-nuoli kasvava-zindex (merge {:anchor [0.5 1]} tiedot) [piste 0] reso))
 
 ;; Käytetään sisäisesti :viiva featurea rakentaessa
 (defn- tee-ikonille-tyyli
-  [zindex laske-taitokset-fn {:keys [tyyppi paikka img] :as ikoni}]
-  ;; Kokonaisuus koodattiin alunperin sillä oletuksella, että :viivalle piirrettäisiin aina jokin ikoni.
-  ;; Oletuksena pieni merkki reitin loppuun. Tuli kuitenkin todettua, että esim tarkastukset joissa ei ilmennyt
-  ;; mitään halutaan todnäk vaan piirtää hyvin haalealla harmaalla tms. Tällaisissa tapauksissa :img arvoa
-  ;; ei ole määritelty, eikä siis haluta piirtää mitään.
-  (when img
-    (assert (#{:nuoli :merkki} tyyppi) "Merkin tyypin pitää olla joko :nuoli tai :merkki")
-    (let [palauta-paikat (fn [paikka]
-                           (assert (#{:alku :loppu :taitokset} paikka)
-                                   "Merkin paikan pitää olla :alku, :loppu, :taitokset")
-                           (condp = paikka
-                             :alku
-                             [[(-> (laske-taitokset-fn) first :sijainti first clj->js ol.geom.Point.)
-                               (-> (laske-taitokset-fn) first :rotaatio)]]
-                             :loppu
-                             [[(-> (laske-taitokset-fn) last :sijainti second clj->js ol.geom.Point.)
-                               (-> (laske-taitokset-fn) last :rotaatio)]]
-                             :taitokset
-                             (apurit/taitokset-valimatkoin
-                              minimi-valimatka maksimi-valimatka (butlast (laske-taitokset-fn)))))
-          pisteet-ja-rotaatiot (mapcat palauta-paikat (if (coll? paikka) paikka [paikka]))]
-      (condp = tyyppi
-        :nuoli (map #(tee-nuoli zindex ikoni %)
-                    pisteet-ja-rotaatiot)
-        :merkki (map #(tee-merkki zindex ikoni %) pisteet-ja-rotaatiot)))))
+  ([zindex laske-taitokset-fn opts]
+    (tee-ikonille-tyyli zindex laske-taitokset-fn opts 10))
+  ([zindex laske-taitokset-fn {:keys [tyyppi paikka img] :as ikoni} reso]
+    ;; Kokonaisuus koodattiin alunperin sillä oletuksella, että :viivalle piirrettäisiin aina jokin ikoni.
+    ;; Oletuksena pieni merkki reitin loppuun. Tuli kuitenkin todettua, että esim tarkastukset joissa ei ilmennyt
+    ;; mitään halutaan todnäk vaan piirtää hyvin haalealla harmaalla tms. Tällaisissa tapauksissa :img arvoa
+    ;; ei ole määritelty, eikä siis haluta piirtää mitään.
+   (when img
+     (assert (#{:nuoli :merkki} tyyppi) "Merkin tyypin pitää olla joko :nuoli tai :merkki")
+     (let [palauta-paikat (fn [paikka]
+                            (assert (#{:alku :loppu :taitokset} paikka)
+                                    "Merkin paikan pitää olla :alku, :loppu, :taitokset")
+                            (condp = paikka
+                              :alku
+                              [[(-> (laske-taitokset-fn) first :sijainti first clj->js ol.geom.Point.)
+                                (-> (laske-taitokset-fn) first :rotaatio)]]
+                              :loppu
+                              [[(-> (laske-taitokset-fn) last :sijainti second clj->js ol.geom.Point.)
+                                (-> (laske-taitokset-fn) last :rotaatio)]]
+                              :taitokset
+                              (apurit/taitokset-valimatkoin
+                                (* minimi-valimatka reso) (* maksimi-valimatka reso) (butlast (laske-taitokset-fn)))))
+           pisteet-ja-rotaatiot (mapcat palauta-paikat (if (coll? paikka) paikka [paikka]))]
+       (condp = tyyppi
+         :nuoli (map #(tee-nuoli zindex ikoni % reso)
+                     pisteet-ja-rotaatiot)
+         :merkki (map #(tee-merkki zindex ikoni % reso) pisteet-ja-rotaatiot))))))
 
 ;; Käytetään sisäisesti :viiva featurea rakentaessa
 (defn- tee-viivalle-tyyli
-  [kasvava-zindex {:keys [color width zindex dash cap join miter]}]
-  (ol.style.Style. #js {:stroke (ol.style.Stroke. #js {:color      (or color "black")
-                                                       :width      (or width 2)
-                                                       :lineDash   (or (clj->js dash) nil)
-                                                       :lineCap    (or cap "round")
-                                                       :lineJoin   (or join "round")
-                                                       :miterLimit (or miter 10)})
-                        :zindex (or zindex (swap! kasvava-zindex inc))}))
+  ([kasvava-zindex opts]
+    (tee-viivalle-tyyli kasvava-zindex opts 10))
+  ([kasvava-zindex {:keys [color width zindex dash cap join miter]} reso]
+   (ol.style.Style. #js {:stroke (ol.style.Stroke. #js {:color (or color "black")
+                                                        :width (or width 2)
+                                                        :lineDash (or (clj->js dash) nil)
+                                                        :lineCap (or cap "round")
+                                                        :lineJoin (or join "round")
+                                                        :miterLimit (or miter 10)})
+                         :zindex (or zindex (swap! kasvava-zindex inc))})))
 
 (defmethod luo-geometria :viiva [{points :points}]
   (ol.geom.LineString. (clj->js points)))
@@ -132,10 +142,11 @@ pienemmällä zindexillä." :const true}
                             @taitokset
                             (reset! taitokset
                                     (apurit/pisteiden-taitokset points false))))
-        tee-ikoni (partial tee-ikonille-tyyli kasvava-zindex laske-taitokset)
-        tee-viiva (partial tee-viivalle-tyyli kasvava-zindex)
-        tyylit (apply concat (mapv tee-viiva viivat) (mapv tee-ikoni ikonit))]
-    (doto feature (.setStyle (clj->js tyylit)))))
+        tee-ikoni #(partial tee-ikonille-tyyli kasvava-zindex laske-taitokset %)
+        tee-viiva #(partial tee-viivalle-tyyli kasvava-zindex %)
+        tyylit (concat (mapv tee-viiva viivat) (mapv tee-ikoni ikonit))]
+    (doto feature (.setStyle (fn [reso]
+                               (clj->js (flatten (map (fn [f] (f reso)) tyylit))))))))
 
 (defmethod luo-geometria :moniviiva
   [{lines :lines}]
@@ -152,10 +163,11 @@ pienemmällä zindexillä." :const true}
                             @taitokset
                             (reset! taitokset
                                     (apurit/pisteiden-taitokset (mapcat :points lines) false))))
-        tee-ikoni (partial tee-ikonille-tyyli kasvava-zindex laske-taitokset)
-        tee-viiva (partial tee-viivalle-tyyli kasvava-zindex)
-        tyylit (apply concat (mapv tee-viiva viivat) (mapv tee-ikoni ikonit))]
-    (doto feature (.setStyle (clj->js tyylit)))))
+        tee-ikoni #(partial tee-ikonille-tyyli kasvava-zindex laske-taitokset %)
+        tee-viiva #(partial tee-viivalle-tyyli kasvava-zindex %)
+        tyylit (concat (mapv tee-viiva viivat) (mapv tee-ikoni ikonit))]
+    (doto feature (.setStyle (fn [reso]
+                               (clj->js (flatten (map (fn [f] (f reso)) tyylit))))))))
 
 (defmethod luo-geometria :merkki
   [{c :coordinates}]
