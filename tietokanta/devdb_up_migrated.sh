6d1951dd5f8092c14669e5b4ed1abace1f6eda5d
Jari Hanhela <jarihanhela@gmail.com>
2017-02-23 21:17:40 +0200

    Varmista, ettei kontti kuole heti alkuunsa

--- tietokanta/devdb_up_migrated.sh
+++ tietokanta/devdb_up_migrated.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Käynnistetään valmiiksi migratoitu harjadb Docker-image"
-docker run -p 5432:5432 --name harjadb -d jarzka/harjadb
+docker run -p 5432:5432 --name harjadb -dit jarzka/harjadb
 
 echo "Odotetaan, että PostgreSQL on käynnissä ja vastaa yhteyksiin portissa 5432"
 while ! nc -z localhost 5432; do
