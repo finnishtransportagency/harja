e9567563883dfc1b9ae469e508139f827b11a2be
Tatu Tarvainen <tatu.tarvainen@me.com>
2017-02-23 11:31:26 +0200

    Nosta ilmoitushaun viivettä 1000ms

--- src/cljs/harja/tiedot/ilmoitukset.cljs
+++ src/cljs/harja/tiedot/ilmoitukset.cljs
@@ -121,9 +121,11 @@ tila-filtterit [:kuittaamaton :vastaanotettu :aloitettu :lopetettu])
                            uusien-kyselyjen-maara)
         optiot))))
 
+(def ^:const ilmoituhaun-aloitusviive-ms 1000)
+
 (defn- hae
   "Ajastaa uuden ilmoitushaun. Jos ilmoitushaku on jo ajastettu, se perutaan ja uusi ajastetaan."
-  ([app] (hae app 300))
+  ([app] (hae app ilmoitushain-aloitusviive-ms))
   ([app timeout] (hae app timeout false))
   ([{valinnat :valinnat haku :ilmoitushaku-id :as app} timeout taustahaku?]
    (if-not (:voi-hakea? valinnat)
