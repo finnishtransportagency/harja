b9c74beacf774f88abf1af2c622ec3d73cc74c53
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-02-24 11:01:15 +0200

    Buildaa image jos harjadb image puuttuu

--- tietokanta/devdb_up_migrated.sh
+++ tietokanta/devdb_up_migrated.sh
@@ -2,12 +2,24 @@
 
 set -e
 
-echo "Käynnistetään valmiiksi migratoitu harjadb Docker-image"
-docker run -p 5432:5432 --name harjadb -dit jarzka/harjadb
+docker images | grep harjadb >> /dev/null || sh build_migrated_db_image.sh
 
-echo "Odotetaan, että PostgreSQL on käynnissä ja vastaa yhteyksiin portissa 5432"
-while ! nc -z localhost 5432; do
-    sleep 0.5;
-done;
+#echo "Käynnistetään valmiiksi migratoitu harjadb Docker-image"
+#docker run -p 5432:5432 --name harjadb -dit harjadb
 
-sh devdb_migrate.sh
+#echo "Odotetaan, että PostgreSQL on käynnissä ja vastaa yhteyksiin portissa 5432"
+#while ! nc -z localhost 5432; do
+#    sleep 0.5;
+#done;
+
+#sh devdb_migrate.sh
+
+echo "Harja käynnissä! Imagen tiedot:"
+echo ""
+
+docker images | head -n1
+docker images | grep harjadb
+
+echo ""
+echo "Jos imagen rakentamisesta on kovin pitkä aika, voit nopeuttaa kehitysmpäristön käynnistymistä buildaamalla imagen uudelleen."
+echo "Komento oh sh build_migrated_db_image.sh"
