4dda050850d39b3608be10a6f6e4d427246bf647
Jari Hanhela <jarihanhela@gmail.com>
2017-02-24 12:38:09 +0200

    Luo testi tiemerkinnän toteumien haulle

--- test/clj/harja/palvelin/palvelut/yllapito_toteumat_test.clj
+++ test/clj/harja/palvelin/palvelut/yllapito_toteumat_test.clj
@@ -12,23 +12,14 @@
 
 (defn jarjestelma-fixture [testit]
   (alter-var-root #'jarjestelma
-    (fn [_]
-      (component/start
-        (component/system-map
-          :db (tietokanta/luo-tietokanta testitietokanta)
-          :http-palvelin (testi-http-palvelin)
-          :hae-yllapito-toteumat (component/using
-                                   (->YllapitoToteumat)
-                                   [:http-palvelin :db])
-          :hae-yllapito-toteuma (component/using
-                                  (->YllapitoToteumat)
-                                  [:http-palvelin :db])
-          :hae-laskentakohteet (component/using
-                                 (->YllapitoToteumat)
-                                 [:http-palvelin :db])
-          :tallenna-yllapito-toteuma (component/using
-                                       (->YllapitoToteumat)
-                                       [:http-palvelin :db])))))
+                  (fn [_]
+                    (component/start
+                      (component/system-map
+                        :db (tietokanta/luo-tietokanta testitietokanta)
+                        :http-palvelin (testi-http-palvelin)
+                        :yllapitototeumat (component/using
+                                            (->YllapitoToteumat)
+                                            [:http-palvelin :db])))))
 
   (testit)
   (alter-var-root #'jarjestelma component/stop))
@@ -38,65 +29,71 @@
                       jarjestelma-fixture
                       urakkatieto-fixture))
 
-;; käyttää testidata.sql:stä tietoa
 (deftest yllapitototeumien-haku-test
-         (let [urakka (hae-oulun-tiemerkintaurakan-id)
-               sopimus (hae-oulun-tiemerkintaurakan-paasopimuksen-id)
-               alkupvm (pvm/luo-pvm 2016 0 1)
-               loppupvm (pvm/luo-pvm 2016 11 31)
-               res (kutsu-palvelua (:http-palvelin jarjestelma)
-                                   :hae-yllapito-toteumat +kayttaja-jvh+
-                                   {:urakka urakka
-                                    :sopimus sopimus
-                                    :alkupvm alkupvm
-                                    :loppupvm loppupvm})
-               oulun-tiemerkintaurakan-toiden-lkm (ffirst (q
+  (let [urakka (hae-oulun-tiemerkintaurakan-id)
+        sopimus (hae-oulun-tiemerkintaurakan-paasopimuksen-id)
+        alkupvm (pvm/luo-pvm 2016 0 1)
+        loppupvm (pvm/luo-pvm 2016 11 31)
+        res (kutsu-palvelua (:http-palvelin jarjestelma)
+                            :hae-yllapito-toteumat +kayttaja-jvh+
+                            {:urakka urakka
+                             :sopimus sopimus
+                             :alkupvm alkupvm
+                             :loppupvm loppupvm})
+        oulun-tiemerkintaurakan-toiden-lkm (ffirst (q
                                                      (str "SELECT count(*)
                                                        FROM yllapito_toteuma
                                                      WHERE urakka = " urakka
                                                           " AND sopimus = " sopimus
                                                           " AND pvm >= '2016-1-01' AND pvm <= '2016-12-31'")))]
-           (is (= (count res) oulun-tiemerkintaurakan-toiden-lkm) "Muiden töiden määrä")))
+    (is (= (count res) oulun-tiemerkintaurakan-toiden-lkm) "Muiden töiden määrä")))
 
 (deftest tallenna-yllapito-toteuma-test
-         (let [urakka (hae-oulun-tiemerkintaurakan-id)
-               sopimus (hae-oulun-tiemerkintaurakan-paasopimuksen-id)
-               alkupvm (pvm/luo-pvm 2016 0 1)
-               loppupvm (pvm/luo-pvm 2016 11 31)
-               toteuman-pvm (pvm/luo-pvm 2016 11 24)
-               hyotykuorma {:pvm toteuman-pvm
-                            :selite "Jouluaattona hommissa"
-                            :laskentakohde [nil "ei kohdetta tässä.."]
-                            :uusi-laskentakohde "Uusikohde"
-                            :urakka urakka
-                            :sopimus sopimus
-                            :yllapitoluokka 1
-                            :alkupvm alkupvm
-                            :loppupvm loppupvm
-                            :hinta 665.5}
-               maara-ennen-lisaysta (ffirst (q
-                                              (str "SELECT count(*)
+  (let [urakka (hae-oulun-tiemerkintaurakan-id)
+        sopimus (hae-oulun-tiemerkintaurakan-paasopimuksen-id)
+        alkupvm (pvm/luo-pvm 2016 0 1)
+        loppupvm (pvm/luo-pvm 2016 11 31)
+        toteuman-pvm (pvm/luo-pvm 2016 11 24)
+        hyotykuorma {:pvm toteuman-pvm
+                     :selite "Jouluaattona hommissa"
+                     :laskentakohde [nil "ei kohdetta tässä.."]
+                     :uusi-laskentakohde "Uusikohde"
+                     :urakka urakka
+                     :sopimus sopimus
+                     :yllapitoluokka 1
+                     :alkupvm alkupvm
+                     :loppupvm loppupvm
+                     :hinta 665.5}
+        maara-ennen-lisaysta (ffirst (q
+                                       (str "SELECT count(*)
                                                        FROM yllapito_toteuma
                                                      WHERE urakka = " urakka
-                                                   " AND sopimus = " sopimus
-                                                   " AND pvm >= '2016-1-01' AND pvm <= '2016-12-31'")))
-               laskentakohdelkm-ennen (ffirst (q
-                                                (str "SELECT count(*)
+                                            " AND sopimus = " sopimus
+                                            " AND pvm >= '2016-1-01' AND pvm <= '2016-12-31'")))
+        laskentakohdelkm-ennen (ffirst (q
+                                         (str "SELECT count(*)
                                                        FROM urakka_laskentakohde
                                                      WHERE urakka = " urakka)))
-               res (kutsu-palvelua (:http-palvelin jarjestelma)
-                                   :tallenna-yllapito-toteuma +kayttaja-jvh+
-                                   hyotykuorma)
-               maara-lisayksen-jalkeen (ffirst (q
-                                              (str "SELECT count(*)
+        res (kutsu-palvelua (:http-palvelin jarjestelma)
+                            :tallenna-yllapito-toteuma +kayttaja-jvh+
+                            hyotykuorma)
+        maara-lisayksen-jalkeen (ffirst (q
+                                          (str "SELECT count(*)
                                                        FROM yllapito_toteuma
                                                      WHERE urakka = " urakka
-                                                   " AND sopimus = " sopimus
-                                                   " AND pvm >= '2016-1-01' AND pvm <= '2016-12-31'")))
-               lisatty-toteuma (first (filter #(= (:pvm %) toteuman-pvm) (:toteumat res)))
-               laskentakohteet-jalkeen (:laskentakohteet res)]
-           (is (= (+ 1 maara-ennen-lisaysta) maara-lisayksen-jalkeen))
-           (is (= (:pvm lisatty-toteuma) toteuman-pvm) "Tallennetun yllapitototeuman pvm")
-           (is (= (:hinta lisatty-toteuma) 665.5M) "Tallennetun yllapitototeuman pvm")
-           (is (= (count (:toteumat res)) maara-lisayksen-jalkeen (+ 1 maara-ennen-lisaysta)) "Tallennuksen jälkeen erilliskustannusten määrä")
-           (is (= (count laskentakohteet-jalkeen) (+ 1 laskentakohdelkm-ennen)))))
+                                               " AND sopimus = " sopimus
+                                               " AND pvm >= '2016-1-01' AND pvm <= '2016-12-31'")))
+        lisatty-toteuma (first (filter #(= (:pvm %) toteuman-pvm) (:toteumat res)))
+        laskentakohteet-jalkeen (:laskentakohteet res)]
+    (is (= (+ 1 maara-ennen-lisaysta) maara-lisayksen-jalkeen))
+    (is (= (:pvm lisatty-toteuma) toteuman-pvm) "Tallennetun yllapitototeuman pvm")
+    (is (= (:hinta lisatty-toteuma) 665.5M) "Tallennetun yllapitototeuman pvm")
+    (is (= (count (:toteumat res)) maara-lisayksen-jalkeen (+ 1 maara-ennen-lisaysta)) "Tallennuksen jälkeen erilliskustannusten määrä")
+    (is (= (count laskentakohteet-jalkeen) (+ 1 laskentakohdelkm-ennen)))))
+
+(deftest hae-tiemerkinnan-yksikkohintaiset-tyot-toimii
+  (let [urakka (hae-oulun-tiemerkintaurakan-id)
+        vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                :hae-tiemerkinnan-yksikkohintaiset-tyot +kayttaja-jvh+
+                                {:urakka-id urakka})]
+    (is (= (count vastaus)) 3)))
