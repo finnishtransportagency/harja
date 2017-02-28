e9f9304be184a396e22000625264471fd37ad16b
Jari Hanhela <jarihanhela@gmail.com>
2017-02-28 10:03:37 +0200

    Varmista, ettei LS-työkalu kaadu jos saadaan virheellinen aikaleima

--- laadunseuranta/clj-src/harja_laadunseuranta/tarkastusreittimuunnin/tarkastusreittimuunnin.clj
+++ laadunseuranta/clj-src/harja_laadunseuranta/tarkastusreittimuunnin/tarkastusreittimuunnin.clj
@@ -47,12 +47,18 @@
                                                   (nil? (:tr-osoite seuraava-reittimerkinta))
                                                   (= (get-in nykyinen-reittimerkinta [:tr-osoite :tie])
                                                      (get-in seuraava-reittimerkinta [:tr-osoite :tie]))))
-        ei-ajallista-gappia? (boolean (or
-                                        (nil? (:aikaleima nykyinen-reittimerkinta))
-                                        (nil? (:aikaleima seuraava-reittimerkinta))
-                                        (<= (t/in-seconds (t/interval (c/from-sql-time (:aikaleima nykyinen-reittimerkinta))
-                                                                      (c/from-sql-time (:aikaleima seuraava-reittimerkinta))))
-                                            +kahden-pisteen-valinen-sallittu-aikaero-s+)))
+        ei-ajallista-gappia? (let [aikaleima-nykyinen-merkinta (c/from-sql-time (:aikaleima nykyinen-reittimerkinta))
+                                   aikaleima-seuraava-merkinta (c/from-sql-time (:aikaleima seuraava-reittimerkinta))]
+                               (if (t/before? aikaleima-seuraava-merkinta aikaleima-nykyinen-merkinta)
+                                 ;; Kerran törmättiin harvinaiseen tilanteeseen, jossa myöhemmin kirjatun pisteen
+                                 ;; aikaleima oli ennen seuraavaa. Tällainen tilanne pitää pystyä käsittelemään
+                                 false
+                                 (boolean (or
+                                            (nil? (:aikaleima nykyinen-reittimerkinta))
+                                            (nil? (:aikaleima seuraava-reittimerkinta))
+                                            (<= (t/in-seconds (t/interval aikaleima-nykyinen-merkinta
+                                                                          aikaleima-seuraava-merkinta))
+                                                +kahden-pisteen-valinen-sallittu-aikaero-s+)))))
         jatkuvat-mittausarvot-samat? (boolean (and (seuraava-mittausarvo-sama? nykyinen-reittimerkinta seuraava-reittimerkinta :soratie-tasaisuus)
                                                    (seuraava-mittausarvo-sama? nykyinen-reittimerkinta seuraava-reittimerkinta :kiinteys)
                                                    (seuraava-mittausarvo-sama? nykyinen-reittimerkinta seuraava-reittimerkinta :polyavyys)
