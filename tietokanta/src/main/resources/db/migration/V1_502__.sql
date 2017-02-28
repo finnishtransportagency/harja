e93c91ad58b1d59f89b47a946bbd837da740a4d4
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2017-02-28 09:57:02 +0200

    Tarkenna sql-kyselyä joka poistaa duplikaatit

--- tietokanta/src/main/resources/db/migration/V1_502__.sql
+++ tietokanta/src/main/resources/db/migration/V1_502__.sql
@@ -2,10 +2,10 @@
 DELETE
   FROM ilmoitus
  WHERE id IN (SELECT id
-                FROM (SELECT id, ulkoinen_id,
+                FROM (SELECT id, ulkoinen_id, luoja,
                 ROW_NUMBER() OVER (partition BY ulkoinen_id, luoja ORDER BY id) AS rnum
   FROM ilmoitus) t
- WHERE t.rnum > 1 and t.ulkoinen_id IS NOT NULL);
+ WHERE t.rnum > 1 and t.ulkoinen_id IS NOT NULL and t.luoja = 45);
 
 -- Estä duplikaattien tuominen uudelleen Harja-järjestelmään
 ALTER TABLE ilmoitus ADD CONSTRAINT uniikki_ulkoinen_ilmoitus UNIQUE (ulkoinen_id, luoja);
