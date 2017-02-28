b05004dfd4d094f7c2e004829fc5d8df6fb3b71d
Jari Hanhela <jarihanhela@gmail.com>
2017-02-28 15:16:27 +0200

    Luo pohja laadunseurannan viestintä-namespacelle

---
+++ src/clj/harja/palvelin/palvelut/laadunseuranta/viestinta.clj
@@ -0,0 +1,10 @@
+(ns harja.palvelin.palvelut.laadunseuranta.viestinta
+  "Tässä namespacessa on palveluita laadunseuranta-asioihin liittyvään teksti-/sähköpostiviestintään."
+  (:require [taoensso.timbre :as log]
+            [clojure.core.match :refer [match]]
+            [harja.tyokalut.html :as html-tyokalut]
+
+            [harja.palvelin.integraatiot.sahkoposti :as sahkoposti]
+            [hiccup.core :refer [html]]
+            [harja.palvelin.komponentit.fim :as fim])
+  (:use [slingshot.slingshot :only [try+ throw+]]))
