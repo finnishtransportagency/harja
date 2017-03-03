f5e52123028e444e69951f9f17bb897bf55a8fdd
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-03-03 12:23:49 +0200

    Lisää REPL/QGIS käyttöä varten kysely

--- src/clj/harja/kyselyt/debug.sql
+++ src/clj/harja/kyselyt/debug.sql
@@ -6,3 +6,11 @@ SELECT t.reitti,
   FROM toteuma t
        LEFT JOIN reittipiste rp ON rp.toteuma = t.id
  WHERE t.id = :toteuma-id
+
+-- name: hae-toteuman-reitti-ja-pisteet-wkt
+-- single?: true
+SELECT st_astext(st_collect(t.reitti, st_collect((SELECT array_agg(rp.sijainti)::geometry[]
+                                                    FROM reittipiste rp
+						   WHERE rp.toteuma=t.id))))
+  FROM toteuma t
+ WHERE t.id = :toteuma-id
