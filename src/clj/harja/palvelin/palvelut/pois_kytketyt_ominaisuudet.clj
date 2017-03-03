5bf08be69f6b786575ea5f1cc16283548eecd735
Erno Kuusela <erno.kuusela@solita.fi>
2017-03-03 12:48:45 +0000

    "ominaisuus-käytössä?" back-koodin käytettäväksi

--- src/clj/harja/palvelin/palvelut/pois_kytketyt_ominaisuudet.clj
+++ src/clj/harja/palvelin/palvelut/pois_kytketyt_ominaisuudet.clj
@@ -3,9 +3,17 @@
             [harja.palvelin.komponentit.http-palvelin :refer [julkaise-palvelu poista-palvelut]]
             [harja.domain.oikeudet :as oikeudet]))
 
+(defonce pois-ominaisuudet (atom #{}))
+
+(defn ominaisuus-kaytossa? [k]
+  (let [pko @pois-ominaisuudet]
+    (not (and (set? pko) (contains? pko k)))))
+
+
 (defrecord PoisKytketytOminaisuudet [pois-kytketyt-ominaisuudet-joukko]
   component/Lifecycle
   (start [this]
+    (reset! pois-ominaisuudet pois-kytketyt-ominaisuudet-joukko)
     (let [http (:http-palvelin this)]
       (julkaise-palvelu http :pois-kytketyt-ominaisuudet
                         (fn [user tiedot]
