05b4868b0c7718d603a247b3bf92a0ed4db54429
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2017-03-06 15:06:46 +0200

    HAR-4749 Järjestä aikataulu aloitushetken mukaan

--- src/clj/harja/kyselyt/yllapitokohteet.sql
+++ src/clj/harja/kyselyt/yllapitokohteet.sql
@@ -376,7 +376,8 @@ WHERE
   AND sopimus = :sopimus
   AND (:vuosi::INTEGER IS NULL OR (cardinality(vuodet) = 0
      OR vuodet @> ARRAY[:vuosi]::int[]))
-  AND poistettu IS NOT TRUE;
+  AND poistettu IS NOT TRUE
+ORDER BY aikataulu_kohde_alku;
 
 -- name: hae-tiemerkintaurakan-aikataulu
 -- Hakee tiemerkintäurakan kohteiden aikataulutiedot
@@ -408,7 +409,8 @@ WHERE
   suorittava_tiemerkintaurakka = :suorittava_tiemerkintaurakka
   AND (:vuosi::INTEGER IS NULL OR (cardinality(vuodet) = 0
      OR vuodet @> ARRAY[:vuosi]::int[]))
-  AND poistettu IS NOT TRUE;
+  AND poistettu IS NOT TRUE
+ORDER BY aikataulu_tiemerkinta_alku;
 
 -- name: hae-urakan-tyyppi
 SELECT tyyppi
