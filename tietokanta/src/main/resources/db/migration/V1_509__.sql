fb4c7b3a36c4577878555bbca59e7f226ccedf57
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-03-07 16:10:53 +0200

    Lisää uudet integraatiot lokiin

---
+++ tietokanta/src/main/resources/db/migration/V1_509__.sql
@@ -0,0 +1,4 @@
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'kirjaa-tiemerkintatoteuma');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'poista-tiemerkintatoteuma');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'kirjaa-yllapitokohteen-tiemerkintatoteuma');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'poista-yllapitokohteen-tiemerkintatoteuma');
