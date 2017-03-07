ff070a3aaea17099a9b2759411d4bbfabc62dec7
Erno Kuusela <erno.kuusela@solita.fi>
2017-03-07 14:47:43 +0000

    tietyoilmoitusten tietolomake wip

--- src/cljs/harja/views/ilmoitukset/tietyotilmoitukset.cljs
+++ src/cljs/harja/views/ilmoitukset/tietyotilmoitukset.cljs
@@ -107,6 +107,14 @@
              (interleave
               (mapv (comp str :id) urakat) (mapv :nimi urakat))))
 
+(defn- pvm-vali-paivina [p1 p2]
+
+  (let [pp1 (pvm/->pvm p1)
+        pp2 (pvm/->pvm p2)]
+    (log "pvm-vali-paivina kutsuttu" p1 p2 pp1 pp2)
+    (when (and pp1 pp2)
+      (/ (Math/abs (- p1 p2)) (* 1000 60 60 24)))))
+
 (defn ilmoituksen-tiedot [e! ilmoitus kayttajan-urakat]
   (fn [e! ilmoitus]
     [:div
@@ -115,7 +123,7 @@
       [lomake/lomake {:otsikko "Muokkaa ilmoitusta"
                       :muokkaa #(e! tiedot/->IlmoitustaMuokattu %)}
        [(lomake/ryhma
-         "zz"
+         "Ilmoitus koskee"
          {:nimi :koskee
           :otsikko "Ilmoitus koskee:"
           :tyyppi :valinta
@@ -123,19 +131,95 @@
           :valinta-nayta second
           :valinta-arvo first
           :muokattava? (constantly true)}
+         )
+        (lomake/ryhma "Tiedot koko kohteesta"
          {:nimi :projekti-tai-urakka
           :otsikko "Projekti tai urakka:"
           :tyyppi :valinta
           :valinnat (projekti-valinnat kayttajan-urakat)
-          ;; :valinnat [["1" "jeejee"]]
           :valinta-nayta second :valinta-arvo first
           :muokattava? (constantly true)
           }
+         {:nimi :urakoitsijan-nimi
+          :otsikko "Urakoitsijan nimi"
+          :muokattava? (constantly true)
+          :tyyppi :string
+          }
+         {:nimi :urakoitsijan-yhteyshenkilo
+          :otsikko "Urakoitsijan yhteyshenkilö"
+          :hae #(str (:urakoitsijayhteyshenkilo_etunimi %) " " (:urakoitsijayhteyshenkilo_sukunimi %))
+          :muokattava? (constantly true)
+          :tyyppi :string
+          }
+         {:nimi :urakoitsijayhteyshenkilo_matkapuhelin
+          :otsikko "Puhelinnumero"
+          :tyyppi :string
+          }
+         {:nimi :tilaajan-nimi
+          :otsikko "Tilaajan nimi"
+          :muokattava? (constantly true)
+          :tyyppi :string
+          }
+        {:nimi :tilaajan-yhteyshenkilo
+         :otsikko "Tilaajan yhteyshenkilö"
+         :hae #(str (:tilaajayhteyshenkilo_etunimi %) " " (:tilaajayhteyshenkilo_sukunimi %))
+         :muokattava? (constantly true)
+         :tyyppi :string
+         }
+        {:nimi :tilaajayhteyshenkilo_matkapuhelin
+         :otsikko "Puhelinnumero"
+         :tyyppi :string
+         }
          {:nimi :tr_numero
           :otsikko "Tienumero"
           :tyyppi :positiivinen-numero
-          :muokattava? (constantly true)}
-         )]
+          :muokattava? (constantly true)
+          }
+         {:otsikko "Tierekisteriosoite"
+          :nimi :tr-osoite
+          :pakollinen? true
+          :tyyppi :tierekisteriosoite
+          :ala-nayta-virhetta-komponentissa? true
+          :validoi [[:validi-tr "Reittiä ei saada tehtyä" [:sijainti]]]
+          :sijainti (r/wrap (:sijainti ilmoituksen-tiedot)
+                            #(log "laitettas sijainti" (pr-str %)))
+          }
+
+
+         {:otsikko "Tien nimi" :nimi :tien_nimi
+          :tyyppi :string
+          }
+         {:otsikko "Kunta/kunnat" :nimi :kunnat
+          :tyyppi :string
+          }
+
+         {:otsikko "Työn alkupiste (osoite, paikannimi)" :nimi :alkusijainnin_kuvaus
+          :tyyppi :string
+          }
+         {:otsikko "Työn aloituspvm" :nimi :alku :tyyppi :pvm}
+         {:otsikko "Työn loppupiste (osoite, paikannimi)" :nimi :loppusijainnin_kuvaus
+          :tyyppi :string
+          }
+         {:otsikko "Työn lopetuspvm" :nimi :loppu :tyyppi :pvm}
+
+         {:otsikko "Työn pituus" :nimi :tyon-pituus
+          :tyyppi :positiivinen-numero
+          :hae #(pvm-vali-paivina (:alku %) (:loppu %))}
+
+         )
+
+        (lomake/ryhma "Työvaihe"
+                      )
+        (lomake/ryhma "Työn tyyppi"
+                      )
+        (lomake/ryhma "Työaika"
+                      )
+        (lomake/ryhma "Vaikutukset liikenteelle"
+                      )
+        (lomake/ryhma "Vaikutussuunta"
+                      )
+        (lomake/ryhma "Muuta"
+                      )]
        ilmoitus]
       #_[:div
        [bs/panel {}
