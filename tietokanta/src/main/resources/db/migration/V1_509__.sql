dd4915f7ea7c8b022a930d709822ec25e4ca7b9d
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-03-08 07:14:36 +0200

    Lisää tiemerkinnän toteumille luoja ja ulkoinen id

--- tietokanta/src/main/resources/db/migration/V1_509__.sql
+++ tietokanta/src/main/resources/db/migration/V1_509__.sql
@@ -2,3 +2,11 @@ INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'kirjaa-tiemerkintato
 INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'poista-tiemerkintatoteuma');
 INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'kirjaa-yllapitokohteen-tiemerkintatoteuma');
 INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'poista-yllapitokohteen-tiemerkintatoteuma');
+
+ALTER TABLE tiemerkinnan_yksikkohintainen_toteuma
+  ADD COLUMN ulkoinen_id INTEGER;
+ALTER TABLE tiemerkinnan_yksikkohintainen_toteuma
+  ADD COLUMN luoja INTEGER;
+
+ALTER TABLE tiemerkinnan_yksikkohintainen_toteuma
+  ADD CONSTRAINT uniikki_tiemerkinnan_yksikkohintainen_toteuma_luojan_mukaan UNIQUE (ulkoinen_id, luoja);
