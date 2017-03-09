1d80a16beccf9c12fe667f5ee6eebc94e15d4dd9
Jari Hanhela <jarihanhela@gmail.com>
2017-03-09 15:35:08 +0200

    Merge branch 'develop' of https://github.com/finnishtransportagency/harja into HAR-4707_toimenpide_sarake
    # Conflicts:
    #	resources/api/documentation/harja-api-versiohistoria.md
    #	tietokanta/src/main/resources/db/migration/V1_509__.sql

--- tietokanta/src/main/resources/db/migration/V1_511__.sql
+++ tietokanta/src/main/resources/db/migration/V1_511__.sql
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
 -- Ylläpitokohdeosalle tarkemmin eritellyt toimenpiteet
 ALTER TABLE yllapitokohdeosa ADD COLUMN paallystetyyppi INT; -- ks. koodisto +paallystetyypit+
 ALTER TABLE yllapitokohdeosa ADD COLUMN raekoko INT;
 ALTER TABLE yllapitokohdeosa ADD COLUMN tyomenetelma INT; -- ks. koodisto +tyomenetelmat+
-ALTER TABLE yllapitokohdeosa ADD COLUMN massamaara NUMERIC(10, 2);
-=======
-INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'kirjaa-tiemerkintatoteuma');
-INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'poista-tiemerkintatoteuma');
-INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'kirjaa-yllapitokohteen-tiemerkintatoteuma');
-INSERT INTO integraatio (jarjestelma, nimi) VALUES ('api', 'poista-yllapitokohteen-tiemerkintatoteuma');
-
-ALTER TABLE tiemerkinnan_yksikkohintainen_toteuma
-  ADD COLUMN ulkoinen_id INTEGER;
-ALTER TABLE tiemerkinnan_yksikkohintainen_toteuma
-  ADD COLUMN luoja INTEGER;
-
-ALTER TABLE tiemerkinnan_yksikkohintainen_toteuma
-  ADD CONSTRAINT uniikki_tiemerkinnan_yksikkohintainen_toteuma_luojan_mukaan UNIQUE (ulkoinen_id, luoja);
->>>>>>> ea6e7e9805bc6edd2a72a6ea6edd61d097d38c22
+ALTER TABLE yllapitokohdeosa ADD COLUMN massamaara NUMERIC(10, 2)
