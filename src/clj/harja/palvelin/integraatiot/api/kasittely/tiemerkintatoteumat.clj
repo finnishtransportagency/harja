14696476940abbf754a990749e302d4afdf9f796
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-03-09 13:00:35 +0200

    Aja tiemerkintätoteumien tallennus transaktiossa

--- src/clj/harja/palvelin/integraatiot/api/kasittely/tiemerkintatoteumat.clj
+++ src/clj/harja/palvelin/integraatiot/api/kasittely/tiemerkintatoteumat.clj
@@ -8,37 +8,39 @@
             [harja.id :as id]
             [harja.domain.tiemerkinta-toteumat :as d]
             [harja.palvelin.integraatiot.api.tyokalut.json :as json]
-            [harja.kyselyt.konversio :as konversio])
+            [harja.kyselyt.konversio :as konversio]
+            [clojure.java.jdbc :as jdbc])
   (:use [slingshot.slingshot :only [throw+ try+]]))
 
 (defn luo-tai-paivita-tiemerkintatoteumat
   ([db kayttaja urakka-id tiemerkintatoteumat]
    (luo-tai-paivita-tiemerkintatoteumat db kayttaja urakka-id nil tiemerkintatoteumat))
   ([db kayttaja urakka-id yllapitokohde-id tiemerkintatoteumat]
-   (doseq [{{:keys [tunniste hinta muutospvm selite tienumero pituus yllapitoluokka hintatyyppi] :as toteuma}
-            :tiemerkintatoteuma}
-           tiemerkintatoteumat]
-     (let [ulkoinen-id (:id tunniste)
-           luoja-id (:id kayttaja)
-           id (yllapitototeuma-q/hae-tiemerkintatoteuman-id-ulkoisella-idlla db luoja-id ulkoinen-id)
-           yllapitokohde (when yllapitokohde-id (first (yllapitokohteet-q/hae-yllapitokohde db {:id yllapitokohde-id})))
-           hinta-kohteelle (when yllapitokohde-id (d/maarittele-hinnan-kohde yllapitokohde))
-           muutospvm (json/aika-string->java-sql-date muutospvm)
-           sql-parametrit {:yllapitokohde yllapitokohde-id
-                           :hinta hinta
-                           :hintatyyppi hintatyyppi
-                           :muutospvm muutospvm
-                           :hinta_kohteelle (when yllapitokohde-id hinta-kohteelle)
-                           :selite selite
-                           :tr_numero (when-not yllapitokohde-id tienumero)
-                           :yllapitoluokka (when-not yllapitokohde-id yllapitoluokka)
-                           :pituus (when-not yllapitokohde-id pituus)
-                           :ulkoinen_id ulkoinen-id
-                           :luoja luoja-id}]
-       (if (id/id-olemassa? id)
-         (yllapitototeuma-q/paivita-tiemerkintaurakan-yksikkohintainen-tyo<!
-           db (merge sql-parametrit {:id id :urakka urakka-id :poistettu false}))
-         (yllapitototeuma-q/luo-tiemerkintaurakan-yksikkohintainen-tyo<! db (merge sql-parametrit {:urakka urakka-id})))))))
+   (jdbc/with-db-transaction [db db]
+     (doseq [{{:keys [tunniste hinta muutospvm selite tienumero pituus yllapitoluokka hintatyyppi] :as toteuma}
+              :tiemerkintatoteuma}
+             tiemerkintatoteumat]
+       (let [ulkoinen-id (:id tunniste)
+             luoja-id (:id kayttaja)
+             id (yllapitototeuma-q/hae-tiemerkintatoteuman-id-ulkoisella-idlla db luoja-id ulkoinen-id)
+             yllapitokohde (when yllapitokohde-id (first (yllapitokohteet-q/hae-yllapitokohde db {:id yllapitokohde-id})))
+             hinta-kohteelle (when yllapitokohde-id (d/maarittele-hinnan-kohde yllapitokohde))
+             muutospvm (json/aika-string->java-sql-date muutospvm)
+             sql-parametrit {:yllapitokohde yllapitokohde-id
+                             :hinta hinta
+                             :hintatyyppi hintatyyppi
+                             :muutospvm muutospvm
+                             :hinta_kohteelle (when yllapitokohde-id hinta-kohteelle)
+                             :selite selite
+                             :tr_numero (when-not yllapitokohde-id tienumero)
+                             :yllapitoluokka (when-not yllapitokohde-id yllapitoluokka)
+                             :pituus (when-not yllapitokohde-id pituus)
+                             :ulkoinen_id ulkoinen-id
+                             :luoja luoja-id}]
+         (if (id/id-olemassa? id)
+           (yllapitototeuma-q/paivita-tiemerkintaurakan-yksikkohintainen-tyo<!
+             db (merge sql-parametrit {:id id :urakka urakka-id :poistettu false}))
+           (yllapitototeuma-q/luo-tiemerkintaurakan-yksikkohintainen-tyo<! db (merge sql-parametrit {:urakka urakka-id}))))))))
 
 (defn poista-tiemerkintatoteumat
   ([db kayttaja urakka-id tiemerkintatoteumat] (poista-tiemerkintatoteumat db kayttaja urakka-id nil tiemerkintatoteumat))
