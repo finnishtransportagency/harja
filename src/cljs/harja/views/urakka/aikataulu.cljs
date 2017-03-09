9f66c4854788cc0c359199697de47da377f383fc
Jari Hanhela <jarihanhela@gmail.com>
2017-03-09 10:33:34 +0200

    Korjaa syntaksivirhe

--- src/cljs/harja/views/urakka/aikataulu.cljs
+++ src/cljs/harja/views/urakka/aikataulu.cljs
@@ -276,7 +276,7 @@
             :fmt pvm/pvm-opt
             :muokattava? (fn [rivi]
                            (and (= (:nakyma optiot) :tiemerkinta)
-                                (:aikataulu-tiemerkinta-alku %)
+                                (:aikataulu-tiemerkinta-alku rivi)
                                 saa-merkita-valmiiksi?
                                 (:valmis-tiemerkintaan rivi)))
             :validoi [[:toinen-arvo-annettu-ensin :aikataulu-tiemerkinta-alku
