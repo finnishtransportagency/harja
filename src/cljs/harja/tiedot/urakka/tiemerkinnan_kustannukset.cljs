48cc894ae05f36afcac869a9b529009902277787
Jari Hanhela <jarihanhela@gmail.com>
2017-03-09 08:52:38 +0200

    Luo pohja tiemerkinnän kustannusyhteenvedon upotetulle raportille

---
+++ src/cljs/harja/tiedot/urakka/tiemerkinnan_kustannukset.cljs
@@ -0,0 +1,26 @@
+(ns harja.tiedot.urakka.tiemerkinnan-kustannukset
+  "Tiemerkintäurakan Kustannukset-välilehden tiedot"
+  (:require [reagent.core :refer [atom] :as r]
+            [cljs.core.async :refer [<! >! chan]]
+            [harja.ui.raportti :refer [muodosta-html]]
+            [harja.asiakas.kommunikaatio :as k]
+            [harja.transit :as t]
+            [harja.tiedot.navigaatio :as nav]
+            [harja.tiedot.urakka :as u]
+            [harja.tiedot.raportit :as raportit])
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [reagent.ratom :refer [reaction run!]]
+                   [harja.atom :refer [reaction<!]]))
+
+(defonce kustannukset-valilehti-nakyvissa? (atom false))
+
+(defonce raportin-parametrit
+  (reaction (let [ur @nav/valittu-urakka
+                  [alkupvm loppupvm] @u/valittu-hoitokauden-kuukausi
+                  nakymassa? @kustannukset-valilehti-nakyvissa?]
+              (when (and ur alkupvm loppupvm nakymassa?)
+                (raportit/urakkaraportin-parametrit
+                  (:id ur)
+                  :tiemerkinnan-kustannusyhteenveto
+                  {:alkupvm  alkupvm
+                   :loppupvm loppupvm})))))
