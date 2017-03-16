e46c1358ab1793509242dd047e2ecd1a6408923d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-03-16 11:46:51 +0200

    Hyödynnä speciä testissä

--- test/clj/harja/palvelin/palvelut/tietyoilmoitukset_test.clj
+++ test/clj/harja/palvelin/palvelut/tietyoilmoitukset_test.clj
@@ -7,7 +7,8 @@
             [harja.testi :refer :all]
             [com.stuartsierra.component :as component]
             [harja.jms-test :refer [feikki-sonja]]
-            [harja.palvelin.integraatiot.tloik.tyokalut :refer :all]))
+            [harja.palvelin.integraatiot.tloik.tyokalut :refer :all]
+            [harja.palvelin.komponentit.pdf-vienti :as pdf-vienti]))
 
 (defn jarjestelma-fixture [testit]
   (alter-var-root #'jarjestelma
@@ -16,9 +17,12 @@
                       (component/system-map
                         :db (tietokanta/luo-tietokanta testitietokanta)
                         :http-palvelin (testi-http-palvelin)
+                        :pdf-vienti (component/using
+                                     (pdf-vienti/luo-pdf-vienti)
+                                     [:http-palvelin])
                         :tietyoilmoitukset (component/using
                                              (tietyoilmoitukset/->Tietyoilmoitukset)
-                                             [:http-palvelin :db])))))
+                                             [:http-palvelin :db :pdf-vienti])))))
 
   (testit)
   (alter-var-root #'jarjestelma component/stop))
@@ -29,80 +33,13 @@
                       urakkatieto-fixture))
 
 (deftest hae-ilmoitukset-sarakkeet
-  (let [sarakkeet [:alkusijainnin_kuvaus
-                   :ajoneuvo_max_korkeus
-                   :tyotyypit
-                   :liikenteenohjaus
-                   :sijainti
-                   :ilmoittaja_matkapuhelin
-                   :loppusijainnin_kuvaus
-                   :urakoitsijan_nimi
-                   :kiertotienpinnat
-                   :liikenteenohjaaja
-                   :tilaajayhteyshenkilo_sukunimi
-                   :tilaajayhteyshenkilo_etunimi
-                   :urakoitsijayhteyshenkilo_sukunimi
-                   :ajoneuvo_max_paino
-                   :lisatietoja
-                   :luotu
-                   :tilaajayhteyshenkilo_sahkoposti
-                   :ilmoittaja_sukunimi
-                   :tilaajan_nimi
-                   :ajoneuvo_max_pituus
-                   :ajoittaiset_pysatykset
-                   :urakka_nimi
-                   :luvan_diaarinumero
-                   :urakoitsijayhteyshenkilo
-                   :ilmoittaja_sahkoposti
-                   :urakoitsijayhteyshenkilo_matkapuhelin
-                   :tloik_paatietyoilmoitus_id
-                   :tilaajayhteyshenkilo_matkapuhelin
-                   :tloik_id
-                   :tyovaiheet
-                   :pysaytysten_loppu
-                   :luoja
-                   :urakka
-                   :kaistajarjestelyt
-                   :tien_nimi
-                   :paatietyoilmoitus
-                   :pysaytysten_alku
-                   :nopeusrajoitukset
-                   :tr_loppuosa
-                   :ilmoittaja_etunimi
-                   :tyoajat
-                   :muokkaaja
-                   :vaikutussuunta
-                   :huomautukset
-                   :viivastys_normaali_liikenteessa
-                   :tr_numero
-                   :viivastys_ruuhka_aikana
-                   :id
-                   :poistettu
-                   :kiertotien_mutkaisuus
-                   :ajoneuvo_max_leveys
-                   :kunnat
-                   :urakoitsijayhteyshenkilo_etunimi
-                   :tienpinnat
-                   :tr_loppuetaisyys
-                   :urakoitsija
-                   :tr_alkuetaisyys
-                   :poistaja
-                   :alku
-                   :ajoittain_suljettu_tie
-                   :tilaajayhteyshenkilo
-                   :loppu
-                   :muokattu
-                   :tr_alkuosa
-                   :urakkatyyppi
-                   :ilmoittaja
-                   :urakoitsijayhteyshenkilo_sahkoposti
-                   :tilaaja]
-        parametrit {:alkuaika (pvm/luo-pvm 2016 1 1)
+  (let [parametrit {:alkuaika (pvm/luo-pvm 2016 1 1)
                     :loppuaika (pvm/luo-pvm 2017 3 1)
                     :urakka nil
                     :sijainti nil
                     :vain-kayttajan-luomat nil}]
-    (is (oikeat-sarakkeet-palvelussa? sarakkeet :hae-tietyoilmoitukset parametrit))))
+    (is (= 1 (count (kutsu-palvelua (:http-palvelin jarjestelma)
+                                    :hae-tietyoilmoitukset +kayttaja-jvh+ parametrit))))))
 
 (deftest hae-ilmoituksia
   (let [parametrit {:alkuaika (pvm/luo-pvm 2016 1 1)
@@ -116,4 +53,3 @@
                                           parametrit)]
     (is (= 1 (count tietyoilmoitukset)) "Ilmoituksia on palautunut oikea määrä")
     (is (= 1 (count (:tyovaiheet (first tietyoilmoitukset)))) "Ilmoituksella on työvaiheita oikea määrä")))
-
