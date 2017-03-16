14d70d2ad97cb5ac716179563e6f63aa66ed6a91
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-03-16 14:21:36 +0200

    Refaktoroi tietyöilmoitusnäkymä käyttämään specattuja avaimia

--- src/clj/harja/kyselyt/specql.clj
+++ src/clj/harja/kyselyt/specql.clj
@@ -3,9 +3,13 @@
   (:require [specql.core :refer [define-tables]]
             [specql.data-types :as d]
             [harja.domain.tietyoilmoitukset :as t]
+            [harja.domain.tierekisteri :as tr]
             [clojure.spec :as s]
             [clojure.future :refer :all]))
 
 (def db {:connection-uri "jdbc:postgresql://localhost/harjatest_template?user=postgres"})
 
 (s/def ::d/geometry any?)
+
+(define-tables db
+  ["tr_osoite" ::tr/osoite])
