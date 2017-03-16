1b9607e3c99fb501cb5c64a43bf325f2a9ad29c0
Jari Hanhela <jarihanhela@gmail.com>
2017-03-16 12:25:43 +0200

    Lisää kantaconstraint: ylläpidon sakon oltava positiivinen ja bonuksen negatiivinen

--- tietokanta/src/main/resources/db/migration/V1_512__.sql
+++ tietokanta/src/main/resources/db/migration/V1_512__.sql
@@ -9,3 +9,9 @@ INSERT INTO raportti (nimi, kuvaus, konteksti, parametrit, koodi, urakkatyyppi) 
 
 -- Tiemerkinnän "muutospvm" on jatkossa vain pvm
 ALTER TABLE tiemerkinnan_yksikkohintainen_toteuma RENAME COLUMN muutospvm TO paivamaara;
+
+-- Varmistetaan, että ylläpidon sanktio on aina 0 tai positiivinen ja bonus on aina 0 tai negatiivinen
+ALTER TABLE sanktio ADD CONSTRAINT yllapidon_sakko_positiivinen
+CHECK (sakkoryhma != 'yllapidon_sakko' OR (sakkoryhma = 'yllapidon_sakko' AND maara >= 0));
+ALTER TABLE sanktio ADD CONSTRAINT yllapidon_bonus_negatiivinen
+CHECK (sakkoryhma != 'yllapidon_bonus' OR (sakkoryhma = 'yllapidon_bonus' AND maara <= 0));
