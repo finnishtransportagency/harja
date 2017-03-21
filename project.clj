7b9a015e6104d150086d0dc5aeb658c689dacaa1
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-03-21 14:37:19 +0200

    Päivitä dep

--- project.clj
+++ project.clj
@@ -49,7 +49,7 @@
                  ;;[org.postgis/postgis-jdbc "2.1.4dev"] ;; mvnrepossa vain 1.3.3 versio, piti buildata itse!
                  [com.mchange/c3p0 "0.9.5.2"]
                  [webjure/jeesql "0.4.6"]
-                 [specql "0.4.9"]
+                 [specql "0.4.10"]
 
                  ;; GeoTools
                  [org.geotools/gt-shapefile "16.1"]
