3b262f169e29335a59a0ddb5bd03ba0026b57c1d
Jari Hanhela <jarihanhela@gmail.com>
2017-03-30 11:14:13 +0300

    Luo pohja tuck-tilalle

--- src/cljs/harja/tiedot/urakka/paallystyksen_maksuerat.cljs
+++ src/cljs/harja/tiedot/urakka/paallystyksen_maksuerat.cljs
@@ -1,5 +1,27 @@
 (ns harja.tiedot.urakka.paallystyksen-maksuerat
   "Päällystysurakan maksuerät"
-  (:require [reagent.core :refer [atom] :as r]))
+  (:require [reagent.core :refer [atom] :as r]
+            [harja.tiedot.navigaatio :as nav]
+            [harja.tiedot.urakka :as u])
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [reagent.ratom :refer [reaction]]))
 
+;; Tila
+(def muut-tyot (atom {:valinnat {:urakka nil
+                                 :sopimus nil
+                                 :vuosi nil
+                                 :tienumero nil
+                                 :kohdenumero nil}
+                      :maksuerat nil}))
 
+(defonce valinnat
+  (reaction
+    {:urakka (:id @nav/valittu-urakka)
+     :sopimus (first @u/valittu-sopimusnumero)
+     :vuosi nil ;; TODO HAE NÄMÄ
+     :tienumero nil
+     :kohdenumero nil))
+
+(defrecord YhdistaValinnat [valinnat])
+(defrecord MaksueratHaettu [tulokset])
+(defrecord MaksueratTallennettu [vastaus])
