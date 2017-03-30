339805f130a8d677900f351b66b7afb9ef8d0f00
Jari Hanhela <jarihanhela@gmail.com>
2017-03-30 10:29:10 +0300

    Viimeistele ylläpitokohteen maksuerien migraatio

--- tietokanta/src/main/resources/db/migration/V1_522__.sql
+++ tietokanta/src/main/resources/db/migration/V1_522__.sql
@@ -1,5 +1,7 @@
 -- Uusi taulu ylläpitokohteen maksuerille
-CREATE TABLE (
+CREATE TABLE yllapitokohteen_maksuerat (
   id SERIAL PRIMARY KEY,
-  yllapitokohde INTEGER REFERENCES yllapitokohde (id)
+  yllapitokohde INTEGER REFERENCES yllapitokohde (id) NOT NULL UNIQUE,
+  maksuerat VARCHAR(512)[],
+  maksueratunnus VARCHAR (512)
 )
