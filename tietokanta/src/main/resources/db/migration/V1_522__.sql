b2218c8271b314dff1eb67c7ee291e1439de7633
Jari Hanhela <jarihanhela@gmail.com>
2017-04-03 10:23:02 +0300

    Luo oma taulu ylläpitokohteen maksuerätunnukselle

--- tietokanta/src/main/resources/db/migration/V1_522__.sql
+++ tietokanta/src/main/resources/db/migration/V1_522__.sql
@@ -1,4 +1,5 @@
--- Uusi taulu ylläpitokohteen maksuerille
+-- Ylläpitokohteen maksuerät
+
 CREATE TABLE yllapitokohteen_maksuera (
   id SERIAL PRIMARY KEY,
   yllapitokohde INTEGER REFERENCES yllapitokohde (id) NOT NULL,
@@ -9,3 +10,10 @@ CREATE TABLE yllapitokohteen_maksuera (
 ALTER TABLE yllapitokohteen_maksuera ADD CONSTRAINT uniikki_maksueranumero UNIQUE (yllapitokohde, maksueranumero);
 ALTER TABLE yllapitokohteen_maksuera ADD CONSTRAINT validi_maksueranumero
 CHECK (maksueranumero >= 1 AND maksueranumero <= 5);
+
+CREATE TABLE yllapitokohteen_maksueratunnus (
+  id SERIAL PRIMARY KEY,
+  yllapitokohde INTEGER REFERENCES yllapitokohde (id) NOT NULL UNIQUE,
+  maksueratunnus VARCHAR(512)
+);
+
