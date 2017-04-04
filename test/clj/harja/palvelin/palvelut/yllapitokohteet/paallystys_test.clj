2614b851a3071188ff32b90802c56fa06d31f1a1
Jari Hanhela <jarihanhela@gmail.com>
2017-04-04 10:11:27 +0300

    Lisää puuttuvat requiret

--- test/clj/harja/palvelin/palvelut/yllapitokohteet/paallystys_test.clj
+++ test/clj/harja/palvelin/palvelut/yllapitokohteet/paallystys_test.clj
@@ -7,6 +7,8 @@
             [com.stuartsierra.component :as component]
             [harja.kyselyt.konversio :as konv]
             [cheshire.core :as cheshire]
+            [harja.domain.urakka :as urakka-domain]
+            [harja.domain.sopimus :as sopimus-domain]
             [harja.palvelin.palvelut.yllapitokohteet-test :as yllapitokohteet-test]
             [harja.pvm :as pvm]
             [harja.domain.paallystysilmoitus :as paallystysilmoitus-domain]
