213712beaccc3e4ceb17acb9d6fa91b8a1cd1e84
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-04-06 20:07:14 +0300

    Lisää alustava näkymä vesiväyläurakoitsijoiden luomiselle

---
+++ src/cljs/harja/tiedot/vesivaylat/urakoitsijan_luonti.cljs
@@ -0,0 +1,103 @@
+(ns harja.tiedot.vesivaylat.urakoitsijan-luonti
+  (:require [tuck.core :as tuck]
+            [reagent.core :refer [atom]]
+            [harja.asiakas.kommunikaatio :as k]
+            [harja.ui.viesti :as viesti]
+            [cljs.core.async :as async])
+  (:require-macros [reagent.ratom :refer [reaction run!]]
+                   [cljs.core.async.macros :refer [go]]))
+
+(def uusi-urakoitsija {})
+
+(defonce tila
+  (atom {:nakymassa? false
+         :valittu-urakoitsija nil
+         :tallennus-kaynnissa? false
+         :urakoitsijoiden-haku-kaynnissa? false
+         :haetut-urakoitsijat nil}))
+
+(defrecord ValitseUrakoitsija [urakoitsija])
+(defrecord Nakymassa? [nakymassa?])
+(defrecord UusiUrakoitsija [])
+(defrecord TallennaUrakoitsija [urakoitsija])
+(defrecord UrakoitsijaTallennettu [urakoitsija])
+(defrecord UrakoitsijaEiTallennettu [virhe])
+(defrecord UrakoitsijaaMuokattu [urakoitsija])
+(defrecord HaeUrakoitsijat [])
+(defrecord UrakoitsijatHaettu [urakoitsijat])
+(defrecord UrakoitsijatEiHaettu [virhe])
+
+(extend-protocol tuck/Event
+  ValitseUrakoitsija
+  (process-event [{urakoitsija :urakoitsija} app]
+    (assoc app :valittu-urakoitsija urakoitsija))
+
+  Nakymassa?
+  (process-event [{nakymassa? :nakymassa?} app]
+    (assoc app :nakymassa? nakymassa?))
+
+  UusiUrakoitsija
+  (process-event [_ app]
+    (assoc app :valittu-urakoitsija uusi-urakoitsija))
+
+  TallennaUrakoitsija
+  (process-event [{urakoitsija :urakoitsija} app]
+    (assert (some? (:haetut-urakoitsijat app)) "Urakoitsija ei voi yrittää tallentaa, ennen kuin urakoitsijoiden haku on valmis.")
+    (let [tulos! (tuck/send-async! ->UrakoitsijaTallennettu)
+          fail! (tuck/send-async! ->UrakoitsijaEiTallennettu)]
+      (go
+        (try
+          (let [vastaus urakoitsija] ;;TODO lisää tallennus
+            (if (k/virhe? vastaus)
+              (fail! vastaus)
+              (tulos! vastaus)))
+          (catch :default e
+            (fail! nil)
+            (throw e)))))
+    (assoc app :tallennus-kaynnissa? true))
+
+  UrakoitsijaTallennettu
+  (process-event [{urakoitsija :urakoitsija} app]
+    (viesti/nayta! "Urakoitsija tallennettu!")
+    (let [vanhat (group-by :id (:haetut-urakoitsijat app))
+          uusi {(:id urakoitsija) [urakoitsija]}]
+      ;; Yhdistetään tallennettu jo haettuihin.
+      ;; Gridiin tultaessa Grid hakee vielä taustalla kaikki urakoitsijat
+      (assoc app :haetut-urakoitsijat (vec (apply concat (vals (merge vanhat uusi))))
+                 :tallennus-kaynnissa? false
+                 :valittu-urakoitsija nil)))
+
+  UrakoitsijaEiTallennettu
+  (process-event [{virhe :virhe} app]
+    (viesti/nayta! [:span "Virhe tallennuksessa! Urakoitsijaa ei tallennettu."] :danger)
+    (assoc app :tallennus-kaynnissa? false
+               :valittu-urakoitsija nil))
+
+  UrakoitsijaaMuokattu
+  (process-event [{urakoitsija :urakoitsija} app]
+    (assoc app :valittu-urakoitsija urakoitsija))
+
+  HaeUrakoitsijat
+  (process-event [_ app]
+    (let [tulos! (tuck/send-async! ->UrakoitsijatHaettu)
+          fail! (tuck/send-async! ->UrakoitsijatEiHaettu)]
+      (go
+        (try
+          (let [vastaus [{:nimi "Kalle" :id 1}] #_(async/<! (k/post! :hae-harjassa-luodut-urakoitsijat {}))] ;;FIXME toteuta palvelu
+            (if (k/virhe? vastaus)
+              (fail! vastaus)
+              (tulos! vastaus)))
+          (catch :default e
+            (fail! nil)
+            (throw e)))))
+    (assoc app :urakoitsijoiden-haku-kaynnissa? true))
+
+  UrakoitsijatHaettu
+  (process-event [{urakoitsijat :urakoitsijat} app]
+    (assoc app :haetut-urakoitsijat urakoitsijat
+               :urakoitsijoiden-haku-kaynnissa? false))
+
+  UrakoitsijatEiHaettu
+  (process-event [_ app]
+    (viesti/nayta! [:span "Virhe urakoitsijoiden haussa!"] :danger)
+    (assoc app :urakoitsijoiden-haku-kaynnissa? false)))
---
+++ src/cljs/harja/views/vesivaylat/urakoitsijan_luonti.cljs
@@ -0,0 +1,66 @@
+(ns harja.views.vesivaylat.urakoitsijan-luonti
+  (:require [harja.ui.komponentti :as komp]
+            [tuck.core :refer [tuck]]
+            [harja.tiedot.vesivaylat.urakoitsijan-luonti :as tiedot]
+            [harja.ui.napit :as napit]
+            [harja.ui.yleiset :refer [ajax-loader ajax-loader-pieni]]
+            [harja.ui.grid :as grid]
+            [harja.pvm :as pvm]
+            [harja.ui.lomake :as lomake]
+            [harja.ui.ikonit :as ikonit]))
+
+(defn luontilomake [e! {:keys [valittu-urakoitsija tallennus-kaynnissa?] :as app}]
+  [:div
+   [napit/takaisin "Takaisin luetteloon"
+    #(e! (tiedot/->ValitseUrakoitsija nil))
+    {:disabled tallennus-kaynnissa?}]
+   [harja.ui.debug/debug valittu-urakoitsija]
+   [lomake/lomake
+    {:otsikko (if (:id valittu-urakoitsija)
+                "Muokkaa urakoitsijaa"
+                "Luo uusi urakoitsija")
+     :muokkaa! #(e! (tiedot/->UrakoitsijaaMuokattu (lomake/ilman-lomaketietoja %)))
+     :voi-muokata? (constantly true) #_(oikeudet/voi-kirjoittaa? oikeudet/hallinta-vesivaylaurakoiden-luonti)
+     :footer-fn (fn [hanke]
+                  [napit/tallenna
+                   "Tallenna urakoitsija"
+                   #(e! (tiedot/->TallennaUrakoitsija (lomake/ilman-lomaketietoja hanke)))
+                   {:ikoni (ikonit/tallenna)
+                    :disabled (or tallennus-kaynnissa?
+                                  (not (lomake/voi-tallentaa? hanke)))
+                    :tallennus-kaynnissa? tallennus-kaynnissa?}])}
+    [{:otsikko "Nimi" :nimi :nimi :tyyppi :string}]
+    valittu-urakoitsija]])
+
+(defn urakoitsijagrid [e! app]
+  (komp/luo
+    (komp/sisaan #(e! (tiedot/->HaeUrakoitsijat)))
+    (fn [e! {:keys [haetut-urakoitsijat urakoitsijoiden-haku-kaynnissa?] :as app}]
+      [:div
+       [napit/uusi "Lisää urakoitsija"
+        #(e! (tiedot/->UusiUrakoitsija))
+        {:disabled (nil? haetut-urakoitsijat)}]
+       [grid/grid
+        {:otsikko (if (and (some? haetut-urakoitsijat) urakoitsijoiden-haku-kaynnissa?)
+                    [ajax-loader-pieni "Päivitetään listaa"] ;; Listassa on jo jotain, mutta sitä päivitetään
+                    "Harjaan perustetut vesiväyläurakoitsijat")
+         :tunniste :id
+         :tyhja (if (nil? haetut-urakoitsijat)
+                  [ajax-loader "Haetaan urakoitsijoita"]
+                  "Urakoitsijoita ei löytynyt")
+         :rivi-klikattu #(e! (tiedot/->ValitseUrakoitsija %))}
+        [{:otsikko "Nimi" :nimi :nimi}]
+        haetut-urakoitsijat]])))
+
+(defn vesivaylaurakoitsijoiden-luonti* [e! app]
+  (komp/luo
+    (komp/sisaan-ulos #(e! (tiedot/->Nakymassa? true))
+                      #(e! (tiedot/->Nakymassa? false)))
+
+    (fn [e! {valittu-urakoitsija :valittu-urakoitsija :as app}]
+      (if valittu-urakoitsija
+        [luontilomake e! app]
+        [urakoitsijagrid e! app]))))
+
+(defn vesivaylaurakoitsijoiden-luonti []
+  [tuck tiedot/tila vesivaylaurakoitsijoiden-luonti*])
