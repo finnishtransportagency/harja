66b713920b3827bd60c889463aeada2ec1a279fd
Jari Hanhela <jarihanhela@gmail.com>
2017-04-10 15:21:02 +0300

    Hae kohteen id oikeasta avaimesta

--- src/clj/harja/kyselyt/yllapitokohteet.clj
+++ src/clj/harja/kyselyt/yllapitokohteet.clj
@@ -1,19 +1,20 @@
 (ns harja.kyselyt.yllapitokohteet
   (:require [jeesql.core :refer [defqueries]]
+            [taoensso.timbre :as log]
             [harja.geo :as geo]))
 
 (defqueries "harja/kyselyt/yllapitokohteet.sql")
 
 (def kohdeosa-xf (geo/muunna-pg-tulokset :sijainti))
 
-(defn liita-kohdeosat-kohteisiin [db kohteet id-avain]
+(defn liita-kohdeosat-kohteisiin [db kohteet kohde-id-avain]
   (let [kohdeosat (into []
                         kohdeosa-xf
                         (hae-urakan-yllapitokohteiden-yllapitokohdeosat
-                          db {:idt (map id-avain kohteet)}))]
+                          db {:idt (map kohde-id-avain kohteet)}))]
     (mapv
       (fn [kohde]
-        (let [kohteen-kohdeosat (filterv #(= (:yllapitokohde-id %) (id-avain kohteet)) kohdeosat)]
+        (let [kohteen-kohdeosat (filterv #(= (:yllapitokohde-id %) (kohde-id-avain kohde)) kohdeosat)]
           (assoc kohde :kohdeosat kohteen-kohdeosat)))
       kohteet)))
 
