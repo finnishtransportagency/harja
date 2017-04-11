bc13b914c11af0acfa7d95b6cf38ffbd3f2e8a7f
Jari Hanhela <jarihanhela@gmail.com>
2017-04-11 11:51:23 +0300

    Korjaa virhe migraatiossa; pakotti kaikki tyypit muuksi.

--- tietokanta/src/main/resources/db/migration/V1_527__.sql
+++ tietokanta/src/main/resources/db/migration/V1_527__.sql
@@ -1,3 +1,2 @@
 -- Ylläpidon muun toteuman hinnan etumerkin tarkistus
-ALTER TABLE yllapito_muu_toteuma ADD CONSTRAINT ennuste_tai_maara CHECK
-(toteutunut_maara IS NOT NULL OR ennustettu_maara IS NOT NULL);
+ALTER TABLE yllapito_muu_toteuma ADD CONSTRAINT ei_negatiivinen_hinta CHECK ((tyyppi = 'muu' AND hinta >= 0) OR tyyppi != 'muu');
