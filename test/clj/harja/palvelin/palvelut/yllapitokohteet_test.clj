51d3d50842c7b73dd285870b2b5c5759fd3cc6f9
Jari Hanhela <jarihanhela@gmail.com>
2017-04-12 16:23:50 +0300

    Korjaa testi

--- test/clj/harja/palvelin/palvelut/yllapitokohteet_test.clj
+++ test/clj/harja/palvelin/palvelut/yllapitokohteet_test.clj
@@ -382,7 +382,7 @@
         kohde-jolla-ilmoitus (first (filter :paallystysilmoitus-id kohteet-ennen-testia))
         paivitetyt-kohteet (map
                              (fn [kohde] (if (= (:id kohde) (:id kohde-jolla-ilmoitus))
-                                           (assoc kohde :poistettu true)
+                                            (assoc kohde :poistettu true)
                                            kohde))
                              kohteet-ennen-testia)]
 
@@ -398,7 +398,7 @@
                                                  +kayttaja-jvh+ {:urakka-id urakka-id
                                                                  :sopimus-id sopimus-id})]
       (is (= maara-ennen-testia maara-testin-jalkeen))
-      (is (= kohteet-testin-jalkeen kohteet-ennen-testia)))))
+      (is (= (sort-by :id kohteet-ennen-testia) (sort-by :id kohteet-testin-jalkeen))))))
 
 (deftest tallenna-yllapitokohdeosa-kantaan
   (let [yllapitokohde-id (yllapitokohde-id-jolla-on-paallystysilmoitus)]
