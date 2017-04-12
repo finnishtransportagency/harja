4e17972dea570fb4094ec4baae4091aeea73abca
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-04-12 10:18:35 +0300

    Luonnin metatiedot urakoille, sopimuksille, hankkeille, ja organisaatioille

---
+++ tietokanta/src/main/resources/db/migration/V1_532__.sql
@@ -0,0 +1,23 @@
+ALTER TABLE urakka ADD COLUMN luotu timestamp;
+ALTER TABLE urakka ADD COLUMN muokattu timestamp;
+ALTER TABLE urakka ADD COLUMN luoja integer REFERENCES kayttaja (id);
+ALTER TABLE urakka ADD COLUMN muokkaaja integer REFERENCES kayttaja (id);
+ALTER TABLE urakka ADD COLUMN poistettu boolean default false;
+
+ALTER TABLE sopimus ADD COLUMN luotu timestamp;
+ALTER TABLE sopimus ADD COLUMN muokattu timestamp;
+ALTER TABLE sopimus ADD COLUMN luoja integer REFERENCES kayttaja (id);
+ALTER TABLE sopimus ADD COLUMN muokkaaja integer REFERENCES kayttaja (id);
+ALTER TABLE sopimus ADD COLUMN poistettu boolean default false;
+
+ALTER TABLE hanke ADD COLUMN luotu timestamp;
+ALTER TABLE hanke ADD COLUMN muokattu timestamp;
+ALTER TABLE hanke ADD COLUMN luoja integer REFERENCES kayttaja (id);
+ALTER TABLE hanke ADD COLUMN muokkaaja integer REFERENCES kayttaja (id);
+ALTER TABLE hanke ADD COLUMN poistettu boolean default false;
+
+ALTER TABLE organisaatio ADD COLUMN luotu timestamp;
+ALTER TABLE organisaatio ADD COLUMN muokattu timestamp;
+ALTER TABLE organisaatio ADD COLUMN luoja integer REFERENCES kayttaja (id);
+ALTER TABLE organisaatio ADD COLUMN muokkaaja integer REFERENCES kayttaja (id);
+ALTER TABLE organisaatio ADD COLUMN poistettu boolean default false;
