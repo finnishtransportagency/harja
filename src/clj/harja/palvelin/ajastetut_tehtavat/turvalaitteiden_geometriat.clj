0a7c28c80178d5ffe15ff10491ab73cb6b228e36
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-04-18 10:30:00 +0300

    Lisää tarkistus paivityksen ajamiselle

---
+++ src/clj/harja/palvelin/ajastetut_tehtavat/turvalaitteiden_geometriat.clj
@@ -0,0 +1,37 @@
+(ns harja.palvelin.ajastetut-tehtavat.turvalaitteiden-geometriat
+  (:require [taoensso.timbre :as log]
+            [com.stuartsierra.component :as component]
+            [harja.palvelin.tyokalut.ajastettu-tehtava :as ajastettu-tehtava]
+            [harja.kyselyt.geometriapaivitykset :as q-geometriapaivitykset]
+            [harja.pvm :as pvm]
+            [clj-time.coerce :as c]))
+
+(defn- paivitys-tarvitaan? [db paivitysvali-paivissa]
+  (let [viimeisin-paivitys (c/from-sql-time
+                             (:viimeisin_paivitys
+                               (first (q-geometriapaivitykset/hae-paivitys db "turvalaitteet"))))]
+    (or (nil? viimeisin-paivitys)
+        (>= (pvm/paivia-valissa viimeisin-paivitys (pvm/nyt-suomessa)) paivitysvali-paivissa))))
+
+(defn- turvalaitteiden-geometriahakutehtava [integraatioloki db url paivittainen-tarkistusaika paivitysvali-paivissa]
+  (log/debug (format "Ajastetaan turvalaitteiden geometrioiden haku tehtäväksi %s päivän välein osoitteesta: %s."
+                     paivitysvali-paivissa
+                     url))
+
+  (ajastettu-tehtava/ajasta-paivittain
+    paivittainen-tarkistusaika
+    (fn [] (when (paivitys-tarvitaan? db paivitysvali-paivissa)))))
+
+(defrecord TurvalaitteidenGeometriahaku [url tarkistus aika paivittainen-tarkistusaika paivitysvali-paivissa]
+  component/Lifecycle
+  (start [{:keys [integraatioloki db] :as this}]
+    (assoc this :turvalaitteiden-geometriahaku
+                (turvalaitteiden-geometriahakutehtava
+                  integraatioloki
+                  db
+                  url
+                  paivittainen-tarkistusaika
+                  paivitysvali-paivissa)))
+  (stop [this]
+    ((:turvalaitteiden-geometriahaku this))
+    this))
