3461f20a31456a2d33596af3289474b063e22dea
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-04-18 12:08:24 +0300

    Poista turvalaitteet ennen uusien tuontia

--- src/clj/harja/palvelin/ajastetut_tehtavat/turvalaitteiden_geometriat.clj
+++ src/clj/harja/palvelin/ajastetut_tehtavat/turvalaitteiden_geometriat.clj
@@ -6,7 +6,10 @@
             [harja.pvm :as pvm]
             [clj-time.coerce :as c]
             [harja.palvelin.integraatiot.integraatiotapahtuma :as integraatiotapahtuma]
-            [cheshire.core :as cheshire]))
+            [cheshire.core :as cheshire]
+            [clojure.walk :as walk]
+            [clojure.java.jdbc :as jdbc]
+            [harja.kyselyt.turvalaitteet :as q-turvalaitteet]))
 
 (defn paivitys-tarvitaan? [db paivitysvali-paivissa]
   (let [viimeisin-paivitys (c/from-sql-time
@@ -15,22 +18,30 @@
     (or (nil? viimeisin-paivitys)
         (>= (pvm/paivia-valissa viimeisin-paivitys (pvm/nyt-suomessa)) paivitysvali-paivissa))))
 
+(defn tallenna-turvalaite [param1]
+  )
+
 (defn kasittele-vastaus [vastaus]
   (let [data (cheshire/decode vastaus)
         turvalaitteet (get data "features")]
-        
+    (doseq [turvalaite turvalaitteet]
+      (tallenna-turvalaite (walk/keywordize-keys turvalaite)))
     (println "----> " (first turvalaitteet))))
 
 (defn paivita-turvalaitteet [integraatioloki db url]
   (log/debug "Päivitetään turvalaitteiden geometriat")
-  (integraatiotapahtuma/suorita-integraatio
-    db integraatioloki "ptj" "turvalaitteiden-haku"
-    (fn [konteksti]
-      (let [http-asetukset {:metodi :GET
-                            :url url}
-            {body :body headers :headers}
-            (integraatiotapahtuma/laheta konteksti :http http-asetukset)]
-        (kasittele-vastaus body))))
+
+  (jdbc/with-db-transaction [db db]
+    (q-turvalaitteet/poista-turvalaitteet! db)
+
+    (integraatiotapahtuma/suorita-integraatio
+      db integraatioloki "ptj" "turvalaitteiden-haku"
+      (fn [konteksti]
+        (let [http-asetukset {:metodi :GET
+                              :url url}
+              {body :body headers :headers}
+              (integraatiotapahtuma/laheta konteksti :http http-asetukset)]
+          (kasittele-vastaus body)))))
   (log/debug "Turvalaitteidein päivitys tehty"))
 
 (defn- turvalaitteiden-geometriahakutehtava [integraatioloki db url paivittainen-tarkistusaika paivitysvali-paivissa]
