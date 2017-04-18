0dd7af1f5cb57484c47adcd6a22bbf0cf81069b5
Jari Hanhela <jarihanhela@gmail.com>
2017-04-18 15:36:36 +0300

    Testaa, että hankkeiden haku palauttaa hankkeita
    Erillistä validointia ei tarvinne, koska spec validoi datan jo

---
+++ test/clj/harja/palvelin/palvelut/hankkeet_test.clj
@@ -0,0 +1,41 @@
+(ns harja.palvelin.palvelut.hankkeet-test
+  (:require [clojure.test :refer :all]
+            [com.stuartsierra.component :as component]
+            [harja
+             [pvm :as pvm]
+             [testi :refer :all]]
+            [harja.kyselyt.konversio :as konv]
+            [harja.palvelin.komponentit.tietokanta :as tietokanta]
+            [harja.domain.tiemerkinta-toteumat :as tt]
+            [harja.palvelin.palvelut.yllapito-toteumat :refer :all]
+            [harja.tyokalut.functor :refer [fmap]]
+            [taoensso.timbre :as log]
+            [clojure.spec.gen :as gen]
+            [clojure.spec :as s]
+            [clojure.string :as str]
+            [harja.palvelin.palvelut.hankkeet :as hankkeet]))
+
+(defn jarjestelma-fixture [testit]
+  (alter-var-root #'jarjestelma
+                  (fn [_]
+                    (component/start
+                      (component/system-map
+                        :db (tietokanta/luo-tietokanta testitietokanta)
+                        :http-palvelin (testi-http-palvelin)
+                        :hankkeet (component/using
+                                    (hankkeet/->Hankkeet)
+                                    [:db :http-palvelin])))))
+
+  (testit)
+  (alter-var-root #'jarjestelma component/stop))
+
+
+(use-fixtures :once (compose-fixtures
+                      jarjestelma-fixture
+                      urakkatieto-fixture))
+
+(deftest hankkeiden-haku-test
+  (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                :hae-harjassa-luodut-hankkeet +kayttaja-jvh+ {})]
+
+    (is (>= (count vastaus) 10))))
