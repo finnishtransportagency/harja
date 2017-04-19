35f38504a334a8afcc06d19d4aad213c85c8bd1c
Jari Hanhela <jarihanhela@gmail.com>
2017-04-19 18:42:27 +0300

    Lisää apufunktio, jolla voi helposti "namespacettaa" mapissa olevat avaimet

--- src/cljc/harja/tyokalut/spec_apurit.cljc
+++ src/cljc/harja/tyokalut/spec_apurit.cljc
@@ -1,5 +1,6 @@
 (ns harja.tyokalut.spec-apurit
   (:require [clojure.spec :as s]
+            [clojure.set :as set]
     #?@(:clj [[clojure.future :refer :all]])))
 
 ;; PostgreSQL raja-arvot
@@ -22,3 +23,45 @@
             m))
         elementti))
     mappi))
+
+
+(defn namespacefy
+  "Lisää annetun mapin (X) keywordien nimien eteen namespacen.
+
+   Optiot on map, joka määrittelee, millä logiikalla namespace lisätään:
+   :ns        Avain, joka kertoo lisättävän namespacen nimen.
+              Tämä namespace lisätään X:n kaikille ykköstason avaimille (ks. poikkeukset alta).
+   :except    Setti avaimia, joiden nimiä ei määritellä uudelleen X:ssä.
+   :custom    Mappi avaimia X:ssä. Arvoilla määritellään eri namespace kuin mitä :ns määrittelee.
+   :inner     Mappi avaimia X:ssä, joiden arvo on niin ikään map.
+              Arvolla määritellään logiikka, jolla sisemmän mapin avainten nimet muunnetaan.
+
+  Esimerkki:
+  Olkoon map:
+  {:name \"player1\" :hp 1 :tasks {:id 666 :time 5} :points 7 :foobar nil}
+
+  Sen muunnos:
+  (namespacefy data {:ns :our.domain.player ;; Määrittelee oletusnamespacen kaikille avaimille
+                     :except [:foobar] ;; :foobar avaimeen ei kosketa
+                     :custom {:points :our.domain.point/points} ;; :point avaimelle määritellään poikkeava namespace
+                     :inner {:tasks {:ns :our.domain.task}}}) ;; :tasks sisällölle muunnoslogiikka samalla tavalla
+  {:our.domain.player/name \"player1\"
+   :our.domain.player/hp 1
+   :our.domamain.player/tasks {:our.domain.task/id 666
+                               :our.domain.task/time 5}
+   :our.domain.point/points 7
+   :foobar nil}"
+  [map-x {:keys [ns except custom inner] :as options}]
+  (let [except (or except #{})
+        custom (or custom {})
+        inner (or inner {})
+        keys-to-be-modified (filter (comp not except) (keys map-x))
+        original-keyword->namespaced-keyword (apply merge (map
+                                                            #(-> {% (keyword (str (name ns) "/" (name %)))})
+                                                            keys-to-be-modified))
+        namespacefied-inner-maps (apply merge (map
+                                                #(-> {% (namespacefy (% map-x) (% inner))})
+                                                (keys inner)))
+        map-x-with-modified-inner-maps (merge map-x namespacefied-inner-maps)
+        final-rename-logic (merge original-keyword->namespaced-keyword custom)]
+    (set/rename-keys map-x-with-modified-inner-maps final-rename-logic)))
--- test/clj/harja/tyokalut/spec_apurit_test.clj
+++ test/clj/harja/tyokalut/spec_apurit_test.clj
@@ -2,12 +2,65 @@
   (:require
     [clojure.test :refer [deftest is use-fixtures]]
     [harja.testi :refer :all]
-    [harja.tyokalut.spec-apurit :as spec-apurit]))
+    [harja.tyokalut.spec-apurit :refer [namespacefy poista-nil-avaimet]))
 
 (deftest nil-arvojen-poisto-mapista-toimii
-  (is (= (spec-apurit/poista-nil-avaimet {:a "1" :b nil}) {:a "1"}))
-  (is (= (spec-apurit/poista-nil-avaimet {:a "1" :b "2"}) {:a "1" :b "2"}))
-  (is (= (spec-apurit/poista-nil-avaimet {:a "1" :b {:c "3"}}) {:a "1" :b {:c "3"}}))
-  (is (= (spec-apurit/poista-nil-avaimet {:a "1" :b {:c nil}}) {:a "1"}))
-  (is (= (spec-apurit/poista-nil-avaimet {:a nil :b {:c "3"}}) {:b {:c "3"}})))
+  (is (= (poista-nil-avaimet {:a "1" :b nil}) {:a "1"}))
+  (is (= (poista-nil-avaimet {:a "1" :b "2"}) {:a "1" :b "2"}))
+  (is (= (poista-nil-avaimet {:a "1" :b {:c "3"}}) {:a "1" :b {:c "3"}}))
+  (is (= (poista-nil-avaimet {:a "1" :b {:c nil}}) {:a "1"}))
+  (is (= (poista-nil-avaimet {:a nil :b {:c "3"}}) {:b {:c "3"}})))
 
+(deftest namespacefy-toimii
+  (let [yksitasoinen-map {:name "player1" :hp 1}
+        kaksitasoinen-map {:name "player1"
+                           :hp 1
+                           :tasks {:id 666
+                                   :time 5}
+                           :points 7
+                           :foobar nil}
+        kolmitasoinen-map {:name "player1"
+                           :hp 1
+                           :tasks {:id 666
+                                   :time 5
+                                   :more-info {:description "Stupid task"
+                                               :useless-keyword nil}}
+                           :points 7
+                           :foobar nil}]
+    (is (= (namespacefy yksitasoinen-map {:ns :our.domain.player})
+           {:our.domain.player/name "player1"
+            :our.domain.player/hp 1}))
+
+    (is (= (namespacefy yksitasoinen-map {:ns :our.domain.player :except #{:hp}})
+           {:our.domain.player/name "player1"
+            :hp 1}))
+
+    (is (= (namespacefy yksitasoinen-map {:ns :our.domain.player :custom {:hp :our.domain.hp/hp}})
+           {:our.domain.player/name "player1"
+            :our.domain.hp/hp 1}))
+
+    (is (= (namespacefy kaksitasoinen-map {:ns :our.domain.player
+                                           :except #{:foobar}
+                                           :custom {:points :our.domain.point/points}
+                                           :inner {:tasks {:ns :our.domain.task}}})
+           {:our.domain.player/name "player1"
+            :our.domain.player/hp 1
+            :our.domain.player/tasks {:our.domain.task/id 666
+                                      :our.domain.task/time 5}
+            :our.domain.point/points 7
+            :foobar nil}))
+
+    (is (= (namespacefy kolmitasoinen-map {:ns :our.domain.player
+                                           :except #{:foobar}
+                                           :custom {:points :our.domain.point/points}
+                                           :inner {:tasks {:ns :our.domain.task
+                                                           :inner {:more-info {:ns :our.domain.task
+                                                                               :except #{:useless-keyword}}}}}})
+           {:our.domain.player/name "player1"
+            :our.domain.player/hp 1
+            :our.domain.player/tasks {:our.domain.task/id 666
+                                      :our.domain.task/time 5
+                                      :our.domain.task/more-info {:our.domain.task/description "Stupid task"
+                                                                  :useless-keyword nil}}
+            :our.domain.point/points 7
+            :foobar nil}))))
