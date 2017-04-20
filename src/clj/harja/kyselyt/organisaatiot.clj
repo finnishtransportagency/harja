b989d5dd7fe4eaab38702d54a3f8b71805209214
Jari Hanhela <jarihanhela@gmail.com>
2017-04-20 09:32:42 +0300

    Määritä organisaation spec käyttäen define-tables

--- src/clj/harja/kyselyt/organisaatiot.clj
+++ src/clj/harja/kyselyt/organisaatiot.clj
@@ -1,6 +1,11 @@
 (ns harja.kyselyt.organisaatiot
   "Organisaatioihin liittyvät tietokantakyselyt"
-  (:require [jeesql.core :refer [defqueries]]))
+  (:require [jeesql.core :refer [defqueries]]
+            [specql.core :refer [define-tables fetch]]
+            [harja.domain.organisaatio :as o]
+            [harja.kyselyt.specql :refer [db]]))
 
 (defqueries "harja/kyselyt/organisaatiot.sql"
   {:positional? true})
+
+(define-tables db ["organisaatio" ::o/organisaatio])
