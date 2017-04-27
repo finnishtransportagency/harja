a7b88d38cde0cf616cd97f2248d2bb80b540461d
Jari Hanhela <jarihanhela@gmail.com>
2017-04-27 13:13:45 +0300

    Määritä, ettei poistettu-sarakkeella voi olla arvoa NULL (pitää olla true / false)

--- tietokanta/src/main/resources/db/migration/V1_534__.sql
+++ tietokanta/src/main/resources/db/migration/V1_534__.sql
@@ -26,24 +26,24 @@ ALTER TABLE urakka ADD COLUMN luotu timestamp DEFAULT NOW();
 ALTER TABLE urakka ADD COLUMN muokattu timestamp;
 ALTER TABLE urakka ADD COLUMN luoja integer REFERENCES kayttaja (id);
 ALTER TABLE urakka ADD COLUMN muokkaaja integer REFERENCES kayttaja (id);
-ALTER TABLE urakka ADD COLUMN poistettu boolean default false;
+ALTER TABLE urakka ADD COLUMN poistettu boolean not null default false;
 
 ALTER TABLE sopimus ADD COLUMN luotu timestamp DEFAULT NOW();
 ALTER TABLE sopimus ADD COLUMN muokattu timestamp;
 ALTER TABLE sopimus ADD COLUMN luoja integer REFERENCES kayttaja (id);
 ALTER TABLE sopimus ADD COLUMN muokkaaja integer REFERENCES kayttaja (id);
-ALTER TABLE sopimus ADD COLUMN poistettu boolean default false;
+ALTER TABLE sopimus ADD COLUMN poistettu boolean not null default false;
 
 ALTER TABLE hanke ADD COLUMN luotu timestamp DEFAULT NOW();
 ALTER TABLE hanke ADD COLUMN muokattu timestamp;
 ALTER TABLE hanke ADD COLUMN luoja integer REFERENCES kayttaja (id);
 ALTER TABLE hanke ADD COLUMN muokkaaja integer REFERENCES kayttaja (id);
-ALTER TABLE hanke ADD COLUMN poistettu boolean default false;
+ALTER TABLE hanke ADD COLUMN poistettu boolean not null default false;
 
 ALTER TABLE organisaatio ADD COLUMN luotu timestamp DEFAULT NOW();
 ALTER TABLE organisaatio ADD COLUMN muokattu timestamp;
 ALTER TABLE organisaatio ADD COLUMN muokkaaja integer REFERENCES kayttaja (id);
-ALTER TABLE organisaatio ADD COLUMN poistettu boolean default false;
+ALTER TABLE organisaatio ADD COLUMN poistettu boolean not null default false;
 
 -- Tiukkoja constraintteja lisää
 ALTER TABLE hanke ALTER COLUMN nimi SET NOT NULL;
