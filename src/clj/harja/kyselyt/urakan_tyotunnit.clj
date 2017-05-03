a61dbf26482ed41d6b33a06aaf6e431c8d81856a
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-05-03 11:29:29 +0300

    Lisää kysely urakan vuosikolmanneksen tuntien hakemiselle

--- src/clj/harja/kyselyt/urakan_tyotunnit.clj
+++ src/clj/harja/kyselyt/urakan_tyotunnit.clj
@@ -3,7 +3,8 @@
     [harja.kyselyt.specql-db :refer [define-tables]]
     [harja.domain.specql-db :refer [db]]
     [harja.domain.urakan-tyotunnit :as ut]
-    [specql.core :refer [fetch]]))
+    [specql.core :refer [fetch]]
+    [specql.op :as op]))
 
 (def kaikki-kentat
   #{::ut/id
@@ -14,3 +15,10 @@
 (defn hae-urakan-tyotunnit [db urakka-id]
   (fetch db ::ut/urakan-tyotunnit kaikki-kentat
          (::ut/urakka urakka-id)))
+
+(defn hae-urakan-vuosikolmanneksen-tyotunnit [db urakka-id vuosi vuosikolmannes]
+  (fetch db ::ut/urakan-tyotunnit #{::ut/tyotunnit}
+         (op/and
+           {::ut/urakka urakka-id
+            ::ut/vuosi vuosi
+            ::ut/vuosikolmannes vuosikolmannes})))
