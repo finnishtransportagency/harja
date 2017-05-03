e85d9ab86376e121cd342a1b612efd3eb5f73a02
Jari Hanhela <jarihanhela@gmail.com>
2017-05-03 15:57:42 +0300

    Lisää turvalaitteen näkymälle alustava tuck-tuki

--- src/cljs/harja/views/vesivaylat/urakka/turvalaitteet.cljs
+++ src/cljs/harja/views/vesivaylat/urakka/turvalaitteet.cljs
@@ -1,8 +1,13 @@
 (ns harja.views.vesivaylat.urakka.turvalaitteet
-  (:require [reagent.core :refer [atom]])
+  (:require [reagent.core :refer [atom]]
+            [harja.tiedot.vesivaylat.urakka.turvalaitteet :as tiedot]
+            [tuck.core :refer [tuck]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn turvalaitteet []
+(defn turvalaitteet* []
   [:div {:style {:padding "10px"}}
    [:img {:src "images/harja_favicon.png"}]
    [:div {:style {:color "orange"}} "Work In Progress"]])
+
+(defn turvalaitteet []
+  [tuck tiedot/tila turvalaitteet*])
