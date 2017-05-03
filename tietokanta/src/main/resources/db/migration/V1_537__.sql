57e66318eac64a818275a9cb2db8fac49255b8ca
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-05-03 13:10:04 +0300

    Lisää lähetystiedot kantatauluun

--- tietokanta/src/main/resources/db/migration/V1_537__.sql
+++ tietokanta/src/main/resources/db/migration/V1_537__.sql
@@ -1,9 +1,11 @@
 CREATE TABLE urakan_tyotunnit (
-  id             SERIAL PRIMARY KEY,
-  urakka         INTEGER REFERENCES urakka (id)      NOT NULL,
-  vuosi          INTEGER                             NOT NULL,
-  vuosikolmannes INTEGER                             NOT NULL,
-  tyotunnit      INTEGER                             NOT NULL,
+  id                 SERIAL PRIMARY KEY,
+  urakka             INTEGER REFERENCES urakka (id)      NOT NULL,
+  vuosi              INTEGER                             NOT NULL,
+  vuosikolmannes     INTEGER                             NOT NULL,
+  tyotunnit          INTEGER                             NOT NULL,
+  lahetetty          TIMESTAMP,
+  lahetys_onnistunut BOOLEAN,
   UNIQUE (urakka, vuosi, vuosikolmannes),
   CONSTRAINT validi_vuosikolmannes CHECK (vuosikolmannes >= 1 AND vuosikolmannes <= 3)
 );
