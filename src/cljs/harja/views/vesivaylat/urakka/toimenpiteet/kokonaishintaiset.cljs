112ca1889dcfd2035c0cf996936b31865125dd27
Jari Hanhela <jarihanhela@gmail.com>
2017-05-04 13:14:21 +0300

    Irrota kok. hint. näkymän pohja omaan haaraan

--- src/cljs/harja/views/vesivaylat/urakka/toimenpiteet/kokonaishintaiset.cljs
+++ src/cljs/harja/views/vesivaylat/urakka/toimenpiteet/kokonaishintaiset.cljs
@@ -10,81 +10,6 @@
             [harja.pvm :as pvm])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(def testidata [{::t/id 0
-                 ::t/tyoluokka "Asennus ja huolto"
-                 ::t/toimenpide "Huoltotyö"
-                 ::t/pvm (pvm/nyt)
-                 ::t/turvalaite "Siitenluoto (16469)"}
-                {::t/id 1
-                 ::t/tyoluokka "Asennus ja huolto"
-                 ::t/toimenpide "Huoltotyö"
-                 ::t/pvm (pvm/nyt)
-                 ::t/turvalaite "Siitenluoto (16469)"}
-                {::t/id 2
-                 ::t/tyoluokka "Asennus ja huolto"
-                 ::t/toimenpide "Huoltotyö"
-                 ::t/pvm (pvm/nyt)
-                 ::t/turvalaite "Siitenluoto (16469)"}
-                {::t/id 3
-                 ::t/tyoluokka "Asennus ja huolto"
-                 ::t/toimenpide "Huoltotyö"
-                 ::t/pvm (pvm/nyt)
-                 ::t/turvalaite "Siitenluoto (16469)"}
-                {::t/id 4
-                 ::t/tyoluokka "Asennus ja huolto"
-                 ::t/toimenpide "Huoltotyö"
-                 ::t/pvm (pvm/nyt)
-                 ::t/turvalaite "Siitenluoto (16469)"}])
-
-(def ryhmitelty-testidata [(grid/otsikko "Varkaus, Kuopion väylä")
-                {::t/id 0
-                 ::t/tyoluokka "Asennus ja huolto"
-                 ::t/toimenpide "Huoltotyö"
-                 ::t/pvm (pvm/nyt)
-                 ::t/turvalaite "Siitenluoto (16469)"}
-                {::t/id 1
-                 ::t/tyoluokka "Asennus ja huolto"
-                 ::t/toimenpide "Huoltotyö"
-                 ::t/pvm (pvm/nyt)
-                 ::t/turvalaite "Siitenluoto (16469)"}
-                {::t/id 2
-                 ::t/tyoluokka "Asennus ja huolto"
-                 ::t/toimenpide "Huoltotyö"
-                 ::t/pvm (pvm/nyt)
-                 ::t/turvalaite "Siitenluoto (16469)"}
-                (grid/otsikko "Kopio, Iisalmen väylä")
-                {::t/id 3
-                 ::t/tyoluokka "Asennus ja huolto"
-                 ::t/toimenpide "Huoltotyö"
-                 ::t/pvm (pvm/nyt)
-                 ::t/turvalaite "Siitenluoto (16469)"}
-                {::t/id 4
-                 ::t/tyoluokka "Asennus ja huolto"
-                 ::t/toimenpide "Huoltotyö"
-                 ::t/pvm (pvm/nyt)
-                 ::t/turvalaite "Siitenluoto (16469)"}])
-
-(defn- otsikon-sisalto [sijainnit]
-  [grid/grid
-   {:tunniste ::t/id
-    ;; TODO Piilota otsikkosarivi (th) kokonaan?
-    :tyhja (if (nil? sijainnit)
-             [ajax-loader "Haetaan toimenpiteitä"]
-             "Ei toimenpiteitä")}
-   [{:nimi ::t/tyoluokka}
-    {:nimi ::t/toimenpide}
-    {:nimi ::t/pvm :fmt pvm/pvm-opt}
-    {:nimi ::t/turvalaite}]
-   sijainnit])
-
-(defn- toimenpidepaneelin-otsikko [otsikko maara]
-  (str otsikko
-       " ("
-       maara
-       (when (not= maara 0)
-         "kpl")
-       ")"))
-
 (defn kokonaishintaiset-toimenpiteet* [e! app]
   (komp/luo
     (komp/sisaan-ulos #(e! (tiedot/->Nakymassa? true))
@@ -93,14 +18,7 @@
       [:div
        [:div {:style {:padding "10px"}}
         [:img {:src "images/harja_favicon.png"}]
-        [:div {:style {:color "orange"}} "Työmaa"]]
-
-       [otsikot [(toimenpidepaneelin-otsikko "Viitat" (count testidata))
-                 (fn [] [otsikon-sisalto ryhmitelty-testidata])
-                 (toimenpidepaneelin-otsikko "Poljut" 0)
-                 (fn [] [otsikon-sisalto []])
-                 (toimenpidepaneelin-otsikko "Tykityöt" 0)
-                 (fn [] [otsikon-sisalto []])]]])))
+        [:div {:style {:color "orange"}} "Työmaa"]]])))
 
 (defn kokonaishintaiset-toimenpiteet []
   [tuck tiedot/tila kokonaishintaiset-toimenpiteet*])
