6278120ef5956698851190bb67f41cb2c67ad7e1
Jari Hanhela <jarihanhela@gmail.com>
2017-05-04 09:04:56 +0300

    Aseta Näkymässä? tila oikein toteuma-välilehden alla vesiväylissä

--- src/cljs/harja/views/vesivaylat/urakka/toimenpiteet/kokonaishintaiset.cljs
+++ src/cljs/harja/views/vesivaylat/urakka/toimenpiteet/kokonaishintaiset.cljs
@@ -1,8 +1,18 @@
 (ns harja.views.vesivaylat.urakka.toimenpiteet.kokonaishintaiset
-  (:require [reagent.core :refer [atom]])
+  (:require [reagent.core :refer [atom]]
+            [tuck.core :refer [tuck]]
+            [harja.tiedot.vesivaylat.urakka.toimenpiteet.kokonaishintaiset :as tiedot]
+            [harja.ui.komponentti :as komp])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
+(defn kokonaishintaiset-toimenpiteet* [e! app]
+  (komp/luo
+    (komp/sisaan-ulos #(e! (tiedot/->Nakymassa? true))
+                      #(e! (tiedot/->Nakymassa? false)))
+    (fn [e! app]
+      [:div {:style {:padding "10px"}}
+       [:img {:src "images/harja_favicon.png"}]
+       [:div {:style {:color "orange"}} "Työmaa"]])))
+
 (defn kokonaishintaiset-toimenpiteet []
-  [:div {:style {:padding "10px"}}
-   [:img {:src "images/harja_favicon.png"}]
-   [:div {:style {:color "orange"}} "Työmaa"]])
+  [tuck tiedot/tila kokonaishintaiset-toimenpiteet*])
