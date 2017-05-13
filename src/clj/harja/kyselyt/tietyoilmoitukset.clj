cad2eab7c2cb2016aa287ba41049716b56ced2b0
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-05-13 16:31:10 +0300

    Lisää uuden specql:n vaatima 3. parametri

--- src/clj/harja/kyselyt/tietyoilmoitukset.clj
+++ src/clj/harja/kyselyt/tietyoilmoitukset.clj
@@ -166,13 +166,13 @@
 
 (defn intersects? [threshold geometry]
   (reify op/Op
-    (to-sql [this value-accessor]
+    (to-sql [this value-accessor _]
       [(str "ST_Intersects(ST_Buffer(?,?), " value-accessor ")")
        [geometry threshold]])))
 
 (defn intersects-envelope? [{:keys [xmin ymin xmax ymax]}]
   (reify op/Op
-    (to-sql [this val]
+    (to-sql [this val _]
       [(str "ST_Intersects(" val ", ST_MakeEnvelope(?,?,?,?))")
        [xmin ymin xmax ymax]])))
 
@@ -183,7 +183,7 @@
 
 (defn interval? [start interval]
   (reify op/Op
-    (to-sql [this value]
+    (to-sql [this value _]
       [(str "(? - " value " < ?::INTERVAL)")
        [start interval]])))
 
