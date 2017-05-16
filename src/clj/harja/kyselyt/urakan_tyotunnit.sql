0342a8f3a8d3255ff69141e34455941873ac1016
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-05-16 11:45:28 +0300

    Muodosta sähköpostiviesti muistutuksille

--- src/clj/harja/kyselyt/urakan_tyotunnit.sql
+++ src/clj/harja/kyselyt/urakan_tyotunnit.sql
@@ -1,6 +1,6 @@
 -- name: hae-urakat-joilla-puuttuu-kolmanneksen-tunnit
 -- Tarkistaa löytyykö toteumaa ulkoisella id:llä
-SELECT u.sampoid
+SELECT u.id, u.sampoid, u.nimi, hallintayksikko
 FROM urakka u
 WHERE NOT exists(SELECT ut.id
                  FROM urakan_tyotunnit ut
