80c5e064c06119b4cf6db44e640c159f2e752780
Erno Kuusela <erno.kuusela@solita.fi>
2017-05-17 06:50:28 +0000

    toimenpidehakusanoman muodostus

--- src/clj/harja/palvelin/integraatiot/reimari/toimenpidehaku.clj
+++ src/clj/harja/palvelin/integraatiot/reimari/toimenpidehaku.clj
@@ -5,20 +5,44 @@
             [harja.palvelin.integraatiot.integraatiotapahtuma :as integraatiotapahtuma]
             [harja.palvelin.tyokalut.ajastettu-tehtava :as ajastettu-tehtava]
             [harja.palvelin.integraatiot.reimari.sanomat.hae-toimenpiteet :as sanoma]
+            [harja.tyokalut.xml :as xml]
             [harja.palvelin.tyokalut.lukot :as lukko]))
 
+(defn kasittele-vastaus [body]
+  (log/debug "kasittele-vastaus" body))
+
+(defn hae-toimenpiteet* [konteksti db pohja-url kayttajatunnus salasana muutosaika]
+
+  (let [otsikot {"Content-Type" "application/xml; charset=utf-8"}
+        http-asetukset {:metodi :POST
+                        :url (str pohja-url "/hae-toimenpiteet")
+                        :kayttajatunnus kayttajatunnus
+                        :salasana salasana
+                        :muutosaika muutosaika}
+        {body :body headers :headers} (integraatiotapahtuma/laheta konteksti :http http-asetukset)]
+    (integraatiotapahtuma/lisaa-tietoja konteksti (str "Haetaan uudet toimenpiteet alkaen " muutosaika))
+    (kasittele-vastaus body headers)))
 
 
 
+(defn kysely-sanoma [muutosaika]
+  (xml/tee-xml-sanoma
+   [:soap:Envelope {:xmlns:soap "http://schemas.xmlsoap.org/soap/envelope/"}
+    [:soap:Body
+     [:HaeToimenpiteet {:xmlns "http://www.liikennevirasto.fi/xsd/harja/reimari"}
+      [:HaeToimenpiteetRequest {:muutosaika muutosaika}]]
+     ]]))
 
-(defn hae-toimenpiteet* [x]
-  (log/debug "hae-toimenpiteet* - parametri:" x)
-  )
+(defn edellisen-integraatiotapahtuman-alkuaika [db]
+  ;; tähän kysely joka katsoo integraatiotapahtumat-tauusta edellisen
+  ;; haetoimenpiteet-tapahtuman alkuajan
+  "2017-05-01")
 
-(defn hae-toimenpiteet [db integraatioloki]
-  (lukko/yrita-ajaa-lukon-kanssa
-   db "reimari-hae-toimenpiteet"
-   (fn []
-     (integraatiotapahtuma/suorita-integraatio
-      db integraatioloki "reimari" "hae-toimenpiteet"
-      (hae-toimenpiteet* db)))))
+(defn hae-toimenpiteet [db integraatioloki pohja-url kayttajatunnus salasana]
+  (let [muutosaika (edellisen-integraatiotapahtuman-alkuaika db)]
+    (lukko/yrita-ajaa-lukon-kanssa
+     db "reimari-hae-toimenpiteet"
+     (fn []
+       (integraatiotapahtuma/suorita-integraatio
+        db integraatioloki "reimari" "hae-toimenpiteet"
+        #(hae-toimenpiteet* % db pohja-url kayttajatunnus salasana muutosaika))))))
