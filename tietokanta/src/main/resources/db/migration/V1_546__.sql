d176ec7d59497cec73b0cc83035e052800141fcf
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-05-17 09:03:29 +0300

    Poista kirjotushärö migraatiotiedostosta

--- tietokanta/src/main/resources/db/migration/V1_546__.sql
+++ tietokanta/src/main/resources/db/migration/V1_546__.sql
@@ -6,4 +6,4 @@ ALTER TABLE sopimus ADD CONSTRAINT sallittu_sampoid CHECK (nimi NOT LIKE '%=%');
 ALTER TABLE sopimus ADD CONSTRAINT sallittu_nimi CHECK (nimi NOT LIKE '%=%');
 
 -- Toimenpidekoodissa sama homma, kielletään SQL-parsinnassa käytetyt merkit
-ALTER TABLE toimenpidekoodi ADD CONSTRAINT sallittu_nimi CHECK (nimi NOT LIKE '%^%');git
+ALTER TABLE toimenpidekoodi ADD CONSTRAINT sallittu_nimi CHECK (nimi NOT LIKE '%^%');
