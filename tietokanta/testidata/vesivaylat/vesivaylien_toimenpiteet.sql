d770851863481e662284acbaa258290275fc3bb5
Jari Hanhela <jarihanhela@gmail.com>
2017-05-19 09:49:22 +0300

    Lisää testidataa

--- tietokanta/testidata/vesivaylat/vesivaylien_toimenpiteet.sql
+++ tietokanta/testidata/vesivaylat/vesivaylien_toimenpiteet.sql
@@ -292,6 +292,144 @@ VALUES
    '(123, Hietasaaren läntinen rinnakkaisväylä, 55)',
    (SELECT id FROM vv_vayla WHERE nimi = 'Hietasaaren läntinen rinnakkaisväylä'));
 
+INSERT INTO toteuma
+(urakka, sopimus, luotu,
+ luoja,alkanut, paattynyt, suorittajan_nimi, tyyppi, lahde, lisatieto)
+VALUES
+  ((SELECT id
+    FROM urakka
+    WHERE nimi = 'Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'),
+   (SELECT id
+    FROM sopimus
+    WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
+   NOW(),
+   (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
+   '2017-05-04 00:00:00+02',
+   '2017-05-04 12:00:00+02',
+   'Pohjanmeren venepojat',
+   'vv-kokonaishintainen',
+   'harja-api',
+   'TESTITOIMENPIDE 5');
+
+INSERT INTO reimari_toimenpide
+("toteuma-id",
+ "reimari-id",
+ "reimari-urakoitsija",
+ "urakoitsija-id",
+ "reimari-sopimus",
+ "sopimus-id",
+ "reimari-turvalaite",
+ "turvalaite-id",
+ lisatieto,
+ lisatyo,
+ suoritettu,
+ luotu,
+ luoja,
+ "reimari-luotu",
+ "reimari-alus",
+ "reimari-tila",
+ "reimari-tyyppi",
+ "reimari-tyolaji",
+ "reimari-tyoluokka",
+ "reimari-vayla",
+ "vayla-id")
+VALUES
+  ((SELECT id
+    FROM toteuma
+    WHERE lisatieto = 'TESTITOIMENPIDE 5'),
+    42,
+    '(23, Pohjanmeren venepojat)',
+    (SELECT id
+     FROM organisaatio
+     WHERE nimi = 'Pohjanmeren venepojat'),
+    '(-5, 1022542301, Helsingin väyläyksikön pääsopimus)',
+    (SELECT id FROM sopimus WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
+    '(62, Hietasaaren poiju, 555)',
+    (SELECT id FROM vv_turvalaite WHERE nimi = 'Hietasaaren poiju'),
+    '',
+    FALSE,
+    '2017-05-08T23:23Z',
+    '2017-05-08',
+    (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
+    '2017-05-08',
+    '(MBKE24524, MS Piggy)',
+    '1022541202',
+    '1022542049',
+    '1022541802',
+    '1022541903',
+    '(123, Akonniemen väylät, 55)',
+    (SELECT id FROM vv_vayla WHERE nimi = 'Akonniemen väylät'));
+
+
+INSERT INTO toteuma
+(urakka, sopimus, luotu,
+ luoja,alkanut, paattynyt, suorittajan_nimi, tyyppi, lahde, lisatieto)
+VALUES
+  ((SELECT id
+    FROM urakka
+    WHERE nimi = 'Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'),
+   (SELECT id
+    FROM sopimus
+    WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
+   NOW(),
+   (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
+   '2017-05-01 00:00:00+02',
+   '2017-05-01 12:00:00+02',
+   'Pohjanmeren venepojat',
+   'vv-kokonaishintainen',
+   'harja-api',
+   'TESTITOIMENPIDE 6');
+
+INSERT INTO reimari_toimenpide
+("toteuma-id",
+ "reimari-id",
+ "reimari-urakoitsija",
+ "urakoitsija-id",
+ "reimari-sopimus",
+ "sopimus-id",
+ "reimari-turvalaite",
+ "turvalaite-id",
+ lisatieto,
+ lisatyo,
+ suoritettu,
+ luotu,
+ luoja,
+ "reimari-luotu",
+ "reimari-alus",
+ "reimari-tila",
+ "reimari-tyyppi",
+ "reimari-tyolaji",
+ "reimari-tyoluokka",
+ "reimari-vayla",
+ "vayla-id")
+VALUES
+  ((SELECT id
+    FROM toteuma
+    WHERE lisatieto = 'TESTITOIMENPIDE 6'),
+    42,
+    '(23, Pohjanmeren venepojat)',
+    (SELECT id
+     FROM organisaatio
+     WHERE nimi = 'Pohjanmeren venepojat'),
+    '(-5, 1022542301, Helsingin väyläyksikön pääsopimus)',
+    (SELECT id FROM sopimus WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
+    '(62, Hietasaaren poiju, 555)',
+    (SELECT id FROM vv_turvalaite WHERE nimi = 'Hietasaaren poiju'),
+    '',
+    FALSE,
+    '2017-05-08T23:23Z',
+    '2017-05-08',
+    (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
+    '2017-05-08',
+    '(MBKE24524, MS Piggy)',
+    '1022541202',
+    '1022542049',
+    '1022541803',
+    '1022541903',
+    '(123, Akonniemen väylät, 55)',
+    (SELECT id FROM vv_vayla WHERE nimi = 'Akonniemen väylät'));
+
+
 -- TODO: YKSIKKÖHINTAISIIN SIIRRETTY TYÖ, HINTAERITTELY TEHTY
 
 -- TODO: REIMARISTA YKSIKKÖHINTAISENA RAPORTOITU TYÖ
