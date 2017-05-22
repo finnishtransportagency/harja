798f8cd2b9df0e3fd3c32e00a4dc0ff832cd61dc
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-05-22 15:52:31 +0300

    Päivitä testidata vastaamaan muutettua tietomallia

--- tietokanta/testidata/vesivaylat/vesivaylien_toimenpiteet.sql
+++ tietokanta/testidata/vesivaylat/vesivaylien_toimenpiteet.sql
@@ -1,32 +1,18 @@
+-- ***********************************************
 -- VIKA ILMAN KORJAUSTA
+-- ***********************************************
 
 INSERT INTO vv_vikailmoitus
 ("reimari-id", kuvaus, pvm, "turvalaite-id")
 VALUES
   ('1234', 'Akonniemen kyltti on lähtenyt irti myrskyn takia', '2017-04-02', (SELECT id FROM vv_turvalaite WHERE nimi = 'Akonniemen kyltti'));
 
--- KOKONAISHINTAINEN TOIMENPIDE ILMAN VIKAA
-
-INSERT INTO toteuma
-(urakka, sopimus, luotu,luoja,alkanut, paattynyt, suorittajan_nimi, tyyppi, lahde, lisatieto)
-VALUES
-  ((SELECT id
-    FROM urakka
-    WHERE nimi = 'Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'),
-   (SELECT id
-    FROM sopimus
-    WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
-   NOW(),
-   (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
-   '2017-05-05 00:00:00+02',
-   '2017-05-05 12:00:00+02',
-   'Pohjanmeren venepojat',
-   'vv-kokonaishintainen',
-   'harja-api',
-   'TESTITOIMENPIDE 1');
+-- ***********************************************
+-- KOKONAISHINTAISET TOIMENPITEET ILMAN VIKAA
+-- ***********************************************
 
 INSERT INTO reimari_toimenpide
-("toteuma-id",
+(hintatyyppi,
  "reimari-id",
  "reimari-urakoitsija",
  "urakoitsija-id",
@@ -48,9 +34,7 @@ INSERT INTO reimari_toimenpide
  "reimari-vayla",
 "vayla-id")
 VALUES
-  ((SELECT id
-     FROM toteuma
-     WHERE lisatieto = 'TESTITOIMENPIDE 1'),
+  ('kokonaishintainen',
     12,
     '(23, Pohjanmeren venepojat)',
     (SELECT id
@@ -74,29 +58,106 @@ VALUES
    '(123, Hietasaaren läntinen rinnakkaisväylä, 55)',
    (SELECT id FROM vv_vayla WHERE nimi = 'Hietasaaren läntinen rinnakkaisväylä'));
 
--- KOKONAISHINTAINEN TOIMENPIDE VIALLA
-
-INSERT INTO toteuma
-(urakka, sopimus, luotu,
- luoja,alkanut, paattynyt, suorittajan_nimi, tyyppi, lahde, lisatieto)
+INSERT INTO reimari_toimenpide
+(hintatyyppi,
+ "reimari-id",
+ "reimari-urakoitsija",
+ "urakoitsija-id",
+ "reimari-sopimus",
+ "sopimus-id",
+ "reimari-turvalaite",
+ "turvalaite-id",
+ lisatieto,
+ lisatyo,
+ suoritettu,
+ luotu,
+ luoja,
+ "reimari-luotu",
+ "reimari-alus",
+ "reimari-tila",
+ "reimari-toimenpidetyyppi",
+ "reimari-tyolaji",
+ "reimari-tyoluokka",
+ "reimari-vayla",
+ "vayla-id")
 VALUES
-  ((SELECT id
-    FROM urakka
-    WHERE nimi = 'Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'),
-   (SELECT id
-    FROM sopimus
-    WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
-   NOW(),
+  ('kokonaishintainen',
+    52,
+    '(23, Pohjanmeren venepojat)',
+    (SELECT id
+     FROM organisaatio
+     WHERE nimi = 'Pohjanmeren venepojat'),
+    '(-5, 1022542301, Helsingin väyläyksikön pääsopimus)',
+    (SELECT id FROM sopimus WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
+    '(62, Hietasaaren poiju, 555)',
+    (SELECT id FROM vv_turvalaite WHERE nimi = 'Hietasaaren poiju'),
+    '',
+    FALSE,
+    '2017-05-08T23:23Z',
+    '2017-05-08',
     (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
-   '2017-04-04 00:00:00+02',
-   '2017-04-04 12:00:00+02',
-   'Pohjanmeren venepojat',
-   'vv-kokonaishintainen',
-   'harja-api',
-   'TESTITOIMENPIDE 2');
+    '2017-05-08',
+    '(MBKE24524, MS Piggy)',
+    '1022541202',
+    '1022542049',
+    '1022541802',
+    '1022541903',
+    '(123, Akonniemen väylät, 55)',
+    (SELECT id FROM vv_vayla WHERE nimi = 'Akonniemen väylät'));
 
 INSERT INTO reimari_toimenpide
-("toteuma-id",
+(hintatyyppi,
+ "reimari-id",
+ "reimari-urakoitsija",
+ "urakoitsija-id",
+ "reimari-sopimus",
+ "sopimus-id",
+ "reimari-turvalaite",
+ "turvalaite-id",
+ lisatieto,
+ lisatyo,
+ suoritettu,
+ luotu,
+ luoja,
+ "reimari-luotu",
+ "reimari-alus",
+ "reimari-tila",
+ "reimari-toimenpidetyyppi",
+ "reimari-tyolaji",
+ "reimari-tyoluokka",
+ "reimari-vayla",
+ "vayla-id")
+VALUES
+  ('kokonaishintainen',
+    62,
+    '(23, Pohjanmeren venepojat)',
+    (SELECT id
+     FROM organisaatio
+     WHERE nimi = 'Pohjanmeren venepojat'),
+    '(-5, 1022542301, Helsingin väyläyksikön pääsopimus)',
+    (SELECT id FROM sopimus WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
+    '(62, Hietasaaren poiju, 555)',
+    (SELECT id FROM vv_turvalaite WHERE nimi = 'Hietasaaren poiju'),
+    '',
+    FALSE,
+    '2017-05-08T23:23Z',
+    '2017-05-08',
+    (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
+    '2017-05-08',
+    '(MBKE24524, MS Piggy)',
+    '1022541202',
+    '1022542049',
+    '1022541803',
+    '1022541903',
+    '(123, Akonniemen väylät, 55)',
+    (SELECT id FROM vv_vayla WHERE nimi = 'Akonniemen väylät'));
+
+-- ***********************************************
+-- KOKONAISHINTAISET TOIMENPITEET VIALLA
+-- ***********************************************
+
+INSERT INTO reimari_toimenpide
+(hintatyyppi,
  "reimari-id",
  "reimari-urakoitsija",
  "urakoitsija-id",
@@ -118,9 +179,7 @@ INSERT INTO reimari_toimenpide
  "reimari-vayla",
  "vayla-id")
 VALUES
-  ((SELECT id
-     FROM toteuma
-     WHERE lisatieto = 'TESTITOIMENPIDE 2'),
+  ('kokonaishintainen',
     22,
     '(23, Pohjanmeren venepojat)',
     (SELECT id
@@ -152,29 +211,12 @@ INSERT INTO vv_vikailmoitus
          FROM reimari_toimenpide
          WHERE lisatieto = 'TESTITOIMENPIDE 2'));
 
--- KOKONAISHINTAISIIN SIIRRETTY, REIMARISTA YKSIKKÖHINTAISENA RAPORTOITU TYÖ
-
-INSERT INTO toteuma
-(urakka, sopimus, luotu,
- luoja,alkanut, paattynyt, suorittajan_nimi, tyyppi, lahde, lisatieto)
-VALUES
-  ((SELECT id
-    FROM urakka
-    WHERE nimi = 'Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'),
-   (SELECT id
-    FROM sopimus
-    WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
-   NOW(),
-    (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
-   '2017-05-03 09:00:00+02',
-   '2017-05-03 15:00:00+02',
-   'Pohjanmeren venepojat',
-   'vv-kokonaishintainen',
-   'harja-api',
-   'TESTITOIMENPIDE 3');
+-- ***********************************************
+-- KOKONAISHINTAISIIN SIIRRETYT, REIMARISTA YKSIKKÖHINTAISENA RAPORTOIDUT TYÖT
+-- ***********************************************
 
 INSERT INTO reimari_toimenpide
-("toteuma-id",
+(hintatyyppi,
  "reimari-id",
  "reimari-urakoitsija",
  "urakoitsija-id",
@@ -196,9 +238,7 @@ INSERT INTO reimari_toimenpide
  "reimari-vayla",
  "vayla-id")
 VALUES
-  ((SELECT id
-     FROM toteuma
-     WHERE lisatieto = 'TESTITOIMENPIDE 3'),
+  ('kokonaishintainen',
     32,
     '(23, Pohjanmeren venepojat)',
     (SELECT id
@@ -222,29 +262,12 @@ VALUES
    '(123, Hietasaaren läntinen rinnakkaisväylä, 55)',
    (SELECT id FROM vv_vayla WHERE nimi = 'Hietasaaren läntinen rinnakkaisväylä'));
 
--- YKSIKKÖHINTAISIIN SIIRRETTY TYÖ, ILMAN HINTAERITTELYÄ
-
-INSERT INTO toteuma
-(urakka, sopimus, luotu,
- luoja,alkanut, paattynyt, suorittajan_nimi, tyyppi, lahde, lisatieto)
-VALUES
-  ((SELECT id
-    FROM urakka
-    WHERE nimi = 'Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'),
-   (SELECT id
-    FROM sopimus
-    WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
-   NOW(),
-    (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
-   '2017-05-03 09:00:00+02',
-   '2017-05-03 15:00:00+02',
-   'Pohjanmeren venepojat',
-   'vv-yksikkohintainen',
-   'harja-api',
-   'TESTITOIMENPIDE 4');
+-- ***********************************************
+-- YKSIKKÖHINTAISIIN SIIRRETYT TYÖT, ILMAN HINTAERITTELYÄ
+-- ***********************************************
 
 INSERT INTO reimari_toimenpide
-("toteuma-id",
+(hintatyyppi,
  "reimari-id",
  "reimari-urakoitsija",
  "urakoitsija-id",
@@ -266,9 +289,7 @@ INSERT INTO reimari_toimenpide
  "reimari-vayla",
  "vayla-id")
 VALUES
-  ((SELECT id
-     FROM toteuma
-     WHERE lisatieto = 'TESTITOIMENPIDE 4'),
+  ('yksikkohintainen',
     42,
     '(23, Pohjanmeren venepojat)',
     (SELECT id
@@ -292,149 +313,20 @@ VALUES
    '(123, Hietasaaren läntinen rinnakkaisväylä, 55)',
    (SELECT id FROM vv_vayla WHERE nimi = 'Hietasaaren läntinen rinnakkaisväylä'));
 
-INSERT INTO toteuma
-(urakka, sopimus, luotu,
- luoja,alkanut, paattynyt, suorittajan_nimi, tyyppi, lahde, lisatieto)
-VALUES
-  ((SELECT id
-    FROM urakka
-    WHERE nimi = 'Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'),
-   (SELECT id
-    FROM sopimus
-    WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
-   NOW(),
-   (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
-   '2017-05-04 00:00:00+02',
-   '2017-05-04 12:00:00+02',
-   'Pohjanmeren venepojat',
-   'vv-kokonaishintainen',
-   'harja-api',
-   'TESTITOIMENPIDE 5');
 
-INSERT INTO reimari_toimenpide
-("toteuma-id",
- "reimari-id",
- "reimari-urakoitsija",
- "urakoitsija-id",
- "reimari-sopimus",
- "sopimus-id",
- "reimari-turvalaite",
- "turvalaite-id",
- lisatieto,
- lisatyo,
- suoritettu,
- luotu,
- luoja,
- "reimari-luotu",
- "reimari-alus",
- "reimari-tila",
- "reimari-toimenpidetyyppi",
- "reimari-tyolaji",
- "reimari-tyoluokka",
- "reimari-vayla",
- "vayla-id")
-VALUES
-  ((SELECT id
-    FROM toteuma
-    WHERE lisatieto = 'TESTITOIMENPIDE 5'),
-    42,
-    '(23, Pohjanmeren venepojat)',
-    (SELECT id
-     FROM organisaatio
-     WHERE nimi = 'Pohjanmeren venepojat'),
-    '(-5, 1022542301, Helsingin väyläyksikön pääsopimus)',
-    (SELECT id FROM sopimus WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
-    '(62, Hietasaaren poiju, 555)',
-    (SELECT id FROM vv_turvalaite WHERE nimi = 'Hietasaaren poiju'),
-    '',
-    FALSE,
-    '2017-05-08T23:23Z',
-    '2017-05-08',
-    (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
-    '2017-05-08',
-    '(MBKE24524, MS Piggy)',
-    '1022541202',
-    '1022542049',
-    '1022541802',
-    '1022541903',
-    '(123, Akonniemen väylät, 55)',
-    (SELECT id FROM vv_vayla WHERE nimi = 'Akonniemen väylät'));
-
-
-INSERT INTO toteuma
-(urakka, sopimus, luotu,
- luoja,alkanut, paattynyt, suorittajan_nimi, tyyppi, lahde, lisatieto)
-VALUES
-  ((SELECT id
-    FROM urakka
-    WHERE nimi = 'Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'),
-   (SELECT id
-    FROM sopimus
-    WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
-   NOW(),
-   (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
-   '2017-05-01 00:00:00+02',
-   '2017-05-01 12:00:00+02',
-   'Pohjanmeren venepojat',
-   'vv-kokonaishintainen',
-   'harja-api',
-   'TESTITOIMENPIDE 6');
-
-INSERT INTO reimari_toimenpide
-("toteuma-id",
- "reimari-id",
- "reimari-urakoitsija",
- "urakoitsija-id",
- "reimari-sopimus",
- "sopimus-id",
- "reimari-turvalaite",
- "turvalaite-id",
- lisatieto,
- lisatyo,
- suoritettu,
- luotu,
- luoja,
- "reimari-luotu",
- "reimari-alus",
- "reimari-tila",
- "reimari-toimenpidetyyppi",
- "reimari-tyolaji",
- "reimari-tyoluokka",
- "reimari-vayla",
- "vayla-id")
-VALUES
-  ((SELECT id
-    FROM toteuma
-    WHERE lisatieto = 'TESTITOIMENPIDE 6'),
-    42,
-    '(23, Pohjanmeren venepojat)',
-    (SELECT id
-     FROM organisaatio
-     WHERE nimi = 'Pohjanmeren venepojat'),
-    '(-5, 1022542301, Helsingin väyläyksikön pääsopimus)',
-    (SELECT id FROM sopimus WHERE nimi = 'Helsingin väyläyksikön pääsopimus'),
-    '(62, Hietasaaren poiju, 555)',
-    (SELECT id FROM vv_turvalaite WHERE nimi = 'Hietasaaren poiju'),
-    '',
-    FALSE,
-    '2017-05-08T23:23Z',
-    '2017-05-08',
-    (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'),
-    '2017-05-08',
-    '(MBKE24524, MS Piggy)',
-    '1022541202',
-    '1022542049',
-    '1022541803',
-    '1022541903',
-    '(123, Akonniemen väylät, 55)',
-    (SELECT id FROM vv_vayla WHERE nimi = 'Akonniemen väylät'));
-
-
+-- ***********************************************
 -- TODO: YKSIKKÖHINTAISIIN SIIRRETTY TYÖ, HINTAERITTELY TEHTY
+-- ***********************************************
 
+-- ***********************************************
 -- TODO: REIMARISTA YKSIKKÖHINTAISENA RAPORTOITU TYÖ
+-- ***********************************************
 
+-- ***********************************************
 -- TODO: REIMARISTA YKSIKKÖHINTAISENA RAPORTOITU, KÖNTTÄSUMMALLA HINNOITELTU TOIMENPIDE
+-- ***********************************************
 
 
+-- ***********************************************
 -- TODO: ERIKSEEN TILATTU YKSIKKÖHINTAINEN TYÖ?
+-- ***********************************************
