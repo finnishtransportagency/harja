0dea5eab02c81ff4e048b90fa0477d4f5c0f7e25
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-05-24 10:29:25 +0300

    Lisää reimari-toimenpiteelle urakka-id

--- tietokanta/src/main/resources/db/migration/V1_549__.sql
+++ tietokanta/src/main/resources/db/migration/V1_549__.sql
@@ -1,34 +1,45 @@
 -- Poista reimari_toimenpide taulusta toteuma-linkki
 
-CREATE TYPE vv_toimenpide_hintatyyppi AS ENUM ('kokonaishintainen', 'yksikkohintainen');
+CREATE TYPE VV_TOIMENPIDE_HINTATYYPPI AS ENUM ('kokonaishintainen', 'yksikkohintainen');
 
 ALTER TABLE reimari_toimenpide
-    ADD COLUMN hintatyyppi vv_toimenpide_hintatyyppi;
+  ADD COLUMN hintatyyppi VV_TOIMENPIDE_HINTATYYPPI,
+  ADD COLUMN "urakka-id" INTEGER REFERENCES urakka (id) NOT NULL DEFAULT 0;
+
+ALTER TABLE reimari_toimenpide
+  ALTER COLUMN "urakka-id" DROP DEFAULT;
 
 UPDATE reimari_toimenpide
-  SET hintatyyppi = (SELECT
-                       CASE WHEN tyyppi = 'vv-kokonaishintainen' THEN 'kokonaishintainen' ELSE 'yksikkohintainen' END
-                     FROM toteuma t JOIN reimari_toimenpide r ON t.id = r."toteuma-id")::vv_toimenpide_hintatyyppi;
+SET hintatyyppi = (SELECT CASE WHEN tyyppi = 'vv-kokonaishintainen'
+  THEN 'kokonaishintainen'
+                          ELSE 'yksikkohintainen' END
+                   FROM toteuma t
+                     JOIN reimari_toimenpide r
+                       ON t.id = r."toteuma-id") :: VV_TOIMENPIDE_HINTATYYPPI,
+  "urakka-id"   = (SELECT t.urakka
+                   FROM toteuma t
+                     JOIN reimari_toimenpide rt ON t.id = reimari_toimenpide."toteuma-id");
 
 ALTER TABLE reimari_toimenpide
-    DROP COLUMN "toteuma-id";
+  DROP COLUMN "toteuma-id";
 
 -- Poista toteumataulusta vesiväylätoteumat
 
-DELETE FROM toteuma WHERE tyyppi = 'vv-kokonaishintainen' OR tyyppi = 'vv-yksikkohintainen';
+DELETE FROM toteuma
+WHERE tyyppi = 'vv-kokonaishintainen' OR tyyppi = 'vv-yksikkohintainen';
 
 -- Lisää hinnoittelutaulut
 
 CREATE TABLE vv_hinnoittelu
 (
   id         SERIAL PRIMARY KEY,
-  nimi       VARCHAR CONSTRAINT nipulla_oltava_nimi CHECK(hintanippu IS FALSE OR nimi IS NOT NULL),
+  nimi       VARCHAR CONSTRAINT nipulla_oltava_nimi CHECK (hintanippu IS FALSE OR nimi IS NOT NULL),
   hintanippu BOOLEAN                          NOT NULL DEFAULT FALSE,
 
   muokkaaja  INTEGER REFERENCES kayttaja (id),
   muokattu   TIMESTAMP,
   luoja      INTEGER REFERENCES kayttaja (id) NOT NULL,
-  luotu      TIMESTAMP                        NOT NULL,
+  luotu      TIMESTAMP                        NOT NULL DEFAULT NOW(),
   poistettu  BOOLEAN                          NOT NULL DEFAULT FALSE,
   poistaja   INTEGER REFERENCES kayttaja (id)
 );
@@ -40,7 +51,7 @@ CREATE TABLE vv_hinnoittelu_toimenpide
 (
   "toimenpide-id"  INTEGER REFERENCES reimari_toimenpide (id),
   "hinnoittelu-id" INTEGER REFERENCES vv_hinnoittelu (id),
-  UNIQUE("toimenpide-id", "hinnoittelu-id")
+  UNIQUE ("toimenpide-id", "hinnoittelu-id")
 );
 
 CREATE TABLE vv_hinta
@@ -54,7 +65,7 @@ CREATE TABLE vv_hinta
   muokkaaja          INTEGER REFERENCES kayttaja (id),
   muokattu           TIMESTAMP,
   luoja              INTEGER REFERENCES kayttaja (id) NOT NULL,
-  luotu              TIMESTAMP                        NOT NULL,
+  luotu              TIMESTAMP                        NOT NULL DEFAULT NOW(),
   poistettu          BOOLEAN                          NOT NULL DEFAULT FALSE,
   poistaja           INTEGER REFERENCES kayttaja (id)
 );
