867c8e3b6c620596b0ef9f82cecd98a0929e403c
Jari Hanhela <jarihanhela@gmail.com>
2017-05-26 12:46:29 +0300

    Luo simppeli pohjaa tarkastuksille ja laatupoikkeamille

--- src/cljs/harja/views/vesivaylat/urakka/laadunseuranta/tarkastukset.cljs
+++ src/cljs/harja/views/vesivaylat/urakka/laadunseuranta/tarkastukset.cljs
@@ -5,8 +5,11 @@
             [harja.tiedot.vesivaylat.urakka.laadunseuranta.tarkastukset :as tiedot]
             [harja.ui.valinnat :as valinnat]
             [harja.loki :refer [log]]
+            [harja.ui.otsikkopaneeli :refer [otsikkopaneeli]]
+            [harja.ui.yleiset :refer [ajax-loader ajax-loader-pieni]]
             [harja.views.urakka.valinnat :as urakka-valinnat]
-            [harja.ui.napit :as napit]))
+            [harja.ui.napit :as napit]
+            [harja.ui.grid :as grid]))
 
 (defn- valinnat [e! app]
   ;; TODO Tuleeko kaikkiin laadunseurannan näkymiin nämä samat? Jos tulee, käytä yhteistä komponenttia
@@ -26,6 +29,36 @@
     ^{:key "siirtonappi"}
     [napit/uusi "Lisää uusi tarkastus" #(log "Painoit nappia")]]])
 
+
+(defn- paneelin-sisalto [e! laatupoikkeamat]
+  [grid/grid
+   {:tunniste :id ;; TODO namespaceta
+    :ei-footer-muokkauspaneelia? true}
+   [{:otsikko "Pvm ja aika" :nimi :pvm}
+    {:otsikko "Tyyppi" :nimi :tyyppi}
+    {:otsikko "Turvalaite" :nimi :turvalaite}
+    {:otsikko "Havainnot" :nimi :havainnot}]
+   laatupoikkeamat])
+
+(defn- luo-otsikkorivit
+  [e! laatupoikkeamat haku-kaynnissa?]
+  (let [vaylat (keys (group-by :vayla laatupoikkeamat))] ;; TODO namespaceta
+    (vec (mapcat
+           (fn [vayla]
+             [vayla
+              [:span
+               (grid/otsikkorivin-tiedot "Väylä" 0) ; TODO Väylän nimi ja rivien määrä tähän
+               (when haku-kaynnissa? [:span " " [ajax-loader-pieni]])]
+              [paneelin-sisalto
+               e!
+               laatupoikkeamat ;; TODO Suodata väylällä
+               ]])
+           vaylat))))
+
+(defn- tarkastukset-listaus [e! app]
+  (into [otsikkopaneeli]
+        (luo-otsikkorivit e! (:laatupoikkeamat app) (:haku-kaynnissa? app))))
+
 (defn tarkastukset* [e! app]
   (komp/luo
     (komp/sisaan-ulos #(e! (tiedot/->Nakymassa? true))
@@ -34,6 +67,7 @@
       [:div
 
        [valinnat e! app]
+       [tarkastukset-listaus e! app]
 
        [:div {:style {:padding "10px"}}
         [:img {:src "images/harja_favicon.png"}]
