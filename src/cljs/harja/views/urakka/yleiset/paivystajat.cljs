af23f8f368bf816d9bacb91ac57741569ee90c1c
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-05-30 11:22:19 +0300

    Alustava päivystysten aikajana
    Päivystykset näkyvät nyt aikajanana, jossa jokaiselle henkilölle on
    oma uimaratansa. Siirretty päivystysasiat omaan nimiavaruuteen.

---
+++ src/cljs/harja/views/urakka/yleiset/paivystajat.cljs
@@ -0,0 +1,148 @@
+(ns harja.views.urakka.yleiset.paivystajat
+  "Urakan yleiset välilehden päivystäjälista ja aikajana."
+  (:require [reagent.core :as r]
+            [harja.ui.grid :as grid]
+            [harja.pvm :as pvm]
+            [cljs-time.core :as t]
+            [harja.ui.komponentti :as komp]
+            [harja.domain.oikeudet :as oikeudet]
+            [cljs.core.async :refer [<!]]
+            [clojure.string :as str]
+            [harja.ui.viesti :as viesti]
+            [harja.ui.yleiset :as yleiset]
+            [harja.tiedot.urakka.yhteystiedot :as yht]
+            [harja.fmt :as fmt]
+            [harja.loki :refer [log]]
+            [harja.asiakas.kommunikaatio :as k]
+            [harja.ui.napit :as napit]
+            [harja.ui.kentat :as kentat]
+            [harja.ui.aikajana :as aikajana]
+            [harja.ui.debug :as debug])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(defn tallenna-paivystajat [ur paivystajat uudet-paivystajat]
+  (log "tallenna päivystäjät!" (pr-str uudet-paivystajat))
+  (go (let [tallennettavat
+            (into []
+                  ;; Kaikki tiedon mankelointi ennen lähetystä tähän
+                  (comp (filter #(not (:poistettu %)))
+                        (map #(if-let [nimi (:nimi %)]
+                                (let [[_ etu suku] (re-matches #"^ *([^ ]+)( *.*?) *$" nimi)]
+                                  (assoc %
+                                    :etunimi (str/trim etu)
+                                    :sukunimi (str/trim suku)))
+                                %)))
+                  uudet-paivystajat)
+            poistettavat
+            (into []
+                  (keep #(when (and (:poistettu %)
+                                    (> (:id %) 0))
+                           (:id %)))
+                  uudet-paivystajat)
+            vastaus (<! (yht/tallenna-urakan-paivystajat (:id ur) tallennettavat poistettavat))]
+        (if (k/virhe? vastaus)
+          (viesti/nayta! "Päivystäjien tallennus epäonnistui." :warning viesti/viestin-nayttoaika-keskipitka)
+          (do (reset! paivystajat (reverse (sort-by :loppu vastaus)))
+              true)))))
+
+(defn- nayta-aikajana []
+  [kentat/tee-kentta {:tyyppi :toggle
+                      :paalle-teksti "Näytä aikajana"
+                      :pois-teksti "Piilota aikajana"
+                      :toggle! yht/toggle-nayta-aikajana!} yht/nayta-aikajana?])
+
+(defn paivystajalista
+  [ur paivystajat tallenna!]
+  [:div
+   [grid/grid
+    {:otsikko "Päivystystiedot"
+     :tyhja "Ei päivystystietoja."
+     :tallenna tallenna!
+     :rivin-luokka #(when (and (< (:alku %) (pvm/nyt))
+                               (< (pvm/nyt) (:loppu %)))
+                      " bold")}
+    [{:otsikko "Nimi" :hae #(if-let [nimi (:nimi %)]
+                              nimi
+                              (str (:etunimi %)
+                                   (when-let [suku (:sukunimi %)]
+                                     (str " " suku))))
+      :aseta (fn [yht arvo]
+               (assoc yht :nimi arvo))
+
+
+      :tyyppi :string :leveys 15
+      :validoi [[:ei-tyhja "Anna päivystäjän nimi"]]}
+     {:otsikko "Organisaatio" :nimi :organisaatio :fmt :nimi :leveys 10
+      :tyyppi :valinta
+      :valinta-nayta #(if % (:nimi %) "- Valitse organisaatio -")
+      :valinnat [nil (:urakoitsija ur) (:hallintayksikko ur)]}
+
+     {:otsikko "Puhelin (virka)" :nimi :tyopuhelin :tyyppi :puhelin :leveys 10
+      :pituus 16}
+     {:otsikko "Puhelin (gsm)" :nimi :matkapuhelin :tyyppi :puhelin :leveys 10
+      :pituus 16}
+     {:otsikko "Sähköposti" :nimi :sahkoposti :tyyppi :email :leveys 20
+      :validoi [[:ei-tyhja "Anna päivystäjän sähköposti"]]}
+     {:otsikko "Alkupvm" :nimi :alku :tyyppi :pvm-aika :fmt pvm/pvm-aika :leveys 10
+      :validoi [[:ei-tyhja "Aseta alkupvm"]
+                (fn [alku rivi]
+                  (let [loppu (:loppu rivi)]
+                    (when (and alku loppu
+                               (t/before? loppu alku))
+                      "Alkupvm ei voi olla lopun jälkeen.")))
+                ]}
+     {:otsikko "Loppupvm" :nimi :loppu :tyyppi :pvm-aika :fmt pvm/pvm-aika :leveys 10
+      :validoi [[:ei-tyhja "Aseta loppupvm"]
+                (fn [loppu rivi]
+                  (let [alku (:alku rivi)]
+                    (when (and alku loppu
+                               (t/before? loppu alku))
+                      "Loppupvm ei voi olla alkua ennen.")))]}
+     {:otsikko "Vastuuhenkilö" :nimi :vastuuhenkilo :tyyppi :checkbox
+      :leveys 10
+      :fmt fmt/totuus :tasaa :keskita}]
+    paivystajat]
+   [yleiset/vihje "Kaikista ilmoituksista lähetetään aina kaikille vuorossaoleville päivystäjille tieto sähköpostilla.
+   Uusista toimenpidepyynnöistä lähetetään tekstiviesti vain vuorossaoleville vastuuhenkilöille"]])
+
+(defn- aikajanariveiksi
+  "Muunna päivystäjälista aikajanriveiksi. Ryhmitellään tiedot nimen ja organisaation mukaan,
+  joten yhdelle päivystäjälle tulee vain yksi uimarata, vaikka hänellä olisi useita
+  eri päivystysvuoroja."
+  [paivystajat]
+  (let [ryhmitellyt-paivystykset (group-by (juxt :etunimi :sukunimi :organisaatio)
+                                           paivystajat)]
+    (for [[[etunimi sukunimi org] paivystykset] ryhmitellyt-paivystykset]
+      {::aikajana/otsikko (str etunimi " " sukunimi)
+       ::aikajana/ajat (for [{:keys [alku loppu varahenkilo vastuuhenkilo]} paivystykset]
+                         {::aikajana/alku alku
+                          ::aikajana/loppu loppu
+                          ::aikajana/teksti (str (pvm/pvm-aika alku) " \u2013 "
+                                                 (pvm/pvm-aika loppu))
+                          ::aikajana/vari (cond varahenkilo "yellow"
+                                                vastuuhenkilo "green"
+                                                :default "gray")})})))
+(defn- aikajana [paivystajat]
+  [:div.paivystys-aikajana
+   [nayta-aikajana]
+   (when @yht/nayta-aikajana?
+     [:div.paivystajat-aikajana
+      [aikajana/aikajana (aikajanariveiksi paivystajat)]])])
+
+(defn paivystajat [ur]
+  (let [paivystajat (r/atom nil)
+        hae! (fn [urakka-id]
+               (log "HAETAAN PÄIVYSTÄJÄT: " urakka-id)
+               (reset! paivystajat nil)
+               (go (reset! paivystajat
+                           (reverse (sort-by :loppu
+                                             (<! (yht/hae-urakan-paivystajat urakka-id)))))))]
+    (hae! (:id ur))
+    (komp/luo
+      (komp/kun-muuttuu (comp hae! :id))
+      (fn [ur]
+        [:div.paivystajat
+         [paivystajalista ur @paivystajat
+          (when (oikeudet/voi-kirjoittaa? oikeudet/urakat-yleiset (:id ur))
+            #(tallenna-paivystajat ur paivystajat %))]
+         [aikajana @paivystajat]]))))
