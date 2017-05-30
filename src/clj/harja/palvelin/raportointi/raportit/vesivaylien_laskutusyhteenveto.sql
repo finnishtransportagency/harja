3eeadbc78b6b4888c86272bd6fe81a98ef6e5f8b
Jari Hanhela <jarihanhela@gmail.com>
2017-05-30 11:51:52 +0300

    Päivitä kysely

--- src/clj/harja/palvelin/raportointi/raportit/vesivaylien_laskutusyhteenveto.sql
+++ src/clj/harja/palvelin/raportointi/raportit/vesivaylien_laskutusyhteenveto.sql
@@ -29,9 +29,14 @@ WHERE "urakka-id" = :urakkaid
 -- name: hae-yksikkohintaiset-ryhmalliset-toimenpiteet
 SELECT
   hinnoittelu.nimi as "hinnoittelu",
-  (SELECT SUM(maara) FROM vv_hinta WHERE "hinnoittelu-id" = hinnoittelu.id) as "summa"
--- Hinnoittelut, jotka ovat ryhmiä, sisältävät useita reimari-toimenpiteitä,
--- jotka voivat potentiaalisesti liittyä eri väyliin / väylätyyppeihin
+  (SELECT SUM(maara) FROM vv_hinta WHERE "hinnoittelu-id" = hinnoittelu.id) as "summa",
+  -- Hinnoittelut, jotka ovat ryhmiä, sisältävät useita reimari-toimenpiteitä,
+  -- jotka voivat potentiaalisesti liittyä eri väyliin / väylätyyppeihin
+  -- Listataan kaikki väylätyypit taulukossa
+  (SELECT ARRAY(SELECT DISTINCT(tyyppi) FROM vv_vayla WHERE id IN
+                                                            (SELECT "vayla-id" FROM reimari_toimenpide WHERE id IN
+                                                                                                             (SELECT "toimenpide-id" FROM vv_hinnoittelu_toimenpide WHERE "hinnoittelu-id" = hinnoittelu.id))))
+    as vaylatyyppi
 FROM vv_hinnoittelu hinnoittelu
 WHERE "urakka-id" = :urakkaid
       AND hintaryhma IS TRUE
