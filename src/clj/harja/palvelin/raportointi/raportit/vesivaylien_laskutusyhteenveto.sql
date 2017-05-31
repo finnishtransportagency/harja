9aade17f1f6c7ccc0927a2347266dec5e52cb75d
Jari Hanhela <jarihanhela@gmail.com>
2017-05-31 08:22:52 +0300

    Yks. hint. hinnoittelujen haussa hae myös esnimmäinen ja viimeinen toimenpide

--- src/clj/harja/palvelin/raportointi/raportit/vesivaylien_laskutusyhteenveto.sql
+++ src/clj/harja/palvelin/raportointi/raportit/vesivaylien_laskutusyhteenveto.sql
@@ -1,14 +1,38 @@
 -- name: hae-yksikkohintaiset-toimenpiteet
 SELECT
-  hinnoittelu.nimi as "hinnoittelu",
-  (SELECT SUM(maara) FROM vv_hinta WHERE "hinnoittelu-id" = hinnoittelu.id) as "summa",
+  hinnoittelu.nimi                          AS "hinnoittelu",
+  (SELECT SUM(maara)
+   FROM vv_hinta
+   WHERE "hinnoittelu-id" = hinnoittelu.id) AS "summa",
   -- Hinnoittelut, jotka ovat hinnoitteluryhmiä, sisältävät useita reimari-toimenpiteitä
   -- jotka voivat potentiaalisesti liittyä eri väyliin / väylätyyppeihin
   -- Listataan hinnoitteluun liittyvät väylätyypit taulukossa.
-  (SELECT ARRAY(SELECT DISTINCT(tyyppi) FROM vv_vayla WHERE id IN
-                 (SELECT "vayla-id" FROM reimari_toimenpide WHERE id IN
-                   (SELECT "toimenpide-id" FROM vv_hinnoittelu_toimenpide WHERE "hinnoittelu-id" = hinnoittelu.id))))
-    as vaylatyyppi
+  (SELECT ARRAY(SELECT DISTINCT (tyyppi)
+                FROM vv_vayla
+                WHERE id IN
+                      (SELECT "vayla-id"
+                       FROM reimari_toimenpide
+                       WHERE id IN
+                             (SELECT "toimenpide-id"
+                              FROM vv_hinnoittelu_toimenpide
+                              WHERE "hinnoittelu-id" = hinnoittelu.id))))
+                                            AS vaylatyyppi,
+  (SELECT suoritettu
+   FROM reimari_toimenpide
+   WHERE id IN (SELECT "toimenpide-id"
+                FROM vv_hinnoittelu_toimenpide
+                WHERE "hinnoittelu-id" = hinnoittelu.id)
+   ORDER BY suoritettu
+   LIMIT 1)
+                                            AS ensimmainen_toimenpide,
+  (SELECT suoritettu
+   FROM reimari_toimenpide
+   WHERE id IN (SELECT "toimenpide-id"
+                FROM vv_hinnoittelu_toimenpide
+                WHERE "hinnoittelu-id" = hinnoittelu.id)
+   ORDER BY suoritettu DESC
+   LIMIT 1)
+                                            AS viimeinen_toimenpide
 FROM vv_hinnoittelu hinnoittelu
 WHERE "urakka-id" = :urakkaid
       -- Hinnoittelulle on kirjattu toimenpiteitä valitulla aikavälillä
@@ -24,15 +48,15 @@ WHERE "urakka-id" = :urakkaid
 
 -- name: hae-kokonaishintaiset-toimenpiteet
 SELECT
-  "reimari-toimenpidetyyppi" AS koodi,
+  "reimari-toimenpidetyyppi"                                             AS koodi,
   (SELECT COUNT(id)
    FROM reimari_toimenpide
    WHERE "urakka-id" = :urakkaid
          AND suoritettu >= :alkupvm
          AND suoritettu <= :loppupvm
          AND hintatyyppi = 'kokonaishintainen'
-         AND "reimari-toimenpidetyyppi" = rt."reimari-toimenpidetyyppi") as maara,
-  vayla.tyyppi as vaylatyyppi
+         AND "reimari-toimenpidetyyppi" = rt."reimari-toimenpidetyyppi") AS maara,
+  vayla.tyyppi                                                           AS vaylatyyppi
 FROM reimari_toimenpide rt
   LEFT JOIN vv_vayla vayla ON rt."vayla-id" = vayla.id
 WHERE "urakka-id" = :urakkaid
