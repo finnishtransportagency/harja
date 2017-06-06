abd9346dfb5bf0b7711417d927fd56e4b4dafc8d
Erno Kuusela <erno.kuusela@solita.fi>
2017-06-06 07:06:39 +0000

    alustava tietokantaskeema reimarin komponenttityypeille

---
+++ tietokanta/src/main/resources/db/migration/V1_552__.sql
@@ -0,0 +1,17 @@
+CREATE TABLE reimari_komponenttityyppi (
+       "id" TEXT PRIMARY KEY,
+       "nimi" TEXT NOT NULL,
+       "lisatiedot" TEXT NOT NULL,
+       "luokan-id" TEXT NOT NULL,
+       "luokan-nimi" TEXT NOT NULL,
+       "luokan-lisatiedot" TEXT NOT NULL,
+       "luokan-paivitysaika" TIMESTAMP,
+       "luokan-luontiaika" TIMESTAMP,
+       "merk-cod" TEXT NOT NULL,
+       "paivitysaika" TIMESTAMP,
+       "luontiaika" TIMESTAMP,
+       "muokattu" TIMESTAMP,
+       "alkupvm" TIMESTAMP,
+       "loppupvm" TIMESTAMP
+       -- Tarvitaanko Harjan vakiot muokkaustiedot?
+)
