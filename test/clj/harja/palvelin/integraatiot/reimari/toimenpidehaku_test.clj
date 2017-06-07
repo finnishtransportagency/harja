ba6404a4a521be3369ab7ad41d81fe8fe6f25599
Erno Kuusela <erno.kuusela@solita.fi>
2017-06-07 13:52:27 +0000

    korjaa esimerkkiviestin polku testissä

--- test/clj/harja/palvelin/integraatiot/reimari/toimenpidehaku_test.clj
+++ test/clj/harja/palvelin/integraatiot/reimari/toimenpidehaku_test.clj
@@ -16,7 +16,7 @@
 
 (t/deftest kasittele-vastaus-kantatallennus
   (ht/tarkista-map-arvot
-   (first (sut/kasittele-vastaus (:db ht/jarjestelma)  (slurp "resources/xsd/reimari/vastaus.xml") ))
+   (first (sut/kasittele-vastaus (:db ht/jarjestelma)  (slurp "resources/xsd/reimari/haetoimenpiteet-vastaus.xml") ))
    {:harja.domain.vesivaylat.toimenpide/suoritettu
     #inst "2017-04-24T09:42:04.123-00:00",
     :harja.domain.vesivaylat.toimenpide/reimari-id -123456,
