3f4f1763d9901dae349f178e1c77c6d457dd0879
Jari Hanhela <jarihanhela@gmail.com>
2017-06-16 13:08:02 +0300

    LIsää testejä hinnalle ja hinnoittelulle

--- test/clj/harja/domain/vesivaylat/hinta_test.clj
+++ test/clj/harja/domain/vesivaylat/hinta_test.clj
@@ -17,3 +17,35 @@
   (is (= :baz
          (hinta/hinnan-yleiskustannuslisa [{::hinta/otsikko :foobar ::hinta/yleiskustannuslisa :baz}]
                                           :foobar))))
+
+(deftest hintojen-lasku
+  (is (= (hinta/perushinta [{::hinta/maara 1} {::hinta/maara 2} {::hinta/maara 3}])
+         6))
+  (is (= (hinta/yleiskustannuslisien-osuus [{::hinta/maara 1}
+                                            {::hinta/maara 100 ::hinta/yleiskustannuslisa 10}])
+         10))
+  (is (= (hinta/kokonaishinta-yleiskustannuslisineen [{::hinta/maara 1}
+                                                      {::hinta/maara 100 ::hinta/yleiskustannuslisa 10}])
+         111)))
+
+(deftest hinnan-ominaisuus
+  (is (= (hinta/hinnan-ominaisuus [{::hinta/maara 1 ::hinta/otsikko "A"}
+                                   {::hinta/maara 2 ::hinta/otsikko "B"}
+                                   {::hinta/maara 3 ::hinta/otsikko "C"}]
+                                  "B" ::hinta/maara)
+         2))
+  (is (= (hinta/hinnan-maara [{::hinta/maara 1 ::hinta/otsikko "A"}
+                              {::hinta/maara 2 ::hinta/otsikko "B"}
+                              {::hinta/maara 3 ::hinta/otsikko "C"}]
+                             "C")
+         3))
+  (is (= (hinta/hinnan-yleiskustannuslisa [{::hinta/maara 1 ::hinta/otsikko "A" ::hinta/yleiskustannuslisa 666}
+                                           {::hinta/maara 2 ::hinta/otsikko "B"}
+                                           {::hinta/maara 3 ::hinta/otsikko "C"}]
+                                          "A")
+         666))
+  (is (= (hinta/hinta-otsikolla "A"
+                                [{::hinta/maara 1 ::hinta/otsikko "A" ::hinta/yleiskustannuslisa 666}
+                                 {::hinta/maara 2 ::hinta/otsikko "B"}
+                                 {::hinta/maara 3 ::hinta/otsikko "C"}])
+         {::hinta/maara 1 ::hinta/otsikko "A" ::hinta/yleiskustannuslisa 666})))
