c247345277c3d098410c9d0c03705fb86aa62614
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-06-16 10:06:25 +0300

    Lisää kiintiönäkymään grid+vetolaatikot, joissa kiintiöt+toimenpiteet

--- src/cljs/harja/tiedot/vesivaylat/urakka/suunnittelu/kiintiot.cljs
+++ src/cljs/harja/tiedot/vesivaylat/urakka/suunnittelu/kiintiot.cljs
@@ -1,33 +1,60 @@
 (ns harja.tiedot.vesivaylat.urakka.suunnittelu.kiintiot
   (:require [reagent.core :as r]
-            [tuck.core :as t]
-            [harja.tyokalut.tuck :as tuck]
+            [tuck.core :as tuck]
+            [harja.tyokalut.tuck :as tuck-apurit]
             [harja.loki :refer [log]]
             [harja.asiakas.kommunikaatio :as k]
-            [harja.ui.viesti :as viesti]))
+            [harja.ui.viesti :as viesti]
+            [harja.tiedot.navigaatio :as nav]
+            [harja.tiedot.urakka :as u]
 
-(defonce app (r/atom {:nakymassa? false
+            [harja.domain.vesivaylat.kiintio :as kiintio])
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [reagent.ratom :refer [reaction]]))
+
+(defonce tila (r/atom
+                {:nakymassa? false
                       :kiintioiden-haku-kaynnissa? false
-                      :kiintiot nil}))
+                       :kiintioiden-tallennus-kaynnissa? false
+                      :kiintiot nil
+                      :valinnat nil}))
+
+(defonce valinnat
+  (reaction
+    (when (:nakymassa? tila)
+      {:urakka-id (:id @nav/valittu-urakka)
+       :sopimus-id (first @u/valittu-sopimusnumero)})))
 
 (defrecord Nakymassa? [nakymassa?])
+(defrecord PaivitaValinnat [valinnat])
 (defrecord HaeKiintiot [])
 (defrecord KiintiotHaettu [tulos])
 (defrecord KiintiotEiHaettu [])
+(defrecord TallennaKiintiot [])
+(defrecord KiintiotTallennettu [tulos])
+(defrecord KiintiotEiTallennettu [])
 
-(extend-protocol t/Event
+(extend-protocol tuck/Event
   Nakymassa?
   (process-event [{nak :nakymassa?} app]
     (assoc app :nakymassa? nak))
 
+  PaivitaValinnat
+  (process-event [{val :valinnat} app]
+    (let [uudet-valinnat (merge (:valinnat app) val)
+          haku (tuck/send-async! ->HaeKiintiot)]
+      (go (haku uudet-valinnat))
+      (assoc app :valinnat uudet-valinnat)))
+
   HaeKiintiot
   (process-event [_ app]
     (if-not (:kiintioiden-haku-kaynnissa? app)
-      (let [parametrit {}]
+      (let [parametrit {::kiintio/urakka-id (get-in app [:valinnat :urakka-id])
+                        ::kiintio/sopimus-id (get-in app [:valinnat :sopimus-id])}]
         (-> app
-           (tuck/palvelukutsu :hae-kiintiot
-                              parametrit
-                              {:onnistui ->KiintiotHaettu
+           (tuck-apurit/palvelukutsu :hae-kiintiot
+                                     parametrit
+                                     {:onnistui ->KiintiotHaettu
                                :epaonnistui ->KiintiotEiHaettu})
            (assoc :kiintioiden-haku-kaynnissa? true)))
 
@@ -41,5 +68,28 @@
   KiintiotEiHaettu
   (process-event [_ app]
     (viesti/nayta! "Kiintiöiden haku epäonnistui!" :danger)
-    (assoc app :kiintioiden-haku-kaynnissa? false)))
+    (assoc app :kiintioiden-haku-kaynnissa? false))
+
+  TallennaKiintiot
+  (process-event [_ app]
+    (if-not (:kiintioiden-tallennus-kaynnissa? app)
+      (let [parametrit {}]
+        (-> app
+            (tuck-apurit/palvelukutsu :tallenna-kiintiot
+                                      parametrit
+                                      {:onnistui ->KiintiotTallennettu
+                                       :epaonnistui ->KiintiotEiTallennettu})
+            (assoc :kiintioiden-tallennus-kaynnissa? true)))
+
+      app))
+
+  KiintiotTallennettu
+  (process-event [{kiintiot :tulos} app]
+    (assoc app :kiintiot kiintiot
+               :kiintioiden-tallennus-kaynnissa? false))
+
+  KiintiotEiTallennettu
+  (process-event [_ app]
+    (viesti/nayta! "Kiintiöiden tallennus epäonnistui!" :danger)
+    (assoc app :kiintioiden-tallennus-kaynnissa? false)))
 
--- src/cljs/harja/views/vesivaylat/urakka/suunnittelu/kiintiot.cljs
+++ src/cljs/harja/views/vesivaylat/urakka/suunnittelu/kiintiot.cljs
@@ -1,39 +1,80 @@
 (ns harja.views.vesivaylat.urakka.suunnittelu.kiintiot
-  (:require [tuck.core :as tuck]
+  (:require [reagent.core :as r]
+            [tuck.core :as tuck]
+            [harja.pvm :as pvm]
+            [harja.ui.debug :refer [debug]]
+            [harja.fmt :as fmt]
+            [harja.loki :refer [log]]
             [harja.ui.grid :as grid]
-            [harja.domain.vesivaylat.kiintio :as kiintio]
-            [harja.tiedot.vesivaylat.urakka.suunnittelu.kiintiot :as tiedot]
             [harja.ui.komponentti :as komp]
-            [harja.tiedot.navigaatio :as nav]))
+            [harja.tiedot.vesivaylat.urakka.suunnittelu.kiintiot :as tiedot]
+            [harja.tiedot.navigaatio :as nav]
 
+            [harja.tiedot.urakka :as u]
+            [harja.views.urakka.valinnat :as valinnat]
+            [harja.domain.vesivaylat.kiintio :as kiintio]
+            [harja.domain.vesivaylat.toimenpide :as to]
+            [harja.domain.vesivaylat.turvalaite :as tu]))
+
+(defn kiintion-toimenpiteet [e! app kiintio]
+  [grid/grid
+   {:tyhja "Ei liitettyjä toimenpiteitä"
+    :tunniste ::to/id}
+   [{:otsikko "Työluokka" :nimi ::to/tyoluokka :fmt to/reimari-tyoluokka-fmt :leveys 10}
+    {:otsikko "Toimenpide" :nimi ::to/toimenpide :fmt to/reimari-toimenpidetyyppi-fmt :leveys 10}
+    {:otsikko "Päivämäärä" :nimi ::to/pvm :fmt pvm/pvm-opt :leveys 10}
+    {:otsikko "Turvalaite" :nimi ::to/turvalaite :leveys 10 :hae #(get-in % [::to/turvalaite ::tu/nimi])}]
+   (::kiintio/toimenpiteet kiintio)])
 
 (defn kiintiot* [e! app]
   (komp/luo
+    (komp/watcher tiedot/valinnat (fn [_ _ uusi]
+                                    (e! (tiedot/->PaivitaValinnat uusi))))
     (komp/sisaan-ulos #(do (e! (tiedot/->Nakymassa? true))
+                           (e! (tiedot/->PaivitaValinnat
+                                 {:urakka-id (:id @nav/valittu-urakka)
+                                  :sopimus-id (first @u/valittu-sopimusnumero)}))
                            (e! (tiedot/->HaeKiintiot)))
                       #(do (e! (tiedot/->Nakymassa? false))))
-    #_(komp/sisaan #(e! (tiedot/->PaivitaUrakka @nav/valittu-urakka)))
-    #_(komp/watcher nav/valittu-urakka (fn [_ _ ur]
-                                       (e! (tiedot/->PaivitaUrakka ur))))
-    (fn [e! app]
-      [grid/muokkaus-grid
-       {:otsikko ""
-        :voi-muokata? (constantly true) ;;TODO oikeustarkastus
-        :voi-poistaa? (constantly false) ;;TODO oikeustarkastus
-        :tyhja "Ei määriteltyjä kiintiöitä"
-        :jarjesta ::kiintio/nimi
-        :tunniste ::kiintio/id
-        :uusi-rivi (fn [rivi] rivi)
-        }
-       [{:otsikko "Nimi"
-         :nimi ::kiintio/nimi}
-        {:otsikko "Kuvaus"
-         :nimi ::kiintio/kuvaus}
-        {:otsikko "Koko"
-         :nimi ::kiintio/koko}
-        {:otsikko "Toteutunut"
-         :nimi :toteutunut
-         :hae (constantly 0)}]])))
+    (fn [e! {:keys [kiintiot] :as app}]
+      @tiedot/valinnat ;; Reaktio on pakko lukea komponentissa, muuten se ei päivity.
+      [:div
+       [debug app]
+       [valinnat/urakan-sopimus @nav/valittu-urakka]
+       [grid/grid
+        {:otsikko ""
+         :voi-poistaa? (constantly false) ;;TODO oikeustarkastus + pitää olla tyhjä
+         :piilota-toiminnot? true ;; TODO
+         :voi-lisata? true
+         :tallenna #(e! (tiedot/->TallennaKiintiot))
+         :tyhja "Ei määriteltyjä kiintiöitä"
+         :jarjesta ::kiintio/nimi
+         :tunniste ::kiintio/id
+         :uusi-rivi (fn [rivi] rivi)
+         :vetolaatikot (into {}
+                             (map (juxt ::kiintio/id (fn [rivi] [kiintion-toimenpiteet e! app rivi])))
+                             kiintiot)
+         }
+          [{:tyyppi :vetolaatikon-tila :leveys 3}
+           {:otsikko "Nimi"
+            :nimi ::kiintio/nimi
+            :tyyppi :string
+            :leveys 3}
+           {:otsikko "Kuvaus"
+            :nimi ::kiintio/kuvaus
+            :tyyppi :text
+            :leveys 3}
+           {:otsikko "Koko"
+            :nimi ::kiintio/koko
+            :tyyppi :positiivinen-numero
+            :leveys 3}
+           {:otsikko "Toteutunut"
+            :nimi :toteutunut
+            :hae (constantly 0)
+            :tyyppi :positiivinen-numero
+            :muokattava? (constantly false)
+            :leveys 3}]
+          kiintiot]])))
 
 (defn kiintiot []
-  [tuck/tuck tiedot/app kiintiot*])
+  [tuck/tuck tiedot/tila kiintiot*])
