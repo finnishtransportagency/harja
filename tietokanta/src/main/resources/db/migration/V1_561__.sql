c9cb28ca571706aead6c4d006ddd173bcccd44a7
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-06-19 20:41:55 +0300

    Indeksoi urakka ja paranna kommentteja

--- tietokanta/src/main/resources/db/migration/V1_561__.sql
+++ tietokanta/src/main/resources/db/migration/V1_561__.sql
@@ -19,20 +19,30 @@ CREATE TABLE vv_materiaali (
   lisatieto TEXT -- vapaatekstinä lisätieto
 );
 
+CREATE INDEX vv_materiaali_urakka_idx ON vv_materiaali ("urakka-id");
+
 CREATE TYPE vv_materiaali_muutos AS (
  pvm DATE,
  maara INTEGER,
  lisatieto TEXT
 );
 
+-- Tehdään näkymä, josta saadaan kerralla koko urakan
+-- materiaalilistaus: se kertoo materiaalin, sen alkuperäisen määrän,
+-- nykyisen määrän sekä kerää yhteen taulukkoon kaikki muutokset
+-- vetolaatikossa näytettäväksi.
+
 CREATE VIEW vv_materiaalilistaus AS
   SELECT DISTINCT ON (m1."urakka-id", m1.nimi)
          m1."urakka-id", m1.nimi,
+	 -- Haetaan alkuperäinen määrä (ajallisesti ensimmäinen kirjaus)
          (SELECT maara FROM vv_materiaali m2
            WHERE m2."urakka-id" = m1."urakka-id" AND m2.nimi = m1.nimi
           ORDER BY pvm ASC LIMIT 1) AS "alkuperainen-maara",
-	 	 (SELECT SUM(maara) FROM vv_materiaali m3
-	 	   WHERE m3."urakka-id" = m1."urakka-id" AND m3.nimi = m1.nimi) AS "maara-nyt",
+	 -- Haetaan "nykyinen" määrä: kaikkien kirjausten summa
+	 (SELECT SUM(maara) FROM vv_materiaali m3
+	   WHERE m3."urakka-id" = m1."urakka-id" AND m3.nimi = m1.nimi) AS "maara-nyt",
+	 -- Kerätään kaikki muutokset omaan taulukkoon
 	 (SELECT array_agg(ROW(l.pvm, l.maara, l.lisatieto)::vv_materiaali_muutos)
 	    FROM vv_materiaali l
 	   WHERE l."urakka-id" = m1."urakka-id" AND m1.nimi = l.nimi) AS muutokset
