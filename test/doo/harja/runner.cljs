a982d7f29c699a0987b5500a8bdaf84e78ecb2c9
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-06-19 15:37:33 +0300

    Lisää materiaalinäkymälle testi

--- test/doo/harja/runner.cljs
+++ test/doo/harja/runner.cljs
@@ -45,6 +45,7 @@
             [harja.tiedot.urakka.urakan-tyotunnit-test]
             [harja.ui.validointi-test]
             [harja.views.urakka.suunnittelu.yksikkohintaiset-tyot-test]
+            [harja.views.vesivaylat.urakka.materiaalit-test]
             ))
 
 (doo-tests 'harja.pvm-test
@@ -92,5 +93,6 @@
            'harja.tiedot.urakka.urakan-tyotunnit-test
            'harja.ui.validointi-test
            'harja.views.urakka.suunnittelu.yksikkohintaiset-tyot-test
+           'harja.views.vesivaylat.urakka.materiaalit-test
            ;; uusi testi tähän
            )
