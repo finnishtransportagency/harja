38ced1d53a19da3f2b97459b7931c0ad35409937
Jari Hanhela <jarihanhela@gmail.com>
2017-06-20 08:11:24 +0300

    Poista kuollutta koodis

--- src/clj/harja/palvelin/raportointi/raportit/vesivaylien_laskutusyhteenveto.clj
+++ src/clj/harja/palvelin/raportointi/raportit/vesivaylien_laskutusyhteenveto.clj
@@ -1,8 +1,6 @@
 (ns harja.palvelin.raportointi.raportit.vesivaylien-laskutusyhteenveto
-  (:require [harja.kyselyt.urakat :as urakat-q]
-            [jeesql.core :refer [defqueries]]
+  (:require [jeesql.core :refer [defqueries]]
             [taoensso.timbre :as log]
-            [harja.kyselyt.hallintayksikot :as hallintayksikot-q]
             [harja.domain.vesivaylat.toimenpide :as to]
             [harja.palvelin.raportointi.raportit.yleinen :refer [raportin-otsikko]]
             [clj-time.core :as t]
@@ -75,42 +73,6 @@
    :toimenpiteet (vec (hae-toimenpiteet db {:urakkaid urakka-id
                                             :alkupvm alkupvm
                                             :loppupvm loppupvm}))})
-
-(defn- kk-kasittelyrivi [kk kauppamerenkulku-hinta hinta-muu]
-  [["Kauppamerenkulku"
-    kk
-    [:varillinen-teksti {:arvo "?" :tyyli :virhe}]
-    kauppamerenkulku-hinta]
-   ["Muu"
-    kk
-    [:varillinen-teksti {:arvo "?" :tyyli :virhe}]
-    hinta-muu]])
-
-(defn- kk-kasittelyrivit [raportin-tiedot kk-vali]
-  (let [hinta (fn [hinnoittelut vaylatyyppi kk-vali]
-                (let [osuvat-rivit (filter #(and (or (not (empty? (set/intersection #{vaylatyyppi}
-                                                                                    (:vaylatyyppi %))))
-                                                     (= (:vaylatyyppi %) vaylatyyppi))
-                                                 (or (pvm/valissa? (:ensimmainen-toimenpide %)
-                                                                   (first (:kk-vali kk-vali))
-                                                                   (second (:kk-vali kk-vali)))
-                                                     (pvm/valissa? (:viimeinen-toimenpide %)
-                                                                   (first (:kk-vali kk-vali))
-                                                                   (second (:kk-vali kk-vali)))))
-                                           hinnoittelut)]
-                  (reduce + 0 (map :summa osuvat-rivit))))
-        kauppamerenkulku-hinta (hinta (:yksikkohintaiset raportin-tiedot) "kauppamerenkulku" kk-vali)
-        muu-hinta (hinta (:yksikkohintaiset raportin-tiedot) "muu" kk-vali)]
-    (kk-kasittelyrivi (:kk-tekstina kk-vali) kauppamerenkulku-hinta muu-hinta)))
-
-(defn- hinnoittelun-toimenpiteet [hinnoittelu toimenpiteet]
-  (apply concat
-         [[{:otsikko (or hinnoittelu "Muut")}]
-          (mapv (fn [toimenpide]
-                  [(to/reimari-toimenpidetyyppi-fmt
-                     (get to/reimari-toimenpidetyypit (:toimenpide toimenpide)))
-                   (pvm/pvm-opt (:suoritettu toimenpide))])
-                (sort-by :suoritettu toimenpiteet))]))
 
 (defn suorita [db user {:keys [urakka-id alkupvm loppupvm] :as parametrit}]
   (let [raportin-tiedot (hinnoittelutiedot {:db db
