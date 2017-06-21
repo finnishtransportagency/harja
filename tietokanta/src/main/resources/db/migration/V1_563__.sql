85bccd9cf56ae6f7cbecd725ee734dc2bd63238f
Jari Hanhela <jarihanhela@gmail.com>
2017-06-21 09:42:55 +0300

    Selvitä migraatio-konflikti

--- tietokanta/src/main/resources/db/migration/V1_563__.sql
+++ tietokanta/src/main/resources/db/migration/V1_563__.sql
@@ -1,24 +1,17 @@
-ALTER TABLE reimari_toimenpide ALTER COLUMN "urakka-id" DROP NOT NULL;
-CREATE OR REPLACE FUNCTION toimenpiteen_urakka_id_trigger_proc()
-  RETURNS TRIGGER AS
-$$
-DECLARE id_temp TEXT;
-BEGIN
-  id_temp := (SELECT id FROM urakka u
-    WHERE
-    u.urakkanro = (NEW."reimari-turvalaite").ryhma::text) LIMIT 1;
+CREATE TABLE vv_kiintio(
+  id SERIAL PRIMARY KEY,
+  "urakka-id" INTEGER REFERENCES urakka(id) NOT NULL,
+  "sopimus-id" INTEGER REFERENCES sopimus(id) NOT NULL,
+  nimi VARCHAR(80) NOT NULL,
+  kuvaus VARCHAR (1024),
+  koko INTEGER NOT NULL CONSTRAINT koko_yli_nolla CHECK (koko > 0),
 
-  NEW."urakka-id" = id_temp;
-                -- urakka-id:ksi tulee NULL jos ei löydy, joka on ok
-  RAISE NOTICE 'trigger: urakka-id arvoksi %', NEW."urakka-id";
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-DROP TRIGGER IF EXISTS toimenpiteen_urakka_id_trigger ON reimari_toimenpide;
-CREATE TRIGGER toimenpiteen_urakka_id_trigger
-  BEFORE INSERT OR UPDATE ON reimari_toimenpide
-  FOR EACH ROW
-  EXECUTE PROCEDURE toimenpiteen_urakka_id_trigger_proc();
+  muokkaaja  INTEGER REFERENCES kayttaja (id),
+  muokattu   TIMESTAMP,
+  luoja      INTEGER REFERENCES kayttaja (id) NOT NULL,
+  luotu      TIMESTAMP                        NOT NULL DEFAULT NOW(),
+  poistettu  BOOLEAN                          NOT NULL DEFAULT FALSE,
+  poistaja   INTEGER REFERENCES kayttaja (id));
 
-CREATE UNIQUE INDEX reimari_id_index
-  ON reimari_toimenpide ("reimari-id");
+ALTER TABLE reimari_toimenpide
+  ADD COLUMN "kiintio-id" INTEGER REFERENCES vv_kiintio(id)
