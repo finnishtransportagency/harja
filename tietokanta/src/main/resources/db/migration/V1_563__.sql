ae3704634519ec94655dcb2c9b049ddf655284d1
Jari Hanhela <jarihanhela@gmail.com>
2017-06-21 12:09:49 +0300

    Nimeä tyyppi -> väylätyyppi

--- tietokanta/src/main/resources/db/migration/V1_563__.sql
+++ tietokanta/src/main/resources/db/migration/V1_563__.sql
@@ -1,7 +1,7 @@
 -- Toimenpideinstanssille VV-spesifistä tietoa
 CREATE TABLE toimenpideinstanssi_vesivaylat (
   "toimenpideinstanssi-id" integer NOT NULL REFERENCES toimenpideinstanssi(id),
-   tyyppi VV_VAYLATYYPPI NOT NULL
+   vaylatyyppi VV_VAYLATYYPPI NOT NULL
 );
 
 CREATE INDEX toimenpideinstanssi_vesivaylat_index ON toimenpideinstanssi_vesivaylat ("toimenpideinstanssi-id");
