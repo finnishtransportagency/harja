9df02f02cf26fe3acc0c573a4bbb2b2fa41c609d
Erno Kuusela <erno.kuusela@solita.fi>
2017-06-26 12:06:35 +0000

    poista käyttämätön apuri

--- src/clj/harja/palvelin/integraatiot/reimari/apurit.clj
+++ src/clj/harja/palvelin/integraatiot/reimari/apurit.clj
@@ -19,7 +19,6 @@
     (xml/parsi-paivamaara text)))
 
 (defn formatoi-aika [muutosaika]
-  (log/debug "formatoi-aika: saatiin" muutosaika)
   (let [aika-ilman-vyohyketta (xml/formatoi-xsd-datetime muutosaika)]
     (if (s/ends-with? aika-ilman-vyohyketta "Z")
       aika-ilman-vyohyketta
@@ -34,18 +33,3 @@
                                                                         :harja.palvelin.integraatiot/jarjestelma}] }
                                 {::integraatiotapahtuma/integraatio {:harja.palvelin.integraatiot/jarjestelma jarjestelma
                                                                      :harja.palvelin.integraatiot/nimi nimi}})))))
-
-(defn soap-kutsu! [soap-nimi lokiviesti sanoma-fn kasittele-vastaus-fn]
-  (fn [konteksti db pohja-url kayttajatunnus salasana muutosaika]
-    (let [otsikot {"Content-Type" "text/xml"
-                   "SOAPAction" (str "http://www.liikennevirasto.fi/xsd/harja/reimari/" soap-nimi)}
-          http-asetukset {:metodi :POST
-                          :url pohja-url
-                          :otsikot otsikot
-                          :kayttajatunnus kayttajatunnus
-                          :salasana salasana
-                          :muutosaika muutosaika}
-          {body :body headers :headers} (integraatiotapahtuma/laheta konteksti :http http-asetukset (sanoma-fn muutosaika))]
-      (integraatiotapahtuma/lisaa-tietoja konteksti lokiviesti)
-
-      (kasittele-vastaus-fn db body))))
