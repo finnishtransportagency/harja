d15a2aef4c0ec396eaf749ad6c56e6878e504e98
Erno Kuusela <erno.kuusela@solita.fi>
2017-06-27 13:38:37 +0000

    ennen alustusta palautetaan konservatiivinen "kyllä" kaikkiin ominaisuus-kaytossa? kutsuihin

--- src/clj/harja/palvelin/palvelut/pois_kytketyt_ominaisuudet.clj
+++ src/clj/harja/palvelin/palvelut/pois_kytketyt_ominaisuudet.clj
@@ -1,13 +1,18 @@
 (ns harja.palvelin.palvelut.pois-kytketyt-ominaisuudet
   (:require [com.stuartsierra.component :as component]
             [harja.palvelin.komponentit.http-palvelin :refer [julkaise-palvelu poista-palvelut]]
+            [taoensso.timbre :as log]
             [harja.domain.oikeudet :as oikeudet]))
 
-(defonce pois-kytketyt-ominaisuudet (atom #{}))
+(defonce pois-kytketyt-ominaisuudet (atom nil))
 
 (defn ominaisuus-kaytossa? [k]
   (let [pko @pois-kytketyt-ominaisuudet]
-    (not (and (set? pko) (contains? pko k)))))
+    (if (nil? pko)
+      (do
+        (log/warn "ominaisuus-kaytossa? " k " kutsuttu ennen ko asetusten lukemista")
+        false) ;; ei alustetu vielä -> väitetään kaikkea pois kytketyksi
+      (not (contains? pko k)))))
 
 (defrecord PoisKytketytOminaisuudet [pois-kytketyt-ominaisuudet-joukko]
   component/Lifecycle
