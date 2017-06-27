26a0d23d23f2bed5ed0a3e408c5c36693710728a
Jari Hanhela <jarihanhela@gmail.com>
2017-06-27 13:33:54 +0300

    Merge develop

--- test/cljs/harja/tiedot/vesivaylat/urakka/suunnittelu/kiintiot_test.cljs
+++ test/cljs/harja/tiedot/vesivaylat/urakka/suunnittelu/kiintiot_test.cljs
@@ -22,7 +22,7 @@
   (vaadi-async-kutsut
     #{tiedot/->KiintiotEiHaettu tiedot/->KiintiotHaettu}
     (is (= {:kiintioiden-haku-kaynnissa? true}
-           (e! (tiedot/->HaeKiintiot)))))
+           (e! (tiedot/->HaeKiintiot)))))h
 
   (vaadi-async-kutsut
     #{}
--- tietokanta/src/main/resources/db/migration/V1_567__.sql
+++ tietokanta/src/main/resources/db/migration/V1_567__.sql
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 -- Toimenpideinstanssille VV-spesifistä tietoa
 CREATE TABLE toimenpideinstanssi_vesivaylat (
   "toimenpideinstanssi-id" integer NOT NULL REFERENCES toimenpideinstanssi(id),
-   vaylatyyppi VV_VAYLATYYPPI NOT NULL
+  vaylatyyppi VV_VAYLATYYPPI NOT NULL
 );
 
 CREATE INDEX toimenpideinstanssi_vesivaylat_index ON toimenpideinstanssi_vesivaylat ("toimenpideinstanssi-id");
-=======
-ALTER TABLE reimari_toimenpide DROP COLUMN "lisatyo";
->>>>>>> develop
