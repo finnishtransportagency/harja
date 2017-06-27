7301aa2d9f8b3ac1c00ec4dfdbed16152e7f6544
Erno Kuusela <erno.kuusela@solita.fi>
2017-06-27 06:48:37 +0000

    korjaa komponenttihaun testidata paremmin

--- test/clj/harja/palvelin/integraatiot/reimari/komponenttihaku_test.clj
+++ test/clj/harja/palvelin/integraatiot/reimari/komponenttihaku_test.clj
@@ -16,41 +16,50 @@
 
 (t/use-fixtures :each (t/compose-fixtures ht/tietokanta-fixture jarjestelma-fixture))
 
-(t/deftest kasittele-vastaus-kantatallennus-turvalaitekomponentille
+(t/deftest kasittele-vastaus-kantatallennus
+  (clojure.pprint/pprint (first (sut/kasittele-komponenttityypit-vastaus (:db ht/jarjestelma) (slurp "resources/xsd/reimari/komponenttityypit-vastaus.xml"))))
   (ht/tarkista-map-arvot
    (first (sut/kasittele-komponenttityypit-vastaus (:db ht/jarjestelma) (slurp "resources/xsd/reimari/komponenttityypit-vastaus.xml")))
-   {::kt/loppupvm #inst "2017-08-25T00:00:00.000-00:00",
-    ::kt/luokan-id "283748923",
-    ::kt/luokan-nimi "lamput",
-    ::kt/luokan-luontiaika #inst "2012-02-24T09:42:04.000-00:00",
-    ::kt/luokan-paivitysaika #inst "2017-04-24T09:42:04.000-00:00",
-    ::kt/id "4242",
-    ::kt/muokattu #inst "2017-04-29T00:00:00.000-00:00",
-    ::kt/alkupvm #inst "2011-04-25T00:00:00.000-00:00",
-    ::kt/nimi "Punainen lamppu",
-    ::kt/merk-cod "234",
-    ::kt/luontiaika #inst "2012-03-24T09:11:04.000-00:00",
-    ::kt/luokan-lisatiedot "sähköllä toimivat",
-    ::kt/lisatiedot "hyvä kampe"})
+   {:harja.domain.vesivaylat.komponenttityyppi/loppupvm
+ #inst "2017-08-24T21:00:00.000-00:00",
+ :harja.domain.vesivaylat.komponenttityyppi/luokan-id "283748923",
+ :harja.domain.vesivaylat.komponenttityyppi/luokan-nimi "lamput",
+ :harja.domain.vesivaylat.komponenttityyppi/merk-cod "234",
+ :harja.domain.vesivaylat.komponenttityyppi/luokan-luontiaika
+ #inst "2012-02-24T09:42:04.000-00:00",
+ :harja.domain.vesivaylat.komponenttityyppi/luokan-paivitysaika
+ #inst "2017-04-24T09:42:04.000-00:00",
+ :harja.domain.vesivaylat.komponenttityyppi/id "4242",
+ :harja.domain.vesivaylat.komponenttityyppi/muokattu
+ #inst "2017-04-29T00:00:00.000-00:00",
+ :harja.domain.vesivaylat.komponenttityyppi/alkupvm
+ #inst "2011-04-24T21:00:00.000-00:00",
+ :harja.domain.vesivaylat.komponenttityyppi/nimi "Punainen lamppu",
+ :harja.domain.vesivaylat.komponenttityyppi/luontiaika
+ #inst "2012-03-24T09:11:04.000-00:00",
+ :harja.domain.vesivaylat.komponenttityyppi/luokan-lisatiedot
+ "sähköllä toimivat",
+ :harja.domain.vesivaylat.komponenttityyppi/lisatiedot
+ "hyvä kampe"})
 
+  (clojure.pprint/pprint (first (sut/kasittele-turvalaitekomponentit-vastaus (:db ht/jarjestelma) (slurp "resources/xsd/reimari/turvalaitekomponentit-vastaus.xml"))))
   (ht/tarkista-map-arvot
    (first (sut/kasittele-turvalaitekomponentit-vastaus (:db ht/jarjestelma) (slurp "resources/xsd/reimari/turvalaitekomponentit-vastaus.xml")))
    {:harja.domain.vesivaylat.turvalaitekomponentti/sarjanumero
     "234234423",
     :harja.domain.vesivaylat.turvalaitekomponentti/loppupvm
-    #inst "2017-07-20T00:00:00.000-00:00",
+    #inst "2017-07-19T21:00:00.000-00:00",
     :harja.domain.vesivaylat.turvalaitekomponentti/turvalaitenro "234",
     :harja.domain.vesivaylat.turvalaitekomponentti/valiaikainen false,
     :harja.domain.vesivaylat.turvalaitekomponentti/luoja "Aatos",
-    :harja.domain.vesivaylat.turvalaitekomponentti/komponentti-id
-    "4242",
+    :harja.domain.vesivaylat.turvalaitekomponentti/komponentti-id "4242",
     :harja.domain.vesivaylat.turvalaitekomponentti/id "9595",
     :harja.domain.vesivaylat.turvalaitekomponentti/alkupvm
-    #inst "2011-05-01T00:00:00.000-00:00",
+    #inst "2011-04-30T21:00:00.000-00:00",
     :harja.domain.vesivaylat.turvalaitekomponentti/muokkaaja "Vilho",
     :harja.domain.vesivaylat.turvalaitekomponentti/muokattu
-    #inst "2016-07-20T00:00:00.000-00:00",
+    #inst "2016-07-19T22:00:00.000-00:00",
     :harja.domain.vesivaylat.turvalaitekomponentti/luontiaika
-    #inst "2010-05-01T01:00:00.000-00:00",
+    #inst "2010-04-30T23:00:00.000-00:00",
     :harja.domain.vesivaylat.turvalaitekomponentti/lisatiedot
     "asennettu kivasti"}))
