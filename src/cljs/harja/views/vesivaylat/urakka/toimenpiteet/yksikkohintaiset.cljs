ba0ca05130ad57fa19863ca46348429a181cd6c4
Jari Hanhela <jarihanhela@gmail.com>
2017-06-28 13:11:41 +0300

    Käytä UI:lla "Matkakulut" nimitystä matkoista

--- src/cljs/harja/views/vesivaylat/urakka/toimenpiteet/yksikkohintaiset.cljs
+++ src/cljs/harja/views/vesivaylat/urakka/toimenpiteet/yksikkohintaiset.cljs
@@ -89,7 +89,7 @@
 ;;;;;;;;;;;;
 ;; Hinnan antamisen leijuke
 
-(defn- kenttarivi [e! app* otsikko]
+(defn- kenttarivi [e! app* otsikko tunniste]
   [:tr
    [:td [:b otsikko]]
    [:td
@@ -97,9 +97,9 @@
      [tee-kentta {:tyyppi :numero :kokonaisosan-maara 7}
       (r/wrap (hinta/hinnan-maara
                 (get-in app* [:hinnoittele-toimenpide ::h/hintaelementit])
-                otsikko)
+                tunniste)
               (fn [uusi]
-                (e! (tiedot/->HinnoitteleToimenpideKentta {::hinta/otsikko otsikko
+                (e! (tiedot/->HinnoitteleToimenpideKentta {::hinta/otsikko tunniste
                                                            ::hinta/maara uusi}))))]
      [:span " "]
      "€"]]
@@ -107,12 +107,12 @@
     [tee-kentta {:tyyppi :checkbox}
      (r/wrap (if-let [yleiskustannuslisa (hinta/hinnan-yleiskustannuslisa
                                            (get-in app* [:hinnoittele-toimenpide ::h/hintaelementit])
-                                           otsikko)]
+                                           tunniste)]
                (pos? yleiskustannuslisa)
                false)
              (fn [uusi]
                (e! (tiedot/->HinnoitteleToimenpideKentta
-                     {::hinta/otsikko otsikko
+                     {::hinta/otsikko tunniste
                       ::hinta/yleiskustannuslisa (if uusi
                                                    hinta/yleinen-yleiskustannuslisa
                                                    0)}))))]]])
@@ -136,11 +136,11 @@
              [:th {:style {:width "35%"}} "Hinta"]
              [:th {:style {:width "20%"}} "Yleis\u00ADkustan\u00ADnusli\u00ADsä"]]]
            [:tbody
-            [kenttarivi e! app* "Työ"]
-            [kenttarivi e! app* "Komponentit"]
-            [kenttarivi e! app* "Yleiset materiaalit"]
-            [kenttarivi e! app* "Matkat"]
-            [kenttarivi e! app* "Muut kulut"]]]
+            [kenttarivi e! app* "Työ" "Työ"]
+            [kenttarivi e! app* "Komponentit" "Komponentit"]
+            [kenttarivi e! app* "Yleiset materiaalit" "Yleiset materiaalit"]
+            [kenttarivi e! app* "Matkakulut" "Matkat"]
+            [kenttarivi e! app* "Muut kulut" "Muut kulut"]]]
 
           [:div {:style {:margin-top "1em" :margin-bottom "1em"}}
            [yleiset/tietoja {:tietokentan-leveys "180px"}
