1dc66cff3b3c47e99735056a4b05f681a9c87692
Jari Hanhela <jarihanhela@gmail.com>
2017-07-04 16:05:50 +0300

    Peru vahingossa commitoitu runner.cljs

--- test/doo/harja/runner.cljs
+++ test/doo/harja/runner.cljs
@@ -48,7 +48,51 @@
             [harja.views.vesivaylat.urakka.materiaalit-test]
             ))
 
-(doo-tests
+(doo-tests 'harja.pvm-test
+           'harja.ui.dom-test
+           'harja.tiedot.urakka.toteumat.tiemerkinta-muut-kustannukset-test
+           'harja.tiedot.urakka.suunnittelu-test
+           'harja.tiedot.urakka.yhatuonti-test
+           'harja.tiedot.muokkauslukko-test
+           'harja.views.kartta.infopaneeli-test
+           'harja.views.urakka.siltatarkastukset-test
+           'harja.views.urakka.paallystysilmoitukset-test
+           'harja.views.urakka.paikkausilmoitukset-test
+           'harja.views.urakka.yllapitokohteet-test
+           'harja.views.urakka.valitavoitteet-test
+           'harja.views.urakka.yleiset-test
+           'harja.ui.historia-test
+           'harja.ui.kentat-test
+           'harja.ui.grid-test
+           'harja.ui.edistymispalkki-test
+           'harja.fmt-test
+           'harja.tiedot.urakka.siirtymat-test
+           'harja.tiedot.tierekisteri.varusteet-test
+           'harja.ui.kartta.infopaneelin-sisalto-test
+           'harja.tiedot.tilannekuva.tienakyma-test
+           'harja.tiedot.urakka.aikataulu-test
+           'harja.views.kartta-test
+           'harja.tiedot.tilannekuva.tilannekuva-test
+           'harja.views.urakka.tiemerkinnan-yksikkohintaiset-tyot-test
+           'harja.views.ilmoitukset.tietyoilmoitushakulomake-test
+           'harja.views.ilmoitukset.tietyoilmoituslomake-test
+           'harja.views.kartta.tasot-test
+           'harja.tiedot.urakka.yllapitokohteet-test
+           'harja.ui.kartta.esitettavat-asiat-test
+           'harja.views.urakka.paallystyksen-maksuerat-test
+           'harja.tiedot.vesivaylat.hallinta.urakoiden-luonti-test
+           'harja.tiedot.vesivaylat.hallinta.urakoitsijoiden-luonti-test
+           'harja.tiedot.vesivaylat.hallinta.hankkeiden-luonti-test
+           'harja.tiedot.vesivaylat.hallinta.sopimuksien-luonti-test
+           'harja.tiedot.vesivaylat.urakka.turvalaitteet-test
+           'harja.tiedot.vesivaylat.urakka.toimenpiteet.yksikkohintaiset-test
+           'harja.tiedot.vesivaylat.urakka.toimenpiteet.kokonaishintaiset-test
+           'harja.tiedot.hallintayksikot-test
+           'harja.tiedot.vesivaylat.urakka.laadunseuranta.viat-test
+           'harja.tiedot.urakka.urakan-tyotunnit-test
+           'harja.ui.validointi-test
+           'harja.views.urakka.suunnittelu.yksikkohintaiset-tyot-test
            'harja.tiedot.vesivaylat.urakka.suunnittelu.kiintiot-test
+           'harja.views.vesivaylat.urakka.materiaalit-test
            ;; uusi testi tähän
            )
