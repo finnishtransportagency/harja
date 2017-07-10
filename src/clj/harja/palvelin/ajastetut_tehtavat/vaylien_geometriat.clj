ffac8f87658d2dbdb80dbb8c28995f9506529c2f
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-07-10 15:04:07 +0300

    Lisää komponentti ja asetukset väylien geometrioiden hakemiselle

--- src/clj/harja/palvelin/ajastetut_tehtavat/vaylien_geometriat.clj
+++ src/clj/harja/palvelin/ajastetut_tehtavat/vaylien_geometriat.clj
@@ -1,4 +1,4 @@
-(ns harja.palvelin.ajastetut-tehtavat.turvalaitteiden-geometriat
+(ns harja.palvelin.ajastetut-tehtavat.vaylien-geometriat
   (:require [taoensso.timbre :as log]
             [com.stuartsierra.component :as component]
             [harja.palvelin.tyokalut.ajastettu-tehtava :as ajastettu-tehtava]
@@ -8,11 +8,11 @@
             [harja.palvelin.integraatiot.integraatiotapahtuma :as integraatiotapahtuma]
             [cheshire.core :as cheshire]
             [clojure.java.jdbc :as jdbc]
-            [harja.kyselyt.turvalaitteet :as q-turvalaitteet]
+            [harja.kyselyt.vaylat :as q-vaylat]
             [harja.geo :as geo])
   (:import (org.postgis Point)))
 
-(def geometriapaivitystunnus "turvalaitteet")
+(def geometriapaivitystunnus "vaylat")
 (defn paivitys-tarvitaan? [db paivitysvali-paivissa]
   (let [viimeisin-paivitys (c/from-sql-time
                              (:viimeisin_paivitys
@@ -20,39 +20,39 @@
     (or (nil? viimeisin-paivitys)
         (>= (pvm/paivia-valissa viimeisin-paivitys (pvm/nyt-suomessa)) paivitysvali-paivissa))))
 
-(defn tallenna-turvalaite [db {:keys [id geometry properties]}]
+(defn tallenna-vayla [db {:keys [id geometry properties]}]
   (let [koordinaatit (:coordinates geometry)
         geometria (geo/geometry (Point. (first koordinaatit) (second koordinaatit)))
         arvot (cheshire/encode properties)
         sql-parametrit {:sijainti geometria
                         :tunniste id
                         :arvot arvot}]
-    (q-turvalaitteet/luo-turvalaite<! db sql-parametrit)))
+    (q-vaylat/luo-vayla<! db sql-parametrit)))
 
-(defn kasittele-turvalaitteet [db vastaus]
+(defn kasittele-vaylat [db vastaus]
   (let [data (cheshire/decode vastaus keyword)
-        turvalaitteet (get data :features)]
+        vaylat (get data :features)]
     (jdbc/with-db-transaction [db db]
-      (q-turvalaitteet/poista-turvalaitteet! db)
-      (doseq [turvalaite turvalaitteet]
-        (tallenna-turvalaite db turvalaite))
+      (q-vaylat/poista-vaylat! db)
+      (doseq [vayla vaylat]
+        (tallenna-vayla db vayla))
       (q-geometriapaivitykset/paivita-viimeisin-paivitys db geometriapaivitystunnus (harja.pvm/nyt)))))
 
-(defn paivita-turvalaitteet [integraatioloki db url]
-  (log/debug "Päivitetään turvalaitteiden geometriat")
+(defn paivita-vaylat [integraatioloki db url]
+  (log/debug "Päivitetään väylien geometriat")
   (integraatiotapahtuma/suorita-integraatio
     db
     integraatioloki
     "inspire"
-    "turvalaitteiden-haku"
+    "vaylien-haku"
     (fn [konteksti]
       (let [http-asetukset {:metodi :GET :url url}
             {vastaus :body} (integraatiotapahtuma/laheta konteksti :http http-asetukset)]
-        (kasittele-turvalaitteet db vastaus))))
-  (log/debug "Turvalaitteidein päivitys tehty"))
+        (kasittele-vaylat db vastaus))))
+  (log/debug "Väylien päivitys tehty"))
 
-(defn- turvalaitteiden-geometriahakutehtava [integraatioloki db url paivittainen-tarkistusaika paivitysvali-paivissa]
-  (log/debug (format "Ajastetaan turvalaitteiden geometrioiden haku tehtäväksi %s päivän välein osoitteesta: %s."
+(defn- vaylien-geometriahakutehtava [integraatioloki db url paivittainen-tarkistusaika paivitysvali-paivissa]
+  (log/debug (format "Ajastetaan väylien geometrioiden haku tehtäväksi %s päivän välein osoitteesta: %s."
                      paivitysvali-paivissa
                      url))
 
@@ -61,19 +61,19 @@
      paivittainen-tarkistusaika
      (fn [_]
        (when (paivitys-tarvitaan? db paivitysvali-paivissa)
-         (paivita-turvalaitteet integraatioloki db url))))))
+         (paivita-vaylat integraatioloki db url))))))
 
-(defrecord TurvalaitteidenGeometriahaku [url paivittainen-tarkistusaika paivitysvali-paivissa]
+(defrecord VaylienGeometriahaku [url paivittainen-tarkistusaika paivitysvali-paivissa]
   component/Lifecycle
   (start [{:keys [integraatioloki db] :as this}]
-    (assoc this :turvalaitteiden-geometriahaku
-                (turvalaitteiden-geometriahakutehtava
+    (assoc this :vaylien-geometriahaku
+                (vaylien-geometriahakutehtava
                   integraatioloki
                   db
                   url
                   paivittainen-tarkistusaika
                   paivitysvali-paivissa)))
   (stop [this]
-    (when-let [lopeta-fn (:turvalaitteiden-geometriahaku this)]
+    (when-let [lopeta-fn (:vaylien-geometriahaku this)]
       (lopeta-fn))
     this))
