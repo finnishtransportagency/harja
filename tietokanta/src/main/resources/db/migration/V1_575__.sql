eab967d30b44f37b6a925fd5c2468017ed252888
Erno Kuusela <erno.kuusela@solita.fi>
2017-07-11 10:08:14 +0000

    WIP urakkalinkkaus

--- tietokanta/src/main/resources/db/migration/V1_575__.sql
+++ tietokanta/src/main/resources/db/migration/V1_575__.sql
@@ -40,3 +40,25 @@ CREATE TRIGGER toimenpiteen_linkit_trigger
   BEFORE INSERT OR UPDATE ON reimari_toimenpide
   FOR EACH ROW
   EXECUTE PROCEDURE toimenpiteen_linkit_trigger_proc();
+
+
+CREATE OR REPLACE FUNCTION toimenpiteen_urakka_id_trigger_proc()
+  RETURNS TRIGGER AS
+$$
+DECLARE id_temp TEXT;
+BEGIN
+  id_temp := (SELECT id FROM urakka u, sopimus s, reimari_sopimuslinkki rsl
+    WHERE ?? LIMIT 1;
+
+  IF id_temp IS NULL THEN
+    RAISE NOTICE 'trigger: linkataan urakka-id turvalaiteryhmällä koska sopimus/diaarinro linkkaus ei onnistunut';
+    id_temp := (SELECT id FROM urakka u
+      WHERE
+      u.urakkanro = (NEW."reimari-turvalaite").ryhma::text) LIMIT 1;
+  END IF;
+  NEW."urakka-id" = id_temp;
+                -- urakka-id:ksi tulee NULL jos ei löydy, joka on ok
+  RAISE NOTICE 'trigger: urakka-id arvoksi %', NEW."urakka-id";
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
