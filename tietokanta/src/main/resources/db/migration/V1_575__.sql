db67311828b69dc880d3ef46796c7f838ac88552
Jari Hanhela <jarihanhela@gmail.com>
2017-07-12 09:58:08 +0300

    Korjaa viittauksen nimi

--- tietokanta/src/main/resources/db/migration/V1_575__.sql
+++ tietokanta/src/main/resources/db/migration/V1_575__.sql
@@ -1,5 +1,5 @@
 CREATE TABLE reimari_toimenpide_liite (
-  "reimari-toimenpide-id" INTEGER REFERENCES reimari_toimenpide (id) NOT NULL,
+  "toimenpide-id" INTEGER REFERENCES reimari_toimenpide (id) NOT NULL,
   liite INTEGER REFERENCES liite (id) NOT NULL,
   poistettu BOOLEAN NOT NULL DEFAULT FALSE
 );
