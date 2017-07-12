76577523fe2344d233244dd2fafa73f11539d89d
Jari Hanhela <jarihanhela@gmail.com>
2017-07-12 10:05:55 +0300

    Korjaa virheellinen liite-sarakkeen nimi

--- tietokanta/src/main/resources/db/migration/V1_575__.sql
+++ tietokanta/src/main/resources/db/migration/V1_575__.sql
@@ -1,5 +1,5 @@
 CREATE TABLE reimari_toimenpide_liite (
   "toimenpide-id" INTEGER REFERENCES reimari_toimenpide (id) NOT NULL,
-  liite INTEGER REFERENCES liite (id) NOT NULL,
+  "liite-id" INTEGER REFERENCES liite (id) NOT NULL,
   poistettu BOOLEAN NOT NULL DEFAULT FALSE
 );
