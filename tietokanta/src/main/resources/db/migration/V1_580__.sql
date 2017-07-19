4fa5c635b0d0556f54dae4f53b306e7cbac8dc28
Jari Hanhela <Jarzka@users.noreply.github.com>
2017-07-19 09:37:29 +0300

    Update and rename V1_578__.sql to V1_580__.sql

--- tietokanta/src/main/resources/db/migration/V1_580__.sql
+++ tietokanta/src/main/resources/db/migration/V1_580__.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- Lisää vesiväyläurakoiden toimenpideinstansseilla sopivat toimenpidekoodit (Kaukon "MERIVÄYLIEN URAKKARAKENNE HARJASSA - EHDOTUS - 24.4.2017" Excelin pohjalta)
 
 INSERT INTO toimenpidekoodi (taso, emo, nimi) VALUES (3, (SELECT id
@@ -62,44 +61,3 @@ $$ LANGUAGE plpgsql;
 SELECT * FROM lisaa_vv_urakoille_toimenpiteet();
 
 DROP FUNCTION lisaa_vv_urakoille_toimenpiteet();
-=======
-CREATE TABLE reimari_sopimuslinkki (
-"harja-sopimus-id"     INTEGER NOT NULL
-                       UNIQUE
-                       REFERENCES sopimus,
-"reimari-sopimus-id"   INTEGER NOT NULL
-                       UNIQUE);
-
-DROP TRIGGER IF EXISTS toimenpiteen_sopimus_id_trigger ON reimari_toimenpide;
-DROP FUNCTION IF EXISTS toimenpiteen_sopimus_id_trigger_proc();
-
-CREATE OR REPLACE FUNCTION toimenpiteen_linkit_trigger_proc()
-  RETURNS TRIGGER AS
-$$
-DECLARE id_temp INTEGER;
-BEGIN
-  id_temp := (SELECT id FROM sopimus hs, reimari_sopimuslinkki sl
-    WHERE
-      sl."harja-sopimus-id" = hs.id AND
-      sl."reimari-sopimus-id" = (NEW."reimari-sopimus").nro LIMIT 1);
-
-  NEW."sopimus-id" = id_temp;
-                -- id:ksi tulee NULL jos ei löydy, joka on ok
-  RAISE NOTICE 'reimari_toimenpide linkit trigger: sopimus-id arvoksi %', NEW."sopimus-id";
-
-  id_temp := (SELECT id FROM vv_turvalaite
-               WHERE tunniste IS NOT NULL AND tunniste = (NEW."reimari-turvalaite").nro LIMIT 1);
-
-  NEW."turvalaite-id" = id_temp;
-                -- id:ksi tulee NULL jos ei löydy, joka on ok
-  RAISE NOTICE 'reimari_toimenpide linkit trigger: turvalaite-id arvoksi %', NEW."turvalaite-id";
-
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-DROP TRIGGER IF EXISTS toimenpiteen_linkit_trigger ON reimari_toimenpide;
-CREATE TRIGGER toimenpiteen_linkit_trigger
-  BEFORE INSERT OR UPDATE ON reimari_toimenpide
-  FOR EACH ROW
-  EXECUTE PROCEDURE toimenpiteen_linkit_trigger_proc();
->>>>>>> develop
