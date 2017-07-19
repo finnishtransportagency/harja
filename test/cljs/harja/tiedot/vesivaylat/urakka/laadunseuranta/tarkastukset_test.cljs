82bb31fa5ceeb53db3aa8d4a28ae28a0054726ab
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-07-19 08:22:37 +0300

    Poista turhat tiedostot

--- test/cljs/harja/tiedot/vesivaylat/urakka/laadunseuranta/tarkastukset_test.cljs
+++
@@ -1,8 +0,0 @@
-(ns harja.tiedot.vesivaylat.urakka.laadunseuranta.tarkastukset-test
-  (:require [harja.tiedot.vesivaylat.urakka.laadunseuranta.tarkastukset :as tiedot]
-            [clojure.test :refer-macros [deftest is testing]]
-            [harja.testutils.tuck-apurit :refer-macros [vaadi-async-kutsut] :refer [e!]]))
-
-(deftest nakymaan-tuleminen
-  (is (true? (:nakymassa? (e! (tiedot/->Nakymassa? true)))))
-  (is (false? (:nakymassa? (e! (tiedot/->Nakymassa? false))))))
