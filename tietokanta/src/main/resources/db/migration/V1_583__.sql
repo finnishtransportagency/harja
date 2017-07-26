53af61092cd530407d86de5629661856824b5dc9
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-07-26 10:32:27 +0300

    Käytä olemassaolevaa 'VV111' tpi

--- tietokanta/src/main/resources/db/migration/V1_583__.sql
+++ tietokanta/src/main/resources/db/migration/V1_583__.sql
@@ -1,15 +1,14 @@
 -- Lisää vesiväyläurakoiden toimenpideinstansseilla sopivat toimenpidekoodit (Kaukon "MERIVÄYLIEN URAKKARAKENNE HARJASSA - EHDOTUS - 24.4.2017" Excelin pohjalta)
 
-INSERT INTO toimenpidekoodi (taso, emo, nimi) VALUES (3, (SELECT id
+INSERT INTO toimenpidekoodi (koodi, taso, emo, nimi) VALUES ('VV112', 3, (SELECT id
                                                           FROM toimenpidekoodi
                                                           WHERE nimi = 'Väylänhoito'), 'Kauppamerenkulun kustannukset');
-INSERT INTO toimenpidekoodi (taso, emo, nimi) VALUES (3, (SELECT id
+INSERT INTO toimenpidekoodi (koodi, taso, emo, nimi) VALUES ('VV113', 3, (SELECT id
                                                           FROM toimenpidekoodi
                                                           WHERE nimi = 'Väylänhoito'),
                                                       'Muun vesiliikenteen kustannukset');
-INSERT INTO toimenpidekoodi (taso, emo, nimi) VALUES (3, (SELECT id
-                                                          FROM toimenpidekoodi
-                                                          WHERE nimi = 'Väylänhoito'), 'Urakan yhteiset kustannukset');
+
+UPDATE toimenpidekoodi SET nimi = 'Urakan yhteiset kustannukset' WHERE koodi='VV111';
 
 -- Lisää olemassa oleville VV-urakoille toimenpideinstanssit
 
@@ -60,7 +59,7 @@ BEGIN
              FROM toimenpidekoodi
              WHERE nimi = 'Urakan yhteiset kustannukset'),
             urakka_rivi.alkupvm,
-            urakka_rivi.loppupvm);
+            urakka_rivi.loppupvm) ON CONFLICT DO NOTHING;
 
   END LOOP;
   RETURN;
