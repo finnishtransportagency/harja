e838bcab6357ddce8a043f51384f6410b86b18d0
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-07-30 09:31:25 +0300

    Lisää testidataan materiaaleja

---
+++ tietokanta/testidata/vesivaylat/vesivaylien_materiaalit.sql
@@ -0,0 +1,3 @@
+INSERT INTO vv_materiaali(nimi, maara, pvm, "urakka-id", luoja, luotu)
+    VALUES ('Bensakanisteri', 100, '2017-06-06', (SELECT id FROM urakka WHERE nimi ='Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'), (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'), NOW())
+    VALUES ('Hiekkasäkki', 30, '2017-06-06', (SELECT id FROM urakka WHERE nimi ='Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'), (SELECT id FROM kayttaja WHERE kayttajanimi = 'tero'), NOW())
