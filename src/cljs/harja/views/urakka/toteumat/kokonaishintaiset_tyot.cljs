afd577442d415e964fff92e495150adb7ad5eb51
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2017-08-02 10:00:30 +0300

    Salli kht-lomakkeen tallennus vain jos lomaketta muokattu

--- src/cljs/harja/views/urakka/toteumat/kokonaishintaiset_tyot.cljs
+++ src/cljs/harja/views/urakka/toteumat/kokonaishintaiset_tyot.cljs
@@ -161,6 +161,7 @@
                                              ;; Päiväkohtaiset tiedot vanhentui -> tyhjennä
                                              (reset! tiedot/toteumien-paivakohtaiset-tiedot {}))
                              :disabled     (or (not (lomake/voi-tallentaa? tiedot))
+                                               (not (lomake/muokattu? tiedot))
                                                jarjestelman-lisaama-toteuma?
                                                (not (oikeudet/voi-kirjoittaa? oikeudet/urakat-toteumat-kokonaishintaisettyot (:id @nav/valittu-urakka))))}])}
            ;; lisatieto, suorittaja {ytunnus, nimi}, pituus
