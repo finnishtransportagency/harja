96afa8691668f718d16d0d5f5a9e2862e2fe465a
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-08-04 10:28:03 +0300

    Näytä toimenpiteen pvm varmistusdialogissa

--- src/cljs/harja/views/vesivaylat/urakka/toimenpiteet/jaettu.cljs
+++ src/cljs/harja/views/vesivaylat/urakka/toimenpiteet/jaettu.cljs
@@ -36,8 +36,10 @@
      [:p varmistusteksti-header]
      [:ul
       (for* [toimenpide naytettavat-toimenpiteet]
-        [:li (str (to/reimari-toimenpidetyyppi-fmt (::to/toimenpide toimenpide)) ". " (when toimenpide-lisateksti-fn
-                                                                                        (toimenpide-lisateksti-fn toimenpide)))])
+        [:li (str (pvm/pvm-opt (::to/pvm toimenpide)) " "
+                  (to/reimari-toimenpidetyyppi-fmt (::to/toimenpide toimenpide)) ". "
+                  (when toimenpide-lisateksti-fn
+                    (toimenpide-lisateksti-fn toimenpide)))])
       (when (> (count varmistettavat-toimenpiteet) nayta-max)
         [:li (str "...sekä " (- (count varmistettavat-toimenpiteet) nayta-max) " muuta toimenpidettä.")])]
      [:p varmistusteksti-footer]]))
