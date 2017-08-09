74afab585681bdf6e247220db31dcd02439659c3
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-08-09 13:21:22 +0300

    Tarkista poistaja oikeaoppisesti

--- tietokanta/src/main/resources/db/migration/V1_591__.sql
+++ tietokanta/src/main/resources/db/migration/V1_591__.sql
@@ -1,3 +1,4 @@
 -- vv_materiaali-tauluun poistettu ja poistaja
 ALTER TABLE vv_materiaali ADD COLUMN poistettu BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE vv_materiaali ADD COLUMN poistaja INTEGER REFERENCES kayttaja (id) NOT NULL;
+ALTER TABLE vv_materiaali ADD COLUMN poistaja INTEGER REFERENCES kayttaja (id);
+ALTER TABLE vv_materiaali ADD CONSTRAINT poistaja_olemassa CHECK (poistettu IS NOT TRUE OR (poistettu IS TRUE AND poistaja IS NOT NULL));
