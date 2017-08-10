8b12c9f70fb3bcb98249ff2dab779692a2c8ae15
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-08-10 09:17:26 +0300

    Lisää failaava testi matskun poistolle

--- test/clj/harja/palvelin/palvelut/vesivaylat/materiaalit_test.clj
+++ test/clj/harja/palvelin/palvelut/vesivaylat/materiaalit_test.clj
@@ -1,7 +1,7 @@
 (ns harja.palvelin.palvelut.vesivaylat.materiaalit-test
   (:require [harja.palvelin.palvelut.vesivaylat.materiaalit :as sut]
             [clojure.test :as t :refer [deftest is]]
-            [harja.testi :refer [jarjestelma kutsu-palvelua] :as testi]
+            [harja.testi :refer [jarjestelma kutsu-palvelua q-map] :as testi]
             [com.stuartsierra.component :as component]
             [harja.palvelin.palvelut.vesivaylat.materiaalit :as vv-materiaalit]
             [harja.palvelin.komponentit.tietokanta :as tietokanta]
@@ -52,29 +52,49 @@
                gen/string-alphanumeric)))
 
 (deftest materiaalen-kirjaus-ja-haku
-  (loop [testauskerrat 50
-         generoidut-materiaalit []]
+  (let [urakka-id (testi/hae-helsingin-vesivaylaurakan-id)]
+    (loop [testauskerrat 50
+           generoidut-materiaalit []]
 
-    ;; Assertoi, että palvelun kautta haettu ja generoiduista materiaaleista laskettu
-    ;; summa on sama kaikille materiaaleille
-    (let [haettu-listaus (kutsu-palvelua (:http-palvelin jarjestelma)
-                                         :hae-vesivayla-materiaalilistaus
-                                         testi/+kayttaja-jvh+
-                                         {::m/urakka-id 1})
+      ;; Assertoi, että palvelun kautta haettu ja generoiduista materiaaleista laskettu
+      ;; summa on sama kaikille materiaaleille
+      (let [haettu-listaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                           :hae-vesivayla-materiaalilistaus
+                                           testi/+kayttaja-jvh+
+                                           {::m/urakka-id urakka-id})
 
-          listauksen-maara (fmap (comp ::m/maara-nyt first)
-                                 (group-by ::m/nimi haettu-listaus))
+            listauksen-maara (fmap (comp ::m/maara-nyt first)
+                                   (group-by ::m/nimi haettu-listaus))
 
-          generoidut-maarat (fmap #(reduce + 0 (map ::m/maara %))
-                                  (group-by ::m/nimi generoidut-materiaalit))]
+            generoidut-maarat (fmap #(reduce + 0 (map ::m/maara %))
+                                    (group-by ::m/nimi generoidut-materiaalit))]
 
-      (is (= listauksen-maara generoidut-maarat))
+        (is (= listauksen-maara generoidut-maarat))
 
-      (when (pos? testauskerrat)
-        ;; Generoi ja kirjaa uusi materiaali
-        (let [m (gen/generate testimateriaali-gen)]
-          (kutsu-palvelua (:http-palvelin jarjestelma)
-                          :kirjaa-vesivayla-materiaali
+        (when (pos? testauskerrat)
+          ;; Generoi ja kirjaa uusi materiaali
+          (let [m (gen/generate testimateriaali-gen)]
+            (kutsu-palvelua (:http-palvelin jarjestelma)
+                            :kirjaa-vesivayla-materiaali
+                            testi/+kayttaja-jvh+
+                            m)
+            (recur (dec testauskerrat) (conj generoidut-materiaalit m))))))))
+
+(deftest materiaalien-poisto
+  (let [urakka-id (testi/hae-helsingin-vesivaylaurakan-id)
+        poistettava-materiaali-ennen (first (q-map "SELECT id, poistettu FROM vv_materiaali WHERE poistettu IS NOT TRUE LIMIT 1"))
+        materiaalien-lkm-ennen (:maara (first (q-map "SELECT COUNT(*) as maara FROM vv_materiaali WHERE poistettu IS NOT TRUE")))
+        _ (kutsu-palvelua (:http-palvelin jarjestelma)
+                          :poista-materiaalikirjaus
                           testi/+kayttaja-jvh+
-                          m)
-          (recur (dec testauskerrat) (conj generoidut-materiaalit m)))))))
+                          {::m/urakka-id urakka-id
+                           ::m/id (:id poistettava-materiaali-ennen)})
+        poistettava-materiaali-jalkeen (first (q-map "SELECT id, poistettu FROM vv_materiaali LIMIT 1"))
+        materiaalien-lkm-jalkeen (:maara (first (q-map "SELECT COUNT(*) as maara FROM vv_materiaali WHERE poistettu IS NOT TRUE")))]
+
+    ;; Ei-poistettu materiaali merkittiin poistetuksi
+    (is (false? (:poistettu poistettava-materiaali-ennen)))
+    (is (true? (:poistettu poistettava-materiaali-jalkeen)))
+
+    ;; Muita matskuja ei poistettu
+    (is (= materiaalien-lkm-ennen (+ materiaalien-lkm-jalkeen 1)))))
