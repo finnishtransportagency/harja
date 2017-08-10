94a6b7212e94188f0864be4ef38b3ed9ff59fd64
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-08-10 08:30:14 +0300

    Nimeä materiaalin testin bindingit uudelleen

--- test/clj/harja/palvelin/palvelut/vesivaylat/materiaalit_test.clj
+++ test/clj/harja/palvelin/palvelut/vesivaylat/materiaalit_test.clj
@@ -23,8 +23,8 @@
                         :http-palvelin (testi/testi-http-palvelin)
                         :pois-kytketyt-ominaisuudet testi/testi-pois-kytketyt-ominaisuudet
                         :vv-materiaalit (component/using
-                                         (vv-materiaalit/->Materiaalit)
-                                         [:db :http-palvelin])))))
+                                          (vv-materiaalit/->Materiaalit)
+                                          [:db :http-palvelin])))))
 
   (testit)
   (alter-var-root #'jarjestelma component/stop))
@@ -40,23 +40,20 @@
 
 (def testimateriaali-gen
   (gen/fmap
-   (fn [[nimi maara pvm lisatieto]]
-     {::m/urakka-id 1
-      ::m/nimi nimi
-      ::m/maara maara
-      ::m/pvm pvm
-      ::m/lisatieto lisatieto})
-   (gen/tuple (gen/elements #{"poiju" "viitta" "akku"})
-              (gen/choose -1000 1000)
-              pvm-gen
-              gen/string-alphanumeric)))
+    (fn [[nimi maara pvm lisatieto]]
+      {::m/urakka-id 1
+       ::m/nimi nimi
+       ::m/maara maara
+       ::m/pvm pvm
+       ::m/lisatieto lisatieto})
+    (gen/tuple (gen/elements #{"poiju" "viitta" "akku"})
+               (gen/choose -1000 1000)
+               pvm-gen
+               gen/string-alphanumeric)))
 
 (deftest materiaalen-kirjaus-ja-haku
-  (loop [;; kuinka monta kertaa testataan
-         i 50
-
-         ;; generoidut materiaalit
-         materiaalit []]
+  (loop [testauskerrat 50
+         generoidut-materiaalit []]
 
     ;; Assertoi, että palvelun kautta haettu ja generoiduista materiaaleista laskettu
     ;; summa on sama kaikille materiaaleille
@@ -69,15 +66,15 @@
                                  (group-by ::m/nimi haettu-listaus))
 
           generoidut-maarat (fmap #(reduce + 0 (map ::m/maara %))
-                                  (group-by ::m/nimi materiaalit))]
+                                  (group-by ::m/nimi generoidut-materiaalit))]
 
       (is (= listauksen-maara generoidut-maarat))
 
-      (when (pos? i)
+      (when (pos? testauskerrat)
         ;; Generoi ja kirjaa uusi materiaali
         (let [m (gen/generate testimateriaali-gen)]
           (kutsu-palvelua (:http-palvelin jarjestelma)
                           :kirjaa-vesivayla-materiaali
                           testi/+kayttaja-jvh+
                           m)
-          (recur (dec i) (conj materiaalit m)))))))
+          (recur (dec testauskerrat) (conj generoidut-materiaalit m)))))))
