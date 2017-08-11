59c18b4968916488a5e6183e412322b0622743fa
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-08-11 21:36:28 +0300

    Muuta häiriöilmoituksen aikaleima timestampiksi

--- tietokanta/src/main/resources/db/migration/V1_593__.sql
+++ tietokanta/src/main/resources/db/migration/V1_593__.sql
@@ -2,6 +2,6 @@
 CREATE TABLE hairioilmoitus (
   id        SERIAL PRIMARY KEY,
   viesti    VARCHAR(1024) NOT NULL,
-  pvm       DATE          NOT NULL,
+  pvm       TIMESTAMP     NOT NULL,
   voimassa  BOOLEAN       NOT NULL DEFAULT FALSE
 )
