c428167cae319a3c134e12c9166f4bf4b45bd9ec
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-08-14 12:17:47 +0300

    Pakota vv_hinta taululle tpk jos otsikkoa ei ole.

--- tietokanta/src/main/resources/db/migration/V1_593__.sql
+++ tietokanta/src/main/resources/db/migration/V1_593__.sql
@@ -1,1 +1,5 @@
+-- vv_hinta -taululle viittaus toimenpidekoodiin
+-- Omaa otsikkoa ei ole enää pakko olla jos on toimenpidekoodi
 ALTER TABLE vv_hinta ADD COLUMN toimenpidekoodi INTEGER REFERENCES toimenpidekoodi(id);
+ALTER TABLE vv_hinta ALTER COLUMN otsikko DROP NOT NULL;
+ALTER TABLE vv_hinta ADD CONSTRAINT otsikko_tai_tpk CHECK (otsikko IS NOT NULL OR (otsikko IS NULL AND toimenpidekoodi IS NOT NULL));
