3bbed69706ee8518320b033309c9d714d4931518
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-08-15 08:53:45 +0300

    Luo uusi taulu vv_tyo. vv_hinta taipuu huonosti työrivien tallennukseen

--- tietokanta/src/main/resources/db/migration/V1_595__.sql
+++ tietokanta/src/main/resources/db/migration/V1_595__.sql
@@ -1,8 +1,14 @@
--- vv_hinta -taululle viittaus toimenpidekoodiin
-ALTER TABLE vv_hinta ADD COLUMN toimenpidekoodi INTEGER REFERENCES toimenpidekoodi(id);
-ALTER TABLE vv_hinta ALTER COLUMN otsikko DROP NOT NULL; -- Otsikkoa ei tarvi jos on TPK
-ALTER TABLE vv_hinta RENAME COLUMN maara TO summa; -- Määrä tarkoittaa jatkossa kpl-määrää
-ALTER TABLE vv_hinta ALTER COLUMN summa DROP NOT NULL; -- Summaa ei tarvi jos on määrä ja TPK
-ALTER TABLE vv_hinta ADD COLUMN maara NUMERIC;
-ALTER TABLE vv_hinta ADD CONSTRAINT otsikko_tai_tpk CHECK (otsikko IS NOT NULL OR (otsikko IS NULL AND toimenpidekoodi IS NOT NULL));
-ALTER TABLE vv_hinta ADD CONSTRAINT summa_tai_maara CHECK ((maara IS NULL AND summa IS NOT NULL) OR (summa IS NULL AND maara IS NOT NULL AND toimenpidekoodi IS NOT NULL));
+-- Uusi taulu vv_tyo, johon tallennetaan toimenpiteen hinnoittelussa tehty työ
+CREATE TABLE vv_tyo
+(
+  id               SERIAL PRIMARY KEY,
+  "hinnoittelu-id" INTEGER REFERENCES vv_hinnoittelu (id) NOT NULL,
+  toimenpidekoodi  INTEGER REFERENCES toimenpidekoodi (id) NOT NULL,
+  maara            NUMERIC                          NOT NULL,
+  muokkaaja        INTEGER REFERENCES kayttaja (id),
+  muokattu         TIMESTAMP,
+  luoja            INTEGER REFERENCES kayttaja (id) NOT NULL,
+  luotu            TIMESTAMP                        NOT NULL DEFAULT NOW(),
+  poistettu        BOOLEAN                          NOT NULL DEFAULT FALSE,
+  poistaja         INTEGER REFERENCES kayttaja (id)
+);
