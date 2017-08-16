9b1ae982aa59ce9e20ca57ec9812f289ebc51c2a
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2017-08-16 10:28:55 +0300

    Lisää testejä elyn lask.yht.veto -raportille

--- test/clj/harja/palvelin/raportointi/laskutusyhteenveto_raportti_test.clj
+++ test/clj/harja/palvelin/raportointi/laskutusyhteenveto_raportti_test.clj
@@ -41,7 +41,7 @@
 (defn- arvo-raportin-nnesta-elementista [vastaus n]
   (second (first (second (second (last (nth (nth (last vastaus) n) 3)))))))
 
-(deftest raportin-suoritus-urakalle-toimii
+(deftest raportin-suoritus-urakalle-toimii-hoitokausi-2014-2015
   (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
                                 :suorita-raportti
                                 +kayttaja-jvh+
@@ -53,15 +53,12 @@
                                               :loppupvm (c/to-date (t/local-date 2015 9 30))}})]
     (is (vector? vastaus))
     (let [odotettu-otsikko "Oulun alueurakka 2014-2019, 01.10.2014-30.09.2015"
-
           saatu-otsikko (second (nth vastaus 2))
 
           kok-hint (arvo-raportin-nnesta-elementista vastaus 0)
           yks-hint (arvo-raportin-nnesta-elementista vastaus 1)
           sanktiot (arvo-raportin-nnesta-elementista vastaus 2)
-          _ (log/debug " sanktiot " sanktiot)
           talvisuolasakot (arvo-raportin-nnesta-elementista vastaus 3)
-          _ (log/debug " talvisuolasakot " talvisuolasakot)
           muutos-ja-lisatyot (arvo-raportin-nnesta-elementista vastaus 4)
           akilliset (arvo-raportin-nnesta-elementista vastaus 5)
           vahinkojen-korjaukset (arvo-raportin-nnesta-elementista vastaus 6)
@@ -81,11 +78,9 @@
           indeksitarkistukset-kaikki (arvo-raportin-nnesta-elementista vastaus 19)
           kaikki-paitsi-kokhint-yhteensa (arvo-raportin-nnesta-elementista vastaus 20)
           kaikki-yhteensa (arvo-raportin-nnesta-elementista vastaus 21)
-          _ (log/debug " kaikki-yhteensa " kaikki-yhteensa )
 
-          nurkkasumma (:arvo (second (second (last (last (last (last vastaus)))))))
-          _ (log/debug " nurkkasumma  " nurkkasumma)
-          ]
+          nurkkasumma (:arvo (second (second (last (last (last (last vastaus)))))))]
+
       (is (= odotettu-otsikko saatu-otsikko) "otsikko")
       (is (= kok-hint "162 010,00"))
       (is (= yks-hint "6 000,00"))
@@ -113,7 +108,46 @@
 
       (is (= kaikki-yhteensa nurkkasumma "155 250,20")))))
 
-(deftest raportin-suoritus-pop-elylle-toimii
+(deftest raportin-suoritus-urakalle-toimii-hoitokausi-2016-2017
+  (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                :suorita-raportti
+                                +kayttaja-jvh+
+                                {:nimi :laskutusyhteenveto
+                                 :konteksti "urakka"
+                                 :urakka-id (hae-oulun-alueurakan-2014-2019-id)
+                                 :parametrit {:urakkatyyppi :hoito
+                                              :alkupvm (c/to-date (t/local-date 2016 10 1))
+                                              :loppupvm (c/to-date (t/local-date 2017 9 30))}})]
+    (is (vector? vastaus))
+    (let [odotettu-otsikko "Oulun alueurakka 2014-2019, 01.10.2016-30.09.2017"
+          saatu-otsikko (second (nth vastaus 2))
+          varoitus-indeksiarvojen-puuttumisesta (nth vastaus 5)
+          yks-hint (arvo-raportin-nnesta-elementista vastaus 0)
+          sanktiot (arvo-raportin-nnesta-elementista vastaus 1)
+
+          indeksitarkistukset-yks-hint (arvo-raportin-nnesta-elementista vastaus 2)
+          indeksitarkistukset-sanktiot (arvo-raportin-nnesta-elementista vastaus 3)
+          indeksitarkistukset-muut-kuin-kokhint (arvo-raportin-nnesta-elementista vastaus 4)
+          indeksitarkistukset-kaikki (arvo-raportin-nnesta-elementista vastaus 5)
+          kaikki-paitsi-kokhint-yhteensa (arvo-raportin-nnesta-elementista vastaus 6)
+          kaikki-yhteensa (arvo-raportin-nnesta-elementista vastaus 7)
+
+          nurkkasumma (:arvo (second (second (last (last (last (last vastaus)))))))]
+
+      (is (= odotettu-otsikko saatu-otsikko) "otsikko")
+      (is (= varoitus-indeksiarvojen-puuttumisesta
+             [:varoitusteksti "Laskutusyhteenvedon laskennassa tarvittavia indeksiarvoja puuttuu. "]))
+
+      (is (= yks-hint "7 882,50"))
+      (is (= sanktiot "-1 900,67"))
+      (is (= indeksitarkistukset-yks-hint "0,00"))
+      (is (= indeksitarkistukset-sanktiot "0,00"))
+      (is (= indeksitarkistukset-muut-kuin-kokhint "0,00"))
+      (is (= indeksitarkistukset-kaikki "0,00"))
+      (is (= kaikki-paitsi-kokhint-yhteensa "5 981,83"))
+      (is (= kaikki-yhteensa nurkkasumma "5 981,83")))))
+
+(deftest raportin-suoritus-pop-elylle-toimii-hoitokausi-2014-2015
   (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
                                 :suorita-raportti
                                 +kayttaja-jvh+
@@ -179,3 +213,37 @@
       (is (= kaikki-paitsi-kokhint-yhteensa "-13 519,60"))
 
       (is (= kaikki-yhteensa nurkkasumma "310 500,40")))))
+
+(deftest raportin-suoritus-pop-elylle-toimii-hoitokausi-2016-2017
+  (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                :suorita-raportti
+                                +kayttaja-jvh+
+                                {:nimi :laskutusyhteenveto
+                                 :konteksti "hallintayksikko"
+                                 :hallintayksikko-id (hae-pohjois-pohjanmaan-hallintayksikon-id)
+                                 :parametrit {:urakkatyyppi :hoito
+                                              :alkupvm (c/to-date (t/local-date 2016 10 1))
+                                              :loppupvm (c/to-date (t/local-date 2017 9 30))}})]
+    (is (vector? vastaus))
+    (let [odotettu-otsikko "Pohjois-Pohjanmaa, 01.10.2016-30.09.2017"
+
+          saatu-otsikko (second (nth vastaus 2))
+
+          yks-hint (arvo-raportin-nnesta-elementista vastaus 0)
+          sanktiot (arvo-raportin-nnesta-elementista vastaus 1)
+
+          indeksitarkistukset-yks-hint (arvo-raportin-nnesta-elementista vastaus 2)
+          indeksitarkistukset-sanktiot (arvo-raportin-nnesta-elementista vastaus 3)
+          kaikki-paitsi-kokhint-yhteensa (arvo-raportin-nnesta-elementista vastaus 4)
+          kaikki-yhteensa (arvo-raportin-nnesta-elementista vastaus 5)
+
+          nurkkasumma (:arvo (second (second (last (last (last (last vastaus)))))))]
+
+      (is (= odotettu-otsikko saatu-otsikko) "otsikko")
+      (is (= yks-hint "7 882,50"))
+      (is (= sanktiot "-1 900,67"))
+      (is (= indeksitarkistukset-yks-hint "0,00"))
+      (is (= indeksitarkistukset-sanktiot "0,00"))
+      (is (= indeksitarkistukset-muut-kuin-kokhint "0,00"))
+      (is (= kaikki-paitsi-kokhint-yhteensa "5 981,83"))
+      (is (= kaikki-yhteensa nurkkasumma "5 981,83")))))
