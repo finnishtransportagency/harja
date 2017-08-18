718d32eff085ecf74bbb2aa2cb084d40236d0f46
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-08-18 11:55:00 +0300

    Yksinkertaista taustakartan luontia, koska projektio on sama
    Livin JHS180 tasojen extent ja projektio on sama kuin MML kartoissa,
    joten ei tarvita erillistä

--- src/cljs/harja/ui/openlayers/projektiot.cljs
+++ src/cljs/harja/ui/openlayers/projektiot.cljs
@@ -4,14 +4,7 @@
 
 (def suomen-extent
   "Suomalaisissa kartoissa olevan projektion raja-arvot."
-  [-548576.000000, 6291456.000000, 1548576.000000, 8388608.000000])
-
-(def suomen-extent-livi
-  "Suomalaisissa livi (EPSG:3067_PTP_JHS180) kartoissa olevan projektion raja-arvot."
-  [-548576 6291456 1548576 8388608])
+  [-548576.000000 6291456.000000 1548576.000000 8388608.000000])
 
 (def projektio (ol-proj/Projection. #js {:code   "EPSG:3067"
                                          :extent (clj->js suomen-extent)}))
-
-(def projektio-livi (ol-proj/Projection. #js {:code   "EPSG:3067"
-                                              :extent (clj->js suomen-extent-livi)}))
--- src/cljs/harja/ui/openlayers/taustakartta.cljs
+++ src/cljs/harja/ui/openlayers/taustakartta.cljs
@@ -8,29 +8,15 @@
             [harja.ui.openlayers.projektiot :as p]
             [taoensso.timbre :as log]))
 
-(defn mml-tilegrid []
-  (let [koko (/ (ol-extent/getWidth (.getExtent p/projektio)) 256)]
-    (loop [resoluutiot []
-           matrix-idt []
-           i 0]
-      (if (= i 16)
-        (let [optiot (clj->js
-                      {:origin (ol-extent/getTopLeft (.getExtent p/projektio))
-                       :resolutions (clj->js resoluutiot)
-                       :matrixIds   (clj->js matrix-idt)})]
-          (ol.tilegrid.WMTS. optiot))
-        (recur (conj resoluutiot (/ koko (Math/pow 2 i)))
-               (conj matrix-idt i)
-               (inc i))))))
 
-(defn livi-tilegrid []
+(defn tilegrid []
   (ol.tilegrid.WMTS.
-   (clj->js {:origin (ol-extent/getTopLeft (.getExtent p/projektio-livi))
+   (clj->js {:origin (ol-extent/getTopLeft (.getExtent p/projektio))
              :resolutions [8192 4096 2048 1024 512 256 128 64 32 16 8 4 2 1 0.5 0.25]
              :matrixIds (range 16)
              :tileSize 256})))
 
-(defn- wmts-layer [projektio matrixset tilegrid-fn attribuutio url layer]
+(defn- wmts-layer [matrixset attribuutio url layer]
   (ol.layer.Tile.
    #js {:source
         (ol.source.WMTS. #js {:attributions [(ol.Attribution.
@@ -39,8 +25,8 @@
                               :layer        layer
                               :matrixSet    matrixset
                               :format       "image/png"
-                              :projection   projektio
-                              :tileGrid     (tilegrid-fn)
+                              :projection   p/projektio
+                              :tileGrid     (tilegrid)
                               :style        "default"
                               :wrapX        true})}))
 
@@ -48,9 +34,8 @@
 
 (defmethod luo-taustakartta :mml [{:keys [url layer]}]
   (log/info "Luodaan MML karttataso: " layer)
-  (wmts-layer p/projektio "ETRS-TM35FIN" mml-tilegrid "MML" url layer))
+  (wmts-layer "ETRS-TM35FIN" "MML" url layer))
 
 (defmethod luo-taustakartta :livi [{:keys [url layer]}]
   (log/info "Luodaan livi karttataso: layer")
-  (wmts-layer p/projektio-livi "EPSG:3067_PTP_JHS180" livi-tilegrid "Liikennevirasto"
-              url layer))
+  (wmts-layer "EPSG:3067_PTP_JHS180" "Liikennevirasto" url layer))
