a0031001cc990efa542c327d5ab0fc29176b2b33
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-08-24 10:28:08 +0300

    Pumppaa migraationumeroa

--- tietokanta/src/main/resources/db/migration/V1_602__.sql
+++ tietokanta/src/main/resources/db/migration/V1_602__.sql
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 ALTER TABLE yllapitokohde
   RENAME yhatunnus TO tunnus;
 
 ALTER TABLE yllapitokohdeosa
   DROP COLUMN tunnus;
-=======
--- Indeksoi toteuman alkamisaika
-
-CREATE INDEX toteuma_alkanut_idx ON toteuma (alkanut);
->>>>>>> develop
