72f234dae7543b902abe25b587d2563c7febcf96
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-08-30 09:35:51 +0300

    HAR-5967: Vesiväylissä tilaaja aina Liikennevirasto

--- src/cljs/harja/views/urakka/yleiset.cljs
+++ src/cljs/harja/views/urakka/yleiset.cljs
@@ -408,7 +408,7 @@
       "Aikaväli:" [:span.aikavali (pvm/pvm (:alkupvm ur)) " \u2014 " (pvm/pvm (:loppupvm ur))]
       "Takuu päättyy:" (when paallystys-tai-paikkausurakka?
                          [takuuaika ur])
-      "Tilaaja:" (:nimi (:hallintayksikko ur))
+      "Tilaaja:" (if (u/vesivaylaurakka? ur) "Liikennevirasto" (:nimi (:hallintayksikko ur)))
       "Urakanvalvoja: " [nayta-vastuuhenkilo paivita-vastuuhenkilot!
                          (:id ur) @istunto/kayttaja kayttajat vastuuhenkilot
                          (if (u/vesivaylaurakka? ur)
