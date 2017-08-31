cc23c3897316a119463c977e480e6b82c7079e5a
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-08-31 13:33:18 +0300

    Palauta vanha runner.cljs

--- test/doo/harja/runner.cljs
+++ test/doo/harja/runner.cljs
@@ -88,5 +88,13 @@
            'harja.tiedot.vesivaylat.urakka.turvalaitteet-test
            'harja.tiedot.vesivaylat.urakka.toimenpiteet.yksikkohintaiset-test
            'harja.tiedot.vesivaylat.urakka.toimenpiteet.kokonaishintaiset-test
+           'harja.tiedot.hallintayksikot-test
+           'harja.tiedot.vesivaylat.urakka.laadunseuranta.viat-test
+           'harja.tiedot.urakka.urakan-tyotunnit-test
+           'harja.ui.validointi-test
+           'harja.views.urakka.suunnittelu.yksikkohintaiset-tyot-test
+           'harja.tiedot.vesivaylat.urakka.suunnittelu.kiintiot-test
+           'harja.views.vesivaylat.urakka.materiaalit-test
+           'harja.tiedot.vesivaylat.urakka.toimenpiteet.jaettu-test
            ;; uusi testi tähän
            )
