d96f78844eb19e0f42342d9b3054d0cc159cdb1a
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-09-07 13:37:58 +0300

    Korjaa hakuehdot

--- src/clj/harja/palvelin/palvelut/ilmoitukset.clj
+++ src/clj/harja/palvelin/palvelut/ilmoitukset.clj
@@ -140,8 +140,8 @@
          selite-annettu? (boolean (and selite (first selite)))
          selite (if selite-annettu? (name (first selite)) "")
          tilat (into #{} tilat)
-         vain-myohassa? (contains vaikutukset :myohassa)
-         vain-toimenpiteita-aiheuttaneet? (contains vaikutukset :aiheutti-toimenpiteita )
+         vain-myohassa? (contains? vaikutukset :myohassa)
+         vain-toimenpiteita-aiheuttaneet? (contains? vaikutukset :aiheutti-toimenpiteita )
          debug-viesti (str "Haetaan ilmoituksia: "
                            (viesti urakat "urakoista" "ilman urakoita")
                            (viesti aikavali-alku "alkaen" "ilman alkuaikaa")
