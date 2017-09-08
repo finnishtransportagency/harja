81e936fc6da17ce683183fcdf9e7190331097a26
Joonas Mikkonen <Joonas.Mikkonen@solita.fi>
2017-09-08 10:14:00 +0300

    kuittaukset näyetään nyt modaalissakin aikajärjestyksessä.

--- src/cljs/harja/views/tilannekuva/tilannekuva_jaettu.cljs
+++ src/cljs/harja/views/tilannekuva/tilannekuva_jaettu.cljs
@@ -1,12 +1,13 @@
 (ns harja.views.tilannekuva.tilannekuva-jaettu
   (:require [harja.ui.modal :as modal]
+            [harja.pvm :as pvm]
             [harja.views.ilmoituskuittaukset :as ik])
   (:require-macros [harja.tyokalut.ui :refer [for*]]))
 
 (defn nayta-kuittausten-tiedot
   [kuittaukset]
   (modal/nayta! {:otsikko "Kuittaukset"}
-                (for* [kuittaus kuittaukset
+                (for* [kuittaus (sort-by :kuitattu pvm/jalkeen? kuittaukset)
                        :when (not= :valitys (:kuittaustyyppi kuittaus))]
                   (let [kuittaus (update kuittaus :kanava #(keyword %))]
                     (ik/kuittauksen-tiedot kuittaus)))))
