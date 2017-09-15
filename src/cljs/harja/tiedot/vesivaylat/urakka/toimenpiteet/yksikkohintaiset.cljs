b7d792e99fc7c4cde1de775aed49960062a975fe
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-09-15 14:00:26 +0300

    Poista käyttämätön hinnoiteltavan-toimenpiteen-komponentit-ilman-hintaa

--- src/cljs/harja/tiedot/vesivaylat/urakka/toimenpiteet/yksikkohintaiset.cljs
+++ src/cljs/harja/tiedot/vesivaylat/urakka/toimenpiteet/yksikkohintaiset.cljs
@@ -300,12 +300,6 @@
        %)
     (:toimenpiteet app)))
 
-(defn hinnoiteltavan-toimenpiteen-komponentit-ilman-hintaa [app]
-  (let [toimenpide (hinnoiteltava-toimenpide app)
-        komponentit (::to/komponentit toimenpide)
-        komponentit-hinnoilla (into #{} (map ::hinta/komponentti-id (komponenttien-hinnat app)))]
-    (remove (comp komponentit-hinnoilla ::tkomp/id) komponentit)))
-
 (extend-protocol tuck/Event
 
   Nakymassa?
