b9d2a40092a84d58491b68b3f42692e4ecee224b
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-09-16 11:51:16 +0300

    Vesiväylien laskutusyhteenvedolle mukaan työt, joilla on yksikköhinta+määrä

--- src/clj/harja/palvelin/raportointi/raportit/vesivaylien_laskutusyhteenveto.sql
+++ src/clj/harja/palvelin/raportointi/raportit/vesivaylien_laskutusyhteenveto.sql
@@ -8,7 +8,11 @@ SELECT
   hintaryhma.nimi                   AS "hinnoittelu",
   -- Hae hintaryhmään kuuluvien ei-poistettujen toimenpiteiden kaikki hinnoittelut, eli
   -- hintaryhmän oma hinnoittelu ja toimenpiteiden omat hinnoittelut. Summaa kaikki yhteen.
-  (SELECT COALESCE(SUM(summa), 0)
+  (SELECT COALESCE(SUM(CASE
+                       WHEN summa IS NOT NULL
+                         THEN (summa + (summa * yleiskustannuslisa))
+                           ELSE ((yksikkohinta * maara) + ((yksikkohinta * maara) * yleiskustannuslisa))
+                         END), 0)
    FROM vv_hinta
    WHERE "hinnoittelu-id" IN
          -- Hinta kuuluu toimenpiteeseen, joka kuuluu ko. hintaryhmään
@@ -86,7 +90,11 @@ WHERE "urakka-id" = :urakkaid
 -- tällaiset tapaukset summataan jo hintaryhmien hintaan (ks. kysely: hae-yksikkohintaisten-toimenpiteiden-hintaryhmat)
 SELECT
   oma_hinnoittelu.nimi                              AS "hinnoittelu",
-  (SELECT COALESCE(SUM(summa), 0)
+  (SELECT COALESCE(SUM(CASE
+                       WHEN summa IS NOT NULL
+                         THEN (summa + (summa * yleiskustannuslisa))
+                       ELSE ((yksikkohinta * maara) + ((yksikkohinta * maara) * yleiskustannuslisa))
+                       END), 0)
    FROM vv_hinta
    WHERE "hinnoittelu-id" = oma_hinnoittelu.id
          AND poistettu IS NOT TRUE)
