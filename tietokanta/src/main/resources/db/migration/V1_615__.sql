771353c8e51da78b865e9511ca1ae45877061828
Jari Hanhela <Jari.Hanhela@solita.fi>
2017-09-21 13:29:43 +0300

    Korjaa typo

--- tietokanta/src/main/resources/db/migration/V1_615__.sql
+++ tietokanta/src/main/resources/db/migration/V1_615__.sql
@@ -1,2 +1,2 @@
--- Vaadi ylläpitokohteille NOT NULL YHA-kohdenumero jos YHA_kohde
+-- Vaadi ylläpitokohteille NOT NULL YHA-kohdenumero jos YHA-kohde
 ALTER TABLE yllapitokohde ADD CONSTRAINT validi_kohde CHECK (yhaid IS NULL OR (yhaid IS NOT NULL AND yha_kohdenumero IS NOT NULL));
