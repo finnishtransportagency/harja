8c99354407679fd6911fef323ef6ca6c55071da1
Jari Hanhela <jarihanhela@gmail.com>
2017-09-22 14:04:53 +0300

    Korosta viimeisin avattu rivi ilmoitusnäkymässä

--- src/cljs/harja/views/ilmoituksen_tiedot.cljs
+++ src/cljs/harja/views/ilmoituksen_tiedot.cljs
@@ -20,7 +20,7 @@
             [reagent.core :refer [atom] :as r]))
 
 (defn selitelista [{:keys [selitteet] :as ilmoitus}]
-  (let [virka-apu? (ilmoitukset/virka-apupyynto? ilmoitus)]
+  (let [virka-apu? true]
     [:div.selitelista.inline-block
      (when virka-apu?
        [:div.selite-virkaapu
