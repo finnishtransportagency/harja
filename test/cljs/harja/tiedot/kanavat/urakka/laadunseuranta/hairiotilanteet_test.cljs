b23f50d4624f3ad5d2541e15c0808d5261248d1e
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-09-27 11:42:06 +0300

    Lisää testien pohjat uusille näkymille

---
+++ test/cljs/harja/tiedot/kanavat/urakka/laadunseuranta/hairiotilanteet_test.cljs
@@ -0,0 +1,8 @@
+(ns harja.tiedot.kanavat.urakka.laadunseuranta.hairiotilanteet-test
+  (:require [harja.tiedot.kanavat.urakka.laadunseuranta.hairiotilanteet :as tiedot]
+            [clojure.test :refer :all]
+            [harja.testutils.tuck-apurit :refer-macros [vaadi-async-kutsut] :refer [e!]]))
+
+(deftest nakymaan-tuleminen
+  (is (true? (:nakymassa? (e! (tiedot/->Nakymassa? true)))))
+  (is (false? (:nakymassa? (e! (tiedot/->Nakymassa? false))))))
