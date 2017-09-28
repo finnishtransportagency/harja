5d0c8a26c96db84a9447b71f19c70c7299a59067
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2017-09-28 13:16:26 +0300

    HAR-5735 Kuvansinjoen raittisilta (SK-3279) väärässä urakassa, pitäisi olla Juva eikä Kuopio
    Päättele sillan hoitourakka sillan TR-osoitteen eikä sillan
    geometrian perusteella.

---
+++ tietokanta/src/main/resources/db/migration/V1_618__.sql
@@ -0,0 +1,8 @@
+DROP MATERIALIZED VIEW sillat_alueurakoittain;
+CREATE MATERIALIZED VIEW sillat_alueurakoittain AS
+  SELECT
+    u.id AS urakka,
+    s.id AS silta
+  FROM urakka u
+    JOIN silta s ON ST_CONTAINS(u.alue, tierekisteriosoitteelle_piste(s.tr_numero, s.tr_alkuosa, s.tr_alkuetaisyys))
+  WHERE u.tyyppi = 'hoito' :: URAKKATYYPPI;
