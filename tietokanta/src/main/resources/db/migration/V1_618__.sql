da5072bdc6ccfcf03ecb45719cf3d15a9ae39d6e
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2017-09-29 10:04:55 +0300

    Päivitä silta viewiä

--- tietokanta/src/main/resources/db/migration/V1_618__.sql
+++ tietokanta/src/main/resources/db/migration/V1_618__.sql
@@ -1,8 +1,9 @@
-DROP MATERIALIZED VIEW sillat_alueurakoittain;
+DROP MATERIALIZED VIEW IF EXISTS sillat_alueurakoittain;
 CREATE MATERIALIZED VIEW sillat_alueurakoittain AS
   SELECT
     u.id AS urakka,
     s.id AS silta
   FROM urakka u
-    JOIN silta s ON ST_CONTAINS(u.alue, tierekisteriosoitteelle_piste(s.tr_numero, s.tr_alkuosa, s.tr_alkuetaisyys))
+    JOIN silta s
+      ON ST_CONTAINS(u.alue, (select * from tierekisteriosoitteelle_piste(s.tr_numero, s.tr_alkuosa, s.tr_alkuetaisyys)))
   WHERE u.tyyppi = 'hoito' :: URAKKATYYPPI;
