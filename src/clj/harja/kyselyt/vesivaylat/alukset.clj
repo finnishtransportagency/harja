ff02859b2ab42bd3fbf7ea72a3a4275ec9aa59cc
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-10-03 16:58:45 +0300

    Hae alusten sijaintitiedot ja muodosta reitit
    Kysely palauttaa sekvenssin mäppejä, joissa
    aluksen mmsi-tunnus ja yhdistetty reitti. Lisäksi
    ::alus/pisteet vektorissa on kaikki aluksen reittipisteet
    ja näiden aikaleimat.

--- src/clj/harja/kyselyt/vesivaylat/alukset.clj
+++ src/clj/harja/kyselyt/vesivaylat/alukset.clj
@@ -7,7 +7,52 @@
             [taoensso.timbre :as log]
             [harja.id :refer [id-olemassa?]]
             [harja.pvm :as pvm]
-            [clj-time.core :as t]))
+            [clj-time.core :as t]
+            [clj-time.coerce :as coerce]
+            [harja.geo :as geo]
+            [specql.core :as specql]
+            [harja.domain.vesivaylat.alus :as alus]
+            [specql.op :as op]
+            [namespacefy.core :refer [namespacefy]]
+            [harja.kyselyt.konversio :as konv]
+            [clojure.string :as str]))
 
 (defqueries "harja/kyselyt/vesivaylat/alukset.sql"
   {:positional? true})
+
+(defn oletus-alku-reitin-hakemiselle [] (t/minus (t/now) (t/days 2)))
+(defn oletus-loppu-reitin-hakemiselle [] (t/now))
+
+(defn alusten-reitit [db {:keys [alukset alku loppu]}]
+  (let [loppu (or loppu (oletus-loppu-reitin-hakemiselle))
+        alku (or alku (oletus-alku-reitin-hakemiselle))]
+    (->
+      (into []
+           (comp
+             (map #(update % :sijainti geo/pg->clj))
+             (map #(konv/array->vec % :pisteet))
+             (map #(update % :pisteet (fn [pisteet]
+                                        (map
+                                          (comp
+                                            (fn [m]
+                                              (namespacefy m {:ns :harja.domain.vesivaylat.alus}))
+                                            (fn [aika-piste]
+                                              (update
+                                                aika-piste
+                                                :sijainti
+                                                (fn [s]
+                                                  (let [[alku loppu] (-> s
+                                                                         rest
+                                                                         butlast
+                                                                         ((partial apply str))
+                                                                       (str/split #","))]
+                                                    {:type :point
+                                                     :coordinates [(Float/parseFloat alku)
+                                                                   (Float/parseFloat loppu)]}))))
+                                            (fn [o]
+                                             (konv/pgobject->map o
+                                                                 :aika :date
+                                                                 :sijainti :string)))
+                                          pisteet)))))
+           (hae-alusten-reitit db))
+      (namespacefy {:ns :harja.domain.vesivaylat.alus}))))
--- src/clj/harja/kyselyt/vesivaylat/alukset.sql
+++ src/clj/harja/kyselyt/vesivaylat/alukset.sql
@@ -4,7 +4,7 @@ SELECT
   nimi,
   a.lisatiedot
 FROM vv_alus a
-LEFT JOIN vv_alus_urakka au ON au.alus = a.mmsi
+  LEFT JOIN vv_alus_urakka au ON au.alus = a.mmsi
 WHERE urakka = :urakka
 ORDER BY mmsi;
 
@@ -14,8 +14,20 @@ SELECT
   a.nimi,
   a.lisatiedot
 FROM vv_alus a
-LEFT JOIN vv_alus_urakka au ON au.alus = a.mmsi
+  LEFT JOIN vv_alus_urakka au ON au.alus = a.mmsi
   LEFT JOIN urakka u ON u.id = au.urakka
-LEFT JOIN organisaatio o ON u.urakoitsija = o.id
+  LEFT JOIN organisaatio o ON u.urakoitsija = o.id
 WHERE o.id = :urakoitsija
 ORDER BY mmsi;
+
+-- name: hae-alusten-reitit
+SELECT
+  alus,
+  -- yhdistetään pisteet reittiviivaksi. Nimetään sijainniksi, koska
+  -- sitä nimeä on käytetty ympäri harjan..
+  ST_Simplify (ST_MakeLine(array_agg(sijainti :: GEOMETRY)), 200, true) AS sijainti,
+  -- Taulukkoon aikaleimat ja pisteet, jotta voidaan kartalla näyttää,
+  -- missä laiva on milloinkin ollut
+  array_agg((aika, sijainti)) AS pisteet
+FROM vv_alus_sijainti
+GROUP BY alus;
