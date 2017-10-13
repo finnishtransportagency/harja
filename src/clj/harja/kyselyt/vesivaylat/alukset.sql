a095e288d377b4f72f2b00b400d9c4117190a427
Jari Hanhela <jarihanhela@gmail.com>
2017-10-13 10:30:21 +0300

    Huomioi poistetut alukset ja linkit kyselyssä

--- src/clj/harja/kyselyt/vesivaylat/alukset.sql
+++ src/clj/harja/kyselyt/vesivaylat/alukset.sql
@@ -4,8 +4,10 @@ SELECT
   nimi,
   au.lisatiedot
 FROM vv_alus a
-LEFT JOIN vv_alus_urakka au ON au.alus = a.mmsi
+  LEFT JOIN vv_alus_urakka au ON au.alus = a.mmsi
+                              AND au.poistettu IS NOT TRUE
 WHERE urakka = :urakka
+      AND a.poistettu IS NOT TRUE;
 ORDER BY mmsi;
 
 -- name: hae-urakoitsijan-alukset
@@ -14,8 +16,10 @@ SELECT
   a.nimi,
   a.lisatiedot
 FROM vv_alus a
-LEFT JOIN vv_alus_urakka au ON au.alus = a.mmsi
+  LEFT JOIN vv_alus_urakka au ON au.alus = a.mmsi
+                              AND au.poistettu IS NOT TRUE
   LEFT JOIN urakka u ON u.id = au.urakka
 LEFT JOIN organisaatio o ON u.urakoitsija = o.id
 WHERE o.id = :urakoitsija
+AND a.poistettu IS NOT TRUE
 ORDER BY mmsi;
