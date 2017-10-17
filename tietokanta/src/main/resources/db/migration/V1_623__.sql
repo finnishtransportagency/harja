471cba2d6fda2dbf21f101d0a501681f2dcf8e1f
Jari Hanhela <jarihanhela@gmail.com>
2017-10-17 10:18:44 +0300

    Pakota poistettu-kenttien arvoksi NOT NULL

--- tietokanta/src/main/resources/db/migration/V1_623__.sql
+++ tietokanta/src/main/resources/db/migration/V1_623__.sql
@@ -1,4 +1,7 @@
--- Toiukenna urakka-alus taulua
+-- Toiukenna alus-tauluja
+ALTER TABLE vv_alus ALTER COLUMN poistettu SET NOT NULL;
+
 ALTER TABLE vv_alus_urakka ALTER COLUMN alus SET NOT NULL;
 ALTER TABLE vv_alus_urakka ALTER COLUMN urakka SET NOT NULL;
 ALTER TABLE vv_alus_urakka ALTER COLUMN lisatiedot TYPE VARCHAR(512);
+ALTER TABLE vv_alus_urakka ALTER COLUMN poistettu SET NOT NULL;
