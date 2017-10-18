1019ae72611634db890534ed8432dfecd0bf9b50
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-10-18 15:38:28 +0300

    Pääteltäessä tarviiko aineisto päivittää, huomioi ajastetut aineistot

--- src/clj/harja/kyselyt/geometriapaivitykset.clj
+++ src/clj/harja/kyselyt/geometriapaivitykset.clj
@@ -3,16 +3,24 @@
   (:require [taoensso.timbre :as log]
             [jeesql.core :refer [defqueries]]
             [harja.pvm :as pvm]
-            [clj-time.coerce :as time-coerce]))
+            [clj-time.coerce :as time-coerce]
+            [harja.kyselyt.geometriaaineistot :as geometria-aineistot]
+            [harja.domain.geometriaaineistot :as ga]))
 
 (defqueries "harja/kyselyt/geometriapaivitykset.sql"
-  {:positional? true})
+            {:positional? true})
 
 (defn pitaako-paivittaa? [db paivitystunnus tiedoston-muutospvm]
-  (let [paivityksen-tiedot (first (harja.kyselyt.geometriapaivitykset/hae-paivitys db paivitystunnus))
+  (let [aineisto (geometria-aineistot/hae-voimassaoleva-geometria-aineisto db paivitystunnus)
+        paivityksen-tiedot (first (harja.kyselyt.geometriapaivitykset/hae-paivitys db paivitystunnus))
         viimeisin-paivitys (:viimeisin_paivitys paivityksen-tiedot)]
     (log/debug (format "Geometriapäivitys: %s on päivitetty viimeksi: %s. Tiedosto on muuttunut viimeksi: %s " paivitystunnus viimeisin-paivitys tiedoston-muutospvm))
     (or (nil? viimeisin-paivitys)
+        (not (pvm/valissa?
+               (time-coerce/from-sql-time viimeisin-paivitys)
+               (time-coerce/from-sql-time (::ga/voimassaolo-alkaa aineisto))
+               (time-coerce/from-sql-time (::ga/voimassaolo-paattyy aineisto))
+               false))
         (pvm/jalkeen?
           (time-coerce/from-sql-time tiedoston-muutospvm)
           (time-coerce/from-sql-time viimeisin-paivitys)))))
