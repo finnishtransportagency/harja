82eae849da4983f887e6076f7a12627b3a5ab490
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-10-18 15:18:02 +0300

    Lisää aineistot urakoille kantaan valmiiksi

--- tietokanta/src/main/resources/db/migration/V1_622__.sql
+++ tietokanta/src/main/resources/db/migration/V1_622__.sql
@@ -5,3 +5,12 @@ CREATE TABLE geometriaaineisto (
   "voimassaolo-alkaa"   TIMESTAMP,
   "voimassaolo-paattyy" TIMESTAMP
 );
+
+INSERT INTO geometriaaineisto (nimi, tiedostonimi, "voimassaolo-alkaa", "voimassaolo-paattyy") VALUES
+  ('urakat', 'PTV_Hoitourakat10_2016', '2016-10-01 11:01:00.000000', '2017-9-30 11:00:00.000000');
+
+INSERT INTO geometriaaineisto (nimi, tiedostonimi, "voimassaolo-alkaa", "voimassaolo-paattyy") VALUES
+  ('urakat', 'PTV_Hoitourakat10_2017', '2017-10-01 11:01:00.000000', '2018-09-30 11:00:00.000000');
+
+INSERT INTO geometriaaineisto (nimi, tiedostonimi, "voimassaolo-alkaa", "voimassaolo-paattyy") VALUES
+  ('urakat', 'PTV_Hoitourakat10_2017', '2018-10-01 11:01:00.000000', '2019-09-30 11:00:00.000000');
