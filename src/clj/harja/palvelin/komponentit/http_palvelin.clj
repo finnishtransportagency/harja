87d3e31dc0f9f759fc350da643389f8f92bea652
Jari Hanhela <jarihanhela@gmail.com>
2017-10-23 10:35:07 +0300

    Ota anti-csrf-token käyttöön myös LS-työkalussa

--- src/clj/harja/palvelin/komponentit/http_palvelin.clj
+++ src/clj/harja/palvelin/komponentit/http_palvelin.clj
@@ -11,9 +11,9 @@
 
             [cognitect.transit :as t]
             [clojure.spec.alpha :as s]
-            ;; Pyyntöjen todennus (autentikointi)
+    ;; Pyyntöjen todennus (autentikointi)
             [harja.palvelin.komponentit.todennus :as todennus]
-            ;; Metriikkadatan julkaisu
+    ;; Metriikkadatan julkaisu
             [harja.palvelin.komponentit.metriikka :as metriikka]
             [harja.palvelin.index :as index]
             [harja.geo :as geo]
@@ -32,9 +32,9 @@
 (defn transit-vastaus
   ([data] (transit-vastaus 200 data))
   ([status data]
-   {:status  status
+   {:status status
     :headers {"Content-Type" "application/transit+json"}
-    :body    (transit/clj->transit data)
+    :body (transit/clj->transit data)
     :vastaus data}))
 
 (defrecord AsyncResponse [channel])
@@ -45,12 +45,12 @@
 (defmacro async [& body]
   `(->AsyncResponse (async/thread
                       (try+
-                       (transit-vastaus ~@body)
-                       (catch harja.domain.roolit.EiOikeutta eo#
-                         (transit-vastaus 403 eo#))
-                       (catch Throwable e#
-                         (log/warn e# "Virhe async POST palvelussa")
-                         (transit-vastaus 500 {:virhe (.getMessage e#)}))))))
+                        (transit-vastaus ~@body)
+                        (catch harja.domain.roolit.EiOikeutta eo#
+                          (transit-vastaus 403 eo#))
+                        (catch Throwable e#
+                          (log/warn e# "Virhe async POST palvelussa")
+                          (transit-vastaus 500 {:virhe (.getMessage e#)}))))))
 
 (def
   ^{:doc "Vastauksen HTTP statuskoodit, joille ei vaadita oikeustarkistusta."}
@@ -63,7 +63,7 @@
     (try
       (let [res (apply compojure/routing
                        (if
-                           (= "/" (:uri req))
+                         (= "/" (:uri req))
                          (assoc req :uri "/index.html")
                          req)
                        (remove nil? kasittelijat))]
@@ -123,25 +123,25 @@
                       [kysely nil]))))]
           (if (= kysely ::ei-validi-kysely)
             {:status 400
-             :body   virhe}
+             :body virhe}
             (try+
-             (let [palvelu-vastaus (palvelu-fn (:kayttaja req) kysely)]
-               (if (async-response? palvelu-vastaus)
-                 (http/with-channel req channel
-                   (async/go
-                     (let [vastaus (async/<! (:channel palvelu-vastaus))]
-                       (validoi-vastaus! (:vastaus-spec optiot) (:vastaus vastaus))
-                       (http/send! channel vastaus)
-                       (http/close channel))))
-                 (do
-                   (validoi-vastaus! (:vastaus-spec optiot) palvelu-vastaus)
-                   (transit-vastaus palvelu-vastaus))))
-             (catch harja.domain.roolit.EiOikeutta eo
-               ;; Valutetaan oikeustarkistuksen epäonnistuminen frontille asti
-               (transit-vastaus 403 eo))
-             (catch Throwable e
-               (log/error e "Virhe POST palvelussa " nimi ", payload: " (pr-str kysely))
-               {:virhe (.getMessage e)}))))))))
+              (let [palvelu-vastaus (palvelu-fn (:kayttaja req) kysely)]
+                (if (async-response? palvelu-vastaus)
+                  (http/with-channel req channel
+                                     (async/go
+                                       (let [vastaus (async/<! (:channel palvelu-vastaus))]
+                                         (validoi-vastaus! (:vastaus-spec optiot) (:vastaus vastaus))
+                                         (http/send! channel vastaus)
+                                         (http/close channel))))
+                  (do
+                    (validoi-vastaus! (:vastaus-spec optiot) palvelu-vastaus)
+                    (transit-vastaus palvelu-vastaus))))
+              (catch harja.domain.roolit.EiOikeutta eo
+                ;; Valutetaan oikeustarkistuksen epäonnistuminen frontille asti
+                (transit-vastaus 403 eo))
+              (catch Throwable e
+                (log/error e "Virhe POST palvelussa " nimi ", payload: " (pr-str kysely))
+                {:virhe (.getMessage e)}))))))))
 
 (def muokkaus-pvm-muoto "EEE, dd MMM yyyy HH:mm:ss zzz")
 
@@ -164,15 +164,15 @@
             {:status 304}
             (let [vastaus (palvelu-fn (:kayttaja req))]
               (validoi-vastaus! (:vastaus-spec optiot) vastaus)
-              {:status  200
+              {:status 200
                :headers (merge {"Content-Type" "application/transit+json"}
                                (if last-modified
                                  {"cache-control" "private, max-age=0, must-revalidate"
                                   "Last-Modified" (.format (SimpleDateFormat. muokkaus-pvm-muoto) last-modified)}
                                  {"cache-control" "no-cache"}))
-               :body    (with-open [out (ByteArrayOutputStream.)]
-                          (t/write (t/writer out :json) vastaus)
-                          (ByteArrayInputStream. (.toByteArray out)))})))))))
+               :body (with-open [out (ByteArrayOutputStream.)]
+                       (t/write (t/writer out :json) vastaus)
+                       (ByteArrayInputStream. (.toByteArray out)))})))))))
 
 (defprotocol HttpPalvelut
   "Protokolla HTTP palveluiden julkaisemiseksi."
@@ -218,15 +218,15 @@
     (when (or (= uri "/")
               (= uri "/index.html"))
       (oikeudet/ei-oikeustarkistusta!)
-      {:status  200
-       :headers {"Content-Type"  "text/html"
+      {:status 200
+       :headers {"Content-Type" "text/html"
                  "Cache-Control" "no-cache, no-store, must-revalidate"
-                 "Pragma"        "no-cache"
-                 "Expires"       "0"}
-       :cookies {"anti-csrf-token" {:value     salattu
+                 "Pragma" "no-cache"
+                 "Expires" "0"}
+       :cookies {"anti-csrf-token" {:value salattu
                                     :http-only true
-                                    :max-age   36000000}}
-       :body    (index/tee-paasivu token kehitysmoodi)})))
+                                    :max-age 36000000}}
+       :body (index/tee-paasivu token kehitysmoodi)})))
 
 (defn ls-index-kasittelija [kehitysmoodi req]
   (let [uri (:uri req)
@@ -245,37 +245,31 @@
 
       (= uri "/laadunseuranta/")
       (do (oikeudet/ei-oikeustarkistusta!)
-          {:status  200
-           :headers {"Content-Type"  "text/html"
+          {:status 200
+           :headers {"Content-Type" "text/html"
                      "Cache-Control" "no-cache, no-store, must-revalidate"
-                     "Pragma"        "no-cache"
-                     "Expires"       "0"}
-           :body    (index/tee-ls-paasivu kehitysmoodi)})
+                     "Pragma" "no-cache"
+                     "Expires" "0"}
+           :body (index/tee-ls-paasivu kehitysmoodi)})
       :default
-       nil)))
+      nil)))
 
 (defn wrap-anti-forgery
   "Vertaa headerissa lähetettyä tokenia http-only cookiessa tulevaan"
-  [f anti-csrf-kaytossa?]
+  [f]
   (fn [{:keys [cookies headers uri] :as req}]
-    (if (or (not (anti-csrf-kaytossa? req))
-             (and (not (nil? (headers "x-csrf-token")))
-                  (= (index/laske-mac (headers "x-csrf-token"))
-                     (:value (cookies "anti-csrf-token")))))
+    (if (or (and (some? (headers "x-csrf-token"))
+                 (= (index/laske-mac (headers "x-csrf-token"))
+                    (:value (cookies "anti-csrf-token")))))
       (f req)
-      {:status  403
+      {:status 403
        :headers {"Content-Type" "text/html"}
-       :body    "Access denied"})))
+       :body "Access denied"})))
 
 (defn- jaa-todennettaviin-ja-ei-todennettaviin [kasittelijat]
   (let [{ei-todennettavat true
          todennettavat false} (group-by #(or (:ei-todennettava %) false) kasittelijat)]
     [todennettavat ei-todennettavat]))
-
-(defn- anti-csrf-kaytossa? [req]
-  ;; ls- sisältävät palvelukutsut (mobiili laadunseuranta), ei käytä anti csrf tokenia
-  (not (or (str/includes? (:uri req) "ls-")
-           (str/includes? (:uri req) "laadunseuranta"))))
 
 (defrecord HttpPalvelin [asetukset kasittelijat sessiottomat-kasittelijat lopetus-fn kehitysmoodi
                          mittarit]
@@ -290,33 +284,33 @@
                           (route/files "" {:root "dev-resources"}))]
       (swap! lopetus-fn
              (constantly
-              (http/run-server
-                (cookies/wrap-cookies
-                 (fn [req]
-                   (try+
-                    (metriikka/inc! mittarit :aktiiviset_pyynnot)
-                    (let [[todennettavat ei-todennettavat] (jaa-todennettaviin-ja-ei-todennettaviin @sessiottomat-kasittelijat)
-                          ui-kasittelijat (mapv :fn @kasittelijat)
-                          ui-kasittelija (-> (apply compojure/routes ui-kasittelijat)
-                                             (wrap-anti-forgery anti-csrf-kaytossa?))]
+               (http/run-server
+                 (cookies/wrap-cookies
+                   (fn [req]
+                     (try+
+                       (metriikka/inc! mittarit :aktiiviset_pyynnot)
+                       (let [[todennettavat ei-todennettavat] (jaa-todennettaviin-ja-ei-todennettaviin @sessiottomat-kasittelijat)
+                             ui-kasittelijat (mapv :fn @kasittelijat)
+                             ui-kasittelija (-> (apply compojure/routes ui-kasittelijat)
+                                                (wrap-anti-forgery))]
 
-                      (or (reitita req (conj (mapv :fn ei-todennettavat)
-                                             dev-resurssit resurssit) false)
-                          (reitita (todennus/todenna-pyynto todennus req)
-                                   (-> (mapv :fn todennettavat)
-                                       (conj (partial index-kasittelija kehitysmoodi))
-                                       (conj (partial ls-index-kasittelija kehitysmoodi))
-                                       (conj ui-kasittelija))
-                                   true)))
-                    (catch [:virhe :todennusvirhe] _
-                      {:status 403 :body "Todennusvirhe"})
-                    (finally
-                      (metriikka/muuta! mittarit
-                                        :aktiiviset_pyynnot dec
-                                        :pyyntoja_palveltu inc)))))
+                         (or (reitita req (conj (mapv :fn ei-todennettavat)
+                                                dev-resurssit resurssit) false)
+                             (reitita (todennus/todenna-pyynto todennus req)
+                                      (-> (mapv :fn todennettavat)
+                                          (conj (partial index-kasittelija kehitysmoodi))
+                                          (conj (partial ls-index-kasittelija kehitysmoodi))
+                                          (conj ui-kasittelija))
+                                      true)))
+                       (catch [:virhe :todennusvirhe] _
+                         {:status 403 :body "Todennusvirhe"})
+                       (finally
+                         (metriikka/muuta! mittarit
+                                           :aktiiviset_pyynnot dec
+                                           :pyyntoja_palveltu inc)))))
 
-                 {:port     (or (:portti asetukset) asetukset)
-                  :thread   (or (:threads asetukset) 8)
+                 {:port (or (:portti asetukset) asetukset)
+                  :thread (or (:threads asetukset) 8)
                   :max-body (or (:max-body-size asetukset) (* 1024 1024 8))
                   :max-line 40960})))
       this))
@@ -341,9 +335,9 @@
                           palvelu-fn)]
       (if (:ring-kasittelija? optiot)
         (swap! sessiottomat-kasittelijat conj {:nimi nimi
-                                               :fn   (if (= false (:tarkista-polku? optiot))
-                                                       transaktio-fn
-                                                       (ring-kasittelija nimi transaktio-fn))
+                                               :fn (if (= false (:tarkista-polku? optiot))
+                                                     transaktio-fn
+                                                     (ring-kasittelija nimi transaktio-fn))
                                                :ei-todennettava (:ei-todennettava optiot)})
         (do
           (when-let [liikaa-parametreja (some #(when (or (= 0 %) (> % 2)) %) ar)]
@@ -375,8 +369,8 @@
   ([http nimi reitti ei-todennettava?]
    (julkaise-palvelu http nimi (wrap-params reitti)
                      {:ring-kasittelija? true
-                      :tarkista-polku?   false
-                      :ei-todennettava   ei-todennettava?})))
+                      :tarkista-polku? false
+                      :ei-todennettava ei-todennettava?})))
 
 (defn julkaise-palvelut [http & nimet-ja-palvelut]
   (doseq [[nimi palvelu-fn] (partition 2 nimet-ja-palvelut)]
