071e95bdeb2073b6f99e900067c924e95a361227
Erno Kuusela <erno-laptop!erno>
2017-10-23 11:14:35 +0300

    Päivitä sopimuksen paasopimus oikein

--- src/clj/harja/kyselyt/sopimukset.sql
+++ src/clj/harja/kyselyt/sopimukset.sql
@@ -14,7 +14,11 @@ SET nimi              = :nimi,
   alkupvm             = :alkupvm,
   loppupvm            = :loppupvm,
   urakka_sampoid      = :urakka_sampoid,
-  urakoitsija_sampoid = :urakoitsija_sampoid
+  urakoitsija_sampoid = :urakoitsija_sampoid,
+  paasopimus = (SELECT id
+       FROM sopimus
+       WHERE urakka_sampoid = :urakka_sampoid
+             AND paasopimus IS NULL)
 WHERE id = :id;
 
 -- name: hae-id-sampoidlla
