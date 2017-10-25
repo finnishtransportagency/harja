4d29f66d9d4cfc4d788dc733ea536df9d06d2bfe
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-10-25 11:05:14 +0300

    Muokkaa kohteita lomakkeella

--- src/cljs/harja/tiedot/kanavat/hallinta/kohteiden_luonti.cljs
+++ src/cljs/harja/tiedot/kanavat/hallinta/kohteiden_luonti.cljs
@@ -2,6 +2,7 @@
   (:require [reagent.core :refer [atom]]
             [tuck.core :as tuck]
             [cljs.core.async :as async]
+            [clojure.set :as set]
             [harja.pvm :as pvm]
             [harja.id :refer [id-olemassa?]]
             [harja.asiakas.kommunikaatio :as k]
@@ -10,20 +11,28 @@
             [harja.tyokalut.tuck :as tt]
 
             [harja.domain.kanavat.kanava :as kanava]
-            [harja.domain.kanavat.kanavan-kohde :as kohde])
+            [harja.domain.kanavat.kanavan-kohde :as kohde]
+            [harja.domain.muokkaustiedot :as m])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def tila (atom {:nakymassa? false
                  :kohteiden-haku-kaynnissa? false
                  :kohdelomake-auki? false
                  :kohderivit nil
-                 :kanavat nil}))
+                 :kanavat nil
+                 :lomakkeen-tiedot nil}))
 
 (defrecord Nakymassa? [nakymassa?])
 (defrecord HaeKohteet [])
 (defrecord KohteetHaettu [tulos])
 (defrecord KohteetEiHaettu [virhe])
 (defrecord AvaaKohdeLomake [])
+(defrecord SuljeKohdeLomake [])
+(defrecord ValitseKanava [kanava])
+(defrecord LisaaKohteita [tiedot])
+(defrecord TallennaKohteet [])
+(defrecord KohteetTallennettu [tulos])
+(defrecord KohteetEiTallennettu [virhe])
 
 (defn kohderivit [tulos]
   (mapcat
@@ -45,6 +54,32 @@
 
 (defn kanavat [tulos]
   (map #(select-keys % #{::kanava/id ::kanava/nimi}) tulos))
+
+(defn kohteet-voi-tallentaa? [kohteet]
+  (and (:kanava kohteet)
+       (not-empty (:kohteet kohteet))
+       (every?
+         (fn [kohde]
+           (and (::kohde/tyyppi kohde)))
+         (:kohteet kohteet))))
+
+(defn muokattavat-kohteet [app]
+  (get-in app [:lomakkeen-tiedot :kohteet]))
+
+(defn tallennusparametrit [lomake]
+  (let [kanava-id (get-in lomake [:kanava ::kanava/id])
+        params (->> (:kohteet lomake)
+                    (map #(assoc % ::kohde/kanava-id kanava-id))
+                    (map #(set/rename-keys % {:id ::kohde/id
+                                              :poistettu ::m/poistettu?}))
+                    (map #(select-keys
+                            %
+                            [::kohde/nimi
+                             ::kohde/id
+                             ::kohde/kanava-id
+                             ::kohde/tyyppi
+                             ::m/poistettu?])))]
+    params))
 
 (extend-protocol tuck/Event
   Nakymassa?
@@ -77,4 +112,54 @@
 
   AvaaKohdeLomake
   (process-event [_ app]
-    (assoc app :kohdelomake-auki? true)))
+    (assoc app :kohdelomake-auki? true))
+
+  SuljeKohdeLomake
+  (process-event [_ app]
+    (-> app
+        (assoc :kohdelomake-auki? false)
+        (assoc :lomakkeen-tiedot nil)))
+
+  ValitseKanava
+  (process-event [{kanava :kanava} app]
+    (-> app
+        (assoc-in [:lomakkeen-tiedot :kanava] kanava)
+        (assoc-in [:lomakkeen-tiedot :kohteet]
+                  (filter
+                    (fn [kohde]
+                      (= (::kanava/id kohde)
+                         (::kanava/id kanava)))
+                    (:kohderivit app)))))
+
+  LisaaKohteita
+  (process-event [{tiedot :tiedot} app]
+    (assoc-in app [:lomakkeen-tiedot :kohteet] tiedot))
+
+  TallennaKohteet
+  (process-event [_ {tiedot :lomakkeen-tiedot :as app}]
+    (if-not (:kohteiden-haku-kaynnissa? app)
+      (-> app
+          (tt/post! :lisaa-kanavalle-kohteita
+                    (tallennusparametrit tiedot)
+                    {:onnistui ->KohteetTallennettu
+                     :epaonnistui ->KohteetEiTallennettu})
+
+          (assoc :kohteiden-tallennus-kaynnissa? true))
+
+      app))
+
+  KohteetTallennettu
+  (process-event [{tulos :tulos} app]
+    (log "Saatiin tulos" (pr-str tulos))
+    (-> app
+        (assoc :kohderivit (kohderivit tulos))
+        (assoc :kanavat (kanavat tulos))
+        (assoc :kohdelomake-auki? false)
+        (assoc :lomakkeen-tiedot nil)
+        (assoc :kohteiden-tallennus-kaynnissa? false)))
+
+  KohteetEiTallennettu
+  (process-event [_ app]
+    (viesti/nayta! "Kohteiden tallennus epäonnistui!" :danger)
+    (-> app
+        (assoc :kohteiden-tallennus-kaynnissa? false))))
--- src/cljs/harja/views/kanavat/hallinta/kohteiden_luonti.cljs
+++ src/cljs/harja/views/kanavat/hallinta/kohteiden_luonti.cljs
@@ -17,14 +17,75 @@
 
             [harja.domain.oikeudet :as oikeudet]
             [harja.domain.kanavat.kanavan-kohde :as kohde]
-            [harja.ui.napit :as napit])
+            [harja.domain.kanavat.kanava :as kanava]
+            [harja.ui.napit :as napit]
+            [harja.ui.debug :as debug])
   (:require-macros
     [cljs.core.async.macros :refer [go]]
     [harja.makrot :refer [defc fnc]]
     [harja.tyokalut.ui :refer [for*]]))
 
-(defn luontilomake [e! app]
-  [:div "Moi"])
+(defn kohteet-grid [e! kanavan-kohteet]
+  [grid/muokkaus-grid
+   {:tyhja "Lisää kohteita oikeasta yläkulmasta"
+    :tunniste ::kohde/id}
+   [{:otsikko "Kohteen nimi"
+     :tyyppi :string
+     :nimi ::kohde/nimi}
+    {:otsikko "Kohteen tyyppi"
+     :tyyppi :valinta
+     :nimi ::kohde/tyyppi
+     :valinnat #{:silta :sulku :sulku-ja-silta}
+     :valinta-nayta kohde/tyyppi->str
+     :pakollinen? true
+     :validoi [[:ei-tyhja "Anna kohteelle tyyppi"]]}]
+   (r/wrap
+     (into {}
+           (map-indexed
+             (fn [i k] [i k])
+             kanavan-kohteet))
+     #(e! (tiedot/->LisaaKohteita (vals %))))])
+
+(defn luontilomake [e! {tallennus-kaynnissa? :kohteiden-tallennus-kaynnissa?
+                        kanavat :kanavat
+                        {muokattava-kanava :kanava
+                         :as uudet-tiedot} :lomakkeen-tiedot
+                        :as app}]
+  [:div
+   [debug/debug app]
+   [napit/takaisin #(e! (tiedot/->SuljeKohdeLomake))]
+   [lomake/lomake
+    {:otsikko "Lisää kanavalle kohteita"
+     ;; muokkaa! on nykyään pakollinen lomakkeelle, mutta tässä lomakeessa
+     ;; tietojen päivittämienn tehdään suoraan riville asetetuilla funktioilla
+     :muokkaa! (constantly true)
+     :footer-fn (fn [kohteet]
+                  [napit/tallenna
+                   "Tallenna kohteet"
+                   #(e! (tiedot/->TallennaKohteet))
+                   {:tallennus-kaynnissa? tallennus-kaynnissa?
+                    :disabled (or (not (lomake/voi-tallentaa? kohteet))
+                                  (not (tiedot/kohteet-voi-tallentaa? kohteet))
+                                  (not (oikeudet/voi-kirjoittaa? oikeudet/hallinta-vesivaylat)))}])}
+    [{:otsikko "Kanava"
+      :tyyppi :valinta
+      :nimi :kanava
+      :valinnat (sort-by ::kanava/nimi kanavat)
+      :valinta-nayta #(or (::kanava/nimi %) "Valitse kanava")
+      :aseta (fn [_ arvo]
+               ;; kentat/atomina funktiossa katsotaan, onko :aseta ehto annettu
+               ;; jos on, oletetaan, että funktio palauttaa lomakkeen käyttämän datan
+               (:lomakkeen-tiedot (e! (tiedot/->ValitseKanava arvo))))}
+     (when muokattava-kanava
+       {:otsikko "Kohteet"
+        :nimi :kohteet
+        :tyyppi :komponentti
+        :palstoja 2
+        :komponentti (fn [_]
+                       [kohteet-grid
+                        e!
+                        (tiedot/muokattavat-kohteet app)])})]
+    uudet-tiedot]])
 
 (defn kohteiden-luonti* [e! app]
   (komp/luo
@@ -35,23 +96,32 @@
     (fn [e! {:keys [kohderivit kohteiden-haku-kaynnissa? kohdelomake-auki?] :as app}]
       (if-not kohdelomake-auki?
         [:div
-         [grid/grid
-          {:tunniste ::kohde/id
-           :tyhja (if kohteiden-haku-kaynnissa?
-                    [ajax-loader "Haetaan kohteita"]
-                    "Ei perustettuja kohteita")}
-          [{:otsikko "Kanava, kohde, ja kohteen tyyppi" :nimi :rivin-teksti}]
-          kohderivit]
+         [debug/debug app]
+         [:div
+          {:style {:width "50%"
+                   :display "inline-block"
+                   :padding-right "30px"}}
+          [grid/grid
+           {:tunniste ::kohde/id
+            :tyhja (if kohteiden-haku-kaynnissa?
+                     [ajax-loader "Haetaan kohteita"]
+                     "Ei perustettuja kohteita")}
+           [{:otsikko "Kanava, kohde, ja kohteen tyyppi" :nimi :rivin-teksti}]
+           (sort-by :rivin-teksti kohderivit)]]
+         [:div
+          {:style {:width "50%"
+                   :display "inline-block"
+                   :vertical-align "top"}}
+          [grid/grid
+           {:tyhja "Placeholder"}
+           [{:otsikko "Tämä grid on vain placeholder" :nimi :rivin-teksti}]
+           []]]
          [napit/uusi
           "Lisää uusi kohde"
-          #(e! (tiedot/->AvaaKohdeLomake))]]
+          #(e! (tiedot/->AvaaKohdeLomake))
+          {:disabled (not (oikeudet/voi-kirjoittaa? oikeudet/hallinta-vesivaylat))}]]
 
-        [modal/modal
-         {:otsikko "Lisää kohde Harjaan"
-          :footer [:span
-                   [napit/tallenna "Tallenna kohteet" #(println "Tallenna!")]
-                   [napit/peruuta "Peruuta tallentamatta" #(println "Peruuta!")]]}
-         [luontilomake e! app]]))))
+        [luontilomake e! app]))))
 
 (defc kohteiden-luonti []
   [tuck tiedot/tila kohteiden-luonti*])
