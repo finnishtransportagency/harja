99749da493ac72b5261a889114afecb395c53e7b
mikkoronkkomaki <mikko.ronkkomaki@solita.fi>
2017-10-27 13:57:36 +0300

    Nimeä huoltokohteet käyttämään "standaria prefiksiä" kanavatauluille

--- tietokanta/src/main/resources/db/migration/V1_630__.sql
+++ tietokanta/src/main/resources/db/migration/V1_630__.sql
@@ -1,8 +1,11 @@
+ALTER TABLE kanavahuoltokohde
+  RENAME TO kan_huoltokohde;
+
 CREATE TABLE kan_toimenpide (
   id          SERIAL PRIMARY KEY,
   pvm         DATE,
   kohde       INTEGER REFERENCES kan_kohde (id)          NOT NULL,
-  huoltokohde INTEGER REFERENCES kanavahuoltokohde (id) NOT NULL,
+  huoltokohde INTEGER REFERENCES kan_huoltokohde (id)    NOT NULL,
   toimenpide  INTEGER REFERENCES toimenpidekoodi (id),
   lisatieto   TEXT,
   suorittaja  INTEGER REFERENCES kayttaja (id)           NOT NULL,
