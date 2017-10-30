65724658f4f266281f3f17a217672d6c316b3cfc
Jari Hanhela <jarihanhela@gmail.com>
2017-10-30 12:07:18 +0200

    Aseta poistettu-kentälle DEFAULT arvo

--- tietokanta/src/main/resources/db/migration/V1_631__.sql
+++ tietokanta/src/main/resources/db/migration/V1_631__.sql
@@ -1,3 +1,4 @@
 -- vv_vaylan poistettu-kenttä ei voi olla NULL
 UPDATE vv_vayla SET poistettu = FALSE WHERE poistettu IS NULL;
 ALTER TABLE vv_vayla ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE vv_vayla ALTER COLUMN poistettu SET DEFAULT FALSE;
