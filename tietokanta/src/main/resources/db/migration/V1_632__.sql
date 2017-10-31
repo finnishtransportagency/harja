7b082833afc193b7b41bdefdd27fa38010cb115b
Jari Hanhela <jarihanhela@gmail.com>
2017-10-31 14:43:46 +0200

    Tarkenna häiriö-taulun sisältöä

--- tietokanta/src/main/resources/db/migration/V1_632__.sql
+++ tietokanta/src/main/resources/db/migration/V1_632__.sql
@@ -2,20 +2,20 @@
 
 CREATE TYPE kan_hairio_korjauksen_tila AS ENUM ('kesken', 'valmis');
 
-CREATE TABLE kan_hairio {
+CREATE TABLE kan_hairio (
   id serial PRIMARY KEY,
-  urakka integer references urakka (id),
+  urakka INTEGER REFERENCES urakka (id) NOT NULL,
   pvm DATE NOT NULL,
-  kohde,
-  vikaluokka, -- TODO PDF?
-  syy,
-  odotusaika,
-  ammatil-lkm, -- TODO Mistä tämä on lyhenne?
-  huvil-lkm, -- TODO Mistä tämä on lyhenne?
-  korjaustoimenpide,
-  korjausaika,
-  korjauksen_tila,
-  paikallinen_kaytto,
-}
+  kohde INTEGER REFERENCES kan_kohde (id) NOT NULL,
+  vikaluokka, -- TODO Kanava-epicin PDF:ssä, enum?
+  syy VARCHAR(512) NOT NULL,
+  odotusaika_h INTEGER, -- TODO INTEGER  tunteina
+  ammatilaiva-lkm INTEGER, -- TODO integer, tarkista pdf
+  huvilaiva-lkm INTEGER, -- TODO integer, tarkista pdf
+  korjaustoimenpide VARCHAR(512),
+  korjausaika_h INTEGER,
+  korjauksen_tila kan_hairio_korjauksen_tila,
+  paikallinen_kaytto BOOLEAN NOT NULL
+);
 
 -- TODO DOC
