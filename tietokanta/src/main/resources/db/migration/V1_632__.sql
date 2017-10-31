49e368da0808764602d40f5a823fa6cda2e50fa7
Jari Hanhela <jarihanhela@gmail.com>
2017-10-31 15:10:30 +0200

    Viimeistele häiriön tietomalli

--- tietokanta/src/main/resources/db/migration/V1_632__.sql
+++ tietokanta/src/main/resources/db/migration/V1_632__.sql
@@ -1,13 +1,14 @@
 -- Häiriötilanteet
 
 CREATE TYPE kan_hairio_korjauksen_tila AS ENUM ('kesken', 'valmis');
+CREATE TYPE kan_hairio_vikaluokka AS ENUM ('sahkotekninen_vika', 'konetekninen_vika', 'liikennevaurio');
 
 CREATE TABLE kan_hairio (
   id serial PRIMARY KEY,
   urakka INTEGER REFERENCES urakka (id) NOT NULL,
   pvm DATE NOT NULL,
   kohde INTEGER REFERENCES kan_kohde (id) NOT NULL,
-  vikaluokka, -- TODO Kanava-epicin PDF:ssä, enum?
+  vikaluokka kan_hairio_vikaluokka,
   syy VARCHAR(512) NOT NULL,
   odotusaika_h INTEGER,
   ammatiliikenne_lkm INTEGER,
