5be1a273119d6ba3e0685c2e95a28d4a9ef1139b
Jari Hanhela <jarihanhela@gmail.com>
2017-10-31 15:38:38 +0200

    Luo pohjaa häiriötilannenäkymälle

--- src/cljs/harja/tiedot/kanavat/urakka/laadunseuranta/hairiotilanteet.cljs
+++ src/cljs/harja/tiedot/kanavat/urakka/laadunseuranta/hairiotilanteet.cljs
@@ -6,14 +6,33 @@
             [harja.id :refer [id-olemassa?]]
             [harja.asiakas.kommunikaatio :as k]
             [harja.loki :refer [log tarkkaile!]]
-            [harja.ui.viesti :as viesti])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+            [harja.ui.viesti :as viesti]
+            [harja.tiedot.navigaatio :as nav]
+            [harja.tiedot.urakka :as u])
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [reagent.ratom :refer [reaction]]))
 
-(def tila (atom {:nakymassa? false}))
+(def tila (atom {:nakymassa? false
+                 :valinnat nil}))
 
 (defrecord Nakymassa? [nakymassa?])
+(defrecord PaivitaValinnat [valinnat])
+
+(defonce valinnat
+  (reaction
+    (when (:nakymassa? @tila)
+      {:urakka-id (:id @nav/valittu-urakka)
+       :sopimus-id (first @u/valittu-sopimusnumero)})))
 
 (extend-protocol tuck/Event
   Nakymassa?
   (process-event [{nakymassa? :nakymassa?} app]
-    (assoc app :nakymassa? nakymassa?)))
+    (assoc app :nakymassa? nakymassa?))
+
+  PaivitaValinnat
+  (process-event [{val :valinnat} app]
+    (let [uudet-valinnat (merge (:valinnat app) val)
+          ;haku (tuck/send-async! ->HaeKiintiot)
+          ]
+      #_(go (haku uudet-valinnat))
+      (assoc app :valinnat uudet-valinnat))))
--- src/cljs/harja/views/kanavat/urakka/laadunseuranta/hairiotilanteet.cljs
+++ src/cljs/harja/views/kanavat/urakka/laadunseuranta/hairiotilanteet.cljs
@@ -15,19 +15,50 @@
             [harja.ui.debug :refer [debug]]
             [harja.ui.modal :as modal]
 
-            [harja.domain.oikeudet :as oikeudet])
+            [harja.domain.oikeudet :as oikeudet]
+            [harja.ui.valinnat :as valinnat]
+            [harja.views.urakka.valinnat :as urakka-valinnat]
+            [harja.ui.yleiset :as yleiset]
+            [harja.ui.napit :as napit])
   (:require-macros
     [cljs.core.async.macros :refer [go]]
     [harja.makrot :refer [defc fnc]]
     [harja.tyokalut.ui :refer [for*]]))
 
+(defn- suodattimet-ja-toiminnot [valittu-urakka]
+  [valinnat/urakkavalinnat {:urakka valittu-urakka}
+   ^{:key "urakkavalinnat"}
+   [urakka-valinnat/urakan-hoitokausi valittu-urakka]
+   ^{:key "urakkatoiminnot"}
+   [valinnat/urakkatoiminnot {:urakka valittu-urakka}
+    (let [oikeus? true ;; TODO Oikeustarkistus, roolit-excelin päivitys
+          #_(oikeudet/voi-kirjoittaa? oikeudet/urakat-laadunseuranta-sanktiot
+                                            (:id valittu-urakka))]
+      (yleiset/wrap-if
+        (not oikeus?)
+        [yleiset/tooltip {} :%
+         (oikeudet/oikeuden-puute-kuvaus :kirjoitus
+                                         ;; TODO Oikea oikeustarkistus
+                                         oikeudet/urakat-laadunseuranta-sanktiot)]
+        ^{:key "Lisää sanktio"}
+        [napit/uusi "Lisää häiriötilanne"
+         #(log "TODO Lisää häiriötilanne")
+         {:disabled (not oikeus?)}]))]])
+
+(defn- hairiolista []
+  [:div "TODO Häiriölista"])
+
 (defn hairiotilanteet* [e! app]
   (komp/luo
+    (komp/watcher tiedot/valinnat (fn [_ _ uusi]
+                                    (e! (tiedot/->PaivitaValinnat uusi))))
     (komp/sisaan-ulos #(e! (tiedot/->Nakymassa? true))
                       #(e! (tiedot/->Nakymassa? false)))
 
     (fn [e! app]
-      [:div "WIP"])))
+      [:div
+       [suodattimet-ja-toiminnot (get-in app [:valinnat :urakka-id])]
+       [hairiolista]])))
 
 (defc hairiotilanteet []
   [tuck tiedot/tila hairiotilanteet*])
