df1be3c8dd3b9386c1f8d2c34a0924341dc7fce2
Jari Hanhela <jarihanhela@gmail.com>
2017-11-01 10:34:22 +0200

    Lisää testidataa häiriöille

--- tietokanta/testidata/kanavat/hairiotilanteet.sql
+++ tietokanta/testidata/kanavat/hairiotilanteet.sql
@@ -1,1 +1,41 @@
-INSERT INTO kan_hairio (urakka, sopimus, pvm, kohde, vikaluokka, syy, odotusaika_h, ammattiliikenne_lkm, huviliikenne_lkm, korjaustoimenpide, korjausaika_h, korjauksen_tila, paikallinen_kaytto, luoja, luotu) VALUES ((SELECT id FROM urakka WHERE nimi = 'Saimaan kanava'), (SELECT id FROM sopimus WHERE nimi = 'Saimaan huollon pääsopimus'), '2017-07-14', (SELECT id FROM kan_kohde WHERE nimi = 'Taipaleen sulku'), 'sahkotekninen_vika'::kan_hairio_vikaluokka, 'Jotain meni vikaan', 60, 1, 2, 'Vika korjattiin', 100, 'valmis'::kan_hairio_korjauksen_tila, true, (SELECT id FROM kayttaja WHERE kayttajanimi = 'jvh'), NOW());
+INSERT INTO kan_hairio (urakka, sopimus, pvm, kohde, vikaluokka, syy, odotusaika_h, ammattiliikenne_lkm,
+                        huviliikenne_lkm, korjaustoimenpide, korjausaika_h, korjauksen_tila, paikallinen_kaytto,
+                        luoja, luotu)
+VALUES (
+  (SELECT id
+   FROM urakka
+   WHERE nimi = 'Saimaan kanava'),
+  (SELECT id
+   FROM sopimus
+   WHERE nimi = 'Saimaan huollon pääsopimus'), '2017-07-14',
+  (SELECT id
+   FROM kan_kohde
+   WHERE nimi =
+         'Taipaleen sulku'),
+  'sahkotekninen_vika' :: KAN_HAIRIO_VIKALUOKKA, 'Jotain meni vikaan', 60, 1, 2,
+  'Vika korjattiin', 100, 'valmis' :: KAN_HAIRIO_KORJAUKSEN_TILA, TRUE,
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  NOW());
+
+INSERT INTO kan_hairio (urakka, sopimus, pvm, kohde, vikaluokka, syy, odotusaika_h, ammattiliikenne_lkm,
+                        huviliikenne_lkm, korjaustoimenpide, korjausaika_h, korjauksen_tila, paikallinen_kaytto,
+                        luoja, luotu)
+VALUES (
+  (SELECT id
+   FROM urakka
+   WHERE nimi = 'Saimaan kanava'),
+  (SELECT id
+   FROM sopimus
+   WHERE nimi = 'Saimaan huollon pääsopimus'), '2017-07-15',
+  (SELECT id
+   FROM kan_kohde
+   WHERE nimi =
+         'Taipaleen sulku'),
+  'sahkotekninen_vika' :: KAN_HAIRIO_VIKALUOKKA, 'Edellinen korjaus tehtiin huonosti, korjattu nyt uudestaan.', 60, 1, 2,
+  'Vika korjattiin', 20, 'valmis' :: KAN_HAIRIO_KORJAUKSEN_TILA, TRUE,
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  NOW());
