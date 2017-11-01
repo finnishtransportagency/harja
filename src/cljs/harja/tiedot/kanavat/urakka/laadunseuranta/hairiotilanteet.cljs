792d9a3f407b99af126f38ce1baf58a2c52dd7c4
Jari Hanhela <jarihanhela@gmail.com>
2017-11-01 08:35:41 +0200

    Luo eventit häiriötilanteiden haulle

--- src/cljs/harja/tiedot/kanavat/urakka/laadunseuranta/hairiotilanteet.cljs
+++ src/cljs/harja/tiedot/kanavat/urakka/laadunseuranta/hairiotilanteet.cljs
@@ -8,7 +8,8 @@
             [harja.loki :refer [log tarkkaile!]]
             [harja.ui.viesti :as viesti]
             [harja.tiedot.navigaatio :as nav]
-            [harja.tiedot.urakka :as u])
+            [harja.tiedot.urakka :as u]
+            [harja.tyokalut.tuck :as tuck-apurit])
   (:require-macros [cljs.core.async.macros :refer [go]]
                    [reagent.ratom :refer [reaction]]))
 
@@ -17,8 +18,13 @@
                  :hairiotilanteiden-haku-kaynnissa? false
                  :valinnat nil}))
 
+;; Yleiset
 (defrecord Nakymassa? [nakymassa?])
 (defrecord PaivitaValinnat [valinnat])
+;; Haut
+(defrecord HaeHairioTilanteet [valinnat])
+(defrecord HairioTilanteetHaettu [tulos])
+(defrecord HairioTilanteetEiHaettu [])
 
 (defonce valinnat
   (reaction
@@ -37,4 +43,31 @@
           ;haku (tuck/send-async! ->HaeKiintiot)
           ]
       #_(go (haku uudet-valinnat))
-      (assoc app :valinnat uudet-valinnat))))
+      (assoc app :valinnat uudet-valinnat)))
+
+  HaeHairioTilanteet
+  (process-event [{valinnat :valinnat} app]
+    (if (and (not (:hairiotilanteiden-haku-kaynnissa? app))
+             (some? (:urakka-id valinnat)))
+      (let [parametrit {::hairiotilanne/urakka-id (:urakka-id valinnat)
+                        ::hairiotilanne/sopimus-id (:sopimus-id valinnat)}]
+        (-> app
+            (tuck-apurit/post! :hae-hairiotilanteet
+                               parametrit
+                               {:onnistui ->HairioTilanteetHaettu
+                                :epaonnistui ->HairioTilanteetEiHaettu})
+            (assoc :hairiotilanteiden-haku-kaynnissa? true)))
+
+      app))
+
+  HairioTilanteetHaettu
+  (process-event [{tulos :tulos} app]
+    (assoc app :hairiotilanteiden-haku-kaynnissa? false
+               :hairiotilanteet tulos))
+
+  HairioTilanteetEiHaettu
+  (process-event [_ app]
+    (viesti/nayta! "Häiriötilanteiden haku epäonnistui!" :danger)
+    (assoc app :hairiotilanteiden-haku-kaynnissa? false
+               :hairiotilanteet [])))
+
