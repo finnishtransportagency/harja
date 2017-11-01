d80b079efca633bce9b13eaa03fc8ce3645b0eca
mikkoro <mikko.ronkkomaki@gmail.com>
2017-11-01 10:51:11 +0200

    Tarkenna hakuehtoja

--- src/clj/harja/palvelin/palvelut/kanavat/kanavatoimenpiteet.clj
+++ src/clj/harja/palvelin/palvelut/kanavat/kanavatoimenpiteet.clj
@@ -4,10 +4,17 @@
             [taoensso.timbre :as log]
             [harja.palvelin.komponentit.http-palvelin :refer [julkaise-palvelu poista-palvelut]]
             [harja.domain.oikeudet :as oikeudet]
+            [harja.domain.sopimus :as sopimus]
+            [harja.domain.toimenpidekoodi :as toimenpidekoodi]
             [harja.domain.kanavat.kanavan-toimenpide :as kanavan-toimenpide]
             [harja.kyselyt.kanavat.kanavan-toimenpide :as q-kanavan-toimenpide]))
 
-(defn hae-kanavatoimenpiteet [db user {:keys [sopimus alkupvm loppupvm toimenpidekoodi tyyppi] :as hakuehdot}]
+(defn hae-kanavatoimenpiteet [db user {sopimus ::sopimus/id
+                                       alkupvm :alkupvm
+                                       loppupvm :loppupvm
+                                       toimenpidekoodi ::toimenpidekoodi/id
+                                       tyyppi ::kanavan-toimenpide/kanava-toimenpidetyyppi
+                                       :as hakuehdot}]
   (oikeudet/vaadi-lukuoikeus oikeudet/hallinta-vesivaylat user)
   (q-kanavan-toimenpide/hae-sopimuksen-toimenpiteet-aikavalilta db sopimus alkupvm loppupvm toimenpidekoodi tyyppi))
 
