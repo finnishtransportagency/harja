769a8e5444620ae6700e6dd3f85dceb9f3171c95
mikkoro <mikko.ronkkomaki@gmail.com>
2017-11-02 09:06:21 +0200

    Lisää testitoimenpideinstanssi Saimaan urakalle

--- tietokanta/testidata/kanavat/kanavat_ja_kohteet.sql
+++ tietokanta/testidata/kanavat/kanavat_ja_kohteet.sql
@@ -1,18 +1,48 @@
 -- Kanavien ja kohteiden testidata
 
 INSERT INTO kan_kohde (nimi, tyyppi, "kanava-id", luoja)
-    VALUES
-      (NULL, 'sulku' :: kohteen_tyyppi, (SELECT id FROM kan_kanava WHERE NIMI = 'Keiteleen kanava: Kapeenkosken kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
-      (NULL, 'sulku' :: kohteen_tyyppi, (SELECT id FROM kan_kanava WHERE NIMI = 'Keiteleen kanava: Kuhankosken kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
-      ('Tikkalansaaren avattava ratasilta', 'silta' :: kohteen_tyyppi, (SELECT id FROM kan_kanava WHERE NIMI = 'Taipaleen kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
-      ('Taipaleen sulku', 'sulku' :: kohteen_tyyppi, (SELECT id FROM kan_kanava WHERE NIMI = 'Taipaleen kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
-      (NULL, 'sulku-ja-silta' :: kohteen_tyyppi, (SELECT id FROM kan_kanava WHERE NIMI = 'Nerkoon kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'));
+VALUES
+  (NULL, 'sulku' :: KOHTEEN_TYYPPI, (SELECT id
+                                     FROM kan_kanava
+                                     WHERE NIMI = 'Keiteleen kanava: Kapeenkosken kanava'), (SELECT id
+                                                                                             FROM kayttaja
+                                                                                             WHERE
+                                                                                               kayttajanimi = 'tero')),
+  (NULL, 'sulku' :: KOHTEEN_TYYPPI, (SELECT id
+                                     FROM kan_kanava
+                                     WHERE NIMI = 'Keiteleen kanava: Kuhankosken kanava'), (SELECT id
+                                                                                            FROM kayttaja
+                                                                                            WHERE
+                                                                                              kayttajanimi = 'tero')),
+  ('Tikkalansaaren avattava ratasilta', 'silta' :: KOHTEEN_TYYPPI, (SELECT id
+                                                                    FROM kan_kanava
+                                                                    WHERE NIMI = 'Taipaleen kanava'), (SELECT id
+                                                                                                       FROM kayttaja
+                                                                                                       WHERE
+                                                                                                         kayttajanimi =
+                                                                                                         'tero')),
+  ('Taipaleen sulku', 'sulku' :: KOHTEEN_TYYPPI, (SELECT id
+                                                  FROM kan_kanava
+                                                  WHERE NIMI = 'Taipaleen kanava'), (SELECT id
+                                                                                     FROM kayttaja
+                                                                                     WHERE kayttajanimi = 'tero')),
+  (NULL, 'sulku-ja-silta' :: KOHTEEN_TYYPPI, (SELECT id
+                                              FROM kan_kanava
+                                              WHERE NIMI = 'Nerkoon kanava'), (SELECT id
+                                                                               FROM kayttaja
+                                                                               WHERE kayttajanimi = 'tero'));
 
 INSERT INTO kan_kohde_urakka ("kohde-id", "urakka-id", luoja)
-    VALUES
-      ((SELECT id FROM kan_kohde WHERE nimi = 'Tikkalansaaren avattava ratasilta'),
-       (SELECT id FROM urakka WHERE nimi = 'Saimaan kanava'),
-       (SELECT id FROM kayttaja WHERE kayttajanimi='tero'));
+VALUES
+  ((SELECT id
+    FROM kan_kohde
+    WHERE nimi = 'Tikkalansaaren avattava ratasilta'),
+   (SELECT id
+    FROM urakka
+    WHERE nimi = 'Saimaan kanava'),
+   (SELECT id
+    FROM kayttaja
+    WHERE kayttajanimi = 'tero'));
 
 INSERT INTO kan_toimenpide
 (tyyppi,
@@ -59,4 +89,15 @@ VALUES ('kokonaishintainen' :: KAN_TOIMENPIDETYYPPI,
          FROM kayttaja
          WHERE kayttajanimi = 'jvh'),
         NULL,
-        NULL)
+        NULL);
+
+INSERT INTO toimenpideinstanssi (urakka,
+                                 toimenpide,
+                                 nimi,
+                                 alkupvm,
+                                 loppupvm)
+VALUES (31,
+        597,
+        'Testitoimenpideinstanssi',
+        '2017-01-01',
+        '2090-01-01');
