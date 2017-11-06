b8f3f99481f48b3ce2ecfe154e1ce23c2c12dc8c
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-11-06 10:50:41 +0200

    Näytä liikennetapahtumat gridissä

--- src/cljs/harja/tiedot/kanavat/urakka/liikenne.cljs
+++ src/cljs/harja/tiedot/kanavat/urakka/liikenne.cljs
@@ -1,19 +1,92 @@
 (ns harja.tiedot.kanavat.urakka.liikenne
   (:require [reagent.core :refer [atom]]
             [tuck.core :as tuck]
+            [harja.tyokalut.tuck :as tt]
             [cljs.core.async :as async]
             [harja.pvm :as pvm]
             [harja.id :refer [id-olemassa?]]
             [harja.asiakas.kommunikaatio :as k]
             [harja.loki :refer [log tarkkaile!]]
-            [harja.ui.viesti :as viesti])
+            [harja.ui.viesti :as viesti]
+            [harja.tiedot.navigaatio :as nav]
+
+            [harja.domain.urakka :as ur]
+            [harja.domain.kanavat.kanavan-kohde :as kohde]
+            [harja.domain.kanavat.kanava :as kanava]
+            [harja.domain.kanavat.liikennetapahtuma :as lt]
+            [harja.domain.kanavat.lt-alus :as lt-alus]
+            [harja.domain.kanavat.lt-nippu :as lt-nippu])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(def tila (atom {:nakymassa? false}))
+(def tila (atom {:nakymassa? false
+                 :liikennetapahtumien-haku-kaynnissa? false
+                 :valittu-liikennetapahtuma nil
+                 :haetut-liikennetapahtumat nil
+                 :tapahtumarivit nil}))
+
+(def uusi-tapahtuma {})
 
 (defrecord Nakymassa? [nakymassa?])
+(defrecord HaeLiikennetapahtumat [])
+(defrecord LiikennetapahtumatHaettu [tulos])
+(defrecord LiikennetapahtumatEiHaettu [virhe])
+(defrecord ValitseTapahtuma [tapahtuma])
+
+(defn tapahtumarivit [tapahtuma]
+  (let [yleistiedot
+        (merge
+          tapahtuma
+          {:kohteen-nimi (str
+                           (when-let [nimi (get-in tapahtuma [::lt/kohde ::kohde/kohteen-kanava ::kanava/nimi])]
+                             (str nimi ", "))
+                           (when-let [nimi (get-in tapahtuma [::lt/kohde ::kohde/nimi])]
+                             (str nimi ", "))
+                           (when-let [tyyppi (kohde/tyyppi->str (get-in tapahtuma [::lt/kohde ::kohde/tyyppi]))]
+                             (str tyyppi)))})
+        alustiedot
+        (map
+          (fn [alus]
+            (merge
+              yleistiedot
+              alus
+              {:suunta (::lt-alus/suunta alus)}))
+          (::lt/alukset tapahtuma))
+
+        nipputiedot
+        (map
+          (fn [nippu]
+            (merge yleistiedot nippu {:suunta (::lt-nippu/suunta nippu)}))
+          (::lt/niput tapahtuma))]
+
+    (concat alustiedot nipputiedot)))
 
 (extend-protocol tuck/Event
   Nakymassa?
   (process-event [{nakymassa? :nakymassa?} app]
-    (assoc app :nakymassa? nakymassa?)))
+    (assoc app :nakymassa? nakymassa?))
+
+  HaeLiikennetapahtumat
+  (process-event [_ app]
+    (tt/post! :hae-liikennetapahtumat
+              {::ur/id (:id @nav/valittu-urakka)}
+              {:onnistui ->LiikennetapahtumatHaettu
+               :epaonnistui ->LiikennetapahtumatEiHaettu})
+
+    (log "HaeLiikennetapahtumat")
+    (assoc app :liikennetapahtumien-haku-kaynnissa? true))
+
+  LiikennetapahtumatHaettu
+  (process-event [{tulos :tulos} app]
+    (log (pr-str tulos))
+    (-> app
+        (assoc :liikennetapahtumien-haku-kaynnissa? false)
+        (assoc :tapahtumarivit (mapcat tapahtumarivit tulos))))
+
+  LiikennetapahtumatEiHaettu
+  (process-event [_ app]
+    (viesti/nayta! "Liikennetapahtumien haku epäonnistui! " :danger)
+    (assoc app :liikennetapahtumien-haku-kaynnissa? false))
+
+  ValitseTapahtuma
+  (process-event [{t :tapahtuma} app]
+    (assoc app :valittu-liikennetapahtuma t)))
--- src/cljs/harja/views/kanavat/urakka/liikenne.cljs
+++ src/cljs/harja/views/kanavat/urakka/liikenne.cljs
@@ -14,20 +14,84 @@
             [harja.ui.yleiset :refer [ajax-loader ajax-loader-pieni tietoja]]
             [harja.ui.debug :refer [debug]]
             [harja.ui.modal :as modal]
+            [harja.ui.valinnat :as valinnat]
+            [harja.tiedot.navigaatio :as nav]
+            [harja.views.urakka.valinnat :as suodattimet]
+            [harja.ui.napit :as napit]
 
-            [harja.domain.oikeudet :as oikeudet])
+            [harja.domain.kayttaja :as kayttaja]
+            [harja.domain.oikeudet :as oikeudet]
+            [harja.domain.kanavat.liikennetapahtuma :as lt]
+            [harja.domain.kanavat.lt-alus :as lt-alus]
+            [harja.domain.kanavat.lt-nippu :as lt-nippu])
   (:require-macros
     [cljs.core.async.macros :refer [go]]
     [harja.makrot :refer [defc fnc]]
     [harja.tyokalut.ui :refer [for*]]))
 
+(defn liikennetapahtumalomake [e! app]
+  [:div
+   [napit/takaisin "Takaisin" #(e! (tiedot/->ValitseTapahtuma nil))]
+   [:div "WIP"]])
+
+(defn liikennetapahtumataulukko [e! {:keys [tapahtumarivit
+                                            liikennetapahtumien-haku-kaynnissa?] :as app}]
+  [:div
+   [debug app]
+   [valinnat/urakkavalinnat
+    {}
+    ^{:key "valinnat"}
+    [suodattimet/urakan-sopimus-ja-hoitokausi-ja-aikavali @nav/valittu-urakka]]
+   [napit/uusi
+    "Lisää tapahtuma"
+    #(e! (tiedot/->ValitseTapahtuma tiedot/uusi-tapahtuma))]
+   [grid/grid
+    {:otsikko "Liikennetapahtumat"
+     :tunniste (juxt ::lt/id ::lt-alus/id ::lt-nippu/id)
+     :tyhja (if liikennetapahtumien-haku-kaynnissa?
+              [ajax-loader "Haku käynnissä"]
+              "Ei liikennetapahtumia")}
+    [{:otsikko "Päivämäärä"
+      :nimi ::lt/aika
+      :fmt pvm/pvm-aika-opt}
+     {:otsikko "Kohde"
+      :nimi :kohteen-nimi}
+     {:otsikko "Tyyppi"
+      :nimi ::lt/toimenpide
+      :fmt lt/toimenpide->str}
+     {:otsikko "Suunta"
+      :nimi :suunta}
+     {:otsikko "Alus"
+      :nimi ::lt-alus/nimi}
+     {:otsikko "Aluslaji"
+      :nimi ::lt-alus/laji}
+     {:otsikko "Matkustajia"
+      :nimi ::lt-alus/matkustajalkm}
+     {:otsikko "Aluksia"
+      :nimi ::lt-alus/lkm}
+     {:otsikko "IP kpl"
+      :nimi ::lt/palvelumuoto-lkm}
+     {:otsikko "Nippuja"
+      :nimi ::lt-nippu/lkm}
+     {:otsikko "Ylävesi"
+      :nimi ::lt/vesipinta-ylaraja}
+     {:otsikko "Alavesi"
+      :nimi ::lt/vesipinta-alaraja}
+     {:otsikko "Kuittaaja"
+      :nimi :kuittaaja
+      :hae (comp ::kayttaja/kayttajanimi ::lt/kuittaaja)}]
+    tapahtumarivit]])
+
 (defn liikenne* [e! app]
   (komp/luo
-    (komp/sisaan-ulos #(e! (tiedot/->Nakymassa? true))
+    (komp/sisaan-ulos #(do (e! (tiedot/->Nakymassa? true))
+                           (e! (tiedot/->HaeLiikennetapahtumat)))
                       #(e! (tiedot/->Nakymassa? false)))
 
-    (fn [e! app]
-      [:div "WIP"])))
+    (fn [e! {:keys [valittu-liikennetapahtuma] :as app}]
+      (if-not valittu-liikennetapahtuma
+        [liikennetapahtumataulukko e! app]
+        [liikennetapahtumalomake e! app]))))
 
 (defc liikenne []
   [tuck tiedot/tila liikenne*])
