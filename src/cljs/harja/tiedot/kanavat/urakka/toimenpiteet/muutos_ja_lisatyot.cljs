216bc01824af9130ad7bef6f42dbfaea3adaed90
Jari Hanhela <jarihanhela@gmail.com>
2017-11-06 15:44:24 +0200

    Luo pohjaa muutos- ja lisätöiden näkymälle

--- src/cljs/harja/tiedot/kanavat/urakka/toimenpiteet/muutos_ja_lisatyot.cljs
+++ src/cljs/harja/tiedot/kanavat/urakka/toimenpiteet/muutos_ja_lisatyot.cljs
@@ -6,15 +6,34 @@
             [harja.id :refer [id-olemassa?]]
             [harja.asiakas.kommunikaatio :as k]
             [harja.loki :refer [log tarkkaile!]]
-            [harja.ui.viesti :as viesti])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+            [harja.ui.viesti :as viesti]
+            [harja.tiedot.urakka :as u]
+            [harja.tiedot.navigaatio :as nav])
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [reagent.ratom :refer [reaction]]))
 
-(def tila (atom {:nakymassa? false}))
+(def tila (atom {:nakymassa? false
+                 :toimenpiteiden-haku-kaynnissa? false}))
+
+(defonce valinnat
+  (reaction
+    (when (:nakymassa? @tila)
+      {:urakka-id (:id @nav/valittu-urakka)
+       :sopimus-id (first @u/valittu-sopimusnumero)
+       :aikavali @u/valittu-aikavali})))
 
 (defrecord Nakymassa? [nakymassa?])
+(defrecord PaivitaValinnat [valinnat])
 
 (extend-protocol tuck/Event
   Nakymassa?
   (process-event [{nakymassa? :nakymassa?} app]
-    (assoc app :nakymassa? nakymassa?)))
+    (assoc app :nakymassa? nakymassa?))
+
+  PaivitaValinnat
+  (process-event [{valinnat :valinnat} app]
+    (let [;haku (tuck/send-async! ->HaeKokonaishintaisetToimenpiteet)
+    ]
+      #_(go (haku valinnat))
+      (assoc app :valinnat valinnat))))
 
