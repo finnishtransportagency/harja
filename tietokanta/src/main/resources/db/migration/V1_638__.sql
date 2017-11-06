4d027416cd9195fb82262bec96aa168a2b559b0e
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-11-06 08:52:06 +0200

    Lisää kantaan liikennetapahtumat

---
+++ tietokanta/src/main/resources/db/migration/V1_638__.sql
@@ -0,0 +1,94 @@
+CREATE TYPE LIIKENNETAPAHTUMA_TOIMENPIDETYYPPI AS ENUM ('sulutus', 'tyhjennys', 'sillan-avaus');
+CREATE TYPE LIIKENNETAPAHTUMA_PALVELUMUOTO AS ENUM ('kauko', 'itse', 'paikallis', 'muu');
+
+CREATE TABLE kan_liikennetapahtuma (
+  id                  SERIAL PRIMARY KEY,
+  "kohde-id"          INTEGER REFERENCES kan_kohde (id),
+  aika                TIMESTAMP                          NOT NULL,
+  toimenpide          LIIKENNETAPAHTUMA_TOIMENPIDETYYPPI NOT NULL,
+  palvelumuoto        LIIKENNETAPAHTUMA_PALVELUMUOTO,
+  "palvelumuoto-lkm"  INTEGER
+    CONSTRAINT vain_itsepalvelua_enemman_kuin_yksi CHECK
+    ("palvelumuoto-lkm" = 1 OR palvelumuoto = 'itse' OR palvelumuoto IS NULL),
+  lisatieto           TEXT,
+  "vesipinta-ylaraja" INTEGER,
+  "vesipinta-alaraja" INTEGER,
+
+  kuittaaja           INTEGER REFERENCES kayttaja (id),
+
+  luotu               TIMESTAMP DEFAULT NOW(),
+  luoja               INTEGER REFERENCES kayttaja (id)   NOT NULL,
+  muokattu            TIMESTAMP,
+  muokkaaja           INTEGER REFERENCES kayttaja (id),
+  poistettu           BOOLEAN   DEFAULT FALSE,
+  poistaja            INTEGER REFERENCES kayttaja (id)
+);
+
+CREATE OR REPLACE FUNCTION toimenpide_kohteen_mukaan_proc()
+  RETURNS TRIGGER AS
+$$
+DECLARE kohteen_tyyppi KOHTEEN_TYYPPI;
+BEGIN
+  kohteen_tyyppi := (SELECT kan_kohde.tyyppi
+                     FROM kan_kohde
+                     WHERE id = NEW."kohde-id");
+  IF ((NEW.toimenpide = 'sillan-avaus'
+       AND
+       (kohteen_tyyppi IN ('silta', 'sulku-ja-silta'))
+       AND (NEW.palvelumuoto IS NULL))
+      OR
+      (NEW.toimenpide IN ('sulutus', 'tyhjennys')
+       AND
+       (kohteen_tyyppi IN ('sulku', 'sulku-ja-silta'))
+       AND (NEW.palvelumuoto IS NOT NULL)))
+  THEN
+    RETURN NEW;
+  ELSE
+    RAISE EXCEPTION 'Liikennetapahtuman toimenpide %, % ei vastaa kohteen tyyppiä %', NEW.toimenpide, NEW.palvelumuoto, kohteen_tyyppi;
+    RETURN NULL;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS toimenpide_kohteen_mukaan_trigger
+ON kan_liikennetapahtuma;
+
+CREATE TRIGGER toimenpide_kohteen_mukaan_trigger
+BEFORE INSERT OR UPDATE ON kan_liikennetapahtuma
+FOR EACH ROW
+EXECUTE PROCEDURE toimenpide_kohteen_mukaan_proc();
+
+CREATE TYPE LIIKENNETAPAHTUMA_SUUNTA AS ENUM ('ylös', 'alas');
+CREATE TYPE LIIKENNETAPAHTUMA_ALUSLAJI AS ENUM ('RAH', 'MAT', 'ÖLJ', 'HINT', 'HUV', 'PRO', 'SEK', 'LAU');
+
+CREATE TABLE kan_liikennetapahtuma_alus (
+  id                     SERIAL PRIMARY KEY,
+  "liikennetapahtuma-id" INTEGER REFERENCES kan_liikennetapahtuma (id),
+  nimi                   TEXT,
+  laji                   LIIKENNETAPAHTUMA_ALUSLAJI         NOT NULL,
+  lkm                    INTEGER                            NOT NULL
+    CONSTRAINT vain_huviveneita_enemman_kuin_yksi CHECK (lkm = 1 OR laji = 'HUV'),
+  matkustajalkm          INTEGER,
+  suunta                 LIIKENNETAPAHTUMA_SUUNTA           NOT NULL,
+
+  luotu                  TIMESTAMP DEFAULT NOW(),
+  luoja                  INTEGER REFERENCES kayttaja (id)   NOT NULL,
+  muokattu               TIMESTAMP,
+  muokkaaja              INTEGER REFERENCES kayttaja (id),
+  poistettu              BOOLEAN   DEFAULT FALSE,
+  poistaja               INTEGER REFERENCES kayttaja (id)
+);
+
+CREATE TABLE kan_liikennetapahtuma_nippu (
+  id                     SERIAL PRIMARY KEY,
+  "liikennetapahtuma-id" INTEGER REFERENCES kan_liikennetapahtuma (id),
+  lkm                    INTEGER                            NOT NULL,
+  suunta                 LIIKENNETAPAHTUMA_SUUNTA           NOT NULL,
+
+  luotu                  TIMESTAMP DEFAULT NOW(),
+  luoja                  INTEGER REFERENCES kayttaja (id)   NOT NULL,
+  muokattu               TIMESTAMP,
+  muokkaaja              INTEGER REFERENCES kayttaja (id),
+  poistettu              BOOLEAN   DEFAULT FALSE,
+  poistaja               INTEGER REFERENCES kayttaja (id)
+);
---
+++ tietokanta/testidata/kanavat/liikennetapahtumat.sql
@@ -0,0 +1,20 @@
+INSERT INTO kan_liikennetapahtuma ("kohde-id", aika, toimenpide, palvelumuoto, "palvelumuoto-lkm", lisatieto, "vesipinta-ylaraja", "vesipinta-alaraja", kuittaaja, luoja, poistettu)
+VALUES
+  ((SELECT id FROM kan_kohde WHERE nimi = 'Tikkalansaaren sulku'),NOW(), 'sulutus', 'kauko', 1, 'Testidata 1', 7000, 8000, (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), false),
+  ((SELECT id FROM kan_kohde WHERE nimi = 'Tikkalansaaren avattava ratasilta'),NOW(), 'sillan-avaus', NULL, NULL,  'Testidata 2', 7000, 8000, (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), false),
+  ((SELECT id FROM kan_kohde WHERE nimi = 'Tikkalansaaren sulku'),NOW(), 'sulutus', 'paikallis', 1, 'Testidata 3', 7000, 8000, (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), false),
+  ((SELECT id FROM kan_kohde WHERE nimi = 'Tikkalansaaren sulku'),NOW(), 'tyhjennys', 'kauko', 1, 'Testidata 4', 7000, 8000, (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), TRUE),
+  ((SELECT id FROM kan_kohde WHERE nimi = 'Tikkalansaaren sulku'),NOW(), 'sulutus', 'itse', 15, 'Testidata 5', 7000, 8000, (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), false);
+
+INSERT INTO kan_liikennetapahtuma_alus ("liikennetapahtuma-id", nimi, laji, lkm, matkustajalkm, suunta, luoja)
+    VALUES
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1'), 'Rohmu', 'RAH', 1, NULL, 'ylös', (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1'), 'Rölli', 'ÖLJ', 1, NULL, 'ylös', (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2'), '', 'HUV', 4, 26, 'alas', (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3'), '', 'SEK', 1, NULL, 'alas', (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3'), 'Ronsu', 'LAU', 1, NULL, 'alas', (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 4'), '', 'HINT', 1, NULL, 'ylös', (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 5' ));
+
+INSERT INTO kan_liikennetapahtuma_nippu ("liikennetapahtuma-id", lkm, suunta, luoja)
+    VALUES
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2'), 8, 'ylös', (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 4' ));
