daf16dfd5d48030dc4707ea5de15055a7fad84a8
Erno Kuusela <erno-laptop!erno>
2017-11-07 13:28:11 +0200

    paranna vaylanro-migraatiota

--- tietokanta/src/main/resources/db/migration/V1_639__.sql
+++ tietokanta/src/main/resources/db/migration/V1_639__.sql
@@ -1,4 +1,8 @@
+ALTER TABLE reimari_toimenpide DISABLE TRIGGER USER; -- nopeushack vaylanro:n päivityksen ajaksi
 ALTER TABLE reimari_toimenpide ADD COLUMN vaylanro text;
 UPDATE reimari_toimenpide SET vaylanro = ("reimari-vayla").nro;
+ALTER TABLE reimari_toimenpide ENABLE TRIGGER USER;
 ALTER TABLE reimari_toimenpide DROP COLUMN "vayla-id";
+DELETE FROM vv_vayla a USING vv_vayla b WHERE a.id < b.id AND a.vaylanro = b.vaylanro;
 ALTER TABLE vv_vayla DROP COLUMN id;
+CREATE UNIQUE INDEX uniikki_vaylan_vaylanro on vv_vayla (vaylanro);
