467af43ec8772d48263a6432c4a4998fc7f828f9
mikkoro <mikko.ronkkomaki@gmail.com>
2017-11-07 12:27:09 +0200

    Lisää testidataa

--- tietokanta/testidata/kanavat/kanavat_ja_kohteet.sql
+++ tietokanta/testidata/kanavat/kanavat_ja_kohteet.sql
@@ -109,3 +109,113 @@ VALUES ('kokonaishintainen' :: KAN_TOIMENPIDETYYPPI,
          WHERE kayttajanimi = 'jvh'),
         NULL,
         NULL);
+
+INSERT INTO kan_toimenpide
+(tyyppi,
+ urakka,
+ sopimus,
+ pvm,
+ kohde,
+ huoltokohde,
+ toimenpidekoodi,
+ lisatieto,
+ suorittaja,
+ kuittaaja,
+ luotu,
+ luoja,
+ muokattu,
+ muokkaaja,
+ poistettu,
+ poistaja)
+VALUES ('kokonaishintainen' :: KAN_TOIMENPIDETYYPPI,
+  (SELECT id
+   FROM urakka
+   WHERE nimi = 'Saimaan kanava'),
+  (SELECT id
+   FROM sopimus
+   WHERE nimi = 'Saimaan huollon pääsopimus'),
+  '2016-11-07',
+  (SELECT id
+   FROM kan_kohde
+   WHERE nimi = 'Taipaleen sulku'),
+  (SELECT id
+   FROM kan_huoltokohde
+   WHERE nimi = 'ASENNONMITTAUSLAITTEET'),
+  (SELECT id
+   FROM toimenpidekoodi
+   WHERE emo = (SELECT id
+                FROM toimenpidekoodi
+                WHERE koodi = '24104') AND
+         nimi = 'Ei yksilöity'),
+  'Testitoimenpide',
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  '2016-11-07',
+        (SELECT id
+         FROM kayttaja
+         WHERE kayttajanimi = 'jvh'),
+        '2016-11-07',
+        (SELECT id
+         FROM kayttaja
+         WHERE kayttajanimi = 'jvh'),
+        NULL,
+        NULL);
+
+INSERT INTO kan_toimenpide
+(tyyppi,
+ urakka,
+ sopimus,
+ pvm,
+ kohde,
+ huoltokohde,
+ toimenpidekoodi,
+ lisatieto,
+ suorittaja,
+ kuittaaja,
+ luotu,
+ luoja,
+ muokattu,
+ muokkaaja,
+ poistettu,
+ poistaja)
+VALUES ('kokonaishintainen' :: KAN_TOIMENPIDETYYPPI,
+  (SELECT id
+   FROM urakka
+   WHERE nimi = 'Saimaan kanava'),
+  (SELECT id
+   FROM sopimus
+   WHERE nimi = 'Saimaan huollon lisäsopimus'),
+  '2017-01-07',
+  (SELECT id
+   FROM kan_kohde
+   WHERE nimi = 'Tikkalansaaren avattava ratasilta'),
+  (SELECT id
+   FROM kan_huoltokohde
+   WHERE nimi = 'ASENNONMITTAUSLAITTEET'),
+  (SELECT id
+   FROM toimenpidekoodi
+   WHERE emo = (SELECT id
+                FROM toimenpidekoodi
+                WHERE koodi = '24104') AND
+         nimi = 'Ei yksilöity'),
+  'Testitoimenpide',
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  '2017-01-07',
+        (SELECT id
+         FROM kayttaja
+         WHERE kayttajanimi = 'jvh'),
+        '2017-01-07',
+        (SELECT id
+         FROM kayttaja
+         WHERE kayttajanimi = 'jvh'),
+        NULL,
+        NULL)
