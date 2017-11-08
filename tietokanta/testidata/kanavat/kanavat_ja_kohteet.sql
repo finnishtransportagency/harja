760dc57aec99c25e7eb39daf01de92765228264f
Jari Hanhela <jarihanhela@gmail.com>
2017-11-08 14:07:54 +0200

    Lisää testidataa muutos- ja lisätöille

--- tietokanta/testidata/kanavat/kanavat_ja_kohteet.sql
+++ tietokanta/testidata/kanavat/kanavat_ja_kohteet.sql
@@ -107,7 +107,7 @@ VALUES ('kokonaishintainen' :: KAN_TOIMENPIDETYYPPI,
         (SELECT id
          FROM kayttaja
          WHERE kayttajanimi = 'jvh'),
-        NULL,
+        FALSE,
         NULL);
 
 INSERT INTO kan_toimenpide
@@ -162,7 +162,7 @@ VALUES ('kokonaishintainen' :: KAN_TOIMENPIDETYYPPI,
         (SELECT id
          FROM kayttaja
          WHERE kayttajanimi = 'jvh'),
-        NULL,
+        FALSE,
         NULL);
 
 INSERT INTO kan_toimenpide
@@ -217,5 +217,170 @@ VALUES ('kokonaishintainen' :: KAN_TOIMENPIDETYYPPI,
         (SELECT id
          FROM kayttaja
          WHERE kayttajanimi = 'jvh'),
-        NULL,
-        NULL)
+        FALSE,
+        NULL);
+
+INSERT INTO kan_toimenpide
+(tyyppi,
+ urakka,
+ sopimus,
+ pvm,
+ kohde,
+ huoltokohde,
+ toimenpidekoodi,
+ lisatieto,
+ suorittaja,
+ kuittaaja,
+ luotu,
+ luoja,
+ muokattu,
+ muokkaaja,
+ poistettu,
+ poistaja)
+VALUES ('muutos-lisatyo' :: KAN_TOIMENPIDETYYPPI,
+  (SELECT id
+   FROM urakka
+   WHERE nimi = 'Saimaan kanava'),
+  (SELECT id
+   FROM sopimus
+   WHERE nimi = 'Saimaan huollon lisäsopimus'),
+  '2017-01-07',
+  (SELECT id
+   FROM kan_kohde
+   WHERE nimi = 'Tikkalansaaren avattava ratasilta'),
+  (SELECT id
+   FROM kan_huoltokohde
+   WHERE nimi = 'ASENNONMITTAUSLAITTEET'),
+  (SELECT id
+   FROM toimenpidekoodi
+   WHERE emo = (SELECT id
+                FROM toimenpidekoodi
+                WHERE koodi = '24104') AND
+         nimi = 'Ei yksilöity'),
+  'Testitoimenpide',
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  '2017-01-07',
+        (SELECT id
+         FROM kayttaja
+         WHERE kayttajanimi = 'jvh'),
+        '2017-01-07',
+        (SELECT id
+         FROM kayttaja
+         WHERE kayttajanimi = 'jvh'),
+        FALSE,
+        NULL);
+
+INSERT INTO kan_toimenpide
+(tyyppi,
+ urakka,
+ sopimus,
+ pvm,
+ kohde,
+ huoltokohde,
+ toimenpidekoodi,
+ lisatieto,
+ suorittaja,
+ kuittaaja,
+ luotu,
+ luoja,
+ muokattu,
+ muokkaaja,
+ poistettu,
+ poistaja)
+VALUES ('muutos-lisatyo' :: KAN_TOIMENPIDETYYPPI,
+  (SELECT id
+   FROM urakka
+   WHERE nimi = 'Saimaan kanava'),
+  (SELECT id
+   FROM sopimus
+   WHERE nimi = 'Saimaan huollon pääsopimus'),
+  '2017-01-11',
+  (SELECT id
+   FROM kan_kohde
+   WHERE nimi = 'Tikkalansaaren avattava ratasilta'),
+  (SELECT id
+   FROM kan_huoltokohde
+   WHERE nimi = 'ASENNONMITTAUSLAITTEET'),
+  (SELECT id
+   FROM toimenpidekoodi
+   WHERE emo = (SELECT id
+                FROM toimenpidekoodi
+                WHERE koodi = '24104') AND
+         nimi = 'Ei yksilöity'),
+  'Testitoimenpide',
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  '2017-01-07',
+        (SELECT id
+         FROM kayttaja
+         WHERE kayttajanimi = 'jvh'),
+        '2017-01-07',
+        (SELECT id
+         FROM kayttaja
+         WHERE kayttajanimi = 'jvh'),
+        FALSE,
+        NULL);
+
+INSERT INTO kan_toimenpide
+(tyyppi,
+ urakka,
+ sopimus,
+ pvm,
+ kohde,
+ huoltokohde,
+ toimenpidekoodi,
+ lisatieto,
+ suorittaja,
+ kuittaaja,
+ luotu,
+ luoja,
+ muokattu,
+ muokkaaja,
+ poistettu,
+ poistaja)
+VALUES ('muutos-lisatyo' :: KAN_TOIMENPIDETYYPPI,
+  (SELECT id
+   FROM urakka
+   WHERE nimi = 'Saimaan kanava'),
+  (SELECT id
+   FROM sopimus
+   WHERE nimi = 'Saimaan huollon pääsopimus'),
+  '2017-01-12',
+  (SELECT id
+   FROM kan_kohde
+   WHERE nimi = 'Tikkalansaaren avattava ratasilta'),
+  (SELECT id
+   FROM kan_huoltokohde
+   WHERE nimi = 'ASENNONMITTAUSLAITTEET'),
+  (SELECT id
+   FROM toimenpidekoodi
+   WHERE emo = (SELECT id
+                FROM toimenpidekoodi
+                WHERE koodi = '24104') AND
+         nimi = 'Ei yksilöity'),
+  'Testitoimenpide',
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  (SELECT id
+   FROM kayttaja
+   WHERE kayttajanimi = 'jvh'),
+  '2017-01-07',
+        (SELECT id
+         FROM kayttaja
+         WHERE kayttajanimi = 'jvh'),
+        '2017-01-07',
+        (SELECT id
+         FROM kayttaja
+         WHERE kayttajanimi = 'jvh'),
+        FALSE,
+        NULL);
