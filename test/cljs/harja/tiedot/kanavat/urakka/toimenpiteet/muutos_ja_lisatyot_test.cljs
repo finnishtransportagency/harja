49b91cdf77287344355d5063005fbb90af0d420c
Jari Hanhela <jarihanhela@gmail.com>
2017-11-08 14:16:08 +0200

    Lisää fronttitesti

--- test/cljs/harja/tiedot/kanavat/urakka/toimenpiteet/muutos_ja_lisatyot_test.cljs
+++ test/cljs/harja/tiedot/kanavat/urakka/toimenpiteet/muutos_ja_lisatyot_test.cljs
@@ -6,3 +6,26 @@
 (deftest nakymaan-tuleminen
   (is (true? (:nakymassa? (e! (tiedot/->Nakymassa? true)))))
   (is (false? (:nakymassa? (e! (tiedot/->Nakymassa? false))))))
+
+(deftest PaivitaValinnat
+  (vaadi-async-kutsut
+    #{tiedot/->HaeToimenpiteet}
+    (is (= {:valinnat {:foo :bar}}
+           (e! (tiedot/->PaivitaValinnat {:foo :bar}))))))
+
+(deftest HaeToimenpiteet
+  (vaadi-async-kutsut
+    #{tiedot/->ToimenpiteetHaettu tiedot/->ToimenpiteetEiHaettu}
+    (is (= {:toimenpiteiden-haku-kaynnissa? true}
+           (e! (tiedot/->HaeToimenpiteet {:sopimus-id 666
+                                          :toimenpide {:id 666}}))))))
+
+(deftest ToimenpiteetHaettu
+  (is (= {:toimenpiteiden-haku-kaynnissa? false
+          :toimenpiteet [{:id 1}]}
+         (e! (tiedot/->ToimenpiteetHaettu [{:id 1}])))))
+
+(deftest ToimenpiteetEiHaettu
+  (is (= {:toimenpiteiden-haku-kaynnissa? false
+          :toimenpiteet []}
+         (e! (tiedot/->ToimenpiteetEiHaettu)))))
