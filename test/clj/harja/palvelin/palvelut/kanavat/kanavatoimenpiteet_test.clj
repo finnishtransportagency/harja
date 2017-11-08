e9366c58986bc67bd3a75fea55017ce3973450f0
Jari Hanhela <jarihanhela@gmail.com>
2017-11-08 10:38:50 +0200

    Muuita kanavatoimenpiteiden testin toteutusta: ei verrata enää hardkoodattuun vastaukseen, koska id:t saattavat muuttua.
    Tehdään sen sijaan nopea check avaimista.

--- test/clj/harja/palvelin/palvelut/kanavat/kanavatoimenpiteet_test.clj
+++ test/clj/harja/palvelin/palvelut/kanavat/kanavatoimenpiteet_test.clj
@@ -44,35 +44,12 @@
         vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
                                 :hae-kanavatoimenpiteet
                                 +kayttaja-jvh+
-                                parametrit)
-        odotettu {:harja.domain.kanavat.kanavan-toimenpide/kohde
-                  {:harja.domain.kanavat.kanavan-kohde/id 3
-                   :harja.domain.kanavat.kanavan-kohde/nimi "Tikkalansaaren avattava ratasilta"
-                   :harja.domain.kanavat.kanavan-kohde/tyyppi :silta}
-                  :harja.domain.kanavat.kanavan-toimenpide/kuittaaja
-                  {:harja.domain.kayttaja/kayttajanimi "jvh"
-                   :harja.domain.kayttaja/etunimi "Jalmari"
-                   :harja.domain.kayttaja/sukunimi "Järjestelmävastuuhenkilö"
-                   :harja.domain.kayttaja/puhelin "040123456789"
-                   :harja.domain.kayttaja/sahkoposti "jalmari@example.com"
-                   :harja.domain.kayttaja/id 2}
-                  :harja.domain.kanavat.kanavan-toimenpide/suorittaja
-                  {:harja.domain.kayttaja/etunimi "Jalmari"
-                   :harja.domain.kayttaja/id 2
-                   :harja.domain.kayttaja/kayttajanimi "jvh"
-                   :harja.domain.kayttaja/sahkoposti "jalmari@example.com"
-                   :harja.domain.kayttaja/sukunimi "Järjestelmävastuuhenkilö"
-                   :harja.domain.kayttaja/puhelin "040123456789"}
-                  :harja.domain.kanavat.kanavan-toimenpide/lisatieto "Testitoimenpide"
-                  :harja.domain.kanavat.kanavan-toimenpide/toimenpidekoodi
-                  {:harja.domain.toimenpidekoodi/id 2997
-                   :harja.domain.toimenpidekoodi/nimi "Ei yksilöity"}
-                  :harja.domain.kanavat.kanavan-toimenpide/tyyppi :kokonaishintainen
-                  :harja.domain.kanavat.kanavan-toimenpide/huoltokohde
-                  {:harja.domain.kanavat.kanavan-huoltokohde/nimi "ASENNONMITTAUSLAITTEET"
-                   :harja.domain.kanavat.kanavan-huoltokohde/id 5}
-                  :harja.domain.kanavat.kanavan-toimenpide/pvm #inst "2017-10-09T21:00:00.000-00:00"
-                  :harja.domain.kanavat.kanavan-toimenpide/id 1}]
+                                parametrit)]
     (is (s/valid? ::kanavan-toimenpide/hae-kanavatoimenpiteet-kutsu parametrit) "Kutsu on validi")
     (is (s/valid? ::kanavan-toimenpide/hae-kanavatoimenpiteet-vastaus vastaus) "Vastaus on validi")
-    (is (= odotettu (first vastaus)) "Vastauksena saadaan oletettu data")))
+
+    (is (>= (count vastaus) 1))
+    (is (every? ::kanavan-toimenpide/id vastaus))
+    (is (every? ::kanavan-toimenpide/kohde vastaus))
+    (is (every? ::kanavan-toimenpide/toimenpidekoodi vastaus))
+    (is (every? ::kanavan-toimenpide/huoltokohde vastaus))))
