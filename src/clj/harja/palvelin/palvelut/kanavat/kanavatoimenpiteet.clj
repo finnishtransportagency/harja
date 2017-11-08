a6c0d4f5b5e73072709ce5d14a88a1e34f43b7b8
Jari Hanhela <jarihanhela@gmail.com>
2017-11-08 15:30:16 +0200

    Assertoi urakka-id

--- src/clj/harja/palvelin/palvelut/kanavat/kanavatoimenpiteet.clj
+++ src/clj/harja/palvelin/palvelut/kanavat/kanavatoimenpiteet.clj
@@ -10,23 +10,24 @@
             [harja.domain.kanavat.kanavan-toimenpide :as kanavan-toimenpide]
             [harja.kyselyt.kanavat.kanavan-toimenpide :as q-kanavan-toimenpide]))
 
-(defn hae-kanavatoimenpiteet [db user {urakka ::kanavan-toimenpide/urakka-id
-                                       sopimus ::kanavan-toimenpide/sopimus-id
+(defn hae-kanavatoimenpiteet [db user {urakka-id ::kanavan-toimenpide/urakka-id
+                                       sopimus-id ::kanavan-toimenpide/sopimus-id
                                        alkupvm :alkupvm
                                        loppupvm :loppupvm
                                        toimenpidekoodi ::toimenpidekoodi/id
                                        tyyppi ::kanavan-toimenpide/kanava-toimenpidetyyppi
                                        :as hakuehdot}]
 
+  (assert urakka-id "Urakka-id puuttuu!")
   (case tyyppi
-    :kokonaishintainen (oikeudet/vaadi-lukuoikeus oikeudet/urakat-kanavat-kokonaishintaiset user urakka)
-    :muutos-lisatyo (oikeudet/vaadi-lukuoikeus oikeudet/urakat-kanavat-lisatyot user urakka))
+    :kokonaishintainen (oikeudet/vaadi-lukuoikeus oikeudet/urakat-kanavat-kokonaishintaiset user urakka-id)
+    :muutos-lisatyo (oikeudet/vaadi-lukuoikeus oikeudet/urakat-kanavat-lisatyot user urakka-id))
 
   (let [tyyppi (when tyyppi (name tyyppi))]
     (q-kanavan-toimenpide/hae-sopimuksen-toimenpiteet-aikavalilta
       db
-      {:urakka urakka
-       :sopimus sopimus
+      {:urakka urakka-id
+       :sopimus sopimus-id
        :alkupvm alkupvm
        :loppupvm loppupvm
        :toimenpidekoodi toimenpidekoodi
