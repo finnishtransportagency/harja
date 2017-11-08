6d4266d75ebd5831d472a3d0909b872a04de759f
Jari Hanhela <jarihanhela@gmail.com>
2017-11-08 11:34:32 +0200

    Poista hardkoodattu testidata: ongelmallinen, koska id:t voivat muuttua

--- test/clj/harja/kyselyt/kanavat/kanavan_toimenpide_test.clj
+++ test/clj/harja/kyselyt/kanavat/kanavan_toimenpide_test.clj
@@ -1,45 +1,25 @@
 (ns harja.kyselyt.kanavat.kanavan-toimenpide-test
   (:require [clojure.test :refer :all]
             [harja.testi :refer :all]
-            [harja.kyselyt.kanavat.kanavan-toimenpide :as kanavan-toimenpide]
-            [harja.palvelin.komponentit.tietokanta :as tietokanta]))
+            [harja.kyselyt.kanavat.kanavan-toimenpide :as kanava-q]
+            [harja.domain.kanavat.kanavan-toimenpide :as kanavan-toimenpide]
+            [harja.palvelin.komponentit.tietokanta :as tietokanta]
+            [taoensso.timbre :as log]))
 
 (deftest kanavan-toimenpiteet
   (let [db (tietokanta/luo-tietokanta testitietokanta)
-        odotettu {:harja.domain.kanavat.kanavan-toimenpide/kohde
-                  {:harja.domain.kanavat.kanavan-kohde/id 3
-                   :harja.domain.kanavat.kanavan-kohde/nimi "Tikkalansaaren avattava ratasilta"
-                   :harja.domain.kanavat.kanavan-kohde/tyyppi :silta}
-                  :harja.domain.kanavat.kanavan-toimenpide/kuittaaja
-                  {:harja.domain.kayttaja/kayttajanimi "jvh"
-                   :harja.domain.kayttaja/etunimi "Jalmari"
-                   :harja.domain.kayttaja/sukunimi "Järjestelmävastuuhenkilö"
-                   :harja.domain.kayttaja/puhelin "040123456789"
-                   :harja.domain.kayttaja/sahkoposti "jalmari@example.com"
-                   :harja.domain.kayttaja/id 2}
-                  :harja.domain.kanavat.kanavan-toimenpide/suorittaja
-                  {:harja.domain.kayttaja/etunimi "Jalmari"
-                   :harja.domain.kayttaja/id 2
-                   :harja.domain.kayttaja/kayttajanimi "jvh"
-                   :harja.domain.kayttaja/sahkoposti "jalmari@example.com"
-                   :harja.domain.kayttaja/sukunimi "Järjestelmävastuuhenkilö"
-                   :harja.domain.kayttaja/puhelin "040123456789"}
-                  :harja.domain.kanavat.kanavan-toimenpide/lisatieto "Testitoimenpide"
-                  :harja.domain.kanavat.kanavan-toimenpide/toimenpidekoodi
-                  {:harja.domain.toimenpidekoodi/id 2997
-                   :harja.domain.toimenpidekoodi/nimi "Ei yksilöity"}
-                  :harja.domain.kanavat.kanavan-toimenpide/tyyppi :kokonaishintainen
-                  :harja.domain.kanavat.kanavan-toimenpide/huoltokohde
-                  {:harja.domain.kanavat.kanavan-huoltokohde/nimi "ASENNONMITTAUSLAITTEET"
-                   :harja.domain.kanavat.kanavan-huoltokohde/id 5}
-                  :harja.domain.kanavat.kanavan-toimenpide/pvm #inst "2017-10-09T21:00:00.000-00:00"
-                  :harja.domain.kanavat.kanavan-toimenpide/id 1}]
-    (is (= (first (kanavan-toimenpide/hae-sopimuksen-toimenpiteet-aikavalilta
-                    db
-                    {:urakka (hae-saimaan-kanavaurakan-id)
-                     :sopimus (hae-saimaan-kanavaurakan-paasopimuksen-id)
-                     :alkupvm (harja.pvm/luo-pvm 2016 1 1)
-                     :loppupvm (harja.pvm/luo-pvm 2018 1 1)
-                     :toimenpidekoodi 597
-                     :tyyppi "kokonaishintainen"}))
-           odotettu))))
+        vastaus (kanava-q/hae-sopimuksen-toimenpiteet-aikavalilta
+                  db
+                  {:urakka (hae-saimaan-kanavaurakan-id)
+                   :sopimus (hae-saimaan-kanavaurakan-paasopimuksen-id)
+                   :alkupvm (harja.pvm/luo-pvm 2016 1 1)
+                   :loppupvm (harja.pvm/luo-pvm 2018 1 1)
+                   :toimenpidekoodi 597
+                   :tyyppi "kokonaishintainen"})]
+
+    (log/debug vastaus)
+
+    (is (every? ::kanavan-toimenpide/id vastaus))
+    (is (every? ::kanavan-toimenpide/kohde vastaus))
+    (is (every? ::kanavan-toimenpide/toimenpidekoodi vastaus))
+    (is (every? ::kanavan-toimenpide/huoltokohde vastaus))))
