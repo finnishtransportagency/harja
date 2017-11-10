f8dd2d92b245fcb032fb56a7fe1016d86181cfe8
Erno Kuusela <erno-laptop!erno>
2017-11-10 10:03:21 +0200

    testidata kan_hinta-tauluun

---
+++ tietokanta/testidata/kanavat/hinnat.sql
@@ -0,0 +1,6 @@
+insert into kan_hinta (otsikko, yksikko, yksikkohinta, ryhma, maara, luoja) values ('Henkilöstö: työnjohto', 'h', 55, 'yksikkohinnoiteltu-tyo', 3, 42);
+insert into kan_hinta (otsikko, yksikko, yksikkohinta, ryhma, maara, luoja) values ('Ylimääräinen työ', 'h', 35, 'muu-tyo', 3, 42);
+insert into kan_hinta (otsikko, yksikko, yksikkohinta, yleiskustannuslisa, ryhma, maara, luoja) values ('SAFF SUN 10-5', 'kpl', 100, 14, 'varaosat-ja-materiaalit', 1, 42);
+insert into kan_hinta (summa, ryhma, yleiskustannuslisa, luoja) values (101, 'yleiset-materiaalit', 8, 42);
+insert into kan_hinta (summa, ryhma, luoja) values (44.25, 'matkakulut', 42);
+insert into kan_hinta (otsikko, summa, ryhma, yleiskustannuslisa, luoja) values ('Muita sekalaisia hankintoja', 44.25, 'muut-kulut', 3.1, 42);
