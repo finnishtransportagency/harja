b64a901a6be69f17227c55606eb0d875812ee6cb
Erno Kuusela <erno-laptop!erno>
2017-11-10 14:06:11 +0200

    tee linkkitaulu kanavien toimenpiteiden hinnoille

---
+++ tietokanta/src/main/resources/db/migration/V1_645__.sql
@@ -0,0 +1,9 @@
+CREATE TABLE kan_toimenpide_hinta (
+  toimenpide INTEGER
+             NOT NULL
+             REFERENCES kan_toimenpide(id),
+  hinta      INTEGER
+             NOT NULL
+             REFERENCES kan_hinta(id),
+  CONSTRAINT uniikki_kan_toimenpiteen_hinta UNIQUE (toimenpide,  hinta)) ;
+q
