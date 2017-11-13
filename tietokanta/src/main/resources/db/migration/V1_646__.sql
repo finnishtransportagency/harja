a83cfee26edfa371513f86fac25204f737357f0d
Erno Kuusela <erno-laptop!erno>
2017-11-13 12:13:00 +0200

    Lisää kanaville työ mukaillen vesiväyläpuolta (pl. hinnoittelu)

--- tietokanta/src/main/resources/db/migration/V1_646__.sql
+++ tietokanta/src/main/resources/db/migration/V1_646__.sql
@@ -6,3 +6,16 @@ CREATE TABLE kan_toimenpide_hinta (
              NOT NULL
              REFERENCES kan_hinta(id),
   CONSTRAINT uniikki_kan_toimenpiteen_hinta UNIQUE (toimenpide,  hinta));
+
+CREATE TABLE kan_tyo (
+  id         SERIAL PRIMARY KEY,
+  "toimenpidekoodi-id" integer NOT NULL,
+  maara      NUMERIC NOT NULL,
+  muokkaaja  INTEGER,
+  muokattu   TIMESTAMP WITHOUT TIME ZONE,
+  luoja      INTEGER NOT NULL,
+  luotu      TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+  poistettu  BOOLEAN DEFAULT FALSE NOT NULL,
+  poistaja   INTEGER,
+  CONSTRAINT maara_positiivinen CHECK ((maara >= (0)::NUMERIC))
+);
