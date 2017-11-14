63674cad8df64dbbd9088b64f3597577b3b72ef2
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-11-14 15:32:16 +0200

    Lisää liikennetapahtuma yhdistetylle kohteelle

--- tietokanta/testidata/kanavat/liikennetapahtumat.sql
+++ tietokanta/testidata/kanavat/liikennetapahtumat.sql
@@ -15,13 +15,19 @@ INSERT INTO kan_liikennetapahtuma("urakka-id", "sopimus-id", "kohde-id", aika, "
       ((SELECT id FROM urakka WHERE nimi = 'Saimaan kanava'), (SELECT id FROM sopimus WHERE nimi = 'Saimaan huollon pääsopimus'),
                                                               (SELECT id FROM kan_kohde WHERE nimi = 'Tikkalansaaren avattava ratasilta'),NOW(), TRUE, 'kauko', 1,  'Testidata 2', 7020, 8900, (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), false);
 
-
-INSERT INTO kan_liikennetapahtuma_alus ("liikennetapahtuma-id", nimi, laji, lkm, matkustajalkm, suunta, nippulkm, luoja)
+-- Lisätään yhdistettyjä tapahtumia
+INSERT INTO kan_liikennetapahtuma ("urakka-id", "sopimus-id", "kohde-id", aika, "sulku-toimenpide", "sulku-palvelumuoto", "sulku-lkm", "silta-avaus", "silta-palvelumuoto", "silta-lkm", lisatieto, "vesipinta-ylaraja", "vesipinta-alaraja", "kuittaaja-id", luoja, poistettu)
     VALUES
-      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1'), 'Rohmu', 'RAH', 1, NULL, 'ylos', NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1' )),
-      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1'), 'Rölli', 'ÖLJ', 1, NULL, 'ylos', NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1' )),
-      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2'), '', 'HUV', 4, 26, 'alas', NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2' )),
-      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2'), '', 'HUV', 1, 4, 'ylos', NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2' )),
-      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3'), '', 'SEK', 1, NULL, 'alas', NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3' )),
-      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3'), 'Ronsu', 'LAU', 1, NULL, 'alas', NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3' )),
-      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 4'), '', 'HIN', 1, NULL, 'ylos', 280, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 5' ));
+      ((SELECT id FROM urakka WHERE nimi = 'Saimaan kanava'), (SELECT id FROM sopimus WHERE nimi = 'Saimaan huollon pääsopimus'),
+                                                              (SELECT id FROM kan_kohde WHERE nimi = 'Taipaleen sulku ja silta'),NOW() - INTERVAL '5 minutes', 'sulutus', 'kauko', 1, TRUE, 'kauko', 1, 'Testidata 6', 7010, 8103, (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), false);
+
+INSERT INTO kan_liikennetapahtuma_alus ("liikennetapahtuma-id", nimi, laji, lkm, matkustajalkm, suunta, nippulkm, lisatieto, luoja)
+    VALUES
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1'), 'Rohmu', 'RAH', 1, NULL, 'ylos', NULL, NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1'), 'Rölli', 'ÖLJ', 1, NULL, 'ylos', NULL, NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 1' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2'), '', 'HUV', 4, 26, 'alas', NULL, NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2'), '', 'HUV', 1, 4, 'ylos', NULL, NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 2' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3'), '', 'SEK', 1, NULL, 'alas', NULL, NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3'), 'Ronsu', 'LAU', 1, NULL, 'alas', NULL, NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 3' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 4'), '', 'HIN', 1, NULL, 'ylos', 280, NULL, (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 4' )),
+      ((SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 6'), '', 'ÖLJ', 1, NULL, 'alas', NULL, 'Liian iso laiva', (SELECT luoja FROM kan_liikennetapahtuma WHERE lisatieto = 'Testidata 6' ));
