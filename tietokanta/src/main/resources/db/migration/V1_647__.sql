810a5486a3f40835edd814131bace5c7ed6a9973
maaritlaine <maarit.laine@gmail.com>
2017-11-14 13:48:17 +0200

    Alusta integraation reimari_meta-tieto.

--- tietokanta/src/main/resources/db/migration/V1_647__.sql
+++ tietokanta/src/main/resources/db/migration/V1_647__.sql
@@ -5,4 +5,11 @@ CREATE TABLE reimari_turvalaiteryhma (
   turvalaitteet INTEGER []
 );
 
-INSERT INTO integraatio (jarjestelma, nimi) VALUES ('reimari', 'turvalaiteryhmat-haku');
+INSERT INTO integraatio (jarjestelma, nimi) VALUES ('reimari', 'hae-turvalaiteryhmat');
+
+INSERT INTO reimari_meta (integraatio, enimmaishakuvali, aikakursori)
+VALUES (
+  (SELECT id FROM integraatio WHERE jarjestelma = 'reimari' AND nimi = 'hae-turvalaiteryhmat'),
+  '10 years',
+  '2007-01-01T12:12:12Z');
+
