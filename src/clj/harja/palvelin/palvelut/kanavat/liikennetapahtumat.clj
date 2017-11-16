f0b8ecdc44276cc64e6945aa064566f87146d4c3
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-11-16 09:56:45 +0200

    Lisää palvelu tapahtuman tallentamiselle

--- src/clj/harja/palvelin/palvelut/kanavat/liikennetapahtumat.clj
+++ src/clj/harja/palvelin/palvelut/kanavat/liikennetapahtumat.clj
@@ -10,8 +10,14 @@
             [harja.domain.kanavat.liikennetapahtuma :as lt]
             [harja.domain.urakka :as ur]))
 
+(defn tallenna-liikennetapahtuma [db user tiedot]
+  (assert (::lt/urakka-id tiedot) "Urakka id puuttuu, ei voi tallentaa liikennetapahtumaa!")
+  (oikeudet/vaadi-kirjoitusoikeus oikeudet/urakat-kanavat-liikenne user (::lt/urakka-id tiedot))
+  (q/tallenna-liikennetapahtuma db user (dissoc tiedot :hakuparametrit))
+  (q/hae-liikennetapahtumat db (:hakuparametrit tiedot)))
 
 (defn hae-liikennetapahtumat [db user tiedot]
+  (assert (::ur/id tiedot) "Urakka id puuttuu, ei voida hakea liikennetapahtumia!")
   (oikeudet/vaadi-lukuoikeus oikeudet/urakat-kanavat-liikenne user (::ur/id tiedot))
   (q/hae-liikennetapahtumat db tiedot))
 
@@ -26,11 +32,19 @@
         (hae-liikennetapahtumat db user tiedot))
       {:kysely-spec ::lt/hae-liikennetapahtumat-kysely
        :vastaus-spec ::lt/hae-liikennetapahtumat-vastaus})
+    (julkaise-palvelu
+      http
+      :tallenna-liikennetapahtuma
+      (fn [user tiedot]
+        (tallenna-liikennetapahtuma db user tiedot))
+      {:kysely-spec ::lt/tallenna-liikennetapahtuma-kysely
+       :vastaus-spec ::lt/tallenna-liikennetapahtuma-vastaus})
     this)
 
   (stop [this]
     (poista-palvelut
       (:http-palvelin this)
-      :hae-liikennetapahtumat)
+      :hae-liikennetapahtumat
+      :tallenna-liikennetapahtuma)
     this))
 
