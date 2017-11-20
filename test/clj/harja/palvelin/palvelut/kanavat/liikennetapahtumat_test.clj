dd6fc287b36ca2aab661a54bb6d97768061e9fb6
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-11-20 18:30:26 +0200

    Lisää blackbox palvelintestit

--- test/clj/harja/palvelin/palvelut/kanavat/liikennetapahtumat_test.clj
+++ test/clj/harja/palvelin/palvelut/kanavat/liikennetapahtumat_test.clj
@@ -40,7 +40,7 @@
                       jarjestelma-fixture
                       urakkatieto-fixture))
 
-(deftest kanavien-haku
+(deftest tapahtumien-haku
   (let [urakka-id (hae-saimaan-kanavaurakan-id)
         sopimus-id (hae-saimaan-kanavaurakan-paasopimuksen-id)
         params {::ur/id urakka-id
@@ -59,3 +59,138 @@
               (fn [tapahtuma]
                 (not-empty (::lt/alukset tapahtuma)))
               vastaus))))))
+
+(deftest edellisten-haku
+  (let [urakka-id (hae-saimaan-kanavaurakan-id)
+        sopimus-id (hae-saimaan-kanavaurakan-paasopimuksen-id)
+        kohde-id (hae-kanavakohde-taipaleen-sulku)
+        params {::lt/urakka-id urakka-id
+                ::lt/sopimus-id sopimus-id
+                ::lt/kohde-id kohde-id}
+        vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                :hae-edelliset-tapahtumat
+                                +kayttaja-jvh+
+                                params)]
+
+    (is (s/valid? ::lt/hae-edelliset-tapahtumat-kysely params))
+    (is (s/valid? ::lt/hae-edelliset-tapahtumat-vastaus vastaus))
+
+    ;; Nämä muuttuu kun palvelu päivitetään palauttamaan
+    ;; edellisten kohteiden tietoja
+    (is (nil? (:ylos vastaus)))
+    (is (nil? (:alas vastaus)))
+
+    (is (and
+          (some? (:kohde vastaus))
+          (number? (get-in vastaus [:kohde ::lt/id]))))))
+
+(deftest tapahtuman-tallentaminen
+  (testing "Uuden luonti"
+    (let [urakka-id (hae-saimaan-kanavaurakan-id)
+          sopimus-id (hae-saimaan-kanavaurakan-paasopimuksen-id)
+          kohde-id (hae-kanavakohde-taipaleen-sulku)
+          hakuparametrit {::ur/id urakka-id
+                          ::sop/id sopimus-id}
+          vanhat (kutsu-palvelua (:http-palvelin jarjestelma)
+                                 :hae-liikennetapahtumat
+                                 +kayttaja-jvh+
+                                 hakuparametrit)
+          _ (is (not-empty vanhat))
+          params {::lt/urakka-id urakka-id
+                  ::lt/sopimus-id sopimus-id
+                  ::lt/kohde-id kohde-id
+                  ::lt/aika (pvm/nyt)
+                  ::lt/silta-avaus true
+                  ::lt/silta-palvelumuoto :kauko
+                  ::lt/silta-lkm 1
+                  ::lt/vesipinta-alaraja 500
+                  ::lt/vesipinta-ylaraja 1000
+                  ::lt/kuittaaja-id (:id +kayttaja-jvh+)
+                  ::lt/lisatieto "FOOBAR FOOBAR"
+                  :hakuparametrit hakuparametrit}
+          vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                  :tallenna-liikennetapahtuma
+                                  +kayttaja-jvh+
+                                  params)]
+
+      (is (s/valid? ::lt/tallenna-liikennetapahtuma-kysely params))
+      (is (s/valid? ::lt/tallenna-liikennetapahtuma-vastaus vastaus))
+
+      (is (= (count vastaus) (inc (count vanhat))))))
+
+  (testing "Muokkaaminen"
+    (let [urakka-id (hae-saimaan-kanavaurakan-id)
+          sopimus-id (hae-saimaan-kanavaurakan-paasopimuksen-id)
+          kohde-id (hae-kanavakohde-taipaleen-sulku)
+          tapahtuma-id (ffirst (q (str "SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'FOOBAR FOOBAR';")))
+          hakuparametrit {::ur/id urakka-id
+                          ::sop/id sopimus-id}
+          vanhat (kutsu-palvelua (:http-palvelin jarjestelma)
+                                 :hae-liikennetapahtumat
+                                 +kayttaja-jvh+
+                                 hakuparametrit)
+          _ (is (not-empty vanhat))
+          params {::lt/urakka-id urakka-id
+                  ::lt/sopimus-id sopimus-id
+                  ::lt/kohde-id kohde-id
+                  ::lt/id tapahtuma-id
+                  ::lt/aika (pvm/nyt)
+                  ::lt/silta-avaus true
+                  ::lt/silta-palvelumuoto :kauko
+                  ::lt/silta-lkm 1
+                  ::lt/vesipinta-alaraja 500
+                  ::lt/vesipinta-ylaraja 1000
+                  ::lt/kuittaaja-id (:id +kayttaja-jvh+)
+                  ::lt/lisatieto "FOOBAR FOOBAR FOOBAR"
+                  :hakuparametrit hakuparametrit}
+          vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                  :tallenna-liikennetapahtuma
+                                  +kayttaja-jvh+
+                                  params)]
+
+      (is (s/valid? ::lt/tallenna-liikennetapahtuma-kysely params))
+      (is (s/valid? ::lt/tallenna-liikennetapahtuma-vastaus vastaus))
+
+      (is (= (count vastaus) (count vanhat)))
+
+      (is (some #(= (::lt/lisatieto %) "FOOBAR FOOBAR") vanhat))
+      (is (some #(= (::lt/lisatieto %) "FOOBAR FOOBAR FOOBAR") vastaus))))
+
+  (testing "Poistaminen"
+    (let [urakka-id (hae-saimaan-kanavaurakan-id)
+          sopimus-id (hae-saimaan-kanavaurakan-paasopimuksen-id)
+          kohde-id (hae-kanavakohde-taipaleen-sulku)
+          tapahtuma-id (ffirst (q (str "SELECT id FROM kan_liikennetapahtuma WHERE lisatieto = 'FOOBAR FOOBAR FOOBAR';")))
+          hakuparametrit {::ur/id urakka-id
+                          ::sop/id sopimus-id}
+          vanhat (kutsu-palvelua (:http-palvelin jarjestelma)
+                                 :hae-liikennetapahtumat
+                                 +kayttaja-jvh+
+                                 hakuparametrit)
+          _ (is (not-empty vanhat))
+          params {::lt/urakka-id urakka-id
+                  ::lt/sopimus-id sopimus-id
+                  ::lt/kohde-id kohde-id
+                  ::lt/id tapahtuma-id
+                  ::lt/aika (pvm/nyt)
+                  ::lt/silta-avaus true
+                  ::lt/silta-palvelumuoto :kauko
+                  ::lt/silta-lkm 1
+                  ::lt/vesipinta-alaraja 500
+                  ::lt/vesipinta-ylaraja 1000
+                  ::lt/kuittaaja-id (:id +kayttaja-jvh+)
+                  ::lt/lisatieto "FOOBAR FOOBAR FOOBAR"
+                  ::m/poistettu? true
+                  :hakuparametrit hakuparametrit}
+          vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                  :tallenna-liikennetapahtuma
+                                  +kayttaja-jvh+
+                                  params)]
+
+      (is (s/valid? ::lt/tallenna-liikennetapahtuma-kysely params))
+      (is (s/valid? ::lt/tallenna-liikennetapahtuma-vastaus vastaus))
+
+      (is (= (count vastaus) (dec (count vanhat))))
+
+      (is (some #(= (::lt/lisatieto %) "FOOBAR FOOBAR FOOBAR") vanhat))
+      (is (empty? (filter #(= (::lt/lisatieto %) "FOOBAR FOOBAR FOOBAR") vastaus))))))
