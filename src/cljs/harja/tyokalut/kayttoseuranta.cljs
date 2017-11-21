f95a9c29ed8be58837ba06cfaf0ae1585d1d466d
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-11-21 12:35:08 +0200

    Mahdollista atomin seuraaminen polun perusteella

--- src/cljs/harja/tyokalut/kayttoseuranta.cljs
+++ src/cljs/harja/tyokalut/kayttoseuranta.cljs
@@ -1,9 +1,14 @@
 (ns harja.tyokalut.kayttoseuranta
+  "Mahdollistaa datan keräämisen käyttöliittymän käytöstä.
+
+  Lähettää palvelimelle timestampin, sivun, ja lisätiedon.
+  Katso myös harja.ui.komponentti/kirjaa-kaytto!"
   (:require [harja.asiakas.kommunikaatio :as k]
-            [cljs.core.async :refer [<!]])
+            [cljs.core.async :refer [<!]]
+            [reagent.core :as r])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn- laheta-kaytto! [sivu lisatieto tila]
+(defn laheta-kaytto! [sivu lisatieto tila]
   (go (let [vastaus (<! (k/post!
                           :lokita-kaytto
                           {:sivu sivu
@@ -11,6 +16,8 @@
                            :tila tila}))])))
 
 (defn seuraa-kayttoa!
+  "Rekisteröi atomin seurattavaksi, ja lähettää palvelimelle tiedon,
+  kun atomin arvo muuttuu. Lähetettävä tila on aina boolean."
   ([atomi sivu]
     (seuraa-kayttoa! atomi sivu nil))
   ([atomi sivu lisatieto]
@@ -21,3 +28,11 @@
                        uusi (boolean uusi)]
                    (when-not (= vanha uusi)
                      (laheta-kaytto! sivu lisatieto uusi)))))))
+
+(defn seuraa-polkua!
+  "Luo kursorin atomista annetun polun perusteella, ja rekisteröi atomin
+  seurattavaksi."
+  ([atomi polku sivu]
+    (seuraa-polkua! atomi polku sivu nil))
+  ([atomi polku sivu lisatieto]
+    (seuraa-kayttoa! (r/cursor atomi polku) sivu lisatieto)))
