34aa44df75cb37dad691a80edb7167a88a536b9e
mikkoro <mikko.ronkkomaki@gmail.com>
2017-11-24 09:53:44 +0200

    Lisää palvelutestit

--- test/clj/harja/palvelin/palvelut/kanavat/hairiotilanteet_test.clj
+++ test/clj/harja/palvelin/palvelut/kanavat/hairiotilanteet_test.clj
@@ -5,21 +5,12 @@
             [harja
              [pvm :as pvm]
              [testi :refer :all]]
-            [harja.kyselyt.konversio :as konv]
-            [harja.palvelin.palvelut.pois-kytketyt-ominaisuudet :as pois-kytketyt-ominaisuudet]
             [harja.palvelin.palvelut.yllapito-toteumat :refer :all]
             [harja.tyokalut.functor :refer [fmap]]
-            [taoensso.timbre :as log]
-            [clojure.spec.gen.alpha :as gen]
             [clojure.spec.alpha :as s]
-            [harja.palvelin.palvelut.kanavat.hairiotilanteet :as kan-hairio]
-            [clojure.string :as str]
-
-            [harja.domain.kanavat.kanava :as kanava]
-            [harja.domain.kanavat.kanavan-kohde :as kohde]
-            [harja.domain.kanavat.hairiotilanne :as hairio]
-            [harja.domain.urakka :as ur]
-            [harja.domain.muokkaustiedot :as m]))
+            [harja.palvelin.palvelut.kanavat.hairiotilanteet :as hairiotilanteet]
+            [harja.domain.kanavat.hairiotilanne :as hairiotilanne])
+  (:import (java.util UUID)))
 
 (defn jarjestelma-fixture [testit]
   (alter-var-root #'jarjestelma
@@ -30,7 +21,7 @@
                         :http-palvelin (testi-http-palvelin)
                         :pois-kytketyt-ominaisuudet testi-pois-kytketyt-ominaisuudet
                         :kan-hairio (component/using
-                                      (kan-hairio/->Hairiotilanteet)
+                                      (hairiotilanteet/->Hairiotilanteet)
                                       [:http-palvelin :db :pois-kytketyt-ominaisuudet])))))
   (testit)
   (alter-var-root #'jarjestelma component/stop))
@@ -41,24 +32,23 @@
 
 (deftest hairiotilanteiden-haku
   (let [urakka-id (hae-saimaan-kanavaurakan-id)
-        paasopimus-id (hae-saimaan-kanavaurakan-paasopimuksen-id)
         lisasopimus-id (hae-saimaan-kanavaurakan-lisasopimuksen-id)
         saimaan-kaikki-hairiot (ffirst (q "SELECT COUNT(*) FROM kan_hairio WHERE urakka = " urakka-id ";"))]
 
     (testing "Haku urakkalla"
-      (let [args {::hairio/urakka-id urakka-id}
+      (let [args {::hairiotilanne/urakka-id urakka-id}
             vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
                                     :hae-hairiotilanteet
                                     +kayttaja-jvh+
                                     args)]
 
-        (is (s/valid? ::hairio/hae-hairiotilanteet-kysely args))
-        (is (s/valid? ::hairio/hae-hairiotilanteet-vastaus vastaus))
+        (is (s/valid? ::hairiotilanne/hae-hairiotilanteet-kysely args))
+        (is (s/valid? ::hairiotilanne/hae-hairiotilanteet-vastaus vastaus))
         (is (>= (count vastaus) saimaan-kaikki-hairiot))))
 
     (testing "Haku tyhjällä urakkalla ei ole validi"
-      (is (not (s/valid? ::hairio/hae-hairiotilanteet-kysely
-                         {::hairio/urakka-id nil}))))
+      (is (not (s/valid? ::hairiotilanne/hae-hairiotilanteet-kysely
+                         {::hairiotilanne/urakka-id nil}))))
 
     ;; Testataan filtterit: jokaisen käyttö pitäisi palauttaa pienempi setti kuin
     ;; kaikki urakan häiriöt
@@ -66,65 +56,103 @@
     (is (< (count (kutsu-palvelua (:http-palvelin jarjestelma)
                                   :hae-hairiotilanteet
                                   +kayttaja-jvh+
-                                  {::hairio/urakka-id urakka-id
+                                  {::hairiotilanne/urakka-id urakka-id
                                    :haku-sopimus-id lisasopimus-id}))
            saimaan-kaikki-hairiot))
 
     (is (< (count (kutsu-palvelua (:http-palvelin jarjestelma)
                                   :hae-hairiotilanteet
                                   +kayttaja-jvh+
-                                  {::hairio/urakka-id urakka-id
+                                  {::hairiotilanne/urakka-id urakka-id
                                    :haku-vikaluokka :sahkotekninen_vika}))
            saimaan-kaikki-hairiot))
 
     (is (< (count (kutsu-palvelua (:http-palvelin jarjestelma)
                                   :hae-hairiotilanteet
                                   +kayttaja-jvh+
-                                  {::hairio/urakka-id urakka-id
+                                  {::hairiotilanne/urakka-id urakka-id
                                    :haku-korjauksen-tila :kesken}))
            saimaan-kaikki-hairiot))
 
     (is (< (count (kutsu-palvelua (:http-palvelin jarjestelma)
                                   :hae-hairiotilanteet
                                   +kayttaja-jvh+
-                                  {::hairio/urakka-id urakka-id
+                                  {::hairiotilanne/urakka-id urakka-id
                                    :haku-korjauksen-tila :kesken}))
            saimaan-kaikki-hairiot))
 
     (is (< (count (kutsu-palvelua (:http-palvelin jarjestelma)
                                   :hae-hairiotilanteet
                                   +kayttaja-jvh+
-                                  {::hairio/urakka-id urakka-id
+                                  {::hairiotilanne/urakka-id urakka-id
                                    :haku-odotusaika-h [60 60]}))
            saimaan-kaikki-hairiot))
 
     (is (< (count (kutsu-palvelua (:http-palvelin jarjestelma)
                                   :hae-hairiotilanteet
                                   +kayttaja-jvh+
-                                  {::hairio/urakka-id urakka-id
+                                  {::hairiotilanne/urakka-id urakka-id
                                    :haku-korjausaika-h [20 20]}))
            saimaan-kaikki-hairiot))
 
     (is (< (count (kutsu-palvelua (:http-palvelin jarjestelma)
                                   :hae-hairiotilanteet
                                   +kayttaja-jvh+
-                                  {::hairio/urakka-id urakka-id
+                                  {::hairiotilanne/urakka-id urakka-id
                                    :haku-paikallinen-kaytto? true}))
            saimaan-kaikki-hairiot))
 
     (is (< (count (kutsu-palvelua (:http-palvelin jarjestelma)
                                   :hae-hairiotilanteet
                                   +kayttaja-jvh+
-                                  {::hairio/urakka-id urakka-id
+                                  {::hairiotilanne/urakka-id urakka-id
                                    :haku-aikavali [(pvm/luo-pvm 2015 0 1)
                                                    (pvm/luo-pvm 2015 2 1)]}))
            saimaan-kaikki-hairiot))))
 
 (deftest hairiotilanteiden-haku-ilman-oikeuksia
   (let [urakka-id (hae-saimaan-kanavaurakan-id)
-        args {::hairio/urakka-id urakka-id}]
+        args {::hairiotilanne/urakka-id urakka-id}]
 
     (is (thrown? Exception (kutsu-palvelua (:http-palvelin jarjestelma)
                                            :hae-hairiotilanteet
                                            +kayttaja-ulle+
                                            args)))))
+
+(defn tallennuksen-parametrit [syy]
+  (let [urakka-id (hae-saimaan-kanavaurakan-id)
+        sopimus-id (hae-saimaan-kanavaurakan-paasopimuksen-id)
+        kohde-id (hae-saimaan-kanavan-tikkalasaaren-sulun-kohde-id)]
+    {::hairiotilanne/hairiotilanne {::hairiotilanne/sopimus-id sopimus-id
+                                    ::hairiotilanne/kohde-id kohde-id
+                                    ::hairiotilanne/paikallinen-kaytto? true
+                                    ::hairiotilanne/vikaluokka :sahkotekninen_vika
+                                    ::hairiotilanne/korjaustoimenpide "Vähennetään sähköä"
+                                    ::hairiotilanne/korjauksen-tila :kesken
+                                    ::hairiotilanne/pvm (pvm/luo-pvm 2017 11 17)
+                                    ::hairiotilanne/urakka-id urakka-id
+                                    ::hairiotilanne/kuittaaja-id (:id +kayttaja-jvh+)
+                                    ::hairiotilanne/huviliikenne-lkm 1
+                                    ::hairiotilanne/korjausaika-h 1
+                                    ::hairiotilanne/syy syy
+                                    ::hairiotilanne/odotusaika-h 4
+                                    ::hairiotilanne/ammattiliikenne-lkm 2}
+     ::hairiotilanne/hae-hairiotilanteet-kysely {::hairiotilanne/urakka-id urakka-id}}))
+
+(deftest hairiotilanteen-tallennus
+  (let [syy (str "hairiotilanteen-tallennus-testi-" (UUID/randomUUID))
+        parametrit (tallennuksen-parametrit syy)
+        vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
+                                :tallenna-hairiotilanne
+                                +kayttaja-jvh+
+                                parametrit)]
+    (is (some #(= syy (::hairiotilanne/syy %)) vastaus))))
+
+(deftest hairiotilanteiden-tallennus-ilman-oikeuksia
+  (let [syy (str "hairiotilanteen-tallennus-testi-" (UUID/randomUUID))
+        parametrit (tallennuksen-parametrit syy)]
+
+    (is (thrown? Exception (kutsu-palvelua (:http-palvelin jarjestelma)
+                                           :tallenna-hairiotilanne
+                                           +kayttaja-ulle+
+                                           parametrit)))))
--- test/clj/harja/testi.clj
+++ test/clj/harja/testi.clj
@@ -351,6 +351,13 @@
 (defn hae-kanavakohde-taipaleen-sulku []
   (ffirst (q (str "SELECT id FROM kan_kohde WHERE nimi = 'Taipaleen sulku';"))))
 
+(defn hae-saimaan-kanavan-tikkalasaaren-sulun-kohde-id []
+  (ffirst (q "SELECT kk.id
+              FROM kan_kohde kk
+                JOIN kan_kohde_urakka kku ON kk.id = kku.\"kohde-id\"
+                JOIN urakka u ON u.id = kku.\"urakka-id\"
+              WHERE u.nimi = 'Saimaan kanava' AND kk.nimi = 'Tikkalansaaren sulku';")))
+
 (defn hae-helsingin-vesivaylaurakan-urakoitsija []
   (ffirst (q (str "SELECT urakoitsija
                    FROM   urakka
