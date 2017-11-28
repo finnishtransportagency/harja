94c8b5a277b01823efdfe644f35d129c7db01c11
Joonas Mikkonen <joonas.mikkonen@solita.fi>
2017-11-28 21:16:40 +0200

    refactoroi materiaalit palveluja

--- src/clj/harja/kyselyt/vesivaylat/materiaalit.clj
+++ src/clj/harja/kyselyt/vesivaylat/materiaalit.clj
@@ -1,4 +1,21 @@
 (ns harja.kyselyt.vesivaylat.materiaalit
-  (:require [jeesql.core :refer [defqueries]]))
+  (:require [jeesql.core :refer [defqueries]]
+            [specql.core :refer [fetch insert! update!] :as specql]
+            [harja.domain.vesivaylat.materiaali :as m]
+            [harja.domain.muokkaustiedot :as muok]))
 
 (defqueries "harja/kyselyt/vesivaylat/materiaalit.sql")
+
+(defn hae-materiaalilistaus [db hakuehdot]
+  (fetch db ::m/materiaalilistaus (specql/columns ::m/materiaalilistaus) hakuehdot))
+
+(defn kirjaa-materiaali [db user materiaalikirjaus]
+  "Kirjaa materiaalin käytön tai lisäyksen"
+  (insert! db ::m/materiaali
+           (muok/lisaa-muokkaustiedot materiaalikirjaus ::m/id user)))
+
+(defn poista-materiaalikirjaus [db user materiaali-id]
+  (update! db ::m/materiaali
+           (muok/poistotiedot user)
+           {::m/id (::m/id materiaali-id)}))
+
