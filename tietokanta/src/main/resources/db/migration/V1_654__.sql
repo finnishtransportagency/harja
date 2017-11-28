e0c99ec007a8707eb39abdc456d29e8db6e2d7c0
mikkoro <mikko.ronkkomaki@gmail.com>
2017-11-28 08:08:56 +0200

    Lisää loput tieluvan tiedot

--- tietokanta/src/main/resources/db/migration/V1_654__.sql
+++ tietokanta/src/main/resources/db/migration/V1_654__.sql
@@ -37,38 +37,57 @@ CREATE TYPE TIELUPAHAKIJATYYPPI AS ENUM ('kunta', 'kotitalous', 'elinkeinoelama'
 -- tieluvan perustaulu
 CREATE TABLE tielupa (
   -- hakemuksen perustiedot
-  id                             SERIAL PRIMARY KEY,
-  "ulkoinen-tunniste"            INTEGER                              NOT NULL,
-  tyyppi                         VARCHAR(128)                         NOT NULL,
-  "paatoksen-diaarinumero"       VARCHAR(128)                         NOT NULL,
-  saapumispvm                    DATE,
-  myontamispvm                   DATE,
-  "voimassaolon-alkupvm"         DATE,
-  "voimassaolon-loppupvm"        DATE,
-  otsikko                        VARCHAR(2048)                        NOT NULL,
-  "katselmus-url"                TEXT,
-  ely                            INTEGER REFERENCES organisaatio (id) NOT NULL,
-  urakka                         INTEGER REFERENCES urakka (id),
-  "urakan-nimi"                  VARCHAR(512),
-  kunta                          VARCHAR(256)                         NOT NULL,
-  "kohde-lahiosoite"             VARCHAR(512),
-  "kohde-postinumero"            VARCHAR(5),
-  "kohde-postitoimipaikka"       VARCHAR(512),
-  "tien-nimi"                    VARCHAR(512),
-  sijainnit                      TR_OSOITE [],
+  id                                      SERIAL PRIMARY KEY,
+  "ulkoinen-tunniste"                     INTEGER                              NOT NULL,
+  tyyppi                                  VARCHAR(128)                         NOT NULL,
+  "paatoksen-diaarinumero"                VARCHAR(128)                         NOT NULL,
+  saapumispvm                             DATE,
+  myontamispvm                            DATE,
+  "voimassaolon-alkupvm"                  DATE,
+  "voimassaolon-loppupvm"                 DATE,
+  otsikko                                 VARCHAR(2048)                        NOT NULL,
+  "katselmus-url"                         TEXT,
+  ely                                     INTEGER REFERENCES organisaatio (id) NOT NULL,
+  urakka                                  INTEGER REFERENCES urakka (id),
+  "urakan-nimi"                           VARCHAR(512),
+  kunta                                   VARCHAR(256)                         NOT NULL,
+  "kohde-lahiosoite"                      VARCHAR(512),
+  "kohde-postinumero"                     VARCHAR(5),
+  "kohde-postitoimipaikka"                VARCHAR(512),
+  "tien-nimi"                             VARCHAR(512),
+  sijainnit                               TR_OSOITE [],
 
   -- hakijan tiedot
-  "hakija-nimi"                  VARCHAR(512)                         NOT NULL,
-  "hakija-osasto"                VARCHAR(512),
-  "hakija-postinosoite"          VARCHAR(512)                         NOT NULL,
-  "hakija-postinumero"           VARCHAR(5)                           NOT NULL,
-  "hakija-puhelinnumero"         VARCHAR(16),
-  "hakija-sahkopostiosoite"      VARCHAR(16)                          NOT NULL,
-  "hakija-tyyppi"                TIELUPAHAKIJATYYPPI,
-  "hakija-maakoodi"              VARCHAR(128),
+  "hakija-nimi"                           VARCHAR(512)                         NOT NULL,
+  "hakija-osasto"                         VARCHAR(512),
+  "hakija-postinosoite"                   VARCHAR(512)                         NOT NULL,
+  "hakija-postinumero"                    VARCHAR(5)                           NOT NULL,
+  "hakija-puhelinnumero"                  VARCHAR(16),
+  "hakija-sahkopostiosoite"               VARCHAR(512)                         NOT NULL,
+  "hakija-tyyppi"                         TIELUPAHAKIJATYYPPI,
+  "hakija-maakoodi"                       VARCHAR(128),
 
   -- urakoitsijan tiedot
-  "urakoitsija-nimi"             VARCHAR(512)                         NOT NULL,
-  "urakoitsija-yhteyshenkilo"    VARCHAR(512)                         NOT NULL,
-  "urakoitsija-puhelinnumero"    VARCHAR(16),
-  "urakoitsija-sahkopostiosoite" VARCHAR(16)                          NOT NULL);
+  "urakoitsija-nimi"                      VARCHAR(512)                         NOT NULL,
+  "urakoitsija-yhteyshenkilo"             VARCHAR(512),
+  "urakoitsija-puhelinnumero"             VARCHAR(16),
+  "urakoitsija-sahkopostiosoite"          VARCHAR(512),
+
+  -- liikenteenohjauksesta vastaavan tiedot
+  "liikenneohjaajan-nimi"                 VARCHAR(512)                         NOT NULL,
+  "liikenneohjaajan-yhteyshenkilo"        VARCHAR(512),
+  "liikenneohjaajan-puhelinnumero"        VARCHAR(16),
+  "liikenneohjaajan-sahkopostiosoite"     VARCHAR(512),
+
+  -- tienpitoviranomaisen tiedot
+  "tienpitoviranomainen-yhteyshenkilo"    VARCHAR(512),
+  "tienpitoviranomainen-puhelinnumero"    VARCHAR(16),
+  "tienpitoviranomainen-sahkopostiosoite" VARCHAR(512),
+  "tienpitoviranomainen-lupapaallikko"    VARCHAR(512),
+  "tienpitoviranomainen-kasittelija"      VARCHAR(512),
+
+  -- valmistumisilmoitus
+  "valmistumisilmoitus-vaaditaan"         BOOLEAN,
+  "valmistumisilmoitus-palautettu"        BOOLEAN,
+  "valmistumisilmoitus"                   TEXT
+);
