7dd2788f801ca83a52e32565bb9b93e37de788ab
Erno Kuusela <erno-laptop!erno>
2017-11-28 12:23:14 +0200

    korjaa keyword/string-fiba

--- src/cljs/harja/tiedot/kanavat/urakka/toimenpiteet/muutos_ja_lisatyot.cljs
+++ src/cljs/harja/tiedot/kanavat/urakka/toimenpiteet/muutos_ja_lisatyot.cljs
@@ -331,7 +331,7 @@
   LisaaMuuTyorivi
   (process-event [_ app]
     (lisaa-hintarivi-toimenpiteelle
-      {::hinta/ryhma :tyo}
+      {::hinta/ryhma "tyo"}
       app))
 
   PoistaHinnoiteltavaTyorivi
