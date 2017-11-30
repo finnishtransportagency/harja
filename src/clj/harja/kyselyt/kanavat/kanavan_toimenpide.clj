d029fb84306cadda1ff869c10897c7c235b410eb
Erno Kuusela <erno-laptop!erno>
2017-11-30 17:11:27 +0200

    poista debug-printtejä

--- src/clj/harja/kyselyt/kanavat/kanavan_toimenpide.clj
+++ src/clj/harja/kyselyt/kanavat/kanavan_toimenpide.clj
@@ -80,7 +80,6 @@
     m))
 
 (defn tallenna-toimenpiteen-omat-hinnat! [{:keys [db user hinnat toimenpide-id]}]
-  (println "tallenna-omat-hinnat: saatiin" (pr-str hinnat))
   (doseq [hinta (map #(poista-frontin-keksima-id % ::hinta/id) hinnat)]
 
     (specql/upsert! db
@@ -90,7 +89,6 @@
 
 (defn tallenna-toimenpiteen-tyot! [{:keys [db user tyot toimenpide-id]}]
   (doseq [tyo (map #(poista-frontin-keksima-id % ::tyo/id) tyot)]
-    (println "upsertoidaan" (pr-str (kasittele-muokkaustiedot user tyo ::tyo/id)))
     (specql/upsert! db
                     ::tyo/toimenpiteen-tyo
                     (kasittele-muokkaustiedot user tyo ::tyo/id)
