eb25fa3399e5850353796de824b4b03a32c5ef9d
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-12-01 13:20:31 +0200

    Ketjutustapahtumalle kohteelta ja kohteelle tiedot, ei kohde ja suunta

--- tietokanta/src/main/resources/db/migration/V1_656__.sql
+++ tietokanta/src/main/resources/db/migration/V1_656__.sql
@@ -1,9 +1,9 @@
 CREATE TABLE kan_liikennetapahtuma_ketjutus(
   id SERIAL PRIMARY KEY,
-  suunta liikennetapahtuma_suunta NOT NULL,
   aika TIMESTAMP NOT NULL,
 
-  "kohde-id" INTEGER REFERENCES kan_kohde(id) NOT NULL,
+  "kohteelle-id" INTEGER REFERENCES kan_kohde(id) NOT NULL,
+  "kohteelta-id" INTEGER REFERENCES kan_kohde(id) NOT NULL,
   "sopimus-id" INTEGER REFERENCES sopimus(id) NOT NULL,
   "urakka-id" INTEGER REFERENCES urakka(id) NOT NULL,
   "alus-id" INTEGER REFERENCES kan_liikennetapahtuma_alus(id) NOT NULL,
