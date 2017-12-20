8a426e5fe8384371d9b43b28cca1e9487686ac07
mikkoro <mikko.ronkkomaki@gmail.com>
2017-12-20 10:00:50 +0200

    Lisää tuotenumero kanavaurakan väylänhoidolle

--- tietokanta/src/main/resources/db/migration/R__Toimenpidekoodit.sql
+++ tietokanta/src/main/resources/db/migration/R__Toimenpidekoodit.sql
@@ -1964,7 +1964,7 @@ SELECT lisaa_toimenpidekoodi(
 SELECT lisaa_toimenpidekoodi(
     'Hoito, meri','24000',1,  null,  null,  NULL ,  NULL ,  NULL );
 SELECT lisaa_toimenpidekoodi(
-    'Väylänhoito ','24100',2,  null,  null,  (SELECT nimi FROM toimenpidekoodi WHERE koodi='24000') ,  (SELECT koodi FROM toimenpidekoodi WHERE koodi='24000') ,  (SELECT taso FROM toimenpidekoodi WHERE koodi='24000') );
+    'Väylänhoito ','24100',2,  null,  201,  (SELECT nimi FROM toimenpidekoodi WHERE koodi='24000') ,  (SELECT koodi FROM toimenpidekoodi WHERE koodi='24000') ,  (SELECT taso FROM toimenpidekoodi WHERE koodi='24000') );
 SELECT lisaa_toimenpidekoodi(
     'Sopimuksen mukaiset työt','24101',3,  null,  null,  (SELECT nimi FROM toimenpidekoodi WHERE koodi='24100') ,  (SELECT koodi FROM toimenpidekoodi WHERE koodi='24100') ,  (SELECT taso FROM toimenpidekoodi WHERE koodi='24100') );
 SELECT lisaa_toimenpidekoodi(
@@ -2830,3 +2830,7 @@ SET hinnoittelu = ARRAY['kokonaishintainen'::hinnoittelutyyppi],
 WHERE taso = 4 AND
       emo in (SELECT id from toimenpidekoodi t3 WHERE t3.nimi = 'Laaja toimenpide')
       AND nimi = 'Ei yksilöity';
+
+UPDATE toimenpidekoodi
+SET tuotenumero = 201
+WHERE koodi = '24100';
