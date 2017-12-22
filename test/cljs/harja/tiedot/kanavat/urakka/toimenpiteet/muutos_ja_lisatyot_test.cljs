2914d3a30a4c1859d8b2f737706f53c148278643
Jari Hanhela <jarihanhela@gmail.com>
2017-12-22 10:09:17 +0200

    Formatoi koodi paremmin

--- test/cljs/harja/tiedot/kanavat/urakka/toimenpiteet/muutos_ja_lisatyot_test.cljs
+++ test/cljs/harja/tiedot/kanavat/urakka/toimenpiteet/muutos_ja_lisatyot_test.cljs
@@ -8,8 +8,6 @@
             [clojure.test :refer-macros [deftest is testing]]
             [harja.testutils.tuck-apurit :refer-macros [vaadi-async-kutsut] :refer [e!]]))
 
-
-
 (deftest nakymaan-tuleminen
   (is (true? (:nakymassa? (e! (tiedot/->Nakymassa? true)))))
   (is (false? (:nakymassa? (e! (tiedot/->Nakymassa? false))))))
