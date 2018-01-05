71ddb788eea2859ef5f6fa41557a6ea1995513e2
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2018-01-05 09:38:05 +0200

    Lisää ilmoitukset_testiin futuren valmistumien odotus

--- test/clj/harja/palvelin/integraatiot/tloik/ilmoitukset_test.clj
+++ test/clj/harja/palvelin/integraatiot/tloik/ilmoitukset_test.clj
@@ -124,6 +124,7 @@
                                                        kayttaja portti))]
       (sonja/laheta (:sonja jarjestelma) +tloik-ilmoitusviestijono+ +testi-ilmoitus-sanoma+)
 
+      (odota-ehdon-tayttymista #(realized? ilmoitushaku) "Saatiin vastaus ilmoitushakuun." 100000)
       (odota-ehdon-tayttymista #(= 1 (count @viestit)) "Kuittaus on vastaanotettu." 100000)
 
       (let [xml (first @viestit)
