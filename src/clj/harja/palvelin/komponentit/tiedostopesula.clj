ed9f5e3bfcce78aae572b3a1f79fa9b0a6e7bc2f
Erno Kuusela <erno-laptop!erno>
2018-01-10 16:39:53 +0200

    tiedostopesula-asiakas wip

--- src/clj/harja/palvelin/komponentit/tiedostopesula.clj
+++ src/clj/harja/palvelin/komponentit/tiedostopesula.clj
@@ -9,7 +9,9 @@
             [harja.palvelin.palvelut.pois-kytketyt-ominaisuudet :refer [ominaisuus-kaytossa?]]
             [harja.palvelin.tyokalut.ajastettu-tehtava :as ajastettu-tehtava]
             [harja.palvelin.tyokalut.lukot :as lukot]
-            [clojure.java.jdbc :as jdbc])
+            [clojure.java.jdbc :as jdbc]
+            [org.httpkit.client :as http]
+            [clojure.java.io :refer [file output-stream input-stream]])
   (:import (java.io InputStream ByteArrayOutputStream)
            (org.postgresql.largeobject LargeObjectManager)
            (com.mchange.v2.c3p0 C3P0ProxyConnection)
@@ -17,20 +19,40 @@
            (net.coobird.thumbnailator.tasks UnsupportedFormatException))
   (:use [slingshot.slingshot :only [try+ throw+]]))
 
-(defn pdf-preview "java.io/file -> png byte-array | nil" [pdf-file]
+(defn pdfa-convert! "java.io/file -> png byte-array | nil" [pdf-file]
   (try
-    (let [request-opts
+    (let [laundry-pdf2pdfa-url "https://laundry.solitacloud.fi/pdf/pdf2pdfa"
+          request-opts
           {:as :byte-array
            :multipart [{:name "file"
                         :content pdf-file
                         :mime-type "application/pdf"}]}
-          resp (http/post (str (env/value :muuntaja :url) pdf-preview-path)
-                          request-opts)]
+          resp (deref (http/post laundry-pdf2pdfa-url request-opts))]
       (if (= 200 (:status resp))
         (:body resp)
         (do
-          (log/info "pdf preview failed for " (.getName pdf-file))
+          (log/info "PDF/A conversion failed for " (.getName pdf-file))
+          (log/info "got: " (pr-str resp))
           nil)))
     (catch Exception ex
       (log/error ex)
       nil)))
+
+(defonce pdf-resp (atom nil))
+
+(defn test-pdfa-convert []
+  (let [in-file (file "/tmp/pdfa-test.pdf")]
+    (let [resp (pdfa-convert! in-file)]
+      (reset! pdf-resp resp)
+      (with-open [out (output-stream "pdfa-test.out.pdf")]
+        (log/info "resp type" (type resp))
+        (.write out resp)))))
+
+;; (defn test-pdf-preview []
+;;   (with-open [in (input-stream (file "/tmp/pdfa-test.pdf"))]
+;;     (let [pdf-buffer (byte-array (* 10 1024 1024))
+;;           n (.read in pdf-buffer)
+;;           resp (pdfa-convert! pdf-buffer)]
+;;       (reset! pdf-resp resp)
+;;       (with-open [out (output-stream "pdfa-test.out.pdf")]
+;;         (.write out resp)))))
