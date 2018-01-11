690df317d72f344a8ad7107a213acc0cbaf73510
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2018-01-11 14:47:55 +0200

    Jos ei urakassa indeksiä, ei näytetä peruslukuakaan.

--- src/cljs/harja/views/urakka/yleiset.cljs
+++ src/cljs/harja/views/urakka/yleiset.cljs
@@ -264,10 +264,11 @@
   (let [auki? (atom false)]
     (fn [ur]
       [:span
-       (or (str (:indeksi ur)
-                (when (:indeksilaskennan_perusluku ur)
-                  (str ", perusluku: " (:indeksilaskennan_perusluku ur))))
-           "Ei käytössä")
+       (if (:indeksi ur)
+         (or (str (:indeksi ur)
+                  (when (:indeksilaskennan_perusluku ur)
+                    (str ", perusluku: " (:indeksilaskennan_perusluku ur))))
+             "Ei käytössä"))
        (when (and (:indeksi ur)
                   (roolit/tilaajan-kayttaja? @istunto/kayttaja)
                   (oikeudet/voi-kirjoittaa? oikeudet/urakat-yleiset (:id ur)))
