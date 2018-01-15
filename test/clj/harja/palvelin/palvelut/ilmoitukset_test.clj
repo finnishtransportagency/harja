84ebc29299b61434fa77aed9337354ca1535288a
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2018-01-15 14:34:27 +0200

    Poista turhan test check depsut

--- test/clj/harja/palvelin/palvelut/ilmoitukset_test.clj
+++ test/clj/harja/palvelin/palvelut/ilmoitukset_test.clj
@@ -1,9 +1,5 @@
 (ns harja.palvelin.palvelut.ilmoitukset-test
   (:require [clojure.test :refer :all]
-            [clojure.test.check :as tc]
-            [clojure.test.check.clojure-test :refer [defspec]]
-            [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop]
             [clojure.set :as set]
 
             [harja.domain.tieliikenneilmoitukset :refer [+ilmoitustyypit+ ilmoitustyypin-nimi +ilmoitustilat+]]
