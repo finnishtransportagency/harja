8ca8873ac3fec08f5a24d8e15a95b65ec9388992
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2018-01-16 10:06:49 +0200

    Lisää kohteenosille sijainnit testidataan

--- tietokanta/testidata/kanavat/kohteet.sql
+++ tietokanta/testidata/kanavat/kohteet.sql
@@ -18,25 +18,25 @@ INSERT INTO kan_kohde(nimi, "kohdekokonaisuus-id", luoja)
 
       ('Iisalmen kanava',(SELECT id FROM kan_kohdekokonaisuus WHERE nimi = 'Iisalmen reitti'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'));
 
-INSERT INTO kan_kohteenosa (tyyppi, nimi, oletuspalvelumuoto, "kohde-id", luoja)
+INSERT INTO kan_kohteenosa (tyyppi, nimi, oletuspalvelumuoto, "kohde-id", luoja, sijainti)
     VALUES
-      ('silta', NULL, 'itse', (SELECT id FROM kan_kohde WHERE nimi = 'Pälli'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
+      ('silta', NULL, 'itse', (SELECT id FROM kan_kohde WHERE nimi = 'Pälli'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(430925.930308596 7198503.58736702)'),
 
-      ('sulku', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Kansola'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
+      ('sulku', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Kansola'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(410925.930308596 7498503.58736702)'),
 
-      ('sulku', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Soskua'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
-      ('silta', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Soskua'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
+      ('sulku', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Soskua'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(420525.930308596 7198503.58736702)'),
+      ('silta', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Soskua'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(420925.930308596 7199503.58736702)'),
 
-      ('sulku', NULL, 'paikallis', (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
-      ('silta', NULL, 'paikallis', (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
-      ('rautatiesilta', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
+      ('sulku', NULL, 'paikallis', (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(440925.930308596 7108503.58736702)'),
+      ('silta', NULL, 'paikallis', (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(440525.930308596 7103503.58736702)'),
+      ('rautatiesilta', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(430925.930308596 7198503.58736702)'),
 
-      ('silta', 'Iso silta', NULL, (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun mutkan sillat'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
-      ('silta', 'Pieni silta', NULL, (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun mutkan sillat'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
-      ('rautatiesilta', NULL, NULL, (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun mutkan sillat'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
+      ('silta', 'Iso silta', NULL, (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun mutkan sillat'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(490025.930308596 7198503.58736702)'),
+      ('silta', 'Pieni silta', NULL, (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun mutkan sillat'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(490725.930308596 7190503.58736702)'),
+      ('rautatiesilta', NULL, NULL, (SELECT id FROM kan_kohde WHERE nimi = 'Joensuun mutkan sillat'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(490625.930308596 7198503.58736702)'),
 
-      ('sulku', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Iisalmen kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero')),
-      ('silta', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Iisalmen kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'));
+      ('sulku', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Iisalmen kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(450925.930308596 7198503.58736702)'),
+      ('silta', NULL, 'kauko', (SELECT id FROM kan_kohde WHERE nimi = 'Iisalmen kanava'), (SELECT id FROM kayttaja WHERE kayttajanimi='tero'), 'POINT(450025.930308596 7198503.58736702)');
 
 -- Ketjutus
 UPDATE kan_kohde
