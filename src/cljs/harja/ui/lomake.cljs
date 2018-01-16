5d720fb45cf2de503921374a08474ed56c950c53
Jari Hanhela <jarihanhela@gmail.com>
2018-01-16 12:32:39 +0200

    Revertoi vahinko-commitit

--- src/cljs/harja/ui/lomake.cljs
+++ src/cljs/harja/ui/lomake.cljs
@@ -178,12 +178,25 @@ Ryhmien otsikot lisätään väliin Otsikko record tyyppinä."
           [:div.vihjeen-lisarivi (str "  " vihje)])
         (rest vihjeet))]]))
 
-(defn kentan-vihje [{:keys [vihje vihje-leijuke vihje-leijuke-optiot] :as skeema}]
+(defn kentan-vihje-leijuke [leijuke-sisalto]
+  (let [nakyvissa? (atom false)]
+    (fn [leijuke-sisalto]
+      [:div {:class
+             (str "inline-block yleinen-pikkuvihje klikattava")}
+       [:div.vihjeen-sisalto {:on-click #(reset! nakyvissa? true)}
+        (if @nakyvissa?
+          [leijuke/leijuke {:otsikko [ikonit/ikoni-ja-teksti (ikonit/livicon-info-sign) "Vihje"]
+                            :sulje! #(reset! nakyvissa? false)}
+           [:div {:style {:min-width "300px"}}
+            leijuke-sisalto]]
+          (harja.ui.ikonit/livicon-info-sign))]])))
+
+(defn kentan-vihje [{:keys [vihje vihje-leijuke] :as skeema}]
   [:span
    (when vihje
      [kentan-vihje-inline vihje])
    (when vihje-leijuke
-     [leijuke/vihjeleijuke vihje-leijuke-optiot vihje-leijuke])])
+     [kentan-vihje-leijuke vihje-leijuke])])
 
 (defn yleinen-huomautus
   "Yleinen huomautus, joka voidaan näyttää esim. lomakkeen tallennuksen yhteydessä"
