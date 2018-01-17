a179956bc0491e109e187a36c465d2c227f43d95
Jari Hanhela <jarihanhela@gmail.com>
2018-01-17 12:16:59 +0200

    Poista turha CREATE TYPE

--- tietokanta/src/main/resources/db/migration/V1_676__.sql
+++ tietokanta/src/main/resources/db/migration/V1_676__.sql
@@ -1,5 +1,3 @@
 -- Lisää aikataulun toimenpiteitä
-CREATE TYPE yllapitokohteen_aikataulu_toimenpide AS ENUM ('ojankaivuu', 'rp_tyot', 'rumpujen_vaihto', 'muu');
-
 ALTER TYPE yllapitokohteen_aikataulu_toimenpide ADD VALUE 'sekoitusjyrsinta';
 ALTER TYPE yllapitokohteen_aikataulu_toimenpide ADD VALUE 'murskeenlisays';
