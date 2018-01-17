4a2421018429ddc771affb71531f6206357e6891
Jari Hanhela <jarihanhela@gmail.com>
2018-01-17 12:16:16 +0200

    Korjaa SQL-typo

--- tietokanta/src/main/resources/db/migration/V1_676__.sql
+++ tietokanta/src/main/resources/db/migration/V1_676__.sql
@@ -2,4 +2,4 @@
 CREATE TYPE yllapitokohteen_aikataulu_toimenpide AS ENUM ('ojankaivuu', 'rp_tyot', 'rumpujen_vaihto', 'muu');
 
 ALTER TYPE yllapitokohteen_aikataulu_toimenpide ADD VALUE 'sekoitusjyrsinta';
-ALTER TYPE yllapitokohteen_aikataulu_toimenpide ADD VALUE 'murskeenlisays'
+ALTER TYPE yllapitokohteen_aikataulu_toimenpide ADD VALUE 'murskeenlisays';
