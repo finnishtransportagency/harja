bce56cb902eb7dc599e33bd03542e99ea2eabf69
Jari Hanhela <jarihanhela@gmail.com>
2018-01-26 17:22:38 +0200

    Tunnista 403 vastaus frontissa

--- src/cljs/harja/asiakas/kommunikaatio.cljs
+++ src/cljs/harja/asiakas/kommunikaatio.cljs
@@ -93,6 +93,10 @@
              (when (some? vastaus)
                (cond
                  (= (:status vastaus) 302)
+                 (do (kasittele-istunto-vanhentunut) ; Extranet-kirjautuminen vanhentunut
+                     (close! chan))
+
+                 (= (:status vastaus) 403) ; Harjan anti-CSRF-sessio vanhentunut
                  (do (kasittele-istunto-vanhentunut)
                      (close! chan))
 
