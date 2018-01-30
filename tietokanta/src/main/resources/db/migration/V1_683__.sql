06e4443f0253bf16fbc3f123f95df8e8c9ab949a
Jari Hanhela <jarihanhela@gmail.com>
2018-01-30 08:08:34 +0200

    Formatoi migraatio oikein

--- tietokanta/src/main/resources/db/migration/V1_683__.sql
+++ tietokanta/src/main/resources/db/migration/V1_683__.sql
@@ -1,10 +1,10 @@
 -- Lisää taulu, johon tullaan tallentamaan odottavien sähköpostien tietoja (aluksi tiemerkinnän valmistuminen)
-CREATE TYPE odottava_sahkoposti_tyyppi AS ENUM ('tiemerkinta_valmistunut');
+CREATE TYPE ODOTTAVA_SAHKOPOSTI_TYYPPI AS ENUM ('tiemerkinta_valmistunut');
 
 CREATE TABLE odottava_sahkoposti (
   id                 SERIAL PRIMARY KEY,
-  tyyppi odottava_sahkoposti_tyyppi NOT NULL,
-  "vastaanottajat"   TEXT []   NOT NULL,
-  viesti             TEXT      NOT NULL,
+  tyyppi             ODOTTAVA_SAHKOPOSTI_TYYPPI NOT NULL,
+  vastaanottajat     TEXT []                    NOT NULL,
+  viesti             TEXT                       NOT NULL,
   kopio_lahettajalle TEXT -- Mailin aikaansaaneen käyttäjän s-posti, johon lähetetään kopio viestistä (tai NULL)
 );
