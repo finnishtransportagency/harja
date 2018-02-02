75955acaf587b907253f7645aaf526b7058f2e46
Jari Hanhela <jarihanhela@gmail.com>
2018-02-02 09:26:41 +0200

    Päivitä migraatio, lisää linkitys ylläpitokohteeseen

--- tietokanta/src/main/resources/db/migration/V1_686__.sql
+++ tietokanta/src/main/resources/db/migration/V1_686__.sql
@@ -3,8 +3,9 @@ CREATE TYPE ODOTTAVA_SAHKOPOSTI_TYYPPI AS ENUM ('tiemerkinta_valmistunut');
 
 CREATE TABLE odottava_sahkoposti (
   id                 SERIAL PRIMARY KEY,
-  tyyppi             ODOTTAVA_SAHKOPOSTI_TYYPPI NOT NULL,
-  vastaanottajat     TEXT []                    NOT NULL,
-  viesti             TEXT                       NOT NULL,
+  tyyppi             ODOTTAVA_SAHKOPOSTI_TYYPPI            NOT NULL,
+  yllapitokohde_id   INTEGER REFERENCES yllapitokohde (id) NOT NULL,
+  vastaanottajat     TEXT []                               NOT NULL,
+  viesti             TEXT                                  NOT NULL,
   kopio_lahettajalle TEXT -- Mailin aikaansaaneen käyttäjän s-posti, johon lähetetään kopio viestistä (tai NULL)
 );
