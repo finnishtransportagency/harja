f7df6a2041358e1d51e9d32a10f820d597688673
mikkoro <mikko.ronkkomaki@gmail.com>
2018-02-12 12:09:24 +0200

    Poista toimimaton testi

--- cypress/integration/nakymien_avaus_spec.js
+++ cypress/integration/nakymien_avaus_spec.js
@@ -1,22 +1,11 @@
- describe('Harja smoke testit', function () {
+describe('Päänäkymien avaamiset', function () {
     beforeEach(function () {
         cy.visit("https://harja-test.solitaservices.fi/#urakat/yleiset?")
     })
+    
     it("Urakkavalinta listan kautta toimii", function () {
         cy.contains('.haku-lista-item', 'Pohjois-Pohjanmaa').click()
-    })
-    it("Urakkavalinta kartalta toimii", function () {
-        // cy.server()
-        // cy.route('POST','**hallintayksikon-urakat**').as('hallintayksikon-urakat')
-
-        // valitse hallintayksikkö
-        cy.wait(10000)
-        cy.get('#kartta').click(402, 268)
-
-        // valitse urakka
-        //cy.wait('@hallintayksikon-urakat')
-        cy.wait(10000)
-        cy.get('#kartta').click(493, 574)
-
+        cy.contains('.haku-lista-item', 'Oulu').click()
+        cy.contains('Oulun alueurakka')
     })
 })
