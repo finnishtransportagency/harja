0526c64f52c3c8ecffd59333fd9c9ac478d48ad2
mikkoro <mikko.ronkkomaki@gmail.com>
2018-02-13 11:36:39 +0200

    Aseta suoraan varustekortin URL viittaamaan tuotantoon

--- src/cljs/harja/asiakas/kommunikaatio.cljs
+++ src/cljs/harja/asiakas/kommunikaatio.cljs
@@ -341,20 +341,9 @@ Kahden parametrin versio ottaa lisäksi transducerin jolla tulosdata vektori muu
       transit/clj->transit
       gstr/urlEncode))
 
-(defn kehitysymparistossa? []
-  "Tarkistaa ollaanko kehitysympäristössä"
-  (let [host (.-host js/location)]
-    (or (gstr/startsWith host "10.10.")
-        (#{"localhost" "localhost:3000" "localhost:8000" "harja-c7-dev.lxd:8000"
-           "harja-test.solitaservices.fi"
-           "harja-dev1" "harja-dev2" "harja-dev3" "harja-dev4" "harja-dev5" "harja-dev6"
-           "testiextranet.liikennevirasto.fi"} host))))
-
 (defn varustekortti-url [alkupvm tietolaji tunniste]
-  (->
-    (if (kehitysymparistossa?)
-      "https://testiextranet.liikennevirasto.fi/trkatselu/TrKatseluServlet?page=varuste&tpvm=<pvm>&tlaji=<tietolaji>&livitunniste=<tunniste>&act=haku"
-      "https://extranet.liikennevirasto.fi/trkatselu/TrKatseluServlet?page=varuste&tpvm=<pvm>&tlaji=<tietolaji>&livitunniste=<tunniste>&act=haku")
+  (-> 
+    "https://extranet.liikennevirasto.fi/trkatselu/TrKatseluServlet?page=varuste&tpvm=<pvm>&tlaji=<tietolaji>&livitunniste=<tunniste>&act=haku"
     (str/replace "<pvm>" (pvm/pvm alkupvm))
     (str/replace "<tietolaji>" tietolaji)
     (str/replace "<tunniste>" tunniste)))
