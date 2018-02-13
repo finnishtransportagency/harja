e630f3c69c4123befefcca13ec89e83d2e277a17
mikkoro <mikko.ronkkomaki@gmail.com>
2018-02-13 15:46:34 +0200

    Korjaa main-test

--- test/clj/harja/palvelin/main_test.clj
+++ test/clj/harja/palvelin/main_test.clj
@@ -70,7 +70,8 @@
     :kan-toimenpiteet
     :api-tieluvat
     :koordinaatit
-    :tiedostopesula})
+    :tiedostopesula
+    :api-paikkaukset})
 
 (deftest main-komponentit-loytyy
   (let [jarjestelma (sut/luo-jarjestelma (asetukset/lue-asetukset *testiasetukset*))
