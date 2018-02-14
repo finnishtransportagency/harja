bf22414928a15ed8905639f609363e7f49c5c871
Joonas Mikkonen <joonas.mikkonen@solita.fi>
2018-02-14 13:41:37 +0200

    Korjaa vv_materiaali testidata

--- tietokanta/testidata/kanavat/kanavien_materiaalit.sql
+++ tietokanta/testidata/kanavat/kanavien_materiaalit.sql
@@ -5,11 +5,11 @@ DO $$ DECLARE
 BEGIN
 INSERT INTO vv_materiaali(nimi, maara, pvm, "urakka-id", luoja, luotu, halytysraja, toimenpide, yksikko)
   VALUES ('Naulat', 1000, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), 200, NULL, 'kpl'),
-         ('Naulat', -3, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), NULL, toimenpide_id, 'kpl'),
-         ('Naulat', -10, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), NULL, NULL, 'kpl'),
-         ('Naulat', -23, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), NULL, NULL, 'kpl'),
+         ('Naulat', -3, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), 200, toimenpide_id, 'kpl'),
+         ('Naulat', -10, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), 200, NULL, 'kpl'),
+         ('Naulat', -23, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), 200, NULL, 'kpl'),
          ('Ämpäreitä', 500, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), 10, NULL, 'kpl'),
-         ('Ämpäreitä', -2, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), NULL, NULL, 'kpl'),
-         ('Ämpäreitä', -15, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), NULL, NULL, 'kpl'),
-         ('Ämpäreitä', -9, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), NULL, NULL, 'kpl');
+         ('Ämpäreitä', -2, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), 10, NULL, 'kpl'),
+         ('Ämpäreitä', -15, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), 10, NULL, 'kpl'),
+         ('Ämpäreitä', -9, '2017-11-28', saimaan_urakan_id, urakan_kayttaja, NOW(), 10, NULL, 'kpl');
 END $$
