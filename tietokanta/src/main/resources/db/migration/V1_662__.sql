fc4d9571020cc2819b499ef407f6652bdd40a2f8
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2018-02-16 15:28:34 +0200

    Revert "Salli tyhjä nimi tieluvan urakoitsijalle"
    This reverts commit dc7c52d4eb9f588e98098627a469c3b6a1d226d7.

--- tietokanta/src/main/resources/db/migration/V1_662__.sql
+++ tietokanta/src/main/resources/db/migration/V1_662__.sql
@@ -134,7 +134,7 @@ CREATE TABLE tielupa (
   "hakija-maakoodi"                                               TEXT,
 
   -- urakoitsijan tiedot
-  "urakoitsija-nimi"                                              TEXT,
+  "urakoitsija-nimi"                                              TEXT                                 NOT NULL,
   "urakoitsija-yhteyshenkilo"                                     TEXT,
   "urakoitsija-puhelinnumero"                                     TEXT,
   "urakoitsija-sahkopostiosoite"                                  TEXT,
