d463f6fb874433504b0f4a7ad5476f00553bbb12
Joonas Mikkonen <joonas.mikkonen@solita.fi>
2018-02-21 13:44:49 +0200

    Kartta toimintaan myös muutos- ja lisätöihin. Jos kohteessa on useampi toimenpide, näytä ne kaikki.

--- src/cljs/harja/tiedot/kanavat/kohteet_kartalla.cljs
+++ src/cljs/harja/tiedot/kanavat/kohteet_kartalla.cljs
@@ -3,6 +3,7 @@
             [harja.ui.kartta.esitettavat-asiat :refer [kartalla-esitettavaan-muotoon]]
             [harja.tiedot.kanavat.urakka.kanavaurakka :as kanavaurakka]
             [harja.tiedot.kanavat.urakka.toimenpiteet.kokonaishintaiset :as kokonaishintaiset]
+            [harja.tiedot.kanavat.urakka.toimenpiteet.muutos-ja-lisatyot :as lisatyot]
             [harja.domain.kanavat.kohde :as kohde]
             [harja.domain.kanavat.kanavan-toimenpide :as kanavan-toimenpide]
             [harja.domain.kanavat.kanavan-huoltokohde :as kanavan-huoltokohde]
@@ -16,10 +17,17 @@
 
 (defonce karttataso-kohteet (atom false))
 
+(defonce aktiivinen-nakyma
+  (reaction
+    (let [kokonaishintaiset-nakymassa? (:nakymassa? @kokonaishintaiset/tila)
+          lisatyot-nakymassa? (:nakymassa? @lisatyot/tila)]
+      (zipmap [:tila :nakyma]
+              (cond
+                kokonaishintaiset-nakymassa? [@kokonaishintaiset/tila :kokonaishintaiset]
+                lisatyot-nakymassa? [@lisatyot/tila :lisatyot])))))
+
 (defn- kohde-valittu? [kohde]
-  (let [kokonaishintaiset-nakymassa? (:nakymassa? @kokonaishintaiset/tila)]
-    (when kokonaishintaiset-nakymassa?
-      (= (::kohde/id kohde) (-> @kokonaishintaiset/tila :avattu-toimenpide ::kanavan-toimenpide/kohde ::kohde/id)))))
+  (= (::kohde/id kohde) (-> @aktiivinen-nakyma :tila :avattu-toimenpide ::kanavan-toimenpide/kohde ::kohde/id)))
 
 (defn- kohde-on-gridissa? [kohde toimenpiteet]
   (some #(= (::kohde/id kohde) (-> % ::kanavan-toimenpide/kohde ::kohde/id))
@@ -27,26 +35,27 @@
 
 (defonce naytettavat-kanavakohteet
   (reaction
-    (let [{:keys [toimenpiteet avattu-toimenpide nakymassa?]} @kokonaishintaiset/tila
+    (let [{:keys [toimenpiteet avattu-toimenpide nakymassa?]} (:tila @aktiivinen-nakyma)
           lomakkeella? (boolean avattu-toimenpide)
           kokonaishintaiset-nakymassa? nakymassa?
           ;; Yhdistetään kohteen ja kohteelle tehdyn toimenpiteen tiedot. Toimenpiteen tietoja näytetään kartan
           ;; infopaneelissa.
-          kohteet (map (fn [kohde]
-                         (let [kohteen-toimenpiteet (some #(when (= (-> % ::kanavan-toimenpide/kohde ::kohde/id) (::kohde/id kohde))
-                                                             {:huoltokohde (-> % ::kanavan-toimenpide/huoltokohde ::kanavan-huoltokohde/nimi)
-                                                              :kohteenosan-tyyppi (when-let [tyyppi (-> % ::kanavan-toimenpide/kohteenosa ::kohteenosa/tyyppi)]
-                                                                                    (name tyyppi))
-                                                              :kuittaaja (str (-> % ::kanavan-toimenpide/kuittaaja ::kayttaja/etunimi) " "
-                                                                              (-> % ::kanavan-toimenpide/kuittaaja ::kayttaja/sukunimi))
-                                                              :lisatieto (::kanavan-toimenpide/lisatieto %)
-                                                              :muu-toimenpide (::kanavan-toimenpide/muu-toimenpide %)
-                                                              :pvm (pvm/pvm (::kanavan-toimenpide/pvm %))
-                                                              :suorittaja (::kanavan-toimenpide/suorittaja %)
-                                                              :toimenpide (-> % ::kanavan-toimenpide/toimenpidekoodi ::toimenpidekoodi/nimi)})
-                                                          toimenpiteet)]
-                           (assoc kohde :toimenpiteet kohteen-toimenpiteet)))
-                       @kanavaurakka/kanavakohteet)]
+          kohteet (flatten (map (fn [kohde]
+                                  (let [kohteen-toimenpiteet (keep #(when (= (-> % ::kanavan-toimenpide/kohde ::kohde/id) (::kohde/id kohde))
+                                                                      {:huoltokohde (-> % ::kanavan-toimenpide/huoltokohde ::kanavan-huoltokohde/nimi)
+                                                                       :kohteenosan-tyyppi (when-let [tyyppi (-> % ::kanavan-toimenpide/kohteenosa ::kohteenosa/tyyppi)]
+                                                                                             (name tyyppi))
+                                                                       :kuittaaja (str (-> % ::kanavan-toimenpide/kuittaaja ::kayttaja/etunimi) " "
+                                                                                       (-> % ::kanavan-toimenpide/kuittaaja ::kayttaja/sukunimi))
+                                                                       :lisatieto (::kanavan-toimenpide/lisatieto %)
+                                                                       :muu-toimenpide (::kanavan-toimenpide/muu-toimenpide %)
+                                                                       :pvm (pvm/pvm (::kanavan-toimenpide/pvm %))
+                                                                       :suorittaja (::kanavan-toimenpide/suorittaja %)
+                                                                       :toimenpide (-> % ::kanavan-toimenpide/toimenpidekoodi ::toimenpidekoodi/nimi)})
+                                                                   toimenpiteet)]
+                                    (map #(assoc kohde :toimenpiteet %)
+                                         kohteen-toimenpiteet)))
+                                @kanavaurakka/kanavakohteet))]
       (reduce (fn [kasitellyt kasiteltava]
                 (cond
                   ;; Jos ollaan lomakkeella, näytetään kaikki kohteet
