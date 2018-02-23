8429a4ae241c47c4d2254b0d4fbf7a5bf231e3a3
mikkoro <mikko.ronkkomaki@gmail.com>
2018-02-23 14:49:42 +0200

    Testaa paikkaustoteumien kirjaus

--- test/clj/harja/palvelin/integraatiot/api/paikkaukset_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/paikkaukset_test.clj
@@ -19,11 +19,8 @@
                        (api-paikkaukset/->Paikkaukset)
                        [:http-palvelin :db :pois-kytketyt-ominaisuudet :integraatioloki])))
 
-
-
 (use-fixtures :once (compose-fixtures tietokanta-fixture
                                       jarjestelma-fixture))
-
 
 (deftest kirjaa-paikkaus
   (let [db (luo-testitietokanta)
@@ -33,90 +30,74 @@
         json (->
                (slurp "test/resurssit/api/paikkauksen-kirjaus.json")
                (.replace "<PAIKKAUSTUNNISTE>" (str paikkaustunniste))
-               (.replace "<KOHDETUNNISTE>" (str kohdetunniste)))]
-    (let [vastaus (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/paikkaus"] kayttaja portti json)
-          haettu-tielupa (first (paikkaus-q/hae-paikkaukset db {::paikkaus/ulkoinen-id paikkaustunniste}))
-          odotettu {::paikkaus/id 2,
-                    ::paikkaus/alkuaika #inst"2018-01-30T12:00:00.000000000-00:00",
-                    ::paikkaus/tyomenetelma "massapintaus",
-                    ::paikkaus/paikkauskohde-id 2,
-                    ::paikkaus/materiaalit [{::paikkaus/esiintyma "testi",
-                                             ::paikkaus/kuulamylly-arvo "testi",
-                                             ::paikkaus/muotoarvo "testi",
-                                             ::paikkaus/lisa-aineet "lisäaineet",
-                                             ::paikkaus/pitoisuus 1.2M,
-                                             ::paikkaus/sideainetyyppi "20/30"}],
-                    ::paikkaus/raekoko 1,
-                    ::paikkaus/ulkoinen-id 3453455,
-                    ::paikkaus/leveys 10M,
-                    ::paikkaus/urakka-id 4,
-                    ::paikkaus/tierekisteriosoite {::tierekisteri/aet 1,
-                                                   ::tierekisteri/let 16,
-                                                   ::tierekisteri/tie 20,
-                                                   ::tierekisteri/aosa 1,
-                                                   ::tierekisteri/losa 5},
-                    ::paikkaus/massatyyppi "asfalttibetoni",
-                    ::paikkaus/tienkohdat [{::paikkaus/ajourat [2 3],
-                                            ::paikkaus/ajorata 1,
-                                            ::paikkaus/keskisaumat [1 1],
-                                            ::paikkaus/ajouravalit [5 7],
-                                            ::paikkaus/reunat [1]}],
-                    ::paikkaus/kuulamylly "AN5",
-                    ::paikkaus/paikkauskohde {::paikkaus/ulkoinen-id 1231234,
-                                              ::paikkaus/nimi "Testipaikkauskohde"},
-                    ::paikkaus/loppuaika #inst"2018-01-30T18:00:00.000000000-00:00",
-                    ::paikkaus/massamenekki 12}]
-      (is (= 200 (:status vastaus)) "Tietueen lisäys onnistui")
-      (is (.contains (:body vastaus) "Paikkaukset kirjattu onnistuneesti"))
-      (is (= odotettu haettu-tielupa)))))
+               (.replace "<KOHDETUNNISTE>" (str kohdetunniste)))
+        vastaus (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/paikkaus"] kayttaja portti json)
+        odotettu-paikkaus {                           ::paikkaus/tyomenetelma "massapintaus"
+                           ::paikkaus/paikkauskohde-id 2
+                           ::paikkaus/materiaalit [{::paikkaus/esiintyma "testi"
+                                                    ::paikkaus/kuulamylly-arvo "testi"
+                                                    ::paikkaus/muotoarvo "testi"
+                                                    ::paikkaus/lisa-aineet "lisäaineet"
+                                                    ::paikkaus/pitoisuus 1.2M
+                                                    ::paikkaus/sideainetyyppi "20/30"}]
+                           ::paikkaus/raekoko 1
+                           ::paikkaus/ulkoinen-id 3453455
+                           ::paikkaus/leveys 10M
+                           ::paikkaus/urakka-id 4
+                           ::paikkaus/tierekisteriosoite {::tierekisteri/aet 1
+                                                          ::tierekisteri/let 16
+                                                          ::tierekisteri/tie 20
+                                                          ::tierekisteri/aosa 1
+                                                          ::tierekisteri/losa 5}
+                           ::paikkaus/massatyyppi "asfalttibetoni"
+                           ::paikkaus/tienkohdat [{::paikkaus/ajourat [2 3]
+                                                   ::paikkaus/ajorata 1
+                                                   ::paikkaus/keskisaumat [1 1]
+                                                   ::paikkaus/ajouravalit [5 7]
+                                                   ::paikkaus/reunat [1]}]
+                           ::paikkaus/kuulamylly "AN5"
+                           ::paikkaus/paikkauskohde {::paikkaus/ulkoinen-id 1231234
+                                                     ::paikkaus/nimi "Testipaikkauskohde"}
 
-(deftest kirjaa-paikkaustoteuma
+                           ::paikkaus/massamenekki 12}
+        odotettu-kohde {::paikkaus/id 2
+                        ::paikkaus/nimi "Testipaikkauskohde"
+                        ::paikkaus/ulkoinen-id 1231234}]
+    (is (= 200 (:status vastaus)) "Tietueen lisäys onnistui")
+    (is (.contains (:body vastaus) "Paikkaukset kirjattu onnistuneesti"))
+    (is (= odotettu-paikkaus (dissoc (first (paikkaus-q/hae-paikkaukset db {::paikkaus/ulkoinen-id paikkaustunniste}))
+                                     ::paikkaus/id
+                                     ::paikkaus/loppuaika
+                                     ::paikkaus/alkuaika)))
+    (is (= odotettu-kohde (first (paikkaus-q/hae-paikkauskohteet db {::paikkaus/ulkoinen-id kohdetunniste}))))))
+
+(deftest kirjaa-paikkauskustannu
   (let [db (luo-testitietokanta)
         urakka (hae-oulun-alueurakan-2014-2019-id)
-        paikkaustunniste 3453455
-        kohdetunniste 1231234
+        toteumatunniste 234531
+        kohdetunniste 466645
         json (->
-               (slurp "test/resurssit/api/paikkauksen-kirjaus.json")
-               (.replace "<PAIKKAUSTUNNISTE>" (str paikkaustunniste))
-               (.replace "<KOHDETUNNISTE>" (str kohdetunniste)))]
-    (let [vastaus (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/paikkaus"] kayttaja portti json)
-          haettu-tielupa (first (paikkaus-q/hae-paikkaukset db {::paikkaus/ulkoinen-id paikkaustunniste}))
-          odotettu-paikkaus {::paikkaus/id 2,
-                             ::paikkaus/alkuaika #inst"2018-01-30T12:00:00.000000000-00:00",
-                             ::paikkaus/tyomenetelma "massapintaus",
-                             ::paikkaus/paikkauskohde-id 2,
-                             ::paikkaus/materiaalit [{::paikkaus/esiintyma "testi",
-                                                      ::paikkaus/kuulamylly-arvo "testi",
-                                                      ::paikkaus/muotoarvo "testi",
-                                                      ::paikkaus/lisa-aineet "lisäaineet",
-                                                      ::paikkaus/pitoisuus 1.2M,
-                                                      ::paikkaus/sideainetyyppi "20/30"}],
-                             ::paikkaus/raekoko 1,
-                             ::paikkaus/ulkoinen-id 3453455,
-                             ::paikkaus/leveys 10M,
-                             ::paikkaus/urakka-id 4,
-                             ::paikkaus/tierekisteriosoite {::tierekisteri/aet 1,
-                                                            ::tierekisteri/let 16,
-                                                            ::tierekisteri/tie 20,
-                                                            ::tierekisteri/aosa 1,
-                                                            ::tierekisteri/losa 5},
-                             ::paikkaus/massatyyppi "asfalttibetoni",
-                             ::paikkaus/tienkohdat [{::paikkaus/ajourat [2 3],
-                                                     ::paikkaus/ajorata 1,
-                                                     ::paikkaus/keskisaumat [1 1],
-                                                     ::paikkaus/ajouravalit [5 7],
-                                                     ::paikkaus/reunat [1]}],
-                             ::paikkaus/kuulamylly "AN5",
-                             ::paikkaus/paikkauskohde {::paikkaus/ulkoinen-id 1231234,
-                                                       ::paikkaus/nimi "Testipaikkauskohde"},
-                             ::paikkaus/loppuaika #inst"2018-01-30T18:00:00.000000000-00:00",
-                             ::paikkaus/massamenekki 12}
-          odotettu-kohde {:harja.domain.paikkaus/id 2,
-                          :harja.domain.paikkaus/nimi "Testipaikkauskohde",
-                          :harja.domain.paikkaus/ulkoinen-id 1231234}]
-      (is (= 200 (:status vastaus)) "Tietueen lisäys onnistui")
-      (is (.contains (:body vastaus) "Paikkaukset kirjattu onnistuneesti"))
-      (is (= odotettu-paikkaus haettu-tielupa))
-      (is (= odotettu-kohde (first (paikkaus-q/hae-paikkauskohteet db {::paikkaus/ulkoinen-id kohdetunniste})))))))
+               (slurp "test/resurssit/api/paikkaustoteuman-kirjaus.json")
+               (.replace "<TOTEUMATUNNISTE>" (str toteumatunniste))
+               (.replace "<KOHDETUNNISTE>" (str kohdetunniste)))
+        vastaus (api-tyokalut/post-kutsu ["/api/urakat/" urakka "/paikkaus/kustannus"] kayttaja portti json)
+        odotetut-paikkaustoteumat [{:harja.domain.paikkaus/kirjattu #inst "2018-01-30T12:00:00.000000000-00:00"
+                                    :harja.domain.paikkaus/paikkauskohde-id 3
+                                    :harja.domain.paikkaus/selite "asfaltti"
+                                    :harja.domain.paikkaus/tyyppi "kokonaishintainen"
+                                    :harja.domain.paikkaus/ulkoinen-id 234531
+                                    :harja.domain.paikkaus/urakka-id 4}
+                                   {:harja.domain.paikkaus/kirjattu #inst "2018-01-30T12:00:00.000000000-00:00"
+                                    :harja.domain.paikkaus/paikkauskohde-id 3
+                                    :harja.domain.paikkaus/selite "Liikennejärjestelyt"
+                                    :harja.domain.paikkaus/tyyppi "yksikkohintainen"
+                                    :harja.domain.paikkaus/ulkoinen-id 234531
+                                    :harja.domain.paikkaus/urakka-id 4}]
+        odotettu-kohde {:harja.domain.paikkaus/nimi "Testipaikkauskohde"
+                        :harja.domain.paikkaus/ulkoinen-id 466645}]
 
+    (is (= 200 (:status vastaus)) "Tietueen lisäys onnistui")
+    (is (.contains (:body vastaus) "Paikkauskustannukset kirjattu onnistuneesti"))
+    (is (= odotetut-paikkaustoteumat (paikkaus-q/hae-paikkaustoteumat db {::paikkaus/ulkoinen-id toteumatunniste})))
+    (is (= odotettu-kohde (first (paikkaus-q/hae-paikkauskohteet db {::paikkaus/ulkoinen-id kohdetunniste}))))))
 
