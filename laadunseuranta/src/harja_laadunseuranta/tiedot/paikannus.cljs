26975146bc7243d08ea96e74e02af995b52fb754
Jari Hanhela <jarihanhela@gmail.com>
2018-02-27 19:29:35 +0200

    Käytä watchPositionia sijainnin määritykseen

--- laadunseuranta/src/harja_laadunseuranta/tiedot/paikannus.cljs
+++ laadunseuranta/src/harja_laadunseuranta/tiedot/paikannus.cljs
@@ -8,9 +8,9 @@
             [harja-laadunseuranta.utils :refer [timestamp ipad?]]
             [harja-laadunseuranta.tiedot.projektiot :as projektiot]
             [harja-laadunseuranta.tiedot.ilmoitukset :as ilmoitukset]
-            [harja-laadunseuranta.tiedot.sovellus :as s]))
+            [harja-laadunseuranta.tiedot.sovellus :as s]
+            [harja-laadunseuranta.tiedot.reitintallennus :as reitintallennus]))
 
-(def +paikan-raportointivali+ 2000) ; ms
 (def +max-maara-alustuksen-paikannnusyrityksia+ 10)
 
 (defn- geolocation-api []
@@ -41,8 +41,8 @@
      :speed (or (.-speed coords) 0)}))
 
 (def paikannusoptiot #js {:enableHighAccuracy true
-                          :maximumAge 1000
-                          :timeout +paikan-raportointivali+})
+                          :maximumAge 0
+                          :timeout js/Infinity})
 
 (defn paivita-sijainti [{:keys [nykyinen]} sijainti ts]
   (let [uusi-sijainti (assoc sijainti :timestamp ts)
@@ -62,36 +62,44 @@
            ensimmainen-sijainti-yritys-atom]}]
   (when (geolokaatio-tuettu?)
     (.log js/console "Paikannus käynnistetään")
-    (js/setInterval
-      (fn []
-        (let [sijainti-saatu (fn [sijainti]
-                               (ilmoitukset/ilmoita (str "Sijainti saatu, tarkkuus: " (-> sijainti .-coords .-accuracy)) s/ilmoitus {:tyyppi :onnistui})
-                               (when (and ensimmainen-sijainti-saatu-atom
-                                          (not @ensimmainen-sijainti-saatu-atom))
-                                 (reset! ensimmainen-sijainti-saatu-atom true))
+    (let [sijainti-saatu (fn [sijainti]
+                           (let [tarkkuus (-> sijainti .-coords .-accuracy)
+                                 riittavan-tarkka? (reitintallennus/nykyinen-sijainti-riittavan-tarkka?
+                                                     tarkkuus
+                                                     asetukset/+suurin-sallittu-tarkkuus+)]
+
+                             (ilmoitukset/ilmoita (str "Sijainti saatu, tarkkuus: " (-> sijainti .-coords .-accuracy))
+                                                  s/ilmoitus {:tyyppi :onnistui})
+
+                             (when (and ensimmainen-sijainti-saatu-atom
+                                        (not @ensimmainen-sijainti-saatu-atom))
+                               (reset! ensimmainen-sijainti-saatu-atom true))
+
+                             (if riittavan-tarkka?
                                (swap! sijainti-atom (fn [entinen]
                                                       (paivita-sijainti entinen
                                                                         (konvertoi-latlon sijainti)
-                                                                        (timestamp)))))
-              sijainti-epaonnistui (fn [virhe]
-                                     (ilmoitukset/ilmoita (str "Paikannus epäonnistui! " (.-message virhe)) s/ilmoitus {:tyyppi :virhe})
-                                     (when (and ensimmainen-sijainti-saatu-atom
-                                                ensimmainen-sijainti-yritys-atom
-                                                (not @ensimmainen-sijainti-saatu-atom))
-                                       (swap! ensimmainen-sijainti-yritys-atom inc))
-                                     (when (and ensimmainen-sijainti-saatu-atom
-                                                ensimmainen-sijainti-virhekoodi-atom
-                                                (>= @ensimmainen-sijainti-yritys-atom +max-maara-alustuksen-paikannnusyrityksia+)
-                                                (not @ensimmainen-sijainti-saatu-atom))
-                                       (.log js/console "Paikannus epäonnistui: " (.-message virhe))
-                                       (reset! ensimmainen-sijainti-virhekoodi-atom (.-code virhe))
-                                       (reset! ensimmainen-sijainti-saatu-atom false))
-                                     (swap! sijainti-atom identity))]
-          (.getCurrentPosition (geolocation-api)
-                               sijainti-saatu
-                               sijainti-epaonnistui
-                               paikannusoptiot)))
-      +paikan-raportointivali+)))
+                                                                        (timestamp))))
+                               (ilmoitukset/ilmoita (str "Paikannus epäonnistui: sijainti epätarkka (" tarkkuus "m)")
+                                                    s/ilmoitus {:tyyppi :virhe}))))
+          sijainti-epaonnistui (fn [virhe]
+                                 (ilmoitukset/ilmoita (str "Paikannus epäonnistui: " (.-message virhe)) s/ilmoitus {:tyyppi :virhe})
+                                 (when (and ensimmainen-sijainti-saatu-atom
+                                            ensimmainen-sijainti-yritys-atom
+                                            (not @ensimmainen-sijainti-saatu-atom))
+                                   (swap! ensimmainen-sijainti-yritys-atom inc))
+                                 (when (and ensimmainen-sijainti-saatu-atom
+                                            ensimmainen-sijainti-virhekoodi-atom
+                                            (>= @ensimmainen-sijainti-yritys-atom +max-maara-alustuksen-paikannnusyrityksia+)
+                                            (not @ensimmainen-sijainti-saatu-atom))
+                                   (.log js/console "Paikannus epäonnistui: " (.-message virhe))
+                                   (reset! ensimmainen-sijainti-virhekoodi-atom (.-code virhe))
+                                   (reset! ensimmainen-sijainti-saatu-atom false))
+                                 (swap! sijainti-atom identity))]
+      (.watchPosition (geolocation-api)
+                      sijainti-saatu
+                      sijainti-epaonnistui
+                      paikannusoptiot))))
 
 (defn aseta-testisijainti
   "HUOM: testikäyttöön. Asettaa nykyisen sijainnin koordinaatit. Oikean geolocation pollerin tulisi
