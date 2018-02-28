324f1852e9ea5d3d3ce3e0643b5057e603c433e6
Joonas Mikkonen <joonas.mikkonen@solita.fi>
2018-02-28 10:51:15 +0200

    Lisää backille tallennuksen käsittelyä

--- src/clj/harja/palvelin/palvelut/yllapitokohteet/muut_kohdeosat.clj
+++ src/clj/harja/palvelin/palvelut/yllapitokohteet/muut_kohdeosat.clj
@@ -4,9 +4,43 @@
             [taoensso.timbre :as log]
             [clojure.java.jdbc :as jdbc]
             [harja.palvelin.palvelut.yllapitokohteet.yleiset :as yy]
+            [harja.kyselyt.yllapitokohteet :as q]
             [harja.domain.oikeudet :as oikeudet]))
 
-(defn tallenna-muut-kohdeosat [db user tiedot])
+(defn vaadi-kohdeosa-ei-kuulu-yllapitokohteeseen
+  [db yllapitokohde-id kohdeosa])
+
+(defn tallenna-muut-kohdeosat [db user {:keys [urakka-id yllapitokohde-id muut-kohdeosat]}]
+  (oikeudet/vaadi-kirjoitusoikeus oikeudet/urakat-kohdeluettelo-paallystyskohteet user urakka-id)
+  (yy/vaadi-yllapitokohde-kuuluu-urakkaan db urakka-id yllapitokohde-id)
+
+  (doseq [kohdeosa muut-kohdeosat]
+    (vaadi-kohdeosa-ei-kuulu-yllapitokohteeseen db yllapitokohde-id kohdeosa))
+
+  (jdbc/with-db-transaction [db db]
+    ;; (yha-apurit/lukitse-urakan-yha-sidonta db urakka-id) <- Mikäs tää on?
+    ;; TODO:
+    ;; - Hae kannassa olevat kohteen muut osat
+    ;; - Vertaile tallennettavia osia olemassaoleviin, jotta tiedät mitä poistetaan ja mitä tallennetaan
+    ;; - Poista poistettavat osat
+    ;; - Tallenna tallennettavat osat
+    ;; - Päivitä päivitettävät osat
+    ;; - Palauta frontille jotain järkevää
+
+    (let [kannasta-loytyvat-osat (into #{}
+                                       (map :id (yy/hae-yllapitokohteen-muut-kohdeosat db yllapitokohde-id)))
+          poistettavat-osat (filter #(and (:poista %)
+                                          (kannasta-loytyvat-osat %))
+                                    muut-kohdeosat)
+          tallennettavat-osat (remove :poista muut-kohdeosat)]
+      (doseq [poistettava-osa poistettavat-osat]
+        (q/poista-yllapitokohdeosa {:id (:id poistettava-osa) :urakka urakka-id}))
+      (doseq [tallennettava-osa tallennettavat-osat]
+        (if (kannasta-loytyvat-osat (:id tallennettava-osa))
+          ;:nimi :tr_numero :tr_alkuosa :tr_alkuetaisyys :tr_loppuosa :tr_loppuetaisyys :tr_ajorata :tr_kaista :paallystetyyppi :raekoko :tyomenetelma :massamaara :toimenpide
+          (q/paivita-yllapitokohdeosa tallennettava-osa)
+          ;:yllapitokohde :nimi :tr_numero :tr_alkuosa :tr_alkuetaisyys :tr_loppuosa :tr_loppuetaisyys :tr_ajorata :tr_kaista :toimenpide :paallystetyyppi :raekoko :tyomenetelma :massamaara :ulkoinen-id
+          (q/luo-yllapitokohdeosa tallennettava-osa))))))
 
 (defn hae-yllapitokohteen-muut-kohdeosat [db user {:keys [urakka-id yllapitokohde-id]}]
   (oikeudet/vaadi-lukuoikeus oikeudet/urakat-kohdeluettelo-paallystyskohteet user urakka-id)
