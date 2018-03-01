56b25040b964b0572a1bc8935b3119170e86a196
Jari Hanhela <jarihanhela@gmail.com>
2018-03-01 12:55:47 +0200

    Lisää naivi yhteenveto

--- src/clj/harja/palvelin/raportointi/raportit/vastaanottotarkastus.clj
+++ src/clj/harja/palvelin/raportointi/raportit/vastaanottotarkastus.clj
@@ -19,87 +19,129 @@
 
 (defqueries "harja/palvelin/raportointi/raportit/vastaanottotarkastus.sql")
 
-(defn yllapitokohteet-taulukko [yllapitokohteet raportin-nimi taulukkotyyppi]
-  [:taulukko {:otsikko (case taulukkotyyppi
-                         :yha "YHA-kohteet"
-                         :paikkaus "Harjan paikkauskohteet ja muut kohteet")
+(defn yllapitokohteet-taulukko [yllapitokohteet taulukkotyyppi]
+  (let [nimi (case taulukkotyyppi
+               :yha "YHA-kohteet"
+               :paikkaus "Harjan paikkauskohteet ja muut kohteet")]
+    [:taulukko {:otsikko nimi
+                :tyhja (when (empty? yllapitokohteet) "Ei raportoitavaa.")
+                :sheet-nimi nimi}
+     (concat
+       [{:otsikko "Kohde\u00ADnumero" :leveys 5}
+        {:otsikko "Tunnus" :leveys 5}
+        {:otsikko "Nimi" :leveys 10}
+        {:otsikko "Tie\u00ADnumero" :leveys 5}
+        {:otsikko "Ajorata" :leveys 5}
+        {:otsikko "Kaista" :leveys 5}
+        {:otsikko "Aosa" :leveys 5}
+        {:otsikko "Aet" :leveys 5}
+        {:otsikko "Losa" :leveys 5}
+        {:otsikko "Let" :leveys 5}
+        {:otsikko "Pit. (m)" :leveys 5}
+        {:otsikko "KVL" :leveys 5}
+        {:otsikko "YP-lk" :leveys 5}]
+       (when (= taulukkotyyppi :yha)
+         [{:otsikko "Tarjous\u00ADhinta" :leveys 5 :fmt :raha}
+          {:otsikko "Määrä\u00ADmuu\u00ADtokset" :leveys 5 :fmt :raha}])
+       (when (= taulukkotyyppi :paikkaus)
+         [{:otsikko "Toteutunut hinta" :leveys 10 :fmt :raha}])
+       [{:otsikko "Arvon muu\u00ADtok\u00ADset" :leveys 5 :fmt :raha}
+        {:otsikko "Sakko/bonus" :leveys 5 :fmt :raha}
+        {:otsikko "Bitumi-indeksi" :leveys 5 :fmt :raha}
+        {:otsikko "Kaasu\u00ADindeksi" :leveys 5 :fmt :raha}
+        {:otsikko "Koko\u00ADnais\u00ADhinta (indek\u00ADsit mukana)" :leveys 5 :fmt :raha}])
+     (map
+       (fn [yllapitokohde]
+         (case taulukkotyyppi
+           :yha
+           [(:kohdenumero yllapitokohde)
+            (:tunnus yllapitokohde)
+            (:nimi yllapitokohde)
+            (:tr-numero yllapitokohde)
+            (:tr-ajorata yllapitokohde)
+            (:tr-kaista yllapitokohde)
+            (:tr-alkuosa yllapitokohde)
+            (:tr-alkuetaisyys yllapitokohde)
+            (:tr-loppuosa yllapitokohde)
+            (:tr-loppuetaisyys yllapitokohde)
+            (:pituus yllapitokohde)
+            (:kvl yllapitokohde)
+            (:yplk yllapitokohde)
+            (:sopimuksen-mukaiset-tyot yllapitokohde)
+            (:maaramuutokset yllapitokohde)
+            (:arvonmuutokset yllapitokohde)
+            (:sakot-ja-bonukset yllapitokohde)
+            (:bitumi-indeksi yllapitokohde)
+            (:kaasuindeksi yllapitokohde)
+            (:kokonaishinta yllapitokohde)]
+           :paikkaus
+           [(:kohdenumero yllapitokohde)
+            (:tunnus yllapitokohde)
+            (:nimi yllapitokohde)
+            (:tr-numero yllapitokohde)
+            (:tr-ajorata yllapitokohde)
+            (:tr-kaista yllapitokohde)
+            (:tr-alkuosa yllapitokohde)
+            (:tr-alkuetaisyys yllapitokohde)
+            (:tr-loppuosa yllapitokohde)
+            (:tr-loppuetaisyys yllapitokohde)
+            (:pituus yllapitokohde)
+            (:kvl yllapitokohde)
+            (:yplk yllapitokohde)
+            (:toteutunut-hinta yllapitokohde)
+            (:arvonmuutokset yllapitokohde)
+            (:sakot-ja-bonukset yllapitokohde)
+            (:bitumi-indeksi yllapitokohde)
+            (:kaasuindeksi yllapitokohde)
+            (:kokonaishinta yllapitokohde)]))
+       yllapitokohteet)]))
+
+(defn yllapitokohteet-yhteensa-taulukko [yllapitokohteet]
+  [:taulukko {:otsikko "Yhteenveto"
               :tyhja (when (empty? yllapitokohteet) "Ei raportoitavaa.")
-              :sheet-nimi raportin-nimi}
-   (concat
-     [{:otsikko "Kohde\u00ADnumero" :leveys 5}
-      {:otsikko "Tunnus" :leveys 5}
-      {:otsikko "Nimi" :leveys 10}
-      {:otsikko "Tie\u00ADnumero" :leveys 5}
-      {:otsikko "Ajorata" :leveys 5}
-      {:otsikko "Kaista" :leveys 5}
-      {:otsikko "Aosa" :leveys 5}
-      {:otsikko "Aet" :leveys 5}
-      {:otsikko "Losa" :leveys 5}
-      {:otsikko "Let" :leveys 5}
-      {:otsikko "Pit. (m)" :leveys 5}
-      {:otsikko "KVL" :leveys 5}
-      {:otsikko "YP-lk" :leveys 5}]
-     (when (= taulukkotyyppi :yha)
-       [{:otsikko "Tarjous\u00ADhinta" :leveys 5 :fmt :raha}
-        {:otsikko "Määrä\u00ADmuu\u00ADtokset" :leveys 5 :fmt :raha}])
-     (when (= taulukkotyyppi :paikkaus)
-       [{:otsikko "Toteutunut hinta" :leveys 10 :fmt :raha}])
-     [{:otsikko "Arvon muu\u00ADtok\u00ADset" :leveys 5 :fmt :raha}
-      {:otsikko "Sakko/bonus" :leveys 5 :fmt :raha}
-      {:otsikko "Bitumi-indeksi" :leveys 5 :fmt :raha}
-      {:otsikko "Kaasu\u00ADindeksi" :leveys 5 :fmt :raha}
-      {:otsikko "Koko\u00ADnais\u00ADhinta (indek\u00ADsit mukana)" :leveys 5 :fmt :raha}])
-   (map
-     (fn [yllapitokohde]
-       (case taulukkotyyppi
-         :yha
-         [(:kohdenumero yllapitokohde)
-          (:tunnus yllapitokohde)
-          (:nimi yllapitokohde)
-          (:tr-numero yllapitokohde)
-          (:tr-ajorata yllapitokohde)
-          (:tr-kaista yllapitokohde)
-          (:tr-alkuosa yllapitokohde)
-          (:tr-alkuetaisyys yllapitokohde)
-          (:tr-loppuosa yllapitokohde)
-          (:tr-loppuetaisyys yllapitokohde)
-          (:pituus yllapitokohde)
-          (:kvl yllapitokohde)
-          (:yplk yllapitokohde)
-          (:sopimuksen-mukaiset-tyot yllapitokohde)
-          (:maaramuutokset yllapitokohde)
-          (:arvonmuutokset yllapitokohde)
-          (:sakot-ja-bonukset yllapitokohde)
-          (:bitumi-indeksi yllapitokohde)
-          (:kaasuindeksi yllapitokohde)
-          (:kokonaishinta yllapitokohde)]
-         ;; TODO Yhteenvetorivi
-         :paikkaus
-         [(:kohdenumero yllapitokohde)
-          (:tunnus yllapitokohde)
-          (:nimi yllapitokohde)
-          (:tr-numero yllapitokohde)
-          (:tr-ajorata yllapitokohde)
-          (:tr-kaista yllapitokohde)
-          (:tr-alkuosa yllapitokohde)
-          (:tr-alkuetaisyys yllapitokohde)
-          (:tr-loppuosa yllapitokohde)
-          (:tr-loppuetaisyys yllapitokohde)
-          (:pituus yllapitokohde)
-          (:kvl yllapitokohde)
-          (:yplk yllapitokohde)
-          (:toteutunut-hinta yllapitokohde)
-          (:arvonmuutokset yllapitokohde)
-          (:sakot-ja-bonukset yllapitokohde)
-          (:bitumi-indeksi yllapitokohde)
-          (:kaasuindeksi yllapitokohde)
-          (:kokonaishinta yllapitokohde)]))
-     ;; TODO Yhteenvetorivi
-     yllapitokohteet)])
+              :sheet-nimi "Ylläpitokohteet yhteensä"}
+   [{:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 10}
+    {:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 5}
+    {:otsikko "" :leveys 5}
+    {:otsikko "Muut kustannukset" :leveys 10 :fmt :raha}
+    {:otsikko "Arvon muu\u00ADtok\u00ADset" :leveys 5 :fmt :raha}
+    {:otsikko "Sakko/bonus" :leveys 5 :fmt :raha}
+    {:otsikko "Bitumi-indeksi" :leveys 5 :fmt :raha}
+    {:otsikko "Kaasu\u00ADindeksi" :leveys 5 :fmt :raha}
+    {:otsikko "Koko\u00ADnais\u00ADhinta (indek\u00ADsit mukana)" :leveys 5 :fmt :raha}]
+   [[nil
+     nil
+     nil
+     nil
+     nil
+     nil
+     nil
+     nil
+     nil
+     nil
+     nil
+     nil
+     nil
+     0 ;; TODO LASKE!
+     (reduce + 0 (keep :arvonmuutokset yllapitokohteet))
+     (reduce + 0 (keep :sakot-ja-bonukset yllapitokohteet))
+     (reduce + 0 (keep :bitumi-indeksi yllapitokohteet))
+     (reduce + 0 (keep :kaasuindeksi yllapitokohteet))
+     (reduce + 0 (keep :kokonaishinta yllapitokohteet))]] ; TODO HUOMIO MUUT KUSTANNUKSET (korjaa taulukko urakasta)
+   ])
 
 (defn suorita [db user {:keys [urakka-id] :as tiedot}]
-  (let [konteksti :urakka
-        raportin-nimi "Vastaanottotarkastus"
+  (let [raportin-nimi "Vastaanottotarkastus"
         otsikko (str (:nimi (first (urakat-q/hae-urakka db urakka-id)))
                      ", " raportin-nimi ", suoritettu " (fmt/pvm (pvm/nyt)))
         yllapitokohteet (->> (into []
@@ -111,9 +153,10 @@
                              (yllapitokohteet-domain/jarjesta-yllapitokohteet))]
     [:raportti {:nimi raportin-nimi}
 
-     (yllapitokohteet-taulukko (filter :yhaid yllapitokohteet) raportin-nimi :yha)
+     (yllapitokohteet-taulukko (filter :yhaid yllapitokohteet) :yha)
      (when (some :maaramuutokset-ennustettu? yllapitokohteet) [:teksti "Taulukko sisältää ennustettuja määrämuutoksia."])
-     (yllapitokohteet-taulukko (filter (comp not :yhaid) yllapitokohteet) raportin-nimi :paikkaus)
+     (yllapitokohteet-taulukko (filter (comp not :yhaid) yllapitokohteet) :paikkaus)
+     (yllapitokohteet-yhteensa-taulukko yllapitokohteet)
 
      (mapcat (fn [[aja-parametri otsikko raportti-fn]]
                (concat [[:otsikko otsikko]]
