6cb42bdeb75dff03ded6f60f0b745a79edf652ff
Jari Hanhela <jarihanhela@gmail.com>
2018-03-05 21:38:22 +0200

    Lisää kyselyyn puuttuva parametri

--- src/clj/harja/palvelin/palvelut/valitavoitteet/valtakunnalliset_valitavoitteet.clj
+++ src/clj/harja/palvelin/palvelut/valitavoitteet/valtakunnalliset_valitavoitteet.clj
@@ -82,6 +82,7 @@
                                         :aloituspvm nil
                                         :takaraja (konv/sql-date takaraja)
                                         :nimi nimi
+                                        :yllapitokohde nil
                                         :valtakunnallinen_valitavoite valtakunnallinen-valitavoite-id
                                         :luoja (:id user)}))))
 
