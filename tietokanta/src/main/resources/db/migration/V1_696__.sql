bb810e04baf9efcc3c31f2a0924f423559d47d7f
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2018-03-05 15:51:01 +0200

    Poista buginen deleten käsittely kokonaan
    urakan deletointi on käytännössä UI:lta mahdotonta, ei lähdetä tässä
    cachen invalidoinnissa varautumaan siihen. Deletet aiheuttivat bugeja
    triggerit NEW-arvojen käsittelyssä koska NEW:tä ei enää ole olemassa.

--- tietokanta/src/main/resources/db/migration/V1_696__.sql
+++ tietokanta/src/main/resources/db/migration/V1_696__.sql
@@ -1,17 +1,11 @@
 -- Jos urakan käyttämä indeksi muuttuu, poistetaan kaikki laskutusyhteenvedot cachesta
 CREATE OR REPLACE FUNCTION poista_muistetut_laskutusyht_urakan_indeksi() RETURNS trigger AS $$
 BEGIN
-  -- Jos urakka poistetaan, tyhjennetään myös cachesta sen laskutusyhteenvedot. Ei tapahtune juuri koskaan
-
-  IF TG_OP = 'DELETE' THEN
-    DELETE FROM laskutusyhteenveto_cache WHERE urakka = NEW.id;
-    RETURN NEW;
-  ELSIF TG_OP = 'UPDATE' AND
-        (OLD.indeksi != NEW.indeksi
-          -- Postgres ei tue vertailuoperaattoria NULL arvoille, vertaillaan ne eksplisiittisesti
-         OR OLD.indeksi IS NULL AND NEW.indeksi IS NOT NULL
-         OR NEW.indeksi IS NULL AND OLD.indeksi IS NOT NULL)
-    THEN
+  IF (OLD.indeksi != NEW.indeksi
+      -- Postgres ei tue vertailuoperaattoria NULL arvoille, vertaillaan ne eksplisiittisesti
+      OR OLD.indeksi IS NULL AND NEW.indeksi IS NOT NULL
+      OR NEW.indeksi IS NULL AND OLD.indeksi IS NOT NULL)
+  THEN
     DELETE FROM laskutusyhteenveto_cache WHERE urakka = NEW.id;
     RETURN NULL;
   ELSE RETURN NULL;
@@ -20,7 +14,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER tg_poista_muistetut_laskutusyht_urakan_indeksi
-AFTER UPDATE OR DELETE
+AFTER UPDATE
   ON urakka
 FOR EACH ROW
 EXECUTE PROCEDURE poista_muistetut_laskutusyht_urakan_indeksi();
