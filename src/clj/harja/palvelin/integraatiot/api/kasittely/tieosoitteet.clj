d86bab2491146c9fd5fa2e8717166a6c9bc045ac
Jari Hanhela <jarihanhela@gmail.com>
2018-03-09 10:11:44 +0200

    Poista toisen haaran muutokset

--- src/clj/harja/palvelin/integraatiot/api/kasittely/tieosoitteet.clj
+++ src/clj/harja/palvelin/integraatiot/api/kasittely/tieosoitteet.clj
@@ -1,8 +1,7 @@
 (ns harja.palvelin.integraatiot.api.kasittely.tieosoitteet
   (:require [harja.palvelin.integraatiot.vkm.vkm-komponentti :as vkm]
             [harja.kyselyt.geometriapaivitykset :as q-geometriapaivitykset]
-            [harja.pvm :as pvm]
-            [taoensso.timbre :as log]))
+            [harja.pvm :as pvm]))
 
 (defn- sisaltaa-sijainnin? [osoitteet vkm-id]
   (some #(= vkm-id (:vkm-id %)) osoitteet))
@@ -21,13 +20,13 @@
             (dissoc a :vkm-id)))
         alkuperaiset))
 
-(defn muunna-yllapitokohteen-tieosoitteet-paivan-verkolle
-  "Muuntaa ylläpitokohteiden tiosoitteet annetun päivän verkolle käyttäen VKM-palvelua."
-  [vkm db kohteen-tienumero karttapvm {:keys [sijainti alikohteet] :as kohde}]
+(defn muunna-yllapitokohteen-tieosoitteet [vkm db kohteen-tienumero karttapvm {:keys [sijainti alikohteet] :as kohde}]
   (if karttapvm
     (let [paakohteen-vkm-id "paakohde"
           muunnettavat-alikohteet (map-indexed (fn [i {sijainti :sijainti :as alikohde}]
-                                                 (assoc alikohde :vkm-id (str "alikohde-" i)))
+                                                 (assoc alikohde
+                                                   :vkm-id (str "alikohde-" i)
+                                                   :sijainti (assoc sijainti :tie kohteen-tienumero)))
                                                alikohteet)
           muunnettavat-sijainnit (conj
                                    (map #(assoc (:sijainti %) :vkm-id (:vkm-id %)) muunnettavat-alikohteet)
--- src/clj/harja/palvelin/integraatiot/api/yllapitokohteet.clj
+++ src/clj/harja/palvelin/integraatiot/api/yllapitokohteet.clj
@@ -138,26 +138,22 @@
     (validointi/tarkista-urakka-ja-kayttaja db urakka-id kayttaja)
     (validointi/tarkista-yllapitokohde-kuuluu-urakkaan db urakka-id kohde-id)
     (validointi/tarkista-saako-kohteen-paivittaa db kohde-id)
-    ;; Muunnetaan kohde ja alikohteet tallennusmuotoon
-    (let [;; TODO Miksi haetaan kannasta? Miksei lueta payloadista?
-          kohteen-tienumero (:tr_numero (first (q-yllapitokohteet/hae-kohteen-tienumero db {:kohdeid kohde-id})))
+    (let [kohteen-tienumero (:tr_numero (first (q-yllapitokohteet/hae-kohteen-tienumero db {:kohdeid kohde-id})))
           kohde (-> (:yllapitokohde data)
                     (assoc :id kohde-id)
                     (assoc-in [:sijainti :tie] kohteen-tienumero))
-          muunnetut-alikohteet (mapv #(as-> (:alikohde %) alikohde
-                                            (assoc alikohde :ulkoinen-id (get-in alikohde [:tunniste :id]))
-                                            (assoc-in alikohde [:sijainti :numero]
-                                                      (or (get-in alikohde [:sijainti :numero])
-                                                          kohteen-tienumero)))
+         muunnettavat-alikohteet (mapv #(-> (:alikohde %)
+                                            (assoc :ulkoinen-id (get-in (:alikohde %) [:tunniste :id]))
+                                            (assoc-in [:sijainti :numero] kohteen-tienumero))
                                      (:alikohteet kohde))
-          muunnettu-kohde (assoc kohde :alikohteet muunnetut-alikohteet)
-          karttapvm (as-> (get-in muunnettu-kohde [:sijainti :karttapvm]) karttapvm
+         muunnettava-kohde (assoc kohde :alikohteet muunnettavat-alikohteet)
+         karttapvm (as-> (get-in muunnettava-kohde [:sijainti :karttapvm]) karttapvm
                           (when karttapvm (parametrit/pvm-aika karttapvm)))
-          kohde (tieosoitteet/muunna-yllapitokohteen-tieosoitteet-paivan-verkolle
-                  vkm db kohteen-tienumero karttapvm muunnettu-kohde)
+         kohde (tieosoitteet/muunna-yllapitokohteen-tieosoitteet vkm db kohteen-tienumero karttapvm muunnettava-kohde)
           kohteen-sijainti (:sijainti kohde)
           alikohteet (:alikohteet kohde)]
-      (validointi/tarkista-yllapitokohde-ja-alikohteet db kohde-id kohteen-tienumero kohteen-sijainti alikohteet)
+
+     (validointi/tarkista-paallystysilmoituksen-kohde-ja-alikohteet db kohde-id kohteen-tienumero kohteen-sijainti alikohteet)
       (jdbc/with-db-transaction [db db]
         (kasittely/paivita-kohde db kohde-id kohteen-sijainti)
         (kasittely/paivita-alikohteet db kohde alikohteet)
