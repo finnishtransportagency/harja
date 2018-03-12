3046682d78c8978b8325f96cf710d512a6c0a578
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2018-03-12 16:21:48 +0200

    Älä piirrä sijaintivalitsimen pinniä valittuna

--- src/cljs/harja/tiedot/sijaintivalitsin.cljs
+++ src/cljs/harja/tiedot/sijaintivalitsin.cljs
@@ -15,7 +15,7 @@
 (def sijainti-kartalla
   (reaction
     (when @karttataso-sijainti
-      (let [feature (maarittele-feature @valittu-sijainti true (kartta-ikonit/pinni-ikoni "musta"))]
+      (let [feature (maarittele-feature @valittu-sijainti false (kartta-ikonit/pinni-ikoni "musta"))]
         ;; Jos feature on nil, ei haluta näyttää mitään ja [{:alue nil}] aiheuttaa erroria luo-geometria funktiossa
         (when feature
           [{:alue feature}])))))
