29080a29c4355700e918181d2414f30c7409ccb8
Erno Kuusela <erno-laptop!erno>
2018-03-14 10:39:25 +0200

    korjaa päivämäärien formatointi

--- src/cljs/harja/views/vesivaylat/urakka/toimenpiteet/jaettu.cljs
+++ src/cljs/harja/views/vesivaylat/urakka/toimenpiteet/jaettu.cljs
@@ -68,7 +68,7 @@
     "Työluokka" (to/reimari-tyoluokka-fmt (::to/tyoluokka toimenpide))
     "Toimenpide" (to/reimari-toimenpidetyyppi-fmt (::to/toimenpide toimenpide))
     "Lisätyö?" (to/reimari-lisatyo-fmt (::to/reimari-lisatyo? toimenpide))
-    "Päivämäärä ja aika" (pvm/pvm-opt (::to/pvm toimenpide))
+    "Päivämäärä ja aika" (pvm/pvm-aika-opt (::to/pvm toimenpide))
     "Turvalaite" (get-in toimenpide [::to/turvalaite ::tu/nimi])
     "Henkilömäärä" (::to/reimari-henkilo-lkm toimenpide)
     "Lisätiedot" (::to/lisatieto toimenpide)]
@@ -84,11 +84,11 @@
            :jata-kaventamatta #{}
            :otsikot-samalla-rivilla #{}
            :tyhja-rivi-otsikon-jalkeen #{}}
-          "Ilmoittaja" (::vika/reimari-ilmoittaja vi)
+          "Ilmoittaja"  (::vika/reimari-ilmoittaja vi)
           "Luoja" (::vika/reimari-luoja vi)
-          "Kirjattu" (::vika/reimari-kirjattu vi)
-          "Muokkaaja" (::vika/reimari-muokkaaja vi)
-          "Muokattu" (::vika/reimari-muokattu vi)
+          "Kirjattu" (pvm/pvm-aika-opt (::vika/reimari-kirjattu vi))
+          "Muokkaaja"  (::vika/reimari-muokkaaja vi)
+          "Muokattu" (pvm/pvm-aika-opt (::vika/reimari-muokattu vi))
           "Lisätiedot" (::vika/reimari-lisatiedot vi)]))])
    [:footer.livi-grid-infolaatikko-footer
     [:h5 "Turvalaitteen komponentit"]
