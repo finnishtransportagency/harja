05360e77f8aaa6fb1ce267ea50408c39f2a809a0
Erno Kuusela <erno-laptop!erno>
2018-03-15 13:54:40 +0200

    Kierrä hardkoodattu tietokantaosoite testissä ja .harja-viittaukset asetuksissa

--- .circleci/back-sisus.sh
+++ .circleci/back-sisus.sh
@@ -9,4 +9,8 @@ cd harja
 git pull origin develop
 sed -i -e 's!jdbc:postgresql://localhost/!jdbc:postgresql://harjadb/!' src/clj/harja/kyselyt/specql_db.clj
 sed -i -e 's!:palvelin "localhost"!:palvelin "harjadb"!' asetukset.edn
+sed -i -e 's!"localhost"!"harjadb"!' test/clj/harja/testi.clj
+mkdir -p ../.harja
+echo aaaa > ../.harja/anti-csrf-token
+touch ../.harja/{google-static-maps-key,turi-salasana,ava-salasana,yha-salasana,labyrintti-salasana}
 lein test
