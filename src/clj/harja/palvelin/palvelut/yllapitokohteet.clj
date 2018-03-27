91c91e7f1d7fdf365eec65ccb49e808feff36867
mikkoro <mikko.ronkkomaki@gmail.com>
2018-03-27 10:43:53 +0300

    Validoi että muut kohdeosat eivät mene päällekkäin

--- src/clj/harja/palvelin/palvelut/yllapitokohteet.clj
+++ src/clj/harja/palvelin/palvelut/yllapitokohteet.clj
@@ -431,37 +431,39 @@
   (yy/tarkista-urakkatyypin-mukainen-kirjoitusoikeus db user urakka-id)
   (yy/vaadi-yllapitokohde-kuuluu-urakkaan db urakka-id yllapitokohde-id)
   (jdbc/with-db-transaction [db db]
-    (let [kohteen-tienumero (:tr-numero (first (q/hae-yllapitokohde db {:id yllapitokohde-id})))]
-      (yy/vaadi-etta-kohdeosat-eivat-mene-paallekkain db vuosi (filter #(not= (:tr-numero %) kohteen-tienumero) osat))
+    (let [kohteen-tienumero (:tr-numero (first (q/hae-yllapitokohde db {:id yllapitokohde-id})))
+          muut-kohteet (filter #(not= (:tr-numero %) kohteen-tienumero) osat)]
+      (if-let [paallekkaiset-kohdeosat (yy/vaadi-etta-kohdeosat-eivat-mene-paallekkain db vuosi muut-kohteet)]
+        {:validointivirheet paallekkaiset-kohdeosat}
 
-      (yha-apurit/lukitse-urakan-yha-sidonta db urakka-id)
-      (let [hae-kaikki-osat #(hae-yllapitokohteen-yllapitokohdeosat db user
-                                                                    {:urakka-id urakka-id
-                                                                     :sopimus-id sopimus-id
-                                                                     :yllapitokohde-id yllapitokohde-id})
-            kaikki-osat (hae-kaikki-osat)
-            tarkasteltavat-osat (case osatyyppi
-                                  :kohteen-omat-kohdeosat (filter #(= (:tr-numero %) kohteen-tienumero) kaikki-osat)
-                                  :kohteen-muut-kohdeosat (filter #(not= (:tr-numero %) kohteen-tienumero) kaikki-osat)
-                                  kaikki-osat)
-            vanhat-osa-idt (into #{} (map :id) tarkasteltavat-osat)
-            uudet-osa-idt (into #{} (keep :id) osat)
-            poistuneet-osa-idt (set/difference vanhat-osa-idt uudet-osa-idt)]
+        (do
+          (yha-apurit/lukitse-urakan-yha-sidonta db urakka-id)
+          (let [hae-kaikki-osat #(hae-yllapitokohteen-yllapitokohdeosat db user
+                                                                        {:urakka-id urakka-id
+                                                                         :sopimus-id sopimus-id
+                                                                         :yllapitokohde-id yllapitokohde-id})
+                kaikki-osat (hae-kaikki-osat)
+                tarkasteltavat-osat (case osatyyppi
+                                      :kohteen-omat-kohdeosat (filter #(= (:tr-numero %) kohteen-tienumero) kaikki-osat)
+                                      :kohteen-muut-kohdeosat (filter #(not= (:tr-numero %) kohteen-tienumero) kaikki-osat)
+                                      kaikki-osat)
+                vanhat-osa-idt (into #{} (map :id) tarkasteltavat-osat)
+                uudet-osa-idt (into #{} (keep :id) osat)
+                poistuneet-osa-idt (set/difference vanhat-osa-idt uudet-osa-idt)]
 
-        (doseq [id poistuneet-osa-idt]
-          (q/poista-yllapitokohdeosa! db {:urakka urakka-id
-                                          :id id}))
+            (doseq [id poistuneet-osa-idt]
+              (q/poista-yllapitokohdeosa! db {:urakka urakka-id
+                                              :id id}))
 
-        (log/debug "Tallennetaan ylläpitokohdeosat: " (pr-str osat) " Ylläpitokohde-id: " yllapitokohde-id)
-        (doseq [osa osat]
-          (if (id-olemassa? (:id osa))
-            (paivita-yllapitokohdeosa db user urakka-id osa)
-            (luo-uusi-yllapitokohdeosa db user yllapitokohde-id osa)))
-        (yy/paivita-yllapitourakan-geometria db urakka-id)
-        (let [yllapitokohdeosat (hae-kaikki-osat)]
-          (log/debug "Tallennus suoritettu. Uudet ylläpitokohdeosat: " (pr-str yllapitokohdeosat))
-          (tr-domain/jarjesta-tiet yllapitokohdeosat)
-          #_{:validointivirheet ["TESTI VIRHE!" "JA TOINENKI!"]})))))
+            (log/debug "Tallennetaan ylläpitokohdeosat: " (pr-str osat) " Ylläpitokohde-id: " yllapitokohde-id)
+            (doseq [osa osat]
+              (if (id-olemassa? (:id osa))
+                (paivita-yllapitokohdeosa db user urakka-id osa)
+                (luo-uusi-yllapitokohdeosa db user yllapitokohde-id osa)))
+            (yy/paivita-yllapitourakan-geometria db urakka-id)
+            (let [yllapitokohdeosat (hae-kaikki-osat)]
+              (log/debug "Tallennus suoritettu. Uudet ylläpitokohdeosat: " (pr-str yllapitokohdeosat))
+              (tr-domain/jarjesta-tiet yllapitokohdeosat))))))))
 
 (defn- luo-uusi-yllapitokohde [db user urakka-id sopimus-id vuosi
                                {:keys [kohdenumero nimi
