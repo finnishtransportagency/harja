648ff11cf176f37670213dfe0fe88622f737e6b8
Joonas Mikkonen <joonas.mikkonen@solita.fi>
2018-03-27 10:21:00 +0300

    Aloittele kustannusnäkymän tekoa. Lisätään siirtymänappi

--- src/cljs/harja/tiedot/urakka/paikkaukset_kustannukset.cljs
+++ src/cljs/harja/tiedot/urakka/paikkaukset_kustannukset.cljs
@@ -1,4 +1,146 @@
 (ns harja.tiedot.urakka.paikkaukset-kustannukset
-  (:require [reagent.core :refer [atom] :as r]))
+  (:require [reagent.core :refer [atom] :as r]
+            [tuck.core :as tuck]
+            [harja.pvm :as pvm]
+            [harja.tiedot.navigaatio :as nav]
+            [harja.tyokalut.tuck :as tt]
+            [harja.ui.viesti :as viesti]
+            [harja.ui.grid :as grid]
+            [harja.ui.napit :as napit]
+            [harja.domain.paikkaus :as paikkaus]
+            [harja.domain.tierekisteri :as tierekisteri])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(def app (atom nil))
+(def app (atom {:paikkauksien-haku-kaynnissa? false
+                :valinnat nil}))
+
+(defn kiinnostavat-tiedot-grid [{tierekisteriosoite ::paikkaus/tierekisteriosoite paikkauskohde ::paikkaus/paikkauskohde
+                                 :as paikkaus}]
+  (let [sellaisenaan-naytettavat-arvot (select-keys paikkaus #{::paikkaus/tyomenetelma
+                                                               ::paikkaus/massatyyppi ::paikkaus/leveys ::paikkaus/massamenekki
+                                                               ::paikkaus/raekoko ::paikkaus/kuulamylly ::paikkaus/id})
+        alkuaika {::paikkaus/alkuaika (pvm/pvm (::paikkaus/alkuaika paikkaus))}
+        loppuaika {::paikkaus/loppuaika (pvm/pvm (::paikkaus/loppuaika paikkaus))}
+        paikkauskohde (select-keys paikkauskohde #{::paikkaus/nimi ::paikkaus/paikkauskohde-id})]
+    (merge sellaisenaan-naytettavat-arvot alkuaika loppuaika tierekisteriosoite paikkauskohde)))
+
+;; Muokkaukset
+(defrecord PaikkausValittu [paikkauskohde valittu? otsikko-fn])
+(defrecord PaivitaValinnat [uudet otsikko-fn])
+(defrecord Nakymaan [otsikko-fn])
+(defrecord NakymastaPois [])
+(defrecord SiirryToimenpiteisiin [paikkaus-id])
+;; Haut
+(defrecord HaePaikkaukset [otsikko-fn])
+(defrecord EnsimmainenHaku [tulos otsikko-fn])
+(defrecord PaikkauksetHaettu [tulos otsikko-fn])
+(defrecord PaikkauksetEiHaettu [])
+(defrecord HaePaikkauksetKutsuLahetetty [])
+
+(defn kasittele-haettu-tulos
+  [tulos otsikko-fn]
+  (let [kiinnostavat-tiedot (map #(kiinnostavat-tiedot-grid %)
+                                 tulos)
+        paikkaukset-grid (mapcat (fn [[otsikko paikkaukset]]
+                                   (cons (grid/otsikko otsikko {:otsikkokomponentit (otsikko-fn (::paikkaus/paikkauskohde-id (first paikkaukset)))}) paikkaukset))
+                                 (group-by ::paikkaus/nimi kiinnostavat-tiedot))
+        paikkauket-vetolaatikko (map #(select-keys % [::paikkaus/tienkohdat ::paikkaus/materiaalit ::paikkaus/id])
+                                     tulos)]
+    {:paikkaukset-grid paikkaukset-grid
+     :paikkauket-vetolaatikko paikkauket-vetolaatikko}))
+
+(defn valinta-wrap [e! app polku otsikko-fn]
+  (r/wrap (get-in app [:valinnat polku])
+          (fn [u]
+            (e! (->PaivitaValinnat {polku u} otsikko-fn)))))
+
+(def valintojen-avaimet [:tr :aikavali :urakan-paikkauskohteet])
+
+(extend-protocol tuck/Event
+  PaivitaValinnat
+  (process-event [{u :uudet otsikko-fn :otsikko-fn} app]
+    (let [uudet-valinnat (merge (:valinnat app)
+                                (select-keys u valintojen-avaimet))
+          haku (tuck/send-async! ->HaePaikkaukset otsikko-fn)]
+      (go (haku uudet-valinnat))
+      (assoc app :valinnat uudet-valinnat)))
+  PaikkausValittu
+  (process-event [{{:keys [id]} :paikkauskohde valittu? :valittu? otsikko-fn :otsikko-fn} app]
+    (let [uudet-paikkausvalinnat (map #(if (= (:id %) id)
+                                         (assoc % :valittu? valittu?)
+                                         %)
+                                      (get-in app [:valinnat :urakan-paikkauskohteet]))]
+      (tuck/process-event (->PaivitaValinnat {:urakan-paikkauskohteet uudet-paikkausvalinnat} otsikko-fn) app)
+      (assoc-in app [:valinnat :urakan-paikkauskohteet] uudet-paikkausvalinnat)))
+  HaePaikkaukset
+  (process-event [{otsikko-fn :otsikko-fn} app]
+    (if-not (:paikkauksien-haku-kaynnissa? app)
+      (let [paikkauksien-idt (into #{} (keep #(when (:valittu? %)
+                                                (:id %))
+                                             (get-in app [:valinnat :urakan-paikkauskohteet])))
+            params (-> (:valinnat app)
+                       (dissoc :urakan-paikkauskohteet)
+                       (assoc :paikkaus-idt paikkauksien-idt)
+                       (assoc ::paikkaus/urakka-id @nav/valittu-urakka-id))]
+        (-> app
+            (tt/post! :hae-urakan-paikkauskohteet
+                      params
+                      ;; Checkbox-group ja aluksen nimen kirjoitus generoisi
+                      ;; liikaa requesteja ilman viivettä.
+                      {:viive 1000
+                       :tunniste :hae-paikkaukset-toteumat-nakymaan
+                       :lahetetty ->HaePaikkauksetKutsuLahetetty
+                       :onnistui ->PaikkauksetHaettu
+                       :onnistui-parametrit [otsikko-fn]
+                       :epaonnistui ->PaikkauksetEiHaettu})
+            (assoc :paikkauksien-haku-tulee-olemaan-kaynnissa? true)))
+      app))
+  HaePaikkauksetKutsuLahetetty
+  (process-event [_ app]
+    (assoc app :paikkauksien-haku-kaynnissa? true))
+  Nakymaan
+  (process-event [{otsikko-fn :otsikko-fn} app]
+    (-> app
+        (tt/post! :hae-urakan-paikkauskohteet
+                  {::paikkaus/urakka-id @nav/valittu-urakka-id}
+                  {:onnistui ->EnsimmainenHaku
+                   :onnistui-parametrit [otsikko-fn]
+                   :epaonnistui ->PaikkauksetEiHaettu})
+        (assoc :nakymassa? true
+               :paikkauksien-haku-kaynnissa? true)))
+  NakymastaPois
+  (process-event [_ app]
+    (assoc app :nakymassa? false))
+  EnsimmainenHaku
+  (process-event [{tulos :tulos otsikko-fn :otsikko-fn} app]
+    (let [paikkauskohteet (reduce (fn [paikkaukset paikkaus]
+                                    (if (some #(= (:id %) (get-in paikkaus [::paikkaus/paikkauskohde ::paikkaus/id]))
+                                              paikkaukset)
+                                      paikkaukset
+                                      (conj paikkaukset
+                                            {:id (get-in paikkaus [::paikkaus/paikkauskohde ::paikkaus/id])
+                                             :nimi (get-in paikkaus [::paikkaus/paikkauskohde ::paikkaus/nimi])
+                                             :valittu? true})))
+                                  [] tulos)
+          naytettavat-tiedot (kasittele-haettu-tulos tulos otsikko-fn)]
+      (-> app
+          (merge naytettavat-tiedot)
+          (assoc :paikkauksien-haku-kaynnissa? false)
+          (assoc-in [:valinnat :urakan-paikkauskohteet] paikkauskohteet))))
+  PaikkauksetHaettu
+  (process-event [{tulos :tulos otsikko-fn :otsikko-fn} app]
+    (let [naytettavat-tiedot (kasittele-haettu-tulos tulos otsikko-fn)]
+      (-> app
+          (merge naytettavat-tiedot)
+          (assoc :paikkauksien-haku-kaynnissa? false
+                 :paikkauksien-haku-tulee-olemaan-kaynnissa? false))))
+  PaikkauksetEiHaettu
+  (process-event [_ app]
+    (viesti/nayta! "Liikennetapahtumien haku epäonnistui! " :danger)
+    (assoc app
+           :paikkauksien-haku-kaynnissa? false
+           :paikkauksien-haku-tulee-olemaan-kaynnissa? false))
+  SiirryToimenpiteisiin
+  (process-event [{paikkaus-id :paikkaus-id} app]
+    (js/console.log "SIIRTYMÄ NAPPIA PAINETTU")
+    app))
--- src/cljs/harja/views/urakka/paikkaukset_kustannukset.cljs
+++ src/cljs/harja/views/urakka/paikkaukset_kustannukset.cljs
@@ -1,9 +1,185 @@
 (ns harja.views.urakka.paikkaukset-kustannukset
   (:require [tuck.core :as tuck]
-            [harja.tiedot.urakka.paikkaukset-kustannukset :as tiedot]))
+            [reagent.core :as r]
+            [harja.domain.paikkaus :as paikkaus]
+            [harja.domain.tierekisteri :as tierekisteri]
+            [harja.tiedot.urakka.paikkaukset-kustannukset :as tiedot]
+            [harja.views.kartta :as kartta]
+            [harja.views.urakka.yllapitokohteet :as yllapitokohteet]
+            [harja.ui.debug :as debug]
+            [harja.ui.grid :as grid]
+            [harja.ui.kentat :as kentat]
+            [harja.ui.komponentti :as komp]
+            [harja.ui.napit :as napit]
+            [harja.ui.valinnat :as valinnat]
+            [harja.ui.yleiset :as yleiset]))
+
+(defn otsikkokomponentti
+  [e! id]
+  [{:sijainti "80%"
+    :sisalto
+    (fn [_]
+      [napit/yleinen-ensisijainen
+       "Siirry toimenpiteisiin"
+       #(e! (tiedot/->SiirryToimenpiteisiin id))])}])
+
+(defn hakuehdot [e! {:keys [valinnat] :as app}]
+  (let [tr-atomi (partial tiedot/valinta-wrap e! app (partial otsikkokomponentti e!))
+        aikavali-atomi (partial tiedot/valinta-wrap e! app (partial otsikkokomponentti e!))]
+    [:span
+     [kentat/tee-otsikollinen-kentta
+      {:otsikko "Tierekisteriosoite"
+       :kentta-params {:tyyppi :tierekisteriosoite
+                       :tr-otsikot? false
+                       #_#_:validoi [(fn [osoite {sijainti :sijainti}]
+                                       (when (and (tr-osoite-taytetty? osoite)
+                                                  (nil? sijainti))
+                                         "Tarkista tierekisteriosoite"))]}
+       :arvo-atom (tr-atomi :tr)
+       :tyylit {:width "fit-content"}}]
+     [valinnat/aikavali (aikavali-atomi :aikavali)]
+     [:span.label-ja-kentta
+      [:span.kentan-otsikko "Paikkauskohteet"]
+      [:div.kentta
+       [yleiset/livi-pudotusvalikko
+        {:naytettava-arvo (let [valittujen-paikkauskohteiden-maara (count (filter :valittu? (:urakan-paikkauskohteet valinnat)))]
+                            (str valittujen-paikkauskohteiden-maara (if (= 1 valittujen-paikkauskohteiden-maara)
+                                                                      " paikkauskohde valittu"
+                                                                      " paikkauskohdetta valittu")))
+         :itemit-komponentteja? true}
+        (mapv (fn [paikkauskohde]
+                [:label {:on-click #(.stopPropagation %)
+                         :style {:width "100%"}}
+                 (:nimi paikkauskohde)
+                 [:input {:style {:display "inline-block"
+                                  :float "right"}
+                          :type "checkbox"
+                          :checked (:valittu? paikkauskohde)
+                          :on-change #(let [valittu? (-> % .-target .-checked)]
+                                        (e! (tiedot/->PaikkausValittu paikkauskohde valittu? (partial otsikkokomponentti e!))))}]])
+              (:urakan-paikkauskohteet valinnat))]]]]))
+
+(defn paikkaukset-vetolaatikko
+  [e! {tienkohdat ::paikkaus/tienkohdat materiaalit ::paikkaus/materiaalit id ::paikkaus/id :as rivi}]
+  (let [nayta-numerot #(apply str (interpose ", " %))
+        skeema [{:otsikko "Ajo\u00ADrata"
+                 :leveys 1
+                 :nimi ::paikkaus/ajorata}
+                {:otsikko "Reu\u00ADnat"
+                 :leveys 1
+                 :nimi ::paikkaus/reunat
+                 :fmt nayta-numerot}
+                {:otsikko "Ajo\u00ADurat"
+                 :leveys 1
+                 :nimi ::paikkaus/ajourat
+                 :fmt nayta-numerot}
+                {:otsikko "Ajoura\u00ADvälit"
+                 :leveys 1
+                 :nimi ::paikkaus/ajouravalit
+                 :fmt nayta-numerot}
+                {:otsikko "Kes\u00ADkisau\u00ADmat"
+                 :leveys 1
+                 :nimi ::paikkaus/keskisaumat
+                 :fmt nayta-numerot}
+                {:otsikko "Esiin\u00ADtymä"
+                 :leveys 1
+                 :nimi ::paikkaus/esiintyma}
+                {:otsikko "Kuu\u00ADlamyl\u00ADly\u00ADarvo"
+                 :leveys 2
+                 :nimi ::paikkaus/kuulamyllyarvo}
+                {:otsikko "Muoto\u00ADarvo"
+                 :leveys 2
+                 :nimi ::paikkaus/muotoarvo}
+                {:otsikko "Side\u00ADaine\u00ADtyyp\u00ADpi"
+                 :leveys 2
+                 :nimi ::paikkaus/sideainetyyppi}
+                {:otsikko "Pitoi\u00ADsuus"
+                 :leveys 1
+                 :nimi ::paikkaus/pitoisuus}
+                {:otsikko "Lisä\u00ADaineet"
+                 :leveys 2
+                 :nimi ::paikkaus/lisa-aineet}]
+        yhdistetty [(apply merge {::paikkaus/id id} (concat tienkohdat materiaalit))]]
+    [:div
+     [grid/grid
+      {:otsikko "Tienkohdat & Materiaalit"
+       :tunniste ::paikkaus/id
+       :sivuta grid/vakiosivutus
+       :tyhja "Ei tietoja"}
+      skeema
+      yhdistetty]
+     [napit/yleinen-ensisijainen
+      "Siirry kustannuksiin"
+      #(e! (tiedot/->SiirryKustannuksiin id))]]))
+
+(defn paikkaukset [e! app]
+  (let [tierekisteriosoite-sarakkeet [nil
+                                      {:nimi ::tierekisteri/tie}
+                                      nil nil
+                                      {:nimi ::tierekisteri/aosa}
+                                      {:nimi ::tierekisteri/aet}
+                                      {:nimi ::tierekisteri/losa}
+                                      {:nimi ::tierekisteri/let}]
+        skeema (into []
+                     (concat
+                       [{:tyyppi :vetolaatikon-tila :leveys 1}]
+                       (yllapitokohteet/tierekisteriosoite-sarakkeet 5 tierekisteriosoite-sarakkeet)
+                       [{:otsikko "Alku\u00ADaika"
+                         :leveys 5
+                         :nimi ::paikkaus/alkuaika}
+                        {:otsikko "Loppu\u00ADaika"
+                         :leveys 5
+                         :nimi ::paikkaus/loppuaika}
+                        {:otsikko "Työ\u00ADmene\u00ADtelmä"
+                         :leveys 5
+                         :nimi ::paikkaus/tyomenetelma}
+                        {:otsikko "Massa\u00ADtyyp\u00ADpi"
+                         :leveys 5
+                         :nimi ::paikkaus/massatyyppi}
+                        {:otsikko "Leveys"
+                         :leveys 5
+                         :nimi ::paikkaus/leveys}
+                        {:otsikko "Massa\u00ADmenek\u00ADki"
+                         :leveys 5
+                         :nimi ::paikkaus/massamenekki}
+                        {:otsikko "Raekoko"
+                         :leveys 5
+                         :nimi ::paikkaus/raekoko}
+                        {:otsikko "Kuula\u00ADmylly"
+                         :leveys 5
+                         :nimi ::paikkaus/kuulamylly}]))]
+    (fn [e! {:keys [paikkauksien-haku-kaynnissa? paikkauksien-haku-tulee-olemaan-kaynnissa? paikkaukset-grid paikkauket-vetolaatikko]}]
+      [:div
+       [grid/grid
+        {:otsikko (if (or paikkauksien-haku-kaynnissa? paikkauksien-haku-tulee-olemaan-kaynnissa?)
+                    [yleiset/ajax-loader-pieni "Päivitetään listaa.."]
+                    "Paikkauksien toteumat")
+         :salli-valiotsikoiden-piilotus? true
+         :tunniste ::paikkaus/id
+         :sivuta grid/vakiosivutus
+         :tyhja (if paikkauksien-haku-kaynnissa?
+                  [yleiset/ajax-loader "Haku käynnissä"]
+                  "Ei paikkauksia")
+         :listaus-tunniste ::paikkaus/nimi
+         :vetolaatikot
+         (into {}
+               (map (juxt
+                      ::paikkaus/id
+                      (fn [rivi]
+                        [paikkaukset-vetolaatikko e! rivi])))
+               paikkauket-vetolaatikko)}
+        skeema
+        paikkaukset-grid]])))
 
 (defn kustannukset* [e! app]
-  [:p "Viel ei oo mittää tääl"])
+  (komp/luo
+    (komp/sisaan-ulos #(e! (tiedot/->Nakymaan (partial otsikkokomponentti e!)))
+                      #(e! (tiedot/->NakymastaPois)))
+    (fn [e! app]
+      [:div
+       [debug/debug app]
+       [hakuehdot e! app]
+       [paikkaukset e! app]])))
 
 (defn kustannukset []
   [tuck/tuck tiedot/app kustannukset*])
