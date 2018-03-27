436dd4e87e1a88f793a1df73c61ba4408ad4aaa8
Erno Kuusela <erno-laptop!erno>
2018-03-27 17:49:07 +0300

    päivitä phantom-kontin käynnistysskripti vastaamaan sisus-skriptiä

--- .circleci/kaynnista-phantom-kontti.sh
+++ .circleci/kaynnista-phantom-kontti.sh
@@ -1,1 +1,1 @@
-docker run -v /home/ernoku/src/harja:/harja-src --link harjadb --rm -it  harja-phantom-testit 
+time docker run -v /home/ernoku/src/harja:/harja-src --link harjadb:postgres -p 127.0.0.1:3000:3000 -it  harja-testit back develop
