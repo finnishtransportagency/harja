fb4532218f1ea844cd0e539939fb4c059ed0eb50
Jari Hanhela <jarihanhela@gmail.com>
2018-04-27 13:35:23 +0300

    Poista turhaa koodia

--- tietokanta/src/main/resources/db/migration/V1_711__.sql
+++ tietokanta/src/main/resources/db/migration/V1_711__.sql
@@ -2,4 +2,4 @@
 
 CREATE TYPE hairioilmoitus_tyyppi AS ENUM ('hairio', 'tiedote');
 ALTER TABLE hairioilmoitus ADD COLUMN tyyppi hairioilmoitus_tyyppi;
-UPDATE harja.public.hairioilmoitus SET tyyppi =  'hairio'::hairioilmoitus_tyyppi
+UPDATE hairioilmoitus SET tyyppi = 'hairio'::hairioilmoitus_tyyppi;
