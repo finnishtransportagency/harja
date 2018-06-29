7119ab3011456741acba36364d05ef7c07510de8
maaritlaine <maarit.laine@gmail.com>
2018-06-29 16:38:57 +0300

    Poista insertit migraatiosta.

--- tietokanta/src/main/resources/db/migration/V1_718__.sql
+++ tietokanta/src/main/resources/db/migration/V1_718__.sql
@@ -2,24 +2,4 @@
 ALTER TYPE organisaatiotyyppi ADD VALUE 'hallintayksikko-tilu';
 
 
-INSERT INTO organisaatio (nimi, lyhenne, elynumero,  liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Kainuu', null, 14, 'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
-
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Ahvenanmaa', null, 16, 'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
-
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Etelä-Savo', null, 7,'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
-
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Häme', null, 4,'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
-
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Pohjanmaa', null, 12,'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
-
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Pohjois-Karjala', null, 9,'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
-
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Satakunta', null, 3,'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
 
