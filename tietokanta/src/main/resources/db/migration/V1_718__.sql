b862b339ad7dd9e8fbe6cd88e5ce6b8e80d98eb9
Maarit <maaritlaine@users.noreply.github.com>
2018-06-29 16:26:17 +0300

    Yritä korjata migraatio, osa 2

--- tietokanta/src/main/resources/db/migration/V1_718__.sql
+++ tietokanta/src/main/resources/db/migration/V1_718__.sql
@@ -1,26 +1,25 @@
 -- Tielupasanomissa tulee ELY-nimiä, jotka eivät ole Harjan elyissä mukana.
 ALTER TYPE organisaatiotyyppi ADD VALUE 'hallintayksikko-tilu';
 
-BEGIN
+
 INSERT INTO organisaatio (nimi, lyhenne, elynumero,  liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Kainuu', null, 14, 'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+VALUES ('Kainuu', null, 14, 'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
 
 INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Ahvenanmaa', null, 16, 'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+VALUES ('Ahvenanmaa', null, 16, 'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
 
 INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Etelä-Savo', null, 7,'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+VALUES ('Etelä-Savo', null, 7,'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
 
 INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Häme', null, 4,'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+VALUES ('Häme', null, 4,'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
 
 INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Pohjanmaa', null, 12,'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+VALUES ('Pohjanmaa', null, 12,'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
 
 INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Pohjois-Karjala', null, 9,'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+VALUES ('Pohjois-Karjala', null, 9,'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
 
 INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
-VALUES ('Satakunta', null, 3,'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
-END;
+VALUES ('Satakunta', null, 3,'T', 'hallintayksikko-tilu' , null, current_timestamp, false);
 
