607685984290e39babcbd4a431a6a25e2040561f
maaritlaine <maarit.laine@gmail.com>
2018-06-29 16:10:53 +0300

    Lisää loput ELY:t tielupia varten.

---
+++ tietokanta/src/main/resources/db/migration/V1_718__.sql
@@ -0,0 +1,25 @@
+-- Tielupasanomissa tulee ELY-nimiä, jotka eivät ole Harjan elyissä mukana.
+ALTER TYPE organisaatiotyyppi ADD VALUE 'hallintayksikko-tilu';
+
+INSERT INTO organisaatio (nimi, lyhenne, elynumero,  liikennemuoto, tyyppi, luoja, luotu, poistettu)
+VALUES ('Kainuu', null, 14, 'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+
+INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
+VALUES ('Ahvenanmaa', null, 16, 'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+
+INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
+VALUES ('Etelä-Savo', null, 7,'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+
+INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
+VALUES ('Häme', null, 4,'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+
+INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
+VALUES ('Pohjanmaa', null, 12,'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+
+INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
+VALUES ('Pohjois-Karjala', null, 9,'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+
+INSERT INTO organisaatio (nimi, lyhenne, elynumero, liikennemuoto, tyyppi, luoja, luotu, poistettu)
+VALUES ('Satakunta', null, 3,'T', 'hallintayksikko-tilu' , (select id from kayttaja where kayttajanimi = 'Integraatio'), current_timestamp, false);
+
+
