13a360c8487140488537ada86944b24f017b8558
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2018-09-19 10:49:36 +0300

    Normalisoi talvihoitoluokat Toimenpidekilometrit raportissa

--- src/clj/harja/palvelin/raportointi/raportit/toimenpidekilometrit.sql
+++ src/clj/harja/palvelin/raportointi/raportit/toimenpidekilometrit.sql
@@ -6,7 +6,7 @@ SELECT
   tpk.id   AS "toimenpidekoodi-id",
   tpk.nimi AS "toimenpidekoodi-nimi",
   yksikko  AS "toimenpidekoodi-yksikko",
-  rp.talvihoitoluokka AS hoitoluokka
+  hl.hoitoluokka
 FROM toteuma t
   JOIN urakka u ON t.urakka = u.id
   JOIN toteuma_tehtava tt ON t.id = tt.toteuma
@@ -15,6 +15,8 @@ FROM toteuma t
   JOIN toteuman_reittipisteet tr ON tr.toteuma = t.id
   LEFT JOIN LATERAL unnest(tr.reittipisteet) AS rp ON TRUE
   LEFT JOIN LATERAL unnest(rp.tehtavat) AS rt ON TRUE
+  LEFT JOIN LATERAL (select normalisoi_talvihoitoluokka(rp.talvihoitoluokka::INTEGER, t.alkanut::TIMESTAMP) AS hoitoluokka) hl ON TRUE
+
 WHERE (:urakka::INTEGER IS NULL OR t.urakka = :urakka)
       AND (:hallintayksikko::INTEGER IS NULL OR t.urakka IN (SELECT id
                                                               FROM urakka
@@ -28,4 +30,4 @@ WHERE (:urakka::INTEGER IS NULL OR t.urakka = :urakka)
       AND tpk.yksikko IN ('tiekm', 'jkm')
 AND t.tyyppi = 'kokonaishintainen'
 AND t.poistettu IS NOT TRUE
-GROUP BY urakka, hallintayksikko, tpk.id, talvihoitoluokka;
+GROUP BY urakka, hallintayksikko, tpk.id, hl.hoitoluokka;
