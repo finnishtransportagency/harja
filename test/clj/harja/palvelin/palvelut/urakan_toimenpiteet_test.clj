b29e1be58ff3ee5a8412cdb3d334c49fe2680065
maaritlaine <maarit.laine@gmail.com>
2018-10-01 14:36:18 +0300

    Järjestä testissä saatu hakutulos ennen vertailua osa 2.

--- test/clj/harja/palvelin/palvelut/urakan_toimenpiteet_test.clj
+++ test/clj/harja/palvelin/palvelut/urakan_toimenpiteet_test.clj
@@ -41,7 +41,7 @@
     (is (not (nil? response)))
     (is (= (count response) tpi-maara))
     (is (= (:t3_nimi (first response)) "Laaja toimenpide"))
-    (is (= (:id (first (sort-by :id response))) 608))
+    (is (= (:id (first (sort-by :tpi_id response))) 618))
     (is (contains? tpit "Oulu Talvihoito TP"))
     (is (contains? tpit "Oulu Liikenneympäristön hoito TP"))
     (is (contains? tpit "Oulu Sorateiden hoito TP"))))
