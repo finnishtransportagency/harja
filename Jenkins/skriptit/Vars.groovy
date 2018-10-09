d6886891607149165a77c9efe73422561f45e306
Joonas Mikkonen <joonas.mikkonen@solita.fi>
2018-10-09 23:08:52 +0300

    Palauta mappi Vars.groovy filestä

--- Jenkins/skriptit/Vars.groovy
+++ Jenkins/skriptit/Vars.groovy
@@ -1,22 +1,20 @@
-// Stage nimet
-String testikannanLuonti = 'Luo testikanta'
-String jarJaTestit = 'Luo JAR ja aja testit'
-String testiserverinKannanLuonti = 'Luo testiserverin kanta'
-String testiserverinAppLuonti = 'Luo testiserverin app'
-String e2eTestit = 'Aja E2E testit'
-String stagingserverinKannanLuonti = 'Luo stagingserverin kanta'
-String stagingserverinAppLuonti = 'Luo stagingserverin app'
-String tuotantoserverinKannanLuonti = 'Luodaan tuotantokanta'
-String tuotantoserverinAppLuonti = 'Luodaan tuotantoserverin app'
-String e2eTestitTuotanto = 'E2E testit tuotanto'
-
-// Mitkä staget ajetaan
-Boolean testikannanLuontiAjetaan = STAGE_ETIAPAIN == "Kaikki"
-Boolean jarJaTestitAjetaan = ["Kaikki", "JAR ja testit"].contains(STAGE_ETIAPAIN)
-Boolean testiserverinKannanLuontiAjetaan = ["Kaikki", "JAR ja testit", "Testiserveri"].contains(STAGE_ETIAPAIN)
-Boolean testiserverinAppLuontiAjetaan = ["Kaikki", "JAR ja testit", "Testiserveri"].contains(STAGE_ETIAPAIN)
-Boolean e2eTestitAjetaan = ["Kaikki", "JAR ja testit", "Testiserveri", "E2E"].contains(STAGE_ETIAPAIN)
-Boolean stagingserverinKannanLuontiAjetaan = ["Kaikki", "JAR ja testit", "Testiserveri", "Stagingserveri"].contains(STAGE_ETIAPAIN)
-Boolean stagingserverinAppLuontiAjetaan = ["Kaikki", "JAR ja testit", "Testiserveri", "Stagingserveri"].contains(STAGE_ETIAPAIN)
-
-return this
+return [// Stage nimet
+        testikannanLuonti: 'Luo testikanta',
+        jarJaTestit: 'Luo JAR ja aja testit',
+        testiserverinKannanLuonti: 'Luo testiserverin kanta',
+        testiserverinAppLuonti: 'Luo testiserverin app',
+        e2eTestit: 'Aja E2E testit',
+        stagingserverinKannanLuonti: 'Luo stagingserverin kanta',
+        stagingserverinAppLuonti: 'Luo stagingserverin app',
+        tuotantoserverinKannanLuonti: 'Luodaan tuotantokanta',
+        tuotantoserverinAppLuonti: 'Luodaan tuotantoserverin app',
+        e2eTestitTuotanto: 'E2E testit tuotanto',
+        // Mitkä staget ajetaan
+        testikannanLuontiAjetaan: STAGE_ETIAPAIN == "Kaikki",
+        jarJaTestitAjetaan: ["Kaikki", "JAR ja testit"].contains(STAGE_ETIAPAIN),
+        testiserverinKannanLuontiAjetaan: ["Kaikki", "JAR ja testit", "Testiserveri"].contains(STAGE_ETIAPAIN),
+        testiserverinAppLuontiAjetaan: ["Kaikki", "JAR ja testit", "Testiserveri"].contains(STAGE_ETIAPAIN),
+        e2eTestitAjetaan: ["Kaikki", "JAR ja testit", "Testiserveri", "E2E"].contains(STAGE_ETIAPAIN),
+        stagingserverinKannanLuontiAjetaan: ["Kaikki", "JAR ja testit", "Testiserveri", "Stagingserveri"].contains(STAGE_ETIAPAIN),
+        stagingserverinAppLuontiAjetaan: ["Kaikki", "JAR ja testit", "Testiserveri", "Stagingserveri"].contains(STAGE_ETIAPAIN)
+]
