a8816cff1857945a1c03c6d6b1921d205a235059
Joonas Mikkonen <joonas.mikkonen@solita.fi>
2018-11-12 15:57:34 +0200

    ...

--- Jenkins/skriptit/kannan_puukotus.sh
+++ Jenkins/skriptit/kannan_puukotus.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-psql -h localhost -U harjatest -c "ALTER TABLE yllapitokohdeosa ADD COLUMN keskimaarainen_vuorokausiliikenne INTEGER, ADD COLUMN yllapitoluokka INTEGER, ADD COLUMN nykyinen_paallyste INTEGER;"
+psql -h localhost -U harjatest harjatest_template -c "ALTER TABLE yllapitokohdeosa ADD COLUMN keskimaarainen_vuorokausiliikenne INTEGER, ADD COLUMN yllapitoluokka INTEGER, ADD COLUMN nykyinen_paallyste INTEGER;"
--- Jenkins/skriptit/testitietokanta_test_stg.sh
+++ Jenkins/skriptit/testitietokanta_test_stg.sh
@@ -7,11 +7,11 @@ psql -h localhost -U harjatest template1 -c "CREATE DATABASE harjatest_template 
 psql -h localhost -U harjatest harjatest_template -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 psql -h localhost -U harjatest harjatest_template -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
 
-sh Jenkins/skriptit/kannan_puukotus.sh
-
 cd tietokanta
 
 mvn clean compile flyway:migrate -Dflyway.url=jdbc:postgresql://localhost/harjatest_template -Dflyway.user=harjatest
+
+sh ../Jenkins/skriptit/kannan_puukotus.sh
 
 PGOPTIONS='--client-min-messages=warning' psql -h localhost -U harjatest harjatest_template  -X -q -a -l -v ON_ERROR_STOP=1 --pset pager=off -f testidata.sql
 
