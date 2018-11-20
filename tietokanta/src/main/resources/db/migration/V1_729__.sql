4e2ed774d5bef9d329a988bef1acb4e7990eac6b
Marko Lauronen <marko.lauronen@solita.fi>
2018-11-20 13:02:46 +0200

    proseduuri urakan suolatoteumien purkamiseksi erilliseen cache-tauluun (suolatoteuma_reittipiste) josta helpompi hakea kuin toteuman_reittipisteet-taulusta jossa materiaalit on arraynä.

--- tietokanta/src/main/resources/db/migration/V1_729__.sql
+++ tietokanta/src/main/resources/db/migration/V1_729__.sql
@@ -10,6 +10,7 @@ BEGIN
 	SELECT kandidaatit.id FROM
 	   (SELECT id, ST_Distance(alue, tmp.sijainti::geometry) AS etaisyys
 	      FROM pohjavesialue
+	      WHERE suolarajoitus IS TRUE
 	      ORDER BY 2
 	      LIMIT 1) AS kandidaatit
 	   WHERE kandidaatit.etaisyys<threshold
@@ -21,3 +22,61 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE INDEX pohjavesialue_alue_idx ON pohjavesialue USING GIST (alue);
+CREATE INDEX pohjavesialue_suolarajoitettu_idx ON pohjavesialue (suolarajoitus);
+
+CREATE TABLE suolatoteuma_reittipiste (
+       toteuma INTEGER REFERENCES toteuma(id),
+       aika TIMESTAMP DEFAULT NOW(),
+       pohjavesialue INTEGER REFERENCES pohjavesialue(id),
+       sijainti POINT,
+       materiaalikoodi INTEGER,
+       maara NUMERIC
+);
+
+CREATE INDEX suolatoteuma_reittipiste_idx ON suolatoteuma_reittipiste USING GIST(sijainti);
+
+CREATE OR REPLACE FUNCTION pisteen_pohjavesialue(piste POINT, threshold INTEGER) RETURNS INTEGER AS $$
+DECLARE
+  pohjavesialue INTEGER;
+BEGIN
+  SELECT t.id, t.etaisyys
+    FROM (SELECT id, ST_Distance(alue, piste::geometry) AS etaisyys
+            FROM pohjavesialue
+	    ORDER BY 2
+	    LIMIT 1) AS t
+    WHERE t.etaisyys<=threshold
+    INTO pohjavesialue;
+  RETURN pohjavesialue;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION konvertoi_urakan_suolatoteumat(urakkaid INTEGER, threshold INTEGER) RETURNS VOID AS $$
+DECLARE
+  cur CURSOR FOR SELECT rp.toteuma,
+  	     	 	rp.luotu,
+			(unnest(rp.reittipisteet)).*
+		FROM toteuman_reittipisteet rp
+  		LEFT JOIN toteuma tot ON tot.id = rp.toteuma
+  		WHERE tot.urakka = urakkaid;
+  tmp INTEGER[];
+  suolamateriaalikoodit INTEGER[];
+  m reittipiste_materiaali;
+BEGIN
+  SELECT array_agg(id) FROM materiaalikoodi
+   WHERE materiaalityyppi='talvisuola' INTO suolamateriaalikoodit;
+  
+  FOR r IN cur LOOP
+    FOREACH m IN ARRAY r.materiaalit LOOP
+      IF suolamateriaalikoodit @> ARRAY[m.materiaalikoodi] THEN
+        INSERT INTO suolatoteuma_reittipiste (toteuma, aika, sijainti, materiaalikoodi, maara, pohjavesialue) 
+             VALUES (r.toteuma,
+	     	    r.aika,
+		    r.sijainti,
+		    m.materiaalikoodi,
+		    m.maara,
+		    pisteen_pohjavesialue(r.sijainti, threshold));
+      END IF;
+    END LOOP;
+  END LOOP;
+END;
+$$ LANGUAGE plpgsql;
