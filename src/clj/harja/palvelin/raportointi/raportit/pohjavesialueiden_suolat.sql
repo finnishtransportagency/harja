0ea614a53d9ffb48a91f63a3df4caa7c6c233f98
Marko Lauronen <marko.lauronen@solita.fi>
2018-11-28 10:41:38 +0200

    pohjavesialueiden suolat urakalle, query

---
+++ src/clj/harja/palvelin/raportointi/raportit/pohjavesialueiden_suolat.sql
@@ -0,0 +1,12 @@
+-- name: hae-urakan-pohjavesialueiden-suolatoteumat
+-- Hakee urakan kaikki sillat ja niiden annettuna vuonna tehdyn uusimman siltatarkastuksen
+SELECT pv.tunnus,
+       pv.nimi,
+       sum(rp.maara) AS maara_t_per_km,
+       sum(rp.maara)*sum(st_length(pv.alue)) AS yhteensa,
+       0 AS kayttoraja
+FROM suolatoteuma_reittipiste rp
+  INNER JOIN toteuma tot ON tot.id = rp.toteuma
+  INNER JOIN pohjavesialue pv ON pv.id = rp.pohjavesialue
+WHERE tot.urakka = :urakkaid
+GROUP BY pv.tunnus, pv.nimi;
