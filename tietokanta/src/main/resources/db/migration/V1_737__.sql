089c6c1b15c353a87d2ba19b084ad3cfc85af8a4
maaritlaine <maarit.laine@gmail.com>
2018-12-13 15:08:43 +0200

    Lisää uudet vikatyypit tietokannan enumiin.

---
+++ tietokanta/src/main/resources/db/migration/V1_737__.sql
@@ -0,0 +1,4 @@
+-- Lisätään kanavaurakoihin vikatyyppejä
+ALTER TYPE kan_hairio_vikaluokka ADD VALUE 'ilkivalta';
+ALTER TYPE kan_hairio_vikaluokka ADD VALUE 'sahkokatko';
+ALTER TYPE kan_hairio_vikaluokka ADD VALUE 'muut_viat';
