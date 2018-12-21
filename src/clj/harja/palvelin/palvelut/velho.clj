8cc09b6b5da935e0de34caa0d416d5de5affcceb
mikkoro <mikko.ronkkomaki@gmail.com>
2018-12-21 10:35:42 +0200

    Lisää pohjat kohteiden lähettämiselle Velhoon

---
+++ src/clj/harja/palvelin/palvelut/velho.clj
@@ -0,0 +1,30 @@
+(ns harja.palvelin.palvelut.velho
+  "Velho on suunnitelma ja toteumatietovarasto sekä uusi tierekisteri"
+  (:require [com.stuartsierra.component :as component]
+            [harja.palvelin.komponentit.http-palvelin :refer [julkaise-palvelu poista-palvelut]]
+            [taoensso.timbre :as log]
+            [harja.domain.skeema :refer [Toteuma validoi]]
+            [harja.palvelin.integraatiot.yha.yha-komponentti :as yha]
+            [harja.domain.oikeudet :as oikeudet]))
+
+(defn laheta-kohteet-velhoon
+  "Lähettää annetut kohteet Velhoon."
+  [velho user {:keys [urakka-id  kohde-idt ]}]
+  (oikeudet/vaadi-oikeus "sido" oikeudet/urakat-kohdeluettelo-paallystyskohteet user urakka-id)
+  (log/debug (format "Lähetetään kohteet: %s Velhoon" kohde-idt))
+  (velho/laheta-kohteet velho urakka-id kohde-idt))
+
+(defrecord Yha []
+  component/Lifecycle
+  (start [this]
+    (let [http (:http-palvelin this)
+          velho (:yha-integraatio this)]
+      (julkaise-palvelu http :laheta-kohteet-velhoon
+                        (fn [user data]
+                          (laheta-kohteet-velhoon velho user data))))
+    this)
+  (stop [this]
+    (poista-palvelut
+      (:http-palvelin this)
+      :laheta-kohteet-velhoon)
+    this))
