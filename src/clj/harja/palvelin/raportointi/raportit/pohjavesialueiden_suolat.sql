54dd8ad85b535405ea36fc1060e92d6549946385
Marko Lauronen <marko.lauronen@solita.fi>
2019-02-06 12:46:33 +0200

    raporttihaku käyttämään koostettua pohjavesialuenäkymää

--- src/clj/harja/palvelin/raportointi/raportit/pohjavesialueiden_suolat.sql
+++ src/clj/harja/palvelin/raportointi/raportit/pohjavesialueiden_suolat.sql
@@ -1,18 +1,17 @@
 -- name: hae-urakan-pohjavesialueiden-suolatoteumat
 SELECT pv.tunnus,
        pv.nimi,
-       pv.tr_numero AS tie,
-       min(pv.tr_alkuosa) AS alkuosa,
-       min(pv.tr_alkuetaisyys) AS alkuet,
-       max(pv.tr_loppuosa) AS loppuosa,
-       max(pv.tr_loppuetaisyys) AS loppuet,
-       sum(st_length(pv.alue)) AS pituus,
-       sum(rp.maara) AS maara_t_per_km,
-       sum(rp.maara)*sum(st_length(pv.alue))/1000 AS yhteensa,
+       pv.tie AS tie,
+       pv.alkuosa AS alkuosa,
+       pv.alkuet AS alkuet,
+       pv.loppuosa AS loppuosa,
+       pv.loppuet AS loppuet,
+       pv.pituus AS pituus,
+       rp.maara AS maara_t_per_km,
+       rp.maara*pv.pituus/1000 AS yhteensa,
        ts.talvisuolaraja AS kayttoraja
 FROM suolatoteuma_reittipiste rp
   INNER JOIN toteuma tot ON tot.id = rp.toteuma
-  INNER JOIN pohjavesialue pv ON pv.tunnus = rp.pohjavesialue
+  INNER JOIN pohjavesialue_kooste pv ON pv.tunnus = rp.pohjavesialue
   LEFT JOIN pohjavesialue_talvisuola ts on ts.pohjavesialue = rp.pohjavesialue
-WHERE tot.urakka=:urakkaid AND rp.aika BETWEEN :alkupvm AND :loppupvm
-GROUP BY pv.tunnus, pv.tr_numero, pv.nimi, ts.talvisuolaraja;
+WHERE tot.urakka=:urakkaid AND rp.aika BETWEEN :alkupvm AND :loppupvm;
