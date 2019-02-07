e56b917f57cfa75198e5936f2d766c55cc1be553
maaritlaine <maarit.laine@gmail.com>
2019-02-07 10:35:55 +0200

    Kutsu työkoneen viivageometrian tallennusta omaa reittiä.

--- src/clj/harja/palvelin/integraatiot/api/tyokoneenseuranta.clj
+++ src/clj/harja/palvelin/integraatiot/api/tyokoneenseuranta.clj
@@ -11,6 +11,7 @@
             [clojure.data.json :as json]))
 
 (def +tyokone-seurantakirjaus-url+ "/api/seuranta/tyokone")
+(def +tyokone-reitti-url+ "/api/seuranta/tyokone/reitti")
 
 (defn arrayksi [db v]
   (with-open [conn (.getConnection (:datasource db))]
@@ -53,18 +54,24 @@
      :urakkaid urakka-id
      :tehtavat (arrayksi db (get-in havainto [:havainto :suoritettavatTehtavat]))}))
 
+(defn- tallenna-seurantakirjaus-viivageometriana [_ data kayttaja db]
+  (validointi/tarkista-onko-kayttaja-organisaation-jarjestelma db
+                                                               (get-in data [:otsikko :lahettaja :organisaatio :ytunnus])
+                                                               kayttaja)
+  (doseq [havainto (:havainnot data)]
+    (let [urakka-id (get-in havainto [:havainto :urakkaid])]
+      (when urakka-id (validointi/tarkista-urakka db urakka-id)
+                      (tallenna-tyokoneen-reitti db data havainto urakka-id))))
+  (tee-kirjausvastauksen-body {:ilmoitukset "Kirjauksen tallennus onnistui"}))
 
 (defn- tallenna-seurantakirjaus [_ data kayttaja db]
   (validointi/tarkista-onko-kayttaja-organisaation-jarjestelma db
                                                                (get-in data [:otsikko :lahettaja :organisaatio :ytunnus])
                                                                kayttaja)
   (doseq [havainto (:havainnot data)]
-    (let [urakka-id (get-in havainto [:havainto :urakkaid])
-          tallenna-havainto (if (= nil (get-in havainto [:havainto :sijainti :viivageometria]))
-                              tallenna-tyokoneen-koordinaatti
-                              tallenna-tyokoneen-reitti)]
+    (let [urakka-id (get-in havainto [:havainto :urakkaid])]
       (when urakka-id (validointi/tarkista-urakka db urakka-id)
-                      (tallenna-havainto db data havainto urakka-id))))
+                      (tallenna-tyokoneen-koordinaatti db data havainto urakka-id))))
   (tee-kirjausvastauksen-body {:ilmoitukset "Kirjauksen tallennus onnistui"}))
 
 (defrecord Tyokoneenseuranta []
@@ -77,6 +84,12 @@
                                         :tallenna-tyokoneenseurantakirjaus
                                         request json-skeemat/tyokoneenseuranta-kirjaus json-skeemat/kirjausvastaus
                                         tallenna-seurantakirjaus)))
+    (julkaise-reitti http :tallenna-tyokoneen-reitti
+                     (POST +tyokone-reitti-url+ request
+                       (kasittele-kutsu db nil
+                                        :tallenna-tyokoneen-reitti
+                                        request json-skeemat/tyokoneenseuranta-kirjaus json-skeemat/kirjausvastaus
+                                        tallenna-seurantakirjaus-viivageometriana)))
     this)
   (stop [{http :http-palvelin :as this}]
     (poista-palvelut http :tallenna-tyokoneenseurantakirjaus)
