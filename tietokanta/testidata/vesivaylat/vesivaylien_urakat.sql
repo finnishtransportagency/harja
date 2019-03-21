1020139c6cf317a6f63aabcbb8b8723e1bae8081
maaritlaine <maarit.laine@gmail.com>
2019-03-21 16:11:48 +0200

    Korjaa testidataa.

--- tietokanta/testidata/vesivaylat/vesivaylien_urakat.sql
+++ tietokanta/testidata/vesivaylat/vesivaylien_urakat.sql
@@ -157,4 +157,4 @@ VALUES (3, 132, 'Rannikon muut');
 
 -- URAKKA-ALUEET
 INSERT INTO vv_urakka_turvalaiteryhma(id, urakka, turvalaiteryhmat)
-VALUES (1, (select id from urakka where nimi = 'Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'), '{3333}')
+VALUES (1, (select id from urakka where nimi = 'Helsingin väyläyksikön väylänhoito ja -käyttö, Itäinen SL'), '{555,3333}')
