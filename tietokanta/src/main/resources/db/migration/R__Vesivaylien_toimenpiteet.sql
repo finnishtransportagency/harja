c8904bf1bd7eb709775f72cbd40e96ce25688302
Maarit Laine <maarit.laine@solita.fi>
2019-03-27 13:14:13 +0200

    Päivitä proseduuri

--- tietokanta/src/main/resources/db/migration/R__Vesivaylien_toimenpiteet.sql
+++ tietokanta/src/main/resources/db/migration/R__Vesivaylien_toimenpiteet.sql
@@ -39,43 +39,54 @@ END;
 $$
 LANGUAGE plpgsql;
 
-
--- Linkkaa urakka toimenpiteeseen sopimuksen perusteella
+-- Linkkaa urakka toimenpiteeseen sopimuksen tai turvalaiteryhmän perusteella
 CREATE OR REPLACE FUNCTION toimenpiteen_urakka_id_trigger_proc()
   RETURNS TRIGGER AS
 $$
-DECLARE id_temp TEXT;
+DECLARE
+  id_urakka_alue  INT;
+  DECLARE id_temp TEXT;
 BEGIN
   IF NEW."harjassa-luotu" IS TRUE
   THEN
     RETURN NEW;
   ELSE
-    id_temp := (SELECT u.id
-                FROM urakka u, sopimus s, reimari_sopimuslinkki rsl
-                WHERE u.urakkanro = (NEW."reimari-turvalaite").ryhma :: TEXT AND
-                      rsl."harja-sopimus-id" = s.id AND
-                      rsl."reimari-diaarinro" = btrim((NEW."reimari-sopimus").diaarinro) AND
-                      s.urakka = u.id
-                LIMIT 1);
-
-    IF id_temp IS NULL
-    THEN
-      -- RAISE NOTICE 'trigger: linkataan urakka-id turvalaiteryhmällä koska sopimus/diaarinro linkkaus ei onnistunut';
-      id_temp := (SELECT id
-                  FROM urakka u
-                  WHERE
-                    u.urakkanro IS NOT NULL AND
-                    u.urakkanro = (NEW."reimari-turvalaite").ryhma :: TEXT
-                  LIMIT 1) LIMIT 1;
+    id_urakka_alue := (SELECT id
+                       from vv_urakka_turvalaiteryhma
+                       WHERE turvalaiteryhmat @> ARRAY [(NEW."reimari-turvalaite").ryhma::text]
+                         AND NEW."reimari-luotu"::DATE BETWEEN alkupvm::DATE and loppupvm::DATE
+                       LIMIT 1);
+    IF (id_urakka_alue IS NULL) THEN
+      RAISE NOTICE 'Toimenpiteen % urakka-aluetta ei voitu päätellä, joten urakkaa ei voi selvittää.', NEW."reimari-id"::text;
+    ELSE
+      RAISE NOTICE 'Toimenpiteen % urakka-alue on %.', NEW."reimari-id"::text, CAST(id_urakka_alue AS text);
+      id_temp := (SELECT u.id
+                  FROM urakka u,
+                       sopimus s,
+                       reimari_sopimuslinkki rsl
+                  WHERE u.urakkanro = CAST(id_urakka_alue AS text)
+                    AND rsl."harja-sopimus-id" = s.id
+                    AND rsl."reimari-diaarinro" = btrim((NEW."reimari-sopimus").diaarinro)
+                    AND s.urakka = u.id
+                  LIMIT 1);
+      IF id_temp IS NULL THEN
+        RAISE NOTICE 'trigger: linkataan urakka-id turvalaiteryhmällä koska sopimus/diaarinro linkkaus ei onnistunut';
+        id_temp := (SELECT id
+                    FROM urakka u
+                    WHERE u.urakkanro IS NOT NULL
+                      AND u.urakkanro = CAST(id_urakka_alue AS text)
+                    LIMIT 1)
+                   LIMIT 1;
+      END IF;
     END IF;
-    NEW."urakka-id" = id_temp;
-    -- urakka-id:ksi tulee NULL jos ei löydy, joka on ok
-    -- RAISE NOTICE 'trigger: urakka-id arvoksi %', NEW."urakka-id";
-    RETURN NEW;
   END IF;
+  NEW."urakka-id" = id_temp;
+  -- urakka-id:ksi tulee NULL jos ei löydy, joka on ok
+  RAISE NOTICE 'trigger: urakka-id arvoksi %', NEW."urakka-id";
+  RETURN NEW;
 END;
-$$
-LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
+
 
 -- Aseta vaylanro Reimarin väylätietojen perusteella
 
--- tietokanta/src/main/resources/db/migration/V1_762__.sql
+++ tietokanta/src/main/resources/db/migration/V1_762__.sql
@@ -16,7 +16,7 @@ CREATE UNIQUE INDEX uniikki_urakka
 
 COMMENT ON TABLE vv_urakka_turvalaiteryhma IS 'Urakan relaatiot turvalaiteryhmiin. Yksi urakka-alue voi koostua useasta turvalaiteryhmästä. Vesiväyläurakoiden urakka.urakkanro viittaa taulun id-kenttään.';
 
--- Päivitä toimenpiteen urakan käsittely
+-- Päivitä toimenpiteen urakan käsittely. Hox. Varsinainen kantaan päätyvä sproc on R_Vesivaylien_toimenpiteet.sql-tiedostossa.
 CREATE OR REPLACE FUNCTION toimenpiteen_urakka_id_trigger_proc()
   RETURNS TRIGGER AS
 $$
@@ -24,30 +24,37 @@ DECLARE
   id_urakka_alue  INT;
   DECLARE id_temp TEXT;
 BEGIN
-   id_urakka_alue := (SELECT id
-                      from vv_urakka_turvalaiteryhma
-                      WHERE turvalaiteryhmat @> ARRAY [(NEW."reimari-turvalaite").ryhma::text]
-                      AND NEW."reimari-luotu"::DATE BETWEEN alkupvm::DATE and loppupvm::DATE LIMIT 1);
-  IF (id_urakka_alue IS NULL) THEN
-    RAISE NOTICE 'Toimenpiteen % urakka-aluetta ei voitu päätellä, joten urakkaa ei voi selvittää.', NEW."reimari-id"::text;
+  IF NEW."harjassa-luotu" IS TRUE
+  THEN
+    RETURN NEW;
   ELSE
-    id_temp := (SELECT u.id
-                FROM urakka u,
-                     sopimus s,
-                     reimari_sopimuslinkki rsl
-                WHERE u.urakkanro = CAST(id_urakka_alue AS text)
-                  AND rsl."harja-sopimus-id" = s.id
-                  AND rsl."reimari-diaarinro" = btrim((NEW."reimari-sopimus").diaarinro)
-                  AND s.urakka = u.id
-                LIMIT 1);
-    IF id_temp IS NULL THEN
-      RAISE NOTICE 'trigger: linkataan urakka-id turvalaiteryhmällä koska sopimus/diaarinro linkkaus ei onnistunut';
-      id_temp := (SELECT id
-                  FROM urakka u
-                  WHERE u.urakkanro IS NOT NULL
-                    AND u.urakkanro = CAST(id_urakka_alue AS text)
-                  LIMIT 1)
-                 LIMIT 1;
+    id_urakka_alue := (SELECT id
+                       from vv_urakka_turvalaiteryhma
+                       WHERE turvalaiteryhmat @> ARRAY [(NEW."reimari-turvalaite").ryhma::text]
+                         AND NEW."reimari-luotu"::DATE BETWEEN alkupvm::DATE and loppupvm::DATE
+                       LIMIT 1);
+    IF (id_urakka_alue IS NULL) THEN
+      RAISE NOTICE 'Toimenpiteen % urakka-aluetta ei voitu päätellä, joten urakkaa ei voi selvittää.', NEW."reimari-id"::text;
+    ELSE
+      RAISE NOTICE 'Toimenpiteen % urakka-alue on %.', NEW."reimari-id"::text, CAST(id_urakka_alue AS text);
+      id_temp := (SELECT u.id
+                  FROM urakka u,
+                       sopimus s,
+                       reimari_sopimuslinkki rsl
+                  WHERE u.urakkanro = CAST(id_urakka_alue AS text)
+                    AND rsl."harja-sopimus-id" = s.id
+                    AND rsl."reimari-diaarinro" = btrim((NEW."reimari-sopimus").diaarinro)
+                    AND s.urakka = u.id
+                  LIMIT 1);
+      IF id_temp IS NULL THEN
+        RAISE NOTICE 'trigger: linkataan urakka-id turvalaiteryhmällä koska sopimus/diaarinro linkkaus ei onnistunut';
+        id_temp := (SELECT id
+                    FROM urakka u
+                    WHERE u.urakkanro IS NOT NULL
+                      AND u.urakkanro = CAST(id_urakka_alue AS text)
+                    LIMIT 1)
+                   LIMIT 1;
+      END IF;
     END IF;
   END IF;
   NEW."urakka-id" = id_temp;
@@ -57,20 +64,3 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Päivitä
-INSERT INTO vv_urakka_turvalaiteryhma (urakka, turvalaiteryhmat, alkupvm, loppupvm, luotu, luoja)
-  (SELECT id,
-          string_to_array(urakkanro, ','),
-          alkupvm,
-          loppupvm,
-          NOW(),
-          (select id from kayttaja where kayttajanimi = 'Integraatio')
-   from urakka
-   where tyyppi = 'vesivayla-hoito'
-     and urakkanro is not null);
-UPDATE urakka
-set urakkanro = (select v.id from vv_urakka_turvalaiteryhma v where v.urakka = urakka.id)::TEXT,
-    muokattu  = NOW(),
-    muokkaaja = (select id from kayttaja where kayttajanimi = 'Integraatio')
-WHERE tyyppi = 'vesivayla-hoito'
-  and urakkanro is not null;
