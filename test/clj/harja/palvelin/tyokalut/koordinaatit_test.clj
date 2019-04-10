6921ecb9a13e2b24dede84060a1dd40e9fa7452e
Joonas-M-S <joonas.mikkonen@solita.fi>
2019-04-10 16:57:25 +0300

    testien korjailua

--- test/clj/harja/palvelin/tyokalut/koordinaatit_test.clj
+++ test/clj/harja/palvelin/tyokalut/koordinaatit_test.clj
@@ -24,10 +24,14 @@
 (deftest pisteen-hakeminen-kartalle-wgs84-koordinaateilla
   (let [x 25.472488
         y 65.011322
-        oikea-vastaus {:type :point :coordinates [428003.60827314324 7210586.34520906]}
+        oikea-vastaus {:type :point :coordinates [428003.608273143 7210586.34520906]}
         vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
                                 :hae-piste-kartalle
                                 +kayttaja-jvh+
                                 {:x x
-                                 :y y})]
+                                 :y y})
+        pyorista #(/ (Math/round (* %1 (Math/pow 10 %2))) (Math/pow 10 %2))
+        vastaus (-> vastaus
+                    (update-in [:coordinates 0] #(pyorista % 9))
+                    (update-in [:coordinates 1] #(pyorista % 8)))]
     (is (= vastaus oikea-vastaus) "wgs84 koordinaattien muuttaminen pointiksi euref koordinaateilla ei toimi")))
