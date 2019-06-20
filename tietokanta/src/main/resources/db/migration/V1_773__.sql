07c5f5989948b46d28ebf023fe1efa970892b468
Maarit Laine <maarit.laine@solita.fi>
2019-06-20 15:59:47 +0300

    Tehtäväryhmät

--- tietokanta/src/main/resources/db/migration/V1_773__.sql
+++ tietokanta/src/main/resources/db/migration/V1_773__.sql
@@ -10,14 +10,25 @@ CREATE TYPE tehtavaryhmatyyppi AS ENUM (
 
 CREATE TABLE tehtavaryhma (
    id serial primary key,    -- sisäinen id
-   nimi varchar (128),       -- tehtäväryhmän nimi (esim. "Laaja toimenpide")
+   nimi varchar (128) not null,       -- tehtäväryhmän nimi (esim. "Laaja toimenpide")
    emo INTEGER REFERENCES toimenpidekoodi (id),  -- ylemmän tason tehtäväryhmä (NULL jos taso on tehtäväryhmissä ylimpänä)
    tyyppi TEHTAVARYHMATYYPPI not null,
-   poistettu boolean DEFAULT false,
+   "ui-taso" integer, -- ryhmän järjestys käyttöliittymässä
+   "ei-nayteta" boolean, -- true, jos taso ei näy käyttäjälle käyttöliittymässä
+   poistettu boolean DEFAULT false, -- true jos taso on poistettu käytöstä kaikissa urakoissa
    luotu timestamp,
    luoja integer REFERENCES kayttaja (id),
    muokattu timestamp,
    muokkaaja integer REFERENCES kayttaja (id)
 );
 
+-- Täytyy voida selvittää mistä puhuttaessa tarkoitetaan, uniikit nimet tehtäväryhmille
+CREATE UNIQUE INDEX uniikki_tehtavaryhma_nimi ON tehtavaryhma (nimi);
 
+-- Lisää toimenpidekooditauluun viittaus tehtäväryhmään ja käyttöliittymä
+ALTER TABLE toimenpidekoodi
+ADD COLUMN api_tunnus numeric,
+ADD COLUMN tehtavaryhma numeric;
+
+UPDATE toimenpidekoodi
+  SET api_tunnus = id WHERE taso = 4;
