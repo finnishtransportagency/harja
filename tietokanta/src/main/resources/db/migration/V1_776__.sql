b183b8d208bc4769b269aee3c32091deea4bdf9d
Maarit Laine <maarit.laine@solita.fi>
2019-07-10 16:49:37 +0300

    Korjaa typo.

--- tietokanta/src/main/resources/db/migration/V1_776__.sql
+++ tietokanta/src/main/resources/db/migration/V1_776__.sql
@@ -1,29 +1,11 @@
-CREATE TYPE tehtavaryhmatyyppi AS ENUM (
-  'ylataso',
-  'valitaso',
-  'alataso');
-
-CREATE TABLE tehtavaryhma (
-                            id serial primary key,    -- sisäinen id
-                            otsikko  text, -- korkean tason otsikko, joka kuvaa tehtäväryhmää (esim. Talvihoito)
-                            nimi text not null,       -- tehtäväryhmän nimi (esim. "Viheralueiden hoito")
-                            emo INTEGER REFERENCES toimenpidekoodi (id),  -- ylemmän tason tehtäväryhmä (NULL jos taso on tehtäväryhmissä ylimpänä)
-                            tyyppi TEHTAVARYHMATYYPPI not null,
-                            jarjestys integer, -- ryhmän järjestys käyttöliittymässä
-                            nakyva boolean, -- true, jos taso näkyy käyttäjälle käyttöliittymissä
-                            poistettu boolean DEFAULT false, -- true jos taso on poistettu käytöstä kaikissa urakoissa
-                            luotu timestamp,
-                            luoja integer REFERENCES kayttaja (id),
-                            muokattu timestamp,
-                            muokkaaja integer REFERENCES kayttaja (id)
-);
-
-CREATE TABLE kiinteahinainen_tyo (
+CREATE TABLE kiinteahintainen_tyo (
                                      id serial primary key,       -- sisäinen ID
                                      vuosi smallint not null,
                                      kuukausi smallint not null,
-                                     summa numeric (7,6),
+                                     summa numeric,
                                      toimenpideinstanssi integer not null REFERENCES toimenpideinstanssi (id),
+                                     tehtavaryhma integer REFERENCES tehtavaryhma (id),
+                                     tehtava integer REFERENCES toimenpidekoodi (id),
                                      sopimus integer not null REFERENCES sopimus(id),
                                      luotu timestamp,
                                      luoja integer references kayttaja(id),
@@ -32,7 +14,7 @@ CREATE TABLE kiinteahinainen_tyo (
                                      unique (toimenpideinstanssi, sopimus, vuosi, kuukausi));
 
 
-COMMENT ON table kiinteahinainen_tyo IS
+COMMENT ON table kiinteahintainen_tyo IS
   E'Kiinteähintaista työtä suunnitellaan urakkatyypissä teiden-hoito (MHU).
    Työlle suunniteltu kustannus lasketaan mukaan Sampoon lähetettävään kokonaishintaiseen kustannussuunnitelmaan. Suunniteltu summa myös kasvattaa Sampoon lähetettävää maksuerää (samaan tapaan kuin kokonaishintainen_tyo urakkatyypissä hoito).
     Kustannussuunnitelma tehdään kuukausi- ja toimenpidetasolla.' ;
@@ -42,7 +24,7 @@ CREATE TABLE kustannusarvioitu_tyo (
                                      id serial primary key,       -- sisäinen ID
                                      vuosi smallint not null,
                                      kuukausi smallint not null,
-                                     summa numeric (7,6),
+                                     summa numeric,
                                      tyyppi toteumatyyppi,
                                      tehtava integer REFERENCES toimenpidekoodi (id),
                                      tehtavaryhma integer REFERENCES tehtavaryhma (id),
