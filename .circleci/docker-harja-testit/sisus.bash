ca1c47a54b782dcdb0f641be725894fb97a06aed
Joonas-M-S <joonas.mikkonen@solita.fi>
2019-09-19 12:48:07 +0300

    Palauta sisus.bash

--- .circleci/docker-harja-testit/sisus.bash
+++ .circleci/docker-harja-testit/sisus.bash
@@ -56,10 +56,6 @@ pwd
 rm -vf asetukset.edn
 git checkout -b "t_$BRANCH" "origin/$BRANCH"
 git checkout asetukset.edn
-sed -i -e 's/:tietokanta {:palvelin "localhost"/:tietokanta {:palvelin "possu"/g' asetukset.edn
-sed -i -e 's/:tietokanta-replica {:palvelin "localhost"/:tietokanta-replica {:palvelin "possu"/g' asetukset.edn
-sed -i -e 's/localhost/possu/g' tietokanta/pom.xml
-sed -i -e 's/localhost/possu/g' src/clj/harja/kyselyt/specql_db.clj
 
 mkdir -p ../.harja
 echo aaaa > ../.harja/anti-csrf-token
@@ -67,8 +63,8 @@ touch ../.harja/{mml,google-static-maps-key,turi-salasana,ava-salasana,yha-salas
 # todo: kokeile lein trampoline "$@"
 cd tietokanta
 mvn flyway:migrate
-psql -h possu -U harja harja -X -q -a -v ON_ERROR_STOP=1 --pset pager=off -f testidata.sql > /dev/null
-psql -h possu -U harja harja -c "CREATE DATABASE harjatest_template WITH TEMPLATE harja OWNER harjatest;"
-psql -h possu -U harja harja -c "CREATE DATABASE harjatest WITH TEMPLATE harjatest_template OWNER harjatest;"
+psql -h localhost -U harja harja -X -q -a -v ON_ERROR_STOP=1 --pset pager=off -f testidata.sql > /dev/null
+psql -h localhost -U harja harja -c "CREATE DATABASE harjatest_template WITH TEMPLATE harja OWNER harjatest;"
+psql -h localhost -U harja harja -c "CREATE DATABASE harjatest WITH TEMPLATE harjatest_template OWNER harjatest;"
 cd ..
 eval "cmd_$SUBCMD"
