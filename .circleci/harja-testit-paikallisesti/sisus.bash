504348dc7f1345406f4985b6ab72243b0104c949
Joonas-M-S <joonas.mikkonen@solita.fi>
2019-09-20 10:11:32 +0300

    Muuta maximi muistikokoa javalle

--- .circleci/harja-testit-paikallisesti/sisus.bash
+++ .circleci/harja-testit-paikallisesti/sisus.bash
@@ -56,6 +56,9 @@ pwd
 rm -vf asetukset.edn
 git checkout -b "t_$BRANCH" "origin/$BRANCH"
 git checkout asetukset.edn
+
+sed -i -e 's/:jvm-opts \^:replace \["-Xms256m" "-Xmx2g"\]/:jvm-opts \^:replace \["-Xms256m" "-Xmx1g"\]/g' project.clj
+
 sed -i -e 's/:tietokanta {:palvelin "localhost"/:tietokanta {:palvelin "possu"/g' asetukset.edn
 sed -i -e 's/:tietokanta-replica {:palvelin "localhost"/:tietokanta-replica {:palvelin "possu"/g' asetukset.edn
 sed -i -e 's/localhost/possu/g' tietokanta/pom.xml
