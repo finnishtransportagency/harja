40511ed1209eb029b47807693b6265dfd5dc1aa5
Joonas-M-S <joonas.mikkonen@solita.fi>
2019-10-09 15:29:28 +0300

    Budjettisuunnittelun hakutesti loppuun

--- test/clj/harja/palvelin/palvelut/budjettisuunnittelu_test.clj
+++ test/clj/harja/palvelin/palvelut/budjettisuunnittelu_test.clj
@@ -3,7 +3,8 @@
             [harja.palvelin.palvelut.budjettisuunnittelu :refer :all]
             [harja.palvelin.palvelut.pois-kytketyt-ominaisuudet :as pois-kytketyt-ominaisuudet]
             [harja.testi :refer :all]
-            [com.stuartsierra.component :as component]))
+            [com.stuartsierra.component :as component]
+            [harja.pvm :as pvm]))
 
 (defn jarjestelma-fixture [testit]
   (alter-var-root #'jarjestelma
@@ -35,12 +36,33 @@
 ;; sampoa varten likaiseksi merkitseminen
 
 (deftest budjetoidut-tyot-haku
-  (let [urakka-id (:id (first (q-map "SELECT id FROM urakka WHERE nimi='Pellon MHU testiurakka (3. hoitovuosi)';")))
-        budjetoidut-tyot (hae-urakan-budjetoidut-tyot (:db jarjestelma) +kayttaja-jvh+ {:urakka-id urakka-id})]
+  (let [{urakka-id :id urakan-alkupvm :alkupvm} (first (q-map "SELECT id, alkupvm FROM urakka WHERE nimi='Pellon MHU testiurakka (3. hoitovuosi)';"))
+        budjetoidut-tyot (hae-urakan-budjetoidut-tyot (:db jarjestelma) +kayttaja-jvh+ {:urakka-id urakka-id})
+        testaa-ajat (fn [tehtavat toimenpide]
+                      (let [tehtavat-vuosittain (group-by :vuosi tehtavat)]
+                        (is (= (ffirst (sort-by key tehtavat-vuosittain))
+                               (pvm/vuosi urakan-alkupvm))
+                            (str "Urakan ensimmäinen vuosi pitäisi olla " (pvm/vuosi urakan-alkupvm)
+                                 ", mutta se oli " (ffirst (sort-by key tehtavat-vuosittain))))
+                        (is (= 6 (count tehtavat-vuosittain)) (str "Tehtäviä pitäisi olla merkattuna kuudelle kalenterivuodelle. Toimenpiteelle "
+                                                                   toimenpide " löytyi " (count tehtavat-vuosittain) " vuodelle"))
+                        (is (= #{10 11 12}
+                               (into #{}
+                                     (map :kuukausi
+                                          (val (first (sort-by key tehtavat-vuosittain))))))
+                            "Ensimmäisellä kalenterivuodella pitäisi olla arvoja vain kolmelle viimeiselle kuukaudelle")
+                        (doseq [[_ tehtavat-vuodelle] (butlast (rest (sort-by key tehtavat-vuosittain)))]
+                          (is (= (into #{} (range 1 13))
+                                 (into #{} (map :kuukausi tehtavat-vuodelle))))
+                          "Muille kalenterivuosille pitäisi arvo olla joka kuulle")
+                        (is (= (into #{} (range 1 10))
+                               (into #{}
+                                     (map :kuukausi
+                                          (val (last (sort-by key tehtavat-vuosittain))))))
+                            "Viimeisellä kalenterivuodella pitäisi olla arvoja vain tammikuusta syysykuuhun")))]
     (testing "Kiinteähintaiset työt on oikein"
       (let [kiinteahintaiset-tyot-toimenpiteittain (group-by :toimenpide (:kiinteahintaiset-tyot budjetoidut-tyot))]
-        (doseq [[toimenpide tehtavat] kiinteahintaiset-tyot-toimenpiteittain
-                :let [tehtavat-vuosittain (group-by :vuosi tehtavat)]]
+        (doseq [[toimenpide tehtavat] kiinteahintaiset-tyot-toimenpiteittain]
           (is (nil? (some (fn [{:keys [tehtava tehtavaryhma]}]
                             (when (or tehtava tehtavaryhma)
                               true))
@@ -51,15 +73,81 @@
                               true))
                           tehtavat))
               "Kaikille summille pitäisi olla jokin arvo")
-          (is (= 6 (count tehtavat-vuosittain)) (str "Tehtäviä pitäisi olla merkattuna kuudelle kalenterivuodelle. Toimenpiteelle "
-                                                     toimenpide " löytyi " (count tehtavat-vuosittain) " vuodelle"))
-          (is (= #{10 11 12}
-                 (into #{}
-                       (map :kuukausi
-                            (val (first (sort-by key tehtavat-vuosittain))))))
-              "Ensimmäisellä kalenterivuodella pitäisi olla arvoja vain kolmelle viimeiselle kuukaudelle")
-          (is (= (into #{} (range 1 10))
-                 (into #{}
-                       (map :kuukausi
-                            (val (last (sort-by key tehtavat-vuosittain))))))
-              "Viimeisellä kalenterivuodella pitäisi olla arvoja vain tammikuusta syysykuuhun"))))))
+          (testaa-ajat tehtavat toimenpide))))
+    (testing "Kustannusarvioidut työt on oikein"
+      (let [kustannusarvioidut-tyot-toimenpiteittain (group-by :toimenpide-avain (:kustannusarvioidut-tyot budjetoidut-tyot))]
+        (doseq [[toimenpide-avain tehtavat] kustannusarvioidut-tyot-toimenpiteittain
+                :let [ryhmiteltyna (group-by (juxt :tyyppi :tehtava-nimi) tehtavat)]]
+          (case toimenpide-avain
+            :paallystepaikkaukset (is (= ryhmiteltyna {}))
+            :mhu-yllapito (do
+                            (is (= (keys ryhmiteltyna) [["muut-rahavaraukset" nil]]))
+                            (testaa-ajat tehtavat toimenpide-avain))
+            :talvihoito (do
+                          (is (= (into #{} (keys ryhmiteltyna))
+                                 #{["vahinkojen-korjaukset" nil]
+                                   ["akillinen-hoitotyo" nil]}))
+                          (doseq [[_ tehtavat] ryhmiteltyna]
+                            (testaa-ajat tehtavat toimenpide-avain)))
+            :liikenneympariston-hoito (do
+                                        (is (= (into #{} (keys ryhmiteltyna))
+                                               #{["laskutettava-tyo" nil] ;; Tässä testidatassa on siis painettu päälle "Haluan suunnitella myös määrämitattavia töitä toimenpiteelle"
+                                                 ["vahinkojen-korjaukset" nil]
+                                                 ["akillinen-hoitotyo" nil]}))
+                                        (doseq [[_ tehtavat] ryhmiteltyna]
+                                          (testaa-ajat tehtavat toimenpide-avain)))
+            :sorateiden-hoito (do
+                                (is (= (into #{} (keys ryhmiteltyna))
+                                       #{["vahinkojen-korjaukset" nil]
+                                         ["akillinen-hoitotyo" nil]}))
+                                (doseq [[_ tehtavat] ryhmiteltyna]
+                                  (testaa-ajat tehtavat toimenpide-avain)))
+            :mhu-korvausinvestointi (is (= ryhmiteltyna {}))
+            :mhu-johto (do
+                         (is (= (into #{} (keys ryhmiteltyna))
+                                #{["laskutettava-tyo" nil]
+                                  ["laskutettava-tyo" "Toimistotarvike- ja ICT-kulut, tiedotus, opastus, kokousten järjestäminen jne."]
+                                  ["laskutettava-tyo" "Hoitourakan työnjohto"]}))
+                         (doseq [[_ tehtavat] ryhmiteltyna]
+                           (testaa-ajat tehtavat toimenpide-avain)))))))
+    (testing "Johto ja hallintokorvaukset ovat oikein"
+      (let [johto-ja-hallintokorvaukset (group-by :toimenkuva (:johto-ja-hallintokorvaukset budjetoidut-tyot))]
+        (doseq [[toimenkuva tiedot] johto-ja-hallintokorvaukset]
+          (case toimenkuva
+            "hankintavastaava" (is (= (into #{}
+                                            (mapcat (fn [hoitokausi]
+                                                      [[hoitokausi :molemmat]])
+                                                    (range 0 6)))
+                                      (into #{} (keys (group-by (juxt :hoitokausi :maksukausi) tiedot)))))
+            "sopimusvastaava" (is (= (into #{}
+                                           (mapcat (fn [hoitokausi]
+                                                     [[hoitokausi :molemmat]])
+                                                   (range 1 6)))
+                                     (into #{} (keys (group-by (juxt :hoitokausi :maksukausi) tiedot)))))
+            "vastuunalainen työnjohtaja" (is (= (into #{}
+                                            (mapcat (fn [hoitokausi]
+                                                      [[hoitokausi :molemmat]])
+                                                    (range 1 6)))
+                                      (into #{} (keys (group-by (juxt :hoitokausi :maksukausi) tiedot)))))
+            "päätoiminen apulainen" (is (= (into #{}
+                                            (mapcat (fn [hoitokausi]
+                                                      [[hoitokausi :talvi]
+                                                       [hoitokausi :kesa]])
+                                                    (range 1 6)))
+                                      (into #{} (keys (group-by (juxt :hoitokausi :maksukausi) tiedot)))))
+            "apulainen/työnjohtaja" (is (= (into #{}
+                                            (mapcat (fn [hoitokausi]
+                                                      [[hoitokausi :talvi]
+                                                       [hoitokausi :kesa]])
+                                                    (range 1 6)))
+                                      (into #{} (keys (group-by (juxt :hoitokausi :maksukausi) tiedot)))))
+            "viherhoidosta vastaava henkilö" (is (= (into #{}
+                                            (mapcat (fn [hoitokausi]
+                                                      [[hoitokausi :molemmat]])
+                                                    (range 1 6)))
+                                      (into #{} (keys (group-by (juxt :hoitokausi :maksukausi) tiedot)))))
+            "harjoittelija" (is (= (into #{}
+                                            (mapcat (fn [hoitokausi]
+                                                      [[hoitokausi :molemmat]])
+                                                    (range 1 6)))
+                                      (into #{} (keys (group-by (juxt :hoitokausi :maksukausi) tiedot)))))))))))
