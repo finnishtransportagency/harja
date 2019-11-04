4ae49536e3e790ce1cba0e2eb9212a2df9a0c881
Jukka Länsineva <jukka.lansineva@solita.fi>
2019-11-04 14:37:16 +0200

    kalenterivalitsinta uudenlaista

--- src/cljs/harja/ui/pvm.cljs
+++ src/cljs/harja/ui/pvm.cljs
@@ -28,7 +28,7 @@
     (reset! sijainti-atom uusi-suunta)))
 
 (defn- pilko-viikoiksi [vuosi kk]
-  (let [kk (inc kk) ;; cljs-time käyttää luonnollisia kk numeroita
+  (let [kk (inc kk)                                         ;; cljs-time käyttää luonnollisia kk numeroita
         kk-alku (t/date-time vuosi kk 1)
         ;;_ (log "kk-alku on: " kk-alku)
         viikon-alku (loop [pvm (t/date-time vuosi kk 1)]
@@ -53,7 +53,6 @@
         (vec (partition 7 7 [] paivat))
         (recur (conj paivat p)
                (t/plus p (t/days 1)))))))
-
 
 (defn pvm-valintakalenteri
   "Luo uuden päivämäärävalinnan.
@@ -84,80 +83,93 @@
          (when (not pakota-suunta)
            (selvita-kalenterin-suunta this sijainti-atom)))}
 
-     (komp/dom-kuuntelija js/window
-                          EventType/SCROLL scroll-kuuntelija)
+      (komp/dom-kuuntelija js/window
+                           EventType/SCROLL scroll-kuuntelija)
 
-     (fn [{:keys [pvm valitse style valittava?-fn] :as optiot}]
-       (let [[vuosi kk] @nayta
-             naytettava-kk (t/date-time vuosi (inc kk) 1)
-             naytettava-kk-paiva? #(pvm/sama-kuukausi? naytettava-kk %)]
-         [:table.pvm-valinta {:style (merge
-                                      {:display (if @sijainti-atom "table" "none")
-                                       ;; Etenkin jos kalenteri avataan ylöspäin, on tärkeää, että korkeus pysyy vakiona
-                                       ;; Muuten otsikkorivi hyppii sen mukaan paljonko kuussa on päiviä.
-                                       :height "200px"}
-                                      (case @sijainti-atom
-                                        :ylos-oikea {:bottom "100%" :left 0}
-                                        :ylos-vasen {:bottom "100%" :right 0}
-                                        :alas-oikea {:top "100%" :left 0}
-                                        :alas-vasen {:top "100%" :right 0}
-                                        {}))}
-          [:thead.pvm-kontrollit
-           [:tr
-            [:td.pvm-edellinen-kuukausi.klikattava
-             {:on-click #(do (.preventDefault %)
-                             (swap! nayta
-                                    (fn [[vuosi kk]]
-                                      (if (= kk 0)
-                                        [(dec vuosi) 11]
-                                        [vuosi (dec kk)])))
-                             nil)}
-             (ikonit/livicon-chevron-left)]
-            [:td {:col-span 5} [:span.pvm-kuukausi (nth pvm/+kuukaudet+ kk)] " " [:span.pvm-vuosi vuosi]]
-            [:td.pvm-seuraava-kuukausi.klikattava
-             {:on-click #(do (.preventDefault %)
-                             (swap! nayta
-                                    (fn [[vuosi kk]]
-                                      (if (= kk 11)
-                                        [(inc vuosi) 0]
-                                        [vuosi (inc kk)])))
-                             nil)}
-             (ikonit/livicon-chevron-right)]]
-           [:tr.pvm-viikonpaivat
-            (for [paiva +paivat+]
-              ^{:key paiva}
-              [:td paiva])]]
+      (fn [{:keys [pvm valitse style valittava?-fn] :as optiot}]
+        (let [[vuosi kk] @nayta
+              naytettava-kk (t/date-time vuosi (inc kk) 1)
+              naytettava-kk-paiva? #(pvm/sama-kuukausi? naytettava-kk %)]
+          [:table.pvm-valinta {:style (merge
+                                        {:display (if @sijainti-atom "table" "none")
+                                         ;; Etenkin jos kalenteri avataan ylöspäin, on tärkeää, että korkeus pysyy vakiona
+                                         ;; Muuten otsikkorivi hyppii sen mukaan paljonko kuussa on päiviä.
+                                         :height  "200px"}
+                                        (case @sijainti-atom
+                                          :ylos-oikea {:bottom "100%" :left 0}
+                                          :ylos-vasen {:bottom "100%" :right 0}
+                                          :alas-oikea {:top "100%" :left 0}
+                                          :alas-vasen {:top "100%" :right 0}
+                                          {}))}
+           [:thead.pvm-kontrollit
+            [:tr
+             [:td.pvm-edellinen-kuukausi.klikattava
+              {:on-click #(do (.preventDefault %)
+                              (swap! nayta
+                                     (fn [[vuosi kk]]
+                                       (if (= kk 0)
+                                         [(dec vuosi) 11]
+                                         [vuosi (dec kk)])))
+                              nil)}
+              (ikonit/livicon-chevron-left)]
+             [:td {:col-span 5} [:span.pvm-kuukausi (nth pvm/+kuukaudet+ kk)] " " [:span.pvm-vuosi vuosi]]
+             [:td.pvm-seuraava-kuukausi.klikattava
+              {:on-click #(do (.preventDefault %)
+                              (swap! nayta
+                                     (fn [[vuosi kk]]
+                                       (if (= kk 11)
+                                         [(inc vuosi) 0]
+                                         [vuosi (inc kk)])))
+                              nil)}
+              (ikonit/livicon-chevron-right)]]
+            [:tr.pvm-viikonpaivat
+             (for [paiva +paivat+]
+               ^{:key paiva}
+               [:td paiva])]]
 
-          [:tbody.pvm-paivat
-           (for [paivat (pilko-viikoiksi vuosi kk)]
-             ^{:key (pvm/millisekunteina (first paivat))}
-             [:tr
-              (for [paiva paivat
-                    :let [valittava? (or (not (some? valittava?-fn))
-                                         (valittava?-fn paiva))]]
-                ^{:key (pvm/millisekunteina paiva)}
-                [:td.pvm-paiva {:class (str
-                                         (if valittava?
-                                           "klikattava "
-                                           "pvm-disabloitu ")
-                                         (when (pvm/sama-pvm? (pvm/nyt) paiva) "pvm-tanaan ")
-                                         (when (and pvm
-                                                    (= (t/day paiva) (t/day pvm))
-                                                    (= (t/month paiva) (t/month pvm))
-                                                    (= (t/year paiva) (t/year pvm)))
-                                           "pvm-valittu ")
-                                         (if (naytettava-kk-paiva? paiva)
-                                           "pvm-naytettava-kk-paiva" "pvm-muu-kk-paiva"))
+           [:tbody.pvm-paivat
+            (for [paivat (pilko-viikoiksi vuosi kk)]
+              ^{:key (pvm/millisekunteina (first paivat))}
+              [:tr
+               (for [paiva paivat
+                     :let [valittava? (or (not (some? valittava?-fn))
+                                          (valittava?-fn paiva))]]
+                 ^{:key (pvm/millisekunteina paiva)}
+                 [:td.pvm-paiva {:class    (str
+                                             (if valittava?
+                                               "klikattava "
+                                               "pvm-disabloitu ")
+                                             (when (pvm/sama-pvm? (pvm/nyt) paiva) "pvm-tanaan ")
+                                             (when (and pvm
+                                                        (= (t/day paiva) (t/day pvm))
+                                                        (= (t/month paiva) (t/month pvm))
+                                                        (= (t/year paiva) (t/year pvm)))
+                                               "pvm-valittu ")
+                                             (if (naytettava-kk-paiva? paiva)
+                                               "pvm-naytettava-kk-paiva" "pvm-muu-kk-paiva"))
 
-                                :on-click #(do (.stopPropagation %)
-                                               (when valittava?
-                                                 (valitse paiva))
-                                               nil)}
-                 (t/day paiva)])])]
-          [:tfoot.pvm-tanaan-text
-           [:tr [:td {:colSpan 7}
-                 [:a {:on-click #(do
-                                   (.preventDefault %)
-                                   (.stopPropagation %)
-                                   (valitse (pvm/nyt)))}
-                  "Tänään"]]]]])))))
+                                 :on-click #(do (.stopPropagation %)
+                                                (when valittava?
+                                                  (valitse paiva))
+                                                nil)}
+                  (t/day paiva)])])]
+           [:tfoot.pvm-tanaan-text
+            [:tr [:td {:colSpan 7}
+                  [:a {:on-click #(do
+                                    (.preventDefault %)
+                                    (.stopPropagation %)
+                                    (valitse (pvm/nyt)))}
+                   "Tänään"]]]]])))))
+
+(defn pvm-valintakalenteri-inputilla
+  [optiot]
+  (let [auki? (atom false)]
+    (fn [{:keys [pvm valitse valittava?-fn]}]
+      [:div
+       [:input.form-control {:type     :text
+                             :value    (when-not (nil? pvm) (str (.getDate pvm) "." (.getMonth pvm) "." (.getYear pvm)))
+                             :on-focus #(reset! auki? true)
+                             :on-blur  (fn [] (js/setTimeout #(reset! auki? false) 100))}]
+       (when @auki? [pvm-valintakalenteri {:valitse       valitse
+                                           :valittava?-fn #(true? true)
+                                           :pvm pvm}])])))
--- src/cljs/harja/views/urakka/kulut.cljs
+++ src/cljs/harja/views/urakka/kulut.cljs
@@ -12,7 +12,8 @@
             [harja.ui.modal :as modal]
             [harja.loki :refer [log]]
             [harja.loki :as loki]
-            [harja.ui.yleiset :as yleiset])
+            [harja.ui.yleiset :as yleiset]
+            [harja.ui.pvm :as pvm-valinta])
   (:require-macros [harja.ui.taulukko.tyokalut :refer [muodosta-taulukko]]))
 
 (defn- osien-paivitys-fn
@@ -33,7 +34,7 @@
 ;
 
 (defn alihankkija-modaali
-  [paivitys-fn {:keys [nimi y-tunnus] :as alihankkija}]
+  [paivitys-fn tallennus-fn sulku-fn {:keys [nimi ytunnus] :as alihankkija}]
   [:div
    [:h1 "Lisää alihankkija"]
    [:div
@@ -44,11 +45,18 @@
    [:div
     [:label "Y-tunnus"
      [:input {:type      :text
-              :value     y-tunnus
-              :on-change #(paivitys-fn (assoc alihankkija :y-tunnus (-> % .-target .-value)))}]]]])
+              :value     ytunnus
+              :on-change #(paivitys-fn (assoc alihankkija :ytunnus (-> % .-target .-value)))}]]]
+   [:div
+    [:input {:type     :button
+             :value    "Tallenna"
+             :on-click tallennus-fn}]
+    [:input {:type     :button
+             :value    "Sulje"
+             :on-click sulku-fn}]]])
 
 (defn- sijainti-modaali
-  [paivitys-fn {:keys [tie tie-patka-piste tieosa-alku etaisyys-alku tieosa-loppu etaisyys-loppu] :as sijainti}]
+  [paivitys-fn sulku-fn {:keys [tie tie-patka-piste tieosa-alku etaisyys-alku tieosa-loppu etaisyys-loppu] :as sijainti}]
   [:div
    [:h1 "Lisää sijainti"]
    [:div
@@ -57,44 +65,46 @@
              :value     tie
              :on-change #(paivitys-fn (assoc sijainti :tie (-> % .-target .-value)))}]]
    [:div
-    [:input {:type :radio
-             :value :tie
-             :name "tie-patka-piste"
+    [:input {:type     :radio
+             :value    :tie
+             :name     "tie-patka-piste"
              :on-click #(paivitys-fn (assoc sijainti :tie-patka-piste (-> % .-target .-value)))}]
     [:label "Valitse koko tie"]
-    [:input {:type :radio
-             :value :patka
-             :name "tie-patka-piste"
+    [:input {:type     :radio
+             :value    :patka
+             :name     "tie-patka-piste"
              :on-click #(paivitys-fn (assoc sijainti :tie-patka-piste (-> % .-target .-value)))}]
     [:label "Valitse tietty tienpätkä"]
-    [:input {:type :radio
-             :value :piste
-             :name "tie-patka-piste"
+    [:input {:type     :radio
+             :value    :piste
+             :name     "tie-patka-piste"
              :on-click #(paivitys-fn (assoc sijainti :tie-patka-piste (-> % .-target .-value)))}]
     [:label "Valitse piste"]]
    [:div
     [:label.col-xs-2 "Tieosa, alku"
      [:input.form-control {:type      :text
-              :value     tieosa-alku
-              :on-change #(paivitys-fn (assoc sijainti :tieosa-alku (-> % .-target .-value)))}]]
+                           :value     tieosa-alku
+                           :on-change #(paivitys-fn (assoc sijainti :tieosa-alku (-> % .-target .-value)))}]]
     [:label.col-xs-2 "Aloitusetäisyys, m"
      [:input.form-control {:type      :text
-              :value     etaisyys-alku
-              :on-change #(paivitys-fn (assoc sijainti :etaisyys-alku (-> % .-target .-value)))}]]
+                           :value     etaisyys-alku
+                           :on-change #(paivitys-fn (assoc sijainti :etaisyys-alku (-> % .-target .-value)))}]]
     [:span.col-xs-2 "->"]
     [:label.col-xs-2 "Tieosa, loppu"
      [:input.form-control {:type      :text
-              :value     tieosa-loppu
-              :on-change #(paivitys-fn (assoc sijainti :tieosa-loppu (-> % .-target .-value)))}]]
+                           :value     tieosa-loppu
+                           :on-change #(paivitys-fn (assoc sijainti :tieosa-loppu (-> % .-target .-value)))}]]
     [:label.col-xs-2 "Lopetusetäisyys, m"
      [:input.form-control {:type      :text
-              :value     etaisyys-loppu
-              :on-change #(paivitys-fn (assoc sijainti :etaisyys-loppu (-> % .-target .-value)))}]]]
+                           :value     etaisyys-loppu
+                           :on-change #(paivitys-fn (assoc sijainti :etaisyys-loppu (-> % .-target .-value)))}]]]
    [:div
-    [:input {:type :button
-             :value "Tallenna"}]
-    [:input {:type :button
-             :value "Sulje"}]]])
+    [:input {:type     :button
+             :value    "Tallenna"
+             :on-click sulku-fn}]
+    [:input {:type     :button
+             :value    "Sulje"
+             :on-click sulku-fn}]]])
 
 (defn- validoi
   [pakolliset objekti]
@@ -103,11 +113,10 @@
 (defn- kulujen-syottolomake
   [e! {:keys [toimenpiteet tehtavaryhmat] :as app}]
   (let [lomakkeen-tila (reagent.core/atom {:validi?               false
-                                           :nayta nil
+                                           :nayta                 nil
+                                           :alihankkijat          [(get-in @tila/yleiset [:urakka :urakoitsija])]
                                            :tehtavat-lkm          1
-                                           :tehtavat              [{:tehtava      nil
-                                                                    :tehtavaryhma nil
-                                                                    :sijainti     nil
+                                           :tehtavat              [{:tehtavaryhma nil
                                                                     :maara        nil}]
                                            :koontilaskun-kuukausi nil
                                            :koontilaskun-era      nil})
@@ -119,20 +128,22 @@
                                                     :sijainti     nil
                                                     :maara        nil})))
         paivitys-fn (fn [& polut-ja-arvot]
+                      (apply loki/log "päivitän " polut-ja-arvot)
                       (let [polut-ja-arvot (partition 2 polut-ja-arvot)]
                         (doseq
                           [[polku arvo] polut-ja-arvot]
                           (swap! lomakkeen-tila
                                  (if (vector? polku)
-                                   assoc-in
-                                   assoc)
+                                   (if (fn? arvo) update-in assoc-in)
+                                   (if (fn? arvo) update assoc))
                                  polku arvo))))]
     (fn [e! {:keys [syottomoodi]}]
-      (let [{:keys [tehtavat tehtavat-lkm validi? koontilaskun-kuukausi koontilaskun-era alihankkija]} @lomakkeen-tila
+      (let [{:keys [tehtavat tehtavat-lkm nayta validi? koontilaskun-kuukausi koontilaskun-pvm koontilaskun-era alihankkija alihankkijat]} @lomakkeen-tila
             validointi-fn (partial validoi #{:maara :koontilaskun-kuukausi})
             kuukaudet [:lokakuu :marraskuu :joulukuu :tammikuu :helmikuu :maaliskuu :huhtikuu :toukokuu :kesakuu :heinakuu :elokuu :syyskuu]
             erat [:era-1 :era-2 :era-3 :muu]]
         [:div
+         [debug/debug @tila/yleiset]
          [debug/debug @lomakkeen-tila]
          [:div.row
           [:h1 "Uusi kulu"]
@@ -140,7 +151,7 @@
                    :on-change #(swap! lomakkeen-tila lisaa-tehtava)}]
           [:label "Kulut kohdistuvat useammalle eri tehtävälle"]]
          (into [:div] (keep-indexed (fn [indeksi t]
-                                      (let [{:keys [tehtavaryhma sijainti maara]} t]
+                                      (let [{:keys [tehtavaryhma maara]} t]
                                         [:div.lomake-rivi
                                          [:div.row
                                           [:div.col-xs-12.col-sm-6.label-ja-alasveto
@@ -149,9 +160,15 @@
                                                                          :valitse-fn #(paivitys-fn [:tehtavat indeksi :tehtavaryhma] %)
                                                                          :format-fn  #(get % :tehtavaryhma)}
                                             tehtavaryhmat]]
-                                          [:div.col-xs-12.col-sm-6
-                                           [:label "Ilmoita sijainti"]
-                                           [sijainti-modaali (partial paivitys-fn [:tehtavat indeksi :sijainti]) sijainti]]]
+                                          #_[:div.col-xs-12.col-sm-6
+                                           [:input {:type     :button
+                                                    :value    "Ilmoita missä sijainnissa työ tehtiin"
+                                                    :on-click #(paivitys-fn :nayta :sijainti-modaali)}]
+                                           (when (= nayta :sijainti-modaali)
+                                             [sijainti-modaali
+                                              (partial paivitys-fn [:tehtavat indeksi :sijainti])
+                                              #(paivitys-fn :nayta nil)
+                                              sijainti])]]
 
                                          (when (> tehtavat-lkm 1)
                                            [:div.row
@@ -166,56 +183,71 @@
            [:div.row
             [:div.col-xs-12.col-sm-6 {:on-click #(swap! lomakkeen-tila lisaa-tehtava)} "+ lisää juttu"]])
          [:div.row
-          [:div.col-xs-12.col-sm-6.label-ja-alasveto
-           [:label "Koontilaskun kuukausi"]
-           [yleiset/livi-pudotusvalikko {:valinta    koontilaskun-kuukausi
-                                         :valitse-fn #(paivitys-fn :koontilaskun-kuukausi %)
-                                         :format-fn  #(str "- " %)}
-            kuukaudet]]
-          [:div.col-xs-12.col-sm-6.label-ja-alasveto
-           [:label "Alihankkija"]
-           [yleiset/livi-pudotusvalikko {:valinta    :eka
-                                         :valitse-fn #(paivitys-fn :suorittaja %)
-                                         :format-fn  #(str "- " %)}
-            [:eka :toka]]]]
-         [:div.row
-          [:div.col-xs-12.col-sm-6.label-ja-alasveto
-           [:label "Koontilaskun erä"]
-           [yleiset/livi-pudotusvalikko {:valinta    koontilaskun-era
-                                         :valitse-fn #(paivitys-fn :koontilaskun-era %)
-                                         :format-fn  #(str "- " %)}
-            erat]]
-          [:div.col-xs-12.col-sm-6
-           [:label "Suorittajan y-tunnus"]
-           [alihankkija-modaali (partial paivitys-fn [:alihankkija]) alihankkija]
-           [:input.form-control
-            {:type      :text
-             :value  (:y-tunnus alihankkija)
-             :on-change #(paivitys-fn :suorittajan-ytunnus (-> % .-target .-value))}]]]
-         [:div.row
-          [:div.col-xs-12.col-sm-6
-           [:label "Laskun viite"]
-           [:input.form-control
-            {:type      :text
-             :on-change #(paivitys-fn :laskun-viite (-> % .-target .-value))}]]
-          [:div.col-xs-12.col-sm-6
-           [:label "Kirjoita tähän halutessasi lisätietoa"]
-           [:input.form-control
-            {:type      :text
-             :on-change #(paivitys-fn :lisatieto (-> % .-target .-value))}]]]
-         [:div.row
-          [:div.col-xs-12.col-sm-6
-           [:label "Laskun numero"]
-           [:input.form-control
-            {:type      :text
-             :on-change #(paivitys-fn :laskun-numero (-> % .-target .-value))}]]]
-         (when (< tehtavat-lkm 2)
-           [:div.row
-            [:div.col-xs-12.col-sm-6
-             [:label "Määrä"]
-             [:input.form-control
-              {:type      :text
-               :on-change #(paivitys-fn [:tehtavat 0 :maara] (-> % .-target .-value js/parseFloat))}]]])
+          [:div.col-sm-6.col-xs-12
+           [:h2 "Koontilaskun tiedot"]
+           [:div.col-xs-12
+            [:label "Koontilaskun kuukausi"]
+            [yleiset/livi-pudotusvalikko {:valinta    koontilaskun-kuukausi
+                                          :valitse-fn #(paivitys-fn :koontilaskun-kuukausi %)
+                                          :format-fn  #(str "- " %)}
+             kuukaudet]]
+           [:div.col-xs-12
+            [:label "Laskun pvm"]
+            [pvm-valinta/pvm-valintakalenteri-inputilla {:valitse       #(paivitys-fn :koontilaskun-pvm %)
+                                                         :pvm           koontilaskun-pvm
+                                                         :pakota-suunta false
+                                                         :valittava?-fn #(true? true)}]
+            #_[yleiset/livi-pudotusvalikko {:valinta    koontilaskun-era
+                                          :valitse-fn #(paivitys-fn :koontilaskun-era %)
+                                          :format-fn  #(str "- " %)}
+             erat]]
+           [:div.col-xs-12
+            [:label "Laskun viite"]
+            [:input.form-control
+             {:type      :text
+              :on-change #(paivitys-fn :laskun-viite (-> % .-target .-value))}]]
+           [:div.col-xs-12
+            [:label "Laskun numero"]
+            [:input.form-control
+             {:type      :text
+              :on-change #(paivitys-fn :laskun-numero (-> % .-target .-value))}]]
+           (when (< tehtavat-lkm 2)
+             [:div.row
+              [:div.col-xs-12
+               [:label "Määrä"]
+               [:input.form-control
+                {:type      :text
+                 :on-change #(paivitys-fn [:tehtavat 0 :maara] (-> % .-target .-value js/parseFloat))}]]])]
+          [:div.col-sm-6.col-xs-12
+           [:h2 "Lisätiedot"]
+           [:div.col-xs-12
+            [:label "Alihankkija"]
+            [yleiset/livi-pudotusvalikko {:valinta    alihankkija
+                                          :valitse-fn #(paivitys-fn :alihankkija %)
+                                          :format-fn  #(get % :nimi)}
+             alihankkijat]]
+           [:div.col-xs-12
+            [:label "Alihankkijan y-tunnus"]
+            (when (= nayta nayta                             ;:alihankkija-modaali
+                     )
+              [alihankkija-modaali
+               (fn [arvo]
+                 (paivitys-fn
+                   :alihankkija arvo))
+               (fn [] (paivitys-fn :alihankkijat
+                                   (fn [alihankkijat]
+                                     (conj alihankkijat alihankkija))))
+               #(paivitys-fn :nayta nil)
+               alihankkija])
+            [:input.form-control
+             {:type      :text
+              :value     (:ytunnus alihankkija)
+              :on-change #(paivitys-fn :ytunnus (-> % .-target .-value))}]]
+           [:div.col-xs-12
+            [:label "Kirjoita tähän halutessasi lisätietoa"]
+            [:input.form-control
+             {:type      :text
+              :on-change #(paivitys-fn :lisatieto (-> % .-target .-value))}]]]]
          [:input {:type     :button
                   :value    "Tallenna"
                   :on-click #(e! (tiedot/->TallennaKulu @lomakkeen-tila))}]
