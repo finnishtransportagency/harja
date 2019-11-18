4b19dd929782b1ca329518974a3df04e881af27a
Maarit Laine <maarit.laine@solita.fi>
2019-11-18 15:00:27 +0200

    Siirrä osa taulumuokkauksista myöhempään migraatioon.

--- tietokanta/src/main/resources/db/migration/V1_792__.sql
+++ tietokanta/src/main/resources/db/migration/V1_792__.sql
@@ -11,10 +11,3 @@ ALTER TABLE johto_ja_hallintokorvaus
   ADD COLUMN kuukausi INTEGER,
   ADD COLUMN "ennen-urakkaa-id" INTEGER REFERENCES johto_ja_hallintokorvaus_ennen_urakkaa (id);
 
-ALTER TABLE johto_ja_hallintokorvaus
-    DROP COLUMN "kk-v",
-    DROP COLUMN maksukausi,
-    DROP COLUMN hoitokausi,
-    ALTER COLUMN vuosi SET NOT NULL,
-    ALTER COLUMN kuukausi SET NOT NULL,
-    ADD CONSTRAINT uniikki_johto_ja_hallintokorvaus UNIQUE ("urakka-id", "toimenkuva-id", vuosi, kuukausi, "ennen-urakkaa-id");
