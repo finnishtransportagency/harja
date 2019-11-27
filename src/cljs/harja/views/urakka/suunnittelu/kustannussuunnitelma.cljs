d02b1a79da3a449d9ef3346e3b3fcd441f6b8c98
Joonas-M-S <joonas.mikkonen@solita.fi>
2019-11-27 15:29:52 +0200

    Laske myös rahavarausten indeksikorjaukset

--- src/cljs/harja/views/urakka/suunnittelu/kustannussuunnitelma.cljs
+++ src/cljs/harja/views/urakka/suunnittelu/kustannussuunnitelma.cljs
@@ -1023,6 +1023,7 @@
                                                                                                 :on-blur (fn [arvo]
                                                                                                            (when arvo
                                                                                                              (e! (t/->MuutaTaulukonOsanSisarta osa/*this* "Yhteensä" polku-taulukkoon (str (* 12 arvo))))
+                                                                                                             (e! (t/->MuutaTaulukonOsanSisarta osa/*this* "Indeksikorjattu" polku-taulukkoon (str (t/indeksikorjaa (* 12 arvo)))))
                                                                                                              (e! (t/->TallennaKustannusarvoituTyo (tyyppi->tallennettava-asia tyyppi) toimenpide-avain arvo nil))
                                                                                                              (e! (t/->PaivitaKustannussuunnitelmanYhteenvedot))
                                                                                                              (go (>! kaskytyskanava [:tavoite-ja-kattohinta (t/->TallennaJaPaivitaTavoiteSekaKattohinta)]))))
