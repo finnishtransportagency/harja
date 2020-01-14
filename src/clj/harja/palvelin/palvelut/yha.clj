85ea33fb97d4ab778822afd1b6a293816cbb042d
Maarit Laine <maarit.laine@solita.fi>
2020-01-14 12:28:24 +0200

    Estä 2020 edeltävien kohteiden lähettäminen YHA:an.

--- src/clj/harja/palvelin/palvelut/yha.clj
+++ src/clj/harja/palvelin/palvelut/yha.clj
@@ -183,7 +183,8 @@
 
 (defn- tarkista-lahetettavat-kohteet
   "Tarkistaa, että kaikki annetut kohteet ovat siinä tilassa, että ne voidaan lähettää.
-   Jos ei ole, heittää poikkeuksen."
+   Jos ei ole, heittää poikkeuksen. Vuotta 2020 edeltäviä kohteita ei alkuvuoden kaistauudistuksen jälkeen
+   voi enää lähettää."
   [db kohde-idt]
   (doseq [kohde-id kohde-idt]
     (let [paallystysilmoitus (first (into []
@@ -195,7 +196,8 @@
                                           (paallystys-q/hae-paallystysilmoitus-kohdetietoineen-paallystyskohteella
                                             db
                                             {:paallystyskohde kohde-id})))]
-      (when-not (and (= :hyvaksytty (get-in paallystysilmoitus [:tekninen-osa :paatos]))
+      (when-not (and (every? #(> % 2019) (konv/pgarray->vector(:vuodet paallystysilmoitus)))
+                     (= :hyvaksytty (get-in paallystysilmoitus [:tekninen-osa :paatos]))
                      (or (= :valmis (:tila paallystysilmoitus))
                          (= :lukittu (:tila paallystysilmoitus))))
         (throw (SecurityException. (str "Kohteen " kohde-id " päällystysilmoituksen lähetys ei ole sallittu.")))))))
