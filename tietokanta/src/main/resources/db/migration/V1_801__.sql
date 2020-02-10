5c3bfc771423af08fc6efec41a26fe17de88a431
maaritlaine <maarit.laine@solita.fi>
2020-02-10 12:39:14 +0200

    Lisää uudet sanktiolajit.

--- tietokanta/src/main/resources/db/migration/V1_801__.sql
+++ tietokanta/src/main/resources/db/migration/V1_801__.sql
@@ -1,10 +1,12 @@
-INSERT INTO sanktiotyyppi(nimi, sanktiolaji, urakkatyyppi)
-VALUES
- ('Lupaussanktio', ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'C'::sanktiolaji, 'muistutus'::sanktiolaji],
-   ARRAY['teiden-hoito']::urakkatyyppi[]),
-  ('Vastuuhenkilöiden vaihtosanktio', ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'C'::sanktiolaji, 'muistutus'::sanktiolaji],
-   ARRAY['teiden-hoito']::urakkatyyppi[]),
-  ('Sanktio vastuuhenkilöiden testikeskiarvon laskemisesta', ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'C'::sanktiolaji, 'muistutus'::sanktiolaji],
-   ARRAY['teiden-hoito']::urakkatyyppi[]),
-  ('Sanktio vastuuhenkilöiden tenttikeskiarvon laskemisesta*', ARRAY['A'::sanktiolaji, 'B'::sanktiolaji, 'C'::sanktiolaji, 'muistutus'::sanktiolaji],
-   ARRAY['teiden-hoito']::urakkatyyppi[]);
+-- Hoitourakoiden sanktiolajit ovat käytössä myös teiden hoidon urakoissa (MHU).
+UPDATE sanktiotyyppi SET urakkatyyppi = '{hoito,teiden-hoito}' WHERE urakkatyyppi = '{hoito}';
+
+-- MHU-urakoissa on myös neljä omaa sanktiolajiaan:
+ALTER TYPE sanktiolaji ADD VALUE 'lupaussanktio';
+ALTER TYPE sanktiolaji ADD VALUE 'vaihtosanktio';
+ALTER TYPE sanktiolaji ADD VALUE 'testikeskiarvo-sanktio';
+ALTER TYPE sanktiolaji ADD VALUE 'tenttikeskiarvo-sanktio';
+
+-- Dummy-sanktiotyyppi teiden hoidon sanktiolajeille, jotka eivät varsinaisesti liity mihinkään sanktiotyyppiin.
+INSERT INTO sanktiotyyppi (nimi, toimenpidekoodi, urakkatyyppi, sanktiolaji) VALUES ('Ei sanktiotyyppiä', null, '{teiden-hoito}', '{lupaussanktio, vaihtosanktio, testikeskiarvo-sanktio, tenttikeskiarvo-sanktio}');
+
