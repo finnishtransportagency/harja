9936fd87a245fa98406c34d27c4d73b20e7c4728
maaritlaine <maarit.laine@solita.fi>
2020-02-10 09:02:09 +0200

    Konversio mukaan transduceriin ja empty-tarkastus.

--- src/clj/harja/palvelin/palvelut/yha.clj
+++ src/clj/harja/palvelin/palvelut/yha.clj
@@ -189,14 +189,15 @@
   (doseq [kohde-id kohde-idt]
     (let [paallystysilmoitus (first (into []
                                           (comp (map konv/alaviiva->rakenne)
-                                                (map #(konv/string-poluista->keyword
-                                                        %
-                                                        [[:tekninen-osa :paatos]
-                                                         [:tila]])))
+                                                (map #(konv/pgarray->vector (:vuodet %)))
+                                                (map #(konv/string-poluista->keyword %
+                                                                                     [[:tekninen-osa :paatos]
+                                                                                      [:tila]])))
                                           (paallystys-q/hae-paallystysilmoitus-kohdetietoineen-paallystyskohteella
                                             db
                                             {:paallystyskohde kohde-id})))]
-      (when-not (and (every? #(> % 2019) (konv/pgarray->vector(:vuodet paallystysilmoitus)))
+      (when-not (and (not (empty? (:vuodet paallystysilmoitus)))
+                     (every? #(> % 2019) (:vuodet paallystysilmoitus))
                      (= :hyvaksytty (get-in paallystysilmoitus [:tekninen-osa :paatos]))
                      (or (= :valmis (:tila paallystysilmoitus))
                          (= :lukittu (:tila paallystysilmoitus))))
