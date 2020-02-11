ad014524f97875525f1374513eaed403d93853c2
maaritlaine <maarit.laine@solita.fi>
2020-02-11 16:18:53 +0200

    Korjaa migraatio.

--- tietokanta/src/main/resources/db/migration/V1_801__.sql
+++ tietokanta/src/main/resources/db/migration/V1_801__.sql
@@ -1,12 +1,5 @@
--- Hoitourakoiden sanktiolajit ovat käytössä myös teiden hoidon urakoissa (MHU).
-UPDATE sanktiotyyppi SET urakkatyyppi = '{hoito,teiden-hoito}' WHERE urakkatyyppi = '{hoito}';
-
 -- MHU-urakoissa on myös neljä omaa sanktiolajiaan:
 ALTER TYPE sanktiolaji ADD VALUE 'lupaussanktio';
 ALTER TYPE sanktiolaji ADD VALUE 'vaihtosanktio';
 ALTER TYPE sanktiolaji ADD VALUE 'testikeskiarvo-sanktio';
 ALTER TYPE sanktiolaji ADD VALUE 'tenttikeskiarvo-sanktio';
-
--- Dummy-sanktiotyyppi teiden hoidon sanktiolajeille, jotka eivät varsinaisesti liity mihinkään sanktiotyyppiin.
-INSERT INTO sanktiotyyppi (nimi, toimenpidekoodi, urakkatyyppi, sanktiolaji) VALUES ('Ei sanktiotyyppiä', null, '{teiden-hoito}', '{lupaussanktio, vaihtosanktio, testikeskiarvo-sanktio, tenttikeskiarvo-sanktio}');
-
