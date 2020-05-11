512ea848005b42ee6c4da64089526d4267f20c68
Joonas-M-S <joonas.mikkonen@solita.fi>
2020-05-11 03:02:41 +0300

    Ota -l lippu pois. Se näyttää dbt ja sitten exitoi

--- Jenkins/skriptit/testitietokanta.sh
+++ Jenkins/skriptit/testitietokanta.sh
@@ -12,7 +12,7 @@ cd tietokanta
 
 mvn clean compile flyway:migrate -Dflyway.url=jdbc:postgresql://localhost/harjatest_template -Dflyway.user=harjatest -Dflyway.password=$(cat ~/.pgpass | sed 's/.*harjatest://')
 
-PGOPTIONS='--client-min-messages=warning' psql -h localhost -U harjatest harjatest_template  -X -q -a -l -v ON_ERROR_STOP=1 --pset pager=off -f testidata.sql
+PGOPTIONS='--client-min-messages=warning' psql -h localhost -U harjatest harjatest_template  -X -q -a -v ON_ERROR_STOP=1 --pset pager=off -f testidata.sql
 
 psql -h localhost -U harjatest template1 -c "DROP DATABASE IF EXISTS harjatest;"
 psql -h localhost -U harjatest template1 -c "CREATE DATABASE harjatest OWNER harjatest TEMPLATE harjatest_template;"
