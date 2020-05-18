de60f97ad1e21404a10b6c58f4b2d573b482748c
Joonas-M-S <joonas.mikkonen@solita.fi>
2020-05-18 16:44:10 +0300

    Lisää bitumi indeksi päällystys- ja paikkausurakoille. Siirrä V1_817__.sql migraatio oikeaan kansioon ja nimeä V1_823__.sql

---
+++ tietokanta/src/main/resources/db/migration/V1_822__.sql
@@ -0,0 +1,4 @@
+INSERT INTO urakkatyypin_indeksi(urakkatyyppi, indeksinimi, koodi, raakaaine)
+VALUES
+  ('paallystys'::urakkatyyppi, 'Bitumi 2015', 'BITUMI2015', 'bitumi'),
+  ('paikkaus'::urakkatyyppi, 'Bitumi 2015', 'BITUMI2015', 'bitumi');
