ac02f2ff2441f5399534d7eb360a4a9a8347c6bd
Markus Vähäkangas <markus.vahakangas@solita.fi>
2020-05-26 10:26:27 +0300

    Korjattu mhu excel raportin yhteenveto
    Taulukolle oli annettu 2 saman nimistä elementtiä.

--- src/clj/harja/palvelin/raportointi/raportit/laskutusyhteenveto_mhu.clj
+++ src/clj/harja/palvelin/raportointi/raportit/laskutusyhteenveto_mhu.clj
@@ -95,123 +95,144 @@
   [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Hankinnat")
-    [:varillinen-teksti {:arvo (:hankinnat_laskutettu tp-rivi) #_(or 100 (summa-fmt nil))
-                         :fmt (when (:hankinnat_laskutettu tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (or (:hankinnat_laskutettu tp-rivi) (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
       [:varillinen-teksti {:arvo (or (:hankinnat_laskutetaan tp-rivi) (summa-fmt nil))
-
-                           :fmt (when (:hankinnat_laskutetaan tp-rivi) :raha)}])))
+                           :fmt :raha}])))
 
 (defn- lisatyot
   [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Lisätyöt")
     [:varillinen-teksti {:arvo (or (:lisatyot_laskutettu tp-rivi) (summa-fmt nil))
-                         :fmt (when (:lisatyot_laskutettu tp-rivi) :raha)}]
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
       [:varillinen-teksti {:arvo (or (:lisatyot_laskutetaan tp-rivi) (summa-fmt nil))
-                           :fmt (when (:lisatyot_laskutetaan tp-rivi) :raha)}])))
+                           :fmt :raha}])))
 
 (defn- sanktiot
   [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Sanktiot")
-    [:varillinen-teksti {:arvo (or (:sakot_laskutettu tp-rivi) (summa-fmt nil))
-                         :fmt (when (:sakot_laskutettu tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (if (:sakot_laskutettu tp-rivi)
+                                 (fmt/formatoi-arvo-raportille (:sakot_laskutettu tp-rivi))
+                                 (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
-      [:varillinen-teksti {:arvo (or (:sakot_laskutetaan tp-rivi) (summa-fmt nil))
-                           :fmt (when (:sakot_laskutetaan tp-rivi) :raha)}])))
-;; TODO: laskentalogiikat täysin tekemättä
+      [:varillinen-teksti {:arvo (if (:sakot_laskutetaan tp-rivi)
+                                   (fmt/formatoi-arvo-raportille (:sakot_laskutetaan tp-rivi))
+                                   (summa-fmt nil))
+                           :fmt :raha}])))
+
 (defn- suolasanktiot
   [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Suolasanktiot")
-    [:varillinen-teksti {:arvo (or (:suolasakot_laskutettu tp-rivi) (summa-fmt nil))
-                         :fmt (when (:suolasakot_laskutettu tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (if (:suolasakot_laskutettu tp-rivi)
+                                 (fmt/formatoi-arvo-raportille (:suolasakot_laskutettu tp-rivi))
+                                 (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
       [:varillinen-teksti {:arvo (or (:suolasakot_laskutetaan tp-rivi) (summa-fmt nil))
-                           :fmt (when (:suolasakot_laskutetaan tp-rivi) :raha)}])))
+                           :fmt :raha}])))
 
 (defn- johto-hallintakorvaukset
   [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Johto- ja hallintakorvaukset")
-    [:varillinen-teksti {:arvo (:johto_ja_hallinto_laskutettu tp-rivi) #_(or 100 (summa-fmt nil))
-                         :fmt (when (:johto_ja_hallinto_laskutettu tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (or (:johto_ja_hallinto_laskutettu tp-rivi) (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
       [:varillinen-teksti {:arvo (or (:johto_ja_hallinto_laskutetaan tp-rivi) (summa-fmt nil))
-
-                           :fmt (when (:johto_ja_hallinto_laskutetaan tp-rivi) :raha)}])))
+                           :fmt :raha}])))
 
 (defn- erillishankinnat
   [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Erillishankinnat")
-    [:varillinen-teksti {:arvo (:hj_erillishankinnat_laskutettu tp-rivi) #_(or 100 (summa-fmt nil))
-                         :fmt (when (:hj_erillishankinnat_laskutettu tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (or (:hj_erillishankinnat_laskutettu tp-rivi)  (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
-      [:varillinen-teksti {:arvo (or (summa-fmt (:hj_erillishankinnat_laskutetaan tp-rivi)) (summa-fmt nil))
-
-                           :fmt (when (summa-fmt (:hj_erillishankinnat_laskutetaan tp-rivi)) :raha)}])))
+      [:varillinen-teksti {:arvo (or (:hj_erillishankinnat_laskutetaan tp-rivi) (summa-fmt nil))
+                           :fmt :raha}])))
 
 (defn- hj-palkkio
   [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "HJ-palkkio")
-    [:varillinen-teksti {:arvo (:hj_palkkio_laskutettu tp-rivi) #_(or 100 (summa-fmt nil))
-                         :fmt (when (:hj_palkkio_laskutettu tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (or (:hj_palkkio_laskutettu tp-rivi) (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
-      [:varillinen-teksti {:arvo (or (summa-fmt (:hj_palkkio_laskutetaan tp-rivi)) (summa-fmt nil))
-
-                           :fmt (when (summa-fmt (:hj_palkkio_laskutetaan tp-rivi)) :raha)}])))
+      [:varillinen-teksti {:arvo (or (:hj_palkkio_laskutetaan tp-rivi) (summa-fmt nil))
+                           :fmt :raha} ])))
 
 (defn- bonukset
   [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Bonukset")
-    [:varillinen-teksti {:arvo (:bonukset_laskutettu tp-rivi) #_(or 100 (summa-fmt nil))
-                         :fmt (when (:bonukset_laskutettu tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (if (:bonukset_laskutettu tp-rivi)
+                                 (fmt/formatoi-arvo-raportille (:bonukset_laskutettu tp-rivi))
+                                 (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
-      [:varillinen-teksti {:arvo (or (:bonukset_laskutetaan tp-rivi) (summa-fmt nil))
-
-                           :fmt (when (:bonukset_laskutetaan tp-rivi) :raha)}])))
+      [:varillinen-teksti {:arvo (if (:bonukset_laskutetaan tp-rivi)
+                                   (fmt/formatoi-arvo-raportille (:bonukset_laskutetaan tp-rivi))
+                                   (summa-fmt nil))
+                           :fmt :raha}])))
 
 (defn- yhteensa
   [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Yhteensä")
-    [:varillinen-teksti {:arvo (or (:kaikki_laskutettu tp-rivi) (summa-fmt nil))
-                         :fmt (when (:kaikki_laskutettu tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (if (:kaikki_laskutettu tp-rivi)
+                                 (fmt/formatoi-arvo-raportille (:kaikki_laskutettu tp-rivi))
+                                 (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
-      [:varillinen-teksti {:arvo (or (:kaikki_laskutetaan tp-rivi) (summa-fmt nil))
-                           :fmt (when (:kaikki_laskutetaan tp-rivi) :raha)}])))
+      [:varillinen-teksti {:arvo (if (:kaikki_laskutetaan tp-rivi)
+                                   (fmt/formatoi-arvo-raportille (:kaikki_laskutetaan tp-rivi))
+                                   (summa-fmt nil))
+                           :fmt :raha}])))
 
 (defn- toteutuneet-yhteensa [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Toteutuneet kustannukset yhteensä")
-    [:varillinen-teksti {:arvo (or (:kaikki-yhteensa-laskutettu tp-rivi) (summa-fmt nil))
-                         :fmt (when (:kaikki-yhteensa-laskutettu tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (if (:kaikki-yhteensa-laskutettu tp-rivi)
+                                 (fmt/formatoi-arvo-raportille (:kaikki-yhteensa-laskutettu tp-rivi))
+                                 (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
-      [:varillinen-teksti {:arvo (or (:kaikki-yhteensa-laskutetaan tp-rivi) (summa-fmt nil))
-                           :fmt (when (:kaikki-yhteensa-laskutetaan tp-rivi) :raha)}])))
+      [:varillinen-teksti {:arvo (if (:kaikki-yhteensa-laskutetaan tp-rivi)
+                                   (fmt/formatoi-arvo-raportille (:kaikki-yhteensa-laskutetaan tp-rivi))
+                                   (summa-fmt nil))
+                           :fmt :raha}])))
 
 (defn- tavoitteeseen-kuuluvat-toteutuneet-yhteensa [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Toteutuneet kustannukset, jotka kuuluvat tavoitehintaan")
-    [:varillinen-teksti {:arvo (or (:kaikki-tavoitehintaiset-laskutettu tp-rivi) (summa-fmt nil))
-                         :fmt (when (:kaikki-tavoitehintaiset-laskutettu tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (if (:kaikki-tavoitehintaiset-laskutettu tp-rivi)
+                                 (fmt/formatoi-arvo-raportille (:kaikki-tavoitehintaiset-laskutettu tp-rivi))
+                                 (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
-      [:varillinen-teksti {:arvo (or (:kaikki-tavoitehintaiset-laskutetaan tp-rivi) (summa-fmt nil))
-                           :fmt (when (:kaikki-tavoitehintaiset-laskutetaan tp-rivi) :raha)}])))
+      [:varillinen-teksti {:arvo (if (:kaikki-tavoitehintaiset-laskutetaan tp-rivi)
+                                   (fmt/formatoi-arvo-raportille (:kaikki-tavoitehintaiset-laskutetaan tp-rivi))
+                                   (summa-fmt nil))
+                           :fmt :raha}])))
 
 (defn- tavoitehinta [tp-rivi kyseessa-kk-vali?]
   (rivi
     (str "Tavoite / Jäljellä")
-    [:varillinen-teksti {:arvo (or (:tavoite-hinta tp-rivi) (summa-fmt nil))
-                         :fmt (when (:tavoite-hinta tp-rivi) :raha)}]
+    [:varillinen-teksti {:arvo (if (:tavoite-hinta tp-rivi)
+                                 (fmt/formatoi-arvo-raportille (:tavoite-hinta tp-rivi))
+                                 (summa-fmt nil))
+                         :fmt :raha}]
     (when kyseessa-kk-vali?
-      [:varillinen-teksti {:arvo (or (:jaljella tp-rivi) (summa-fmt nil))
-                           :fmt (when (:jaljella tp-rivi) :raha)}])))
+      [:varillinen-teksti {:arvo (if (:jaljella tp-rivi)
+                                   (fmt/formatoi-arvo-raportille (:jaljella tp-rivi))
+                                   (summa-fmt nil))
+                           :fmt :raha}])))
 
 (defn- taulukko [{:keys [tp-rivi laskutettu-teksti laskutetaan-teksti
                          kyseessa-kk-vali?]}]
@@ -307,7 +328,6 @@
                                                kaikki-tuotteittain))
         tiedot (into []
                      (map #(merge {:nimi (key %)} (val %)) kaikki-tuotteittain-summattuna))
-        _ (println "tiedot" (pr-str tiedot))
         yhteenveto (koosta-yhteenveto tiedot)
         tavoite (koosta-tavoite tiedot urakka-tavoite)
         koostettu-yhteenveto (conj [] yhteenveto tavoite)]
@@ -333,16 +353,15 @@
                         :laskutetaan-teksti kuukausi-otsikko
                         :kyseessa-kk-vali? kyseessa-kk-vali?})))))
      [:teksti ""]
-     (lyv-yhteiset/aseta-sheet-nimi
-       (concat
-         (for [tp-rivi koostettu-yhteenveto
-               :let [yhteensa-otsikko (if (not (= (:nimi tp-rivi) "Tavoite")) laskutettu-teksti "Tavoitehinta")
-                     kuukausi-otsikko (if (not (= (:nimi tp-rivi) "Tavoite")) laskutetaan-teksti "Jäljellä")]]
-           (do
-             (taulukko {:tp-rivi tp-rivi
-                        :laskutettu-teksti yhteensa-otsikko
-                        :laskutetaan-teksti kuukausi-otsikko
-                        :kyseessa-kk-vali? kyseessa-kk-vali?})))))
+     (concat
+       (for [tp-rivi koostettu-yhteenveto
+             :let [yhteensa-otsikko (if (not (= (:nimi tp-rivi) "Tavoite")) laskutettu-teksti "Tavoitehinta")
+                   kuukausi-otsikko (if (not (= (:nimi tp-rivi) "Tavoite")) laskutetaan-teksti "Jäljellä")]]
+         (do
+           (taulukko {:tp-rivi tp-rivi
+                      :laskutettu-teksti yhteensa-otsikko
+                      :laskutetaan-teksti kuukausi-otsikko
+                      :kyseessa-kk-vali? kyseessa-kk-vali?}))))
 
 
      (when (and hallintayksikko-id (= 0 (count urakat)))
