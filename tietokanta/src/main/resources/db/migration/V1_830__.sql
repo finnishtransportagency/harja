40548e1e6d926d1d74eeb2f186e3da7e944b233f
Janne Nissinen <janne.nissinen@solita.fi>
2020-06-15 10:39:07 +0300

    Lisätty tietokantamalli ja testidata urakkakohtaisille massoille

---
+++ tietokanta/src/main/resources/db/migration/V1_830__.sql
@@ -0,0 +1,27 @@
+CREATE TABLE paallystysmassa
+(
+    id                SERIAL PRIMARY KEY,
+    urakka            INTEGER REFERENCES urakka (id),
+
+    massatyyppitunnus TEXT,
+    raekoko           INTEGER,
+    nimi              TEXT,
+    rc                NUMERIC,
+
+--Kivi- ja sideaines
+    esiintyma         TEXT,
+    km_arvo           TEXT,
+    muotoarvo         TEXT,
+    sideainetyyppi    TEXT,
+    pitoisuus         NUMERIC,
+    lisaaineet        TEXT,
+
+-- muokkausmetatiedot
+    poistettu         BOOLEAN   DEFAULT FALSE,
+    muokkaaja         INTEGER REFERENCES kayttaja (id),
+    muokattu          TIMESTAMP,
+    luoja             INTEGER REFERENCES kayttaja (id),
+    luotu             TIMESTAMP DEFAULT NOW()
+);
+
+CREATE INDEX paallystysmassa_urakka_idx ON paallystysmassa (urakka);
---
+++ tietokanta/testidata/yllapito/paallystysmassat.sql
@@ -0,0 +1,8 @@
+INSERT INTO paallystysmassa (urakka, massatyyppitunnus, raekoko, nimi, rc, esiintyma, km_arvo, muotoarvo,
+                             sideainetyyppi, pitoisuus, lisaaineet, luoja)
+VALUES ((SELECT id from urakka WHERE nimi = 'Utajärven päällystysurakka'), 'AB-16', 16,
+        'Alfattibetoni', null, 'Kaislakallio', 'AN14', 20, '70/100', 5.40, 'Pippuria ja suolaa',
+        (SELECT id FROM kayttaja WHERE kayttajanimi = 'skanska')),
+       ((SELECT id from urakka WHERE nimi = 'Utajärven päällystysurakka'), 'SMA 16', 16,
+        'Kivimastiksiasfaltti', null, 'Karjukallio', 'AN7', 10, '70/100', 5.80, 'Chiliä ja Currya',
+        (SELECT id FROM kayttaja WHERE kayttajanimi = 'skanska'));
