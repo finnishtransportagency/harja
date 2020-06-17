8fc8e65d841de6b89a861372d721411ccc403eef
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2020-06-17 13:07:04 +0300

    VHAR-2489 Korjaa siltatarkastusten haun SQL

--- src/clj/harja/palvelin/raportointi/raportit/siltatarkastus.sql
+++ src/clj/harja/palvelin/raportointi/raportit/siltatarkastus.sql
@@ -1,82 +1,39 @@
 -- name: hae-urakan-siltatarkastukset
 -- Hakee urakan kaikki sillat ja niiden annettuna vuonna tehdyn uusimman siltatarkastuksen
 SELECT
-  s.id,
-  siltanro,
-  siltanimi,
-  (SELECT tarkastusaika
-   FROM siltatarkastus st
-   WHERE st.silta = s.id
-         AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
-         AND st.poistettu = FALSE
-   ORDER BY tarkastusaika DESC
-   LIMIT 1),
-  (SELECT tarkastaja
-   FROM siltatarkastus st
-   WHERE st.silta = s.id
-         AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
-         AND st.poistettu = FALSE
-   ORDER BY tarkastusaika DESC
-   LIMIT 1),
-  (SELECT COUNT(*)
-   FROM siltatarkastuskohde
-   WHERE tulos && '{A}'
-         AND siltatarkastus = (SELECT id
-                               FROM siltatarkastus st
-                               WHERE st.silta = s.id
-                                     AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
-                                     AND st.poistettu = FALSE
-                               ORDER BY tarkastusaika DESC
-                               LIMIT 1)) AS "a",
-  (SELECT COUNT(*)
-   FROM siltatarkastuskohde
-   WHERE tulos && '{B}'
-         AND siltatarkastus = (SELECT id
-                               FROM siltatarkastus st
-                               WHERE st.silta = s.id
-                                     AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
-                                     AND st.poistettu = FALSE
-                               ORDER BY tarkastusaika DESC
-                               LIMIT 1)) AS "b",
-  (SELECT COUNT(*)
-   FROM siltatarkastuskohde
-   WHERE tulos && '{C}'
-         AND siltatarkastus = (SELECT id
-                               FROM siltatarkastus st
-                               WHERE st.silta = s.id
-                                     AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
-                                     AND st.poistettu = FALSE
-                               ORDER BY tarkastusaika DESC
-                               LIMIT 1)) AS "c",
-  (SELECT COUNT(*)
-   FROM siltatarkastuskohde
-   WHERE tulos && '{D}'
-         AND siltatarkastus = (SELECT id
-                               FROM siltatarkastus st
-                               WHERE st.silta = s.id
-                                     AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
-                                     AND st.poistettu = FALSE
-                               ORDER BY tarkastusaika DESC
-                               LIMIT 1)) AS "d",
-  l.id AS liite_id,
-  l.tyyppi AS liite_tyyppi,
-  l.koko AS liite_koko,
-  l.nimi AS liite_nimi
-FROM silta s
-  LEFT JOIN liite l ON l.id IN (SELECT id
-                                FROM
-                                  liite l
-                                  JOIN siltatarkastus_kohde_liite skl ON l.id = skl.liite
-                                WHERE skl.siltatarkastus IN (SELECT id
-                                                             FROM siltatarkastus st
-                                                             WHERE st.silta = s.id
-                                                                   AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
-                                                                   AND st.poistettu = FALSE
-                                                                   AND urakka = :urakka
-                                                             ORDER BY tarkastusaika DESC
-                                                             LIMIT 1))
-WHERE s.urakat @> ARRAY[:urakka] ::INT[]
-ORDER BY siltanro;
+    s.id,
+    s.siltanro,
+    s.siltanimi,
+    st.tarkastusaika,
+    st.tarkastaja,
+    (SELECT COUNT(*)
+       FROM siltatarkastuskohde
+      WHERE tulos && '{A}' AND siltatarkastus = st.id) AS "a",
+    (SELECT COUNT(*)
+       FROM siltatarkastuskohde
+      WHERE tulos && '{B}' AND siltatarkastus = st.id) AS "b",
+    (SELECT COUNT(*)
+       FROM siltatarkastuskohde
+      WHERE tulos && '{C}' AND siltatarkastus = st.id) AS "c",
+    (SELECT COUNT(*)
+       FROM siltatarkastuskohde
+      WHERE tulos && '{D}' AND siltatarkastus = st.id) AS "d",
+    l.id AS liite_id,
+    l.tyyppi AS liite_tyyppi,
+    l.koko AS liite_koko,
+    l.nimi AS liite_nimi
+  FROM silta s
+           LEFT JOIN siltatarkastus st ON st.id in (SELECT id
+                                                      FROM siltatarkastus st
+                                                     WHERE st.silta = s.id
+                                                       AND EXTRACT(YEAR FROM tarkastusaika) = :vuosi
+                                                       AND st.poistettu = FALSE
+                                                     ORDER BY tarkastusaika DESC
+                                                     LIMIT 1)
+           LEFT JOIN siltatarkastus_kohde_liite skl ON skl.siltatarkastus IN (st.id)
+           LEFT JOIN liite l ON skl.liite = l.id
+ WHERE s.urakat @> ARRAY[:urakka] ::INT[]
+ ORDER BY siltanro;
 
 -- name: hae-sillan-tarkastus
 SELECT
