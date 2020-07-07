4ea5334b0a58490d9c4b6dfa531c0a5c766f6be2
maaritlaine <maarit.laine@solita.fi>
2020-07-07 08:41:18 +0300

    Päivitä testit.

--- src/clj/harja/palvelin/raportointi/raportit/materiaali.clj
+++ src/clj/harja/palvelin/raportointi/raportit/materiaali.clj
@@ -145,7 +145,7 @@
                                (for [m materiaaliotsikot]
                                 (reduce + (keep :kokonaismaara (toteumat-materiaalin-mukaan m)))))))]))))]
      (when-not (empty? toteumat)
-       [:teksti (str "Formiaatteja ei lasketa talvisuolan kokonaiskäyttöön. ")])
+       [:teksti (str "Formiaatteja ei lasketa talvisuolan kokonaiskäyttöön. \n")])
      [:teksti (str yleinen/materiaalitoteumien-paivitysinfo)]]))
 
     
