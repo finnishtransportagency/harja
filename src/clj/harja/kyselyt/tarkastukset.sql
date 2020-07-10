eac830069315c73b3702039c14e6389b14dda82b
Janne Nissinen <janne.nissinen@solita.fi>
2020-07-10 13:55:15 +0300

    Tiestötarkastusraportti nyt huomioi 'teiden-hoito' tyypin.

--- src/clj/harja/kyselyt/tarkastukset.sql
+++ src/clj/harja/kyselyt/tarkastukset.sql
@@ -450,7 +450,10 @@ FROM tarkastus t
 WHERE t.urakka IN (SELECT id
                    FROM urakka
                    WHERE hallintayksikko = :hallintayksikko
-                         AND (:urakkatyyppi IS NULL OR tyyppi = :urakkatyyppi :: urakkatyyppi))
+                         AND (:urakkatyyppi IS NULL OR (
+                            CASE WHEN :urakkatyyppi = 'hoito' THEN tyyppi IN ('hoito', 'teiden-hoito')
+                            ELSE tyyppi = :urakkatyyppi::urakkatyyppi
+                            END)))
       AND (t.aika >= :alku AND t.aika <= :loppu)
       AND (:rajaa_tienumerolla = FALSE OR t.tr_numero = :tienumero)
       AND t.tyyppi = 'tiesto' :: tarkastustyyppi
@@ -489,7 +492,10 @@ FROM tarkastus t
   LEFT JOIN liite ON tarkastus_liite.liite = liite.id
 WHERE t.urakka IN (SELECT id
                    FROM urakka
-                   WHERE (:urakkatyyppi :: urakkatyyppi IS NULL OR tyyppi = :urakkatyyppi :: urakkatyyppi))
+                   WHERE (:urakkatyyppi :: urakkatyyppi IS NULL OR (
+                       CASE WHEN :urakkatyyppi = 'hoito' THEN tyyppi IN ('hoito', 'teiden-hoito')
+                       ELSE tyyppi = :urakkatyyppi::urakkatyyppi
+                       END)))
       AND (t.aika >= :alku AND t.aika <= :loppu)
       AND (:rajaa_tienumerolla = FALSE OR t.tr_numero = :tienumero)
       AND t.tyyppi = 'tiesto' :: tarkastustyyppi
