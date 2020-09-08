14713d8356d56db279a193dcaeb6d0083ba79b34
maaritlaine <maarit.laine@solita.fi>
2020-09-08 09:24:43 +0300

    Käytä voimassaoloa urakan kokonaishintaisissa tehtävissä.

--- src/clj/harja/kyselyt/urakan_toimenpiteet.sql
+++ src/clj/harja/kyselyt/urakan_toimenpiteet.sql
@@ -77,32 +77,37 @@ WHERE t4.taso = 4 AND
 --name: hae-urakan-kokonaishintaiset-toimenpiteet-ja-tehtavat
 -- Hakee kaikki urakan 3. ja 4. tason toimenpiteet jotka ovat kokonaishintaisia
 SELECT
-  tpi.id               AS tpi_id,
-  tpi.nimi             AS tpi_nimi,
-  t4.id                AS t4_id,
-  t4.koodi             AS t4_koodi,
-  t4.nimi              AS t4_nimi,
-  t4.yksikko           AS t4_yksikko,
-  t4.hinnoittelu       AS t4_hinnoittelu,
-  t4.jarjestys         AS t4_jarjestys,
-  t3.id                AS t3_id,
-  t3.koodi             AS t3_koodi,
-  t3.nimi              AS t3_nimi,
-  t2.id                AS t2_id,
-  t2.koodi             AS t2_koodi,
-  t2.nimi              AS t2_nimi,
-  t1.id                AS t1_id,
-  t1.koodi             AS t1_koodi,
-  t1.nimi              AS t1_nimi
+    tpi.id               AS tpi_id,
+    tpi.nimi             AS tpi_nimi,
+    t4.id                AS t4_id,
+    t4.koodi             AS t4_koodi,
+    t4.nimi              AS t4_nimi,
+    t4.yksikko           AS t4_yksikko,
+    t4.hinnoittelu       AS t4_hinnoittelu,
+    t4.voimassaolo_alkuvuosi AS t4_voimassaolo_alkuvuosi,
+    t4.voimassaolo_loppuvuosi AS t4_voimassaolo_loppuvuosi,
+    t4.jarjestys         AS t4_jarjestys,
+    t3.id                AS t3_id,
+    t3.koodi             AS t3_koodi,
+    t3.nimi              AS t3_nimi,
+    t2.id                AS t2_id,
+    t2.koodi             AS t2_koodi,
+    t2.nimi              AS t2_nimi,
+    t1.id                AS t1_id,
+    t1.koodi             AS t1_koodi,
+    t1.nimi              AS t1_nimi
 FROM toimenpidekoodi t4
-  LEFT JOIN toimenpidekoodi t3 ON t3.id = t4.emo
-  LEFT JOIN toimenpidekoodi t2 ON t2.id = t3.emo
-  LEFT JOIN toimenpidekoodi t1 ON t1.id = t2.emo
-  LEFT JOIN toimenpideinstanssi tpi ON t3.id = tpi.toimenpide
+         LEFT JOIN toimenpidekoodi t3 ON t3.id = t4.emo
+         LEFT JOIN toimenpidekoodi t2 ON t2.id = t3.emo
+         LEFT JOIN toimenpidekoodi t1 ON t1.id = t2.emo
+         LEFT JOIN toimenpideinstanssi tpi ON t3.id = tpi.toimenpide
+         JOIN urakka u on tpi.urakka = u.id
 WHERE t4.taso = 4 AND
-      t4.hinnoittelu @> ARRAY['kokonaishintainen'::hinnoittelutyyppi] AND
-      t4.poistettu = FALSE AND
-      tpi.urakka = :urakka;
+        t4.hinnoittelu @> ARRAY['kokonaishintainen'::hinnoittelutyyppi] AND
+        t4.poistettu = FALSE AND
+    (t4.voimassaolo_alkuvuosi IS NULL OR t4.voimassaolo_alkuvuosi <= date_part('year', u.alkupvm)::INTEGER) AND
+    (t4.voimassaolo_loppuvuosi IS NULL OR t4.voimassaolo_loppuvuosi >= date_part('year', u.alkupvm)::INTEGER) AND
+        tpi.urakka = :urakka;
 
 -- name: hae-urakan-yksikkohintaiset-toimenpiteet-ja-tehtavat
 -- Hakee kaikki urakan 3. ja 4. tason toimenpiteet jotka eivät ole kokonaishintaisia
@@ -112,6 +117,8 @@ SELECT
   t4.nimi              AS t4_nimi,
   t4.yksikko           AS t4_yksikko,
   t4.hinnoittelu       AS t4_hinnoittelu,
+  t4.voimassaolo_alkuvuosi AS t4_voimassaolo_alkuvuosi,
+  t4.voimassaolo_loppuvuosi AS t4_voimassaolo_loppuvuosi,
   t4.jarjestys         AS t4_jarjestys,
   t3.id                AS t3_id,
   t3.koodi             AS t3_koodi,
