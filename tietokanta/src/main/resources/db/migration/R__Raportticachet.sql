f9fc58421c22d960ad6dfcf2aedd0ec473b96a85
Markus Vähäkangas <markus.vahakangas@solita.fi>
2020-09-10 11:21:52 +0300

    Poista ylimääräinen index.
    Ja nopeuta raportti cachejen päivitystä järjestämällä toteutuneet_materiaalit viewin hakutaulut eri järjestykseen.

--- tietokanta/src/main/resources/db/migration/R__Raportticachet.sql
+++ tietokanta/src/main/resources/db/migration/R__Raportticachet.sql
@@ -25,16 +25,13 @@ WITH NO DATA;
 
 DROP MATERIALIZED VIEW raportti_toteutuneet_materiaalit;
 CREATE MATERIALIZED VIEW raportti_toteutuneet_materiaalit AS
-SELECT
-    SUM(tm.maara)                AS kokonaismaara,
-    u.id                         AS "urakka-id",
-    mk.id                        AS "materiaali-id",
-    date_trunc('day', t.alkanut) AS paiva
-  FROM toteuma t
-           JOIN toteuma_materiaali tm ON t.id = tm.toteuma
-      AND tm.poistettu IS NOT TRUE
-           LEFT JOIN materiaalikoodi mk ON mk.id = tm.materiaalikoodi
-           JOIN urakka u ON u.id = t.urakka
- WHERE t.poistettu IS NOT TRUE
- GROUP BY "urakka-id", paiva, "materiaali-id"
+SELECT SUM(tm.maara)                AS kokonaismaara,
+       t.urakka                     AS "urakka-id",
+       mk.id                        AS "materiaali-id",
+       date_trunc('day', t.alkanut) AS paiva
+    FROM toteuma_materiaali tm
+             JOIN toteuma t ON t.id = tm.toteuma AND t.poistettu IS NOT TRUE
+             LEFT JOIN materiaalikoodi mk ON mk.id = tm.materiaalikoodi
+    WHERE tm.poistettu IS NOT TRUE
+    GROUP BY "urakka-id", paiva, "materiaali-id"
 WITH NO DATA;
