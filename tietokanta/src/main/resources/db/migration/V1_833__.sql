9619e21c78383f7adc16b44d92013700a29f588b
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2020-09-11 09:26:01 +0300

    Poista logitukset toteuman update triggeristä

--- tietokanta/src/main/resources/db/migration/V1_833__.sql
+++ tietokanta/src/main/resources/db/migration/V1_833__.sql
@@ -1,18 +1,10 @@
 CREATE OR REPLACE FUNCTION update_toteuma_check_partition()
     RETURNS TRIGGER AS $$
 BEGIN
-    RAISE NOTICE 'update_toteuma_check_partition start for toteuma : %', NEW;
-    RAISE NOTICE 'update_toteuma_check_partition start for toteuma id: %', NEW.id;
-    RAISE NOTICE 'TG_TABLE_NAME %', TG_TABLE_NAME;
-    RAISE NOTICE 'format %', format('DELETE FROM %I.%I WHERE id = %L::INTEGER;',
-        TG_TABLE_SCHEMA, TG_TABLE_NAME, NEW.id);
-
     EXECUTE format('DELETE FROM %I.%I WHERE id = %L::INTEGER;',
         TG_TABLE_SCHEMA, TG_TABLE_NAME, NEW.id);
     INSERT INTO toteuma VALUES (NEW.*);
-    RAISE NOTICE 'Siirto uuteen partitioon OK';
     RETURN NULL;
-
 END;
 $$
     LANGUAGE plpgsql;
