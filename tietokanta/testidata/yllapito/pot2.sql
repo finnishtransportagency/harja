bf73f532cde0bd8f6fc678c77e8888dc1908f11b
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2020-09-30 08:24:23 +0300

    Päivitä päällystyksen testidatoja
    Mm. jatka Muhoksen urakkaa jotta on yhä käynnissä

---
+++ tietokanta/testidata/yllapito/pot2.sql
@@ -0,0 +1,43 @@
+-- Replikoidaan olemassa oleva potti vanhasta tietomallista
+-- Kenties päästään tekemään niiden pohjalta aikanaan myös
+-- hyviä testejä, esim. että YHA:an lähtevä hyötykuorma on identtinen jne.
+
+INSERT INTO pot2(
+    yllapitokohde,
+    takuupvm,
+    tila,
+    paatos_tekninen_osa,
+    lahetetty_yhaan,
+    poistettu,
+    muokkaaja,
+    muokattu,
+    luoja,
+    luotu)
+VALUES
+((SELECT id FROM yllapitokohde WHERE nimi = 'Ouluntie'),
+ '2022-12-31',
+ 'aloitettu' ::paallystystila,
+ NULL,
+ FALSE,
+ FALSE,
+ NULL,
+ NULL,
+ (SELECT id FROM kayttaja WHERE nimi = 'Skanska Asfaltti Oy'));
+
+INSERT INTO pot2_massa(
+    urakka_id,
+    nimi,
+    massatyyppi,
+    max_raekoko,
+    asfalttiasema,
+    kuulamyllyluokka,
+    litteyslukuluokka,
+    dop_nro,
+    poistettu,
+    muokkaaja,
+    muokattu,
+    luoja,
+    luotu) VALUES (
+                      (SELECT id FROM urakka WHERE nimi = 'Utajärven päällystysurakka',
+                   '')
+                 )
