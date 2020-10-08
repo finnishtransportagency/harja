e134db4df5c2d6b92bd877c29045fa61d95c4cc5
Markus Vähäkangas <markus.vahakangas@solita.fi>
2020-10-08 08:05:05 +0300

    Käytetään lyhennettyjä kolumnin nimiä, jotta tiedon siirto serverin ja clientin välillä pienenee

--- src/cljs/harja/views/urakka/toteumat/maarien_toteumat.cljs
+++ src/cljs/harja/views/urakka/toteumat/maarien_toteumat.cljs
@@ -31,7 +31,8 @@
 (defn- laske-prosentti
   "Olettaa saavansa molemmat parametrit big arvoina."
   [toteuma suunniteltu]
-  (if (or (= (big/->big 0) toteuma)
+  (if (or (big/eq (big/->big 0) toteuma)
+          (and (big/eq (big/->big 0) toteuma) (big/eq (big/->big 0) suunniteltu))
           (nil? toteuma))
     0
     (big/fmt (big/mul (big/->big 100) (big/div suunniteltu toteuma)) 2)))
@@ -66,7 +67,7 @@
 (defn maarita-yksikko [rivi]
   (cond
     (= (:tyyppi rivi) "kokonaishintainen")
-    (:yksikko rivi)
+    (:yk rivi)
 
     (or (= (:tyyppi rivi) "lisatyo")
         (= (:tyyppi rivi) "akillinen-hoitotyo")
@@ -94,16 +95,16 @@
                                   (let [_ (reset! row-index-atom (inc @row-index-atom))
                                         avattava? true
                                         nayta-nuoli? (or
-                                                       (:toteuma_aika (first (second rivi)))
-                                                       (:kasin-lisattava? (first (second rivi))))
+                                                       (:taika (first (second rivi)))
+                                                       (:k? (first (second rivi))))
                                         lapsi-rivit (if (and
                                                           (= (get-in app [:valittu-rivi]) (first rivi))
-                                                          (not (nil? (:toteutunut (first (second rivi))))))
+                                                          (not (nil? (:tot (first (second rivi))))))
                                                       (second rivi)
                                                       nil)
                                         toteutunut-maara (reduce big/plus (big/->big 0)
-                                                                 (keep #(big/->big (:toteutunut %)) (second rivi)))
-                                        suunniteltu-maara (big/->big (:suunniteltu_maara (first (second rivi))))
+                                                                 (keep #(big/->big (:tot %)) (second rivi)))
+                                        suunniteltu-maara (big/->big (:skpl (first (second rivi))))
                                         {:keys [tyyppi]} (first (second rivi))]
                                     (concat
                                       [^{:key (hash rivi)}
@@ -125,22 +126,22 @@
                                                                                            "---")
                                                                                          (str (if (big/gt suunniteltu-maara (big/->big -1))
                                                                                                 (big/fmt suunniteltu-maara 1)
-                                                                                                0) " " (:yksikko (first (second rivi)))))]
+                                                                                                0) " " (:yk (first (second rivi)))))]
                                         [:td {:style {:width (:prosentti leveydet)}} (if (big/eq (big/->big -1) suunniteltu-maara)
                                                                                        (case tyyppi
                                                                                          "kokonaishintainen" [:span.tila-virhe (ikonit/exclamation-sign)]
                                                                                          "---")
                                                                                        (str (laske-prosentti
-                                                                                              (big/->big (:suunniteltu_maara (first (second rivi))))
+                                                                                              (big/->big (:skpl (first (second rivi))))
                                                                                               toteutunut-maara) " %"))]]]
 
                                       ;; "+ Lisää toteuma" rivi - jos rivi on auki ja jos tehtävämäärän/toimenpiteen tehtävälle on tietokantaan sallittu käsin lisäys
                                       (when (and
                                               (= (get-in app [:valittu-rivi]) (first rivi))
-                                              (= true (:kasin-lisattava? (first (second rivi)))))
+                                              (= true (:k? (first (second rivi)))))
                                         [^{:key (str "lisää-toteuma-" (hash rivi))}
                                          [:tr {:class (str "table-default-" (if (odd? @row-index-atom) "even" "odd"))}
-                                          [:td {:style {:width (:tehtava leveydet)}} [lisaa-toteuma-linkki e! app (first rivi) (:tehtavaryhma (first (second rivi)))]]
+                                          [:td {:style {:width (:tehtava leveydet)}} [lisaa-toteuma-linkki e! app (first rivi) (:tryh (first (second rivi)))]]
                                           [:td {:style {:width (:caret leveydet)}} ""]
                                           [:td {:style {:width (:toteuma leveydet)}} ""]
                                           [:td {:style {:width (:suunniteltu leveydet)}} ""]
@@ -153,9 +154,9 @@
                                             (let [_ (reset! row-index-atom (inc @row-index-atom))]
                                               [^{:key (hash lapsi)}
                                                [:tr {:class (str "table-default-" (if (odd? @row-index-atom) "even" "odd"))}
-                                                [:td {:style {:width (:tehtava leveydet)}} [muokkaa-toteumaa-linkki e! (:toteuma_aika lapsi) (:toteuma_id lapsi)]]
+                                                [:td {:style {:width (:tehtava leveydet)}} [muokkaa-toteumaa-linkki e! (:taika lapsi) (:tid lapsi)]]
                                                 [:td {:style {:width (:caret leveydet)}} ""]
-                                                [:td {:style {:width (:toteuma leveydet)}} (str (big/fmt (big/->big (:toteutunut lapsi)) 2) " " (maarita-yksikko lapsi))]
+                                                [:td {:style {:width (:toteuma leveydet)}} (str (big/fmt (big/->big (:tot lapsi)) 2) " " (maarita-yksikko lapsi))]
                                                 [:td {:style {:width (:suunniteltu leveydet)}} "---"]
                                                 [:td {:style {:width (:prosentti leveydet)}} "---"]]]))
                                           lapsi-rivit)
@@ -167,9 +168,9 @@
                                            (do
                                              (reset! row-index-atom (inc @row-index-atom))
                                              [:tr {:class (str "table-default-" (if (odd? @row-index-atom) "even" "odd"))}
-                                              [:td {:style {:width (:tehtava leveydet)}} [muokkaa-toteumaa-linkki e! (:toteuma_aika (first (second rivi))) (:toteuma_id (first (second rivi)))]]
+                                              [:td {:style {:width (:tehtava leveydet)}} [muokkaa-toteumaa-linkki e! (:taika (first (second rivi))) (:tid (first (second rivi)))]]
                                               [:td {:style {:width (:caret leveydet)}} ""]
-                                              [:td {:style {:width (:toteuma leveydet)}} (str (big/fmt (big/->big (:toteutunut (first (second rivi)))) 2) " " (maarita-yksikko (first (second rivi))))]
+                                              [:td {:style {:width (:toteuma leveydet)}} (str (big/fmt (big/->big (:tot (first (second rivi)))) 2) " " (maarita-yksikko (first (second rivi))))]
                                               [:td {:style {:width (:suunniteltu leveydet)}} "---"]
                                               [:td {:style {:width (:prosentti leveydet)}} "---"]])])))))
                                 rivit)]
