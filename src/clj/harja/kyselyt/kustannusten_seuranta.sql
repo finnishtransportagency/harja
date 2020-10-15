efe514223509c946a3f86c425199c51f5f28be10
Markus Vähäkangas <markus.vahakangas@solita.fi>
2020-10-15 09:36:08 +0300

    Hae kustannuksille tehtävän nimi, jos se on annettu
    Ja lisää päivämäärähaku

--- src/clj/harja/kyselyt/kustannusten_seuranta.sql
+++ src/clj/harja/kyselyt/kustannusten_seuranta.sql
@@ -6,6 +6,7 @@ SELECT tpi.id AS toimenpideinstanssi_id,
        0 AS toteutunut_summa,
        tk.koodi,
        kt.tyyppi::TEXT AS maksutyyppi,
+       tk_tehtava.nimi AS tehtava_nimi,
        CASE
            WHEN tk.koodi = '23104' THEN 'Talvihoito'
            WHEN tk.koodi = '23116' THEN 'Liikenneympäristön hoito'
@@ -14,14 +15,17 @@ SELECT tpi.id AS toimenpideinstanssi_id,
            WHEN tk.koodi = '20191' THEN 'MHU Ylläpito'
            WHEN tk.koodi = '14301' THEN 'MHU Korvausinvestointi'
            WHEN tk.koodi = '23151' THEN 'MHU Hoidonjohto'
-           END AS toimenpide
-       -- kt.vuosi, kt.kuukausi, kt.summa, kt.tyyppi
+       END AS toimenpide,
+       kt.luotu AS luotu,
+       concat(kt.vuosi,'-',kt.kuukausi,'-01') AS ajankohta
     FROM toimenpidekoodi tk,
+         kustannusarvioitu_tyo kt
+            LEFT JOIN toimenpidekoodi tk_tehtava ON tk_tehtava.id = kt.tehtava,
          toimenpideinstanssi tpi,
-         kustannusarvioitu_tyo kt,
          sopimus s
     WHERE s.urakka = :urakka
       AND kt.sopimus = s.id
+      AND (concat(kt.vuosi,'-',kt.kuukausi,'-01')::DATE BETWEEN :alkupvm::DATE AND :loppupvm::DATE)
       AND kt.toimenpideinstanssi = tpi.id
       AND tpi.toimenpide = tk.id
       AND (tk.koodi = '23104' -- talvihoito
@@ -32,7 +36,7 @@ SELECT tpi.id AS toimenpideinstanssi_id,
         OR tk.koodi = '14301' -- mhu-korvausinvestointi
         OR tk.koodi = '23151' -- mhu-johto
         )
-UNION
+UNION ALL
 -- Toteutuneet kustannukset
 SELECT tpi.id AS toimenpideinstanssi_id,
        tk.nimi AS toimenpidekoodi_nimi,
@@ -40,6 +44,7 @@ SELECT tpi.id AS toimenpideinstanssi_id,
        lk.summa AS toteutunut_summa,
        tk.koodi,
        lk.maksueratyyppi::TEXT AS maksutyyppi,
+       tk_tehtava.nimi AS tehtava_nimi,
        CASE
            WHEN tk.koodi = '23104' THEN 'Talvihoito'
            WHEN tk.koodi = '23116' THEN 'Liikenneympäristön hoito'
@@ -48,21 +53,23 @@ SELECT tpi.id AS toimenpideinstanssi_id,
            WHEN tk.koodi = '20191' THEN 'MHU Ylläpito'
            WHEN tk.koodi = '14301' THEN 'MHU Korvausinvestointi'
            WHEN tk.koodi = '23151' THEN 'MHU Hoidonjohto'
-           END AS toimenpide
-       -- kt.vuosi, kt.kuukausi, kt.summa, kt.tyyppi
-    FROM lasku_kohdistus lk,
+           END AS toimenpide,
+           lk.luotu AS luotu,
+           l.erapaiva::TEXT AS ajankohta
+    FROM lasku_kohdistus lk
+            LEFT JOIN toimenpidekoodi tk_tehtava ON tk_tehtava.id = lk.tehtava,
          toimenpideinstanssi tpi,
          toimenpidekoodi tk,
          lasku l
     WHERE l.urakka = :urakka
+      AND l.erapaiva BETWEEN :alkupvm::DATE AND :loppupvm::DATE
       AND lk.lasku = l.id
       AND lk.toimenpideinstanssi = tpi.id
       AND tpi.toimenpide = tk.id
       AND (tk.koodi = '23104' OR tk.koodi = '23116'
         OR tk.koodi = '23124' OR tk.koodi = '20107' OR tk.koodi = '20191' OR
            tk.koodi = '14301' OR tk.koodi = '23151')
-ORDER BY toimenpideinstanssi_id ASC
-    LIMIT 200;
+ORDER BY toimenpideinstanssi_id ASC;
 
 -- name: hae-kustannusten-seurannan-toimenpiteet
 -- Toimenpiteet menee joka paikassa ristiin. Tässä tarkoitetaan kovakoodattua listaa,
