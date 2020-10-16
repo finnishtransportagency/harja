cf5dfae656b55963ec0e9fbbab103ea580789129
Jukka Länsineva <jukka.lansineva@solita.fi>
2020-10-16 15:17:25 +0300

    päivämäärien laskenta oikein korjattu

--- src/clj/harja/palvelin/raportointi/raportit/kulut_tehtavaryhmittain.clj
+++ src/clj/harja/palvelin/raportointi/raportit/kulut_tehtavaryhmittain.clj
@@ -2,10 +2,10 @@
   (:require
     [clojure.string :as cstr]
     [harja.kyselyt
-             [urakat :as urakat-q]
-             [laskut :as kulut-q]
-             [budjettisuunnittelu :as budjetti-q]]
-            [harja.pvm :as pvm]))
+     [urakat :as urakat-q]
+     [laskut :as kulut-q]
+     [budjettisuunnittelu :as budjetti-q]]
+    [harja.pvm :as pvm]))
 
 
 (defn- laske-hoitokaudet
@@ -40,23 +40,62 @@
                    :tavoitehinta
                    (+ %1)) 0 hoitokaudet))))
 
+(def piste->pilkku
+  (comp (map str)
+        (map #(cstr/replace % #"(\d+)\.(\d+)" "$1,$2"))))
+
+(def muuta-pilkut-ja-poista-id
+  (map (fn [k]
+         (rest
+           (transduce
+             piste->pilkku
+             conj
+             k)))))
+
 (defn- kulut-urakalle
   [db {:keys [alkupvm urakka-id loppupvm]}]
   (let [kuukausi-valittu? (pvm/kyseessa-kk-vali? alkupvm loppupvm)
         hoitokausi-valittu? (pvm/kyseessa-hoitokausi-vali? alkupvm loppupvm)
         alkupvm-valittu-kuu-tai-vali (cond
-                                       hoitokausi-valittu? (pvm/kuukauden-ensimmainen-paiva (pvm/nyt))
+                                       hoitokausi-valittu? (pvm/kuukauden-ensimmainen-paiva alkupvm)
                                        :default alkupvm)
         loppupvm-valittu-kuu-tai-vali (cond
-                                        hoitokausi-valittu? (pvm/kuukauden-viimeinen-paiva (pvm/nyt))
+                                        hoitokausi-valittu? (pvm/kuukauden-viimeinen-paiva loppupvm)
                                         :default loppupvm)
-        alkupvm-hoitokausi (if (pvm/ennen? loppupvm (pvm/hoitokauden-loppupvm (pvm/vuosi loppupvm)))
-                             (pvm/hoitokauden-alkupvm (dec (pvm/vuosi loppupvm)))
-                             (pvm/hoitokauden-alkupvm (pvm/vuosi loppupvm)))
-        loppupvm-hoitokausi (let [loppu (pvm/hoitokauden-loppupvm (inc (pvm/vuosi alkupvm-hoitokausi)))]
-                              (if (pvm/jalkeen? loppu (pvm/nyt))
-                                (pvm/nyt)
-                                loppu))
+        alkupvm-hoitokausi
+        (cond
+          hoitokausi-valittu?
+          alkupvm
+
+          (pvm/ennen? alkupvm
+                      (pvm/hoitokauden-alkupvm
+                        (pvm/vuosi alkupvm)))
+          (-> alkupvm
+              pvm/vuosi
+              dec
+              pvm/hoitokauden-alkupvm)
+
+          (pvm/sama-tai-jalkeen? alkupvm
+                                 (pvm/hoitokauden-alkupvm
+                                   (pvm/vuosi alkupvm)))
+          (-> alkupvm
+              pvm/vuosi
+              pvm/hoitokauden-alkupvm))
+
+        loppupvm-hoitokausi
+        (cond
+          (and hoitokausi-valittu?
+               (pvm/sama-tai-ennen? loppupvm (pvm/nyt))) loppupvm
+
+          (and hoitokausi-valittu?
+               (pvm/jalkeen? loppupvm (pvm/nyt))) (pvm/nyt)
+
+          (pvm/jalkeen? loppupvm (pvm/nyt))
+          (pvm/nyt)
+
+          (pvm/sama-tai-ennen? loppupvm (pvm/nyt))
+          loppupvm)
+
         rivit-hoitokauden-alusta (mapv #(->
                                           [(:jarjestys %)
                                            (:nimi %)
@@ -106,13 +145,6 @@
                              (+ (nth %1 2) (nth %2 3))])
                          ["Yhteensä" 0 0]
                          rivit)
-        muuta-pilkut-ja-poista-id (map (fn [k]
-                                         (rest
-                                           (transduce
-                                             (comp (map str)
-                                                   (map #(cstr/replace % #"(\d+)\.(\d+)" "$1,$2")))
-                                             conj
-                                             k))))
         rivit (into [] muuta-pilkut-ja-poista-id rivit)]
     {:otsikot  otsikot
      :yhteensa yhteensa
@@ -173,15 +205,20 @@
       {:viimeinen-rivi-yhteenveto? true
        :otsikko                    (str "Kulut tehtäväryhmittäin ajalla " (pvm/pvm alkupvm) " - " (pvm/pvm loppupvm))}
       otsikot
-      (conj rivit yhteensa)]
+      (conj rivit (transduce piste->pilkku
+                             conj
+                             yhteensa))]
      [:taulukko
       {:otsikko                    "Urakkavuoden alusta"
        :viimeinen-rivi-yhteenveto? true}
       [{:leveys 1 :otsikko ""} {:leveys 1 :otsikko ""}]
-      [["Urakkavuoden alusta tav.hintaan kuuluvia: " (str yhteensa-hoitokauden-alusta)]
-       ["Tavoitehinta: " (if (some? tavoitehinta)
-                           (str tavoitehinta)
-                           "Tavoitehintaa ei saatu")]
-       ["Jäljellä: " (if (some? tavoitehinta)
-                       (str (- tavoitehinta yhteensa-hoitokauden-alusta))
-                       "Tavoitehintaa ei saatu - jäljellä olevaa ei voitu laskea")]]]]))
+      (map #(transduce piste->pilkku
+                      conj
+                      %)
+           [["Urakkavuoden alusta tav.hintaan kuuluvia: " (str yhteensa-hoitokauden-alusta)]
+            ["Tavoitehinta: " (if (some? tavoitehinta)
+                                (str tavoitehinta)
+                                "Tavoitehintaa ei saatu")]
+            ["Jäljellä: " (if (some? tavoitehinta)
+                            (str (- tavoitehinta yhteensa-hoitokauden-alusta))
+                            "Tavoitehintaa ei saatu - jäljellä olevaa ei voitu laskea")]])]]))
