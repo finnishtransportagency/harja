dc4df466182970a5a9b851efc52edec24980fa53
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2020-11-04 13:54:09 +0200

    Poista turha do-kääre

--- src/cljs/harja/views/urakka/paallystysilmoitukset.cljs
+++ src/cljs/harja/views/urakka/paallystysilmoitukset.cljs
@@ -1184,8 +1184,7 @@
 (defn- ilmoitusluettelo
   [e! app]
   (komp/luo
-    (komp/sisaan #(do
-                    (nav/vaihda-kartan-koko! :M)))
+    (komp/sisaan #(nav/vaihda-kartan-koko! :M))
     (fn [e! {urakka                :urakka {:keys [valittu-sopimusnumero valittu-urakan-vuosi]} :urakka-tila
              paallystysilmoitukset :paallystysilmoitukset kohteet-yha-lahetyksessa :kohteet-yha-lahetyksessa :as app}]
       (let [urakka-id (:id urakka)
