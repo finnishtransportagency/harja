bc3cb415e339f06a17d0ea6308aa71142f6c283b
Markus Vähäkangas <markus.vahakangas@solita.fi>
2020-11-04 09:50:29 +0200

    Yritä korjata migraatiotiedostojen konflikti

--- tietokanta/src/main/resources/db/migration/V1_840__.sql
+++ tietokanta/src/main/resources/db/migration/V1_840__.sql
@@ -1,21 +1,144 @@
--- Samassa migraatiotiedostossa ei voi olla transaktion sisällä olevia päivityksiä
--- ja ilman transaktiota pyöriviä päivitytksiä.
--- CONCURRENTLY indeksien luominen on non-transactional päivitys, joten ne on erotettu tähän erilliseen tiedostoon
+-- Vain osa toimenpidekoodeista on sellaisia tehtäviä, että niitä voidaan lisätä
+-- käyttöliittymän kautta käsin suunnitelluille tehtäville.
+-- Määritellään kaikki toimenpidekoodit aluksi käsin lisättävien joukosta.
+-- Ja koska ympäristöt ovat erilaisia (tehtävien nimet ja id;t vaihtelevat ympäristön mukaan)
+-- , niin etukäteen ei voida määritellä, että mitkä laitetaan käsin lisättävien listalle ja mitä ei.
+-- Niinpä ne on tehtävä jokaiseen ympäristöön erikseen suorilla tietokanta-ajoilla.
+ALTER TABLE toimenpidekoodi
+    ADD kasin_lisattava_maara BOOLEAN DEFAULT FALSE;
 
--- Päivitä toteuma taulujen indeksejä, jotta suorituskyky paranee
-CREATE INDEX CONCURRENTLY toteuma_urakka_alkanut_idx ON toteuma (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_010101_191001_urakka_alkanut_poistettu_idx ON toteuma_010101_191001 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_191001_200701_urakka_alkanut_poistettu_idx ON toteuma_191001_200701 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_200701_210101_urakka_alkanut_poistettu_idx ON toteuma_200701_210101 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_210101_210701_urakka_alkanut_poistettu_idx ON toteuma_210101_210701 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_210701_220101_urakka_alkanut_poistettu_idx ON toteuma_210701_220101 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_220101_220701_urakka_alkanut_poistettu_idx ON toteuma_220101_220701 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_220701_230101_urakka_alkanut_poistettu_idx ON toteuma_220701_230101 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_230101_230701_urakka_alkanut_poistettu_idx ON toteuma_230101_230701 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_230701_240101_urakka_alkanut_poistettu_idx ON toteuma_230701_240101 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_240101_240701_urakka_alkanut_poistettu_idx ON toteuma_240101_240701 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_240701_250101_urakka_alkanut_poistettu_idx ON toteuma_240701_250101 (urakka, alkanut, poistettu);
-CREATE INDEX CONCURRENTLY toteuma_250101_991231_urakka_alkanut_poistettu_idx ON toteuma_250101_991231 (urakka, alkanut, poistettu);
+-- Lisätään uuid tehtäväryhmän muutamalle lisätyöhön liittyvälle ryhmälle, jotta nimen vaihto
+-- ei sotke tehtäväryhmän käyttöä hakulausekkeissa.
+UPDATE tehtavaryhma SET yksiloiva_tunniste = '91896f23-1d4a-4385-8e1f-28a8f0c8ba80'
+WHERE nimi = 'Lisätyöt' AND otsikko = '7.0 LISÄTYÖT';
+UPDATE tehtavaryhma SET yksiloiva_tunniste = '0b65b36d-e84e-40b6-a0ad-e4f539f05227'
+WHERE nimi = 'Välitaso Lisätyöt' AND otsikko = '7.0 LISÄTYÖT';
+UPDATE tehtavaryhma SET yksiloiva_tunniste = '6a2f1000-bb92-48d7-af80-7510c532115a'
+WHERE nimi = 'Alataso Lisätyöt' AND otsikko = '7.0 LISÄTYÖT';
 
--- Lisätään sitten indeksi toteuma_tehtava taululle, jotta suorituskyky paranee
-CREATE INDEX CONCURRENTLY toteuma_tehtava_urakka_poistettu ON toteuma_tehtava (urakka_id, poistettu);
+
+-- Tee uusi 9kk partitio toteuma taululle
+SELECT * FROM luo_toteumataulun_partitio( '2019-10-01'::DATE, '2020-07-01'::DATE);
+-- Muokataan aiemmin kaikki ensimmäiset toteumat sisältänyt partitio uuden nimiseksi, koska se sisältää
+-- nyt eri tiedot
+-- Poistetaan ensin constraint
+ALTER TABLE toteuma_010101_200701 DROP CONSTRAINT toteuma_010101_200701_alkanut_check;
+-- Muutetaan partition nimi vastaamaan uutta tulevaa sisältöä
+ALTER TABLE toteuma_010101_200701 rename to toteuma_010101_191001;
+
+-- Uusi insert trigger toteuma partitioille
+CREATE OR REPLACE FUNCTION toteuma_insert() RETURNS trigger AS $$
+DECLARE
+    alkanut date;
+BEGIN
+    alkanut := NEW.alkanut;
+    IF alkanut < '2019-10-01'::date THEN
+        INSERT INTO toteuma_010101_191001 VALUES (NEW.*);
+    ELSIF alkanut >= '2019-10-01'::date AND alkanut < '2020-07-01'::date THEN
+        INSERT INTO toteuma_191001_200701 VALUES (NEW.*);
+    ELSIF alkanut >= '2020-07-01'::date AND alkanut < '2021-01-01'::date THEN
+        INSERT INTO toteuma_200701_210101 VALUES (NEW.*);
+    ELSIF alkanut >= '2021-01-01'::date AND alkanut < '2021-07-01'::date THEN
+        INSERT INTO toteuma_210101_210701 VALUES (NEW.*);
+    ELSIF alkanut >= '2021-07-01'::date AND alkanut < '2022-01-01'::date THEN
+        INSERT INTO toteuma_210701_220101 VALUES (NEW.*);
+    ELSIF alkanut >= '2022-01-01'::date AND alkanut < '2022-07-01'::date THEN
+        INSERT INTO toteuma_220101_220701 VALUES (NEW.*);
+    ELSIF alkanut >= '2022-07-01'::date AND alkanut < '2023-01-01'::date THEN
+        INSERT INTO toteuma_220701_230101 VALUES (NEW.*);
+    ELSIF alkanut >= '2023-01-01'::date AND alkanut < '2023-07-01'::date THEN
+        INSERT INTO toteuma_230101_230701 VALUES (NEW.*);
+    ELSIF alkanut >= '2023-07-01'::date AND alkanut < '2024-01-01'::date THEN
+        INSERT INTO toteuma_230701_240101 VALUES (NEW.*);
+    ELSIF alkanut >= '2024-01-01'::date AND alkanut < '2024-07-01'::date THEN
+        INSERT INTO toteuma_240101_240701 VALUES (NEW.*);
+    ELSIF alkanut >= '2024-07-01'::date AND alkanut < '2025-01-01'::date THEN
+        INSERT INTO toteuma_240701_250101 VALUES (NEW.*);
+        -- kaatissäkki kaikelle liian uudelle, typotetulle jne. Jos ja kun Harja elää 1.1.2025 pitempään, muuta
+        -- tätä funktiota ja luo tarvittava määrä hoitokausipartitioita lisää jotta saadaan partitioinnin
+        -- hyödyt myös silloin käyttöön
+    ELSIF alkanut >= '2025-01-01'::date THEN
+        INSERT INTO toteuma_250101_991231 VALUES (NEW.*);  ELSE
+        RAISE EXCEPTION 'Taululle toteuma ei löydy insert ehtoa, korjaa toteuma_insert() sproc!';
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Päivitetään aikavälin siirtoon tehty funktio
+CREATE OR REPLACE FUNCTION siirra_aikavalin_toteumat(alkupvm DATE, loppupvm DATE)
+    RETURNS VOID AS
+$$
+BEGIN
+    RAISE NOTICE 'Siirretään toteumat aikaväliltä % - %', alkupvm, loppupvm;
+    WITH x AS (
+        DELETE FROM toteuma_010101_191001 WHERE alkanut BETWEEN alkupvm AND loppupvm returning *
+    )
+    INSERT INTO toteuma SELECT * FROM x;
+END
+$$
+    LANGUAGE plpgsql;
+
+-- Siirretään toteumat vanhasta isosta partitiosta uuteen 9kk pitkään
+SELECT * FROM siirra_aikavalin_toteumat( '2019-10-01'::DATE, '2020-07-01'::DATE);
+
+-- Päivitetään check constraint vanhalle partitiolle
+ALTER TABLE toteuma_010101_191001
+    ADD CONSTRAINT toteuma_010101_191001_alkanut_check
+        CHECK (alkanut >= '0001-01-01' AND alkanut < '2019-10-01');
+
+-- Muokkaa toteuma taulun poistettu kenttä pelkästään boolean arvoksi
+-- Ensin muutetaan null arvot falseksi
+UPDATE toteuma set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_010101_191001 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_191001_200701 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_200701_210101 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_210101_210701 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_210701_220101 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_220101_220701 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_220701_230101 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_230101_230701 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_230701_240101 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_240101_240701 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_240701_250101 set poistettu = false WHERE poistettu is null;
+UPDATE toteuma_250101_991231 set poistettu = false WHERE poistettu is null;
+-- Sitten muokataan itse kenttä, jotta sinne ei voi null arvoja enää lisätä
+ALTER TABLE toteuma ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_010101_191001 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_191001_200701 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_200701_210101 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_210101_210701 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_210701_220101 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_220101_220701 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_220701_230101 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_230101_230701 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_230701_240101 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_240101_240701 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_240701_250101 ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_250101_991231 ALTER COLUMN poistettu SET NOT NULL;
+-- set default false
+ALTER TABLE toteuma ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_010101_191001 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_191001_200701 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_200701_210101 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_210101_210701 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_210701_220101 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_220101_220701 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_220701_230101 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_230101_230701 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_230701_240101 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_240101_240701 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_240701_250101 ALTER COLUMN poistettu SET DEFAULT FALSE;
+ALTER TABLE toteuma_250101_991231 ALTER COLUMN poistettu SET DEFAULT FALSE;
+
+
+
+-- Muokataan toteuma_tehtava taulu suorituskyvyltään paremmaksi
+-- Lisätään urakkaindeksiä varten uusi columni
+ALTER TABLE toteuma_tehtava ADD COLUMN urakka_id INTEGER;
+-- Muokataan poistettu kenttä sisältämään pelkästään true/false arvoja, koska se parantaa indeksin toimintaa
+UPDATE toteuma_tehtava set poistettu = false where poistettu is null;
+ALTER TABLE toteuma_tehtava ALTER COLUMN poistettu SET NOT NULL;
+ALTER TABLE toteuma_tehtava ALTER COLUMN poistettu SET DEFAULT FALSE;
+
+
--- tietokanta/src/main/resources/db/migration/V1_842__.sql
+++ tietokanta/src/main/resources/db/migration/V1_842__.sql
@@ -1,10 +1,84 @@
--- Lisätään toteuma_materiaali taululle indeksi, jotta toteumataulun linkitys nopeutuu.
--- Jotta indeksi saadaan lisättyä muokataan toteuma_materiaalitaulun poistettu kolumni ottamaan vastaan vain false/true arvoja. Null ei ole
--- enää hyväksyttävä.
--- Sitten lisätään urakka_id, jonka varaan uusi indeksi rakennetaan ja päivitetään olemassa oleville riveille tieto omistavasta urakasta
-UPDATE toteuma_materiaali set poistettu = false where poistettu is null;
-ALTER TABLE toteuma_materiaali ALTER COLUMN poistettu SET NOT NULL;
-ALTER TABLE toteuma_materiaali ALTER COLUMN poistettu SET DEFAULT FALSE;
+-- Koska toteuma tauluun on lisätty uusi indeksi pitää tulevaisuutta varten päivittää uusille mahdollisille partitioille tämä sama uusi indeksi
+-- Tästä syystä indeksin luonti funkkari päivitetään
+CREATE OR REPLACE FUNCTION luo_toteumataulun_partitio(alkupvm DATE, loppupvm DATE)
+    RETURNS VOID AS
+$$
+DECLARE
+    partitio text;
+BEGIN
+    partitio :=  'toteuma_' || TO_CHAR(alkupvm, 'YYMMDD') || '_' || TO_CHAR(loppupvm, 'YYMMDD');
 
--- Lisää urakka_id kenttä
-ALTER TABLE toteuma_materiaali ADD COLUMN urakka_id INTEGER;
+    PERFORM validoi_hoitokauden_alkupvm(alkupvm);
+
+    -- CREATE PARTITION TABLE, INHERITING FROM TOTEUMA
+    EXECUTE 'CREATE TABLE IF NOT EXISTS ' || partitio ||
+            ' ( CHECK( alkanut >= '''|| alkupvm || ''' AND alkanut < '''|| loppupvm ||''')) INHERITS (toteuma)';
+
+    -- PRIMARY KEY SUBSTITUTE
+    EXECUTE 'CREATE UNIQUE INDEX ' || partitio || '_id_idx ON ' || partitio || '(id)';
+
+    -- OTHER INDEXES
+    EXECUTE 'ALTER TABLE ' || partitio || ' ADD CONSTRAINT ' || partitio || '_uniikki_ulkoinen_id_luoja_urakka UNIQUE (ulkoinen_id, luoja, urakka)';
+    EXECUTE 'CREATE INDEX ' || partitio || '_alkanut_idx ON ' || partitio || '(alkanut)';
+    EXECUTE 'CREATE INDEX ' || partitio || '_urakka_idx ON ' || partitio || '(urakka)';
+    EXECUTE 'CREATE INDEX ' || partitio || '_sopimus_idx ON ' || partitio || '(sopimus)';
+    EXECUTE 'CREATE INDEX ' || partitio || '_tyyppi_urakka_alkanut_idx ON ' || partitio || '(tyyppi, urakka, alkanut)';
+    EXECUTE 'CREATE INDEX ' || partitio || '_envelope_idx ON ' || partitio || ' USING GIST (envelope);';
+    -- Alla muutettu rivi --
+    EXECUTE 'CREATE INDEX ' || partitio || '_urakka_alkanut_idx ON ' || partitio || ' (urakka, alkanut, poistettu);';
+    -- Muutos päättyy --
+
+    -- FOREIGN KEYS
+    EXECUTE 'ALTER TABLE ' || partitio || ' ADD CONSTRAINT ' || partitio || '_luoja_fkey FOREIGN KEY (luoja) REFERENCES kayttaja (id);';
+    EXECUTE 'ALTER TABLE ' || partitio || ' ADD CONSTRAINT ' || partitio || '_urakka_fkey FOREIGN KEY (urakka) REFERENCES urakka (id);';
+    EXECUTE 'ALTER TABLE ' || partitio || ' ADD CONSTRAINT ' || partitio || '_sopimus_fkey FOREIGN KEY (sopimus) REFERENCES sopimus (id);';
+
+    -- Toteuman envelopen luonti
+    EXECUTE 'CREATE TRIGGER tg_muodosta_toteuman_envelope
+        BEFORE INSERT OR UPDATE
+        ON ' || partitio ||'
+        FOR EACH ROW
+    EXECUTE PROCEDURE muodosta_toteuman_envelope();';
+
+
+    -- On luotava triggerit lapsitauluihin, koska UPDATE triggerit eivät mene ajoon emotaulusta (updatea ei sinne tapahdu koska data ei ole siellä)
+    EXECUTE 'CREATE TRIGGER tg_poista_muistetut_laskutusyht_tot
+       AFTER INSERT OR UPDATE
+       ON ' || partitio || '
+       FOR EACH ROW
+       WHEN (NEW.tyyppi != ''kokonaishintainen''::toteumatyyppi)
+    EXECUTE PROCEDURE poista_muistetut_laskutusyht_tot();';
+
+    -- Toteuman luontitransaktion lopuksi päivitetään materiaalin käyttö
+    EXECUTE 'CREATE CONSTRAINT TRIGGER tg_vahenna_urakan_materiaalin_kayttoa_hoitoluokittain
+       AFTER UPDATE
+       ON '|| partitio ||'
+       DEFERRABLE INITIALLY DEFERRED
+       FOR EACH ROW
+       WHEN (NEW.lahde = ''harja-api'')
+    EXECUTE PROCEDURE vahenna_urakan_materiaalin_kayttoa_hoitoluokittain();';
+
+    -- Toteuman päivitys jos alkanut-kenttä muuttuu
+    EXECUTE 'CREATE TRIGGER update_toteuma_check_partition_tg
+       BEFORE UPDATE ON '|| partitio ||'
+       FOR EACH ROW
+       WHEN (OLD.alkanut IS DISTINCT FROM NEW.alkanut)
+    EXECUTE PROCEDURE update_toteuma_check_partition();';
+END
+$$
+    LANGUAGE plpgsql;
+
+
+-- Päivitetään toteuma_tehtava tauluun kaikki tarvittavat urakka_id:t.
+-- Tämä täytyy tehdä osissa, koska se tulee muuten viemään vuorokausia.
+-- Päivitys aloitetaan pienestä ja annetaan postgresin muistin pikkuhiljaa nopeuttaa päivitystä.
+update toteuma_tehtava tt SET urakka_id = t2.urakka FROM (select  id, urakka from toteuma t order by id asc limit 1 offset 0) t2 WHERE tt.toteuma = t2.id;
+update toteuma_tehtava tt SET urakka_id = t2.urakka FROM (select  id, urakka from toteuma t order by id asc limit 5 offset 1) t2 WHERE tt.toteuma = t2.id;
+update toteuma_tehtava tt SET urakka_id = t2.urakka FROM (select  id, urakka from toteuma t order by id asc limit 10 offset 6) t2 WHERE tt.toteuma = t2.id;
+update toteuma_tehtava tt SET urakka_id = t2.urakka FROM (select  id, urakka from toteuma t order by id asc limit 100 offset 16) t2 WHERE tt.toteuma = t2.id;
+update toteuma_tehtava tt SET urakka_id = t2.urakka FROM (select  id, urakka from toteuma t order by id asc limit 1000 offset 116) t2 WHERE tt.toteuma = t2.id;
+update toteuma_tehtava tt SET urakka_id = t2.urakka FROM (select  id, urakka from toteuma t order by id asc limit 10000 offset 1116) t2 WHERE tt.toteuma = t2.id;
+update toteuma_tehtava tt SET urakka_id = t2.urakka FROM (select  id, urakka from toteuma t order by id asc limit 100000 offset 11116) t2 WHERE tt.toteuma = t2.id;
+update toteuma_tehtava tt SET urakka_id = t2.urakka FROM (select  id, urakka from toteuma t order by id asc limit 1000000 offset 111116) t2 WHERE tt.toteuma = t2.id;
+update toteuma_tehtava tt SET urakka_id = t2.urakka FROM (select  id, urakka from toteuma t order by id asc limit 10000000 offset 1111116) t2 WHERE tt.toteuma = t2.id;
+update toteuma_tehtava tt SET urakka_id = t2.urakka FROM (select  id, urakka from toteuma t order by id asc limit 30000000 offset 11111116) t2 WHERE tt.toteuma = t2.id;
