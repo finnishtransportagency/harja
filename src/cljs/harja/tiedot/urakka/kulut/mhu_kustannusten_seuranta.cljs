3857fea7766e9f40513c7a97f238b1331bcc9232
Markus Vähäkangas <markus.vahakangas@solita.fi>
2020-11-17 12:45:54 +0200

    Uudista kustannusten ryhmittelyä ui:lla

--- src/cljs/harja/tiedot/urakka/kulut/mhu_kustannusten_seuranta.cljs
+++ src/cljs/harja/tiedot/urakka/kulut/mhu_kustannusten_seuranta.cljs
@@ -17,7 +17,7 @@
 
 (declare hae-kustannukset)
 
-(defrecord HaeKustannukset [urakka toimenpide hoitokauden-alkuvuosi aikavali-alkupvm aikavali-loppupvm])
+(defrecord HaeKustannukset [toimenpide hoitokauden-alkuvuosi aikavali-alkupvm aikavali-loppupvm])
 (defrecord KustannustenHakuOnnistui [vastaus])
 (defrecord KustannustenHakuEpaonnistui [vastaus])
 (defrecord ValitseToimenpide [urakka toimenpide])
@@ -28,7 +28,7 @@
 (defrecord ValitseHoitokausi [urakka vuosi])
 
 (defn hae-kustannukset [urakka-id toimenpide hoitokauden-alkuvuosi aikavali-alkupvm aikavali-loppupvm]
-  (let [_ (js/console.log "hae-kustannukset ! " (pr-str hoitokauden-alkuvuosi))
+  (let [_ (js/console.log "hae-kustannukset ! " (pr-str hoitokauden-alkuvuosi) " toimenpide" (pr-str toimenpide))
         alkupvm (when hoitokauden-alkuvuosi
                   (str hoitokauden-alkuvuosi "-10-01"))
         #_alkupvm #_(if aikavali-alkupvm
@@ -39,7 +39,7 @@
                        aikavali-loppupvm loppupvm)]
     (tuck-apurit/post! :urakan-kustannusten-seuranta-toimenpideittain
                        {:urakka-id urakka-id
-                        :tehtavaryhma (:otsikko toimenpide)
+                        :toimenpidekoodi (:koodi toimenpide)
                         :alkupvm alkupvm
                         :loppupvm loppupvm}
                        {:onnistui ->KustannustenHakuOnnistui
@@ -59,12 +59,13 @@
           (assoc :valittu-toimenpide toimenpide))))
 
   HaeKustannukset
-  (process-event [{urakka-id :urakka-id toimenpide :toimenpide hoitokauden-alkuvuosi :hoitokauden-alkuvuosi
+  (process-event [{toimenpide :toimenpide hoitokauden-alkuvuosi :hoitokauden-alkuvuosi
                    aikavali-alkupvm :aikavali-alkupvm aikavali-loppupvm :aikavali-loppupvm} app]
     (let [alkupvm (when aikavali-alkupvm
                     (pvm/iso8601 aikavali-alkupvm))
           loppupvm (when aikavali-loppupvm
-                     (pvm/iso8601 aikavali-loppupvm))]
+                     (pvm/iso8601 aikavali-loppupvm))
+          urakka-id (-> @tila/yleiset :urakka :id)]
       (hae-kustannukset urakka-id toimenpide hoitokauden-alkuvuosi alkupvm loppupvm))
     app)
 
@@ -73,23 +74,43 @@
     (let [ryhmitelty-tehtava (group-by :toimenpide vastaus)
           toimenpiteet (mapv
                          (fn [toimenpide]
-                           (let [toimenpiteen-tehtavat (second toimenpide)]
+                           (let [toimenpiteen-tehtavat (second toimenpide)
+                                 ;; Toimenpiteet mäpissä on budjetoidut ja toteutuneet toimenpiteet
+                                 ;; UI:lla budjetointi lasketaan yhteen (yllä yhteensä kohdassa) ja toteutuneet kustannukset näytetään
+                                 ;; rivikohtaisesti.
+                                 ;; Poistetaan siis budjetointiin liittyvät tehtävät :toteutunut = budjetoitu tai hth
+                                 toteutuneet-tehtavat (filter
+                                                        (fn [tehtava]
+                                                          (when (and
+                                                                  (not= "hjh" (:toteutunut tehtava))
+                                                                  (not= "budjetointi" (:toteutunut tehtava)))
+                                                            tehtava))
+                                                        toimenpiteen-tehtavat)
+                                 ;_ (js/console.log "toimenpiteen-tehtavat1" (pr-str toimenpiteen-tehtavat))
+                                 _ (js/console.log "toimenpiteen: " (pr-str (first toimenpide)) "tehtavat" (pr-str (map (juxt :tehtava_nimi :kustannustyyppi :jarjestys) toimenpiteen-tehtavat)))
+                                 jarjestys (some #(:jarjestys %) toimenpiteen-tehtavat)
+                                 _ (js/console.log "jarjestys" (pr-str jarjestys))]
                              {:toimenpide (first toimenpide)
+                              :jarjestys jarjestys
                               :toimenpide-toteutunut-summa (apply + (map (fn [rivi]
                                                                            (:toteutunut_summa rivi))
                                                                          toimenpiteen-tehtavat))
                               :toimenpide-budjetoitu-summa (apply + (map (fn [rivi]
                                                                            (:budjetoitu_summa rivi))
                                                                          toimenpiteen-tehtavat))
-                              :tehtavat toimenpiteen-tehtavat}))
+                              :tehtavat toteutuneet-tehtavat}))
                          ryhmitelty-tehtava)
+          _ (js/console.log "toimenpiteet 1" (pr-str (map (juxt :toimenpide :jarjestys) toimenpiteet)))
+          toimenpiteet (sort-by :jarjestys toimenpiteet)
+          _ (js/console.log "toimenpiteet 2" (pr-str (map (juxt :toimenpide :jarjestys) toimenpiteet)))
           yhteensa {:toimenpide "Tavoitehinta/Yhteensä"
                     :yht-toteutunut-summa (apply + (map (fn [rivi]
                                                           (:toimenpide-toteutunut-summa rivi))
                                                         toimenpiteet))
                     :yht-budjetoitu-summa (apply + (map (fn [rivi]
                                                           (:toimenpide-budjetoitu-summa rivi))
-                                                        toimenpiteet))}]
+                                                        toimenpiteet))}
+          ]
       (-> app
           (assoc-in [:kustannukset-yhteensa] yhteensa)
           (assoc-in [:kustannukset] vastaus)
