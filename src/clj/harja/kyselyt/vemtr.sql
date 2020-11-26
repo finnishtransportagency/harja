3a7db8de70a09f957e30d07dc5959d0d053ba6da
Erno Kuusela <erno!erno-laptop>
2020-11-26 10:14:22 +0200

    vemtr-kysely union toimii tyyppien puolesta

--- src/clj/harja/kyselyt/vemtr.sql
+++ src/clj/harja/kyselyt/vemtr.sql
@@ -1,4 +1,5 @@
 
+-- name: kysely
 -- select tpk4.emo, tpk4.nimi as tpknimi, tpi.nimi as tpinimi, tyo.id, tyo.urakka, tt.maara as "tot-maara", tyo.maara as "suunniteltu-maara", tyo.yksikko
 -- v1
 select tpk4.emo as emo, tpk4.nimi as tpknimi, tt.maara as "tot-maara", tm.maara as "toteutunut-materiaalimaara", 0 as "suunniteltu-maara"
@@ -98,8 +99,9 @@ group by tpk.id, tpk.nimi, tpk.yksikko, ut."hoitokauden-alkuvuosi", tpk.jarjesty
 
 -- v5
 
+DROP VIEW IF EXISTS yh_vemtr_toteutuneet;
 CREATE OR REPLACE TEMPORARY VIEW yh_vemtr_toteutuneet AS
-  SELECT MIN(tpk4.nimi) as nimi, 0 as jarjestys, 0 as "suunniteltu", null as "hoitokauden-alkuvuosi", tpk4.suunnitteluyksikko, tpk4.yksikko, tpk4.id as toimenpidekoodi, ROUND(SUM(tm.maara), 2) as "toteutunut-materiaalimaara", null as toimenpide, ROUND(SUM(tt.maara), 2) as "toteuma"
+  SELECT MIN(tpk4.nimi) as nimi, 0 as jarjestys, 0 as "suunniteltu", 0::smallint as "hoitokauden-alkuvuosi", tpk4.suunnitteluyksikko, tpk4.yksikko, tpk4.id as toimenpidekoodi, 0 as urakka, '' as toimenpide, ROUND(SUM(tt.maara), 2) as toteuma, ROUND(SUM(tm.maara), 2) as "toteutunut-materiaalimaara"
           FROM toteuma t
                    JOIN toteuma_tehtava tt ON t.id = tt.toteuma AND tt.poistettu = FALSE
                    LEFT JOIN toteuma_materiaali tm
@@ -107,18 +109,20 @@ CREATE OR REPLACE TEMPORARY VIEW yh_vemtr_toteutuneet AS
   toimenpidekoodi tpk4, urakka u
   WHERE u.tyyppi = 'hoito' and (not u.poistettu) and t.urakka = u.id and tpk4.id = tt.toimenpidekoodi AND (not tpk4.poistettu) AND t.alkanut > '2020-11-01' GROUP BY tpk4.id;
   
+DROP VIEW IF EXISTS yh_vemtr_suunnitellut;
 CREATE OR REPLACE TEMPORARY VIEW yh_vemtr_suunnitellut AS
   select LEFT(tpk4.nimi, 10) as nimi, 0 as jarjestys, SUM(tyo.maara) as "suunniteltu",
-         --  xxx as "hoitokauden-alkuvuosi", -- XXX miten hanskata? pitääkö kyselystä tehdä semmoinen että se partitioi hoitokausittain rivit? onko meillä joku taulu, tai gen_hoitokausi sproc joka antaa mahdolliset hoitokaudet?
+         0::smallint as "hoitokauden-alkuvuosi", --  xxx as "hoitokauden-alkuvuosi", -- XXX miten hanskata? pitääkö kyselystä tehdä semmoinen että se partitioi hoitokausittain rivit? onko meillä joku taulu, tai gen_hoitokausi sproc joka antaa mahdolliset hoitokaudet?
 	 tpk4.suunnitteluyksikko, tpk4.suunnitteluyksikko as yksikko, tpk4.id as toimenpidekoodi,
-	 null as urakka, -- u.id as urakka,
-	 null as toimenpide, -- mistä tpi (josta tpi.nimi as toimenpide)?
+	 0 as urakka, -- u.id as urakka,
+	 '' as toimenpide, -- mistä tpi (josta tpi.nimi as toimenpide)?
 	 0 as "toteuma", 0 as "toteutunut-materiaalimaara"
 from yksikkohintainen_tyo tyo
   JOIN toimenpidekoodi tpk4 ON tpk4.id = tyo.tehtava,
   urakka u
   WHERE u.id = tyo.urakka AND u.tyyppi = 'hoito' GROUP BY tpk4.id;
 
+DROP VIEW IF EXISTS mhu_vemtr;
 CREATE OR REPLACE TEMPORARY VIEW mhu_vemtr AS
   WITH urakat AS (select u.id
                 from urakka u
@@ -136,13 +140,14 @@ CREATE OR REPLACE TEMPORARY VIEW mhu_vemtr AS
 select tpk.nimi            as "nimi",
        tpk.jarjestys       as "jarjestys",
        sum(ut.maara)       as "suunniteltu", -- ut = urakka_tehtavamaara
-       ut."hoitokauden-alkuvuosi",
+       ut."hoitokauden-alkuvuosi"::smallint as "hoitokauden-alkuvuosi",
        tpk.suunnitteluyksikko as "suunnitteluyksikko",
        tpk.yksikko         as "yksikko",
        tpk.id              as "toimenpidekoodi",
        ut.urakka as "urakka",
        tpi.nimi             as "toimenpide",
-       sum(toteumat.maara) as "toteuma"
+       sum(toteumat.maara) as "toteuma",
+       0 as "toteutunut-materiaalimaara"
 from urakka_tehtavamaara ut
        join toimenpidekoodi tpk on ut.tehtava = tpk.id
        join toimenpideinstanssi tpi on tpi.toimenpide = tpk.emo and tpi.urakka = ut.urakka
@@ -154,6 +159,8 @@ where ut.poistettu is not true
 group by tpk.id, tpk.nimi, tpk.yksikko, ut."hoitokauden-alkuvuosi", tpk.jarjestys, tpi.nimi, tpk.suunnitteluyksikko, ut.urakka;
 
 select * from mhu_vemtr union select * from yh_vemtr_suunnitellut union select * from yh_vemtr_toteutuneet;
+
+select nimi, jarjestys, suunniteltu, "hoitokauden-alkuvuosi", suunnitteluyksikko, yksikko, toimenpidekoodi, toimenpide, toteuma, "toteutunut-materiaalimaara" from mhu_vemtr union select nimi, jarjestys, suunniteltu, "hoitokauden-alkuvuosi", suunnitteluyksikko, yksikko, toimenpidekoodi, toimenpide, toteuma, "toteutunut-materiaalimaara" from yh_vemtr_suunnitellut union select nimi, jarjestys, suunniteltu, "hoitokauden-alkuvuosi", suunnitteluyksikko, yksikko, toimenpidekoodi, toimenpide, toteuma, "toteutunut-materiaalimaara" from yh_vemtr_toteutuneet;
 
 -- 1. emo tpknimi tot-maara toteutunut-materiaalimaara suunniteltu-maara
 -- 2. nimi jarjestys suunniteltu ut.hoitokauden-alkluvuosi suunnitteluyksikko yksikko toimenpidekoodi urakka toimenpdie toteuma
