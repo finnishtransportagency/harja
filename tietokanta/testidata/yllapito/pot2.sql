aed1447a98a4eefe957f4bb7d8dda5027db98c91
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2020-12-10 11:49:53 +0200

    Lisää testidataan POT2_paallystekerros tietoja

--- tietokanta/testidata/yllapito/pot2.sql
+++ tietokanta/testidata/yllapito/pot2.sql
@@ -1,6 +1,24 @@
 -- Replikoidaan olemassa oleva potti vanhasta tietomallista
 -- Kenties päästään tekemään niiden pohjalta aikanaan myös
 -- hyviä testejä, esim. että YHA:an lähtevä hyötykuorma on identtinen jne.
+DO $$
+DECLARE
+    urakkaid INTEGER;
+    kohdeid INTEGER;
+    kohdeosaid_kaista11 INTEGER;
+    kohdeosaid_kaista21 INTEGER;
+    massa1_id INTEGER;
+    massa2_id INTEGER;
+    paallystysilmoituksen_id INTEGER;
+
+BEGIN
+    urakkaid = (SELECT id FROM urakka where nimi = 'Utajärven päällystysurakka');
+    kohdeid = (SELECT id FROM yllapitokohde WHERE nimi = 'Tärkeä kohde mt20');
+    kohdeosaid_kaista11 = (SELECT id FROM yllapitokohdeosa WHERE nimi = 'Tärkeä kohdeosa kaista 11');
+    kohdeosaid_kaista21 = (SELECT id FROM yllapitokohdeosa WHERE nimi = 'Tärkeä kohdeosa kaista 21');
+    massa1_id = (SELECT id FROM pot2_massa WHERE dop_nro = '1234567' and urakka_id = urakkaid);
+    massa2_id = (SELECT id FROM pot2_massa WHERE dop_nro = '987654331-2' and urakka_id = urakkaid);
+
 
 INSERT INTO paallystysilmoitus(
     paallystyskohde,
@@ -14,7 +32,7 @@ INSERT INTO paallystysilmoitus(
     luotu,
     versio)
 VALUES
-((SELECT id FROM yllapitokohde WHERE nimi = 'Tärkeä kohde mt20'),
+(kohdeid,
  '2024-12-31',
  'aloitettu' ::paallystystila,
  NULL,
@@ -24,3 +42,12 @@ VALUES
  (SELECT id FROM kayttaja WHERE kayttajanimi = 'skanska'),
  NOW(),
  2);
+
+    paallystysilmoituksen_id = (SELECT id FROM paallystysilmoitus WHERE versio = 2 and paallystyskohde = kohdeid);
+
+INSERT INTO public.pot2_paallystekerros (kohdeosa_id, toimenpide, materiaali, leveys, massamenekki, pinta_ala, kokonaismassamaara, piennar, lisatieto, pot2_id) VALUES (kohdeosaid_kaista11, 22, massa1_id, 3, 333, 15000, 5000, true, null, paallystysilmoituksen_id);
+INSERT INTO public.pot2_paallystekerros (kohdeosa_id, toimenpide, materiaali, leveys, massamenekki, pinta_ala, kokonaismassamaara, piennar, lisatieto, pot2_id) VALUES (kohdeosaid_kaista21, 23, massa2_id, 3, 333, 15000, 5000, false, null, paallystysilmoituksen_id);
+
+
+END;
+$$ LANGUAGE plpgsql;
