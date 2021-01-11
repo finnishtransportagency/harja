151c0964c6a24b1eaab1aee8cc89aed9e2d126f2
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2021-01-11 13:52:02 +0200

    Bumppaa migraationumeroita

--- tietokanta/src/main/resources/db/migration/V1_851__.sql
+++ tietokanta/src/main/resources/db/migration/V1_851__.sql
@@ -1,2 +1,4 @@
--- non-transactional, ajettava erikseen...
-ALTER TYPE murskeen_rakeisuus ADD VALUE 'Muu';
+-- Kun käytettään taulun kaikissa POT versioissa, on kätevä että on versio kirjoittetu selkeästi
+ALTER TABLE paallystysilmoitus ADD COLUMN versio INTEGER;
+UPDATE paallystysilmoitus SET versio = 1;
+ALTER TABLE paallystysilmoitus ALTER COLUMN versio SET NOT NULL;
