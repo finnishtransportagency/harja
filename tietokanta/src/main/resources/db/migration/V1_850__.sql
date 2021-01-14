8c90e16779a10494cd20f02c53c0386146a24acd
Jukka Länsineva <jukka.lansineva@solita.fi>
2021-01-14 15:21:46 +0200

    lisätään sarake indeksikorjatuille summille

---
+++ tietokanta/src/main/resources/db/migration/V1_850__.sql
@@ -0,0 +1,3 @@
+alter table kustannusarvioitu_tyo add column indeksikorjattu_summa numeric;
+alter table johto_ja_hallintokorvaus add column indeksikorjattu_palkka numeric;
+alter table kiinteahintainen_tyo add column indeksikorjattu_summa numeric;
