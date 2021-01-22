4c97a589b8022a68a6fd0dbc939e2f45c3317e68
Maarit <maaritlaine@users.noreply.github.com>
2021-01-22 12:52:24 +0200

    Rename V1_852__.sql to V1_855__.sql

--- tietokanta/src/main/resources/db/migration/V1_852__.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO materiaalikoodi (nimi, yksikko, urakkatyyppi, kohdistettava, materiaalityyppi)
-VALUES ('Natriumformiaatti', 't', 'hoito'::urakkatyyppi, false, 'talvisuola'::materiaalityyppi);
---
+++ tietokanta/src/main/resources/db/migration/V1_855__.sql
@@ -0,0 +1,2 @@
+INSERT INTO materiaalikoodi (nimi, yksikko, urakkatyyppi, kohdistettava, materiaalityyppi)
+VALUES ('Natriumformiaatti', 't', 'hoito'::urakkatyyppi, false, 'talvisuola'::materiaalityyppi);
