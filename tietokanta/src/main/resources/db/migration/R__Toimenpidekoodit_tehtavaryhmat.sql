094094cdff2da2d7493f2f8b2285d947b75c7114
maaritlaine <maarit.laine@solita.fi>
2021-01-22 12:33:30 +0200

    Lisää guid tehtäväryhmälle Digitalisaatiot ja innovaatiot

--- tietokanta/src/main/resources/db/migration/R__Toimenpidekoodit_tehtavaryhmat.sql
+++ tietokanta/src/main/resources/db/migration/R__Toimenpidekoodit_tehtavaryhmat.sql
@@ -1010,6 +1010,7 @@ UPDATE tehtavaryhma SET yksiloiva_tunniste = '1b374802-dbe7-430b-bfc5-4635383d18
 UPDATE tehtavaryhma SET yksiloiva_tunniste = 'df612065-20d5-47b9-8cca-51ffd250e1f8' WHERE nimi = 'Vahinkojen korjaukset, Soratiet (T2)';
 UPDATE tehtavaryhma SET yksiloiva_tunniste = '37d3752c-9951-47ad-a463-c1704cf22f4c' WHERE nimi = 'Erillishankinnat (W)';
 UPDATE tehtavaryhma SET yksiloiva_tunniste = '0e78b556-74ee-437f-ac67-7a03381c64f6' WHERE nimi = 'Tilaajan rahavaraus (T3)';
+UPDATE tehtavaryhma SET yksiloiva_tunniste = '9dd75da4-0fde-4db1-81dd-c5f5c6fd4d6f' WHERE nimi = 'Digitalisaatiot ja innovaatiot (T4)';
 -- TODO Tarkista tämä
 UPDATE tehtavaryhma SET yksiloiva_tunniste = 'a6614475-1950-4a61-82c6-fda0fd19bb54' WHERE nimi = 'Johto- ja hallintokorvaus (J)';
 
