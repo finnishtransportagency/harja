91619ab70f2055a07b613f571a3ad3cfe6de5943
maaritlaine <maarit.laine@solita.fi>
2021-01-25 11:24:38 +0200

    Lisää natriumformiaattiliuos.

--- tietokanta/src/main/resources/db/migration/V1_856__.sql
+++ tietokanta/src/main/resources/db/migration/V1_856__.sql
@@ -1,2 +1,2 @@
 INSERT INTO materiaalikoodi (nimi, yksikko, urakkatyyppi, kohdistettava, materiaalityyppi)
-VALUES ('Natriumformiaatti', 't', 'hoito'::urakkatyyppi, false, 'talvisuola'::materiaalityyppi);
+VALUES ('Natriumformiaatti 2', 't', 'hoito'::urakkatyyppi, false, 'talvisuola'::materiaalityyppi);
