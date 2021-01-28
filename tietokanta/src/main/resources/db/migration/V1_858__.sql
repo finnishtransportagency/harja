44a3c4542018c31eb830f75084d4e450b0ee8341
Petar Teodosin Jr <petar.teodosin@solita.fi>
2021-01-28 17:00:32 +0200

    Lisää alusta toimenpidet sarakkeet

---
+++ tietokanta/src/main/resources/db/migration/V1_858__.sql
@@ -0,0 +1,13 @@
+-- Lisää muut toiminta sarakkeet pot2_alusta tauluun
+
+ALTER TABLE pot2_alusta ADD COLUMN lisatty_paksuus INTEGER;
+ALTER TABLE pot2_alusta ADD COLUMN massamaara INTEGER;
+ALTER TABLE pot2_alusta ADD COLUMN murske INTEGER REFERENCES pot2_mk_mursketyyppi (koodi);
+ALTER TABLE pot2_alusta ADD COLUMN kasittelysyvyys INTEGER;
+ALTER TABLE pot2_alusta ADD COLUMN leveys INTEGER;
+ALTER TABLE pot2_alusta ADD COLUMN pinta_ala INTEGER;
+ALTER TABLE pot2_alusta ADD COLUMN kokonaismassamaara INTEGER;
+ALTER TABLE pot2_alusta ADD COLUMN massa INTEGER REFERENCES pot2_mk_massatyyppi (koodi);
+ALTER TABLE pot2_alusta ADD COLUMN sideaine INTEGER REFERENCES  pot2_mk_massan_sideaine (id);
+ALTER TABLE pot2_alusta ADD COLUMN sideainepitoisuus NUMERIC(10, 1);
+ALTER TABLE pot2_alusta ADD COLUMN seosaine INTEGER; -- puuttuu koodisto taulukko
