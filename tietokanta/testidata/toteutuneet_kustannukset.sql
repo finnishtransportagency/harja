23f5252e1ef249dd0ebd068854112008dc8aa23f
Markus Vähäkangas <markus.vahakangas@solita.fi>
2021-02-03 11:55:29 +0200

    Muokataan toteutuneet_kustannukset taulun sisältöä
    kun kustannusarvioitu_tyo tai johto_ja_hallintakorvaus tauluisat siirrettyjä datoja muokataan tai poistetaan

--- tietokanta/testidata/toteutuneet_kustannukset.sql
+++ tietokanta/testidata/toteutuneet_kustannukset.sql
@@ -1,7 +1,7 @@
 -- Siirretään kaikki rivit mikäli tehtäväryhmä: Erillishankinnat (W) tai tehtävät: Toimistotarvike- ja ICT-kulut ja Hoitourakan työnjohto
 -- kustannusarvoidut_tyot taulusta toteutuneet_kustannukset tauluun,
 INSERT INTO toteutuneet_kustannukset (vuosi, kuukausi, summa, tyyppi, tehtava, tehtavaryhma, toimenpideinstanssi, sopimus_id,
-                            urakka_id, luotu)
+                            urakka_id, luotu, rivin_tunnistin)
 SELECT k.vuosi,
        k.kuukausi,
        k.summa,
@@ -11,7 +11,9 @@ SELECT k.vuosi,
        k.toimenpideinstanssi,
        k.sopimus,
        (select s.urakka FROM sopimus s where s.id = k.sopimus) as "urakka-id",
-       NOW()
+       NOW(),
+       MD5(concat(k.id, k.vuosi, k.kuukausi, k.summa, k.tyyppi, k.tehtava, k.tehtavaryhma,
+                  k.toimenpideinstanssi, k.sopimus, k.luotu, k.luoja, k.muokattu, k.muokkaaja)::TEXT)
 FROM kustannusarvioitu_tyo k
 WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', k.vuosi, k.kuukausi, 1)::DATE))) <
       date_trunc('month', current_date)
@@ -24,7 +26,7 @@ WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', k.vuosi, k.kuukausi, 1)::D
 
 -- Siirretään kaikki olemassaolevat rivit johto_ja_hallintakorvaus taulusta toteutuneet_kustannukset tauluun
 INSERT INTO toteutuneet_kustannukset (vuosi, kuukausi, summa, tyyppi, tehtava, tehtavaryhma, toimenpideinstanssi,
-                            sopimus_id, urakka_id, luotu)
+                            sopimus_id, urakka_id, luotu, rivin_tunnistin)
 SELECT j.vuosi,
        j.kuukausi,
        (j.tunnit * j.tuntipalkka)                     AS summa,
@@ -47,7 +49,10 @@ SELECT j.vuosi,
           AND s.poistettu IS NOT TRUE
         ORDER BY s.loppupvm DESC)                     AS sopimus_id,
        j."urakka-id",
-       NOW()
+       NOW(),
+       MD5(concat(j.id, j."urakka-id", j."toimenkuva-id", j.tunnit, j.tuntipalkka,
+                  j.luotu, j.luoja, j.muokattu, j.muokkaaja, j.vuosi, j.kuukausi,
+                  j."ennen-urakkaa", j."osa-kuukaudesta")::TEXT)
 FROM johto_ja_hallintokorvaus j
 WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', j.vuosi, j.kuukausi, 1)::DATE))) <
       date_trunc('month', current_date);
