5ae2655e7ea6112acc29e07867122246d03cc4b7
maaritlaine <maarit.laine@solita.fi>
2021-03-31 10:01:19 +0300

    Korjaa typo.

--- src/clj/harja/palvelin/integraatiot/turi/sanomat/tyotunnit.clj
+++ src/clj/harja/palvelin/integraatiot/turi/sanomat/tyotunnit.clj
@@ -35,7 +35,7 @@
    [:sampourakkaid urakka-sampoid]
    [:urakanpaattymispvm (xml/formatoi-paivamaara urakka-loppupvm)]
    [:urakkavaylamuoto (vaylamuoto urakka-tyyppi)]
-   [:urakkatyyppi (if (= urakka-tyyppi "teiden-hoito") hoito urakka-tyyppi)]
+   [:urakkatyyppi (if (= urakka-tyyppi "teiden-hoito") "hoito" urakka-tyyppi)]
    (when urakka-ely
      [:elyalue (str urakka-ely " ELY")])
    [:alueurakkanro alueurakkanro]
