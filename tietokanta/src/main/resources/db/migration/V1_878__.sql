4f32174ac161e3a573290a8c412e8c0499f5d033
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2021-05-05 16:00:24 +0300

    Poista turhat massatyypit, tee massaprosentista numeric

--- tietokanta/src/main/resources/db/migration/V1_878__.sql
+++ tietokanta/src/main/resources/db/migration/V1_878__.sql
@@ -1,2 +1,5 @@
 DELETE FROM pot2_mk_massatyyppi WHERE nimi =  'Pehmeät asfalttibetonit';
 DELETE FROM pot2_mk_massatyyppi WHERE nimi =  'Kovat asfalttibetonit';
+-- Fillerikiviaines haluaa yhden desimaalin, moni muu massa integerin
+ALTER TABLE pot2_mk_massan_runkoaine
+    ALTER COLUMN massaprosentti TYPE NUMERIC(3,1);
