d16d1e514e17e9c678a993dda6f97dddc7513020
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2021-05-05 08:32:47 +0300

    Korjaa Aikataulun e2e testiä koska ikoni muuttui

--- cypress/integration/aikataulunakyma.js
+++ cypress/integration/aikataulunakyma.js
@@ -23,7 +23,7 @@ describe('Aikataulunäkymien avaaminen päällystysurakassa', function () {
 
     it("Aikataulun avaaminen toimii päällystyksessä", function () {
         cy.contains('.klikattava', 'Aikataulu', {timeout: 40000}).click()
-        cy.get('.livicon-chevron.livicon-chevron-right', {timeout: 40000}).first().click()
+        cy.get('.navigation-ympyrassa', {timeout: 40000}).first().click()
         // Ei saa näkyä alikohteet omassa taulukossaan
         cy.contains('.panel-title', 'Kohteen tierekisteriosoitteet').should('not.visible')
         cy.contains('.panel-title', 'Muut tierekisteriosoitteet').should('not.visible')
