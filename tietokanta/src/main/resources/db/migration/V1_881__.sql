dd0d00de37c9482650d1bef3e7f0f7ddcbc3274b
Markus Vähäkangas <markus.vahakangas@solita.fi>
2021-05-20 10:12:33 +0300

    Päivitä paikkauskohteen tila valmiiksi jos se on null
    ja paikkauskohteelle työmenetelmä, jos se on null

--- tietokanta/src/main/resources/db/migration/V1_881__.sql
+++ tietokanta/src/main/resources/db/migration/V1_881__.sql
@@ -15,3 +15,20 @@ ALTER TABLE paikkauskohde
 ALTER TABLE paikkaus
     ADD COLUMN massamaara NUMERIC default null,
     ADD COLUMN "pinta-ala" NUMERIC default null;
+
+
+-- Päivitetään "vanhoille" paikkauskohteille työmenetelmät paikkaustoteumien perusteella
+UPDATE paikkauskohde pk
+   SET tyomenetelma = (SELECT p.tyomenetelma
+                         FROM paikkaus p
+                        WHERE p."paikkauskohde-id" = pk.id
+                          AND p.tyomenetelma IS NOT NULL
+                        ORDER BY p.id DESC
+                        LIMIT 1)
+WHERE pk.tyomenetelma IS NULL;
+
+-- Päivitetään "vanhoille" paikkauskohteille paikkauskohde-tila -> valmis, jotta niitäkin voidaan tarkistella
+-- paikkauskohdelistauksessa
+UPDATE paikkauskohde pk
+   SET "paikkauskohteen-tila" = 'valmis'
+ WHERE pk."paikkauskohteen-tila" IS NULL;
