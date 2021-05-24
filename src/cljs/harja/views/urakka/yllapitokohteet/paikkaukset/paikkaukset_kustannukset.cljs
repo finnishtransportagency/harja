324b8e35a7114becfc37b869c8ef44428ac53381
Markus Vähäkangas <markus.vahakangas@solita.fi>
2021-05-24 12:23:15 +0300

    Vielä muutos kustannusten tila parametriksi

--- src/cljs/harja/views/urakka/yllapitokohteet/paikkaukset/paikkaukset_kustannukset.cljs
+++ src/cljs/harja/views/urakka/yllapitokohteet/paikkaukset/paikkaukset_kustannukset.cljs
@@ -3,7 +3,7 @@
             [reagent.core :as r]
             [harja.tiedot.urakka.paikkaukset-kustannukset :as tiedot]
             [harja.tiedot.urakka.paikkaukset-yhteinen :as yhteiset-tiedot]
-            [harja.tiedot.urakka.urakka :as tila]
+            [harja.tiedot.urakka.paikkaukset-toteumat :as t-paikkaukset-toteumat]
             [harja.ui.debug :as debug]
             [harja.loki :refer [log]]
             [harja.ui.grid :as grid]
@@ -97,6 +97,6 @@
 
 (defn kustannukset [ur]
   (komp/luo
-    (komp/sisaan #(yhteiset-tiedot/nakyman-urakka tila ur))
+    (komp/sisaan #(yhteiset-tiedot/nakyman-urakka t-paikkaukset-toteumat/filtterit ur))
     (fn [_]
       [tuck/tuck tiedot/app kustannukset*])))
