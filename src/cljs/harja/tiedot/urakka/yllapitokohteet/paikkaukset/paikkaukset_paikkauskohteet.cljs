9b41ecd670f343e0a4d506b8493676684f67ce84
Jukka Länsineva <jukka.lansineva@solita.fi>
2021-07-01 15:19:47 +0300

    kommentoidut rivit pois

--- src/cljs/harja/tiedot/urakka/yllapitokohteet/paikkaukset/paikkaukset_paikkauskohteet.cljs
+++ src/cljs/harja/tiedot/urakka/yllapitokohteet/paikkaukset/paikkaukset_paikkauskohteet.cljs
@@ -358,13 +358,11 @@
                   ;; Poistetaan joku muu kuin "kaikki" valinta
                   (and (not valittu?) (not= "Kaikki" (:nimi uusi-tila)))
                   (disj valitut-tilat (:nimi uusi-tila)))] 
-      (assoc app :valitut-tilat tilat)
-      #_(hae-paikkauskohteet (-> @tila/yleiset :urakka :id) app)))
+      (assoc app :valitut-tilat tilat)))
 
   FiltteriValitseVuosi
   (process-event [{uusi-vuosi :uusi-vuosi} app]
-    (assoc app :valittu-vuosi uusi-vuosi)
-    #_(hae-paikkauskohteet (-> @tila/yleiset :urakka :id) app))
+    (assoc app :valittu-vuosi uusi-vuosi))
 
   FiltteriValitseTyomenetelma
   (process-event [{uusi-menetelma :uusi-menetelma valittu? :valittu?} app]
@@ -387,8 +385,7 @@
                        ;; Poistetaan joku muu kuin "kaikki" valinta
                        (and (not valittu?) (not= "Kaikki" (:nimi uusi-menetelma)))
                        (disj valitut-tyomenetelmat (:id uusi-menetelma)))] 
-      (assoc app :valitut-tyomenetelmat menetelmat)
-      #_(hae-paikkauskohteet (-> @tila/yleiset :urakka :id) app)))
+      (assoc app :valitut-tyomenetelmat menetelmat)))
 
   FiltteriValitseEly
   (process-event [{uusi-ely :uusi-ely valittu? :valittu?} app]
@@ -411,8 +408,7 @@
                  ;; Poistetaan joku muu kuin "kaikki" valinta
                  (and (not valittu?) (not= 0 (:id uusi-ely)))
                  (disj valitut-elyt (:id uusi-ely)))] 
-      (assoc app :valitut-elyt elyt)
-      #_(hae-paikkauskohteet (-> @tila/yleiset :urakka :id) app)))
+      (assoc app :valitut-elyt elyt)))
 
   TiedostoLadattu
   (process-event [{vastaus :vastaus} app]
