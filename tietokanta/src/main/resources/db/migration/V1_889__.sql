2303596a9bdf46ffaa71a7ad37c11cb026feb338
Petar Teodosin Jr <petar.teodosin@solita.fi>
2021-07-27 14:40:41 +0300

    Lisaa insert event triggeriin

---
+++ tietokanta/src/main/resources/db/migration/V1_889__.sql
@@ -0,0 +1,6 @@
+DROP TRIGGER tg_update_paivita_toteutuneet_kustannukset_jjh ON johto_ja_hallintokorvaus;
+CREATE TRIGGER tg_update_paivita_toteutuneet_kustannukset_jjh
+    AFTER INSERT OR UPDATE OR DELETE
+    ON johto_ja_hallintokorvaus
+    FOR EACH ROW
+EXECUTE PROCEDURE paivita_toteutuneet_kustannukset_jjh();
