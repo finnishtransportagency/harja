888404016f01687984b3012b3f6381e25bf7c9b4
Petar Teodosin Jr <petar.teodosin@solita.fi>
2021-07-28 16:45:15 +0300

    Lisää "insert event" myös kustanusarvioitu_tyo taulukkoon
    Ei ole varma tarvitseko se.

--- tietokanta/src/main/resources/db/migration/V1_889__.sql
+++ tietokanta/src/main/resources/db/migration/V1_889__.sql
@@ -1,3 +1,11 @@
+DROP TRIGGER tg_update_paivita_toteutuneet_kustannukset_kat ON kustannusarvioitu_tyo;
+CREATE TRIGGER tg_update_paivita_toteutuneet_kustannukset_kat
+    AFTER INSERT OR UPDATE OR DELETE
+    ON kustannusarvioitu_tyo
+    FOR EACH ROW
+EXECUTE PROCEDURE paivita_toteutuneet_kustannukset_kat();
+
+
 DROP TRIGGER tg_update_paivita_toteutuneet_kustannukset_jjh ON johto_ja_hallintokorvaus;
 CREATE TRIGGER tg_update_paivita_toteutuneet_kustannukset_jjh
     AFTER INSERT OR UPDATE OR DELETE
