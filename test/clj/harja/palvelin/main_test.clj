5e228c0d66f225918088bf27665d22756d0b6bee
Petri Sirkkala <petri.sirkkala@solita.fi>
2021-09-08 11:16:49 +0300

    Korjattu `:velho` -> `:yha-velho` `ei-statusta` mapissa

--- test/clj/harja/palvelin/main_test.clj
+++ test/clj/harja/palvelin/main_test.clj
@@ -174,7 +174,7 @@
     :yllapitokohteet :muokkauslukko :yhteyshenkilot :toimenpidekoodit :pohjavesialueet
     :materiaalit :selainvirhe :valitavoitteet :siltatarkastukset :lampotilat :maksuerat
     :liitteet :laadunseuranta :tarkastukset :ilmoitukset :tietyoilmoitukset
-    :turvallisuuspoikkeamat :integraatioloki-palvelu :raportit :yha :velho :tr-haku
+    :turvallisuuspoikkeamat :integraatioloki-palvelu :raportit :yha :yha-velho :tr-haku
     :geometriapaivitykset :api-yhteysvarmistus #_:sonja-jms-yhteysvarmistus :tilannekuva
     :tienakyma :karttakuvat :debug :sahke :api-jarjestelmatunnukset :geometria-aineistot
     :organisaatiot :api-urakat :api-laatupoikkeamat :api-paivystajatiedot :api-pistetoteuma
