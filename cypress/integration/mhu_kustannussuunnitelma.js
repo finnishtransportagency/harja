230c9d0296bd29b59a26095c0eb55471ebfda7e5
Toni Dahl <toni.dahl@solita.fi>
2021-09-10 18:36:22 +0300

    Lisää cypress-testi osion vahvistuksen kumoamiselle.
    RelNotes:
    Related:

--- cypress/integration/mhu_kustannussuunnitelma.js
+++ cypress/integration/mhu_kustannussuunnitelma.js
@@ -1834,8 +1834,41 @@ describe('Tilaajan rahavaraukset osio', function () {
 })
 
 describe('Osion vahvistaminen', function () {
-//TODO: Tee testi, jossa perutaan vahvistaminen.
 //TODO: Tee testi, jossa osiota muokataan vahvistamisen jälkeen.
+    beforeEach(function () {
+        cy.intercept('POST', '_/vahvista-kustannussuunnitelman-osa-vuodella')
+            .as('vahvista-kustannussuunnitelman-osa-vuodella');
+
+        cy.intercept('POST', '_/kumoa-suunnitelman-osan-vahvistus-hoitovuodelle')
+            .as('kumoa-suunnitelman-osan-vahvistus-hoitovuodelle');
+    });
+
+    it('Testaa Hankintakustannukset osion vahvistaminen', function () {
+        // Klikkaa osion vahvistusnappulaa
+        cy.get('[data-cy="vahvista-osio-hankintakustannukset"]')
+            .click()
+            .find('[data-cy="vahvista-osio-btn"]')
+            .click();
+
+        // Pitäisi tulla esiin onnistumis-alert, joka piilotetaan klikkaamalla.
+        cy.get('.modal')
+            .find('.alert-success')
+            .click();
+
+
+        // Varmista, että tallennuskyselyt menevät läpi
+        cy.wait('@vahvista-kustannussuunnitelman-osa-vuodella')
+
+
+        // -- Palauta UI-valinnat ennalleen --
+
+        // Pienennä osion vahvistuslaatikko
+        cy.get('[data-cy="vahvista-osio-hankintakustannukset"]')
+            .click();
+
+        // -- Arvojen tarkastus --
+        ks.testaaTilayhteenveto(1, 'Hankintakustannukset', true);
+    });
 
     it('Testaa Erillishankinnat osion vahvistaminen', function () {
         // Klikkaa osion vahvistusnappulaa
@@ -1849,10 +1882,46 @@ describe('Osion vahvistaminen', function () {
             .find('.alert-success')
             .click();
 
+        // Varmista, että tallennuskyselyt menevät läpi
+        cy.wait('@vahvista-kustannussuunnitelman-osa-vuodella')
+
+
+        // -- Palauta UI-valinnat ennalleen --
+
+        // Pienennä osion vahvistuslaatikko
+        cy.get('[data-cy="vahvista-osio-erillishankinnat"]')
+            .click();
+
 
         // -- Arvojen tarkastus --
         ks.testaaTilayhteenveto(1, 'Erillishankinnat', true);
+    });
 
+    it('Kumoa Erillishankinnat osion vahvistus', function () {
+        // Klikkaa osion vahvistuksen kumoamis nappia
+        cy.get('[data-cy="vahvista-osio-erillishankinnat"]')
+            .click()
+            .find('[data-cy="kumoa-osion-vahvistus-btn"]')
+            .click();
+
+        // Pitäisi tulla esiin onnistumis-alert, joka piilotetaan klikkaamalla.
+        cy.get('.modal')
+            .find('.alert-success')
+            .click();
+
+        // Varmista, että tallennuskyselyt menevät läpi
+        cy.wait('@kumoa-suunnitelman-osan-vahvistus-hoitovuodelle')
+
+
+        // -- Palauta UI-valinnat ennalleen --
+
+        // Pienennä osion vahvistuslaatikko
+        cy.get('[data-cy="vahvista-osio-erillishankinnat"]')
+            .click();
+
+
+        // -- Arvojen tarkastus --
+        ks.testaaTilayhteenveto(1, 'Erillishankinnat', false);
     });
 
 });
@@ -1884,9 +1953,15 @@ describe('Tarkasta tallennetut arvot', function () {
         });
 
         describe('Testaa pääyhteenvedon osioiden tilat ja summat', function () {
-            it('Testataan onko Erillishankinnat osio vahvistettu 1. hoitovuodelle', function () {
-                ks.testaaTilayhteenveto(1, 'Erillishankinnat', true,);
-                ks.testaaTilayhteenveto(2, 'Erillishankinnat', false,);
+            it('Testataan onko Hankintakustannukset osio vahvistettu 1. hoitovuodelle', function () {
+                ks.testaaTilayhteenveto(1, 'Hankintakustannukset', true);
+                ks.testaaTilayhteenveto(2, 'Hankintakustannukset', false);
+            });
+
+            // Testataan tallentuiko vahvistuksen kumoaminen tietokantaan asti.
+            it('Testataan, että Erillishankinnat osio ei ole enää vahvistettu.', function () {
+                ks.testaaTilayhteenveto(1, 'Erillishankinnat', false);
+                ks.testaaTilayhteenveto(2, 'Erillishankinnat', false);
             });
         })
 
