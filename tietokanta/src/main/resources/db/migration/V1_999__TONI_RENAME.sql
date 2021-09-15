3a77b5ae1f234fd35f5c1fe5fa330ac8bee087ee
Toni Dahl <toni.dahl@solita.fi>
2021-09-15 17:27:43 +0300

    Lisää indeksikorjausta varten sarakkeet urakka_tavoite tauluun. Lisää "osio"-sarake kustannusarvion lukuihin liittyviin tauluihin.
    RelNotes:
    Related:

--- tietokanta/src/main/resources/db/migration/V1_999__TONI_RENAME.sql
+++ tietokanta/src/main/resources/db/migration/V1_999__TONI_RENAME.sql
@@ -7,12 +7,22 @@ ALTER TABLE johto_ja_hallintokorvaus
 ALTER TABLE kiinteahintainen_tyo
     DROP CONSTRAINT "kiinteahintainen_tyo_toimenpideinstanssi_tehtavaryhma_tehta_key";
 
--- indeksikorjattu hinta tallennetaan omana rivinään ja tätä merkkaa boolean
--- versio on juokseva numero, joka lähtee nollasta. jos kustannussuunnitelmaa muokataan jälkeenpäin,
--- nostetaan versionumeroa tietoja tallennettaessa. näin saadaan sitten muodostettua historiatiedot.
+ALTER TABLE urakka_tavoite
+    DROP CONSTRAINT "urakka_tavoite_urakka_hoitokausi_key";
+
+
+CREATE TYPE SUUNNITTELU_OSIO AS ENUM ('hankintakustannukset', 'erillishankinnat', 'johto-ja-hallintokorvaus',
+    'hoidonjohtopalkkio', 'tavoite-ja-kattohinta', 'tilaajan-rahavaraukset');
+
+-- Indeksikorjattu hinta tallennetaan omana rivinään ja tätä merkkaa boolean
+-- versio on juokseva numero, joka lähtee nollasta.
+-- Jos kustannussuunnitelmaa muokataan jälkeenpäin, nostetaan versionumeroa tietoja tallennettaessa.
+-- Näin saadaan sitten muodostettua historiatiedot.
 ALTER TABLE kustannusarvioitu_tyo
     ADD COLUMN indeksikorjattu BOOLEAN NOT NULL DEFAULT FALSE,
     ADD COLUMN versio          NUMERIC NOT NULL DEFAULT 0,
+    -- Mistä kustannussuunnitelman osiosta rivi on peräisin.
+    ADD COLUMN osio            SUUNNITTELU_OSIO,
     ADD CONSTRAINT uniikki_kustannusarvioitu_tyo
         UNIQUE (toimenpideinstanssi, tehtava, sopimus, vuosi, kuukausi, versio,
                 indeksikorjattu);
@@ -20,6 +30,8 @@ ALTER TABLE kustannusarvioitu_tyo
 ALTER TABLE johto_ja_hallintokorvaus
     ADD COLUMN indeksikorjattu BOOLEAN NOT NULL DEFAULT FALSE,
     ADD COLUMN versio          NUMERIC NOT NULL DEFAULT 0,
+    -- Mistä kustannussuunnitelman osiosta rivi on peräisin.
+    ADD COLUMN osio            SUUNNITTELU_OSIO,
     ADD CONSTRAINT uniikki_johto_ja_hallintokorvaus
         EXCLUDE ("urakka-id" WITH =, "toimenkuva-id" WITH =, vuosi WITH =, kuukausi WITH =,
         indeksikorjattu WITH =, versio WITH =, ei_ennen_urakka("ennen-urakkaa", id) WITH =);
@@ -27,12 +39,17 @@ ALTER TABLE johto_ja_hallintokorvaus
 ALTER TABLE kiinteahintainen_tyo
     ADD COLUMN indeksikorjattu BOOLEAN NOT NULL DEFAULT FALSE,
     ADD COLUMN versio          NUMERIC NOT NULL DEFAULT 0,
+    -- Mistä kustannussuunnitelman osiosta rivi on peräisin.
+    ADD COLUMN osio            SUUNNITTELU_OSIO,
     ADD CONSTRAINT uniikki_kiinteahintainen_tyo
         UNIQUE (toimenpideinstanssi, tehtavaryhma, tehtava, sopimus, vuosi,
                 kuukausi, versio, indeksikorjattu);
 
-CREATE TYPE SUUNNITTELU_OSIO AS ENUM ('hankintakustannukset', 'erillishankinnat', 'johto-ja-hallintokorvaus',
-    'hoidonjohtopalkkio', 'tavoite-ja-kattohinta', 'tilaajan-rahavaraukset');
+ALTER TABLE urakka_tavoite
+    ADD COLUMN indeksikorjattu BOOLEAN NOT NULL DEFAULT FALSE,
+    ADD COLUMN versio          NUMERIC NOT NULL DEFAULT 0,
+    ADD CONSTRAINT uniikki_urakka_tavoite_key
+        UNIQUE (urakka, hoitokausi, versio, indeksikorjattu);
 
 CREATE TABLE suunnittelu_kustannussuunnitelman_tila (
     id            SERIAL PRIMARY KEY,
@@ -50,7 +67,8 @@ CREATE TABLE suunnittelu_kustannussuunnitelman_tila (
         UNIQUE (urakka, osio, hoitovuosi)
 );
 
--- jos vahvistamisen jälkeen suunnitelmaa muokataan, tallennetaan selite sille. muokkaus liittyy aina johonkin yksittäiseen riviin, joka voi olla jhk/kiint.tyo/kust.tyo
+-- Jos vahvistamisen jälkeen suunnitelmaa muokataan, tallennetaan selite sille.
+-- Muokkaus liittyy aina johonkin yksittäiseen riviin, joka voi olla jhk/kiint.tyo/kust.tyo
 CREATE TABLE suunnittelu_kustannussuunnitelman_muutos (
     id                       SERIAL PRIMARY KEY,
     johto_ja_hallintokorvaus INT REFERENCES johto_ja_hallintokorvaus (id),
