1ca2fa50734277edb2f1b84b6d45da66a29dcf00
Toni Dahl <toni.dahl@solita.fi>
2021-09-23 17:30:10 +0300

    Lisää apurifunktiot indeksikorjaamattomien rivien hakuun tauluista.
    RelNotes:
    Related:

--- src/clj/harja/kyselyt/budjettisuunnittelu.sql
+++ src/clj/harja/kyselyt/budjettisuunnittelu.sql
@@ -52,7 +52,44 @@ WHERE "urakka-id" = :urakka-id
 -- name:lisaa-oma-johto-ja-hallintokorvaus-toimenkuva<!
 INSERT INTO johto_ja_hallintokorvaus_toimenkuva (toimenkuva, "urakka-id")
 VALUES (:toimenkuva, :urakka-id)
-RETURNING id
+RETURNING id;
+
+--name: hae-vahvistamattomat-kiinteahintaiset-tyot
+SELECT *
+  FROM kiinteahintainen_tyo kht
+           LEFT JOIN toimenpideinstanssi tpi ON kht.toimenpideinstanssi = tpi.id
+ WHERE tpi.urakka = :urakka-id
+   AND kht.vuosi = :hoitovuosi
+   AND kht.osio = :osio
+   AND kht.indeksikorjaus_vahvistettu IS NULL
+   AND kht.versio = 0;
+
+--name: hae-vahvistamattomat-kustannusarvioidut-tyot
+SELECT *
+  FROM kustannusarvioitu_tyo kat
+           LEFT JOIN toimenpideinstanssi tpi ON kat.toimenpideinstanssi = tpi.id
+ WHERE tpi.urakka = :urakka-id
+   AND kat.vuosi = :hoitovuosi
+   AND kat.osio = :osio
+   AND kat.indeksikorjaus_vahvistettu IS NULL
+   AND kat.versio = 0;
+
+--name: hae-vahvistamattomat-jh-korvaukset
+SELECT *
+  FROM johto_ja_hallintokorvaus jh
+ WHERE jh."urakka-id" = :urakka-id
+   AND jh.vuosi = :hoitovuosi
+   AND jh.indeksikorjaus_vahvistettu IS NULL
+   AND jh.versio = 0;
+
+--name: hae-vahvistamattomat-budjettitavoitteet
+SELECT *
+  FROM urakka_tavoite ut
+ WHERE ut.urakka = :urakka-id
+   -- hoitokausi ei ole hoitovuosi e.g. 2020, vaan hoitovuoden järjestysnumero e.g. 1
+   AND ut.hoitokausi = :hoitokausi
+   AND ut.indeksikorjaus_vahvistettu IS NULL
+   AND ut.versio = 0;
 
 -- name: hae-suunnitelman-tilat
 select * from suunnittelu_kustannussuunnitelman_tila skt where skt.urakka = :urakka;
--- src/clj/harja/kyselyt/kustannusarvioidut_tyot.sql
+++ src/clj/harja/kyselyt/kustannusarvioidut_tyot.sql
@@ -9,13 +9,13 @@ SELECT kat.id,
        tr.yksiloiva_tunniste AS "tehtavaryhman-tunniste",
        tpik_tpi.koodi AS "toimenpiteen-koodi",
        kat.sopimus,
-       kat.indeksikorjattu
-FROM kustannusarvioitu_tyo kat
-       LEFT JOIN toimenpideinstanssi tpi ON kat.toimenpideinstanssi = tpi.id
-       LEFT JOIN toimenpidekoodi tpik_tpi ON tpik_tpi.id = tpi.toimenpide
-       LEFT JOIN toimenpidekoodi tpik_t ON tpik_t.id = kat.tehtava
-       LEFT JOIN tehtavaryhma tr ON kat.tehtavaryhma = tr.id
-WHERE tpi.urakka = :urakka and (case when 'kaikki' = :indeksikorjatut then true else kat.indeksikorjattu = :indeksikorjatut::boolean end);
+       kat.indeksikorjaus_vahvistettu
+  FROM kustannusarvioitu_tyo kat
+           LEFT JOIN toimenpideinstanssi tpi ON kat.toimenpideinstanssi = tpi.id
+           LEFT JOIN toimenpidekoodi tpik_tpi ON tpik_tpi.id = tpi.toimenpide
+           LEFT JOIN toimenpidekoodi tpik_t ON tpik_t.id = kat.tehtava
+           LEFT JOIN tehtavaryhma tr ON kat.tehtavaryhma = tr.id
+ WHERE tpi.urakka = :urakka;
 
 
 -- name: merkitse-kustannussuunnitelmat-likaisiksi!
