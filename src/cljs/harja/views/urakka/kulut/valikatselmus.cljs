2d7fa7af51d432209ef78485a47dfe8109490617
Topi Pohjosaho <topi.pohjosaho@solita.fi>
2021-09-28 14:27:26 +0300

    Vaihda "Lisää oikaisu"-napin tyyli: ensisijainen -> reunaton

--- src/cljs/harja/views/urakka/kulut/valikatselmus.cljs
+++ src/cljs/harja/views/urakka/kulut/valikatselmus.cljs
@@ -127,7 +127,7 @@
                                      (e! (valikatselmus-tiedot/->PoistaOikaisu rivi id))
                                      (reset! tallennettu-tila hoitokauden-oikaisut))
                                   {:luokka "napiton-nappi"}]))
-           :uusi-rivi-nappi-luokka "nappi-ensisijainen"
+           :uusi-rivi-nappi-luokka "nappi-reunaton"
            :lisaa-rivi "Lisää oikaisu"
            :validoi-uusi-rivi? false
            :on-rivi-blur (fn [oikaisu i]
