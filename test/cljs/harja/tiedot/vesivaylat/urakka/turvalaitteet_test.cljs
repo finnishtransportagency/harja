e53f82c7c620f43c3d460efb8afced7de0a9f46b
Markus Vähäkangas <markus.vahakangas@solita.fi>
2021-09-29 11:17:22 +0300

    Poistetaan fronttitesteistä kuolleet turvalaitteet test

--- test/cljs/harja/tiedot/vesivaylat/urakka/turvalaitteet_test.cljs
+++
@@ -1,8 +0,0 @@
-(ns harja.tiedot.vesivaylat.urakka.turvalaitteet-test
-  (:require [clojure.test :refer-macros [deftest is testing]]
-            [harja.testutils.tuck-apurit :refer-macros [vaadi-async-kutsut] :refer [e!]]
-            [harja.pvm :as pvm]))
-
-(deftest nakymaan-tuleminen
-  (is (true? (:nakymassa? (e! (tiedot/->Nakymassa? true)))))
-  (is (false? (:nakymassa? (e! (tiedot/->Nakymassa? false))))))
