9e05f1285d2d546b664bc79a35ea567dcd9ccaf3
Markus Vähäkangas <markus.vahakangas@solita.fi>
2021-09-29 10:50:52 +0300

    Testeissä ei löydy turvalaitteet namespacea. Kokeillaan, josko sen poistaminen vaikuttaisi

--- test/cljs/harja/tiedot/vesivaylat/urakka/turvalaitteet_test.cljs
+++ test/cljs/harja/tiedot/vesivaylat/urakka/turvalaitteet_test.cljs
@@ -1,6 +1,5 @@
 (ns harja.tiedot.vesivaylat.urakka.turvalaitteet-test
-  (:require [harja.tiedot.vesivaylat.urakka.turvalaitteet :as tiedot]
-            [clojure.test :refer-macros [deftest is testing]]
+  (:require [clojure.test :refer-macros [deftest is testing]]
             [harja.testutils.tuck-apurit :refer-macros [vaadi-async-kutsut] :refer [e!]]
             [harja.pvm :as pvm]))
 
