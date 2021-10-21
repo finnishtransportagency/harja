51755daffbc65447a0c3b3a34f3f4c6abd2c2d6a
Petri Sirkkala <petri.sirkkala@solita.fi>
2021-10-21 16:47:34 +0300

    Siivousta

--- tietokanta/src/main/resources/db/migration/V1_998__petrisi.sql
+++ tietokanta/src/main/resources/db/migration/V1_998__petrisi.sql
@@ -5,26 +5,28 @@
 -- Kaikkia tietoja ei haeta talteen Harjaan, vaan osa Varusteiden tiedoista saadaan jatkossakin
 -- kyselemällä tarvittaessa Velhon rajapinnasta.
 
-CREATE TABLE varustetoteuma2 (
-                                 id                       serial PRIMARY KEY,
-                                 velho_oid                varchar(128),
-                                 urakkakoodi              integer,
-                                 arvot                    varchar(4096),
-                                 karttapvm                date,
-                                 tr_numero                integer,
-                                 tr_alkuosa               integer,
-                                 tr_loppuosa              integer,
-                                 tr_alkuetaisyys          integer,
-                                 tr_loppuetaisyys         integer,
-                                 sijainti                 geometry,
-                                 tietolaji                varchar(128), -- tl506,tl501 jne.
-                                 toimenpide               varustetoteuma_tyyppi,
-                                 kuntoluokka              integer,
-                                 alkupvm                  date not null,
-                                 loppupvm                 date,
-                                 muokkaaja                text,
-                                 muokattu                 timestamp
+CREATE TABLE varustetoteuma2
+(
+    id               serial PRIMARY KEY,
+    velho_oid        varchar(128),
+    urakkakoodi      integer,
+    arvot            varchar(4096),
+    karttapvm        date,
+    tr_numero        integer,
+    tr_alkuosa       integer,
+    tr_loppuosa      integer,
+    tr_alkuetaisyys  integer,
+    tr_loppuetaisyys integer,
+    sijainti         geometry,
+    tietolaji        varchar(128), -- tl506,tl501 jne.
+    toimenpide       varustetoteuma_tyyppi,
+    kuntoluokka      integer,
+    alkupvm          date not null,
+    loppupvm         date,
+    muokkaaja        text,
+    muokattu         timestamp
 );
 
 -- Lisätään varustehaun integraation tyyppi integraatio tauluun
-INSERT INTO integraatio (jarjestelma, nimi) VALUES ('velho', 'varusteiden-haku');
+INSERT INTO integraatio (jarjestelma, nimi)
+VALUES ('velho', 'varusteiden-haku');
