596bf5f83f0084a4d92746518ee74ab82cd34589
Petri Sirkkala <petri.sirkkala@solita.fi>
2021-10-25 23:25:14 +0300

    Parannettu varustetoteuma2 constraineja

--- tietokanta/src/main/resources/db/migration/V1_998__petrisi.sql
+++ tietokanta/src/main/resources/db/migration/V1_998__petrisi.sql
@@ -7,24 +7,25 @@
 
 CREATE TABLE varustetoteuma2
 (
-    id               serial PRIMARY KEY,
-    velho_oid        varchar(128),
-    urakkakoodi      integer,
+    id               serial PRIMARY KEY not null,
+    velho_oid        varchar(128) not null,
+    urakka_id        integer not null,
     arvot            varchar(4096),
     karttapvm        date,
-    tr_numero        integer,
-    tr_alkuosa       integer,
+    tr_numero        integer not null,
+    tr_alkuosa       integer not null,
+    tr_alkuetaisyys  integer not null,
     tr_loppuosa      integer,
     tr_alkuetaisyys  integer,
     tr_loppuetaisyys integer,
-    sijainti         geometry,
-    tietolaji        varchar(128), -- tl506,tl501 jne.
-    toimenpide       varustetoteuma_tyyppi,
-    kuntoluokka      integer,
+    sijainti         geometry not null,
+    tietolaji        varchar(128) not null, -- tl506,tl501 jne.
+    toimenpide       varustetoteuma_tyyppi not null,
+    kuntoluokka      integer not null,
     alkupvm          date not null,
     loppupvm         date,
-    muokkaaja        text,
-    muokattu         timestamp
+    muokkaaja        text not null,
+    muokattu         timestamp not null
 );
 
 -- Lisätään varustehaun integraation tyyppi integraatio tauluun
