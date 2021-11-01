5f0a464c7a8b297502773b5c9f0013e84487f161
Petri Sirkkala <petri.sirkkala@solita.fi>
2021-11-01 22:52:07 +0200

    Testejä tietokantariveille

---
+++ src/clj/harja/palvelin/integraatiot/velho/sanomat/varuste_vastaanottosanoma.clj
@@ -0,0 +1,133 @@
+(ns harja.palvelin.integraatiot.velho.sanomat.varuste-vastaanottosanoma
+  (:require [taoensso.timbre :as log]))
+
+; Varusteiden nimikkeistö
+; TL 501 Kaiteet
+; TODO Mikä erottaa melurakenteiden kaiteet tavallisista kaiteista.
+; TL 503 Levähdysalueiden varusteet
+(def +poyta-ja-penkki+ "tienvarsikalustetyyppi/tvkt03")
+(def +eko-kierratyspiste+ "tienvarsikalustetyyppi/tvkt06")
+(def +kemiallisen-wc_n-tyhjennyspiste+ "tienvarsikalustetyyppi/tvkt07")
+(def +leikkialue+ "tienvarsikalustetyyppi/tvkt12")
+(def +kuntoiluvaline+ "tienvarsikalustetyyppi/tvkt13")
+(def +katos+ "tienvarsikalustetyyppi/tvkt02")
+(def +laituri+ "tienvarsikalustetyyppi/tvkt19")
+(def +pukukoppi+ "tienvarsikalustetyyppi/tvkt14")
+(def +opastuskartta+ "tienvarsikalustetyyppi/tvkt15")
+(def +tulentekopaikka+ "tienvarsikalustetyyppi/tvkt16")
+(def +polkupyorakatos+ "tienvarsikalustetyyppi/tvkt27")
+; #ext-urpo
+; Petri Sirkkala  7 days ago
+;@Erkki Mattila En löydä Latauspalvelun tienvarsikalusteet.(nd)json tiedostosta yhtään tvkt27:aa.
+; Sensijaan tvkt17 ilmenee 152 kertaa.
+;
+;Erkki Mattila  6 days ago
+;[...] Tuossa tosiaan oli joku semmonen juttu muistaakseni, että tierekkarista ei pystynyt päättelemään,
+; että onko kyseessä teline vai katos, niin ne kaikki on telineitä nyt
+;
+; Myöhemmin todettu, että kaikki (def +polkupyorateline+ "tienvarsikalustetyyppi/tvkt17") ovat TL 507 bussipysäkin varusteita
+
+(def +tl503-ominaisuustyyppi-arvot+ #{+poyta-ja-penkki+
+                                      +eko-kierratyspiste+
+                                      +kemiallisen-wc_n-tyhjennyspiste+
+                                      +leikkialue+
+                                      +kuntoiluvaline+
+                                      +katos+
+                                      +laituri+
+                                      +pukukoppi+
+                                      +opastuskartta+
+                                      +tulentekopaikka+
+                                      +polkupyorakatos+})
+; TL 504 WC
+(def +wc+ "tienvarsikalustetyyppi/tvkt11")
+; TL 505 Jätehuolto
+(def +maanpaallinen-jateastia-alle-240-l+ "tienvarsikalustetyyppi/tvkt08")
+(def +maanpaallinen-jatesailio-yli-240-l+ "tienvarsikalustetyyppi/tvkt09")
+(def +upotettu-jatesailio+ "tienvarsikalustetyyppi/tvkt10")
+(def +tl505-ominaisuustyyppi-arvot+ #{+maanpaallinen-jateastia-alle-240-l+
+                                      +maanpaallinen-jatesailio-yli-240-l+
+                                      +upotettu-jatesailio+})
+; TL 507 Bussipysäkin varusteet
+(def +roska-astia+ "tienvarsikalustetyyppi/tvkt05")
+(def +polkupyorateline+ "tienvarsikalustetyyppi/tvkt17")
+(def +aikataulukehikko+ "tienvarsikalustetyyppi/tvkt20")
+(def +penkki+ "tienvarsikalustetyyppi/tvkt04")
+(def +tl507-ominaisuustyyppi-arvot+ #{+roska-astia+
+                                      +polkupyorateline+
+                                      +aikataulukehikko+
+                                      +penkki+})
+; TL 508 Bussipysäkin katos
+(def +bussipysakin-katos+ "tienvarsikalustetyyppi/tvkt01")
+; TL 516 Hiekkalaatikot
+(def +hiekkalaatikko+ "tienvarsikalustetyyppi/tvkt18")
+; TL 518 Kivetyt alueet
+(def +liikennesaareke+ "erotusalue-tyyppi/erty05")
+(def +korotettu-erotusalue+ "erotusalue-tyyppi/erty02")
+(def +bussipysakin-odotusalue+ "erotusalue-tyyppi/erty07")
+(def +tl518_ominaisuustyyppi-arvot+ #{+liikennesaareke+
+                                      +korotettu-erotusalue+
+                                      +bussipysakin-odotusalue+})
+
+(defn filter-by-vals [pred m] (into {} (filter (fn [[k v]] (pred v)) m)))
+
+(defn paattele-varusteen-tietolaji [kohde]
+  (let [kohdeluokka (:kohdeluokka kohde)
+        rakenteelliset-ominaisuudet (get-in kohde [:ominaisuudet :rakenteelliset-ominaisuudet])
+        rakenteelliset-jarjestelmakokonaisuudet (get-in kohde [:ominaisuudet :infranimikkeisto :rakenteellinen-jarjestelmakokonaisuus])
+        melurakenne? (and false rakenteelliset-jarjestelmakokonaisuudet)
+        tl-map {:tl501 (and (= kohdeluokka "varusteet/kaiteet")
+                            (not melurakenne?))
+                :tl503 (and (= kohdeluokka "varusteet/tienvarsikalusteet")
+                            (contains? +tl503-ominaisuustyyppi-arvot+ (:tyyppi rakenteelliset-ominaisuudet)))
+                :tl504 (and (= kohdeluokka "varusteet/tienvarsikalusteet")
+                            (= +wc+ (:tyyppi rakenteelliset-ominaisuudet)))
+                :tl505 (and (= kohdeluokka "varusteet/tienvarsikalusteet")
+                            (contains? +tl505-ominaisuustyyppi-arvot+ (:tyyppi rakenteelliset-ominaisuudet)))
+                :tl506 (= kohdeluokka "varusteet/liikennemerkit")
+                :tl507 (and (= kohdeluokka "varusteet/tienvarsikalusteet")
+                            (contains? +tl507-ominaisuustyyppi-arvot+ (:tyyppi rakenteelliset-ominaisuudet)))
+                :tl508 (and (= kohdeluokka "varusteet/tienvarsikalusteet")
+                            (= +bussipysakin-katos+ (:tyyppi rakenteelliset-ominaisuudet)))
+                :tl509 (= kohdeluokka "varusteet/rumpuputket")
+                :tl512 (= kohdeluokka "varusteet/kaivot")
+                :tl513 (= kohdeluokka "varusteet/reunapaalut")
+                :tl515 (= kohdeluokka "varusteet/aidat")
+                :tl516 (and (= kohdeluokka "varusteet/tienvarsikalusteet")
+                            (= +hiekkalaatikko+ (:tyyppi rakenteelliset-ominaisuudet)))
+                :tl517 (= kohdeluokka "varusteet/portaat")
+                :tl518 (or (and (= kohdeluokka "tiealueen-poikkileikkaus/erotusalueet")
+                                (contains? +tl518_ominaisuustyyppi-arvot+ (:tyyppi rakenteelliset-ominaisuudet)))
+                           (and (= kohdeluokka "tiealueen-poikkileikkaus/luiskat")
+                                (= "luiska-tyyppi/luity01" (:tyyppi rakenteelliset-ominaisuudet))))
+                :tl520 (= kohdeluokka "varusteet/puomit-sulkulaitteet-pollarit")
+                :tl522 (= kohdeluokka "varusteet/reunatuet")
+                :tl524 (= kohdeluokka "ymparisto/viherkuviot")}
+        tl-keys (keys (filter-by-vals identity tl-map))]
+    (cond
+      (> 1 (count tl-keys)) (do (log/error
+                                  (format "Varustekohteen tietolaji ole yksikäsitteinen. oid: %s tietolajit: %s"
+                                          (:oid kohde)
+                                          tl-keys))
+                                nil)
+      (= 0 (count tl-keys)) nil
+      :else (first tl-keys))))
+
+(defn velho-kohde->harja-varustetoteuma2
+  "Muuttaa Velhosta saadun varustetiedon Harjan varustetoteuma2 muotoon."
+  [velho-kohde]
+  (let [velho_oid (:oid velho-kohde)
+        urakka_id (:a velho-kohde)
+        tr_numero (:a velho-kohde)
+        tr_alkuosa (:a velho-kohde)
+        tr_alkuetaisyys (:a velho-kohde)
+        tr_loppuosa (:a velho-kohde)
+        tr_loppuetaisyys (:a velho-kohde)
+        sijainti (:a velho-kohde)
+        tietolaji (paattele-varusteen-tietolaji velho-kohde)
+        lisatieto (:a velho-kohde)
+        toimenpide (:a velho-kohde)
+        kuntoluokka (:a velho-kohde)
+        alkupvm (:a velho-kohde)
+        loppupvm (:a velho-kohde)
+        muokkaaja (:a velho-kohde)
+        muokattu (:a velho-kohde)]))
---
+++ test/clj/harja/palvelin/integraatiot/velho/sanomat/varuste_vastaanottosanoma_test.clj
@@ -0,0 +1,100 @@
+(ns harja.palvelin.integraatiot.velho.sanomat.varuste-vastaanottosanoma-test
+  (:require [clojure.data.json :as json]
+            [clojure.test :refer :all]
+            [harja.palvelin.integraatiot.velho.sanomat.varuste-vastaanottosanoma :as varuste-vastaanottosanoma]
+            [taoensso.timbre :as log])
+  (:import (java.nio.file FileSystems)))
+
+(defn listaa-matchaavat-tiedostot [juuri glob]
+  (let [tietolaji-matcher (.getPathMatcher
+                            (FileSystems/getDefault)
+                            (str "glob:" glob))]
+    (->> juuri
+         clojure.java.io/file
+         .listFiles
+         (filter #(.isFile %))
+         (filter #(.matches tietolaji-matcher (.getFileName (.toPath %))))
+         (mapv #(.getPath %))
+         )))
+
+(defn json->kohde [json-lahde lahdetiedosto]
+  (let [lahderivi (inc (first json-lahde))                  ; inc, koska 0-based -> järjestysluvuksi
+        json (second json-lahde)]
+    (log/debug "Ladataan JSON tiedostosta: " lahdetiedosto " riviltä:" lahderivi)
+    (->
+      json
+      (json/read-str :key-fn keyword)
+      (assoc :lahdetiedosto (str lahdetiedosto) :lahderivi (str lahderivi)))))
+
+(defn lue-ndjson->kohteet [tiedosto]
+  (let [rivit (clojure.string/split-lines (slurp tiedosto))]
+    (filter #(contains? % :oid) (map #(json->kohde % tiedosto) (map-indexed #(vector %1 %2) rivit)))))
+
+(defn muunna-tiedostolista-kohteiksi [tiedostot]
+  (flatten (mapv lue-ndjson->kohteet tiedostot)))
+
+(defn lataa-kohteet [palvelu kohdeluokka]
+  (->
+    (listaa-matchaavat-tiedostot
+      (str "test/resurssit/velho/" palvelu)
+      (str "*" kohdeluokka ".jsonl"))
+    muunna-tiedostolista-kohteiksi))
+
+(defn poimi-tietolaji-oidista [oid]
+  "Poimitaan oid-merkkijonosta tietolaji.
+  Esimerkiksi: oid \"1.2.246.578.4.3.11.507.51457624\" tietolaji 507"
+  (as-> oid a
+        (clojure.string/split a #"\.")
+        (nth a 7)
+        (str "tl" a)
+        (keyword a)))
+
+(defn assertoi-kohteen-tietolaji-on-kohteen-oidissa [kohteet]
+  (log/debug (format "Testiaineistossa %s kohdetta." (count kohteet)))
+  (doseq [kohde kohteet]
+    (let [tietolaji-oidista (poimi-tietolaji-oidista (:oid kohde))
+          tietolaji-poikkeus-map {:tl514 :tl501}            ; Melukaiteet ovat kaiteita nyt! :tl514 -> :tl501
+          odotettu-tietolaji (get tietolaji-poikkeus-map tietolaji-oidista tietolaji-oidista)]
+      (let [paatelty-tietolaji (varuste-vastaanottosanoma/paattele-varusteen-tietolaji kohde)]
+        (is (= odotettu-tietolaji paatelty-tietolaji)
+            (format "Testitiedoston: %s rivillä: %s (oid: %s) odotettu tietolaji: %s ei vastaa pääteltyä tietolajia: %s"
+                    (:lahdetiedosto kohde)
+                    (:lahderivi kohde)
+                    (:oid kohde)
+                    odotettu-tietolaji
+                    paatelty-tietolaji
+                    ))))))
+
+(deftest paattele-varuteen-tl-tienvarsikalusteet-test       ;{:tl503 :tl504 :tl505 :tl507 :tl508 :tl516}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "varusterekisteri" "tienvarsikalusteet")))
+
+(deftest paattele-varuteen-tl-kaiteet-test                  ; {:tl501}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "varusterekisteri" "kaiteet")))
+
+(deftest paattele-varuteen-tl-liikennemerkit-test           ; {:tl505}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "varusterekisteri" "liikennemerkit")))
+
+(deftest paattele-varuteen-tl-rumpuputket-test              ; {:tl509}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "varusterekisteri" "rumpuputket")))
+
+(deftest paattele-varuteen-tl-kaivot-test                   ; {:tl512}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "varusterekisteri" "kaivot")))
+
+(deftest paattele-varuteen-tl-reunapaalut-test              ; {:tl513}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "varusterekisteri" "reunapaalut")))
+
+(deftest paattele-varuteen-tl-aidat-test                    ; {:tl515}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "varusterekisteri" "aidat")))
+
+(deftest paattele-varuteen-tl-portaat-test                  ; {:tl517}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "varusterekisteri" "portaat")))
+
+(deftest paattele-varuteen-tl-puomit-test                   ; {:tl520}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "varusterekisteri" "puomit")))
+
+(deftest paattele-varuteen-tl-reunatuet-test                ; {:tl522}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "varusterekisteri" "reunatuet")))
+
+(deftest paattele-varuteen-tl-viherkuviot-test              ; {:tl524}
+  (assertoi-kohteen-tietolaji-on-kohteen-oidissa (lataa-kohteet "tiekohderekisteri" "viherkuviot"))
+  )
