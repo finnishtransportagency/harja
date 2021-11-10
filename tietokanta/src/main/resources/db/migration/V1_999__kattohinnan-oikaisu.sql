64f7242c439d2d2475fb7ec895052e6e10ecaa7b
Jere Luomajoki <jere.luomajoki@solita.fi>
2021-11-10 10:32:09 +0200

    nimeä hoitokausi->hoitokauden-alkuvuosi

--- tietokanta/src/main/resources/db/migration/V1_999__kattohinnan-oikaisu.sql
+++ tietokanta/src/main/resources/db/migration/V1_999__kattohinnan-oikaisu.sql
@@ -1,12 +1,12 @@
 CREATE TABLE kattohinnan_oikaisu
 (
-    id                SERIAL PRIMARY KEY,
-    "urakka-id"       INTEGER NOT NULL REFERENCES urakka (id),
-    "luoja-id"        INTEGER REFERENCES kayttaja (id),
-    luotu             TIMESTAMP,
-    "muokkaaja-id"    INTEGER NOT NULL REFERENCES kayttaja (id),
-    muokattu          TIMESTAMP,
-    "uusi-kattohinta" NUMERIC NOT NULL,
-    hoitokausi        INT     NOT NULL,
-    poistettu         BOOLEAN
+    id                      SERIAL PRIMARY KEY,
+    "urakka-id"             INTEGER NOT NULL REFERENCES urakka (id),
+    "luoja-id"              INTEGER REFERENCES kayttaja (id),
+    luotu                   TIMESTAMP,
+    "muokkaaja-id"          INTEGER NOT NULL REFERENCES kayttaja (id),
+    muokattu                TIMESTAMP,
+    "uusi-kattohinta"       NUMERIC NOT NULL,
+    "hoitokauden-alkuvuosi" INT     NOT NULL,
+    poistettu               BOOLEAN
 );
