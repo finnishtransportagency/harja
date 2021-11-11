5320f2ef7532449fbea46e39e0e1003c2b0175ef
Petar Teodosin Jr <petar.teodosin@solita.fi>
2021-11-11 18:16:35 +0200

    Lisää test keissia, jotka näyttävät nykyinen implementaatio

--- test/clj/harja/palvelin/palvelut/tierekisteri_haku_test.clj
+++ test/clj/harja/palvelin/palvelut/tierekisteri_haku_test.clj
@@ -13,20 +13,18 @@
   (alter-var-root #'jarjestelma
                   (fn [_]
                     (component/start
-                     (component/system-map
-                      :db (tietokanta/luo-tietokanta testitietokanta)
-                      :http-palvelin (testi-http-palvelin)
-                      :tienakyma (component/using
-                                  (tierekisteri-haku/->TierekisteriHaku)
-                                  [:db :http-palvelin])))))
+                      (component/system-map
+                        :db (tietokanta/luo-tietokanta testitietokanta)
+                        :http-palvelin (testi-http-palvelin)
+                        :tienakyma (component/using
+                                     (tierekisteri-haku/->TierekisteriHaku)
+                                     [:db :http-palvelin])))))
   (testit)
   (alter-var-root #'jarjestelma component/stop))
 
 (use-fixtures :once jarjestelma-fixture)
 
 (def tienumero 6666)
-
-(def tr-osoite-malli {})
 
 (defn luo-tr-osoite [[kaista osa a-et l-et tyyppi]]
   (u (str
@@ -53,13 +51,85 @@
 
 (defn tarkista-oikeus-poikkeus [kutsu-fn]
   (try+
-   (kutsu-fn)
-   (is false "EiOikeutta poikkeusta ei heitetty")
-   (catch EiOikeutta e
-     (is true "EiOikeutta poikkeus heitetty"))))
+    (kutsu-fn)
+    (is false "EiOikeutta poikkeusta ei heitetty")
+    (catch EiOikeutta e
+      (is true "EiOikeutta poikkeus heitetty"))))
 
-(deftest testa-mita-vaan
+(deftest sama-kaista-ja-rako
   (luo-tr-osoitteet [[11 1 0 1500 1]
+                     [11 1 1500 2500 1]
+                     [11 1 4500 5000 1]])
+  (let [odotettu-arvo [{:tr-numero 6666,
+                        :tr-osa 1,
+                        :pituudet {:pituus 3000,
+                                   :ajoradat [{:osiot [{:pituus 2500,
+                                                        :kaistat
+                                                        [{:pituus 1500, :tr-kaista 11, :tr-alkuetaisyys 0}
+                                                         {:pituus 1000, :tr-kaista 11, :tr-alkuetaisyys 1500}],
+                                                        :tr-alkuetaisyys 0}
+                                                       {:pituus 500,
+                                                        :kaistat
+                                                        [{:pituus 500, :tr-kaista 11, :tr-alkuetaisyys 4500}],
+                                                        :tr-alkuetaisyys 4500}],
+                                               :tr-ajorata 1}],
+                                   :tr-alkuetaisyys 0}}]
+        tulos (kutsu-palvelua (:http-palvelin jarjestelma) :hae-tr-tiedot +kayttaja-jvh+ (parametrit tienumero 1 1))]
+    (is (= odotettu-arvo tulos) "Kaistat kenttä on yhdistetty vaikka on tr_osoitteet taulukossa kaksi riviä")))
+
+(deftest eri-kaistat-kaikki-sisalla
+  (luo-tr-osoitteet [[11 1 0 1500 1]
+                     [21 1 100 300 1]
                      [11 1 1500 2500 1]])
-  (let [tulos (kutsu-palvelua (:http-palvelin jarjestelma) :hae-tr-tiedot +kayttaja-jvh+ (parametrit tienumero 1 1))]
-    (is (= 0 tulos) "Kaikki tilaajan käyttäjät saavat saman tuloksen")))
+  (let [odotettu-arvo [{:tr-numero 6666,
+                        :tr-osa 1,
+                        :pituudet {:pituus 2500,
+                                   :ajoradat [{:osiot
+                                               [{:pituus 1500,
+                                                 :kaistat [{:pituus 1500, :tr-kaista 11, :tr-alkuetaisyys 0}
+                                                           {:pituus 200, :tr-kaista 21, :tr-alkuetaisyys 100}],
+                                                 :tr-alkuetaisyys 0}
+                                                {:pituus 1000,
+                                                 :kaistat [{:pituus 1000, :tr-kaista 11, :tr-alkuetaisyys 1500}],
+                                                 :tr-alkuetaisyys 1500}],
+                                               :tr-ajorata 1}],
+                                   :tr-alkuetaisyys 0}}]
+        tulos (kutsu-palvelua (:http-palvelin jarjestelma) :hae-tr-tiedot +kayttaja-jvh+ (parametrit tienumero 1 1))]
+    (is (= odotettu-arvo tulos) "Kaistat kenttä on yhdistetty vaikka on tr_osoitteet taulukossa kaksi riviä")))
+
+(deftest eri-kaistat-menee-yli
+  (luo-tr-osoitteet [[11 1 0 1500 1]
+                     [21 1 100 2300 1]
+                     [11 1 1500 2500 1]])
+  (let [odotettu-arvo [{:tr-numero 6666,
+                        :tr-osa 1,
+                        :pituudet {:pituus 2500,
+                                   :ajoradat [{:osiot
+                                               [{:pituus 2500,
+                                                 :kaistat [{:pituus 1500, :tr-kaista 11, :tr-alkuetaisyys 0}
+                                                           {:pituus 2200, :tr-kaista 21, :tr-alkuetaisyys 100}
+                                                           {:pituus 1000, :tr-kaista 11, :tr-alkuetaisyys 1500}],
+                                                 :tr-alkuetaisyys 0}],
+                                               :tr-ajorata 1}],
+                                   :tr-alkuetaisyys 0}}]
+        tulos (kutsu-palvelua (:http-palvelin jarjestelma) :hae-tr-tiedot +kayttaja-jvh+ (parametrit tienumero 1 1))]
+    (is (= odotettu-arvo tulos) "Kaistat kenttä on yhdistetty vaikka on tr_osoitteet taulukossa kaksi riviä")))
+
+(deftest eri-kaistat-menee-kaikien-yli
+  (luo-tr-osoitteet [[11 1 0 1500 1]
+                     [21 1 100 3300 1]
+                     [11 1 1500 2500 1]])
+  (let [odotettu-arvo [{:tr-numero 6666,
+                        :tr-osa 1,
+                        :pituudet {:pituus 2500,
+                                   :ajoradat [{:osiot
+                                               [{:pituus 3300,
+                                                 :kaistat [{:pituus 1500, :tr-kaista 11, :tr-alkuetaisyys 0}
+                                                           {:pituus 3200, :tr-kaista 21, :tr-alkuetaisyys 100}
+                                                           {:pituus 1000, :tr-kaista 11, :tr-alkuetaisyys 1500}],
+                                                 :tr-alkuetaisyys 0}],
+                                               :tr-ajorata 1}],
+                                   :tr-alkuetaisyys 0}}]
+        tulos (kutsu-palvelua (:http-palvelin jarjestelma) :hae-tr-tiedot +kayttaja-jvh+ (parametrit tienumero 1 1))]
+    (is (= odotettu-arvo tulos) "Kaistat kenttä on yhdistetty vaikka on tr_osoitteet taulukossa kaksi riviä")))
+
