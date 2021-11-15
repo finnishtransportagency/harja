3d6812d84dc89d6c76c2a378a4d7236d6701680b
Topi Pohjosaho <topi.pohjosaho@solita.fi>
2021-11-15 14:35:28 +0200

    Refaktoroi hae-budjettitavoite -kyselyä (alikysely erikseen)

--- src/clj/harja/kyselyt/budjettisuunnittelu.sql
+++ src/clj/harja/kyselyt/budjettisuunnittelu.sql
@@ -17,6 +17,11 @@ UPDATE urakka_tavoite
 
 
 -- name:hae-budjettitavoite
+WITH tavoitehinnan_oikaisut AS
+         (SELECT sum(summa) AS summa, "urakka-id", "hoitokauden-alkuvuosi"
+          FROM tavoitehinnan_oikaisu
+          WHERE NOT poistettu
+          GROUP BY "urakka-id", "hoitokauden-alkuvuosi")
 SELECT ut.id,
        ut.urakka,
        ut.hoitokausi,
@@ -27,28 +32,24 @@ SELECT ut.id,
        ut.luoja,
        ut.muokattu,
        ut.muokkaaja,
-       ut.tavoitehinta_indeksikorjattu                                                     AS "tavoitehinta-indeksikorjattu",
-       ut.tavoitehinta_siirretty_indeksikorjattu                                           AS "tavoitehinta-siirretty-indeksikorjattu",
-       ut.kattohinta_indeksikorjattu                                                       AS "kattohinta-indeksikorjattu",
-       ut.indeksikorjaus_vahvistettu                                                       AS "indeksikorjaus-vahvistettu",
+       ut.tavoitehinta_indeksikorjattu                                                        AS "tavoitehinta-indeksikorjattu",
+       ut.tavoitehinta_siirretty_indeksikorjattu                                              AS "tavoitehinta-siirretty-indeksikorjattu",
+       ut.kattohinta_indeksikorjattu                                                          AS "kattohinta-indeksikorjattu",
+       ut.indeksikorjaus_vahvistettu                                                          AS "indeksikorjaus-vahvistettu",
        ut.vahvistaja,
        ut.versio,
-       (ut.tavoitehinta_indeksikorjattu + COALESCE(t.summa, 0))                            AS "tavoitehinta-oikaistu",
-       COALESCE(ko."uusi-kattohinta", (kattohinta_indeksikorjattu + COALESCE(t.summa, 0))) AS "kattohinta-oikaistu",
-       (EXTRACT(YEAR from u.alkupvm) + ut.hoitokausi - 1)::INTEGER                          AS "hoitokauden-alkuvuosi"
-from urakka_tavoite ut
+       (ut.tavoitehinta_indeksikorjattu + COALESCE(t.summa, 0))                               AS "tavoitehinta-oikaistu",
+       COALESCE(ko."uusi-kattohinta", (ut.kattohinta_indeksikorjattu + COALESCE(t.summa, 0))) AS "kattohinta-oikaistu",
+       (EXTRACT(YEAR from u.alkupvm) + ut.hoitokausi - 1)::INTEGER                            AS "hoitokauden-alkuvuosi"
+FROM urakka_tavoite ut
          LEFT JOIN urakka u ON ut.urakka = u.id
          LEFT JOIN kattohinnan_oikaisu ko ON (u.id = ko."urakka-id" AND
                                               EXTRACT(YEAR from u.alkupvm) + ut.hoitokausi - 1 =
                                               ko."hoitokauden-alkuvuosi" AND
                                               NOT ko.poistettu)
-         LEFT JOIN (SELECT SUM(t.summa) AS summa, t."urakka-id", t."hoitokauden-alkuvuosi"
-                    FROM tavoitehinnan_oikaisu t
-                    WHERE NOT t.poistettu
-                    GROUP BY t."urakka-id", t."hoitokauden-alkuvuosi") t ON
-                                    (ut.urakka = t."urakka-id"
-                                        AND EXTRACT(YEAR from u.alkupvm) + ut.hoitokausi - 1 =
-                                            ko."hoitokauden-alkuvuosi")
+         LEFT JOIN tavoitehinnan_oikaisut t ON u.id = t."urakka-id" AND
+                                               EXTRACT(YEAR from u.alkupvm) + ut.hoitokausi - 1 =
+                                               t."hoitokauden-alkuvuosi"
 WHERE urakka = :urakka
 ORDER BY ut.hoitokausi;
 
