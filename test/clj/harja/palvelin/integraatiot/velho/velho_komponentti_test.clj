65739cef9e41e4516355d3195652505ab7f9925a
Petri Sirkkala <petri.sirkkala@solita.fi>
2021-11-16 02:45:16 +0200

    Uusin voittaa testi

--- test/clj/harja/palvelin/integraatiot/velho/velho_komponentti_test.clj
+++ test/clj/harja/palvelin/integraatiot/velho/velho_komponentti_test.clj
@@ -443,7 +443,7 @@
     ; TARKASTA
     (is (= (+ 1 @annettu-tyhja-oid-vastaus) @saatu-tyhja-oid-vastaus))))
 
-#_(deftest varuste-toteuma-paivittyy-uusin-voittaa-test
+(deftest varuste-toteuma-paivittyy-uusin-voittaa-test
     "On mahdollista, että Velhosta tulee uudelleen vanha toteuma samalla `velho_oid` ja `muokattu` tiedoilla.
     Historiaa saatetaan muokata.
     Silloin tallennetaan tiedot siltä varalta, että jos ne ovat kuitenkin muuttuneet. Uusin tieto voitaa.
@@ -463,9 +463,65 @@
     itseasiassa version voimassaolon loppu (tai miksei alkukin) voi olla myös tulevaisuudessa. Hauissa voi antaa
     tilannepäivän parametrina jolloin saadaan sinä päivänä voimassaollut versio kohteesta. Oletuksena annetaan kuluvan päivän versio.
     Päivämäärätaso tosiaan riittää version voimassaololle. Ei noi oikeat tiekohteet montaa kertaa päivässä muutu.
-    Jos on jokin virheellinen tieto versiolla niin versioita voi myös muuttaa jälkikäteen (eli korjata historiaa)")
+    Jos on jokin virheellinen tieto versiolla niin versioita voi myös muuttaa jälkikäteen (eli korjata historiaa)"
+    ; ASETA
+    (u "DELETE FROM varustetoteuma2")
+    (let [testitunniste "uusin-voittaa-test"
+          odotettu-syotetiedostoparien-maara 1                ;Tämä varmistaa, ettei testisyötteitä jää käyttämättä
+          fake-token-palvelin (fn [_ {:keys [body headers]} _]
+                                "{\"access_token\":\"TEST_TOKEN\",\"expires_in\":3600,\"token_type\":\"Bearer\"}")
+          odotettu-kohteet-vastaus (atom {})
+          odotettu-oidit-vastaus (atom {})
+          odotettu-ei-tyhja-oid-vastaus (atom 0)
+          saatu-ei-tyhja-oid-vastaus (atom 0)
+          odotettu-tyhja-oid-vastaus (atom 0)
+          saatu-tyhja-oid-vastaus (atom 0)
+          laske-oid-vastaukset (fn [raportoi-oid-haku-fn oidit url]
+                                 (if (= 0 (count oidit))
+                                   (swap! saatu-tyhja-oid-vastaus inc)
+                                   (swap! saatu-ei-tyhja-oid-vastaus inc))
+                                 (raportoi-oid-haku-fn oidit url))
+          fake-tunnisteet (fn [_ {:keys [body headers url]} _]
+                            (is (= "Bearer TEST_TOKEN" (get headers "Authorization")) "Oikeaa autorisaatio otsikkoa ei käytetty")
+                            (let [lahde (lahde-oid-urlista url)]
+                              (if (olemassa-testi-tiedostot? lahde testitunniste)
+                                (let [oidit-vastaus (slurp (testi-tiedosto-oideille lahde testitunniste))
+                                      kohteet-vastaus (slurp (testi-tiedosto-kohteille lahde testitunniste))]
+                                  (reset! odotettu-oidit-vastaus oidit-vastaus)
+                                  (reset! odotettu-kohteet-vastaus kohteet-vastaus)
+                                  (swap! odotettu-ei-tyhja-oid-vastaus inc)
+                                  {:status 200 :body @odotettu-oidit-vastaus})
+                                (do (reset! odotettu-oidit-vastaus nil)
+                                    (reset! odotettu-kohteet-vastaus nil)
+                                    (swap! odotettu-tyhja-oid-vastaus inc)
+                                    {:status 200 :body "[]"}))))
+          fake-kohteet (fn [_ {:keys [body headers url]} _]
+                         (is (= (json/read-str @odotettu-oidit-vastaus) (json/read-str body))
+                             "Odotettiin kohteiden hakua samalla oid-listalla kuin hae-oid antoi")
+                         (is (= "Bearer TEST_TOKEN" (get headers "Authorization")) "Oikeaa autorisaatio otsikkoa ei käytetty")
+                         {:status 200 :body @odotettu-kohteet-vastaus})]
+      ; SUORITA
+      (with-fake-http
+        [{:url +velho-token-url+ :method :post} fake-token-palvelin
+         {:url +varuste-tunnisteet-regex+ :method :get} fake-tunnisteet
+         {:url +varuste-kohteet-regex+ :method :post} fake-kohteet]
+        (let [raportoi-oid-haku-fn velho-integraatio/varuste-raportoi-oid-haku]
+          (with-redefs [velho-integraatio/varuste-raportoi-oid-haku (partial laske-oid-vastaukset raportoi-oid-haku-fn)]
+            (velho-integraatio/hae-varustetoteumat (:velho-integraatio jarjestelma))))
+        )
+      ; TARKASTA
+      (is (= @odotettu-ei-tyhja-oid-vastaus @saatu-ei-tyhja-oid-vastaus) "Odotettiin samaa määrää ei-tyhjiä oid-listoja, kuin fake-velho palautti.")
+      (is (= odotettu-syotetiedostoparien-maara @saatu-ei-tyhja-oid-vastaus)
+          "Testitiedostoja on eri määrä kuin fake-tunnisteissa on haettu. Kaikki testitiedostot on käytettävä testissä.")
 
-#_(deftest varuste-velho-palauttaa-virheellisesti-historiassa-eri-oidit-samalle-kohteelle-test) ;TODO Kirjataan warn-lokiin
+      (is (= @odotettu-tyhja-oid-vastaus @saatu-tyhja-oid-vastaus) "Odotettiin samaa määrää tyhjiä oid-listoja, kuin fake-velho palautti.")
+
+      (let [kaikki-varustetoteumat (varuste-lue-kaikki-kohteet) ; TODO tarkista, että kannassa oid-lista vastaa testissä syötettyjä
+            expected-varustetoteuma-maara 1]
+        (is (= expected-varustetoteuma-maara (count kaikki-varustetoteumat))
+            (str "Odotettiin " expected-varustetoteuma-maara " varustetoteumaa tietokannassa testin jälkeen"))
+        (let [kohde (first kaikki-varustetoteumat)]
+          (is (= (:muokkaaja kohde) "uusi muokkaaja") "Odotettiin uusimman tiedon korvanneen vanhan.")))))
 
 (deftest urakka-id-kohteelle-test-test
   (let [db (:db jarjestelma)
