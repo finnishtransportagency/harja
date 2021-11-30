988aa4d5849cb7e48729811de2885ec6651d05b9
Markus Vähäkangas <markus.vahakangas@solita.fi>
2021-11-30 14:52:53 +0200

    Päivitä toteutuneiden kustannusten toimintaa muokkaamalla triggerit kuntoon, jotka päivittävät arvoja jos alkuperäiset muuttuvat (Esim jos vahvistetaan indeksi)

--- tietokanta/src/main/resources/db/migration/V1__912__.sql
+++ tietokanta/src/main/resources/db/migration/V1__912__.sql
@@ -31,9 +31,6 @@ BEGIN
     WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', k.vuosi, k.kuukausi, 1)::DATE))) < date_trunc('month', pvm)
       -- Siirretään vain ne, joita ei ole vielä siirretty
       AND k."siirretty?" = false
-      -- Siirretään vain indeksikorjauksen vahvistetut asiat
-      AND k.indeksikorjaus_vahvistettu IS NOT NULL
-      AND k.summa_indeksikorjattu IS NOT NULL
       -- Siirretään vain tietyn tehtäväryhmän tehtäviä tai tietyn toimenpidekoodin tehtäviä
       AND (k.tehtavaryhma = (SELECT id FROM tehtavaryhma
                              WHERE yksiloiva_tunniste = '37d3752c-9951-47ad-a463-c1704cf22f4c') -- Erillishankinnat (W)
@@ -78,8 +75,6 @@ BEGIN
     WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', j.vuosi, j.kuukausi, 1)::DATE))) < date_trunc('month', pvm)
       -- Ja ne, joita ei ole vielä siirretty
       AND j."siirretty?" = false
-      -- Ja jotka on indeksikorjauksen osalta vahvistettu
-      AND j.indeksikorjaus_vahvistettu IS NOT NULL
     ON CONFLICT DO NOTHING;
 
     -- Päivitetään kaikkiin juuri siirrettyihin riveihin tieto, että ne on käsitelty ja siirretty
@@ -88,9 +83,6 @@ BEGIN
     WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', k.vuosi, k.kuukausi, 1)::DATE))) < date_trunc('month', pvm)
       -- Ja vain ne, joita ei ole aiemin päivitetty siirretyksi
       AND k."siirretty?" = false
-      -- Päivitetään vain indeksikorjauksen vahvistetut asiat
-      AND k.indeksikorjaus_vahvistettu IS NOT NULL
-      AND k.summa_indeksikorjattu IS NOT NULL
       -- Päivitetään vain tietyn tehtäväryhmän tehtäviä tai tietyn toimenpidekoodin tehtäviä
       AND (k.tehtavaryhma =
            (SELECT id FROM tehtavaryhma WHERE yksiloiva_tunniste = '37d3752c-9951-47ad-a463-c1704cf22f4c')
@@ -106,9 +98,7 @@ BEGIN
         -- Päivitetään vain edellisen kuukauden jutut
     WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', j.vuosi, j.kuukausi, 1)::DATE))) < date_trunc('month', pvm)
       -- Ja vain ne, joita ei ole aiemmin päivitetty siirrettäväksi
-      AND j."siirretty?" = false
-      -- Ja jotka on indeksikorjauksen osalta vahvistettu
-      AND j.indeksikorjaus_vahvistettu IS NOT NULL;
+      AND j."siirretty?" = false;
 
     -- Merkitään hoidon johdon maksuerä likaiseksi kaikissa voimassaolevissa MH-urakoissa
     UPDATE maksuera
@@ -121,9 +111,6 @@ BEGIN
                     join urakka u on tpi.urakka = u.id and u.tyyppi = 'teiden-hoito'
                -- Maksueriä voi lähettää Sampoon vielä 3 kk urakan päättymisen jälkeen.
                and (u.loppupvm + interval '1 month' * 3) > pvm);
-
-
-
 END;
 $$ LANGUAGE plpgsql;
 
@@ -158,9 +145,6 @@ BEGIN
     WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', k.vuosi, k.kuukausi, 1)::DATE))) < date_trunc('month', pvm)
       -- Nyt ei kiinnitetä huomiota siihen, onko rivi jo siirretty
       -- AND k."siirretty?" = false
-      -- Siirretään vain indeksikorjauksen vahvistetut asiat
-      AND k.indeksikorjaus_vahvistettu IS NOT NULL
-      AND k.summa_indeksikorjattu IS NOT NULL
       -- Siirretään vain tietyn tehtäväryhmän tehtäviä tai tietyn toimenpidekoodin tehtäviä
       -- Automaattisesti toteumaksi lasketaan tehtäväryhmä: Erillishankinnat (W)
       -- Ja tehtävät: Toimistotarvike- ja ICT-kulut, Hoitourakan työnjohto sekä Hoidonjohtopalkkio
@@ -209,10 +193,8 @@ BEGIN
     FROM johto_ja_hallintokorvaus j
          -- Siirretään menneet
     WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', j.vuosi, j.kuukausi, 1)::DATE))) < date_trunc('month', pvm)
-      -- Eikä kiinnitetä huomiota siihen, onko ne jo siirretty vai ei
-      --AND j."siirretty?" = false
-      -- Ja jotka on indeksikorjauksen osalta vahvistettu
-      AND j.indeksikorjaus_vahvistettu IS NOT NULL
+          -- Eikä kiinnitetä huomiota siihen, onko ne jo siirretty vai ei
+          --AND j."siirretty?" = false
     ON CONFLICT (rivin_tunnistin)
         DO UPDATE SET summa_indeksikorjattu = EXCLUDED.summa_indeksikorjattu,
                       indeksikorjaus_vahvistettu = EXCLUDED.indeksikorjaus_vahvistettu, muokattu = NOW();
@@ -223,9 +205,6 @@ BEGIN
     WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', k.vuosi, k.kuukausi, 1)::DATE))) < date_trunc('month', pvm)
       -- Ja vain ne, joita ei ole aiemin päivitetty siirretyksi
       AND k."siirretty?" = false
-      -- Päivitetään vain indeksikorjauksen vahvistetut asiat
-      AND k.indeksikorjaus_vahvistettu IS NOT NULL
-      AND k.summa_indeksikorjattu IS NOT NULL
       -- Päivitetään vain tietyn tehtäväryhmän tehtäviä tai tietyn toimenpidekoodin tehtäviä
       AND (k.tehtavaryhma =
            (SELECT id FROM tehtavaryhma WHERE yksiloiva_tunniste = '37d3752c-9951-47ad-a463-c1704cf22f4c')
@@ -240,9 +219,7 @@ BEGIN
                                           -- Päivitetään vain edellisen kuukauden jutut
     WHERE (SELECT (date_trunc('MONTH', format('%s-%s-%s', j.vuosi, j.kuukausi, 1)::DATE))) < date_trunc('month', pvm)
       -- Ja vain ne, joita ei ole aiemmin päivitetty siirrettäväksi
-      AND j."siirretty?" = false
-      -- Ja jotka on indeksikorjauksen osalta vahvistettu
-      AND j.indeksikorjaus_vahvistettu IS NOT NULL;
+      AND j."siirretty?" = false;
 
     -- Merkitään hoidon johdon maksuerä likaiseksi kaikissa voimassaolevissa MH-urakoissa
     UPDATE maksuera
@@ -262,3 +239,138 @@ $$ LANGUAGE plpgsql;
 DELETE FROM toteutuneet_kustannukset;
 -- Ja suoritetaan siirto
 select siirra_budjetoidut_tyot_toteumiin_uudestaan(NOW()::DATE);
+
+
+-- Koska toteutuneet_kustannukset tauluun tulee uusia rivejä, niin triggerit pitää myös uudistaa
+-- Dropataan ensin triggeri johto_ja_hallintokorvaus tauluun
+DROP TRIGGER tg_update_paivita_toteutuneet_kustannukset_jjh ON johto_ja_hallintokorvaus;
+-- Ja dropataan myös triggeri kustannusarvioitu_tyo tauluun
+DROP TRIGGER tg_update_paivita_toteutuneet_kustannukset_kat ON kustannusarvioitu_tyo;
+
+-- Päivitetään funktio, jonka update tai delete kustannusarvoitu_tyo trigger ajaa
+CREATE OR REPLACE FUNCTION paivita_toteutuneet_kustannukset_kat() RETURNS trigger AS
+$$
+DECLARE
+    md5hash       TEXT;
+    toteutunut_id INTEGER;
+BEGIN
+    -- Jos toteutuneet_kustannukset taulussa on hash, joka vastaa vanhaa riviä,
+    -- niin poista se ja luo uusi
+    md5hash := MD5(concat(OLD.id, OLD.vuosi, OLD.kuukausi, OLD.summa, OLD.tyyppi, OLD.tehtava, OLD.tehtavaryhma,
+                          OLD.toimenpideinstanssi, OLD.sopimus, OLD.luotu, OLD.luoja, OLD.muokattu,
+                          OLD.muokkaaja)::TEXT);
+    SELECT id FROM toteutuneet_kustannukset WHERE rivin_tunnistin = md5hash INTO toteutunut_id;
+
+    IF (toteutunut_id IS NOT NULL) THEN
+        -- Poista vanha
+        DELETE FROM toteutuneet_kustannukset WHERE rivin_tunnistin = md5hash;
+
+        IF (TG_OP = 'UPDATE') THEN
+            -- Luo uusi, jos on päivityshommia kutsuttu
+            INSERT INTO toteutuneet_kustannukset (vuosi, kuukausi, summa, summa_indeksikorjattu, indeksikorjaus_vahvistettu, tyyppi, tehtava, tehtavaryhma,
+                                                  toimenpideinstanssi, sopimus_id,
+                                                  urakka_id, luotu, rivin_tunnistin)
+            VALUES (NEW.vuosi,
+                    NEW.kuukausi,
+                    NEW.summa,
+                    NEW.summa_indeksikorjattu,
+                    NEW.indeksikorjaus_vahvistettu,
+                    NEW.tyyppi,
+                    NEW.tehtava,
+                    NEW.tehtavaryhma,
+                    NEW.toimenpideinstanssi,
+                    NEW.sopimus,
+                    (SELECT s.urakka FROM sopimus s WHERE s.id = NEW.sopimus),
+                    NOW(),
+                    MD5(concat(NEW.id, NEW.vuosi, NEW.kuukausi, NEW.summa, NEW.tyyppi, NEW.tehtava, NEW.tehtavaryhma,
+                               NEW.toimenpideinstanssi, NEW.sopimus, NEW.luotu, NEW.luoja, NEW.muokattu,
+                               NEW.muokkaaja)::TEXT));
+        END IF;
+    END IF;
+    --
+    IF (TG_OP = 'UPDATE') THEN
+        RETURN NEW;
+    ELSE
+        RETURN OLD;
+    END IF;
+END ;
+$$ LANGUAGE plpgsql;
+
+-- Päivitetään funktio, jonka update tai delete johto_ja_hallintakorvaus trigger ajaa
+CREATE OR REPLACE FUNCTION paivita_toteutuneet_kustannukset_jjh() RETURNS trigger AS
+$$
+DECLARE
+    md5hash       TEXT;
+    toteutunut_id INTEGER;
+    uusihash      TEXT;
+BEGIN
+    -- Jos toteutuneet_kustannukset taulussa on hash, joka vastaa vanhaa riviä,
+    -- niin poista se ja luo uusi
+    md5hash := MD5(concat(OLD.id, OLD."urakka-id", OLD."toimenkuva-id", OLD.tunnit, OLD.tuntipalkka,
+                          OLD.luotu, OLD.luoja, OLD.muokattu, OLD.muokkaaja, OLD.vuosi, OLD.kuukausi,
+                          OLD."ennen-urakkaa", OLD."osa-kuukaudesta")::TEXT);
+    SELECT id FROM toteutuneet_kustannukset WHERE rivin_tunnistin = md5hash INTO toteutunut_id;
+
+    IF (toteutunut_id IS NOT NULL) THEN
+        -- Poista vanha
+        DELETE FROM toteutuneet_kustannukset WHERE rivin_tunnistin = md5hash;
+        IF (TG_OP = 'UPDATE') THEN
+            -- Luo uusi
+            uusihash := MD5(concat(NEW.id, NEW."urakka-id", NEW."toimenkuva-id", NEW.tunnit, NEW.tuntipalkka,
+                                   NEW.luotu, NEW.luoja, NEW.muokattu, NEW.muokkaaja, NEW.vuosi, NEW.kuukausi,
+                                   NEW."ennen-urakkaa", NEW."osa-kuukaudesta")::TEXT);
+
+            INSERT INTO toteutuneet_kustannukset (vuosi, kuukausi, summa, summa_indeksikorjattu, indeksikorjaus_vahvistettu, tyyppi, tehtava, tehtavaryhma,
+                                                  toimenpideinstanssi, sopimus_id,
+                                                  urakka_id, luotu, rivin_tunnistin)
+            VALUES (NEW.vuosi,
+                    NEW.kuukausi,
+                    (NEW.tunnit * NEW.tuntipalkka * NEW."osa-kuukaudesta"),
+                    (NEW.tunnit * NEW.tuntipalkka_indeksikorjattu * NEW."osa-kuukaudesta"),
+                    NEW.indeksikorjaus_vahvistettu,
+                    'laskutettava-tyo',
+                    null,
+                    (SELECT id
+                     FROM tehtavaryhma
+                     WHERE nimi = 'Johto- ja hallintokorvaus (J)'),
+                    (SELECT tpi.id AS id
+                     FROM toimenpideinstanssi tpi
+                              JOIN toimenpidekoodi tpk3 ON tpk3.id = tpi.toimenpide
+                              JOIN toimenpidekoodi tpk2 ON tpk3.emo = tpk2.id,
+                          maksuera m
+                     WHERE tpi.urakka = NEW."urakka-id"
+                       AND m.toimenpideinstanssi = tpi.id
+                       AND tpk2.koodi = '23150'),
+                    (SELECT id
+                     FROM sopimus s
+                     WHERE s.urakka = NEW."urakka-id"
+                       AND s.poistettu IS NOT TRUE
+                     ORDER BY s.loppupvm DESC
+                     LIMIT 1),
+                    NEW."urakka-id",
+                    NOW(),
+                    uusihash);
+        END IF;
+    END IF;
+    --
+    IF (TG_OP = 'UPDATE') THEN
+        RETURN NEW;
+    ELSE
+        RETURN OLD;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Luodaan poistettu triggeri uudestaan
+CREATE TRIGGER tg_update_paivita_toteutuneet_kustannukset_kat
+    AFTER UPDATE OR DELETE
+    ON kustannusarvioitu_tyo
+    FOR EACH ROW
+EXECUTE PROCEDURE paivita_toteutuneet_kustannukset_kat();
+
+-- Luodaan poistettu triggeri uudestaan
+CREATE TRIGGER tg_update_paivita_toteutuneet_kustannukset_jjh
+    AFTER UPDATE OR DELETE
+    ON johto_ja_hallintokorvaus
+    FOR EACH ROW
+EXECUTE PROCEDURE paivita_toteutuneet_kustannukset_jjh();
