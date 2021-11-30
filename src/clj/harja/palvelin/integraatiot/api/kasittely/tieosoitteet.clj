c36ca4f26931610c9133e33d474e745f179cc97a
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2021-11-30 09:26:44 +0200

    Poista käyttämätöntä koodia

--- src/clj/harja/palvelin/integraatiot/api/kasittely/tieosoitteet.clj
+++ src/clj/harja/palvelin/integraatiot/api/kasittely/tieosoitteet.clj
@@ -47,20 +47,3 @@
           (assoc :sijainti muunnettu-kohteen-sijainti :alikohteet muunnetut-alikohteet)
           (dissoc :vkm-id)))
     kohde))
-
-(defn muunna-alustatoimenpiteiden-tieosoitteet [vkm db kohteen-tienumero karttapvm alustatoimenpiteet]
-  (if karttapvm
-    (let [muunnettevat-alustatoimenpiteet (map-indexed
-                                            (fn [i {sijainti :sijainti :as alikohde}]
-                                              (assoc alikohde
-                                                :vkm-id (str "alustatoimenpide-" i)
-                                                :sijainti (assoc sijainti :tie (:tie sijainti))))
-                                            alustatoimenpiteet)
-          muunnetut-sijainnit (vkm/muunna-tieosoitteet-verkolta-toiselle
-                                vkm
-                                muunnettevat-alustatoimenpiteet
-                                (q-geometriapaivitykset/harjan-verkon-pvm db)
-                                karttapvm)
-          muunnetut-alustatoimenpiteet (yhdista-osoitteet muunnettevat-alustatoimenpiteet muunnetut-sijainnit)]
-      muunnetut-alustatoimenpiteet)
-    alustatoimenpiteet))
