11c16c97e83a9ec617d3dac59a204c66dfe26a2a
Jere Luomajoki <jere.luomajoki@solita.fi>
2021-12-08 14:47:53 +0200

    Muotoile vkm-kutsun parametreja ja palautusarvoa

--- src/clj/harja/palvelin/integraatiot/vkm/vkm_komponentti.clj
+++ src/clj/harja/palvelin/integraatiot/vkm/vkm_komponentti.clj
@@ -15,74 +15,51 @@
     "Muuntaa annetut tieosoitteet päivän verkolta toiselle. Jokaisella tieosoitteella täytyy olla mäpissä :vkm-id avain
     kohdistamista varten."))
 
-(defn alkuosan-vkm-tunniste [tunniste]
-  (str tunniste "-alku"))
-
-(defn loppuosan-vkm-tunniste [tunniste]
-  (str tunniste "-loppu"))
-
 (defn vkm-virhe? [hakutunnus vkm-kohteet]
   (some #(and (= hakutunnus (get % "tunniste"))
-              (not (= 1 (get % "palautusarvo"))))
+           (some? (get % "virheet")))
         vkm-kohteet))
 
 (defn hae-vkm-osoite [vkm-kohteet hakutunnus]
   (first (filter #(= hakutunnus (get % "tunniste")) vkm-kohteet)))
 
-(defn paivita-osoite [{:keys [tie aosa aet losa let ajorata] :as tieosoite} alkuosanosoite loppuosanosoite virhe?]
+(defn paivita-osoite [{:keys [tie aosa aet losa let ajr] :as tieosoite} osoite virhe?]
   (if (and (not virhe?)
-           alkuosanosoite
-           (or (nil? aosa) tieosoite))
-    (core/let [uusi-osoite
-               (assoc tieosoite
-                 :tie (get alkuosanosoite "tie" tie)
-                 :ajorata (get alkuosanosoite "ajorata" ajorata)
-                 :aosa (get alkuosanosoite "osa" aosa)
-                 :aet (get alkuosanosoite "etaisyys" aet))]
-      (if loppuosanosoite
-        (assoc uusi-osoite :losa (get loppuosanosoite "osa" losa)
-                           :let (get loppuosanosoite "etaisyys" let))
-        uusi-osoite))
-    tieosoite))
+           osoite)
+    (assoc tieosoite
+      :tie (get osoite "tie" tie)
+      :ajr (get osoite "ajovorata" ajr)
+      :aosa (get osoite "osa" aosa)
+      :aet (get osoite "etaisyys" aet)
+      :losa (get osoite "osa_loppu" losa)
+      :let (get osoite "etaisyys_loppu" let))))
 
 (defn osoitteet-vkm-vastauksesta [tieosoitteet vastaus]
   (if vastaus
     (let [osoitteet-vastauksesta (cheshire/decode vastaus)
-          vkm-osoitteet (get osoitteet-vastauksesta "tieosoitteet")]
-      (mapv (fn [{:keys [vkm-id] :as tieosoite}]
-              (let [alkuosanhakutunnus (alkuosan-vkm-tunniste vkm-id)
-                    loppuosanhakutunnus (loppuosan-vkm-tunniste vkm-id)
-                    alkuosanosoite (hae-vkm-osoite vkm-osoitteet alkuosanhakutunnus)
-                    loppuosanosoite (hae-vkm-osoite vkm-osoitteet loppuosanhakutunnus)
-                    virhe? (or (vkm-virhe? alkuosanhakutunnus vkm-osoitteet)
-                               (vkm-virhe? loppuosanhakutunnus vkm-osoitteet))]
-                (paivita-osoite tieosoite alkuosanosoite loppuosanosoite virhe?)))
+          vkm-osoitteet (mapv #(get-in osoitteet-vastauksesta ["features" % "properties"]) (range (count tieosoitteet)))]
+      (mapv (fn [{:keys [tunniste] :as tieosoite}]
+              (let [osoite (hae-vkm-osoite vkm-osoitteet tunniste)
+                    virhe? (or (vkm-virhe? osoite vkm-osoitteet))]
+                (paivita-osoite tieosoite osoite virhe?)))
             tieosoitteet))
     tieosoitteet))
 
-(defn pura-tieosoitteet [tieosoitteet]
-  (reduce into []
-          (map (fn [{:keys [tie aosa aet losa let ajr vkm-id]}]
-                 (core/let [osoitteet [{:tunniste (alkuosan-vkm-tunniste vkm-id)
-                                        :tie tie
-                                        :osa aosa
-                                        :ajorata ajr
-                                        :etaisyys aet}]]
-                   (if (and losa let)
-                     (conj osoitteet {:tunniste (loppuosan-vkm-tunniste vkm-id)
-                                      :tie tie
-                                      :osa losa
-                                      :ajorata ajr
-                                      :etaisyys let})
-                     osoitteet)))
-               tieosoitteet)))
+(defn pura-tieosoitteet [tieosoitteet paivan-verkolta paivan-verkolle]
+  (map (fn [{:keys [tie aosa aet losa let ajr tunniste]}]
+         {:tunniste tunniste
+          :tie tie
+          :osa aosa
+          :ajorata ajr
+          :etaisyys aet
+          :osa_loppu losa
+          :etaisyys_loppu let
+          :tilannepvm (pvm/pvm paivan-verkolta)
+          :kohdepvm (pvm/pvm paivan-verkolle)})
+    tieosoitteet))
 
 (defn vkm-parametrit [tieosoitteet paivan-verkolta paivan-verkolle]
-  {:in "tieosoite"
-   :out "tieosoite"
-   :tilannepvm (pvm/pvm paivan-verkolta)
-   :kohdepvm (pvm/pvm paivan-verkolle)
-   :json (cheshire/encode {:tieosoitteet (pura-tieosoitteet tieosoitteet)})})
+  {:json (cheshire/encode (pura-tieosoitteet tieosoitteet paivan-verkolta paivan-verkolle))})
 
 (defn muunna-tieosoitteet-verkolta-toiselle [{:keys [db integraatioloki url]} tieosoitteet paivan-verkolta paivan-verkolle]
   (when url
@@ -90,18 +67,19 @@
                        tieosoitteet
                        paivan-verkolta
                        paivan-verkolle))
-    (try+
-      (integraatiotapahtuma/suorita-integraatio
-        db integraatioloki "vkm" "osoitemuunnos" nil
-        (fn [konteksti]
-          (let [parametrit (vkm-parametrit tieosoitteet paivan-verkolta paivan-verkolle)
-                http-asetukset {:metodi :POST
-                                :url url
-                                :parametrit parametrit}
-                {vastaus :body} (integraatiotapahtuma/laheta konteksti :http http-asetukset)]
-            (osoitteet-vkm-vastauksesta tieosoitteet vastaus))))
-      (catch [:type virheet/+ulkoinen-kasittelyvirhe-koodi+] {:keys [virheet]}
-        false))))
+    (let [url (str url "muunna")]
+      (try+
+        (integraatiotapahtuma/suorita-integraatio
+          db integraatioloki "vkm" "osoitemuunnos" nil
+          (fn [konteksti]
+            (let [parametrit (vkm-parametrit tieosoitteet paivan-verkolta paivan-verkolle)
+                  http-asetukset {:metodi :POST
+                                  :url url
+                                  :parametrit parametrit}
+                  {vastaus :body} (integraatiotapahtuma/laheta konteksti :http http-asetukset)]
+              (osoitteet-vkm-vastauksesta tieosoitteet vastaus))))
+        (catch [:type virheet/+ulkoinen-kasittelyvirhe-koodi+] {:keys [virheet]}
+          false)))))
 
 (defrecord VKM [url]
   component/Lifecycle
