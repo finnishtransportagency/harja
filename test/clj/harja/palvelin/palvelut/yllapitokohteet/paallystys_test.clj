f2ab11f223713048ee00fac68212861d731ae49f
Petar Teodosin Jr <petar.teodosin@solita.fi>
2021-12-22 19:21:11 +0200

    Lisaa miniimi unit test

--- test/clj/harja/palvelin/palvelut/yllapitokohteet/paallystys_test.clj
+++ test/clj/harja/palvelin/palvelut/yllapitokohteet/paallystys_test.clj
@@ -922,26 +922,6 @@
                                                                               :vuosi              2021
                                                                               :paallystysilmoitus paallystysilmoitus})))))
 
-(defn- muun-kohteen-paallekkaisyys-testin-paallystekerros [materiaali]
-  [{:kohdeosa-id nil, :tr-kaista 11, :leveys 2, :kokonaismassamaara 342, :tr-ajorata 0, :pinta_ala 560, :tr-loppuosa 3, :jarjestysnro 1, :tr-alkuosa 3, :massamenekki 610.7142857142857, :tr-loppuetaisyys 5000, :nimi nil, :materiaali materiaali, :tr-alkuetaisyys 1, :tr-numero 20, :toimenpide 12}
-   {:kohdeosa-id nil, :tr-kaista 11, :leveys 2, :kokonaismassamaara 342, :tr-ajorata 0, :pinta_ala 560, :tr-loppuosa 23, :jarjestysnro 1, :tr-alkuosa 23, :massamenekki 610.7142857142857, :tr-loppuetaisyys 615, :nimi nil, :materiaali materiaali, :tr-alkuetaisyys 335, :tr-numero 28401, :toimenpide 12}
-   {:kohdeosa-id nil, :tr-kaista 11, :leveys 2, :kokonaismassamaara 234, :tr-ajorata 0, :pinta_ala 760, :tr-loppuosa 23, :jarjestysnro 1, :tr-alkuosa 23, :massamenekki 307.89473684210526, :tr-loppuetaisyys 460, :nimi nil, :materiaali materiaali, :tr-alkuetaisyys 80, :tr-numero 28406, :toimenpide 12}
-   {:kohdeosa-id nil, :tr-kaista 11, :leveys 2, :kokonaismassamaara 342, :tr-ajorata 0, :pinta_ala 980, :tr-loppuosa 23, :jarjestysnro 1, :tr-alkuosa 23, :massamenekki 348.9795918367347, :tr-loppuetaisyys 525, :nimi nil, :materiaali materiaali, :tr-alkuetaisyys 35, :tr-numero 28410, :toimenpide 12}
-   {:kohdeosa-id nil, :tr-kaista 11, :leveys 2, :kokonaismassamaara 324, :tr-ajorata 0, :pinta_ala 650, :tr-loppuosa 23, :jarjestysnro 1, :tr-alkuosa 23, :massamenekki 498.46153846153845, :tr-loppuetaisyys 850, :nimi nil, :materiaali materiaali, :tr-alkuetaisyys 525, :tr-numero 28410, :toimenpide 12}])
-
-;; VHAR-5750 Liian herkästi muun kohteen päällekkäisyys vaikka ei ollut
-(deftest muun-kohteen-paallekkaisyysvalidointi-ei-liian-herkka
-  (let [paallystyskohde-id (hae-yllapitokohde-aloittamaton)
-        urakka-id (hae-utajarven-paallystysurakan-id)
-        materiaali-id (ffirst (q "SELECT id FROM pot2_mk_urakan_massa WHERE urakka_id = " urakka-id))
-        sopimus-id (hae-utajarven-paallystysurakan-paasopimuksen-id)
-        paallystysilmoitus (-> pot2-testidata
-                               (assoc :paallystyskohde-id paallystyskohde-id)
-                               (assoc :paallystekerros (muun-kohteen-paallekkaisyys-testin-paallystekerros materiaali-id)))
-
-        [paallystysilmoitus-kannassa-ennen paallystysilmoitus-kannassa-jalkeen] (tallenna-pot2-testi-paallystysilmoitus
-                                                                                  urakka-id sopimus-id paallystyskohde-id paallystysilmoitus)]))
-
 (deftest ei-saa-paivittaa-jos-on-vaara-versio
   (let [paallystyskohde-vanha-pot-id (ffirst (q "SELECT id FROM yllapitokohde WHERE nimi = 'Ouluntie'"))]
     (is (not (nil? paallystyskohde-vanha-pot-id)))
@@ -1146,25 +1126,21 @@
            (clojure.set/project alustarivit-jalkeen [:pot2a_id :tr-numero])))
     (poista-paallystysilmoitus-paallystyskohtella paallystyskohde-id)))
 
+;; VHAR-5750 Liian herkästi muun kohteen päällekkäisyys vaikka ei ollut
 (deftest tallenna-pot2-jossa-on-alikohde-muulla-tiella-validointi-ottaa-tie-huomioon.
   (let [urakka-id (hae-utajarven-paallystysurakan-id)
         sopimus-id (hae-utajarven-paallystysurakan-paasopimuksen-id)
         paallystyskohde-id (hae-yllapitokohde-tarkea-kohde-pot2)
-        muu-tr-numero 7777
+        muu-tr-numero 837
         paallystysilmoitus (-> pot2-alustatestien-ilmoitus
-                               (assoc-in [:paallystekerros 2]
-                                         {:kohdeosa-id 13, :tr-kaista 11, :leveys 3, :kokonaismassamaara 5000,
-                                          :tr-ajorata 1, :pinta_ala 15000, :tr-loppuosa 10, :jarjestysnro 1,
-                                          :tr-alkuosa 10, :massamenekki 333, :tr-loppuetaisyys 1500, :nimi "Muu tie 1",
-                                          :materiaali 2, :tr-alkuetaisyys 1066, :piennar false,
-                                          :tr-numero muu-tr-numero, :toimenpide 23, :pot2p_id 3})
-                               (assoc-in [:paallystekerros 3]
-                                         {:kohdeosa-id 13, :tr-kaista 11, :leveys 3, :kokonaismassamaara 5000,
-                                          :tr-ajorata 1, :pinta_ala 15000, :tr-loppuosa 10, :jarjestysnro 1,
-                                          :tr-alkuosa 10, :massamenekki 333, :tr-loppuetaisyys 1500, :nimi "Muu tie 2",
-                                          :materiaali 2, :tr-alkuetaisyys 1066, :piennar false,
-                                          :tr-numero muu-tr-numero, :toimenpide 23, :pot2p_id 4})
-                               (assoc :alusta pot2-alusta-esimerkki))
+                               (dissoc :alusta)
+                               (dissoc :paallystekerros)
+                               (assoc :paallystekerros
+                                      [{:kohdeosa-id 13, :tr-kaista 11, :leveys 3, :kokonaismassamaara 5000,
+                                        :tr-ajorata 0, :pinta_ala 15000, :tr-loppuosa 2, :jarjestysnro 1,
+                                        :tr-alkuosa 2, :massamenekki 333, :tr-loppuetaisyys 100, :nimi "Muu tie 1",
+                                        :materiaali 2, :tr-alkuetaisyys 50, :piennar false,
+                                        :tr-numero muu-tr-numero, :toimenpide 23, :pot2p_id 3}]))
         ;; Tehdään tallennus joka lisää kaksi alustariviä
         [_ paallystysilmoitus-kannassa-jalkeen] (tallenna-pot2-testi-paallystysilmoitus
                                                   urakka-id sopimus-id paallystyskohde-id paallystysilmoitus)
