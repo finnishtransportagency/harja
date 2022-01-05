e2457468523b73f56a5faf531ac597fa8d1cff90
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2022-01-05 15:23:55 +0200

    Robustoi lukkotestejä

--- test/clj/harja/tyokalut/lukot_test.clj
+++ test/clj/harja/tyokalut/lukot_test.clj
@@ -7,10 +7,12 @@
 
 (def +testilukko+ "TESTI")
 (def +testilukko2+ "testilukko2")
+(def +testilukko3+ "testilukko3")
+(def +testilukko4+ "testilukko4")
 
-(defn tarkista-lukon-avaus []
-  (let [lukko (first (q (str "SELECT tunniste, lukko, lukittu FROM lukko WHERE tunniste = '" +testilukko+ "'")))]
-    (is (= +testilukko+ (first lukko)))
+(defn tarkista-lukon-avaus [tunniste]
+  (let [lukko (first (q (str "SELECT tunniste, lukko, lukittu FROM lukko WHERE tunniste = '" tunniste "'")))]
+    (is (= tunniste (first lukko)))
     (is (some? (nth lukko 2)) "Lukkoa ei avata, vaan jätetään voimaan aikaleimalla")))
 
 (deftest tarkista-lukon-kanssa-ajaminen
@@ -19,7 +21,7 @@
         toiminto-fn (fn [] (reset! muuttuja true))]
     (is (lukot/yrita-ajaa-lukon-kanssa db +testilukko+ toiminto-fn 1) "Lukko saatiin asetettua oikein")
     (is @muuttuja "Toiminto ajettiin oikein")
-    (tarkista-lukon-avaus)))
+    (tarkista-lukon-avaus +testilukko+)))
 
 (deftest tarkista-lukittu-ajo
   (let [db (luo-testitietokanta)
@@ -34,20 +36,21 @@
   (let [db (luo-testitietokanta)
         muuttuja (atom nil)
         toiminto-fn (fn [] (reset! muuttuja true))]
-    (is (qk/aseta-lukko? db +testilukko+ nil) "Lukon asettaminen onnistui")
+    (is (qk/aseta-lukko? db +testilukko3+ nil) "Lukon asettaminen onnistui")
 
     (Thread/sleep 1000)
 
-    (is (lukot/aja-lukon-kanssa db +testilukko+ toiminto-fn 1) "Lukkoa saatiin asetettua")
+    (is (lukot/aja-lukon-kanssa db +testilukko3+ toiminto-fn 1) "Lukkoa saatiin asetettua")
     (is @muuttuja "Toiminto ajettiin oikein")
-    (tarkista-lukon-avaus)))
+    (tarkista-lukon-avaus +testilukko3+)
+    (qk/avaa-lukko? db +testilukko3+)))
 
 
 (deftest tarkista-poikkeusten-kasittely
   (let [db (luo-testitietokanta)
         toiminto-fn (fn [] (throw (Exception. "Poikkeus")))]
-    (is (thrown? Exception (lukot/aja-lukon-kanssa db +testilukko+ toiminto-fn 1)) "Poikkeus heitettiin ulos")
-    (tarkista-lukon-avaus)))
+    (is (thrown? Exception (lukot/aja-lukon-kanssa db +testilukko4+ toiminto-fn 1)) "Poikkeus heitettiin ulos")
+    (tarkista-lukon-avaus +testilukko4+)))
 
 (deftest tarkista-lukon-odotus
   (let [db (luo-testitietokanta)
@@ -63,7 +66,7 @@
 
 (deftest tarkista-lukon-odotus-poikkeuksen-kanssa
   (let [db (luo-testitietokanta)]
-    (is (thrown? RuntimeException (lukot/aja-lukon-kanssa db "odotus-testi" (fn [] (throw (RuntimeException. ""))) nil 1)))
+    (is (thrown? RuntimeException (lukot/aja-lukon-kanssa db "odotus-testi" (fn [] (throw (RuntimeException. ""))) 1 1)))
     (is (nil? (first (first (q "select lukko from lukko where tunniste = 'sampo-sisaanluku'")))))))
 
 
