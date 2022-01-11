6ccf4e3c0ecebe4652eab721bb5e5c5bf9136bb6
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-01-11 11:45:00 +0200

    Testaa varustelista backendia

--- test/clj/harja/palvelin/palvelut/varuste_velho_test.clj
+++ test/clj/harja/palvelin/palvelut/varuste_velho_test.clj
@@ -39,28 +39,9 @@
                       (component/system-map
                         :db (tietokanta/luo-tietokanta testitietokanta)
                         :http-palvelin (testi-http-palvelin)
-                        :fim (component/using
-                               (fim/->FIM +testi-fim+)
-                               [:db :integraatioloki])
-                        :integraatioloki (component/using
-                                           (integraatioloki/->Integraatioloki nil)
-                                           [:db])
-                        :sonja (feikki-jms "sonja")
-                        :sonja-sahkoposti (component/using
-                                            (sahkoposti/luo-sahkoposti "foo@example.com"
-                                                                       {:sahkoposti-sisaan-jono "email-to-harja"
-                                                                        :sahkoposti-ulos-jono "harja-to-email"
-                                                                        :sahkoposti-ulos-kuittausjono "harja-to-email-ack"})
-                                            [:sonja :db :integraatioloki])
-                        :paallystys (component/using
-                                      (paallystys/->Paallystys)
-                                      [:http-palvelin :db :fim :sonja-sahkoposti])
-
-
-                        :pot2 (component/using
-                                (pot2/->POT2)
-                                [:http-palvelin :db :fim :sonja-sahkoposti])))))
-
+                        :varuste-velho (component/using
+                                         (varuste-velho/->VarusteVelho)
+                                         [:http-palvelin :db])))))
   (testit)
   (alter-var-root #'jarjestelma component/stop))
 
@@ -80,9 +61,21 @@
                                   "1.2.246.578.4.3.12.512.310173997"
                                   "1.2.246.578.4.3.12.512.310173998"])
         saatu-oid-lista (->> (varuste-velho/hae-urakan-varustetoteuma-ulkoiset (:db jarjestelma) +kayttaja-jvh+ {:urakka-id urakka-id})
-                            :toteumat
-                            (map :ulkoinen-oid)
-                            vec
-                            sort
-                            )]
+                             :toteumat
+                             (map :ulkoinen-oid)
+                             vec
+                             sort)]
     (is (= odotettu-oid-lista saatu-oid-lista))))
+
+(deftest palvelu-on-olemassa-ja-vaatii-parametrin-urakka-id
+  (let [urakka-id 35
+        saatu (kutsu-palvelua (:http-palvelin jarjestelma)
+                              :hae-urakan-varustetoteuma-ulkoiset
+                              +kayttaja-jvh+
+                              {:urakka-id urakka-id})]
+    (is (not-empty (:toteumat saatu)))
+    (is (thrown-with-msg? IllegalArgumentException #"urakka-id on pakollinen"
+                          (kutsu-palvelua (:http-palvelin jarjestelma)
+                                          :hae-urakan-varustetoteuma-ulkoiset
+                                          +kayttaja-jvh+
+                                          {:urakka-id nil})))))
