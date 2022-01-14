843309166a6a7648e133656c7878f3db5de49729
Jere Luomajoki <jere.luomajoki@solita.fi>
2022-01-14 10:47:57 +0200

    Lisää specql-määrittely taululle

--- src/clj/harja/kyselyt/tehtavamaarat.clj
+++ src/clj/harja/kyselyt/tehtavamaarat.clj
@@ -1,7 +1,22 @@
 (ns harja.kyselyt.tehtavamaarat
-  (:require [jeesql.core :refer [defqueries]]))
+  (:require [jeesql.core :refer [defqueries]]
+            [specql.core :refer [fetch upsert!]]
+            [harja.kyselyt.specql-db :refer [define-tables]]
+            [harja.domain.urakka :as urakka]
+            [harja.domain.toimenpidekoodi :as toimenpidekoodi]))
 
 (defqueries "harja/kyselyt/tehtavamaarat.sql"
   {:positional? true})
 
+(define-tables
+  ["suunniteltu_tehtavamaara" ::suunniteltu-tehtavamaara
+   {"id" ::suunniteltu-tehtavamara-id
+    "urakka" ::urakka/id
+    "tehtava" ::toimenpidekoodi/id
+    "maara" ::maara
+    "muokattu" ::muokattu
+    }]
+  )
 
+(defn tallenna-suunnitellut-tehtavamaarat [db st]
+  (upsert! db ::suunniteltu-tehtavamaara st))
