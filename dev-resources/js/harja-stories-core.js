b61e251f44c62066b6adb8dd4e0495228115597d
Petar Teodosin Jr <petar.teodosin@solita.fi>
2022-01-19 18:11:57 +0200

    Korjaa profile ja main

---
+++ dev-resources/js/harja-stories-core.js
@@ -0,0 +1,14 @@
+var CLOSURE_UNCOMPILED_DEFINES = {"harja.ui.taulukko_debug.TAULUKKO_DEBUG":false,"harja.ui.grid_debug.GRID_DEBUG":true,"figwheel.core.load_warninged_code":true,"figwheel.repl.connect_url":"ws:\/\/localhost:3449\/figwheel-connect?fwprocess=b4edbf&fwbuild=figwheel_conf%2Fdev-storybook"};
+var CLOSURE_NO_DEPS = true;
+if(typeof goog == "undefined") document.write('<script src="js/out/goog/base.js"></script>');
+document.write('<script src="js/out/goog/deps.js"></script>');
+document.write('<script src="js/out/cljs_deps.js"></script>');
+document.write('<script>if (typeof goog == "undefined") console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?");</script>');
+document.write('<script>goog.require("figwheel.core");</script>');
+document.write('<script>goog.require("figwheel.main");</script>');
+document.write('<script>goog.require("figwheel.repl.preload");</script>');
+document.write('<script>goog.require("devtools.preload");</script>');
+document.write('<script>goog.require("figwheel.main.system_exit");</script>');
+document.write('<script>goog.require("figwheel.main.css_reload");</script>');
+document.write('<script>goog.require("process.env");</script>');
+document.write('<script>goog.require("harja.stories.core.kaka");</script>');
--- test/cljs/harja/stories/core.cljs
+++ test/cljs/harja/stories/core.cljs
@@ -1,28 +1,10 @@
 (ns harja.stories.core
-  (:require [reagent.core :as reagent :refer [atom]]
-            [harja-laadunseuranta.tiedot.paikannus :as paikannus]
-            [harja-laadunseuranta.main :as main]
-            [harja-laadunseuranta.tiedot.sovellus :as sovellus]
-            [harja-laadunseuranta.tiedot.comms :as comms]
-            [harja-laadunseuranta.tiedot.asetukset.asetukset :as asetukset]
-            [harja-laadunseuranta.tiedot.tr-haku :as tr-haku]
-            [harja-laadunseuranta.tiedot.ilmoitukset :as ilmoitukset]
-            [harja-laadunseuranta.utils :as utils]
-            [harja-laadunseuranta.tiedot.puhe :as puhe]
-            [harja-laadunseuranta.tiedot.tarkastusajon-luonti :as tarkastusajon-luonti]
-            [cljs.core.async :as async :refer [<!]]
-            [harja-laadunseuranta.tiedot.reitintallennus :as reitintallennus]
-            [clojure.string :as str]
-            [harja-laadunseuranta.ui.yleiset.dom :as dom]
-            [harja-laadunseuranta.asiakas.tapahtumat :as tapahtumat]
-            [cljs-time.local :as l])
-  (:require-macros [reagent.ratom :refer [run!]]
-                   [cljs.core.async.macros :refer [go]]
-                   [harja-laadunseuranta.macros :refer [after-delay]]))
-
-(defn render []
-  (reagent/render-component [main/main] (.getElementById js/document "app")))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 (defn main []
   [:div])
 
+(defn render []
+  (reagent/render-component [main] (.getElementById js/document "app")))
+
+(render)
