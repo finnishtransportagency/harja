ed3a46fefcdb54ac8c93c1cd64b10f8666824db0
Markus Vähäkangas <markus.vahakangas@solita.fi>
2022-02-10 09:51:33 +0200

    Muuta vähän nimeämisiä

--- src/clj/harja/palvelin/integraatiot/vayla_rest/sanomat/sahkoposti_lahetys.clj
+++ src/clj/harja/palvelin/integraatiot/vayla_rest/sanomat/sahkoposti_lahetys.clj
@@ -1,11 +1,11 @@
-(ns harja.palvelin.integraatiot.vayla-rest.sahkoposti.sanomat.sahkoposti-lahetys
+(ns harja.palvelin.integraatiot.vayla-rest.sanomat.sahkoposti-lahetys
   (:require
    [clojure.spec.alpha :as spec]
-   [clojure.string :as string])
+   [clojure.string :as str])
   (:import (java.util UUID)))
 
-(spec/def ::ei-tyhja-teksti (spec/and string? (complement string/blank?)))
-(spec/def ::sahkopostiosoite (spec/and string? #(string/includes % "@")))
+(spec/def ::ei-tyhja-teksti (spec/and string? (complement str/blank?)))
+(spec/def ::sahkopostiosoite (spec/and string? #(str/includes? % "@")))
 (spec/def ::base64-merkkijono #"[^-A-Za-z0-9+/=]|=[^=]|={3,}$")
 
 (spec/def ::viestiId ::ei-tyhja-teksti)
