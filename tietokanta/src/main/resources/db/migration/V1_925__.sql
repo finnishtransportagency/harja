d94866b985994ce55d096b4546ec4d05a18de14b
Jukka Länsineva <jukka.lansineva@solita.fi>
2022-02-28 09:05:35 +0200

    migraation päivitys vuosittaiseen sopimusten syöttöön

--- tietokanta/src/main/resources/db/migration/V1_925__.sql
+++ tietokanta/src/main/resources/db/migration/V1_925__.sql
@@ -1,1 +1,4 @@
 ALTER TABLE sopimuksen_tehtavamaarat_tallennettu ADD UNIQUE(urakka);
+alter table sopimus_tehtavamaara add column hoitovuosi integer;
+alter table sopimus_tehtavamaara drop constraint sopimus_tehtavamaara_urakka_tehtava_key;
+alter table sopimus_tehtavamaara add unique (urakka, tehtava, hoitovuosi);
