8f57aa3d6dd26c57dab114b4257ff33fa9eccb64
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-03-03 10:50:22 +0200

    Lisää infolaatikko vanhojen varusteiden poistumisesta
    WIP

--- src/cljs/harja/views/urakka/toteumat/varusteet.cljs
+++ src/cljs/harja/views/urakka/toteumat/varusteet.cljs
@@ -424,6 +424,11 @@
 
       [:span
        [debug/debug app]
+       [:div
+        [:span "Varusteet syötetään jatkossa urakoitsijoiden omien järjestelmien kautta.
+         Siirtymävaiheessa varustekirjaukset voidaan kirjata urakoitsijan järjestelmään tai Velhon excel-lomakkeella.
+         Vanhat varustekirjaukset löytyvät Harjasta edelleen, mutta varusteiden yksityiskohtaisia tietoja ei voida tarkastella.
+         Velhon varustetiedot tulevat Harjaan näkyviin kesällä 2022."]]
        (when virhe
          (yleiset/virheviesti-sailio virhe (fn [_] (e! (v/->VirheKasitelty)))))
        [kartta/kartan-paikka]
