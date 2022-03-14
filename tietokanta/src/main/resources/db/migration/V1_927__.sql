bc8a1c4825ffddad63d056c81ce415de41ffe2bd
Jukka Länsineva <jukka.lansineva@solita.fi>
2022-03-14 13:07:58 +0200

    vaaditaan hoitovuosi

--- tietokanta/src/main/resources/db/migration/V1_927__.sql
+++ tietokanta/src/main/resources/db/migration/V1_927__.sql
@@ -1,4 +1,4 @@
 ALTER TABLE sopimuksen_tehtavamaarat_tallennettu ADD UNIQUE(urakka);
-alter table sopimus_tehtavamaara add column hoitovuosi integer;
+alter table sopimus_tehtavamaara add column hoitovuosi integer not null;
 alter table sopimus_tehtavamaara drop constraint sopimus_tehtavamaara_urakka_tehtava_key;
 alter table sopimus_tehtavamaara add unique (urakka, tehtava, hoitovuosi);
