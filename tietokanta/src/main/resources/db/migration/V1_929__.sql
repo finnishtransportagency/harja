bb86ccffec929c2c7a852ed865df525b3fe52667
Petar Teodosin Jr <petar.teodosin@solita.fi>
2022-03-15 16:19:08 +0200

    Lisää sarakkeet

--- tietokanta/src/main/resources/db/migration/V1_929__.sql
+++ tietokanta/src/main/resources/db/migration/V1_929__.sql
@@ -1,1 +1,4 @@
 -- Lisätään uudet sarakkeet harja.public.varustetoteuma_ulkoiset_kohdevirhe tauluun
+
+ALTER TABLE varustetoteuma_ulkoiset_kohdevirhe ADD COLUMN aikaleima TIMESTAMP;
+ALTER TABLE varustetoteuma_ulkoiset_kohdevirhe ADD COLUMN vastaus VARCHAR(8192);
