71b990b4ad44ed10be094eb01cc75053af5c2668
Jere Luomajoki <jere.luomajoki@solita.fi>
2022-03-16 16:04:27 +0200

    poista turha debuglogi

--- src/clj/harja/palvelin/palvelut/kulut.clj
+++ src/clj/harja/palvelin/palvelut/kulut.clj
@@ -283,7 +283,6 @@
                                         :liite-id (:liite-id liite)
                                         :kayttaja (:id user)})))
 
-    (println "jere testaa::" puuttuvat-kohdistukset)
     ;; Kannassa on kohdistuksia, joita ei lähetetty kulun päivityksen yhteydessä. Poistetaan ne.
     (doseq [puuttuva-kohdistus puuttuvat-kohdistukset]
         (poista-kulun-kohdistus db user
