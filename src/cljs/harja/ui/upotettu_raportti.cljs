06c2cbd6ecf4257e4730fc7105efea675b716576
Jukka Länsineva <jukka.lansineva@solita.fi>
2022-03-28 12:07:59 +0300

    multimetodi, jotta voidaan tehdä yhdellä kertaa useammalle parametrille export-nappi

--- src/cljs/harja/ui/upotettu_raportti.cljs
+++ src/cljs/harja/ui/upotettu_raportti.cljs
@@ -9,21 +9,38 @@
             [harja.ui.ikonit :as ikonit])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn raportin-vientimuodot [parametrit]
-  [:span
-   ^{:key "raporttixls"}
-   [:form {:style {:float "right"} :target "_blank" :method "POST"
-           :action (k/excel-url :raportointi)}
-    [:input {:type "hidden" :name "parametrit"
-             :value (t/clj->transit parametrit)}]
-    [:button.nappi-ensisijainen {:type "submit"}
-     (ikonit/print)
-     " Tallenna Excel"]]
-   ^{:key "raporttipdf"}
-   [:form {:style {:float "right"} :target "_blank" :method "POST"
-           :action (k/pdf-url :raportointi)}
-    [:input {:type "hidden" :name "parametrit"
-             :value (t/clj->transit parametrit)}]
-    [:button.nappi-ensisijainen {:type "submit"}
-     (ikonit/print)
-     " Tallenna PDF"]]])
+(defn raportin-vientimuodot 
+  "Parametrit hash-map tai useita hash-mappeja. Yhdellä hash-mapilla tehdään PDF/Excel-napit samoilla
+  parametreilla, usealla hash-mapilla napeilla on omat parametrit ja lisänä avaimet 
+  :kasittelija :pdf/:excel ja :otsikko, joka on napin teksti"
+  ([parametrit & loput-parametrit]
+   [:span  
+    (for [{:keys [kasittelija otsikko]} (concat [parametrit] loput-parametrit)]
+      ^{:key (str "raportti-" otsikko)}
+      [:form {:style {:float "right"} :target "_blank" :method "POST"
+              :action ((if (= kasittelija :excel) 
+                         k/excel-url 
+                         k/pdf-url) :raportointi)}
+       [:input {:type "hidden" :name "parametrit"
+                :value (t/clj->transit (dissoc parametrit :otsikko :kasittelija))}]
+       [:button.nappi-ensisijainen {:type "submit"}
+        (ikonit/print)
+        (str " " otsikko)]])])
+  ([parametrit]
+   [:span
+    ^{:key "raporttixls"}
+    [:form {:style {:float "right"} :target "_blank" :method "POST"
+            :action (k/excel-url :raportointi)}
+     [:input {:type "hidden" :name "parametrit"
+              :value (t/clj->transit parametrit)}]
+     [:button.nappi-ensisijainen {:type "submit"}
+      (ikonit/print)
+      " Tallenna Excel"]]
+    ^{:key "raporttipdf"}
+    [:form {:style {:float "right"} :target "_blank" :method "POST"
+            :action (k/pdf-url :raportointi)}
+     [:input {:type "hidden" :name "parametrit"
+              :value (t/clj->transit parametrit)}]
+     [:button.nappi-ensisijainen {:type "submit"}
+      (ikonit/print)
+      " Tallenna PDF"]]]))
