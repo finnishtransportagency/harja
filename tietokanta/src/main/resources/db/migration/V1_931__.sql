41598d035c620f1940164ced1d215bb3c3048292
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-03-28 13:32:14 +0300

    Muuttaa sarakkeita TEXT tyyppisiksi

--- tietokanta/src/main/resources/db/migration/V1_931__.sql
+++ tietokanta/src/main/resources/db/migration/V1_931__.sql
@@ -1,3 +1,3 @@
 ALTER TABLE varustetoteuma_ulkoiset ALTER COLUMN ulkoinen_oid TYPE TEXT;
 ALTER TABLE varustetoteuma_ulkoiset ALTER COLUMN lisatieto TYPE TEXT;
-ALTER TABLE varustetoteuma_ulkoiset_virhe ALTER COLUMN ulkoinen_oid TYPE TEXT;
+ALTER TABLE varustetoteuma_ulkoiset_kohdevirhe ALTER COLUMN ulkoinen_oid TYPE TEXT;
