ae9711d61780e0d0830dcbda02624e3d2160761d
Markus Vähäkangas <markus.vahakangas@solita.fi>
2022-04-20 07:34:06 +0300

    Varmista yksikkötestillä, että poistetut ovat mukana haussa

--- test/clj/harja/palvelin/integraatiot/api/analytiikka_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/analytiikka_test.clj
@@ -78,3 +78,22 @@
         vastaus (future (api-tyokalut/get-kutsu [(str "/api/analytiikka/toteumat/" alkuaika "/" loppuaika)] kayttaja-analytiikka portti))]
     (is (= 400 (:status @vastaus)))
     (is (str/includes? (:body @vastaus) "Alkuaika väärässä muodossa"))))
+
+(deftest hae-toteumat-test-poistettu-onnistuu
+  (let [; Luo väliaikainen käyttäjä, jolla on oikeudet analytiikkarajapintaan
+        _ (u (str "INSERT INTO kayttaja (etunimi, sukunimi, kayttajanimi, organisaatio, \"analytiikka-oikeus\") VALUES
+          ('etunimi','sukunimi', 'analytiikka-testeri', (SELECT id FROM organisaatio WHERE nimi = 'Liikennevirasto'), true)"))
+        ;; Aseta tiukka hakuväli, josta löytyy vain vähän toteumia
+        alkuaika "2004-01-01T00:00:00+03"
+        loppuaika "2004-12-31T21:00:00+03"
+        vastaus (future (api-tyokalut/get-kutsu [(str "/api/analytiikka/toteumat/" alkuaika "/" loppuaika)] kayttaja-analytiikka portti))
+
+        ;; Merkitään kaikki saman aikavälin toteumat poistetuiksi ja tehdään uusi haku
+        _ (u (str "UPDATE toteuma SET poistettu = true WHERE alkanut >= '2004-01-01T00:00:00+03' AND alkanut <= '2004-12-31T21:00:00+03'"))
+        poistetut (future (api-tyokalut/get-kutsu [(str "/api/analytiikka/toteumat/" alkuaika "/" loppuaika)] kayttaja-analytiikka portti))
+        _ (u (str "UPDATE toteuma SET poistettu = false WHERE alkanut >= '2004-01-01T00:00:00+03' AND alkanut <= '2004-12-31T21:00:00+03'"))]
+    (is (= 200 (:status @vastaus)))
+    (is (= 200 (:status @poistetut)))
+    (sisaltaa-perustiedot (:body @vastaus))
+    (sisaltaa-perustiedot (:body @poistetut))
+    (is (= @vastaus @poistetut))))
