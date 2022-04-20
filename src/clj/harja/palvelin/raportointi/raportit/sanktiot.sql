e547a22dbd425c76fb24553c9936e5d070636cd4
Jukka Länsineva <jukka.lansineva@solita.fi>
2022-04-20 15:04:44 +0300

    sanktiohaut nappaavat urakkaan liittyvät sanktiot urakan päättymisen jälkeenkin

--- src/clj/harja/palvelin/raportointi/raportit/sanktiot.sql
+++ src/clj/harja/palvelin/raportointi/raportit/sanktiot.sql
@@ -24,7 +24,7 @@ FROM sanktio s
   LEFT JOIN toimenpideinstanssi tpi ON s.toimenpideinstanssi = tpi.id
   JOIN sanktiotyyppi st ON s.tyyppi = st.id
   LEFT JOIN laatupoikkeama lp ON s.laatupoikkeama = lp.id
-  JOIN urakka u ON (tpi.urakka = u.id OR lp.urakka = u.id)
+  JOIN urakka u ON (tpi.urakka = u.id OR lp.urakka = u.id) AND u.alkupvm < :loppu AND u.loppupvm > :alku
   JOIN organisaatio o ON u.hallintayksikko = o.id
 WHERE ((:urakka::INTEGER IS NULL AND u.urakkanro IS NOT NULL) OR u.id = :urakka) -- varmistaa ettei testiurakka tule mukaan alueraportteihin
       AND (:urakka::INTEGER IS NOT NULL OR (
@@ -37,7 +37,14 @@ WHERE ((:urakka::INTEGER IS NULL AND u.urakkanro IS NOT NULL) OR u.id = :urakka)
                                                                                         WHERE hallintayksikko =
                                                                                               :hallintayksikko) AND u.urakkanro IS NOT NULL))
       AND s.poistettu IS NOT TRUE
-      AND s.perintapvm BETWEEN :alku AND :loppu
+      -- jos hakurange sisältää urakan viimeisen kuukauden, mahdolliset urakan päättymisen jälkeen tulleet sanktiot sisällytetään siihen  
+      AND (s.perintapvm BETWEEN :alku AND :loppu OR
+          (CASE 
+                EXTRACT(YEAR FROM DATE :loppu) = EXTRACT(YEAR FROM u.loppupvm) 
+                AND EXTRACT(MONTH FROM DATE :loppu) = EXTRACT(MONTH FROM u.loppupvm)
+           WHEN TRUE THEN s.perintapvm > u.loppupvm 
+           ELSE FALSE
+           END))
       -- Ei kuulu poistettuun ylläpitokohteeseen
       AND (lp.yllapitokohde IS NULL
           OR
@@ -69,7 +76,7 @@ FROM sanktio s
   JOIN sanktiotyyppi st ON s.tyyppi = st.id
   LEFT JOIN laatupoikkeama lp ON s.laatupoikkeama = lp.id AND lp.poistettu IS NOT TRUE
   LEFT JOIN yllapitokohde ypk ON lp.yllapitokohde = ypk.id AND ypk.poistettu IS NOT TRUE
-  JOIN urakka u ON (tpi.urakka = u.id OR lp.urakka = u.id)
+  JOIN urakka u ON (tpi.urakka = u.id OR lp.urakka = u.id) AND u.alkupvm < :loppu AND u.loppupvm > :alku
   JOIN organisaatio o ON u.hallintayksikko = o.id
 WHERE ((:urakka::INTEGER IS NULL AND u.urakkanro IS NOT NULL) OR u.id = :urakka) -- varmistaa ettei testiurakka tule mukaan alueraportteihin
       AND (:urakka::INTEGER IS NOT NULL OR
@@ -80,7 +87,14 @@ WHERE ((:urakka::INTEGER IS NULL AND u.urakkanro IS NOT NULL) OR u.id = :urakka)
                                                                                         WHERE hallintayksikko =
                                                                                               :hallintayksikko) AND u.urakkanro IS NOT NULL))
       AND s.poistettu IS NOT TRUE
-      AND s.perintapvm BETWEEN :alku AND :loppu
+      -- jos hakurange sisältää urakan viimeisen kuukauden, mahdolliset urakan päättymisen jälkeen tulleet sanktiot sisällytetään siihen
+      AND (s.perintapvm BETWEEN :alku AND :loppu OR
+          (CASE 
+                EXTRACT(YEAR FROM DATE :loppu) = EXTRACT(YEAR FROM u.loppupvm) 
+                AND EXTRACT(MONTH FROM DATE :loppu) = EXTRACT(MONTH FROM u.loppupvm)
+           WHEN TRUE THEN s.perintapvm > u.loppupvm 
+           ELSE FALSE
+           END))
     -- Ei kuulu poistettuun ylläpitokohteeseen
       AND (lp.yllapitokohde IS NULL
           OR
