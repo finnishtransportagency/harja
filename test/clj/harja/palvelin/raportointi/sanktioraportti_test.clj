62d534c6c5c14bcee1cf07f041a29a0d2b734d7b
Jukka Länsineva <jukka.lansineva@solita.fi>
2022-04-22 10:17:25 +0300

    testit tsekkaamaan urakanjälkeisen sanktion haku mukaan urakan viimeiseen kuukauteen

--- test/clj/harja/palvelin/raportointi/sanktioraportti_test.clj
+++ test/clj/harja/palvelin/raportointi/sanktioraportti_test.clj
@@ -128,3 +128,88 @@
                                           {:otsikko "12 Pohjois-Pohjanmaa"}
                                           {:otsikko "14 Lappi"}
                                           {:otsikko "Yh\u00ADteen\u00ADsä"}))))
+
+(deftest raportin-suoritus-urakan-jalkeen-tulleilla-sanktioilla-toimii
+  (let [urakalla-sanktiot-tulee-mukaan-viimeisella-hoitokaudella
+        (kutsu-palvelua (:http-palvelin jarjestelma)
+          :suorita-raportti
+          +kayttaja-jvh+
+          {:nimi       :sanktioraportti
+           :konteksti  "urakka"
+           :urakka-id  (hae-oulun-alueurakan-2014-2019-id)
+           :parametrit {:alkupvm  (c/to-date (t/local-date 2018 10 1))
+                        :loppupvm (c/to-date (t/local-date 2019 9 30))}})
+        
+        urakalla-sanktiot-ei-tule-mukaan-jollain-toisella-kaudella
+        (kutsu-palvelua (:http-palvelin jarjestelma)
+          :suorita-raportti
+          +kayttaja-jvh+
+          {:nimi       :sanktioraportti
+           :konteksti  "urakka"
+           :urakka-id  (hae-oulun-alueurakan-2014-2019-id)
+           :parametrit {:alkupvm  (c/to-date (t/local-date 2018 9 1))
+                        :loppupvm (c/to-date (t/local-date 2019 8 1))}})
+        
+        elylla-sanktiot-tulee-mukaan-jos-jossain-urakassa-viimeinen-hoitokausi
+        (kutsu-palvelua (:http-palvelin jarjestelma)
+          :suorita-raportti
+          +kayttaja-jvh+
+          {:nimi               :sanktioraportti
+           :konteksti          "hallintayksikko"
+           :hallintayksikko-id (hae-pohjois-pohjanmaan-hallintayksikon-id)
+           :parametrit         {:alkupvm      (c/to-date (t/local-date 2018 10 1))
+                                :loppupvm     (c/to-date (t/local-date 2019 9 30))
+                                :urakkatyyppi :hoito}})
+        
+        elylla-sanktiot-ei-tule-mukaan-jos-edellista-casea-seuraava-hoitokausi
+        (kutsu-palvelua (:http-palvelin jarjestelma)
+          :suorita-raportti
+          +kayttaja-jvh+
+          {:nimi               :sanktioraportti
+           :konteksti          "hallintayksikko"
+           :hallintayksikko-id (hae-pohjois-pohjanmaan-hallintayksikon-id)
+           :parametrit         {:alkupvm      (c/to-date (t/local-date 2019 10 1))
+                                :loppupvm     (c/to-date (t/local-date 2020 9 30))
+                                :urakkatyyppi :hoito}})]
+    (is (some? (some
+                 #(when
+                      (and
+                        (vector? %)
+                        (= (first %) "Ryhmä C, sakot yht."))
+                    (= (second %) 777M))
+                 (as-> urakalla-sanktiot-tulee-mukaan-viimeisella-hoitokaudella u
+                   (nth u 2)
+                   (nth u 3)))) "Urakan jälkeen tullut sanktio näkyy, kun viimeisen hoitokauden viimeinen kuukausi mukana")
+    (is (nil? (some
+                 #(when
+                      (and
+                        (vector? %)
+                        (= (first %) "Ryhmä C, sakot yht."))
+                    (not (= (second %) 0)))
+                 (as-> urakalla-sanktiot-ei-tule-mukaan-jollain-toisella-kaudella u
+                   (nth u 2)
+                   (nth u 3)))) "Urakan jälkeen tullut sanktio ei näy, jos viimeinen kuukausi ei mukana")
+    (is (some?
+          (some
+                #(when
+                     (and
+                       (vector? %)
+                       (= (first %) "Ryhmä C, sakot yht."))
+                   (= 777M (some
+                             (fn [arvo] 
+                               (when-not (zero? arvo)
+                                 arvo))
+                             (filter number? %))))
+                (as-> elylla-sanktiot-tulee-mukaan-jos-jossain-urakassa-viimeinen-hoitokausi u
+                  (nth u 2)
+                  (nth u 3)))) "Elyllä haettassa jos haussa mukana jonkun urakan viimeinen kuukausi, sen jälkeen tulleet sanktiot lasketaan mukaan")
+    (is (nil? (some
+                 #(when
+                      (and
+                        (vector? %)
+                        (= (first %) "Ryhmä C, sakot yht."))
+                    (not (every? zero?
+                           (filter number? %))))
+                 (as-> elylla-sanktiot-ei-tule-mukaan-jos-edellista-casea-seuraava-hoitokausi u
+                   (nth u 2)
+                   (nth u 3)))) "Edellisen vastapari, ei viimeisiä kuukausia, ei sen jälkeen tulleita sanktioita vaikka jonkin perintäpvm tällä jaksolla")))
