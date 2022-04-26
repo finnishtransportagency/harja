367a6427b9662d87284230ee132b698d13842990
Jukka Länsineva <jukka.lansineva@solita.fi>
2022-04-26 16:10:40 +0300

    aluetiedot eivät välitä hoitokaudesta ja niitä on vain yksi rivi per tehtävä per urakka

--- src/clj/harja/kyselyt/tehtavamaarat.sql
+++ src/clj/harja/kyselyt/tehtavamaarat.sql
@@ -208,7 +208,7 @@ FROM tehtavaryhma tr1
                                                                                    ) or tpk4.yksiloiva_tunniste is null)
        JOIN toimenpidekoodi tpk3 ON tpk4.emo = tpk3.id
        LEFT OUTER JOIN urakka_tehtavamaara ut
-                       ON tpk4.id = ut.tehtava AND ut.urakka = :urakka AND ut."hoitokauden-alkuvuosi" in (:hoitokausi)
+                       ON tpk4.id = ut.tehtava AND ut.urakka = :urakka AND (ut."hoitokauden-alkuvuosi" in (:hoitokausi) OR tpk4.aluetieto IS TRUE) 
        LEFT OUTER JOIN urakka u ON ut.urakka = u.id
        LEFT JOIN sopimuksen_tehtavamaarat_tallennettu sp on sp.urakka = :urakka
 WHERE tr1.emo is null
