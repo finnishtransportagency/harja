a85a967ece04b196109248a27850a6455b6162c1
Jukka Länsineva <jukka.lansineva@solita.fi>
2022-05-05 15:38:54 +0300

    tekstimuotoisten kenttien tasaus oikealle

--- src/cljs/harja/views/urakka/suunnittelu/tehtavat.cljs
+++ src/cljs/harja/views/urakka/suunnittelu/tehtavat.cljs
@@ -198,10 +198,10 @@
              "70%")}
           ;; ennen urakkaa -moodi         
           {:otsikko "Tarjouksen määrä" :nimi :sopimuksen-aluetieto-maara :tyyppi :numero :leveys "180px"
-           :muokattava? (constantly (if sopimukset-syotetty? false true)) :veda-oikealle? true}
+           :muokattava? (constantly (if sopimukset-syotetty? false true)) :tasaa :oikea :veda-oikealle? true}
           ;; urakan ajan suunnittelu -moodi         
           (when sopimukset-syotetty? 
-            {:otsikko "Muuttunut määrä" :nimi :muuttunut-aluetieto-maara :tyyppi :numero :muokattava? kun-yksikko :leveys "180px"  :veda-oikealle? true})
+            {:otsikko "Muuttunut määrä" :nimi :muuttunut-aluetieto-maara :tyyppi :numero :muokattava? kun-yksikko :leveys "180px" :tasaa :oikea :veda-oikealle? true})
           {:otsikko "Yksikkö" :nimi :yksikko :tyyppi :string :muokattava? (constantly false) :leveys "140px"}]
          aluetiedot-tila]])
      [:div.tm-otsikko "Määrät"]
@@ -236,16 +236,16 @@
        (when (not sopimukset-syotetty?)
          {:otsikko "Tarjouksen määrä vuodessa" :nimi :sopimuksen-tehtavamaara :tyyppi :numero :leveys "180px" 
           :muokattava? (comp kun-yksikko kun-kaikki-samat) :sarake-disabloitu-arvo-fn sarake-disabloitu-arvo
-           :veda-oikealle? true})
+           :veda-oikealle? true :tasaa :oikea})
        ;; urakan ajan suunnittelu -moodi
        (when sopimukset-syotetty? 
          {:otsikko "Koko urakka-ajan määrä tarjouksessa" :nimi :sopimuksen-tehtavamaarat-yhteensa 
-          :tyyppi :numero :muokattava? (constantly false) :leveys "160px"  :veda-oikealle? true})
+          :tyyppi :numero :muokattava? (constantly false) :leveys "160px" :tasaa :oikea :veda-oikealle? true})
        (when sopimukset-syotetty? 
          {:otsikko "Koko urakka-ajan määrää jäljellä" :nimi :sovittuja-jaljella :tyyppi :string 
-          :muokattava? (constantly false) :leveys "160px"  :veda-oikealle? true})
+          :muokattava? (constantly false) :leveys "160px" :tasaa :oikea :veda-oikealle? true})
        (when sopimukset-syotetty? 
-         {:otsikko "Hoitovuoden suunniteltu määrä" :nimi :maara :tyyppi :numero :muokattava? kun-yksikko :leveys "180px" :veda-oikealle? true})
+         {:otsikko "Hoitovuoden suunniteltu määrä" :nimi :maara :tyyppi :numero :tasaa :oikea :muokattava? kun-yksikko :leveys "180px" :veda-oikealle? true})
        {:otsikko "Yksikkö" :nimi :yksikko :tyyppi :string :muokattava? (constantly false) :leveys "140px"}]
       maarat-tila]]))
 
