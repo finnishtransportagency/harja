4f8a6aaf711849357d86d9743508e4ee259644fb
Jukka Länsineva <jukka.lansineva@solita.fi>
2022-05-05 16:12:40 +0300

    yksiköttömät veks hausta

--- src/clj/harja/kyselyt/tehtavamaarat.sql
+++ src/clj/harja/kyselyt/tehtavamaarat.sql
@@ -206,6 +206,7 @@ FROM tehtavaryhma tr1
                                                                                   '49b7388b-419c-47fa-9b1b-3797f1fab21d' --'Kolmansien osapuolten aiheuttamien vahinkojen korjaaminen (soratiet)'
                                                                                    -- ei ehkä samassa järjestyksessä, mutta nuo tehtävät
                                                                                    ) or tpk4.yksiloiva_tunniste is null)
+                                                                                   AND tpk4.suunnitteluyksikko IS NOT NULL
        JOIN toimenpidekoodi tpk3 ON tpk4.emo = tpk3.id
        LEFT OUTER JOIN urakka_tehtavamaara ut
                        ON tpk4.id = ut.tehtava AND ut.urakka = :urakka AND (ut."hoitokauden-alkuvuosi" in (:hoitokausi) OR tpk4.aluetieto IS TRUE) 
