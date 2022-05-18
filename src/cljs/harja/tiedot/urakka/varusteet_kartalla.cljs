734943e88f4de259c0d74540cc6b1835b397fce9
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-05-18 12:40:34 +0300

    Käytä uusia värejä ja nimiä kartalla

--- src/cljs/harja/tiedot/urakka/varusteet_kartalla.cljs
+++ src/cljs/harja/tiedot/urakka/varusteet_kartalla.cljs
@@ -1,16 +1,17 @@
 (ns harja.tiedot.urakka.varusteet-kartalla
   (:require [reagent.core :refer [atom] :as r]
             [harja.ui.kartta.ikonit :as kartta-ikonit]
-            [harja.ui.kartta.varit.puhtaat :as puhtaat])
+            [harja.ui.kartta.varit.puhtaat :as puhtaat]
+            [harja.ui.kartta.varit.alpha :as alpha])
   (:require-macros [reagent.ratom :refer [reaction]]))
 
+(def toteuma-vari {"lisatty" alpha/fig-default
+                   "paivitetty" alpha/lemon-default
+                   "poistettu" alpha/eggplant-default
+                   "tarkastus" alpha/pitaya-default
+                   "korjaus" alpha/pea-default
+                   "puhdistus" alpha/black-light})
 
-(def toteuma-vari {"lisatty" "green"
-                   "paivitetty" "yellow"
-                   "poistettu" "red"
-                   "tarkastus" "purple"
-                   "korjaus" "black"
-                   "puhdistus" "white"})
 (def karttataso-varusteet (atom []))
 
 (defonce karttataso-nakyvissa? (atom true))
@@ -21,19 +22,21 @@
              (when (and (not-empty kohteet) @karttataso-nakyvissa?)
                (with-meta (mapv (fn [{:keys [toteuma] :as kohde}]
                                   (when (:sijainti kohde)
-                                    {:alue (merge {:stroke {:width 8
-                                                            :color (or (get toteuma-vari toteuma) "pink")}}
-                                                  (:sijainti kohde))
-                                     :selite {:teksti "Varuste"
-                                              :img (kartta-ikonit/pinni-ikoni "sininen")}
-                                     :ikonit [{:tyyppi :merkki
-                                               :paikka [:loppu]
-                                               :zindex 21
-                                               :img (kartta-ikonit/pinni-ikoni "sininen")}]}))
+                                    (let [vari (or (get toteuma-vari toteuma) "white")]
+                                      {:alue (merge {:stroke {:width 8
+                                                              :color vari}}
+                                                    (:sijainti kohde))
+                                       :selite {:teksti toteuma
+                                                :img (kartta-ikonit/pinni-ikoni "sininen")}
+                                       :ikonit [{:tyyppi :merkki
+                                                 :paikka [:loppu]
+                                                 :zindex 21
+                                                 :img (kartta-ikonit/pinni-ikoni "sininen")}]})))
                                 kohteet)
-                          {:selitteet [{:vari (map :color [{:color puhtaat/musta-raja
-                                                            :width 8}
-                                                           {:color puhtaat/vihrea
-                                                            :width 6}])
-                                        :teksti "Varusteet"}]})))))
+                          {:selitteet [{:teksti "Lisätty" :vari puhtaat/fig-default}
+                                       {:teksti "Poistettu" :vari puhtaat/eggplant-default}
+                                       {:teksti "Tarkistettu" :vari puhtaat/pitaya-default}
+                                       {:teksti "Puhdistettu" :vari puhtaat/black-light}
+                                       {:teksti "Korjattu" :vari puhtaat/pea-default}
+                                       {:teksti "Päivitetty" :vari puhtaat/lemon-default}]})))))
 
