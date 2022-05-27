bd4c8fb019dbf3f1b611d2c640fb69be359828da
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-05-27 13:10:16 +0300

    Muuttaa vertailun käyttämään vain varustetoteuman alkupvm

--- src/clj/harja/palvelin/integraatiot/velho/sanomat/varuste_vastaanottosanoma.clj
+++ src/clj/harja/palvelin/integraatiot/velho/sanomat/varuste_vastaanottosanoma.clj
@@ -193,7 +193,7 @@
   1. pakolliset kentät
   2. muutoksen-lahde-oid pitää olla Hallintorekisterin maanteiden-hoitourakka, jonka urakkakoodi vastaa VHAR-6045 mukaisesti Harjassa olevaan hoito
      tai teiden-hoito tyyppisen voimassaolevan urakan tunnisteeseen (URAKKA.urakkanro)
-  3. version-voimassaolon alkupvm ja loppupvm pitää leikata 1. kohdan urakan keston kanssa. (Jos näin ei ole, ei kohde näy käyttöliittymässä.)
+  3. version-voimassaolon alkupvm pitää leikata 1. kohdan urakan keston kanssa. (Jos näin ei ole, ei kohde näy käyttöliittymässä.)
   4. Varusteen tietolajin pitää sisältyä VHAR-5109 kommentissa mainittuihin tietolajeihin
   5. Varusteversion toimenpiteen pitää olla jokin seuraavista: lisäys, päivitys, poisto, tarkastus, korjaus ja puhdistus
   6. Varusten ollessa tl506 (liikennemerkki) tulee sillä olla asetusnumero tai lakinumero, joka kertoo liikennemerkin tyypin
@@ -208,7 +208,9 @@
   Varoitus jättää tämän lähteen viimeisen ajokerran päiväyksen päivittämättä, eli integraatio epäonnistuu osittain.
   Tästä seuraa uudelleen lataus samasta alkupäivämäärästä lähtien seuraavalla ajokerralla."
   [{:keys [alkupvm loppupvm urakka_id] :as varustetoteuma} urakka-olemassaolo]
-  (let [varuste-olemassaolo {:alkupvm alkupvm :loppupvm loppupvm}
+
+  (let [; VHAR-6330 Version alkupvm on oltava urakan sisällä, muuten versio ei ole syntynyt urakassa
+        varuste-olemassaolo {:alkupvm alkupvm :loppupvm alkupvm}
         puuttuvat-pakolliset (puuttuvat-pakolliset-avaimet varustetoteuma)]
     (cond
       ; 2
