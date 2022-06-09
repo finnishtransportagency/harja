d85b96a024e8a502f6edf3a114db000fe904c117
Markus Vähäkangas <markus.vahakangas@solita.fi>
2022-06-09 07:57:48 +0300

    Toteumien siirtoon sql lause

--- src/clj/harja/kyselyt/toteumat.sql
+++ src/clj/harja/kyselyt/toteumat.sql
@@ -1427,3 +1427,40 @@ WHERE (t.alkanut BETWEEN :alkuaika::TIMESTAMP AND :loppuaika::TIMESTAMP)
 GROUP BY t.id, t.alkanut, u.id
 ORDER BY t.alkanut ASC
 LIMIT 100000;
+
+-- name: siirra-toteumat-analytiikalle
+-- Joka yö siirretään raskaista toteumiin liittyvistä tauluista edellisen vuorokauden aikana tulleet ja muokatut
+-- toteumat analytiikalle tehtyyn omaan tauluun.
+INSERT INTO analytiikka_toteumat (
+SELECT t.id as toteuma_tunniste_id,
+       t.sopimus as toteuma_sopimus_id,
+       t.alkanut as toteuma_alkanut,
+       t.paattynyt as toteuma_paattynyt,
+       u.urakkanro AS toteuma_alueurakkanumero,
+       t.suorittajan_ytunnus as toteuma_suorittaja_ytunnus,
+       t.suorittajan_nimi as toteuma_suorittaja_nimi,
+       t.tyyppi as toteuma_toteumatyyppi, -- "yksikkohintainen","kokonaishintainen","akillinen-hoitotyo","lisatyo", "muutostyo","vahinkojen-korjaukset"
+       t.lisatieto as toteuma_lisatieto,
+       json_agg(row_to_json(row(tt.id, tt.maara, tkoodi.yksikko, tt.lisatieto))) AS toteumatehtavat,
+       json_agg(row_to_json(row(mk.nimi, tm.maara, mk.yksikko))) AS toteumamateriaalit,
+       t.tr_numero as toteuma_tiesijainti_numero,
+       t.tr_alkuosa as toteuma_tiesijainti_aosa,
+       t.tr_alkuetaisyys as toteuma_tiesijainti_aet,
+       t.tr_loppuosa as toteuma_tiesijainti_losa,
+       t.tr_loppuetaisyys as toteuma_tiesijainti_let,
+       t.luotu as toteuma_muutostiedot_luotu,
+       t.luoja as toteuma_muutostiedot_luoja,
+       t.muokattu as toteuma_muutostiedot_muokattu,
+       t.muokkaaja as toteuma_muutostiedot_muokkaaja,
+       t.tyokonetyyppi as tyokone_tyokonetyyppi,
+       t.tyokonetunniste as tyokone_tunnus
+FROM toteuma t
+         LEFT JOIN toteuma_tehtava tt ON tt.toteuma = t.id
+         LEFT JOIN toimenpidekoodi tkoodi ON tkoodi.id = tt.toimenpidekoodi
+         LEFT JOIN toteuma_materiaali tm ON tm.toteuma = t.id
+         LEFT JOIN materiaalikoodi mk ON tm.materiaalikoodi = mk.id
+         LEFT JOIN urakka u on t.urakka = u.id
+WHERE (t.alkanut BETWEEN :nyt::TIMESTAMP AND :nyt::TIMESTAMP - '1 day'::interval)
+GROUP BY t.id, t.alkanut, u.id
+ORDER BY t.alkanut ASC
+) ON CONFLICT DO NOTHING; -- Jos vahingossa siirretään sama toteuma toiseen kertaan, niin luovutaan yrityksestä
