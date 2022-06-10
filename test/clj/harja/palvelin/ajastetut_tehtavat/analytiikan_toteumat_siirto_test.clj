3c5a73931e9895e62bf8d483f8e3f328e662b1b1
Markus Vähäkangas <markus.vahakangas@solita.fi>
2022-06-10 08:23:10 +0300

    Yksikkötesti toteumien siirtämiseen analytiikalle

--- test/clj/harja/palvelin/ajastetut_tehtavat/analytiikan_toteumat_siirto_test.clj
+++ test/clj/harja/palvelin/ajastetut_tehtavat/analytiikan_toteumat_siirto_test.clj
@@ -9,10 +9,12 @@
             [harja.palvelin.integraatiot.integraatioloki :as integraatioloki]
             [harja.pvm :refer [luo-pvm]]
             [clj-time.core :as t]
+            [clj-time.coerce :as t-coerce]
             [clojure.java.io :as io]
             [com.stuartsierra.component :as component]
             [harja.palvelin.komponentit.fim :as fim]
-            [harja.palvelin.palvelut.urakat :as urakat])
+            [harja.palvelin.palvelut.urakat :as urakat]
+            [harja.pvm :as pvm])
   (:use org.httpkit.fake))
 
 (defn jarjestelma-fixture [testit]
@@ -27,31 +29,20 @@
   (testit)
   (alter-var-root #'jarjestelma component/stop))
 
-(defn paivamaara-vali
-  "Palautetaan sequenssi DateTime arvoista annettujen päivien väliltä. Steppinä käytetään annettua parametria."
-  [alku end step]
-  (let [inf-range (periodic-seq alku step)
-        loppunut? (fn [t] (t/within? (t/interval alku end)
-                             t))]
-    (take-while loppunut? inf-range)))
-
 (use-fixtures :once jarjestelma-fixture)
 
-(deftest siirra-analytiikan-toteumat-toimii-vuodelle-2015-idempotentisti
+(deftest siirra-analytiikan-toteumat-toimii-vuodelle-2015
   (let [testitietokanta (:db jarjestelma)
         ;; HAetaan vain vuoden 2015 toteumat ja siivotaan varalta kaikki 2015 vuoden mahdollisesti siirretyt toteumat pois
-        _ (u "DELETE FROM analytiikan_toteumat WHERE alkanut > '2014-12-31' AND alkanut < '2016-01-01'")
+        _ (u "DELETE FROM analytiikka_toteumat WHERE toteuma_alkanut > '2014-12-31' AND toteuma_alkanut < '2016-01-01'")
         hae-maarat (fn []
                      [(first (first (q "SELECT count(*) FROM toteuma WHERE alkanut > '2014-12-31' AND alkanut < '2016-01-01'")))
-                      (first (first (q "SELECT count(*) FROM analytiikan_toteumat WHERE alkanut > '2014-12-31' AND alkanut < '2016-01-01'")))])
+                      (first (first (q "SELECT count(*) FROM analytiikka_toteumat WHERE toteuma_alkanut > '2014-12-31' AND toteuma_alkanut < '2016-01-01'")))])
         maarat-alussa (hae-maarat)
-        _ (println "maarat-alussa" maarat-alussa)
-        _ (reduce (fn [maara paiva]
-                    (analytiikan-toteumat/siirra-toteumat testitietokanta paiva))
-            0
-            (paivamaara-vali (luo-pvm 2015 1 1) (luo-pvm 2015 12 31) 1))
-        maarat-lopussa (hae-maarat)
-        _ (println "maarat-lopussa" maarat-lopussa)]
+        _ (mapv (fn [paiva]
+                  (analytiikan-toteumat/siirra-toteumat testitietokanta (t-coerce/to-sql-time paiva)))
+            (pvm/paivat-valissa (pvm/luo-pvm 2015 0 1) (pvm/luo-pvm 2015 11 31)))
+        maarat-lopussa (hae-maarat)]
     (is (> (first maarat-alussa) (second maarat-alussa)))
     (is (= (first maarat-alussa) (second maarat-lopussa)))))
 
