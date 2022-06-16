218d52da0b686109e0f9b5a6d44e10b593bfdaa3
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-06-16 11:29:37 +0300

    Korjaa varustetapahtuman ja urakan aikarajan viestin

--- src/clj/harja/palvelin/integraatiot/velho/sanomat/varuste_vastaanottosanoma.clj
+++ src/clj/harja/palvelin/integraatiot/velho/sanomat/varuste_vastaanottosanoma.clj
@@ -233,7 +233,8 @@
       ; 3
       (not (aikavalit-leikkaavat varuste-olemassaolo urakka-olemassaolo))
       {:toiminto :varoita :viesti
-       (str "version-voimassaolon alkupvm ja loppupvm pitää leikata urakan keston kanssa alkupvm: " alkupvm " loppupvm: " loppupvm)}
+       (str "version-voimassaolon alkupvm pitää sisältyä urakan aikaväliin alkupvm: " alkupvm
+            " urakan aikaväli: {:alkupvm " (:alkupvm urakka-olemassaolo) " :loppupvm " (:loppupvm urakka-olemassaolo) "}")}
       ; 5
       (nil? (:toteuma varustetoteuma))
       {:toiminto :varoita :viesti "Toimenpide ei ole lisäys, päivitys, poisto, tarkastus, korjaus tai puhdistus"}
