15f595d48666d1b5fc06feddfc91f763db1e92b4
Toni Dahl <toni.dahl@solita.fi>
2022-07-05 18:31:33 +0300

    Lisää otsikko, selkiytä sivun tyyliä
    RelNotes:
    Related:

--- src/cljs/harja/views/urakka/toteumat/pohjavesialueiden_suola.cljs
+++ src/cljs/harja/views/urakka/toteumat/pohjavesialueiden_suola.cljs
@@ -16,39 +16,36 @@
 
 
 (defn vetolaatikko-taso-2 [rajoitusalueet]
-  [:div {:style {:display "flex"}}
-   [:div {:style {:width "3px"
-                  :height "auto"
-                  :margin-left "5px" ;; Manuaalinen säätö, jotta sininen viiva menee vatolaatikon avausnapin kanssa samaa linjaa
-                  :border-left "3px solid blue"}}]
-   [:div {:style {:margin-left "1rem"}}
-    [grid/grid {:tunniste (fn [rivi]
-                            (str "rajoitusalue_vetolaatikko_taso_2_rivi_" (hash rivi)))
-                :tyhjä (if (nil? @tiedot/urakan-rajoitusalueet)
-                         [yleiset/ajax-loader "Rajoitusalueita haetaan..."]
-                         "Ei Rajoitusalueita")}
-     [{:tyyppi :avattava-rivi :leveys "2%"}
-      {:otsikko "Tie" :tunniste :tie :hae (comp :tie :tr-osoite) :leveys 1}
-      {:otsikko "Osoiteväli" :tunniste :tie :hae :tr-osoite
-       :fmt (fn [tr-osoite]
-              (str
-                (str (:aosa tr-osoite) " / " (:aet tr-osoite))
-                " - "
-                (str (:losa tr-osoite) " / " (:let tr-osoite))))
-       :leveys 1}
-      {:otsikko "Pohjavesialue (tunnus)" :tunniste :pohjavesialueet :hae :pohjavesialueet :leveys 1}
-      {:otsikko "Pituus (m)" :tunniste :pituus :hae :pituus :leveys 1}
-      {:otsikko "Pituus ajoradat (m)" :tunniste :pituus_ajoradat :hae :pituus_ajoradat :leveys 1}
-      {:otsikko "Formiaatit (t/ajoratakm)" :tunniste :formiaatit_t_per_ajoratakm
-       :hae :formiaatit_t_per_ajoratakm :leveys 1}
-      {:otsikko "Talvisuola (t/ajoratakm)" :tunniste :formiaatit_t_per_ajoratakm
-       :hae :formiaatit_t_per_ajoratakm :leveys 1}
-      {:otsikko "Suolankäyttöraja (t/ajoratakm)" :tunniste :suolankayttoraja :hae :suolankayttoraja :leveys 1}
-      {:otsikko "" :tunniste :pituus :hae :kaytettava-formaattia? :fmt #(when % "Käytettävä formaattia") :leveys 1}]
-     rajoitusalueet]]])
+  [grid/grid {:tunniste (fn [rivi]
+                          (str "rajoitusalue_vetolaatikko_taso_2_rivi_" (hash rivi)))
+              :piilota-muokkaus? true
+              :reunaviiva? true
+              :tyhjä (if (nil? @tiedot/urakan-rajoitusalueet)
+                       [yleiset/ajax-loader "Rajoitusalueita haetaan..."]
+                       "Ei Rajoitusalueita")}
+   [{:tyyppi :avattava-rivi :leveys "2%"}
+    {:otsikko "Tie" :tunniste :tie :hae (comp :tie :tr-osoite) :leveys 1}
+    {:otsikko "Osoiteväli" :tunniste :tie :hae :tr-osoite
+     :fmt (fn [tr-osoite]
+            (str
+              (str (:aosa tr-osoite) " / " (:aet tr-osoite))
+              " - "
+              (str (:losa tr-osoite) " / " (:let tr-osoite))))
+     :leveys 1}
+    {:otsikko "Pohjavesialue (tunnus)" :tunniste :pohjavesialueet :hae :pohjavesialueet :leveys 1}
+    {:otsikko "Pituus (m)" :tunniste :pituus :hae :pituus :leveys 1}
+    {:otsikko "Pituus ajoradat (m)" :tunniste :pituus_ajoradat :hae :pituus_ajoradat :leveys 1}
+    {:otsikko "Formiaatit (t/ajoratakm)" :tunniste :formiaatit_t_per_ajoratakm
+     :hae :formiaatit_t_per_ajoratakm :leveys 1}
+    {:otsikko "Talvisuola (t/ajoratakm)" :tunniste :formiaatit_t_per_ajoratakm
+     :hae :formiaatit_t_per_ajoratakm :leveys 1}
+    {:otsikko "Suolankäyttöraja (t/ajoratakm)" :tunniste :suolankayttoraja :hae :suolankayttoraja :leveys 1}
+    {:otsikko "" :tunniste :pituus :hae :kaytettava-formaattia? :fmt #(when % "Käytettävä formaattia") :leveys 1}]
+   rajoitusalueet])
 
 (defn vetolaatikko-taso-1 [rajoitusalueet]
   [grid/grid {:tunniste :id
+              :piilota-muokkaus? true
               :reunaviiva? true
               :vetolaatikot (into {}
                               (map (juxt :id (fn [rivi] [vetolaatikko-taso-2 rajoitusalueet])))
@@ -85,6 +82,7 @@
   vielä tiettyyn suolariviin liittyviin toteutumiin."
   [rajoitusalueet]
   [grid/grid {:tunniste :id
+              :piilota-muokkaus? true
               :ensimmainen-sarake-sticky? true
               #_#_:esta-tiivis-grid? true
               #_#_:samalle-sheetille? true
@@ -128,10 +126,17 @@
         [:div.pohjavesialueiden-suola
          ;; TODO: Yleiskäyttöinen komponentti. Speksissä halutaan erilainen komponentti, jossa on nappula, jolla
          ;;       käynnistetään tietojen hakeminen aikavälin valinnan jälkeen
-         ;; Aikavälivalinta
-         [urakka-valinnat/aikavali-nykypvm-taakse urakka
-          tiedot/valittu-aikavali
-          {:aikavalin-rajoitus [12 :kuukausi]}]
 
-         ;; Rajoitiusalueiden taulukko
+         [:h2 "Pohjavesialueiden suolatoteumat"]
+
+         ;; Aikavälivalinta ja muut kontrollit
+         [:div.taulukon-kontrollit
+          [urakka-valinnat/aikavali-nykypvm-taakse urakka
+           tiedot/valittu-aikavali
+           {:aikavalin-rajoitus [12 :kuukausi]}]]
+
+         ;; TODO: Kartta
+         #_[kartta]
+
+         ;; Rajoitusalueiden taulukko
          [taulukko rajoitusalueet]]))))
