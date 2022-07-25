aec58ed9f90f97ec0677911ccb86e0b3a231e934
Jere Luomajoki <jere.luomajoki@solita.fi>
2022-07-25 15:19:28 +0300

    Siisti sql-funktiota koodikatselmoinnin perusteella

--- tietokanta/src/main/resources/db/migration/R__Kevyen_liikenteen_talvisuola.sql
+++ tietokanta/src/main/resources/db/migration/R__Kevyen_liikenteen_talvisuola.sql
@@ -1,9 +1,18 @@
+-- hieno
 CREATE OR REPLACE FUNCTION siirra_talvisuola_kelvilta(urakka_ INTEGER, alku TIMESTAMP, loppu TIMESTAMP)
     RETURNS INTEGER AS
 $$
 DECLARE
-    uusi_trp record;
-    maara    int := 0;
+    uusi_trp                 RECORD;
+    maara                    INT   := 0;
+    suolaus_tpk              INT   := (SELECT id
+                                       FROM toimenpidekoodi
+                                       WHERE nimi = 'Suolaus');
+    suolaus_materiaalikoodit INT[] := (SELECT ARRAY_AGG(id)
+                                       FROM materiaalikoodi
+                                       WHERE materiaalityyppi IN ('talvisuola', 'formiaatti'));
+    -- Niiden kelvien talvihoitoluokat, joita ei talvisuolata.
+    kelvien_talvihoitoluokat INT[] := '{9, 10, 11}';
 BEGIN
     FOR uusi_trp IN (SELECT tr.toteuma,
                             tr.luotu,
@@ -12,35 +21,29 @@ BEGIN
                               INNER JOIN toteuman_reittipisteet tr on tr.toteuma = t.id
                      WHERE t.alkanut BETWEEN alku::DATE AND (loppu::DATE + interval '1 day')::DATE
                        AND (urakka_ IS NULL OR t.urakka = urakka_)
-                       AND t.poistettu IS false
+                       AND t.poistettu IS FALSE
                        AND array_length(tr.reittipisteet, 1) > 0
                        -- Vain talvisuola ja formiaatti-materiaalit ja suolaus-toimenpide.
                        AND (EXISTS(SELECT DISTINCT m.materiaalikoodi
                                    FROM unnest(tr.reittipisteet) trp
                                             JOIN LATERAL unnest(trp.materiaalit) m ON TRUE
-                                   WHERE materiaalikoodi IN
-                                         (select id
-                                          FROM materiaalikoodi
-                                          where materiaalityyppi IN ('talvisuola', 'formiaatti')))
+                                   WHERE materiaalikoodi = ANY(suolaus_materiaalikoodit))
                          OR EXISTS(SELECT DISTINCT t.toimenpidekoodi
                                    FROM unnest(tr.reittipisteet) trp
                                             JOIN LATERAL unnest(trp.tehtavat) t ON TRUE
-                                   WHERE t.toimenpidekoodi IN
-                                         (select id
-                                          FROM toimenpidekoodi
-                                          where nimi = 'Suolaus')))
-                       -- Vain kevyen liikenteen väylillä
+                                   WHERE t.toimenpidekoodi = suolaus_tpk))
+                       -- Vain toteumat, joiden reittipisteet osuvat kelveille. 
                        AND EXISTS(SELECT DISTINCT trp.talvihoitoluokka
-                                  FROM unnest(tr.reittipisteet) trp
-                                  WHERE trp.talvihoitoluokka IN (9, 10, 11)))
+                                  FROM UNNEST(tr.reittipisteet) trp
+                                  WHERE trp.talvihoitoluokka = ANY(kelvien_talvihoitoluokat)))
         LOOP
             UPDATE toteuman_reittipisteet trp
             SET reittipisteet = (SELECT ARRAY_AGG((rp.aika, rp.sijainti,
                                                    (CASE
-                                                        WHEN rp.talvihoitoluokka IN (9, 10, 11)
+                                                        WHEN rp.talvihoitoluokka = ANY (kelvien_talvihoitoluokat)
                                                             THEN hoitoluokka_pisteelle(rp.sijainti::GEOMETRY,
                                                                                        'talvihoito',
-                                                                                       250, '{9,10,11}')
+                                                                                       250, kelvien_talvihoitoluokat)
                                                         ELSE
                                                             rp.talvihoitoluokka
                                                        END),
