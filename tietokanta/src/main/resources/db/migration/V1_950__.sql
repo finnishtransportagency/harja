14aded1cef7921834e05fe1f8a46e152971c839d
Jere Luomajoki <jere.luomajoki@solita.fi>
2022-09-06 10:39:58 +0300

    Erota migraatio kahteen tiedostoon

--- tietokanta/src/main/resources/db/migration/V1_950__.sql
+++ tietokanta/src/main/resources/db/migration/V1_950__.sql
@@ -1,8 +1,2 @@
 ALTER TYPE sanktiolaji ADD VALUE 'talvisuolan_ylitys'; -- Talvisuolan kokonaiskäytön ylitys
 ALTER TYPE sanktiolaji ADD VALUE 'pohjavesisuolan_ylitys'; -- Pohjavesialueen suolankäytön ylitys
-
-INSERT INTO sanktiotyyppi (nimi, toimenpidekoodi, urakkatyyppi, sanktiolaji) 
-VALUES ('Suolasakko',
-        (SELECT id FROM toimenpidekoodi WHERE koodi = '23104'),
-        '{hoito, teiden-hoito}',
-        '{talvisuolan_ylitys, pohjavesisuolan_ylitys}');
