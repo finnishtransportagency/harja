dc9ce4dcc7093ada603d51e9f4136490b33044af
Markus Vähäkangas <markus.vahakangas@solita.fi>
2022-09-28 08:42:59 +0300

    Yksikkötestit sampo-api vastaanotto rajapinnalle

---
+++ test/clj/harja/palvelin/integraatiot/vayla_rest/sampo_api_test.clj
@@ -0,0 +1,139 @@
+(ns harja.palvelin.integraatiot.vayla-rest.sampo-api-test
+  (:require [clojure.test :refer :all]
+            [clj-time.core :as t]
+            [com.stuartsierra.component :as component]
+            [harja.testi :refer :all]
+            [harja.tyokalut.xml :as xml]
+            [harja.palvelin.integraatiot.sampo.tyokalut :as sampo-tyokalut]
+            [clojure.data.zip.xml :as z]
+            [harja.palvelin.integraatiot.api.tyokalut :as api-tyokalut]
+            [harja.palvelin.integraatiot.vayla-rest.sampo-api :as sampo-api]))
+
+(def sampo-vastaanotto-url "/api/sampo")
+(def kayttaja "destia")
+(def kayttaja-yit "yit-rakennus")
+(def +xsd-polku+ "xsd/sampo/inbound/")
+
+(def jarjestelma-fixture
+  (laajenna-integraatiojarjestelmafixturea
+    kayttaja
+    :api-sampo (component/using
+                      (sampo-api/->ApiSampo)
+                      [:http-palvelin :db :integraatioloki])))
+
+(use-fixtures :each (compose-fixtures tietokanta-fixture jarjestelma-fixture))
+
+;; Helpperit
+(defn hae-urakat-sampoidlla [sampoid]
+  (q (format "select id from urakka where sampoid = '%s';" sampoid)))
+
+(defn hae-integraatiotapahtumat-tietokannasta []
+  (q-map (str "SELECT it.id, it.integraatio, it.alkanut, it.paattynyt, it.onnistunut, i.jarjestelma, i.nimi
+            FROM integraatiotapahtuma it JOIN integraatio i ON i.id = it.integraatio order by it.id DESC;")))
+
+(defn- tarkista-lahetettava-vastaus [viesti]
+  (let [xml viesti
+        data (xml/lue xml)]
+    (is (xml/validi-xml? +xsd-polku+ "HarjaToSampoAcknowledgement.xsd" xml) "Kuittaus on validia XML:ää.")
+
+    (is (= "UrakkaMessageId" (first (z/xml-> data
+                                      (fn [kuittaus]
+                                        (z/xml1-> (z/xml1-> kuittaus) :Ack (z/attr :MessageId))))))
+      "Kuittaus on tehty oikeaan viestiin.")
+
+    (is (= "Project" (first (z/xml-> data
+                              (fn [kuittaus]
+                                (z/xml1-> (z/xml1-> kuittaus) :Ack (z/attr :ObjectType))))))
+      "Kuittauksen tyyppi on Project eli urakka.")
+
+    (is (= "NA" (first (z/xml-> data
+                         (fn [kuittaus]
+                           (z/xml1-> (z/xml1-> kuittaus) :Ack (z/attr :ErrorCode))))))
+      "Virheitä ei tapahtunut käsittelyssä.")))
+
+;; Testit
+
+(deftest tarkista-viestin-kasittely-ja-kuittaukset
+  (with-redefs [t/now #(t/first-day-of-the-month 2017 2)]
+    (let [urakat-alkuun (hae-urakat-sampoidlla "TESTIURAKKA")
+          vastaus (api-tyokalut/post-kutsu [sampo-vastaanotto-url] kayttaja-yit portti
+                    sampo-tyokalut/+testi-hoitourakka-sanoma+ nil true)
+          sampo-urakat-loppuun (hae-urakat-sampoidlla "TESTIURAKKA")]
+      (is (= 0 (count urakat-alkuun)) "TESTIURAKKA Sampo ID:llä ei löydy urakkaa ennen tuontia.")
+
+      (tarkista-lahetettava-vastaus (:body vastaus))
+      (is (= 1 (count sampo-urakat-loppuun)) "Viesti on käsitelty ja tietokannasta löytyy urakka Sampo id:llä."))
+
+
+    (let [urakan-tpi (ffirst (q "SELECT id
+                                  FROM toimenpideinstanssi
+                                  WHERE urakka = (SELECT id FROM urakka WHERE sampoid = 'TESTIURAKKA')
+                                  AND nimi = 'Päällystyksen yksikköhintaiset työt'"))]
+      (is (nil? urakan-tpi) "Urakalle ei luotu toimenpideinstanssia"))
+
+    (let [urakan-valitavoitteet (map first (q "SELECT nimi
+                                  FROM valitavoite
+                                  WHERE urakka = (SELECT id FROM urakka WHERE sampoid = 'TESTIURAKKA')"))]
+
+      (is (= (count urakan-valitavoitteet) 6))
+      ;; Kertaluontoiset lisättiin kerran
+      (is (= (count (filter #(= "Koko Suomi aurattu" %) urakan-valitavoitteet)) 1))
+      (is (= (count (filter #(= "Kaikkien urakoiden kalusto huollettu" %) urakan-valitavoitteet)) 1))
+      ;; Toistuvat lisättiin jokaiselle urakan jäljellä olevalle vuodelle (huomio testissä feikattu nykyaika)
+      (is (= (count (filter #(= "Koko Suomen liikenneympäristö hoidettu" %) urakan-valitavoitteet)) 4)))))
+
+(deftest tarkista-paallystysurakan-toimenpideinstanssin-luonti
+  (let [integ-tapahtumat-alkuun (hae-integraatiotapahtumat-tietokannasta)
+        sampo-urakat-alkuun (hae-urakat-sampoidlla "TESTIURAKKA")
+        vastaus (api-tyokalut/post-kutsu [sampo-vastaanotto-url] kayttaja-yit portti
+                     sampo-tyokalut/+testi-paallystysurakka-sanoma+ nil true)
+        sampo-urakat-loppuun (hae-urakat-sampoidlla "TESTIURAKKA")
+        integ-tapahtumat-loppuun (hae-integraatiotapahtumat-tietokannasta)]
+    (is (empty? integ-tapahtumat-alkuun))
+    (is (not (empty? integ-tapahtumat-loppuun)))
+    (is (= "sampo" (:jarjestelma (first integ-tapahtumat-loppuun))))
+    (is (= "sisaanluku" (:nimi (first integ-tapahtumat-loppuun))))
+    (is (true? (:onnistunut (first integ-tapahtumat-loppuun))))
+
+    (is (= 0 (count sampo-urakat-alkuun)) "TESTIURAKKA Sampo ID:llä ei löydy urakkaa ennen tuontia.")
+    (is (= 1 (count sampo-urakat-loppuun)) "Viesti on käsitelty ja tietokannasta löytyy urakka Sampo id:llä.")
+
+    (tarkista-lahetettava-vastaus (:body vastaus))
+
+
+    (let [urakan-tpi (ffirst (q "SELECT id
+                                  FROM toimenpideinstanssi
+                                  WHERE urakka = (SELECT id FROM urakka WHERE sampoid = 'TESTIURAKKA')
+                                  AND toimenpide = (SELECT id FROM toimenpidekoodi WHERE koodi = 'PAAL_YKSHINT')"))]
+      (is (some? urakan-tpi) "Urakalle on luotu toimenpideinstanssi"))))
+
+(deftest tarkista-tiemerkintaurakan-toimenpideinstanssin-luonti
+  (let [sampo-urakat-alkuun (hae-urakat-sampoidlla "TESTIURAKKA")
+        vastaus (api-tyokalut/post-kutsu [sampo-vastaanotto-url] kayttaja-yit portti
+                  sampo-tyokalut/+testi-tiemerkintasurakka-sanoma+ nil true)
+        sampo-urakat-loppuun (hae-urakat-sampoidlla "TESTIURAKKA")]
+
+    (is (= 0 (count sampo-urakat-alkuun)) "TESTIURAKKA Sampo ID:llä ei löydy urakkaa ennen tuontia.")
+    (tarkista-lahetettava-vastaus (:body vastaus))
+
+    (is (= 1 (count sampo-urakat-loppuun)) "Viesti on käsitelty ja tietokannasta löytyy urakka Sampo id:llä.")
+    (let [urakan-tpi (ffirst (q "SELECT id
+                                  FROM toimenpideinstanssi
+                                  WHERE urakka = (SELECT id FROM urakka WHERE sampoid = 'TESTIURAKKA')
+                                  AND toimenpide = (SELECT id FROM toimenpidekoodi WHERE koodi = 'TIEM_YKSHINT')"))]
+      (is (some? urakan-tpi) "Urakalle on luotu toimenpideinstanssi"))))
+
+(deftest tarkista-valaistusurakan-toimenpideinstanssin-luonti
+  (let [sampo-urakat-alkuun (hae-urakat-sampoidlla "TESTIURAKKA")
+        vastaus (api-tyokalut/post-kutsu [sampo-vastaanotto-url] kayttaja-yit portti
+                  sampo-tyokalut/+testi-valaistusurakka-sanoma+ nil true)
+        sampo-urakat-loppuun (hae-urakat-sampoidlla "TESTIURAKKA")]
+
+    (is (= 0 (count sampo-urakat-alkuun)) "TESTIURAKKA Sampo ID:llä ei löydy urakkaa ennen tuontia.")
+    (tarkista-lahetettava-vastaus (:body vastaus))
+    (is (= 1 (count sampo-urakat-loppuun)) "Viesti on käsitelty ja tietokannasta löytyy urakka Sampo id:llä.")
+    (let [urakan-tpi (ffirst (q "SELECT id
+                                  FROM toimenpideinstanssi
+                                  WHERE urakka = (SELECT id FROM urakka WHERE sampoid = 'TESTIURAKKA')
+                                  AND toimenpide = (SELECT id FROM toimenpidekoodi WHERE koodi = 'VALA_YKSHINT')"))]
+      (is (some? urakan-tpi) "Urakalle on luotu toimenpideinstanssi"))))
