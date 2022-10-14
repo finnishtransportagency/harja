c470dfb68f0cdab8e7d2695eb48c1f2153528f32
Jukka Länsineva <jukka.lansineva@solita.fi>
2022-10-14 10:45:27 +0300

    tiatokantaan luodaan linkitystaulu

--- tietokanta/src/main/resources/db/migration/V1_960__.sql
+++ tietokanta/src/main/resources/db/migration/V1_960__.sql
@@ -1,1 +1,6 @@
 alter table erilliskustannus add column kasittelytapa laatupoikkeaman_kasittelytapa, add column laskutuskuukausi date;
+create table erilliskustannus_liite (
+       bonus integer references erilliskustannus(id),
+       liite integer references liite(id));
+       
+comment on table erilliskustannus_liite is 'Bonuksien ja sanktioiden näkymässä bonuksiin liitetyt liitteet';
