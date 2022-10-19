55c9f6c995b4f5b1e97c0c432d4507b8aaddbd49
maaritlaine <maarit.laine@solita.fi>
2022-10-19 11:45:57 +0300

    Poista debuggia.

--- src/clj/harja/palvelin/integraatiot/vayla_rest/sahkoposti.clj
+++ src/clj/harja/palvelin/integraatiot/vayla_rest/sahkoposti.clj
@@ -71,7 +71,6 @@
   "Harjalla on lähetetty sähköpostit aiemmin laittamalla niistä jonoon ilmoitus, jonka Sähköpostipalvelin on käynyt
   lukemassa ja lähettänyt. Tämä fn lähettää samaiset sähköpostit suoralla api-rest kutsulla."
   [db asetukset integraatioloki sahkoposti-xml liite?]
-      (println "LÄHETÄ SÄHKÖPOSTI!! ")
   (try+
     (let [_ (log/info "Lähetä rest-api sähköposti.")
           vastaus (integraatiotapahtuma/suorita-integraatio
@@ -127,7 +126,6 @@
                         itmf (get-in asetukset [:tloik :toimenpidekuittausjono]))
         viesti-id (:viesti-id kutsun-data)
         kasitelty-vastaus (tloik-sahkoposti/vastaanota-sahkopostikuittaus jms-lahettaja db kutsun-data)
-        _ (println "VASTAUS " kasitelty-vastaus)
         ;; Lisää mahdolliset virheet kuittausviestiin
         virheet (if (#{"Virheellinen kuittausviesti" "Kuittausta ei voitu käsitellä"} (:otsikko kasitelty-vastaus))
                   [(:sisalto kasitelty-vastaus)]
@@ -138,11 +136,7 @@
                                                kutsun-data virheet)
                                            (xml/tee-xml-sanoma)))
         ;; Rakenna kuittaus xml välitettäväksi rajapinnan kutsujalle
-        kuittaus-xml (muodosta-kuittaus {:viesti-id viesti-id} virheet)
-        _ (println "Virheet " virheet)
-        _ (println "KUITTAUS " kuittaus-xml)
-        _ (println "VASTAUS KÄYTTÄJÄLLE " vastaus-virheelliseen-viestiin)
-        ]
+        kuittaus-xml (muodosta-kuittaus {:viesti-id viesti-id} virheet)]
 
     ;; Lähetetään kuittaus saatuun sähköpostiin, mikäli siinä on virheitä. Onnistuneesta vastaanotosta ei kuittausta lähetetä.
     (when-not (nil? virheet)
