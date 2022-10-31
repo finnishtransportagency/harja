ff93fcadec91139a26d7ba88a3041a5321ab7452
Jere Luomajoki <jere.luomajoki@solita.fi>
2022-10-31 08:40:41 +0200

    Erota migraatio kahteen, jotta flyway ei kaadu

--- tietokanta/src/main/resources/db/migration/V1_967__.sql
+++ tietokanta/src/main/resources/db/migration/V1_967__.sql
@@ -1,13 +1,2 @@
--- Halutaan erottaa muu-tyyppinen erilliskustannus muu bonus-tyyppisestä rivistä,
--- jotta bonukset voidaan näyttää sanktiot ja bonukset-välilehden taulukossa
+-- Tehdään uusi tyyppi bonus-tyyppisiä erilliskustannuksia varten.
 ALTER TYPE erilliskustannustyyppi ADD VALUE 'muu-bonus';
-
-UPDATE
-    erilliskustannus
-SET tyyppi = 'muu-bonus'
-FROM erilliskustannus ek
-         JOIN urakka u ON ek.urakka = u.id
-WHERE ek.tyyppi = 'muu'
-  AND u.loppupvm > current_date
-  AND (u.tyyppi = 'hoito' AND
-       ek.lisatieto ILIKE '%bonus%');
