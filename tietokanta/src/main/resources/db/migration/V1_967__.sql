41c19f6dc44fcc59041630e2efaa088251fc2592
Jere Luomajoki <jere.luomajoki@solita.fi>
2022-10-31 08:38:01 +0200

    Vaihda pakollinen join inneriksi, poista turhia ja lisää kommentti

--- tietokanta/src/main/resources/db/migration/V1_967__.sql
+++ tietokanta/src/main/resources/db/migration/V1_967__.sql
@@ -1,14 +1,13 @@
+-- Halutaan erottaa muu-tyyppinen erilliskustannus muu bonus-tyyppisestä rivistä,
+-- jotta bonukset voidaan näyttää sanktiot ja bonukset-välilehden taulukossa
 ALTER TYPE erilliskustannustyyppi ADD VALUE 'muu-bonus';
 
 UPDATE
     erilliskustannus
 SET tyyppi = 'muu-bonus'
 FROM erilliskustannus ek
-         LEFT JOIN toimenpideinstanssi tpi on ek.toimenpideinstanssi = tpi.id
-         LEFT JOIN toimenpidekoodi tpk3 ON tpi.toimenpide = tpk3.id
-         LEFT JOIN toimenpidekoodi tpk2 ON tpk3.emo = tpk2.id
-        LEFT JOIN urakka u ON ek.urakka = u.id
-WHERE ek.tyyppi = 'muu' AND
-      u.loppupvm > current_date AND
-      (u.tyyppi = 'hoito' AND
+         JOIN urakka u ON ek.urakka = u.id
+WHERE ek.tyyppi = 'muu'
+  AND u.loppupvm > current_date
+  AND (u.tyyppi = 'hoito' AND
        ek.lisatieto ILIKE '%bonus%');
