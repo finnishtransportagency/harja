9e782d6599e53eb6a29754db508c56b1703c08c5
Markus Vähäkangas <markus.vahakangas@solita.fi>
2022-12-22 15:30:46 +0200

    xml prettyprint

--- src/clj/harja/kyselyt/konversio.clj
+++ src/clj/harja/kyselyt/konversio.clj
@@ -439,3 +439,21 @@
   (if (string? x)
     (digest/sha-256 x)
     (digest/sha-256 (to-byte-array x))))
+
+(defn ppxml
+  "Prettyprintataan xml data"
+  [xml]
+  (let [in (javax.xml.transform.stream.StreamSource.
+             (java.io.StringReader. xml))
+        writer (java.io.StringWriter.)
+        out (javax.xml.transform.stream.StreamResult. writer)
+        transformer (.newTransformer
+                      (javax.xml.transform.TransformerFactory/newInstance))]
+    (.setOutputProperty transformer
+      javax.xml.transform.OutputKeys/INDENT "yes")
+    (.setOutputProperty transformer
+      "{http://xml.apache.org/xslt}indent-amount" "2")
+    (.setOutputProperty transformer
+      javax.xml.transform.OutputKeys/METHOD "xml")
+    (.transform transformer in out)
+    (-> out .getWriter .toString)))
