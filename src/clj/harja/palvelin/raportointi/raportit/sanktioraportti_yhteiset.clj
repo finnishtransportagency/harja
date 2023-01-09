f908a6a50df130ed43cfc02a67bbe6e7dd32df82
Markus Vähäkangas <markus.vahakangas@solita.fi>
2023-01-09 13:28:11 +0200

    Päivitä raportin otsikko

--- src/clj/harja/palvelin/raportointi/raportit/sanktioraportti_yhteiset.clj
+++ src/clj/harja/palvelin/raportointi/raportit/sanktioraportti_yhteiset.clj
@@ -9,6 +9,7 @@
     [harja.palvelin.raportointi.raportit.yleinen :as yleinen]
     [harja.kyselyt.urakat :as urakat-q]
     [harja.kyselyt.hallintayksikot :as hallintayksikot-q]
+    [harja.pvm :as pvm]
     [harja.domain.urakka :as urakka-domain]))
 
 (defn- rivi-kuuluu-talvihoitoon? [rivi]
@@ -176,13 +177,7 @@
                                       naytettavat-alueet)
                                     (when yhteensa-sarake?
                                       [{:otsikko "Yh\u00ADteen\u00ADsä" :leveys 15 :fmt :raha}])))
-        otsikko (yleinen/raportin-otsikko
-                  (case konteksti
-                    :urakka (:nimi (first (urakat-q/hae-urakka db urakka-id)))
-                    :hallintayksikko (:nimi (first (hallintayksikot-q/hae-organisaatio
-                                                     db hallintayksikko-id)))
-                    :koko-maa "KOKO MAA")
-                  raportin-nimi alkupvm loppupvm)
+        otsikko (str "Sanktiot, bonukset ja arvonvähennykset " (pvm/pvm alkupvm) " - " (pvm/pvm loppupvm))
         raportin-rivit (raportin-rivit-fn {:konteksti konteksti
                                            :naytettavat-alueet naytettavat-alueet
                                            :sanktiot-kannassa sanktiot-kannassa
@@ -190,7 +185,8 @@
                                            :yhteensa-sarake? yhteensa-sarake?})
         runko [:raportti {:nimi raportin-nimi
                           :orientaatio :landscape}
-               [:taulukko {:otsikko otsikko
+               [:otsikko otsikko]
+               [:taulukko {:otsikko "Sanktiot"
                            :oikealle-tasattavat-kentat (into #{} (range 1 (yleinen/sarakkeiden-maara raportin-otsikot)))
                            :sheet-nimi raportin-nimi}
                 raportin-otsikot
