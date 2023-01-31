9fbf736b34c6cea3f02937c51422ce2193e29c06
Samuli Taskila <samuli.taskila@solita.fi>
2023-01-31 09:41:06 +0200

    migraatio

--- tietokanta/src/main/resources/db/migration/V1_982__.sql
+++
@@ -1,4 +0,0 @@
--- Lisätty "Ei määritelty" suunta
-ALTER TYPE LIIKENNETAPAHTUMA_SUUNTA ADD VALUE 'ei-suuntaa';
--- Lisätty "Ei aluslajia" aluslaji
-ALTER TYPE LIIKENNETAPAHTUMA_ALUSLAJI ADD VALUE 'EI';
