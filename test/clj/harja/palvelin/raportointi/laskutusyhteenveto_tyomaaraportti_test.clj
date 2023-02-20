356d2d8f3c6df3266e0845c7b5d39fb8a5350f60
Markus Vähäkangas <markus.vahakangas@solita.fi>
2023-02-20 08:27:50 +0200

    Keskeneräinen testi

---
+++ test/clj/harja/palvelin/raportointi/laskutusyhteenveto_tyomaaraportti_test.clj
@@ -0,0 +1,52 @@
+(ns harja.palvelin.raportointi.laskutusyhteenveto_tyomaaraportti_test
+  (:require [clojure.test :refer :all]
+            [harja.palvelin.komponentit.tietokanta :as tietokanta]
+            [harja.palvelin.palvelut.toimenpidekoodit :refer :all]
+            [harja.palvelin.palvelut.urakat :refer :all]
+            [harja.testi :refer :all]
+            [taoensso.timbre :as log]
+            [com.stuartsierra.component :as component]
+            [clj-time.core :as t]
+            [clj-time.coerce :as c]
+            [harja.palvelin.komponentit.pdf-vienti :as pdf-vienti]
+            [harja.palvelin.raportointi :as raportointi]
+            [harja.palvelin.palvelut.raportit :as raportit]
+            [harja.fmt :as fmt]))
+
+(defn jarjestelma-fixture [testit]
+  (alter-var-root #'jarjestelma
+    (fn [_]
+      (component/start
+        (component/system-map
+          :db (tietokanta/luo-tietokanta testitietokanta)
+          :http-palvelin (testi-http-palvelin)
+          :pdf-vienti (component/using
+                        (pdf-vienti/luo-pdf-vienti)
+                        [:http-palvelin])
+          :raportointi (component/using
+                         (raportointi/luo-raportointi)
+                         [:db :pdf-vienti])
+          :raportit (component/using
+                      (raportit/->Raportit)
+                      [:http-palvelin :db :raportointi :pdf-vienti])))))
+
+  (testit)
+  (alter-var-root #'jarjestelma component/stop))
+
+(use-fixtures :each (compose-fixtures
+                      jarjestelma-fixture
+                      urakkatieto-fixture))
+
+
+(deftest raportin-suoritus-urakalle-toimii
+  (let [hk_alkupvm "2019-10-01"
+        hk_loppupvm "2020-09-30"
+        aikavali_alkupvm "2019-10-01"
+        aikavali_loppupvm "2020-09-30"
+        urakka-id (hae-oulun-alueurakan-2014-2019-id)
+        vastaus (q (format "select * from ly_raportti_tyomaakokous('%s'::DATE, '%s'::DATE, '%s'::DATE, '%s'::DATE, %s)"
+                     hk_alkupvm hk_loppupvm aikavali_alkupvm aikavali_loppupvm urakka-id))
+        _ (println "vastaus: " vastaus)]
+
+    (is (nil? vastaus))))
+
