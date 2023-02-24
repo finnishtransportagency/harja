5a9315e232dd9078f10069b671bb91e68ceda36b
Toni Dahl <toni.dahl@solita.fi>
2023-02-24 16:16:06 +0200

    Parantele Digiroad integraation virheiden hallintaa

--- src/clj/harja/palvelin/palvelut/digiroad.clj
+++ src/clj/harja/palvelin/palvelut/digiroad.clj
@@ -1,17 +1,32 @@
 (ns harja.palvelin.palvelut.digiroad
   "Digiroad haut"
   (:require [com.stuartsierra.component :as component]
-            [harja.palvelin.komponentit.http-palvelin :refer [julkaise-palvelu poista-palvelut]]
+            [slingshot.slingshot :refer [try+]]
+            [harja.palvelin.komponentit.http-palvelin :refer [julkaise-palvelu poista-palvelut transit-vastaus]]
             [harja.palvelin.integraatiot.digiroad.digiroad-komponentti :as digiroad]
-            [harja.domain.oikeudet :as oikeudet]))
+            [harja.domain.oikeudet :as oikeudet]
+            [harja.palvelin.integraatiot.api.tyokalut.virheet :as virheet]))
 
 
 (defn hae-kaistat [digiroad kayttaja tiedot]
   (let [urakka-id (:urakka-id tiedot)]
     (oikeudet/vaadi-lukuoikeus oikeudet/urakat-kohdeluettelo-paallystysilmoitukset kayttaja urakka-id)
 
-    (let [{:keys [tr-osoite ajorata]} tiedot]
-      (digiroad/hae-kaistat digiroad tr-osoite ajorata))))
+    (let [{:keys [tr-osoite ajorata]} tiedot
+          vastaus (digiroad/hae-kaistat digiroad tr-osoite ajorata)]
+
+      (if (false? (:onnistunut? vastaus))
+        (cond
+          (= digiroad/+virhe-kaistojen-haussa+ (:koodi vastaus))
+          (throw (IllegalArgumentException. "Kaistojen haku epäonnistui"))
+
+          (= virheet/+ulkoinen-kasittelyvirhe-koodi+ (:koodi vastaus))
+          (throw (Exception. "Kaistojen haku epäonnistui ulkoisen virheen takia"))
+
+          :else
+          (throw (Exception. "Kaistojen haku epäonnistui sisäisen virheen takia")))
+
+        vastaus))))
 
 (def hakutyypit
   [{:palvelu :hae-kaistat-digiroadista
