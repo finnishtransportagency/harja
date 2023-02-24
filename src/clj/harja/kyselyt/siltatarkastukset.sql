d036ed06cfeeb49871cf9ef79184e90b0f6a1af0
Jere Luomajoki <jere.luomajoki@solita.fi>
2023-02-24 16:27:31 +0200

    Hae vastuu-urakka myös muissa siltakyselyissä

--- src/clj/harja/kyselyt/siltatarkastukset.sql
+++ src/clj/harja/kyselyt/siltatarkastukset.sql
@@ -15,8 +15,7 @@ SELECT s.id,
        s.lakkautuspvm,
        s.kunnan_vastuulla AS "kunnan-vastuulla?",
        (s.vastuu_urakka = :urakka) AS "urakan-vastuulla?",
-       (SELECT nimi FROM urakka WHERE id = s.vastuu_urakka) AS "vastuu-urakka-nimi",
-       s.urakkatieto_kasin_muokattu AS "urakkatieto-kasin-muokattu"
+       (SELECT nimi FROM urakka WHERE id = s.vastuu_urakka) AS "vastuu-urakka-nimi"
 FROM silta s
        LEFT JOIN siltatarkastus s1 ON (s1.silta = s.id
   AND s1.poistettu = FALSE)
@@ -77,6 +76,8 @@ SELECT s.id,
        s.loppupvm,
        s.lakkautuspvm,
        s.kunnan_vastuulla,
+       (s.vastuu_urakka = :urakka) AS "urakan-vastuulla?",
+       (SELECT nimi FROM urakka WHERE id = s.vastuu_urakka) AS "vastuu-urakka-nimi",
        (SELECT array_agg(
                    concat(k.kohde, '=',
                           CASE
@@ -112,6 +113,8 @@ SELECT s.id,
        s.loppupvm,
        s.lakkautuspvm,
        s.kunnan_vastuulla,
+       (s.vastuu_urakka = :urakka) AS "urakan-vastuulla?",
+       (SELECT nimi FROM urakka WHERE id = s.vastuu_urakka) AS "vastuu-urakka-nimi",
        (SELECT array_agg(
                    concat(k.kohde, '=',
                           CASE
@@ -164,7 +167,9 @@ SELECT (SELECT COUNT(k1.kohde)
        s.tr_numero,
        s.loppupvm,
        s.lakkautuspvm,
-       s.kunnan_vastuulla
+       s.kunnan_vastuulla,
+       (s.vastuu_urakka = :urakka) AS "urakan-vastuulla?",
+       (SELECT nimi FROM urakka WHERE id = s.vastuu_urakka) AS "vastuu-urakka-nimi"
 FROM siltatarkastus st1
        JOIN siltatarkastus st2 ON (st2.silta = st1.silta AND st2.tarkastusaika > st1.tarkastusaika
   AND st2.poistettu = FALSE)
