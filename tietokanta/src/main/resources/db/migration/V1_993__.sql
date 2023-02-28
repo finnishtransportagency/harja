02e562e1f9228310d376f8e6317f24f1ffc7a3b9
Markus Vähäkangas <markus.vahakangas@solita.fi>
2023-02-28 09:39:02 +0200

    Tee kuumapäällysteestä sekä ensisijainen että käsin syötettävä

--- tietokanta/src/main/resources/db/migration/V1_993__.sql
+++ tietokanta/src/main/resources/db/migration/V1_993__.sql
@@ -1,2 +1,3 @@
 -- Tehdään Päällysteiden paikkaus - kuumapäällyste:stä käsin lisättävä määrä
-UPDATE toimenpidekoodi SET kasin_lisattava_maara = TRUE WHERE nimi = 'Päällysteiden paikkaus - kuumapäällyste';
+UPDATE toimenpidekoodi SET kasin_lisattava_maara = TRUE, ensisijainen = TRUE
+                       WHERE nimi = 'Päällysteiden paikkaus - kuumapäällyste';
