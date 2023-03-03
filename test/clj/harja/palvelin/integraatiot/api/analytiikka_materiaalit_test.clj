3c5c15ff76b8f0f8a43282c31112cf14ad09041b
Markus Vähäkangas <markus.vahakangas@solita.fi>
2023-03-03 14:23:00 +0200

    Lisää myös testeihin tieto kehitysmoodista

--- test/clj/harja/palvelin/integraatiot/api/analytiikka_materiaalit_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/analytiikka_materiaalit_test.clj
@@ -26,7 +26,7 @@
   (laajenna-integraatiojarjestelmafixturea
     kayttaja-yit
     :api-analytiikka (component/using
-                       (api-analytiikka/->Analytiikka)
+                       (api-analytiikka/->Analytiikka true)
                        [:http-palvelin :db-replica :integraatioloki])))
 
 (use-fixtures :each jarjestelma-fixture)
