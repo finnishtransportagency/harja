f23ad9556f194e1b11d8cd8ecf549d9eb654a2da
Toni Dahl <toni.dahl@solita.fi>
2023-03-10 19:10:28 +0200

    Siirrä 'indeksikorjaa'-sproc R__indeksilaskenta-migraatioon * Kopioitu viimeisin toteutus migraatiosta 921

--- tietokanta/src/main/resources/db/migration/R__Indeksilaskenta.sql
+++ tietokanta/src/main/resources/db/migration/R__Indeksilaskenta.sql
@@ -190,6 +190,45 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+CREATE OR REPLACE FUNCTION indeksikorjaa(korjattava_arvo NUMERIC, vuosi_ INTEGER, kuukausi_ INTEGER,
+                                         urakka_id INTEGER)
+    RETURNS NUMERIC AS
+$$
+DECLARE
+    -- Perusluku on urakalle sama riippumatta kuluvasta hoitokaudesta
+    perusluku      NUMERIC := indeksilaskennan_perusluku(urakka_id);
+    indeksin_nimi  TEXT    := (SELECT indeksi
+                                 FROM urakka u
+                                WHERE u.id = urakka_id);
+    arvo NUMERIC;
+    vertailuvuosi NUMERIC;
+    indeksikerroin NUMERIC;
+BEGIN
+    -- Indeksikerroin on hoitokausikohtainen, katsotaan aina edellisen hoitokauden syyskuun indeksiä.
+    IF kuukausi_ BETWEEN 1 AND 9
+    THEN
+        vertailuvuosi := vuosi_ - 1;
+    ELSE
+        vertailuvuosi := vuosi_;
+    END IF;
+
+    arvo := (SELECT i.arvo
+               FROM indeksi i
+              WHERE i.vuosi = vertailuvuosi
+                AND i.kuukausi = 9
+                AND nimi = indeksin_nimi);
+
+    -- Indeksikerroin pyöristetään 3 desimaaliin CLJ-puolella (budjettisuunnittelu/hae-urakan-indeksikertoimet)
+    indeksikerroin := round((arvo / perusluku), 3);
+
+    --RAISE NOTICE 'vuosi: %, kuukausi: %, arvo: %, indeksikerroin: %, korjattava arvo: %', vuosi_, kuukausi_, arvo, indeksikerroin, korjattava_arvo;
+
+    -- Tallennettava arvo pyöristetään 6 desimaaliin CLJ-puolella (budjettisuunnittelu/indeksikorjaa)
+    return round(korjattava_arvo * indeksikerroin, 6);
+END ;
+$$ language plpgsql;
+
+
 CREATE OR REPLACE FUNCTION sanktion_indeksikorotus(pvm date, indeksinimi varchar, summa NUMERIC, urakka_id INTEGER, sanktiolaji sanktiolaji)
     RETURNS kuukauden_indeksikorotus_rivi AS $$
 DECLARE
