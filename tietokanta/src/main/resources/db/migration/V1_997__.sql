d3cfa7bb2049530fd11c4aee2087fb647d4d32ad
Samuli Taskila <samuli.taskila@solita.fi>
2023-03-15 10:52:15 +0200

    indeksit lisätty materiaalien timestampeille

--- tietokanta/src/main/resources/db/migration/V1_997__.sql
+++ tietokanta/src/main/resources/db/migration/V1_997__.sql
@@ -1,3 +1,7 @@
---Otettu analytiikka toteumien rajapinnoissa materiaalien timestampit huomioon
+-- Otettu analytiikka toteumien rajapinnoissa materiaalien timestampit huomioon
 ALTER TABLE analytiikka_toteumat ADD COLUMN toteuma_materiaali_luotu timestamp;
 ALTER TABLE analytiikka_toteumat ADD COLUMN toteuma_materiaali_muokattu timestamp;
+
+-- Uusi indeksi materiaaleille
+create index analytiikka_materiaalit_muokattu_luotu_index
+    on analytiikka_toteumat (toteuma_materiaali_muokattu, toteuma_materiaali_luotu);
