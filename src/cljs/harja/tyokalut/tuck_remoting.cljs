e7e83d50077ca50c0278b9affc84dcc5f3f07a15
Toni Dahl <toni.dahl@solita.fi>
2023-03-22 18:01:24 +0200

    Lisää tuck-remoting client-side työkalu ws-yhteyden hallintaan

---
+++ src/cljs/harja/tyokalut/tuck_remoting.cljs
@@ -0,0 +1,33 @@
+(ns harja.tyokalut.tuck-remoting
+  "Tuck-remoting apurit"
+  (:require [taoensso.timbre :as log]
+            [tuck.core :as t]
+            [tuck.remoting :as t-remoting]))
+
+
+(defrecord YhdistaWS [yhteys-onnistui-fn])
+(defrecord KatkaiseWS[])
+
+(extend-protocol t/Event
+  YhdistaWS
+  (process-event [{:keys [yhteys-onnistui-fn]} app]
+    (if (:ws-yhteys app)
+      app
+      (assoc app :ws-yhteys
+                 (t-remoting/connect!
+                   (str "ws://" js/window.location.host "/_/ws?")
+                   app
+                   #(do
+                      (log/info "WebSocket-yhteys aloitettu")
+                      (yhteys-onnistui-fn))))))
+
+  KatkaiseWS
+  (process-event [_ app]
+    (log/info "KatkaiseWS: TODO")
+    ;; TODO: Tuck-remoting -kirjastossa on puute:
+    ;;        Yhteyden katkaisemista varten ei ole toimintoa, vaan mikäli yhteys katkeaa tuck-remoting
+    ;;        yrittää jatkuvasti luoda yhteyden uudelleen.
+    ;;        Tuck-remoting kirjaston connect! toimintoon pitää automaattinen reconnect tehdä optionaaliseksi
+    ;;        ja lisätä disconnect!-toiminto, joka katkaisee yhteyden ja estää automaattisen reconnecting
+    ;;        websocketin onclose eventin yhteydessä.
+    app))
