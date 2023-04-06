94afd4c42b3aee1c08986deae20a48315a308db0
Markus Vähäkangas <markus.vahakangas@solita.fi>
2023-04-06 07:46:25 +0300

    Muodosta bonusten haku uusiksi kustannusten seurannassa
    Jotta alihankintabonus merkitään tilaajan rahavaraukseksi

--- src/clj/harja/kyselyt/kustannusten_seuranta.sql
+++ src/clj/harja/kyselyt/kustannusten_seuranta.sql
@@ -420,26 +420,35 @@ UNION ALL
 -- Tässä ei ole siis mukana kustannusarvoitu_tyo tauluun tallennetut "Tilaajan rahavaraukset" jotka pohjimmiltaan on
 -- budjetoituja bonuksia ja jotka haetaan sitten erikseen toteutuneet_kustannukset taulusta, koska sinne siirretään kaikki toteutuneet
 -- kustannusarvoidut_työt
-SELECT 0                    AS budjetoitu_summa,
-       0                    AS budjetoitu_summa_indeksikorjattu,
-       SUM((SELECT korotettuna from erilliskustannuksen_indeksilaskenta(ek.pvm, ek.indeksin_nimi,
-                                                                        ek.rahasumma, ek.urakka , ek.tyyppi,
-                                                                        CASE
-                                                                            WHEN u.tyyppi = 'teiden-hoito'::urakkatyyppi THEN TRUE
-                                                                            ELSE FALSE
-                                                                            END))) AS toteutunut_summa,
+SELECT 0                        AS budjetoitu_summa,
+       0                        AS budjetoitu_summa_indeksikorjattu,
+       SUM((SELECT CASE WHEN ek.tyyppi = 'alihankintabonus' THEN (korotettuna * -1) ELSE korotettuna END korotettuna
+              FROM erilliskustannuksen_indeksilaskenta(ek.pvm, ek.indeksin_nimi,
+                 ek.rahasumma,ek.urakka , ek.tyyppi,
+CASE WHEN u.tyyppi = 'teiden-hoito'::urakkatyyppi THEN TRUE ELSE FALSE END))) AS toteutunut_summa,
        CASE
            WHEN ek.tyyppi = 'lupausbonus' THEN 'lupausbonus'
-           ELSE 'bonus' END  AS maksutyyppi,
-       'bonus'              AS toimenpideryhma,
-       MIN(ek.tyyppi)::TEXT AS tehtava_nimi,
-       'bonukset'           AS toimenpide,
-       MIN(ek.luotu)        AS luotu,
-       MIN(ek.pvm)::TEXT    AS ajankohta,
-       'bonus'              as toteutunut,
-       0                    AS jarjestys,
-       'bonukset'           AS paaryhma,
-       NOW()                AS indeksikorjaus_vahvistettu -- erilliskustannuksia ei indeksivahvisteta, joten ne on aina "true"
+           WHEN ek.tyyppi = 'alihankintabonus' THEN 'alihankintabonus'
+           ELSE 'bonus' END     AS maksutyyppi,
+       CASE
+           WHEN ek.tyyppi = 'alihankintabonus' THEN 'rahavaraus'
+           ELSE 'bonus' END     AS toimenpideryhma,
+       CASE
+           WHEN ek.tyyppi = 'alihankintabonus' THEN 'Tilaajan rahavaraus (T3)'
+           ELSE MIN(ek.tyyppi)::TEXT END AS tehtava_nimi,
+       CASE
+           WHEN ek.tyyppi = 'alihankintabonus' THEN 'MHU Ylläpito'
+           ELSE 'bonukset' END  AS toimenpide,
+       MIN(ek.luotu)            AS luotu,
+       MIN(ek.pvm)::TEXT        AS ajankohta,
+       CASE
+           WHEN ek.tyyppi = 'alihankintabonus' THEN 'toteutunut'
+           ELSE 'bonus' END     AS toteutunut,
+       0                        AS jarjestys,
+       CASE
+           WHEN ek.tyyppi = 'alihankintabonus' THEN 'rahavaraukset'
+           ELSE 'bonukset' END  AS paaryhma,
+       NOW()                    AS indeksikorjaus_vahvistettu -- erilliskustannuksia ei indeksivahvisteta, joten ne on aina "true"
 FROM erilliskustannus ek
      JOIN urakka u ON ek.urakka = u.id,
      sopimus s
