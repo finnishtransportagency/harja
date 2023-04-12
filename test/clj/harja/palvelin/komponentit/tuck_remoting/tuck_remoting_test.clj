e8f3303ddcee622b822932b58522bdd8a18bccca
Toni Dahl <toni.dahl@solita.fi>
2023-04-12 14:41:59 +0300

    Kokoa WS testaamisen työkalut yhteen nimiavaruuteen

--- test/clj/harja/palvelin/komponentit/tuck_remoting/tuck_remoting_test.clj
+++ test/clj/harja/palvelin/komponentit/tuck_remoting/tuck_remoting_test.clj
@@ -4,11 +4,11 @@
     [clojure.test :as t :refer [deftest is use-fixtures compose-fixtures testing]]
     [harja.testi :refer :all]
     [java-http-clj.websocket :as ws]
-    [taoensso.timbre :as log]
     [harja.oikea-http-palvelin-jarjestelma-fixture :as oikea-palvelin-fixture]
     [tuck.remoting :as tr]
     [harja.palvelin.komponentit.tuck-remoting :as sut]
-    [tuck.remoting.transit :as transit]))
+    [tuck.remoting.transit :as transit]
+    [harja.palvelin.komponentit.tuck-remoting.tyokalut :as tr-tyokalut]))
 
 (defrecord TestiEventti [])
 (tr/define-client-event TestiEventti)
@@ -18,69 +18,7 @@
                                             (sut/luo-tuck-remoting)
                                             [:http-palvelin :db])))
 
-(def ws-client-yhteydet-atom
-  "Kokoelma WS-client yhteysobjekteja"
-  (atom {}))
-
-(def ws-client-yhteys-vastaukset-atom
-  "Saadut vastaukset ws-yhteyden id:llä varustettuna"
-  (atom {}))
-
-(defn websocket-client [id]
-  (let [ws-url (str "ws://" "localhost:" portti "/_/ws?")]
-    (log/info "Yhdistetään WS: " ws-url)
-
-    (ws/build-websocket
-      ws-url
-      {:on-text (fn [ws vastaus last?]
-                  (log/info (str "WS-asiakas " id " sai vastauksen:" vastaus (transit/transit->clj vastaus)))
-                  (swap! ws-client-yhteys-vastaukset-atom assoc id (transit/transit->clj vastaus)))
-       :on-error (fn [ws throwable]
-                   (log/error (str "WS-asiakas " id " sai virheen: ") (.getMessage throwable)))}
-      {:headers {"oam_remote_user" "jvh"
-                 "oam_user_first_name" "Jalmari"
-                 "oam_user_last_name" "Järjestelmävastuuhenkilö"
-                 "oam_user_mail" "erkki@esimerkki.com"
-                 "oam_user_mobile" "1234567890"
-                 "oam_organization" "Liikennevirasto"
-                 "oam_groups" "Jarjestelmavastaava"}})))
-
-(defn luo-ws-yhteys! [id]
-  (when id
-    (swap! ws-client-yhteydet-atom assoc id (websocket-client id))))
-
-(defn ws-yhteys [id]
-  (get-in @ws-client-yhteydet-atom [id]))
-
-(defn sulje-ws-yhteys! [id]
-  (when (get-in @ws-client-yhteydet-atom [id])
-    (ws/close (get-in @ws-client-yhteydet-atom [id]))
-    (swap! ws-client-yhteydet-atom dissoc id)))
-
-(defn sulje-kaikki-ws-yhteydet! []
-  (when (seq @ws-client-yhteydet-atom)
-    (doseq [yhteys (vals @ws-client-yhteydet-atom)]
-      (ws/close yhteys))))
-
-(defn ws-vastaus [id]
-  (when id
-    (get-in @ws-client-yhteys-vastaukset-atom [id])))
-
-(defn siivoa-ws-vastaus! [id]
-  (swap! ws-client-yhteys-vastaukset-atom dissoc id))
-
-(defn websocket-fixture [testit]
-  (reset! ws-client-yhteys-vastaukset-atom {})
-
-  (testit)
-  ;; Jos ws-yhteyksiä on päällä testin jälkeen, varmista että ne suljetaan
-  (when (seq @ws-client-yhteydet-atom)
-    (doseq [yhteys (vals @ws-client-yhteydet-atom)]
-      (ws/close yhteys)))
-
-  (reset! ws-client-yhteydet-atom {}))
-
-(use-fixtures :each (compose-fixtures jarjestelma-fixture websocket-fixture))
+(use-fixtures :each (compose-fixtures jarjestelma-fixture tr-tyokalut/websocket-fixture))
 
 
 (deftest testaa-yhteys-hookit
@@ -93,12 +31,12 @@
                                     (fn [{::tr/keys [e! client-id] :as client}]
                                       (reset! yhdistetty-atom "katkaistu")))
         ;; Luo yhteys
-        _ (luo-ws-yhteys! :asiakas-1)]
+        _ (tr-tyokalut/luo-ws-yhteys! :asiakas-1)]
 
     ;; Testataan toimiiko yhteyden luominen ja hook
     (odota-ehdon-tayttymista #(= "yhdistetty" @yhdistetty-atom) "Tuck-remoting yhdistetty" 1000)
     ;; Kun yhteys on luotu, suljetaan se
-    (sulje-ws-yhteys! :asiakas-1)
+    (tr-tyokalut/sulje-ws-yhteys! :asiakas-1)
 
     ;; Testataan toimiiko yhteys poikki -hook
     (odota-ehdon-tayttymista #(= "katkaistu" @yhdistetty-atom) "Tuck-remoting yhteys katkaistu" 1000)
@@ -110,38 +48,39 @@
 (deftest testaa-tuck-remoting
   (testing "Testaa vastaako tuck-remoting ping-viestiin"
     ;; Luo yhteys
-    (luo-ws-yhteys! :asiakas-1)
+    (tr-tyokalut/luo-ws-yhteys! :asiakas-1)
 
     ;; Lähetä ping-viesti
-    (ws/send (ws-yhteys :asiakas-1) (transit/clj->transit {:tuck.remoting/event-type :ping}))
-    (odota-ehdon-tayttymista #(seq (ws-vastaus :asiakas-1)) "Tuck-remotingilta saatiin vastaus" 1000)
+    (ws/send (tr-tyokalut/ws-yhteys :asiakas-1) (transit/clj->transit {:tuck.remoting/event-type :ping}))
+    (odota-ehdon-tayttymista #(seq (tr-tyokalut/ws-vastaus :asiakas-1)) "Tuck-remotingilta saatiin vastaus" 1000)
 
     ;; Testataan vastaako tuck-remoting pong-viestillä
-    (is (= {:tuck.remoting/event-type :pong} (ws-vastaus :asiakas-1)))
+    (is (= {:tuck.remoting/event-type :pong} (tr-tyokalut/ws-vastaus :asiakas-1)))
 
     ;; Siivoa vastaus
-    (siivoa-ws-vastaus! :asiakas-1)
+    (tr-tyokalut/siivoa-ws-vastaus! :asiakas-1)
 
-    (sulje-kaikki-ws-yhteydet!))
+    (tr-tyokalut/sulje-kaikki-ws-yhteydet!))
 
   (testing "Lähetä kaikille clientille"
     ;; Testaa viestien lähetystä isommalle asiakasjoukolle
 
     (let [asiakkaat-lkm 20]
       (doseq [id (range asiakkaat-lkm)]
-        (luo-ws-yhteys! id))
+        (tr-tyokalut/luo-ws-yhteys! id))
 
       ;; Lähetä testi-eventti kaikille clientille
       (sut/laheta-kaikille! (:tuck-remoting jarjestelma) (->TestiEventti))
 
       (doseq [id (range asiakkaat-lkm)]
-        (odota-ehdon-tayttymista #(seq (ws-vastaus id)) (str "Tuck-remotingilta saatiin vastaus asiakkaalle " id) 2000))
+        (odota-ehdon-tayttymista #(seq (tr-tyokalut/ws-vastaus id)) (str "Tuck-remotingilta saatiin vastaus asiakkaalle " id) 2000))
 
       (doseq [id (range asiakkaat-lkm)]
-        (is (= "harja.palvelin.komponentit.tuck-remoting-test.TestiEventti" (::tr/event-type (ws-vastaus id)))))
+        (is (= "harja.palvelin.komponentit.tuck-remoting.tuck-remoting-test.TestiEventti"
+              (::tr/event-type (tr-tyokalut/ws-vastaus id)))))
 
       ;; Siivoa vastaukset
       (doseq [id (range asiakkaat-lkm)]
-        (siivoa-ws-vastaus! id))
+        (tr-tyokalut/siivoa-ws-vastaus! id))
 
-      (sulje-kaikki-ws-yhteydet!))))
+      (tr-tyokalut/sulje-kaikki-ws-yhteydet!))))
--- test/clj/harja/palvelin/komponentit/tuck_remoting/tyokalut.clj
+++ test/clj/harja/palvelin/komponentit/tuck_remoting/tyokalut.clj
@@ -1,2 +1,92 @@
 (ns harja.palvelin.komponentit.tuck-remoting.tyokalut
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer [deftest is use-fixtures compose-fixtures testing]]
+            [clojure.string :as str]
+            [java-http-clj.websocket :as ws]
+            [taoensso.timbre :as log]
+            [harja.testi :refer [portti]]
+            [tuck.remoting :as tr]
+            [tuck.remoting.transit :as transit]))
+
+(def event-id (atom 0))
+
+(def ws-client-yhteydet-atom
+  "Kokoelma WS-client yhteysobjekteja"
+  (atom {}))
+
+(def ws-client-yhteys-vastaukset-atom
+  "Saadut vastaukset ws-yhteyden id:llä varustettuna"
+  (atom {}))
+
+(defn websocket-client [id]
+  (let [ws-url (str "ws://" "localhost:" portti "/_/ws?")]
+    (log/info "Yhdistetään WS: " ws-url)
+
+    (ws/build-websocket
+      ws-url
+      {:on-text (fn [ws vastaus last?]
+                  (log/info (str "WS-asiakas " id " sai vastauksen:" vastaus (transit/transit->clj vastaus)))
+                  (swap! ws-client-yhteys-vastaukset-atom assoc id (transit/transit->clj vastaus)))
+       :on-error (fn [ws throwable]
+                   (log/error (str "WS-asiakas " id " sai virheen: ") (.getMessage throwable)))}
+      {:headers {"oam_remote_user" "jvh"
+                 "oam_user_first_name" "Jalmari"
+                 "oam_user_last_name" "Järjestelmävastuuhenkilö"
+                 "oam_user_mail" "erkki@esimerkki.com"
+                 "oam_user_mobile" "1234567890"
+                 "oam_organization" "Liikennevirasto"
+                 "oam_groups" "Jarjestelmavastaava"}})))
+
+(defn luo-ws-yhteys! [id]
+  (when id
+    (swap! ws-client-yhteydet-atom assoc id (websocket-client id))))
+
+(defn ws-yhteys [id]
+  (get-in @ws-client-yhteydet-atom [id]))
+
+(defn sulje-ws-yhteys! [id]
+  (when (get-in @ws-client-yhteydet-atom [id])
+    (ws/close (get-in @ws-client-yhteydet-atom [id]))
+    (swap! ws-client-yhteydet-atom dissoc id)))
+
+(defn sulje-kaikki-ws-yhteydet! []
+  (when (seq @ws-client-yhteydet-atom)
+    (doseq [yhteys (vals @ws-client-yhteydet-atom)]
+      (ws/close yhteys))))
+
+(defn ws-vastaus [id]
+  (when id
+    (get-in @ws-client-yhteys-vastaukset-atom [id])))
+
+(defn siivoa-ws-vastaus! [id]
+  (swap! ws-client-yhteys-vastaukset-atom dissoc id))
+
+(defn websocket-fixture
+  "Fixture tuck-remoting ws-testien yhteyteen. Varmistaa, että testissä käytetyt yhteydet
+  varmasti suljateen, ja että vastauksien väliaikaiseen säilömiseen käytetty atom siivotaan
+  testien välillä."
+  [testit]
+  (reset! ws-client-yhteys-vastaukset-atom {})
+
+  (testit)
+  ;; Jos ws-yhteyksiä on päällä testin jälkeen, varmista että ne suljetaan
+  (when (seq @ws-client-yhteydet-atom)
+    (doseq [yhteys (vals @ws-client-yhteydet-atom)]
+      (ws/close yhteys)))
+
+  (reset! ws-client-yhteydet-atom {}))
+
+
+
+(defn eventin-konstruktori->tyyppi [tuck-event]
+  (str/replace (.getName (type tuck-event)) \_ \-))
+
+(defn asiakas-e!
+  "Lähettää tuck-remoten server-eventin, kuten selaimessa toimiva CLJS WS-client sen lähettäisi"
+  [ws-yhteys]
+  (fn [tuck-server-event]
+    (ws/send
+      ws-yhteys
+      (transit/clj->transit
+        {::tr/event-id (swap! event-id inc)
+         ::tr/event-type (eventin-konstruktori->tyyppi tuck-server-event)
+         ::tr/event-args (into {} tuck-server-event)}))))
