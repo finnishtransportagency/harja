b4dfd0a1365aec763f6743f16f56b8496d74d850
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2023-05-05 13:41:21 +0300

    Lisää testi, joka varmistaa toimenpiteiden lukumäärän ja avaimet tasoittain

--- test/clj/harja/palvelin/palvelut/toimenpidekoodit_test.clj
+++ test/clj/harja/palvelin/palvelut/toimenpidekoodit_test.clj
@@ -5,7 +5,8 @@
             [harja.palvelin.palvelut.toimenpidekoodit :refer :all]
             [harja.testi :refer :all]
             [com.stuartsierra.component :as component]
-            [harja.kyselyt.konversio :as konv]))
+            [harja.kyselyt.konversio :as konv]
+            [clojure.set :as set]))
 
 
 (defn jarjestelma-fixture [testit]
@@ -27,9 +28,52 @@
                       jarjestelma-fixture
                       urakkatieto-fixture))
 
-(deftest toimenpidekoodien-haku-toimii
-  (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
-                                :hae-toimenpidekoodit +kayttaja-jvh+)]
+;; Varmuuden vuoksi yhdeksi ototetuksi arvoksi vielä tämä kovakoodattuna,
+;; ettei maskaudu sellainen bugi, joka johtuisi ao. SQL:ään kohdistuvasta muutoksesta.
+(def odotetut-tasojen-lkmt
+  {1 6
+   2 25
+   3 50
+   4 360})
 
-    (is (> (count vastaus) 0))
-    (is (not (nil? vastaus)))))
+(defn- tason-lukumaarat-samat [palvelusta kannasta kovakoodattu taso]
+  (= (count (get palvelusta taso)) kannasta (get kovakoodattu taso)))
+
+(deftest toimenpidekoodien-haku-toimii
+  (let [[[_ taso1-lkm] [_ taso2-lkm] [_ taso3-lkm] [_ taso4-lkm]] (q "select taso, count(*) from toimenpidekoodi\nWHERE piilota IS NOT TRUE\nGROUP BY taso ORDER BY taso")
+        koodit-tasoittain (group-by
+                            :taso
+                            (kutsu-palvelua (:http-palvelin jarjestelma)
+                                           :hae-toimenpidekoodit +kayttaja-jvh+))
+        odotetut-avaimet-tasot-1-2-3 #{:id
+                                       :nimi
+                                       :koodi
+                                       :emo
+                                       :taso
+                                       :luoja
+                                       :poistettu}
+        odotetut-avaimet-taso-4 (set/union odotetut-avaimet-tasot-1-2-3
+                                           #{:voimassaolon-alkuvuosi
+                                             :voimassaolon-loppuvuosi
+                                             :yksikko
+                                             :jarjestys
+                                             :hinnoittelu
+                                             :tehtavaryhma})]
+
+    (is (tason-lukumaarat-samat koodit-tasoittain taso1-lkm odotetut-tasojen-lkmt 1) "Tason 1 koodien lukumäärä")
+    (is (tason-lukumaarat-samat koodit-tasoittain taso2-lkm odotetut-tasojen-lkmt 2) "Tason 2 koodien lukumäärä")
+    (is (tason-lukumaarat-samat koodit-tasoittain taso3-lkm odotetut-tasojen-lkmt 3) "Tason 3 koodien lukumäärä")
+    (is (tason-lukumaarat-samat koodit-tasoittain taso4-lkm odotetut-tasojen-lkmt 4) "Tason 4 koodien lukumäärä")
+
+    (is (every?
+          #(contains? (first (get koodit-tasoittain 1)) %)
+          odotetut-avaimet-tasot-1-2-3) "Löytyy oikeat avaimet tasolta 1")
+    (is (every?
+          #(contains? (first (get koodit-tasoittain 2)) %)
+          odotetut-avaimet-tasot-1-2-3) "Löytyy oikeat avaimet tasolta 2")
+    (is (every?
+          #(contains? (first (get koodit-tasoittain 3)) %)
+          odotetut-avaimet-tasot-1-2-3) "Löytyy oikeat avaimet tasolta 3")
+    (is (every?
+          #(contains? (first (get koodit-tasoittain 4)) %)
+          odotetut-avaimet-taso-4) "Löytyy oikeat avaimet tasolta 4")))
