24656e0d018f1ea9499d3c6e408722a7708ef319
Samuli Taskila <samuli.taskila@solita.fi>
2023-05-10 15:20:11 +0300

    Näkymän grideille omat tiedostot

--- src/cljs/harja/views/urakka/tyomaapaivakirja_nakyma.cljs
+++ src/cljs/harja/views/urakka/tyomaapaivakirja_nakyma.cljs
@@ -5,7 +5,9 @@
             [harja.ui.kentat :as kentat]
             [harja.ui.ikonit :as ikonit]
             [harja.ui.napit :as napit]
-            [harja.pvm :as pvm]))
+            [harja.pvm :as pvm]
+            [harja.views.urakka.tyomaapaivakirja-vahvuus :as vahvuus]
+            [harja.views.urakka.tyomaapaivakirja-saatiedot :as saatiedot]))
 
 (def toimituksen-tila [{:class "ok" :selitys "Ok"}
                        {:class "myohassa" :selitys "Myöhässä"}
@@ -42,118 +44,9 @@
       [:hr]
 
       ;; Päivystäjät, Työnjohtajat
-      [:div {:style {:padding-top "20px"}}
-
-       [:h2 "Vahvuus"]
-       [:div {:class "vahvuus-gridit"}
-        [:div
-         [:h3 "Päivystäjät"]
-
-         [grid/grid {:tyhja "Ei Tietoja."
-                     :tunniste :id
-                     :voi-kumota? false
-                     :piilota-border? true
-                     :piilota-toiminnot? true
-                     :jarjesta :id}
-
-          [{:otsikko "Aikaväli"
-            :otsikkorivi-luokka "nakyma-otsikko"
-            :tyyppi :string
-            :nimi :alkupvm
-            :leveys 0.25}
-
-           {:otsikko "Nimi"
-            :otsikkorivi-luokka "nakyma-otsikko"
-            :tyyppi :string
-            :nimi :nimi
-            :solun-luokka (fn [_ _]
-                            "nakyma-valkoinen-solu")
-            :leveys 1}]
-         ;; Gridille jokin atomi 
-          [{:id 0 :alkupvm "00:00 - 00:00" :nimi "Test"}]]]
-
-
-        [:div
-         [:h3 "Työnjohtajat"]
-
-         [grid/grid {:tyhja "Ei Tietoja."
-                     :tunniste :id
-                     :voi-kumota? false
-                     :piilota-border? true
-                     :piilota-toiminnot? true
-                     :jarjesta :id}
-
-          [{:otsikko "Aikaväli"
-            :otsikkorivi-luokka "nakyma-otsikko"
-            :tyyppi :string
-            :nimi :alkupvm
-            :leveys 0.25}
-
-           {:otsikko "Nimi"
-            :otsikkorivi-luokka "nakyma-otsikko"
-            :tyyppi :string
-            :nimi :nimi
-            :solun-luokka (fn [_ _]
-                            "nakyma-valkoinen-solu")
-            :leveys 1}]
-         ;; Gridille jokin atomi 
-          [{:id 0 :alkupvm "00:00 - 00:00" :nimi "Test"}]]]]]
-
-
+      (vahvuus/vahvuus-grid)
       ;; Sääasemien tiedot
-      [:div {:style {:padding-top "20px"}}
-
-       [:h2 "Sääasemien tiedot"]
-       [:div {:class "vahvuus-gridit"}
-        [:div
-         [:h3 "Tie 4, Oulu, Osoite"]
-
-         [grid/grid {:tyhja "Ei Tietoja."
-                     :tunniste :id
-                     :voi-kumota? false
-                     :piilota-border? true
-                     :piilota-toiminnot? true
-                     :jarjesta :id}
-
-          [{:otsikko "Aikaväli"
-            :otsikkorivi-luokka "nakyma-otsikko"
-            :tyyppi :string
-            :nimi :alkupvm
-            :leveys 0.25}
-
-           {:otsikko "Nimi"
-            :otsikkorivi-luokka "nakyma-otsikko"
-            :tyyppi :string
-            :nimi :nimi
-            :solun-luokka (fn [_ _]
-                            "nakyma-valkoinen-solu")
-            :leveys 1}]
-         ;; Gridille jokin atomi 
-          [{:id 0 :alkupvm "00:00 - 00:00" :nimi "Test"}]]]
-
-
-        [:div
-         [:h3 "Tie 4, Oulu, Osoite"]
-
-         [grid/grid {:tyhja "Ei Tietoja."
-                     :tunniste :id
-                     :voi-kumota? false
-                     :piilota-border? true
-                     :piilota-toiminnot? true
-                     :jarjesta :id}
-
-          [{:otsikko "Aikaväli"
-            :otsikkorivi-luokka "nakyma-otsikko"
-            :tyyppi :string
-            :nimi :alkupvm
-            :leveys 0.25}
-
-           {:otsikko "Nimi"
-            :otsikkorivi-luokka "nakyma-otsikko"
-            :tyyppi :string
-            :nimi :nimi
-            :solun-luokka (fn [_ _]
-                            "nakyma-valkoinen-solu")
-            :leveys 1}]
-         ;; Gridille jokin atomi 
-          [{:id 0 :alkupvm "00:00 - 00:00" :nimi "Test"}]]]]]]]))
+      (saatiedot/saatiedot-grid)
+      
+      
+      ]]))
---
+++ src/cljs/harja/views/urakka/tyomaapaivakirja_saatiedot.cljs
@@ -0,0 +1,146 @@
+(ns harja.views.urakka.tyomaapaivakirja-saatiedot
+  "Työmaapäiväkirja näkymän Sääasemien tieto- gridit"
+  (:require [harja.tiedot.tyomaapaivakirja :as tiedot]
+            [harja.ui.grid :as grid]
+            [harja.ui.kentat :as kentat]
+            [harja.ui.ikonit :as ikonit]
+            [harja.ui.napit :as napit]
+            [harja.pvm :as pvm]))
+
+(defn saatiedot-grid []
+  [:div {:style {:padding-top "10px"}}
+   [:h2 "Sääasemien tiedot"]
+   [:div {:class "flex-gridit"}
+    [:div
+     [:h3 {:class "gridin-otsikko"} "Tie 4, Oulu, Osoite"]
+
+     [grid/grid {:tyhja "Ei Tietoja."
+                 :tunniste :id
+                 :voi-kumota? false
+                 :piilota-border? true
+                 :piilota-toiminnot? true
+                 :jarjesta :id}
+
+      [{:otsikko "Klo"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :alkupvm
+        :leveys 1}
+
+       {:otsikko "Ilma"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}
+
+       {:otsikko "Ilma"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}
+
+       {:otsikko "Tie"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}
+
+       {:otsikko "S-olom"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}
+
+       {:otsikko "K-tuuli"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}
+
+       {:otsikko "K-Sum"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}]
+      ;; TODO 
+      ;; Gridille jokin atomi 
+      [{:id 0 :alkupvm "00:00 - 00:00" :nimi "Test"}]]]
+
+    [:div
+     [:h3 {:class "gridin-otsikko"} "Tie 4, Oulu, Osoite"]
+
+     [grid/grid {:tyhja "Ei Tietoja."
+                 :tunniste :id
+                 :voi-kumota? false
+                 :piilota-border? true
+                 :piilota-toiminnot? true
+                 :jarjesta :id}
+
+      [{:otsikko "Klo"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :alkupvm
+        :leveys 1}
+
+       {:otsikko "Ilma"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}
+
+       {:otsikko "Ilma"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}
+
+       {:otsikko "Tie"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}
+
+       {:otsikko "S-olom"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}
+
+       {:otsikko "K-tuuli"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}
+
+       {:otsikko "K-Sum"
+        :otsikkorivi-luokka "nakyma-otsikko"
+        :tyyppi :string
+        :nimi :nimi
+        :solun-luokka (fn [_ _]
+                        "nakyma-valkoinen-solu")
+        :leveys 1}]
+      ;; TODO 
+      ;; Gridille jokin atomi 
+      [{:id 0 :alkupvm "00:00 - 00:00" :nimi "Test"}]]]]])
