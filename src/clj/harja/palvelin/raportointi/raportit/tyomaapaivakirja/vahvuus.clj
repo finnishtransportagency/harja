5c421d98e7b8c01ba03878d7059d0c3ea11aea92
Markus Vähäkangas <markus.vahakangas@solita.fi>
2023-05-25 07:40:18 +0300

    Lisää vahvuus-taulukkoon oikeat tiedot päivystäjistä ja työnjohtajista

--- src/clj/harja/palvelin/raportointi/raportit/tyomaapaivakirja/vahvuus.clj
+++ src/clj/harja/palvelin/raportointi/raportit/tyomaapaivakirja/vahvuus.clj
@@ -1,45 +1,43 @@
 (ns harja.palvelin.raportointi.raportit.tyomaapaivakirja.vahvuus
   "Työmaapäiväkirja -näkymän urakan henkilöstö gridit"
   (:require
-   [harja.palvelin.raportointi.raportit.yleinen :as yleinen :refer [rivi]]
-   [harja.palvelin.raportointi.raportit.tyomaapaivakirja.yhteiset :as yhteiset]))
+    [harja.palvelin.raportointi.raportit.yleinen :as yleinen :refer [rivi]]
+    [harja.palvelin.raportointi.raportit.tyomaapaivakirja.yhteiset :as yhteiset]))
 
 (defn- vahvuus-rivi [aikavali nimi]
   (rivi
     [:varillinen-teksti {:arvo aikavali}]
     [:varillinen-teksti {:arvo nimi}]))
 
-(defn vahvuus-taulukkojen-parametrit []
+(defn vahvuus-taulukkojen-parametrit [paivystajat tyonjohtajat]
   (let [taulukon-optiot {:tyhja "Ei Tietoja."
                          :piilota-border? false
                          :viimeinen-rivi-yhteenveto? false}
         taulukon-otsikot (rivi
-                           {:otsikko "Aikaväli" 
-                            :otsikkorivi-luokka "nakyma-otsikko" 
-                            :sarakkeen-luokka "vaalen-tumma-tausta" 
-                            :leveys 0.17
+                           {:otsikko "Aikaväli"
+                            :otsikkorivi-luokka "nakyma-otsikko"
+                            :sarakkeen-luokka "vaalen-tumma-tausta"
+                            :leveys 0.27
                             :tyyppi :varillinen-teksti}
-                           {:otsikko "Nimi" 
-                            :otsikkorivi-luokka "nakyma-otsikko" 
+                           {:otsikko "Nimi"
+                            :otsikkorivi-luokka "nakyma-otsikko"
                             :sarakkeen-luokka "nakyma-valkoinen-solu"
                             :leveys 1
                             :tyyppi :varillinen-teksti})
         ;; Vasemman taulukon rivit (Päivystäjät)
         rivit-v (into []
-                  [(vahvuus-rivi "0:00" "Päivi Päivystäjä")
-                   (vahvuus-rivi "0:01" "Petri Päivystäjä")
-                   (vahvuus-rivi "0:02" "Paula Päivystäjä")])
-        
+                  (mapv #(vahvuus-rivi (str (harja.pvm/aika (:aloitus %)) " - " (harja.pvm/aika (:lopetus %))) (:nimi %)) paivystajat))
+
         ;; Työnjohtajat
         rivit-o (into []
-                  [(vahvuus-rivi "0:00" "Tauno Työnjohtaja")
-                   (vahvuus-rivi "0:01" "Tarja Työnjohtaja")])
+                  (mapv #(vahvuus-rivi (str (harja.pvm/aika (:aloitus %)) " - " (harja.pvm/aika (:lopetus %))) (:nimi %)) tyonjohtajat))
 
         taulukko-1 {:otsikko-vasen "Päivystäjät" :optiot-vasen taulukon-optiot :otsikot-vasen taulukon-otsikot :rivit-vasen rivit-v}
         taulukko-2 {:otsikko-oikea "Työnjohtajat" :optiot-oikea taulukon-optiot :otsikot-oikea taulukon-otsikot :rivit-oikea rivit-o}]
     [taulukko-1 taulukko-2]))
 
-(defn vahvuus-taulukot []
-  (into ()
-    [[:gridit-vastakkain (first (vahvuus-taulukkojen-parametrit)) (second (vahvuus-taulukkojen-parametrit))]
-     (yhteiset/osion-otsikko "Vahvuus")]))
+(defn vahvuus-taulukot [paivystajat tyonjohtajat]
+  (let [taulukot (vahvuus-taulukkojen-parametrit paivystajat tyonjohtajat)]
+    (into ()
+      [[:gridit-vastakkain (first taulukot) (second taulukot)]
+       (yhteiset/osion-otsikko "Vahvuus")])))
