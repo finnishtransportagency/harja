9d3c88665bb3d076fbc7ca1c70c5bdac1c997d27
Markus Vähäkangas <markus.vahakangas@solita.fi>
2023-05-29 15:18:44 +0300

    Dynaamisen työmaapäiväkirjan ensimmäisen version hahmottelua

--- src/clj/harja/palvelin/palvelut/tyomaapaivakirja.clj
+++ src/clj/harja/palvelin/palvelut/tyomaapaivakirja.clj
@@ -1,12 +1,18 @@
 (ns harja.palvelin.palvelut.tyomaapaivakirja
   (:require [com.stuartsierra.component :as component]
             [harja.palvelin.komponentit.http-palvelin :refer [julkaise-palvelu poista-palvelut]]
-            [harja.kyselyt.tyomaapaivakirja :as q]
+            [harja.kyselyt.tyomaapaivakirja :as tyomaapaivakirja-kyselyt]
+            [harja.kyselyt.konversio :as konversio]
             [harja.domain.oikeudet :as oikeudet]))
 
-(defn tyomaapaivakirja-hae [db user tiedot]
+(defn hae-tyomaapaivakirjat [db user tiedot]
   (oikeudet/vaadi-lukuoikeus oikeudet/raportit-tyomaapaivakirja user (:urakka-id tiedot))
-  (into [] (q/hae-tiedot db)))
+  (let [_ (println "hae-tyomaapaivakirjat :: tiedot" (pr-str tiedot))
+        paivakirjat (tyomaapaivakirja-kyselyt/hae-paivakirjalistaus db {:urakka-id (:urakka-id tiedot)
+                                                                        :alkuaika (konversio/sql-date (:alkuaika tiedot))
+                                                                         :loppuaika (konversio/sql-date (:loppuaika tiedot))})]
+    paivakirjat
+    ))
 
 (defrecord Tyomaapaivakirja []
   component/Lifecycle
@@ -14,7 +20,7 @@
     (julkaise-palvelu http-palvelin
       :tyomaapaivakirja-hae
       (fn [user tiedot]
-        (tyomaapaivakirja-hae db user tiedot)))
+        (hae-tyomaapaivakirjat db user tiedot)))
     this)
 
   (stop [{:keys [http-palvelin] :as this}]
