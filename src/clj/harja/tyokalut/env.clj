d5559dd2e775d95559fd9ff2da2f1342afac90e8
Toni Dahl <toni.dahl@solita.fi>
2023-06-20 08:53:21 +0300

    Lisää ohituskaistan headereille ENV-muuttuja ja json-parsinta

--- src/clj/harja/tyokalut/env.clj
+++ src/clj/harja/tyokalut/env.clj
@@ -1,15 +1,18 @@
 (ns harja.tyokalut.env
-  (:require [clojure.string :as clj-str]))
+  (:require [clojure.string :as clj-str]
+            [cheshire.core :as cheshire]))
 
 (defn env
   ([ymparisto-muuttuja] (env ymparisto-muuttuja nil))
-  ([ymparisto-muuttuja default-arvo]
+  ([ymparisto-muuttuja default-arvo] (env ymparisto-muuttuja default-arvo false))
+  ([ymparisto-muuttuja default-arvo json?]
    {:pre [(string? ymparisto-muuttuja)]}
    (let [ym (clj-str/trim (str (System/getenv ymparisto-muuttuja)))]
      (cond
        (and (= "" ym)
             (some? default-arvo)) default-arvo
        (= "" ym) nil
+       json? (cheshire/parse-string ym true)
        (= "true" (clj-str/lower-case ym)) true
        (= "false" (clj-str/lower-case ym)) false
        (re-find #"^-?\d+$" ym) (Long/parseLong ym)
