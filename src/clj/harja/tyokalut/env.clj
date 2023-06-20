2da5265a332b7ca6e14507d93d1232cbb3b13387
Toni Dahl <toni.dahl@solita.fi>
2023-06-20 15:26:16 +0300

    Korjaile json-muotoisten env-parametrien parsintaa

--- src/clj/harja/tyokalut/env.clj
+++ src/clj/harja/tyokalut/env.clj
@@ -4,17 +4,26 @@
 
 (defn env
   ([ymparisto-muuttuja] (env ymparisto-muuttuja nil))
-  ([ymparisto-muuttuja default-arvo] (env ymparisto-muuttuja default-arvo false))
-  ([ymparisto-muuttuja default-arvo json?]
+  ([ymparisto-muuttuja default-arvo]
    {:pre [(string? ymparisto-muuttuja)]}
    (let [ym (clj-str/trim (str (System/getenv ymparisto-muuttuja)))]
      (cond
        (and (= "" ym)
             (some? default-arvo)) default-arvo
        (= "" ym) nil
-       json? (cheshire/parse-string ym true)
        (= "true" (clj-str/lower-case ym)) true
        (= "false" (clj-str/lower-case ym)) false
        (re-find #"^-?\d+$" ym) (Long/parseLong ym)
        (re-find #"^-?\d+(\.\d+)?$" ym) (Double/parseDouble ym)
        :default ym))))
+
+(defn env-json
+  ([ymparisto-muuttuja] (env-json ymparisto-muuttuja nil))
+  ([ymparisto-muuttuja default-arvo] (env-json ymparisto-muuttuja default-arvo nil))
+  ([ymparisto-muuttuja default-arvo key-fn]
+   {:pre [(string? ymparisto-muuttuja)]}
+   (let [ym (clj-str/trim (str (System/getenv ymparisto-muuttuja)))]
+     (if
+       (and (= "" ym)
+         (some? default-arvo)) default-arvo
+       (cheshire/decode ym key-fn)))))
