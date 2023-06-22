68be64c51b75d77330bdcb9a056ebe81b3a10907
Samu Mikkonen <samu.mikkonen@solita.fi>
2023-06-22 15:13:03 +0300

    Muokkaa koodistot toteutusta että se ei kaada suoritusta

--- src/clj/harja/kyselyt/koodistot.clj
+++ src/clj/harja/kyselyt/koodistot.clj
@@ -1,7 +1,8 @@
 (ns harja.kyselyt.koodistot
   "Koodistoihin liittyvät tietokantakyselyt
   (hetkellä vain koodisto konversiot käytetty integraatioissa)"
-  (:require [jeesql.core :refer [defqueries]]))
+  (:require [jeesql.core :refer [defqueries]]
+            [taoensso.timbre :as log]))
 
 (defqueries "harja/kyselyt/koodistot.sql"
             {:positional? false})
@@ -9,7 +10,7 @@
 (defn konversio-db [db koodisto-id lahde]
   (let [rivi (first (hae-koodi-harja-koodin-perusteella db {:koodisto_id koodisto-id :lahde (str lahde)}))
         tulos (:tulos rivi)]
-    (assert (some? tulos) (str "Harja koodi '" lahde "' ei voi konvertoida taulukossa " koodisto-id))
+    (when (empty? tulos) (log/error (str "Harja koodi '" lahde "' ei voi konvertoida taulukossa " koodisto-id ". Vaatii uuden koodin lisäämistä kantaan tai selvittelyä Velholaisten kanssa!")))
     tulos))
 
 (def konversio (memoize konversio-db))
