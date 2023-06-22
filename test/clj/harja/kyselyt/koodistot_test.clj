37fec821d208d52ea49c141c19526914fed2198e
Samu Mikkonen <samu.mikkonen@solita.fi>
2023-06-22 15:24:08 +0300

    Poista testi jota ei enää tarvita

--- test/clj/harja/kyselyt/koodistot_test.clj
+++ test/clj/harja/kyselyt/koodistot_test.clj
@@ -8,7 +8,3 @@
 
 (deftest palauta-koodi-kun-loytyy
   (is (= "tienrakennetoimenpide/trtp28" (koodistot/konversio (:db jarjestelma) "v/at" 32))))
-
-(deftest palauta-nil-kun-ei-loyty
-  (is (thrown-with-msg? AssertionError #"Harja koodi '320' ei voi konvertoida taulukossa v/at"
-                        (koodistot/konversio (:db jarjestelma) "v/at" 320))))
