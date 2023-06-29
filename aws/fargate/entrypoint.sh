af92cb5147435276d4de411193b671ae4618427c
Toni Dahl <toni.dahl@solita.fi>
2023-06-29 12:49:14 +0300

    Hae Harja kontin depsut s3-ämpäristä kontin käynnistyksen yhteydessä

--- aws/fargate/entrypoint.sh
+++ aws/fargate/entrypoint.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-# TODO: Lataa kontin tarvitsemat tiedostot S3 bucketista aws cli s3 työkalulla
+# Ladataan Harjan tarvitsemat staattiset tiedostot ja ylimääräiset kirjastot
 
-# Luodaan java-optioille array, johon otetaan mukaan lisäoptioita mikäli ne on määritelty
+## Sonic kirjaston jar-tiedostot
+aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "${HARJA_CONFIG_S3_SONIC_HAKEMISTO}/sonic-client.jar" sonic-client.jar
+aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "${HARJA_CONFIG_S3_SONIC_HAKEMISTO}/sonic-xmessage.jar" sonic-xmessage.jar
+aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "${HARJA_CONFIG_S3_SONIC_HAKEMISTO}/sonic-crypto.jar" sonic-crypto.jar
+
+## Uusin kaista-aineisto (csv)
+aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "$HARJA_CONFIG_S3_KAISTAT_TIEDOSTOPOLKU" uusin_tr_tieto.csv
+
+
+# Valmistellaan java-optiot ja käynnistetään Harja app
+
+## Luodaan java-optioille array, johon otetaan mukaan lisäoptioita mikäli ne on määritelty
 cmd_opts=()
 
 if [[ -n "$HARJA_JAVA_AGENT" ]]; then
@@ -15,5 +26,5 @@ fi
 
 cmd_opts+=("-cp" "$HARJA_LIBS":harja.jar harja.palvelin.main)
 
-# Aja java-komento
+## Aja java-komento
 java "${cmd_opts[@]}"
