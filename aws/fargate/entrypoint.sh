cfb6289f2cef70e6744176f9faf64d5ffd645905
Toni Dahl <toni.dahl@solita.fi>
2023-06-29 14:30:07 +0300

    Lisää lokitusta entrypointtiin

--- aws/fargate/entrypoint.sh
+++ aws/fargate/entrypoint.sh
@@ -4,13 +4,23 @@ set -x
 # Ladataan Harjan tarvitsemat staattiset tiedostot ja ylimääräiset kirjastot
 
 ## Sonic kirjaston jar-tiedostot
+echo "Ladataan sonic-client.jar..."
 aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "${HARJA_CONFIG_S3_SONIC_HAKEMISTO}/sonic-client.jar" sonic-client.jar
+echo "DONE"
+
+echo "Ladataan /sonic-xmessage.jar..."
 aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "${HARJA_CONFIG_S3_SONIC_HAKEMISTO}/sonic-xmessage.jar" sonic-xmessage.jar
+echo "DONE"
+
+echo "Ladataan sonic-crypto.jar..."
 aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "${HARJA_CONFIG_S3_SONIC_HAKEMISTO}/sonic-crypto.jar" sonic-crypto.jar
+echo "DONE"
 
 ## Uusin kaista-aineisto (csv)
-aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "$HARJA_CONFIG_S3_KAISTAT_TIEDOSTOPOLKU" uusin_tr_tieto.csv
 
+echo "Ladataan uusin kaista-aineisto..."
+aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "$HARJA_CONFIG_S3_KAISTAT_TIEDOSTOPOLKU" uusin_tr_tieto.csv
+echo "DONE"
 
 # Valmistellaan java-optiot ja käynnistetään Harja app
 
@@ -27,5 +37,6 @@ fi
 
 cmd_opts+=("-cp" "$HARJA_LIBS":harja.jar harja.palvelin.main)
 
+echo "Käynnistetään harja.jar"
 ## Aja java-komento
 java "${cmd_opts[@]}"
