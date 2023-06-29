f420c4251106cfdab143daac5129848277ec568b
Toni Dahl <toni.dahl@solita.fi>
2023-06-29 15:00:49 +0300

    Varmista, että ladatut riippuvuudet todella löytyvät kontista

--- aws/fargate/entrypoint.sh
+++ aws/fargate/entrypoint.sh
@@ -2,6 +2,13 @@
 
 # set -x
 
+function tarkasta_riippuvuus {
+    if [[ ! -f "$1" ]] ; then
+        echo "Tiedostoa $1 ei löytynyt. Lopetetaan käynnistys..."
+        exit 1
+    fi
+}
+
 # Ladataan Harjan tarvitsemat staattiset tiedostot ja ylimääräiset kirjastot
 
 ## Sonic kirjaston jar-tiedostot
@@ -9,19 +16,27 @@ echo "Ladataan sonic-client.jar..."
 aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "${HARJA_CONFIG_S3_SONIC_HAKEMISTO}/sonic-client.jar" sonic-client.jar
 echo "DONE"
 
+tarkasta_riippuvuus sonic-client.jar
+
 echo "Ladataan sonic-xmessage.jar..."
 aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "${HARJA_CONFIG_S3_SONIC_HAKEMISTO}/sonic-xmessage.jar" sonic-xmessage.jar
 echo "DONE"
 
+tarkasta_riippuvuus sonic-xmessage.jar
+
 echo "Ladataan sonic-crypto.jar..."
 aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "${HARJA_CONFIG_S3_SONIC_HAKEMISTO}/sonic-crypto.jar" sonic-crypto.jar
 echo "DONE"
+
+tarkasta_riippuvuus sonic-crypto.jar
 
 ## Uusin kaista-aineisto (csv)
 
 echo "Ladataan uusin kaista-aineisto..."
 aws s3api get-object --bucket "$HARJA_CONFIG_S3_BUCKET" --key "$HARJA_CONFIG_S3_KAISTAT_TIEDOSTOPOLKU" uusin_tr_tieto.csv
 echo "DONE"
+
+tarkasta_riippuvuus uusin_tr_tieto.csv
 
 # Valmistellaan java-optiot ja käynnistetään Harja app
 
