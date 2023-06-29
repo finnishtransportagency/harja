5be7c62bbbcd1fa283219a322122b22a9de5fa88
Samuli Taskila <samuli.taskila@solita.fi>
2023-06-29 13:06:41 +0300

    Korjaa viestin luku avain parametri

--- src/clj/harja/palvelin/integraatiot/tloik/sanomat/ilmoitus_sanoma.clj
+++ src/clj/harja/palvelin/integraatiot/tloik/sanomat/ilmoitus_sanoma.clj
@@ -41,9 +41,9 @@
      :sukunimi (z/xml1-> lahettaja :sukunimi z/text)
      :tyopuhelin (z/xml1-> lahettaja :tyopuhelin z/text)}))
 
-(defn- lue-data [elementit]
+(defn- lue-data [elementit avain]
   (z/xml-> elementit (fn [e]
-                       (z/xml-> (z/xml1-> e) :url z/text))))
+                       (z/xml-> (z/xml1-> e) avain z/text))))
 
 (defn lue-sijainti [sijainti]
   (let [tienumero (z/xml1-> sijainti :tienumero z/text)]
@@ -79,8 +79,8 @@
                                 (if (empty? ilmoittaja) nil ilmoittaja))
                   :lahettaja (when-let [lahettaja (into {} (z/xml-> data :lahettaja lue-lahettaja))]
                                (if (empty? lahettaja) nil lahettaja))
-                  :selitteet (into [] (z/xml-> data :seliteet lue-data))
-                  :kuvat (into [] (z/xml-> data :kuvat lue-data))
+                  :selitteet (into [] (z/xml-> data :seliteet #(lue-data % :selite)))
+                  :kuvat (into [] (z/xml-> data :kuvat #(lue-data % :url)))
                   :sijainti (when-let [sijainti (into {} (z/xml-> data :sijainti lue-sijainti))]
                               (if (empty? sijainti) nil sijainti))
                   :vastaanottaja (when-let [vastaanottaja (into {} (z/xml-> data :vastaanottaja lue-vastaanottaja))]
