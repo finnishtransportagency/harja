01bc4a9748fb8dd8b5d35738352798484201ccac
Samuli Taskila <samuli.taskila@solita.fi>
2023-06-29 12:29:14 +0300

    Korjaa testi xml

--- test/clj/harja/palvelin/integraatiot/tloik/tyokalut.clj
+++ test/clj/harja/palvelin/integraatiot/tloik/tyokalut.clj
@@ -168,6 +168,10 @@
   <urakkatyyppi>hoito</urakkatyyppi>
   <otsikko>Korkeat vallit</otsikko>
   <paikanKuvaus>Jossain kentällä.</paikanKuvaus>
+  <kuvat>
+  <url>https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Minniecat.png/220px-Minniecat.png</url>
+  <url>https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Six_weeks_old_cat_%28aka%29.jpg/390px-Six_weeks_old_cat_%28aka%29.jpg</url>
+  </kuvat>
   <lisatieto>Vanhat vallit ovat liian korkeat ja uutta lunta on satanut reippaasti.</lisatieto>
   <yhteydenottopyynto>false</yhteydenottopyynto>
   <sijainti>
@@ -192,10 +196,6 @@
   <selite>auraustarve</selite>
   <selite>aurausvallitNakemaesteena</selite>
   </seliteet>
-  <kuvat>
-  <url>https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Minniecat.png/220px-Minniecat.png</url>
-  <url>https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Six_weeks_old_cat_%28aka%29.jpg/390px-Six_weeks_old_cat_%28aka%29.jpg</url>
-  </kuvat>
   <luokittelu>
   <aihe>1</aihe>
   <tarkenne>1</tarkenne>
