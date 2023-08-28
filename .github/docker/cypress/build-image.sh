b16f8705726c7c82cf4db2bbfdf7fcb6da674769
Toni Dahl <toni.dahl@solita.fi>
2023-08-28 16:36:20 +0300

    Lisää GH Actions cypress image, jossa on esiasennettuna e2e testaamiseen tarvittavat depsut * Käytetään samoja cypress ja transit-js versioita, kuin on harja-projektin juuressa olevaan package.jsoniin määritelty. Riittää, että harjan juuressa olevaan package.jsoniin vaihdetaan cypressin versio, jonka jälkeen ajetaan cypress imagen docker build uudestaan ja päivitetään image container registryyn.

---
+++ .github/docker/cypress/build-image.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# ./build_image.sh [true] [auto]
+
+# Exit on any error or an unset variable (use optionally -x to print each command)
+set -Eeu
+
+# Buildaa ilman cachea, jotta esim. uusin chrome tulee ladattua joka buildin yhteydessä (default: true)
+CLEAN_BUILD=${1:-true}
+
+# Kuinka build progressin lokit printataan (default: auto)
+#   Käytetään defaulttina 'plain' lokitusta, jotta nähdään mitä versioita imagen build asentaa paketeista
+#   auto = Tiivistetty
+#   plain = Vanhanmallinen, tulostaa kaiken (esim. echot)
+PROGRESS=${2:-plain}
+
+cmd_opts=()
+
+if [[ "$CLEAN_BUILD" = "true" ]]; then
+  cmd_opts+=("--no-cache")
+fi
+
+cmd_opts+=("--progress=${PROGRESS}")
+
+echo "Buildataan ja tagataan image..."
+
+# Asennetaan samat versiot cypressin tarvitsemista NPM-paketeista, kuin on käytössä paikallisessa kehityksessäkin
+readonly NPM_CYPRESS_VERSION=$(node -pe 'require("../../../package").dependencies.cypress')
+readonly NPM_TRANSITJS_VERSION=$(node -pe 'require("../../../package").dependencies["transit-js"]')
+
+docker build -t ghcr.io/finnishtransportagency/harja_cypress:latest \
+--build-arg="NPM_CYPRESS_VERSION=${NPM_CYPRESS_VERSION}" \
+--build-arg="NPM_TRANSITJS_VERSION=${NPM_TRANSITJS_VERSION}" \
+"${cmd_opts[@]}" .
+
+echo "Build valmis."
