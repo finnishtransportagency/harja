2257ce2d159ccc7d5e399e5f40bb0d969940b589
Samuli Taskila <samuli.taskila@solita.fi>
2023-09-14 14:00:42 +0300

    Korjaa formatointia & päivitä vanhentunut ohjeistus

--- src/clj/harja/palvelin/palvelut/info.clj
+++ src/clj/harja/palvelin/palvelut/info.clj
@@ -4,36 +4,31 @@
             [harja.kyselyt.info :as q]
             [harja.domain.oikeudet :as oikeudet]))
 
-(defn hae-koulutusvideot [db user]
+(defn hae-koulutusvideot [db _]
   (oikeudet/ei-oikeustarkistusta!)
   (into []
-        (q/hae-koulutusvideot db)))
+    (q/hae-koulutusvideot db)))
 
 (defn syotetaan-uusi-rivi?
   "Palauttaa true/false jos halutaan inserttaa uusi rivi"
   [poistetaan indeksi]
   (and
-   (nil? poistetaan)
-   (< indeksi 0)))
+    (nil? poistetaan)
+    (< indeksi 0)))
 
 (defn paivita-koulutusvideot [db user {:keys [tiedot]}]
   (oikeudet/vaadi-kirjoitusoikeus oikeudet/hallinta-koulutusvideot user)
-  
+
   (doseq [x tiedot]
-
     (if (syotetaan-uusi-rivi? (:poistettu x) (:id x))
-
       ; Käyttäjä lisäsi uuden rivin
       (q/lisaa-video! db {:otsikko (:otsikko x)
                           :linkki (:linkki x)
                           :pvm (:pvm x)})
-
       ; Käyttäjä muokkaa rivejä
       (if (and (not= (:id x) -1) (:poistettu x))
-
         ; Poistettu true => Käyttäjä poistaa rivin
         (q/poista-video! db {:id (:id x)})
-
         ; Poistettu False => Käyttäjä päivittää tiedot
         (q/paivita-video! db {:otsikko (:otsikko x)
                               :linkki (:linkki x)
@@ -41,23 +36,23 @@
                               :id (:id x)}))))
   ; Palauta päivitetty lista
   (into []
-        (q/hae-koulutusvideot db)))
+    (q/hae-koulutusvideot db)))
 
 (defrecord Info []
   component/Lifecycle
   (start [this]
-         (julkaise-palvelu (:http-palvelin this)
-                           :hae-koulutusvideot
-                           (fn [user _]
-                             (hae-koulutusvideot (:db this) user)))
-         (julkaise-palvelu (:http-palvelin this)
-                           :paivita-koulutusvideot
-                           (fn [user tiedot]
-                             (paivita-koulutusvideot (:db this) user tiedot)))
-         this)
- 
+    (julkaise-palvelu (:http-palvelin this)
+      :hae-koulutusvideot
+      (fn [user _]
+        (hae-koulutusvideot (:db this) user)))
+    (julkaise-palvelu (:http-palvelin this)
+      :paivita-koulutusvideot
+      (fn [user tiedot]
+        (paivita-koulutusvideot (:db this) user tiedot)))
+    this)
+
   (stop [this]
-        (poista-palvelut (:http-palvelin this) 
-                         :hae-koulutusvideot
-                         :paivita-koulutusvideot)
-        this))
+    (poista-palvelut (:http-palvelin this)
+      :hae-koulutusvideot
+      :paivita-koulutusvideot)
+    this))
