efe665d06eca30f334203df2a0151565c4119f17
Markus Vähäkangas <markus.vahakangas@solita.fi>
2023-09-28 14:43:38 +0300

    Lisää muutkin hoitovuoden päättämiseen
    liittyvät tehtäväryhmät paaria -porukkaan, joita ei näytetä käyttöliittymällä ellei ole pakko.

--- src/cljs/harja/views/urakka/kulut/kulut.cljs
+++ src/cljs/harja/views/urakka/kulut/kulut.cljs
@@ -210,43 +210,70 @@
   [{:keys [paivitys-fn tehtavaryhma tehtavaryhmat tehtavaryhma-meta indeksi disabled
            toimenpiteet toimenpideinstanssi lisatyo? lisatyon-lisatieto lisatyon-lisatieto-meta
            vuoden-paatos-valittu?]}]
-  [:div.palstat
-   [:div.palsta
-    [:label (if lisatyo?
-       "Toimenpide *"
-       "Tehtäväryhmä *")]
-    [tehtavaryhma-tai-toimenpide-dropdown {:paivitys-fn paivitys-fn
-                                           :valittu (if lisatyo?
-                                                      toimenpideinstanssi
-                                                      tehtavaryhma)
-                                           :valinnat (if lisatyo?
-                                                       toimenpiteet
-                                                       tehtavaryhmat)
-                                           :valittu-meta tehtavaryhma-meta
-                                           :indeksi indeksi
-                                           :lisatyo? lisatyo?
-                                           :ryhmat (when-not lisatyo?
-                                                     {:nayta-ryhmat [:ei-hallinnollinen :hallinnollinen]
-                                                      :ryhmittely hallinnollisen-ryhmittely
-                                                      :ryhman-otsikko hallinnollisen-otsikointi})
-                                           :nayta-vihje? (if lisatyo?
-                                                           #(false? true)
-                                                           jos-hallinnollinen)
-                                           :vihje-viesti hallinnollinen-vihje-viesti
-                                           :disabled (or disabled
-                                                       vuoden-paatos-valittu?)}]]
-   [:div.palsta
-    (when lisatyo?
-      [kentat/tee-otsikollinen-kentta 
-       {:otsikko "Lisätieto *"
-        :luokka #{}
-        :kentta-params {:disabled disabled
-                        :vayla-tyyli? true
-                        :virhe? (not (validi-ei-tarkistettu-tai-ei-koskettu? lisatyon-lisatieto-meta))
-                        :tyyppi :string}
-        :arvo-atom (r/wrap lisatyon-lisatieto (r/partial paivita-lomakkeen-arvo 
-                                                {:paivitys-fn paivitys-fn 
-                                                 :polku [:kohdistukset indeksi :lisatyon-lisatieto]}))}])]])
+  (let [;; Kulujen lisäämisessä tehtäväryhmä alasvetovalikosta poistetaan muutamia tehtäväryhmiä, joita ei voida valita.
+        ;; Jos niitä on saatu kululle jotain muuta kautta, niin ne kuitenkin näytetään.
+        ;; Nöitä on
+        ;; Tilaajan Rahavaraus (T3), yksilöllinen tunniste: '0e78b556-74ee-437f-ac67-7a03381c64f6'
+        ;; Hoitovuoden päättäminen / Tavoitepalkkio, yksilöllinen tunniste: '55c920e7-5656-4bb0-8437-1999add714a3'
+        ;; Hoitovuoden päättäminen / Urakoitsija maksaa tavoitehinnan ylityksestä, yksilöllinen tunniste: '19907c24-dd26-460f-9cb4-2ed974b891aa'
+        ;; Hoitovuoden päättäminen / Urakoitsija maksaa kattohinnan ylityksestä, yksilöllinen tunniste: 'be34116b-2264-43e0-8ac8-3762b27a9557'
+        kielletty-tr (some
+                       (fn [rivi]
+                         (when (and (= tehtavaryhma (:id rivi))
+                                 (or
+                                   (= "Tilaajan rahavaraus (T3)" (:tehtavaryhma rivi))
+                                   (= "Hoitovuoden päättäminen / Tavoitepalkkio" (:tehtavaryhma rivi))
+                                   (= "Hoitovuoden päättäminen / Urakoitsija maksaa tavoitehinnan ylityksestä" (:tehtavaryhma rivi))
+                                   (= "Hoitovuoden päättäminen / Urakoitsija maksaa kattohinnan ylityksestä" (:tehtavaryhma rivi))))
+                           rivi))
+                       tehtavaryhmat)
+        tehtavaryhmat (if kielletty-tr
+                        tehtavaryhmat
+                        (filter
+                          (fn [rivi]
+                            (when-not (or
+                                        (= "Tilaajan rahavaraus (T3)" (:tehtavaryhma rivi))
+                                        (= "Hoitovuoden päättäminen / Tavoitepalkkio" (:tehtavaryhma rivi))
+                                        (= "Hoitovuoden päättäminen / Urakoitsija maksaa tavoitehinnan ylityksestä" (:tehtavaryhma rivi))
+                                        (= "Hoitovuoden päättäminen / Urakoitsija maksaa kattohinnan ylityksestä" (:tehtavaryhma rivi))) rivi))
+                          tehtavaryhmat))]
+   [:div.palstat
+    [:div.palsta
+     [:label (if lisatyo?
+               "Toimenpide *"
+               "Tehtäväryhmä *")]
+     [tehtavaryhma-tai-toimenpide-dropdown {:paivitys-fn paivitys-fn
+                                            :valittu (if lisatyo?
+                                                       toimenpideinstanssi
+                                                       tehtavaryhma)
+                                            :valinnat (if lisatyo?
+                                                        toimenpiteet
+                                                        tehtavaryhmat)
+                                            :valittu-meta tehtavaryhma-meta
+                                            :indeksi indeksi
+                                            :lisatyo? lisatyo?
+                                            :ryhmat (when-not lisatyo?
+                                                      {:nayta-ryhmat [:ei-hallinnollinen :hallinnollinen]
+                                                       :ryhmittely hallinnollisen-ryhmittely
+                                                       :ryhman-otsikko hallinnollisen-otsikointi})
+                                            :nayta-vihje? (if lisatyo?
+                                                            #(false? true)
+                                                            jos-hallinnollinen)
+                                            :vihje-viesti hallinnollinen-vihje-viesti
+                                            :disabled (or disabled
+                                                        vuoden-paatos-valittu?)}]]
+    [:div.palsta
+     (when lisatyo?
+       [kentat/tee-otsikollinen-kentta
+        {:otsikko "Lisätieto *"
+         :luokka #{}
+         :kentta-params {:disabled disabled
+                         :vayla-tyyli? true
+                         :virhe? (not (validi-ei-tarkistettu-tai-ei-koskettu? lisatyon-lisatieto-meta))
+                         :tyyppi :string}
+         :arvo-atom (r/wrap lisatyon-lisatieto (r/partial paivita-lomakkeen-arvo
+                                                 {:paivitys-fn paivitys-fn
+                                                  :polku [:kohdistukset indeksi :lisatyon-lisatieto]}))}])]]))
 
 (defn useampi-kohdistus
   [{:keys [paivitys-fn disabled poistettu muokataan? indeksi
