d7cb55e5585a190b7e20a9da81b3affdb74c8f8c
Samuli Taskila <samuli.taskila@solita.fi>
2023-11-22 06:25:42 +0200

    Korjaa raporttien testit

--- test/clj/harja/palvelin/raportointi/kulut_tehtavaryhmittain_test.clj
+++ test/clj/harja/palvelin/raportointi/kulut_tehtavaryhmittain_test.clj
@@ -12,20 +12,20 @@
 
 (defn jarjestelma-fixture [testit]
   (alter-var-root #'jarjestelma
-                  (fn [_]
-                    (component/start
-                      (component/system-map
-                        :db (tietokanta/luo-tietokanta testitietokanta)
-                        :http-palvelin (testi-http-palvelin)
-                        :pdf-vienti (component/using
-                                      (pdf-vienti/luo-pdf-vienti)
-                                      [:http-palvelin])
-                        :raportointi (component/using
-                                       (raportointi/luo-raportointi)
-                                       [:db :pdf-vienti])
-                        :raportit (component/using
-                                    (raportit/->Raportit)
-                                    [:http-palvelin :db :raportointi :pdf-vienti])))))
+    (fn [_]
+      (component/start
+        (component/system-map
+          :db (tietokanta/luo-tietokanta testitietokanta)
+          :http-palvelin (testi-http-palvelin)
+          :pdf-vienti (component/using
+                        (pdf-vienti/luo-pdf-vienti)
+                        [:http-palvelin])
+          :raportointi (component/using
+                         (raportointi/luo-raportointi)
+                         [:db :pdf-vienti])
+          :raportit (component/using
+                      (raportit/->Raportit)
+                      [:http-palvelin :db :raportointi :pdf-vienti])))))
 
   (testit)
   (alter-var-root #'jarjestelma component/stop))
@@ -36,8 +36,11 @@
 
 (def odotettu-raportti
   [:raportti {:nimi "Kulut tehtäväryhmittäin"}
-   [:taulukko {:viimeinen-rivi-yhteenveto? true, :otsikko "Kulut tehtäväryhmittäin ajalla 01.12.2019 - 30.08.2020"}
-    [{:leveys 1, :otsikko "Tehtäväryhmä"} {:leveys 1, :fmt :raha, :otsikko "Hoitokauden alusta 01.10.2019-30.09.2020"} {:leveys 1, :fmt :raha, :otsikko "Jaksolla 01.12.2019-30.08.2020"}]
+   [:taulukko
+    {:viimeinen-rivi-yhteenveto? true, :otsikko "Kulut tehtäväryhmittäin ajalla 01.12.2019 - 30.08.2020"}
+    [{:leveys 1, :otsikko "Tehtäväryhmä"}
+     {:leveys 1, :fmt :raha, :otsikko "Hoitokauden alusta 01.10.2019-30.09.2020"}
+     {:leveys 1, :fmt :raha, :otsikko "Jaksolla 01.12.2019-30.08.2020"}]
     [(list "Talvihoito (A)" 6601.94M 3300.40M)
      (list "Talvisuola (B1)" 0 0)
      (list "KFo, NaFo (B2)" 0 0)
@@ -57,10 +60,9 @@
      (list "Valu (Y7)" 0 0)
      (list "Siltapäällysteet (H)" 0 0)
      (list "Sorapientareet (O)" 0 0)
-     (list "Sorastus (M)" 0 0)
+     (list "Sorastus (M)" 8801.94M 4400.40M)
      (list "Sillat ja laiturit (I)" 0 0)
-     (list "Liikenteen varmistaminen kelirikkokohteessa (M)" 0 0)
-     (list "Sorateiden hoito (C)" 8801.94M 4400.40M)
+     (list "Sorateiden hoito (C)" 0 0)
      (list "Kesäsuola, materiaali (D)" 0 0)
      (list "Äkilliset hoitotyöt, Talvihoito (T1)" 0 0)
      (list "Äkilliset hoitotyöt, Liikenneympäristön hoito (T1)" 4444.44M 0)
@@ -87,50 +89,52 @@
      (list "Alataso Lisätyöt" 0 0)
      (list "Digitalisaatio ja innovaatiot (T4)" 0 0)
      ["Yhteensä" 64307.62M 28366.60M]]]
-   [:taulukko {:otsikko "Urakkavuoden alusta", :viimeinen-rivi-yhteenveto? true}
+   [:taulukko
+    {:otsikko "Urakkavuoden alusta", :viimeinen-rivi-yhteenveto? true}
     [{:leveys 1, :otsikko ""} {:leveys 1, :otsikko "", :fmt :raha}]
     [["Tavoitehinta: " 250000M]
      ["Urakkavuoden alusta tav.hintaan kuuluvia: " 64307.62M]
      ["Jäljellä: " 185692.38M]]]])
 
+
 (deftest kulut-tehtavaryhmittain-testi
   (let [vastaus (kutsu-palvelua (:http-palvelin jarjestelma)
-                                :suorita-raportti
-                                +kayttaja-jvh+
-                                {:nimi       :kulut-tehtavaryhmittain
-                                 :konteksti  "urakka"
-                                 :urakka-id  @oulun-maanteiden-hoitourakan-2019-2024-id
-                                 :parametrit {:alkupvm  (c/to-date (t/local-date 2019 12 1))
-                                              :loppupvm (c/to-date (t/local-date 2020 8 30))}})
+                  :suorita-raportti
+                  +kayttaja-jvh+
+                  {:nimi       :kulut-tehtavaryhmittain
+                   :konteksti  "urakka"
+                   :urakka-id  @oulun-maanteiden-hoitourakan-2019-2024-id
+                   :parametrit {:alkupvm  (c/to-date (t/local-date 2019 12 1))
+                                :loppupvm (c/to-date (t/local-date 2020 8 30))}})
         odotettu-vastaus odotettu-raportti
 
         vastaus-ulkopuolella (kutsu-palvelua (:http-palvelin jarjestelma)
-                                :suorita-raportti
-                                +kayttaja-jvh+
-                                {:nimi       :kulut-tehtavaryhmittain
-                                 :konteksti  "urakka"
-                                 :urakka-id  @oulun-maanteiden-hoitourakan-2019-2024-id
-                                 :parametrit {:alkupvm  (c/to-date (t/local-date 2014 12 1))
-                                              :loppupvm (c/to-date (t/local-date 2015 8 30))}})
+                               :suorita-raportti
+                               +kayttaja-jvh+
+                               {:nimi       :kulut-tehtavaryhmittain
+                                :konteksti  "urakka"
+                                :urakka-id  @oulun-maanteiden-hoitourakan-2019-2024-id
+                                :parametrit {:alkupvm  (c/to-date (t/local-date 2014 12 1))
+                                             :loppupvm (c/to-date (t/local-date 2015 8 30))}})
         yhteensa (some #(when (= "Yhteensä" (first %)) %)
-                       (-> vastaus
-                           (nth 2)
-                           (nth 3)))
+                   (-> vastaus
+                     (nth 2)
+                     (nth 3)))
         eka-luku (second yhteensa)
         toka-luku (nth yhteensa 2)
         raportti-avainsana (first vastaus)
         taulukot (nth vastaus 2)
         taulukko-avainsana (first taulukot)
         taulukon-rivit (-> vastaus
-                           (nth 2)
-                           (nth 3))]
+                         (nth 2)
+                         (nth 3))]
     (is (vector? vastaus) "Raportille palautuu tavaraa")
     (is (and (= :raportti raportti-avainsana)
-             (= :taulukko taulukko-avainsana)
-             (vector? taulukon-rivit)
-             (> (count taulukon-rivit)
-                0)) "Vastaus näyttää raportilta")
-(is (= vastaus odotettu-vastaus))
+          (= :taulukko taulukko-avainsana)
+          (vector? taulukon-rivit)
+          (> (count taulukon-rivit)
+            0)) "Vastaus näyttää raportilta")
+    (is (= vastaus odotettu-vastaus))
 
     (is (and
           (> toka-luku 0)
@@ -138,6 +142,6 @@
     (is (every? #(let [eka (second %)
                        toka (nth % 2)]
                    (= 0 eka toka))
-                (-> vastaus-ulkopuolella
-                    (nth 2)
-                    (nth 3))) "Raportille ei tule väärää tavaraa")))
+          (-> vastaus-ulkopuolella
+            (nth 2)
+            (nth 3))) "Raportille ei tule väärää tavaraa")))
