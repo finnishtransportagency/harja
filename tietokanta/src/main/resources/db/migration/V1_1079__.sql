643a8889d0b88a9872286aea6771f024d993a213
Maarit Laine <maarit.laine@solita.fi>
2024-01-03 12:50:03 +0200

    Poista turi-integraatio integraatiotaulusta

--- tietokanta/src/main/resources/db/migration/V1_1079__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1079__.sql
@@ -1,3 +1,7 @@
 -- Turi-järjestelmä on poistunut käytöstä. Harjasta ei lähetetä enää Turiin turvallisuuspoikkeamia eikä urakan työtunteja.
 -- Jatkossa analytiikkaportaali hakee turvallisuuspoikkeamat Harjasta, urakan työtunteja ei välitetä Harjasta eteenpäin.
+-- Siivotaan ensin lokia, jotta intergaation tietojen poistaminen onnistuu.
+DELETE FROM integraatioviesti WHERE integraatiotapahtuma IN (select id from integraatiotapahtuma where integraatio in (select id from integraatio where jarjestelma = 'turi'));
+DELETE FROM integraatiotapahtuma where integraatio in (select id from integraatio where jarjestelma = 'turi');
 DELETE FROM integraatio WHERE jarjestelma = 'turi';
+
