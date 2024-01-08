74ec32c16cfa1a73de7fe48c9323639af93ab2af
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-01-08 13:36:53 +0200

    Aseta fileyard ja se3url vapaaehtoiseksi, koska aws ei käytä enää fileyardia

--- src/clj/harja/palvelin/asetukset.clj
+++ src/clj/harja/palvelin/asetukset.clj
@@ -190,8 +190,8 @@
 
    (s/optional-key :vkm) {:url s/Str}
 
-   (s/optional-key :liitteet) {:fileyard-url s/Str
-                               :s3-url s/Str}
+   (s/optional-key :liitteet) {(s/optional-key :fileyard-url) s/Str
+                               (s/optional-key :s3-url) s/Str}
 
    (s/optional-key :yllapitokohteet) {:paivittainen-sahkopostin-lahetysaika [s/Num]}
    :komponenttien-tila {:itmf {:paivitystiheys-ms s/Int}
