e14e1b98d34d6273a8317c1be2737a056504ca92
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2024-01-17 15:53:28 +0200

    Lisää afterMigratella GRANT:it flywayn omistamiin tauluihin
    AWS DMS -työkalu luo joitakin metatauluja, joille afterMigrate.sql ajaminen epäonnistui. Korjataan tällä siten, että DMS:n tarvitsemat työkalut jäävät harja-käyttäjän saamien grantien ulkopuolelle.

--- tietokanta/src/main/resources/db/migration/afterMigrate.sql
+++ tietokanta/src/main/resources/db/migration/afterMigrate.sql
@@ -1,3 +1,23 @@
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO harja;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO harja;
-GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO harja;
+DO
+$$
+    DECLARE
+        t RECORD;
+        s RECORD;
+        f RECORD;
+    BEGIN
+        FOR t IN (SELECT schemaname, tablename FROM pg_tables WHERE schemaname = 'public' and tableowner = 'flyway')
+            LOOP
+                EXECUTE format('GRANT ALL PRIVILEGES ON TABLE %I.%I  TO harja;', t.schemaname, t.tablename);
+            END LOOP;
+
+        FOR s IN (SELECT schemaname, sequencename FROM pg_sequences WHERE schemaname = 'public' and sequenceowner = 'flyway')
+            LOOP
+                EXECUTE format('GRANT ALL PRIVILEGES ON SEQUENCE %I.%I  TO harja;', s.schemaname, s.sequencename);
+            END LOOP;
+
+        FOR f IN (SELECT schemaname, funcname FROM pg_stat_user_functions WHERE schemaname = 'public' and funcname NOT ILIKE 'awsdms%')
+            LOOP
+                EXECUTE format('GRANT ALL PRIVILEGES ON FUNCTION %I.%I  TO harja;', f.schemaname, f.funcname);
+            END LOOP;
+    END
+$$
