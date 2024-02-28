6f274167e781529baf82fd0199b096b87c7cc3cf
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2024-02-28 15:16:26 +0200

    Päivitä kommenttia

--- tietokanta/src/main/resources/db/migration/V1_1090__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1090__.sql
@@ -1,2 +1,2 @@
--- Siirrä toteuma-taulun sekvenssin omistajuus takaisin päätaululle, kun se partitioinnin yhteydessä karkasi ensimmäiselle peritylle taululle
+-- Siirrä toteuma-taulun sekvenssin omistajuus takaisin päätaululle, kun se partitioinnin yhteydessä siirtyi RENAME:n myötä ensimmäiselle partitiolle (ks. 832-migraatio)
 ALTER SEQUENCE toteuma_id_seq OWNED BY toteuma.id;
