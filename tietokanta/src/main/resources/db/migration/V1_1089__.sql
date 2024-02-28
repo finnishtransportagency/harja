b4558e461725a3dd9acdc19d092c4e279111ec1f
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2024-02-28 12:28:26 +0200

    Poista käyttämätön sekvenssi livitunnisteet

--- tietokanta/src/main/resources/db/migration/V1_1089__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1089__.sql
@@ -1,2 +1,5 @@
 -- Siirrä maksueränumeron sekvenssin omistajuus ko. sarakkeelle
 ALTER SEQUENCE maksueranumero OWNED BY maksuera.numero;
+
+-- Poista käyttämätön sekvenssi livitunnisteet
+DROP SEQUENCE IF EXISTS livitunnisteet;
