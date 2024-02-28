90e678cce334fbbd18b5c184ef7c9858da14af50
Samu Mikkonen <samu.mikkonen@solita.fi>
2024-02-28 10:03:15 +0200

    Lisää luotu timestamp

--- tietokanta/src/main/resources/db/migration/V1_1089__ .sql
+++ tietokanta/src/main/resources/db/migration/V1_1089__ .sql
@@ -3,7 +3,8 @@ CREATE TABLE lupausryhma_urakka
     id SERIAL PRIMARY KEY,
     lupausryhma_id INTEGER REFERENCES lupausryhma(id) NOT NULL,
     urakka_id INTEGER REFERENCES urakka(id) NOT NULL,
-    muokattu TIMESTAMP NOT NULL DEFAULT NOW(),
+    luotu TIMESTAMP NOT NULL DEFAULT NOW(),
+    muokattu TIMESTAMP,
     UNIQUE (urakka_id, lupausryhma_id)
 );
 
