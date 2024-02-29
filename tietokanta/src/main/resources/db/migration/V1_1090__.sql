8df353e661f496f77b7d5341fd24ff5ac40acb78
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2024-02-29 08:32:04 +0200

    Poista toinen 1090 migraatio

--- tietokanta/src/main/resources/db/migration/V1_1090__.sql
+++
@@ -1,2 +0,0 @@
--- Siirrä toteuma-taulun sekvenssin omistajuus takaisin päätaululle, aikanaan siirtyi RENAME:n myötä ensimmäiselle partitiolle (ks. 832-migraatio)
-ALTER SEQUENCE toteuma_id_seq OWNED BY toteuma.id;
