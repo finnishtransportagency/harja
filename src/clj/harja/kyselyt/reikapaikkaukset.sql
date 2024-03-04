885bf40b506734486e3169061242765ca2ce248c
Samuli Taskila <samuli.taskila@solita.fi>
2024-03-04 19:43:51 +0200

    UPSERT proseduuri reikäpaikkauksille

--- src/clj/harja/kyselyt/reikapaikkaukset.sql
+++ src/clj/harja/kyselyt/reikapaikkaukset.sql
@@ -11,3 +11,29 @@ SELECT
     p.massatyyppi,
     p.alkuaika
 FROM paikkaus p WHERE p."urakka-id" = :urakka-id LIMIT 20;
+
+-- luo-tai-paivita-reikapaikkaus!
+SELECT reikapaikkaus_upsert(
+    'reikapaikkaus'::paikkaustyyppi, -- tyyppi
+    3,                         -- luojaid
+    NOW()::TIMESTAMP,     --luotu
+    NULL::INT,          --muokkaajaid
+    NULL::TIMESTAMP,      --muokattu
+    NULL::INT,      --poistajaid
+    FALSE,      -- poistettu
+    14,       -- urakkaid 
+    NULL::INT,      --paikkauskohdeid
+    123452,     -- ulkoinenid
+    NOW()::TIMESTAMP,     --alkuaika
+    (NOW() + INTERVAL '20 day')::TIMESTAMP,     -- loppuaika 
+    ROW(20, 19, (50 + 3), 19, (51 + 3), NULL)::TR_OSOITE,   -- tr osoite
+    (SELECT id FROM paikkauskohde_tyomenetelma WHERE lyhenne = 'UREM'),   -- tyomenetelma
+    'AB, Asfalttibetoni'::TEXT,     -- massatyyppi 
+    1.2,  -- leveys
+    23.0,   -- massamenekki 
+    0.0276,  -- massamaara 
+    1.2,  -- pintaala
+    5,     -- raekoko 
+    'AN7'::TEXT,  -- kuulamylly
+    (SELECT tierekisteriosoitteelle_viiva(20, 19, (50 + 3), 19, (51 + 3))) -- last but not least, sijainti geometria
+);
--- tietokanta/src/main/resources/db/migration/V1_1092__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1092__.sql
@@ -17,3 +17,110 @@ CHECK           (tyyppi = 'reikapaikkaus' OR paikkauskohde-id IS NOT NULL);
 CREATE UNIQUE INDEX unique_ulkoinen_id_urakalla
 ON paikkaus ("urakka-id", "ulkoinen-id")
 WHERE "ulkoinen-id" <> 0;
+
+DROP FUNCTION IF EXISTS reikapaikkaus_upsert(
+  paikkaustyyppi,  INT, 
+  TIMESTAMP, INT, 
+  TIMESTAMP, INT, 
+  BOOLEAN, INT, 
+  INT, INT, 
+  TIMESTAMP, TIMESTAMP, 
+  TR_OSOITE, INT, 
+  TEXT, NUMERIC, 
+  NUMERIC,  NUMERIC, 
+  NUMERIC, INT, 
+  TEXT, GEOMETRY
+);
+
+-- UPSERT funktio, sen takia koska INSERT .. ON CONFLICT ei toimi tässä tapauksessa  
+-- Vaikka kaikkia paikkaus sarakkeita ei reikäpaikkauksessa tarvita, lyöty silti mukaan jos tarvitaankin myöhemmin 
+CREATE OR REPLACE FUNCTION reikapaikkaus_upsert(
+  _tyyppi paikkaustyyppi,  _luojaid INT, 
+  _luotu TIMESTAMP, _muokkaajaid INT, 
+  _muokattu TIMESTAMP, _poistajaid INT, 
+  _poistettu BOOLEAN, _urakkaid INT, 
+  _paikkauskohdeid INT, _ulkoinenid INT, 
+  _alkuaika TIMESTAMP, _loppuaika TIMESTAMP, 
+  _tierekisteriosoite TR_OSOITE, _tyomenetelma INT, 
+  _massatyyppi TEXT, _leveys NUMERIC, 
+  _massamenekki NUMERIC, _massamaara NUMERIC, 
+  _pintaala NUMERIC, _raekoko INT, 
+  _kuulamylly TEXT, _sijainti GEOMETRY
+) RETURNS VOID AS $$
+BEGIN
+  UPDATE paikkaus SET
+    tyyppi = _tyyppi, 
+    "luoja-id" = _luojaid,
+    luotu = _luotu, 
+    "muokkaaja-id" = _muokkaajaid,
+    muokattu = _muokattu, 
+    "poistaja-id" = _poistajaid,
+    poistettu = _poistettu, 
+    "paikkauskohde-id" = _paikkauskohdeid,
+    alkuaika = _alkuaika, 
+    loppuaika = _loppuaika,
+    tierekisteriosoite = _tierekisteriosoite, 
+    tyomenetelma = _tyomenetelma,
+    massatyyppi = _massatyyppi, 
+    leveys = _leveys,
+    massamenekki = _massamenekki,
+     massamaara = _massamaara,
+    "pinta-ala" = _pintaala, 
+    raekoko = _raekoko,
+    kuulamylly = _kuulamylly, 
+    sijainti = _sijainti
+  WHERE "urakka-id" = _urakkaid AND "ulkoinen-id" = _ulkoinenid;
+  -- FOUND ilmeisesti jokin vakio postgres muuttuja, kertoo viime PERFORM, INSERT, UPDATE tilan
+  IF NOT FOUND THEN
+    -- Eli jos viime UPDATE ei palauttanut mitään, riviä ei ole kannassa -> insert 
+    INSERT INTO paikkaus (
+      tyyppi, 
+      "luoja-id", 
+      luotu, 
+      "muokkaaja-id", 
+      muokattu, 
+      "poistaja-id", 
+      poistettu, 
+      "urakka-id", 
+      "paikkauskohde-id", 
+      "ulkoinen-id", 
+      alkuaika, 
+      loppuaika, 
+      tierekisteriosoite, 
+      tyomenetelma, 
+      massatyyppi, 
+      leveys, 
+      massamenekki, 
+      massamaara, 
+      "pinta-ala", 
+      raekoko, 
+      kuulamylly, 
+      sijainti 
+    )
+    VALUES (
+      _tyyppi, 
+      _luojaid, 
+      _luotu, 
+      _muokkaajaid, 
+      _muokattu, 
+      _poistajaid, 
+      _poistettu, 
+      _urakkaid, 
+      _paikkauskohdeid,
+      _ulkoinenid, 
+      _alkuaika, 
+      _loppuaika, 
+      _tierekisteriosoite, 
+      _tyomenetelma, 
+      _massatyyppi, 
+      _leveys,
+      _massamenekki, 
+      _massamaara, 
+      _pintaala,
+      _raekoko, 
+      _kuulamylly,
+      _sijainti
+    );
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
