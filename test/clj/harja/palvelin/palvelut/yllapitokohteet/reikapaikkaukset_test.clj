016476aaaa6ee8cf962be1cd52d004102145d595
Samuli Taskila <samuli.taskila@solita.fi>
2024-03-20 18:58:15 +0200

    Testaa toteumien päivitys, tallennus ja poisto

--- test/clj/harja/palvelin/palvelut/yllapitokohteet/reikapaikkaukset_test.clj
+++ test/clj/harja/palvelin/palvelut/yllapitokohteet/reikapaikkaukset_test.clj
@@ -70,3 +70,71 @@
     (is (= (-> vastaus count) 19))
     (is (= (-> vastaus first :nimi) "AB-paikkaus levittäjällä"))
     (is (= (-> vastaus second :nimi) "PAB-paikkaus levittäjällä"))))
+
+
+(deftest tallennus-paivitys-ja-poisto-toimii
+  (let [urakka-id (hae-urakan-id-nimella "Muhoksen päällystysurakka")
+        ulkoinen-id 6363336
+        toteumien-maara 5
+        haku-params {:tr nil
+                     :aikavali nil
+                     :urakka-id urakka-id}
+        ;; Hae tiedot ennen uuden toteuman tekoa
+        vastaus-ennen (tee-kutsu haku-params :hae-reikapaikkaukset)
+        ;; Uuden toteuman parametrit 
+        params {:luoja-id 1
+                :urakka-id urakka-id
+                :ulkoinen-id ulkoinen-id
+                :tie 20
+                :aosa 1
+                :aet 1200
+                :losa 1
+                :luotu nil
+                :alkuaika nil
+                :loppuaika nil
+                :let 1300
+                :yksikko "m2"
+                :menetelma 1
+                :maara 123
+                :kustannus 1234.5}
+
+        ;; Tallenna uusi reikäpaikkaus
+        _ (tee-kutsu params :tallenna-reikapaikkaus)
+
+        ;; Hae uudet tulokset 
+        vastaus-lisatty (tee-kutsu haku-params :hae-reikapaikkaukset)
+
+        ;; Ennen lisäystä
+        _ (is (= (-> vastaus-ennen count) toteumien-maara))
+        ;; Lisäyksen jälkeen
+        _ (is (= (-> vastaus-lisatty count) (inc toteumien-maara)))
+
+        ;; Lisätty toteuma
+        _ (is (= (-> vastaus-lisatty first :aosa) 1))
+        _ (is (= (-> vastaus-lisatty first :kustannus) 1234.5M))
+        _ (is (= (-> vastaus-lisatty first :tie) 20))
+        _ (is (= (-> vastaus-lisatty first :let) 1300))
+        _ (is (= (-> vastaus-lisatty first :losa) 1))
+        _ (is (= (-> vastaus-lisatty first :aet) 1200))
+        _ (is (= (-> vastaus-lisatty first :tyomenetelma) 1))
+        _ (is (= (-> vastaus-lisatty first :tyomenetelma-nimi) "AB-paikkaus levittäjällä"))
+        _ (is (= (-> vastaus-lisatty first :reikapaikkaus-yksikko) "m2"))
+
+        ;; Muokkaa yllä olevaa paikkausta 
+        params-muokkaa (assoc params :let 1600)
+        _ (tee-kutsu params-muokkaa :tallenna-reikapaikkaus)
+
+        ;; Katso että toteumaa muokattiin
+        vastaus-muokattu (tee-kutsu haku-params :hae-reikapaikkaukset)
+        _ (is (= (-> vastaus-muokattu first :let) 1600))
+
+        ;; Poista toteuma
+        params {:kayttaja-id 1
+                :urakka-id urakka-id
+                :ulkoinen-id ulkoinen-id}
+        
+        _ (tee-kutsu params :poista-reikapaikkaus)
+        vastaus-poistettu (tee-kutsu haku-params :hae-reikapaikkaukset)
+
+        ;; Toteumia pitäisi olla taas 5
+        _ (is (= (-> vastaus-poistettu count) toteumien-maara))]))
