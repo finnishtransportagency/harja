845f59d36bf8c8a342fa0a3e66df2d9c63a2f770
Samuli Taskila <samuli.taskila@solita.fi>
2024-03-20 18:40:38 +0200

    Testaa hae-tyomenetelmat

--- test/clj/harja/palvelin/palvelut/yllapitokohteet/reikapaikkaukset_test.clj
+++ test/clj/harja/palvelin/palvelut/yllapitokohteet/reikapaikkaukset_test.clj
@@ -1,47 +1,49 @@
 (ns harja.palvelin.palvelut.yllapitokohteet.reikapaikkaukset-test
-  ;; TODO ... 
   (:require [clojure.test :refer :all]
-             [harja.testi :refer :all]
-             [clojure.set :as set]
-             [com.stuartsierra.component :as component]
-             [clojure.data.json :as json]
-             [cheshire.core :as cheshire]
-             [harja.palvelin.palvelut.yllapitokohteet.reikapaikkaukset :as reikapaikkaukset]
-             [harja.kyselyt.tieverkko :as tieverkko-kyselyt]
-             [harja.palvelin.komponentit.tietokanta :as tietokanta]
-             [harja.pvm :as pvm]
-             [clojure.java.io :as io]
-             [harja.kyselyt.reikapaikkaukset :as q]
-             [harja.kyselyt.konversio :as konv]))
-  
-  (defn jarjestelma-fixture [testit]
-    (alter-var-root #'jarjestelma
-      (fn [_]
-        (component/start
-          (component/system-map
-            :db (tietokanta/luo-tietokanta testitietokanta)
-            :http-palvelin (testi-http-palvelin)
-            :paikkauskohteet (component/using
-                               (reikapaikkaukset/->Reikapaikkaukset)
-                               [:http-palvelin :db])))))
-  
-    (testit)
-    (alter-var-root #'jarjestelma component/stop))
-  
-  
-  (use-fixtures :each (compose-fixtures
-                        jarjestelma-fixture
-                        urakkatieto-fixture))
-  
+            [harja.testi :refer :all]
+            [clojure.set :as set]
+            [com.stuartsierra.component :as component]
+            [clojure.data.json :as json]
+            [cheshire.core :as cheshire]
+            [harja.palvelin.palvelut.yllapitokohteet.reikapaikkaukset :as reikapaikkaukset]
+            [harja.kyselyt.tieverkko :as tieverkko-kyselyt]
+            [harja.palvelin.komponentit.tietokanta :as tietokanta]
+            [harja.pvm :as pvm]
+            [clojure.java.io :as io]
+            [harja.kyselyt.reikapaikkaukset :as q]
+            [harja.kyselyt.konversio :as konv]))
+
+
+(defn jarjestelma-fixture [testit]
+  (alter-var-root #'jarjestelma
+    (fn [_]
+      (component/start
+        (component/system-map
+          :db (tietokanta/luo-tietokanta testitietokanta)
+          :http-palvelin (testi-http-palvelin)
+          :paikkauskohteet (component/using
+                             (reikapaikkaukset/->Reikapaikkaukset)
+                             [:http-palvelin :db])))))
+
+  (testit)
+  (alter-var-root #'jarjestelma component/stop))
+
+
+(use-fixtures :each (compose-fixtures
+                      jarjestelma-fixture
+                      urakkatieto-fixture))
+
+
+(defn- tee-kutsu [params kutsu]
+  (kutsu-palvelua (:http-palvelin jarjestelma) kutsu +kayttaja-jvh+ params))
+
 
 (deftest hae-reikapaikkaukset-toimii
   (let [urakka-id (hae-urakan-id-nimella "Muhoksen päällystysurakka")
-        fn-hae-reikapaikkaukset (fn [params]
-                                  (kutsu-palvelua (:http-palvelin jarjestelma) :hae-reikapaikkaukset +kayttaja-jvh+ params))
-
-        vastaus (fn-hae-reikapaikkaukset {:tr nil
-                                          :aikavali nil
-                                          :urakka-id urakka-id})]
+        vastaus (tee-kutsu {:tr nil
+                            :aikavali nil
+                            :urakka-id urakka-id} :hae-reikapaikkaukset)]
+    
     (is (= (-> vastaus count) 5))
     (is (= (-> vastaus first :aosa) 1))
     (is (= (-> vastaus first :kustannus) 215000.0M))
@@ -59,3 +61,12 @@
     (is (some? (-> vastaus first :tyomenetelma-nimi)))
     (is (some? (-> vastaus first :massatyyppi)))
     (is (some? (-> vastaus first :luoja-id)))))
+
+
+(deftest hae-tyomenetelmat-toimii
+  (let [urakka-id (hae-urakan-id-nimella "Muhoksen päällystysurakka")
+        vastaus (tee-kutsu {:urakka-id urakka-id} :hae-tyomenetelmat)]
+
+    (is (= (-> vastaus count) 19))
+    (is (= (-> vastaus first :nimi) "AB-paikkaus levittäjällä"))
+    (is (= (-> vastaus second :nimi) "PAB-paikkaus levittäjällä"))))
