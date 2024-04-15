7ede65ef3b8a8cf1c06bddac8987e5bb046421e6
Jere Luomajoki <jere.luomajoki@solita.fi>
2024-04-15 14:30:05 +0300

    Update gulp task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -11,8 +11,6 @@ async function buildTask() {
   gulp.src(config.src).pipe(connect.reload());
 }
 
-gulp.task('build', gulp.series(browserifyTask,stylesTask, styleImagesTask, htmlTask, function() {
-  buildTask();
-}));
+gulp.task('build', gulp.series(browserifyTask,stylesTask, styleImagesTask, htmlTask, buildTask));
 
 module.exports = buildTask;
