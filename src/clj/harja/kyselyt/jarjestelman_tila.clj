2e225e8b304ace774a1e0e4b9768602abc0bb845
Toni Dahl <toni.dahl@solita.fi>
2024-04-17 21:08:03 +0300

    Tallenna palvelimen versio palvelimen osoitteen lisäksi tila-tauluun ja suodata versiotiedon avulla turhat tulokset pois hallintapaneelin näkymästä

--- src/clj/harja/kyselyt/jarjestelman_tila.clj
+++ src/clj/harja/kyselyt/jarjestelman_tila.clj
@@ -1,11 +1,19 @@
 (ns harja.kyselyt.jarjestelman-tila
-  (:require [jeesql.core :refer [defqueries]]))
+  (:require [clojure.string :as string]
+            [jeesql.core :refer [defqueries]]))
 
 (declare hae-jarjestelman-tila)
 
 (defqueries "harja/kyselyt/jarjestelman_tila.sql")
 
+(def palvelimen-versio (some->
+                         (slurp "harja_image_id.txt")
+                         (string/trim-newline)))
+
 (defn itmfn-tila
   ([db] (itmfn-tila db false))
   ([db kehitysmoodi?]
-   (hae-jarjestelman-tila db {:kehitys? kehitysmoodi? :osa-alue "itmf"})))
+   (hae-jarjestelman-tila db {:kehitys? kehitysmoodi? :osa-alue "itmf"
+                              ;; Klusteri saavuttaa deploymentin jälkeen tilan, jossa jokaisella palvelimella on sama versio.
+                              ;; Suodatetaan pois palvelimet, jotka eivät ole samassa versiossa kuin tämä palvelin.
+                              :palvelimen-versio palvelimen-versio})))
