f2515b007e106388ee5fe51093a9cfd3f0b935b5
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-04-24 09:25:18 +0300

    Päivitä testit vastaamaan muuttunutta toteutusta

--- test/clj/harja/palvelin/palvelut/yllapitokohteet/yllapidon_paikkaukset_test.clj
+++ test/clj/harja/palvelin/palvelut/yllapitokohteet/yllapidon_paikkaukset_test.clj
@@ -43,13 +43,14 @@
         kuukausi 1
         paiva 1
         filtterit {::paikkaus/urakka-id urakka-id
-                   :aikavali [
+                   :aikavali-kuluva [
                               (c/to-date (t/local-date vuosi kuukausi paiva))
                               (c/to-date (t/local-date vuosi kuukausi paiva))]}
         paikaukset-paikkauskohteet-filtteri (kutsu-palvelua (:http-palvelin jarjestelma)
                                                             :hae-urakan-paikkaukset
                                                             +kayttaja-jvh+
-                                                            filtterit)]
+                                                            filtterit)
+        _ (println "paikaukset-paikkauskohteet-filtteri: " (pr-str paikaukset-paikkauskohteet-filtteri))]
     ;; Löytyy paikkauskohteita
     (is (> (count paikkaukset) 0))
     ;; Löytyy paikkauksia
@@ -257,7 +258,7 @@
                                         :hae-urakan-paikkaukset
                                         +kayttaja-jvh+
                                         {::paikkaus/urakka-id urakka-id
-                                         :aikavali %})
+                                         :aikavali-kuluva %})
         urakan-kaikki-paikkaukset (:paikkaukset (paikkaus-kutsu nil))
         testaus-template (fn [[alkuaika loppuaika :as aikavali]]
                            (let [haetut-paikkaukset-valilta (:paikkaukset (paikkaus-kutsu aikavali))
