6803542316b626ac099bae929452a1dff72ed622
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-04-24 07:34:35 +0300

    Poista string md5 konversio apureista. Siihen oli näppärämpi tapa

--- src/clj/harja/palvelin/integraatiot/api/tyokalut/apurit.clj
+++ src/clj/harja/palvelin/integraatiot/api/tyokalut/apurit.clj
@@ -10,16 +10,3 @@
               (get map toka)))
           {}
           (keys map)))
-
-(defn- bytes->hex-string
-  "Muodostetaan bytes arvosta string."
-  [bytes]
-  (reduce str (map #(format "%02x" (bit-and 0xFF %)) bytes)))
-
-;; Harjaan lähetetään valtavia toteumia, joiden käsittely vie kauemmin, kuin mitä vaylapilvi timeout sallii.
-;; Tehdään varmistus, että samaa toteumaa ei lähetetä uudestaan.
-(defn md5-hash [string]
-  (let [bytes (.getBytes string)]
-    (-> (java.security.MessageDigest/getInstance "MD5")
-      (.digest bytes)
-      (bytes->hex-string))))
