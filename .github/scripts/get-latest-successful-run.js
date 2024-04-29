3289eb1a69a5aadfc58f9cedcc60a84cc204ec0c
Toni Dahl <toni.dahl@solita.fi>
2024-04-29 17:55:03 +0300

    Hae workflow id workflown nimellä

--- .github/scripts/get-latest-successful-run.js
+++ .github/scripts/get-latest-successful-run.js
@@ -1,7 +1,7 @@
 module.exports = async ({ github, context, core }) => {
     const owner = context.repo.owner;
     const repo = context.repo.repo;
-    const WORKFLOW_ID = process.env.WORKFLOW_ID;
+    const WORKFLOW_FILENAME = process.env.WORKFLOW_FILENAME;
     const BRANCH_NAME = process.env.BRANCH_NAME;
 
     const { data: repoWorkflows } = await github.rest.actions.listRepoWorkflows({
@@ -9,31 +9,31 @@ module.exports = async ({ github, context, core }) => {
         repo
     });
 
-    const workflow = repoWorkflows.workflows.find(w => w.path.includes(WORKFLOW_ID));
+    const workflow = repoWorkflows.workflows.find(w => w.path.includes(WORKFLOW_FILENAME));
 
     if (!workflow) {
-        core.setFailed(`No workflow found with the specified WORKFLOW_ID: ${WORKFLOW_ID}`);
+        core.setFailed(`No workflow found with the specified WORKFLOW_FILENAME: ${WORKFLOW_FILENAME}`);
 
         return;
     }
 
+    // Palauttaa viimeisimmän määrätyssä branchissa onnistuneen workflow-runin tiedot
     const { data: runs } = await github.rest.actions.listWorkflowRuns({
         owner: context.repo.owner,
         repo: context.repo.repo,
-        workflow_id: WORKFLOW_ID,
+        workflow_id: workflow.id,
         branch: BRANCH_NAME,
         status: 'success',
         per_page: 1
     });
 
-    console.log(JSON.stringify(runs));
-
     if (runs.total_count === 0) {
-        core.setFailed(`No runs found for the WORKFLOW_ID: ${WORKFLOW_ID}`);
+        core.setFailed(`No runs found for the WORKFLOW_ID: ${workflow.id} and BRANCH_NAME: ${BRANCH_NAME}`);
 
         return;
     }
 
+    // Palautetaan onnistuneeseen runiin liittyvä commit SHA ja run ID
     return {
         commit_sha: runs.workflow_runs[0].head_commit.id,
         run_id: runs.workflow_runs[0].id
