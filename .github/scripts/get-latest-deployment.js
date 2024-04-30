b4d4e28401bc4b766e049f5f3cf9af86d1152fd0
Toni Dahl <toni.dahl@solita.fi>
2024-04-30 15:48:08 +0300

    Tarkasta, että kohdeympäristöön ei ole jo ajettu deploymenttia

---
+++ .github/scripts/get-latest-deployment.js
@@ -0,0 +1,35 @@
+// https://docs.github.com/en/rest/deployments/deployments?apiVersion=2022-11-28#list-deployments
+
+module.exports = async ({ github, context, core }) => {
+    const owner = context.repo.owner;
+    const repo = context.repo.repo;
+    const BRANCH_NAME = process.env.BRANCH_NAME;
+    const TARGET_ENVIRONMENT = process.env.TARGET_ENVIRONMENT;
+
+    const { data: deployments } = await github.rest.repos.listDeployments({
+        owner,
+        repo,
+        environment: TARGET_ENVIRONMENT,
+        ref: BRANCH_NAME,
+        per_page: 1,
+    });
+
+    if (deployments.length === 0) {
+        core.setFailed(`No deployments found for the BRANCH_NAME: ${BRANCH_NAME} and TARGET_ENVIRONMENT: ${TARGET_ENVIRONMENT}`);
+
+        return;
+    }
+
+    const latestDeployment = deployments[0];
+    const sha = latestDeployment.sha;
+    const description = latestDeployment.description;
+    const id = latestDeployment.id;
+    const task = latestDeployment.task;
+
+    return {
+        sha,
+        description,
+        task,
+        id,
+    }
+}
