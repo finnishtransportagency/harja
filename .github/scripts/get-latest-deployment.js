fc68424af0523b34b6288166787174cb37a92158
Toni Dahl <toni.dahl@solita.fi>
2024-05-02 18:24:26 +0300

    Korjaile viimeisimmän deploymentin haun heikkouksia * Varmistetaan, että viimeisimmän deploymentin tila on success * Jos deploymentin tila on jokin muu, niin tällöin on OK tehdä automaattinen deployment

--- .github/scripts/get-latest-deployment.js
+++ .github/scripts/get-latest-deployment.js
@@ -3,18 +3,20 @@
 module.exports = async ({ github, context, core }) => {
     const owner = context.repo.owner;
     const repo = context.repo.repo;
-    const BRANCH_NAME = process.env.BRANCH_NAME;
-    const TARGET_ENVIRONMENT = process.env.TARGET_ENVIRONMENT;
+    const DEPLOYMENT_REF = process.env.DEPLOYMENT_REF;
+    const DEPLOYMENT_ENVIRONMENT = process.env.DEPLOYMENT_ENVIRONMENT;
+    // Optional: Filtteröi myös SHA:n perusteella
+    const DEPLOYMENT_SHA = process.env.DEPLOYMENT_SHA;
 
     // Tarkasta pakolliset ympäristömuuttujat
-    if (!BRANCH_NAME) {
-        core.setFailed('ENV BRANCH_NAME is required');
+    if (!DEPLOYMENT_REF) {
+        core.DEPLOYMENT_REF('ENV DEPLOYMENT_REF is required');
 
         return;
     }
 
-    if (!TARGET_ENVIRONMENT) {
-        core.setFailed('ENV TARGET_ENVIRONMENT is required');
+    if (!DEPLOYMENT_ENVIRONMENT) {
+        core.setFailed('ENV DEPLOYMENT_ENVIRONMENT is required');
 
         return;
     }
@@ -22,27 +24,37 @@ module.exports = async ({ github, context, core }) => {
     const { data: deployments } = await github.rest.repos.listDeployments({
         owner,
         repo,
-        environment: TARGET_ENVIRONMENT,
-        ref: BRANCH_NAME,
+        environment: DEPLOYMENT_ENVIRONMENT,
+        // Esimerkiksi branchin nimi
+        ref: DEPLOYMENT_REF,
+        // SHA, joka on tallennettu deploymentin luomisen yhteydessä
+        sha: DEPLOYMENT_SHA,
         per_page: 1,
     });
 
     if (deployments.length === 0) {
-        core.setFailed(`No deployments found for the BRANCH_NAME: ${BRANCH_NAME} and TARGET_ENVIRONMENT: ${TARGET_ENVIRONMENT}`);
+        core.setFailed(`No deployments found for the DEPLOYMENT_REF: ${DEPLOYMENT_REF} and DEPLOYMENT_ENVIRONMENT: ${DEPLOYMENT_ENVIRONMENT}`);
 
         return;
     }
 
     const latestDeployment = deployments[0];
-    const sha = latestDeployment.sha;
-    const description = latestDeployment.description;
-    const id = latestDeployment.id;
-    const task = latestDeployment.task;
+    const deployment_id = latestDeployment.id;
+
+    // Hae viimeisimmän deploymentin tila
+    const { data: deploymentStatuses } = await github.rest.repos.listDeploymentStatuses({
+        owner,
+        repo,
+        deployment_id,
+        per_page: 1,
+    });
+
+    const status = deploymentStatuses[0].state;
 
     return {
-        sha,
-        description,
-        task,
-        id,
+        sha: latestDeployment.sha,
+        description: latestDeployment.description,
+        deployment_id,
+        status,
     }
 }
