77c01dfde2b8479a023032c3ad59db37d0900fdd
Samuli Taskila <samuli.taskila@solita.fi>
2024-05-06 21:15:05 +0300

    Hae reikäpaikkausten ja muiden paikkausten kustannukset

--- src/clj/harja/kyselyt/mpu_kustannukset.sql
+++ src/clj/harja/kyselyt/mpu_kustannukset.sql
@@ -1,17 +1,46 @@
 -- name: hae-paikkaus-kustannukset
-SELECT   
-    pt.id,
-    COALESCE(SUM(p.kustannus), 0) AS kokonaiskustannus,
-    pt.nimi AS tyomenetelma
- FROM     
-    paikkauskohde_tyomenetelma pt
-LEFT JOIN 
-    paikkaus p ON pt.id = p.tyomenetelma AND p."urakka-id" = :urakka-id
-              AND (:alkuaika::DATE IS NULL OR p.alkuaika >= :alkuaika::DATE)
-              AND (:loppuaika::DATE IS NULL OR p.loppuaika <= :loppuaika::DATE)
-              AND p.poistettu = FALSE
-GROUP BY 
-    pt.id, pt.nimi ORDER BY tyomenetelma;
+SELECT id, 
+       tyomenetelma, 
+       SUM(kokonaiskustannus) AS kokonaiskustannus
+FROM (
+    -- Muut paikkaukset
+    SELECT   
+        pt.id AS id,
+        -- 0 jos toteutunutta hintaa ei ole olemassa
+        COALESCE(SUM(pk."toteutunut-hinta"), 0) AS kokonaiskustannus,
+        pt.nimi AS tyomenetelma
+    FROM     
+        paikkauskohde pk
+    LEFT JOIN 
+        paikkauskohde_tyomenetelma pt ON pt.id = pk.tyomenetelma 
+    WHERE 
+        pk.poistettu = FALSE 
+        AND (:alkuaika::DATE IS NULL OR pk.luotu >= :alkuaika::DATE)
+        AND (:loppuaika::DATE IS NULL OR pk.luotu <= :loppuaika::DATE)
+        AND pk."urakka-id" = :urakka-id
+    GROUP BY 
+        pt.nimi, pt.id
+
+    UNION ALL
+
+    -- Reikäpaikkaukset
+    SELECT   
+        pt.id AS id,
+        -- 0 jos hintaa ei ole olemassa
+        COALESCE(SUM(p.kustannus), 0) AS kokonaiskustannus,
+        pt.nimi AS tyomenetelma
+    FROM     
+        paikkauskohde_tyomenetelma pt
+    LEFT JOIN 
+        paikkaus p ON pt.id = p.tyomenetelma AND p."urakka-id" = :urakka-id
+                  AND (:alkuaika::DATE IS NULL OR p.alkuaika >= :alkuaika::DATE)
+                  AND (:loppuaika::DATE IS NULL OR p.loppuaika <= :loppuaika::DATE)
+                  AND p.poistettu = FALSE
+    GROUP BY 
+        pt.nimi, pt.id
+) AS kustannukset
+GROUP BY tyomenetelma, id
+ORDER BY tyomenetelma;
 
 
 -- name: tallenna-mpu-kustannus!
