f614a779f5557b37263de5ccf704a52cb8a1507a
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-05-14 12:50:19 +0300

    Varmista, että cypress testit toimii peräkkäisillä ajoilla

--- cypress/e2e/tehtavamaarat.cy.js
+++ cypress/e2e/tehtavamaarat.cy.js
@@ -3,7 +3,6 @@ import {kuluvaHoitokausiAlkuvuosi} from "../support/apurit.js";
 function alustaKantaanTehtavatJaMaarat(urakkaNimi) {
     cy.terminaaliKomento().then((terminaaliKomento) => {
         // Poista urakalta kaikki vuosittaiset suunnitelmat urakka_tehtavamaara taulusta
-        console.log("Urakka: ", urakkaNimi);
         cy.exec(terminaaliKomento + 'psql -h localhost -U harja harja -c ' +
             "\"DELETE FROM urakka_tehtavamaara ut " +
             ` WHERE ut.urakka = (SELECT id FROM urakka WHERE nimi = '${urakkaNimi}');\"`)
@@ -16,6 +15,26 @@ function alustaKantaanTehtavatJaMaarat(urakkaNimi) {
             ` WHERE stt.urakka = (SELECT id FROM urakka WHERE nimi = '${urakkaNimi}');\"`)
             .then((tulos) => {
                 console.log("Poista tiedot, että onko tarjousta/sopimusta syötetty:", tulos)
+            });
+
+        // Poista muutamalta tehtävältä tarjoustieto
+        cy.exec(terminaaliKomento + 'psql -h localhost -U harja harja -c ' +
+            "\"DELETE FROM sopimus_tehtavamaara where tehtava = (select id from tehtava where nimi = 'Ise ohituskaistat') " +
+            ` AND urakka = (SELECT id FROM urakka WHERE nimi = '${urakkaNimi}');\"`)
+            .then((tulos) => {
+                console.log("Poista tarjoussumma tehtavalta:", tulos)
+            });
+        cy.exec(terminaaliKomento + 'psql -h localhost -U harja harja -c ' +
+            "\"DELETE FROM sopimus_tehtavamaara where tehtava = (select id from tehtava where nimi = 'Ennalta arvaamattomien kuljetusten avustaminen') " +
+            ` AND urakka = (SELECT id FROM urakka WHERE nimi = '${urakkaNimi}');\"`)
+            .then((tulos) => {
+                console.log("Poista tarjoussumma tehtavalta:", tulos)
+            });
+        cy.exec(terminaaliKomento + 'psql -h localhost -U harja harja -c ' +
+            "\"DELETE FROM sopimus_tehtavamaara where tehtava = (select id from tehtava where nimi = 'Opastustaulun/-viitan uusiminen') " +
+            ` AND urakka = (SELECT id FROM urakka WHERE nimi = '${urakkaNimi}');\"`)
+            .then((tulos) => {
+                console.log("Poista tarjoussumma tehtavalta:", tulos)
             });
     });
 }
@@ -46,6 +65,7 @@ describe('Tehtävämäärien syöttö ja käpistely', () => {
         cy.get('h3').contains('Syötä tarjouksen määrät').should('be.visible');
         cy.get('table.grid').contains('Ise ohituskaistat').parent().find('td.muokattava').find('input').clear().type('666').blur()
         cy.get('table.grid').contains('Ennalta arvaamattomien kuljetusten avustaminen').parent().find('td.muokattava').find('input').clear().type('666').blur()
+        cy.get('table.grid').contains('Ennalta arvaamattomien kuljetusten avustaminen').parent().find('td.muokattava').find('input').clear().type('666').blur()
     })
 
     it('Ei voi tallentaa keskeneräisenä', () => {
@@ -58,45 +78,59 @@ describe('Tehtävämäärien syöttö ja käpistely', () => {
         cy.viewport(1100, 2000)
         cy.intercept('POST', '_/tallenna-sopimuksen-tehtavamaara').as('sop1')
 
+        cy.get('table.grid').contains('Opastustaulun/-viitan uusiminen').parent().find('button.vetolaatikon-sailio').click();
+
         // Tämä conditionaalinen tarkistelu toimii. Tee checkboxille sama
         cy.get('table.grid').then(($ele) => {
             if ($ele.text().includes('Haluan syöttää joka vuoden erikseen')) {
                 // Käytä check() koska useammalla peräkkäisellä ajolla checkbox voi olla valittuna ja check varmistaa, että se on valittuna
                 cy.get('table.grid').contains('Haluan syöttää joka vuoden erikseen').parent().find('input.vayla-checkbox').check();
             } else {
-                console.log('Ei ole auki. avataan.');
-                cy.get('table.grid').contains('Päällystettyjen teiden palteiden poisto').parent().find('button.vetolaatikon-sailio').click()
+                cy.get('table.grid').contains('Opastustaulun/-viitan uusiminen').parent().find('button.vetolaatikon-sailio').click()
                 // Käytä check() koska useammalla peräkkäisellä ajolla checkbox voi olla valittuna ja check varmistaa, että se on valittuna
                 cy.get('table.grid').contains('Haluan syöttää joka vuoden erikseen').parent().find('input.vayla-checkbox').check();
             }
 
-            cy.get('table.grid').find('input#vetolaatikko-input-päällystettyjen-teiden-palteiden-poisto-' + urakanAlkuvuosi).should('not.be.disabled')
-            cy.get('table.grid').find('input#vetolaatikko-input-päällystettyjen-teiden-palteiden-poisto-' + (urakanAlkuvuosi + 0)).clear({force: true}).type('661', {force: true}).blur()
+            cy.get('table.grid').find('input#vetolaatikko-input-opastustaulun\\/-viitan-uusiminen-' + urakanAlkuvuosi).should('not.be.disabled')
+            cy.get('table.grid').find('input#vetolaatikko-input-opastustaulun\\/-viitan-uusiminen-' + (urakanAlkuvuosi + 0)).clear({force: true}).type('661', {force: true}).blur()
             cy.wait('@sop1')
-            cy.get('table.grid').find('input#vetolaatikko-input-päällystettyjen-teiden-palteiden-poisto-' + (urakanAlkuvuosi + 1)).clear().type('662').blur()
+            cy.get('table.grid').find('input#vetolaatikko-input-opastustaulun\\/-viitan-uusiminen-' + (urakanAlkuvuosi + 1)).clear().type('662').blur()
             cy.wait('@sop1')
-            cy.get('table.grid').find('input#vetolaatikko-input-päällystettyjen-teiden-palteiden-poisto-' + (urakanAlkuvuosi + 2)).clear().type('663').blur()
+            cy.get('table.grid').find('input#vetolaatikko-input-opastustaulun\\/-viitan-uusiminen-' + (urakanAlkuvuosi + 2)).clear().type('663').blur()
             cy.wait('@sop1')
-            cy.get('table.grid').find('input#vetolaatikko-input-päällystettyjen-teiden-palteiden-poisto-' + (urakanAlkuvuosi + 3)).clear().type('664').blur()
+            cy.get('table.grid').find('input#vetolaatikko-input-opastustaulun\\/-viitan-uusiminen-' + (urakanAlkuvuosi + 3)).clear().type('664').blur()
             cy.wait('@sop1')
-            cy.get('table.grid').find('input#vetolaatikko-input-päällystettyjen-teiden-palteiden-poisto-' + (urakanAlkuvuosi + 4)).clear().type('665').blur()
+            cy.get('table.grid').find('input#vetolaatikko-input-opastustaulun\\/-viitan-uusiminen-' + (urakanAlkuvuosi + 4)).clear().type('665').blur()
             cy.wait('@sop1')
 
         })
     })
 
     it('Voi tallentaa kun kaikki syötetty', () => {
+        cy.intercept('POST', '_/tallenna-sopimuksen-tehtavamaara').as('sopimusmaara')
         cy.intercept('POST', '_/tallenna-sopimuksen-tila').as('tila')
         cy.get('table.grid').contains('Ise ohituskaistat').parent().find('td.muokattava').find('input').clear().type('555').blur()
+        cy.wait('@sopimusmaara')
+        //cy.get('table.grid').contains('Ise ohituskaistat').parent().find('td.muokattava').find('input').should('have.value');
+        cy.get('table.grid').contains('Ennalta arvaamattomien kuljetusten avustaminen').parent().find('td.muokattava').find('input').clear().type('555').blur()
+        cy.wait('@sopimusmaara')
+        //cy.get('table.grid').contains('Ennalta arvaamattomien kuljetusten avustaminen').parent().find('td.muokattava').find('input').should('have.value');
         cy.contains('Tallenna').click()
         cy.wait('@tila')
         cy.contains('Syötä kaikkiin tehtäviin määrät. Jos sopimuksessa ei ole määriä kyseiselle tehtävälle, syötä').should('not.exist')
     })
 
     it('Määrän voi syöttää', () => {
-        cy.intercept('POST', '_/tallenna-tehtavamaarat').as('tehtavamaarat')
+        cy.visit('http://localhost:3000/#urakat/suunnittelu/tehtavat?&hy=13&u=32');
+        cy.intercept('POST', '_/hae-mhu-suunniteltavat-tehtavat').as('HaeSuunniteltavatTehtavat')
+        cy.intercept('POST', '_/hae-sopimuksen-tila').as('HaeSopimuksenTila')
+        cy.intercept('POST', '_/tallenna-tehtavamaarat').as('TallennaTehtavamaarat')
+        cy.wait('@HaeSuunniteltavatTehtavat')
+        cy.wait('@HaeSopimuksenTila')
+        cy.viewport(1100, 2000)
+
         cy.get('table.grid').contains('Ise 2-ajorat').parent().find('td.muokattava').find('input').clear().type('666').blur()
-        cy.wait('@tehtavamaarat')
+        cy.wait('@TallennaTehtavamaarat')
     })
 
     it('Toimenpidettä voi vaihtaa', () => {
