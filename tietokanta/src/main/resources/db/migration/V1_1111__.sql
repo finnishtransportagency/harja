9c06756868eb2b9cefbd31f72ec1ac5e2768e179
Samuli Taskila <samuli.taskila@solita.fi>
2024-05-27 20:07:34 +0300

    Tee toistuvista hauista muuttujat ja käytä muuttujia kyselyissä

--- tietokanta/src/main/resources/db/migration/V1_1111__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1111__.sql
@@ -3,8 +3,13 @@
 CREATE OR REPLACE FUNCTION populoi_rahavaraus_idt()
 RETURNS INTEGER AS $$
   DECLARE
+    vahingot_id INT;
+    akilliset_id INT;
     rivit_paivitetty INTEGER := 0;
   BEGIN
+    SELECT id INTO akilliset_id FROM rahavaraus WHERE nimi LIKE '%Äkilliset hoitotyöt%' ORDER BY id ASC LIMIT 1;
+    SELECT id INTO vahingot_id FROM rahavaraus WHERE nimi LIKE '%Vahinkojen korvaukset%' ORDER BY id ASC LIMIT 1;
+
     -- ~ ~ toteutuneet_kustannukset ~ ~ --
     -- Lisää rahavaraus_id sarakkeet, on olemassa jo parissa taulussa, mutta ei haittaa 
     ALTER TABLE kulu_kohdistus ADD COLUMN IF NOT EXISTS rahavaraus_id INT REFERENCES rahavaraus (id);
@@ -13,47 +18,41 @@ RETURNS INTEGER AS $$
 
     -- Äkilliset hoitotyöt 
     UPDATE toteutuneet_kustannukset
-       SET rahavaraus_id = ra.id
-      FROM (SELECT id FROM rahavaraus WHERE nimi LIKE '%Äkilliset hoitotyöt%' ORDER BY id ASC LIMIT 1) ra
+       SET rahavaraus_id = akilliset_id
      WHERE tyyppi = 'akillinen-hoitotyo' 
-       AND ra.id IS NOT NULL;
+       AND akilliset_id IS NOT NULL;
 
     -- Vahinkojen korvaukset
     UPDATE toteutuneet_kustannukset
-       SET rahavaraus_id = ra.id
-      FROM (SELECT id FROM rahavaraus WHERE nimi LIKE '%Vahinkojen korvaukset%' ORDER BY id ASC LIMIT 1) ra
+       SET rahavaraus_id = vahingot_id
      WHERE tyyppi = 'vahinkojen-korjaukset' 
-       AND ra.id IS NOT NULL;
+       AND vahingot_id IS NOT NULL;
 
     -- ~ ~ kulu_kohdistus ~ ~ --
     -- Äkilliset hoitotyöt 
     UPDATE kulu_kohdistus
-       SET rahavaraus_id = ra.id
-      FROM (SELECT id FROM rahavaraus WHERE nimi LIKE '%Äkilliset hoitotyöt%' ORDER BY id ASC LIMIT 1) AS ra
+       SET rahavaraus_id = akilliset_id
      WHERE maksueratyyppi = 'akillinen-hoitotyo'
-       AND ra.id IS NOT NULL;
+       AND akilliset_id IS NOT NULL;
 
     -- Maksuerätyyppi 'muu', luetaan laskutusyhteenvedeossa Vahinkojen korvauksena
     UPDATE kulu_kohdistus
-       SET rahavaraus_id = ra.id
-      FROM (SELECT id FROM rahavaraus WHERE nimi LIKE '%Vahinkojen korvaukset%' ORDER BY id ASC LIMIT 1) AS ra
+       SET rahavaraus_id = vahingot_id
      WHERE maksueratyyppi = 'muu'
-       AND ra.id IS NOT NULL;
+       AND vahingot_id IS NOT NULL;
 
     -- ~ ~ kustannusarvioitu_tyo ~ ~ --
     -- Äkilliset hoitotyöt 
     UPDATE kustannusarvioitu_tyo
-       SET rahavaraus_id = ra.id
-      FROM (SELECT id FROM rahavaraus WHERE nimi LIKE '%Äkilliset hoitotyöt%' ORDER BY id ASC LIMIT 1) ra
+       SET rahavaraus_id = akilliset_id
      WHERE tyyppi = 'akillinen-hoitotyo' 
-       AND ra.id IS NOT NULL;
+       AND akilliset_id IS NOT NULL;
 
     -- Vahinkojen korvaukset
     UPDATE kustannusarvioitu_tyo
-       SET rahavaraus_id = ra.id
-      FROM (SELECT id FROM rahavaraus WHERE nimi LIKE '%Vahinkojen korvaukset%' ORDER BY id ASC LIMIT 1) ra
+       SET rahavaraus_id = vahingot_id
      WHERE tyyppi = 'vahinkojen-korjaukset' 
-       AND ra.id IS NOT NULL;
+       AND vahingot_id IS NOT NULL;
 
     -- Palauta pävittyneet rivit, debuggausta varten
     GET DIAGNOSTICS rivit_paivitetty = ROW_COUNT;
