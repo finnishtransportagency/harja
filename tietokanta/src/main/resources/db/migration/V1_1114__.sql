e6a5606fccbb5dff68a67e3fcf2a166e945ec707
Jere Luomajoki <jere.luomajoki@solita.fi>
2024-06-05 11:16:51 +0300

    Mahdollista migraation tekeminen käsin tuotantoon etukäteen

--- tietokanta/src/main/resources/db/migration/V1_1114__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1114__.sql
@@ -1,4 +1,6 @@
-DROP INDEX CONCURRENTLY index_integraatioviesti_on_sisalto_trigram;
+-- Indeksit ajetaan tuotantoon manuaalisesti niiden keston takia. Migraatio olemassa dokumentaatiota
+-- ja muiden ympäristöjen yhtenäisenä pitämistä varten.
+DROP INDEX CONCURRENTLY IF EXISTS index_integraatioviesti_on_sisalto_trigram;
 -- Indeksöidään vain ensimmäiset 2000 merkkiä integraatioviesti-taulun sisällöstä
 -- Tällä tavoitellaan integraatioviestien insertoinnin nopeutusta ja samalla indeksin koon pienentämistä.
-CREATE INDEX CONCURRENTLY index_integraatioviesti_on_sisalto_trigram ON integraatioviesti USING gin (substring(sisalto, 0, 2000) gin_trgm_ops);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS index_integraatioviesti_on_sisalto_substring_trigram ON integraatioviesti USING gin (substring(sisalto, 0, 2000) gin_trgm_ops);
