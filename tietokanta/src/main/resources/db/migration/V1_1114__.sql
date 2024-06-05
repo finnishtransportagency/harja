929412ab80a9fdb4b94e8acd307be33347acc602
Jere Luomajoki <jere.luomajoki@solita.fi>
2024-06-05 09:10:49 +0300

    Tee indeksin poistosta non-transaktionaalinen

--- tietokanta/src/main/resources/db/migration/V1_1114__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1114__.sql
@@ -1,4 +1,4 @@
-DROP INDEX index_integraatioviesti_on_sisalto_trigram;
+DROP INDEX CONCURRENTLY index_integraatioviesti_on_sisalto_trigram;
 -- Indeksöidään vain ensimmäiset 2000 merkkiä integraatioviesti-taulun sisällöstä
 -- Tällä tavoitellaan integraatioviestien insertoinnin nopeutusta ja samalla indeksin koon pienentämistä.
 CREATE INDEX CONCURRENTLY index_integraatioviesti_on_sisalto_trigram ON integraatioviesti USING gin (substring(sisalto, 0, 2000) gin_trgm_ops);
