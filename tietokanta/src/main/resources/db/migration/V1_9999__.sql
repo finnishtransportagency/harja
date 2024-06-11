ac0c5cd91bbcb561f206667009a4f747f4e0d25c
Samuli Taskila <samuli.taskila@solita.fi>
2024-06-11 18:29:58 +0300

    Lisää luoja toteutuneet_kustannukset tauluun, ja päivitä funktiot

--- tietokanta/src/main/resources/db/migration/V1_9999__.sql
+++ tietokanta/src/main/resources/db/migration/V1_9999__.sql
@@ -2,6 +2,3 @@
 -- Näille olemassa jo rivit : Rahavaraus B, C ja K
 DELETE FROM rahavaraus
       WHERE nimi IN ('Äkilliset hoitotyöt', 'Vahinkojen korvaukset', 'Kannustinjärjestelmä');
-
--- Lisää luoja kolumni toteutuneisiin kustannuksiin 
-ALTER TABLE toteutuneet_kustannukset ADD COLUMN luoja INTEGER REFERENCES kayttaja (id);
