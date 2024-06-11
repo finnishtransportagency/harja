94dc1543cda69697a79f0a644da1c429a8632144
Samuli Taskila <samuli.taskila@solita.fi>
2024-06-11 17:58:40 +0300

    Populoi kannustinjärjrestelmä rahavaraus_id:t

--- tietokanta/src/main/resources/db/migration/R__Rahavaraus_id_populointi.sql
+++ tietokanta/src/main/resources/db/migration/R__Rahavaraus_id_populointi.sql
@@ -1,5 +1,7 @@
 -- Rahavaraus id:n lisäys ja populointi -- SELECT populoi_rahavaraus_idt(); 
 -- Palauttaa päivitetyt rivit yhteenlaskettuna 
+DROP FUNCTION IF EXISTS populoi_rahavaraus_idt();
+
 CREATE OR REPLACE FUNCTION populoi_rahavaraus_idt()
 RETURNS INTEGER AS $$
   DECLARE
@@ -7,10 +9,14 @@ RETURNS INTEGER AS $$
     akilliset_id INT;
     rivit_paivitetty INTEGER := 0;
   BEGIN
+    
     SELECT id INTO akilliset_id FROM rahavaraus WHERE nimi LIKE '%Äkilliset hoitotyöt%' ORDER BY id ASC LIMIT 1;
     SELECT id INTO vahingot_id FROM rahavaraus WHERE nimi LIKE '%Vahinkojen korvaukset%' ORDER BY id ASC LIMIT 1;
+    SELECT id INTO kannustin_id FROM rahavaraus WHERE nimi LIKE '%Kannustinjärjestelmä%' ORDER BY id ASC LIMIT 1;
 
-    -- ~ ~ toteutuneet_kustannukset ~ ~ --
+    ---------------------------------------------
+    --------- toteutuneet_kustannukset ----------
+    ---------------------------------------------
     -- Lisää rahavaraus_id sarakkeet, on olemassa jo parissa taulussa, mutta ei haittaa 
     ALTER TABLE kulu_kohdistus ADD COLUMN IF NOT EXISTS rahavaraus_id INT REFERENCES rahavaraus (id);
     ALTER TABLE kustannusarvioitu_tyo ADD COLUMN IF NOT EXISTS rahavaraus_id INT REFERENCES rahavaraus (id);
@@ -28,7 +34,18 @@ RETURNS INTEGER AS $$
      WHERE tyyppi = 'vahinkojen-korjaukset' 
        AND vahingot_id IS NOT NULL;
 
-    -- ~ ~ kulu_kohdistus ~ ~ --
+    ---------------------------------------------
+    -------------- kulu_kohdistus ---------------
+    ---------------------------------------------
+    -- Kannustinjärjrestelmä (hae yksilöyvällä tunnisteella)
+    UPDATE kulu_kohdistus
+       SET rahavaraus_id = kannustin_id
+     WHERE tehtavaryhma IN (
+          SELECT t.id
+            FROM tehtavaryhma t
+           WHERE t.yksiloiva_tunniste = '0e78b556-74ee-437f-ac67-7a03381c64f6'
+    ) AND kannustin_id IS NOT NULL;
+
     -- Äkilliset hoitotyöt 
     UPDATE kulu_kohdistus
        SET rahavaraus_id = akilliset_id
@@ -41,7 +58,9 @@ RETURNS INTEGER AS $$
      WHERE maksueratyyppi = 'muu'
        AND vahingot_id IS NOT NULL;
 
-    -- ~ ~ kustannusarvioitu_tyo ~ ~ --
+    ---------------------------------------------
+    ----------- kustannusarvioitu_tyo -----------
+    ---------------------------------------------
     -- Äkilliset hoitotyöt 
     UPDATE kustannusarvioitu_tyo
        SET rahavaraus_id = akilliset_id
