3e4b4cefaf048685e2e77e8657723d292de046cd
Samu Mikkonen <samu.mikkonen@solita.fi>
2024-06-14 15:30:47 +0300

    Lisää uusi testidata

--- tietokanta/testidata/yllapito/pot2.sql
+++ tietokanta/testidata/yllapito/pot2.sql
@@ -110,3 +110,432 @@ VALUES
 
 END;
 $$ LANGUAGE plpgsql;
+
+--- Tuoreimman XSD-skeeman mukainen pot2-kohde
+
+WITH urakka AS (INSERT INTO urakka (sampoid, sopimustyyppi, hallintayksikko, nimi, alkupvm, loppupvm,
+                                    tyyppi, urakkanro, urakoitsija)
+    VALUES ('5731290-TES2', 'kokonaisurakka' :: sopimustyyppi, (SELECT id
+                                                                FROM organisaatio
+                                                                WHERE lyhenne = 'POP'),
+            'POT2 testipäällystysurakka',
+            '2023-01-01', '2023-12-31', 'paallystys', 'testitunnus', (SELECT id
+                                                                       FROM organisaatio
+                                                                       WHERE ytunnus = '0651792-4')) RETURNING id),
+     yhatiedot AS (
+         INSERT INTO yhatiedot (urakka, yhatunnus, yhaid, elyt, vuodet, luotu, muokattu)
+             SELECT id,
+                    'YHA5731290',
+                    5731290,
+                    ARRAY ['POP'],
+                    ARRAY [2023],
+                    '2022-07-15T12:00:00.000',
+                    '2022-07-15T12:00:00.000'
+             FROM urakka),
+     sopimus AS (
+         INSERT INTO sopimus (nimi, alkupvm, loppupvm, sampoid, urakka, muokattu)
+             SELECT 'POT2 testipäällystysurakka',
+                    '2023-01-01',
+                    '2023-12-31',
+                    '5731290-TES2',
+                    urakka.id,
+                    '2022-07-15T12:00:00.000'
+             FROM urakka
+             RETURNING id, urakka),
+     yllapitokohde AS (
+         INSERT
+             INTO yllapitokohde (urakka, sopimus, kohdenumero, nimi, tr_numero, tr_alkuosa, tr_alkuetaisyys,
+                                 tr_loppuosa, tr_loppuetaisyys, yllapitokohdetyotyyppi, tunnus, yhaid, yllapitoluokka,
+                                 keskimaarainen_vuorokausiliikenne, lahetetty, lahetys_onnistunut, yllapitokohdetyyppi,
+                                 vuodet, yha_kohdenumero, yha_tr_osoite, karttapaivamaara, yotyo, luotu)
+                 SELECT urakka.id,
+                        sopimus.id,
+                        '1',
+                        'Kirkonkylä - Toppinen 2',
+                        86,
+                        20,
+                        0,
+                        20,
+                        1300,
+                        'paallystys',
+                        'testitunnus',
+                        123456,
+                        '1',
+                        1000,
+                        '2022-07-15T12:00:00.000',
+                        TRUE,
+                        'paallyste',
+                        ARRAY [2023],
+                        '1',
+                        (86, 20, 0, 20, 1300, NULL)::tr_osoite,
+                        '2022-07-15T12:00:00.000',
+                        FALSE,
+                        '2022-07-15T12:00:00.000'
+                 FROM urakka,
+                      sopimus RETURNING id),
+     alikohde AS (INSERT
+         INTO yllapitokohdeosa (yllapitokohde, tr_numero, tr_alkuosa, tr_alkuetaisyys, tr_loppuosa, tr_loppuetaisyys,
+                                sijainti, yhaid, tr_ajorata, tr_kaista, muokattu, luotu)
+             SELECT yllapitokohde.id,
+                    tr_numero,
+                    tr_alkuosa,
+                    tr_alkuetaisyys,
+                    tr_loppuosa,
+                    tr_loppuetaisyys,
+                    tierekisteriosoitteelle_viiva(tr_numero, tr_alkuosa, tr_alkuetaisyys, tr_loppuosa,
+                                                  tr_loppuetaisyys),
+                    alikohde.id,
+                    1,
+                    11,
+                    '2022-07-15T12:00:00.000',
+                    '2022-07-15T12:00:00.000'
+             FROM yllapitokohde,
+                  (SELECT 86   AS tr_numero,
+                          20   AS tr_alkuosa,
+                          0    AS tr_alkuetaisyys,
+                          20   AS tr_loppuosa,
+                          650  AS tr_loppuetaisyys,
+                          123457 AS id
+                   UNION ALL
+                   SELECT 86   AS tr_numero,
+                          20   AS tr_alkuosa,
+                          650  AS tr_alkuetaisyys,
+                          20   AS tr_loppuosa,
+                          1300 AS tr_loppuetaisyys,
+                          123458 AS id) AS alikohde RETURNING *),
+     aikataulu AS (
+         INSERT INTO yllapitokohteen_aikataulu (yllapitokohde, kohde_alku, paallystys_alku, paallystys_loppu,
+                                                valmis_tiemerkintaan, tiemerkinta_takaraja, tiemerkinta_alku,
+                                                tiemerkinta_loppu, kohde_valmis, muokattu, luotu)
+             SELECT yllapitokohde.id,
+                    '2023-01-01',
+                    '2023-06-01',
+                    '2023-08-01',
+                    '2023-08-01',
+                    '2023-08-01',
+                    '2023-06-01',
+                    '2023-08-01',
+                    '2023-08-01',
+                    '2022-07-15T12:00:00.000',
+                    '2022-07-15T12:00:00.000'
+             FROM yllapitokohde RETURNING id),
+     kustannukset AS (
+         INSERT INTO yllapitokohteen_kustannukset (yllapitokohde, sopimuksen_mukaiset_tyot, arvonvahennykset,
+                                                   bitumi_indeksi, kaasuindeksi, toteutunut_hinta, muokattu,
+                                                   maaramuutokset, maku_paallysteet)
+             SELECT yllapitokohde.id,
+                    100000,
+                    0,
+                    -100,
+                    -100,
+                    0,
+                    '2022-07-15T12:00:00.000',
+                    -1000,
+                    100
+             FROM yllapitokohde),
+     paallystysilmoitus AS (
+         INSERT INTO paallystysilmoitus (paallystyskohde, luotu, muokattu, poistettu, takuupvm, paatos_tekninen_osa,
+                                         kasittelyaika_tekninen_osa, tila, versio)
+             SELECT yllapitokohde.id,
+                    '2023-12-15T12:00:00.000',
+                    '2023-12-15T12:00:00.000',
+                    FALSE,
+                    '2023-08-01',
+                    'hyvaksytty',
+                    '2023-08-01',
+                    'lukittu',
+                    2
+             FROM yllapitokohde RETURNING id),
+     massa AS (
+         INSERT INTO pot2_mk_urakan_massa (urakka_id, tyyppi, nimen_tarkenne, max_raekoko, kuulamyllyluokka, dop_nro,
+                                           muokattu)
+             SELECT urakka.id,
+                    (SELECT koodi FROM pot2_mk_massatyyppi WHERE nimi = 'AB, Asfalttibetoni'),
+                    'RAAHE',
+                    16,
+                    'AN10',
+                    '7-23-1',
+                    '2022-07-15T12:00:00.000'
+             FROM urakka RETURNING id),
+     murske AS (
+         INSERT INTO pot2_mk_urakan_murske (id, urakka_id, nimen_tarkenne, tyyppi, esiintyma, rakeisuus, "iskunkestavyys", dop_nro, poistettu, muokkaaja, muokattu, luoja, luotu)
+             SELECT massa.id,
+                    urakka.id,
+                    'LJYR',
+                    1,
+                    'Tonkkulan Kaivo',
+                    '0/40'::"murskeen_rakeisuus",
+                    'LA30'::"iskunkestavyys",
+                    '1234567-dop', 
+					false, 
+					NULL, 
+					NULL, 
+					13, 
+					'2023-11-02T12:00:00.000' 
+             FROM urakka, massa RETURNING id),        
+     runkoaine AS (
+         INSERT INTO pot2_mk_massan_runkoaine (pot2_massa_id, tyyppi, esiintyma, fillerityyppi, kuvaus, kuulamyllyarvo,
+                                               litteysluku, massaprosentti)
+             SELECT massa.id,
+                    (SELECT koodi FROM pot2_mk_runkoainetyyppi WHERE nimi = 'Kiviaines'),
+                    'Alpua',
+                    NULL,
+                    NULL,
+                    10,
+                    20,
+                    100
+             FROM massa
+             UNION ALL
+             SELECT massa.id,
+                    (SELECT koodi FROM pot2_mk_runkoainetyyppi WHERE lyhenne = 'Filleri'),
+                    NULL,
+                    'Kalkkifilleri (KF)'::fillerityyppi,
+                    NULL,
+                    NULL,
+                    NULL,
+                    1
+             FROM massa),
+     sideaine AS (
+         INSERT INTO pot2_mk_massan_sideaine (pot2_massa_id, "lopputuote?", tyyppi, pitoisuus)
+             SELECT massa.id,
+                    TRUE,
+                    (SELECT koodi FROM pot2_mk_sideainetyyppi WHERE nimi = 'Bitumi, 20/30'),
+                    5.5
+             FROM massa),
+     lisaaine AS (
+         INSERT INTO pot2_mk_massan_lisaaine (pot2_massa_id, tyyppi, pitoisuus)
+             SELECT massa.id,
+                    (SELECT koodi FROM pot2_mk_lisaainetyyppi WHERE nimi = 'Kuitu'),
+                    0.5
+             FROM massa),
+     alusta AS (
+         INSERT INTO pot2_alusta (tr_numero, tr_alkuetaisyys, tr_alkuosa, tr_loppuetaisyys, tr_loppuosa, tr_ajorata,
+                                  tr_kaista, toimenpide, pot2_id, massamenekki, massa, lisatty_paksuus, kasittelysyvyys, verkon_tyyppi, kokonaismassamaara, murske)
+             SELECT tr_numero,
+                    tr_alkuetaisyys,
+                    tr_alkuosa,
+                    tr_loppuetaisyys,
+                    tr_loppuosa,
+                    tr_ajorata,
+                    tr_kaista,
+                    (SELECT koodi FROM pot2_mk_alusta_toimenpide WHERE lyhenne = 'TAS'),
+                    paallystysilmoitus.id,
+                    0.1,
+                    massa.id,
+                    12,
+                    300,
+                    5,
+                    10.2,
+                    murske.id
+             FROM alikohde,
+                  paallystysilmoitus,
+                  massa,
+                  murske
+             RETURNING *),
+     kulutuskerros AS (
+         INSERT INTO pot2_paallystekerros (kohdeosa_id, toimenpide, materiaali, leveys, pinta_ala, kokonaismassamaara,
+                                           piennar, pot2_id, massamenekki)
+             SELECT alikohde.id,
+                    (SELECT koodi FROM pot2_mk_paallystekerros_toimenpide WHERE lyhenne = 'MP'),
+                    massa.id,
+                    4,
+                    2600,
+                    260,
+                    FALSE,
+                    paallystysilmoitus.id,
+                    100
+             FROM alikohde,
+                  massa,
+                  paallystysilmoitus RETURNING *),
+     yllapitokohde2 AS (
+         INSERT INTO yllapitokohde (urakka, sopimus, kohdenumero, nimi, tr_numero, tr_alkuosa, tr_alkuetaisyys,
+                                    tr_loppuosa, tr_loppuetaisyys, yllapitokohdetyotyyppi,
+                                    lahetetty, lahetys_onnistunut, yllapitokohdetyyppi,
+                                    vuodet, luotu, muokattu)
+             SELECT urakka.id,
+                    sopimus.id,
+                    '2',
+                    'MT 86 Paavolantie 2',
+                    86,
+                    20,
+                    0,
+                    20,
+                    100,
+                    'paallystys',
+                    '2023-11-02T12:00:00.000',
+                    TRUE,
+                    'paallyste',
+                    ARRAY [2023],
+                    '2023-11-01T12:00:00.000',
+                    '2023-11-02T12:00:00.000'
+             FROM urakka,
+                  sopimus RETURNING id),
+     aikataulu2 AS (
+         INSERT INTO yllapitokohteen_aikataulu (yllapitokohde, kohde_alku, paallystys_alku, paallystys_loppu,
+                                                valmis_tiemerkintaan, tiemerkinta_takaraja, tiemerkinta_alku,
+                                                tiemerkinta_loppu, kohde_valmis, muokattu, luotu)
+             SELECT yllapitokohde2.id,
+                    '2023-11-01',
+                    '2023-12-01',
+                    '2024-01-01',
+                    '2024-01-01',
+                    '2024-01-01',
+                    '2024-01-01',
+                    '2024-01-01',
+                    '2024-01-01',
+                    '2023-07-15T12:00:00.000',
+                    '2023-07-15T12:00:00.000'
+             FROM yllapitokohde2 RETURNING id),
+     paikkauskohteet AS (
+         INSERT INTO paikkauskohde ("ulkoinen-id", nimi, poistettu, luotu, muokattu,
+                                    "urakka-id", "yhalahetyksen-tila", tarkistettu, alkupvm, loppupvm, tilattupvm,
+                                    tyomenetelma, tierekisteriosoite_laajennettu, "paikkauskohteen-tila",
+                                    "suunniteltu-maara", "suunniteltu-hinta", yksikko, "pot?", valmistumispvm,
+                                    tiemerkintapvm, "toteutunut-hinta", "tiemerkintaa-tuhoutunut?", takuuaika,
+                                    "yllapitokohde-id", "yhalahetyksen-aika")
+             SELECT 1,
+                    'MT 86 Paavolantie',
+                    FALSE,
+                    '2023-11-01T12:00:00.000'::TIMESTAMP,
+                    '2023-11-02T12:00:00.000'::TIMESTAMP,
+                    urakka.id,
+                    'lahetetty'::lahetyksen_tila,
+                    '2023-11-02T12:00:00.000'::TIMESTAMP,
+                    '2023-11-01'::DATE,
+                    '2023-11-02'::DATE,
+                    '2023-11-01'::DATE,
+                    (SELECT id FROM paikkauskohde_tyomenetelma WHERE nimi = 'AB-paikkaus levittäjällä'),
+                    (86, 20, 700, 20, 800, 1, NULL, NULL, NULL, NULL)::tr_osoite_laajennettu,
+                    'valmis'::paikkauskohteen_tila,
+                    700,
+                    1000,
+                    'm2',
+                    FALSE,
+                    '2023-11-02'::DATE,
+                    '2023-11-02'::DATE,
+                    1001.10,
+                    FALSE,
+                    1,
+                    NULL,
+                    '2023-11-02T12:00:00.000'::TIMESTAMP
+             FROM urakka
+             UNION ALL
+             SELECT 2,
+                    'MT 86 Paavolantie 2',
+                    FALSE,
+                    '2023-11-01T12:00:00.000'::TIMESTAMP,
+                    '2023-11-02T12:00:00.000'::TIMESTAMP,
+                    urakka.id,
+                    'lahetetty'::lahetyksen_tila,
+                    '2023-11-02T12:00:00.000'::TIMESTAMP,
+                    '2023-11-01'::DATE,
+                    '2023-11-02'::DATE,
+                    '2023-11-01'::DATE,
+                    (SELECT id FROM paikkauskohde_tyomenetelma WHERE nimi = 'SMA-paikkaus levittäjällä'),
+                    (86, 20, 0, 20, 100, NULL, NULL, NULL, NULL, NULL)::tr_osoite_laajennettu,
+                    'valmis'::paikkauskohteen_tila,
+                    100,
+                    200,
+                    'm2',
+                    TRUE,
+                    '2023-11-02'::DATE,
+                    '2023-11-02'::DATE,
+                    201.10,
+                    FALSE,
+                    1,
+                    yllapitokohde2.id,
+                    '2023-11-02T12:00:00.000'::TIMESTAMP
+             FROM urakka,
+                  yllapitokohde2 RETURNING *),
+     paallystysilmoitus2 AS (
+         INSERT INTO paallystysilmoitus (paallystyskohde, luotu, muokattu, poistettu, takuupvm, paatos_tekninen_osa,
+                                         kasittelyaika_tekninen_osa, tila, versio)
+             SELECT yllapitokohde2.id,
+                    '2023-11-02T12:00:00.000',
+                    '2023-11-02T12:00:00.000',
+                    FALSE,
+                    '2023-11-02',
+                    'hyvaksytty',
+                    '2023-11-02',
+                    'lukittu',
+                    2
+             FROM yllapitokohde2 RETURNING id),
+     alusta2 AS (
+         INSERT INTO pot2_alusta (tr_numero, tr_alkuetaisyys, tr_alkuosa, tr_loppuetaisyys, tr_loppuosa, tr_ajorata,
+                                  tr_kaista, toimenpide, pot2_id, kasittelysyvyys, leveys, pinta_ala)
+             SELECT 86,
+                    0,
+                    20,
+                    100,
+                    20,
+                    1,
+                    11,
+                    (SELECT koodi FROM pot2_mk_alusta_toimenpide WHERE nimi = 'Laatikkojyrsintä'),
+                    paallystysilmoitus2.id,
+                    4,
+                    7,
+                    700
+             FROM paallystysilmoitus2
+             RETURNING *),
+     alikohde2 AS (
+         INSERT INTO yllapitokohdeosa (yllapitokohde, tr_numero, tr_alkuosa, tr_alkuetaisyys, tr_loppuosa,
+                                       tr_loppuetaisyys,
+                                       sijainti, yhaid, tr_ajorata, tr_kaista, muokattu, luotu)
+             SELECT yllapitokohde2.id,
+                    86,
+                    20,
+                    0,
+                    20,
+                    100,
+                    tierekisteriosoitteelle_viiva(86, 20, 0, 20,
+                                                  100),
+                    NULL,
+                    1,
+                    11,
+                    '2023-11-02T12:00:00.000',
+                    '2023-11-01T12:00:00.000'
+             FROM yllapitokohde2 RETURNING *),
+     kulutuskerros2 AS (
+         INSERT
+             INTO pot2_paallystekerros (kohdeosa_id, toimenpide, materiaali, leveys, pinta_ala, kokonaismassamaara,
+                                        piennar, pot2_id, massamenekki)
+                 SELECT alikohde2.id,
+                        (SELECT koodi FROM pot2_mk_paallystekerros_toimenpide WHERE lyhenne = 'LTA'),
+                        massa.id,
+                        4,
+                        400,
+                        46,
+                        FALSE,
+                        paallystysilmoitus2.id,
+                        115
+                 FROM alikohde2,
+                      massa,
+                      paallystysilmoitus2 RETURNING *),
+     paikkaus AS (
+         INSERT INTO paikkaus (luotu, muokattu, "urakka-id", "paikkauskohde-id", "ulkoinen-id", alkuaika, loppuaika,
+                               tierekisteriosoite, tyomenetelma,
+                               massatyyppi, leveys, raekoko, kuulamylly, sijainti, massamaara, "pinta-ala", lahde,
+                               massamenekki)
+             SELECT '2023-11-01T12:00:00'::DATE,
+                    '2023-11-02T13:00:00'::TIMESTAMP,
+                    urakka.id,
+                    paikkauskohteet.id,
+                    0,
+                    '2023-11-02T12:00:00'::TIMESTAMP,
+                    '2023-11-02T13:00:00'::TIMESTAMP,
+                    (86, 20, 700, 20, 800, NULL)::tr_osoite,
+                    paikkauskohteet.tyomenetelma,
+                    'AB, Asfalttibetoni',
+                    1.4,
+                    16,
+                    'AN14',
+                    (SELECT tierekisteriosoitteelle_viiva(86, 20, 700, 20, 800)),
+                    6.3,
+                    140,
+                    'harja-ui',
+                    45
+             FROM urakka,
+                  paikkauskohteet
+             WHERE paikkauskohteet."pot?" = FALSE)
+SELECT *
+FROM kulutuskerros;
