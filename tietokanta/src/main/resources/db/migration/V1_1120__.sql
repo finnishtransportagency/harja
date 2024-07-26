ea79ea9220a5dc25cc14cd702dfad88e3d7e4874
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-07-26 08:14:29 +0300

    Poista tupla analytiikka-hae-tehtavat

--- tietokanta/src/main/resources/db/migration/V1_1120__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1120__.sql
@@ -2,6 +2,7 @@
 INSERT INTO integraatio (jarjestelma, nimi) VALUES ('analytiikka', 'analytiikka-hae-toimenpiteet');
 INSERT INTO integraatio (jarjestelma, nimi) VALUES ('analytiikka', 'analytiikka-hae-rahavaraukset');
 
+    
 -- Koska analytiikkaan liittyviä integraatioita on useita, päivitetään ne kaikki kerralla analytiikka järjestelmän alle
 UPDATE integraatio
    SET jarjestelma = 'analytiikka'
@@ -12,5 +13,5 @@ UPDATE integraatio
                 'analytiikka-hae-paallystysurakat', 'analytiikka-hae-toteumat',
                 'analytiikka-hae-turvallisuuspoikkeamat', 'analytiikka-hae-suunnitellut-tehtavamaarat',
                 'analytiikka-hae-suunnitellut-materiaalimaarat', 'analytiikka-hae-organisaatiot',
-                'analytiikka-hae-urakat', 'analytiikka-hae-tehtavat', 'analytiikka-hae-materiaalikoodit'
+                'analytiikka-hae-urakat', 'analytiikka-hae-materiaalikoodit'
      );
