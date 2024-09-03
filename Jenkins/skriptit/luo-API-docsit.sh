690dae1990847435403633507ac4dbd31a489673
Maarit Laine <maarit.laine@solita.fi>
2024-09-03 08:42:59 +0300

    Muuta apidokumentaation generointia

--- Jenkins/skriptit/luo-API-docsit.sh
+++ Jenkins/skriptit/luo-API-docsit.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-mkdir -p resources/public/apidoc
-cp -r resources/api/documentation/ resources/public/apidoc/
-raml2html resources/api/api.raml > resources/public/apidoc/api.html
-rm -f resources/api/api.html
-cp resources/public/apidoc/api.html resources/api/
-zip -r resources/public/apidoc/api.zip resources/api/ 
-cp resources/api/api.raml resources/public/apidoc
+rm -f ../../resources/api/api.html
+raml2html ../../resources/api/api.raml > ../../resources/api/api.html
+mkdir -p ../../apidoc
+cp -r ../../resources/api/ ../../apidoc
+zip -r ../../apidoc/api.zip ../../resources/api/
