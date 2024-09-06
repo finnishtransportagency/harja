98ddde58483d74bda92c9fcdc8128dedd43baa66
Samuli Taskila <samuli.taskila@solita.fi>
2024-09-06 15:02:39 +0300

    Korjaa tehtäväryhmän nimi täsmäämään uutta develop branchia

--- tietokanta/src/main/resources/db/migration/V1_1133__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1133__.sql
@@ -634,7 +634,7 @@ UPDATE tehtava
 -- Tässä tehdään nuo toimenpideinstanssi kytkökset oikein, jonka jälkeen kulut näytetään oikein sekä niitä pystytään muokata
 --
 -- Päivittää siis 'Liikenneympäristön hoito -> Päällysteiden paikkaukset'
--- -> Päällysteiden paikkaus (hoidon ylläpito)	Päällysteiden paikkaus (Y)
+-- -> Päällysteiden paikkaus (hoidon ylläpito)	Päällysteiden paikkaus, muut työt (Y)
 
 DO
 $$
@@ -651,7 +651,7 @@ $$
         SELECT id
           INTO tehtavaryhma_id
           FROM tehtavaryhma t
-         WHERE t.nimi = 'Päällysteiden paikkaus (Y)';
+         WHERE t.nimi = 'Päällysteiden paikkaus, muut työt (Y)';
 
         -- Looppaa kaikki teiden-hoito urakat
         FOR urakka_id IN
@@ -683,11 +683,11 @@ $$
 
                     -- Päivitetään rivejä mikäli niitä on
                     UPDATE kulu_kohdistus
--- Aseta uusi toimenpideinstanssi 'Päällystepaikkaukset'
+                       -- Aseta uusi toimenpideinstanssi 'Päällystepaikkaukset'
                        SET toimenpideinstanssi = tpi_paallystepaikkaus,
                            muokattu            = CURRENT_TIMESTAMP,
                            muokkaaja           = (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio')
--- Missä tehtäväryhmä on päällysteiden paikkaus
+                     -- Missä tehtäväryhmä on päällysteiden paikkaus
                      WHERE tehtavaryhma = tehtavaryhma_id
                        -- Missä urakan toimenpideinstanssi on vanha '23116' 'Liikenneympäristön hoito'
                        AND toimenpideinstanssi = (
