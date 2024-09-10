0bc2d54be6d9ab6ab37907af56c303a2d17fd54d
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-09-10 11:08:19 +0300

    Nimeä talvihoitoreitit näkymä tiedosto uusiksi

--- src/cljs/harja/views/urakka/laadunseuranta/talvihoitoreitit_nakyma.cljs
+++ src/cljs/harja/views/urakka/laadunseuranta/talvihoitoreitit_nakyma.cljs
@@ -1,4 +1,4 @@
-(ns harja.views.urakka.laadunseuranta.talvihoitoreitit
+(ns harja.views.urakka.laadunseuranta.talvihoitoreitit-nakyma
   "Sanktioiden ja bonusten välilehti"
   (:require [harja.fmt :as fmt]
             [reagent.core :as r]
@@ -7,7 +7,7 @@
             [harja.views.kartta :as kartta]
             [harja.views.kartta.tasot :as kartta-tasot]
             [harja.tiedot.navigaatio :as nav]
-            [harja.tiedot.urakka.laadunseuranta.talvihoitoreitit :as tiedot]
+            [harja.tiedot.urakka.laadunseuranta.talvihoitoreitit-tiedot :as tiedot]
             [harja.ui.grid :as grid]
             [harja.ui.yleiset :refer [ajax-loader] :as yleiset]
             [harja.ui.debug :as debug]
@@ -20,15 +20,19 @@
         valitut-kohteet @tiedot/valitut-kohteet-atom]
     [:div
      [kartta/kartan-paikka]
-     [:h2 "Talvihoitoreititys"]
-     (doall
-       (for [rivi talvihoitoreitit
-             :let [reittien-maara (count (:reitit rivi))
-                   auki? (contains? (:talvihoitoreittien-tilat app) (:id rivi))]]
+     [:div.flex-row
+      [:h2 "Talvihoitoreititys"]
+      [:div "nappi"]]
+     (if (empty? talvihoitoreitit)
+       [:div "Ei talvihoitoreittejä. Aloita tuomalla reitit käyttäen excel-tiedostoa."]
+       (doall
+         (for [rivi talvihoitoreitit
+               :let [reittien-maara (count (:reitit rivi))
+                     auki? (contains? (:talvihoitoreittien-tilat app) (:id rivi))]]
            ^{:key (:id rivi)}
            [:<>
             [:div.flex-row.venyta.otsikkokomponentti {:class (str "" (when (> reittien-maara 0) " klikattava"))
-                                                      :on-click #(when (> reittien-maara 0) (e! (tiedot/->AvaaTalvihoitoreitti (:id rivi))) #_ (avaa-rivi-rn (:id rivi) e! rivi))}
+                                                      :on-click #(when (> reittien-maara 0) (e! (tiedot/->AvaaTalvihoitoreitti (:id rivi))) #_(avaa-rivi-rn (:id rivi) e! rivi))}
              ;; Nuoli
              [:div.basis48.nogrow
               (when (> reittien-maara 0)
@@ -40,32 +44,34 @@
              [:div.basis256.nogrow.shrink3.rajaus
               [:div {:style {:display "flex"}}
                [:span {:style {:margin-top "auto" :margin-bottom "auto" :margin-right "5px" :width "12px" :height "12px" :background-color (:vari rivi)}}]
-               [:h3.ei-marginia (str (:nimi rivi))]]
+               [:div.semibold.musta.body-text (str (:nimi rivi))]]
               [:div.body-text.harmaa (:pituus rivi)]]
 
              [:div.basis256.grow2.shrink3.rajaus
-              [:div.body-text.strong.musta "Hoitoluokkien osuudet reitillä (km)"]
+              [:div.body-text.semibold.musta "Hoitoluokkien osuudet reitillä (km)"]
+              [:div {:style {:display "flex"}}
+               (doall (for [h (:hoitoluokat rivi)]
+                        ^{:key (hash (str "hoitoluokka-" h))}
+                        [:div
+                         [:div.body-text.musta {:style {:padding-right "10px"}} (:hoitoluokka-str h)]
+                         [:div.small-text.musta {:style {:padding-right "10px"}} (fmt/desimaaliluku-opt (:pituus h) 2) ]]))]]
+
+             [:div.basis256.grow2.shrink3.rajaus
+              [:div.body-text.semibold.musta "Kalusto (kpl)"]
               [:div {:style {:display "flex"}}
                (doall (for [reitti (:reitit rivi)]
                         ^{:key (:id reitti)}
-                        [:div.body-text.musta {:style {:padding-right "10px"}}
-                         (str (:hoitoluokka-str reitti) " " (:pituus reitti))]))]]
-
-             [:div.basis256.grow2.shrink3.rajaus
-              [:div.body-text.strong.musta "Kalusto (kpl)"]
-              [:div {:style {:display "flex"}}
-               (doall (for [kalusto (:kalustot rivi)]
-                        ^{:key (:id kalusto)}
-                        [:div.body-text.musta {:style {:padding-right "10px"}}
-                         (str (:kalustotyyppi kalusto) " " (:maara kalusto))]))]]
+                        [:div
+                         [:div.body-text.musta {:style {:padding-right "10px"}} (:kalustotyyppi reitti)]
+                         [:div.small-text.musta {:style {:padding-right "10px"}} (:kalustomaara reitti) ]]))]]
 
              [:div.basis256.grow2.shrink3
               [:div.body-text.strong.musta ""]
               ;; Näytä valittu rivi kartalla tai piilota se
               [:<>
-                 (if (contains? valitut-kohteet (:id rivi))
-                   (harja.ui.napit/avaa "Piilota kartalta" #(e! (tiedot/->PoistaValittuKohdeKartalta (:id rivi))))
-                   (harja.ui.napit/avaa "Näytä kartalla" #(e! (tiedot/->LisaaValittuKohdeKartalle (:id rivi)))))]]]
+               (if (contains? valitut-kohteet (:id rivi))
+                 (harja.ui.napit/avaa "Piilota kartalta" #(e! (tiedot/->PoistaValittuKohdeKartalta (:id rivi))))
+                 (harja.ui.napit/avaa "Näytä kartalla" #(e! (tiedot/->LisaaValittuKohdeKartalle (:id rivi)))))]]]
 
             ;; Otsikkokoponentin voi avata ja avaamisen jälkeen näytetään lista (grid) reiteistä
             (when (get talvihoitoreittien-tilat (:id rivi))
@@ -80,18 +86,20 @@
                   {:otsikko "Hoitoluokka" :nimi :hoitoluokka-str :tyyppi :string :tasaa :vasen}
                   {:otsikko "Suunniteltu pituus (km)" :nimi :pituus :tyyppi :numero :fmt #(fmt/desimaaliluku-opt % 2) :tasaa :oikea}
                   {:otsikko "Laskettu pituus (km)" :nimi :laskettu_pituus :tyyppi :numero :fmt #(fmt/desimaaliluku-opt % 2) :tasaa :oikea}]
-                 (:reitit rivi)]))]))]))
+                 (:reitit rivi)]))])))]))
 
 (defn *talvihoitoreitit [e! app]
   (komp/luo
     (komp/sisaan-ulos
       #(do
-         (reset! tiedot/valitut-kohteet-atom #{})
-         (kartta-tasot/taso-paalle! :talvihoitoreitit)
-         (e! (tiedot/->HaeTalvihoitoreitit))
-         (reset! tiedot/karttataso-nakyvissa? true)
          (reset! nav/kartan-edellinen-koko @nav/kartan-koko)
-         (nav/vaihda-kartan-koko! :M))
+         (nav/vaihda-kartan-koko! :L)
+         (reset! tiedot/valitut-kohteet-atom #{})
+         (e! (tiedot/->HaeTalvihoitoreitit))
+         (kartta-tasot/taso-paalle! :talvihoitoreitit)
+         (reset! tiedot/karttataso-nakyvissa? true)
+         (js/console.log "sisään"))
+
       #(do
          (kartta-tasot/taso-pois! :talvihoitoreitit)
          (reset! tiedot/karttataso-nakyvissa? false)))
