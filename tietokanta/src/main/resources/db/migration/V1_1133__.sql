45d173b403b16ebb0f7db29bc348c80aac227c57
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-09-11 11:03:36 +0300

    Lisää tarvittavat toimenkuvat tietokantaan

---
+++ tietokanta/src/main/resources/db/migration/V1_1133__.sql
@@ -0,0 +1,4 @@
+-- Kustannussuunnitelman johto ja hallintokorvaukset vaativat toimenkuvat '2. työnjohtaja' ja '3. työnjohtaja'.
+-- jotta niitä voidaan tallentaa kantaan.
+INSERT INTO johto_ja_hallintokorvaus_toimenkuva (toimenkuva) VALUES ('2. työnjohtaja');
+INSERT INTO johto_ja_hallintokorvaus_toimenkuva (toimenkuva) VALUES ('3. työnjohtaja');
