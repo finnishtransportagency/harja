c1d551b95ca30480d76eaefca75ea937ecfc5ae3
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-09-19 07:21:21 +0300

    Lisää talvihoitoreitti API:lle lisää testejä

--- test/clj/harja/palvelin/integraatiot/api/talvihoitoreitti_kirjaus_test.clj
+++ test/clj/harja/palvelin/integraatiot/api/talvihoitoreitti_kirjaus_test.clj
@@ -1,15 +1,9 @@
 (ns harja.palvelin.integraatiot.api.talvihoitoreitti-kirjaus-test
   (:require [clojure.test :refer [deftest is use-fixtures testing]]
-            [harja.kyselyt.konversio :as konversio]
-            [harja.palvelin.integraatiot.api.tyokalut.apurit :as apurit]
             [harja.testi :refer :all]
             [harja.palvelin.integraatiot.api.talvihoitoreitit-api :as api-talvihoitoreitit]
             [com.stuartsierra.component :as component]
             [harja.palvelin.integraatiot.api.tyokalut :as tyokalut]
-            [harja.palvelin.integraatiot.api.tyokalut.json :as json-tyokalut]
-            [specql.core :refer [fetch columns]]
-            [harja.domain.reittipiste :as rp]
-            [clojure.data.json :as json]
             [cheshire.core :as cheshire]))
 
 (def kayttaja "destia")
@@ -116,9 +110,29 @@
         ;; Tarkistetaan, että toinen talvihoitoreitti on poistettu
         talvihoitoreitti-kannassa (q-map (format "SELECT nimi FROM talvihoitoreitti
           WHERE urakka_id = %s" urakka-id))
-        _ (is (= 0 (count talvihoitoreitti-kannassa)))
-        ]))
+        _ (is (= 0 (count talvihoitoreitti-kannassa)))]))
 
+(deftest tallenna-talvihoitoreitti-epaonnistuu
+  (let [urakka-id (hae-urakan-id-nimella "Oulun MHU 2019-2024")
+        ;; Siivotaan kanta varulta
+        _ (poista-talvihoitoreitit-urakalta urakka-id)
+        ;; Anna oikeudet käyttäjälle
+        _ (anna-kirjoitusoikeus kayttaja-yit)
+        ulkoinen-id 123456
+        lahetysdata (-> "test/resurssit/api/talvihoitoreitti-nok.json"
+                      slurp
+                      (.replace "__ULKOINENID__" (str ulkoinen-id))
+                      (.replace "__NIMI__" "testinimi"))
+        vastaus (tyokalut/put-kutsu ["/api/urakat/" urakka-id "/talvihoitoreitti"] kayttaja-yit portti
+                         lahetysdata)
+        encoodattu-body (cheshire/decode (:body vastaus) true)
+        virhe (first (:virheet encoodattu-body))]
+
+    (is (= 400 (:status vastaus)))
+    (is (= "invalidi-json" (get-in virhe [:virhe :koodi])))
+    (is (.contains (get-in virhe [:virhe :viesti]) "hoitoluokka"))
+    (is (.contains (get-in virhe [:virhe :viesti]) ":error :invalid-enum-value"))
+    (is (.contains (get-in virhe [:virhe :viesti]) "tie: Väärä tyyppi"))))
 
 
 
