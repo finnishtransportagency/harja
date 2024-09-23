a95bee18fbe01da242095c79703ef98fe08779df
Samuli Taskila <samuli.taskila@solita.fi>
2024-09-23 12:40:55 +0300

    Lisää urakan otsikot laatupoikkeama Exceliin

--- src/clj/harja/palvelin/raportointi/raportit/laatupoikkeama.clj
+++ src/clj/harja/palvelin/raportointi/raportit/laatupoikkeama.clj
@@ -131,7 +131,8 @@
                                                loppupvm)))
      [:taulukko {:otsikko otsikko
                  :tyhja (if (empty? laatupoikkeamarivit) "Ei raportoitavia laatupoikkeamia.")
-                 :sheet-nimi raportin-nimi}
+                 :sheet-nimi raportin-nimi
+                 :lisaa-excel-valiotsikot true}
       [{:leveys 15 :otsikko "Päi\u00ADvä\u00ADmää\u00ADrä" :fmt :pvm}
        {:leveys 20 :otsikko "Koh\u00ADde"}
        {:leveys 10 :otsikko "Te\u00ADki\u00ADjä"}
