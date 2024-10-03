340e3f8214b3e542056d62e1f519ea92ecabb72c
Maarit Laine <maarit.laine@solita.fi>
2024-10-03 09:38:52 +0300

    Lisää tarkistuksia api-julkaisuskriptiin

--- sh/julkaise-apidoc.sh
+++ sh/julkaise-apidoc.sh
@@ -3,12 +3,16 @@ while true; do
 read -p "Onko harja/apidoc-kansiossa uusin API-dokumentaatio? (K/E)" ke
 case $ke in
 [Kk]* )
-    echo "Julkaistaan Harja API:n dokumentaatio paikallisesta apidoc-kansiosta.";
-    git checkout gh-pages
-    git add ../apidoc
-    git commit -m 'Päivitä Harja API dokumentaatio'
-    git push
-    break;;
+  githaara_nykyinen=$(git rev-parse --abbrev-ref HEAD)
+  githaara_vaadittu="gh-pages"
+  if [ "$githaara_nykyinen" == "$githaara_vaadittu" ]; then
+      echo "Julkaistaan Harja API:n dokumentaatio apidoc-kansiosta.";
+      sgit commit -m 'Päivitä Harja API dokumentaatio'
+      sgit push
+  else
+      echo "Aja tämä skripti gh-pages-haarassa. Nykyinen git-haara: $githaara_nykyinen"
+  fi
+  break;;
 [Ee]* )
     echo "Päivitä harja/apidoc-kansion sisältö skriptillä ./muodosta-apidoc.sh"
     exit;;
