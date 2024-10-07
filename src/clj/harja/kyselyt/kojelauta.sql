44e550a85fb00caaee9d63086f1069268d7ff6e3
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2024-10-07 14:06:31 +0300

    Suodata testiurakat pois kojelaudasta

--- src/clj/harja/kyselyt/kojelauta.sql
+++ src/clj/harja/kyselyt/kojelauta.sql
@@ -7,11 +7,13 @@ SELECT u.id,
                                          monesko_hoitokausi(u.alkupvm, u.loppupvm, :hoitokauden_alkuvuosi::INTEGER)) as ks_tila
   FROM urakka u
            join organisaatio o ON u.hallintayksikko = o.id
- WHERE u.tyyppi = 'teiden-hoito' AND
+ WHERE
+     u.tyyppi = 'teiden-hoito' AND
+     u.urakkanro IS NOT NULL AND -- testiurakat pois
      (:hoitokauden_alkuvuosi BETWEEN
          (SELECT EXTRACT (YEAR FROM u.alkupvm)) AND
          (SELECT EXTRACT (YEAR FROM u.loppupvm)) - 1) AND
-      (:urakat_annettu IS NOT TRUE OR u.id IN (:urakka_idt)) AND
-      (:ely_id::INTEGER IS NULL OR u.hallintayksikko = :ely_id)
+     (:urakat_annettu IS NOT TRUE OR u.id IN (:urakka_idt)) AND
+     (:ely_id::INTEGER IS NULL OR u.hallintayksikko = :ely_id)
  ORDER BY u.nimi;
 
