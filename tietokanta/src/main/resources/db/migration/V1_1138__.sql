921260bef5111feee802e04641f9af3916600292
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-10-16 07:33:58 +0300

    Korjaa uuden tehtävän tiedot

--- tietokanta/src/main/resources/db/migration/V1_1138__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1138__.sql
@@ -4,7 +4,8 @@ insert into tehtava (nimi, emo, luotu, muokattu, luoja, poistettu, yksikko, jarj
                      suoritettavatehtava, piilota, api_tunnus, tehtavaryhma, "mhu-tehtava?", yksiloiva_tunniste,
                      suunnitteluyksikko, voimassaolo_alkuvuosi, voimassaolo_loppuvuosi, kasin_lisattava_maara,
                      "raportoi-tehtava?", materiaaliluokka_id, materiaalikoodi_id, aluetieto, nopeusrajoitus)
-values  ( 'Tunneleiden ylläpito', 700, '2024-10-10 08:46:02.742654', null,
+values  ( 'Tunneleiden ylläpito',
+         (SELECT id FROM toimenpide WHERE koodi = '20191'), NOW(), null,
           (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio'), false, 'euroa',
          1531, null, null, null, null, null,
           (SELECT id FROM tehtavaryhma WHERE nimi = 'Muut, MHU ylläpito (F)'), true,
@@ -25,5 +26,5 @@ SELECT rv.id,
        (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio')
   FROM rahavaraus rv,
        tehtava
- WHERE tehtava.nimi = 'Levähdys- ja P-alueiden varusteiden vaurioiden kuntoon saattaminen'
-   AND rv.nimi = 'Rahavaraus E - Pysäkkikatokset';
+ WHERE tehtava.nimi = 'Tunneleiden ylläpito'
+   AND rv.nimi = 'Tunneleiden hoito';
