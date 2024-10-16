04d045673b6de477c734f75ddafa1e909440dd2d
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-10-16 11:00:11 +0300

    SQL:ään ajettu formatointi

--- tietokanta/src/main/resources/db/migration/V1_1139__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1139__.sql
@@ -1,23 +1,23 @@
 -- Lisätään uusi tunneleihin liittyvä tehtävä MHU Ylläpito tehtäväryhmälle, jotta Tunneleiden hoito -rahavaraukselle olisi
 -- mahdollista valita MHY Ylläpito tehtäväryhmä
-insert into tehtava (nimi, emo, luotu, muokattu, luoja, poistettu, yksikko, jarjestys, hinnoittelu, api_seuranta,
+INSERT INTO tehtava (nimi, emo, luotu, muokattu, luoja, poistettu, yksikko, jarjestys, hinnoittelu, api_seuranta,
                      suoritettavatehtava, piilota, api_tunnus, tehtavaryhma, "mhu-tehtava?", yksiloiva_tunniste,
                      suunnitteluyksikko, voimassaolo_alkuvuosi, voimassaolo_loppuvuosi, kasin_lisattava_maara,
                      "raportoi-tehtava?", materiaaliluokka_id, materiaalikoodi_id, aluetieto, nopeusrajoitus)
-values  ( 'Tunneleiden ylläpito',
-          (SELECT id FROM toimenpide WHERE koodi = '20191'), NOW(), null,
-          (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio'), false, 'euroa',
-          1531, null, null, null, null, null,
-          (SELECT id FROM tehtavaryhma WHERE nimi = 'Muut, MHU ylläpito (F)'), true,
-          null, 'euroa', null, null,
-          false, false, null, null, false,
-          108);
+VALUES ('Tunneleiden ylläpito',
+        (SELECT id FROM toimenpide WHERE koodi = '20191'), NOW(), NULL,
+        (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio'), FALSE, 'euroa',
+        1531, NULL, NULL, NULL, NULL, NULL,
+        (SELECT id FROM tehtavaryhma WHERE nimi = 'Muut, MHU ylläpito (F)'), TRUE,
+        NULL, 'euroa', NULL, NULL,
+        FALSE, FALSE, NULL, NULL, FALSE,
+        108);
 
 -- Lisätään uusi tehtävä rahavaraukselle
 -- 'Rahavaraus E - Pysäkkikatokset'
 INSERT INTO rahavaraus_tehtava (rahavaraus_id, tehtava_id, luoja)
-VALUES ((SELECT id from rahavaraus WHERE nimi = 'Tunneleiden hoito'),
-        (SELECT id from tehtava WHERE nimi = 'Tunneleiden ylläpito'),
+VALUES ((SELECT id FROM rahavaraus WHERE nimi = 'Tunneleiden hoito'),
+        (SELECT id FROM tehtava WHERE nimi = 'Tunneleiden ylläpito'),
         (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio'));
 
 
