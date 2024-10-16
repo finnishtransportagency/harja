d7f3602c9f112dabef116fe4a91c5a9ecb287bc0
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2024-10-16 08:52:51 +0300

    Korjaa kust.suunn. tilan päättelyn hakuja

--- tietokanta/src/main/resources/db/migration/R__Kojelauta.sql
+++ tietokanta/src/main/resources/db/migration/R__Kojelauta.sql
@@ -8,28 +8,28 @@ DECLARE
     vahvistettuja INTEGER;
     suunnitelman_tila TEXT;
     kaikkien_osioiden_lkm_per_hoitokausi INTEGER;
-    tavoite_ja_kattohinta_kesken BOOLEAN;
+    jokin_osio_aloitettu BOOLEAN;
 
 BEGIN
-    -- jos kaikki ao. osioit on vahvistettu, on ko. hoitokauden osalta kustannussuunnitelma vahvistettu
+    -- jos kaikki osiot on vahvistettu, on ko. hoitokauden osalta kustannussuunnitelma vahvistettu
     kaikkien_osioiden_lkm_per_hoitokausi := 6;
     SELECT COALESCE(count(*), 0) INTO vahvistettuja
                                  FROM suunnittelu_kustannussuunnitelman_tila
                                 WHERE hoitovuosi = _hoitovuosi AND urakka = _urakka AND vahvistettu IS TRUE
-                                  AND osio NOT IN ('tilaajan-rahavaraukset', 'tavoite-ja-kattohinta');
+                                  AND osio NOT IN ('tilaajan-rahavaraukset');
     SELECT COALESCE(count(*), 0) INTO vahvistamattomia
                                  FROM suunnittelu_kustannussuunnitelman_tila
                                 WHERE hoitovuosi = _hoitovuosi AND urakka = _urakka AND vahvistettu IS FALSE
-                                  AND osio NOT IN ('tilaajan-rahavaraukset');
+                                  AND osio NOT IN ('tilaajan-rahavaraukset', 'tavoite-ja-kattohinta');
 
-    -- Jos mikä tahansa osio on edes aloitettu, tavoite- ja kattohintaosio on täten aina aloitettu
+
     SELECT exists(SELECT id from suunnittelu_kustannussuunnitelman_tila WHERE urakka = _urakka and hoitovuosi = _hoitovuosi)
-      INTO tavoite_ja_kattohinta_kesken;
+      INTO jokin_osio_aloitettu;
 
     -- Harja ei erikseen kirjaa jos tavoite- ja kattohinta on "aloitettu", koska minkä tahansa osion aloittaminen
     -- aloittaa myös tavoite- ja kattohinnan. Tässä lisätään se tarvittaessa aloitetuksi yksinkertaisella tarkistuksella.
     -- Poikkeus: jos kaikki osiot on vahvistettu, ei lisätä tavoite- ja kattohintaosiota keskeneräiseksi
-    IF (tavoite_ja_kattohinta_kesken IS TRUE AND vahvistettuja != kaikkien_osioiden_lkm_per_hoitokausi)
+    IF (jokin_osio_aloitettu IS TRUE AND vahvistettuja != kaikkien_osioiden_lkm_per_hoitokausi)
     THEN vahvistamattomia := vahvistamattomia + 1;
     END IF;
 
