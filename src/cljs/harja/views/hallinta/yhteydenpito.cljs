e543b8cb8bd6d2ee7b122348d6eb214602e3d04a
Santtu Seppänen <santtu.seppanen@solita.fi>
2024-11-15 15:02:03 +0200

    Update yhteydenpito.cljs

--- src/cljs/harja/views/hallinta/yhteydenpito.cljs
+++ src/cljs/harja/views/hallinta/yhteydenpito.cljs
@@ -28,9 +28,9 @@
                    "Lähetä"
                    #((varmista-kayttajalta/varmista-kayttajalta
                       {:otsikko "Sähköposti kaikille Harja käyttäjille"
-                        :sisalto [:div "Oletko varma, että haluat lähettää viestin kaikille vuoden sisällä kirjautuneille Harjan käyttäjille?"]
-                        :hyvaksy "Lähetä"
-                        :toiminto-fn (fn [] (e! (tiedot/->Laheta yhteydenotto)))}))
+                       :sisalto [:div "Oletko varma, että haluat lähettää viestin kaikille vuoden sisällä kirjautuneille Harjan käyttäjille?"]
+                       :hyvaksy "Lähetä"
+                       :toiminto-fn (fn [] (e! (tiedot/->Laheta yhteydenotto)))}))
                     {
                       :disabled (or lahetys-kaynnissa?
                                   (not (lomake/voi-tallentaa? yhteydenotto)))}])
