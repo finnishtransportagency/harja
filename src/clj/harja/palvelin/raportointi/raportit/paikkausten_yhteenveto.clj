8a09b49f283dd8bd4c9364d7116e3e05c06db729
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-12-05 13:51:46 +0200

    Refaktoroi koodia

--- src/clj/harja/palvelin/raportointi/raportit/paikkausten_yhteenveto.clj
+++ src/clj/harja/palvelin/raportointi/raportit/paikkausten_yhteenveto.clj
@@ -7,7 +7,7 @@
 
 (defqueries "harja/palvelin/raportointi/raportit/paikkausten_yhteenveto.sql")
 (declare hae-kustannukset-tyomenetelmittain hae-maarat-tyomenetelmittain hae-kasin-lisatyt-paikkauskustannukset
-         hae-reikapaikkauskustannukset-tyomenetelmittain hae-kustannukset-pkluokittain)
+  hae-reikapaikkauskustannukset-tyomenetelmittain hae-kustannukset-pkluokittain)
 
 (defn tyomenetelma-rivi-xf
   "Parsitaan työmenetelmädatasta raportille sopiva rivi."
@@ -16,18 +16,18 @@
    :korosta-hennosti? (:yhteenveto rivi)
    :rivi
    (into []
-         (concat
-           [(:nimi rivi)]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:suunniteltu-hinta rivi)
-                                          :yksikko nil
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? false
-                                          :ryhmitelty? true}]]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:toteutunut-hinta rivi)
-                                          :yksikko nil
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? true
-                                          :ryhmitelty? true}]]))})
+     (concat
+       [(:nimi rivi)]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:suunniteltu-hinta rivi)
+                                      :yksikko nil
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? false
+                                      :ryhmitelty? true}]]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:toteutunut-hinta rivi)
+                                      :yksikko nil
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? true
+                                      :ryhmitelty? true}]]))})
 
 (defn tyomenetelma-maara-rivi-xf
   "Parsitaan tehtävädatasta raportille sopiva rivi."
@@ -35,19 +35,19 @@
   {:lihavoi? nil
    :rivi
    (into []
-         (concat
-           [(:nimi rivi)]
-           [(:yksikko rivi)]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:suunniteltu-maara rivi)
-                                          :yksikko nil
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? false
-                                          :ryhmitelty? true}]]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:toteutunut-maara rivi)
-                                          :yksikko nil
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? true
-                                          :ryhmitelty? true}]]))})
+     (concat
+       [(:nimi rivi)]
+       [(:yksikko rivi)]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:suunniteltu-maara rivi)
+                                      :yksikko nil
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? false
+                                      :ryhmitelty? true}]]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:toteutunut-maara rivi)
+                                      :yksikko nil
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? true
+                                      :ryhmitelty? true}]]))})
 
 (defn reikapaikkaus-rivi-xf
   "Parsitaan tehtävädatasta raportille sopiva rivi."
@@ -56,18 +56,18 @@
    :korosta-hennosti? (:yhteenveto rivi)
    :rivi
    (into []
-         (concat
-           [(:nimi rivi)]
-           [(:yksikko rivi)]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:toteutunut-maara rivi)
-                                          :yksikko nil
-                                          :korosta-hennosti? false
-                                          :ryhmitelty? true}]]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:toteutunut-hinta rivi)
-                                          :yksikko nil
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? true
-                                          :ryhmitelty? true}]]))})
+     (concat
+       [(:nimi rivi)]
+       [(:yksikko rivi)]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:toteutunut-maara rivi)
+                                      :yksikko nil
+                                      :korosta-hennosti? false
+                                      :ryhmitelty? true}]]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:toteutunut-hinta rivi)
+                                      :yksikko nil
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? true
+                                      :ryhmitelty? true}]]))})
 
 (defn muut-kustannukset-rivi-xf
   "Parsitaan raportille sopiva rivi."
@@ -76,13 +76,13 @@
    :korosta-hennosti? (:yhteenveto rivi)
    :rivi
    (into []
-         (concat
-           [(:nimi rivi)]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:toteutunut-hinta rivi)
-                                          :yksikko nil
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? true
-                                          :ryhmitelty? true}]]))})
+     (concat
+       [(:nimi rivi)]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:toteutunut-hinta rivi)
+                                      :yksikko nil
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? true
+                                      :ryhmitelty? true}]]))})
 
 (defn yhteenveto-rivi-xf
   "Parsitaan yhteenvetodatasta raportille sopiva rivi."
@@ -90,22 +90,22 @@
   {:lihavoi? true
    :rivi
    (into []
-         (concat
-           [[:arvo-ja-yksikko-korostettu {:arvo (:tilatut rivi)
-                                          :yksikko "EUR"
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? false
-                                          :ryhmitelty? true}]]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:toteutuneet rivi)
-                                          :yksikko "EUR"
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? false
-                                          :ryhmitelty? true}]]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:muut rivi)
-                                          :yksikko "EUR"
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? false
-                                          :ryhmitelty? true}]]))})
+     (concat
+       [[:arvo-ja-yksikko-korostettu {:arvo (:tilatut rivi)
+                                      :yksikko "EUR"
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? false
+                                      :ryhmitelty? true}]]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:toteutuneet rivi)
+                                      :yksikko "EUR"
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? false
+                                      :ryhmitelty? true}]]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:muut rivi)
+                                      :yksikko "EUR"
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? false
+                                      :ryhmitelty? true}]]))})
 
 (defn pk-rivi-xf
   "Parsitaan yhteenvetodatasta raportille sopiva rivi."
@@ -113,43 +113,43 @@
   {:lihavoi? true
    :rivi
    (into []
-         (concat
-           [[:arvo-ja-yksikko-korostettu {:arvo (:pk1 rivi)
-                                          :yksikko "EUR"
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? false
-                                          :ryhmitelty? true}]]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:pk2 rivi)
-                                          :yksikko "EUR"
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? false
-                                          :ryhmitelty? true}]]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:pk3 rivi)
-                                          :yksikko "EUR"
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? false
-                                          :ryhmitelty? true}]]
-           [[:arvo-ja-yksikko-korostettu {:arvo (:pk-puuttuu rivi)
-                                          :yksikko "EUR"
-                                          :desimaalien-maara 2
-                                          :korosta-hennosti? false
-                                          :ryhmitelty? true}]]))})
+     (concat
+       [[:arvo-ja-yksikko-korostettu {:arvo (:pk1 rivi)
+                                      :yksikko "EUR"
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? false
+                                      :ryhmitelty? true}]]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:pk2 rivi)
+                                      :yksikko "EUR"
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? false
+                                      :ryhmitelty? true}]]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:pk3 rivi)
+                                      :yksikko "EUR"
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? false
+                                      :ryhmitelty? true}]]
+       [[:arvo-ja-yksikko-korostettu {:arvo (:pk-puuttuu rivi)
+                                      :yksikko "EUR"
+                                      :desimaalien-maara 2
+                                      :korosta-hennosti? false
+                                      :ryhmitelty? true}]]))})
 
 (defn- laske-sakko-bonus-arvo [avain data]
   (reduce + (mapv (fn [rivi]
                     (if (= (:laji rivi) avain)
                       (or (:summa rivi) 0)
                       0))
-                  data)))
+              data)))
 
 (defn koosta-muut-kustannukset [db user urakka-id hoitokauden-alkuvuosi alkupvm loppupvm]
   (let [sankiot-ja-bonukset (laadunseuranta-palvelu/hae-urakan-sanktiot-ja-bonukset db user
-                                                                                    {:urakka-id urakka-id
-                                                                                     :alku alkupvm
-                                                                                     :loppu loppupvm
-                                                                                     :vain-yllapitokohteettomat? false
-                                                                                     :hae-sanktiot? true
-                                                                                     :hae-bonukset? true})
+                              {:urakka-id urakka-id
+                               :alku alkupvm
+                               :loppu loppupvm
+                               :vain-yllapitokohteettomat? false
+                               :hae-sanktiot? true
+                               :hae-bonukset? true})
         bonukset (bigdec (laske-sakko-bonus-arvo :yllapidon_bonus sankiot-ja-bonukset))
         sanktiot (bigdec (laske-sakko-bonus-arvo :yllapidon_sakko sankiot-ja-bonukset))
 
@@ -159,7 +159,7 @@
         sakot-ja-bonukset [{:nimi "Bonukset" :toteutunut-hinta bonukset}
                            {:nimi "Sanktiot" :toteutunut-hinta sanktiot}]
         muut-kustannukset (into [] (remove nil? (concat sakot-ja-bonukset
-                                                        (when-not (empty? paikkauskustannukset) paikkauskustannukset))))
+                                                  (when-not (empty? paikkauskustannukset) paikkauskustannukset))))
         yht-muut-kustannukset {:nimi "Yhteensä"
                                :toteutunut-hinta (apply + (map :toteutunut-hinta muut-kustannukset))
                                :yhteenveto true}
@@ -172,7 +172,7 @@
                                                               (if (and rivi (:toteutunut-hinta rivi))
                                                                 (:toteutunut-hinta rivi)
                                                                 0))
-                                                            reikapaikkauskustannukset))
+                                                         reikapaikkauskustannukset))
         reikapaikkauskustannukset-yht {:nimi "Yhteensä"
                                        :toteutunut-hinta toteutuneet-reikapaikkauskustannukset
                                        :yhteenveto true}]
@@ -180,8 +180,8 @@
 
 (defn parsi-pkluokan-kustannukset [kustannusrivit pkluokka]
   (apply + (map #(if (= pkluokka (:pkluokka %))
-                  (:toteutunut-hinta %)
-                  0)
+                   (:toteutunut-hinta %)
+                   0)
              kustannusrivit)))
 
 (defn hae-toteutuneet-tyomentelmakustannukset [db parametrit]
@@ -265,16 +265,16 @@
 
          ;; Toteutuneet paikkauskustannukset PK-luokittain - odottamaan pk-luokkien generointia
          [:taulukko {:otsikko "Toteutuneet paikkauskustannukset PK-luokittain"
-                       :oikealle-tasattavat-kentat #{}
-                       :sheet-nimi "Toteutuneet paikkauskustannukset PK-luokittain"}
-            [{:leveys 1 :otsikko "PK1"}
-             {:leveys 1 :otsikko "PK2"}
-             {:leveys 1 :otsikko "PK3"}
-             {:leveys 1 :otsikko "PK-luokka puuttuu"}]
-            [(pk-rivi-xf {:pk1 pk1-kustannukset
-                          :pk2 pk2-kustannukset
-                          :pk3 pk3-kustannukset
-                          :pk-puuttuu pk-puuttuu-kustannukset})]]
+                     :oikealle-tasattavat-kentat #{}
+                     :sheet-nimi "Toteutuneet paikkauskustannukset PK-luokittain"}
+          [{:leveys 1 :otsikko "PK1"}
+           {:leveys 1 :otsikko "PK2"}
+           {:leveys 1 :otsikko "PK3"}
+           {:leveys 1 :otsikko "PK-luokka puuttuu"}]
+          [(pk-rivi-xf {:pk1 pk1-kustannukset
+                        :pk2 pk2-kustannukset
+                        :pk3 pk3-kustannukset
+                        :pk-puuttuu pk-puuttuu-kustannukset})]]
 
          ;; Kustannukset tehtäväryhmittäin
          [:taulukko {:otsikko "Kustannukset työmenetelmittäin"
