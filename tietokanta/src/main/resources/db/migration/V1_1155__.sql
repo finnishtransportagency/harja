c087e06fcf3dad91d2b9c6ccae0a7e77d3753d7d
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-12-11 07:45:32 +0200

    Muuta pkluokka enumiksi tekstin sijasta.

--- tietokanta/src/main/resources/db/migration/V1_1155__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1155__.sql
@@ -2,9 +2,83 @@
 ALTER TABLE mpu_kustannukset
     RENAME TO paikkauskustannukset;
 
+-- Muutetaan yllapitokohde taulun pkluokka TEXT -> Enum
+--  1. Lisätään uusi tyyppi
+CREATE TYPE korjausluokkatyyppi AS ENUM ('PK1', 'PK2', 'PK3', 'Ei tiedossa');
+
+ALTER TABLE yllapitokohde RENAME COLUMN pkluokka TO _pkluokka;
+
+ALTER TABLE yllapitokohde ADD pkluokka korjausluokkatyyppi;
+
+UPDATE yllapitokohde SET pkluokka = _pkluokka :: TEXT :: korjausluokkatyyppi;
+
+ALTER TABLE yllapitokohde DROP COLUMN _pkluokka;
+
+-- Päivitetään ylläpitokohteelle laskettavan pkluokan tyyppi enumiksi
+CREATE OR REPLACE FUNCTION laske_yllapitokohdeosien_pk_pituudet(yllapitokohde_id INTEGER, pk1geom geometry,
+                                                                pk2geom geometry, pk3geom geometry)
+    RETURNS VOID AS
+$$
+DECLARE
+    yosa           RECORD;
+    yko_pk1_pituus NUMERIC;
+    yko_pk2_pituus NUMERIC;
+    yko_pk3_pituus NUMERIC;
+    ypkluokka      TEXT;
+
+BEGIN
+
+    RAISE NOTICE 'Haetaan yllapitokohdeosat:';
+    yko_pk1_pituus := 0.0;
+    yko_pk2_pituus := 0.0;
+    yko_pk3_pituus := 0.0;
+
+    FOR yosa IN (SELECT y.id,
+                        COALESCE(st_length(y.sijainti), 0)                         AS pituus,
+                        COALESCE(st_length(st_intersection(y.sijainti, pk1geom)), 0) AS pk1_pituus,
+                        COALESCE(st_length(st_intersection(y.sijainti, pk2geom)), 0) AS pk2_pituus,
+                        COALESCE(st_length(st_intersection(y.sijainti, pk3geom)), 0) AS pk3_pituus
+                   FROM yllapitokohdeosa y
+                  WHERE y.yllapitokohde = yllapitokohde_id)
+        LOOP
+            RAISE NOTICE 'Ylläpitokode: % :: osa :: id: % pituus: % pk1_pituus: % pk2_pituus: % pk3_pituus %',
+                yllapitokohde_id, yosa.id, yosa.pituus, yosa.pk1_pituus, yosa.pk2_pituus, yosa.pk3_pituus;
+
+            UPDATE yllapitokohdeosa
+               SET pk1_pituus = yosa.pk1_pituus,
+                   pk2_pituus = yosa.pk2_pituus,
+                   pk3_pituus = yosa.pk3_pituus
+             WHERE id = yosa.id;
+
+            -- Lasketaan yhteen
+            yko_pk1_pituus := yko_pk1_pituus + yosa.pk1_pituus;
+            yko_pk2_pituus := yko_pk2_pituus + yosa.pk2_pituus;
+            yko_pk3_pituus := yko_pk3_pituus + yosa.pk3_pituus;
+
+        END LOOP;
+
+    -- Lisään ylläpitokohteelle pkluokka sen perusteella, mitä pkluokkaa on eniten
+    CASE
+        -- Jos kaikkien pituus on sama (joskus erityisesti 0.0), ei aseteta PK1:tä ettei tiedot vääristy
+        WHEN yko_pk1_pituus = yko_pk2_pituus AND yko_pk2_pituus = yko_pk3_pituus THEN ypkluokka := NULL;
+        WHEN yko_pk1_pituus >= yko_pk2_pituus AND yko_pk1_pituus >= yko_pk3_pituus THEN ypkluokka := 'PK1';
+        WHEN yko_pk2_pituus >= yko_pk1_pituus AND yko_pk2_pituus >= yko_pk3_pituus THEN ypkluokka := 'PK2';
+        WHEN yko_pk3_pituus >= yko_pk1_pituus AND yko_pk3_pituus >= yko_pk2_pituus THEN ypkluokka := 'PK3';
+        ELSE ypkluokka := NULL;
+        END CASE;
+
+    RAISE NOTICE 'Yhteenlasketut luokkapituudet osapk1: %, osapk2: %, osapk3: %',
+        yko_pk1_pituus, yko_pk2_pituus, yko_pk3_pituus;
+    RAISE NOTICE 'Saatiin yllapikohteelle pkluokka: %', ypkluokka;
+    -- ***************** Muuttunut kohta *****************
+    UPDATE yllapitokohde SET pkluokka = ypkluokka::korjausluokkatyyppi WHERE id = yllapitokohde_id;
+    -- ***************** Muuttunut kohta päättyy *****************
+END;
+$$ LANGUAGE plpgsql;
+
 -- Lisätään paikkauskohteille pkluokka
 ALTER TABLE paikkauskohde
-    ADD COLUMN pkluokka TEXT;
+    ADD COLUMN pkluokka korjausluokkatyyppi;
 
 -- Lasketaan paikkauskohteelle pkluokka tieosoitteen perusteella
 CREATE OR REPLACE FUNCTION paivita_paikkauskohteen_korjausluokka(paikkauskohde_id INTEGER)
@@ -89,7 +163,7 @@ BEGIN
         ELSE pkluokka_t := 'Ei tiedossa';
         END CASE;
 
-    UPDATE paikkauskohde SET pkluokka = pkluokka_t WHERE id = paikkauskohde_id;
+    UPDATE paikkauskohde SET pkluokka = pkluokka_t::korjausluokkatyyppi WHERE id = paikkauskohde_id;
     RETURN pkluokka_t;
 END;
 $$ LANGUAGE plpgsql;
@@ -114,7 +188,7 @@ $$ LANGUAGE plpgsql;
 
 -- Lisätään paikkauskohteille pkluokka
 ALTER TABLE paikkaus
-    ADD COLUMN pkluokka TEXT;
+    ADD COLUMN pkluokka korjausluokkatyyppi;
 
 -- Lasketaan reikäpaikkaukselle pkluokka tieosoitteen perusteella
 CREATE OR REPLACE FUNCTION paivita_reikapaikkauksen_korjausluokka(reikapaikkaus_id INTEGER)
@@ -199,7 +273,7 @@ BEGIN
         ELSE pkluokka_t := 'Ei tiedossa';
         END CASE;
 
-    UPDATE paikkaus SET pkluokka = pkluokka_t WHERE id = reikapaikkaus_id;
+    UPDATE paikkaus SET pkluokka = pkluokka_t::korjausluokkatyyppi WHERE id = reikapaikkaus_id;
     RETURN pkluokka_t;
 END;
 $$ LANGUAGE plpgsql;
