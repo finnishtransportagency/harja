1bce4d7fb4fc9d193a100c5ffacc4e8a9231fee9
Markus Vähäkangas <markus.vahakangas@solita.fi>
2024-12-11 13:27:10 +0200

    Tarkenna vielä spanilla

--- cypress/e2e/aa_yllapidon_kustannukset.cy.js
+++ cypress/e2e/aa_yllapidon_kustannukset.cy.js
@@ -119,8 +119,8 @@ describe('Kustannusnäkymä toimii MPU urakalle', function () {
         cy.get('.grid').eq(1).find('tr').eq(0).find('td').eq(2).contains('88 060,00 €');
 
         // Yhteensä
-        cy.get('.grid').eq(1).find('.kustannukset-yhteenveto').eq(0).find('td').eq(2).contains('337 580,00');
-        cy.get('.grid').eq(1).find('.kustannukset-yhteenveto').eq(1).find('td').eq(2).contains('342 580,00');
+        cy.get('.grid').eq(1).find('.kustannukset-yhteenveto').eq(0).find('td').eq(2).find('span').contains('337 580,00');
+        cy.get('.grid').eq(1).find('.kustannukset-yhteenveto').eq(1).find('td').eq(2).find('span').contains('342 580,00');
     });
 
 
@@ -183,8 +183,8 @@ describe('Kustannusnäkymä toimii MPU urakalle', function () {
         cy.get('.grid').eq(1).find('tr').eq(2).find('td').eq(2).contains('123 456,12 €');
 
         // Yhteensä
-        cy.get('.grid').eq(1).find('.kustannukset-yhteenveto').eq(0).find('td').eq(2).contains('461 036,12');
+        cy.get('.grid').eq(1).find('.kustannukset-yhteenveto').eq(0).find('td').eq(2).find('span').contains('461 036,12');
         // Urakka-ajan kustannukset yhteensä
-        cy.get('.grid').eq(1).find('.kustannukset-yhteenveto').eq(1).find('td').eq(2).contains('466 036,12');
+        cy.get('.grid').eq(1).find('.kustannukset-yhteenveto').eq(1).find('td').eq(2).find('span').contains('466 036,12');
     });
 }); 
