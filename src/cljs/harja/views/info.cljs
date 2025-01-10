7b21772cfa8596a6496d0025f7fbdb1d85458e84
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2025-01-10 14:17:11 +0200

    Poista turha span

--- src/cljs/harja/views/info.cljs
+++ src/cljs/harja/views/info.cljs
@@ -32,30 +32,29 @@
          (e! (tiedot/->HaeKoulutusvideot))))
 
     (fn [e! {:keys [videot]}]
-      [:span
-       [:div.harja-info
-        [:h1 {:class "header-yhteiset"} "Harja Info"]
-         [:div {:class "otsikko-viiva"}]
-         [:section.linkit
-          [:span
-           [yleiset/staattinen-linkki-uuteen-ikkunaan
-            [:h2 "Harja uutiset "
-             [ikonit/livicon-external]]
-            "https://finnishtransportagency.github.io/harja/"]]
-          [:span
-           [yleiset/staattinen-linkki-uuteen-ikkunaan
-            [:h2 "Tietoja henkilötietojesi käsittelystä "
-             [ikonit/livicon-external]]
-            "https://vayla.fi/tietoa-meista/yhteystiedot/tietosuoja"]]
-          [:span
-           [yleiset/staattinen-linkki-uuteen-ikkunaan
-            [:h2 "Saavutettavuusseloste "
-             [ikonit/livicon-external]]
-            "https://palju.vaylapilvi.fi/palju/Extranet/Muut/Saavutettavuusselosteet/Saavutettavuusseloste_Harja.pdf"]]
+      [:div.harja-info
+       [:h1 {:class "header-yhteiset"} "Harja Info"]
+       [:div {:class "otsikko-viiva"}]
+       [:section.linkit
+        [:span
+         [yleiset/staattinen-linkki-uuteen-ikkunaan
+          [:h2 "Harja uutiset "
+           [ikonit/livicon-external]]
+          "https://finnishtransportagency.github.io/harja/"]]
+        [:span
+         [yleiset/staattinen-linkki-uuteen-ikkunaan
+          [:h2 "Tietoja henkilötietojesi käsittelystä "
+           [ikonit/livicon-external]]
+          "https://vayla.fi/tietoa-meista/yhteystiedot/tietosuoja"]]
+        [:span
+         [yleiset/staattinen-linkki-uuteen-ikkunaan
+          [:h2 "Saavutettavuusseloste "
+           [ikonit/livicon-external]]
+          "https://palju.vaylapilvi.fi/palju/Extranet/Muut/Saavutettavuusselosteet/Saavutettavuusseloste_Harja.pdf"]]
 
-          [:div {:class "info-heading"} "Harja-koulutusvideot"]
-          [yleiset/vihje "Huomaathan, että osa Harjan koulutusvideoista on melko vanhoja, ja järjestelmä on monilta osin muuttunut videoiden tekemisen jälkeen."]
-          [:div [videolistaus e! videot]]]]])))
+        [:div {:class "info-heading"} "Harja-koulutusvideot"]
+        [yleiset/vihje "Huomaathan, että osa Harjan koulutusvideoista on melko vanhoja, ja järjestelmä on monilta osin muuttunut videoiden tekemisen jälkeen."]
+        [:div [videolistaus e! videot]]]])))
 
 (defn info
   "Hakee koulutusvideot kun käyttäjä tulee näkymään"
