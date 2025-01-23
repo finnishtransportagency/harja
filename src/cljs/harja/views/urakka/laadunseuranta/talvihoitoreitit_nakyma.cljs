1c1544ecfc2e3b2f272885e46d08e15cd1520496
Markus Vähäkangas <markus.vahakangas@solita.fi>
2025-01-23 08:24:06 +0200

    Poista turha lokirivi

--- src/cljs/harja/views/urakka/laadunseuranta/talvihoitoreitit_nakyma.cljs
+++ src/cljs/harja/views/urakka/laadunseuranta/talvihoitoreitit_nakyma.cljs
@@ -21,8 +21,7 @@
                               {:keys [laskettu_pituus nimi id varikoodi hoitoluokat ulkoinen_id reitit urakka_id
                                       tr_maara ka_maara kup_maara]}]
   
-  (let [_ (js/console.log "talvihoitoreitti-rivi :: hoitoluokat" (pr-str hoitoluokat))
-        valitut-kohteet @tiedot/valitut-kohteet-atom
+  (let [valitut-kohteet @tiedot/valitut-kohteet-atom
         reittien-maara (count reitit)
         auki? (contains? talvihoitoreittien-tilat id)
         reitteja-olemassa? (> reittien-maara 0)
