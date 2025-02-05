7a48e7e7007f57ea668edee94d3f1c6f62906540
solita-staskila <samuli.taskila@solita.fi>
2025-02-05 14:21:11 +0200

    Public avaimen päivitys, korjaa JWT tunnistetietoja, kommentteja, FATAL logitus

--- src/clj/harja/palvelin/komponentit/todennus.clj
+++ src/clj/harja/palvelin/komponentit/todennus.clj
@@ -329,7 +329,7 @@
           kayttaja-id (headerit "oam_remote_user")]
       (if (nil? kayttaja-id)
         (do
-          (log/warn (str "Todennusheader oam_remote_user puuttui kokonaan" headerit))
+          (log/warn (str "Todennusheader oam_remote_user puuttui kokonaan: " headerit))
           (throw+ todennusvirhe))
         (if-let [kayttajatiedot (koka->kayttajatiedot db headerit oikeudet kehitysmoodi?)]
           (assoc req :kayttaja kayttajatiedot)
--- src/clj/harja/palvelin/komponentit/todennus_varmistus.clj
+++ src/clj/harja/palvelin/komponentit/todennus_varmistus.clj
@@ -1,6 +1,7 @@
 (ns harja.palvelin.komponentit.todennus-varmistus
   "Kirjautumisen JWT tokenien varmistus jotka saadaan Cognitolta"
   (:require [clojure.string :as str]
+            [cheshire.core :as cheshire]
             [clojure.data.json :as json]
             [org.httpkit.client :as http]
             [clojure.core.cache :as cache]
@@ -10,9 +11,22 @@
   (:import (org.apache.commons.codec.binary Base64)))
 
 (defonce jwk-cache (atom nil))
+(def +jwk-cache-paivitys-min+ 120)
+(def +kayttaja-varmistus-cache-min+ 15)
 
 
-(defn- onko-jwk-vanhentunut? 
+(defn- tunnistetiedot
+  "Palauttaa dekoodatut tunnistetiedot käyttäjästä, sisältää roolit yms (x-iam-data)"
+  [jwt]
+  (some-> jwt
+    (str/split #"\.")
+    second
+    Base64/decodeBase64
+    String.
+    cheshire/decode))
+
+
+(defn- onko-jwk-vanhentunut?
   "Hakee uuden public keyn, jos sitä ei ole haettu vähään aikaan
    Ei ole tiedossa kuinka tiheään, mutta Cognitolla on ominaisuus rotatoida public avaimia"
   [minuutit]
@@ -21,16 +35,17 @@
 
 (defn hae-jwks-json
   "Tekee GET kutsun joka hakee jwks (json web key set/public avaimet) Cognitolta"
-  [issuer]
+  [issuer paivita?]
   (if
     ;; Jos public avainta ei ole päivitetty 120 minuuttiin
     ;; Tällainen cachetus vielä, koska yksi backend, monta käyttäjää, Cognito ei rotatoi public avaimia kovin tiheään 
     (or
+      paivita? ;; Käyttäjä ei päässyt sisälle, yritetään yhden kerran uudelleen päivittämällä public avain
       (nil? @jwk-cache)
-      (onko-jwk-vanhentunut? 120))
+      (onko-jwk-vanhentunut? +jwk-cache-paivitys-min+))
     (try
       (let [response @(http/get (str issuer "/.well-known/jwks.json") {:headers {;; Lisää Cognitolle tiedoksi mistä pyyntö tulee
-                                                                                 "User-Agent" "HARJA dev: (JWT verification)"
+                                                                                 "User-Agent" "HARJA dev: (JWT signature verification)"
                                                                                  "Content-Type" "application/json"}})
             response (json/read-json (:body response))
             _ (reset! jwk-cache {:keys (:keys response) :fetched-at (System/currentTimeMillis)})]
@@ -42,53 +57,93 @@
 
 
 (defn hae-public-key
-  "Suodattaa public avaimet, täsmätään key identifier siihen avaimeen mitä haetaan
-   Tämä rajapinta palauttaa "
-  [kid issuer]
+  "Kutsuttu rajapinta palauttaa accesstokenin public avaimen
+   Mukana on 2 avainta, joten suodatetaan vaan haluttu avain (key identifier)"
+  [kid issuer paivita?]
   (some #(when (= kid (:kid %)) %)
-    (hae-jwks-json issuer)))
+    (hae-jwks-json issuer paivita?)))
 
 
 (defn- decode-base64-json [s]
   (-> s Base64/decodeBase64 String. (json/read-json)))
 
 
-(defn dekoodaa-token [token]
+(defn dekoodaa-token
+  "Dekoodaa enkoodatut headerit, ja palauttaa tiedot json stringinä"
+  [token]
   (let [[header payload signature] (str/split token #"\.")]
     {:header  (decode-base64-json header)
      :payload (decode-base64-json payload)
      :signature signature}))
 
 
-(defn varmista-jwt-signature 
+(defn varmista-jwt-signature
   "Varmistaa kirjautumisen oikellisuuden Cogniton x-iam-accesstoken header tokenista
    https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html#amazon-cognito-user-pools-using-tokens-manually-inspect"
-  [accesstoken iam-data iam-identity]
+  [accesstoken iam-data iam-identity paivita?]
   (let [header (-> accesstoken dekoodaa-token :header)
         issuer (-> accesstoken dekoodaa-token :payload :iss)
         kid (:kid header)
         ;; Hae public avain jolla sama :kid
-        jwk (hae-public-key kid issuer)
+        jwk (hae-public-key kid issuer paivita?)
         ;; Muunna java muotoon 
         public-key (keys/jwk->public-key jwk)
 
         _ (println "\n accesstoken payload: " header (-> accesstoken dekoodaa-token :payload))
         _ (println "\n accesstoken header: " header (-> accesstoken dekoodaa-token :header))
         _ (println "\n accesstoken signature: " header (-> accesstoken dekoodaa-token :signature) " \n \n" " ---- \n")
-
-        _ (println "\n iam-data payload: " (-> iam-data dekoodaa-token :payload) )
-        _ (println "\n iam-data header: " (-> iam-data dekoodaa-token :header) )
+        _ (println "\n iam-data payload: " (-> iam-data dekoodaa-token :payload))
+        _ (println "\n iam-data header: " (-> iam-data dekoodaa-token :header))
         _ (println "\n iam-data signature: " (-> iam-data dekoodaa-token :signature) " \n \n" " ---- \n")
+        _ (println "\n iam-identity payload: " iam-identity " \n ")
 
-        _ (println "\n iam-identity payload: " iam-identity)
+        ;; Verifioi kutsumalla unsign, joka tarkastaa saapuvien tietojen allekirjoituksen
+        ;; Palauttaa Cognito map responsen, jos kirjautuminen menee läpi ja token on voimassa 
+        ;; Heittää virheen, jos tiedoissa on jotain väärin 
         response (jwt/unsign accesstoken public-key {:alg :rs256})
         _ (println "\n res: " response " \n")]
     response))
 
 
-(def dekoodaa-ja-varmista-cache (atom (cache/ttl-cache-factory {} :ttl (* 3 60 1000))))
+(defn- kasittele-virhe
+  "Käsitellään kirjautumisvirhe, ohjaa käyttäjän 'Ei käyttöoikeutta Harjaan.'- näkymään
+   
+   'Message seems manipulated': 
+     - Public avain voi olla väärin vaikka yritettiin hakea uusi (Ota Harjan kirjautumisen välittäjälle yhteyttä)
+     - On mahdollista, että payloadia sorkittu, tutki mitä logeissa lukee 
+   
+   'Token is expired'
+     - Token mennyt vanhaksi, kestää yleensä noin 10 min, tätä ei pitäisi näkyä (välittäjälle yhteyttä)
+     - Voi olla järjestelmävirhe, mutta tämä testataan huolella
+   
+   'Audience does not match'
+     - Audience, eli järjestelmän tunnus mille token annettu, ei jostain syystä täsmää (välittäjälle yhteyttä)
+   
+   'The subject does not match'
+     - Subjekti, kenelle token myönnetty, ei jostain syystä täsmää (välittäjälle yhteyttä)
+   
+   'Issuer does mot match' 
+     - Tokenin välittäjä ei jostain syystä täsmää (taas, välittäjälle yhteyttä)"
+  [e iam-data]
+  ;; Fatal laukaisee slack-hälytyksen, nämä halutaan aina tutkia 
+  (log/fatal (str
+               "Kirjautumisen varmistus ei onnistunut (JWT signature): " (.getMessage e)))
+  (log/error (str
+               "Saatu JWT Header (iam-data): " (-> iam-data dekoodaa-token :header)
+               "  -  "
+               ;; Tässä on käyttäjän yritetyt roolit yms, jos logeilta löytyy roolissa jotain outoa, tee välittömästi toimenpiteitä 
+               "Saatu payload (iam-data): " (-> iam-data dekoodaa-token :payload)))
+  ;; Kirjautuminen ei mennyt läpi, poista kaikki oikeudet käyttäjältä
+  ;; Tämä uudelleenohjaa "Ei käyttöoikeutta" näkymään 
+  (-> (tunnistetiedot iam-data)
+    ;; Extranet_Kayttaja ei anna oikeutta Harjaan
+    ;; Jokin rooli täytyy antaa, muuten tulee todennusvirhe 
+    (assoc "custom:rooli" "Extranet_Kayttaja")))
 
-(defn vahvista-jwt-signaturet 
+
+(def kayttaja-varmistettu-cache (atom (cache/ttl-cache-factory {} :ttl (* +kayttaja-varmistus-cache-min+ 60 1000))))
+
+(defn vahvista-jwt-signaturet
   "Tekee JWT tokenien signaturen vahvistuksen
    Palauttaa dekoodatut tunnistetiedot, mikäli signaturet OK
    JWT (Json Web Token) sisältää pisteellä erotetut osiot (enkoodattuna): HEADER.PAYLOAD.SIGNATURE
@@ -108,37 +163,35 @@
    
    3. Jos kaikki OK, palautetaan dekoodattu tunnusdata, ja jatketaan kirjautumista 
    4. Tuloksena käyttäjän roolitiedot on vahvistettu oikeiksi, eikä mitään ole sorkittu matkalla"
-  [accesstoken iam-data iam-identity kehitysmoodi?]
-  (let [cache-key accesstoken]
-    ;; Todennusta kutsutaan ilman malttia, joten cachella kutsutaan vaan tarvittaessa per käyttäjä
-    (get (swap! dekoodaa-ja-varmista-cache #(cache/through
-                                              (fn [_]
-                                                (try
-                                                  ;; Cachessa ei ole tietoja, varmistetaan signaturet 
-                                                  (let [_ (when-not kehitysmoodi? (varmista-jwt-signature accesstoken iam-data iam-identity))
-                                                        inject (-> (-> iam-data dekoodaa-token :payload)
-                                                                 (assoc "custom:rooli" "Jarjestelmavastaava"))]
-                                                    (-> iam-data dekoodaa-token :payload)
-                                                    ;; Tykitä jvh koska ei ole kvh oikeuksia
-                                                    inject)
-                                                  (catch Exception e
-                                                    (do
-                                                      (log/error (str
-                                                                   "Kirjautumisen varmistus ei onnistunut (JWT signature): "
-                                                                   (.getMessage e) 
-                                                                   "  -  "
-                                                                   "Käyttäjätiedot: "
-                                                                   (select-keys (-> iam-data dekoodaa-token :payload)
-                                                                     ["custom:rooli" "custom:sukunimi" "custom:email" "custom:ytunnus" "custom:uid"])))
-                                                      (log/error (str
-                                                                   "Saatu JWT Header (iam-data): " (-> iam-data dekoodaa-token :header) 
-                                                                   "  -  "
-                                                                   "Saatu payload (iam-data): " (-> iam-data dekoodaa-token :payload)))
-                                                      ;; Kirjautuminen ei mennyt läpi, poista oikeudet käyttäjältä
-                                                      ;; Tämä uudelleenohjaa "Ei käyttöoikeutta" näkymään 
-                                                      (-> (-> iam-data dekoodaa-token :payload)
-                                                        (assoc "custom:rooli" "Extranet_Kayttaja"))))))
-                                              ;; Tiedot on jo cachessa, palauta ne 
-                                              %
-                                              cache-key))
-      cache-key)))
+  ([accesstoken iam-data iam-identity kehitysmoodi?]
+   (vahvista-jwt-signaturet accesstoken iam-data iam-identity false kehitysmoodi?))
+  ([accesstoken iam-data iam-identity yrita-uudelleen? kehitysmoodi?]
+   (let [cache-key accesstoken]
+     ;; Todennusta kutsutaan ilman malttia, joten cachella kutsutaan vaan tarvittaessa per käyttäjä
+     (get (swap! kayttaja-varmistettu-cache #(cache/through
+                                               (fn [_]
+                                                 (try
+                                                   ;; Cachessa ei ole tietoja, varmistetaan signaturet 
+                                                   (let [
+                                                         _ (println "\n yrita uudelleen: " yrita-uudelleen? "\n ")
+                                                         _ (when-not kehitysmoodi?
+                                                             (varmista-jwt-signature accesstoken iam-data iam-identity yrita-uudelleen?))
+                                                         inject (-> (tunnistetiedot iam-data)
+                                                                  (assoc "custom:rooli" "Jarjestelmavastaava"))]
+                                                     (tunnistetiedot iam-data)
+                                                     (println "\n bb: " (tunnistetiedot iam-data))
+                                                     ;; Tykitä jvh koska ei ole kvh oikeuksia
+                                                     inject)
+                                                   (catch Exception e
+                                                     (if
+                                                       (and
+                                                         (not yrita-uudelleen?)
+                                                         (= (.getMessage e) "Message seems corrupt or manipulated"))
+                                                       ;; Tarkista, onko public key rotatoitunut, yritä uudelleen
+                                                       (vahvista-jwt-signaturet accesstoken iam-data iam-identity true kehitysmoodi?)
+                                                       ;; Public key on ajan tasalla, ja vieläkin tulee virhe, joten käsitellään se
+                                                       (kasittele-virhe e iam-data)))))
+                                               ;; Tiedot on jo cachessa, palauta ne 
+                                               %
+                                               cache-key))
+       cache-key))))
