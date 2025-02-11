4b8c44b7841f77a9552bffa4a65509a2d16d12a4
Maarit Laine <maarit.laine@solita.fi>
2025-02-11 08:16:32 +0200

    Siivoa vanhentunut funktio pois

---
+++ tietokanta/src/main/resources/db/migration/V1_1161__.sql
@@ -0,0 +1,1 @@
+DROP FUNCTION IF EXISTS pistevalin_suolausalueet(piste1 POINT, piste2 POINT, urakka_id_ INTEGER); -- Siivotaan vanhentunut versio funktiosta pois. Uusi versio R__Suolapisteet-migraatiossa.
