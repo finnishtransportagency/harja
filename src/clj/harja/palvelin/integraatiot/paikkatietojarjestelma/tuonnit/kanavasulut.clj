f488d5cc148456cc795705a2e1f8bc8d6743b284
solita-staskila <samuli.taskila@solita.fi>
2025-02-12 15:23:00 +0200

    Passaa uuden aineiston arvot oikein

--- src/clj/harja/palvelin/integraatiot/paikkatietojarjestelma/tuonnit/kanavasulut.clj
+++ src/clj/harja/palvelin/integraatiot/paikkatietojarjestelma/tuonnit/kanavasulut.clj
@@ -4,7 +4,6 @@
             [harja.palvelin.integraatiot.paikkatietojarjestelma.tuonnit.shapefile :as shapefile]
             [harja.kyselyt.kanavat.kanavasulut :as q-kanavasulut]))
 
-
 (defn merkitse-kanavasulut-poistetuksi [db]
   (q-kanavasulut/merkitse-kanavasulut-poistetuksi<! db {:muokkaaja "Integraatio"}))
 
@@ -16,36 +15,33 @@
   [db {:keys [numero nimi kanavaalue kiinnittym porttiseli kayttoseli sulkuleve0 sulkulevey
               alusleveys aluspituus alussyvyys aluskorkeu id sulkuja putouskork putouskor0 vesisto
               kanavapitu kanavaleve mista mihin omistaja the_geom]} :as kanavasulku]
-  (let [geometria (.toString (:the_geom kanavasulku))
-        sql-parametrit {:kanavanro kanavanro
-                        :aluenro aluenro
+  (let [geometria (.toString the_geom)
+        sql-parametrit {:kanavanro numero
+                        :aluenro id
                         :nimi nimi
-                        :kanavatyyppi kanavatyyppi
-                        :aluetyyppi aluetyyppi
-                        :kiinnitys kiinnitys
-                        :porttityyppi porttityyppi
-                        :kayttotapa kayttotapa
-                        :sulku_leveys sulku_leveys
-                        :sulku_pituus sulku_pituus
-                        :alus_leveys alus_leveys
-                        :alus_pituus alus_pituus
-                        :alus_syvyys alus_syvyys
-                        :alus_korkeus alus_korkeus
-                        :sulkumaara sulkumaara
-                        :putouskorkeus_1 putouskorkeus_1
-                        :putouskorkeus_2 putouskorkeus_2
-                        :alakanavan_alavertaustaso alakanavan_alavertaustaso
-                        :alakanavan_ylavertaustaso alakanavan_ylavertaustaso
-                        :ylakanavan_alavertaustaso ylakanavan_alavertaustaso
-                        :ylakanavan_ylavertaustaso ylakanavan_ylavertaustaso
-                        :kynnys_1 kynnys_1
-                        :kynnys_2 kynnys_2
+                        ; :kanavatyyppi kanavatyyppi
+                        ;; Tyyppiä ei enää ole, taitaa olla sama kun aluetyyppi, eli esim "Sulku"
+                        ;; Tätäkään arvoa, ei Harjassa käytet 
+                        :aluetyyppi kanavaalue
+                        :kiinnitys kiinnittym
+                        :porttityyppi porttiseli
+                        :kayttotapa kayttoseli
+                        :sulku_leveys sulkuleve0
+                        :sulku_pituus sulkulevey ;; Jännästi nimettynä, mutta pitää paikkansa
+                        :alus_leveys alusleveys
+                        :alus_pituus aluspituus
+                        :alus_syvyys alussyvyys
+                        :alus_korkeus aluskorkeu
+                        :sulkumaara sulkuja
+                        :putouskorkeus_1 putouskork
+                        :putouskorkeus_2 putouskor0
                         :vesisto vesisto
-                        :kanavakokonaisuus kanavakokonaisuus
-                        :kanava_pituus kanava_pituus
-                        :kanava_leveys kanava_leveys
-                        :lahtopaikka lahtopaikka
-                        :kohdepaikka kohdepaikka
+                        ; :kanavakokonaisuus kanavakokonaisuus
+                        ; Kokonaisuutta ei myöskään uudessa aineistossa enää ole 
+                        :kanava_pituus kanavapitu
+                        :kanava_leveys kanavaleve
+                        :lahtopaikka mista
+                        :kohdepaikka mihin
                         :omistaja omistaja
                         :geometria geometria
                         :luoja "Integraatio"
