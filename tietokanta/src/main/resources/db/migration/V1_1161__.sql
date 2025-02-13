c0fcd2a33657df80a262a7a5c029af2dca1b2056
Maarit Laine <maarit.laine@solita.fi>
2025-02-13 15:42:53 +0200

    Päivitä suolaosuuden tyyppi, poista pohjavesialuetunnus

--- tietokanta/src/main/resources/db/migration/V1_1161__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1161__.sql
@@ -1,1 +1,5 @@
 DROP FUNCTION IF EXISTS pistevalin_suolausalueet(piste1 POINT, piste2 POINT, urakka_id_ INTEGER); -- Siivotaan vanhentunut versio funktiosta pois. Uusi versio R__Suolapisteet-migraatiossa.
+
+-- Pohjavesitietoja ei käytetä enää
+ALTER TYPE suolausalueen_osuus
+DROP ATTRIBUTE pohjavesialue_tunnus;
