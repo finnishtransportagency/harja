6bf8a6bc96e2c4537b99bc898893a4afac3ada67
solita-staskila <samuli.taskila@solita.fi>
2025-02-13 16:32:17 +0200

    Poista kommentti

--- tietokanta/src/main/resources/db/migration/V1_1160__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1160__.sql
@@ -1,5 +1,3 @@
--- Tiedosto uudelleennimetään ennen mergeä 
-
 -- Poista turhaksi jääneitä sarakkeita kanava aineistosta, mitä Harjassa ei käytetä 
 ALTER TABLE kan_sulku DROP COLUMN kanavatyyppi;
 ALTER TABLE kan_sulku DROP COLUMN alakanavan_alavertaustaso;
