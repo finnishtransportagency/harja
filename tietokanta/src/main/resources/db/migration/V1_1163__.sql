be02d6b4f3313981a286d32021d3c066e5e00c81
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2025-02-20 15:50:06 +0200

    Uudelleennimeä tierekisteriosoitteelle_viiva -> tieosoitteelle_geometria

--- tietokanta/src/main/resources/db/migration/V1_1163__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1163__.sql
@@ -1,1 +1,3 @@
-DROP FUNCTION IF EXISTS tierekisteriosoitteelle_viiva(tie_ INTEGER, aosa_ INTEGER, aet_ INTEGER, losa_ INTEGER, let_ INTEGER);
+
+ALTER FUNCTION tierekisteriosoitteelle_viiva(tie_ INTEGER, aosa_ INTEGER, aet_ INTEGER, losa_ INTEGER, let_ INTEGER)
+    RENAME TO tieosoitteelle_geometria;
