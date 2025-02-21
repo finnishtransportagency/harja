1992daf256797870b3469e12a0ff861f73199939
Toni Dahl <toni.dahl@solita.fi>
2025-02-21 12:31:13 +0200

    Eriytä test-depsut tiukemmin omaan karsinaansa, jotta niitä ei poimita mukaan testien ulkopuolella

--- profiles.clj
+++ profiles.clj
@@ -9,27 +9,11 @@
  :pedantic-warn {:pedantic? :warn}
 
  :dev {:dependencies [
-                      ;; Tarvitaan CLJS käännöksessä (dev, prod)
+                      ;; Figwheeliä tarvitaan CLJS käännöksessä (dev, prod)
                       [com.bhauman/figwheel-main "0.2.18"]
-                      [prismatic/dommy "1.1.0"]
-                      [org.clojure/test.check "0.9.0"]
-                      [org.apache.pdfbox/pdfbox "2.0.30"]
                       [data-frisk-reagent "0.4.5"]
                       [cider/piggieback "0.5.2"]
-                      [com.bhauman/rebel-readline-cljs "0.1.4"]
-
-                      ;; -- Testien ajamista varten (replillä ja ilman) --
-                      ;; Fake-HTTP testaukseen
-                      [http-kit.fake "0.2.2"]
-
-                      ;; JMS API (esim. tieliikenneilmoitukset) JMS-jonojen testausta varten
-                      [javax.jms/javax.jms-api "2.0.1"]
-
-                      ;; Gatlingin logback versio ei ole vielä ehtinyt päivittyä, niin haetaan se erikseen
-                      [ch.qos.logback/logback-classic "1.4.14" :exclusions [org.slf4j/slf4j-api]]
-                      [clj-gatling "0.18.0" :exclusions [clj-time org.slf4j/slf4j-api org.clojure/core.memoize
-                                                         org.clojure/tools.analyzer org.clojure/data.priority-map io.pebbletemplates/pebble]]
-                      ]
+                      [com.bhauman/rebel-readline-cljs "0.1.4"]]
        :source-paths ["src/clj-dev" "src/cljs" "src/cljc" "src/cljs-dev" "src/shared-cljc" "script"]
        :resource-paths ["dev-resources/js" "dev-resources/tmp" "resources/public/css" "resources"]
        :plugins [[test2junit "1.4.4" :exclusions [org.clojure/clojure]]
@@ -68,13 +52,17 @@
                                                   :target-path]}
  :dev-emacs {:plugins [[cider/cider-nrepl "0.25.3"]
                        [refactor-nrepl "2.5.0"]]}
- :repl {:dependencies [[cider/piggieback "0.5.2"]]
-        :plugins [[cider/cider-nrepl "0.25.2"]]
-        :repl-options {:init-ns harja.palvelin.main
-                       :init (harja.palvelin.main/-main)
-                       :port 4005
-                       :timeout 120000
-                       :nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}
+ :repl-base {:dependencies [[cider/piggieback "0.5.2"]]
+             :plugins [[cider/cider-nrepl "0.25.2"]]
+             :repl-options {:init-ns harja.palvelin.main
+                            :init (harja.palvelin.main/-main)
+                            :port 4005
+                            :timeout 120000
+                            :nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}
+ ;; Testien ajamiseksi replillä, täytyy mukaan ottaa myös test-profiili
+ :repl [:repl-base :test]
+ ;; Cursive (Intellij IDEA) Napsauta "test"-profiili käyttöön Leiningen-asetuksista, jotta test-depsut tulevat mukaan
+ ;;  https://cursive-ide.com/userguide/leiningen.html#working-with-profiles
  :test {:dependencies [
                        ;; Selenium + webdriver depsut pois käytöstä, koska niitä ei enää ajeta
                        #_[clj-webdriver "0.7.2"]
@@ -84,10 +72,35 @@
                        ;; käyttää sitä, voi kirjoittaa uusiksi Cypressillä.
                        ;; Jotta frontti testit toimii, pitää säilyttää tuo riippuvuus, jonka takia myös
                        ;; reagentti pitää downgradeta testejä varten.
+
+                       ;; -- Front-end testaukseen --
                        [reagent "0.7.0" :exclusions [[cljsjs/react :classifier "*"]]]
                        [cljsjs/react-with-addons "15.6.1-0"]
                        [cljsjs/react-dom "15.4.2-2" :exclusions [cljsjs/react]]
-                       [cljs-react-test "0.1.4-SNAPSHOT"]]
+                       [cljs-react-test "0.1.4-SNAPSHOT"]
+
+                       [org.clojure/test.check "0.9.0"]
+                       [prismatic/dommy "1.1.0"]
+                       ;; --
+
+                       ;; -- PDF-testeihin --
+                       [org.apache.pdfbox/pdfbox "2.0.30"]
+                       ;; --
+
+
+                       ;; Fake-HTTP testaukseen
+                       [http-kit.fake "0.2.2"]
+
+                       ;; JMS API (esim. tieliikenneilmoitukset) JMS-jonojen testausta varten
+                       [javax.jms/javax.jms-api "2.0.1"]
+
+                       ;; -- Suorituskykytestaukseen --
+                       ;; Gatlingin logback versio ei ole vielä ehtinyt päivittyä, niin haetaan se erikseen
+                       [ch.qos.logback/logback-classic "1.4.14" :exclusions [org.slf4j/slf4j-api]]
+                       [clj-gatling "0.18.0" :exclusions [clj-time org.slf4j/slf4j-api org.clojure/core.memoize
+                                                          org.clojure/tools.analyzer org.clojure/data.priority-map io.pebbletemplates/pebble]]
+                       ;; --
+                       ]
         :source-paths ["test/cljs" "test/doo" "test/shared-cljs"]}
  :prod-cljs {:source-paths ^:replace ["src/cljs" "src/cljc" "src/cljs-prod" "src/shared-cljc"]}
 
