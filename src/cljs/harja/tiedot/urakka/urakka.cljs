832904bb1a90b7b521067fdde6f9d3599daa01c2
Samu Mikkonen <samu.mikkonen@solita.fi>
2025-04-15 15:16:39 +0300

    Lisää tiemerkintöjen kustannusten tilan hallinta

--- src/cljs/harja/tiedot/urakka/urakka.cljs
+++ src/cljs/harja/tiedot/urakka/urakka.cljs
@@ -424,7 +424,18 @@
                      :toteumat    toteumat-default-arvot
                      :paikkaukset paikkaus-default-arvot
                      :kustannusten-seuranta kustannusten-seuranta-default-arvot
-                     :talvihoitoreitit talvihoitoreitit-default}))
+                     :talvihoitoreitit talvihoitoreitit-default
+                     :tiemerkinta-korjaukset {}
+                     :tiemerkinta-yhteenveto {}
+                     :tiemerkinta-muut-kustannukset {}
+                     :tiemerkinta-sanktiot-ja-bonukset {}
+                     :tiemerkinta-uusien-paallysteiden-merkkinnat {}}))
+
+(defonce tiemerkinta-korjaukset (cursor tila [:tiemerkinta-korjaukset]))
+(defonce tiemerkinta-yhteenveto (cursor tila [:tiemerkinta-yhteenveto]))
+(defonce tiemerkinta-muut-kustannukset (cursor tila [:tiemerkinta-muut-kustannukset]))
+(defonce tiemerkinta-sanktiot-ja-bonukset (cursor tila [:tiemerkinta-sanktiot-ja-bonukset]))
+(defonce tiemerkinta-uusien-paallysteiden-merkkinnat (cursor tila [:tiemerkinta-uusien-paallysteiden-merkkinnat]))
 
 (defonce laatupoikkeamat (cursor tila [:laatupoikkeamat]))
 (defonce paikkauskohteet (cursor tila [:paikkaukset :paikkauskohteet]))
