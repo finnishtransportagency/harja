2ecc065a8f0955b102e6eb977e1a65b0179a8bf5
Samu Mikkonen <samu.mikkonen@solita.fi>
2025-04-15 15:33:00 +0300

    Lisää tabit

---
+++ src/cljs/harja/views/urakka/kustannusten_kirjaus.cljs
@@ -0,0 +1,33 @@
+(ns harja.views.urakka.kustannusten-kirjaus
+  "Tiemerkintäurakan Kustannukset-välilehti"
+  (:require
+   [harja.ui.bootstrap :as bs]
+   [harja.ui.komponentti :as komp]
+   [harja.views.urakka.kustannusten-kirjaus.tiemerkintojen-korjaus :as tiemerkintojen-korjaus]
+   [harja.views.urakka.kustannusten-kirjaus.uusien-paallysteiden-merkinnat-nakyma :as paallysteiden-merkinnat]
+   [harja.views.urakka.kustannusten-kirjaus.sakot-ja-bonukset :as sakot-ja-bonukset]
+   [harja.views.urakka.kustannusten-kirjaus.muut-kustannukset :as muut-kustannukset]
+   [harja.tiedot.navigaatio :as nav]))
+
+(defn kustannusten-kirjaus []
+  (komp/luo
+    (fn []
+      [:span.kustannusten-kirjaus
+       [bs/tabs {:style :tabs :classes "tabs-tabs2"
+                 :active (nav/valittu-valilehti-atom :kustannusten-kirjaus)}
+
+        "Tiemerkintöjen korjaus"
+        :tiemerkintojen-korjaus
+        [tiemerkintojen-korjaus/tiemerkintojen-korjaus]
+
+        "Uusien päällysteiden merkinnät"
+        :uusien-pallysteiden-merkinnat
+        [paallysteiden-merkinnat/uusien-paallysteiden-merkinnat]
+
+        "Sakot ja bonukset"
+        :sakot-ja-bonukset
+        [sakot-ja-bonukset/sakot-ja-bonukset]
+
+        "Muut kustannukset"
+        :muut-kustannukset
+        [muut-kustannukset/muut-kustannukset]]])))
