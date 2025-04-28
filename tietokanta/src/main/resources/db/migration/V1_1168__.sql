3641d79580b3e66eedbeea0a343138d41edffb4d
solita-staskila <samuli.taskila@solita.fi>
2025-04-28 17:25:18 +0300

    Päivitä migraatio, aseta paikkauskohteille tiemerkinnän tila

--- tietokanta/src/main/resources/db/migration/V1_1168__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1168__.sql
@@ -11,11 +11,19 @@ ALTER TABLE paikkauskohde ADD COLUMN "tiemerkinnan-tila" tiemerkinnan_tila_enum 
 -- Aseta defaultit olemassa oleville kohteille
 UPDATE paikkauskohde
 SET "tiemerkinnan-tila" = CASE
+	-- Tiemerkintää ei voida tehdä -> ei tiemerkintää 
     WHEN "paikkauskohteen-tila" IN ('ehdotettu', 'tilattu', 'tarkistettu', 'hylatty') THEN 'ei-tiemerkintaa'
-    -- Paitsi ..
-    -- Kohde valmistunut yli 45 päivää sitten, voidaan asettaa tiemerkintä tehdyksi 
-    WHEN "paikkauskohteen-tila" = 'valmis' AND "valmistumispvm" <= current_date - interval '1 month 15 days' THEN 'valmis'
-    -- Kohde vasta valmistunut, aseta tiemerkintä käsittelemättä tilaan  
-    WHEN "paikkauskohteen-tila" = 'valmis' THEN 'kasittelematta'
+    -- Valmis, tiemerkintää ei tuhoutunut -> ei tiemerkintää 
+    WHEN "paikkauskohteen-tila" = 'valmis' 
+    AND ("tiemerkintaa-tuhoutunut?" IS NULL
+    	OR "tiemerkintaa-tuhoutunut?" IS FALSE) THEN 'ei-tiemerkintaa'
+    -- Kohde valmistunut yli 3kk, voidaan asettaa tiemerkintä tehdyksi 
+    WHEN "paikkauskohteen-tila" = 'valmis' 
+    AND "tiemerkintaa-tuhoutunut?" IS TRUE 
+    AND "valmistumispvm" <= current_date - interval '3 month' THEN 'valmis'
+    -- Kohde vasta valmistunut (alle 3kk sitten), aseta tiemerkintä käsittelemättä tilaan  
+    WHEN "paikkauskohteen-tila" = 'valmis' 
+    AND "tiemerkintaa-tuhoutunut?" IS TRUE  
+    AND "valmistumispvm" >= current_date - interval '3 month' THEN 'kasittelematta'
     ELSE "tiemerkinnan-tila"
 END;
