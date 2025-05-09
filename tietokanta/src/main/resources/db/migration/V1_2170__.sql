852563643e6276e6b119e666139b3884da45ce8a
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2025-05-09 09:22:06 +0300

    HARJA-1451 Luo mhu-muutosten alustava tietomalli ja testidataa

---
+++ tietokanta/src/main/resources/db/migration/V1_2170__.sql
@@ -0,0 +1,133 @@
+CREATE TYPE MHU_MUUTOSTYYPPI AS ENUM (
+    'pysyva',
+    'rahavaraus',
+    'johto-ja-hallintokorvaus',
+    'erillisrahoitettu',
+
+    -- toteutuneisiin määriin perustuva tavoitehinnan muutos, esim. suolaa meni suunniteltua enemmän, koskee määrämitattavia tehtäviä (ns. lihavoidut ja kursivoidut)
+    'toteutuneet-maarat',
+
+    -- poikkeaminen tehtävä- ja määräluettelon määrästä yksittäisen hoitovuoden osalta, (=ei pysyvä muutos)
+    -- esim. tehdään jotain tehtävä- ja määräluettelon tehtävää toisen sijaan (käytännössä siis siirretään rahaa tehtäviltä toisille).
+    'maarapoikkeama'
+);
+
+CREATE TABLE mhu_muutos (
+    id SERIAL PRIMARY KEY,
+    versio INTEGER DEFAULT 1,  -- jokainen tallennus tallentaa täyden version muutoksesta, ml. alitaulut joissa on tietoa
+    urakka INTEGER REFERENCES urakka(id) NOT NULL,
+    voimassa_alkaen DATE,
+    tyyppi MHU_MUUTOSTYYPPI,
+    nimi TEXT,
+    syy TEXT,
+    luoja INTEGER REFERENCES kayttaja(id) NOT NULL,
+    luotu TIMESTAMP DEFAULT NOW() NOT NULL,
+    muokkaaja INTEGER REFERENCES kayttaja(id),
+    muokattu TIMESTAMP DEFAULT NOW(),
+    poistettu BOOLEAN DEFAULT FALSE, -- vain historiarivit voivat olla poistettuja
+
+    kulu_kohdistus INTEGER REFERENCES kulu_kohdistus(id) DEFAULT NULL,
+    luonnos BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE mhu_muutos_kustannusvaikutus (
+    versio INTEGER DEFAULT 1, -- jokainen tallennus tallentaa täyden version muutoksesta, ml. alitaulut joissa on tietoa
+    muutos INTEGER REFERENCES mhu_muutos(id) NOT NULL,
+    kustannuslaji SUUNNITTELU_OSIO,
+    toimenpide INTEGER REFERENCES toimenpide(id),
+    hoitokauden_alkuvuosi INTEGER NOT NULL, -- kustannusvaikutuksen suuruus ko. hoitovuodelle
+    summa NUMERIC -- euroa
+);
+
+
+-- Tehtävä- ja määräluettelon muutoksia voi olla yhtä mhu_muutosta kohta 0...n
+CREATE TABLE mhu_muutos_tehtava_ja_maaraluettelo (
+    versio INTEGER DEFAULT 1, -- jokainen tallennus tallentaa täyden version muutoksesta, ml. alitaulut joissa on tietoa
+    muutos INTEGER REFERENCES mhu_muutos(id) NOT NULL,
+    tehtava INTEGER REFERENCES tehtava(id) NOT NULL,
+    hoitokauden_alkuvuosi INTEGER,
+    edellinen_maara NUMERIC,
+    maaramuutos NUMERIC,
+    uusi_maara NUMERIC
+);
+
+
+CREATE TABLE mhu_muutos_liite (
+    versio INTEGER DEFAULT 1, -- jokainen tallennus tallentaa täyden version muutoksesta, ml. alitaulut joissa on tietoa
+    muutos INTEGER REFERENCES mhu_muutos(id),
+    liite INTEGER REFERENCES liite(id)
+);
+
+
+-- historiataulujen luonti, pyritään hyödyntämään INHERITS-toimintoa. Tarvittaessa historiatauluihin sallittava
+-- duplikaatit primary keyt, tai tehtävä niistä foreign keyt ja historiariveille oma juokseva id
+CREATE TABLE mhu_muutos_historia () INHERITS (mhu_muutos);
+CREATE TABLE mhu_muutos_liite_historia () INHERITS (mhu_muutos_liite);
+CREATE TABLE mhu_muutos_kustannusvaikutus_historia () INHERITS (mhu_muutos_kustannusvaikutus);
+CREATE TABLE mhu_muutos_tehtava_ja_maaraluettelo_historia () INHERITS (mhu_muutos_tehtava_ja_maaraluettelo);
+
+
+-- historiatauluja päivittävät triggerit ja niiden funktiot
+CREATE FUNCTION paivita_mhu_muutos_historia()
+    RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO mhu_muutos_historia
+    VALUES (OLD.*);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER mhu_muutos_historia_trigger
+    BEFORE UPDATE OR DELETE ON mhu_muutos
+    FOR EACH ROW
+EXECUTE FUNCTION paivita_mhu_muutos_historia();
+
+
+CREATE FUNCTION paivita_mhu_muutos_liite_historia()
+    RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO mhu_muutos_liite_historia
+    VALUES (OLD.*);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER mhu_muutos_historia_trigger
+    BEFORE UPDATE OR DELETE ON mhu_muutos_liite
+    FOR EACH ROW
+EXECUTE FUNCTION paivita_mhu_muutos_liite_historia();
+
+
+CREATE FUNCTION paivita_mhu_muutos_kustannusvaikutus_historia()
+    RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO mhu_muutos_kustannusvaikutus_historia
+    VALUES (OLD.*);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER paivita_mhu_muutos_kustannusvaikutus_historia_trigger
+    BEFORE UPDATE OR DELETE ON mhu_muutos_kustannusvaikutus
+    FOR EACH ROW
+EXECUTE FUNCTION paivita_mhu_muutos_kustannusvaikutus_historia();
+
+
+CREATE FUNCTION paivita_mhu_muutos_tehtava_ja_maaraluettelo_historia()
+    RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO mhu_muutos_tehtava_ja_maaraluettelo_historia
+    VALUES (OLD.*);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER mhu_muutos_tehtava_ja_maaraluettelo_historia_trigger
+    BEFORE UPDATE OR DELETE ON mhu_muutos_tehtava_ja_maaraluettelo
+    FOR EACH ROW
+EXECUTE FUNCTION paivita_mhu_muutos_tehtava_ja_maaraluettelo_historia();
+
+-- mhu_muutos tietomalliin liittyvät kommentit
+COMMENT ON column mhu_muutos_kustannusvaikutus.summa IS 'Muutoksen kustannusvaikutus euroina.';
+COMMENT ON type MHU_MUUTOSTYYPPI IS E'MHU-urakoiden tavoitehintamuutoksien mahdolliset tyypit.';
+COMMENT ON table mhu_muutos IS E'Pitää kirjaa tavoitehintaan vaikuttavista muutoksista MHU-urakoissa.';
