02531855a1bb8d5fd03d6137057e936f94b584e5
Samu Mikkonen <samu.mikkonen@solita.fi>
2025-05-16 15:52:56 +0300

    Lisää uusi migraatio missä muut kustanukset ja suorittavan urakan linkki

---
+++ tietokanta/src/main/resources/db/migration/V1_1171__.sql
@@ -0,0 +1,8 @@
+-- Paikkauskohteelle suorittava TR-urakka
+ALTER TABLE paikkauskohde ADD COLUMN suorittava_tiemerkintaurakka integer REFERENCES urakka (id);
+
+-- Lisää muut kustannukset päällystyskohteille
+ALTER TABLE tiemerkinta_yllapitokohteen_kustannus ADD COLUMN muut_kustannukset NUMERIC(10, 2);
+
+-- Lisää muut kustannukset päällystyskohteille
+ALTER TABLE tiemerkinta_paikkauskohteen_kustannus ADD COLUMN muut_kustannukset NUMERIC(10, 2);
