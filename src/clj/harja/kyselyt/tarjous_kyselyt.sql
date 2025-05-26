db011408ff10d7edb12cc22b025cbad20ff4a865
Markus Vähäkangas <markus.vahakangas@solita.fi>
2025-05-26 11:07:58 +0300

    Varmista yksikkötesteillä, että uusiin tarjoustauluihin voi tallentaa tietoja

---
+++ src/clj/harja/kyselyt/tarjous_kyselyt.sql
@@ -0,0 +1,12 @@
+-- name: tallenna-tarjous<!
+INSERT INTO tarjous (hoitokauden_alkuvuosi, urakka_id, tarjous_tavoitehinta, tarjous_kattohinta, luoja, luotu)
+VALUES (:hoitokauden_alkuvuosi, :urakka_id, :tarjous_tavoitehinta, :tarjous_kattohinta, :luoja, NOW());
+
+-- name: tallenna-tarjouskustannus<!
+INSERT INTO tarjous_kustannukset (tarjous_id, urakka_id, hoitokauden_alkuvuosi, summa, osio, tehtava_id, tehtavaryhma_id, rahavaraus_id, luoja, luotu)
+VALUES (:tarjous_id, :urakka_id, :hoitokauden_alkuvuosi, :summa, :osio::suunnittelu_osio, :tehtava_id, :tehtavaryhma_id, :rahavaraus_id, :luoja, NOW());
+
+-- name: tallenna-tarjousjohto-ja-hallintkorvaus<!
+INSERT INTO tarjous_johto_ja_hallintokorvaus (tarjous_id, urakka_id, hoitokauden_alkuvuosi, summa, osio, johto_ja_hallintokorvaus_toimenkuva_id, tehtavaryhma_id, tehtava_id, luoja, luotu)
+VALUES (:tarjous_id, :urakka_id, :hoitokauden_alkuvuosi, :summa, :osio::suunnittelu_osio, :johto_ja_hallintokorvaus_toimenkuva_id, :tehtavaryhma_id, :tehtava_id, :luoja, NOW());
+
