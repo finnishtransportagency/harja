1a45469653e971dd609785385b79fb52a36e0664
Samu Mikkonen <samu.mikkonen@solita.fi>
2025-06-16 15:26:48 +0300

    Poista hämäävä testi data kannasta

--- tietokanta/testidata/tehtavamaarat.sql
+++ tietokanta/testidata/tehtavamaarat.sql
@@ -179,8 +179,3 @@ WHERE nimi = 'Sorastus';
 -- Korjataan sorastuksen yksikkö
 UPDATE tehtava SET yksikko = 'tonni', suunnitteluyksikko = 'tonni'
 WHERE nimi = 'Sorastus';
-
--- 
-UPDATE tehtava SET pakollinen_uudessa_kulussa = true WHERE nimi IN (
-  'Portaiden talvihoito'
-);
