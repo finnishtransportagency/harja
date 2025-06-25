c0da7e8dd92fedf3485ce5d4303b2450141a1298
Jarno Väyrynen <jarno.vayrynen@solita.fi>
2025-06-25 15:27:10 +0300

    Korjaa testejä ja lisää testidataan JJH-muutos

--- test/clj/harja/palvelin/palvelut/muutos_palvelu_test.clj
+++ test/clj/harja/palvelin/palvelut/muutos_palvelu_test.clj
@@ -48,28 +48,27 @@
         liite-id (ffirst (q "SELECT id FROM liite WHERE nimi = 'rumpu.jpg'"))
         vastaus (hae-urakan-muutostiedot +kayttaja-jvh+ {:urakka-id urakka-id
                                                          :valittu-hoitokausi valittu-hoitokausi})
-        odotetut-kirjatut-muutokset [{:kulu_kohdistus nil,
-                                      :kustannusvaikutukset (list {:summa 1000, :toimenpide toimenpide-id-paall-paikk, :kustannuslaji "hankintakustannukset"}),
-                                      :tavoitehinnan-muutos 1000
+        odotetut-kirjatut-muutokset [{:kulu_kohdistus nil, :kustannusvaikutukset (list {:summa 1000, :toimenpide 2391, :kustannuslaji "hankintakustannukset"}),
                                       :voimassa_alkaen #inst "2025-05-06T21:00:00.000-00:00", :syy "Täytyykin tehdä enemmän päällysteiden paikkausta, koska pahat kelirikot.",
-                                      :tehtavat_ja_maarat
-                                      (list {:tehtava 3116, :uusi_maara 1100, :maaramuutos 100, :edellinen_maara 1000}),
-                                      :urakka urakka-id, :nimi "Päällysteen paikkausmuutos", :id 1, :liitteet nil, :versio 1, :luonnos false, :tyyppi "pysyva"}
-                                     {:kulu_kohdistus nil,
-                                      :kustannusvaikutukset (list {:summa 3000, :toimenpide toimenpide-id-soratiet, :kustannuslaji "hankintakustannukset"}),
-                                      :tavoitehinnan-muutos 3000
+                                      :tehtavat_ja_maarat (list {:tehtava 3116, :uusi_maara 1100, :maaramuutos 100, :edellinen_maara 1000}), :urakka 36, :nimi "Päällysteen paikkausmuutos",
+                                      :id 1, :jjh-muutosten-summa nil, :liitteet nil, :versio 1, :luonnos false, :tavoitehinnan-muutos 1000, :tyyppi "pysyva"}
+                                     {:kulu_kohdistus nil, :kustannusvaikutukset (list {:summa 3000, :toimenpide 608, :kustannuslaji "hankintakustannukset"}),
                                       :voimassa_alkaen #inst "2025-05-06T21:00:00.000-00:00", :syy "Tehdään lisäksi tämä isohko sorastus, ei ollut tiedossa ennen urakan alkua.",
-                                      :tehtavat_ja_maarat nil,
-                                      :urakka urakka-id, :nimi "Erillisrahoitettu sorastusmuutos", :id 2, :liitteet nil, :versio 1, :luonnos false, :tyyppi "erillisrahoitettu"}
-                                     {:kulu_kohdistus nil,
-                                      :kustannusvaikutukset (list {:summa 1000, :toimenpide toimenpide-id-mhu-yllapito, :kustannuslaji "hankintakustannukset"}),
-                                      :tavoitehinnan-muutos 1000
+                                      :tehtavat_ja_maarat nil, :urakka 36, :nimi "Erillisrahoitettu sorastusmuutos",
+                                      :id 2, :jjh-muutosten-summa nil, :liitteet nil, :versio 1, :luonnos false, :tavoitehinnan-muutos 3000, :tyyppi "erillisrahoitettu"}
+                                     {:kulu_kohdistus nil, :kustannusvaikutukset (list {:summa 1000, :toimenpide 700, :kustannuslaji "hankintakustannukset"}),
                                       :voimassa_alkaen #inst "2025-05-06T21:00:00.000-00:00", :syy "Ei tehdä tänä kesänä rumpuja, ovat vielä kunnossa.",
-                                      :tehtavat_ja_maarat
-                                      (list {:tehtava 1406, :uusi_maara 0, :maaramuutos -40, :edellinen_maara 40} {:tehtava 3029, :uusi_maara 0, :maaramuutos -30, :edellinen_maara 30}),
-                                      :urakka urakka-id, :nimi "Tämän hoitovuoden määräpoikkeamamuutos", :id 3,
-                                      :liitteet (list {:liite liite-id, :muutos 3}), :versio 1, :luonnos false, :tyyppi "maarapoikkeama"}]]
-    (is (= (count (:kirjatut-muutokset vastaus)) 3) "oikea määrä muutoksia")
+                                      :tehtavat_ja_maarat (list {:tehtava 1406, :uusi_maara 0, :maaramuutos -40, :edellinen_maara 40} {:tehtava 3029, :uusi_maara 0, :maaramuutos -30, :edellinen_maara 30}),
+                                      :urakka 36, :nimi "Tämän hoitovuoden määräpoikkeamamuutos",
+                                      :id 3, :jjh-muutosten-summa nil, :liitteet (list {:liite 11, :muutos 3}), :versio 1, :luonnos false, :tavoitehinnan-muutos 1000, :tyyppi "maarapoikkeama"}
+                                     {:kulu_kohdistus nil, :kustannusvaikutukset nil,
+                                      :voimassa_alkaen #inst "2025-06-24T21:00:00.000-00:00", :syy "Työmääräarviot ylittyivät",
+                                      :tehtavat_ja_maarat nil, :urakka 36, :nimi nil,
+                                      :id 4, :jjh-muutosten-summa 1230M, :liitteet nil, :versio 1, :luonnos false, :tavoitehinnan-muutos 1230M, :tyyppi "johto-ja-hallintokorvaus"}]
+
+
+        ]
+    (is (= (count (:kirjatut-muutokset vastaus)) 4) "oikea määrä muutoksia")
     (is (every? (fn [rivi] (some #(= rivi %) (:kirjatut-muutokset vastaus))) odotetut-kirjatut-muutokset)
       "Kaikki muutosrivit löytyvät vastausjoukosta")))
 
@@ -97,7 +96,7 @@
                   (hae-urakan-muutostiedot +kayttaja-jvh+ {:urakka-id urakka-id
                                                           :valittu-hoitokausi valittu-hoitokausi})
                   [:budjettitavoitteet :muutosten-vaikutus-yhteensa])]
-    (is (= vastaus -29840M) "Rahavarausten muutosten kokonaissumma")))
+    (is (= vastaus -28610M) "Muutosten vaikutus yhteensä")))
 
 (deftest hae-urakan-muutostiedot-ii-kun-annetuilla-ehdoilla-ei-loydy
   (let [urakka-id (hae-urakan-id-nimella "Iin MHU 2021-2026")
