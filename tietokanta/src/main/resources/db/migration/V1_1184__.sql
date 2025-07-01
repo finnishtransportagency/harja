06056c2825b530bd2768eb19ac201d7e7c7845e1
Maarit Laine <maarit.laine@solita.fi>
2025-07-01 14:31:38 +0300

    Nimeä sarake niin etteivät vanhat SQL-lauseet hämäänny

--- tietokanta/src/main/resources/db/migration/V1_1184__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1184__.sql
@@ -2,4 +2,4 @@
 -- Ideaalitapauksessa toimenpide olisi not null, mutta Alataso, lisätyöt dummy-tehtäväryhmän takia näin ei voida tehdä (se linkittyy tehtävien kautta kolmeen toimenpiteeseen).
 -- Selvitetään erikseen voidaanko dummy-tehtäväryhmästä luopua (siihen kuuluvia tehtäviä käytetään lisätöiden toteumien mutta ei kulujen kirjaamiseen) ja päivitetään saraketta myöhemmin, jos voidaan.
 ALTER TABLE tehtavaryhma
-    ADD COLUMN toimenpide INTEGER references toimenpide (id);
+    ADD COLUMN toimenpide_id INTEGER references toimenpide (id);
