d620b99bb85a29e41c08d5794d265f4a863ed67d
Toni Dahl <toni.dahl@solita.fi>
2025-07-10 22:30:44 +0300

    Päivitä ClojureScript versioon 1.10.764

--- src/cljs/harja/tiedot/istunto.cljs
+++ src/cljs/harja/tiedot/istunto.cljs
@@ -9,10 +9,12 @@
             [goog.dom :as dom]
             [goog.events :as events]
             [reagent.core :as reagent]
-            [goog.net.cookies :as cookie])
+            [goog.net.Cookies])
   (:import [goog.events EventType])
   (:require-macros [cljs.core.async.macros :refer [go]]
                    [reagent.ratom :refer [reaction]]))
+
+(defonce ^:dynamic *cookies* (.getInstance goog.net.Cookies))
 
 (def kayttaja (atom nil))
 
@@ -113,7 +115,7 @@
               (:testikayttajat k))))
 
 (defonce testikayttaja
-  (reaction (let [tk (cookie/get "testikayttaja")]
+  (reaction (let [tk (.get *cookies* "testikayttaja")]
               (some #(when (= tk (:kayttajanimi %)) %) @testikayttajat))))
 
 (defn testikaytto-mahdollista? []
@@ -121,8 +123,8 @@
 
 (defn aseta-testikayttaja! [kayttaja]
   (if-not kayttaja
-    (cookie/remove "testikayttaja")
-    (cookie/set "testikayttaja" (:kayttajanimi kayttaja)))
+    (.remove *cookies* "testikayttaja")
+    (.set *cookies* "testikayttaja" (:kayttajanimi kayttaja)))
   (.reload js/window.location))
 
 
