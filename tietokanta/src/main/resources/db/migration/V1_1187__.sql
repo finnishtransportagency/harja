3204c64a6c708c3655da45f2d7b822a1b843bcee
Maarit Laine <maarit.laine@solita.fi>
2025-07-11 13:04:30 +0300

    Lisää kaista-aineiston sisäänluku geometriapäivitystaululla hallittavaksi

---
+++ tietokanta/src/main/resources/db/migration/V1_1187__.sql
@@ -0,0 +1,2 @@
+-- Uusi geometria-aineiston ajastus, mutta entuudestaan tuttu aineisto: päällystyskohteiden validoinnissa käytetty kaista-aineisto.
+INSERT INTO geometriapaivitys(nimi, paikallinen, kaytossa, lisatieto) VALUES ('laajennettu-tieverkko', true, true, 'Kaista-aineiston tietokantaan luku. Harja lukee kaista-aineiston vain paikallisesta sijainnista. Tiedon hakee rajapinnasta lambda.'
