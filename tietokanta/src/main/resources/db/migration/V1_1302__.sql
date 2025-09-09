c77eede86d86738cce0145d5d0c1bd8acbecda28
solita-staskila <samuli.taskila@solita.fi>
2025-09-09 14:50:38 +0300

    Korjaa enum nimitykset, käytä LIKE .. INCLUDING ALL versiointiin

--- tietokanta/src/main/resources/db/migration/V1_1302__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1302__.sql
@@ -2,7 +2,7 @@
 -- Tehtävä- ja määrätoteumiin perustuvat tavoitehintamuutokset
 -- ---------------------------------------------------------------------
 
-CREATE TYPE muutos_yksikkohinta_lahde_enum AS ENUM ('laskettu','aseta','manuaali');
+CREATE TYPE muutos_yksikkohinta_lahde_enum AS ENUM ('laskettu','valittu','puuttuu');
 
 
 CREATE TABLE mhu_muutos_tehtava_tiedot (
@@ -29,71 +29,59 @@ CREATE TABLE mhu_muutos_tehtava_tiedot (
     AND valitun_yksikkohinnan_hoitokausi IS NULL 
     AND kasin_syotetty_tavoitehintamuutos IS NULL)
     OR
-    (lahde = 'manuaali'                               -- Kun lähde on manuaali, käsin syötetty tavoitehinta hinta on olemassa
+    (lahde = 'puuttuu'                                -- Kun lähde puuttuu, käsin syötetty tavoitehinta hinta on olemassa
     AND kasin_syotetty_tavoitehintamuutos IS NOT NULL 
     AND valitun_yksikkohinnan_hoitokausi IS NULL)
     OR 
-    (lahde = 'aseta'                                  -- Kun lähde on aseta, valittu yksikköhinta hinta on olemassa
+    (lahde = 'valittu'                                -- Kun lähde on valittu, valittu yksikköhinta hinta on olemassa
     AND kasin_syotetty_tavoitehintamuutos IS NULL 
     AND valitun_yksikkohinnan_hoitokausi IS NOT NULL)
   )
 );
 
-CREATE INDEX muutos_tehtava_tiedot_idx ON mhu_muutos_tehtava_tiedot (urakka, tehtava, hoitokauden_alkuvuosi, versio);
+
+-- Versiointi taulu 
+CREATE TABLE mhu_muutos_tehtava_tiedot_historia
+  (LIKE mhu_muutos_tehtava_tiedot INCLUDING ALL);
 
 
-CREATE TABLE IF NOT EXISTS mhu_muutos_tehtava_tiedot_historia (
-  urakka    INTEGER NOT NULL,
-  tehtava   INTEGER NOT NULL,
-  hoitokauden_alkuvuosi INTEGER NOT NULL,
+-- Tehdään tälle oma indeksi versiointiin 
+ALTER TABLE mhu_muutos_tehtava_tiedot_historia
+  DROP CONSTRAINT IF EXISTS mmt_pk,
+  DROP CONSTRAINT IF EXISTS mhu_muutos_tehtava_tiedot_historia_pkey,
+  ADD  CONSTRAINT mmt_hist_pk
+       PRIMARY KEY (urakka, tehtava, hoitokauden_alkuvuosi, versio);
+ 
 
-  lahde muutos_yksikkohinta_lahde_enum NOT NULL,
-  valitun_yksikkohinnan_hoitokausi INTEGER,
-  kasin_syotetty_tavoitehintamuutos NUMERIC(12,2),
-  syy TEXT,
-
-  luotu TIMESTAMP,
-  luoja INTEGER,
-  muokattu TIMESTAMP,
-  muokkaaja INTEGER,
-
-  versio INTEGER NOT NULL,
-  validi_aikana TSTZRANGE NOT NULL,
-
-  PRIMARY KEY (urakka, tehtava, hoitokauden_alkuvuosi, versio)
-);
-
-CREATE INDEX IF NOT EXISTS muutos_tehtava_historia_idx ON mhu_muutos_tehtava_tiedot_historia (urakka, tehtava, hoitokauden_alkuvuosi);
+-- Sekä aikavälille. Tätä luultavasti tarvitaan jos käytetään  
+CREATE INDEX muutos_tehtava_historia_validi_idx
+  ON mhu_muutos_tehtava_tiedot_historia USING GIST (validi_aikana);
 
 
 CREATE OR REPLACE FUNCTION paivita_mhu_muutos_tehtava_tiedot_historia()
-RETURNS TRIGGER AS $$
-  DECLARE versio_seur int;
+  RETURNS TRIGGER AS $$
 BEGIN
 
   IF TG_OP IN ('UPDATE','DELETE') THEN
-    SELECT COALESCE(MAX(versio),0)+1
-      INTO versio_seur
-      FROM mhu_muutos_tehtava_tiedot_historia v
-     WHERE v.urakka = OLD.urakka
-       AND v.tehtava = OLD.tehtava
-       AND v.hoitokauden_alkuvuosi = OLD.hoitokauden_alkuvuosi;
-
-    INSERT INTO mhu_muutos_tehtava_tiedot_historia
-      (urakka, tehtava, hoitokauden_alkuvuosi, lahde,
-       valitun_yksikkohinnan_hoitokausi, kasin_syotetty_tavoitehintamuutos, syy,
-       luotu, luoja, muokattu, muokkaaja,
-       versio, validi_aikana)
-    VALUES
-      (OLD.urakka, OLD.tehtava, OLD.hoitokauden_alkuvuosi, OLD.lahde,
-       OLD.valitun_yksikkohinnan_hoitokausi, OLD.kasin_syotetty_tavoitehintamuutos, OLD.syy,
-       OLD.luotu, OLD.luoja, OLD.muokattu, OLD.muokkaaja,
-       versio_seur, TSTZRANGE(LOWER(OLD.validi_aikana), CURRENT_TIMESTAMP));
+    INSERT INTO mhu_muutos_tehtava_tiedot_historia (
+        urakka, tehtava, hoitokauden_alkuvuosi, lahde,
+        valitun_yksikkohinnan_hoitokausi, 
+        kasin_syotetty_tavoitehintamuutos, syy,
+        luotu, luoja, muokattu, muokkaaja,
+        versio, validi_aikana
+    )
+    VALUES (
+      OLD.urakka, OLD.tehtava, OLD.hoitokauden_alkuvuosi, OLD.lahde,
+      OLD.valitun_yksikkohinnan_hoitokausi, 
+      OLD.kasin_syotetty_tavoitehintamuutos, OLD.syy,
+      OLD.luotu, OLD.luoja, OLD.muokattu, OLD.muokkaaja,
+      OLD.versio, TSTZRANGE(LOWER(OLD.validi_aikana), NOW())
+    );
   END IF;
 
   IF TG_OP = 'UPDATE' THEN
-    NEW.versio := OLD.versio +1;
-    NEW.validi_aikana := TSTZRANGE(CURRENT_TIMESTAMP, 'infinity'::timestamp with time zone);
+    NEW.versio := OLD.versio + 1;
+    NEW.validi_aikana := TSTZRANGE(NOW(),'infinity');
     RETURN NEW;
   ELSE
     RETURN OLD;
@@ -104,9 +92,15 @@ $$ LANGUAGE plpgsql;
 
 
 DROP TRIGGER IF EXISTS muutos_tehtava_update_trigger ON mhu_muutos_tehtava_tiedot;
+DROP TRIGGER IF EXISTS muutos_tehtava_delete_trigger ON mhu_muutos_tehtava_tiedot;
 
 
 CREATE TRIGGER muutos_tehtava_update_trigger
   BEFORE UPDATE ON mhu_muutos_tehtava_tiedot
-  FOR EACH ROW
-EXECUTE FUNCTION paivita_mhu_muutos_tehtava_tiedot_historia();
+  FOR EACH ROW EXECUTE FUNCTION paivita_mhu_muutos_tehtava_tiedot_historia();
+
+
+-- Me ei yleensä käytetä deleteä, mutta laitetaan nyt tämäkin 
+CREATE TRIGGER muutos_tehtava_delete_trigger
+  BEFORE DELETE ON mhu_muutos_tehtava_tiedot
+  FOR EACH ROW EXECUTE FUNCTION paivita_mhu_muutos_tehtava_tiedot_historia();
