794c8b648f4ec081a41de25919c8b3ffa46eafff
Markus Vähäkangas <markus.vahakangas@solita.fi>
2025-10-03 11:41:51 +0300

    Refaktoroi muutamia tiedostoja

--- src/clj/harja/kyselyt/materiaalit.clj
+++ src/clj/harja/kyselyt/materiaalit.clj
@@ -5,4 +5,6 @@
   {:positional? true})
 
 (declare hae-materiaalikoodit listaa-materiaalikoodit hae-materiaaliluokat
-  hae-urakan-suunniteltu-materiaalin-kaytto-analytiikalle hae-talvisuolan-materiaaliluokka)
+  hae-urakan-suunniteltu-materiaalin-kaytto-analytiikalle hae-talvisuolan-materiaaliluokka
+  hae-urakan-toteutuneet-materiaalit-raportille hae-urakan-suunnitellut-materiaalit-raportille
+  hae-hallintayksikon-toteutuneet-materiaalit-raportille hae-koko-maan-toteutuneet-materiaalit-raportille)
