0652e1fe6f3aa0bd5f5ffca99768f87d87d940ee
Lauri Näyhä <102288218+solita-laurin@users.noreply.github.com>
2025-10-10 14:08:45 +0300

    Rahavaraus taulun muutos järjestyskolumni default 0:sta sekvenssiksi

---
+++ tietokanta/src/main/resources/db/migration/V1_1213__.sql
@@ -0,0 +1,16 @@
+-- Muuta rahavaraus-tauluun jarjestys-sarake, joka on automaattisesti kasvava
+-- ja täytä sarake nykyisillä arvoilla.
+-- Tämä helpottaa jatkossa rahavarauksien järjestämistä käyttöliittymässä
+UPDATE rahavaraus
+SET jarjestys = subquery.uusi_jarjestys
+    FROM (
+         SELECT id, ROW_NUMBER() OVER (ORDER BY id) as uusi_jarjestys
+         FROM rahavaraus
+     ) AS subquery
+WHERE rahavaraus.id = subquery.id;
+CREATE SEQUENCE rahavaraus_jarjestys_seq;
+
+SELECT setval('rahavaraus_jarjestys_seq', COALESCE((SELECT MAX(jarjestys) FROM rahavaraus), 0));
+
+ALTER TABLE rahavaraus
+    ALTER COLUMN jarjestys SET DEFAULT nextval('rahavaraus_jarjestys_seq');
