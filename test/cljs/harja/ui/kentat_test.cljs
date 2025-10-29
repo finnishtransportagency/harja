066533365d5302c4ef5de1f2332047214155c96d
solita-staskila <samuli.taskila@solita.fi>
2025-10-29 07:35:12 +0200

    Lisää pHant0m testi kenttiin

--- test/cljs/harja/ui/kentat_test.cljs
+++ test/cljs/harja/ui/kentat_test.cljs
@@ -68,6 +68,13 @@
      (is (= "80" (val)))
      (is (= 80 @data))
 
+      "Keskeneräinen negatiivinen numero ei päivitä dataa"
+      (val! "100")
+      (val! "-")
+      --
+      (is (= "-" (val)))
+      (is (= 100 @data))
+
      "Negatiivinen luku"
      (val! "-42")
      --
