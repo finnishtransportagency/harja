98f98cf9fd5b78f91a206730c602a1556add6ce6
solita-staskila <samuli.taskila@solita.fi>
2025-11-18 16:09:58 +0200

    Implenetoi ja asenna parempi npm less tarkkailu

--- kaynnista_harja_front_dev.sh
+++ kaynnista_harja_front_dev.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-# Tapa less-tarkkailu, kun tämä prosessi tapetaan
-trap 'kill $( pgrep -f [t]arkkaile_less_muutoksia.sh || echo '' ); exit' INT TERM
+clean() {
+  set +e
+  jobs -p | xargs -r kill
+}
+
+trap clean EXIT INT TERM
+
+lein deps
+[ -d node_modules ] || npm ci
 
 if [[ $# -eq 0 ]]
 then
@@ -13,10 +20,10 @@ fi
 
 # shellcheck source=../harja_dir.sh
 source "$( dirname "${BASH_SOURCE[0]}" )/sh/harja_dir.sh" || exit
-bash "${HARJA_DIR}"/sh/tarkkaile_less.sh
+
 if [[ "$ENV_PROFILE" = "true" ]]
 then
-  lein trampoline build-dev
+  npm run less & lein trampoline build-dev
 else
-  lein trampoline build-dev-no-env
+  npm run less & lein trampoline build-dev-no-env
 fi
