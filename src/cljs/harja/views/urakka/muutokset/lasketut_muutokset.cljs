4d4174a12750899dacec75cf1061d713901d86c6
solita-staskila <samuli.taskila@solita.fi>
2025-11-24 09:58:46 +0200

    Tarkenna toteumamäärien desimaaleja

--- src/cljs/harja/views/urakka/muutokset/lasketut_muutokset.cljs
+++ src/cljs/harja/views/urakka/muutokset/lasketut_muutokset.cljs
@@ -143,7 +143,7 @@
             :tyyppi :string
             :solun-luokka solun-luokka-fn
             :muokattava? (constantly false)
-            :leveys 10}
+            :leveys 13}
 
            {:otsikko "Muutoksen syy / lisätieto"
             :nimi :syy
@@ -167,7 +167,7 @@
            {:otsikko "Toteutunut määrä"
             :nimi :maara
             :tyyppi :numero
-            :fmt #(fmt/desimaaliluku-opt % 0 2 true)
+            :fmt #(fmt/desimaaliluku-opt % 0 3 true)
             :solun-luokka solun-luokka-fn
             :muokattava? (constantly false)
             :leveys 15}
