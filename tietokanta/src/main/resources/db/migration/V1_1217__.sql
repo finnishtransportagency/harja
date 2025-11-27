542a1d8230418ecfdbb6f55b4571eebe5464b57f
Maarit Laine <maarit.laine@solita.fi>
2025-11-27 11:44:28 +0200

    Tielupien vaatimat elinvoimakeskusmuutokset tauluihin

---
+++ tietokanta/src/main/resources/db/migration/V1_1217__.sql
@@ -0,0 +1,14 @@
+-- Lisää elivoimakeskustiedolle oma sarake tielupatauluun.
+-- Jatkossa ely- ja elinvoimakeskus-sarakkeet voidaan ehkä yhdistää yhdeksi, organisaatio.id-tauluun
+-- viittaavaksi hallintoyksikko-sarakkeeksi. Siirtymävaiheessa rajapinta vastaanottaa tiedon
+-- molemmista organisaatioista.
+ALTER TABLE tielupa
+ADD COLUMN elinvoimakeskus INTEGER REFERENCES organisaatio(id);
+
+-- Ely ei voi enää olla pakollinen tielupataulussa.
+ALTER TABLE tielupa
+    ALTER COLUMN ely DROP NOT NULL;
+
+ALTER TABLE organisaatio
+    ADD COLUMN elinvoimakeskusnumero INTEGER;
+COMMENT ON COLUMN organisaatio.elinvoimakeskusnumero IS E'Elinvoimakeskusten lyhenteissä on päällekkäisyyttä elyjen kanssa. Erillisen elynumero- ja elinvoimanumerosarakkeen avulla voidaan tunnistaa hallintoyksiköt riittävästi.';
