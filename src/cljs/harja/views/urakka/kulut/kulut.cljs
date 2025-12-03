1aa47a5b4e4c11c77ab6c88432aff669cbc1fed1
Henrik Korpi <henrik.korpi@solita.fi>
2025-12-03 09:36:23 +0200

    Poista uusi kolumni UI:sta

--- src/cljs/harja/views/urakka/kulut/kulut.cljs
+++ src/cljs/harja/views/urakka/kulut/kulut.cljs
@@ -24,8 +24,7 @@
   [:tr.table-default-strong.klikattava
    {:on-click #(swap! auki? not)}
    [:td.col-xs-1 (str (pvm/pvm erapaiva))]
-   [:td.col-xs-1.sailyta-rivilla (str "HA" maksuera)]
-   [:td.col-xs-1.sailyta-rivilla (str maksuera-alias)]
+   [:td.col-xs-1.sailyta-rivilla (if maksuera-alias (str "HA" maksuera " / " maksuera-alias) (str "HA" maksuera))]
    [:td.col-xs-4 (get-in toimenpiteet [tpi :toimenpide])]
    [:td.col-xs-4
     [:span.col-xs-6  "Yhteensä"]
@@ -39,7 +38,7 @@
 (defn koontilasku-otsikko 
   [nro summa]
   [:tr.table-default-thin.valiotsikko.table-default-strong
-   [:td {:colSpan "5"}
+   [:td {:colSpan "4"}
     (str (if (zero? nro)
            "Kulut ilman koontilaskun nroa"
            (str "Koontilasku nro " nro)) " yhteensä")] 
@@ -49,17 +48,16 @@
 (defn laskun-erapaiva-otsikko
   [erapaiva]
   [:tr.table-default-thin.valiotsikko.table-default-strong
-   [:td {:colSpan "7"} (str erapaiva)]])
+   [:td {:colSpan "6"} (str erapaiva)]])
 
 (defn kulu-rivi 
   [{:keys [e!]} {:keys [id toimenpide-nimi tehtavaryhma-nimi maksuera maksuera-alias liitteet summa erapaiva]}]
   [:tr.klikattava 
    {:on-click (fn [] (e! (tiedot/->AvaaKulu id)))}
    [:td.col-xs-1 (str (when erapaiva (pvm/pvm erapaiva)))]
-   [:td.col-xs-1.sailyta-rivilla (str "HA" maksuera)]
-   [:td.col-xs-1.sailyta-rivilla (str maksuera-alias)]
+   [:td.col-xs-1.sailyta-rivilla (if maksuera-alias (str "HA" maksuera " / " maksuera-alias) (str "HA" maksuera)) ]
    [:td.col-xs-4 toimenpide-nimi]
-   [:td.col-xs-3 tehtavaryhma-nimi]
+   [:td.col-xs-4 tehtavaryhma-nimi]
    [:td.col-xs-1.tasaa-oikealle.sailyta-rivilla (fmt/euro-opt summa)]
    [:td.col-xs-1.tasaa-oikealle (when-not (empty? liitteet) [ikonit/harja-icon-action-add-attachment])]])
 
@@ -134,9 +132,8 @@
        [:tr
         [:th.col-xs-1 "Pvm"]
         [:th.col-xs-1 "Maksuerä"]
-        [:th.col-xs-1 "Maksuerä-alias"]
         [:th.col-xs-4 "Toimenpide"]
-        [:th.col-xs-3 "Tehtäväryhmä"]
+        [:th.col-xs-4 "Tehtäväryhmä"]
         [:th.col-xs-1.tasaa-oikealle "Määrä"]
         [:th.col-xs-1 ""]]]
       [:tbody
--- tietokanta/src/main/resources/db/migration/V1_1220__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1220__.sql
@@ -1,3 +1,4 @@
--- Lisää maksuera_alias sarake maksuera tauluun
+-- Lisää maksuera_alias sarake maksuera tauluun Elinvoimakeskus muutoksen takia
+-- Koskee vain 4 urakkaa, tiedot syötetään käsin. 
 ALTER TABLE maksuera
 ADD COLUMN maksuera_alias varchar(128);
