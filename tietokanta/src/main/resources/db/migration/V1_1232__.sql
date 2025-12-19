93925cac0ae70ac01eb98d087333c0f2ba37f5f7
Maarit Laine <maarit.laine@solita.fi>
2025-12-19 08:21:50 +0200

    Poista elinvoimakeskustaulu

--- tietokanta/src/main/resources/db/migration/V1_1232__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1232__.sql
@@ -1,13 +1,10 @@
--- Tallenna elinvoimakeskukset
--- Elynumero-kentän merkitystä on laajennettu. Kun ELYä haetaan numeron perusteella, täytyy rajauksessa ottaa mukaan myös organisaatio.tyyppi.
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, tyyppi, liikennemuoto) VALUES ('Uudenmaan elinvoimakeskus', 'UUD', 1, 'elinvoimakeskus', 'T');
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, tyyppi, liikennemuoto) VALUES ('Lounais-Suomen elinvoimakeskus', 'LOU', 2, 'elinvoimakeskus', 'T');
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, tyyppi, liikennemuoto) VALUES ('Kaakkois-Suomen elinvoimakeskus', 'KAS', 3, 'elinvoimakeskus', 'T');
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, tyyppi, liikennemuoto) VALUES ('Sisä-Suomen elinvoimakeskus', 'SIS', 4, 'elinvoimakeskus', 'T');
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, tyyppi, liikennemuoto) VALUES ('Keski-Suomen elinvoimakeskus', 'KES', 5, 'elinvoimakeskus', 'T');
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, tyyppi, liikennemuoto) VALUES ('Itä-Suomen elinvoimakeskus', 'ITA', 6, 'elinvoimakeskus', 'T');
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, tyyppi, liikennemuoto) VALUES ('Etelä-Pohjanmaan elinvoimakeskus', 'EPO', 7, 'elinvoimakeskus', 'T');
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, tyyppi, liikennemuoto) VALUES ('Pohjanmaan elinvoimakeskus', 'POH', 8, 'elinvoimakeskus', 'T');
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, tyyppi, liikennemuoto) VALUES ('Pohjois-Suomen elinvoimakeskus', 'PSU', 9, 'elinvoimakeskus', 'T');
-INSERT INTO organisaatio (nimi, lyhenne, elynumero, tyyppi, liikennemuoto) VALUES ('Lapin elinvoimakeskus', 'LAP', 10, 'elinvoimakeskus', 'T');
+-- Tallennetaan elinvoimakeskukset omantyyppisenä organisaationa organisaatiotauluun
+ALTER TABLE urakka
+    DROP CONSTRAINT urakka_elinvoimakeskus_id_fkey,
+    ADD CONSTRAINT urakka_elinvoimakeskus_id_fkey FOREIGN KEY (elinvoimakeskus_id) REFERENCES organisaatio (id);
 
+ALTER TABLE tielupa
+    DROP CONSTRAINT tielupa_elinvoimakeskus_id_fkey,
+    ADD CONSTRAINT tielupa_elinvoimakeskus_id_fkey FOREIGN KEY (elinvoimakeskus_id) REFERENCES organisaatio (id);
+
+DROP TABLE elinvoimakeskus;
