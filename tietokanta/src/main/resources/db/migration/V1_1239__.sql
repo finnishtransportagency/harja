04192068ef8827fe0fb2c70dec08c223928f22ba
Mari Korkka <mari.korkka@solita.fi>
2026-01-16 15:15:08 +0200

    Muuta migraatio

--- tietokanta/src/main/resources/db/migration/V1_1239__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1239__.sql
@@ -1,7 +1,2 @@
 -- Lisää "sorateiden pinnan hoito" suoritettavatehtava-enumiin
 ALTER TYPE suoritettavatehtava ADD VALUE 'sorateiden pinnan hoito' AFTER 'sorateiden polynsidonta';
-
--- Päivitä tehtävän suoritettavatehtava-sarake
-UPDATE tehtava
-SET suoritettavatehtava = 'sorateiden pinnan hoito'
-WHERE id = 2869;
---
+++ tietokanta/src/main/resources/db/migration/V1_1240__.sql
@@ -0,0 +1,4 @@
+-- Päivitä tehtävän suoritettavatehtava-sarake
+UPDATE tehtava
+SET suoritettavatehtava = 'sorateiden pinnan hoito'
+WHERE id = 2869;
