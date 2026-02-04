07d0ef1d5975cdc3c644d08965b81986ffda1a46
solita-staskila <samuli.taskila@solita.fi>
2026-02-04 10:02:42 +0200

    Siirrä dissokki takaisin

--- src/clj/harja/palvelin/palvelut/valikatselmus/valikatselmukset.clj
+++ src/clj/harja/palvelin/palvelut/valikatselmus/valikatselmukset.clj
@@ -295,13 +295,17 @@
                                       ::muokkaustiedot/luotu (or (::muokkaustiedot/luotu tiedot) (pvm/nyt))
                                       ::muokkaustiedot/muokattu (pvm/nyt)
                                       ::valikatselmus/summa (bigdec (::valikatselmus/summa tiedot))
-                                      ::valikatselmus/hoitokauden-alkuvuosi hoitokauden-alkuvuosi})]
+                                      ::valikatselmus/hoitokauden-alkuvuosi hoitokauden-alkuvuosi})
+
+        oikaisu-specql (if uusi?
+                         (dissoc oikaisu-specql ::valikatselmus/oikaisun-id)
+                         oikaisu-specql)]
 
     (if (and
           (not uusi?)
           (::valikatselmus/oikaisun-id tiedot))
       (valikatselmus-q/paivita-oikaisu db oikaisu-specql)
-      (valikatselmus-q/tee-oikaisu db (dissoc oikaisu-specql ::valikatselmus/oikaisun-id)))
+      (valikatselmus-q/tee-oikaisu db oikaisu-specql))
     ;; Hae välikatselmuksen tiedot
     (hae-valikatselmuksen-tiedot-hoitovuodelle db kayttaja {:urakkaid urakka-id :hoitovuosi hoitokauden-alkuvuosi})))
 
