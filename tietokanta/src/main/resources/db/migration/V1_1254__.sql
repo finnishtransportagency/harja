d8e90ade173bd5ba5d583ae4955a4def6118bdbf
Maarit Laine <maarit.laine@solita.fi>
2026-04-10 09:23:06 +0300

    Päivitä uudet numerot

--- tietokanta/src/main/resources/db/migration/V1_1254__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1254__.sql
@@ -1,3 +1,22 @@
-ALTER TABLE organisaatio
-    ALTER COLUMN elynumero TYPE BIGINT,
-    ALTER COLUMN elinvoimakeskusnumero TYPE BIGINT;
+UPDATE organisaatio SET elynumero = '380040', elinvoimakeskusnumero = '380040' WHERE tyyppi = 'hallintayksikko' AND lyhenne = 'UUD';
+UPDATE organisaatio SET elynumero = '380041', elinvoimakeskusnumero = '380041' WHERE tyyppi = 'hallintayksikko' AND lyhenne = 'VAR';
+UPDATE organisaatio SET elynumero = '380043', elinvoimakeskusnumero = '380043' WHERE tyyppi = 'hallintayksikko' AND lyhenne = 'PIR';
+UPDATE organisaatio SET elynumero = '380042', elinvoimakeskusnumero = '380042' WHERE tyyppi = 'hallintayksikko' AND lyhenne = 'KAS';
+UPDATE organisaatio SET elynumero = '380045', elinvoimakeskusnumero = '380045' WHERE tyyppi = 'hallintayksikko' AND lyhenne = 'POS';
+UPDATE organisaatio SET elynumero = '380044', elinvoimakeskusnumero = '380044' WHERE tyyppi = 'hallintayksikko' AND lyhenne = 'KES';
+UPDATE organisaatio SET elynumero = '380046', elinvoimakeskusnumero = '380046' WHERE tyyppi = 'hallintayksikko' AND lyhenne = 'EPO';
+UPDATE organisaatio SET elynumero = '380048', elinvoimakeskusnumero = '380048' WHERE tyyppi = 'hallintayksikko' AND lyhenne = 'POP';
+UPDATE organisaatio SET elynumero = '380049', elinvoimakeskusnumero = '380049' WHERE tyyppi = 'hallintayksikko' AND lyhenne = 'LAP';
+
+UPDATE organisaatio SET elinvoimakeskusnumero = '380040' WHERE tyyppi = 'elinvoimakeskus' AND lyhenne = 'UUD';
+UPDATE organisaatio SET elinvoimakeskusnumero = '380041' WHERE tyyppi = 'elinvoimakeskus' AND lyhenne = 'LOU';
+UPDATE organisaatio SET elinvoimakeskusnumero = '380043' WHERE tyyppi = 'elinvoimakeskus' AND lyhenne = 'SIS';
+UPDATE organisaatio SET elinvoimakeskusnumero = '380042' WHERE tyyppi = 'elinvoimakeskus' AND lyhenne = 'KAS';
+UPDATE organisaatio SET elinvoimakeskusnumero = '380045' WHERE tyyppi = 'elinvoimakeskus' AND lyhenne = 'ITÄ';
+UPDATE organisaatio SET elinvoimakeskusnumero = '380044' WHERE tyyppi = 'elinvoimakeskus' AND lyhenne = 'KES';
+UPDATE organisaatio SET elinvoimakeskusnumero = '380046' WHERE tyyppi = 'elinvoimakeskus' AND lyhenne = 'EPO';
+UPDATE organisaatio SET elinvoimakeskusnumero = '380048' WHERE tyyppi = 'elinvoimakeskus' AND lyhenne = 'PSU';
+UPDATE organisaatio SET elinvoimakeskusnumero = '380049' WHERE tyyppi = 'elinvoimakeskus' AND lyhenne = 'LAP';
+UPDATE organisaatio SET elinvoimakeskusnumero = '380047' WHERE tyyppi = 'elinvoimakeskus' AND lyhenne = 'POH';
+
+UPDATE organisaatio SET nimi = 'Väylävirasto' where nimi = 'Liikennevirasto';
