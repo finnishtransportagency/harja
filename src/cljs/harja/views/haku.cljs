6772d6db7d2a596138e550aea746dbe8faf1c4b1
Markus Vähäkangas <markus.vahakangas@solita.fi>
2026-04-22 10:54:49 +0300

    Korjaa typot

--- src/cljs/harja/views/haku.cljs
+++ src/cljs/harja/views/haku.cljs
@@ -111,9 +111,9 @@
                 ;; hae hallintayksikön urakat jo tässä, jottei urakkaa aseteta ennen kuin
                 ;; urakkalista on päivittynyt. Muuten voi tulla ajoitusongelma toisinaan, mikä johtaa siihen että URL-parametriin ei tule urakan id:tä.
                 hallintayksikon-urakkalista (<! (urakat/hae-elinvoimakeskuksen-urakat
-                                                  {:id (get-in haettu-urakka [:elinvoimakekus :id])}))]
+                                                  {:id (:elinvoimakeskus haettu-urakka [:elinvoimakeskus :id])}))]
             (reset! nav/hallintayksikon-urakkalista hallintayksikon-urakkalista)
-            (nav/aseta-hallintayksikko-ja-urakka-varmistuksella! (get-in haettu-urakka [:elinvoimakekus :id]) haettu-urakka))
+            (nav/aseta-hallintayksikko-ja-urakka-varmistuksella! (get-in haettu-urakka [:elinvoimakeskus :id]) haettu-urakka))
           :organisaatio
           (let [haettu-organisaatio (<! (k/post! :hae-organisaatio (:id tulos)))]
             (valitse-organisaatio haettu-organisaatio))))))
