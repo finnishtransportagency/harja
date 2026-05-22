97d0db588f49f3b35baf43af28fa615f7b367464
Markus Vähäkangas <markus.vahakangas@solita.fi>
2026-05-22 07:39:58 +0300

    Päivitä tietokannasta poistettavat triggerit ja funktiot

--- tietokanta/src/main/resources/db/migration/V1_1262__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1262__.sql
@@ -2,9 +2,11 @@
 
 DROP TRIGGER IF EXISTS tg_toteuma_tehtava_hoitokauden_alkuvuosi ON toteuma_tehtava;
 DROP TRIGGER IF EXISTS tg_toteuma_materiaali_hoitokauden_alkuvuosi ON toteuma_materiaali;
+DROP TRIGGER IF EXISTS tg_toteuma_alkanut_paivitta_tehtavamateriaali_hoitokausi ON toteuma;
 
 DROP FUNCTION IF EXISTS paivita_hoitokauden_alkuvuosi_tehtavalle();
 DROP FUNCTION IF EXISTS paivita_hoitokauden_alkuvuosi_materiaalille();
+DROP FUNCTION IF EXISTS paivita_hoitokauden_alkuvuosi_tehtava_ja_materiaalitauluista();
 
 -- Poistetaan toteumittain-hoitokauden_alkuvuosi-funktio, jolla täytettiin hoitokauden alkuvuosi toteuma_tehtava- ja toteuma_materiaali-tauluihin,
 -- koska tätä tietoa ei enää tarvita erikseen, kun hoitokauden alkuvuosi päivitetään suoraan näihin tauluihin koodin avulla.
