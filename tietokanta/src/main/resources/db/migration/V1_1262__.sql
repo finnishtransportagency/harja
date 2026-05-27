170aea5cf43e9c24f4a59c344f57c9b8f4f49c65
Mari Korkka <mari.korkka@solita.fi>
2026-05-27 13:28:45 +0300

    Poista migraatio, koska se on siirretty toiseen branchiin

--- tietokanta/src/main/resources/db/migration/V1_1262__.sql
+++
@@ -1,3 +0,0 @@
--- Tallennetaan laskutusrajan alkuperäinen arvo ennen muutostöiden korotuksia.
--- Käytetään laskutusrajan automaattisessa tarkistuksessa hoitovuoden aikana.
-ALTER TABLE urakka_tavoite ADD COLUMN IF NOT EXISTS laskutusraja_alkuperainen NUMERIC;
