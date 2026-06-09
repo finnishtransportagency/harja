af9091370d75eb12a4a3670e9c9ae370bbe13812
Mari Korkka <mari.korkka@solita.fi>
2026-06-09 14:51:56 +0300

    Muuta laskutusyhteenvedon sarakkeita

--- src/clj/harja/palvelin/raportointi/raportit/laskutusyhteenveto_taulukko_yhteiset.clj
+++ src/clj/harja/palvelin/raportointi/raportit/laskutusyhteenveto_taulukko_yhteiset.clj
@@ -117,11 +117,7 @@
                     (and
                       (not laskutusraja-ylittynyt?)
                       (= otsikko "Laskutusraja"))
-                    [;; Näytä oikealla valittu aikaväli
-                     (when valittu-aikavali?
-                       (valitaulukko-rivi data aikavali {:lihavoi? true :aikavali? true :kyseessa-kk-vali? kyseessa-kk-vali?}))
-
-                     ;; Kun valittu oma aikaväli, laskutusrajaa ei näytetä
+                    [;; Kun valittu oma aikaväli, laskutusrajaa ei näytetä
                      (when-not valittu-aikavali?
                        (valitaulukko-rivi data (str otsikko " " (yhteiset/summa-fmt (:laskutusraja_yht data)))))
 
@@ -155,11 +151,7 @@
                     (and
                       laskutusraja-ylittynyt?
                       (= otsikko "Laskutusraja"))
-                    [;; Näytä oikealla valittu aikaväli
-                     (when valittu-aikavali?
-                       (valitaulukko-rivi data aikavali {:lihavoi? true :aikavali? true :kyseessa-kk-vali? kyseessa-kk-vali?}))
-
-                     ;; Kun valittu oma aikaväli, laskutusrajaa ei näytetä
+                    [;; Kun valittu oma aikaväli, laskutusrajaa ei näytetä
                      (when-not valittu-aikavali?
                        (valitaulukko-rivi data (str otsikko " " (yhteiset/summa-fmt (:laskutusraja_yht data)))))
 
@@ -236,11 +228,7 @@
                     ;; -----------------------------
                     ;; Tavoitehinnan ulkopuoliset viimeisenä 
                     :else
-                    [;; Näytä oikealla valittu aikaväli
-                     (when valittu-aikavali?
-                       (valitaulukko-rivi data aikavali {:lihavoi? true :aikavali? true :kyseessa-kk-vali? kyseessa-kk-vali?}))
-
-                     (valitaulukko-rivi data "Tavoitehinnan ulkopuoliset kustannukset yhteensä"
+                    [(valitaulukko-rivi data "Tavoitehinnan ulkopuoliset kustannukset yhteensä"
                        {:lihavoi? true
                         :tyyli "vahvistamaton"
                         :hk-alku-vahvistamaton? true
@@ -258,7 +246,7 @@
                 :raportin-tunniste :tyomaa-yhteenveto}
      (rivi
        {:otsikko " " :otsikkorivi-luokka "otsikko-ei-taustaa" :leveys 33 :tyyppi :varillinen-teksti}
-       {:otsikko (if valittu-aikavali? vapaa-aikavali-teksti laskutettu-teksti)
+       {:otsikko (if valittu-aikavali? (or vapaa-aikavali-teksti aikavali) laskutettu-teksti)
         :otsikkorivi-luokka "otsikko-ei-taustaa" :leveys 15 :tyyppi :varillinen-teksti}
        (when kyseessa-kk-vali?
          {:otsikko laskutetaan-teksti :otsikkorivi-luokka "otsikko-ei-taustaa" :leveys 33 :tyyppi :varillinen-teksti}))
