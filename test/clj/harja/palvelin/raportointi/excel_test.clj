3e73b1191a1951a030d5e3cf2851a6153957f951
Markus Vähäkangas <markus.vahakangas@solita.fi>
2026-06-11 10:37:24 +0300

    Korjaa numero-opt excel yksikkötesti

--- test/clj/harja/palvelin/raportointi/excel_test.clj
+++ test/clj/harja/palvelin/raportointi/excel_test.clj
@@ -15,12 +15,14 @@
                   :sheet-nimi "Testi"}
        [{:otsikko "Määrä" :fmt :numero-opt}]
        [[400.0]
-        [400.5]]]
+        [400.5]
+        [400.22344534534534]]]
       workbook)
     (let [sheet (.getSheetAt workbook 0)
-          integer-cell (.getCell (.getRow sheet 3) 0)
-          desimaali-cell (.getCell (.getRow sheet 4) 0)]
-      (is (= "400" (.formatCellValue formatter integer-cell)))
-      (is (= "400,5" (.formatCellValue formatter desimaali-cell)))
-      (is (= "#,##0.##" (.getDataFormatString (.getCellStyle integer-cell)))))))
+          integer-arvo (.getCell (.getRow sheet 3) 0)
+          desimaali-arvo (.getCell (.getRow sheet 4) 0)
+          pitkä-desimaali-arvo (.getCell (.getRow sheet 5) 0)]
+      (is (= "400" (.formatCellValue formatter integer-arvo)))
+      (is (= "400,5" (.formatCellValue formatter desimaali-arvo)))
+      (is (= "400,22" (.formatCellValue formatter pitkä-desimaali-arvo))))))
 
