8a0baf1fd5491bb420876f6c5c0956e3c665eca2
etahto <elias.tahto@solita.fi>
2026-06-23 14:36:48 +0300

    HARJA-2248: Korjaa virhe testidatassa

--- tietokanta/testidata/urakat2.sql
+++ tietokanta/testidata/urakat2.sql
@@ -46,11 +46,11 @@ WHERE urakkaid = (SELECT id FROM urakka WHERE nimi = 'POP MHU Kajaani 2025-2030'
 
 -- Kalustoresurssit-alasivun testaamista varten tarvittava testidata.
 -- Luodaan MHU26-urakka (alkuvuosi 2026) Suunnittelu/Kalustoresurssit-alasivun testaamista varten.
--- Kopioidaan hallintayksikkö, elinvoimakeskus ja urakoitsija Oulun MHU 2025-2030 -urakalta.
+-- Kopioidaan hallintayksikkö, elinvoimakeskus ja urakoitsija Kittilän MHU 2025-2030 -urakalta.
 INSERT INTO urakka (sampoid,        hallintayksikko, elinvoimakeskus_id, nimi,                     alkupvm,      loppupvm,     tyyppi, urakkanro, urakoitsija, alue)
-SELECT              '1242141-OULU6', hallintayksikko, elinvoimakeskus_id, 'Sodankylän MHU 2026-2031', '2026-10-01', '2031-09-30', tyyppi, '1447',    urakoitsija, alue
+SELECT              '1242141-KITT6', hallintayksikko, elinvoimakeskus_id, 'Sodankylän MHU 2026-2031', '2026-10-01', '2031-09-30', tyyppi, '1447',    urakoitsija, alue
   FROM urakka
- WHERE nimi = 'Oulun MHU 2025-2030';
+ WHERE nimi = 'Kittilän MHU 2025-2030';
 
 INSERT INTO sopimus (nimi, alkupvm, loppupvm, sampoid, urakka)
 VALUES ('Sodankylän MHU sopimus 26', '2026-10-01', '2031-09-30', '11333380-LAP2',
