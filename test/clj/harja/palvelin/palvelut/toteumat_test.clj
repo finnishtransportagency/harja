88961dd677c54caec0a34b74c9aa56cdc4d55950
Samu Mikkonen <samu.mikkonen@solita.fi>
2026-06-23 10:15:06 +0300

    Refaktoroit testien mappeja

--- test/clj/harja/palvelin/palvelut/toteumat_test.clj
+++ test/clj/harja/palvelin/palvelut/toteumat_test.clj
@@ -139,21 +139,24 @@
       (str "DELETE FROM erilliskustannus
                     WHERE pvm = '2005-12-12' AND lisatieto = '" toteuman-lisatieto "'"))))
 
+(defn- luo-testi-bonus [urakka-id sopimus-id toimenpideinstanssi-id lisatieto tyyppi rahasumma]
+  {:urakka-id urakka-id
+   :sopimus sopimus-id
+   :toimenpideinstanssi toimenpideinstanssi-id
+   :pvm (pvm/->pvm "15.10.2021")
+   :laskutuskuukausi (pvm/->pvm "15.10.2021")
+   :rahasumma rahasumma
+   :indeksin_nimi "MAKU 2020"
+   :tyyppi tyyppi
+   :lisatieto lisatieto
+   :palauta-tallennettu? true})
+
 (deftest tallenna-erilliskustannus-hylkaa-bonusprofiilin-vastaisen-mhu-bonuksen-domain-virheena
   (let [urakka-id (hae-iin-maanteiden-hoitourakan-2021-2026-id)
         sopimus-id (hae-iin-maanteiden-hoitourakan-2021-2026-sopimus-id)
         toimenpideinstanssi-id (hae-toimenpideinstanssi-id-nimella "Iin MHU 2021-2026 MHU ja HJU Hoidon johto")
         lisatieto "Bonusprofiilin vastainen write-path testi"
-        bonus {:urakka-id urakka-id
-               :sopimus sopimus-id
-               :toimenpideinstanssi toimenpideinstanssi-id
-               :pvm (pvm/->pvm "15.10.2021")
-               :laskutuskuukausi (pvm/->pvm "15.10.2021")
-               :rahasumma 1234.0
-               :indeksin_nimi "MAKU 2020"
-               :tyyppi "muu-bonus"
-               :lisatieto lisatieto
-               :palauta-tallennettu? true}
+        bonus (luo-testi-bonus urakka-id sopimus-id toimenpideinstanssi-id lisatieto "muu-bonus" 1234.0)
         maara-ennen (ffirst (q (format "SELECT count(*) FROM erilliskustannus WHERE lisatieto = '%s'" lisatieto)))]
     (try+
       (tallenna-erilliskustannus (:db jarjestelma) +kayttaja-jvh+ bonus)
@@ -169,16 +172,7 @@
         sopimus-id (hae-iin-maanteiden-hoitourakan-2021-2026-sopimus-id)
         toimenpideinstanssi-id (hae-toimenpideinstanssi-id-nimella "Iin MHU 2021-2026 MHU ja HJU Hoidon johto")
         lisatieto "Bonusprofiilin mukainen write-path testi"
-        bonus {:urakka-id urakka-id
-               :sopimus sopimus-id
-               :toimenpideinstanssi toimenpideinstanssi-id
-               :pvm (pvm/->pvm "15.10.2021")
-               :laskutuskuukausi (pvm/->pvm "15.10.2021")
-               :rahasumma 4321.0
-               :indeksin_nimi "MAKU 2020"
-               :tyyppi "asiakastyytyvaisyysbonus"
-               :lisatieto lisatieto
-               :palauta-tallennettu? true}
+        bonus (luo-testi-bonus urakka-id sopimus-id toimenpideinstanssi-id lisatieto "asiakastyytyvaisyysbonus" 4321.0)
         tallennettu (tallenna-erilliskustannus (:db jarjestelma) +kayttaja-jvh+ bonus)]
     (is (= urakka-id (:urakka tallennettu)) "Sallittu bonus tallentuu edelleen oikealle urakalle")
     (is (= toimenpideinstanssi-id (:toimenpideinstanssi tallennettu)) "Sallittu bonus tallentuu oikeaan toimenpideinstanssiin")
@@ -263,22 +257,26 @@
                     WHERE id = " (get-in lisatty [:toteuma :id])))))
 
 
+(defn- luo-testi-yksikkohintainen-tyo [urakka-id sopimus-id tyon-pvm hoitokausi-aloituspvm hoitokausi-lopetuspvm lisatieto]
+  {:urakka-id urakka-id
+   :sopimus-id sopimus-id
+   :alkanut tyon-pvm :paattynyt tyon-pvm
+   :hoitokausi-aloituspvm hoitokausi-aloituspvm
+   :hoitokausi-lopetuspvm hoitokausi-lopetuspvm
+   :suorittajan-nimi "Alihankkijapaja Ky" :suorittajan-ytunnus "123456-Y"
+   :tyyppi :yksikkohintainen
+   :toteuma-id nil
+   :lisatieto lisatieto
+   :tehtavat [{:toimenpidekoodi 1368 :maara 333}]})
+
 (deftest tallenna-yksikkohintainen-toteuma-testi
   (let [tyon-pvm (konv/sql-timestamp (pvm/luo-pvm 2005 11 24)) ;;24.12.2005
         hoitokausi-aloituspvm (pvm/luo-pvm 2005 9 1) ; 1.10.2005
         hoitokausi-lopetuspvm (pvm/luo-pvm 2006 8 30) ;30.9.2006
         urakka-id @oulun-alueurakan-2005-2010-id
         toteuman-lisatieto "Testikeissin lisätieto4"
-        tyo {:urakka-id urakka-id
-             :sopimus-id @oulun-alueurakan-2005-2010-paasopimuksen-id
-             :alkanut tyon-pvm :paattynyt tyon-pvm
-             :hoitokausi-aloituspvm hoitokausi-aloituspvm
-             :hoitokausi-lopetuspvm hoitokausi-lopetuspvm
-             :suorittajan-nimi "Alihankkijapaja Ky" :suorittajan-ytunnus "123456-Y"
-             :tyyppi :yksikkohintainen
-             :toteuma-id nil
-             :lisatieto toteuman-lisatieto
-             :tehtavat [{:toimenpidekoodi 1368 :maara 333}]}
+        tyo (luo-testi-yksikkohintainen-tyo urakka-id @oulun-alueurakan-2005-2010-paasopimuksen-id
+              tyon-pvm hoitokausi-aloituspvm hoitokausi-lopetuspvm toteuman-lisatieto)
         hae-summat #(->> (kutsu-palvelua (:http-palvelin jarjestelma)
                            :urakan-toteumien-tehtavien-summat
                            +kayttaja-jvh+
@@ -362,16 +360,8 @@
         hoitokausi-lopetuspvm (pvm/luo-pvm 2006 8 30) ;30.9.2006
         urakka-id @oulun-alueurakan-2005-2010-id
         toteuman-lisatieto "Testikeissin lisätieto4"
-        tyo {:urakka-id urakka-id
-             :sopimus-id @oulun-alueurakan-2005-2010-paasopimuksen-id
-             :alkanut tyon-pvm :paattynyt tyon-pvm
-             :hoitokausi-aloituspvm hoitokausi-aloituspvm
-             :hoitokausi-lopetuspvm hoitokausi-lopetuspvm
-             :suorittajan-nimi "Alihankkijapaja Ky" :suorittajan-ytunnus "123456-Y"
-             :tyyppi :yksikkohintainen
-             :toteuma-id nil
-             :lisatieto toteuman-lisatieto
-             :tehtavat [{:toimenpidekoodi 1368 :maara 333}]}
+        tyo (luo-testi-yksikkohintainen-tyo urakka-id @oulun-alueurakan-2005-2010-paasopimuksen-id
+              tyon-pvm hoitokausi-aloituspvm hoitokausi-lopetuspvm toteuman-lisatieto)
         hae-summat #(->> (kutsu-palvelua (:http-palvelin jarjestelma)
                            :urakan-toteumien-tehtavien-summat
                            +kayttaja-jvh+
