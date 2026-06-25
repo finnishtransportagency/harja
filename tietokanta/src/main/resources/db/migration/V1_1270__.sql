fd504db90b1f5ba3aedeefdb9fe720a8c7b1648b
Maarit Laine <maarit.laine@solita.fi>
2026-06-25 11:44:49 +0300

    Korjaa tietokantarajoituksen logiikka

--- tietokanta/src/main/resources/db/migration/V1_1270__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1270__.sql
@@ -6,7 +6,9 @@ ALTER TABLE paikkaus
     DROP CONSTRAINT paikkaus_maara_sallitaanko_null,
     DROP CONSTRAINT yksikko_sallitaanko_null,
     ADD CONSTRAINT yksikko_sallitaanko_null
-        CHECK ("paikkaus-tyyppi" = 'paikkaus' OR (("maara" IS NULL OR "maara" = 0) AND "reikapaikkaus-yksikko" IS NULL) OR ("maara" IS NOT NULL AND "reikapaikkaus-yksikko" IS NOT NULL) );
+        CHECK ("paikkaus-tyyppi" = 'paikkaus' OR
+               (("maara" IS NULL OR "maara" = 0) OR
+               ("maara" IS NOT NULL AND "reikapaikkaus-yksikko" IS NOT NULL AND "reikapaikkaus-yksikko" != '')));
 
 -- Lisätään tauluun kenttä, johon voidaan tallentaa reikäpaikkauksiin liittyviä luokitteluja
 ALTER TABLE paikkaus
