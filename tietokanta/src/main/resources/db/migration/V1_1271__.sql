33ef26e5ef7345149dc3a16a61be3173cfb1fc2b
Maarit Laine <maarit.laine@solita.fi>
2026-06-30 08:34:50 +0300

    Mahdollista kaistan ja ajoradan tallennus tieosoitteesta

--- tietokanta/src/main/resources/db/migration/V1_1271__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1271__.sql
@@ -1,6 +1,7 @@
 -- Muutoksia reikäpaikkauksiin liittyvissä tietovaatimuksissa
 -- Yksittäiselle paikkaukselle ei koskaan vaadita kustannustietoa tai materiaalimäärää
 -- Yksikkö on pakollinen vain silloin, kun reikäpaikkauksen materiaalimäärä on yli nolla
+-- Yksikön saa kirjata, vaikka määrää ei ole annettu
 ALTER TABLE paikkaus
     DROP CONSTRAINT kustannus_sallitaanko_null,
     DROP CONSTRAINT paikkaus_maara_sallitaanko_null,
@@ -8,9 +9,16 @@ ALTER TABLE paikkaus
     ADD CONSTRAINT yksikko_sallitaanko_null
         CHECK ("paikkaus-tyyppi" = 'paikkaus' OR
                (("maara" IS NULL OR "maara" = 0) OR
-               ("maara" IS NOT NULL AND "reikapaikkaus-yksikko" IS NOT NULL AND "reikapaikkaus-yksikko" != '')));
+                ("maara" IS NOT NULL AND "reikapaikkaus-yksikko" IS NOT NULL AND "reikapaikkaus-yksikko" != '')));
 
 -- Lisätään tauluun kenttä, johon voidaan tallentaa reikäpaikkauksiin liittyviä luokitteluja
 ALTER TABLE paikkaus
     ADD COLUMN luokittelu TEXT[];
+
+-- Mahdollistetaan ajoradan ja kaistan tallentaminen paikkaukselle
+-- Siivotaan vanha toteutus pois eri pr:ssä
+ALTER TABLE paikkaus
+    ADD COLUMN tieosoite_laaja TR_OSOITE_LAAJENNETTU;
+
+
 
