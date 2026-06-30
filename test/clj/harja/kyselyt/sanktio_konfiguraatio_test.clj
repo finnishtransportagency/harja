ed87616644d52b32eaa094c51cf047d7b068521a
Samu Mikkonen <samu.mikkonen@solita.fi>
2026-06-30 15:41:35 +0300

    Päivitä konfiguraatio testi

--- test/clj/harja/kyselyt/sanktio_konfiguraatio_test.clj
+++ test/clj/harja/kyselyt/sanktio_konfiguraatio_test.clj
@@ -3,13 +3,6 @@
             [clojure.test :refer [deftest is]]
             [harja.kyselyt.sanktio-konfiguraatio :as sanktio-konfiguraatio]))
 
-(def ^:private sanktio-profiili-rivi-summamaaritys-migraatio
-  (slurp "tietokanta/src/main/resources/db/migration/V1_1261__.sql"))
-
-(deftest v1-1261-vaatii-vapaalle-ohjetekstille-tekstin-ilman-euromaaraa
-  (is (str/includes? sanktio-profiili-rivi-summamaaritys-migraatio
-        "(maaritystapa = 'vapaa_ohjeteksti' AND summa_euroina IS NULL AND ohjeteksti IS NOT NULL)")
-    "Migraation pitää estää vapaan ohjetekstin rivit, joilla on samanaikaisesti euromäärä."))
 
 (deftest muunna-sanktio-konfiguraatiorivi-sailyttaa-yhteensopivan-lukittujen-summien-vektorin
   (let [rivi (sanktio-konfiguraatio/muunna-sanktio-konfiguraatiorivi
@@ -17,15 +10,15 @@
                 :soveltuvuuskonteksti "laatupoikkeama"
                 :laji_koodi "A"
                 :profiilirivi_summamaaritykset
-  "[{\"maaritystapa\":\"kiintea_euromaara\",\"summa_euroina\":6000.00,\"ohjeteksti\":\"alkavalta viikolta\",\"jarjestys\":1},{\"maaritystapa\":\"vapaa_ohjeteksti\",\"summa_euroina\":null,\"ohjeteksti\":\"tai sopimuksen mukaan\",\"jarjestys\":2}]"})]
-    (is (= [{:maaritystapa :kiintea-euromaara
+                "[{\"maaritystapa\":\"automaattinen\",\"summa_euroina\":6000.00,\"ohjeteksti\":\"alkavalta viikolta\",\"jarjestys\":1},{\"maaritystapa\":\"manuaalinen\",\"summa_euroina\":null,\"ohjeteksti\":\"tai sopimuksen mukaan\",\"jarjestys\":2}]"})]
+    (is (= [{:maaritystapa :automaattinen
              :summa-euroina 6000M
              :ohjeteksti "alkavalta viikolta"
              :jarjestys 1}
-            {:maaritystapa :vapaa-ohjeteksti
-       :summa-euroina nil
+            {:maaritystapa :manuaalinen
+             :summa-euroina nil
              :ohjeteksti "tai sopimuksen mukaan"
              :jarjestys 2}]
-          (get-in rivi [:profiilirivi :summamaaritykset])))
+           (get-in rivi [:profiilirivi :summamaaritykset])))
     (is (= [6000M]
-          (get-in rivi [:profiilirivi :lukitut-summat])))))
+           (get-in rivi [:profiilirivi :lukitut-summat])))))
