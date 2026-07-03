9367958b9992f2e6a4b68e71ce34f350e490b3aa
Samu Mikkonen <samu.mikkonen@solita.fi>
2026-07-03 15:52:42 +0300

    Lisää tooltip rajauksista raportit näkymään

--- src/cljs/harja/views/raportit.cljs
+++ src/cljs/harja/views/raportit.cljs
@@ -12,7 +12,7 @@
             [harja.tiedot.kanavat.urakka.kanavaurakka :as ku]
             [harja.pvm :as pvm]
             [harja.loki :refer [log]]
-            [harja.ui.yleiset :refer [livi-pudotusvalikko vihje ajax-loader-pieni] :as yleiset]
+            [harja.ui.yleiset :refer [livi-pudotusvalikko vihje ajax-loader-pieni tooltip] :as yleiset]
             [harja.tiedot.raportit :as raportit]
             [cljs.core.async :refer [<! timeout]]
             [harja.views.kartta :as kartta]
@@ -817,7 +817,11 @@
             [:h3 "Raportin tiedot"]
 
             [yleiset/tietoja {:class "border-bottom raporttivalinnat-valistys"}
-             "Elinvoimakeskus" [hallintayksikko-ja-urakkatyyppi v-hal v-ur-tyyppi]
+             [:span "Elinvoimakeskus\u00a0"
+              [tooltip {:suunta :oikea}
+               [ikonit/harja-icon-status-info]
+               "Harjan raportteja voi muodostaa vain elinvoimakeskustiedon pohjalta. Vanhatkin urakat on linkitetty elinvoimakeskukseen ELY:n sijaan."]]
+             [hallintayksikko-ja-urakkatyyppi v-hal v-ur-tyyppi]
              "Urakka" (cond
                         ;; Latausindikaattori jos urakkahaku on käynnissä
                         (and v-hal ladataanko-urakoita? @nav/urakka-haku-kaynnissa?)
