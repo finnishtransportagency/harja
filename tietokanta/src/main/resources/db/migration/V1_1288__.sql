a2a4eca779e8deca95dca362798d755bcab16cb2
Mari Korkka <mari.korkka@solita.fi>
2026-08-21 16:20:12 +0300

    Muuta sql

--- tietokanta/src/main/resources/db/migration/V1_1288__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1288__.sql
@@ -1,6 +1,6 @@
 ---- Muuta lupauksessa elokuun määräpäivä oikeaksi
-update lupaus_kustannusennuste_kuukausi_pisteet set paiva = 31, kuvaus = 'Elokuu 31.päivä (2025 urakat)' where "lupaus-id" = 82 and "urakan-alkuvuosi" = 2025 and kuukausi = 8;
-update lupaus_kustannusennuste_kuukausi_pisteet set paiva = 31, kuvaus = 'Elokuu 31.päivä (2026 urakat)' where "lupaus-id" = 114 and "urakan-alkuvuosi" = 2026 and kuukausi = 8;
+update lupaus_kustannusennuste_kuukausi_pisteet set paiva = 31, kuvaus = 'Elokuu 31.päivä (2025 urakat)' where "urakan-alkuvuosi" = 2025 and kuukausi = 8;
+update lupaus_kustannusennuste_kuukausi_pisteet set paiva = 31, kuvaus = 'Elokuu 31.päivä (2026 urakat)' where "urakan-alkuvuosi" = 2026 and kuukausi = 8;
 
 ---- Poista vahingossa luotu laatupoikkeama ja siihen liittyvä sanktio
 DO $$
