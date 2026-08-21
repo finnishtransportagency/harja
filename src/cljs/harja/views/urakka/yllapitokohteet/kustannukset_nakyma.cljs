8299eae88b25339099ebd63ded5a3561624b744c
solita-staskila <samuli.taskila@solita.fi>
2026-08-21 14:14:54 +0300

    Lisää infoteksti & siirtymä sanktioihin

--- src/cljs/harja/views/urakka/yllapitokohteet/kustannukset_nakyma.cljs
+++ src/cljs/harja/views/urakka/yllapitokohteet/kustannukset_nakyma.cljs
@@ -2,18 +2,20 @@
   "MPU ja PPU sopimustyyppisten urakoiden kustannukset"
   (:require [tuck.core :refer [tuck]]
             [cljs-time.core :as t]
-            [harja.domain.oikeudet :as oikeudet]
-            [harja.ui.debug :as debug]
-            [harja.ui.valinnat :as valinnat]
-            [harja.ui.ikonit :as ikonit]
-            [harja.tiedot.navigaatio :as nav]
+
             [harja.ui.grid :as grid]
-            [harja.ui.komponentti :as komp]
-            [harja.ui.yleiset :refer [ajax-loader ajax-loader-pieni]]
+            [harja.ui.debug :as debug]
             [harja.ui.napit :as napit]
-            [harja.tiedot.urakka.yllapitokohteet.kustannukset-tiedot :as tiedot]
+            [harja.ui.ikonit :as ikonit]
+            [harja.ui.komponentti :as komp]
+            [harja.ui.valinnat :as valinnat]
             [harja.tiedot.urakka :as urakka]
+            [harja.tiedot.navigaatio :as nav]
             [harja.tiedot.istunto :as istunto]
+            [harja.domain.oikeudet :as oikeudet]
+            [harja.tiedot.urakka.siirtymat :as siirtymat]
+            [harja.ui.yleiset :refer [ajax-loader ajax-loader-pieni]]
+            [harja.tiedot.urakka.yllapitokohteet.kustannukset-tiedot :as tiedot]
             [harja.views.urakka.yllapitokohteet.kustannukset-apurit :as apurit]))
 
 
@@ -84,6 +86,18 @@
               "Lisää kustannus"
               #(e! (tiedot/->AvaaLomake))
               {:ikoni [ikonit/harja-icon-action-add] :vayla-tyyli? true}]]]
+
+           [:div
+            "Sopimuksen mukaiset sanktiot ja bonukset tulee syöttää "
+            [:a.klikattava.alleviivaa {:on-click #(siirtymat/siirry-annettuun-valilehteen
+                                                    @nav/valittu-hallintayksikko-id (:id @nav/valittu-urakka)
+                                                    {:taso1 :urakat
+                                                     :taso2 :laadunseuranta
+                                                     :taso3 :sanktiot})} "Sanktiot ja bonukset"]
+            [:span " -osiossa tai laatupoikkeaman kautta."]]
+
+           [:div
+            "Urakkaa koskevat muut kulut voi lisätä tämän osion “Lisää kustannus”-toiminnon kautta."]
 
            ;; Muut kustannukset & Sanktiot ja bonukset
            (apurit/muut-kustannukset-grid e! app @urakka/valittu-urakan-vuosi)])))))
