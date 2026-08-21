aac1995e43eb1e7646fd74c717248323a0c6e635
Mari Korkka <mari.korkka@solita.fi>
2026-08-21 15:56:40 +0300

    Päivitä lupauksen määräpäivä ja poista yksi laatupoikkeama

---
+++ tietokanta/src/main/resources/db/migration/V1_1288__.sql
@@ -0,0 +1,25 @@
+---- Muuta lupauksessa elokuun määräpäivä oikeaksi
+update lupaus_kustannusennuste_kuukausi_pisteet set paiva = 31, kuvaus = 'Elokuu 31.päivä (2025 urakat)' where "lupaus-id" = 82 and "urakan-alkuvuosi" = 2025 and kuukausi = 8;
+update lupaus_kustannusennuste_kuukausi_pisteet set paiva = 31, kuvaus = 'Elokuu 31.päivä (2026 urakat)' where "lupaus-id" = 114 and "urakan-alkuvuosi" = 2026 and kuukausi = 8;
+
+---- Poista vahingossa luotu laatupoikkeama ja siihen liittyvä sanktio
+DO $$
+    DECLARE
+lp_id INTEGER;
+BEGIN
+SELECT id INTO lp_id FROM laatupoikkeama WHERE urakka = 455 and kohde = 'jepjep' and luotu = '2026-07-03 12:52:40.891682' LIMIT 1;
+
+IF lp_id IS NOT NULL THEN
+UPDATE sanktio
+SET poistettu = TRUE,
+    muokattu  = NOW(),
+    muokkaaja = (select id from kayttaja where kayttajanimi = 'Integraatio')
+WHERE laatupoikkeama = lp_id;
+
+UPDATE laatupoikkeama
+SET muokkaaja = (select id from kayttaja where kayttajanimi = 'Integraatio'),
+    muokattu  = CURRENT_TIMESTAMP,
+    poistettu = TRUE
+WHERE id = lp_id;
+END IF;
+END $$;
