4cdda791b7a791876636105ab968876cfa651ac2
Markus Vähäkangas <markus.vahakangas@solita.fi>
2026-08-27 12:50:58 +0300

    Lisää uusi sarake ja muut ui muutokset muihin kustannuksiin.

--- src/cljs/harja/views/urakka/paallystys_muut_kustannukset.cljs
+++ src/cljs/harja/views/urakka/paallystys_muut_kustannukset.cljs
@@ -1,10 +1,13 @@
 (ns harja.views.urakka.paallystys-muut-kustannukset
-  (:require [reagent.core :refer [atom] :as r]
-            [harja.ui.yleiset :refer [ajax-loader]]
+  (:require [harja.tiedot.urakka.siirtymat :as siirtymat]
+            [reagent.core :refer [atom] :as r]
+            [harja.ui.yleiset :as yleiset :refer [ajax-loader]]
             [harja.ui.grid :as grid]
             [harja.loki :refer [log logt tarkkaile!]]
             [harja.ui.komponentti :as komp]
             [harja.tiedot.urakka.paallystys-muut-kustannukset :as tiedot]
+            [harja.tiedot.urakka.urakka :as tila]
+            [harja.tiedot.navigaatio :as nav]
             [harja.ui.valinnat :as valinnat]
             [harja.ui.validointi :as validointi]
             [cljs-time.core :as t]
@@ -14,15 +17,28 @@
                    [cljs.core.async.macros :refer [go]]))
 
 ;; Ylläpitokohteiden sarakkeiden leveydet
-(def kustannus-selite-leveys 5)
-(def kustannus-hinta-leveys 3)
-(def kustannus-pvm-leveys 3)
+(def kustannus-selite-leveys "auto")
+(def kustannus-hinta-leveys "144px")
+(def kustannus-pvm-leveys "144px")
 
 (defn- rivi-poistettavissa? [m]
   (log "rivi-poistettavissa? " (pr-str m))
   (or (-> m :muokattava) (-> m :id neg?)))
 
-(def grid-opts {:otsikko "Urakan muut kustannukset"
+(defn otsikkopaneeli []
+  [:div
+   [:h6 "Urakan muut kustannukset"]
+   [:div.body-text {:style {:max-width "900px" :padding-top "16px"}} "Sopimuksen mukaiset sanktiot ja bonukset tulee syöttää"
+    [:a.klikattava.alleviivaa {:href (str "/#urakat/laadunseuranta/sanktiot?&hy=" @nav/valittu-hallintayksikko-id "&u=" (-> @tila/yleiset :urakka :id))
+                               :on-click #(siirtymat/siirry-annettuun-valilehteen
+                                            @nav/valittu-hallintayksikko-id (-> @tila/yleiset :urakka :id)
+                                            {:taso1 :urakat
+                                             :taso2 :laadunseuranta
+                                             :taso3 :sanktiot})}
+     "Sanktiot ja bonukset"]
+    "-osiossa. Laatupoikkeamat- tai “Sanktiot ja bonukset”-osion kautta syötetyt sanktiot ja bonukset tulevat näkyville urakan muihin kustannuksiin lisäyksen jälkeen."]])
+
+(def grid-opts {:otsikko [otsikkopaneeli]
                 :voi-lisata? true
                 :voi-muokata-rivia? :muokattava
                 :esta-poistaminen? (complement rivi-poistettavissa?)
@@ -31,14 +47,28 @@
 
 
 (def grid-skeema
-  [{:otsikko "Pvm" :nimi :pvm :fmt pvm/pvm-opt
+  [{:otsikko "Pvm"
+    :nimi :pvm
+    :fmt pvm/pvm-opt
     :validoi [[:ei-tyhja "Anna päivämäärä"]]
-    :tyyppi :pvm :leveys kustannus-pvm-leveys}
+    :tyyppi :pvm
+    :vayla-tyyli? true
+    :leveys kustannus-pvm-leveys}
+   {:otsikko "Laji"
+    :tyyppi :string
+    :muokattava? (constantly false)
+    :nimi :tyyppi
+    :fmt (fn [tyyppi]
+           (cond
+             (= tyyppi :muu) "Muu"
+             (= tyyppi :sanktio) "Sanktio"
+             (= tyyppi :bonus) "Bonus"
+             :else "Muu"))}
    {:otsikko "Kustannuksen kuvaus" :nimi :selite
     :validoi [[:ei-tyhja "Anna kuvaus"]]
     :tyyppi :string :leveys kustannus-selite-leveys}
-   {:otsikko "Summa" :nimi :hinta :fmt fmt/euro-opt
-    :tyyppi :numero :leveys kustannus-hinta-leveys
+   {:otsikko "Summa (€)" :nimi :hinta :fmt #(fmt/euro-opt false %)
+    :tyyppi :numero :leveys kustannus-hinta-leveys :tasaa :oikea
     :validoi [[:ei-tyhja "Anna hinta"]]}])
 
 (defn muut-kustannukset [urakka]
