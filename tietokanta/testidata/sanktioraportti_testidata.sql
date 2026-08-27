c8e6b11367005400b72858e90ac2a446404b1c85
Samu Mikkonen <samu.mikkonen@solita.fi>
2026-08-27 12:29:33 +0300

    Lisää testidataa

--- tietokanta/testidata/sanktioraportti_testidata.sql
+++ tietokanta/testidata/sanktioraportti_testidata.sql
@@ -134,3 +134,107 @@ SELECT 'asiakastyytyvaisyysbonus',
          ON tpi.urakka = u.id
         AND tpi.nimi = u.nimi || ' MHU ja HJU Hoidon johto'
  WHERE u.nimi = 'POP MHU Suomussalmi 2024-2029';
+
+      WITH tiedot AS (
+          SELECT u.id AS urakka,
+           tpi.id AS toimenpideinstanssi,
+           (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio') AS kayttaja
+         FROM urakka u
+           JOIN toimenpideinstanssi tpi
+             ON tpi.urakka = u.id
+            AND tpi.nimi = u.nimi || ' MHU ja HJU Hoidon johto'
+        WHERE u.nimi = 'Sodankylän MHU 2026-2031'
+      )
+      INSERT INTO sanktio (sakkoryhma, maara, perintapvm, maarattypvm, indeksi,
+               toimenpideinstanssi, tyyppi, suorasanktio, luoja)
+      SELECT 'A'::SANKTIOLAJI,
+          1800,
+          DATE '2026-10-15',
+          DATE '2026-10-15',
+          NULL,
+          toimenpideinstanssi,
+          (SELECT id FROM sanktiotyyppi WHERE koodi = 18),
+          TRUE,
+          kayttaja
+        FROM tiedot;
+
+      WITH tiedot AS (
+          SELECT u.id AS urakka,
+           s.id AS sopimus,
+           tpi.id AS toimenpideinstanssi,
+           (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio') AS kayttaja
+         FROM urakka u
+           JOIN sopimus s ON s.urakka = u.id AND s.paasopimus IS NULL
+           JOIN toimenpideinstanssi tpi
+             ON tpi.urakka = u.id
+            AND tpi.nimi = u.nimi || ' MHU ja HJU Hoidon johto'
+        WHERE u.nimi = 'Sodankylän MHU 2026-2031'
+      )
+      INSERT INTO erilliskustannus (tyyppi, sopimus, urakka, toimenpideinstanssi,
+                  pvm, laskutuskuukausi, rahasumma, indeksin_nimi,
+                  lisatieto, luotu, luoja)
+      SELECT bonus.tyyppi::erilliskustannustyyppi,
+          tiedot.sopimus,
+          tiedot.urakka,
+          tiedot.toimenpideinstanssi,
+          DATE '2026-10-15',
+          DATE '2026-10-15',
+          bonus.summa,
+          NULL,
+          'Sanktioraportin Sodankylän MHU2026-testibonus',
+          CURRENT_TIMESTAMP,
+          tiedot.kayttaja
+        FROM tiedot
+          CROSS JOIN (VALUES
+           ('asiakastyytyvaisyysbonus', 1100),
+           ('alihankkijatyytyvaisyyskyselybonus', 1200),
+           ('maaraaikaan_tehtavien_toiden_aiempi_toteutusbonus', 1300),
+           ('liikennevahinkojen_aiheuttajien_selvitysbonus', 1400)
+          ) AS bonus(tyyppi, summa);
+
+-- Rajaa MHU2026-liikennevahinkobonus vain positiiviseen testikohteeseen.
+INSERT INTO bonus_profiili_rivi_urakka (bonus_profiili_rivi_id, urakka_id,
+                                        luoja, luotu, muokkaaja, muokattu)
+SELECT bpr.id,
+       u.id,
+       (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio'),
+       CURRENT_TIMESTAMP,
+       (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio'),
+       CURRENT_TIMESTAMP
+  FROM bonus_profiili bp
+       JOIN bonus_laji bl
+         ON bl.koodi = 'liikennevahinkojen_aiheuttajien_selvitysbonus'
+       JOIN bonus_profiili_rivi bpr
+         ON bpr.bonus_profiili_id = bp.id
+        AND bpr.bonus_laji_id = bl.id
+       JOIN urakka u
+         ON u.lyhyt_nimi = 'Nummi 26'
+ WHERE bp.nimi = 'teiden-hoito-bonus-mhu2026';
+
+    WITH tiedot AS (
+        SELECT u.id AS urakka,
+         s.id AS sopimus,
+         tpi.id AS toimenpideinstanssi,
+         (SELECT id FROM kayttaja WHERE kayttajanimi = 'Integraatio') AS kayttaja
+       FROM urakka u
+         JOIN sopimus s ON s.urakka = u.id AND s.paasopimus IS NULL
+         JOIN toimenpideinstanssi tpi
+           ON tpi.urakka = u.id
+          AND tpi.nimi = u.nimi || ' MHU ja HJU Hoidon johto'
+      WHERE u.nimi = 'Nummi 26 - liikennevahinkobonuksen kohdistus'
+    )
+    INSERT INTO erilliskustannus (tyyppi, sopimus, urakka, toimenpideinstanssi,
+                pvm, laskutuskuukausi, rahasumma, indeksin_nimi,
+                lisatieto, luotu, luoja)
+    SELECT 'liikennevahinkojen_aiheuttajien_selvitysbonus'::erilliskustannustyyppi,
+        sopimus,
+        urakka,
+        toimenpideinstanssi,
+        DATE '2026-10-15',
+        DATE '2026-10-15',
+        1400,
+        NULL,
+        'Sanktioraportin Nummi 26 liikennevahinkobonuksen kohdistustesti',
+        CURRENT_TIMESTAMP,
+        kayttaja
+      FROM tiedot;
--- tietokanta/testidata/urakat2.sql
+++ tietokanta/testidata/urakat2.sql
@@ -44,16 +44,23 @@ UPDATE urakka_parametrit
 SET laskutusraja_kaytossa = true
 WHERE urakkaid = (SELECT id FROM urakka WHERE nimi = 'POP MHU Kajaani 2025-2030');
 
--- Kalustoresurssit-alasivun testaamista varten tarvittava testidata.
--- Luodaan MHU26-urakka (alkuvuosi 2026) Suunnittelu/Kalustoresurssit-alasivun testaamista varten.
--- Kopioidaan hallintayksikkö, elinvoimakeskus ja urakoitsija Kittilän MHU 2025-2030 -urakalta.
 INSERT INTO urakka (sampoid,        hallintayksikko, elinvoimakeskus_id, nimi,                     alkupvm,      loppupvm,     tyyppi, urakkanro, urakoitsija, alue)
 SELECT              '1242141-KITT6', hallintayksikko, elinvoimakeskus_id, 'Sodankylän MHU 2026-2031', '2026-10-01', '2031-09-30', tyyppi, '1447',    urakoitsija, alue
   FROM urakka
  WHERE nimi = 'Kittilän MHU 2025-2030';
+-- Urakkakohtaisen rajauksen positiivinen MHU2026-testikohde ilman aluehaun urakkanumeroa.
+INSERT INTO urakka (sampoid, hallintayksikko, elinvoimakeskus_id, nimi,
+                    alkupvm, loppupvm, tyyppi, urakkanro, urakoitsija, alue, lyhyt_nimi)
+SELECT 'TEST-NUMMI26', hallintayksikko, elinvoimakeskus_id, 'Nummi 26 - liikennevahinkobonuksen kohdistus',
+       DATE '2026-10-01', DATE '2031-09-30', tyyppi, NULL, urakoitsija, alue, 'Nummi 26'
+  FROM urakka
+ WHERE nimi = 'Kittilän MHU 2025-2030';
+
+INSERT INTO sopimus (nimi, alkupvm, loppupvm, sampoid, urakka)
+VALUES ('Nummi 26 liikennevahinkobonuksen kohdistussopimus', '2026-10-01', '2031-09-30', 'TEST-NUMMI26-SOP',
+  (SELECT id FROM urakka WHERE nimi = 'Nummi 26 - liikennevahinkobonuksen kohdistus'));
+
 
 INSERT INTO sopimus (nimi, alkupvm, loppupvm, sampoid, urakka)
 VALUES ('Sodankylän MHU sopimus 26', '2026-10-01', '2031-09-30', '11333380-LAP2',
         (SELECT id FROM urakka WHERE nimi = 'Sodankylän MHU 2026-2031'));
-
-
