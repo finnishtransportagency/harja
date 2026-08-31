01c8b35d26a38d6d87e25103043cdd4c9d7e84c8
Samu Mikkonen <samu.mikkonen@solita.fi>
2026-08-31 15:06:16 +0300

    Näytä yhteinen nimi laatupoikkeama käyttöliittymässä

--- src/cljs/harja/views/urakka/laadunseuranta/laatupoikkeama.cljs
+++ src/cljs/harja/views/urakka/laadunseuranta/laatupoikkeama.cljs
@@ -201,7 +201,11 @@
              {:otsikko "Määrätty"  :nimi :maarattypvm :fmt pvm/pvm-opt :leveys 1.2}
              {:otsikko "Käsitelty" :nimi :kasittelyaika :hae #(get-in % [:laatupoikkeama :paatos :kasittelyaika]) :fmt pvm/pvm-opt :leveys 1.2})
            {:otsikko "Laji" :nimi :laji :hae #(sanktiot/valitun-urakan-sanktiolajin-nimi (:laji %)) :leveys 2}
-           {:otsikko "Tyyppi" :nimi :tyyppi-nimi :hae #(get-in % [:tyyppi :nimi]) :leveys 2}
+           {:otsikko "Tyyppi" :nimi :tyyppi-nimi
+            :hae #(sanktio-domain/sanktiotyypin-nimi
+                    (sanktiot/valitun-urakan-sanktiolajin-nimi (:laji %))
+                    (:tyyppi %))
+            :leveys 2}
            {:otsikko "Tapah\u00ADtuma\u00ADpaik\u00ADka/kuvaus" :nimi :tapahtumapaikka :hae #(get-in % [:laatupoikkeama :kuvaus]) :leveys 3}
            {:otsikko "Määrä (€)" :nimi :summa :hae #(when (:summa %) (str (:summa %))) :tyyppi :numero :tasaa :oikea :leveys 1.7}]
           (or sanktiot-lista [])]]
