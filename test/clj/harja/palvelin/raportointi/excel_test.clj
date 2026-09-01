a05b8d9ba51378391c9356d90bf325c63321399c
Samu Mikkonen <samu.mikkonen@solita.fi>
2026-09-01 11:40:43 +0300

    Lisää excel-testejä

--- test/clj/harja/palvelin/raportointi/excel_test.clj
+++ test/clj/harja/palvelin/raportointi/excel_test.clj
@@ -1,6 +1,7 @@
 (ns harja.palvelin.raportointi.excel-test
   (:require
     [clojure.test :refer :all]
+    [clojure.string :as str]
     [harja.palvelin.raportointi.excel :as excel])
   (:import
     (org.apache.poi.ss.usermodel DataFormatter)
@@ -9,6 +10,16 @@
 
 (defn- rgb [vari]
   (mapv #(bit-and 0xff %) (.getRGB vari)))
+
+(defn- sheet-tekstit
+  [sheet]
+  (mapcat (fn [rivi]
+            (keep (fn [solu]
+                    (when (= org.apache.poi.ss.usermodel.CellType/STRING
+                             (.getCellType solu))
+                      (.getStringCellValue solu)))
+              (iterator-seq (.cellIterator rivi))))
+    (iterator-seq (.rowIterator sheet))))
 
 (deftest numero-opt-kayttaa-oikeaa-excel-formatointia
   (let [workbook (XSSFWorkbook.)
@@ -92,3 +103,78 @@
           (is (= [154 199 252]
                  (rgb (.getFillForegroundColorColor (.getCellStyle solu))))))))
 
+(deftest taulukot-kayttavat-omaa-valilehtisopimustaan
+  (let [workbook (XSSFWorkbook.)]
+        (excel/muodosta-excel
+          [:raportti {:nimi "Testi"}
+           [:taulukko {:otsikko "Yhteenveto"
+                       :sheet-nimi "Yhteenveto"}
+            [{:otsikko "Rivi"}]
+            [["Yhteenveto"]]]
+           [:taulukko {:otsikko "Sanktiot"
+                       :sheet-nimi "Urakka"}
+            [{:otsikko "Rivi"}]
+            [["Sanktiot"]]]
+           [:taulukko {:otsikko "Bonukset"
+                       :sheet-nimi "Urakka"
+                       :samalle-sheetille? true}
+            [{:otsikko "Rivi"}]
+            [["Bonukset"]]]]
+          workbook)
+        (is (= 2 (.getNumberOfSheets workbook)))
+        (is (= "Yhteenveto" (.getSheetName (.getSheetAt workbook 0))))
+        (is (= "Urakka" (.getSheetName (.getSheetAt workbook 1))))
+        (let [tekstit (mapcat (fn [rivi]
+                                (keep (fn [solu]
+                                        (when (= org.apache.poi.ss.usermodel.CellType/STRING
+                                                 (.getCellType solu))
+                                          (.getStringCellValue solu)))
+                                      (iterator-seq (.cellIterator rivi))))
+                              (iterator-seq (.rowIterator (.getSheetAt workbook 1))))]
+          (is (some #(= "Sanktiot" %) tekstit))
+          (is (some #(= "Bonukset" %) tekstit)))))
+
+(deftest valilehtien-nimet-puhdistetaan-katkaistaan-ja-yksiloidaan
+  (let [workbook (XSSFWorkbook.)
+        sheet-nimi (str "Sama urakka " (apply str (repeat 30 "x")) "/[?]")]
+    (excel/muodosta-excel
+      [:raportti {:nimi "Testi"}
+       [:taulukko {:otsikko "Ensimmäinen"
+                   :sheet-nimi sheet-nimi}
+        [{:otsikko "Rivi"}]
+        [["Ensimmäinen"]]]
+       [:taulukko {:otsikko "Toinen"
+                   :sheet-nimi sheet-nimi}
+        [{:otsikko "Rivi"}]
+        [["Toinen"]]]]
+      workbook)
+    (let [sheetit (mapv #(.getSheetAt workbook %) (range (.getNumberOfSheets workbook)))
+          nimet (mapv #(.getSheetName %) sheetit)
+          kielletyt-merkit ["\\" "/" "?" "*" "[" "]" ":"]]
+      (is (= 2 (count sheetit)))
+      (is (every? #(<= (count %) 31) nimet))
+      (is (every? #(not-any? (fn [merkki] (str/includes? % merkki))
+                       kielletyt-merkit)
+            nimet))
+      (is (= 2 (count (set (map str/lower-case nimet)))))
+      (is (some #(= "Ensimmäinen" %) (sheet-tekstit (first sheetit))))
+      (is (some #(= "Toinen" %) (sheet-tekstit (second sheetit)))))))
+
+(deftest taulukon-omat-metatiedot-sailyvat-aggregaatissa
+  (let [workbook (XSSFWorkbook.)]
+    (excel/muodosta-excel
+      [:raportti {:nimi "Koko maa"
+                  :raportin-yleiset-tiedot {:raportin-nimi "Koko maa"}}
+       [:taulukko {:sheet-nimi "Testiurakka"
+                   :raportin-tiedot {:raportin-nimi "Testiurakka"
+                                     :alkupvm "01.10.2025"
+                                     :loppupvm "30.09.2030"}}
+        [{:otsikko "Rivi"}]
+        [["Arvo"]]]]
+      workbook)
+    (is (= "Testiurakka, 01.10.2025 - 30.09.2030"
+           (-> workbook
+             (.getSheetAt 0)
+             (.getRow 0)
+             (.getCell 0)
+             (.getStringCellValue))))))
