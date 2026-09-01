f0dfce5fa56b27f164505ad9f32acc4416268bb1
Samu Mikkonen <samu.mikkonen@solita.fi>
2026-09-01 15:02:29 +0300

    Nimeä migraatio uudestaan

--- tietokanta/src/main/resources/db/migration/V1_1284__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1284__.sql
@@ -2,3 +2,9 @@
 ALTER TYPE erilliskustannustyyppi ADD VALUE IF NOT EXISTS 'alihankkijatyytyvaisyyskyselybonus';
 ALTER TYPE erilliskustannustyyppi ADD VALUE IF NOT EXISTS 'maaraaikaan_tehtavien_toiden_aiempi_toteutusbonus';
 ALTER TYPE erilliskustannustyyppi ADD VALUE IF NOT EXISTS 'liikennevahinkojen_aiheuttajien_selvitysbonus';
+
+-- Nimeä raportti uudelleen
+UPDATE raportti
+   SET koodi = '#''harja.palvelin.raportointi.raportit.sanktio/suorita'
+ WHERE nimi = 'sanktioraportti-yllapito'
+   AND koodi = '#''harja.palvelin.raportointi.raportit.sanktioraportti-yllapito/suorita';
