f79fdde454c00b146d2a04e2796543ca6a057331
Markus Vähäkangas <markus.vahakangas@solita.fi>
2026-09-03 13:59:13 +0300

    Korjaa merge conflikti

--- tietokanta/src/main/resources/db/migration/V1_1287__.sql
+++ tietokanta/src/main/resources/db/migration/V1_1287__.sql
@@ -1,15 +1,4 @@
--- -24 alkavilla urakoilla oli virhe kattohinnan ylityksen siirron prosenttirajoituksessa. Se ei ollut niillä käytössä.
--- Korjataan se tässä.
-UPDATE urakka_parametrit up SET kattohintaylityksen_siirron_prosenttirajoitus = 0.03
-  FROM urakka u
- WHERE u.id = up.urakkaid
-   AND EXTRACT (YEAR FROM u.alkupvm) = 2024;
+ALTER TABLE tehtava
+ADD COLUMN "laske-api-maara-mukaan?" BOOLEAN;
 
--- -25 alkavilla urakoilla on virhe tavoitehinnan ylityksen maksuprosenteissa. Korjataan ne tässä
-UPDATE urakka_parametrit up
-SET tavoitehinnan_ylityksen_urakoitsijan_maksuprosentti = 75.00,
-    tavoitehinnan_ylityksen_tilaajan_maksuprosentti = 25.00
-FROM urakka u
-WHERE u.id = up.urakkaid
-  AND EXTRACT(YEAR FROM u.alkupvm) >= 2025
-  AND u.tyyppi = 'teiden-hoito';
+COMMENT ON COLUMN tehtava."laske-api-maara-mukaan?" is 'True, jos urakoitsijajärjestelmästä Harjaan tuotu, (yleensä) koneellisesti kerätty toteumamäärä lasketaan mukaan tehtävän toteuman yhteismäärään Harjan näkymässä Toteumat > Tehtävät.';
