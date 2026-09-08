ae4bfaab6b066f2818cd4b7f4ab8cf0b5ebfea4d
Samu Mikkonen <samu.mikkonen@solita.fi>
2026-09-08 15:56:42 +0300

    Paranna testin luotettavuutta

--- test/clj/harja/palvelin/palvelut/valikatselmus/valikatselmus_test.clj
+++ test/clj/harja/palvelin/palvelut/valikatselmus/valikatselmus_test.clj
@@ -569,8 +569,23 @@
         hoitokauden-loppupvm (pvm/hoitokauden-loppupvm (inc hoitokauden-alkuvuosi))
         kustannukset-jarjestettyna (valikatselmukset/hae-kustannukset-jarjestettyna (:db jarjestelma) urakka-id hoitokauden-alkuvuosi hoitokauden-alkupvm hoitokauden-loppupvm)
         toteutuneet-kustannukset (get-in kustannukset-jarjestettyna [:yhteensa :yht-toteutunut-summa])]
-        (is (= odotetut-paatokset-tavoitehinta-alittuu (get-in vastaus-tavoitehinnan-alitus [:paatokset])))
-        (is (= odotetut-paatokset-tavoitehinta-ylittyy (get-in vastaus-tavoitehinnan-ylitys [:paatokset])))
+        (doseq [[vastaus odotetut-paatokset]
+          [[vastaus-tavoitehinnan-alitus odotetut-paatokset-tavoitehinta-alittuu]
+           [vastaus-tavoitehinnan-ylitys odotetut-paatokset-tavoitehinta-ylittyy]]]
+          (let [pysyvat-muutokset (some :tavoitehinnan-pysyvat-muutokset (:paatokset vastaus))]
+            (is (every? #(some % (:paatokset vastaus))
+            (mapcat keys odotetut-paatokset))
+          "Vastauksessa ovat kaikki odotetut päätöstyypit")
+            (is (= (reduce + 0 (keep #(get pysyvat-muutokset %)
+                                     [:kirjallisesti_sovitut_muutokset
+                                      :pysyvat_muutokset
+                                      :muutostyo_muutokset
+                                      :johto_ja_hallintakorvaus_muutokset
+                                      :tehtava_ja_maaratoteumamuutokset
+                                      :rahavarausten_muutokset
+                                      :arvonvahennysten_muutokset]))
+                   (:tavoitehinnan_muutokset_yhteensa pysyvat-muutokset))
+                "Pysyvien muutosten yhteissummassa ovat mukana kaikki muutostyypit")))
         (is (some :tavoitehinnan-alitus (:paatokset vastaus-tavoitehinnan-alitus))
             "Tavoitehinnan alitus pitää palauttaa, kun kustannukset alittavat tavoitehinnan")
         (is (nil? (some :tavoitehinnan-ylitys (:paatokset vastaus-tavoitehinnan-alitus)))
