e2bdcb0bdd1e8e91fa94e8e6907be05458927004
Toni Dahl <toni.dahl@solita.fi>
2026-09-15 21:18:31 +0300

    Lisää type-hinttejä

--- src/clj/harja/palvelin/tyokalut/arkisto.clj
+++ src/clj/harja/palvelin/tyokalut/arkisto.clj
@@ -1,19 +1,20 @@
 (ns harja.palvelin.tyokalut.arkisto
-  (:require [clojure.java.io :as io]
-            [taoensso.timbre :as log])
-  (:import (java.util.zip ZipInputStream)
-           (org.apache.commons.compress.archivers.tar TarArchiveInputStream)
-           (org.apache.commons.compress.compressors.gzip GzipCompressorInputStream)
+  (:require [clojure.java.io :as io])
+  (:import (java.io File)
+           (java.nio.file Path)
+           (java.util.zip ZipEntry ZipInputStream)
+           (org.apache.commons.compress.archivers.tar TarArchiveEntry TarArchiveInputStream)
+           (org.apache.commons.compress.compressors.gzip GzipCompressorInputStream GzipCompressorInputStream$Builder)
            (org.apache.commons.io FilenameUtils))
   (:use [slingshot.slingshot :only [try+ throw+]]))
 
 (defn- turvallinen-kohdetiedosto
   "Muodostaa arkiston kohdetiedoston kohdekansion sisään turvallisesti.
    Heittää poikkeuksen, jos purku yrittäisi kirjoittaa kohdekansion ulkopuolelle (ns. Zip Slip)."
-  [kohdekansio kohdetiedosto-nimi]
-  (let [kansio (.getCanonicalFile (io/file kohdekansio))
-        kohdetiedosto (.getCanonicalFile (io/file kansio kohdetiedosto-nimi))]
-    (when-not (.startsWith (.toPath kohdetiedosto) (.toPath kansio))
+  ^File [kohdekansio kohdetiedosto-nimi]
+  (let [^File kansio (.getCanonicalFile (io/file kohdekansio))
+        ^File kohdetiedosto (.getCanonicalFile (io/file kansio kohdetiedosto-nimi))]
+    (when-not (.startsWith (.toPath kohdetiedosto) ^Path (.toPath kansio))
       (throw+ {:type :arkiston-purku-epaonnistui
                :error (str "Arkiston purku yrittää kirjoittaa kohdekansion ulkopuolelle: " kohdetiedosto-nimi)}))
     kohdetiedosto))
@@ -21,19 +22,19 @@
 (defn pura-zip-paketti [kohdetiedoston-polku]
   (let [kohdepolku (.getParent (io/file kohdetiedoston-polku))]
     (with-open [zip-virta (ZipInputStream. (io/input-stream kohdetiedoston-polku))]
-      (doseq [tiedosto (repeatedly #(.getNextEntry zip-virta)) :while tiedosto]
+      (doseq [^ZipEntry tiedosto (repeatedly #(.getNextEntry zip-virta)) :while tiedosto]
         (io/copy zip-virta (turvallinen-kohdetiedosto kohdepolku (.getName tiedosto)))))))
 
 (defn pura-gzip-paketti [kohdetiedoston-polku]
-  (let [kohdepolku (.getParent (io/file kohdetiedoston-polku))]
-    ;; Asetettu decompressConcatenated = true, jotta peräkkäiset GZIP-jäsenet puretaan varmasti
-    ;; (vastaa java.util.zip/GZIPInputStream-käytöstä)
-    (with-open [zip-virta (TarArchiveInputStream.
-                            (-> (GzipCompressorInputStream/builder)
-                                (.setInputStream (io/input-stream kohdetiedoston-polku))
-                                (.setDecompressConcatenated true)
-                                (.get)))]
-      (doseq [tiedosto (repeatedly #(.getNextEntry zip-virta)) :while tiedosto]
+  (let [kohdepolku (.getParent (io/file kohdetiedoston-polku))
+        ;; Asetettu decompressConcatenated = true, jotta peräkkäiset GZIP-jäsenet puretaan varmasti
+        ;; (vastaa java.util.zip/GZIPInputStream-käytöstä)
+        ^GzipCompressorInputStream$Builder gzip-builder (doto
+                                                          (GzipCompressorInputStream/builder)
+                                                          (.setInputStream (io/input-stream kohdetiedoston-polku))
+                                                          (.setDecompressConcatenated true))]
+    (with-open [zip-virta (TarArchiveInputStream. (.get gzip-builder))]
+      (doseq [^TarArchiveEntry tiedosto (repeatedly #(.getNextEntry zip-virta)) :while tiedosto]
         (io/copy zip-virta (turvallinen-kohdetiedosto kohdepolku (.getName tiedosto)))))))
 
 (defn pura-paketti [kohdetiedoston-polku]
@@ -43,5 +44,5 @@
       "shz" (pura-zip-paketti kohdetiedoston-polku)
       "gz" (pura-gzip-paketti kohdetiedoston-polku)
       "tgz" (pura-gzip-paketti kohdetiedoston-polku)
-      (throw+ {:type  :tuntematon-arkisto-tyyppi
+      (throw+ {:type :tuntematon-arkisto-tyyppi
                :error "Ei voida purkaa pakettia: " kohdetiedoston-polku ". Tuntematon tiedostotyyppi: " tiedostotyyppi "."}))))
